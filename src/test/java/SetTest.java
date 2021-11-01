9609848c224baf26048a48d081c59c015f3fac4f
changjin.park <pcjin118@gmail.com>
2021-11-02 01:09:48 +0900

    test: Set 테스트 작성

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set의 크기를 확인하기")
+    void size() {
+        int size = numbers.size();
+
+        assertThat(size).isEqualTo(3);
+    }
+
+    @DisplayName("Set에 1, 2, 3이 존재하는지 확인하기")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int number) {
+        assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @DisplayName("Set에 1, 2, 3, 4, 5가 존재하는지 확인하기")
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void containsTrueFalse(int number, boolean expectedResult) {
+        assertThat(numbers.contains(number)).isEqualTo(expectedResult);
+    }
+}
