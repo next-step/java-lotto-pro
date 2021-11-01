a4588d9cda43184bcd178afc307490201c8fc236
ch3224bin <ahrocksil@gmail.com>
2021-11-01 21:03:12 +0900

    test : String.substring()을 테스트 한다

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @DisplayName("문자열을 구분자로 split하면 나누어진 문자열 배열이 반환된다")
@@ -11,5 +12,13 @@ public class StringTest {
     void testSplit() {
         assertThat("1,2".split(",")).containsExactly("1", "2");
         assertThat("1,".split(",")).containsExactly("1");
+    }
+
+    @DisplayName("문자열을 주어진 범위(beginIndex, endIndex) 만큼 반환한다")
+    @Test
+    void testSubString() {
+        assertThat("(1,2)".substring(1, 4)).isEqualTo("1,2");
+        assertThatThrownBy(() -> "(1,2)".substring(5, 6)).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("begin 5, end 6, length 5");
     }
 }
