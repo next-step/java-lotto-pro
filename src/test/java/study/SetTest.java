8329566b26f658df4f38cf126989489ed950bbdb
cus4703 <cus4703@gmail.com>
2021-11-01 22:24:43 +0900

    test: Set Collection 요구사항1 테스트 케이스 생성

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,32 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("numbers 크기 확인")
+    public void numbers_크기_확인() throws Exception{
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+}
