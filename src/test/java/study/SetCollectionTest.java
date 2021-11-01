9f25a9518fd745309da7b9e507168e4698e5e4a0
박창혁 <changchang73@pionnet.co.kr>
2021-11-01 22:57:16 +0900

    test : set size() 메서드 확인

---
+++ src/test/java/study/SetCollectionTest.java
@@ -0,0 +1,30 @@
+package study;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SetCollectionTest {
+	private Set<Integer> numbers;
+
+	@BeforeEach
+	void setUp() {
+		numbers = new HashSet<>();
+		numbers.add(1);
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+	}
+
+	@DisplayName("set Size 확인")
+	@Test
+	void size() {
+		int result = numbers.size();
+		assertThat(result).isEqualTo(3);
+	}
+}
