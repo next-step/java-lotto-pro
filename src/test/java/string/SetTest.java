062a0587b8d75ff37c28808e041af546436001c1
shryu <>
2021-11-02 01:04:46 +0900

    feat : Set size Test 구현

---
+++ src/test/java/string/SetTest.java
@@ -0,0 +1,30 @@
+package string;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    Set numbers;
+
+    @BeforeEach
+    void setup(){
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("size 테스트")
+    void sizeTest(){
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+}
