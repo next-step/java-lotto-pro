629df6ee6c6cca6ed3bb31eea855e92c90736af6
Junhwan Kim <junhwan.tech@gmail.com>
2021-11-01 20:39:42 +0900

    test: Set contains 메소드 테스트 코드 작성

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -6,6 +6,8 @@ import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
 	private Set<Integer> numbers;
@@ -23,5 +25,22 @@ public class SetTest {
 	@DisplayName("size 메소드로 Set의 size를 확인 할 수 있다.")
 	void testSize() {
 		assertThat(numbers.size()).isEqualTo(3);
+	}
+
+	@Test
+	@DisplayName("contains 메소드로 값이 존재하지는 확인 할 수 있다.")
+	void testContains() {
+		assertThat(numbers.contains(1)).isTrue();
+		assertThat(numbers.contains(2)).isTrue();
+		assertThat(numbers.contains(3)).isTrue();
+
+		assertThat(numbers.contains(4)).isFalse();
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {1, 2, 3})
+	@DisplayName("ParameterizedTest를 활용해서 중복 코드를 줄일 수 있다.")
+	void testContainsUsingParameterizedTest(int input) {
+		assertThat(numbers.contains(input)).isTrue();
 	}
 }
