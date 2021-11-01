03056011409d826d1a1f7fc77545681fe8d47bcb
youngrock.choi <tbvjdudfhr@naver.com>
2021-11-02 01:33:53 +0900

    style: 리뷰 수정
    AssertionsForClassTypes.assertThat -> assertThat

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -32,7 +32,7 @@ public class StringTest {
     @CsvSource(value = {"0:a", "1:b", "2:c"}, delimiter = ':')
     void charAt(int index, char singleStr) {
         String str = "abc";
-        AssertionsForClassTypes.assertThat(str.charAt(index)).isEqualTo(singleStr);
+        assertThat(str.charAt(index)).isEqualTo(singleStr);
     }
 
     @DisplayName("String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트")
