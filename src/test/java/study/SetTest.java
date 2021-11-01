1289d03ea6ba017115c57c1d80a56acace36a502
wangkdk <31848426+wangkdk@users.noreply.github.com>
2021-11-01 22:08:01 +0900

    Step1 - 학습 테스트 실습 (#12)
    * test: string 클래스에 대한 학습 테스트
    
    * test: set collection 에 대한 학습 테스트

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,47 @@
+package study;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("set 크기 확인 테스트")
+    @Test
+    void size() {
+        assertEquals(3, numbers.size());
+    }
+
+    @DisplayName("set contains 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int number) {
+        assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @DisplayName("set contains 입력값에 따라 결과 값이 다른 경우 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void contains_dynamic_result(int number, boolean expected) {
+        assertEquals(expected, numbers.contains(number));
+    }
+}
