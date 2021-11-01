9d359b3b40b5fb71d082a323aabf44ba0ce576b6
lewisseo91 <togokr@naver.com>
2021-11-02 00:30:48 +0900

    feat(test): SetTest set 숫자 포함 테스트 추가
    SetTest set 숫자 포함 테스트 추가
    - 숫자 포함 테스트 추가

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,11 +3,14 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -31,5 +34,15 @@ public class SetTest {
 
         numbers.add(4);
         assertThat(numbers.size()).isEqualTo(4);
+    }
+
+    public boolean isContainsNumber(int number) {
+        return numbers.contains(number);
+    }
+
+    @ParameterizedTest(name = "Set 숫자 포함 테스트 {0}")
+    @ValueSource(ints = {1, 2, 3})
+    void set_contains_number(int number) {
+        assertTrue(isContainsNumber(number));
     }
 }
