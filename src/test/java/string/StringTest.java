f66518ab34959f4b3a33d690caaee105435942a3
shryu <>
2021-11-02 01:00:44 +0900

    feat : String charAt Test 작성

--- src/test/java/string/StringTest.java
+++ src/test/java/string/StringTest.java
@@ -1,8 +1,11 @@
 package string;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class StringTest {
@@ -21,5 +24,16 @@ public class StringTest {
     void substringTest() {
         String result = "(1,2)".substring(1, 4);
         assertThat(result).isEqualTo("1,2");
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"0, a", "1, b", "2, c"})
+    @DisplayName("문자열의 인덱스로 특정위치의 문자를 가져온다. 인덱스 범위를 넘을경우 에러가 발생한다.")
+    void charAtTest(int index, char result) {
+        assertAll(
+                () -> assertThat("abc".charAt(index)).isEqualTo(result),
+                () -> assertThatThrownBy(() ->
+                        "abc".charAt(3))
+                        .isInstanceOf(StringIndexOutOfBoundsException.class));
     }
 }
