80f96066b3411e575be200950411c3d56d7124f3
IW.MOON <mooniw02@gmail.com>
2021-11-02 00:34:19 +0900

    test: String contains 테스트

---
+++ src/test/java/string/StringTest.java
@@ -0,0 +1,30 @@
+package string;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringTest {
+
+    @DisplayName("String Split Contains 테스트 (Multi Value)")
+    @ParameterizedTest
+    @CsvSource(value = {"1,2"}, delimiter = ':')
+    public void splitContainsTest(String input) {
+
+        String[] splitInputArr = input.split(",");
+        assertThat(splitInputArr).contains(splitInputArr[0], splitInputArr[1]);
+
+    }
+
+    @DisplayName("String Split ContainsExactly 테스트 (Single Value)")
+    @ParameterizedTest
+    @CsvSource(value = {"1"})
+    public void splitContainsExactlyTest(String input) {
+
+        String[] splitInputArr = input.split(",");
+        assertThat(splitInputArr).containsExactly(input);
+
+    }
+}
