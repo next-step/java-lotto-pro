eab8c2304cd514086de7e09e3dc9d45b7edda080
shryu <>
2021-11-02 00:34:41 +0900

    feat : String substring Test 작성

--- src/test/java/string/StringTest.java
+++ src/test/java/string/StringTest.java
@@ -16,4 +16,10 @@ public class StringTest {
                 () -> assertThat(array1).contains("1","2"),
                 () -> assertThat(array2).containsExactly("1"));
     }
+
+    @Test
+    void substringTest() {
+        String result = "(1,2)".substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
 }
