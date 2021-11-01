59230a8056b977d125c0cd7e85041a5557e36ee7
keepbang <rlqud1125@naver.com>
2021-11-02 02:12:02 +0900

    test: set collection 학습 테스트 요구사항3 구현

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,6 +3,8 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -28,6 +30,16 @@ public class SetTest {
     @DisplayName("Set Collection size 테스")
     public void sizeTest(){
         assertThat(numbers.size()).isEqualTo(SET_SIZE);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "1,true", "2,true", "3,true",
+            "4,false","5,false"
+    },delimiter = ',')
+    @DisplayName("contains 검증")
+    public void containsTest(int number, boolean result){
+        assertThat(numbers.contains(number)).isEqualTo(result);
     }
 
 }
