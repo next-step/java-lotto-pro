ce0ad861f4cc7dbcc3dbcb80c4022bf0d8128ee3
Junhwan Kim <junhwan.tech@gmail.com>
2021-11-01 20:37:19 +0900

    test: Set size 메소드 테스트 코드 작성

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,27 @@
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SetTest {
+	private Set<Integer> numbers;
+
+	@BeforeEach
+	void setUp() {
+		numbers = new HashSet<>();
+		numbers.add(1);
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+	}
+
+	@Test
+	@DisplayName("size 메소드로 Set의 size를 확인 할 수 있다.")
+	void testSize() {
+		assertThat(numbers.size()).isEqualTo(3);
+	}
+}
