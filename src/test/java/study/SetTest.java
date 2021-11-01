84a6e7a772bf606b25f43bbd814b6ba4f83d834a
DESKTOP-HG83T1I\Chaser <a>
2021-11-02 02:34:27 +0900

    refactor(Test): 1단계-학습 테스트 실습에대한 테스트항목 그룹화
    - String에 대한 단위테스트를 String메소드별로 그룹화
    - Set에 대한 단위테스트를 Set메소드별로 그룹화

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -7,11 +7,13 @@ import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+@DisplayName("Set 자료구조에서")
 public class SetTest {
     private Set<Integer> numbers;
 
@@ -24,7 +26,7 @@ public class SetTest {
         numbers.add(3);
     }
 
-	@DisplayName("Set의 size()메소드를 통해 Set의 크기가 확인된다.")
+	@DisplayName("size()메소드를 활용해 크기가 확인된다.")
 	@Test
 	void size() {
 		// given
@@ -33,31 +35,35 @@ public class SetTest {
 		assertThat(numbers.size()).isEqualTo(3);
 	}
 
-	@ValueSource(ints = {1, 2, 3})
-	@DisplayName("Set의 Contains()메소드를 활용해 1,2,3의 값이 존재하는지 확인된다.")
-	@ParameterizedTest(name = "{index} => Set value [{0}] is contained")
-	void contains_onlyExistValues(int value) {
-		// given
+	@DisplayName("Contains()메소드를 활용해")
+	@Nested
+	class constains {
+		@ValueSource(ints = {1, 2, 3})
+		@DisplayName("1, 2, 3의 값이 존재하는지 확인된다.")
+		@ParameterizedTest(name = "{index} => Set value [{0}] is contained")
+		void onlyExistValues(int value) {
+			// given
 
-		// when
+			// when
 
-		// then
-		assertThat(numbers.contains(value)).isTrue();
-	}
+			// then
+			assertThat(numbers.contains(value)).isTrue();
+		}
 
-	@CsvSource({"1, true",
-				"2, true",
-				"3, true",
-				"4, false",
-				"5, false"})
-	@DisplayName(" 1, 2, 3값은 Contains()메소드의 결과값이 true, 4, 5값은 Contains()메소드 값이 false가 반환된다.")
-	@ParameterizedTest(name = "{index} => Contain boolean value of Set value [{0}]  is \"{1}\"")
-	void contains_existValuesAndNotExistValues(int value, boolean isContain) {
-		// given
+		@CsvSource({"1, true",
+					"2, true",
+					"3, true",
+					"4, false",
+					"5, false"})
+		@DisplayName(" 1, 2, 3값은 true, 4, 5값은 값은 false가 반환된다.")
+		@ParameterizedTest(name = "{index} => Contain boolean value of Set value [{0}]  is \"{1}\"")
+		void existValuesAndNotExistValues(int value, boolean isContain) {
+			// given
 
-		// when
+			// when
 
-		// then
-		assertThat(numbers.contains(value)).isEqualTo(isContain);
+			// then
+			assertThat(numbers.contains(value)).isEqualTo(isContain);
+		}
 	}
 }
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -5,62 +5,75 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+@DisplayName("String 자료형에서")
 public class StringTest {
-	@DisplayName("\"1,2\"을 ,로 split했을 때 1과 2로 분리된다.")
-	@Test
-	void split_multiItem() {
-		// given
-		String value = "1,2";
-		String seperator = ",";
+	@DisplayName("split()메소드을 활용해")
+	@Nested
+	class split {
+		@DisplayName("\"1,2\"을 ,로 분리했을 때 1과 2로 분리된다.")
+		@Test
+		void multiItem() {
+			// given
+			String value = "1,2";
+			String seperator = ",";
 
-		// when
-		String[] realValues = value.split(seperator);
+			// when
+			String[] realValues = value.split(seperator);
 
-		// then
-		assertThat(realValues).containsExactly("1", "2");
+			// then
+			assertThat(realValues).containsExactly("1", "2");
+		}
+
+		@DisplayName("\"1\"을 ,로 분리했을 때 1만 포함된 배열이 반환된다.")
+		@Test
+		void singleItem() {
+			// given
+			String value = "1";
+			String seperator = ",";
+
+			// when
+			String[] realValues = value.split(seperator);
+
+			// then
+			assertThat(realValues).containsExactly("1");
+		}
 	}
 
-	@DisplayName("\"1\"을 ,로 split했을 때 1만 포함된 배열이 반환된다.")
-	@Test
-	void split_singleItem() {
-		// given
-		String value = "1";
-		String seperator = ",";
+	@DisplayName("substring() 메소드을 활용해")
+	@Nested
+	class substring {
+		@DisplayName("\"(1,2)\"가 주어졌을 때 ()가 제거된 \"1,2\"가 반환된다.")
+		@Test
+		void removeParentheses() {
+			// given
+			String value = "(1,2)";
+			Integer parenthesesBeginIndex = value.indexOf("(") + 1;
+			Integer parenthesesEndIndex = value.indexOf(")");
 
-		// when
-		String[] realValues = value.split(seperator);
+			// when
+			String realValue = value.substring(parenthesesBeginIndex, parenthesesEndIndex);
 
-		// then
-		assertThat(realValues).containsExactly("1");
+			// then
+			String expectedValue = "1,2";
+			assertThat(realValue).isEqualTo(expectedValue);
+		}
 	}
 
-	@DisplayName("\"(1,2)\"가 주어졌을 때 String의 substring()메소드를 통해 ()가 제거된 \"1,2\"가 반환된다.")
-	@Test
-	void substring_removeParentheses() {
-		// given
-		String value = "(1,2)";
-		Integer parenthesesBeginIndex = value.indexOf("(") + 1;
-		Integer parenthesesEndIndex = value.indexOf(")");
-
-		// when
-		String realValue = value.substring(parenthesesBeginIndex, parenthesesEndIndex);
-
-		// then
-		String expectedValue = "1,2";
-		assertThat(realValue).isEqualTo(expectedValue);
-	}
-
-	@CsvSource({"0, 'a'",
-				"1, 'b'",
-				"2, 'c'"})
-	@DisplayName("\"abc\"값이 주어졌을 때 String의 chatAt()메소드를 통해 특정 문자를 가져오게된다.")
-	@ParameterizedTest(name = "{index} => charAt_{0} value is \"{1}\"")
-	void charAt(int index, char expectedValue) {
+	@DisplayName("chatAt()메소드을 활용해")
+	@Nested
+	class chatAt {
+		@CsvSource({"0, 'a'",
+					"1, 'b'",
+					"2, 'c'"})
+		@DisplayName("\"abc\"값이 주어졌을 때 특정 Index에있는 문자를 가져오게된다.")
+		@ParameterizedTest(name = "{index} => charAt_{0} value is \"{1}\"")
+		void getChar(int index, char expectedValue) {
 		// given
 		String value = "abc";
 
@@ -69,12 +82,12 @@ public class StringTest {
 
 		// then
 		assertThat(realValue).isEqualTo(expectedValue);
-	}
+		}
 
-	@ValueSource(ints = {Integer.MIN_VALUE, -10, -1, 5, 10, Integer.MAX_VALUE})
-	@DisplayName("임의 문자열에 대해 String의 chatAt()메소드를 통해 특정 위치의 문자를 가져올 때 위치를 벗어나면 StringIndexOutOfBoundsException이 발생된다.")
-	@ParameterizedTest(name = "{index} => index value [{0}] StringIndexOutOfBoundsException Throw")
-	void charAt_outOfRange(int index) {
+		@ValueSource(ints = {Integer.MIN_VALUE, -10, -1, 5, 10, Integer.MAX_VALUE})
+		@DisplayName("임의 문자열에 대해 특정 Index에있는 문자를 가져올 때 위치를 벗어나면 StringIndexOutOfBoundsException이 발생된다.")
+		@ParameterizedTest(name = "{index} => index value [{0}] StringIndexOutOfBoundsException Throw")
+		void outOfRange(int index) {
 		// given
 		String value = "abcde";
 
@@ -85,5 +98,6 @@ public class StringTest {
 		assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
 			.isThrownBy(exceptionContent)
 			.withMessageMatching("String index out of range: " + index);
+		}
 	}
 }
