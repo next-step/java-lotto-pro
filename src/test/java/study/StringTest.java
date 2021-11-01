aa47595a74a3b15358d876015044e97cfb97bc73
keepbang <rlqud1125@naver.com>
2021-11-02 02:01:19 +0900

    test: string 클래스 학습 테스트 요구사항1 구현

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,28 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringTest {
+
+    private static Stream<Arguments> splitTest() {
+        return Stream.of(
+                Arguments.of("1,2", new String[] {"1", "2"}),
+                Arguments.of("1", new String[] {"1"})
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    @DisplayName("문자열 나누기 검증")
+    public void splitTest(String input, String[] result){
+        assertThat(input.split(",")).containsExactly(result);
+    }
+
+}
