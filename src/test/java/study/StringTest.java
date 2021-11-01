28194faf09994d52a7ca54bb2ec19578e77ba01b
박창혁 <changchang73@pionnet.co.kr>
2021-11-01 22:30:57 +0900

    test : 1을 로 split 했을 때 1만을 포함하는 배열이 반환되는지 확인

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -8,7 +8,13 @@ public class StringTest {
 	@Test
 	void splitUnitContains() {
 		String[] result = "1,2".split(",");
-		
 		assertThat(result).contains("1","2");
 	}
+
+	@Test
+	void splitUnitContainsExactly() {
+		String[] result = "1,".split(",");
+		assertThat(result).containsExactly("1");
+	}
+
 }
