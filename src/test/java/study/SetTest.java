d5c71e471db12ea58e12a1855911ee05eb91fd74
이승민 <sml7179@naver.com>
2021-11-01 21:24:56 +0900

    test: set contains 메소드 검증

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,6 +3,9 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -25,5 +28,19 @@ public class SetTest {
     @Test
     void size() {
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @DisplayName("contains() 메소드 검증")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int num) {
+        assertThat(numbers.contains(num)).isTrue();
+    }
+
+    @DisplayName("contains() 메소드 결과값이 다를 때 검증")
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false", "6:false"}, delimiter = ':')
+    void containsTrueOrFalse(int num, boolean expected) {
+        assertThat(numbers.contains(num)).isEqualTo(expected);
     }
 }
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
+
 public class StringTest {
 
     @DisplayName("split 검증")
@@ -12,14 +13,14 @@ public class StringTest {
     void split() {
         String[] result = "1,2".split(",");
         assertThat(result).contains("1");
-        assertThat(result).containsExactly("1","2");
+        assertThat(result).containsExactly("1", "2");
     }
 
     @DisplayName("substring 검증")
     @Test
     void substring() {
         String input = "(1,2)";
-        String result = input.substring(1,4);
+        String result = input.substring(1, 4);
         assertThat(result).contains("1,2");
     }
 
@@ -27,7 +28,7 @@ public class StringTest {
     @Test
     void chatAt() {
         String input = "abc";
-        assertThatThrownBy(() ->{
+        assertThatThrownBy(() -> {
             input.charAt(4);
         }).isInstanceOf(StringIndexOutOfBoundsException.class)
                 .hasMessageContaining("String index out of range: 4");
