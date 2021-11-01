1e1bbbf2e8c800d862e21a072fc86620048b8f31
keepbang <rlqud1125@naver.com>
2021-11-02 02:02:41 +0900

    test: string 클래스 학습 테스트 요구사항3 구현

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,14 +1,17 @@
 package study;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
 
@@ -36,5 +39,25 @@ public class StringTest {
         assertThat(result).isEqualTo("1,2");
     }
 
+    @ParameterizedTest
+    @CsvSource(value = {
+            "abc,0,a","abc,2,c"
+    },delimiter = ',')
+    @DisplayName("한글자 뽑기 성공 테스트")
+    public void substringTest_ok(String input, int index, char findItem) {
+        assertThat(input.charAt(index)).isEqualTo(findItem);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "abc,3"
+    },delimiter = ',')
+    @DisplayName("한글자 뽑기 실패 테스트")
+    public void substringTest_fail(String input, int index) {
+        assertThatThrownBy(
+                () -> input.charAt(index)
+        ).isInstanceOf(StringIndexOutOfBoundsException.class)
+        .hasMessageContaining("String index out of range: 3");
+    }
 
 }
