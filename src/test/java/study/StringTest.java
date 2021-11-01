fd15778faf9b00ced10d36dfecf4acaba17b7c3d
박창혁 <changchang73@pionnet.co.kr>
2021-11-01 22:50:51 +0900

    chore : @DisplayName 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -7,18 +7,21 @@ import org.junit.jupiter.api.Test;
 
 public class StringTest {
 
+	@DisplayName("split 배열 값 확인")
 	@Test
 	void splitUnitContains() {
 		String[] result = "1,2".split(",");
 		assertThat(result).contains("1", "2");
 	}
 
+	@DisplayName("split 배열 값 완전 일치하는지 확인")
 	@Test
 	void splitUnitContainsExactly() {
 		String[] result = "1,".split(",");
 		assertThat(result).containsExactly("1");
 	}
 
+	@DisplayName("문자열 substring 동작 확인")
 	@Test
 	void substring() {
 		String data = "(1,2)";
@@ -26,6 +29,7 @@ public class StringTest {
 		assertThat(result).isEqualTo("1,2");
 	}
 
+	@DisplayName("String의 charAt() 메소드 확인")
 	@Test
 	void charAt() {
 		assertThat("abc".charAt(0)).isEqualTo('a');
@@ -33,6 +37,7 @@ public class StringTest {
 		assertThat("abc".charAt(2)).isEqualTo('c');
 	}
 
+	@DisplayName("String의 charAt() 메소드 IndexOutOfBoundsException 확인")
 	@Test
 	void stringIndexOutOfBoundsException() {
 		final String strTest = "abc";
