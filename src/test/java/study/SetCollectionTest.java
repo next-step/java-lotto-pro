b686b18b1e89a7b79e2e8070743f2b5b25a2a2e7
박창혁 <changchang73@pionnet.co.kr>
2021-11-01 23:09:22 +0900

    test : set contains 중복 제거 테스트

--- src/test/java/study/SetCollectionTest.java
+++ src/test/java/study/SetCollectionTest.java
@@ -1,13 +1,17 @@
 package study;
 
 import static org.assertj.core.api.AssertionsForClassTypes.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.HashSet;
 import java.util.Set;
 
+import org.assertj.core.util.Strings;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetCollectionTest {
 	private Set<Integer> numbers;
@@ -26,5 +30,22 @@ public class SetCollectionTest {
 	void size() {
 		int result = numbers.size();
 		assertThat(result).isEqualTo(3);
+	}
+
+	@DisplayName("contains 확인")
+	@Test
+	void contains() {
+		assertThat(numbers.contains(1)).isTrue();
+		assertThat(numbers.contains(2)).isTrue();
+		assertThat(numbers.contains(3)).isTrue();
+		assertThat(numbers.contains(4)).isFalse();
+		assertThat(numbers.contains(5)).isFalse();
+	}
+
+	@DisplayName("contains 중복 제거 후 확인")
+	@ParameterizedTest
+	@ValueSource(ints = {1, 2, 3})
+	void containsParameterized(int number){
+		assertThat(numbers.contains(number)).isTrue();
 	}
 }
