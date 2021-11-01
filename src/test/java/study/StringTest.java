267fb1fe469f218552a7d188626a508b7a48ca59
keepbang <rlqud1125@naver.com>
2021-11-02 02:02:19 +0900

    test: string 클래스 학습 테스트 요구사항2 구현

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,6 +1,7 @@
 package study;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -24,5 +25,16 @@ public class StringTest {
     public void splitTest(String input, String[] result){
         assertThat(input.split(",")).containsExactly(result);
     }
+
+    @Test
+    @DisplayName("괄호 제거 검증")
+    public void substringTest() {
+        String input = "(1,2)";
+
+        String result = input.substring(1,4);
+
+        assertThat(result).isEqualTo("1,2");
+    }
+
 
 }
