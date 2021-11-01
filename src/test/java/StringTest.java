151e8bdb0db3c32a85ac5270d891660bc4ce7c59
Junhwan Kim <junhwan.tech@gmail.com>
2021-11-01 20:22:43 +0900

    test: String charAt 메소드 테스트 코드 작성

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -28,4 +28,28 @@ public class StringTest {
 
 		assertThat(parenthesisRemoved).isEqualTo("1,2");
 	}
+
+	@Test
+	@DisplayName("charAt 메소드를 활용해 특정 위치의 문자를 가져올 수 있다.")
+	void testCharAt() {
+		String example = "abc";
+
+		assertThat(example.charAt(0)).isEqualTo('a');
+		assertThat(example.charAt(1)).isEqualTo('b');
+		assertThat(example.charAt(2)).isEqualTo('c');
+	}
+
+	@Test
+	@DisplayName("charAt 메소드를 사용할 때 위치값을 벗어나면 StringIndexOutObBoundsException 에외가 발생한다.")
+	void testCharAtThrowsException() {
+		String example = "abc";
+
+		assertThatThrownBy(() -> example.charAt(-1))
+			.isInstanceOf(StringIndexOutOfBoundsException.class)
+			.hasMessageContaining("String index out of range");
+
+		assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+			.isThrownBy(() -> example.charAt(4))
+			.withMessageMatching("String index out of range: \\d+");
+	}
 }
