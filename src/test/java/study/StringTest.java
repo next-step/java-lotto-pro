f69224739e39148fc5f99662b90468dc6a2907e1
devsigner <devsigner9920@icloud.com>
2021-11-01 22:26:27 +0900

    feat(StringTest): String 클래스 학습 테스트 수행
    추가내용:
    - String.substring, String.charAt 메소드를 사용한 학습 테스트를 수행하였습니다.
    - charAt 메소드를 사용한 학습 테스트 수행 시, ParameterizedTest를 이용하여 테스트를 수행하였습니다.
    - charAt 메소드를 사용한 학습 테스트 수행 시, AssertJ 에서 제공하는 여러 예외 테스트 방법을 활용하여 테스트를 수행하였습니다.

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,16 @@
 package study;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
   @Test
   @DisplayName("1,2을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인한다.")
-  public void split_learning_test_1() {
+  void split_learning_test_1() {
     // given
     String data = "1,2";
 
@@ -15,12 +18,12 @@ public class StringTest {
     String[] splitData = data.split(",");
 
     // then
-    Assertions.assertThat(splitData).contains("1", "2");
+    assertThat(splitData).contains("1", "2");
   }
 
   @Test
   @DisplayName("1을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지 확인한다.")
-  public void split_learning_test_2() {
+  void split_learning_test_2() {
     // given
     String data = "1";
 
@@ -28,6 +31,55 @@ public class StringTest {
     String[] splitData = data.split(",");
 
     // then
-    Assertions.assertThat(splitData).containsExactly("1");
+    assertThat(splitData).containsExactly("1");
+  }
+
+  @Test
+  @DisplayName("(1,2) 값이 주어졌을 때 substring() 메소드를 활용해 ()을 제거하고 1,2가 반환되는지 확인한다.")
+  void substring_learning_test() {
+    // given
+    String data = "(1,2)";
+
+    // when
+    String substringData = data.substring(1, 4);
+
+    // then
+    assertThat(substringData).isEqualTo("1,2");
+  }
+
+  @ParameterizedTest
+  @CsvSource(value = {"0:a", "1:b", "2:c"}, delimiter = ':')
+  @DisplayName("charAt 메소드를 활용해 특정 위치의 문자를 정상적으로 가져오는지 확인한다.")
+  void charAt_learning_test_1(int index, char expectedValue) {
+    // given
+    String data = "abc";
+
+    // when
+    char charAtData = data.charAt(index);
+
+    assertThat(charAtData).isEqualTo(expectedValue);
+  }
+
+  @Test
+  @DisplayName("charAt 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어날 경우 exception 발생을 확인한다.")
+  void charAt_learning_test_2() {
+    // given
+    String data = "abc";
+
+    // when
+    Throwable thrown = catchThrowable(() -> data.charAt(3));
+
+    // then
+    assertThat(thrown)
+      .isInstanceOf(StringIndexOutOfBoundsException.class)
+      .hasMessageContaining("String index out of range: 3");
+
+    assertThatThrownBy(() -> data.charAt(4))
+      .isInstanceOf(StringIndexOutOfBoundsException.class)
+      .hasMessageContaining("String index out of range: 4");
+
+    assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+      .isThrownBy(() -> data.charAt(5))
+      .withMessageMatching("String index out of range: 5");
   }
 }
