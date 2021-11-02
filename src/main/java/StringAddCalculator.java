21f64046bcdc94eadd5e58f1b5e3f753cc84bdd1
jerry92k <jens92.k@gmail.com>
2021-11-03 00:31:38 +0900

    feat(StringAddCalculator) : 문자열을 구분자로 분리하여 덧셈 기능 구현
    null 또는 빈문자의 text가 입려되면 항상 0 리턴

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -5,11 +5,17 @@ import java.util.regex.Pattern;
 
 public class StringAddCalculator {
 
+	private static final String basicDelimiters = ":|,";
+	private static final int SPLIT_CUSTOM_DELIMITER_INDEX = 1;
+	private static final int SPLIT_NUMBER_TEXT_INDEX = 2;
+
 	public static int splitAndSum(String text) {
 		if (isTextNullOrEmpty(text)) {
 			return 0;
 		}
-		return -1;
+		String[] splitedText = splitText(text);
+		int[] numbers = converToNumbers(splitedText);
+		return sumNumbers(numbers);
 	}
 
 	private static boolean isTextNullOrEmpty(String text) {
@@ -17,5 +23,55 @@ public class StringAddCalculator {
 			return true;
 		}
 		return false;
+	}
+
+	private static String[] splitText(String text) {
+		Matcher m = Pattern.compile("//(.*)\n(.*)").matcher(text);
+		if (hasCustomDelimiter(m)) {
+			String customDelimiter = m.group(SPLIT_CUSTOM_DELIMITER_INDEX);
+			return splitWithDelimiters(m.group(SPLIT_NUMBER_TEXT_INDEX), getDelimitersWithCustom(customDelimiter));
+		}
+		return splitWithDelimiters(text, basicDelimiters);
+	}
+
+	private static boolean hasCustomDelimiter(Matcher m) {
+		if (m.find()) {
+			return true;
+		}
+		return false;
+	}
+
+	private static String[] splitWithDelimiters(String text, String basicDelimiters) {
+		return text.split(basicDelimiters);
+	}
+
+	private static String getDelimitersWithCustom(String customDelimiter) {
+		return String.join("|", basicDelimiters, customDelimiter);
+	}
+
+	private static int[] converToNumbers(String[] splitedText) {
+		List<Integer> numbers = new ArrayList<>();
+		for (String letter : splitedText) {
+			int number = converToInteger(letter);
+			numbers.add(number);
+		}
+		return numbers.stream().mapToInt(number -> number).toArray();
+	}
+
+	private static Integer converToInteger(String letter) {
+		try {
+			int number = Integer.parseInt(letter);
+			return number;
+		} catch (NumberFormatException ex) {
+			throw new RuntimeException();
+		}
+	}
+
+	private static int sumNumbers(int[] numbers) {
+		int sum = 0;
+		for (int num : numbers) {
+			sum += num;
+		}
+		return sum;
 	}
 }
