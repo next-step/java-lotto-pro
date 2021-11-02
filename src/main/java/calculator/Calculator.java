19cba8a0f9c619be4aa269639471e73b9e53f864
shinmj <gkdlqkek@gmail.com>
2021-11-02 10:58:43 +0900

    feat,test: 예외처리 및 테스트케이스 추가
    - StringInput 클래스 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -5,68 +5,20 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Calculator {
-	private static final String DELIMITERS = ",|:";
-	private static final String CUSTOM_DELIMITER_BOUNDARY = "//(.*)\\n(.*)";
-	private static final String NUMBER_REG_EXP = "[0-9]+";
 
-	private String input;
+
+	private StringInput input;
 
 	public Calculator(String input) {
-		if (isNull(input) || isEmpty(input)) {
-			this.input = "0";
-			return;
-		}
-		validation(input);
-		this.input = input;
-	}
-
-	private void validation(String input) {
-		/**
-		 * @TODO
-		 *
-		 * 0, 양의 정수을 제외한 다른 값이 들어오는 경우 runtime exception
-		 */
-	}
-
-	private Matcher patternMatcher(String regexp, String input) {
-		return Pattern.compile(regexp).matcher(input);
+		this.input = new StringInput(input);
 	}
 
 	public int getSum() {
-		if (input.length() == 1) {
-			return Integer.parseInt(input);
-		}
-		String[] tokens = separate();
-		return sum(tokens);
+		return sum(input.separate());
 	}
 
 	private int sum(String[] numbers) {
 		return Arrays.stream(numbers).mapToInt(Integer::parseInt).sum();
 	}
 
-	private String[] separate() {
-		Matcher m = patternMatcher(CUSTOM_DELIMITER_BOUNDARY, input);
-		if (m.find()) {
-			return customDelimiterSeparate(m);
-		}
-		return input.split(DELIMITERS);
-	}
-
-	private String[] customDelimiterSeparate(Matcher matcher) {
-		return matcher.group(2).split(matcher.group(1));
-	}
-
-	private boolean isNull(String input) {
-		if (input == null) {
-			return true;
-		}
-		return false;
-	}
-
-	private boolean isEmpty(String input) {
-		if (input.isEmpty()) {
-			return true;
-		}
-		return false;
-	}
 }
--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -1,11 +1,16 @@
 package calculator;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class StringAddCalculatorTest {
+	private static final String EXCEPTION_MESSAGE = "양의 숫자만 입력 가능합니다.";
+
 	@Test
 	public void splitAndSum_null_또는_빈문자() {
 		int result = StringAddCalculator.splitAndSum(null);
@@ -44,4 +49,27 @@ class StringAddCalculatorTest {
 		assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
 			.isInstanceOf(RuntimeException.class);
 	}
+
+	@Test
+	public void splitAndSum_한글() throws Exception {
+	    assertThatThrownBy(() -> StringAddCalculator.splitAndSum("1,d2,3"))
+			.isInstanceOf(RuntimeException.class).hasMessage(EXCEPTION_MESSAGE);
+	}
+
+	@DisplayName("여러케이스 실패")
+	@ParameterizedTest
+	@ValueSource(strings = {"2,e,32", "-2", "test", "223220]", "2:3:-10"})
+	public void splitAndSum_여러케이스_실패(String input) throws Exception {
+		assertThatThrownBy(() -> StringAddCalculator.splitAndSum(input))
+			.isInstanceOf(RuntimeException.class).hasMessage(EXCEPTION_MESSAGE);
+	}
+
+	@DisplayName("여러케이스 성공")
+	@ParameterizedTest
+	@CsvSource(value = {"2,3,5;10", "4,10:7;21", "1;1", ";0"}, delimiter = ';')
+	public void splitAndSum_여러케이스_성공(String input, int expected) {
+		int result = StringAddCalculator.splitAndSum(input);
+		assertThat(result).isEqualTo(expected);
+	}
+
 }
