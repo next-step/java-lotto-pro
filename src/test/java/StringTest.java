76ff912dbaa9946796b7ef7d7381b4e2dbcb4e20
iia <kha2464@gmail.com>
2021-11-03 00:13:27 +0900

    test: "1"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지 확인

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -20,4 +20,20 @@ public class StringTest {
 			.hasSize(2)
 			.containsExactly("1", "2");
 	}
+
+	@DisplayName("구분자를 포함하지 않는 문자열 분리 테스트")
+	@Test
+	public void splitNotContainDelimilterTest() {
+		// given
+		String input = "1";
+		String delimiter = ",";
+
+		// when
+		String[] result = input.split(delimiter);
+
+		// then
+		assertThat(result)
+			.hasSize(1)
+			.containsExactly("1");
+	}
 }
