00e95dedac0400fd26226f3e8ae62506896674c3
lovethefeel <lovethefeel@gmail.com>
2021-11-03 02:16:53 +0900

    remove 사용자 정의 예외 처리 제거

--- src/main/java/util/StringAddCalculator.java
+++ src/main/java/util/StringAddCalculator.java
@@ -7,7 +7,7 @@ public class StringAddCalculator {
 
     public static final String COMMA = ",";
 
-    public static int splitAndSum(String str) throws MyOwnRuntimeException {
+    public static int splitAndSum(String str) {
         int result = 0;
         if( str == null || str.isEmpty() ) return result;
         if( str.length() == 1 ) return checkOneNumber(str);
@@ -22,23 +22,23 @@ public class StringAddCalculator {
         return Integer.parseInt(str);
     }
 
-    private static int checkComma(String str) throws MyOwnRuntimeException {
+    private static int checkComma(String str) {
         String[] token = str.split(COMMA);
         return addStringArray(token);
     }
 
-    private static int checkCommaAndDelimiter(String str) throws MyOwnRuntimeException {
+    private static int checkCommaAndDelimiter(String str) {
         String[] token = str.split(",|:");
         return addStringArray(token);
     }
 
-    private static int checkCustomDelimiter(Matcher m) throws MyOwnRuntimeException {
+    private static int checkCustomDelimiter(Matcher m) {
         String customDelimiter = m.group(1);
         String[] tokens= m.group(2).split(customDelimiter);
         return addStringArray(tokens);
     }
 
-    private static int addStringArray(String[] strs) throws MyOwnRuntimeException {
+    private static int addStringArray(String[] strs) {
         int result = 0;
         for(String str : strs) {
             result += checkInt(Integer.parseInt(str));
@@ -46,8 +46,8 @@ public class StringAddCalculator {
         return result;
     }
 
-    private static int checkInt(int number) throws MyOwnRuntimeException {
-        if( number < 0 ) throw new MyOwnRuntimeException();
+    private static int checkInt(int number) {
+        if( number < 0 ) throw new RuntimeException("For input only positive integers");
         return number;
     }
 
--- src/test/java/util/StringAddCalculatorTest.java
+++ src/test/java/util/StringAddCalculatorTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class StringAddCalculatorTest {
 
     @Test
-    void splitAndSum_null_또는_빈문자() throws MyOwnRuntimeException {
+    void splitAndSum_null_또는_빈문자() {
         int result = StringAddCalculator.splitAndSum("");
         assertThat(result).isZero();
 
@@ -18,25 +18,25 @@ class StringAddCalculatorTest {
     }
 
     @Test
-    void splitAndSum_숫자하나() throws MyOwnRuntimeException {
+    void splitAndSum_숫자하나() {
         int result = StringAddCalculator.splitAndSum("1");
         assertThat(result).isEqualTo(1);
     }
 
     @Test
-    void splitAndSum_쉼표구분자() throws MyOwnRuntimeException {
+    void splitAndSum_쉼표구분자() {
         int result = StringAddCalculator.splitAndSum("1,2");
         assertThat(result).isEqualTo(3);
     }
 
     @Test
-    void splitAndSum_쉼표_또는_콜론_구분자() throws MyOwnRuntimeException {
+    void splitAndSum_쉼표_또는_콜론_구분자() {
         int result = StringAddCalculator.splitAndSum("1,2:3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
-    void splitAndSum_custom_구분자() throws MyOwnRuntimeException {
+    void splitAndSum_custom_구분자() {
         int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
         assertThat(result).isEqualTo(6);
     }
