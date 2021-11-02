763d6efa8fb89650baa0119e3f81b18aadb9143c
slowin <irooori89@gmail.com>
2021-11-02 12:33:57 +0900

    feat : 기본 구분자 입력 받은 후 sum 계산 코드 추가

--- src/main/java/step2/StringAddCalculator.java
+++ src/main/java/step2/StringAddCalculator.java
@@ -1,11 +1,17 @@
 package step2;
 
+import java.util.Arrays;
+
 public class StringAddCalculator {
+    public static final String DEFAULT_DELIMITER_REGEX = "[,:]";
+
     public static int splitAndSum(String text) {
         if (text == null || text.isEmpty()) {
             return 0;
         }
 
-        return 1;
+        String[] numbers = text.split(DEFAULT_DELIMITER_REGEX);
+
+        return Arrays.stream(numbers).mapToInt(Integer::parseInt).sum();
     }
 }
