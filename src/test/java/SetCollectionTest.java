d97b6bccd823560ee6da183fcf256ce454d34160
codingknowjam <hanwoodww@gmail.com>
2021-11-03 07:35:42 +0900

    feat(SetCollectionTest): 요구사항2 테스트 코드 작성

--- src/test/java/SetCollectionTest.java
+++ src/test/java/SetCollectionTest.java
@@ -4,7 +4,10 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetCollectionTest {
 	private Set<Integer> numbers;
@@ -22,6 +25,13 @@ public class SetCollectionTest {
 	@Test
 	void sizeTest() {
 		assertThat(numbers).hasSize(3);
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {1, 2, 3})
+	@DisplayName("내부에_존재하는_값_확인")
+	void containsTest(int input) {
+		assertThat(numbers).contains(input);
 	}
 }
 
