795e06badee07be6dff183c8eac8e0831c168d7f
Kelicia91 <kelicia91@gmail.com>
2021-11-02 16:48:10 +0900

    test: Set.contains using JUnit.ParameterizedTest

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -4,7 +4,10 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
 	private Set<Integer> numbers;
@@ -21,5 +24,11 @@ public class SetTest {
 	@Test
 	void size() {
 		assertThat(numbers.size()).isEqualTo(3);
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {1, 2, 3})
+	void contains(int input) {
+		assertThat(numbers.contains(input)).isTrue();
 	}
 }
