b7e43563406d49a4483b9aa2c5cfb6eb701f0914
koola97620 <koola97620@nate.com>
2021-11-03 00:15:06 +0900

    feat: 빈 문자열 또는 null 입력시 0 반환한다

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,4 +1,11 @@
 package calculator;
 
 public class StringAddCalculator {
+    public static int splitAndSum(String input) {
+        if ("".equals(input) || input == null) {
+            return 0;
+        }
+
+        return 1;
+    }
 }
--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -1,8 +1,19 @@
 package calculator;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class StringAddCalculatorTest {
+
+    @DisplayName("빈 문자열이나 null 입력시 0을 반환한다")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void inputNullOrEmptyString(String input) {
+        assertThat(StringAddCalculator.splitAndSum(input)).isEqualTo(0);
+    }
 
 
 }
