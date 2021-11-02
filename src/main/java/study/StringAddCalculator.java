26b921a045123108f54a12fb1040d8cd338eb184
jongwoo <jongwoo.dev@gmail.com>
2021-11-03 00:07:07 +0900

    feat: calculate input string when custom delimiters are used
    사용자 정의 구분자를 사용한 수식 계산

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -2,14 +2,48 @@ package study;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringAddCalculator {
+	private static final Pattern patternDelimiter = Pattern.compile("(?<=//)(.*?)(?=\\n)");
+
+
 	public static int splitAndSum(String stringFormula) {
 		if (!validateInput(stringFormula)) {
 			return 0;
 		}
-		List<Integer> nubmers = splitStringFormula(stringFormula);
+		List<String> delimiters = getDelimiters(stringFormula);
+		String stringFormulaBody = getStringFormulaBody(stringFormula);
+		List<Integer> nubmers = splitStringFormula(stringFormulaBody, delimiters);
 		return sum(nubmers);
+	}
+
+	private static String getStringFormulaBody(String stringFormula) {
+		String[] splited = stringFormula.split("\\n");
+		if (splited.length == 1) {
+			return stringFormula;
+		}
+		return splited[1];
+	}
+
+	private static List<String> getDelimiters(String stringFormula) {
+		List<String> result = new ArrayList<>();
+		result.add(",");
+		result.add(":");
+		if (!stringFormula.contains("\n")) {
+			return result;
+		}
+		result.add(parseDelimiter(stringFormula));
+		return result;
+	}
+
+	private static String parseDelimiter(String stringFormula) {
+		Matcher matcher = patternDelimiter.matcher(stringFormula);
+		if (!matcher.find()) {
+			throw new IllegalArgumentException("invalid input string");
+		}
+		return matcher.group();
 	}
 
 	private static int sum(List<Integer> nubmers) {
@@ -20,12 +54,16 @@ public class StringAddCalculator {
 		return result;
 	}
 
-	private static List<Integer> splitStringFormula(String stringFormula) {
+	private static List<Integer> splitStringFormula(String stringFormulaBody, List<String> delimiters) {
 		List<Integer> result = new ArrayList<>();
-		for(String numberString : stringFormula.split(",|:")) {
+		for(String numberString : stringFormulaBody.split(makeRegexDelimiter(delimiters))) {
 			result.add(parseNumber(numberString));
 		}
 		return result;
+	}
+
+	private static String makeRegexDelimiter(List<String> delimiters) {
+		return String.join("|", delimiters);
 	}
 
 	private static Integer parseNumber(String numberString) {
