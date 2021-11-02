26d34bf15681f43f135b099055ad3bd7e53fcf0c
tyanotyrano <tyrano09@gmail.com>
2021-11-02 21:00:13 +0900

    feat(StringAddCalculator.java): 분리된 문자열이 0 이상의 숫자들로만 구성되어 있는지 유효성 체크
    분리된 문자열이 0 이상의 숫자들로만 구성되어 있는지 유효성 체크를 한다.
    - StringAddCalculator : 분리된 문자열이 0 이상의 숫자들로만 구성되어 있는지 유효성 체크 추가
    - ErrorMessage : 유효성 체크 통과 실패 시, 출력하는 에러 메세지
    - README.md : 완료된 기능 요구사항 체크

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,16 +1,30 @@
 package calculator;
 
+import java.util.Arrays;
+
+import calculator.constant.ErrorMessage;
 import calculator.utils.StringUtils;
 
 public class StringAddCalculator {
 	private static final int SUM_INIT_VALUE = 0;
+	private static final String POSITIVE_NUMBERS_REGEX = "^[0-9]+$";
 
 	public static int splitAndSum(String text) {
 		if (StringUtils.isEmpty(text)) {
 			return SUM_INIT_VALUE;
 		}
 
-		String[] splitInputs = StringSplitter.split(text);
+		String[] splitValues = StringSplitter.split(text);
+		validateConsistOfPositiveNumber(splitValues);
+
 		return SUM_INIT_VALUE;
+	}
+
+	private static void validateConsistOfPositiveNumber(String[] splitValues) {
+		boolean isAllPositiveNumbers = Arrays.stream(splitValues)
+											 .allMatch(input -> input.matches(POSITIVE_NUMBERS_REGEX));
+		if (!isAllPositiveNumbers) {
+			throw new RuntimeException(ErrorMessage.INVALID_STRING_INPUT);
+		}
 	}
 }
---
+++ src/main/java/calculator/constant/ErrorMessage.java
@@ -0,0 +1,5 @@
+package calculator.constant;
+
+public class ErrorMessage {
+	public static final String INVALID_STRING_INPUT = "[ERROR] 입력값에 숫자 이외의 값 또는 음수가 포함되어 있습니다.";
+}
