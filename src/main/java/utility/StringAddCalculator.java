a784e0e4761bbb0c2b556fafd920c593374969e5
devyonghee <devyonghee@gmail.com>
2021-11-03 02:44:00 +0900

    chore(StringAddCalculator.java) : fix typo

--- src/main/java/utility/StringAddCalculator.java
+++ src/main/java/utility/StringAddCalculator.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 public final class StringAddCalculator {
 
-	public static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
+	private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
 	private static final String COMMA_DELIMITER = ",";
 	private static final String COLON_DELIMITER = ":";
 
