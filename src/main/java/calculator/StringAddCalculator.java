cc995482e24529d607cf7e133bf1d6fcabcc62a6
youngrock.choi <tbvjdudfhr@naver.com>
2021-11-02 22:19:51 +0900

    feat: 두 숫자를 구분하고 합을 반환하는 기능 추가

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,18 +1,30 @@
 package calculator;
 
+import java.util.Arrays;
+
 public class StringAddCalculator {
+    public static final String DELIMITER = ",";
+
     public static int splitAndSum(String str) {
-        if(isBlank(str)) {
+        if (isBlank(str)) {
             return 0;
         }
-        return getParseInt(str);
+        return sum(toInts(split(str)));
     }
 
-    private static int getParseInt(String str) {
-        return Integer.parseInt(str);
+    private static int sum(int[] values) {
+        return Arrays.stream(values).sum();
     }
 
-    private static  boolean isBlank(String str) {
+    private static int[] toInts(String[] values) {
+        return Arrays.stream(values).mapToInt(Integer::parseInt).toArray();
+    }
+
+    private static String[] split(String str) {
+        return str.split(DELIMITER);
+    }
+
+    private static boolean isBlank(String str) {
         return str == null || str.isEmpty();
     }
 }
--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -2,8 +2,8 @@ package calculator;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,10 +15,17 @@ public class StringAddCalculatorTest {
         assertThat(StringAddCalculator.splitAndSum(input)).isEqualTo(0);
     }
 
-    @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한테스트")
+    @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환테스트")
     @ParameterizedTest
-    @ValueSource(strings = {"1"})
-    public void splitAndSum_숫자하나(String input) throws Exception {
-        assertThat(StringAddCalculator.splitAndSum(input)).isEqualTo(1);
+    @CsvSource({"1,1"})
+    public void splitAndSum_숫자하나(String input, int expected) throws Exception {
+        assertThat(StringAddCalculator.splitAndSum(input)).isEqualTo(expected);
+    }
+
+    @DisplayName("숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"1,2:3"}, delimiter = ':')
+    public void splitAndSum_쉼표구분자(String input, int expected) throws Exception {
+        assertThat(StringAddCalculator.splitAndSum(input)).isEqualTo(expected);
     }
 }
