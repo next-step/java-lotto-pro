655c5ff6e7ac84af09409cc32b668b3ca8082bb8
Kelicia91 <kelicia91@gmail.com>
2021-11-02 18:29:23 +0900

    fix: broken test(splitAndSum_null_또는_빈문자)

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,5 +1,14 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 public class StringAddCalculator {
 	public static int splitAndSum(String s) {
-		return 0;
+		if (isNullOrEmpty(s)) {
+			return 0;
+		}
+		throw new NotImplementedException();
+	}
+
+	private static boolean isNullOrEmpty(String s) {
+		return (null == s) || s.isEmpty();
 	}
 }
