60679591968b6b24c70c26fce512564a5b979231
slowin <irooori89@gmail.com>
2021-11-02 14:39:04 +0900

    feat : 기본구분자, 커스텀구분자 로직의 책임을 나누기 위해 메소드 분리

--- src/main/java/step2/StringAddCalculator.java
+++ src/main/java/step2/StringAddCalculator.java
@@ -27,6 +27,23 @@ public class StringAddCalculator {
         }).sum();
     }
 
+    public static String[] stringSplit(String input) {
+        Matcher matcher = CUSTOM_DELIMITER_REGEX.matcher(input);
+        if (matcher.find()) {
+            return splitDelimiterOfCustom(matcher);
+        }
+
+        return splitDelimiterOfDefault(input);
+    }
+
+    public static String[] splitDelimiterOfDefault(String input) {
+        return input.split(DEFAULT_DELIMITER_REGEX);
+    }
+
+    public static String[] splitDelimiterOfCustom(Matcher matcher) {
+        return matcher.group(2).split(matcher.group(1));
+    }
+
     private static void negativeExceptionValid(String value) {
         if (Integer.parseInt(value) < 0 || !isNumber(value)) {
             throw new RuntimeException();
