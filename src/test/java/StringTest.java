3249a1ce1becbbbe29ff7c0f23b839709c227d45
codingknowjam <hanwoodww@gmail.com>
2021-11-03 07:33:45 +0900

    feat(StringTest): 요구사항1 테스트 코드 작성

---
+++ src/test/java/StringTest.java
@@ -0,0 +1,19 @@
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class StringTest {
+	@Test
+	void splitTest(){
+		String[] stringArray = "1,2".split(",");
+		String[] stringOtherArray = "1".split(",");
+
+		assertThat(stringArray).contains("1","2");
+		assertThat(stringArray).containsExactly("1","2");
+		assertThat(stringArray).containsOnly("2", "1");
+
+		assertThat(stringOtherArray).contains("1");
+		assertThat(stringOtherArray).containsExactly("1");
+		assertThat(stringOtherArray).containsOnly("1");
+	}
+}
