988eff4600ad4494562a20d9e12be3481be768f6
Junhwan Kim <junhwan.tech@gmail.com>
2021-11-02 21:48:15 +0900

    refactor: 합계를 구하는 로직을 메소드로 분리

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -6,6 +6,11 @@ public class StringAddCalculator {
 			return 0;
 
 		String[] values = text.split(",");
+
+		return sum(values);
+	}
+
+	private static int sum(String[] values) {
 		int sum = 0;
 		for (String value: values) {
 			sum += Integer.parseInt(value);
