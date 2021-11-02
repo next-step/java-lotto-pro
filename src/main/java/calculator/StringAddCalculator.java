ba5dbe9e223996ec411ed12a2da966b561fc6310
youngrock.choi <tbvjdudfhr@naver.com>
2021-11-02 22:59:42 +0900

    feat: 커스텀 구분자 기능 추가

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,15 +1,28 @@
 package calculator;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringAddCalculator {
     public static final String DELIMITER = ",|:";
+    public static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
 
-    public static int splitAndSum(String str) {
-        if (isBlank(str)) {
+    public static int splitAndSum(String text) {
+        if (isBlank(text)) {
             return 0;
         }
-        return sum(toInts(split(str)));
+        return isCustomDelimiter(text);
+    }
+
+    private static int isCustomDelimiter(String text) {
+        Matcher m = CUSTOM_DELIMITER_PATTERN.matcher(text);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            String[] tokens = m.group(2).split(customDelimiter);
+            return sum(toInts(tokens));
+        }
+        return sum(toInts(split(text)));
     }
 
     private static int sum(int[] values) {
@@ -20,11 +33,11 @@ public class StringAddCalculator {
         return Arrays.stream(values).mapToInt(Integer::parseInt).toArray();
     }
 
-    private static String[] split(String str) {
-        return str.split(DELIMITER);
+    private static String[] split(String text) {
+        return text.split(DELIMITER);
     }
 
-    private static boolean isBlank(String str) {
-        return str == null || str.isEmpty();
+    private static boolean isBlank(String text) {
+        return text == null || text.isEmpty();
     }
 }
--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -2,12 +2,25 @@ package calculator;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringAddCalculatorTest {
+    public static Stream<Arguments> args() {
+        return Stream.of(
+                Arguments.of(
+                        "//;\n1;2;3",
+                        6
+                )
+        );
+    }
+
     @DisplayName("빈 문자열 또는 null 값을 입력할 경우 0을 반환테스트")
     @ParameterizedTest
     @NullAndEmptySource
@@ -33,6 +46,13 @@ public class StringAddCalculatorTest {
     @ParameterizedTest
     @CsvSource(value = {"1,2:3=6"}, delimiter = '=')
     public void splitAndSum_쉼표_또는_콜론_구분자(String input, int expected) throws Exception {
+        assertThat(StringAddCalculator.splitAndSum(input)).isEqualTo(expected);
+    }
+
+    @DisplayName("//와 \n 문자 사이에 커스텀 구분자를 지정할 경우 합을 반환테스트")
+    @ParameterizedTest
+    @MethodSource("args")
+    public void splitAndSum_custom_구분자(String input, int expected) throws Exception {
         assertThat(StringAddCalculator.splitAndSum(input)).isEqualTo(expected);
     }
 }
