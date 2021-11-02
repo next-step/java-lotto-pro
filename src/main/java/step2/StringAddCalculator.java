2dfe470f4418c92a34a27bc8d66a2af81b7c0d96
minho <lights8615@gmail.com>
2021-11-02 18:42:44 +0900

    feat(step2): 숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환

--- src/main/java/step2/StringAddCalculator.java
+++ src/main/java/step2/StringAddCalculator.java
@@ -9,6 +9,30 @@ public class StringAddCalculator {
             return Integer.parseInt(text);
         }
 
-        return 1;
+        return sum(parseToIntArray(split(text)));
     }
+
+    private static String[] split(String text) {
+        return text.split(",");
+    }
+
+    private static int[] parseToIntArray(String[] textNumbers) {
+        int[] numbers = new int[textNumbers.length];
+
+        for (int i = 0; i < textNumbers.length; i++) {
+            numbers[i] = Integer.parseInt(textNumbers[i]);
+        }
+
+        return numbers;
+    }
+
+    private static int sum(int[] numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+
+        return sum;
+    }
+
 }
