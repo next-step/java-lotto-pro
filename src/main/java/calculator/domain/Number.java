2986a29b954f163713d5a016119c01a3b6be93d1
keepbang <rlqud1125@naver.com>
2021-11-02 21:35:57 +0900

    refactor: 공백, null 입력시 0으로 반환

--- src/main/java/calculator/domain/Number.java
+++ src/main/java/calculator/domain/Number.java
@@ -4,25 +4,19 @@ public class Number {
     private int number;
 
     public Number(String input){
-        validation(input);
-        this.number = Integer.parseInt(input);
+        this.number = parsing(input);
     }
 
     private void validation(String input){
-        isNegative(input);
-        isInteger(input);
-    }
-
-    private void isNegative(String input) {
-        if(Integer.parseInt(input) < 0){
-            throw new RuntimeException("음수는 입력할 수 없습니다.");
-        }
-    }
-
-    private void isInteger(String input) {
         if(!input.chars().allMatch(Character::isDigit)){
             throw new RuntimeException("숫자만 입력할 수 있습니다.");
         }
+    }
+
+    private int parsing(String input){
+        if(input == null || input.trim().isEmpty()) return 0;
+        validation(input);
+        return Integer.parseInt(input);
     }
 
     public int getNumber() {
