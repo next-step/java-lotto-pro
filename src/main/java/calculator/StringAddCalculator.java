49bb7c8dfba7efc7f00ff7a48c25a78b56d4c6b1
youngrock.choi <tbvjdudfhr@naver.com>
2021-11-02 21:52:38 +0900

    feat: 숫자 하나를 문자열로 입력할 경우 해당 숫자 반환 기능 추가

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,21 +1,18 @@
 package calculator;
 
 public class StringAddCalculator {
-    public static int splitSum(String str) {
-        if(isNull(str)) {
+    public static int splitAndSum(String str) {
+        if(isBlank(str)) {
             return 0;
         }
-        if(isEmpty(str)) {
-            return 0;
-        }
-        return 1;
+        return getParseInt(str);
     }
 
-    private static boolean isNull(String str) {
-        return str == null;
+    private static int getParseInt(String str) {
+        return Integer.parseInt(str);
     }
 
-    private static boolean isEmpty(String str) {
-        return str.isEmpty();
+    private static  boolean isBlank(String str) {
+        return str == null || str.isEmpty();
     }
 }
--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -1,15 +1,24 @@
 package calculator;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringAddCalculatorTest {
     @DisplayName("빈 문자열 또는 null 값을 입력할 경우 0을 반환테스트")
     @ParameterizedTest
     @NullAndEmptySource
     void splitAndSum_null_또는_빈문자(String input) {
-        Assertions.assertThat(StringAddCalculator.splitSum(input)).isEqualTo(0);
+        assertThat(StringAddCalculator.splitAndSum(input)).isEqualTo(0);
+    }
+
+    @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"1"})
+    public void splitAndSum_숫자하나(String input) throws Exception {
+        assertThat(StringAddCalculator.splitAndSum(input)).isEqualTo(1);
     }
 }
