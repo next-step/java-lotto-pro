e23eadc152c0517231a2eccb9b94c55fc73c1b58
Kim Junhwan <junhwan.tech@gmail.com>
2021-11-03 00:43:43 +0900

    Step2 - 문자열 덧셈 계산기 (#69)
    * chore: step1 학습 테스트를 별도 패키지로 이동
    
    * feat: null 또는 빈문자일 때 0을 리턴하는 기능 추가
    
    * feat: 숫자 하나를 문자열로 입력할 경우 해당 숫자로 반환하는 기능 추가
    
    * feat: 숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환하는 기능 추가
    
    * refactor: 합계를 구하는 로직을 메소드로 분리
    
    * refactor: 합계를 구하는 메소드에서 int 배열로 변환하는 기능 분리
    
    * feat: 컴마(,)외에 콜론(:) 구분자를 사용할 수 있도록 수정
    
    * refactor: 디폴트 구분자를 상수로 지정
    
    * feat: 커스텀 구분자 기능 추가
    
    * refactor: 구분자로 문자열을 문자로 분리하는 로직 메소드로 분리
    
    * feat: 음수를 전달할 경우 RuntimeException이 발생하도록 기능 추가
    
    * refactor: 양수인지 validate check 하는 메소드로 분리

---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,65 @@
+package calculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+
+	private static final String DEFAULT_DELIMITER = ",|:";
+	private static final Pattern CUSTOM_DELIMITER = Pattern.compile("//(.)\n(.*)");
+
+	public static int splitAndSum(String text) {
+		if (isNullOrEmpty(text))
+			return 0;
+
+		String[] values = split(text);
+
+		int[] numbers = toInts(values);
+
+		return sum(numbers);
+	}
+
+	private static String[] split(String text) {
+		Matcher m = CUSTOM_DELIMITER.matcher(text);
+
+		if (m.find()) {
+			String customDelimiter = m.group(1);
+			return m.group(2).split(customDelimiter);
+		}
+
+		return text.split(DEFAULT_DELIMITER);
+	}
+
+	private static int[] toInts(String[] values) {
+		int[] numbers = new int[values.length];
+
+		for (int i = 0; i < values.length; i++) {
+			int number = Integer.parseInt(values[i]);
+
+			numbers[i] = validatePositiveNumber(number);
+		}
+
+		return numbers;
+	}
+
+	private static int validatePositiveNumber(int number) {
+		if (number < 0)
+			throw new RuntimeException();
+
+		return number;
+	}
+
+	private static int sum(int[] numbers) {
+		int sum = 0;
+
+		for (int number : numbers) {
+			sum += number;
+		}
+
+		return sum;
+	}
+
+	private static boolean isNullOrEmpty(String text) {
+		return text == null || text.isEmpty();
+	}
+}
---
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -0,0 +1,60 @@
+package calculator;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class StringAddCalculatorTest {
+	@Test
+	@DisplayName("입력이 null 또는 빈문자인 경우 0을 반환한다.")
+	public void splitAndSum_null_또는_빈문자() {
+		int result = StringAddCalculator.splitAndSum(null);
+		assertThat(result).isEqualTo(0);
+
+		result = StringAddCalculator.splitAndSum("");
+		assertThat(result).isEqualTo(0);
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"1", "2", "13"})
+	@DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.")
+	public void splitAndSum_숫자하나(String input) throws Exception {
+		int result = StringAddCalculator.splitAndSum(input);
+		assertThat(result).isEqualTo(Integer.parseInt(input));
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"1,2|3", "4,5|9", "13,2|15"}, delimiter = '|')
+	@DisplayName("숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.")
+	public void splitAndSum_쉼표구분자(String input, int expected) throws Exception {
+		int result = StringAddCalculator.splitAndSum(input);
+		assertThat(result).isEqualTo(expected);
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"1,2:3|6", "4,5:6|15", "2,7:3|12"}, delimiter = '|')
+	@DisplayName("구분자를 컴마(,) 이외에 콜론(:)을 사용할 수 있다.")
+	public void splitAndSum_쉼표_또는_콜론_구분자(String input, int expected) throws Exception {
+		int result = StringAddCalculator.splitAndSum(input);
+		assertThat(result).isEqualTo(expected);
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"//;\n1;2;3", "//#\n1#2#3", "//@\n1@2@3"})
+	@DisplayName("// 와 \n 문자 사이에 커스텀 구분자를 지정할 수 있다.")
+	public void splitAndSum_custom_구분자(String input) throws Exception {
+		int result = StringAddCalculator.splitAndSum(input);
+		assertThat(result).isEqualTo(6);
+	}
+
+	@Test
+	@DisplayName("음수를 전달할 경우 RuntimeException 예외가 발생한다.")
+	public void splitAndSum_negative() throws Exception {
+		assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
+			.isInstanceOf(RuntimeException.class);
+	}
+}
