144026dd3c974fca5ba1333033763a3bcdd90714
songseongbin <ppsosqq@gmail.com>
2021-11-02 22:45:07 +0900

    feat(StringAddCalculator): 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환
    - StringAddCalculator 수정
    - 테스트 추가

--- src/main/java/step2/StringAddCalculator.java
+++ src/main/java/step2/StringAddCalculator.java
@@ -1,10 +1,15 @@
 package step2;
 
 public class StringAddCalculator {
+	private static final String NUMBER_REGEX = "^[0-9]+$";
 
 	public static int splitAndSum(String input) {
 		if (input == null || input.isEmpty()) {
 			return 0;
+		}
+
+		if (input.matches(NUMBER_REGEX)) {
+			return Integer.parseInt(input);
 		}
 
 		return 0;
