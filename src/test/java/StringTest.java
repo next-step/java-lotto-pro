cae292de03b8e3c13ba50daee0d75d95259befc7
Kelicia91 <kelicia91@gmail.com>
2021-11-02 16:41:07 +0900

    test: String.charAt

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -1,5 +1,6 @@
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class StringTest {
@@ -13,5 +14,15 @@ public class StringTest {
 	void substring() {
 		final String s = "(1,2)";
 		assertThat(s.substring(1, s.length() - 1)).isEqualTo("1,2");
+	}
+
+	@Test
+	@DisplayName("charAt(): If the index argument is invalid, throw StringIndexOutOfBoundsException")
+	void charAt() {
+		final String s = "abc";
+		assertThat(s.charAt(0)).isEqualTo('a');
+		assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+			.isThrownBy(() -> s.charAt(s.length()))
+			.withMessageMatching("String index out of range: \\d+");
 	}
 }
