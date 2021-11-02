ce9490488a66942fd42689c53e3a9d860932fe9c
devyonghee <devyonghee@gmail.com>
2021-11-03 02:11:54 +0900

    feat(StringAddCalculator.java) : 문자열 덧셈 계산기 추가

---
+++ src/main/java/utility/StringAddCalculator.java
@@ -0,0 +1,27 @@
+package utility;
+
+import java.util.regex.Pattern;
+
+public final class StringAddCalculator {
+
+	public static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
+	private static final String COMMA_DELIMITER = ",";
+	private static final String COLON_DELIMITER = ":";
+
+	private StringAddCalculator() {
+		throw new AssertionError();
+	}
+
+	public static int splitAndSum(String target) {
+		return stringNumbers(target).sum();
+	}
+
+	private static StringNumbers stringNumbers(String target) {
+		StringDelimiterFinder finder = StringDelimiterFinder.of(CUSTOM_DELIMITER_PATTERN, target);
+		if (finder.hasDelimiter()) {
+			return StringNumberSeparator.from(StringDelimiters.of(finder.delimiter()))
+				.separate(finder.targetWithoutDelimiterPattern());
+		}
+		return StringNumberSeparator.from(StringDelimiters.of(COMMA_DELIMITER, COLON_DELIMITER)).separate(target);
+	}
+}
