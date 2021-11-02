be608e1023586b913d5daea52de0d663f87a5e67
Lewis Seo <togokr@naver.com>
2021-11-02 09:20:21 +0900

    1단계 - 학습 테스트 실습 (#31)
    * feat(test): StringTest split 테스트 추가
    
    String 관련 split multiple, single 테스트 추가
    - split multiple 테스트 추가
      - 요소 포함, 모든 요소 포함, 크기 확인
    - split single 테스트 추가
      - 요소 포함, 없는 요소 확인, 크기 확인
    
    * feat(test): StringTest substring 테스트 추가
    
    String 관련 substring 테스트 추가
    - substring 테스트 추가
      - 첫 번째 index, 마지막 index를 제외한 string 값 구한 뒤 확인
    
    * feat(test): StringTest charAt 테스트 추가
    
    String 관련 charAt_in_range, charAt_out_of_range 테스트 추가
    - charAt_in_range 테스트 추가
      - charAt 범위 안에 들어오는 경우 확인
    - charAt_out_of_range 테스트 추가
      - charAt 범위 밖일 경우 StringIndexOutOfBoundsException 처리 확인
    
    * feat(test): StringTest displayName 추가
    
    StringTest displayName 추가
    
    * feat(test): SetTest set 크기 테스트 추가
    
    SetTest set 크기 테스트 추가
    - 기존 크기 확인
    - 중복 숫자 추가 후 크기 확인
    - 비중복 숫자 추가 후 크기 증가 확인
    
    * feat(test): SetTest set 숫자 포함 테스트 추가
    
    SetTest set 숫자 포함 테스트 추가
    - 숫자 포함 테스트 추가
    
    * feat(test): SetTest set 숫자 범위 밖 포함 테스트 추가
    
    SetTest set 숫자 범위 밖 포함 테스트 추가
    - 숫자 범위 밖 포함 테스트 추가

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,58 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setup() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set 크기 테스트")
+    void set_size() {
+        assertThat(numbers.size()).isEqualTo(3);
+
+        numbers.add(3);
+        assertThat(numbers.size()).isEqualTo(3);
+
+        numbers.add(4);
+        assertThat(numbers.size()).isEqualTo(4);
+    }
+
+    public boolean isContainsNumber(int number) {
+        return numbers.contains(number);
+    }
+
+    @ParameterizedTest(name = "Set 숫자 포함 테스트 {0}")
+    @ValueSource(ints = {1, 2, 3})
+    void set_contains_number(int number) {
+        assertTrue(isContainsNumber(number));
+    }
+
+    @ParameterizedTest(name = "Set 숫자 범위 밖 포함 테스트 {0} : {1}")
+    @CsvSource(value = {"1,true", "2,true", "3,true", "4,false", "5,false"}, delimiter = ',')
+    void set_contains_number_include_out_of_range(String numberString, String expectFlagString) {
+        int number = Integer.parseInt(numberString);
+        boolean expect = Boolean.parseBoolean(expectFlagString);
+        assertEquals(isContainsNumber(number), expect);
+    }
+}
---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,60 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StringTest {
+    @Test
+    @DisplayName("문자열 split 복수개")
+    void split_multiple() {
+        String given = "1,2";
+        String[] when = given.split(",");
+        // then
+        assertThat(when).contains("1");
+        assertThat(when).containsExactly("1", "2");
+        assertEquals(when.length, 2);
+    }
+
+    @Test
+    @DisplayName("문자열 split 1개")
+    void split_single() {
+        String given = "1";
+        String[] when = given.split(",");
+        // then
+        assertThat(when).containsExactly("1");
+        assertThat(when).doesNotContain("2");
+        assertEquals(when.length, 1);
+    }
+
+    @Test
+    @DisplayName("문자열 substring")
+    void substring() {
+        String given = "(1,2)";
+        String when = given.substring(1, given.length()-1);
+        // then
+        assertThat(when).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("문자열 charAt 범위 안")
+    void charAt_in_range() {
+        String given = "abc";
+        char when = given.charAt(1);
+        // then
+        assertThat(when).isEqualTo('b');
+    }
+
+    @Test
+    @DisplayName("문자열 charAt 범위 밖 오류")
+    void charAt_out_of_range() {
+        String given = "abc";
+        assertThatThrownBy(()-> {
+            char when = given.charAt(given.length());
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+        .hasMessageContaining("out of range: " + given.length());
+    }
+}
