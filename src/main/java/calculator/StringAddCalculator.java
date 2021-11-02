c8e45735b3231ad783324e9a02020bff77116950
koola97620 <koola97620@nate.com>
2021-11-03 00:24:13 +0900

    feat: 숫자 하나를 문자열로 입력시 해당 숫자를 반환하는 기능 구현

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -6,6 +6,6 @@ public class StringAddCalculator {
             return 0;
         }
 
-        return 1;
+        return Integer.parseInt(input);
     }
 }
--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -3,6 +3,7 @@ package calculator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,8 +12,15 @@ class StringAddCalculatorTest {
     @DisplayName("빈 문자열이나 null 입력시 0을 반환한다")
     @ParameterizedTest
     @NullAndEmptySource
-    void inputNullOrEmptyString(String input) {
+    void givenNullOrEmptyString(String input) {
         assertThat(StringAddCalculator.splitAndSum(input)).isEqualTo(0);
+    }
+
+    @DisplayName("숫자 하나 입력시 해당 숫자를 반환한다")
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "3", "5", "7"})
+    void givenOneNumber(String input) {
+        assertThat(StringAddCalculator.splitAndSum(input)).isEqualTo(Integer.parseInt(input));
     }
 
 
