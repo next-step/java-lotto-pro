44a9b82e68041d7bca8dffa4248febd18a488cb7
donkey <rlaehdzlsla@gmail.com>
2021-11-02 14:32:36 +0900

    test: ParameterizedTest, ValueSource 어노테이션을 활용한 중복 코드 제거 테스트

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,6 +3,8 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -25,6 +27,13 @@ public class SetTest {
     @DisplayName("Set의 size 메소드로 Set의 크기를 확인")
     public void size() {
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("ParameterizedTest, ValueSource 어노테이션을 활용한 중복 코드 제거 테스트")
+    public void contains(int input) {
+        assertThat(numbers.contains(input)).isTrue();
     }
 
 }
