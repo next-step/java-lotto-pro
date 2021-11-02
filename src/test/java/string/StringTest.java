612242bc12e74e517255455523ad5972fa35b217
smpark1020 <smpark911020@gmail.com>
2021-11-02 16:57:56 +0900

    test: String 요구사항1

---
+++ src/test/java/string/StringTest.java
@@ -0,0 +1,33 @@
+package string;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StringTest {
+
+    @Test
+    void split_contains() {
+        // given
+        String input = "1,2";
+
+        // when
+        String[] splitResult = input.split(",");
+
+        // then
+        assertThat(splitResult).contains("1");
+        assertThat(splitResult).contains("2");
+    }
+
+    @Test
+    void split_contains_exactly() {
+        // given
+        String input = "1";
+
+        // when
+        String[] splitResult = input.split(",");
+
+        // then
+        assertThat(splitResult).containsExactly("1");
+    }
+}
