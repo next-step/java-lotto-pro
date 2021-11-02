1dff33106f8572aa8a55ddc8947dee147099b031
Junhwan Kim <junhwan.tech@gmail.com>
2021-11-02 20:50:11 +0900

    feat: null 또는 빈문자일 때 0을 리턴하는 기능 추가

---
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -0,0 +1,18 @@
+package calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringAddCalculatorTest {
+	@Test
+	@DisplayName("입력이 null 또는 빈문자인 경우 0을 반환한다.")
+	public void splitAndSum_null_또는_빈문자() {
+		int result = StringAddCalculator.splitAndSum(null);
+		assertThat(result).isEqualTo(0);
+
+		result = StringAddCalculator.splitAndSum("");
+		assertThat(result).isEqualTo(0);
+	}
+}
