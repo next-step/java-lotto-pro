349e680f960e83044f027a675c427c22131c0168
minho <lights8615@gmail.com>
2021-11-02 18:29:23 +0900

    feat(step2): 빈 문자열 또는 null 값을 입력할 경우 0을 반환

---
+++ src/main/java/step2/StringAddCalculator.java
@@ -0,0 +1,11 @@
+package step2;
+
+public class StringAddCalculator {
+    public static int splitAndSum(String str) {
+        if (str == null || str.isEmpty()) {
+            return 0;
+        }
+
+        return 1;
+    }
+}
