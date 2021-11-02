c3b22e0028edce0fc42315e7153b25962e6c3c44
HeeSeok Noh <nhs0912@nate.com>
2021-11-02 15:14:48 +0900

    refactor: 요구사항에 맞도록 수정
    - 상수로 나타내는 부분 상수로 설정
    - 상수에게 의미 부여하기
    - 불필요한 객제 생성하지 않기
    - static method로만 이루어진 클래스는 private 생성자 만들기

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -5,10 +5,18 @@ import java.util.regex.Pattern;
 
 public class StringAddCalculator {
     private static final int ZERO = 0;
+    private static final int CHECK_START_DELIMITER_INDEX = 0;
+    private static final int CUSTOM_DELIMITER_INDEX = 1;
+    private static final int CUSTOM_DELIMITER_INPUT_TEXT_INDEX = 2;
+    private static final Pattern customPattern = Pattern.compile("//(.)\n(.*)");
+    private static final String ONLY_NUMBER_REGULAR_EXPRESSION = "^[0-9]*$";
+    private static final String COMMA_AND_COLON = ",|:";
+
+    private StringAddCalculator() {}
 
     public static int splitAndSum(String inputNumberText) {
         if (isEmpty(inputNumberText)) {
-            return 0;
+            return ZERO;
         }
         return calculateNumber(inputNumberText);
     }
@@ -18,41 +26,42 @@ public class StringAddCalculator {
             return addNumberByCustomDelimiter(inputNumberText);
         }
 
-        String[] numbers = inputNumberText.split(",|:");
+        String[] numbers = inputNumberText.split(COMMA_AND_COLON);
         return addNumber(numbers);
-
     }
 
     private static int addNumberByCustomDelimiter(String inputNumberText) {
-        // java.util.regex 패키지의 Matcher, Pattern import
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(inputNumberText);
+        Matcher m = customPattern.matcher(inputNumberText);
         if (m.find()) {
-            String customDelimiter = m.group(1);
-            String[] tokens = m.group(2).split(customDelimiter);
+            String customDelimiter = m.group(CUSTOM_DELIMITER_INDEX);
+            String[] tokens = m.group(CUSTOM_DELIMITER_INPUT_TEXT_INDEX).split(customDelimiter);
             return addNumber(tokens);
         }
-        throw new RuntimeException("잘못된 문자를 입력하였습니다");
+        throw new RuntimeException("잘못된 문자를 입력하였습니다.(숫자만 입력가능)");
     }
 
     private static boolean isCustomDelimeter(String inputNumberText) {
-        return inputNumberText.charAt(ZERO) == '/';
+        return inputNumberText.charAt(CHECK_START_DELIMITER_INDEX) == '/';
     }
 
     private static int addNumber(String[] numbers) {
-        isWrongNumber(numbers);
-        int sum = 0;
+        checkWrongNumber(numbers);
+        int sum = ZERO;
         for (String number : numbers) {
             sum += Integer.parseInt(number);
         }
         return sum;
     }
 
-    private static void isWrongNumber(String[] numbers) {
-        String regularExpression = "^[0-9]*$";
+    private static void checkWrongNumber(String[] numbers) {
         for (String number : numbers) {
-            if (!Pattern.matches(regularExpression, number)) {
-                throw new RuntimeException("잘못된 문자를 입력하였습니다.");
-            }
+            findWrongNumber(number);
+        }
+    }
+
+    private static void findWrongNumber(String number) {
+        if (!Pattern.matches(ONLY_NUMBER_REGULAR_EXPRESSION, number)) {
+            throw new RuntimeException("잘못된 문자를 입력하였습니다.(숫자만 입력가능)");
         }
     }
 
--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -8,6 +8,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringAddCalculatorTest {
     @Test
+    @DisplayName("null or 빈문자는 0이 나오는지 테스트")
     public void splitAndSum_null_또는_빈문자() {
         int result = StringAddCalculator.splitAndSum(null);
         assertThat(result).isEqualTo(0);
@@ -17,37 +18,42 @@ public class StringAddCalculatorTest {
     }
 
     @Test
+    @DisplayName("숫자 하나를 더하는 테스트")
     public void splitAndSum_숫자하나() throws Exception {
         int result = StringAddCalculator.splitAndSum("1");
         assertThat(result).isEqualTo(1);
     }
 
     @Test
+    @DisplayName("숫자를 쉼표(,)로 구분해서 더하는 테스트")
     public void splitAndSum_쉼표구분자() throws Exception {
         int result = StringAddCalculator.splitAndSum("1,2");
         assertThat(result).isEqualTo(3);
     }
 
     @Test
+    @DisplayName("숫자를 쉼표(,)와 콜론(:)으로 구분해서 더하는 테스트")
     public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
         int result = StringAddCalculator.splitAndSum("1,2:3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
+    @DisplayName("숫자를 custom구분자를 설정한 다음 그 구분자로 구분해서 더하는 테스트")
     public void splitAndSum_custom_구분자() throws Exception {
         int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
+    @DisplayName("음수 숫자가 있으면 에러발생하는 테스트")
     public void splitAndSum_negative() throws Exception {
         assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
                 .isInstanceOf(RuntimeException.class);
     }
 
     @Test
-    @DisplayName("잘못된 문자 입력")
+    @DisplayName("잘못된 문자 입력하면 에러발생하는 테스트")
     public void splitAndSum_wrongText() throws Exception {
         assertThatThrownBy(() -> StringAddCalculator.splitAndSum("&,2,3"))
                 .isInstanceOf(RuntimeException.class);
