05e4af8d8208a40887a8eceeff9560bc897009eb
jongwoo <jongwoo.dev@gmail.com>
2021-11-02 23:16:13 +0900

    feat: calculate input string when comma separated
    입력값이 콤마로 구분된 숫자들일때 계산기능

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -1,11 +1,35 @@
 package study;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringAddCalculator {
 	public static int splitAndSum(String stringFormula) {
 		if (!validateInput(stringFormula)) {
 			return 0;
 		}
-		return Integer.parseInt(stringFormula);
+		List<Integer> nubmers = splitStringFormula(stringFormula);
+		return sum(nubmers);
+	}
+
+	private static int sum(List<Integer> nubmers) {
+		int result = 0;
+		for(int number : nubmers) {
+			result += number;
+		}
+		return result;
+	}
+
+	private static List<Integer> splitStringFormula(String stringFormula) {
+		List<Integer> result = new ArrayList<>();
+		for(String numberString : stringFormula.split(",")) {
+			result.add(parseNumber(numberString));
+		}
+		return result;
+	}
+
+	private static Integer parseNumber(String numberString) {
+		return Integer.parseInt(numberString);
 	}
 
 	private static boolean validateInput(String stringFormula) {
