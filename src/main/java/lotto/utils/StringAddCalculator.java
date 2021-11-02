fe19f48c288bf50e5d7c2d388d7e38d97012b278
cus4703 <cus4703@gmail.com>
2021-11-02 22:53:22 +0900

    feat: StringAddCalculator 최종 생성

---
+++ src/main/java/lotto/utils/StringAddCalculator.java
@@ -0,0 +1,76 @@
+package lotto.utils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import static lotto.constants.CalculatorConstants.*;
+
+public class StringAddCalculator {
+
+    private static Pattern pattern = Pattern.compile("//(.)\n(.*)");
+
+    public void stringCalculator(String inputString) {
+        int sum = getNumberList(inputString);
+        System.out.println("합산 : " + sum);
+    }
+
+    public int getNumberList(String inputString) {
+        String args = getCheckInputString(inputString);
+        List<Integer> list = splitInputString(args);
+        return list.stream().mapToInt(Integer::intValue).sum();
+    }
+
+    public String getCheckInputString(String inputString) {
+        if (inputString == null || inputString.isEmpty()) {
+            return "0";
+        }
+        return inputString;
+    }
+
+    public List<Integer> splitInputString(String inputString) {
+        List<String> result = getSplitList(inputString);
+        validateNumberFormat(result);
+        return result
+                .stream()
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+
+    private List<String> getSplitList(String inputString) {
+        Matcher matcher = pattern.matcher(inputString);
+        if (matcher.find()) {
+            return getCustomRegexSplitList(matcher);
+        }
+        return getDefaultRegexSlitList(inputString);
+    }
+
+    private List<String> getCustomRegexSplitList(Matcher matcher) {
+        String customRegex = matcher.group(1);
+        String remainderString = matcher.group(2);
+        return Arrays
+                .asList(remainderString.split(customRegex));
+    }
+
+    private List<String> getDefaultRegexSlitList(String matcher) {
+        return Arrays
+                .asList(matcher.split(DEFAULT_SPLIT_REGEX));
+    }
+
+    public void validateNumberFormat(List<String> strings) {
+        String joinString = String.join(",", strings);
+        if (!isNumber(joinString)) {
+            System.out.println("[ERROR] 잘못된 입력 값입니다.");
+            throw new RuntimeException("[ERROR] 잘못된 입력 값입니다.");
+        }
+    }
+
+    public boolean isNumber(String checkValue) {
+        return checkValue.matches("^[0-9]+$");
+    }
+
+}
