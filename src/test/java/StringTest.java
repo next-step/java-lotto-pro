e0c0b2a08a58a653ffe363faf6a504513ff72a41
JiHun Hong <hongji3354@gmail.com>
2021-11-02 20:10:47 +0900

    refactor : 불필요한 접근제어자 제거

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
-public class StringTest {
+class StringTest {
 
     @Test
     void splitTest() {
