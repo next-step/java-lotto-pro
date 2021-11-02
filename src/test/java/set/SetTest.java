45584fd46b7a605e9c02446dead2b4b9f99ae2c1
smpark1020 <smpark911020@gmail.com>
2021-11-02 22:43:07 +0900

    test: Set 요구사항1

---
+++ src/test/java/set/SetTest.java
@@ -0,0 +1,34 @@
+package set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set의 크기를 확인하는 테스트")
+    void size() {
+        // when
+        int size = numbers.size();
+
+        // then
+        assertThat(size).isEqualTo(3);
+    }
+}
