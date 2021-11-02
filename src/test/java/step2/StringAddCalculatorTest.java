7f7e1bc5209da2eadad3b618334d4b8512138407
slowin <irooori89@gmail.com>
2021-11-02 12:27:06 +0900

    feat : StringAddCalculator.splitAndSum null or 빈문자열 입력시 0 리턴 구현 및 테스트 코드 작성
    테스트 코드
    - splitAndSum_null_또는_빈문자
    - splitAndSum_숫자하나

--- src/test/java/step2/StringAddCalculatorTest.java
+++ src/test/java/step2/StringAddCalculatorTest.java
@@ -1,4 +1,28 @@
 package step2;
 
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
 public class StringAddCalculatorTest {
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("StringAddCalculator.splitAndSum 의 null 또는 빈문자 입력시 0 리턴")
+    public void splitAndSum_null_또는_빈문자(String input) {
+        int result = StringAddCalculator.splitAndSum(input);
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("문자 길이 1일때 그대로 반환, \"1\" 문자열 입력시 1을 반환")
+    public void splitAndSum_숫자하나() {
+        int result = StringAddCalculator.splitAndSum("1");
+        assertThat(result).isEqualTo(1);
+    }
+
 }
