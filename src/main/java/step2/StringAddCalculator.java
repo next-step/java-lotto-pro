1f528384747b57025c212a28fc0b7fd403ae9fcf
slowin <irooori89@gmail.com>
2021-11-02 22:07:09 +0900

    refactor : 비교문을 함수로 분리

--- src/main/java/step2/StringAddCalculator.java
+++ src/main/java/step2/StringAddCalculator.java
@@ -4,12 +4,16 @@ import java.util.Arrays;
 
 public class StringAddCalculator {
     public static int splitAndSum(String text) {
-        if (text == null || text.isEmpty()) {
+        if (isBlank(text)) {
             return 0;
         }
 
         return Arrays.stream(SplitDelimiter.stringSplit(text))
             .mapToInt(SplitNumber::valueOf)
             .sum();
+    }
+
+    private static boolean isBlank(String text) {
+        return text == null || text.isEmpty();
     }
 }
