d6cbf219b749ce1eab4c4b5c489d26088c74f018
Chloe <page.prologue@gmail.com>
2021-11-02 19:04:14 +0900

    refactor : 접근제어자 수정

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -8,7 +8,7 @@ import java.util.regex.Pattern;
 public class StringAddCalculator {
 
     private static final String[] DELIMITER = {",", ":"};
-    public static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
 
     private StringAddCalculator() {
     }
