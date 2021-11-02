7f86380a3193a65a2eab0b72e552f1eea33031cc
Kelicia91 <kelicia91@gmail.com>
2021-11-02 18:53:50 +0900

    fix: broken test(splitAndSum_숫자하나, 쉼표 구분자, 쉼표 또는 콜론 구분자)

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,14 +1,32 @@
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 public class StringAddCalculator {
+
+	private final static String DEFAULT_DELIMITER = ",|:";
+
 	public static int splitAndSum(String s) {
 		if (isNullOrEmpty(s)) {
 			return 0;
 		}
-		throw new NotImplementedException();
+		return sum(parse(s, DEFAULT_DELIMITER));
 	}
 
 	private static boolean isNullOrEmpty(String s) {
 		return (null == s) || s.isEmpty();
+	}
+
+	private static int[] parse(String s, String delimiter) {
+		final String[] tokens = s.split(delimiter);
+		final int[] parsedTokens = new int[tokens.length];
+		for (int i = 0; i < tokens.length; ++i) {
+			parsedTokens[i] = Integer.parseInt(tokens[i]);
+		}
+		return parsedTokens;
+	}
+
+	private static int sum(int[] numbers) {
+		int sum = 0;
+		for (int number : numbers) {
+			sum += number;
+		}
+		return sum;
 	}
 }
