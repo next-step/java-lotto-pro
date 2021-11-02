6390ef7907ef95fbf9b9210dff0a8168766c5b52
eedys <eedys@ex-em.com>
2021-11-02 22:06:36 +0900

    String 클래스 학습 테스트

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -1,13 +1,37 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
 
+    @DisplayName("구분자 ,로 String Split 테스트")
     @Test
-    public void SplitTest() {
-
+    public void StringSplitByRestTest() {
         String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1", "2");
     }
+
+    @DisplayName("괄호 (, ) 을 제거 테스트")
+    @Test
+    public void extractBracketTest() {
+        String result = "(1,2)".substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @DisplayName("String의 chatAt 메서드 테스트")
+    @Test
+    public void charAtMethodTest() {
+        String input = "abc";
+        assertThat(input.charAt(0)).isEqualTo('a');
+        assertThat(input.charAt(1)).isEqualTo('b');
+        assertThat(input.charAt(2)).isEqualTo('c');
+        assertThatThrownBy(() -> input.charAt(3))
+                .isInstanceOf(StringIndexOutOfBoundsException.class);
+    }
+
+
+
+
 }
