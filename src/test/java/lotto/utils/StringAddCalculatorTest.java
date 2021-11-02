fe0a7abc23537f9ff73de5b2eee4257086d65c0f
cus4703 <cus4703@gmail.com>
2021-11-02 22:15:06 +0900

    test: StringAddCalculatorTest 테스트 케이스 추가
    1. 숫자 형식 검증

--- src/test/java/lotto/utils/StringAddCalculatorTest.java
+++ src/test/java/lotto/utils/StringAddCalculatorTest.java
@@ -1,16 +1,20 @@
 package lotto.utils;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class StringAddCalculatorTest {
 
@@ -42,5 +46,32 @@ public class StringAddCalculatorTest {
     }
 
 
+    static Stream<Arguments> listProvide() {
+        List<String> strings = Arrays.asList("1", "12", "2", "3", "4", "1");
+        return Stream.of(arguments(strings));
+    }
+
+    @ParameterizedTest
+    @MethodSource("listProvide")
+    @DisplayName("숫자를 제외한 값 또는 음수 값이 있는지 검증")
+    public void validationNumber(List<String> strings) {
+        assertThatThrownBy(() -> {
+            validateNumberFormat(strings);
+        }).isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+
+
+    public void validateNumberFormat(List<String> strings) {
+        String joinString = String.join(",", strings);
+        if (!isNumber(joinString)) {
+            System.out.println("[ERROR] 잘못된 입력 값입니다.");
+            throw new RuntimeException("[ERROR] 잘못된 입력 값입니다.");
+        }
+    }
+
+    public boolean isNumber(String checkValue) {
+        return checkValue.matches("^[0-9]+$");
+    }
 
 }
