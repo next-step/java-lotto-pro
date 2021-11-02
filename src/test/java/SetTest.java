4ad38312ccbb0989b950e659142359e39a5d27f1
JiHunHong <hongji3354@gmail.com>
2021-11-02 12:50:24 +0900

    test : set collection size Test

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -1,7 +1,10 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SetTest {
 
@@ -14,6 +17,11 @@ public class SetTest {
         numbers.add(1);
         numbers.add(2);
         numbers.add(3);
+    }
+
+    @Test
+    void sizeTest() {
+        assertThat(numbers.size()).isEqualTo(3);
     }
 
 }
