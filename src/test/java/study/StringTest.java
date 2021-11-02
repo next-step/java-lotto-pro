56f2d20b0bc8d8c2134d62f65d7a049acdd888e9
김도영 <95doyoung@gmail.com>
2021-11-02 16:39:45 +0900

    refactor: split test
    - "1,2" 문자열을 변수로 추출

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,15 +1,22 @@
 package study;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
 
     @Test
     void split() {
-        final String[] result = "1,2".split(",");
-        Assertions.assertThat(result).contains("1");
-        Assertions.assertThat(result).contains("2");
-        Assertions.assertThat(result).containsExactly("1", "2");
+        // given
+        final String text = "1,2";
+        // when
+        final String[] result = text.split(",");
+        // then
+        assertThat(result).contains("1");
+        assertThat(result).contains("2");
+        assertThat(result).containsExactly("1", "2");
     }
+
+  
 }
