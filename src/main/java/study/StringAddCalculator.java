5d2e6770f45401a1d1a8821926de1aa6bf0acbea
jongwoo <jongwoo.dev@gmail.com>
2021-11-02 23:01:03 +0900

    feat: validate input string formula
    입력 수식 문자열의 유효성을 체크하는 기능

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -2,6 +2,16 @@ package study;
 
 public class StringAddCalculator {
 	public static int splitAndSum(String stringFormula) {
+		if (!validateInput(stringFormula)) {
+			return 0;
+		}
 		return 0;
+	}
+
+	private static boolean validateInput(String stringFormula) {
+		if (stringFormula == null || "".equals(stringFormula)) {
+			return false;
+		}
+		return true;
 	}
 }
