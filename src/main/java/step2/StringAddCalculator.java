caa0d260be78e7799c67ff63ff6d2cbe56d0e6e5
minho <lights8615@gmail.com>
2021-11-02 18:45:47 +0900

    feat(step2): “//”와 “\n” 문자 사이에 커스텀 구분자를 지정 가능

--- src/main/java/step2/StringAddCalculator.java
+++ src/main/java/step2/StringAddCalculator.java
@@ -1,5 +1,8 @@
 package step2;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
     public static int splitAndSum(String text) {
         if (text == null || text.isEmpty()) {
@@ -13,6 +16,11 @@ public class StringAddCalculator {
     }
 
     private static String[] split(String text) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            return m.group(2).split(customDelimiter);
+        }
         return text.split(",|:");
     }
 
