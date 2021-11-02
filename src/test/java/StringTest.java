f274eaf3a0a64362fc6c4648049e5f2a81c3c3f0
iia <kha2464@gmail.com>
2021-11-03 00:45:08 +0900

    test: String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -53,4 +53,32 @@ public class StringTest {
 			.hasSize(end - begin)
 			.isEqualTo("1,2");
 	}
+
+	@DisplayName("문자열의 특정 위치에 해당하는 문자 반환")
+	@Test
+	public void charAtTest() {
+		// given
+		String input = "abc";
+
+		// when
+		char[] charAtList = new char[input.length()];
+		for (int i = 0; i < input.length(); i++) {
+			charAtList[i] = input.charAt(i);
+		}
+
+		// then
+		assertThat(charAtList).containsExactly(input.toCharArray());
+	}
+
+	@DisplayName("문자열 범위에 벗어나는 위치의 문자를 반환할 경우 오류 발생")
+	@Test
+	public void charAtThrowExceptionWhenOutOfIndexTest() {
+		// given
+		String input = "abc";
+		int outOfIndex = input.length();
+
+		// when/then
+		assertThatThrownBy(() -> input.charAt(outOfIndex))
+			.isInstanceOf(StringIndexOutOfBoundsException.class);
+	}
 }
