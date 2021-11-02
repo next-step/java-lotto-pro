85b4391fa14e4d641fa1bdeb8882fb4e97d3c175
박창혁 <changchang73@pionnet.co.kr>
2021-11-02 23:24:30 +0900

    style : 정규표현식 상수로 변경

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -5,6 +5,10 @@ import java.util.regex.Pattern;
 
 public class StringAddCalculator {
 
+	public static final String REGEX_DEFAULT_DELIMITER = ",|:";
+	public static final String REGEX_CUSTOM_DELIMITER = "//(.)\\n(.*)";
+	public static final String REGEX_TEXT = "\\D";
+
 	public static int splitAndSum(String text) {
 		if (isEmpty(text)) {
 			return 0;
@@ -16,7 +20,7 @@ public class StringAddCalculator {
 	}
 
 	private static int customDelimiterSum(String text) {
-		Matcher matcher = Pattern.compile("//(.)\\n(.*)").matcher(text);
+		Matcher matcher = Pattern.compile(REGEX_CUSTOM_DELIMITER).matcher(text);
 		if (matcher.find()) {
 			String customDelimiter = matcher.group(1);
 			String[] tokens = split(matcher.group(2), customDelimiter);
@@ -26,7 +30,7 @@ public class StringAddCalculator {
 	}
 
 	private static boolean isCustomDelimiter(String text) {
-		return text.matches("//(.)\\n(.*)");
+		return text.matches(REGEX_CUSTOM_DELIMITER);
 	}
 
 	private static int sum(String[] numbers) {
@@ -39,10 +43,10 @@ public class StringAddCalculator {
 	}
 
 	private static void numberValidate(String text) {
-		if(isText(text)){
+		if (isText(text)) {
 			throw new RuntimeException("문자가 포함되어 있습니다.");
 		}
-		if(isNegative(text)){
+		if (isNegative(text)) {
 			throw new RuntimeException();
 		}
 	}
@@ -52,11 +56,11 @@ public class StringAddCalculator {
 	}
 
 	private static boolean isText(String text) {
-		return text.matches("\\D");
+		return text.matches(REGEX_TEXT);
 	}
 
 	private static String[] split(String text) {
-		return split(text, ",|:");
+		return split(text, REGEX_DEFAULT_DELIMITER);
 	}
 
 	private static String[] split(String text, String delimiter) {
