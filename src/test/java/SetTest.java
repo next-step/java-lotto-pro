54657ac16f4e8c506abdd598d69344a1ee0c1a35
JiHunHong <hongji3354@gmail.com>
2021-11-02 12:54:29 +0900

    test : set collection contains Test

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -1,5 +1,8 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -24,4 +27,9 @@ public class SetTest {
         assertThat(numbers.size()).isEqualTo(3);
     }
 
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    void containsTest(Integer input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
 }
