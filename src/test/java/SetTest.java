24f45832db792a430ea886495e23ef27d9f9a910
Kelicia91 <kelicia91@gmail.com>
2021-11-02 16:55:30 +0900

    test: Set.contains returns true/false

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -4,9 +4,9 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
@@ -28,7 +28,13 @@ public class SetTest {
 
 	@ParameterizedTest
 	@ValueSource(ints = {1, 2, 3})
-	void contains(int input) {
+	void contains_onlyTrue(int input) {
 		assertThat(numbers.contains(input)).isTrue();
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+	void contains(int input, boolean expected) {
+		assertThat(numbers.contains(input)).isEqualTo(expected);
 	}
 }
