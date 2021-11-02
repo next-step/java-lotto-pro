e4094651b9af62799c71220d83542751ebc0994d
Sangwoo Han <82221869+codingknowjam@users.noreply.github.com>
2021-11-03 08:55:26 +0900

    1단계 - 학습 테스트 실습 (#90)
    * docs(empty.txt): 파일 삭제
    
    * feat(StringTest): 요구사항1 테스트 코드 작성
    
    * feat(StringTest): 요구사항2 테스트 코드 작성
    
    * feat(StringTest): 요구사항3 테스트 코드 작성
    
    * feat(SetCollectionTest): 요구사항1 테스트 코드 작성
    
    * feat(SetCollectionTest): 요구사항2 테스트 코드 작성
    
    * feat(SetCollectionTest): 요구사항3 테스트 코드 작성

---
+++ src/test/java/SetCollectionTest.java
@@ -0,0 +1,46 @@
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class SetCollectionTest {
+	private Set<Integer> numbers;
+
+	@BeforeEach
+	void setUp() {
+		numbers = new HashSet<>();
+		numbers.add(1);
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+	}
+
+	// Test Case 구현
+	@Test
+	void sizeTest() {
+		assertThat(numbers).hasSize(3);
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {1, 2, 3})
+	@DisplayName("내부에_존재하는_값_확인")
+	void containsTest(int input) {
+		assertThat(numbers).contains(input);
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+	@DisplayName("값_존재여부에_따라_TRUE_OR_FALSE")
+	void containsTrueOrFalseTest(int input, boolean expected) {
+		assertThat(numbers.contains(input)).isEqualTo(expected);
+	}
+}
+
+
---
+++ src/test/java/StringTest.java
@@ -0,0 +1,36 @@
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class StringTest {
+	@Test
+	void splitTest(){
+		String[] stringArray = "1,2".split(",");
+		String[] stringOtherArray = "1".split(",");
+
+		assertThat(stringArray).contains("1","2");
+		assertThat(stringArray).containsExactly("1","2");
+		assertThat(stringArray).containsOnly("2", "1");
+
+		assertThat(stringOtherArray).contains("1");
+		assertThat(stringOtherArray).containsExactly("1");
+		assertThat(stringOtherArray).containsOnly("1");
+	}
+
+	@Test
+	void removeBracketTest(){
+		String string = "(1,2)".substring(1,4);
+		assertThat(string).isEqualTo("1,2");
+	}
+
+	@Test
+	@DisplayName("배열_인덱스_익셉션_테스트")
+	void stringIndexOutOfBoundsExceptionTest(){
+		assertThatThrownBy(() -> "abc".charAt(3))
+			.isInstanceOf(StringIndexOutOfBoundsException.class).hasMessageContaining("range: 3");
+
+		assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+			.isThrownBy(() -> "abc".charAt(3)).withMessageContaining("range: 3");
+	}
+}
