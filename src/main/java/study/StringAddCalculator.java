f7e56e85913f77ac222845230b5b1fa101799be9
Junhong-Kim <jhk1005jhk@gmail.com>
2021-11-03 00:30:17 +0900

    feat(StringAddCalculator): 문자열 분리 메서드 추가

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -2,7 +2,15 @@ package study;
 
 import study.utils.StringUtils;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
+
+    private static final String DEFAULT_DELIMITER = ",|:";
+    private static final String CUSTOM_DELIMITER = "//(.)\n(.*)";
+
+    private static final Pattern CUSTOM_PATTERN = Pattern.compile(CUSTOM_DELIMITER);
 
     private StringAddCalculator() {
         throw new UnsupportedOperationException();
@@ -17,6 +25,16 @@ public class StringAddCalculator {
             return Integer.parseInt(text);
         }
 
+        split(text);
         return 0;
+    }
+
+    private static String[] split(String text) {
+        Matcher matcher = CUSTOM_PATTERN.matcher(text);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            return matcher.group(2).split(customDelimiter);
+        }
+        return text.split(DEFAULT_DELIMITER);
     }
 }
