e33896453ec0cae9ca8be653ff10045c6de6fc79
jerry92k <62507373+jerry92k@users.noreply.github.com>
2021-11-02 19:29:41 +0900

    String, Set 학습 테스트 (#48)
    * chore(empty.txt) : 불필요 파일 삭제
    
    * test(StringTest) : String 메서드 학습테스트
    
    1. split 테스트
    2. substring 테스트
    3. charAt 테스트
    
    * style(StringTest) : 포맷팅
    
    * style(StringTest) : displayname 변경
    
    * test(SetTest) : Set 학습테스트, ParameterizedTest 학습 테스트
    
    1. Set의 size 메서드 테스트
    2. Set의 contains 메서드 테스트
    3. ParameterizedTest로 동일 테스트 로직에 다양한 파라미터를 대입하여 테스트

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,45 @@
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class SetTest {
+	private Set<Integer> numbers;
+
+	@BeforeEach
+	void setUp() {
+		numbers = new HashSet<>();
+		numbers.add(1);
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+	}
+
+	@DisplayName("size: Set의 원소 갯수 반환 메서드 학습테스트")
+	@Test
+	void size() {
+		assertThat(numbers.size()).isEqualTo(3);
+	}
+
+	@DisplayName("contains: Set에 element가 포함되어있는지 여부 반환 메서드 학습테스트. ParameterizedTest로 복수개 테스트")
+	@ParameterizedTest
+	@ValueSource(ints = {1, 2, 3})
+		// six numbers
+	void contains(int element) {
+		assertThat(numbers.contains(element)).isTrue();
+	}
+
+	@DisplayName("CsvSource로 각각의 element에 따른 예상값을 파라미터화")
+	@ParameterizedTest
+	@CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+	void containsIncludeFalseCase(int element, boolean expectedIsExist) {
+		assertThat(numbers.contains(element)).isEqualTo(expectedIsExist);
+	}
+}
