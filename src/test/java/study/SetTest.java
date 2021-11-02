a9c1e41572909c7a8242cf6713c684b437605594
이승민 <sml7179@naver.com>
2021-11-02 10:49:50 +0900

    Step - 학습 테스트 실습 (#9)
    * test: split() 검증
    
    * test: substring 검증
    
    * test: StringIndexOutOfBoundsException 검증
    
    * test: set size 크기 검증
    
    * test: set contains 메소드 검증

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,46 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("size() 메소드를 활용한 크기 검증")
+    @Test
+    void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @DisplayName("contains() 메소드 검증")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int num) {
+        assertThat(numbers.contains(num)).isTrue();
+    }
+
+    @DisplayName("contains() 메소드 결과값이 다를 때 검증")
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false", "6:false"}, delimiter = ':')
+    void containsTrueOrFalse(int num, boolean expected) {
+        assertThat(numbers.contains(num)).isEqualTo(expected);
+    }
+}
---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,37 @@
+package study;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class StringTest {
+
+    @DisplayName("split 검증")
+    @Test
+    void split() {
+        String[] result = "1,2".split(",");
+        assertThat(result).contains("1");
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @DisplayName("substring 검증")
+    @Test
+    void substring() {
+        String input = "(1,2)";
+        String result = input.substring(1, 4);
+        assertThat(result).contains("1,2");
+    }
+
+    @DisplayName("StringIndexOutOfBoundsException 검증")
+    @Test
+    void chatAt() {
+        String input = "abc";
+        assertThatThrownBy(() -> {
+            input.charAt(4);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 4");
+    }
+
+}
