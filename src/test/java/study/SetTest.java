543d237fb11e70c5206c0e09ce0f75f7306b619e
DaehyunJoo <dd70296@gmail.com>
2021-11-03 00:40:48 +0900

    test: TestMethodOrder 변경
    - DisplayName을 이용해 정렬하던 것을 Order 어노테이션 기준으로 변경

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -8,13 +8,14 @@ import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-@TestMethodOrder(MethodOrderer.DisplayName.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class SetTest {
 	private Set<Integer> numbers;
 
@@ -28,21 +29,24 @@ public class SetTest {
 	}
 
 	@Test
-	@DisplayName("1. Set.size()가 중복된 요소 제거를 반영하는지 크기를 반환하는지 확인")
+	@DisplayName("Set.size()가 중복된 요소 제거를 반영하는지 크기를 반환하는지 확인")
+	@Order(1)
 	void test_size1() {
 		assertThat(numbers.size()).isEqualTo(3);
 	}
 
 	@ParameterizedTest
 	@ValueSource(strings = {"1", "2", "3"})
-	@DisplayName("2. Set.contains가 추가한 요소에 true를 반환하는지 확인")
+	@DisplayName("Set.contains가 추가한 요소에 true를 반환하는지 확인")
+	@Order(2)
 	void test_contains1(int element) {
 		assertThat(numbers.contains(element)).isTrue();
 	}
 
 	@ParameterizedTest
 	@CsvSource(value = {"1,true", "2,true", "3,true", "4,false", "5,false"})
-	@DisplayName("3. Set.contains가 추가한 요소에는 true를, 추가하지 않은 요소에는 false를 반환하는지 확인")
+	@DisplayName("Set.contains가 추가한 요소에는 true를, 추가하지 않은 요소에는 false를 반환하는지 확인")
+	@Order(3)
 	void test_contains2(int element, boolean isContain) {
 		assertThat(numbers.contains(element)).isEqualTo(isContain);
 	}
