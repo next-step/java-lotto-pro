96ef59407bc62e129952839f1692d327090d4c1f
slowin <irooori89@gmail.com>
2021-11-02 17:03:58 +0900

    refactor : 변수이름 개선 및 테스트 @ParameterizedTest 적용

--- src/main/java/step2/SplitNumber.java
+++ src/main/java/step2/SplitNumber.java
@@ -1,7 +1,7 @@
 package step2;
 
 public class SplitNumber {
-    public static final String NOT_POSITIVE_NUMBER = "입력 값은 양수만 가능합니다.";
+    public static final String NOT_POSITIVE_NUMBER_MESSAGE = "입력 값은 양수만 가능합니다.";
 
     public static int valueOf(String value) {
         valid(value);
@@ -10,7 +10,7 @@ public class SplitNumber {
 
     private static void valid(String value) {
         if (!isNumber(value) || Integer.parseInt(value) < 0) {
-            throw new RuntimeException(NOT_POSITIVE_NUMBER);
+            throw new RuntimeException(NOT_POSITIVE_NUMBER_MESSAGE);
         }
     }
 
--- src/test/java/step2/SplitNumberTest.java
+++ src/test/java/step2/SplitNumberTest.java
@@ -3,17 +3,20 @@ package step2;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SplitNumberTest {
-    @Test
+
+    @ParameterizedTest
+    @ValueSource(strings = {"A", "-1"})
     @DisplayName("양수가 아닌경우 예외발생")
-    void splitNumber_예외() {
-        // when
+    void splitNumber_예외(String input) {
         assertThatExceptionOfType(RuntimeException.class)
-            // then
             .isThrownBy(() -> {
-                SplitNumber.valueOf("A");
-            }).withMessageMatching(SplitNumber.NOT_POSITIVE_NUMBER);
+                // when
+                SplitNumber.valueOf(input);
+            }) // then
+            .withMessageMatching(SplitNumber.NOT_POSITIVE_NUMBER_MESSAGE);
     }
 }
