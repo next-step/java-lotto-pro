564d5bc5a297e74dab2eff3d82d3a19f891e9cb7
cus4703 <cus4703@gmail.com>
2021-11-03 08:51:42 +0900

    refactor: 상수 위치 변경으로 인한 import 제거

--- src/test/java/lotto/utils/StringAddCalculatorTest.java
+++ src/test/java/lotto/utils/StringAddCalculatorTest.java
@@ -1,7 +1,6 @@
 package lotto.utils;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -11,11 +10,10 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static lotto.constants.CalculatorConstants.DEFAULT_SPLIT_REGEX;
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class StringAddCalculatorTest {
