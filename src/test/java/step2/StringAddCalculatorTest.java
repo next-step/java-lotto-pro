eb36d686f6ff90a206665cfc96afd5b075dd26b6
slowin <irooori89@gmail.com>
2021-11-02 12:34:19 +0900

    test : 기본 구분자 테스트 코드 추가
    테스트 코드
    - splitAndSum_쉼표구분자
    - splitAndSum_쉼표_또는_콜론_구분자

--- src/test/java/step2/StringAddCalculatorTest.java
+++ src/test/java/step2/StringAddCalculatorTest.java
@@ -25,4 +25,16 @@ public class StringAddCalculatorTest {
         assertThat(result).isEqualTo(1);
     }
 
+    @Test
+    public void splitAndSum_쉼표구분자() throws Exception {
+        int result = StringAddCalculator.splitAndSum("1,2");
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
+        int result = StringAddCalculator.splitAndSum("1,2:3:3");
+        assertThat(result).isEqualTo(9);
+    }
+
 }
