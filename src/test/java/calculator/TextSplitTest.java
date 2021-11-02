f9bc8b8a2b591fbfec30881b5b839dccce804ecc
Wangki Lee <lwk3007@gmail.com>
2021-11-02 21:40:02 +0900

    feature: 커스텀 구분자를 기준으로 텍스트 분할

--- src/test/java/calculator/TextSplitTest.java
+++ src/test/java/calculator/TextSplitTest.java
@@ -18,4 +18,14 @@ class TextSplitTest {
         String[] numbersByMix = TextSplit.split("1,2:3");
         assertThat(numbersByMix).containsExactly("1", "2", "3");
     }
+
+    @DisplayName("커스텀 구분자를 기준으로 split test")
+    @Test
+    void splitCustomDelimiter() {
+        String[] numbersBySemiColon = TextSplit.split("//;\n1;2;3");
+        assertThat(numbersBySemiColon).containsExactly("1", "2", "3");
+
+        String[] numbersByDash = TextSplit.split("//-\n1-2-3");
+        assertThat(numbersByDash).containsExactly("1", "2", "3");
+    }
 }
