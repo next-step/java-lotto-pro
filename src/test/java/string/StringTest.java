ceb5cf4a170e4e3fdb608eff4f6424358c62ef15
smpark1020 <smpark911020@gmail.com>
2021-11-02 22:39:37 +0900

    test: String 요구사항3

--- src/test/java/string/StringTest.java
+++ src/test/java/string/StringTest.java
@@ -1,8 +1,10 @@
 package string;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class StringTest {
 
@@ -41,5 +43,33 @@ class StringTest {
 
         // then
         assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("정상 범위의 값을 가져오는 테스트")
+    void charAt() {
+        // given
+        String input = "abc";
+
+        // when
+        char result1 = input.charAt(0);
+        char result2 = input.charAt(1);
+        char result3 = input.charAt(2);
+
+        // then
+        assertThat(result1).isEqualTo('a');
+        assertThat(result2).isEqualTo('b');
+        assertThat(result3).isEqualTo('c');
+    }
+
+    @Test
+    @DisplayName("위치 값을 벗어나 Exception 발생하는 테스트")
+    void charAt_exception() {
+        // given
+        String input = "abc";
+
+        // when, then
+        assertThatThrownBy(() -> input.charAt(3)).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 3");
     }
 }
