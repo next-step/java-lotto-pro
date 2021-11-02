88cd2406d6c830c25e0f68b6798398ba33a4f481
Kelicia91 <kelicia91@gmail.com>
2021-11-02 22:58:12 +0900

    fix: broken test(splitAndSum_custom_구분자)

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,19 +1,34 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
 
 	private final static String DEFAULT_DELIMITER = ",|:";
+	private final static Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
 
 	public static int splitAndSum(String s) {
 		if (isNullOrEmpty(s)) {
 			return 0;
 		}
-		return sum(parse(s, DEFAULT_DELIMITER));
+		String[] delimiterAndRest = parseDelimiter(s);
+		return sum(parseInt(delimiterAndRest[0], delimiterAndRest[1]));
 	}
 
 	private static boolean isNullOrEmpty(String s) {
 		return (null == s) || s.isEmpty();
 	}
 
-	private static int[] parse(String s, String delimiter) {
+	private static String[] parseDelimiter(String s) {
+		final Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(s);
+		if (matcher.find()) {
+			final String customDelimiter = matcher.group(1);
+			final String rest = matcher.group(2);
+			return new String[]{ customDelimiter, rest };
+		}
+		return new String[]{ DEFAULT_DELIMITER, s };
+	}
+
+	private static int[] parseInt(String delimiter, String s) {
 		final String[] tokens = s.split(delimiter);
 		final int[] parsedTokens = new int[tokens.length];
 		for (int i = 0; i < tokens.length; ++i) {
@@ -23,7 +38,7 @@ public class StringAddCalculator {
 	}
 
 	private static int parseNotNegativeInt(String s) {
-		int notNegativeInt = Integer.parseInt(s);
+		final int notNegativeInt = Integer.parseInt(s);
 		if (notNegativeInt < 0) {
 			throw new RuntimeException();
 		}
