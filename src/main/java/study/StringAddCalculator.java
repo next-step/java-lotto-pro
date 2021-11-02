20c590846ed05f4b18a9567fe93dae11e8ee2b47
ch3224bin <ahrocksil@gmail.com>
2021-11-02 21:05:39 +0900

    feat : 문자열 덧셈 계산기 StringAddCalculator.splitAndSum 구현
    int splitAndSum(int text)은 아래의 기능을 가지고 있다
    - 쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환 (예: “” => 0, "1,2" => 3, "1,2,3" => 6, “1,2:3” => 6)
    - 앞의 기본 구분자(쉼표, 콜론)외에 커스텀 구분자를 지정할 수 있다. 커스텀 구분자는 문자열 앞부분의 “//”와 “\n” 사이에 위치하는 문자를 커스텀 구분자로 사용한다. 예를 들어 “//;\n1;2;3”과 같이 값을 입력할 경우 커스텀 구분자는 세미콜론(;)이며, 결과 값은 6이 반환되어야 한다.
    - 문자열 계산기에 숫자 이외의 값 또는 음수를 전달하는 경우 RuntimeException 예외를 throw한다.

---
+++ src/main/java/study/StringAddCalculator.java
@@ -0,0 +1,69 @@
+package study;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+    private static final String[] DEFAULT_DELIMITER = {",", ":"};
+    private static final int DEFAULT_FAILURE_VALUE = 0;
+    private static final Pattern PATTERN_FIND_CUSTOM_DELIMITER = Pattern.compile("//(.)\n(.+)");
+    private static final Pattern PATTERN_FIND_BODY_TEXT = Pattern.compile("(?://.\n)?(.+)");
+
+    private StringAddCalculator() {
+    }
+
+    public static int splitAndSum(String text) {
+        if (isEmpty(text)) {
+            return DEFAULT_FAILURE_VALUE;
+        }
+
+        String customDelimiter = getCustomDelimiter(text);
+        String[] numbers = splitWithCustomDelimiter(text, customDelimiter);
+        return getSumValue(numbers);
+    }
+
+    private static String[] splitWithCustomDelimiter(String text, String customDelimiter) {
+        String regex = String.join("|", DEFAULT_DELIMITER);
+        if (!isEmpty(customDelimiter)) {
+            regex += "|" + customDelimiter;
+        }
+        return getBodyText(text).split(regex);
+    }
+
+    private static String getCustomDelimiter(String text) {
+        return findFirstMatchedGroup(text, PATTERN_FIND_CUSTOM_DELIMITER);
+    }
+
+    private static String getBodyText(String text) {
+        return findFirstMatchedGroup(text, PATTERN_FIND_BODY_TEXT);
+    }
+
+    private static String findFirstMatchedGroup(String text, Pattern pattern) {
+        String result = "";
+        Matcher m = pattern.matcher(text);
+        if (m.find()) {
+            result = m.group(1);
+        }
+        return result;
+    }
+
+    private static int getSumValue(String[] numbers) {
+        int result = 0;
+        for (String number : numbers) {
+            result += parseInt(number);
+        }
+        return result;
+    }
+
+    private static int parseInt(String number) {
+        int value = Integer.parseInt(number);
+        if (Integer.signum(value) == -1) {
+            throw new RuntimeException("양의 정수 값을 입력해 주세요");
+        }
+        return value;
+    }
+
+    private static boolean isEmpty(String text) {
+        return text == null || text.isEmpty();
+    }
+}
