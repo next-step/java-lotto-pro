48638a85023713c0845fd59c6632a078a62f2c04
doyoung0205 <39691109+doyoung0205@users.noreply.github.com>
2021-11-03 22:28:00 +0900

    2단계 - 문자열 덧셈 계산기 (#97)
    * split study test
    
    * refactor: split test
    
    - "1,2" 문자열을 변수로 추출
    
    * feat: substring 메소드 테스트 코드 추가
    
    * test: charAt 메소드 테스트 코드 추가
    
    * test: set 메소드 테스트 코드 추가
    
    - size : 포함하고 있는 요소 개수 테스트
    - contain: 포함 하는지 확인 및 포함 유무에 따른 결과값 테스트
    
    * refactor: StringTest 에서 여러 assert 를 assertAll 로 묶기
    
    * refactor: 테스트 코드 리뷰 사항 반영
    
    - DisplayName 적용
    - charAt 의 성공 케이스 추가
    - contains 결과 값이 True 인 케이스 추가
    
    * feat: empty txt 파일 삭제
    
    * docs: 구현할 목록 및 요구사항 정리
    
    * feat:  splitAndSum 메소드 실행시 빈 문자열 또는 null 값이 입력될 경우 0을 반환하는 기능 추가
    
    * feat: 숫자 하나를 문자열로 입력될 경우, 해당 숫자를 반환하는 기능 추가
    
    * feat (splitAndSum) :  숫자 두개를 컴마(,) 구분자로 입력될 경우 두 숫자의 합을 반환 하는 기능 추가
    
    * feat (splitAndSum) : 구분자를 컴마(,) 이외에 콜론(:)을 사용할 수 있다.
    
    * feat (splitAndSum) :  “//”와 “\n” 문자 사이에 커스텀 구분자를 지정할 수 있다. (예 : “//;\n1;2;3” => 6)
    
    * feat (splitAndSum) : 음수를 전달할 경우 RuntimeException 예외가 발생해야 한다. (예 : “-1,2,3”)
    
    * refactor: 함수로 추출 및 함수명 변경
    
    hasNotSplitText : 분리된 문자열이 없는지 확인하는 함수 생성
    sumBySplitText : 분리된 문자열을 바탕으로 합을 구하는 함수명 변경
    - before : sum
    
    * refactor: 에러메시지 상수로 추출
    
    * refactor (splitAndSum) : 커스텀 구분 관련 매직넘버 상수로 변경
    
    * refactor (StringAddCalculator) : 실수 방지를 위해 매개변수에 final 추가
    
    - 추가된 메소드 리스트
     parsePositiveNumber
     hasNotSplitText
     isNotPositiveNumber
    
    Co-authored-by: 김도영 <95doyoung@gmail.com>

---
+++ src/main/java/study/StringAddCalculator.java
@@ -0,0 +1,62 @@
+package study;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+    private static final String DEFAULT_SEPARATORS = "[,:]";
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
+    private static final int CUSTOM_DELIMITER_GROUP = 1;
+    private static final int CUSTOM_INPUT_GROUP = 2;
+    private static final String NOT_POSITIVE_ERROR_MESSAGE_FORMAT = "문자열 계산기는 음수를 입력할 수 없습니다. [%d]";
+
+    public static int splitAndSum(final String text) {
+        if (isEmptyOrNull(text)) {
+            return 0;
+        }
+
+        final String[] splitText = split(text);
+        if (hasNotSplitText(splitText)) {
+            return parsePositiveNumber(text);
+        }
+
+        return sumBySplitText(splitText);
+    }
+
+    private static String[] split(final String text) {
+        final Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(text);
+        if (matcher.find()) {
+            final String customDelimiter = matcher.group(CUSTOM_DELIMITER_GROUP);
+            return matcher.group(CUSTOM_INPUT_GROUP).split(customDelimiter);
+        }
+        return text.split(DEFAULT_SEPARATORS);
+    }
+
+    private static int sumBySplitText(final String[] splitText) {
+        int sum = 0;
+        for (final String text : splitText) {
+            sum += parsePositiveNumber(text);
+        }
+        return sum;
+    }
+
+    private static int parsePositiveNumber(final String text) {
+        final int number = Integer.parseInt(text);
+        if (isNotPositiveNumber(number)) {
+            throw new RuntimeException(String.format(NOT_POSITIVE_ERROR_MESSAGE_FORMAT, number));
+        }
+        return number;
+    }
+
+    private static boolean hasNotSplitText(final String[] splitText) {
+        return splitText.length <= 1;
+    }
+
+    private static boolean isNotPositiveNumber(final int number) {
+        return number < 0;
+    }
+
+    private static boolean isEmptyOrNull(final String text) {
+        return text == null || text.isEmpty();
+    }
+}
