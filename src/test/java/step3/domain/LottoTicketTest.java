966b09c3c3ec2f08821546fbb284f061c3581732
slowin <irooori89@gmail.com>
2021-11-04 00:44:33 +0900

    test : LottoTicketTest 추가
    - 로또_티켓_생성
    - 로또_티켓_6개_숫자_미달
    - 로또_티켓_6개_숫자_초과

---
+++ src/test/java/step3/domain/LottoTicketTest.java
@@ -0,0 +1,51 @@
+package step3.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoTicketTest {
+
+    @Test
+    @DisplayName("입력 (1, 2, 3, 4, 5, 6) LottoTicket 생성, 숫자 포함 검증")
+    void 로또_티켓_생성() {
+        // given
+        int[] numbers = {1, 2, 3, 4, 5, 6};
+
+        // when
+        LottoTicket lottoTicket = new LottoTicket(numbers);
+
+        // then
+        assertThat(lottoTicket.toLottoNumbers()).containsExactly(1, 2, 3, 4, 5, 6);
+
+    }
+
+    @Test
+    void 로또_티켓_6개_숫자_미달() {
+        // given
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+            .isThrownBy(() -> {
+                // when
+                new LottoTicket(numbers);
+            })
+            // then
+            .withMessageMatching(LottoTicket.LOTTO_TICKET_OVER_SIZE_EXCEPTION_MESSAGE);
+    }
+
+    @Test
+    void 로또_티켓_6개_숫자_초과() {
+        // given
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7};
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+            .isThrownBy(() -> {
+                // when
+                new LottoTicket(numbers);
+            })
+            // then
+            .withMessageMatching(LottoTicket.LOTTO_TICKET_OVER_SIZE_EXCEPTION_MESSAGE);
+    }
+}
