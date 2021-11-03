c93032ab91ef455ea3176add4d4382955f76ed7f
IW.MOON <mooniw02@gmail.com>
2021-11-03 23:16:11 +0900

    feat: GameMessage, InputView 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,11 @@
+package lotto.view;
+
+public class InputView {
+
+    public static void printBuyPriceInput() {
+        System.out.println(GameMessage.BUY_PRICE_INPUT);
+    }
+    public static void printWinningNumberInput() {
+        System.out.println(GameMessage.WINNING_NUMBER_INPUT);
+    }
+}
