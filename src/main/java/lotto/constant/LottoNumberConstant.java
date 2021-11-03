e8a94b9af95d46a9eb557a0d398ff038b3ea0d45
tyanotyrano <tyrano09@gmail.com>
2021-11-03 21:04:37 +0900

    feat(MessageBuilder.java): 가변인자를 받아 메세지를 만들어주는 Util 클래스 구현
    가변인자를 받아 메세지를 만들어주는 Util 클래스를 구현한다.
    - MessageBuilder : 가변인자를 받아 메세지를 만들어주는 Util 클래스
    - LottoNumberConstant : private 생성자 추가

--- src/main/java/lotto/constant/LottoNumberConstant.java
+++ src/main/java/lotto/constant/LottoNumberConstant.java
@@ -4,4 +4,6 @@ public class LottoNumberConstant {
 	public static final int LOTTO_NUMBER_MIN = 1;
 	public static final int LOTTO_NUMBER_MAX = 45;
 	public static final int VALID_LOTTO_NUMBER_COUNT = 6;
+
+	private LottoNumberConstant() {}
 }
