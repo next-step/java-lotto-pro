f659204b127f297cedbd6ac3405b5777d6cedd34
slowin <irooori89@gmail.com>
2021-11-04 00:42:20 +0900

    feat : LottoNumber 추가
    - rangeNumberValid 숫자 범위 검증 메소드

---
+++ src/main/java/step3/domain/LottoNumber.java
@@ -0,0 +1,42 @@
+package step3.domain;
+
+import java.util.Objects;
+
+public class LottoNumber {
+    public static final int RANGE_MIN_LOTTO_NUMBER = 1;
+    public static final int RANGE_MAX_LOTTO_NUMBER = 45;
+    public static final String RANGE_OVER_EXCEPTION = String.format("%s부터 %s 까지의 숫자를 입력해주세요.", RANGE_MIN_LOTTO_NUMBER,
+        RANGE_MAX_LOTTO_NUMBER);
+
+    private final int lotteNumber;
+
+    public LottoNumber(int lotteNumber) {
+        rangeNumberValid(lotteNumber);
+        this.lotteNumber = lotteNumber;
+    }
+
+    private void rangeNumberValid(int lotteNumber) {
+        if (lotteNumber < RANGE_MIN_LOTTO_NUMBER || lotteNumber > RANGE_MAX_LOTTO_NUMBER) {
+            throw new IllegalArgumentException(RANGE_OVER_EXCEPTION);
+        }
+    }
+
+    public int toInt() {
+        return lotteNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        LottoNumber that = (LottoNumber)o;
+        return lotteNumber == that.lotteNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lotteNumber);
+    }
+}
