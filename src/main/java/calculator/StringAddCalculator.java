a8f064bd455b76e0d48913dfc1f08c42af947ab0
don-key <rlaehdzlsla@gmail.com>
2021-11-03 19:00:34 +0900

    2단계 - 문자열 덧셈 계산기 (#65)
    * docs: 불필요한 파일 제거
    
    * docs: 기능 요구사항 정리
    
    * test: 문자열 덧셈 계산기 TestCase 소스 코드 추가
    
    * docs: 기능 요구사항 수정
    
    * feat: 쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환
    
    * feat: 커스텀 구분자는 문자열 앞부분의 “//”와 “\n” 사이에 위치하는 문자를 커스텀 구분자로 사용한다.
    
    * feat: 문자열 계산기에 숫자 이외의 값 또는 음수를 전달하는 경우 RuntimeException 예외를 throw 한다.

---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,27 @@
+package calculator;
+
+import static java.util.Arrays.stream;
+
+public class StringAddCalculator {
+
+    private StringAddCalculator() {
+    }
+
+    public static int splitAndSum(String input) {
+        if (isNullOrEmpty(input)) {
+            return 0;
+        }
+
+        String[] numbers = StringAddParser.split(input);
+
+        return sum(numbers);
+    }
+
+    private static boolean isNullOrEmpty(String input) {
+        return input == null || input.trim().isEmpty();
+    }
+
+    private static int sum(String[] numbers) {
+        return stream(numbers).mapToInt(Integer::parseInt).sum();
+    }
+}
---
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -0,0 +1,54 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
+public class StringAddCalculatorTest {
+    @ParameterizedTest
+    @NullAndEmptySource
+    public void splitAndSum_null_또는_빈문자(String input) {
+        int result = StringAddCalculator.splitAndSum(input);
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    public void splitAndSum_숫자하나() {
+        int result = StringAddCalculator.splitAndSum("1");
+        assertThat(result).isEqualTo(1);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,2=3", "1,2,3=6"}, delimiter = '=')
+    public void splitAndSum_쉼표구분자(String input, int expected) {
+        int result = StringAddCalculator.splitAndSum(input);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,2=3", "1,2,3=6", "1,2:3=6"}, delimiter = '=')
+    public void splitAndSum_쉼표_또는_콜론_구분자(String input, int expected) {
+        int result = StringAddCalculator.splitAndSum(input);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"//;\n1;2;3", "//@\n1@2@3", "//#\n1#2#3"})
+    public void splitAndSum_custom_구분자(String input) {
+        int result = StringAddCalculator.splitAndSum(input);
+        assertThat(result).isEqualTo(6);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"-1,2,3", "2,3,-1", "4,5,@"})
+    public void splitAndSum_negative(String input) {
+        assertThatThrownBy(() -> StringAddCalculator.splitAndSum(input))
+                .isInstanceOf(RuntimeException.class);
+    }
+}
