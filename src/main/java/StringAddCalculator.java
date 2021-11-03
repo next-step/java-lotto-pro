8ed65cc6194e5a0ca8c8bf77f9ce7b7de24e7fe8
eedys <eedys@ex-em.com>
2021-11-03 19:54:31 +0900

    테스트코드에서 중복되는 StringAddCalculator 생성코드 제거, 문자열을 숫자로 변환하는 기능 개발

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -3,8 +3,7 @@ import java.util.Objects;
 public class StringAddCalculator {
 
     public int calculate(String input) {
-        if(Objects.isNull(input)) return 0;
-        if(input.isEmpty()) return 0;
-        return 0;
+        if(Objects.isNull(input) || input.isEmpty()) return 0;
+        return Integer.parseInt(input);
     }
 }
--- src/test/java/StringAddCalculatorTest.java
+++ src/test/java/StringAddCalculatorTest.java
@@ -1,17 +1,28 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringAddCalculatorTest {
 
-    @DisplayName("빈 문자열 or null 일경우 0으로 리턴")
+    private StringAddCalculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new StringAddCalculator();
+    }
+
+    @DisplayName("빈 문자열 or null 일경우 0으로 리턴하는 기능 테스트")
     @Test
     public void nullOrEmptyString_ReturnZeroTest() {
-
-        StringAddCalculator calculator = new StringAddCalculator();
         assertThat(calculator.calculate("")).isEqualTo(0);
         assertThat(calculator.calculate(null)).isEqualTo(0);
+    }
+
+    @DisplayName("문자열을 숫자로 변환하는 기능 테스트")
+    @Test
+    public void stringToIntTest() {
+        assertThat(calculator.calculate("1")).isEqualTo(1);
     }
 }
