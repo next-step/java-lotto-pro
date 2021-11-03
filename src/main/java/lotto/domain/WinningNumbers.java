041a2a5a98ebd57fc2008690c8099372231899a2
tyanotyrano <tyrano09@gmail.com>
2021-11-03 23:14:43 +0900

    feat(WinningNumbers.java): 지난 주 당첨 번호 생성
    지난 주 당첨 번호를 생성한다.
    - WinningNumbers : 지난 주 당첨 번호를 담고있는 1급 컬렉션
    - README.md : 기능 요구사항 추가 및 완료된 기능 요구사항 체크

---
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -0,0 +1,21 @@
+package lotto.domain;
+
+import java.util.List;
+
+import lotto.utils.LottoNumberMapper;
+import lotto.utils.LottoNumberValidator;
+
+public class WinningNumbers {
+	private final List<LottoNumber> winningNumbers;
+
+	private WinningNumbers(List<LottoNumber> winningNumbers) {
+		this.winningNumbers = winningNumbers;
+	}
+
+	public static WinningNumbers createBy(List<Integer> numbers) {
+		List<LottoNumber> winningNumbers = LottoNumberMapper.mapToLottoNumbers(numbers);
+		LottoNumberValidator.validateLottoNumbers(winningNumbers);
+
+		return new WinningNumbers(winningNumbers);
+	}
+}
