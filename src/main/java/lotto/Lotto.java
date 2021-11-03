17f0e2734da640061a71a69d519edd9219665a9f
minho <lights8615@gmail.com>
2021-11-03 19:23:08 +0900

    feat(step3): 구입 개수 메시지 출력

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -5,9 +5,12 @@ import view.ResultView;
 
 public class Lotto {
     private static final String ASK_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String PRINT_NUMBER_OF_PURCHASED_LOTTO = "개를 구매했습니다.";
 
     public void start() {
         ResultView.print(ASK_PURCHASE_AMOUNT);
         LottoMoney lottoMoney = new LottoMoney(InputView.readLine());
+        int count = lottoMoney.calculateLottoCount();
+        ResultView.print(count + PRINT_NUMBER_OF_PURCHASED_LOTTO);
     }
 }
