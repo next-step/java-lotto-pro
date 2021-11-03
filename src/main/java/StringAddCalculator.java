1f91ab324dbab3a6760d7ff77afc9b2c12d42abb
eedys <eedys@ex-em.com>
2021-11-03 20:30:59 +0900

    feat 기본 구분자를 이용하여 구분자를 기준으로 분할된 숫자를 합치는 기능 개발

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -2,19 +2,44 @@ import java.util.Objects;
 
 public class StringAddCalculator {
 
-    public int calculate(String input) {
-        if(Objects.isNull(input) || input.isEmpty()) return 0;
-        validateNotNumber(input);
-        int number = Integer.parseInt(input);
-        if(number < 0) throw new IllegalArgumentException();
-        return number;
+    public int calculate(String text) {
+        if(Objects.isNull(text) || text.isEmpty()) return 0;
+        int[] numbers = stringsToNumbers(splitString(text));
+        return sum(numbers);
     }
 
-    private void validateNotNumber(String input) {
+    private String[] splitString(String text) {
+        return text.split(",|:");
+    }
+
+    private int sum(int[] numbers) {
+        int sum = 0;
+        for(int number : numbers)
+        {
+            sum += number;
+        }
+        return sum;
+    }
+
+    private int[] stringsToNumbers(String[] splitStrings) {
+        int[] numbers = new int[splitStrings.length];
+        for(int i=0;i<splitStrings.length;i++)
+        {
+            numbers[i] = stringToNumber(splitStrings[i]);
+            validateNegative(numbers[i]);
+        }
+        return numbers;
+    }
+
+    private int stringToNumber(String text) {
         try {
-            Integer.parseInt(input);
+            return Integer.parseInt(text);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private void validateNegative(int number) {
+        if(number < 0) throw new IllegalArgumentException();
     }
 }
--- src/test/java/StringAddCalculatorTest.java
+++ src/test/java/StringAddCalculatorTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -34,5 +35,12 @@ public class StringAddCalculatorTest {
     @ValueSource(strings = {"TEST", "-1"})
     public void stringToNumber_ThrowExceptionTest(String fixture) {
         assertThatThrownBy(() -> calculator.calculate(fixture)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("기본 구분자(,:)를 통해 문자열을 split 하여 각각을 합치는 기능 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"1,2|3", "1:2:3|6", "1,2:3:4|10"}, delimiter = '|')
+    public void splitStringByPrimarySeparatorTest(String fixture, int result) {
+        assertThat(calculator.calculate(fixture)).isEqualTo(result);
     }
 }
