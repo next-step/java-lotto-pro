2ca40e3ec09aa75f3792b68748524b8de16f3db0
parkchanghyuk <changchang743@gmail.com>
2021-11-03 20:25:32 +0900

    2단계 - 문자열 덧셈 계산기 (#77)
    * docs : 2단계 Readme 작성
    
    * feat : spilitAndSum null 또는 빈문자 0처리
    
    * feat : splitAndSum 숫자 변환 메소드 추가
    
    * feat : splitAndSum split 메서드, sum 메서드 추가
    
    * style : 배열 변수명 변경
    
    * test : splitAndSum 쉼표 또는 콜론 구분자 split 확인
    
    * feat : splitAndSum 커스텀구분자로 합계구하기
    
    * feat : splitAndSum 음수 또는 문자를 전달할 경우 RuntimeException 발생
    
    * test : 구분자 이외의 문자가 입력될 경우 RuntimeException
    
    * style : 정규표현식 상수로 변경
    
    Co-authored-by: 박창혁 <changchang73@pionnet.co.kr>

---
+++ src/test/java/study/StringAddCalculatorTest.java
@@ -0,0 +1,59 @@
+package study;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import org.junit.jupiter.api.Test;
+
+public class StringAddCalculatorTest {
+	@Test
+	public void splitAndSum_null_또는_빈문자() {
+		String text = null;
+		int result = StringAddCalculator.splitAndSum(text);
+		assertThat(result).isEqualTo(0);
+
+		text = "";
+		result = StringAddCalculator.splitAndSum(text);
+		assertThat(result).isEqualTo(0);
+	}
+
+	@Test
+	public void splitAndSum_숫자하나() throws Exception {
+		String text = "1";
+		int result = StringAddCalculator.splitAndSum(text);
+		assertThat(result).isEqualTo(1);
+	}
+
+	@Test
+	public void splitAndSum_쉼표구분자() throws Exception {
+		String text = "1,2";
+		int result = StringAddCalculator.splitAndSum(text);
+		assertThat(result).isEqualTo(3);
+	}
+
+	@Test
+	public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
+		String text = "1,2:3";
+		int result = StringAddCalculator.splitAndSum(text);
+		assertThat(result).isEqualTo(6);
+	}
+
+	@Test
+	public void splitAndSum_custom_구분자() throws Exception {
+		String text = "//;\n1;2;3";
+		int result = StringAddCalculator.splitAndSum(text);
+		assertThat(result).isEqualTo(6);
+	}
+
+	@Test
+	public void splitAndSum_negative() throws Exception {
+		assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
+			.isInstanceOf(RuntimeException.class);
+	}
+
+	@Test
+	public void splitAndSum_숫자_대신_문자() throws Exception {
+		assertThatThrownBy(() -> StringAddCalculator.splitAndSum("@,D,3"))
+			.isInstanceOf(RuntimeException.class)
+			.hasMessageContaining("문자가 포함되어 있습니다.");
+	}
+}
