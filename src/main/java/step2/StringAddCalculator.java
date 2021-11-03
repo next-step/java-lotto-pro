fc2be0662bfa0b0847a38acd230ec4126827c4c0
codingknowjam <hanwoodww@gmail.com>
2021-11-03 23:48:52 +0900

    feat: 검증로직 추가(null 또는 빈 문자인 경우)
    null 또는 빈문자가 들어오면 0을 리턴

--- src/main/java/step2/StringAddCalculator.java
+++ src/main/java/step2/StringAddCalculator.java
@@ -1,7 +1,17 @@
 package step2;
 
 public class StringAddCalculator {
-	public static int splitAndSum(Object o) {
+	public static int splitAndSum(String string) {
+		if (validateNullOrBlank(string)) {
+			return 0;
+		}
 		return -1;
+	}
+
+	private static boolean validateNullOrBlank(String string) {
+		if (string == null || string.equals("")) {
+			return true;
+		}
+		return false;
 	}
 }
