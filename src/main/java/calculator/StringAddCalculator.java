4c17a0813ee15bb0830837b4aa9013125c4a8de6
이승민 <sml7179@naver.com>
2021-11-03 14:09:00 +0900

    Step2 - 문자열 덧셈 계산기 (#61)
    * test: StringAddCalculatorTest 테스트 코드 생성
    
    * feat: 문자열 계산기 기능 추가
    
    * Chore: empty.txt 파일 삭제
    
    * feat: 문자값이 들어왔을때 에러처
    
    * feat: 음수값이 들어왔을때 에러처

---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,57 @@
+package calculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+
+    private static final String SEPARATOR = ",|:";
+    private static final String CUSTOM_PATTERN_SEPARATOR = "//(.)\n(.*)";
+    private static final int NEGATIVE_CANNOT_INT = 0;
+
+    public static int splitAndSum(String text) {
+        if (isEmptyOrNull(text)) {
+            return 0;
+        }
+        String[] numbers = splitString(text);
+        return sum(numbers);
+    }
+
+    private static int sum(String[] numbers) {
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += mapToInt(numbers[i]);
+        }
+        return sum;
+    }
+
+    private static int mapToInt(String text) {
+        int number;
+        try {
+            number = Integer.parseInt(text);
+            checkIfPositive(number);
+        } catch (NumberFormatException e) {
+            throw new RuntimeException("숫자형식이 아닙니다.");
+        }
+        return number;
+    }
+
+    private static void checkIfPositive(int number) {
+        if (number < NEGATIVE_CANNOT_INT) {
+            throw new RuntimeException("음수는 사용할 수 없습니다.");
+        }
+    }
+
+    private static boolean isEmptyOrNull(String text) {
+        return text == null || text.isEmpty();
+    }
+
+    private static String[] splitString(String text) {
+        Matcher matcher = Pattern.compile(CUSTOM_PATTERN_SEPARATOR).matcher(text);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            return matcher.group(2).split(customDelimiter);
+        }
+        return text.split(SEPARATOR);
+    }
+}
