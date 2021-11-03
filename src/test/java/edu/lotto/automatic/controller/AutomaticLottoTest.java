fbc6e1c5ef89a3846e076abfb8b9b61ae644b731
InmookJeong <jeonginmook@gmail.com>
2021-11-03 23:03:51 +0900

    test: add more than thousand number test

--- src/test/java/edu/lotto/automatic/controller/AutomaticLottoTest.java
+++ src/test/java/edu/lotto/automatic/controller/AutomaticLottoTest.java
@@ -19,4 +19,10 @@ public class AutomaticLottoTest {
 	void isNumber(String value, boolean expected) {
 		assertThat(Pattern.matches("[0-9]+$", value)).isEqualTo(expected);
 	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"1:false", "100:false", "999:false", "1000:true", "9999:true", "10000:true"}, delimiter = ':')
+	void isMoreThanThousand(int value, boolean expected) {
+		assertThat(value >= 1000).isEqualTo(expected);
+	}
 }
