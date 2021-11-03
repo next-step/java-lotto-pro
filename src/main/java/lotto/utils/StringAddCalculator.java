efc8b805babab9908d5e83a57decd4a837e6f85d
UNGSEOK CHOI <36910795+ungseokchoi@users.noreply.github.com>
2021-11-03 09:19:48 +0900

    step2 - 문자열 덧셈 계산기 (#68)
    * test: 요구사항1 테스트 케이스 생성
    
    "1,2" split -> {"1","2"}
    "1" split -> {"1"}
    
    * test: 요구사항2 테스트 케이스 생성
    
    "(1,2)" substring -> "1,2"
    
    * test: 요구사항3 테스트 케이스 생성
    
    * test: Set Collection 요구사항1 테스트 케이스 생성
    
    * test: Set Collection 요구사항2 테스트 케이스 생성
    
    * test: Set Collection 요구사항3 테스트 케이스 생성
    
    * test: 공식 문서 조금 따라해보기
    
    * test: StringAddCalculatorTest 생성
    
    1.기본 regex 를 이용한 문자열 분리 후 숫자 합산
    2.커스텀 regex 를 이용한 문자열 분리 후 숫자 합산
    
    * test: StringAddCalculatorTest 테스트 케이스 추가
    
    1. 숫자 형식 검증
    
    * feat: CalculatorConstants 상수 클래스 생성
    
    * refactor: StringAddCalculatorTest
    
    직접 사용할 메소드 구조 형식으로 변경
    
    * feat: StringAddCalculator 최종 생성
    
    * refactor: 피드백 수정 반영
    
    1. 한 라인에 점은 하나만 사용한다.
    2. Exception 은 추상적이지 않게 작성하도록 한다.
    
    * feat: 구현기능 목록 추가 작성
    
    * refactor: 피드백 수정 반영
    
    1. 상수는 final 을 사용하여 불변을 보장받아야한다.
    2. 필요한 메소드 외에 접근제어자를 활용해 안전하게 보호한다.
    
    * fix: validateNumberFormat 오류 수정
    
    1.테스트 하며 수정했던 부분 미반영되어 수정
    
    * refactor: Test 클래스 피드백 수정 반영
    
    1. 최종 클래스를 이용하여 테스트를 진행한다.
    2. given when then 을 준수하다.
    
    * remove: 상수 위치 변경으로 인한 상수클래스 제거
    
    1. 상수는 다양한 곳에서 활용하지 않고
    특정한 클래스에서만 사용될 경우 클래스의 분리보다
    해당 클래스에 작성하는것이 좋다.
    DEFAULT_SPLIT_REGEX
    CalculatorConstants -> StringAddCalculator
    
    * refactor: 상수 위치 변경으로 인한 import 제거

---
+++ src/main/java/lotto/utils/StringAddCalculator.java
@@ -0,0 +1,76 @@
+package lotto.utils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class StringAddCalculator {
+
+    private final static Pattern pattern = Pattern.compile("//(.)\n(.*)");
+    public final static String DEFAULT_SPLIT_REGEX = ",|:";
+
+    public void stringCalculator(String inputString) {
+        int sum = getNumberList(inputString);
+    }
+
+    public int getNumberList(String inputString) {
+        String args = getCheckInputString(inputString);
+        List<Integer> list = splitInputString(args);
+        return list
+                .stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+
+    public String getCheckInputString(String inputString) {
+        if (inputString == null || inputString.isEmpty()) {
+            return "0";
+        }
+        return inputString;
+    }
+
+    private List<Integer> splitInputString(String inputString) {
+        List<String> result = getSplitList(inputString);
+        validateNumberFormat(result);
+        return result
+                .stream()
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+
+    private List<String> getSplitList(String inputString) {
+        Matcher matcher = pattern.matcher(inputString);
+        if (matcher.find()) {
+            return getCustomRegexSplitList(matcher);
+        }
+        return getDefaultRegexSlitList(inputString);
+    }
+
+    private List<String> getCustomRegexSplitList(Matcher matcher) {
+        String customRegex = matcher.group(1);
+        String remainderString = matcher.group(2);
+        return Arrays
+                .asList(remainderString.split(customRegex));
+    }
+
+    private List<String> getDefaultRegexSlitList(String matcher) {
+        return Arrays
+                .asList(matcher.split(DEFAULT_SPLIT_REGEX));
+    }
+
+    private void validateNumberFormat(List<String> strings) {
+        String joinString = String.join("", strings);
+        if (!isNumber(joinString)) {
+            System.out.println("[ERROR] 잘못된 입력 값입니다.");
+            throw new IllegalArgumentException("[ERROR] 잘못된 입력 값입니다.");
+        }
+    }
+
+    private boolean isNumber(String checkValue) {
+        return checkValue.matches("^[0-9]+$");
+    }
+
+}
---
+++ src/test/java/lotto/utils/StringAddCalculatorTest.java
@@ -0,0 +1,93 @@
+package lotto.utils;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class StringAddCalculatorTest {
+
+    private static Pattern pattern = Pattern.compile("//(.)\n(.*)");
+
+    @ParameterizedTest
+    @ValueSource(strings = "1,2:3")
+    @DisplayName("문자열을 분리하여 숫자를 합산")
+    public void getSumNumber(String inputString) {
+        String[] strings = inputString.split(",|:");
+
+        int sum = Arrays.asList(strings).stream().mapToInt(Integer::parseInt).sum();
+
+        assertThat(sum).isEqualTo(6);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = "//,\n1,2,3")
+    @DisplayName("문자열을 커스텀 regex 를 이용하여 분리 후 숫자를 합산")
+    public void getCustomRegexSumNumber(String inputString) {
+        // given
+        String customRegex = ",|:";
+        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(inputString);
+        if (matcher.find()) {
+            customRegex = matcher.group(1);
+            inputString = matcher.group(2);
+        }
+        String[] strings = inputString.split(customRegex);
+
+        // when
+        int sum = Arrays.asList(strings).stream().mapToInt(Integer::parseInt).sum();
+
+
+        // then
+        assertThat(sum).isEqualTo(6);
+    }
+
+
+    static Stream<Arguments> listProvide() {
+        List<String> strings = Arrays.asList("1", "2", "3", "-3");
+        return Stream.of(arguments(strings));
+    }
+
+    @ParameterizedTest
+    @MethodSource("listProvide")
+    @DisplayName("숫자를 제외한 값 또는 음수 값이 있는지 검증")
+    public void validationNumber(List<String> strings) {
+        assertThatThrownBy(() -> {
+            validateNumberFormatTest(strings);
+        }).isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+
+
+    public void validateNumberFormatTest(List<String> strings) {
+        String joinString = String.join("", strings);
+        if (!isNumberTest(joinString)) {
+            System.out.println("[ERROR] 잘못된 입력 값입니다.");
+            throw new IllegalArgumentException("[ERROR] 잘못된 입력 값입니다.");
+        }
+    }
+
+    public boolean isNumberTest(String checkValue) {
+        return checkValue.matches("^[0-9]+$");
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {"//;\n1;2;3;4"})
+    public void stringCalculator(String inputString) {
+        StringAddCalculator stringAddCalculator = new StringAddCalculator();
+        int sum = stringAddCalculator.getNumberList(inputString);
+        assertThat(sum).isEqualTo(10);
+    }
+
+}
