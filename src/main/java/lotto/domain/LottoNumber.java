40d7e595d7108bd59f1d160ed251696c874603e5
peter-alethio <peter@alethio.io>
2021-11-03 21:22:23 +0900

    feat(lottoNumber): 1이상 45이하 숫자만 가능

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -4,10 +4,19 @@ import java.util.Objects;
 
 public class LottoNumber {
 
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
     private final int number;
 
     public LottoNumber(int number) {
+        check(number);
         this.number = number;
+    }
+
+    private void check(int number) {
+        if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
+            throw new IllegalArgumentException("1이상 45이하의 숫자만 가능합니다");
+        }
     }
 
     @Override
--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -1,13 +1,23 @@
 package lotto.domain;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class LottoNumberTest {
 
     @Test
     void 로또번호를_생성한다() {
         assertThat(new LottoNumber(1)).isEqualTo(new LottoNumber(1));
+    }
+
+    @ParameterizedTest(name = "숫자 {0}은 로또 번호가 될 수 없다")
+    @ValueSource(ints = {-1, 0, 46})
+    void 로또번호는_1이상_45이하의_숫자만_가진다(int number) {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> new LottoNumber(number));
     }
 }
