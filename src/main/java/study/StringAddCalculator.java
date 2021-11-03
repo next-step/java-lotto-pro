4e4badf143659f0e2db8797e7addb58bdc55550e
김도영 <95doyoung@gmail.com>
2021-11-03 15:30:16 +0900

    feat (splitAndSum) :  “//”와 “\n” 문자 사이에 커스텀 구분자를 지정할 수 있다. (예 : “//;\n1;2;3” => 6)

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -1,20 +1,33 @@
 package study;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
-    private static final String DEFAULT_SEPARATOR = "[,:]";
+    private static final String DEFAULT_SEPARATORS = "[,:]";
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
 
     public static int splitAndSum(final String text) {
         if (isEmptyOrNull(text)) {
             return 0;
         }
-        
-        final String[] splitText = text.split(DEFAULT_SEPARATOR);
+
+        final String[] splitText = split(text);
         if (splitText.length > 1) {
             return sum(splitText);
         }
 
         final int number = Integer.parseInt(text);
         return number;
+    }
+
+    private static String[] split(final String text) {
+        final Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(text);
+        if (matcher.find()) {
+            final String customDelimiter = matcher.group(1);
+            return matcher.group(2).split(customDelimiter);
+        }
+        return text.split(DEFAULT_SEPARATORS);
     }
 
     private static int sum(final String[] splitText) {
