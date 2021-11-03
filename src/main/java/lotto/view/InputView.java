4b356fc5ebff3455ea937c1fe4019c47c59561e6
shryu <>
2021-11-04 01:31:15 +0900

    refactor : 불필요한 scanner nextLine 삭제 후 nextInt 처리 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,12 +7,11 @@ public class InputView {
 
     public static int inputBoughtMoney() {
         System.out.println("구입금액을 입력해 주세요.");
-        return scanner.nextInt();
+        return Integer.parseInt(scanner.nextLine());
     }
 
     public static String inputWinningNumber() {
         try {
-            scanner.nextLine();
             System.out.println("지난 주 당첨 번호를 입력해 주세요.");
             return scanner.nextLine();
         } finally {
