5e5d6b01c7b368cf9b1b6ae156a4841c327eba48
mnonm <ppsosqq@gmail.com>
2021-11-04 00:02:44 +0900

    Merge pull request #76
    * docs(README.md): step2 기능 요구 사항
    
    * feat(StringAddCalculator): 빈 문자열 또는 null 값을 입력할 경우 0을 반환
    
    * feat(StringAddCalculator): 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환
    
    * feat(StringAddCalculator): 숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환
    
    * feat(StringAddCalculator): 구분자를 컴마(,) 이외에 콜론(:)을 사용
    
    * feat(StringAddCalculator): “//”와 “\n” 문자 사이에 커스텀 구분자 지정
    
    * feat(StringAddCalculator): 숫자 이외의 값 또는 음수를 전달할 경우 예외 발생
    
    * refactor(StringAddCalculator, Splitter): split, sum 역할 분리
    
    * refactor(StringAddCalculator, Splitter): 중복 정규식 변수 제거
    
    * docs(empty): 미사용 파일 제거

---
+++ src/main/java/step2/StringAddCalculator.java
@@ -0,0 +1,30 @@
+package step2;
+
+import java.util.Arrays;
+
+public class StringAddCalculator {
+	public static final String NUMBER_REGEX = "^[0-9]+$";
+
+	public static int splitAndSum(String input) {
+		String[] numbers = Splitter.split(input);
+		validateNumbers(numbers);
+		return sum(numbers);
+	}
+
+	private static int sum(String[] numbers) {
+		return Arrays.stream(numbers)
+			.mapToInt(Integer::parseInt)
+			.sum();
+	}
+
+	private static void validateNumbers(String[] numbers) {
+		if (isNegativeNumberOrNotNumber(numbers)) {
+			throw new RuntimeException();
+		}
+	}
+
+	private static boolean isNegativeNumberOrNotNumber(String[] numbers) {
+		return Arrays.stream(numbers)
+			.anyMatch(number -> !number.matches(NUMBER_REGEX));
+	}
+}
