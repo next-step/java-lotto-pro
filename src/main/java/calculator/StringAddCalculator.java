9d958d80f56cd46f6a721568fc8e4210ccd40dc9
Jeongkyo Kim <chavitak@gmail.com>
2021-11-03 22:39:39 +0900

    refactor: 각 메서드가 한 가지 역할만 수행하도록 리팩토링한다.

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,31 +1,38 @@
 package calculator;
 
 import java.util.Arrays;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
+
+    public static final String FIXED_DELIMITER = "[,:]";
+    public static final String USER_DELIMITER_PATTERN = "//(.)\n(.*)";
+
     public static int splitAndSum(String text) {
         if (text == null || text.isEmpty()) {
             return 0;
         }
-        try {
-            return Arrays.stream(text.split("[,:]"))
-                    .mapToInt(Integer::parseInt)
-                    .map(StringAddCalculator::parsePositiveInteger)
-                    .sum();
-        } catch (NumberFormatException ignored) {
-        }
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        Optional<String[]> tokens = getTokens(text);
+        return getSum(tokens);
+    }
+
+    private static int getSum(Optional<String[]> tokens) {
+        return Arrays.stream(tokens.get())
+                .mapToInt(Integer::parseInt)
+                .map(StringAddCalculator::parsePositiveInteger)
+                .sum();
+    }
+
+    private static Optional<String[]> getTokens(String text) {
+        Optional<String[]> tokens = Optional.of(text.split(FIXED_DELIMITER));
+        Matcher m = Pattern.compile(USER_DELIMITER_PATTERN).matcher(text);
         if (m.find()) {
             String customDelimiter = m.group(1);
-            String[] tokens = m.group(2).split(customDelimiter);
-            return Arrays.stream(tokens)
-                    .mapToInt(Integer::parseInt)
-                    .map(StringAddCalculator::parsePositiveInteger)
-                    .sum();
+            tokens = Optional.of(m.group(2).split(customDelimiter));
         }
-        return 0;
+        return tokens;
     }
 
     private static int parsePositiveInteger(Integer integer) {
