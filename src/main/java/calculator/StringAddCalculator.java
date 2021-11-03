df8fb4ec3a7fd2fe3ea8384b59f7a4aa4b4e87b2
Jeongkyo Kim <chavitak@gmail.com>
2021-11-03 22:02:11 +0900

    feat: 문자열 계산기에 숫자 이외의 값 또는 음수를 전달하는 경우 RuntimeException 예외를 throw한다.

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -12,6 +12,7 @@ public class StringAddCalculator {
         try {
             return Arrays.stream(text.split("[,:]"))
                     .mapToInt(Integer::parseInt)
+                    .map(StringAddCalculator::parsePositiveInteger)
                     .sum();
         } catch (NumberFormatException ignored) {
         }
@@ -21,8 +22,16 @@ public class StringAddCalculator {
             String[] tokens = m.group(2).split(customDelimiter);
             return Arrays.stream(tokens)
                     .mapToInt(Integer::parseInt)
+                    .map(StringAddCalculator::parsePositiveInteger)
                     .sum();
         }
         return 0;
+    }
+
+    private static int parsePositiveInteger(Integer integer) {
+        if (integer < 0) {
+            throw new RuntimeException();
+        }
+        return integer;
     }
 }
