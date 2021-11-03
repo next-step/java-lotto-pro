82c898f5fde7ff1be6f7b4d9556e729b17663f5c
tyanotyrano <tyrano09@gmail.com>
2021-11-03 19:56:40 +0900

    feat(LottoNumberGenerator.java): 로또 자동 생성 기능 구현
    자동으로 로또 번호를 생성하는 기능을 구현한다.
    - LottoNumberGenerator : 1 ~ 45 사이의 서로 중복되지 않는 6개의 숫자를 반환
    - NumberGenerator : 숫자 목록을 반환하는 기능을 추상화한 Interface
    - LottoNumberConstant : 로또 숫자와 관련된 상수들을 정의
    - README.md : 완료된 기능 요구사항 체크

---
+++ src/main/java/lotto/constant/LottoNumberConstant.java
@@ -0,0 +1,7 @@
+package lotto.constant;
+
+public class LottoNumberConstant {
+	public static final int LOTTO_NUMBER_MIN = 1;
+	public static final int LOTTO_NUMBER_MAX = 45;
+	public static final int VALID_LOTTO_NUMBER_COUNT = 6;
+}
