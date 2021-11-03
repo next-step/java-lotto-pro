999dfde876e2fbab04a8db62decde367a7b3d643
sungwoo <ssw890418@gmail.com>
2021-11-03 22:32:49 +0900

    String 클래스에 대한 학습 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,14 +1,38 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
 
     @Test
     void split() {
         String[] result = "1,2".split(",");
+        assertThat(result).contains("1", "2");
         assertThat(result).containsExactly("1","2");
+    }
+
+    @Test
+    void substring() {
+        String str = "(1,2)";
+        String result = str.substring(str.indexOf("(")+1, str.indexOf(")"));
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("특정  위치의 문자를 가져오는 테스트")
+    void charAt() {
+        char result = "abc".charAt(2);
+        assertThat(result).isEqualTo('c');
+    }
+
+    @Test
+    @DisplayName("문자열의 범위를 벗어난 index를 charAt메소드의 전달인자로 사용시 예외발생 테스트")
+    void charAtUsingInvalidIndex() {
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> "abc".charAt(3))
+                .withMessageMatching("String index out of range: \\d+");
     }
 }
