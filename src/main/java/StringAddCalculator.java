082d8fd6c06efad81049eef70a825269d2fcd460
eedys <eedys@ex-em.com>
2021-11-03 20:04:21 +0900

    문자열(숫자가 아닌 문자 or 음수)를 숫자로 변화할 시 예외 기능 개발

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -4,6 +4,17 @@ public class StringAddCalculator {
 
     public int calculate(String input) {
         if(Objects.isNull(input) || input.isEmpty()) return 0;
+        validate(input);
         return Integer.parseInt(input);
+    }
+
+    private void validate(String input) {
+        try {
+            if(Integer.parseInt(input) < 0) {
+                throw new IllegalArgumentException();
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
     }
 }
--- src/test/java/StringAddCalculatorTest.java
+++ src/test/java/StringAddCalculatorTest.java
@@ -1,8 +1,11 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringAddCalculatorTest {
 
@@ -20,9 +23,16 @@ public class StringAddCalculatorTest {
         assertThat(calculator.calculate(null)).isEqualTo(0);
     }
 
-    @DisplayName("문자열을 숫자로 변환하는 기능 테스트")
+    @DisplayName("문자열(숫자)을 숫자로 변환하는 기능 테스트")
     @Test
     public void stringToIntTest() {
         assertThat(calculator.calculate("1")).isEqualTo(1);
+    }
+
+    @DisplayName("문자열(숫자가 아닌 or 음수)을 숫자로 변환할 시 예외 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"TEST", "-1"})
+    public void stringToInt_ThrowExceptionTest(String fixture) {
+        assertThatThrownBy(() -> calculator.calculate(fixture)).isInstanceOf(IllegalArgumentException.class);
     }
 }
