8987a974cb416cf062e40a086de13f485e7bdd40
Chaeyun Lee <chaeyunn@iochord.com>
2021-11-03 13:48:19 +0900

    test: 주요기능에 대한 기본 테스트케이스 작성

---
+++ src/main/java/lotto/LottoApp.java
@@ -0,0 +1,9 @@
+package lotto;
+
+public class LottoApp {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/test/java/lotto/LottoAppTest.java
@@ -0,0 +1,32 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoAppTest {
+
+	@Test
+	@DisplayName("구매 금액에 맞는 구매 갯수를 계산해야 한다.(가격: 1000원)")
+	public void lottoCountTest() {
+
+	}
+
+	@Test
+	@DisplayName("갯수에 맞게 로또번호(오름차순, 1~45, 6개 번호)들을 생성해야 한다(")
+	public void generateLottoNumbersTest() {
+
+	}
+
+	@Test
+	@DisplayName("당첨번호와 로또번호들이 주어지면, 당첨내역들을 계산해야 한다")
+	public void calculateWinningPoints() {
+
+	}
+
+	@Test
+	@DisplayName("당첨 내역과 구매 금액이 주어지면, 수익률을 계산해야 한다")
+	public void calculateProfit() {
+
+	}
+
+}
