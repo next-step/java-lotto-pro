9c9f79db6e70ab6bc2ff22752cf33930efd21138
tyanotyrano <tyrano09@gmail.com>
2021-11-04 00:43:21 +0900

    feat(Lotto.java): 로또 당첨 여부 확인
    당첨번호와 비교하여 로또 당첨 여부를 확인한다.
    - Lotto : 로또 당첨 여부 확인 기능 추가
    - WinningNumbers : 당첨인지 판단하는 기능 추가
    - README.md : 완료된 기능 요구사항 체크

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,11 +1,14 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import lotto.utils.LottoNumberMapper;
 import lotto.utils.LottoNumberValidator;
 
 public class WinningNumbers {
+	public static final int WINNING_LOTTO_CONDITION_MIN_NUMBER_SIZE = 3;
+
 	private final List<LottoNumber> winningNumbers;
 
 	private WinningNumbers(List<LottoNumber> winningNumbers) {
@@ -17,6 +20,22 @@ public class WinningNumbers {
 		LottoNumberValidator.validateLottoNumbers(winningNumbers);
 
 		return new WinningNumbers(winningNumbers);
+	}
+
+	public static WinningNumbers createDefault() {
+		return new WinningNumbers(new ArrayList<>());
+	}
+
+	public boolean isWinningNumber(LottoNumber lottoNumber) {
+		return this.winningNumbers.contains(lottoNumber);
+	}
+
+	public boolean isWinning() {
+		return this.winningNumbers.size() >= WINNING_LOTTO_CONDITION_MIN_NUMBER_SIZE;
+	}
+
+	public void add(LottoNumber lottoNumber) {
+		this.winningNumbers.add(lottoNumber);
 	}
 
 	public int getSize() {
