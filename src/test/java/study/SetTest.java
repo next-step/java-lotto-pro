b2790dbcc612664f4c643b4a8d70bbdce8ad4a99
daehyun0 <51039838+daehyun0@users.noreply.github.com>
2021-11-03 11:49:45 +0900

    1단계 - 학습 테스트 실습 (#80)
    * test: String 학습테스트 추가
    
    * test: StringTest의 반복되는 테스트를 ParameterizedTest로 변경
    
    * test: MethodOrder를 이용해 테스트 순서 조절
    
    - 작성 순서대로 순서를 조절하기 위해 DisplayName에 숫자 표기
    
    * test: 테스트 메소드 이름 단순하게 변경
    
    - 테스트 메소드의 이름은 클래스의 어떤 메소드를 테스트할지에 집중
    
    * test: Set 학습테스트 추가
    
    * test: TestMethodOrder 변경
    
    - DisplayName을 이용해 정렬하던 것을 Order 어노테이션 기준으로 변경

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,53 @@
+package study;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class SetTest {
+	private Set<Integer> numbers;
+
+	@BeforeEach
+	void setUp() {
+		numbers = new HashSet<>();
+		numbers.add(1);
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+	}
+
+	@Test
+	@DisplayName("Set.size()가 중복된 요소 제거를 반영하는지 크기를 반환하는지 확인")
+	@Order(1)
+	void test_size1() {
+		assertThat(numbers.size()).isEqualTo(3);
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"1", "2", "3"})
+	@DisplayName("Set.contains가 추가한 요소에 true를 반환하는지 확인")
+	@Order(2)
+	void test_contains1(int element) {
+		assertThat(numbers.contains(element)).isTrue();
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"1,true", "2,true", "3,true", "4,false", "5,false"})
+	@DisplayName("Set.contains가 추가한 요소에는 true를, 추가하지 않은 요소에는 false를 반환하는지 확인")
+	@Order(3)
+	void test_contains2(int element, boolean isContain) {
+		assertThat(numbers.contains(element)).isEqualTo(isContain);
+	}
+}
---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,53 @@
+package study;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class StringTest {
+
+	@Test
+	@DisplayName("\"1,2\"을 ,로 split 했을 때 [1,2] 배열 반환")
+	@Order(1)
+	void test_split1() {
+		assertThat("1,2".split(",")).containsExactly("1", "2");
+	}
+
+	@Test
+	@DisplayName("\"1\"을 ,로 split 했을 때 [1] 배열 반환")
+	@Order(2)
+	void test_split2() {
+		assertThat("1".split(",")).containsExactly("1");
+	}
+
+	@Test
+	@DisplayName("\"(1,2)\" 값이 주어졌을 때 substring 메서드의 파라미터로 1,4를 넘겨주면 1,2 반환")
+	@Order(3)
+	void test_substring1() {
+		assertThat("(1,2)".substring(1, 4)).isEqualTo("1,2");
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"0,a", "1,b", "2,c"})
+	@DisplayName("\"abc\" 값이 주어졌을 때 String의 charAt() 메소드 파라미터로 인덱스를 넘겨주면 char 반환")
+	@Order(4)
+	void test_charAt1(int idx, char result) {
+		assertThat("abc".charAt(idx)).isEqualTo(result);
+	}
+
+	@ParameterizedTest
+	@CsvSource(value= {"3", "4"})
+	@DisplayName("\"abc\" 값이 주어졌을 때 String의 charAt() 메소드 파라미터로 문자열 길이보다 같거나 큰 인덱스를 넘겨주면 예외 발생")
+	@Order(5)
+	void test_charAt2(int idx) {
+		assertThatThrownBy(() -> "abc".charAt(idx)).isInstanceOf(StringIndexOutOfBoundsException.class)
+			.hasMessageMatching(String.format("String index out of range: %d", idx));
+	}
+}
