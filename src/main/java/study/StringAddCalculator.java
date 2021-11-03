bda2fdeb66293b980e98e7f03a5fd6dcd3450290
김도영 <95doyoung@gmail.com>
2021-11-03 15:11:29 +0900

    feat (splitAndSum) :  숫자 두개를 컴마(,) 구분자로 입력될 경우 두 숫자의 합을 반환 하는 기능 추가

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -6,8 +6,22 @@ public class StringAddCalculator {
         if (isEmptyOrNull(text)) {
             return 0;
         }
+
+        final String[] splitText = text.split(",");
+        if (splitText.length > 1) {
+            return sum(splitText);
+        }
+
         final int number = Integer.parseInt(text);
         return number;
+    }
+
+    private static int sum(final String[] splitText) {
+        int sum = 0;
+        for (final String text : splitText) {
+            sum += Integer.parseInt(text);
+        }
+        return sum;
     }
 
     private static boolean isEmptyOrNull(final String text) {
