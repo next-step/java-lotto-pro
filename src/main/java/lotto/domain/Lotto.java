e0512df5aa5d31d32a24207e93d76c588c8bc649
peter-alethio <peter@alethio.io>
2021-11-03 21:31:07 +0900

    feat(lotto): 6개의 로또 번호

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -6,11 +6,19 @@ import java.util.Objects;
 
 public class Lotto {
 
+    private static final int MAX_SIZE = 6;
     private final List<LottoNumber> lotto = new ArrayList<>();
 
     public Lotto(final List<Integer> lotto) {
+        check(lotto);
         for (Integer number : lotto) {
             this.lotto.add(new LottoNumber(number));
+        }
+    }
+
+    private void check(List<Integer> lotto) {
+        if (lotto.size() != MAX_SIZE) {
+            throw new IllegalArgumentException("로또 번호는 6개여야 합니다");
         }
     }
 
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -5,11 +5,18 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class LottoTest {
 
     @Test
     void 로또를_생성한다() {
         assertThat(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6))).isEqualTo(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)));
+    }
+
+    @Test
+    void 로또는_6개의_숫자만_가진다() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6, 7)));
     }
 }
