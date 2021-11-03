605cc795b9fda248557dac2c79d337d9331d5551
minho <lights8615@gmail.com>
2021-11-03 19:18:49 +0900

    feat(step3): 입력받은 구입 금액으로 구입 개수를 반환

--- src/main/java/lotto/LottoMoney.java
+++ src/main/java/lotto/LottoMoney.java
@@ -1,9 +1,14 @@
 package lotto;
 
 public class LottoMoney {
+    private static final int LOTTO_PRICE = 1000;
     private int money;
 
     public LottoMoney(String moneyText) {
         money = Integer.parseInt(moneyText);
+    }
+
+    public int calculateLottoCount() {
+        return money / LOTTO_PRICE;
     }
 }
---
+++ src/test/java/lotto/LottoMoneyTest.java
@@ -0,0 +1,15 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class LottoMoneyTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"1000,1", "0,0", "999,0", "1001,1"})
+    void calculateLottoCount_success(String moneyText, int lottoCount) {
+        assertThat(new LottoMoney(moneyText).calculateLottoCount()).isEqualTo(lottoCount);
+    }
+}
