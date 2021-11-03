22ec09da0fe76671bd8209a40a85efadb6393b60
eedys <eedys@ex-em.com>
2021-11-03 19:49:03 +0900

    입력값이 빈 문자열 or null 값일 시 0 리턴하는 기능 개발

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,6 +1,10 @@
+import java.util.Objects;
+
 public class StringAddCalculator {
 
     public int calculate(String input) {
+        if(Objects.isNull(input)) return 0;
+        if(input.isEmpty()) return 0;
         return 0;
     }
 }
