ddfdcdafb7694b45618f72a8c43cb4bf90bc4463
류성호 <>
2021-11-03 10:44:15 +0900

    style : LottoNumbers Integer List 에서 String List로 변경

--- src/main/java/lotto/domain/LottoNumbers.java
+++ src/main/java/lotto/domain/LottoNumbers.java
@@ -6,16 +6,16 @@ import java.util.List;
 public class LottoNumbers {
     private static final Integer LOTTO_START_NUMBER = 1;
     private static final Integer LOTTO_END_NUMBER = 45;
-    private List<Integer> lottoNumbers = new ArrayList<>();
+    private List<String> lottoNumbers = new ArrayList<>();
 
-    public LottoNumbers(List<Integer> lottoNumbers) {
+    public LottoNumbers(List<String> lottoNumbers) {
         validateNumbers(lottoNumbers);
         this.lottoNumbers = lottoNumbers;
     }
 
-    private void validateNumbers(List<Integer> lottoNumbers) {
+    private void validateNumbers(List<String> lottoNumbers) {
         lottoNumbers.stream()
-                .filter(l -> l < LOTTO_START_NUMBER || l > LOTTO_END_NUMBER)
+                .filter(l -> Integer.parseInt(l) < LOTTO_START_NUMBER || Integer.parseInt(l) > LOTTO_END_NUMBER)
                 .forEach(l -> {
                     throw new IllegalArgumentException("");
                 });
@@ -27,7 +27,7 @@ public class LottoNumbers {
                         .count();
     }
 
-    private boolean isContainNumber(Integer lottoNumber) {
+    private boolean isContainNumber(String lottoNumber) {
         return lottoNumbers.contains(lottoNumber);
     }
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -17,41 +17,41 @@ public class LottoTest {
 
     @BeforeEach
     void init() {
-        winningNumbers = new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6));
+        winningNumbers = new LottoNumbers(Arrays.asList("1", "2", "3", "4", "5", "6"));
     }
 
     @Test
     void 로또번호_범위_에러() {
         assertAll(
                 () -> assertThatThrownBy(() ->
-                        new Lotto(new LottoNumbers(Arrays.asList(0, 2, 3, 7, 8, 9))))
+                        new Lotto(new LottoNumbers(Arrays.asList("0", "2", "3", "7", "8", "9"))))
                         .isInstanceOf(IllegalArgumentException.class),
                 () -> assertThatThrownBy(() ->
-                        new Lotto(new LottoNumbers(Arrays.asList(1, 2, 3, 7, 8, 46))))
+                        new Lotto(new LottoNumbers(Arrays.asList("1", "2", "3", "7", "8", "46"))))
                         .isInstanceOf(IllegalArgumentException.class));
     }
 
     @Test
     void 로또_4등() {
-        Lotto lotto = new Lotto(new LottoNumbers(Arrays.asList(1, 2, 3, 7, 8, 9)));
+        Lotto lotto = new Lotto(new LottoNumbers(Arrays.asList("1", "2", "3", "7", "8", "9")));
         assertThat(lotto.compareWinningNumbers(winningNumbers)).isEqualTo(LottoPrize.FOURTH);
     }
 
     @Test
     void 로또_3등() {
-        Lotto lotto = new Lotto(new LottoNumbers(Arrays.asList(1, 2, 3, 4, 8, 9)));
+        Lotto lotto = new Lotto(new LottoNumbers(Arrays.asList("1", "2", "3", "4", "8", "9")));
         assertThat(lotto.compareWinningNumbers(winningNumbers)).isEqualTo(LottoPrize.THIRD);
     }
 
     @Test
     void 로또_2등() {
-        Lotto lotto = new Lotto(new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 9)));
+        Lotto lotto = new Lotto(new LottoNumbers(Arrays.asList("1", "2", "3", "4", "5", "9")));
         assertThat(lotto.compareWinningNumbers(winningNumbers)).isEqualTo(LottoPrize.SECOND);
     }
 
     @Test
     void 로또_1등() {
-        Lotto lotto = new Lotto(new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6)));
+        Lotto lotto = new Lotto(new LottoNumbers(Arrays.asList("1", "2", "3", "4", "5", "6")));
         assertThat(lotto.compareWinningNumbers(winningNumbers)).isEqualTo(LottoPrize.FIRST);
     }
 }
