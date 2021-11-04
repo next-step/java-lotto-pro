4ef5f5858901d228fd9291d1f3e1a32eaf6181db
lewisseo91 <togokr@naver.com>
2021-11-05 03:01:47 +0900

    feat(test): StringAddCalculatorTest 테스트 추가
    StringAddCalculatorTest 테스트 추가
    - 커스텀 구분자 없이 입력 후 합계 확인
    - 커스텀 구분자가 있는 입력 후 합계 확인
    - 값 하나 합계 테스트
    - 값 여러개 합계 테스트

--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -16,4 +16,48 @@ public class StringAddCalculatorTest {
         // then
         assertThat(result).isEqualTo(0);
     }
+
+    @DisplayName("커스텀 구분자 없이 입력 후 합계 확인")
+    @Test
+    void splitAndSumNullWithoutCustomDelimiter() {
+        // given
+        String text = "1,2,3";
+        // when
+        int result = StringAddCalculator.splitAndSum(text);
+        // then
+        assertThat(result).isEqualTo(6);
+    }
+
+    @DisplayName("커스텀 구분자가 있는 입력 후 합계 확인")
+    @Test
+    void splitAndSumNullWithCustomDelimiter() {
+        // given
+        String text = "//b\n1b2b3";
+        // when
+        int result = StringAddCalculator.splitAndSum(text);
+        // then
+        assertThat(result).isEqualTo(6);
+    }
+
+    @DisplayName("값 하나 합계 테스트")
+    @Test
+    void splitAndSumNullSingle() {
+        // given
+        String text = "//b\n1";
+        // when
+        int result = StringAddCalculator.splitAndSum(text);
+        // then
+        assertThat(result).isEqualTo(1);
+    }
+
+    @DisplayName("값 여러개 합계 테스트")
+    @Test
+    void splitAndSumNullMultiple() {
+        // given
+        String text = "//b\n1,2b3";
+        // when
+        int result = StringAddCalculator.splitAndSum(text);
+        // then
+        assertThat(result).isEqualTo(6);
+    }
 }
