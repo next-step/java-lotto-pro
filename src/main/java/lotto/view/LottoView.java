4d2979c688ea118bc060ad4761e6850ea0b93c2d
peter-alethio <peter@alethio.io>
2021-11-05 00:23:34 +0900

    refactor(lottoView): 포멧 추가

--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -15,7 +15,7 @@ public class LottoView {
     }
 
     public static void displayCount(int count) {
-        System.out.println(count + "개를 구매했습니다.");
+        System.out.printf("%d개를 구매했습니다.\n", count);
     }
 
     public static void displayCount(Money money) {
@@ -41,9 +41,9 @@ public class LottoView {
         System.out.println("---------");
 
         for (Map.Entry<Rank, Integer> entry : result.getResult().entrySet()) {
-            System.out.println(entry.getKey().getCount() + "개 일치 (" + entry.getKey().getPrice() + "원)- " + entry.getValue() +"개");
+            System.out.printf("%d개 일치 (%d원)- %d개\n", entry.getKey().getCount(), entry.getKey().getPrice(), entry.getValue());
         }
 
-        System.out.println("총 수익률은 " + result.getProfitRate() + "입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)");
+        System.out.printf("총 수익률은 %.2f입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)", result.getProfitRate());
     }
 }
