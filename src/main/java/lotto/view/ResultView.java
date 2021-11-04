f8830ff35a90a398e55044faeee7cab186fe5ca0
Chaeyun Lee <chaeyunn@iochord.com>
2021-11-05 01:19:09 +0900

    refactor: vo 잘못된 변수명 수정, 로또번호 출력

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -1,5 +1,6 @@
 package lotto.view;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import lotto.domain.LottoNumber;
@@ -11,23 +12,31 @@ public class ResultView {
 		System.out.println(count + "개를 구매했습니다.");
 		StringBuilder result = new StringBuilder();
 
-		for (LottoNumber numbers : numbersList) {
+		for (LottoNumber lottoNumber : numbersList) {
 			result.append("[ ");
-			result.append(numbers.toString());
+			result.append(showLottoNumber(lottoNumber));
 			result.append(" ]\n");
 		}
 		System.out.println(result);
 	}
 
-	public static void showPrize(LottoPrize vo, double profit) {
+	public static void showPrize(LottoPrize lottoPrize, double profit) {
 		String result = "\n당첨통계\n";
 		result += "---------\n";
-		result += "3개 일치 (5000원)- " + vo.getCnt4St() + "개\n";
-		result += "4개 일치 (50000원)- " + vo.getCnt3St() + "개\n";
-		result += "5개 일치 (1500000원)- " + vo.getCnt2St() + "개\n";
-		result += "6개 일치 (2000000000원)- " + vo.getCnt1St() + "개\n";
+		result += "3개 일치 (5000원)- " + lottoPrize.getCnt4St() + "개\n";
+		result += "4개 일치 (50000원)- " + lottoPrize.getCnt3St() + "개\n";
+		result += "5개 일치 (1500000원)- " + lottoPrize.getCnt2St() + "개\n";
+		result += "6개 일치 (2000000000원)- " + lottoPrize.getCnt1St() + "개\n";
 		System.out.println(result);
 		System.out.printf("총 수익률은 %.2f입니다\n", profit);
+	}
+
+	public static String showLottoNumber(LottoNumber lottoNumber) {
+		List<String> elements = new ArrayList<>();
+		for (int number : lottoNumber.getNumbers()) {
+			elements.add(String.valueOf(number));
+		}
+		return String.join(",", elements);
 	}
 
 }
