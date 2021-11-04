50789240f42ff5832a2ab70718835245b5a992f6
ddabizz <ddabizz@gmail.com>
2021-11-04 15:38:05 +0900

    feat: 문자열 계산기 기능 추가

--- src/main/java/StringUtil.java
+++ src/main/java/StringUtil.java
@@ -1,7 +1,13 @@
+import java.util.Objects;
+
 public class StringUtil {
 
     public static String[] splitString(String str){
         return str.split(",");
+    }
+    public static String[] splitString(String str, String delimiter){
+
+        return str.split(delimiter);
     }
 
     public static String removeParenthesis(String str){
@@ -11,5 +17,16 @@ public class StringUtil {
     public static String findCharUseIndex(int index){
         String abc = "abc";
         return Character.toString(abc.charAt(index));
+    }
+
+    public static boolean isNullOrEmpty(String str){
+        if(Objects.isNull(str)) return true;
+        if(str.isEmpty()) return true;
+        return false;
+    }
+
+    public static boolean isOneNumber(String str){
+        if(str.length() == 1) return true;
+        return false;
     }
 }
