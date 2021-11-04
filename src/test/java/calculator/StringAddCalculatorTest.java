a608cd50508c0a7790ecfba0e36f6f5c8e703c94
Junhwan Kim <junhwan.tech@gmail.com>
2021-11-04 23:45:54 +0900

    refactor: step2 테스트 메소드 시그니처에서 throws Exception 제거

--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -22,7 +22,7 @@ public class StringAddCalculatorTest {
 	@ParameterizedTest
 	@ValueSource(strings = {"1", "2", "13"})
 	@DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.")
-	public void splitAndSum_숫자하나(String input) throws Exception {
+	public void splitAndSum_숫자하나(String input) {
 		int result = StringAddCalculator.splitAndSum(input);
 		assertThat(result).isEqualTo(Integer.parseInt(input));
 	}
@@ -30,7 +30,7 @@ public class StringAddCalculatorTest {
 	@ParameterizedTest
 	@CsvSource(value = {"1,2|3", "4,5|9", "13,2|15"}, delimiter = '|')
 	@DisplayName("숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.")
-	public void splitAndSum_쉼표구분자(String input, int expected) throws Exception {
+	public void splitAndSum_쉼표구분자(String input, int expected) {
 		int result = StringAddCalculator.splitAndSum(input);
 		assertThat(result).isEqualTo(expected);
 	}
@@ -38,7 +38,7 @@ public class StringAddCalculatorTest {
 	@ParameterizedTest
 	@CsvSource(value = {"1,2:3|6", "4,5:6|15", "2,7:3|12"}, delimiter = '|')
 	@DisplayName("구분자를 컴마(,) 이외에 콜론(:)을 사용할 수 있다.")
-	public void splitAndSum_쉼표_또는_콜론_구분자(String input, int expected) throws Exception {
+	public void splitAndSum_쉼표_또는_콜론_구분자(String input, int expected) {
 		int result = StringAddCalculator.splitAndSum(input);
 		assertThat(result).isEqualTo(expected);
 	}
@@ -46,14 +46,14 @@ public class StringAddCalculatorTest {
 	@ParameterizedTest
 	@ValueSource(strings = {"//;\n1;2;3", "//#\n1#2#3", "//@\n1@2@3"})
 	@DisplayName("// 와 \n 문자 사이에 커스텀 구분자를 지정할 수 있다.")
-	public void splitAndSum_custom_구분자(String input) throws Exception {
+	public void splitAndSum_custom_구분자(String input) {
 		int result = StringAddCalculator.splitAndSum(input);
 		assertThat(result).isEqualTo(6);
 	}
 
 	@Test
 	@DisplayName("음수를 전달할 경우 RuntimeException 예외가 발생한다.")
-	public void splitAndSum_negative() throws Exception {
+	public void splitAndSum_negative() {
 		assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
 			.isInstanceOf(RuntimeException.class);
 	}
