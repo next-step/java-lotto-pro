282142b9061e402d2d922fee559cc18bccb405d4
jung0930 <wjddlqkqh57@naver.com>
2021-11-05 03:13:19 +0900

    feat, test : 수동으로 뽑고 남은 뽑기횟수만큼 로또를 자동생성하도록 기능 수정, 테스트 코드 수정

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoMachineTest {
@@ -13,7 +16,9 @@ class LottoMachineTest {
     @ParameterizedTest
     @CsvSource(value = {"1500,1", "5000,5", "10000,10"})
     void buy(int payment, int count) {
-        assertThat(LottoMachine.buy(Payment.from(payment), new AutoNumberGenerationStrategy()).count()).isEqualTo(count);
+        List<String> manualLottos = Arrays.asList("1,2,3,4,5,6");
+
+        assertThat(LottoMachine.buy(Payment.from(payment), new AutoNumberGenerationStrategy(), manualLottos).count()).isEqualTo(count);
     }
 
 }
