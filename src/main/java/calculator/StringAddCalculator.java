9d307c3058b535b2de0b4027fe4eb5343d3285d8
Jeongkyo Kim <43088225+chapitak@users.noreply.github.com>
2021-11-04 09:47:42 +0900

    2단계 - 문자열 덧셈 계산기 (#105)
    * docs: 문자열 덧셈 계산기에 대한 요구사항을 추가한다
    
    * test: StringAddCalculatorTest 추가
    
    * feat: 빈 문자열 또는 null 값을 입력할 경우 0을 반환한다
    
    - calculator 패키지를 만들고 StringAddCalculator 클래스를 생성한다
    - splitAndSum_null_또는_빈문자 테스트를 통과할 수 있도록 splitAndSum() 메서드를 생성한다
    
    * feat:  숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.
    
    - splitAndSum_쉼표구분자() 테스트를 통과할 수 있도록 splitAndSum() 메서드를 수정한다
    
    * file: empty.txt 파일을 삭제한다
    
    * feat: 숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을반환한다
    
    * refactor: 구분자가 없는 경우에 대한 예외처리를 삭제한다
    
    - 구분자가 없는 경우에 대한 예외처리는 split()을 활용해 구현할 수 있기에  더 이상 불필요하므로 삭제한다
    
    * feat:  구분자를 컴마(,) 이외에 콜론(:)을 사용할 수 있다.
    
    - split() 메서드의 구분자로 콜론을 추가한다
    
    * feat: “//”와 “\n” 문자 사이에 커스텀 구분자를 지정할 수 있다.
    
    * feat: 문자열 계산기에 숫자 이외의 값 또는 음수를 전달하는 경우 RuntimeException 예외를 throw한다.
    
    * refactor: 각 메서드가 한 가지 역할만 수행하도록 리팩토링한다.
    
    * feat: 간단한 테스트를 위한 콘솔 입출력을 구현한다
    
    * docs: 요구사항의 표시형태를  리스트에서 체크박스로 변경한다
    
    * refactor: 매직넘버를 상수로 추출한다
    
    * refactor: RuntimeException을 IllegalArgumentException으로 대체한다
    
    - RuntimeException을  더 명확한 예외인 IllegalArgumentException으로 대체한다
    
    * Test: DisplayName 애노테이션을 사용하여 테스트의 용도를 명확히 한다
    
    * refactor: 매번 초기화가 필요했던 Pattern 객체를 클래스 필드에 캐싱한다

---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,56 @@
+package calculator;
+
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+
+    public static final String FIXED_DELIMITER = "[,:]";
+    public static final String USER_DELIMITER_REGEX = "//(.)\n(.*)";
+    public static final Pattern USER_DELIMITER_PATTERN = Pattern.compile(USER_DELIMITER_REGEX);
+    public static final int NULL_OR_EMPTY_VALUE = 0;
+    public static final int DELIMITER_INDEX = 1;
+    public static final int SPLIT_TARGET_INDEX = 2;
+
+    public static void main(String[] args) {
+        System.out.println("합을 구할 문자열을 입력해주세요");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        System.out.println("합은 " + splitAndSum(input));
+    }
+
+    public static int splitAndSum(String text) {
+        if (text == null || text.isEmpty()) {
+            return NULL_OR_EMPTY_VALUE;
+        }
+        Optional<String[]> tokens = getTokens(text);
+        return getSum(tokens);
+    }
+
+    private static int getSum(Optional<String[]> tokens) {
+        return Arrays.stream(tokens.get())
+                .mapToInt(Integer::parseInt)
+                .map(StringAddCalculator::parsePositiveInteger)
+                .sum();
+    }
+
+    private static Optional<String[]> getTokens(String text) {
+        Optional<String[]> tokens = Optional.of(text.split(FIXED_DELIMITER));
+        Matcher m = USER_DELIMITER_PATTERN.matcher(text);
+        if (m.find()) {
+            String customDelimiter = m.group(DELIMITER_INDEX);
+            tokens = Optional.of(m.group(SPLIT_TARGET_INDEX).split(customDelimiter));
+        }
+        return tokens;
+    }
+
+    private static int parsePositiveInteger(Integer integer) {
+        if (integer < 0) {
+            throw new IllegalArgumentException();
+        }
+        return integer;
+    }
+}
