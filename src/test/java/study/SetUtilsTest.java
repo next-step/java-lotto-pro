e29cccc7362f32d97138cc42f8aae721a529df79
유한결 <hangyul.yu@42dot.io>
2021-11-04 16:41:02 +0900

    test (Set Collection) : size, contains 테스트

---
+++ src/test/java/study/SetUtilsTest.java
@@ -0,0 +1,41 @@
+package study;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.assertj.core.internal.Strings;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class SetUtilsTest {
+
+	private Set<Integer> numbers;
+
+	@BeforeEach
+	void setUp() {
+		numbers = new HashSet<>();
+		numbers.add(1);
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+	}
+
+	@Test
+	@DisplayName("Set의 size() 메소드로 Set의 크기를 확인")
+	void sizeTest() {
+		assertThat(numbers.size()).isEqualTo(3);
+	}
+
+	@ParameterizedTest
+	@DisplayName("Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인")
+	@ValueSource(ints = {1, 2, 3})
+	void containsTest(int input) {
+		assertTrue(numbers.contains(input));
+	}
+}
