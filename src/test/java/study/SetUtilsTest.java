877c3eca72c24f51e9f714a15ba4fa548e3c1647
유한결 <hangyul.yu@42dot.io>
2021-11-04 16:49:44 +0900

    test (Set Collection) : contains 테스트2 - 다른 결과 값 추가

--- src/test/java/study/SetUtilsTest.java
+++ src/test/java/study/SetUtilsTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetUtilsTest {
@@ -35,7 +36,15 @@ public class SetUtilsTest {
 	@ParameterizedTest
 	@DisplayName("Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인")
 	@ValueSource(ints = {1, 2, 3})
-	void containsTest(int input) {
+	void containsTest1(int input) {
 		assertTrue(numbers.contains(input));
+	}
+
+	@ParameterizedTest
+	@DisplayName("Set의 contains() 메소드를 활용해 1, 2, 3의 값은 있고 4, 5가 없는 것을 확인")
+	@CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+	void containsTest2(int input, boolean expected) {
+		boolean actual = numbers.contains(input);
+		assertEquals(expected, actual);
 	}
 }
