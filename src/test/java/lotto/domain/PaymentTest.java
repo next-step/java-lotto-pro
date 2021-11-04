c78e95fa7289d6e8ef843e496a399ecb947085ea
jung0930 <wjddlqkqh57@naver.com>
2021-11-05 03:11:23 +0900

    feat, test : 구입 시 차감된 구입금액을 반환하는 기능 추가, 테스트 케이스 추가

--- src/test/java/lotto/domain/PaymentTest.java
+++ src/test/java/lotto/domain/PaymentTest.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -23,6 +24,13 @@ class PaymentTest {
     @CsvSource(value = {"5900,5", "1200,1", "10000,10"})
     void getTryCount(int payment, int tryCount) {
         assertThat(Payment.from(payment).getTryCount()).isEqualTo(tryCount);
+    }
+
+    @DisplayName("구입 시 차감된 구입금액을 반환한다")
+    @ParameterizedTest
+    @CsvSource(value = {"5000,2,3000", "2000,1,1000", "2700,1,1700"})
+    void spend(int payment, int spendCount, int change) {
+        assertThat(Payment.from(payment).spend(spendCount)).isEqualTo(Payment.from(change));
     }
 
 }
