dca5a1e832dd3f6a4eb2dd3564f87f5540b886e4
vicjed <vicjed@icloud.com>
2021-11-04 19:08:47 +0900

    style(SpliterManager): import 삭제

--- src/main/java/step2/SplitManager.java
+++ src/main/java/step2/SplitManager.java
@@ -1,11 +1,8 @@
 package step2;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class SplitManager {
     public Spliter findSpliter(String inputvalue) {
-        if (inputvalue.startsWith("//")){
+        if (inputvalue.startsWith("//")) {
             return new PatternSpliter(inputvalue);
         }
         return new DefaultSpliter(inputvalue);
