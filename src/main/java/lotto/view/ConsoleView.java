23f98c0541b6568e491ba713e905ddf368a344c4
keepbang <rlqud1125@naver.com>
2021-11-04 20:14:33 +0900

    refactor: 출력 텍스트 상수로 변경

--- src/main/java/lotto/view/ConsoleView.java
+++ src/main/java/lotto/view/ConsoleView.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 
 public class ConsoleView {
     private static final String ENTER_MONEY_TEXT = "구입금액을 입력해 주세요.";
+    private static final String BUY_LOTTO_TEXT = "%d개를 구매했습니다.";
     private static Scanner scanner = new Scanner(System.in);
 
     public static int enterMoney() {
@@ -16,7 +17,7 @@ public class ConsoleView {
     }
 
     public static void outputBoughtLotto(int boughtCount) {
-        System.out.println(boughtCount + "개를 구매했습니다.");
+        System.out.printf(BUY_LOTTO_TEXT, boughtCount);
     }
 
     public static void outputLottoTicket(LottoTicket lottoTicket) {
