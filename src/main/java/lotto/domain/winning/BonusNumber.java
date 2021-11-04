e7452161f0435dce5bcd130692f6775641f3057a
Chloe <page.prologue@gmail.com>
2021-11-04 17:31:31 +0900

    feat : 보너스 번호 객체 생성 및 테스트

---
+++ src/main/java/lotto/domain/winning/BonusNumber.java
@@ -0,0 +1,26 @@
+package lotto.domain.winning;
+
+public class BonusNumber {
+    private static final int MIN_BONUS_BOUND = 1;
+    private static final int MAX_BONUS_BOUND = 45;
+
+    private final int bonusNumber;
+
+    public BonusNumber(WinningNumbers winningNumbers, int bonusNumber) {
+        validateRange(bonusNumber);
+        validateDuplication(winningNumbers, bonusNumber);
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validateRange(int bonusNumber) {
+        if (bonusNumber < MIN_BONUS_BOUND || bonusNumber > MAX_BONUS_BOUND) {
+            throw new IllegalArgumentException("보너스 번호는 1 ~ 45 까지 입니다.");
+        }
+    }
+
+    private void validateDuplication(WinningNumbers winningNumbers, int bonusNumber) {
+        if (winningNumbers.isContains(bonusNumber)) {
+            throw new IllegalArgumentException("보너스 번호가 당첨 번호와 중복됩니다.");
+        }
+    }
+}
