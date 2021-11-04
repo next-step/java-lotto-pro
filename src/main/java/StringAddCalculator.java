0da76bbb5fe8f90218d2ad709cf8831624711a3a
daehyun0 <dd70296@gmail.com>
2021-11-04 20:14:25 +0900

    style(StringAddCalculator): if 구문에 중괄호 추가

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -3,14 +3,17 @@ public class StringAddCalculator {
 
 	public int splitAndSum(String source) {
 		CalculatorInputString calculatorInputString = new CalculatorInputString(source);
-		if (calculatorInputString.isEmpty())
+		if (calculatorInputString.isEmpty()) {
 			return 0;
+		}
 
 		CalculatorNumbers calculatorNumbers = calculatorInputString.toCalculateNumbers();
-		if (calculatorNumbers.hasSingleNumber())
+		if (calculatorNumbers.hasSingleNumber()) {
 			return calculatorNumbers.getNumber(0);
-		if (calculatorNumbers.containExceptional())
+		}
+		if (calculatorNumbers.containExceptional()) {
 			throw new IllegalArgumentException(MESSAGE_HAS_NEGATIVE_NUMBER);
+		}
 		return calculatorNumbers.sum();
 	}
 }
