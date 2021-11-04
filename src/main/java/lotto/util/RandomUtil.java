d88c264b2f72644b39108a3291b30905ec5ab4d1
moon <hoopster@airi.kr>
2021-11-04 17:38:33 +0900

    feat (RandomUtil) : 입력 한 범위의 숫자를 랜덤하게 생성하는 유틸 추가

---
+++ src/main/java/lotto/util/RandomUtil.java
@@ -0,0 +1,20 @@
+package lotto.util;
+
+import java.util.Random;
+
+import lotto.model.LottoNumber;
+
+public class RandomUtil {
+	private static final Random RANDOM = new Random();
+
+	public static int pickNumber(final int min, final int max) {
+		validMinMaxRange(min, max);
+		return RANDOM.nextInt(max - min + 1);
+	}
+
+	private static void validMinMaxRange(final int min, final int max) {
+		if (min < LottoNumber.MIN_LOTTO_NUMBER || max > LottoNumber.MAX_LOTTO_NUMBER) {
+			throw new IllegalArgumentException();
+		}
+	}
+}
---
+++ src/test/java/lotto/util/RandomUtilTest.java
@@ -0,0 +1,35 @@
+package lotto.util;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import lotto.model.LottoNumber;
+
+@DisplayName("랜덤 숫자생성 유틸 테스트")
+class RandomUtilTest {
+
+	@DisplayName("랜덤생성시 로또 번호 범위 검증 예외처리 테스트")
+	@Test
+	void 랜덤생성시_로또_번호_범위_검증_예외처리_테스트() {
+		// given // when // then
+		assertThatThrownBy(() -> {
+			RandomUtil.pickNumber(0, 8);
+		}).isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@DisplayName("로또번호 랜덤 생성 테스트")
+	@Test
+	void 로또번호_랜덤_생성_테스트() {
+		// given
+		int min = LottoNumber.MIN_LOTTO_NUMBER;
+		int max = LottoNumber.MAX_LOTTO_NUMBER;
+
+		// when
+		int lottoNumber = RandomUtil.pickNumber(min, max);
+
+		// then
+		assertThat(lottoNumber).isBetween(min, max);
+	}
+}
