3f6e60d8499de8447d3d4f1a36f2e40ac630908e
jung0930 <wjddlqkqh57@naver.com>
2021-11-04 09:17:58 +0900

    refactor : 에러 메시지 정의 수정

--- src/main/java/lotto/domain/exception/DuplicateOfBonusBallNumberException.java
+++ src/main/java/lotto/domain/exception/DuplicateOfBonusBallNumberException.java
@@ -2,7 +2,7 @@ package lotto.domain.exception;
 
 public class DuplicateOfBonusBallNumberException extends IllegalArgumentException {
 
-    private static final String DUPLICATE_OF_BONUS_BALL_NUMBER_ERROR_MESSAGE = "숫자가 6개가 아닙니다.";
+    private static final String DUPLICATE_OF_BONUS_BALL_NUMBER_ERROR_MESSAGE = "보너스볼과 당첨번호가 중복되었습니다.";
 
     public DuplicateOfBonusBallNumberException() {
         super(DUPLICATE_OF_BONUS_BALL_NUMBER_ERROR_MESSAGE);
