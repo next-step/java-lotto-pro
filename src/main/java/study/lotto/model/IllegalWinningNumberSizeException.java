91340461ad563e097a9cc80b9170047e4c982554
김도영 <95doyoung@gmail.com>
2021-11-05 04:15:51 +0900

    docs : 기능 목록 구현 내용 추가
    - `로또생성기` 를 통해서 `로또복권` 와 `당첨번호` 를 생성할 수 있다.

--- src/main/java/study/lotto/model/IllegalWinningNumberSizeException.java
+++
@@ -1,7 +0,0 @@
-package study.lotto.model;
-
-public class IllegalWinningNumberSizeException extends IllegalArgumentException {
-    public IllegalWinningNumberSizeException(final String message) {
-        super(message);
-    }
-}
