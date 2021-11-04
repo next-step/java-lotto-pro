c3b145ff1ea74cf37dbfbb5d3f308e06d8cbe7d6
Chaeyun Lee <chaeyunn@iochord.com>
2021-11-05 01:20:20 +0900

    refactor: 재사용을 위해 scanner 정적으로 변환

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,15 +6,16 @@ import java.util.Scanner;
 
 public class InputView {
 
+	private static final Scanner sc = new Scanner(System.in);
+
 	public static int getMoney() {
 		System.out.println("구입금액을 입력해 주세요.");
-		Scanner sc = new Scanner(System.in);
-		return sc.nextInt();
+		String input = sc.nextLine();
+		return Integer.parseInt(input);
 	}
 
 	public static List<Integer> getLottoNumbers() {
 		System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-		Scanner sc = new Scanner(System.in);
 		String line = sc.nextLine();
 		String[] str = line.split(",");
 		List<Integer> numbers = new ArrayList<>();
