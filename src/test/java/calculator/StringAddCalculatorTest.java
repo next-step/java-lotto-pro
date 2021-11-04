7c5318587ae1277ab7ef15f08973afc0b67a77d4
IW.MOON <mooniw02@gmail.com>
2021-11-05 00:04:45 +0900

    refactor: 역할에 따른 클래스 분리(문자열 분리, 문자열 변환, 변환문자 합), 매직넘버 상수화, 테스트케이스에 @DisplayName 추가

--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -1,47 +1,55 @@
 package calculator;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringAddCalculatorTest {
+    
+    @DisplayName("null 또는 빈문자 입력 테스트 -> 0 반환")
     @Test
     public void splitAndSum_null_또는_빈문자() {
-        int result = StringAddCalculator.splitAndSum(null);
+        int result = StringSplitAndSum.splitAndSum(null);
         assertThat(result).isEqualTo(0);
 
-        result = StringAddCalculator.splitAndSum("");
+        result = StringSplitAndSum.splitAndSum("");
         assertThat(result).isEqualTo(0);
     }
 
+    @DisplayName("숫자하나 입력 테스트 -> 입력값 그대로 반환")
     @Test
     public void splitAndSum_숫자하나() throws Exception {
-        int result = StringAddCalculator.splitAndSum("1");
+        int result = StringSplitAndSum.splitAndSum("1");
         assertThat(result).isEqualTo(1);
     }
 
+    @DisplayName("쉼표 구분자로 분리된 입력 테스트")
     @Test
     public void splitAndSum_쉼표구분자() throws Exception {
-        int result = StringAddCalculator.splitAndSum("1,2");
+        int result = StringSplitAndSum.splitAndSum("1,2");
         assertThat(result).isEqualTo(3);
     }
 
+    @DisplayName("쉼표,콜론 구분자로 분리된 입력 테스트")
     @Test
     public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
-        int result = StringAddCalculator.splitAndSum("1,2:3");
+        int result = StringSplitAndSum.splitAndSum("1,2:3");
         assertThat(result).isEqualTo(6);
     }
 
+    @DisplayName("custom 구분자 입력 테스트")
     @Test
     public void splitAndSum_custom_구분자() throws Exception {
-        int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
+        int result = StringSplitAndSum.splitAndSum("//;\n1;2;3");
         assertThat(result).isEqualTo(6);
     }
 
+    @DisplayName("음수 입력 예외 테스트")
     @Test
     public void splitAndSum_negative() throws Exception {
-        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
+        assertThatThrownBy(() -> StringSplitAndSum.splitAndSum("-1,2,3"))
                 .isInstanceOf(RuntimeException.class);
     }
 }
