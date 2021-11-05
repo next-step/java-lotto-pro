d341b9dec5e163730e64752db497a5b7830c3c07
Wangki Lee <lwk3007@gmail.com>
2021-11-05 23:13:01 +0900

    refactor: 상수 추출 LottoNumbersFactory

--- src/main/java/lotto/domain/LottoNumbersFactory.java
+++ src/main/java/lotto/domain/LottoNumbersFactory.java
@@ -6,19 +6,24 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class LottoNumbersFactory {
+    public static final int LOTTO_NUMBERS_ZERO_SIZE = 0;
+    public static final int LOTTO_NUMBERS_SIZE = 6;
+    public static final int LOTTO_NUMBER_MIN_RANGE = 1;
+    public static final int LOTTO_NUMBER_MAX_RANGE = 45;
+
     private LottoNumbersFactory() {
     }
 
     public static List<Integer> createLottoNumbers() {
         List<Integer> allLottoNumbers = createAllLottoNumbers();
         Collections.shuffle(allLottoNumbers);
-        List<Integer> lottoNumbers = allLottoNumbers.subList(0, 6);
+        List<Integer> lottoNumbers = allLottoNumbers.subList(LOTTO_NUMBERS_ZERO_SIZE, LOTTO_NUMBERS_SIZE);
         Collections.sort(lottoNumbers);
         return lottoNumbers;
     }
 
     private static List<Integer> createAllLottoNumbers() {
-        return IntStream.range(1, 45)
+        return IntStream.range(LOTTO_NUMBER_MIN_RANGE, LOTTO_NUMBER_MAX_RANGE)
             .boxed()
             .collect(Collectors.toList());
     }
