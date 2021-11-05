819545d54bfd45da2411f4d9c8146368056cca92
IW.MOON <mooniw02@gmail.com>
2021-11-05 14:32:23 +0900

    refactor: 단일데이터 테스트케이스에 대해서 @test로 변경, 예외 테스트케이스 분리

--- src/test/java/string/StringTest.java
+++ src/test/java/string/StringTest.java
@@ -1,6 +1,7 @@
 package string;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -10,42 +11,51 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class StringTest {
 
     @DisplayName("String Split Contains 테스트 (Multi Value)")
-    @ParameterizedTest
-    @CsvSource(value = {"1,2"}, delimiter = ':')
-    public void splitContainsTest(String input) {
+    @Test
+    public void splitContainsTest() {
 
-        String[] splitInputArr = input.split(",");
+        String[] splitInputArr = "1,2".split(",");
         assertThat(splitInputArr).contains(splitInputArr[0], splitInputArr[1]);
 
     }
 
     @DisplayName("String Split ContainsExactly 테스트 (Single Value)")
-    @ParameterizedTest
-    @CsvSource(value = {"1"})
-    public void splitContainsExactlyTest(String input) {
+    @Test
+    public void splitContainsExactlyTest() {
 
+        String input = "1";
         String[] splitInputArr = input.split(",");
         assertThat(splitInputArr).containsExactly(input);
 
     }
 
     @DisplayName("String substring 테스트")
-    @ParameterizedTest
-    @CsvSource(value = {"(1,2)"}, delimiter = ':')
-    public void substringTest(String input) {
+    @Test
+    public void substringTest() {
 
+        String input = "(1,2)";
         String substringInput = input.substring(1, input.length() - 1);
         assertThat(substringInput).isEqualTo("1,2");
 
     }
 
-    @DisplayName("String charAt 테스트")
-    @ParameterizedTest
-    @CsvSource(value = {"abc"}, delimiter = ':')
-    public void charAtTest(String input) {
+    @DisplayName("[정상] String charAt 테스트")
+    @Test
+    public void charAtTest() {
 
+        String input = "abc";
         assertThat(input.charAt(0)).isEqualTo('a');
         assertThat(input.charAt(1)).isEqualTo('b');
-        assertThatThrownBy(() -> input.charAt(5)).isInstanceOf(IndexOutOfBoundsException.class).hasMessageContaining("String index out of range: 5");
+
+    }
+
+    @DisplayName("[예외] String charAt IndexOutOfBoundsException 테스트")
+    @Test
+    public void charAtExceptionTest() {
+
+        String input = "abc";
+        assertThatThrownBy(() -> input.charAt(5))
+                .isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 5");
     }
 }
