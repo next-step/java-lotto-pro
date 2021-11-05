c44d028761ba3ddfee384db733236396fada97fc
JiHunHong <hongji3354@gmail.com>
2021-11-05 22:50:08 +0900

    feat : 로또 번호 생성

---
+++ src/main/java/lotto/LottoNumber.java
@@ -0,0 +1,26 @@
+package lotto;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+// 로또 번호를 발급하는 역할을 가진 클래스
+public class LottoNumber {
+
+    private static final List<Integer> LOTTO_NUMBERS = IntStream.rangeClosed(LottoProperty.LOTTO_START_NUMBER, LottoProperty.LOTTO_END_NUMBER)
+            .boxed()
+            .collect(Collectors.toList());
+
+    private LottoNumber() {
+    }
+
+    public static List<Integer> generator() {
+        Collections.shuffle(LOTTO_NUMBERS);
+
+        return LOTTO_NUMBERS.stream()
+                .limit(LottoProperty.LOTTO_COUNT)
+                .sorted()
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/lotto/LottoNumberTest.java
@@ -0,0 +1,49 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LottoNumberTest {
+
+    @DisplayName("로또 번호는 6개")
+    @Test
+    void lottoNumberSixTest() {
+        List<Integer> lottoNumbers = LottoNumber.generator();
+
+        assertThat(lottoNumbers.size()).isEqualTo(LottoProperty.LOTTO_COUNT);
+    }
+
+    @DisplayName("로또 번호는 중복 불가")
+    @Test
+    void nonDuplicateLottoNumberTest() {
+        List<Integer> lottoNumbers = LottoNumber.generator();
+
+        HashSet<Integer> nonDuplicateLottoNumber = new HashSet<>(lottoNumbers);
+
+        assertThat(nonDuplicateLottoNumber.size()).isEqualTo(lottoNumbers.size());
+    }
+
+    @DisplayName("로또 번호는 오름차순 정렬")
+    @Test
+    void lottoNumberAscSortTest() {
+        List<Integer> lottoNumbers = LottoNumber.generator();
+
+        assertThat(lottoNumbers).isSorted();
+    }
+
+    @DisplayName("로또 번호는 1부터 45 사이")
+    @Test
+    void lottoNumberRangeTest() {
+        List<Integer> lottoNumbers = LottoNumber.generator();
+
+        boolean lottoNumberRange = lottoNumbers.stream()
+                .allMatch(number -> number >= LottoProperty.LOTTO_START_NUMBER && number <= LottoProperty.LOTTO_END_NUMBER);
+
+        assertThat(lottoNumberRange).isTrue();
+    }
+}
