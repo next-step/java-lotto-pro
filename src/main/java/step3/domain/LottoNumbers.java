346d22874117bf856845a8fdd03ff443062bd990
slowin <irooori89@gmail.com>
2021-11-05 11:50:37 +0900

    feat : LottoNumber 리스트를 가지는 LottoNumbers 추가

--- src/main/java/step3/domain/LottoNumbers.java
+++ src/main/java/step3/domain/LottoNumbers.java
@@ -1,8 +1,10 @@
 package step3.domain;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 import step3.common.exception.InvalidParamException;
 import step3.domain.numbers.NumbersStrategy;
@@ -11,24 +13,24 @@ public class LottoNumbers {
     public static final int MAX_LOTTO_NUMBERS_SIZE = 6;
     public static final String RANGE_OUTBOUNT_SIZE_EXCEPTION_MESSAGE = String.format("로또 티켓은 %s 개의 숫자만 가능합니다.",
         MAX_LOTTO_NUMBERS_SIZE);
-    private List<LottoNumber> lottoNumbers;
+    private Set<LottoNumber> lottoNumbers;
 
     public LottoNumbers() {
     }
 
     public LottoNumbers(NumbersStrategy numbersStrategy) {
-        validSize(numbersStrategy);
         this.lottoNumbers = mapOf(numbersStrategy.getNumbers());
+        validSize(numbersStrategy);
     }
 
     private void validSize(NumbersStrategy numbersStrategy) {
-        if (numbersStrategy.getNumbers().length != MAX_LOTTO_NUMBERS_SIZE) {
+        if (this.lottoNumbers.size() != MAX_LOTTO_NUMBERS_SIZE) {
             throw new InvalidParamException(RANGE_OUTBOUNT_SIZE_EXCEPTION_MESSAGE);
         }
     }
 
-    private List<LottoNumber> mapOf(int[] numbers) {
-        List<LottoNumber> result = new ArrayList<>();
+    private Set<LottoNumber> mapOf(int[] numbers) {
+        Set<LottoNumber> result = new HashSet<>();
         for (int number : numbers) {
             result.add(new LottoNumber(number));
         }
