4140ef43a5e46242c7b7062cfc4c7f2a91d6e33f
slowin <irooori89@gmail.com>
2021-11-05 16:59:35 +0900

    feat : service 레이어 계층 테스트 추가

--- src/test/java/step3/LottoNumbersBundleTest.java
+++ src/test/java/step3/LottoNumbersBundleTest.java
@@ -1,5 +1,8 @@
 package step3;
 
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import step3.domain.LottoNumbers;
@@ -15,6 +18,22 @@ public class LottoNumbersBundleTest {
         lottoNumbersBundle.addLottoNumbers(
             new RandomNumbers(LottoConstant.MIN_NUMBER_RANGE, LottoConstant.MAX_NUMBER_RANGE,
                 LottoNumbers.MAX_LOTTO_NUMBERS_SIZE));
+    }
+
+    @Test
+    @DisplayName("출력된 로또번호는 불변성객체로 수정불가 테스트")
+    void toArrayUnmodifiableTest() {
+        // given
+        LottoNumbersBundle lottoNumbersBundle = new LottoNumbersBundle();
+        lottoNumbersBundle.addLottoNumbers(
+            new RandomNumbers(LottoConstant.MIN_NUMBER_RANGE, LottoConstant.MAX_NUMBER_RANGE,
+                LottoNumbers.MAX_LOTTO_NUMBERS_SIZE));
+
+        assertThatThrownBy(() -> {
+            // when
+            lottoNumbersBundle.toList().remove(0);
+        })// then
+            .isInstanceOf(UnsupportedOperationException.class);
     }
 
 }
--- src/test/java/step3/LottoProviderTest.java
+++ src/test/java/step3/LottoProviderTest.java
@@ -2,18 +2,13 @@ package step3;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import step3.domain.LottoNumbers;
-import step3.domain.LottoNumbersBundle;
 import step3.domain.LottoProvider;
-import step3.domain.LottoRanks;
 import step3.domain.constance.LottoConstant;
 import step3.domain.strategy.lotto.LottoProviderStrategy;
 import step3.domain.strategy.numbers.NumbersStrategy;
@@ -47,36 +42,6 @@ public class LottoProviderTest {
 
         // then
         assertThat(lottoProvider.lottoNumbersBundleSize()).isEqualTo(expected);
-    }
-
-    @Test
-    void 당첨금액합산구하기() {
-        // given
-        // 3개 구매
-        // 4등 2개 당첨
-        LottoProviderStrategy lottoProvider = new LottoProvider();
-
-        // 당첨 금액 만원
-
-        List<int[]> numbers = new ArrayList<>();
-        int[] winNumbers = {10, 20, 3, 4, 5, 6};
-        numbers.add(new int[] {1, 2, 3, 4, 5, 6});
-        numbers.add(new int[] {1, 2, 3, 4, 5, 6});
-        numbers.add(randomNumberStrategy().getNumbers());
-
-        for (int[] numberArr : numbers) {
-            lottoProvider.buyLottoOfNumbersStrategy(generateNumberStrategy(numberArr));
-        }
-
-        // when
-        NumbersStrategy winnerLottoNumbers = generateNumberStrategy(winNumbers);
-        LottoNumbers winLottoNumbers = new LottoNumbers(winnerLottoNumbers);
-        LottoNumbersBundle lottoNumbersBundle = lottoProvider.getLottoNumbersBundle();
-
-        LottoRanks lottoRanks = lottoNumbersBundle.lottoRanksOf(winLottoNumbers);
-        System.out.println(lottoRanks);
-
-        // then
     }
 
     private NumbersStrategy generateNumberStrategy(int[] numbers) {
