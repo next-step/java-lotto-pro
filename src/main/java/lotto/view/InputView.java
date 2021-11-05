5ba6b1ee3eaf9992fef3845b16c735d46c2e3c4b
moon <hoopster@airi.kr>
2021-11-05 18:46:55 +0900

    feat (InputView, OutputView) : 뷰 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,23 @@
+package lotto.view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+	private static final Scanner SCANNER = new Scanner(System.in);
+
+	public static String inputMoneyPurchaseLotto() {
+		String inputMoney = SCANNER.nextLine();
+		System.out.println("구입금액을 입력해 주세요.");
+		SCANNER.close();
+		return inputMoney;
+	}
+
+	public static String innputWinningLottoNumber() {
+		System.out.println("");
+		System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+		String winningNumber = SCANNER.nextLine();
+		SCANNER.close();
+		return winningNumber;
+	}
+}
