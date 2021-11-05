ad6827062f45a11bbc25279f456ff611941f5d97
haedoang <haedoang@gmail.com>
2021-11-05 13:44:00 +0900

    feat: add purchaseprice class
    - add PurchasePriceTest, PurchasePrice class

---
+++ src/main/java/lotto/domain/PurchasePrice.java
@@ -0,0 +1,8 @@
+package lotto.domain;/** 
+ * packageName : lotto.domain
+ * fileName : PurchasePrice
+ * author : haedoang
+ * date : 2021-11-05
+ * description : 
+ */public class PurchasePrice {
+}
---
+++ src/test/java/lotto/domain/PurchasePriceTest.java
@@ -0,0 +1,8 @@
+package lotto.domain;/** 
+ * packageName : lotto.domain
+ * fileName : PurchasePriceTest
+ * author : haedoang
+ * date : 2021-11-05
+ * description : 
+ */public class PurchasePriceTest {
+}
