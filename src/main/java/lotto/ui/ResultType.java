02706888036b9c5c162f7a117e6c56f7d6bf1a79
haedoang <haedoang@gmail.com>
2021-11-05 18:11:46 +0900

    feat: output purchase count msg
    - add Purchase count message

---
+++ src/main/java/lotto/ui/ResultType.java
@@ -0,0 +1,12 @@
+package lotto.ui;
+
+/**
+ * packageName : lotto.ui
+ * fileName : OutputType
+ * author : haedoang
+ * date : 2021-11-05
+ * description :
+ */
+public enum OutputType {
+    PURCHASE, RESULT, YIELD
+}
