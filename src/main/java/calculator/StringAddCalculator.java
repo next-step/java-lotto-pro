37e8933d713bea844c7b9dfb082ab74a7c6ace22
IW.MOON <mooniw02@gmail.com>
2021-11-05 14:37:48 +0900

    refactor: 역할에 따른 클래스 분리(문자열 분리, 문자열 변환, 변환문자 합), 매직넘버 상수화, 테스트케이스에 @DisplayName 추가

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,53 +1,15 @@
 package calculator;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class StringAddCalculator {
 
-    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
-    private static final String DEFAULT_DELIMITER = ",|:";
-
-    public static int splitAndSum(String text) {
-        if (text == null || text.isEmpty()) {
-            return 0;
-        }
-        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(text);
-        if (matcher.find()) {
-            return customDelimiterAdd(matcher);
-        }
-        String[] numbers = text.split(DEFAULT_DELIMITER);
-        return stringAdd(numbers);
-    }
-
-    private static int customDelimiterAdd(Matcher matcher) {
-        String customDelimiter = matcher.group(1);
-        String[] numbers = matcher.group(2).split(customDelimiter);
-        return stringAdd(numbers);
-    }
-
-    private static int stringAdd(String[] numbers) {
+    public static int stringAdd(String[] numbers) {
         int sum = 0;
         for (String number : numbers) {
-            int parseNumber = parseIntNumber(number);
-            isPositiveNumber(parseNumber);
+            int parseNumber = StringParseNumber.parseNumber(number);
             sum += parseNumber;
         }
         return sum;
-    }
-
-    private static int parseIntNumber(String number) {
-        try {
-            return Integer.parseInt(number);
-        } catch (NumberFormatException e) {
-            throw new RuntimeException();
-        }
-    }
-
-    private static void isPositiveNumber(int number) {
-        if (number < 0) {
-            throw new RuntimeException();
-        }
     }
 
 }
