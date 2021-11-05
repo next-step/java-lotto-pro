ee46a0ddc11d5b898119f95ba69beec5fb8890c0
이승민 <sml7179@naver.com>
2021-11-06 00:55:43 +0900

    feat: 로또숫자 범위에 맞게 생성하고 범위가 아니면 에러나는 기능 추가

--- src/main/java/lotto/util/LottoNumber.java
+++ src/main/java/lotto/util/LottoNumber.java
@@ -1,11 +1,18 @@
 package lotto.util;
 
 public class LottoNumber {
+    private final int number;
     public static final int MIN_NUMBER = 1;
     public static final int MAX_NUMBER = 45;
-    public static final int SIZE = 6;
 
-    private LottoNumber(){
+    public LottoNumber(int number) {
+        validCheck(number);
+        this.number = number;
+    }
 
+    private void validCheck(int number) {
+        if (number < MIN_NUMBER || number > MAX_NUMBER) {
+            throw new IllegalArgumentException("로또 숫자 범위가 아닙니다.");
+        }
     }
 }
---
+++ src/test/java/lotto/model/LottoNumberTest.java
@@ -0,0 +1,19 @@
+package lotto.model;
+
+import lotto.util.LottoNumber;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class LottoNumberTest {
+
+    @DisplayName("로또 숫자의 범위 검증")
+    @Test
+    void lottoNumberRange() {
+        assertThatThrownBy(() -> {
+            new LottoNumber(46);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("로또 숫자 범위가 아닙니다.");
+    }
+}
