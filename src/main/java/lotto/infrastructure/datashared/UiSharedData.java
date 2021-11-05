2d9d4b0b9c3821c13ad8c503d382c86f2b90b9df
DESKTOP-HG83T1I\Chaser <a>
2021-11-06 03:43:44 +0900

    feat(Infrastructure): Cui Component추가, 화면간 state 관리로직 추가
    - Margin Component 추가
    - 화면간 데이터 공유를 위한 로직 추가

---
+++ src/main/java/lotto/infrastructure/datashared/UiSharedData.java
@@ -0,0 +1,34 @@
+package lotto.infrastructure.datashared;
+
+import lotto.domain.lotto.Lotto;
+import lotto.domain.lotto.Lottos;
+
+public class UiSharedData {
+  private static Lottos buyLottos = new Lottos();
+  private static Lotto latestWinLotto = new Lotto();
+
+  public UiSharedData() {
+  }
+
+  public static void setBuyLottos(Lottos buyLottos) {
+    UiSharedData.buyLottos = buyLottos;
+  }
+
+  public static void setLatestWinLotto(Lotto latestWinLotto) {
+    UiSharedData.latestWinLotto = latestWinLotto;
+  }
+
+  public static Lottos getBuyLottos() {
+    if(UiSharedData.buyLottos == null ) {
+      return new Lottos();
+    }
+    return UiSharedData.buyLottos;
+  }
+
+  public static Lotto getLatestWinLotto() {
+    if (UiSharedData.latestWinLotto == null) {
+      return new Lotto();
+    }
+    return UiSharedData.latestWinLotto;
+  }
+}
