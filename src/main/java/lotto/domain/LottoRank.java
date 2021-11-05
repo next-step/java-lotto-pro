8e9138b19b5354cf97658b88bc5696e8bf7a4411
Chaeyun Lee <chaeyunn@iochord.com>
2021-11-06 00:49:11 +0900

    feat: 당첨 순위 2위 추가

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -6,19 +6,23 @@ public class LottoRank {
 	public static final int RANK_2ST = 2;
 	public static final int RANK_3ST = 3;
 	public static final int RANK_4ST = 4;
-	private static final int LOTTO_4ST_MATCH_CNT = 3;
-	private static final int LOTTO_3ST_MATCH_CNT = 4;
-	private static final int LOTTO_2ST_MATCH_CNT = 5;
+	public static final int RANK_5ST = 5;
 	private static final int LOTTO_1ST_MATCH_CNT = 6;
+	private static final int LOTTO_2ST_MATCH_CNT = 5;
+	private static final int LOTTO_3ST_MATCH_CNT = 5;
+	private static final int LOTTO_4ST_MATCH_CNT = 4;
+	private static final int LOTTO_5ST_MATCH_CNT = 3;
+
 	private int rank;
 
 	public LottoRank(LottoNumber winningNumber, LottoNumber number) {
 		int matchCnt = winningNumber.getMatchCount(number);
-		setRank(matchCnt);
+		boolean isMatchBonus = LottoNumber.isContainBonusNumber(winningNumber, number);
+		setRank(matchCnt, isMatchBonus);
 	}
 
 	public LottoRank(int rankNumber) {
-		if (rankNumber < 1 || rankNumber > 4) {
+		if (rankNumber < 1 || rankNumber > 5) {
 			throw new IllegalArgumentException("순위는 1~4등만 존재합니다");
 		}
 		this.rank = rankNumber;
@@ -28,21 +32,25 @@ public class LottoRank {
 		return this.rank;
 	}
 
-	private void setRank(int matchCount) {
-		if (matchCount == LOTTO_4ST_MATCH_CNT) {
-			this.rank = RANK_4ST;
+	private void setRank(int matchCount, boolean isMatchBonus) {
+		if (matchCount == LOTTO_1ST_MATCH_CNT) {
+			this.rank = RANK_1ST;
+			return;
+		}
+		if (matchCount == LOTTO_2ST_MATCH_CNT && isMatchBonus) {
+			this.rank = RANK_2ST;
 			return;
 		}
 		if (matchCount == LOTTO_3ST_MATCH_CNT) {
 			this.rank = RANK_3ST;
 			return;
 		}
-		if (matchCount == LOTTO_2ST_MATCH_CNT) {
-			this.rank = RANK_2ST;
+		if (matchCount == LOTTO_4ST_MATCH_CNT) {
+			this.rank = RANK_4ST;
 			return;
 		}
-		if (matchCount == LOTTO_1ST_MATCH_CNT) {
-			this.rank = RANK_1ST;
+		if (matchCount == LOTTO_5ST_MATCH_CNT) {
+			this.rank = RANK_5ST;
 		}
 	}
 
@@ -60,5 +68,9 @@ public class LottoRank {
 
 	public boolean isFourth() {
 		return this.rank == RANK_4ST;
+	}
+
+	public boolean isFifth() {
+		return this.rank == LottoRank.RANK_5ST;
 	}
 }
--- src/test/java/lotto/domain/LottoRankTest.java
+++ src/test/java/lotto/domain/LottoRankTest.java
@@ -1,10 +1,9 @@
 package lotto.domain;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.params.provider.Arguments.*;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -14,30 +13,33 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class LottoRankTest {
 
-	static List<Arguments> winningNumberAndLottoNumberAndRank() {
-		return Arrays.asList(
-			arguments(Arrays.asList(1, 2, 3, 4, 5, 6), Arrays.asList(1, 2, 3, 4, 5, 6), 1),
-			arguments(Arrays.asList(1, 2, 3, 4, 5, 6), Arrays.asList(1, 2, 3, 4, 5, 10), 2),
-			arguments(Arrays.asList(1, 2, 3, 4, 5, 6), Arrays.asList(1, 2, 3, 4, 9, 10), 3),
-			arguments(Arrays.asList(1, 2, 3, 4, 5, 6), Arrays.asList(1, 2, 3, 8, 9, 10), 4)
+	static Stream<Arguments> winningNumberAndLottoNumberAndRank() {
+		return Stream.of(
+			Arguments.of(LottoNumber.of(Arrays.asList(1, 2, 3, 4, 5, 6)), 1),
+			Arguments.of(LottoNumber.of(Arrays.asList(1, 2, 3, 4, 5, 45)), 2),
+			Arguments.of(LottoNumber.of(Arrays.asList(1, 2, 3, 4, 5, 10)), 3),
+			Arguments.of(LottoNumber.of(Arrays.asList(1, 2, 3, 4, 10, 11)), 4),
+			Arguments.of(LottoNumber.of(Arrays.asList(1, 2, 3, 10, 11, 12)), 5)
 		);
 	}
 
 	@ParameterizedTest
 	@MethodSource("winningNumberAndLottoNumberAndRank")
 	@DisplayName("로또번호와 당첨번호가 주어지면, 순위가 반환되어야 한다")
-	public void rankTest(List<Integer> winNumberList, List<Integer> numberList, int rank) {
-		LottoNumber winningNumber = LottoNumber.of(winNumberList);
-		LottoNumber lottoNumber = LottoNumber.of(numberList);
+	public void rankTest(LottoNumber lottoNumber, int rank) {
+		// given
+		LottoNumber winningNumber = LottoNumber.of(Arrays.asList(1, 2, 3, 4, 5, 6), 45);
 
+		// when
 		LottoRank lottoRank = new LottoRank(winningNumber, lottoNumber);
 
+		// then
 		assertThat(lottoRank.getRank()).isEqualTo(rank);
 	}
 
 	@ParameterizedTest
-	@ValueSource(ints = {-1, 0, 5})
-	@DisplayName("순위는 1~4위로 이루어져야 한다")
+	@ValueSource(ints = {-1, 0, 6})
+	@DisplayName("순위는 1~5위로 이루어져야 한다")
 	public void outOfRankTest(int rank) {
 		// when, then
 		assertThatExceptionOfType(IllegalArgumentException.class)
