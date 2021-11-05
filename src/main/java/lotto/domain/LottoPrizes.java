62230d38be29ef666810eb7fc52b0b0dfbc9e454
Chaeyun Lee <chaeyunn@iochord.com>
2021-11-06 00:50:20 +0900

    feat: 당첨 2위 상금 추가

--- src/main/java/lotto/domain/LottoPrizes.java
+++ src/main/java/lotto/domain/LottoPrizes.java
@@ -2,10 +2,11 @@ package lotto.domain;
 
 public class LottoPrizes {
 
-	private static final int LOTTO_4ST_PRIZE = 5000;
-	private static final int LOTTO_3ST_PRIZE = 50000;
-	private static final int LOTTO_2ST_PRIZE = 1500000;
-	private static final int LOTTO_1ST_PRIZE = 2000000000;
+	private static final int LOTTO_5ST_PRIZE = 5_000;
+	private static final int LOTTO_4ST_PRIZE = 50_000;
+	private static final int LOTTO_3ST_PRIZE = 1_500_000;
+	private static final int LOTTO_2ST_PRIZE = 30_000_000;
+	private static final int LOTTO_1ST_PRIZE = 2_000_000_000;
 
 	private int totalPrizeMoney;
 
@@ -13,7 +14,8 @@ public class LottoPrizes {
 		this.totalPrizeMoney = ranks.getFirstCnt() * LOTTO_1ST_PRIZE
 			+ ranks.getSecondCnt() * LOTTO_2ST_PRIZE
 			+ ranks.getThirdCnt() * LOTTO_3ST_PRIZE
-			+ ranks.getFourthCnt() * LOTTO_4ST_PRIZE;
+			+ ranks.getFourthCnt() * LOTTO_4ST_PRIZE
+			+ ranks.getFifthCnt() * LOTTO_5ST_PRIZE;
 	}
 
 	public int getTotalPrizeMoney() {
--- src/test/java/lotto/domain/LottoPrizesTest.java
+++ src/test/java/lotto/domain/LottoPrizesTest.java
@@ -5,22 +5,25 @@ import static org.assertj.core.api.Assertions.*;
 import java.util.Arrays;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class LottoPrizesTest {
 
-	@Test
+	@ParameterizedTest
+	@CsvSource(delimiter = ',', value = {"5,2,30005000", "5,5,10000", "1,2,2030000000"})
 	@DisplayName("여러 순위들이 주어지면, 총 당첨금을 계산한다")
-	public void getTotalPrizeTest() {
+	public void getTotalPrizeTest(int rank1, int rank2, int prize) {
 		// given
 		LottoRanks ranks = new LottoRanks(Arrays.asList(
-			new LottoRank(4),
-			new LottoRank(4)));
+			new LottoRank(rank1),
+			new LottoRank(rank2)));
 
 		// when
 		LottoPrizes prizes = new LottoPrizes(ranks);
 
 		// then
-		assertThat(prizes.getTotalPrizeMoney()).isEqualTo(10_000);
+		assertThat(prizes.getTotalPrizeMoney()).isEqualTo(prize);
 	}
+
 }
