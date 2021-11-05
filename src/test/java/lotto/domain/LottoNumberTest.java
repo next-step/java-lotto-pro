09e48d8b933c7caac78b1543e879dec2dd7b057f
Chaeyun Lee <chaeyunn@iochord.com>
2021-11-06 00:46:28 +0900

    feat: 당첨번호 생성 시 보너스번호 추가 기능

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -11,8 +11,8 @@ import org.junit.jupiter.api.Test;
 public class LottoNumberTest {
 
 	@Test
-	@DisplayName("주어진 로또 숫자들로 정렬된 로또번호를 생성해야 한다")
-	public void addTest() {
+	@DisplayName("주어진 로또번호로 정렬된 로또번호를 생성해야 한다")
+	public void createTest() {
 		// given
 		List<Integer> numberList = Arrays.asList(3, 2, 1, 4, 5, 6);
 
@@ -22,6 +22,22 @@ public class LottoNumberTest {
 		// then
 		List<Integer> integers = lottoNumber.getNumbers();
 		assertThat(integers).containsExactly(1, 2, 3, 4, 5, 6);
+	}
+
+	@Test
+	@DisplayName("주어진 번호(+보너스번호)로 당첨번호를 생성해야 한다")
+	public void createWithBonusTest() {
+		// given
+		List<Integer> numberList = Arrays.asList(3, 2, 1, 4, 5, 6);
+		int bonusNumber = 9;
+
+		// when
+		LottoNumber winningNumber = LottoNumber.of(numberList, bonusNumber);
+
+		// then
+		List<Integer> integers = winningNumber.getNumbers();
+		assertThat(integers).containsExactly(1, 2, 3, 4, 5, 6);
+		assertThat(winningNumber.isMatchBonusNumber(9)).isTrue();
 	}
 
 	@Test
@@ -45,14 +61,49 @@ public class LottoNumberTest {
 	}
 
 	@Test
+	@DisplayName("보너스 번호는 1~45 범위의 숫자이며, 다른 번호들과 중복되면 안된다")
+	public void bonusNumberValidTest() {
+		// given
+		List<Integer> numberList = Arrays.asList(1, 2, 3, 4, 5, 6);
+		int outRangeBonusNumber = 100;
+		int duplicateBonusNumber = 1;
+
+		// when, then
+		assertThatExceptionOfType(IllegalArgumentException.class)
+			.isThrownBy(() -> LottoNumber.of(numberList, duplicateBonusNumber))
+			.withMessageContaining("보너스번호는 다른 번호들과 중복이 되면 안됩니다");
+		assertThatExceptionOfType(IllegalArgumentException.class)
+			.isThrownBy(() -> LottoNumber.of(numberList, outRangeBonusNumber))
+			.withMessageContaining("1이상 45이하인 숫자여야 합니다");
+
+	}
+
+	@Test
 	@DisplayName("서로 일치하는 숫자 갯수를 반환해야 한다")
 	public void getMatchCountTest() {
+		// given
 		LottoNumber lottoNumber = LottoNumber.of(Arrays.asList(1, 2, 3, 4, 5, 6));
 		LottoNumber lottoNumber2 = LottoNumber.of(Arrays.asList(1, 2, 3, 9, 10, 11));
 
+		// when
 		int result = lottoNumber.getMatchCount(lottoNumber2);
 
+		// then
 		assertThat(result).isEqualTo(3);
 	}
+
+	// @Test
+	// @DisplayName("보너스 번호가 포함되어있는지 확인할 수 있어야 한다")
+	// public void isEqualBonusNumberTest() {
+	// 	// given
+	// 	LottoNumber winningNumber = LottoNumber.of(Arrays.asList(1, 2, 3, 4, 5, 6), 7);
+	// 	LottoNumber lottoNumber = LottoNumber.of(Arrays.asList(1, 2, 3, 9, 10, 11));
+	// 	LottoNumber diffLottoNumber = LottoNumber.of(Arrays.asList(1, 2, 3, 9, 10, 11));
+	//
+	// 	// when, then
+	// 	assertThat(winningNumber.isMatchBonusNumber(lottoNumber)).isTrue();
+	// 	assertThat(winningNumber.isMatchBonusNumber(diffLottoNumber)).isFalse();
+	//
+	// }
 
 }
