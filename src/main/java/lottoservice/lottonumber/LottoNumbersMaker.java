858a279acbbc64bd6f58a3c910493443ccad6960
jerry92k <jens92.k@gmail.com>
2021-11-05 19:13:49 +0900

    refactor : lottoNumberCandidate를 Integer 리스트에서 LottoNumber 리스트로 변경

--- src/main/java/lottoservice/lottonumber/LottoNumbersMaker.java
+++ src/main/java/lottoservice/lottonumber/LottoNumbersMaker.java
@@ -3,23 +3,28 @@ package lottoservice.lottonumber;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+/**
+ * 로또 번호 생성 클래스
+ */
 public class LottoNumbersMaker {
-	protected static final int START_INCLUSIVE_NUMBER=1;	/* 로또 번호 최소값 */
-	protected static final int END_EXCLUSIVE_NUMBER=45;	/* 로또 번호 최대값 */
-	private static final List<Integer> lottoNumberCandidates = getLottoNumberCandidates();
+	public static final int SIZE_OF_LOTTERY_NUMBERS = 6;	/* 로또 번호 갯수 */
+	protected static final int START_INCLUSIVE_NUMBER = 1;    /* 로또 번호 최소값 */
+	protected static final int END_EXCLUSIVE_NUMBER = 45;    /* 로또 번호 최대값 */
+	private static final List<LottoNumber> lottoNumberCandidates = getLottoNumberCandidates();    /* 1~45사이의 수를 로또 번호로 포장하여 저장 */
 
+	/* Collections.shuffle 메서드를 이용하여 collection을 섞은 후 앞에서 부터 SIZE_OF_LOTTERY_NUMBERS 갯수만큼 subList  */
 	public static List<LottoNumber> makelottoNumbers() {
 		Collections.shuffle(lottoNumberCandidates);
-		return lottoNumberCandidates.stream().limit(6).map(number->new LottoNumber(number)).collect(Collectors.toList());
+		return new ArrayList<>(lottoNumberCandidates.subList(0,
+			SIZE_OF_LOTTERY_NUMBERS + 1));    /*List의 subList는 deepCopy가 아니므로 새로 인스턴스를 생성하여 전달 */
 	}
 
-	private static List<Integer> getLottoNumberCandidates(){
-		return IntStream.range(START_INCLUSIVE_NUMBER, END_EXCLUSIVE_NUMBER+1)
+	private static List<LottoNumber> getLottoNumberCandidates() {
+		return IntStream.range(START_INCLUSIVE_NUMBER, END_EXCLUSIVE_NUMBER + 1)
 			.collect(ArrayList::new,
-				(pickedNumbers, number) -> pickedNumbers.add(number)
+				(pickedNumbers, number) -> pickedNumbers.add(new LottoNumber(number))
 				, (pickedNumbers1, pickedNumbers2) -> pickedNumbers1.addAll(pickedNumbers2));
 	}
 }
