3ba048d47208f2ee6ab3af1c04d0af2070f211c7
jerry92k <jens92.k@gmail.com>
2021-11-05 14:48:50 +0900

    feat: 당첨 종류와 갯수에 따른 수익률 계산

--- src/main/java/lottogame/LottoMatchResult.java
+++ src/main/java/lottogame/LottoMatchResult.java
@@ -1,24 +1,54 @@
 package lottogame;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class LottoMatchResult {
-	private Map<MatchRank, Integer> result;
+	private Map<LottoMatchRank, Integer> result;
 
 	public LottoMatchResult() {
-		this.result = new HashMap<>();
+		this.result = initResultSet();
 	}
 
-	public void addCount(MatchRank matchRank){
-		result.put(matchRank, result.getOrDefault(matchRank,0)+1);
+	private Map<LottoMatchRank, Integer> initResultSet() {
+		return Arrays.stream(LottoMatchRank.values()).collect(() -> new HashMap<LottoMatchRank, Integer>(),
+			(matches, rank) -> matches.put(rank, 0),
+			(matches1, matches2) -> matches1.putAll(matches2));
 	}
 
-	public Map<MatchRank, Integer> getResult() {
+	public void addCount(LottoMatchRank lottoMatchRank) {
+		result.put(lottoMatchRank, result.getOrDefault(lottoMatchRank, 0) + 1);
+	}
+
+	public Map<LottoMatchRank, Integer> getResult() {
 		return result;
 	}
 
-	public String getResultToString(){
+	public String getResultToString() {
 		return "";
+	}
+
+	public long calculateProfit() {
+		return result.entrySet().stream()
+			.map(rank -> rank.getKey().getWinningMoney() * rank.getValue())
+			.mapToLong(profit -> profit)
+			.sum();
+	}
+
+	public double calculateProfitPercentage() {
+		long profit = calculateProfit() * 100; // 소수점 세번째 자리에서 반올림 하는 포맷팅을 위해
+		long dividedProfit = profit / (getNumOfMatchResults() * 1000);
+		return (double)dividedProfit / 100;
+	}
+
+	/* HashMap의 size() 메서드를 사용하면 key의 갯수를 구하기 때문에 map을 탐색하여 각각의 비교 결과 합을 구해야 함*/
+	public int getNumOfMatchResults() {
+		return result.entrySet()
+			.stream()
+			.map(matchResult -> matchResult.getValue())
+			.mapToInt(numOfResults -> numOfResults)
+			.sum();
 	}
 }
