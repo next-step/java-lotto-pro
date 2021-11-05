6de1c65fbe9d8aef01d38aeff2e35fb1c04eeb15
jennie267 <jennieee267@gmail.com>
2021-11-06 00:20:48 +0900

    feat : 구매금액을 입력받는다, 구입금액을 입력해 주세요. 메시지 출력 호출

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,14 @@
+package lotto.controller;
+
+import lotto.view.InputView;
+
+public class LottoController {
+    private final InputView inputView = new InputView();
+    
+    public void start() {
+        inputView.enterMoney();
+    }
+    
+    
+
+}
