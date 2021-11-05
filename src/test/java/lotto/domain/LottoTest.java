c4999f9018cae0bf627e67cf292b8b5fac876f2d
cus4703 <cus4703@gmail.com>
2021-11-06 00:07:10 +0900

    refactor: Lotto 클래스 책임과 필드 모두 수정

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,42 +1,21 @@
 package lotto.domain;
 
-import lotto.view.OutputView;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoTest {
 
-    @ParameterizedTest
-    @ValueSource(ints = 15000)
-    @DisplayName("금액에 따라 로또 구입개수 확인")
-    public void purchaseLotto(int purchaseAmount) {
-        Lotto lotto = new Lotto(purchaseAmount);
+    @Test
+    @DisplayName("로또 매치 개수 확인")
+    public void lottoTest() {
 
-        assertThat(lotto.getLottoNumbers().size()).isEqualTo(15);
-    }
+        Lotto lotto = new Lotto(Arrays.asList(Rank.FIRST, Rank.FIRST, Rank.FIRST));
 
-    @ParameterizedTest
-    @ValueSource(ints = 15500)
-    @DisplayName("구입 금액 검증(1000원으로 나눴을경우 나머지가 0이 아닐경우)")
-    public void validateRemainderAmount(int purchaseAmount) {
-        assertThatThrownBy(() -> {
-            Lotto lotto = new Lotto(purchaseAmount);
-        }).isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR]");
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = 0)
-    @DisplayName("구입 금액 검증(0원을 입력했을 경우)")
-    public void validateZeroAmount(int purchaseAmount) {
-        assertThatThrownBy(() -> {
-            Lotto lotto = new Lotto(purchaseAmount);
-        }).isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR]");
+        assertThat(lotto.getMatchRankCount(Rank.FIRST)).isEqualTo(3);
     }
 
 }
