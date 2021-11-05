c8691ddb9cb5aa217af60f5b3f4203c541fa53c2
jerry92k <jens92.k@gmail.com>
2021-11-05 15:16:05 +0900

    feat: (UI연계) 구매금액을 입력받아 티켓 생성 후 화면에 출력

---
+++ src/main/java/lottogame/ResultView.java
@@ -0,0 +1,12 @@
+package lottogame;
+
+public class ResultView {
+
+	public static void printGuideMessage(String message){
+		System.out.println(message);
+	}
+
+	public static void printLottoTickets(LottoTickets lottoTickets) {
+		System.out.println(lottoTickets);
+	}
+}
