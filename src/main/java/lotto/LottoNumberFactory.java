4c041eba5463549edd5cb168aebc94876a4967bd
codingknowjam <hanwoodww@gmail.com>
2021-11-05 13:01:31 +0900

    feat: 로또번호 6자리 생성 기능 구현
    - 테스트 코드 작성
      6개의 번호가 생성되었는지 확인
      생성된 6개의 번호가 중복이 있는지 확인

---
+++ src/main/java/lotto/LottoNumberFactory.java
@@ -0,0 +1,22 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoNumberFactory {
+	private static final int MIN_BOUNDARY_NUMBER = 1;
+	private static final int MAX_BOUNDARY_NUMBER = 45;
+	private static final List<Integer> allLottoNumbers = new ArrayList<>();
+
+	static {
+		for (int i = MIN_BOUNDARY_NUMBER; i <= MAX_BOUNDARY_NUMBER; i++) {
+			allLottoNumbers.add(i);
+		}
+	}
+
+	public static List<Integer> create() {
+		Collections.shuffle(allLottoNumbers);
+		return allLottoNumbers.subList(0, 6);
+	}
+}
