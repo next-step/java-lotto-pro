142813e6a8c6adae9a77dfc2f137adb694e9b7d6
jerry92k <jens92.k@gmail.com>
2021-11-06 13:50:24 +0900

    refactor(LottoMain) : 추후 확장을 고려하여 입력, 출력 인스턴스를 DI.

--- src/main/java/lottoservice/LottoMain.java
+++ src/main/java/lottoservice/LottoMain.java
@@ -7,28 +7,39 @@ import lottoservice.lottoticket.LottoTickets;
 import lottoservice.matcher.LottoMatchResult;
 import lottoservice.matcher.LottoMatcher;
 import lottoservice.matcher.LottoWinningNumbers;
+import lottoservice.ui.ConsoleInputView;
+import lottoservice.ui.ConsoleResultView;
 import lottoservice.ui.InputView;
 import lottoservice.ui.ResultView;
 
 public class LottoMain {
 
 	private static String GUIDE_MESSAGE_ENTER_INPUT_AMOUNT = "구매금액을 입력해주세요.";
-	private static String GUIDE_MESSAGE_ENTER_LAST_WEEK_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해주세요.";
-	private static String RESULT_MESSAGE_MATCH_STATISTICS_START_LINE = "당첨통계\n---------";
-	private static String RESULT_MESSAGE_MATCH_PROFIT_STATEMENT = "총 수익률은 %s 입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미)\n";
+	private static String GUIDE_MESSAGE_ENTER_LAST_WEEK_WINNING_NUMBERS = "\n지난 주 당첨 번호를 입력해주세요.";
+	private static String RESULT_MESSAGE_MATCH_STATISTICS_START_LINE = "\n당첨통계\n---------";
+	private static String RESULT_MESSAGE_MATCH_PROFIT_STATEMENT = "총 수익률은 %s 입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)";
+
+	private InputView inputView;
+	private ResultView resultView;
+
+	public LottoMain(InputView inputView, ResultView resultView) {
+		this.inputView = inputView;
+		this.resultView = resultView;
+	}
 
 	public static void main(String[] args) {
-		LottoMain lottoMain = new LottoMain();
+		InputView userInputView = new ConsoleInputView();
+		ResultView resultView = new ConsoleResultView();
+		LottoMain lottoMain = new LottoMain(userInputView, resultView);
 		lottoMain.startLottoMain();
 	}
 
 	public void startLottoMain() {
 		LottoTickets lottoTickets = buyLottoTickets();
-		showTickets(lottoTickets);
-		ResultView.printNewLine();
+		outputBoughtLottoTickets(lottoTickets);
 		LottoWinningNumbers lottoWinningNumbers = getLastWeekWinningNumbers();
-		ResultView.printNewLine();
-		showMatchLottoWinningResult(lottoWinningNumbers, lottoTickets);
+		LottoMatchResult lottoMatchResult = matchLottoWinning(lottoWinningNumbers, lottoTickets);
+		outputLottoMatchResults(lottoMatchResult);
 	}
 
 	private LottoTickets buyLottoTickets() {
@@ -36,18 +47,18 @@ public class LottoMain {
 			String inputAmount = inputAmountForBuyLotto();
 			return LottoTicketIssuer.buyTickets(inputAmount);
 		} catch (InvalidMoneyException ex) {
-			ResultView.printErrorMessage(ex.getMessage());
+			resultView.outputError(ex.getMessage());
 			return buyLottoTickets();    /* 사용자가 잘못된 입력을 했을 경우 재입력 */
 		}
 	}
 
 	private String inputAmountForBuyLotto() {
-		ResultView.printGuideMessage(GUIDE_MESSAGE_ENTER_INPUT_AMOUNT);
-		return InputView.readLine();
+		resultView.outputGuide(GUIDE_MESSAGE_ENTER_INPUT_AMOUNT);
+		return inputView.readInputLine();
 	}
 
-	private void showTickets(LottoTickets lottoTickets) {
-		ResultView.printResultMessage(lottoTickets);
+	private void outputBoughtLottoTickets(LottoTickets lottoTickets) {
+		resultView.outputResult(lottoTickets);
 	}
 
 	private LottoWinningNumbers getLastWeekWinningNumbers() {
@@ -55,22 +66,25 @@ public class LottoMain {
 			String lastWeekWinningNumbers = inputLastWeekWinningNumbers();
 			return LottoWinningNumbers.makeLottoWinningNumbers(lastWeekWinningNumbers);
 		} catch (InvalidLottoFormatException ex) {
-			ResultView.printErrorMessage(ex.getMessage());
+			resultView.outputError(ex.getMessage());
 			return getLastWeekWinningNumbers();    /* 사용자가 잘못된 입력을 했을 경우 재입력*/
 		}
 	}
 
 	private String inputLastWeekWinningNumbers() {
-		ResultView.printGuideMessage(GUIDE_MESSAGE_ENTER_LAST_WEEK_WINNING_NUMBERS);
-		return InputView.readLine();
+		resultView.outputGuide(GUIDE_MESSAGE_ENTER_LAST_WEEK_WINNING_NUMBERS);
+		return inputView.readInputLine();
 	}
 
-	private void showMatchLottoWinningResult(LottoWinningNumbers lottoWinningNumbers, LottoTickets lottoTickets) {
-		ResultView.printResultMessage(RESULT_MESSAGE_MATCH_STATISTICS_START_LINE);
+	private LottoMatchResult matchLottoWinning(LottoWinningNumbers lottoWinningNumbers, LottoTickets lottoTickets) {
 		LottoMatcher lottoMatcher = new LottoMatcher(lottoWinningNumbers);
-		LottoMatchResult matchResult = lottoMatcher.matchWinningAndTickets(lottoTickets);
-		ResultView.printResultMessage(matchResult);
-		ResultView.printFormatResultMessage(RESULT_MESSAGE_MATCH_PROFIT_STATEMENT,
-			matchResult.calculateProfitPercentage());
+		return lottoMatcher.matchWinningAndTickets(lottoTickets);
+	}
+
+	private void outputLottoMatchResults(LottoMatchResult matchResult) {
+		resultView.outputGuide(RESULT_MESSAGE_MATCH_STATISTICS_START_LINE);
+		resultView.outputResult(matchResult);
+		resultView.outputResult(String.format(RESULT_MESSAGE_MATCH_PROFIT_STATEMENT,
+			matchResult.calculateProfitPercentage()));
 	}
 }
