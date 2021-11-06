a7ff986b2f2f70828fd560f56c22acd75d7d0b2e
ch3224bin <ahrocksil@gmail.com>
2021-11-06 13:05:35 +0900

    refactor : Rank 가격에 관한 숫자 천단위로 구분

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,10 +1,10 @@
 package lotto.domain;
 
 public enum Rank {
-    FOURTH(3, 5000),
-    THIRD(4, 50000),
-    SECOND(5, 1500000),
-    FIRST(6, 2000000000);
+    FOURTH(3, 5_000),
+    THIRD(4, 50_000),
+    SECOND(5, 1_500_000),
+    FIRST(6, 2_000_000_000);
 
     private final int matchedCount;
     private final long winningMoney;
