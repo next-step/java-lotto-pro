65a7241d8f6f9415bfe3e98b7af3e5acec5b588b
IW.MOON <mooniw02@gmail.com>
2021-11-06 20:56:57 +0900

    feat: BONUS 값 추가

--- src/main/java/lotto/model/LottoWinningPrice.java
+++ src/main/java/lotto/model/LottoWinningPrice.java
@@ -6,20 +6,23 @@ import java.util.Arrays;
 
 public enum LottoWinningPrice {
 
-    NONE(0, GameRule.MATCH_PRICE_NONE, false),
-    THREE(3, GameRule.MATCH_PRICE_3, true),
-    FOUR(4, GameRule.MATCH_PRICE_4, true),
-    FIVE(5, GameRule.MATCH_PRICE_5, true),
-    SIX(6, GameRule.MATCH_PRICE_6, true)
+    NONE(0, GameRule.MATCH_PRICE_NONE, false, false),
+    THREE(3, GameRule.MATCH_PRICE_3, true, false),
+    FOUR(4, GameRule.MATCH_PRICE_4, true, false),
+    FIVE(5, GameRule.MATCH_PRICE_5, true, false),
+    BONUS(5, GameRule.MATCH_PRICE_BONUS, true, true),
+    SIX(6, GameRule.MATCH_PRICE_6, true, false)
     ;
     private final int winningCount;
     private final int reward;
     private final boolean view;
+    private final boolean bonus;
 
-    LottoWinningPrice(int winningCount, int reward, boolean view) {
+    LottoWinningPrice(int winningCount, int reward, boolean view, boolean bonus) {
         this.winningCount = winningCount;
         this.reward = reward;
         this.view = view;
+        this.bonus = bonus;
     }
 
     public int getWinningCount() {
@@ -41,4 +44,5 @@ public enum LottoWinningPrice {
     public boolean isView(){
         return view;
     }
+
 }
