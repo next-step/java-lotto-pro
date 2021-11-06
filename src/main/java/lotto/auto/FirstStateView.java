8d9af34c88410b27b848e82790a143ae724e184b
ch3224bin <ahrocksil@gmail.com>
2021-11-06 14:08:49 +0900

    refactor : View 종속적인 toString 제거

--- src/main/java/lotto/auto/FirstStateView.java
+++ src/main/java/lotto/auto/FirstStateView.java
@@ -1,9 +1,12 @@
 package lotto.auto;
 
 import lotto.domain.LotteryTicket;
+import lotto.domain.LottoNumber;
 import lotto.domain.LottoNumbers;
 
 import java.io.PrintStream;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class FirstStateView {
     public void printQuestion(PrintStream out) {
@@ -13,8 +16,17 @@ public class FirstStateView {
     public void printResult(PrintStream out, LotteryTicket lotteryTicket) {
         out.printf("%d개를 구매했습니다.%n", lotteryTicket.size());
         for (LottoNumbers lottoNumbers : lotteryTicket.getLottoNumbersList()) {
-            out.println(lottoNumbers);
+            printLottoNumbers(out, lottoNumbers);
         }
         out.println();
+    }
+
+    private void printLottoNumbers(PrintStream out, LottoNumbers lottoNumbers) {
+        List<LottoNumber> numbers = lottoNumbers.getNumbers();
+        String text = numbers.stream()
+                .map(LottoNumber::value)
+                .map(String::valueOf)
+                .collect(Collectors.joining(", ", "[", "]"));
+        out.println(text);
     }
 }
--- src/main/java/lotto/domain/LottoNumbers.java
+++ src/main/java/lotto/domain/LottoNumbers.java
@@ -72,14 +72,6 @@ public class LottoNumbers {
     }
 
     @Override
-    public String toString() {
-        return numbers.stream()
-                .map(LottoNumber::value)
-                .map(String::valueOf)
-                .collect(Collectors.joining(", ", "[", "]"));
-    }
-
-    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
