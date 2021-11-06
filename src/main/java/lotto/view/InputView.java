3ccc8b3d52256bf81d1d16b462de9ba9755f5f1d
jennie267 <jennieee267@gmail.com>
2021-11-06 23:02:59 +0900

    refactor : 사용하지않는부분 정리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -10,8 +10,6 @@ public class InputView {
     private static final String INSERT_MONEY = "구입금액을 입력해 주세요.";
     private static final String INSERT_WINNING_NUMBER = "지난 주 당첨 번호를 입력해 주세요.";
 
-    private static final Scanner scanner = new Scanner(System.in);
-
     public Money enterMoney() {
         System.out.println(INSERT_MONEY);
         return new Money(ConsoleUtils.consoleInt());
