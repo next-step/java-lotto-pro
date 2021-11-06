b15c7d4254abe44aab0ca6d4e2d82004417cbf61
devsigner <devsigner9920@icloud.com>
2021-11-07 01:54:17 +0900

    feat(constants): 상수 클래스 추가
    추가 내용:
    - 에러 메시지 및 결과 출력 메시지를 추가하였습니다.

---
+++ src/main/java/lotto/constants/ErrorMessage.java
@@ -0,0 +1,9 @@
+package lotto.constants;
+
+public class ErrorMessage {
+  public static final String ERROR_PREFIX = "[Error] ";
+  public static final String PURCHASE_AMOUNT_LOWER_ERROR_MESSAGE =
+    ERROR_PREFIX + "로또 가격보다 낮은 가격을 입력할 수 없습니다.";
+  public static final String INPUT_EMPTY_ERROR_MESSAGE =
+    ERROR_PREFIX + "공백을 입력할 수 없습니다.";
+}
---
+++ src/main/java/lotto/constants/OutputMessage.java
@@ -0,0 +1,11 @@
+package lotto.constants;
+
+public class OutputMessage {
+  public static final String INPUT_PURCHASE_AMOUNT_GUIDE_MESSAGE = "구입금액을 입력해 주세요.";
+  public static final String INPUT_BUY_LOTTO_COUNTS_MESSAGE = "%d개를 구매했습니다.";
+  public static final String LAST_WEEK_DRAWN_LOTTO = "지난 주 당첨 번호를 입력해 주세요.";
+  public static final String STATISTICS_GUIDE_MESSAGE = "당첨 통계";
+  public static final String PERFORATION = "-------";
+  public static final String MATCH_RESULT_MESSAGE = "%d개 일치 (%d원)- %d개";
+  public static final String YIELD_MESSAGE = "총 수익률은 %.2f입니다.";
+}
