23c8276ca8d3a96caafc58540d8a6328f9448d8b
lewisseo91 <togokr@naver.com>
2021-11-06 16:02:33 +0900

    feat(BuyAmount): 구매 개수 추가
    BuyAmount 생성
     - 구매 개수 구하는 메서드(getAmount) 추가
     - 구매 개수를 토대로 가격 구하는 메서드(getPrice) 추가

---
+++ src/main/java/lotto/BuyAmount.java
@@ -0,0 +1,19 @@
+package lotto;
+
+public class BuyAmount {
+    public static final int LOTTO_TICKET_PRICE = 1000;
+
+    private final int amount;
+
+    public BuyAmount(int buyPrice) {
+        this.amount = buyPrice / LOTTO_TICKET_PRICE;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public int getPrice() {
+        return amount * LOTTO_TICKET_PRICE;
+    }
+}
