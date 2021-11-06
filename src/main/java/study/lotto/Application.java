1d7fb33dd321b90b8ea27753d428fc67530a0cd5
김도영 <95doyoung@gmail.com>
2021-11-06 11:32:19 +0900

    refactor : LottoGameController 에서 LottoController 로 이름 변경

--- src/main/java/study/lotto/Application.java
+++ src/main/java/study/lotto/Application.java
@@ -1,6 +1,6 @@
 package study.lotto;
 
-import study.lotto.controller.LottoGameController;
+import study.lotto.controller.LottoController;
 import study.lotto.controller.dto.LottoOrderMoneyRequestDto;
 import study.lotto.controller.dto.LottoWinningNumberRequestDto;
 import study.lotto.controller.dto.TicketLotteryBundleResponseDto;
@@ -11,14 +11,14 @@ import study.lotto.view.LottoWinningNumberInputView;
 import study.lotto.view.LottoWinningStatisticsView;
 
 public class Application {
-    private static final LottoGameController controller; // 로또 게임 컨트롤러
+    private static final LottoController controller; // 로또 게임 컨트롤러
     private static final LottoOrderCountInputView orderInputView; // 구매 입력 뷰
     private static final LottoWinningNumberInputView winningNumberInputView; // 당첨 번호 입력 뷰
     private static final LottoOrderResultView orderResultView; // 구매 내역 뷰
     private static final LottoWinningStatisticsView winningStatisticsView;// 당첨 통계 뷰
 
     static {
-        controller = new LottoGameController();
+        controller = new LottoController();
         orderInputView = new LottoOrderCountInputView();
         orderResultView = new LottoOrderResultView();
         winningNumberInputView = new LottoWinningNumberInputView();
--- src/main/java/study/lotto/controller/LottoController.java
+++ src/main/java/study/lotto/controller/LottoController.java
@@ -7,14 +7,15 @@ import study.lotto.controller.dto.WinningStatisticsResponseDto;
 import study.lotto.model.LottoStore;
 import study.lotto.model.WinningStatistics;
 
-public class LottoGameController {
+public class LottoController {
+
     public TicketLotteryBundleResponseDto orderTicketLotteryBundleByMoney(final LottoOrderMoneyRequestDto orderCountRequestDto) {
         final int orderCount = orderCountRequestDto.getOrderCount();
         return new TicketLotteryBundleResponseDto(LottoStore.orderTicketLotteryBundleByMoney(orderCount));
     }
 
     public WinningStatisticsResponseDto referee(final LottoWinningNumberRequestDto winningNumberRequestDto,
-                                     final TicketLotteryBundleResponseDto ticketLotteryBundleResponseDto) {
+                                                final TicketLotteryBundleResponseDto ticketLotteryBundleResponseDto) {
         final WinningStatistics winningStatistics = WinningStatistics.valueOf(ticketLotteryBundleResponseDto.toEntity(), winningNumberRequestDto.toEntity());
         return new WinningStatisticsResponseDto(winningStatistics);
     }
