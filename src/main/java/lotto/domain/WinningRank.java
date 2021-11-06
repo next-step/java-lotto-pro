6e555a7598bb3c4d8203bb33ee7e69882ee40a2d
jennie267 <jennieee267@gmail.com>
2021-11-06 13:56:12 +0900

    feat : 당첨 기준 및 결과 Enum 생성

---
+++ src/main/java/lotto/domain/WinningRank.java
@@ -0,0 +1,28 @@
+package lotto.domain;
+
+import java.util.Arrays;
+
+public enum WinningRank {
+    SIX(6, 2000000000),
+    FIVE(5, 1500000),
+    FOUR(4, 50000),
+    THREE(3, 5000),
+    ZERO(0, 0);
+
+    private final int matchCount;
+    private final int reward;
+
+    WinningRank(int matchCount, int reward) {
+        this.matchCount = matchCount;
+        this.reward = reward;
+    }
+
+    public static WinningRank result(int matchCount) {
+        return Arrays.stream(values()).filter(winningRank -> winningRank.matchCount == matchCount).findFirst().orElse(ZERO);
+    }
+
+    public int getReward() {
+        return this.reward;
+    }
+
+}
