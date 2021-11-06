de01ed8557f14a5398d50987260e0615f48d097e
Wangki Lee <lwk3007@gmail.com>
2021-11-06 12:04:17 +0900

    feature: 게임 시작 기능

---
+++ src/main/java/lotto/Application.java
@@ -0,0 +1,10 @@
+package lotto;
+
+import lotto.controller.Lotto;
+
+public class Application {
+    public static void main(String[] args) {
+        Lotto lotto = new Lotto();
+        lotto.start();
+    }
+}
