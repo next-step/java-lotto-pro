527da214d8ca01fd4dc908bfdd190401ff59a62e
lewisseo91 <togokr@naver.com>
2021-11-06 20:58:27 +0900

    feat(BuyAmountTest): 구매 오류 테스트 추가
    최소 티켓 금액 이하 구매 시도 시 오류 발생하도록 추가
     - 요구사항에는 없었고 자의적으로 필요하다고 생각해서 추가했기에 오류 발생 동작은 변경 가능성 있음

--- src/test/java/lotto/BuyAmountTest.java
+++ src/test/java/lotto/BuyAmountTest.java
@@ -1,10 +1,13 @@
 package lotto;
 
+import lotto.exception.MinimumTicketPriceException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@DisplayName("구매 개수 관련 테스트")
 public class BuyAmountTest {
     @DisplayName("구매 개수 테스트")
     @Test
@@ -13,5 +16,15 @@ public class BuyAmountTest {
         // when
         BuyAmount buyAmount = new BuyAmount(buyPrice);
         assertThat(buyAmount.getAmount()).isEqualTo(30);
+    }
+
+    @DisplayName("구매 오류 테스트")
+    @Test
+    void getBuyAmountExceptionTest() {
+        int buyPrice = 100;
+        // when
+        assertThatThrownBy(() -> {
+            BuyAmount buyAmount = new BuyAmount(buyPrice);
+        }).isInstanceOf(MinimumTicketPriceException.class);
     }
 }
