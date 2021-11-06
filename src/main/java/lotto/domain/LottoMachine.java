c4199213faafe1ac27330c14e744f1d69193a968
Junhong-Kim <jhk1005jhk@gmail.com>
2021-11-06 23:13:11 +0900

    feat(LottoMachine): 지난주 당첨 번호 입력받기
    - 1~45까지 정수에서 중복되지 않은 6개 번호

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -8,21 +8,36 @@ import java.util.List;
 
 public class LottoMachine implements Machine {
 
+    private static final String NUMBER_DELIMITER = ",";
+
     @Override
     public void start() {
         PurchaseAmount purchaseAmount = getPurchaseAmount();
         sell(purchaseAmount);
+        Lotto lastWeekWinningLotto = getLastWeekWinningLotto();
     }
 
     private PurchaseAmount getPurchaseAmount() {
         System.out.println(Messages.PURCHASE_AMOUNT_INPUT);
-        int amount = Console.readInt();
+        int amount = Integer.parseInt(Console.readLine());
         return new PurchaseAmount(amount);
     }
 
     private void sell(PurchaseAmount purchaseAmount) {
         int purchasedLottoCount = getPurchasedLottoCount(purchaseAmount);
         List<Lotto> lottoList = getLottoList(purchasedLottoCount);
+    }
+
+    private Lotto getLastWeekWinningLotto() {
+        System.out.println(System.lineSeparator() + Messages.LAST_WEEK_WINNING_NUMBER_INPUT);
+        String[] stringNumbers = Console.readLine().split(NUMBER_DELIMITER);
+
+        Lotto lotto = new Lotto();
+        for (String stringNumber : stringNumbers) {
+            int number = Integer.parseInt(stringNumber.trim());
+            lotto.addLottoNumber(new LottoNumber(number));
+        }
+        return lotto;
     }
 
     private int getPurchasedLottoCount(PurchaseAmount purchaseAmount) {
