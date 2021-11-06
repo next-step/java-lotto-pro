8db63be41a4b1176d8a3211f64d31ef596335781
songseongbin <ppsosqq@gmail.com>
2021-11-06 16:08:29 +0900

    refactor(InputView): 입력을 받는 로직 InputView 로 이동

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -1,7 +1,5 @@
 package controller;
 
-import java.util.Scanner;
-
 import model.LastWeekWinningNumber;
 import model.Lottos;
 import model.Money;
@@ -10,37 +8,17 @@ import view.InputView;
 import view.ResultView;
 
 public class LottoController {
-	private static Scanner sc = new Scanner(System.in);
 
 	public void start() {
-		InputView.printPurchaseAmountMessage();
-		Money money = inputForPurchaseAmountUntilValid(sc.nextLine());
+		Money money = InputView.printPurchaseAmountMessageAndInput();
 		PurchaseCount purchaseCount = Lottos.purchaseCountFrom(money);
 		ResultView.printPurchaseVolumeMessage(purchaseCount);
 		Lottos lottos = Lottos.purchase(purchaseCount);
 		ResultView.printLottoNumbers(lottos);
 
-		InputView.printLastWeekWinningNumberMessage();
-		LastWeekWinningNumber lastWeekWinningNumber = inputForLastWeekWinningNumberUntilValid(sc.nextLine());
+		LastWeekWinningNumber lastWeekWinningNumber = InputView.printLastWeekWinningNumberAndInput();
 
 		ResultView.printWinningStatisticsMessage(lottos.matchResult(lastWeekWinningNumber), money);
 	}
 
-	public Money inputForPurchaseAmountUntilValid(String money) {
-		while (!Money.validate(money)) {
-			InputView.printErrorPurchaseAmountInvalidation();
-			money = sc.nextLine();
-		}
-
-		return Money.of(money);
-	}
-
-	private LastWeekWinningNumber inputForLastWeekWinningNumberUntilValid(String lastWeekNumber) {
-		while (!LastWeekWinningNumber.validate(lastWeekNumber)) {
-			InputView.printErrorLastWeekWinningNumberInvalidation();
-			lastWeekNumber = sc.nextLine();
-		}
-
-		return LastWeekWinningNumber.of(lastWeekNumber);
-	}
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -2,13 +2,30 @@ package view;
 
 import static view.InputMessage.*;
 
+import java.util.Scanner;
+
+import model.LastWeekWinningNumber;
+import model.Money;
+
 public class InputView {
 
-	public static void printPurchaseAmountMessage() {
+	private static final Scanner sc = new Scanner(System.in);
+
+	public static Money printPurchaseAmountMessageAndInput() {
+		printPurchaseAmountMessage();
+		return inputForPurchaseAmountUntilValid(sc.nextLine());
+	}
+
+	public static LastWeekWinningNumber printLastWeekWinningNumberAndInput() {
+		printLastWeekWinningNumberMessage();
+		return inputForLastWeekWinningNumberUntilValid(sc.nextLine());
+	}
+
+	private static void printPurchaseAmountMessage() {
 		System.out.println(PURCHASE_AMOUNT_MESSAGE);
 	}
 
-	public static void printLastWeekWinningNumberMessage() {
+	private static void printLastWeekWinningNumberMessage() {
 		nextLine();
 		System.out.println(LASE_WEEK_WINNING_NUMBER_MESSAGE);
 	}
@@ -19,6 +36,24 @@ public class InputView {
 
 	public static void printErrorLastWeekWinningNumberInvalidation() {
 		System.out.println(ERROR_LAST_WEEK_WINNING_NUMBER_MESSAGE);
+	}
+
+	private static Money inputForPurchaseAmountUntilValid(String money) {
+		while (!Money.validate(money)) {
+			InputView.printErrorPurchaseAmountInvalidation();
+			money = sc.nextLine();
+		}
+
+		return Money.of(money);
+	}
+
+	private static LastWeekWinningNumber inputForLastWeekWinningNumberUntilValid(String lastWeekNumber) {
+		while (!LastWeekWinningNumber.validate(lastWeekNumber)) {
+			InputView.printErrorLastWeekWinningNumberInvalidation();
+			lastWeekNumber = sc.nextLine();
+		}
+
+		return LastWeekWinningNumber.of(lastWeekNumber);
 	}
 
 	private static void nextLine() {
