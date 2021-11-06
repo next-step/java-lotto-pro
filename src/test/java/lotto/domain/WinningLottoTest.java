a100a8098a46514ed0cdce808a6bbb4e46a3707d
cus4703 <cus4703@gmail.com>
2021-11-06 18:46:04 +0900

    test: WinningLottoTest 테스트 케이스 생성

---
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -0,0 +1,77 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+class WinningLottoTest {
+
+    static Stream<Arguments> listProvide() {
+        List<Number> matchNumber = Arrays.asList(new Number(1), new Number(2), new Number(3),
+                new Number(4), new Number(5), new Number(6));
+        Number bonusNumber = new Number(7);
+
+        List<Number> lottoNumber = Arrays.asList(new Number(1), new Number(2), new Number(3),
+                new Number(4), new Number(5), new Number(7));
+
+        return Stream.of(arguments(lottoNumber, matchNumber, bonusNumber));
+    }
+
+    @ParameterizedTest
+    @MethodSource("listProvide")
+    @DisplayName("로또 번호 보너스 매칭 확인")
+    public void isExistBonusNumberTest(List<Number> lottoNumber, List<Number> matchNumber, Number bonusNumber) {
+        WinningLotto winningLotto = new WinningLotto(matchNumber, bonusNumber);
+
+        boolean excepted = winningLotto.isExistBonusNumber(lottoNumber);
+
+        assertThat(excepted).isTrue();
+    }
+
+    static Stream<Arguments> listProvide2() {
+        List<Number> matchNumber = Arrays.asList(new Number(1), new Number(2), new Number(3),
+                new Number(4), new Number(5), new Number(6));
+        Number bonusNumber = new Number(7);
+        return Stream.of(arguments(matchNumber, bonusNumber));
+    }
+
+    @ParameterizedTest
+    @MethodSource("listProvide2")
+    @DisplayName("로또 번호 매칭 확인")
+    public void isNumberTest(List<Number> matchNumber, Number bonusNumber) {
+        WinningLotto winningLotto = new WinningLotto(matchNumber, bonusNumber);
+        Number number = new Number(3);
+
+        boolean excepted = winningLotto.isMatchNumber(number);
+
+        assertThat(excepted).isTrue();
+    }
+
+    static Stream<Arguments> listProvide3() {
+        List<Number> lottoNumber = Arrays.asList(new Number(1), new Number(2), new Number(3), new Number(4), new Number(5), new Number(7));
+        List<Number> matchNumber = Arrays.asList(new Number(1), new Number(2), new Number(3), new Number(4), new Number(5), new Number(6));
+        return Stream.of(arguments(lottoNumber, matchNumber, new Number(7)));
+    }
+
+    @ParameterizedTest
+    @MethodSource("listProvide3")
+    @DisplayName("로또 매칭 결과 확인")
+    public void getLottoMatchResult(List<Number> lottoNumber, List<Number> matchNumber, Number bonusNumber) {
+        WinningLotto winningLotto = new WinningLotto(matchNumber, bonusNumber);
+        LottoNumber lotto = new LottoNumber(lottoNumber);
+
+        LottoResult lottoMatchResult = winningLotto.getLottoMatchResult(Arrays.asList(lotto));
+        int matchRankCount = lottoMatchResult.getMatchRankCount(Rank.SECOND);
+
+        assertThat(matchRankCount).isEqualTo(1);
+    }
+
+}
