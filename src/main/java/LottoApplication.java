56f43e17bc2e286e9cc454d61e00b8028593b220
lewisseo91 <togokr@naver.com>
2021-11-06 20:41:20 +0900

    feat(LottoApplication): 로또 메인 함수 추가
    로또 메인 함수 추가

---
+++ src/main/java/LottoApplication.java
@@ -0,0 +1,9 @@
+import lotto.ui.LottoController;
+
+public class LottoApplication {
+
+    public static void main(String[] args) {
+        LottoController lottoController = new LottoController();
+        lottoController.startGame();
+    }
+}
