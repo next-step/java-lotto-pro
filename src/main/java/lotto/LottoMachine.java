935637a7a28aa3d37e8c217683e43b0be5f0adbf
minho <lights8615@gmail.com>
2021-11-06 14:15:03 +0900

    refactor(step3): 출력 메시지 인터페이스 구현체로 적용

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -7,30 +7,30 @@ import view.ResultView;
 public class LottoMachine {
     public void start() {
         LottoMoney lottoMoney = getLottoMoney();
+
         LottoCount lottoCount = lottoMoney.calculateLottoCount();
-        ResultView.print(() ->
-            lottoCount.makePrintableMessage() + OutputMessage.PRINT_NUMBER_OF_PURCHASED_LOTTO.getMessage());
+        ResultView.print(() -> String.format(OutputMessage.PRINT_NUMBER_OF_PURCHASED_LOTTO.makePrintableMessage(),
+            lottoCount.makePrintableMessage()));
 
         LottoTickets lottoTickets = LottoTicketFactory.createRandomLottoTickets(lottoCount);
         ResultView.print(lottoTickets);
-
         printWinningStatistics(lottoTickets.calculateResult(getWinnerTicket()));
     }
 
     private LottoMoney getLottoMoney() {
-        ResultView.print(OutputMessage.ASK_PURCHASE_AMOUNT::getMessage);
+        ResultView.print(OutputMessage.ASK_PURCHASE_AMOUNT);
         return new LottoMoney(InputView.readLine());
     }
 
     private LottoTicket getWinnerTicket() {
-        ResultView.print(OutputMessage.ASK_WINNER_TICKET::getMessage);
+        ResultView.print(OutputMessage.ASK_WINNER_TICKET);
         return new LottoTicket(InputView.readLine());
     }
 
     private void printWinningStatistics(LottoResults lottoResults) {
-        ResultView.print(OutputMessage.PRINT_STATISTICS_INTRO::getMessage);
+        ResultView.print(OutputMessage.PRINT_STATISTICS_INTRO);
         ResultView.print(lottoResults);
-        ResultView.print(() -> String.format(OutputMessage.PRINT_TOTAL_EARNING_RATE.getMessage(),
+        ResultView.print(() -> String.format(OutputMessage.PRINT_TOTAL_EARNING_RATE.makePrintableMessage(),
             lottoResults.calculateEarningRate().makePrintableMessage()));
     }
 }
--- src/main/java/view/OutputMessage.java
+++ src/main/java/view/OutputMessage.java
@@ -1,8 +1,8 @@
 package view;
 
-public enum OutputMessage {
+public enum OutputMessage implements Printable {
     ASK_PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
-    PRINT_NUMBER_OF_PURCHASED_LOTTO("개를 구매했습니다."),
+    PRINT_NUMBER_OF_PURCHASED_LOTTO("%s개를 구매했습니다."),
     ASK_WINNER_TICKET("지난 주 당첨 번호를 입력해 주세요."),
     PRINT_STATISTICS_INTRO("당첨 통계\n---------"),
     PRINT_TOTAL_EARNING_RATE("총 수익률은 %s 입니다.");
@@ -13,7 +13,8 @@ public enum OutputMessage {
         this.message = message;
     }
 
-    public String getMessage() {
+    @Override
+    public String makePrintableMessage() {
         return message;
     }
 }
