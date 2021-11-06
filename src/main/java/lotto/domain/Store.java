1dbdae367f5a2e49f0a5439b6fd3d319af689909
Junhwan Kim <junhwan.tech@gmail.com>
2021-11-06 22:47:55 +0900

    feat: 로또 판매점을 표현하는 Store 클래스 구현

---
+++ src/main/java/lotto/domain/Store.java
@@ -0,0 +1,42 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Store {
+	public static final String MINIMUM_PRICE_REQUIRED_ERROR = "구매 금액은 1000원 이상이어야 합니다.";
+
+	private static int LOTTO_PRICE = 1000;
+
+	private Store() {
+	}
+
+	public static List<Lotto> sell(final Money money) {
+		validateMinimumPrice(money);
+
+		int count = calcBuyAbleCount(money);
+		money.spend(count * LOTTO_PRICE);
+
+		return issueLottos(count);
+	}
+
+	private static List<Lotto> issueLottos(int count) {
+		List<Lotto> lottos = new ArrayList<>();
+
+		for (int i = 0; i < count; i++) {
+			lottos.add(LottoGenerator.generate());
+		}
+
+		return lottos;
+	}
+
+	private static int calcBuyAbleCount(Money money) {
+		return money.amount() / LOTTO_PRICE;
+	}
+
+	private static void validateMinimumPrice(final Money money) {
+		if (money.amount() < LOTTO_PRICE) {
+			throw new IllegalArgumentException(MINIMUM_PRICE_REQUIRED_ERROR);
+		}
+	}
+}
