8cb28090171f18a7f1e8736cde54ada0c30d754e
minho <lights8615@gmail.com>
2021-11-06 14:10:29 +0900

    refactor(step3): 로또 머신 기능 분리

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -6,20 +6,31 @@ import view.ResultView;
 
 public class LottoMachine {
     public void start() {
-        ResultView.print(OutputMessage.ASK_PURCHASE_AMOUNT::getMessage);
-        LottoMoney lottoMoney = new LottoMoney(InputView.readLine());
+        LottoMoney lottoMoney = getLottoMoney();
         LottoCount lottoCount = lottoMoney.calculateLottoCount();
         ResultView.print(() ->
             lottoCount.makePrintableMessage() + OutputMessage.PRINT_NUMBER_OF_PURCHASED_LOTTO.getMessage());
 
         LottoTickets lottoTickets = LottoTicketFactory.createRandomLottoTickets(lottoCount);
         ResultView.print(lottoTickets);
-        ResultView.print(OutputMessage.ASK_WINNER_TICKET::getMessage);
 
-        LottoTicket winnerTicket = new LottoTicket(InputView.readLine());
-        LottoResults lottoResults = lottoTickets.calculateResult(winnerTicket);
+        printWinningStatistics(lottoTickets.calculateResult(getWinnerTicket()));
+    }
+
+    private LottoMoney getLottoMoney() {
+        ResultView.print(OutputMessage.ASK_PURCHASE_AMOUNT::getMessage);
+        return new LottoMoney(InputView.readLine());
+    }
+
+    private LottoTicket getWinnerTicket() {
+        ResultView.print(OutputMessage.ASK_WINNER_TICKET::getMessage);
+        return new LottoTicket(InputView.readLine());
+    }
+
+    private void printWinningStatistics(LottoResults lottoResults) {
         ResultView.print(OutputMessage.PRINT_STATISTICS_INTRO::getMessage);
         ResultView.print(lottoResults);
-        ResultView.print(() -> String.format(OutputMessage.PRINT_TOTAL_EARNING_RATE.getMessage(), lottoResults.calculateEarningRate().makePrintableMessage()));
+        ResultView.print(() -> String.format(OutputMessage.PRINT_TOTAL_EARNING_RATE.getMessage(),
+            lottoResults.calculateEarningRate().makePrintableMessage()));
     }
 }
