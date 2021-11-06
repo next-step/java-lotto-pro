0d77f2c1903d16b41f7c4e1be4ec27208e3dc3b4
Jeongkyo Kim <chavitak@gmail.com>
2021-11-06 11:50:20 +0900

    feat: 로또 번호가 6개의 정수로 구성되지 않으면 예외를 출력한다

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -1,14 +1,18 @@
 package lotto.domain;
 
+import lotto.exception.IllegalLottoNumberSizeException;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class LottoTicket {
+    public static final int LOTTO_NUMBER_SIZE = 6;
     List<Integer> lottoNumbers;
 
     public LottoTicket(List<Integer> lottoNumbers) {
+        checkLottoNumberSize(lottoNumbers);
         this.lottoNumbers = lottoNumbers;
     }
 
@@ -16,6 +20,12 @@ public class LottoTicket {
         List<Integer> lottoNumbers = IntStream.range(1, 46).boxed().collect(Collectors.toList());
         Collections.shuffle(lottoNumbers);
         return new LottoTicket(lottoNumbers.subList(0, 6));
+    }
+
+    private void checkLottoNumberSize(List<Integer> lottoNumbers) {
+        if (lottoNumbers.size() != LOTTO_NUMBER_SIZE) {
+            throw new IllegalLottoNumberSizeException();
+        }
     }
 
     public String toResultString() {
--- src/test/java/lotto/domain/LottoTicketTest.java
+++ src/test/java/lotto/domain/LottoTicketTest.java
@@ -1,19 +1,27 @@
 package lotto.domain;
 
+import lotto.exception.IllegalLottoNumberSizeException;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class LottoTicketTest {
 
     @Test
     void toStringTest() {
-        // given
+        // given, when
         LottoTicket lottoTicket = new LottoTicket(Arrays.asList(1, 2, 3, 4, 5, 6));
 
         // then
         assertThat(lottoTicket.toResultString()).isEqualTo("[1, 2, 3, 4, 5, 6]");
+    }
+
+    @Test
+    void numberSizeTest() {
+        assertThatThrownBy(() -> new LottoTicket(Arrays.asList(1, 2, 3, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalLottoNumberSizeException.class);
     }
 }
