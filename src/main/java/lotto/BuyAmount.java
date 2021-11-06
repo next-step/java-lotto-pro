ff14298450be66fdb20ad0219684980bc1c8944c
lewisseo91 <togokr@naver.com>
2021-11-06 21:00:11 +0900

    feat(MinimumTicketPriceException): 최소 티켓 금액 이하 구매 시도 오류 추가
    MinimumTicketPriceException 추가
    - 최소 티켓 금액 이하 구매 시도 오류 추가 : runtimeException 상속
    BuyAmount validateBuyPrice 추가
    - 최소 티켓 금액 이하 구매 시 오류 발생하도록 메서드 추가

--- src/main/java/lotto/BuyAmount.java
+++ src/main/java/lotto/BuyAmount.java
@@ -1,12 +1,22 @@
 package lotto;
 
+import lotto.exception.MinimumTicketPriceException;
+
 public class BuyAmount {
+    private static final String MINIMUM_TICKET_PRICE_MESSAGE = "구입 금액을 확인해 주세요.";
     public static final int LOTTO_TICKET_PRICE = 1000;
 
     private final int amount;
 
     public BuyAmount(int buyPrice) {
+        validateBuyPrice(buyPrice);
         this.amount = buyPrice / LOTTO_TICKET_PRICE;
+    }
+
+    private void validateBuyPrice(int buyPrice) {
+        if (buyPrice < LOTTO_TICKET_PRICE) {
+            throw new MinimumTicketPriceException(MINIMUM_TICKET_PRICE_MESSAGE);
+        }
     }
 
     public int getAmount() {
