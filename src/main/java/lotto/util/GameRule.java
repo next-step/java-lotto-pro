d23b2cc3411b14d997f11cef1983a86ce7b2887e
IW.MOON <mooniw02@gmail.com>
2021-11-06 20:59:26 +0900

    feat: 보너스 금액 추가

--- src/main/java/lotto/util/GameRule.java
+++ src/main/java/lotto/util/GameRule.java
@@ -12,6 +12,7 @@ public class GameRule {
     public static final int MATCH_PRICE_3 = 5_000;
     public static final int MATCH_PRICE_4 = 5_0000;
     public static final int MATCH_PRICE_5 = 1_500_000;
+    public static final int MATCH_PRICE_BONUS = 30_000_000;
     public static final int MATCH_PRICE_6 = 2_000_000_000;
 
     public static final String LOTTO_NUMBER_DELIMITER = ",";
