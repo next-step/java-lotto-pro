ca496bc52b613479353a7d1c53e76fc18eb59fe5
Wangki Lee <lwk3007@gmail.com>
2021-11-06 12:03:32 +0900

    fix: 당첨번호 미입력시 다시 입력하도록 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -27,8 +27,8 @@ public class InputView {
     public static LottoTicket winningNumbersOfLastWeek() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         String numbers = scanner.nextLine();
-        List<Integer> winningNumbers = LottoNumbersFactory.convertInputNumbersToNumbers(numbers);
         try {
+            List<Integer> winningNumbers = LottoNumbersFactory.createManualLottoNumbers(numbers);
             return new LottoTicket(winningNumbers);
         } catch (IllegalArgumentException exception) {
             System.out.println(exception.getMessage());
