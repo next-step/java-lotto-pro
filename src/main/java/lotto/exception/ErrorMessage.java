eb0c748894d3e974ef79ca24eb7750df3f7c8634
JiHunHong <hongji3354@gmail.com>
2021-11-06 20:34:03 +0900

    refactor : ErrorMessage에 문자열 형식화 적용

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -4,9 +4,9 @@ import lotto.domain.LottoProperty;
 
 public enum ErrorMessage {
 
-    PURCHASE_AMOUNT_NOT_ONE_THOUSAND_WON("로또 구입금액은" + LottoProperty.LOTTO_PRICE + "원 단위 입니다."),
-    LOTTO_NUMBER_COUNT("로또 번호는 " + LottoProperty.LOTTO_COUNT + " 개 입니다."),
-    LOTTO_NUMBER_RANGE("로또 번호는 " + LottoProperty.LOTTO_START_NUMBER + " 부터 " + LottoProperty.LOTTO_END_NUMBER + " 사이의 숫자입니다."),
+    PURCHASE_AMOUNT_NOT_ONE_THOUSAND_WON(String.format("로또 구입금액은 %d원 단위 입니다.", LottoProperty.LOTTO_PRICE)),
+    LOTTO_NUMBER_COUNT(String.format("로또 번호는 %d개 입니다.", LottoProperty.LOTTO_COUNT)),
+    LOTTO_NUMBER_RANGE(String.format("로또 번호는 %d부터 %d 사이의 숫자입니다.", LottoProperty.LOTTO_START_NUMBER, LottoProperty.LOTTO_END_NUMBER)),
     LOTTO_NUMBER_DUPLICATE("로또 번호는 중복될 수 없습니다.");
 
     private String message;
