f66e3caa26ed447ad159f98e2210a8130c9cbd64
peter-alethio <peter@alethio.io>
2021-11-06 10:33:56 +0900

    refactor(randomNumbers): 랜덤값 생성 로직 변경

--- src/main/java/lotto/domain/RandomNumbers.java
+++ src/main/java/lotto/domain/RandomNumbers.java
@@ -1,29 +1,28 @@
 package lotto.domain;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class RandomNumbers implements Numbers{
 
     private static final int MAX_SIZE = 6;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
 
-    private Number number;
+    private static final List<Integer> numbers;
 
-    public RandomNumbers(final Number number) {
-        this.number = number;
+    static {
+        numbers = IntStream.range(MIN_NUMBER, MAX_NUMBER)
+                .mapToObj(Integer::new)
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<Integer> random() {
-        final List list = new ArrayList(getRandomNumbers());
-        Collections.shuffle(list);
-        return list;
-    }
-
-    private Set<Integer> getRandomNumbers() {
-        final Set<Integer> numbers = new HashSet();
-        while (numbers.size() < MAX_SIZE) {
-            numbers.add(number.generate());
-        }
-        return numbers;
+        Collections.shuffle(numbers);
+        return numbers.stream()
+                .limit(MAX_SIZE)
+                .collect(Collectors.toList());
     }
 }
