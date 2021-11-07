4e9716fa9089cca57c4ccdce67caef5ca7a1bfb5
peter-alethio <peter@alethio.io>
2021-11-07 19:51:07 +0900

    test(rank): 3개 일치 보너스 추가 테스트

--- src/test/java/lotto/domain/RankTest.java
+++ src/test/java/lotto/domain/RankTest.java
@@ -33,4 +33,9 @@ class RankTest {
     void 당첨번호_개수가_5개이고_보너스_숫자가_있으면_SECOND_BONUS를_반환한다() {
         assertThat(Rank.rank(5, true)).isEqualTo(Rank.SECOND);
     }
+
+    @Test
+    void 당첨번호_개수가_3개이고_보너스_숫자가_존재한다() {
+        assertThat(Rank.rank(3, true)).isEqualTo(Rank.FIFTH);
+    }
 }
