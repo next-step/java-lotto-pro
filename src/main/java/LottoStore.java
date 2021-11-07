9645cf9fcb73f99612ba1b07d35906f9c3f871ce
kelicia91 <kelicia91@gmail.com>
2021-11-07 20:50:35 +0900

    feat: LottoStore.sell() - 수동 판매 추가

--- src/main/java/LottoStore.java
+++ src/main/java/LottoStore.java
@@ -1,3 +1,4 @@
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -6,14 +7,15 @@ public class LottoStore {
 
 	public static final String KRW_UNIT = "원";
 
-	public List<Lotto> sell(String pay) {
+	public List<Lotto> sell(String pay, List<String> manuallyLottos) {
 		final int paidKRW = parsePaidKRW(pay);
-		validatePayment(paidKRW);
+		validatePayment(paidKRW, manuallyLottos);
 
-		final int numOfLottos = paidKRW / Lotto.PRICE_KRW;
-		return Stream.iterate(1, num -> num + 1)
-			.limit(numOfLottos)
-			.map(num -> LottoFactory.of())
+		final List<Lotto> lottosManually = sellLottosManually(manuallyLottos);
+		final List<Lotto> lottosAuto = sellLottosAuto(paidKRW, lottosManually);
+
+		return Stream.of(lottosManually, lottosAuto)
+			.flatMap(Collection::stream)
 			.collect(Collectors.toList());
 	}
 
@@ -25,9 +27,29 @@ public class LottoStore {
 		}
 	}
 
-	private void validatePayment(int paidKRW) {
+	private void validatePayment(int paidKRW, List<String> manuallyLottos) {
 		if (paidKRW < Lotto.PRICE_KRW) {
 			throw new LottoStorePaymentException();
 		}
+		final int numOfLottosCanBeSold = paidKRW / Lotto.PRICE_KRW;
+		if (numOfLottosCanBeSold < manuallyLottos.size()) {
+			throw new LottoStorePaymentException();
+		}
+	}
+
+	private List<Lotto> sellLottosManually(List<String> lottos) {
+		return lottos.stream()
+			.limit(lottos.size())
+			.map(LottoFactory::from)
+			.collect(Collectors.toList());
+	}
+
+	private List<Lotto> sellLottosAuto(int paidKRW, List<Lotto> lottosManually) {
+		final int numOfLottosCanBeSold = paidKRW / Lotto.PRICE_KRW;
+		final int numOfLottosAuto = numOfLottosCanBeSold - lottosManually.size();
+		return Stream.iterate(1, num -> num + 1)
+			.limit(numOfLottosAuto)
+			.map(num -> LottoFactory.of())
+			.collect(Collectors.toList());
 	}
 }
--- src/test/java/LottoStoreTest.java
+++ src/test/java/LottoStoreTest.java
@@ -1,7 +1,10 @@
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -9,19 +12,44 @@ import org.junit.jupiter.params.provider.ValueSource;
 public class LottoStoreTest {
 
 	private final LottoStore lottoStore = new LottoStore();
+	private final List<String> EMPTY_MANUALLY_LOTTOS = Collections.emptyList();
 
 	@ParameterizedTest
 	@CsvSource(value = {"3000:3", "1234:1"}, delimiter = ':')
-	public void sell(String paidKRW, int numOfLottos) {
-		final List<Lotto> soldLottos = lottoStore.sell(paidKRW);
+	void sell_onlyAuto(String paidKRW, int numOfLottos) {
+		final List<Lotto> soldLottos = lottoStore.sell(paidKRW, EMPTY_MANUALLY_LOTTOS);
 		assertThat(soldLottos.size()).isEqualTo(numOfLottos);
 	}
 
 	@ParameterizedTest
-	@ValueSource(strings = {"-1", "0", "NaN"})
-	public void sell_notEnoughKRW(String pay) {
+	@ValueSource(strings = {"-1", "0", "500", "NaN"})
+	void sell_invalidPay(String pay) {
 		assertThatExceptionOfType(LottoStorePaymentException.class)
-			.isThrownBy(() -> lottoStore.sell(pay))
+			.isThrownBy(() -> lottoStore.sell(pay, EMPTY_MANUALLY_LOTTOS))
 			.withMessage(LottoStorePaymentException.ERROR_NOT_ENOUGH_KRW);
+	}
+
+	@Test
+	void sell_tooManyManuallyLottos() {
+		final String pay = String.valueOf(Lotto.PRICE_KRW);
+		final List<String> manuallyLottos = Arrays.asList("1,2,3,4,5,6", "1,2,3,4,5,6");
+		assertThatExceptionOfType(LottoStorePaymentException.class)
+			.isThrownBy(() -> lottoStore.sell(pay, manuallyLottos))
+			.withMessage(LottoStorePaymentException.ERROR_NOT_ENOUGH_KRW);
+	}
+
+	@Test
+	void sell_onlyManually() {
+		final List<String> manuallyLottos = Collections.singletonList("1,2,3,4,5,6");
+		final String pay = String.valueOf(manuallyLottos.size() * Lotto.PRICE_KRW);
+		assertThat(lottoStore.sell(pay, manuallyLottos).size()).isEqualTo(manuallyLottos.size());
+	}
+
+	@Test
+	void sell_mixedAutoWithManually() {
+		final List<String> manuallyLottos = Collections.singletonList("1,2,3,4,5,6");
+		final int numOfLottos = 1 + manuallyLottos.size();
+		final String pay = String.valueOf(numOfLottos * Lotto.PRICE_KRW);
+		assertThat(lottoStore.sell(pay, manuallyLottos).size()).isEqualTo(numOfLottos);
 	}
 }
