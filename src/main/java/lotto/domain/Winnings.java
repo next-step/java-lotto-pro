91eb3168ca561bff51f2b8829ebae88906ea6681
eedys <eedys@ex-em.com>
2021-11-07 13:55:01 +0900

    feat 2등 추가에 따른 일치하는 개수, 당첨금 추가하는 기능 개발

--- src/main/java/lotto/domain/Winnings.java
+++ src/main/java/lotto/domain/Winnings.java
@@ -10,9 +10,10 @@ import static java.util.stream.Collectors.toMap;
 public enum Winnings {
 
     FIRST(6, 2_000_000_000),
-    SECOND(5, 1_500_000),
-    THIRD(4, 50_000),
-    FORTH(3, 5_000);
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FORTH(4, 50_000),
+    FIFTH(3, 5_000);
 
     private final int correspondCount;
     private final long amount;
