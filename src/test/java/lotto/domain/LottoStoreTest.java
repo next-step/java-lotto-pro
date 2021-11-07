08c4985ecc8ce8d9059950494d8504114a652b82
JiHunHong <hongji3354@gmail.com>
2021-11-07 16:30:16 +0900

    test : 구입 금액을 가지고 로또 발급 개수 확인하는 테스트

--- src/test/java/lotto/domain/LottoStoreTest.java
+++ src/test/java/lotto/domain/LottoStoreTest.java
@@ -2,15 +2,20 @@ package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoStoreTest {
 
-    //TODO : 질문2
     @DisplayName("로또 구매")
     @ParameterizedTest
-    @ValueSource(ints = {1000, 10000, 50000})
-    void lottoPurchase(int purchaseAmount) {
+    @CsvSource(value = {"1000:1", "10000:10", "50000:50"}, delimiter = ':')
+    void lottoPurchase(int purchaseAmount, int expectLottoPurchaseNumber) {
         Lottos lottos = LottoStore.purchase(purchaseAmount);
+
+        int lottoPurchaseNumber = lottos.purchaseNumber();
+
+        assertThat(lottoPurchaseNumber).isEqualTo(expectLottoPurchaseNumber);
     }
 }
