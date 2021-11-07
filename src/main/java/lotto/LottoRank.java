8ce994f4b0d95fb88d16adfa99ee8c1d232e10c0
youngrock.choi <tbvjdudfhr@naver.com>
2021-11-07 17:09:16 +0900

    feat: 로또 순위 enum 추가

---
+++ src/main/java/lotto/LottoRank.java
@@ -0,0 +1,38 @@
+package lotto;
+
+import java.util.Arrays;
+
+public enum LottoRank {
+    NO_MATCHES(0, 0),
+    THREE_MATCHES(3, 5000),
+    FOUR_MATCHES(4, 50000),
+    FIVE_MATCHES(5, 1500000),
+    SIX_MATCHES(6, 2000000000);
+
+    private final int matchCount;
+    private final int money;
+
+    LottoRank(int matchCount, int money) {
+        this.matchCount = matchCount;
+        this.money = money;
+    }
+
+    public static LottoRank valueOf(int matchCount) {
+        return Arrays.stream(LottoRank.values())
+                .filter(lottoRank -> lottoRank.matchCount == matchCount)
+                .findAny()
+                .orElse(LottoRank.NO_MATCHES);
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public long getTotal(int count) {
+        return money * count;
+    }
+}
---
+++ src/test/java/lotto/LottoRankTest.java
@@ -0,0 +1,14 @@
+package lotto;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoRankTest {
+    @ParameterizedTest
+    @EnumSource(names = {"FIVE_MATCHES"})
+    void rankTest(LottoRank lottoRank) {
+        assertThat(lottoRank).isEqualTo(LottoRank.valueOf(5));
+    }
+}
