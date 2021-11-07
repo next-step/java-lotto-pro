8fe021f928fa4132d84092edec32fa2e78cb0326
JiHunHong <hongji3354@gmail.com>
2021-11-07 15:38:13 +0900

    refactor : 구매금액만큼 로또를 발급하는 기능 분리
    Getter를 사용하지 않기 위해 로또발급개수 데이터를 가지고 있는 클래스에서 로또를 발급하는 기능을 제공하였으나 클래스가 가진 책임에 맞지도 않고 책임이 과도하다 생각하여 Getter를 제공하고 로또를 발급하는 책임을 가진 클래스에서 로또를 발급하는 기능을 제공하도록 변경

--- src/main/java/lotto/domain/LottoIssuanceCount.java
+++ src/main/java/lotto/domain/LottoIssuanceCount.java
@@ -2,8 +2,6 @@ package lotto.domain;
 
 import lotto.exception.ErrorMessage;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 //로또 발급 개수와 관련된 역할을 가진 클래스
@@ -21,19 +19,14 @@ public class LottoIssuanceCount {
         return new LottoIssuanceCount(issuanceCount);
     }
 
+    public int getIssuanceCount() {
+        return issuanceCount;
+    }
+
     private static void purchaseAmountOneThousandWonUnitValid(int purchaseAmount) {
         if (purchaseAmount < LottoProperty.LOTTO_PRICE || purchaseAmount % LottoProperty.LOTTO_PRICE != 0) {
             throw new IllegalArgumentException(ErrorMessage.PURCHASE_AMOUNT_NOT_ONE_THOUSAND_WON.getMessage());
         }
-    }
-
-    //TODO : 질문1
-    public List<Lotto> createLotto() {
-        List<Lotto> lottos = new ArrayList<>();
-        for (int i = 0; i < this.issuanceCount; i++) {
-            lottos.add(new Lotto(LottoNumber.generator()));
-        }
-        return lottos;
     }
 
     @Override
--- src/main/java/lotto/domain/LottoStore.java
+++ src/main/java/lotto/domain/LottoStore.java
@@ -1,5 +1,8 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //구매 금액만큼 로또를 발급하는 역할을 가진 클래스
 public class LottoStore {
 
@@ -7,8 +10,16 @@ public class LottoStore {
     }
 
     public static Lottos purchase(int purchaseAmount) {
-        LottoIssuanceCount lottoIssuanceCount = LottoIssuanceCount.issuanceNumberCalculation(purchaseAmount);
+        LottoIssuanceCount issuance = LottoIssuanceCount.issuanceNumberCalculation(purchaseAmount);
+        List<Lotto> lottos = createLottos(issuance);
+        return new Lottos(lottos);
+    }
 
-        return new Lottos(lottoIssuanceCount.createLotto());
+    private static List<Lotto> createLottos(LottoIssuanceCount issuance) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < issuance.getIssuanceCount(); i++) {
+            lottos.add(new Lotto(LottoNumber.generator()));
+        }
+        return lottos;
     }
 }
