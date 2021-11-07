0aba21653a8969bd41e0405f17e7dc90e553f1e4
신민준 <jxin95@gmail.com>
2021-11-07 15:22:18 +0900

    refactor: 큰 숫자에 언더바(_)를 사용하여 가독성 개선

--- src/main/java/lotto/common/Constants.java
+++ src/main/java/lotto/common/Constants.java
@@ -8,12 +8,12 @@ public class Constants {
     public static final int MIN_RANGE_VALUE = 1;
     public static final int MAX_RANGE_VALUE = 45;
     public static final int GET_NUMBER_COUNT = 6;
-    public static final int GAME_PRICE = 1000;
+    public static final int GAME_PRICE = 1_000;
     public static final Map<Integer, Integer> PRIZE_MONEY = new HashMap<Integer, Integer>(){{
-        put(3, 5000);
-        put(4, 50000);
-        put(5, 1500000);
-        put(6, 2000000000);
+        put(3, 5_000);
+        put(4, 50_000);
+        put(5, 1_500_000);
+        put(6, 2_000_000_000);
     }};
 
 }
