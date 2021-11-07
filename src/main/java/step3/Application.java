a4beb729ec8d0f1d730998e9a4ec98125d90b14e
vicjed <vicjed@icloud.com>
2021-11-07 22:55:24 +0900

    fix(Application): 실행 로직 변경

--- src/main/java/step3/Application.java
+++ src/main/java/step3/Application.java
@@ -1,6 +1,7 @@
 package step3;
 
 import step3.machine.AutoMachineValidation;
+import step3.machine.LottoMachineFacade;
 import step3.view.RequestView;
 import step3.view.ReturnView;
 
@@ -8,9 +9,10 @@ public class Application {
 	public static void main(String[] args) {
 		LottoMachineFacade lottoMachineFacade = new LottoMachineFacade(new LottoNumberService(),
 			new AutoMachineValidation(),
-			new RequestView(), new ReturnView());
+			new RequestView(),
+			new ReturnView());
 
-		lottoMachineFacade.pick();
-		lottoMachineFacade.result();
+		lottoMachineFacade.start();
+		lottoMachineFacade.findWinner();
 	}
 }
