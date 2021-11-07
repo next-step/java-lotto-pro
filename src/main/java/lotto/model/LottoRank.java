1e6734653309d1cd07e93aab4544054172f26bce
moon <hoopster@airi.kr>
2021-11-08 00:18:40 +0900

    feat (LottoRank) : 2등(보너스) 당첨 랭크 추가

--- src/main/java/lotto/model/LottoRank.java
+++ src/main/java/lotto/model/LottoRank.java
@@ -5,23 +5,33 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum LottoRank {
-	FIRST(6, 2_000_000_000),
-	SECOND(5, 1_500_000),
-	THIRD(4, 50_000),
-	FORTH(3, 5_000),
-	NOTHING(0, 0);
+	FIRST(6, 0, 2_000_000_000),
+	SECOND(5, 1, 30_000_000),
+	THIRD(5, 0, 1_500_000),
+	FORTH(4, 0, 50_000),
+	FIFTH(3, 0, 5_000),
+	NOTHING(0, 0, 0);
 
 	private int containsCount;
+	private int bonusCount;
 	private int money;
 
-	LottoRank(int containsCount, int money) {
+	LottoRank(int containsCount, int bonusCount, int money) {
 		this.containsCount = containsCount;
+		this.bonusCount = bonusCount;
 		this.money = money;
 	}
 
 	public static LottoRank getRankCode(int containsCount) {
 		return Arrays.stream(LottoRank.values())
 			.filter(rankCode -> rankCode.containsCount == containsCount)
+			.findAny()
+			.orElse(NOTHING);
+	}
+
+	public static LottoRank getRankCode(int containsCount, int bonusCount) {
+		return Arrays.stream(LottoRank.values())
+			.filter(rankCode -> rankCode.containsCount == containsCount && rankCode.bonusCount == bonusCount)
 			.findAny()
 			.orElse(NOTHING);
 	}
