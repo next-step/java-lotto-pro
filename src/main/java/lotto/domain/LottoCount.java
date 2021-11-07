06b794f6cb0951a0ddf6d994ca10aa87c67ed449
minho <lights8615@gmail.com>
2021-11-07 23:22:55 +0900

    feat(step3): 로또 숫자 validation 추가

--- src/main/java/lotto/domain/LottoCount.java
+++ src/main/java/lotto/domain/LottoCount.java
@@ -2,13 +2,23 @@ package lotto.domain;
 
 import java.util.Objects;
 
+import lotto.exception.LottoErrorCode;
+import lotto.exception.LottoException;
 import view.Printable;
 
 public class LottoCount implements Printable {
+    private static final int MIN_COUNT = 0;
     private final long count;
 
     public LottoCount(long count) {
+        validate(count);
         this.count = count;
+    }
+
+    private void validate(long count) {
+        if (count < MIN_COUNT) {
+            throw new LottoException(LottoErrorCode.INVALID_LOTTO_COUNT);
+        }
     }
 
     public boolean isBiggerThan(long count) {
--- src/test/java/lotto/domain/LottoCountTest.java
+++ src/test/java/lotto/domain/LottoCountTest.java
@@ -6,14 +6,26 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import lotto.exception.LottoException;
 
 public class LottoCountTest {
 
     @DisplayName("로또 개수 생성")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1})
+    void constructLottoCount_success(int count) {
+        LottoCount lottoCount = new LottoCount(count);
+        assertThat(lottoCount).isEqualTo(new LottoCount(count));
+    }
+
+    @DisplayName("로또 개수 생성")
     @Test
-    void constructLottoCount() {
-        LottoCount lottoCount = new LottoCount(2);
-        assertThat(lottoCount).isEqualTo(new LottoCount(2));
+    void throwsError_whenNegativeLottoCount() {
+        assertThatExceptionOfType(LottoException.class)
+            .isThrownBy(() -> new LottoCount(-1))
+            .withMessage("로또 티켓 갯수는 양수여야 합니다.");
     }
 
     @DisplayName("입력받은 숫자보다 큰 로또개수인지 확인")
