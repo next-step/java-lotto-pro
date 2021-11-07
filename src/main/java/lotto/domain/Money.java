7d6a2fb659a8393d3472c101c7dbf4cbd17c38b4
cus4703 <cus4703@gmail.com>
2021-11-07 22:24:55 +0900

    refactor: 로또 구입 금액에 대한 검증 책임 삭제

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -12,31 +12,11 @@ public class Money {
     private final BigDecimal money;
 
     public Money(BigDecimal money) {
-        validation(money);
         this.money = money;
     }
 
-    public int getPurchaseCount(BigDecimal gamePrice) {
-        return money.divide(gamePrice).intValue();
-    }
-
-    private void validation(BigDecimal purchaseAmount) {
-        validateZeroAmount(purchaseAmount);
-        validateRemainderAmount(purchaseAmount);
-    }
-
-    private void validateRemainderAmount(BigDecimal purchaseAmount) {
-        if (!purchaseAmount.remainder(GAME_PRICE).equals(BigDecimal.ZERO)) {
-            System.out.println("[ERROR] 구입 시도 금액이 잘못되었습니다.");
-            throw new LottoPurchaseAmountException("[ERROR] 구입 시도 금액이 잘못되었습니다.");
-        }
-    }
-
-    private void validateZeroAmount(BigDecimal purchaseAmount) {
-        if (purchaseAmount.equals(BigDecimal.ZERO)) {
-            System.out.println("[ERROR] 구입 할 금액을 입력해주세요.");
-            throw new LottoPurchaseAmountException("[ERROR] 구입 할 금액을 입력해주세요.");
-        }
+    public BigDecimal getMoney() {
+        return money;
     }
 
     @Override
--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 
 import static lotto.domain.LottoNumber.GAME_PRICE;
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class MoneyTest {
@@ -18,33 +19,38 @@ public class MoneyTest {
 
         Money expected = new Money(BigDecimal.valueOf(1000));
 
-        Assertions.assertThat(money).isEqualTo(expected);
+        assertThat(money).isEqualTo(expected);
     }
 
     @Test
     @DisplayName("입력 금액 테스트(0원)")
     public void money2() {
-        assertThatThrownBy(() -> {
-            new Money(BigDecimal.valueOf(0));
-        }).isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR]");
+        Money money = new Money(BigDecimal.valueOf(0));
+        LottoMachine machine = new LottoMachine(money);
+
+        int count = machine.getLottoList().size();
+
+        assertThat(count).isEqualTo(0);
     }
 
     @Test
     @DisplayName("구매 회수 테스트")
     public void money3() {
         Money money = new Money(BigDecimal.valueOf(1000));
+        LottoMachine machine = new LottoMachine(money);
 
-        int purchaseCount = money.getPurchaseCount(GAME_PRICE);
+        int purchaseCount = machine.getLottoList().size();
 
-        Assertions.assertThat(purchaseCount).isEqualTo(1);
+        assertThat(purchaseCount).isEqualTo(1);
     }
 
     @Test
     @DisplayName("입력 금액 나머지 존재")
     public void money4() {
         assertThatThrownBy(() -> {
-            new Money(BigDecimal.valueOf(1550));
+            Money money = new Money(BigDecimal.valueOf(1500));
+
+            LottoMachine machine = new LottoMachine(money);
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR]");
     }
