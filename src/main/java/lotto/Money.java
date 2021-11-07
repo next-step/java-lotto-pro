c32316769a97fd5f0edc663149cbbc076f25b2b0
youngrock.choi <tbvjdudfhr@naver.com>
2021-11-07 17:18:15 +0900

    refactor: 머니 생성자 수정, 예외 추가

--- src/main/java/lotto/Money.java
+++ src/main/java/lotto/Money.java
@@ -2,16 +2,27 @@ package lotto;
 
 public class Money {
     public static final int PRICE_ONE_LOTTO = 1000;
+    public static final String NUMBER_PATTERN = "-?\\d+";
+    public static final String NUMBER_MESSAGE = "숫자를 입력해 주세요.";
+    public static final String MINIMUM_AMOUNT_MESSAGE = "최소 금액 1000원보다 작은 금액을 입력하였습니다.";
     private int amount;
 
-    public Money(int amount) {
-        validateMinimumAmount(amount);
-        this.amount = amount;
+    public Money(String text) {
+        validateNumber(text);
+        int number = Integer.parseInt(text);
+        validateMinimumAmount(number);
+        amount = number;
+    }
+
+    private void validateNumber(String text) {
+        if (!text.matches(NUMBER_PATTERN)) {
+            throw new IllegalArgumentException(NUMBER_MESSAGE);
+        }
     }
 
     private void validateMinimumAmount(int amount) {
         if (amount < PRICE_ONE_LOTTO) {
-            throw new RuntimeException("최소 금액 1000원보다 작은 금액을 입력하였습니다.");
+            throw new IllegalArgumentException(MINIMUM_AMOUNT_MESSAGE);
         }
     }
 
@@ -19,3 +30,4 @@ public class Money {
         return this.amount / PRICE_ONE_LOTTO;
     }
 }
+
--- src/test/java/lotto/MoneyTest.java
+++ src/test/java/lotto/MoneyTest.java
@@ -12,16 +12,24 @@ public class MoneyTest {
     @DisplayName("구입금액에 따른 로또 개수 테스트")
     @ParameterizedTest
     @CsvSource(value = {"1000:1", "3600:3"}, delimiter = ':')
-    void lottoCountTest(int input, int expected) {
+    void lottoCountTest(String input, int expected) {
         Money money = new Money(input);
         assertThat(money.getLottoCount()).isEqualTo(expected);
     }
 
-    @DisplayName("최소 구입금액보다 작은 금액을 입력할 경우 RuntimeException 예외가 발생테스트")
+    @DisplayName("최소 구입금액보다 작은 금액을 입력할 경우 예외 발생테스트")
     @ParameterizedTest
-    @ValueSource(ints = {-1000, 500})
-    public void minimumAmountTest(int input) throws Exception {
+    @ValueSource(strings = {"-1000", "500"})
+    public void minimumAmountTest(String input) throws Exception {
         assertThatThrownBy(() -> new Money(input))
-                .isInstanceOf(RuntimeException.class);
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("숫자가 아닌 문자를 입력할 경우 예외 발생테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"asdfs", "[[]]]"})
+    public void textTest(String input) throws Exception {
+        assertThatThrownBy(() -> new Money(input))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
