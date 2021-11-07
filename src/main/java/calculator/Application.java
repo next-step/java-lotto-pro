ac20d2e94c856275ade53b4a79d8a1db9f2e9831
coder013 <74584170+coder013@users.noreply.github.com>
2021-11-07 22:13:58 +0900

    Step2 (#190)
    * test : StringAddCalculator 테스트 코드 작성
    
    * feat: 문자열 덧셈 계산기 기능 개발

---
+++ src/main/java/calculator/Application.java
@@ -0,0 +1,9 @@
+package calculator;
+
+public class Application {
+
+    public static void main(String[] args) {
+        StringAddCalculator stringAddCalculator = new StringAddCalculator();
+        stringAddCalculator.start();
+    }
+}
---
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -0,0 +1,60 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringAddCalculatorTest {
+
+    private final StringAddCalculator stringAddCalculator;
+
+    public StringAddCalculatorTest() {
+        stringAddCalculator = new StringAddCalculator();
+    }
+
+    @Test
+    public void splitAndSum_null_또는_빈문자() {
+        int result = stringAddCalculator.splitAndSum(null);
+        assertThat(result).isEqualTo(0);
+
+        result = stringAddCalculator.splitAndSum("");
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    public void splitAndSum_숫자하나() {
+        int result = stringAddCalculator.splitAndSum("1");
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    public void splitAndSum_쉼표구분자() {
+        int result = stringAddCalculator.splitAndSum("1,2");
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    public void splitAndSum_쉼표_또는_콜론_구분자() {
+        int result = stringAddCalculator.splitAndSum("1,2:3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void splitAndSum_custom_구분자() {
+        int result = stringAddCalculator.splitAndSum("//;\n1;2;3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void splitAndSum_negative() {
+        assertThatThrownBy(() -> stringAddCalculator.splitAndSum("-1,2,3"))
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void splitAndSum_문자열() {
+        assertThatThrownBy(() -> stringAddCalculator.splitAndSum("1,2,qsqd"))
+                .isInstanceOf(RuntimeException.class);
+    }
+}
