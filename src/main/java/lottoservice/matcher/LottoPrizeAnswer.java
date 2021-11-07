df5bcc77e5a10c0648348d3fd0e53256687f11c7
jerry92k <jens92.k@gmail.com>
2021-11-07 17:53:19 +0900

    feat(LottoPrizeAnswer): 당첨정답과 티켓 비교 기능

--- src/main/java/lottoservice/matcher/LottoPrizeAnswer.java
+++ src/main/java/lottoservice/matcher/LottoPrizeAnswer.java
@@ -1,11 +1,16 @@
 package lottoservice.matcher;
 
 import lottoservice.lottonumber.LottoNumber;
+import lottoservice.lottoticket.LottoTicket;
+import lottoservice.lottoticket.LottoTickets;
 
 public class LottoPrizeAnswer {
 
 	private LottoWinningNumbers lottoWinningNumbers;
 	private BonusNumber bonusNumber;
+
+	private LottoPrizeAnswer(){
+	}
 
 	public LottoPrizeAnswer(LottoWinningNumbers lottoWinningNumbers, BonusNumber bonusNumber) {
 		this.lottoWinningNumbers = lottoWinningNumbers;
@@ -18,5 +23,35 @@ public class LottoPrizeAnswer {
 
 	public boolean isMatchBonusNumber(LottoNumber lottoNumber) {
 		return bonusNumber.isMatchNumber(lottoNumber);
+	}
+
+	public LottoMatchResult matchTickets(LottoTickets lottoTickets) {
+		LottoMatchResult lottoMatchResult = new LottoMatchResult();
+		for (LottoTicket lottoTicket : lottoTickets.getLottoTickets()) {
+			// setMatchResult(lottoMatchResult, lottoTicket);
+		}
+		return lottoMatchResult;
+	}
+
+	// /* 인자로 넘어온 티켓들을 정답과 비교하여 결과를 리턴 */
+	// public LottoMatchResult matchWinningAndTickets(LottoTickets lottoTickets) {
+	//
+	// 	for (LottoTicket lottoTicket : lottoTickets.getLottoTickets()) {
+	// 		setMatchResult(lottoMatchResult, lottoTicket);
+	// 	}
+	// 	return lottoMatchResult;
+	// }
+	//
+	// private void setMatchResult(LottoMatchResult lottoMatchResult, LottoTicket lottoTicket) {
+	// 	int matchCount = matchCountWinningAndTicket(lottoTicket);
+	// 	if (hasMatch(matchCount)) {
+	// 		lottoMatchResult.addMatchCount(LottoMatchRank.valueOf(matchCount,false));
+	// 	}
+	// }
+
+	public LottoMatchRank matchTicket(LottoTicket lottoTicket) {
+		boolean matchBonus = bonusNumber.matchTicket(lottoTicket);
+		int matchCount = lottoWinningNumbers.matchCountWinningAndTicket(lottoTicket);
+		return LottoMatchRank.valueOf(matchCount,matchBonus);
 	}
 }
--- src/test/java/lottoservice/matcher/LottoPrizeAnswerTest.java
+++ src/test/java/lottoservice/matcher/LottoPrizeAnswerTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -11,21 +12,57 @@ import org.junit.jupiter.api.Test;
 import lottoservice.lottonumber.LottoArrangeManipulator;
 import lottoservice.lottonumber.LottoNumber;
 import lottoservice.lottonumber.LottoNumbersMaker;
+import lottoservice.lottoticket.LottoTicket;
 
 public class LottoPrizeAnswerTest {
 
 	private LottoNumbersMaker lottoNumbersMaker = new LottoNumbersMaker(new LottoArrangeManipulator());
 
 	@Test
-	public void 보너스번호_포함_당첨번호와_티켓_매칭() {
+	public void 당첨정답_생성() {
 		List<LottoNumber> lottoNumbers = lottoNumbersMaker.makeLottoNumbers(
 			Arrays.asList(1, 22, 3, 7, 30, 45));
 		LottoWinningNumbers lottoWinningNumbers = new LottoWinningNumbers(lottoNumbers);
-		BonusNumber bonusNumber = new BonusNumber(17);
+		int number=17;
+		BonusNumber bonusNumber = new BonusNumber(number);
 		LottoPrizeAnswer lottoPrizeAnswer = new LottoPrizeAnswer(lottoWinningNumbers,bonusNumber);
 
 		lottoNumbers.stream().forEach(it->
 			assertThat(lottoPrizeAnswer.hasNumberInWinningNumbers(it)).isEqualTo(true));
-		assertThat(lottoPrizeAnswer.isMatchBonusNumber(LottoNumber.valueOf(17))).isEqualTo(true);
+		assertThat(lottoPrizeAnswer.isMatchBonusNumber(LottoNumber.valueOf(number))).isEqualTo(true);
+	}
+
+	@Test
+	public void 당첨정답과_티켓_비교() {
+		int number=17;
+		List<LottoNumber> winningNumbers = lottoNumbersMaker.makeLottoNumbers(
+			Arrays.asList(1, 22, 3, 7, 30, 45));
+		LottoWinningNumbers lottoWinningNumbers = new LottoWinningNumbers(winningNumbers);
+		BonusNumber bonusNumber = new BonusNumber(number);
+		LottoPrizeAnswer lottoPrizeAnswer = new LottoPrizeAnswer(lottoWinningNumbers,bonusNumber);
+
+		List<LottoNumber> ticketNumbers = Arrays.asList(1,22,10,2,30,45)
+			.stream().map(it -> LottoNumber.valueOf(it)).collect(Collectors.toList());
+		LottoTicket lottoTicket = new LottoTicket(ticketNumbers);
+
+		LottoMatchRank rank = lottoPrizeAnswer.matchTicket(lottoTicket);
+		assertThat(rank).isEqualTo(LottoMatchRank.FOUR_POINT);
+	}
+
+	@Test
+	public void 당첨정답과_티켓_비교_2등() {
+		int number=17;
+		List<LottoNumber> winningNumbers = lottoNumbersMaker.makeLottoNumbers(
+			Arrays.asList(1, 22, 3, 7, 30, 45));
+		LottoWinningNumbers lottoWinningNumbers = new LottoWinningNumbers(winningNumbers);
+		BonusNumber bonusNumber = new BonusNumber(number);
+		LottoPrizeAnswer lottoPrizeAnswer = new LottoPrizeAnswer(lottoWinningNumbers,bonusNumber);
+
+		List<LottoNumber> ticketNumbers = Arrays.asList(1,22,17,7,30,45)
+			.stream().map(it -> LottoNumber.valueOf(it)).collect(Collectors.toList());
+		LottoTicket lottoTicket = new LottoTicket(ticketNumbers);
+
+		LottoMatchRank rank = lottoPrizeAnswer.matchTicket(lottoTicket);
+		assertThat(rank).isEqualTo(LottoMatchRank.FIVE_POINT_AND_BONUS);
 	}
 }
