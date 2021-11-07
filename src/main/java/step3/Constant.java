b5a031b702b313651125abeeeefde2b0d238fa76
vicjed <vicjed@icloud.com>
2021-11-07 21:37:05 +0900

    feat(Constant): 상수 클래스 추가

---
+++ src/main/java/step3/Constant.java
@@ -0,0 +1,8 @@
+package step3;
+
+public class Constant {
+	public static final String EACH = "개";
+	public static final String ENTER = "\n";
+	public static final String TOTAL_YIELD = "총 수익률은 ";
+	public static final String END_OF_WORD = "입니다.";
+}
