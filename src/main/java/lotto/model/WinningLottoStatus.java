565f6e6f43a33bcb04be010cac9c078707d24dca
jongwoo <jongwoo.dev@gmail.com>
2021-11-07 14:39:13 +0900

    feat: calculate total reward and rate of return

--- src/main/java/lotto/model/WinningLottoStatus.java
+++ src/main/java/lotto/model/WinningLottoStatus.java
@@ -18,4 +18,25 @@ public class WinningLottoStatus {
 	public int getMatchCount(LottoResult result) {
 		return winningLottoCount.get(result);
 	}
+
+	/**
+	 * 당첨된 총 상금 반환
+	 * @return 총 상금
+	 */
+	public int getTotalReward() {
+		int totalReward = 0;
+		for (Map.Entry<LottoResult, Integer> entry: winningLottoCount.entrySet()) {
+			totalReward += entry.getKey().getReward() * entry.getValue();
+		}
+		return totalReward;
+	}
+
+	/**
+	 * 수익율 계산
+	 * @param useMoney 사용한 금액
+	 * @return 수익률
+	 */
+	public double getRateOfReturn(int useMoney) {
+		return (double)getTotalReward() / useMoney;
+	}
 }
--- src/test/java/lotto/model/WinningLottoStatusTest.java
+++ src/test/java/lotto/model/WinningLottoStatusTest.java
@@ -29,4 +29,23 @@ public class WinningLottoStatusTest {
 		assertThat(matchFiveCount).isEqualTo(0);
 		assertThat(matchSixCount).isEqualTo(0);
 	}
+
+	@Test
+	@DisplayName("총 상금 및 총 수익률 반환")
+	void totalRewardAndRate() {
+		Lottos lottos = new Lottos(Arrays.asList(
+			new Lotto(new LottoNumbers(3,6,23,24,32,34)),
+			new Lotto(new LottoNumbers(3,7,23,24,32,34)),
+			new Lotto(new LottoNumbers(5,7,13,34,36,37)),
+			new Lotto(new LottoNumbers(3,7,17,22,31,34))
+		));
+		WinningLottoNumbers winningLottoNumbers = new WinningLottoNumbers(3,7,17,18,32,34);
+		WinningLottoStatus status = lottos.getWinningStatus(winningLottoNumbers);
+		int useMoney = 4000;	//로또구매에 사용한 금액
+		int totalReward = status.getTotalReward();
+		double rateOfReturn = status.getRateOfReturn(useMoney);
+
+		assertThat(totalReward).isEqualTo(105000);
+		assertThat(rateOfReturn).isEqualTo(26.25);
+	}
 }
