87af283dbc208ac82cf6325bfecb9ba469b68ab9
cus4703 <cus4703@gmail.com>
2021-11-08 00:55:56 +0900

    refactor: ParameterizedTest 를 이용하도록

--- src/test/java/lotto/domain/RankTest.java
+++ src/test/java/lotto/domain/RankTest.java
@@ -1,36 +1,40 @@
 package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RankTest {
 
-    @Test
+    @ParameterizedTest
+    @CsvSource(value = "2,false", delimiter = ',')
     @DisplayName("랭크 테스트")
-    public void none() {
-        Rank none = Rank.of(2, false);
+    public void none(int matCount, boolean matchBonus) {
+        Rank none = Rank.of(matCount, matchBonus);
 
         Rank excepted = Rank.NONE;
 
         assertThat(none).isEqualTo(excepted);
     }
 
-    @Test
+    @ParameterizedTest
+    @CsvSource(value = "6,false", delimiter = ',')
     @DisplayName("랭크 테스트")
-    public void first() {
-        Rank none = Rank.of(6, false);
+    public void first(int matCount, boolean matchBonus) {
+        Rank none = Rank.of(matCount, matchBonus);
 
         Rank excepted = Rank.FIRST;
 
         assertThat(none).isEqualTo(excepted);
     }
 
-    @Test
+    @ParameterizedTest
+    @CsvSource(value = "5,true", delimiter = ',')
     @DisplayName("랭크 테스트")
-    public void second() {
-        Rank none = Rank.of(5, true);
+    public void second(int matCount, boolean matchBonus) {
+        Rank none = Rank.of(matCount, matchBonus);
 
         Rank excepted = Rank.SECOND;
 
