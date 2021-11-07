3c45d7df216604f3f8b019a413553e933cbcb178
Arasia <rlacndtjs261@gmail.com>
2021-11-07 16:55:16 +0900

    fix: LottoRank내부 변수에 접근제어자 누락 수정

--- src/main/java/lotto/constant/LottoRank.java
+++ src/main/java/lotto/constant/LottoRank.java
@@ -9,8 +9,8 @@ public enum LottoRank {
     THIRD(5, 150_0000),
     FIRST(6, 20_0000_0000);
 
-    int matchingCount;
-    int price;
+    private final int matchingCount;
+    private final int price;
 
     LottoRank(int matchingCount, int price) {
         this.matchingCount = matchingCount;
