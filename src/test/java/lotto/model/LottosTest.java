e986c381c969fede31659095bf9a929c9ce5398d
moon <hoopster@airi.kr>
2021-11-08 00:19:39 +0900

    fix (LottosTest) : 기존 코드 리팩토링 중 누락된 리팩토링 수정

--- src/test/java/lotto/model/LottosTest.java
+++ src/test/java/lotto/model/LottosTest.java
@@ -3,6 +3,8 @@ package lotto.model;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Collections;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -25,20 +27,20 @@ class LottosTest {
 
 	@ParameterizedTest(name = "index {index} ==> inputMoney {0}, inputNumber {1}, lottoNumbersListSize {2}")
 	@CsvSource(value = {
-		"10000:1,2,3,4,5,6:10",
+		"1000:1,2,3,4,5,6:1",
 		"1111:1,2,3,4,5,6:1",
-		"20000:1,2,3,4,5,6:20",
-		"13200:1,2,3,4,5,6:13"}, delimiter = ':')
+		"2000:1,2,3,4,5,6:1",
+		"13200:1,2,3,4,5,6:1"}, delimiter = ':')
 	void 입력된_구입금액과_입력된숫자로_로또를_생성하는_테스트(String inputMoney, String inputNumber, int lottoNumbersListSize) {
 		// given
-		LottoGenerator lottoGenerator = new LottoGenerator(inputMoney, inputNumber);
+		LottoGenerator lottoGenerator = new LottoGenerator(inputMoney, Collections.singletonList(inputNumber));
 
 		// when
 		Lottos lottos = new Lottos(lottoGenerator);
 
 		// then
 		assertAll(
-			() -> assertThat(lottoGenerator).isInstanceOf(LottoGenerator.class),
+			() -> assertThat(lottos).isInstanceOf(Lottos.class),
 			() -> assertThat(lottos.size()).isEqualTo(lottoNumbersListSize),
 			() -> assertThat(lottos.contains(new LottoNumbers("1,2,3,4,5,6")))
 		);
