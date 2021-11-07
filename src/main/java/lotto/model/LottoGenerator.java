372e3128fc5c134909bf77633551fcf3e8373734
moon <hoopster@airi.kr>
2021-11-08 00:22:00 +0900

    refac (LottoGenerator) : 여러개의 숫자 입력을 받아 로또를 생성할 수 있도록 리팩토링

--- src/main/java/lotto/model/LottoGenerator.java
+++ src/main/java/lotto/model/LottoGenerator.java
@@ -7,15 +7,15 @@ import java.util.stream.Stream;
 public class LottoGenerator {
 	public static final int LOTTO_PRICE = 1000;
 	private final Money money;
-	private String inputNumber;
+	private List<String> inputNumberList;
 
 	public LottoGenerator(String inputMoney) {
 		this.money = new Money(inputMoney);
 	}
 
-	public LottoGenerator(String inputMoney, String inputNumber) {
+	public LottoGenerator(String inputMoney, List<String> inputNumberList) {
 		this.money = new Money(inputMoney);
-		this.inputNumber = inputNumber;
+		this.inputNumberList = inputNumberList;
 	}
 
 	private int calculateLottoAmount(String inputMoney) {
@@ -23,13 +23,29 @@ public class LottoGenerator {
 	}
 
 	public List<LottoNumbers> generateLottoNumbers() {
-		return Stream.generate(LottoNumbers::new)
-			.limit(calculateLottoAmount(money.money())).collect(Collectors.toList());
+		if (isNullInputNumberList()) {
+			return generateRandomLottoNumbers();
+		}
+		return generateLottoInputNumbers();
+	}
+
+	public List<LottoNumbers> generateRandomLottoNumbers() {
+		return Stream
+			.generate(LottoNumbers::new)
+			.limit(calculateLottoAmount(money.money()))
+			.collect(Collectors.toList());
 	}
 
 	public List<LottoNumbers> generateLottoInputNumbers() {
-		return Stream.generate(() -> new LottoNumbers(inputNumber))
-			.limit(calculateLottoAmount(money.money())).collect(Collectors.toList());
+		return inputNumberList
+			.stream()
+			.map(LottoNumbers::new)
+			.limit(calculateLottoAmount(money.money()))
+			.collect(Collectors.toList());
+	}
+
+	private boolean isNullInputNumberList() {
+		return this.inputNumberList == null || this.inputNumberList.isEmpty();
 	}
 
 	public String getInputMoney() {
