fa6ea109300f04c61674d7a3ae5632d3f789bcce
peter-alethio <peter@alethio.io>
2021-11-07 19:56:05 +0900

    fix(rank): 테스트 수정

--- src/test/java/lotto/domain/RankTest.java
+++ src/test/java/lotto/domain/RankTest.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 class RankTest {
 
     @ParameterizedTest(name = "{0}개를 맞추면 {1}")
-    @CsvSource(value = {"6,FIRST", "5,SECOND", "4,THIRD", "3,FOURTH"})
+    @CsvSource(value = {"6,FIRST", "5,THIRD", "4,FORTH", "3,FIFTH"})
     void 당첨번호_개수에_따라_랭크를_반환한다(int matchCount, Rank rank) {
         assertThat(Rank.rank(matchCount, false)).isEqualTo(rank);
     }
