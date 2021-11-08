29f1efec736c581d20e750880d486f6bfb2314cd
daehyun0 <dd70296@gmail.com>
2021-11-09 02:48:01 +0900

    feat: 잘못된 입력을 받을 경우 다시 입력받도록 수정

--- src/main/java/controller/LottoGame.java
+++ src/main/java/controller/LottoGame.java
@@ -1,5 +1,7 @@
 package controller;
 
+import java.util.Objects;
+
 import model.Lotto;
 import model.LottoNumberChoiceRandom;
 import model.LottoPurchaseCount;
@@ -9,19 +11,57 @@ import view.InputView;
 import view.ResultView;
 
 public class LottoGame {
+	InputView inputView = new InputView();
+	ResultView resultView = new ResultView();
 
 	public void start() {
-		InputView inputView = new InputView();
-		inputView.showRequestInputOfPurchasePrice();
-		LottoPurchaseCount lottoPurchaseCount = new LottoPurchaseCount(inputView.pollInput());
-		Lottos purchasedLottos = new Lottos(new LottoNumberChoiceRandom(), lottoPurchaseCount);
+		LottoPurchaseCount lottoPurchaseCount = requestPurchasingPrice();
+		Lottos purchasedLottos = buyLotto(lottoPurchaseCount);
 		inputView.showResponseInputOfPurchaseLottos(purchasedLottos);
 
-		inputView.showRequestInputOfWinningNumbers();
-		Lotto winningLotto = new Lotto(inputView.pollInput());
+		Lotto winningLotto = requestWinningLotto();
 
 		RewardCalculator rewardCalculator = purchasedLottos.calcReward(winningLotto);
-		ResultView resultView = new ResultView();
 		resultView.showResult(lottoPurchaseCount, rewardCalculator);
+	}
+
+	private Lotto requestWinningLotto() {
+		Lotto winningLotto = null;
+		while (Objects.isNull(winningLotto)) {
+			inputView.showRequestInputOfWinningNumbers();
+			winningLotto = parseInputOfWinningLotto();
+		}
+		return winningLotto;
+	}
+
+	private Lotto parseInputOfWinningLotto() {
+		try {
+			return new Lotto(inputView.pollInput());
+		} catch (RuntimeException e) {
+			inputView.showRequestInputExceptionOfWinningNumbers();
+			return null;
+		}
+	}
+
+	private Lottos buyLotto(LottoPurchaseCount lottoPurchaseCount) {
+		return new Lottos(new LottoNumberChoiceRandom(), lottoPurchaseCount);
+	}
+
+	private LottoPurchaseCount requestPurchasingPrice() {
+		LottoPurchaseCount lottoPurchaseCount = null;
+		while (Objects.isNull(lottoPurchaseCount)) {
+			inputView.showRequestInputOfPurchasePrice();
+			lottoPurchaseCount = parseInputOfPurchasePrice();
+		}
+		return lottoPurchaseCount;
+	}
+
+	private LottoPurchaseCount parseInputOfPurchasePrice() {
+		try {
+			return new LottoPurchaseCount(inputView.pollInput());
+		} catch (RuntimeException e) {
+			inputView.showRequestInputExceptionOfPurchasedPrice();
+			return null;
+		}
 	}
 }
