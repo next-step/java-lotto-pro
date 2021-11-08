271b2eb726b3920aa000d1e44fb710d5ea9a630c
Jeongkyo Kim <chavitak@gmail.com>
2021-11-09 05:12:01 +0900

    refactor: HashMap의 getOrDefault 메서드를 사용한다

--- src/main/java/lotto/domain/GameResult.java
+++ src/main/java/lotto/domain/GameResult.java
@@ -4,41 +4,30 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class GameResult {
+    public static final int DEFAULT_VALUE = 0;
+    public static final int INCREASE_AMOUNT = 1;
+    private final Prize threeMatchPrize = Prize.THREE_MATCH_PRIZE;
+    private final Prize fourMatchPrize = Prize.FOUR_MATCH_PRIZE;
+    private final Prize fiveMatchPrize = Prize.FIVE_MATCH_PRIZE;
+    private final Prize sixMatchPrize = Prize.SIX_MATCH_PRIZE;
     Map<Integer, Integer> gameResult;
 
     public GameResult() {
         gameResult = new HashMap<>();
-        gameResult.put(3, 0);
-        gameResult.put(4, 0);
-        gameResult.put(5, 0);
-        gameResult.put(6, 0);
     }
 
     public void add(int sameNumberCount) {
-        if (gameResult.containsKey(sameNumberCount)) {
-            gameResult.put(sameNumberCount, gameResult.get(sameNumberCount) + 1);
-        }
-        if (!gameResult.containsKey(sameNumberCount)) {
-            gameResult.put(sameNumberCount, 1);
-        }
-    }
-
-    @Override
-    public String toString() {
-        return 3 + "개 일치 (" + Prize.THREE_MATCH_PRIZE.getPrizeMoney() + "원)- " + gameResult.get(3) + "개\n" +
-                4 + "개 일치 (" + Prize.FOUR_MATCH_PRIZE.getPrizeMoney() + "원)- " + gameResult.get(4) + "개\n" +
-                5 + "개 일치 (" + Prize.FIVE_MATCH_PRIZE.getPrizeMoney() + "원)- " + gameResult.get(5) + "개\n" +
-                6 + "개 일치 (" + Prize.SIX_MATCH_PRIZE.getPrizeMoney() + "원)- " + gameResult.get(6) + "개";
+        gameResult.put(sameNumberCount, gameResult.getOrDefault(sameNumberCount, DEFAULT_VALUE) + INCREASE_AMOUNT);
     }
 
     public int getPrize() {
-        return gameResult.get(3) * Prize.THREE_MATCH_PRIZE.getPrizeMoney()
-                + gameResult.get(4) * Prize.FOUR_MATCH_PRIZE.getPrizeMoney()
-                + gameResult.get(5) * Prize.FIVE_MATCH_PRIZE.getPrizeMoney()
-                + gameResult.get(6) * Prize.SIX_MATCH_PRIZE.getPrizeMoney();
+        return gameResult.getOrDefault(threeMatchPrize.getMatchCount(), DEFAULT_VALUE) * threeMatchPrize.getPrizeMoney()
+                + gameResult.getOrDefault(fourMatchPrize, DEFAULT_VALUE) * fourMatchPrize.getPrizeMoney()
+                + gameResult.getOrDefault(fiveMatchPrize, DEFAULT_VALUE) * fiveMatchPrize.getPrizeMoney()
+                + gameResult.getOrDefault(sixMatchPrize, DEFAULT_VALUE) * sixMatchPrize.getPrizeMoney();
     }
 
     public Integer getMatchCount(int matchCount) {
-        return gameResult.get(matchCount);
+        return gameResult.getOrDefault(matchCount, DEFAULT_VALUE);
     }
 }
