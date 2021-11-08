b22999671d5c0e5e200f022c009dbcb63664243e
Junhong-Kim <jhk1005jhk@gmail.com>
2021-11-09 00:02:43 +0900

    refactor(LottoResult): 불필요한 인스턴스 변수 제거

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import lotto.exception.LottoException;
 import lotto.utils.NumberUtils;
 
 import java.util.HashMap;
@@ -8,31 +9,32 @@ import java.util.Map;
 
 public class LottoResult {
 
-    private final Lotto winningLotto;
-    private final List<Lotto> lottoList;
-    private final Map<Integer, Integer> resultMap = new HashMap<>();
+    private static final String LOTTO_LIST_CAN_NOT_BE_EMPTY = "로또 리스트는 1개 이상이어야 합니다.";
+
+    private final Map<Integer, Integer> result = new HashMap<>();
 
     public LottoResult(Lotto winningLotto, List<Lotto> lottoList) {
-        this.winningLotto = winningLotto;
-        this.lottoList = lottoList;
-        setResultMap();
-    }
-
-    private void setResultMap() {
+        if (lottoList.isEmpty()) {
+            throw new LottoException(LOTTO_LIST_CAN_NOT_BE_EMPTY);
+        }
         for (Lotto lotto : lottoList) {
             int matchCount = winningLotto.match(lotto);
-            resultMap.put(matchCount, resultMap.getOrDefault(matchCount, 0) + 1);
+            result.put(matchCount, result.getOrDefault(matchCount, 0) + 1);
         }
     }
 
     public int getResult(int key) {
-        return resultMap.get(key) == null ? 0 : resultMap.get(key);
+        return result.getOrDefault(key, 0);
     }
 
     public double getEarningsRate() {
-        int purchaseAmount = Lotto.PRICE * lottoList.size();
+        int lottoCount = result.values().stream()
+                .reduce(Integer::sum)
+                .orElseThrow(() -> new LottoException(LOTTO_LIST_CAN_NOT_BE_EMPTY));
+        int purchaseAmount = Lotto.PRICE * lottoCount;
+
         double winningAmount = 0;
-        for (Map.Entry<Integer, Integer> entry : resultMap.entrySet()) {
+        for (Map.Entry<Integer, Integer> entry : result.entrySet()) {
             winningAmount += (LottoPrize.getPrizeMoney(entry.getKey()) * entry.getValue());
         }
         double earningsRate = winningAmount / purchaseAmount;
