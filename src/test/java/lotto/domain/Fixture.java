aacc09e294251f735d96ddb894136773cc74a77f
lewisseo91 <togokr@naver.com>
2021-11-08 22:24:58 +0900

    style(FixtureTest): 테스트 lottoNumberOf 줄 바꿈 수정
    FixtureTest 한 줄에 점 하나만 찍는다. -> 규칙 지키기! 잊지 말기!

--- src/test/java/lotto/domain/Fixture.java
+++ src/test/java/lotto/domain/Fixture.java
@@ -4,10 +4,15 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class Fixture {
+
     private Fixture() {
     }
 
     public static LottoNumbers lottoNumbersOf(int... numbers) {
-        return Arrays.stream(numbers).mapToObj(LottoNumber::new).collect(Collectors.collectingAndThen(Collectors.toSet(), LottoNumbers::new));
+        return Arrays.stream(numbers)
+                .mapToObj(LottoNumber::new)
+                .collect(Collectors
+                        .collectingAndThen(Collectors.toSet(), LottoNumbers::new)
+                );
     }
 }
