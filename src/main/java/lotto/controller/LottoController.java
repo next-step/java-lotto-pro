f8b52ed89cdf693943c0bdf0e2057c630b68aff6
Jeongkyo Kim <chavitak@gmail.com>
2021-11-08 21:32:21 +0900

    style: 문맥에 따른 개행을 추가한다

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -20,10 +20,15 @@ public class LottoController {
     }
 
     public void run() {
+        // 로또 구입
         Money inputMoney = new Money(inputView.inputMoney());
         LottoTickets lottoTickets = lottoService.buyLottoTickets(inputMoney);
         resultView.printBuyResult(lottoTickets.count(), lottoTickets.toResultString());
+        
+        // 당첨번호 입력
         LottoTicket winningLottoTicket = LottoTickets.fromString(inputView.inputWinningNumber());
+
+        // 결과 출력
         GameResult gameResult = lottoService.getGameResult(lottoTickets, winningLottoTicket);
         resultView.printGameResult(gameResult.toString());
         resultView.printEarningRatio(inputMoney, new Money(gameResult.getPrize()));
