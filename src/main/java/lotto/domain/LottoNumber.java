a63b2b8df6955f4971258d65d44eb8b1e01d00d0
Arasia <rlacndtjs261@gmail.com>
2021-11-08 21:34:08 +0900

    fix: LottoNumber의 Error Message 상수화

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -6,6 +6,7 @@ public class LottoNumber implements Comparable<LottoNumber> {
 
     public static final int FIRST_LOTTO_NUMBER = 1;
     public static final int LAST_LOTTO_NUMBER = 45;
+    public static final String ERROR_MESSAGE_NUMBER_BOUND = "로또 숫자 범위를 초과하였습니다.";
 
     private final int number;
 
@@ -17,7 +18,7 @@ public class LottoNumber implements Comparable<LottoNumber> {
     private void validateLottoNumber(int number) {
         if (number < FIRST_LOTTO_NUMBER
                 || number > LAST_LOTTO_NUMBER) {
-            throw new IllegalArgumentException("로또 숫자 범위를 초과하였습니다.");
+            throw new IllegalArgumentException(ERROR_MESSAGE_NUMBER_BOUND);
         }
     }
 
