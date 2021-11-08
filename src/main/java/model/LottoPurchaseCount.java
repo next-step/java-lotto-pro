59f114f39af0d389ae56a585669b4e6c08a802a0
daehyun0 <dd70296@gmail.com>
2021-11-09 02:14:46 +0900

    fix: LottoPurchaseCount 버그 수정
    - 생성자에서 입력받는 값을 횟수 -> 가격으로 수정

--- src/main/java/model/LottoPurchaseCount.java
+++ src/main/java/model/LottoPurchaseCount.java
@@ -1,13 +1,19 @@
 package model;
 
 public class LottoPurchaseCount {
+	public static final int LOTTO_PRICE = 1000;
+
 	private final int purchaseCount;
 
-	public LottoPurchaseCount(String purchaseCountString) {
-		this.purchaseCount = Integer.parseInt(purchaseCountString);
+	public LottoPurchaseCount(String purchasePriceString) {
+		this.purchaseCount = Integer.parseInt(purchasePriceString) / LOTTO_PRICE;
 	}
 
 	public int get() {
 		return purchaseCount;
+	}
+
+	public int getTotalPrice() {
+		return purchaseCount * LOTTO_PRICE;
 	}
 }
--- src/test/java/LottoPurchaseCountTest.java
+++ src/test/java/LottoPurchaseCountTest.java
@@ -17,8 +17,18 @@ public class LottoPurchaseCountTest {
 	@DisplayName("숫자인 경우 정상 동작")
 	void test_constructor2() {
 		assertThatNoException()
-			.isThrownBy(() -> new LottoPurchaseCount("1"));
+			.isThrownBy(() -> new LottoPurchaseCount("1000"));
+	}
 
-		assertThat(new LottoPurchaseCount("1").get()).isEqualTo(1);
+	@Test
+	@DisplayName("호출시 구매한 횟수 반환")
+	void test_get1() {
+		assertThat(new LottoPurchaseCount("1000").get()).isEqualTo(1);
+	}
+
+	@Test
+	@DisplayName("호출시 구매시 사용한 비용 반환")
+	void test_getTotalPrice() {
+		assertThat(new LottoPurchaseCount("2000").getTotalPrice()).isEqualTo(2000);
 	}
 }
