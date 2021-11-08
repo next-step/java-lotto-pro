ac1e11c15ec4b77425f6fb5565d439873db05bc4
Jeongkyo Kim <chavitak@gmail.com>
2021-11-09 05:24:51 +0900

    refactor(ResultView): enum을 순회하게 만들어 중복되는 코드를 줄인다

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -17,10 +17,6 @@ public class ResultView {
     public static final String GAME_RESULT_MESSAGE = "%d개 일치 (%d원)- %d개\n";
     public static final String EARNING_RATIO_MESSAGE = "총 수익률은 %f입니다. ";
     public static final String EARNING_RATIO_LOSS_MESSAGE = "기준이 1이기 때문에 결과적으로 손해라는 의미임";
-    private final Prize threeMatchPrize = Prize.THREE_MATCH_PRIZE;
-    private final Prize fourMatchPrize = Prize.FOUR_MATCH_PRIZE;
-    private final Prize fiveMatchPrize = Prize.FIVE_MATCH_PRIZE;
-    private final Prize sixMatchPrize = Prize.SIX_MATCH_PRIZE;
 
     public ResultView() {
     }
@@ -40,10 +36,13 @@ public class ResultView {
     }
 
     public void printGameResult(GameResult gameResult) {
-        System.out.printf(GAME_RESULT_MESSAGE, threeMatchPrize.getMatchCount(), threeMatchPrize.getPrizeMoney(), gameResult.getMatchCount(threeMatchPrize.getMatchCount()));
-        System.out.printf(GAME_RESULT_MESSAGE, fourMatchPrize.getMatchCount(), fourMatchPrize.getPrizeMoney(), gameResult.getMatchCount(fourMatchPrize.getMatchCount()));
-        System.out.printf(GAME_RESULT_MESSAGE, fiveMatchPrize.getMatchCount(), fiveMatchPrize.getPrizeMoney(), gameResult.getMatchCount(fiveMatchPrize.getMatchCount()));
-        System.out.printf(GAME_RESULT_MESSAGE, sixMatchPrize.getMatchCount(), sixMatchPrize.getPrizeMoney(), gameResult.getMatchCount(sixMatchPrize.getMatchCount()));
+        for (Prize prize : Prize.values()) {
+            System.out.printf(GAME_RESULT_MESSAGE,
+                    prize.getMatchCount(),
+                    prize.getPrizeMoney(),
+                    gameResult.getMatchCount(prize
+                            .getMatchCount()));
+        }
     }
 
     public void printEarningRatio(MoneyDTO inputMoney, MoneyDTO prize) {
