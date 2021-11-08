5d30b197c4c22bcc4e0ba4ab68369442c470d8e2
Jeongkyo Kim <chavitak@gmail.com>
2021-11-08 21:49:12 +0900

    style: 숫자 상수 표기 시 자릿수 구분 추가

--- src/main/java/lotto/domain/GameResult.java
+++ src/main/java/lotto/domain/GameResult.java
@@ -4,10 +4,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class GameResult {
-    public static final int THREE_MATCH_PRIZE = 5000;
-    public static final int FOUR_MATCH_PRIZE = 50000;
-    public static final int FIVE_MATCH_PRIZE = 1500000;
-    public static final int SIX_MATCH_PRIZE = 2000000000;
+    public static final int THREE_MATCH_PRIZE = 5_000;
+    public static final int FOUR_MATCH_PRIZE = 50_000;
+    public static final int FIVE_MATCH_PRIZE = 1_500_000;
+    public static final int SIX_MATCH_PRIZE = 2_000_000_000;
     Map<Integer, Integer> gameResult;
 
     public GameResult() {
