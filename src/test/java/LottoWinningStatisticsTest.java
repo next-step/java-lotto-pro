d7332adc9b1baabf127ee35ee1a60dde58c69924
kelicia91 <kelicia91@gmail.com>
2021-11-08 15:36:31 +0900

    refactor: LottoWinningStatisticsTest.earningRate - 기대값에 계산식 결과가 아닌 상수값 대입

--- src/test/java/LottoWinningStatisticsTest.java
+++ src/test/java/LottoWinningStatisticsTest.java
@@ -2,6 +2,8 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -9,33 +11,41 @@ import org.junit.jupiter.params.provider.EnumSource;
 
 public class LottoWinningStatisticsTest {
 
-	private final String bonus = "45";
-	private final WinningLotto winningLotto = WinningLottoBuilder.aWinningLotto()
-		.withLottoNumbers("1,2,3,4,5,6")
-		.withBonus(bonus)
-		.build();
-	private final List<Lotto> lottos = Arrays.asList(
-		Lotto.from("1,2,3,4,5,6"),
-		Lotto.from(String.format("1,2,3,4,5,%s", bonus)),
-		Lotto.from("1,2,3,4,5,10"),
-		Lotto.from("1,2,3,4,10,11"),
-		Lotto.from("1,2,3,10,11,12"),
-		Lotto.from("10,11,12,13,14,15")
-	);
-	private final LottoWinningStatistics statistics = LottoWinningStatistics.of(winningLotto, lottos);
-
 	@ParameterizedTest
 	@EnumSource(LottoWinningRank.class)
 	void countLottos(LottoWinningRank rank) {
+		final String bonus = "45";
+		final WinningLotto winningLotto = WinningLottoBuilder.aWinningLotto()
+			.withLottoNumbers("1,2,3,4,5,6")
+			.withBonus(bonus)
+			.build();
+		final List<Lotto> lottos = Arrays.asList(
+			Lotto.from("1,2,3,4,5,6"),
+			Lotto.from(String.format("1,2,3,4,5,%s", bonus)),
+			Lotto.from("1,2,3,4,5,10"),
+			Lotto.from("1,2,3,4,10,11"),
+			Lotto.from("1,2,3,10,11,12"),
+			Lotto.from("10,11,12,13,14,15")
+		);
+		final LottoWinningStatistics statistics = LottoWinningStatistics.of(winningLotto, lottos);
+
 		assertThat(statistics.countLottos(rank)).isEqualTo(1L);
 	}
 
 	@Test
 	void earningRate() {
-		final long prizeKRW = Arrays.stream(LottoWinningRank.values())
-			.reduce(0L, (acc, rank) -> acc + rank.getPrizeKRW(), Long::sum);
-		assertThat(statistics.earningRate()).isEqualTo(
-			(double)prizeKRW / (Lotto.PRICE_KRW * lottos.size())
-		);
+		final WinningLotto winningLotto = WinningLottoBuilder.aWinningLotto()
+			.withLottoNumbers("1,2,3,4,5,6")
+			.withBonus("45")
+			.build();
+
+		final List<Lotto> lottos = Stream.iterate(1, num -> num + 1)
+			.limit(4)
+			.map(num -> Lotto.from("10,11,12,13,14,15"))
+			.collect(Collectors.toList());
+		lottos.add(Lotto.from("1,2,3,10,11,12"));
+
+		final LottoWinningStatistics statistics = LottoWinningStatistics.of(winningLotto, lottos);
+		assertThat(statistics.earningRate()).isEqualTo(1);
 	}
 }
