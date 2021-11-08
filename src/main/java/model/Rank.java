503436cdea899d4503a69b31278ba8f3cd30c007
daehyun0 <dd70296@gmail.com>
2021-11-09 02:09:09 +0900

    feat: Rank 추가

---
+++ src/main/java/model/Rank.java
@@ -0,0 +1,21 @@
+package model;
+
+public enum Rank {
+	FIRST(2000000000),
+	SECOND(0),
+	THIRD(1500000),
+	FOURTH(50000),
+	FIFTH(5000),
+	NONE(0)
+	;
+
+	private final int reward;
+
+	Rank(int reward) {
+		this.reward = reward;
+	}
+
+	public int getReward() {
+		return reward;
+	}
+}
---
+++ src/test/java/RankTest.java
@@ -0,0 +1,30 @@
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import model.Rank;
+
+public class RankTest {
+	@ParameterizedTest
+	@DisplayName("등급과 상금이 매핑되어있는지 확인")
+	@MethodSource("test_getReward1_parameter")
+	void test_getReward1(Rank rank, int reward) {
+		assertThat(rank.getReward()).isEqualTo(reward);
+	}
+
+	static Stream<Arguments> test_getReward1_parameter() {
+		return Stream.of(
+			Arguments.of(Rank.FIRST, 2000000000),
+			Arguments.of(Rank.SECOND, 0),
+			Arguments.of(Rank.THIRD, 1500000),
+			Arguments.of(Rank.FOURTH, 50000),
+			Arguments.of(Rank.FIFTH, 5000),
+			Arguments.of(Rank.NONE, 0)
+		);
+	}
+}
