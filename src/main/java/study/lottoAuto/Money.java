8af27c4d3e36babc9056f62845a479f2af8f7dff
sungwoo <ssw890418@gmail.com>
2021-11-08 22:04:44 +0900

    feat:로또 구매 금액 입력 기능 테스트 추가

---
+++ src/main/java/study/lottoAuto/Money.java
@@ -0,0 +1,25 @@
+package study.lottoAuto;
+
+import static study.lottoAuto.MessageUtil.MINIMUM_MONEY_INPUT_ERR_MSG;
+import static study.lottoAuto.MessageUtil.NEGATIVE_NUMBER_ERR_MSG;
+
+public class Money {
+    private final int money;
+
+    public Money(String userInput) {
+        this.money = Integer.parseInt(userInput);
+        validateMoney();
+    }
+
+    private void validateMoney() {
+        if(money < 0)
+            throw new IllegalStateException(NEGATIVE_NUMBER_ERR_MSG);
+        if(money < 1000)
+            throw new IllegalStateException(MINIMUM_MONEY_INPUT_ERR_MSG);
+    }
+
+    //구매갯수를 구하는 책임을 여기서 맡을 필요가 있을까...?
+    public int getBuyCount() {
+        return money/1000;
+    }
+}
---
+++ src/test/java/study/lottoAuto/MoneyTest.java
@@ -0,0 +1,45 @@
+package study.lottoAuto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static study.lottoAuto.MessageUtil.MINIMUM_MONEY_INPUT_ERR_MSG;
+import static study.lottoAuto.MessageUtil.NEGATIVE_NUMBER_ERR_MSG;
+
+class MoneyTest {
+
+    @Test
+    @DisplayName("음수입력시 예외가 발생해야한다.")
+    void 음수입력_테스트() {
+        assertThatThrownBy(() -> new Money("-1000"))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining(NEGATIVE_NUMBER_ERR_MSG);
+    }
+
+    @Test
+    @DisplayName("1000 미만의 숫자 입력시 예외가 발생해야한다.")
+    void 최소금액_테스트() {
+        assertThatThrownBy(() -> new Money("999"))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining(MINIMUM_MONEY_INPUT_ERR_MSG);
+    }
+
+    @Test
+    @DisplayName("숫자형태가 아닐시 예외가 발생해야 한다.")
+    void 숫자형태_테스트() {
+        assertThatThrownBy(() -> new Money("1000a"))
+                .isInstanceOf(NumberFormatException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("구매갯수 테스트(1000원 미만의 금액은 무시한다)")
+    @ValueSource(strings = {"14000", "14999"})
+    void buyCountTest(String userInput) {
+        Money money = new Money(userInput);
+        assertThat(money.getBuyCount()).isEqualTo(14);
+    }
+}
