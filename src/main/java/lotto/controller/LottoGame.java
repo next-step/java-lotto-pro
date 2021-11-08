8693eef8b493a4304b639c134debcf90f935094c
peter-alethio <peter@alethio.io>
2021-11-08 23:37:31 +0900

    feat(view): 수동 로또와 수동로또 개수를 입력받는다

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -3,7 +3,9 @@ package lotto.controller;
 import lotto.domain.*;
 import lotto.view.LottoView;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class LottoGame {
 
@@ -15,6 +17,10 @@ public class LottoGame {
 
     public void start() {
         final Money money = LottoView.getMoney();
+
+        final int manualCount = LottoView.getManualCount();
+        final ManualLottos manualLottos = LottoView.getManualLottos(manualCount);
+
         final Lottos lottos = new Lottos(money, randomNumbers);
 
         LottoView.displayCount(money);
