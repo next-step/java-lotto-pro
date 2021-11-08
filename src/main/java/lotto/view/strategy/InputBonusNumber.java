33f6dcc5075d0b4bbff6a32f087f16e820f34df2
신민준 <jxin95@gmail.com>
2021-11-08 23:28:04 +0900

    feat: 보너스번호 입력 기능 추가

---
+++ src/main/java/lotto/view/strategy/InputBonusNumber.java
@@ -0,0 +1,26 @@
+package lotto.view.strategy;
+
+import java.util.regex.Pattern;
+
+public class InputBonusNumber implements Input {
+
+    private static final String MESSAGE = "\r\n보너스 볼을 입력해 주세요.";
+    private static final Pattern REGEX_PATTERN = Pattern.compile("^[0-9]{1,2}$");
+    private static final String ERROR_MESSAGE = "숫자만 입력할 수 있습니다.";
+
+    @Override
+    public String getMessage() {
+        return MESSAGE;
+    }
+
+    @Override
+    public Pattern inputRegexCompiler() {
+        return REGEX_PATTERN;
+    }
+
+    @Override
+    public String getErrorMessage() {
+        return ERROR_MESSAGE;
+    }
+
+}
