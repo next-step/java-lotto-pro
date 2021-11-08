6eac81f1bd79023b876d626158a97256c09f33f2
Jeongkyo Kim <chavitak@gmail.com>
2021-11-09 04:59:30 +0900

    refactor: 문자열을 불변상수로 변경한다
    - 문자열을 인라인에서 불변상수로 변경한다
    - 문자열 포매터를 사용한다

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,18 +4,20 @@ import java.util.Scanner;
 
 public class InputView {
 
+    public static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+    public static final String INPUT_WINNING_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     Scanner scanner = new Scanner(System.in);
 
     public InputView() {
     }
 
     public int inputMoney() {
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(INPUT_MONEY_MESSAGE);
         return scanner.nextInt();
     }
 
     public String inputWinningNumber() {
-        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        System.out.println(INPUT_WINNING_NUMBER_MESSAGE);
         return scanner.next();
     }
 }
--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -12,10 +12,11 @@ import java.util.stream.Collectors;
 public class ResultView {
 
     public static final int BEP = 1;
-    public static final String GAME_RESULT_VIEW_1 = "개 일치 (";
-    public static final String GAME_RESULT_VIEW_2 = "원)- ";
-    public static final String GAME_RESULT_VIEW_3 = "개\n";
     public static final String JOIN_DELIMITER = "\n";
+    public static final String BUY_MESSAGE = "%d개를 구매했습니다.";
+    public static final String GAME_RESULT_MESSAGE = "%d개 일치 (%d원)- %d개\n";
+    public static final String EARNING_RATIO_MESSAGE = "총 수익률은 %f입니다. ";
+    public static final String EARNING_RATIO_LOSS_MESSAGE = "기준이 1이기 때문에 결과적으로 손해라는 의미임";
     private final Prize threeMatchPrize = Prize.THREE_MATCH_PRIZE;
     private final Prize fourMatchPrize = Prize.FOUR_MATCH_PRIZE;
     private final Prize fiveMatchPrize = Prize.FIVE_MATCH_PRIZE;
@@ -25,7 +26,7 @@ public class ResultView {
     }
 
     public void printBuyResult(LottoTicketsDTO lottoTicketsDTO) {
-        System.out.println(lottoTicketsDTO.size() + "개를 구매했습니다.");
+        System.out.printf(BUY_MESSAGE, lottoTicketsDTO.size());
         String lottoResultStrings = getLottoResultString(lottoTicketsDTO);
         System.out.println(lottoResultStrings);
     }
@@ -39,17 +40,17 @@ public class ResultView {
     }
 
     public void printGameResult(GameResult gameResult) {
-        System.out.println(threeMatchPrize.getMatchCount() + GAME_RESULT_VIEW_1 + threeMatchPrize.getPrizeMoney() + GAME_RESULT_VIEW_2 + gameResult.getMatchCount(threeMatchPrize.getMatchCount()) + GAME_RESULT_VIEW_3 +
-                fourMatchPrize.getMatchCount() + GAME_RESULT_VIEW_1 + fourMatchPrize.getPrizeMoney() + GAME_RESULT_VIEW_2 + gameResult.getMatchCount(fourMatchPrize.getMatchCount()) + GAME_RESULT_VIEW_3 +
-                fiveMatchPrize.getMatchCount() + GAME_RESULT_VIEW_1 + fiveMatchPrize.getPrizeMoney() + GAME_RESULT_VIEW_2 + gameResult.getMatchCount(fiveMatchPrize.getMatchCount()) + GAME_RESULT_VIEW_3 +
-                sixMatchPrize.getMatchCount() + GAME_RESULT_VIEW_1 + sixMatchPrize.getPrizeMoney() + GAME_RESULT_VIEW_2 + gameResult.getMatchCount(sixMatchPrize.getMatchCount()) + GAME_RESULT_VIEW_3);
+        System.out.printf(GAME_RESULT_MESSAGE, threeMatchPrize.getMatchCount(), threeMatchPrize.getPrizeMoney(), gameResult.getMatchCount(threeMatchPrize.getMatchCount()));
+        System.out.printf(GAME_RESULT_MESSAGE, fourMatchPrize.getMatchCount(), fourMatchPrize.getPrizeMoney(), gameResult.getMatchCount(fourMatchPrize.getMatchCount()));
+        System.out.printf(GAME_RESULT_MESSAGE, fiveMatchPrize.getMatchCount(), fiveMatchPrize.getPrizeMoney(), gameResult.getMatchCount(fiveMatchPrize.getMatchCount()));
+        System.out.printf(GAME_RESULT_MESSAGE, sixMatchPrize.getMatchCount(), sixMatchPrize.getPrizeMoney(), gameResult.getMatchCount(sixMatchPrize.getMatchCount()));
     }
 
     public void printEarningRatio(MoneyDTO inputMoney, MoneyDTO prize) {
         double earningRatio = (double) prize.get() / inputMoney.get();
-        String result = "총 수익률은 " + earningRatio + "입니다. ";
+        String result = String.format(EARNING_RATIO_MESSAGE, earningRatio);
         if (earningRatio < BEP) {
-            result += "기준이 1이기 때문에 결과적으로 손해라는 의미임";
+            result += EARNING_RATIO_LOSS_MESSAGE;
         }
         System.out.print(result);
     }
