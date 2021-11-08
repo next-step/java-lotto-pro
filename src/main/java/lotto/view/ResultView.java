9458e628d38f7da165bd69ef4abe285e93e2e6b7
신민준 <jxin95@gmail.com>
2021-11-08 23:46:42 +0900

    refactor: 결과출력시 2등 당첨부분 추가

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -1,20 +1,16 @@
 package lotto.view;
 
-import java.util.HashMap;
+import lotto.model.Rank;
+
 import java.util.LinkedHashMap;
-import java.util.Map;
 
 public class ResultView {
 
     private static final String PURCHASED_GAME_COUNT = "%s개를 구매했습니다.";
     private static final String RESULT_TITLE_MESSAGE = "당첨 통계\r\n---------";
-    private static final Map<Integer, Integer> PRIZE_MONEY = new HashMap<Integer, Integer>(){{
-        put(3, 5_000);
-        put(4, 50_000);
-        put(5, 1_500_000);
-        put(6, 2_000_000_000);
-    }};
-    private static final String MATCHED_RESULT_MESSAGE = "%s개 일치 (%s원)- %s개";
+    private static final String MATCHED_RESULT_MESSAGE_PREFIX = "%s개 일치";
+    private static final String MATCHED_RESULT_MESSAGE_SUFFIX = " (%s원) - %s개";
+    private static final String MATCHED_RESULT_MESSAGE_FOR_SECOND_RANK = ", 보너스 볼 일치";
     private static final String EARNING_RATE_MESSAGE = "총 수익률은 %s입니다.";
     private static double totalPrizeMoney = 0.0;
 
@@ -22,19 +18,48 @@ public class ResultView {
         throw new AssertionError();
     }
 
+    /**
+     * 구매금액 출력
+     *
+     * @param gameCount
+     */
     public static void printPurchasedGameCount(int gameCount) {
         System.out.println(String.format(PURCHASED_GAME_COUNT, gameCount));
     }
 
-    public static void printResult(LinkedHashMap<Integer, Integer> results) {
+    /**
+     * 결과 출력
+     *
+     * @param results
+     */
+    public static void printResult(LinkedHashMap<Rank, Integer> results) {
         System.out.println(RESULT_TITLE_MESSAGE);
 
-        PRIZE_MONEY.forEach((matchCount, prizeMoney) -> {
-            System.out.println(String.format(MATCHED_RESULT_MESSAGE, matchCount, prizeMoney, results.get(matchCount)));
-            totalPrizeMoney += prizeMoney * results.get(matchCount);
-        });
+        results.entrySet()
+                .stream()
+                .filter(entry -> !entry.getKey().equals(Rank.MISS))
+                .forEach(entry -> {
+                    String messageFormat = MATCHED_RESULT_MESSAGE_PREFIX + appendSecondRankMessage(entry.getKey()) + MATCHED_RESULT_MESSAGE_SUFFIX;
+                    System.out.println(String.format(messageFormat, entry.getKey().getCountOfMatch(), entry.getKey().getWinningMoney(), entry.getValue()));
+                    totalPrizeMoney += entry.getKey().getWinningMoney() * entry.getValue();
+                });
     }
 
+    /**
+     * 2위일 경우 추가 메시지 반환
+     * 
+     * @param rank 당첨순위
+     * @return 추가메시지
+     */
+    private static String appendSecondRankMessage(Rank rank) {
+        return rank == Rank.SECOND ? MATCHED_RESULT_MESSAGE_FOR_SECOND_RANK : "";
+    }
+
+    /**
+     * 수익률 출력
+     *
+     * @param purchaseAmount
+     */
     public static void printEarningRate(int purchaseAmount) {
         double earningRate = Math.round(totalPrizeMoney / purchaseAmount * 100) / 100.0;
         System.out.println(String.format(EARNING_RATE_MESSAGE, earningRate));
