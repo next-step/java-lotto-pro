d3512da3d5ef9499133b3a0751305b6d6d9885eb
daehyun0 <dd70296@gmail.com>
2021-11-09 02:18:12 +0900

    feat: ResultView 추가
    - 로또 게임 통계에 관련된 View 클래스 추가

---
+++ src/main/java/view/ResultView.java
@@ -0,0 +1,24 @@
+package view;
+
+import model.LottoPurchaseCount;
+import model.Rank;
+import model.RewardCalculator;
+
+public class ResultView {
+	public void showResult(LottoPurchaseCount lottoPurchaseCount, RewardCalculator rewardCalculator) {
+		System.out.println(getMessage(lottoPurchaseCount, rewardCalculator));
+	}
+
+	private String getMessage(LottoPurchaseCount lottoPurchaseCount, RewardCalculator rewardCalculator) {
+		float revenueRate = rewardCalculator.sum() / (float)lottoPurchaseCount.getTotalPrice();
+		return new StringBuilder()
+			.append("당첨 통계\n")
+			.append("---------\n")
+			.append(String.format("3개 일치 (%d원)- %d개\n", Rank.FIFTH.getReward(), rewardCalculator.getCount(Rank.FIFTH)))
+			.append(String.format("4개 일치 (%d원)- %d개\n", Rank.FOURTH.getReward(), rewardCalculator.getCount(Rank.FOURTH)))
+			.append(String.format("5개 일치 (%d원)- %d개\n", Rank.THIRD.getReward(), rewardCalculator.getCount(Rank.THIRD)))
+			.append(String.format("6개 일치 (%d원)- %d개\n", Rank.FIRST.getReward(), rewardCalculator.getCount(Rank.FIRST)))
+			.append(String.format("총 수익률은 %.2f입니다.\n", revenueRate))
+			.toString();
+	}
+}
