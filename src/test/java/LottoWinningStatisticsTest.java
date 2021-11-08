7e4faa9ec3c8156a7639ce994ec34f3d1dd711f6
kelicia91 <kelicia91@gmail.com>
2021-11-08 16:24:07 +0900

    refactor: LottoWinningStatisticsTest.countLottos() - ParameterizedTest를 통하면 동일 객체를 매번 생성하게 되서 Test 하나로 검증되도록 대체

--- src/test/java/LottoWinningStatisticsTest.java
+++ src/test/java/LottoWinningStatisticsTest.java
@@ -6,30 +6,27 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
 
 public class LottoWinningStatisticsTest {
 
-	@ParameterizedTest
-	@EnumSource(LottoWinningRank.class)
-	void countLottos(LottoWinningRank rank) {
-		final String bonus = "45";
+	@Test
+	void countLottos() {
 		final WinningLotto winningLotto = WinningLottoBuilder.aWinningLotto()
 			.withLottoNumbers("1,2,3,4,5,6")
-			.withBonus(bonus)
+			.withBonus("45")
 			.build();
 		final List<Lotto> lottos = Arrays.asList(
 			Lotto.from("1,2,3,4,5,6"),
-			Lotto.from(String.format("1,2,3,4,5,%s", bonus)),
+			Lotto.from(String.format("1,2,3,4,5,%s", winningLotto.getBonus())),
 			Lotto.from("1,2,3,4,5,10"),
 			Lotto.from("1,2,3,4,10,11"),
 			Lotto.from("1,2,3,10,11,12"),
 			Lotto.from("10,11,12,13,14,15")
 		);
-		final LottoWinningStatistics statistics = LottoWinningStatistics.of(winningLotto, lottos);
 
-		assertThat(statistics.countLottos(rank)).isEqualTo(1L);
+		final LottoWinningStatistics statistics = LottoWinningStatistics.of(winningLotto, lottos);
+		Arrays.stream(LottoWinningRank.values())
+			.forEach(rank -> assertThat(statistics.countLottos(rank)).isEqualTo(1L));
 	}
 
 	@Test
@@ -38,7 +35,6 @@ public class LottoWinningStatisticsTest {
 			.withLottoNumbers("1,2,3,4,5,6")
 			.withBonus("45")
 			.build();
-
 		final List<Lotto> lottos = Stream.iterate(1, num -> num + 1)
 			.limit(4)
 			.map(num -> Lotto.from("10,11,12,13,14,15"))
@@ -46,6 +42,6 @@ public class LottoWinningStatisticsTest {
 		lottos.add(Lotto.from("1,2,3,10,11,12"));
 
 		final LottoWinningStatistics statistics = LottoWinningStatistics.of(winningLotto, lottos);
-		assertThat(statistics.earningRate()).isEqualTo(1);
+		assertThat(statistics.earningRate()).isEqualTo(1.d);
 	}
 }
