db50827ce20ee5198111279d06c6c1263286f57d
김도영 <95doyoung@gmail.com>
2021-11-08 18:58:17 +0900

    test (WinningStatisticsTest) : 2등 당첨에 대한 테스트 케이스 추가

--- src/test/java/study/lotto/model/WinningStatisticsTest.java
+++ src/test/java/study/lotto/model/WinningStatisticsTest.java
@@ -33,7 +33,7 @@ class WinningStatisticsTest {
                 )),
                 TicketLottery.valueOf(new HashSet<>(Arrays.asList(
                         LottoNumber.valueOf(1), LottoNumber.valueOf(2), LottoNumber.valueOf(3),
-                        LottoNumber.valueOf(4), LottoNumber.valueOf(5), LottoNumber.valueOf(6))
+                        LottoNumber.valueOf(4), LottoNumber.valueOf(5), LottoNumber.valueOf(9))
                 ))
         );
 
@@ -49,14 +49,15 @@ class WinningStatisticsTest {
     void 로또복권묶음과_당첨번호를_비교하여_수익률을_알_수_있다() {
         final WinningStatistics winningStatistics = WinningStatistics.valueOf(ticketLotteryBundle, winningLottery);
         final double incomeRate = winningStatistics.getIncomeRate();
-        assertEquals((double) 2_000_000_000 * 4 / 4000, incomeRate);
+        assertEquals(((2_000_000_000d * 3) + 30_000_000) / 4000, incomeRate);
     }
 
     @Test
     void 로또복권묶음과_당첨번호를_비교하여_당첨등수별_개수를_확인_할_수_있다() {
         final WinningStatistics winningStatistics = WinningStatistics.valueOf(ticketLotteryBundle, winningLottery);
         assertAll(() -> {
-            assertThat(winningStatistics.countByRank(Rank.FIRST)).isEqualTo(4);
+            assertThat(winningStatistics.countByRank(Rank.FIRST)).isEqualTo(3);
+            assertThat(winningStatistics.countByRank(Rank.SECOND)).isEqualTo(1);
             assertThat(winningStatistics.countByRank(Rank.THIRD)).isEqualTo(0);
             assertThat(winningStatistics.countByRank(Rank.FOURTH)).isEqualTo(0);
             assertThat(winningStatistics.countByRank(Rank.FIFTH)).isEqualTo(0);
