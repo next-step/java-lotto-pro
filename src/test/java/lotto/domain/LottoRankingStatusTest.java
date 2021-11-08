187e3e21c29f9f252f437722841b21eaab2f4ce3
lewisseo91 <togokr@naver.com>
2021-11-08 16:23:34 +0900

    feat(LottoRankingStatusTest): LottoRankingStatus 보너스 테스트 추가
    LottoRankingStatus 보너스 테스트 추가

--- src/test/java/lotto/domain/LottoRankingStatusTest.java
+++ src/test/java/lotto/domain/LottoRankingStatusTest.java
@@ -1,9 +1,11 @@
 package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayName("로또 등수 상태 테스트")
@@ -17,5 +19,11 @@ public class LottoRankingStatusTest {
     void getLottoRankingMatchRangeTest(LottoRankingStatus lottoRankingStatus) {
         int matchNumber = lottoRankingStatus.getMatchAmount();
         assertTrue(matchNumber >= 3 && matchNumber <= 6);
+    }
+
+    @DisplayName("LottoRankingStatus 보너스 확인")
+    @Test
+    void getLottoRankingStatusMatch5Bonus1Test() {
+        assertThat(LottoRankingStatus.valueOf(5, true)).isEqualTo(LottoRankingStatus.MATCH5BONUS1);
     }
 }
