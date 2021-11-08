19c86a11219bc78caa02ae57a324e022cfbedf17
jennie267 <jennieee267@gmail.com>
2021-11-09 00:05:07 +0900

    refactor : 로또 구매하는 기능, 발행 타입별로 메소드 분리

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -5,36 +5,48 @@ import java.util.Iterator;
 import java.util.List;
 
 public class Lottos implements Iterable<Lotto> {
-    private final List<Lotto> lottos;
+	private final List<Lotto> lottos;
 
-    public Lottos(IssueQuantity issueQuantity, List<LottoNumbers> manualNumbers) {
-        this.lottos = buyLotto(issueQuantity, manualNumbers);
-    }
+	public Lottos(IssueQuantity issueQuantity, List<LottoNumbers> manualNumbers) {
+		this.lottos = buyLotto(issueQuantity, manualNumbers);
+	}
 
-    public static List<Lotto> buyLotto(IssueQuantity issueQuantity, List<LottoNumbers> manualNumbers) {
-        List<Lotto> lottos = new ArrayList<Lotto>();
-        for (int i = 0; i < issueQuantity.getManualQuantity(); i++) {
-        	lottos.add(Lotto.buyManual(manualNumbers.get(i)));
+	public static List<Lotto> buyLotto(IssueQuantity issueQuantity, List<LottoNumbers> manualNumbers) {
+		List<Lotto> lottos = new ArrayList<Lotto>();
+		lottos.addAll(buyManual(issueQuantity.getManualQuantity(), manualNumbers));
+		lottos.addAll(buyAuto(issueQuantity.getAutoQuantity()));
+		return lottos;
+	}
+
+	private static List<Lotto> buyAuto(int autoQuantity) {
+		List<Lotto> lottos = new ArrayList<Lotto>();
+		for (int i = 0; i < autoQuantity; i++) {
+			lottos.add(Lotto.buyAuto());
 		}
-        for (int i = 0; i < issueQuantity.getAutoQuantity(); i++) {
-            lottos.add(Lotto.buyAuto());
-        }
-        return lottos;
-    }
-    
-    public List<Lotto> getLottos() {
-        return this.lottos;
-    }
+		return lottos;
+	}
 
-    public void execute(WinningLotto winningLotto) {
-        for (Lotto lotto : lottos) {
-            lotto.judgeRank(winningLotto);
-        }
-    }
+	private static List<Lotto> buyManual(int manualQuantity, List<LottoNumbers> manualNumbers) {
+		List<Lotto> lottos = new ArrayList<Lotto>();
+		for (int i = 0; i < manualQuantity; i++) {
+			lottos.add(Lotto.buyManual(manualNumbers.get(i)));
+		}
+		return lottos;
+	}
 
-    @Override
-    public Iterator<Lotto> iterator() {
-        return lottos.iterator();
-    }
+	public List<Lotto> getLottos() {
+		return this.lottos;
+	}
+
+	public void execute(WinningLotto winningLotto) {
+		for (Lotto lotto : lottos) {
+			lotto.judgeRank(winningLotto);
+		}
+	}
+
+	@Override
+	public Iterator<Lotto> iterator() {
+		return lottos.iterator();
+	}
 
 }
