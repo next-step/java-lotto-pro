ad89e388a0190c9cef5d560d90ba24ba1de00ce1
wook <mdir2@naver.com>
2021-11-09 13:27:47 +0900

    test(step3): 로또 유효성 테스트 추가

--- src/test/java/step3/domain/LottoTest.java
+++ src/test/java/step3/domain/LottoTest.java
@@ -5,15 +5,13 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import step3.domain.Lotto;
-import step3.domain.LottoNumber;
 
 @DisplayName("로또 테스트")
 class LottoTest {
 
     @ParameterizedTest(name = DISPLAY_NAME)
     @CsvSource(value = "3,2,42,12,40,11")
-    void _6개의_숫자를_조합해_로또_티켓하나를_만들_수_있다(final int one, final int two, final int three,
+    void _6개의_숫자를_조합해_로또를_만들_수_있다(final int one, final int two, final int three,
         final int four, final int five, final int six) {
         //given
         final LottoNumber[] lottoNumbers = {
@@ -26,5 +24,19 @@ class LottoTest {
 
         //then
         assertThat(lotto).isNotNull().isEqualTo(new Lotto(lottoNumbers));
+    }
+
+    @ParameterizedTest(name = DISPLAY_NAME)
+    @CsvSource(value = "3,2,42,12,40,11,7")
+    void _6개의_숫자이상으로_조합해_로또를_만드려고_하면_예외가발생한다(final int one, final int two, final int three,
+        final int four, final int five, final int six, final int seven) {
+        //given
+        final LottoNumber[] lottoNumbers = {
+            new LottoNumber(one), new LottoNumber(two), new LottoNumber(three),
+            new LottoNumber(four), new LottoNumber(five), new LottoNumber(six), new LottoNumber(seven)
+        };
+
+        //when
+        assertThatIllegalArgumentException().isThrownBy(() -> new Lotto(lottoNumbers));
     }
 }
