608a3ad4d24a0c34c991e1ba57ced38121ebd747
Junhwan Kim <junhwan.tech@gmail.com>
2021-11-10 01:32:00 +0900

    feat(InputView): 수동 구매를 위한 입력처리 로직 추가  - 수동 구매 개수  - 수동 구매 번호 (수동 구매 개수 만큼)

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,6 @@
 package lotto.view;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -10,6 +11,8 @@ public class InputView {
 	private static final String PREV_WINNING_NUMBERS_REQUEST = "지난주 당첨 번호를 입력해 주세요";
 	private static final String NUMBER_DELIMITER = ",";
 	private static final String BONUS_NUMBER_REQUEST = "보너스 볼을 입력해주세요";
+	private static final String MANUAL_LOTTO_COUNT_REQUEST = "수동으로 구매할 로또 수를 입력해 주세요.";
+	private static final String MANUAL_NUMBERS_REQUEST = "수동으로 구매할 번호를 입력해 주세요.";
 
 	public int purchaseMoneyAmount() {
 		System.out.println(PURCHASE_AMOUNT_REQUEST);
@@ -17,6 +20,25 @@ public class InputView {
 		String input = userInput();
 
 		return Integer.parseInt(input);
+	}
+
+	public int manualLottoCount() {
+		System.out.println(MANUAL_LOTTO_COUNT_REQUEST);
+
+		String input = userInput();
+
+		return Integer.parseInt(input);
+	}
+
+	public List<List<Integer>> manualLottoNumbers(final int manualCount) {
+		System.out.println(MANUAL_NUMBERS_REQUEST);
+
+		List<String> inputs = userInputs(manualCount);
+
+		return inputs.stream()
+			.map(s -> s.split(NUMBER_DELIMITER))
+			.map(this::convertIntegers)
+			.collect(Collectors.toList());
 	}
 
 	public List<Integer> previousWinningNumber() {
@@ -40,6 +62,15 @@ public class InputView {
 			.map(String::trim)
 			.map(Integer::parseInt)
 			.collect(Collectors.toList());
+	}
+
+	private List<String> userInputs(int count) {
+		List<String> inputs = new ArrayList<>();
+
+		for(int i = 0; i < count; i++) {
+			inputs.add(userInput());
+		}
+		return inputs;
 	}
 
 	private String userInput() {
