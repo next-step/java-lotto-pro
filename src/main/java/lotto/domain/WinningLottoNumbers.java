5e947880846f530c6ced2518c7650d41c895d3bd
shryu <>
2021-11-10 02:35:40 +0900

    feat : 당첨 로또 번호 객체 생성

---
+++ src/main/java/lotto/domain/WinningLottoNumbers.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+import java.util.List;
+import java.util.Optional;
+
+public class WinningLottoNumbers extends LottoNumbers{
+    private BonusNumber bonusNumber;
+
+    public WinningLottoNumbers(List<String> lottoNumbers, BonusNumber bonusNumber) {
+        super(lottoNumbers);
+        this.bonusNumber = bonusNumber;
+    }
+
+    public String getBonusNumber() {
+        Optional.ofNullable(bonusNumber).orElseThrow(
+                () -> new RuntimeException("보너스 번호가 존재하지 않습니다."));
+        return bonusNumber.getBonusNumber();
+    }
+}
