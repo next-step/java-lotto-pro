da0fcccf02bea7e0b66ce5e7df0ddaf9cb161b58
JiHunHong <hongji3354@gmail.com>
2021-11-09 15:47:45 +0900

    feat : 당첨 통계에 5개 일치, 보너스 볼 일치 결과 추가

--- src/main/java/lotto/view/LottoWinnerStatisticView.java
+++ src/main/java/lotto/view/LottoWinnerStatisticView.java
@@ -16,7 +16,8 @@ public class LottoWinnerStatisticView {
         System.out.println("3개 일치 (5000원) - " + winningResult.winnerPerRank(Rank.FOURTH_PLACE));
         System.out.println("4개 일치 (50000원) - " + winningResult.winnerPerRank(Rank.THIRD_PLACE));
         System.out.println("5개 일치 (1500000원) - " + winningResult.winnerPerRank(Rank.SECOND_PLACE));
+        System.out.println("5개 일치, 보너스 볼 일치(30000000원) - " + winningResult.winnerPerRank(Rank.BONUS_SECOND_PLACE));
         System.out.println("6개 일치 (2000000000원) - " + winningResult.winnerPerRank(Rank.FIRST_PLACE));
-        System.out.println(String.format("총 수익률은 %s 입니다.", winningResult.profitRate(money)));
+        System.out.printf("총 수익률은 %s 입니다. %n", winningResult.profitRate(money));
     }
 }
