bdd6201ce6a1c209b332c018849cc35d95e75aa5
DESKTOP-HG83T1I\Chaser <a>
2021-11-09 13:11:15 +0900

    refactor(Infrastructure): 유틸 Class인 Console 의 private 생성자 추가
    - 유틸 클래스이므로 인스턴스변수가 생성되지 않도록 생성자를 숨김
    - 맴버변수 private로 처리

--- src/main/java/lotto/infrastructure/util/Console.java
+++ src/main/java/lotto/infrastructure/util/Console.java
@@ -3,7 +3,11 @@ package lotto.infrastructure.util;
 import java.util.Scanner;
 
 public class Console {
-  public static Scanner scanner = new Scanner(System.in);
+  private static Scanner scanner = new Scanner(System.in);
+
+  private Console() {
+    throw new IllegalAccessError("유틸 클래스이므로 생성자를 이용할 수 없습니다.");
+  }
 
   public static String nextLine() {
     return scanner.nextLine();
