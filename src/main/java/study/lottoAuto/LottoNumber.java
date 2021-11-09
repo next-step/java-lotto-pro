4c38dd46a14aabf2dd4416df87ed630c7fb7d3a5
sungwoo <ssw890418@gmail.com>
2021-11-09 21:45:56 +0900

    feat:로또 숫자 6개를 만드는 기능 구현 및 테스트 추가

---
+++ src/main/java/study/lottoAuto/LottoNumber.java
@@ -0,0 +1,47 @@
+package study.lottoAuto;
+
+import java.util.Comparator;
+import java.util.Objects;
+
+import static study.lottoAuto.MessageUtil.INVALID_LOTTO_NUMBER_ERR_MSG;
+
+public class LottoNumber implements Comparable<LottoNumber> {
+
+    private final int MIN_LOTTO_NUMBER = 1;
+    private final int MAX_LOTTO_NUMBER = 45;
+
+    private final int number;
+
+    public LottoNumber(int number) {
+        this.number = number;
+        validateNumber();
+    }
+
+    private void validateNumber() {
+        if(number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
+            throw new IllegalStateException(INVALID_LOTTO_NUMBER_ERR_MSG);
+        }
+    }
+
+    @Override
+    public int compareTo(LottoNumber o) {
+        return this.number - o.number;
+    }
+
+    public int getNumber() {
+        return this.number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return getNumber() == that.getNumber();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNumber());
+    }
+}
---
+++ src/main/java/study/lottoAuto/LottoNumbersGroup.java
@@ -0,0 +1,16 @@
+package study.lottoAuto;
+
+import java.util.List;
+
+public class LottoNumbersGroup {
+
+    private final List<LottoNumbers> lottoNumbersList;
+
+    public LottoNumbersGroup(List<LottoNumbers> lottoNumbersList) {
+        this.lottoNumbersList = lottoNumbersList;
+    }
+
+    public List<LottoNumbers> getLottoNumbersList() {
+        return lottoNumbersList;
+    }
+}
