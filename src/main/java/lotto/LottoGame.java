3335e701034ece45b9cc6def8cf5d91a0fba2c89
Arasia <rlacndtjs261@gmail.com>
2021-11-09 23:38:29 +0900

    feat: 로또 게임에 수동 로또 기능 추가

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,25 +1,45 @@
 package lotto;
 
 import lotto.domain.LottoBundle;
+import lotto.domain.LottoNumber;
 import lotto.domain.LottoResult;
 import lotto.domain.Money;
 import lotto.domain.WinningLotto;
 import lotto.view.InputView;
 import lotto.view.LottoView;
 
+import java.util.List;
+
 public class LottoGame {
 
     public void play() {
         Money money = InputView.getUserMoney();
 
+        int customLottoCount;
+        do {
+            customLottoCount = InputView.getCustomLottoCount();
+        } while (checkAbleToBuyCustomLotto(money, customLottoCount));
+
+        LottoBundle customLottoBundle = money.buyCustomLotto(InputView.getCustomLottoNumbersBundle(customLottoCount), customLottoCount);
         LottoBundle lottoBundle = money.buyAllRandomLotto();
 
-        LottoView.printLottoBundleStatus(lottoBundle);
+        LottoView.printLottoBundleStatus(customLottoBundle, lottoBundle);
+
+        LottoBundle totalLottoBundle = lottoBundle.addAll(customLottoBundle);
 
         WinningLotto winningLotto = InputView.getWinningLotto();
 
-        LottoResult lottoResult = lottoBundle.getLottoResult(winningLotto);
+        LottoResult lottoResult = totalLottoBundle.getLottoResult(winningLotto);
 
         LottoView.printResult(lottoResult);
+    }
+
+    private boolean checkAbleToBuyCustomLotto(Money money, int customLottoCount) {
+        if (money.isAbleToBuyCustomLotto(customLottoCount)) {
+            return false;
+        }
+
+        LottoView.printErrorToBuyCustomLotto();
+        return true;
     }
 }
