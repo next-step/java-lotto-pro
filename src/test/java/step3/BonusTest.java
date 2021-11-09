c2c469f570426127e10a9488b0a39c1e66901b21
vicjed <vicjed@icloud.com>
2021-11-09 21:58:03 +0900

    test(BonusBall) : 로또번호는 1 ~ 45까지의 숫자 검증 로직 추가

--- src/test/java/step3/BonusTest.java
+++ src/test/java/step3/BonusTest.java
@@ -1,6 +1,7 @@
 package step3;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -14,5 +15,13 @@ public class BonusTest {
 		}).isInstanceOf(IllegalArgumentException.class);
 	}
 
+	@DisplayName("보너스 번호는 1 ~ 45까지 숫자이어야합니다.")
+	@ParameterizedTest()
+	@CsvSource(value = {"1,2,3,4,5,6:-1", "1,42,3,4,5,6:0"}, delimiter = ':')
+	void lottoNumberIsMinOneAndMaxFiftyFive(String userLottoNumbers, int number) {
+		Assertions.assertThatThrownBy(() -> BonusBall.of(number, LottoNumbers.from(userLottoNumbers)))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("로또 번호는 1 ~ 45까지 숫자이어야 합니다.");
+	}
 
 }
