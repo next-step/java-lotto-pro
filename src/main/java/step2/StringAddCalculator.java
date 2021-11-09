fedf1732b7b8017d4207eaa793c3edbf6f7803d3
wookeeeeeee <58969072+wookeeeeeee@users.noreply.github.com>
2021-11-09 16:24:32 +0900

    🚀 STEP2 문자열 덧셈 계산기 코드리뷰 요청합니다. (#232)
    * docs: step1 마일스톤 정리
    
    * test(step1-string): 1,2을 `,`로 split 했을 때 1과 2로 잘 분리되는가
    
    * test(step1-string): 1을 `,`로 split 했을 때 1만을 포함하는 배열을 반환하는가?
    
    * test(step1-string): (1,2)값이 주어졌을 때 1,2을 반환하는가
    
    * test(step1-string): charAt() 메소드를 활용시 StringIndexOutOfBoundsException이 발생하는가
    
    * test(step1-set): 기본으로 주어진 Set의 Size가 3인가
    
    * test(step1-set): `ParameterizedTest` 를 활용해 중복 코드를 제거 할 수 있는가
    
    * test(step1-set): 1 과 2 그리고 3값은 `contains` 메소드 실행결과 `true` 4 와 5 값을 넣으면 `false` 가 반환되는가
    
    * docs: step2 마일스톤 정리
    
    * feat(step2): 빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.
    
    * feat(step2): 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.
    
    * feat(step2): 숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.
    
    * feat(step2): 구분자를 컴마(,) 이외에 콜론(:)을 사용할 수 있다.
    
    * feat(step2): “//”와 “\n” 문자 사이에 커스텀 구분자를 지정할 수 있다.
    
    * feat(step2): 음수를 전달할 경우 RuntimeException 예외가 발생해야 한다.
    
    Co-authored-by: wook <mdir2@naver.com>

---
+++ src/main/java/step2/StringAddCalculator.java
@@ -0,0 +1,97 @@
+package step2;
+
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public final class StringAddCalculator {
+
+    private static final int ZERO = 0;
+    private static final int SINGLE_DIGIT_LENGTH = 1;
+
+    private static final String DELIMITER = "[,:]";
+    private static final Pattern CUSTOM_DELIMITER = Pattern.compile("//(.)\n(.*)");
+
+    private static final String[] EMPTY_TEXT = {};
+
+    private StringAddCalculator() {
+    }
+
+    public static int splitAndSum(final String text) {
+        if (isNull(text)) {
+            return ZERO;
+        }
+
+        if (isEmpty(text)) {
+            return ZERO;
+        }
+
+        if (hasSingleNumber(text)) {
+            return parseInt(text);
+        }
+
+        if (isDelimiterContainCommaAndColon(text)) {
+            return sum(split(text));
+        }
+
+        return sum(customSplit(text));
+    }
+
+    private static boolean isNull(final String text) {
+        return Objects.isNull(text);
+    }
+
+    private static boolean isEmpty(final String text) {
+        return text.isEmpty();
+    }
+
+    private static boolean hasSingleNumber(final String text) {
+        return text.length() == SINGLE_DIGIT_LENGTH;
+    }
+
+    private static int parseInt(final String text) {
+        return Integer.parseInt(text);
+    }
+
+    private static boolean isDelimiterContainCommaAndColon(final String text) {
+        return text.contains(",") || text.contains(":");
+    }
+
+    private static String[] split(final String text) {
+        return text.split(DELIMITER);
+    }
+
+    private static String[] customSplit(final String text) {
+        final Matcher matcher = CUSTOM_DELIMITER.matcher(text);
+
+        if (matcher.find()) {
+            final String customDelimiter = matcher.group(1);
+            return matcher.group(2).split(customDelimiter);
+        }
+
+        return EMPTY_TEXT;
+    }
+
+    private static boolean isNegative(final int number) {
+        return number < ZERO;
+    }
+
+    private static void validateNegative(final int number) {
+        if (isNegative(number)) {
+            throw new RuntimeException("not allowed negative numbers.");
+        }
+    }
+
+    private static int sum(final String[] splitText) {
+        int sum = 0;
+
+        for (String text : splitText) {
+            final int number = parseInt(text);
+            validateNegative(number);
+
+            sum += number;
+        }
+
+        return sum;
+    }
+}
