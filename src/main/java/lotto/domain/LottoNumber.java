63aa69dad74a1f86b6c937ee06dd0105ad1eae4f
shryu <>
2021-11-10 03:11:34 +0900

    feat : 로또 번호 객체 변수 int로 수정, equals, hashcode 추가

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,22 +1,37 @@
 package lotto.domain;
 
+import java.util.Objects;
+
 public class LottoNumber {
     public static final int LOTTO_START_NUMBER = 1;
     public static final int LOTTO_END_NUMBER = 45;
-    private String lottoNumber;
+    private int lottoNumber;
 
-    public LottoNumber(String lottoNumber) {
+    public LottoNumber(int lottoNumber) {
         validateLottoNumber(lottoNumber);
         this.lottoNumber = lottoNumber;
     }
 
-    private void validateLottoNumber(String lottoNumber) {
-        if (Integer.parseInt(lottoNumber) < LottoNumber.LOTTO_START_NUMBER || Integer.parseInt(lottoNumber) > LottoNumber.LOTTO_END_NUMBER) {
+    private void validateLottoNumber(int lottoNumber) {
+        if (lottoNumber < LottoNumber.LOTTO_START_NUMBER || lottoNumber > LottoNumber.LOTTO_END_NUMBER) {
             throw new IllegalArgumentException("로또 번호 범위는 1 ~ 45 입니다.");
         }
     }
 
     public String getValue() {
-        return lottoNumber;
+        return Integer.toString(lottoNumber);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return lottoNumber == that.lottoNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoNumber);
     }
 }
