2ac09a226e18490657c8cae6f8bedd564c83fed3
wook <mdir2@naver.com>
2021-11-09 13:27:40 +0900

    test(step3): 로또번호 유효성 테스트 추가

--- src/test/java/step3/domain/LottoNumberTest.java
+++ src/test/java/step3/domain/LottoNumberTest.java
@@ -3,9 +3,10 @@ package step3.domain;
 import static helper.Constants.*;
 import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import step3.domain.LottoNumber;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("로또번호 테스트")
 class LottoNumberTest {
@@ -13,12 +14,28 @@ class LottoNumberTest {
     @ParameterizedTest(name = DISPLAY_NAME)
     @CsvSource(value = "1,45")
     void _1에서_45번으로_로또번호를_만들_수_있다(final int startNumber, final int endNumber) {
-        for (int lottoNumber = startNumber; lottoNumber <= endNumber; lottoNumber++) {
+        for (int number = startNumber; number <= endNumber; number++) {
             //given when
-            final LottoNumber lotto = new LottoNumber(lottoNumber);
+            final LottoNumber lottoNumber = new LottoNumber(number);
 
             //then
-            assertThat(lotto).isNotNull().isEqualTo(new LottoNumber(lottoNumber));
+            assertThat(lottoNumber).isNotNull().isEqualTo(new LottoNumber(number));
         }
+    }
+
+    @Test
+    void 음수로_로또번호를_만드려고_하면_예외가발생한다() {
+        //given
+        final int number = -1;
+
+        //when then
+        assertThatIllegalArgumentException().isThrownBy(() -> new LottoNumber(number)).withMessageContaining("음수");
+    }
+
+    @ParameterizedTest(name = DISPLAY_NAME)
+    @ValueSource(ints = {0, 46, 57, 100})
+    void _1에서_45번_이외의_숫자로_로또번호를_만드려고_하면_예외가발생한다(final int number) {
+        //when then
+        assertThatIllegalArgumentException().isThrownBy(() -> new LottoNumber(number));
     }
 }
