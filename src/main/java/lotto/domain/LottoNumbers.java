8c0d92bbf54269b1c6a22b966ba52b3910bc33a8
shryu <>
2021-11-10 03:37:01 +0900

    refactor : LottoNumber List로 수정 하면서 validate 로직 삭제

--- src/main/java/lotto/domain/LottoNumbers.java
+++ src/main/java/lotto/domain/LottoNumbers.java
@@ -8,19 +8,9 @@ public class LottoNumbers {
     private List<LottoNumber> lottoNumbers = new ArrayList<>();
 
     public LottoNumbers(List<String> lottoNumbers) {
-        validateNumbers(lottoNumbers);
         this.lottoNumbers = lottoNumbers.stream()
                                 .map(l -> new LottoNumber(Integer.parseInt(l)))
                                 .collect(Collectors.toList());
-    }
-
-    private void validateNumbers(List<String> lottoNumbers) {
-        lottoNumbers.stream()
-                .filter(l -> Integer.parseInt(l) < LottoNumber.LOTTO_START_NUMBER || Integer.parseInt(l) > LottoNumber.LOTTO_END_NUMBER)
-                .findAny()
-                .ifPresent(l -> {
-                    throw new IllegalArgumentException("로또 번호 범위는 1 ~ 45 입니다.");
-                });
     }
 
     public long compare(LottoNumbers winningNumbers) {
@@ -29,7 +19,7 @@ public class LottoNumbers {
                         .count();
     }
 
-    private boolean isContainNumber(LottoNumber lottoNumber) {
+    protected boolean isContainNumber(LottoNumber lottoNumber) {
         return lottoNumbers.contains(lottoNumber);
     }
 
