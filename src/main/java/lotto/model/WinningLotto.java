3ce565030dc6a2e0aaf587f2a3a0db7de7fb2f37
IW.MOON <mooniw02@gmail.com>
2021-11-09 23:58:52 +0900

    refactor: 당첨번호에 보너스 볼이 포함되어 있는지 판단 검증 로직을 WinningLotto로 변경

--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -1,11 +1,18 @@
 package lotto.model;
 
+import lotto.view.GameMessage;
+
 public class WinningLotto {
 
     private final LottoPaper winningLottoPaper;
     private final LottoNumber bonusLottoNumber;
 
     public WinningLotto(LottoPaper winningLottoPaper, LottoNumber bonusLottoNumber) {
+
+        if (winningLottoPaper.isContainsLottoNumber(bonusLottoNumber)) {
+            throw new IllegalArgumentException(GameMessage.invalidInputMsg(GameMessage.ERROR_BONUS_NUMBER_INPUT));
+        }
+
         this.winningLottoPaper = winningLottoPaper;
         this.bonusLottoNumber = bonusLottoNumber;
     }
@@ -17,4 +24,5 @@ public class WinningLotto {
     public LottoNumber getBonusLottoNumber() {
         return bonusLottoNumber;
     }
+
 }
