a6fcb3bd39d084b713e1ce4b2f83eaa570c00fb5
vicjed <vicjed@icloud.com>
2021-11-09 21:46:09 +0900

    style: 코드 포맷

--- src/main/java/step3/BonusBall.java
+++ src/main/java/step3/BonusBall.java
@@ -5,15 +5,14 @@ public class BonusBall extends LottoNumber {
 	private BonusBall(int no) {
 		super(no);
 	}
+
 	public static BonusBall of(int no, LottoNumbers userLottoNumbers) {
 		BonusBall bonusBall = new BonusBall(no);
 		if (userLottoNumbers.hasBonusBall(bonusBall)) {
 			throw new IllegalArgumentException("보너스 볼 번호가 지난주 보너스 볼 번호와 동일 할 수 없습니다.");
-		};
+		}
 		return new BonusBall(no);
 	}
-
-
 
 	@Override
 	public boolean equals(Object o) {
