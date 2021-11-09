118ecf559daad93e7d5c0564759dd5b19c1d1b28
shryu <>
2021-11-10 02:24:08 +0900

    feat : 로또 번호 유효성 검사 로직 추가, value get 메소드 구현

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -6,6 +6,17 @@ public class LottoNumber {
     private String lottoNumber;
 
     public LottoNumber(String lottoNumber) {
+        validateLottoNumber(lottoNumber);
         this.lottoNumber = lottoNumber;
+    }
+
+    private void validateLottoNumber(String lottoNumber) {
+        if (Integer.parseInt(lottoNumber) < LottoNumber.LOTTO_START_NUMBER || Integer.parseInt(lottoNumber) > LottoNumber.LOTTO_END_NUMBER) {
+            throw new IllegalArgumentException("로또 번호 범위는 1 ~ 45 입니다.");
+        }
+    }
+
+    public String getValue() {
+        return lottoNumber;
     }
 }
