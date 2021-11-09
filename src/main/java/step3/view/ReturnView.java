95e9907c8e663283698cee0dee9fb82e205d4703
vicjed <vicjed@icloud.com>
2021-11-10 00:04:34 +0900

    fix : 화면 출력 정수에서 문자열로 변경

--- src/main/java/step3/view/ReturnView.java
+++ src/main/java/step3/view/ReturnView.java
@@ -10,7 +10,7 @@ public class ReturnView implements ResultView {
 	private static final String MESSAGE_BUY_COUNT = "%s개를 구매했습니다.";
 	private static final String MESSAGE_WINNING_STATISTICS = "당첨 통계";
 	private static final String MESSAGE_PERFORATED_LINE = "---------";
-	private static final String MESSAGE_TOTAL_YIELD = "총 수익률은 %d 입니다.";
+	private static final String MESSAGE_TOTAL_YIELD = "총 수익률은 %s 입니다.";
 
 	public void purchasedLottoPrint(LottoPapers lottoPapers) {
 		System.out.println(lottoPapers);
@@ -27,5 +27,5 @@ public class ReturnView implements ResultView {
 	}
 
 	public void yieldPrint(BigDecimal yield) {
-		System.out.println(String.format(MESSAGE_TOTAL_YIELD, yield));	}
+		System.out.println(String.format(MESSAGE_TOTAL_YIELD, yield.toString()));	}
 }
