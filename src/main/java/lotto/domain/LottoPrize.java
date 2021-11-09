512e91f90322eaa92fd80cda2caaf2a4bc972646
shryu <>
2021-11-10 02:38:49 +0900

    feat : 로또 맞은 개수 5개, 보너스 번호 맞을 경우 2등, 맞은 개수 5개 보너스 번호 맞지 않을 경우 3등 리턴 하도록 수정

--- src/main/java/lotto/domain/LottoPrize.java
+++ src/main/java/lotto/domain/LottoPrize.java
@@ -3,11 +3,12 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum LottoPrize {
-    NONE (0, 0),
-    FIFTH (3, 5000),
-    FOURTH (4, 50_000),
+    FIRST (6, 2_000_000_000),
+    SECOND (5, 30_000_000),
     THIRD (5, 1_500_000),
-    FIRST (6, 2_000_000_000);
+    FOURTH (4, 50_000),
+    FIFTH (3, 5000),
+    NONE (0, 0);
 
     private int count;
     private int money;
@@ -17,11 +18,16 @@ public enum LottoPrize {
         this.money = money;
     }
 
-    public static LottoPrize findPrize(long matchCount) {
-        return Arrays.stream(LottoPrize.values())
-                .filter(l -> l.isMatchCount(matchCount))
-                .findFirst()
-                .orElse(NONE);
+    public static LottoPrize findPrize(long matchCount, boolean matchBonus) {
+        LottoPrize[] values = LottoPrize.values();
+        LottoPrize lottoPrize = Arrays.stream(LottoPrize.values())
+                                    .filter(l -> l.isMatchCount(matchCount))
+                                    .findFirst()
+                                    .orElse(NONE);
+        if (!matchBonus && lottoPrize.equals(SECOND)) {
+            return THIRD;
+        }
+        return lottoPrize;
     }
 
     private boolean isMatchCount(long matchCount) {
