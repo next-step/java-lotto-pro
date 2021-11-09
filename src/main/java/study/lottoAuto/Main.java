61b3cae78c79a4fcc29ef14a4b3273813c352670
sungwoo <ssw890418@gmail.com>
2021-11-09 21:54:41 +0900

    feat:컨트롤러를 실행시키는 main() 메소드 추가

---
+++ src/main/java/study/lottoAuto/Main.java
@@ -0,0 +1,8 @@
+package study.lottoAuto;
+
+public class Main {
+    public static void main(String[] args) {
+        LottoController controller = new LottoController();
+        controller.run();
+    }
+}
