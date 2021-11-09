f424dc8f68900b36453f520ea49ba1e654634093
shryu <>
2021-11-10 01:16:12 +0900

    feat : LottoNumber 객체 생성

---
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -0,0 +1,11 @@
+package lotto.domain;
+
+public class LottoNumber {
+    public static final int LOTTO_START_NUMBER = 1;
+    public static final int LOTTO_END_NUMBER = 45;
+    private String lottoNumber;
+
+    public LottoNumber(String lottoNumber) {
+        this.lottoNumber = lottoNumber;
+    }
+}
