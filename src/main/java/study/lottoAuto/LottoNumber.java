87906c10f319f933d8349b4865780352956aff9e
sungwoo <ssw890418@gmail.com>
2021-11-10 00:16:27 +0900

    style:자동 생성된 코드를 컨벤션을 지키도록 수정

--- src/main/java/study/lottoAuto/LottoNumber.java
+++ src/main/java/study/lottoAuto/LottoNumber.java
@@ -34,8 +34,12 @@ public class LottoNumber implements Comparable<LottoNumber> {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         LottoNumber that = (LottoNumber) o;
         return getNumber() == that.getNumber();
     }
