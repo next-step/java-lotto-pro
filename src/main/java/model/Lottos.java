e71d92505566a2726fa91b4909be032ebcacdfc2
daehyun0 <dd70296@gmail.com>
2021-11-11 04:40:59 +0900

    test: LottosTest 추가

--- src/main/java/model/Lottos.java
+++ src/main/java/model/Lottos.java
@@ -2,6 +2,7 @@ package model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Lottos {
 	private final List<Lotto> lottos;
@@ -27,6 +28,21 @@ public class Lottos {
 		if (lottoRank != Rank.NONE) {
 			rewardCalculator.addCount(lottoRank);
 		}
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Lottos lottos1 = (Lottos)o;
+		return Objects.equals(lottos, lottos1.lottos);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(lottos);
 	}
 
 	@Override
