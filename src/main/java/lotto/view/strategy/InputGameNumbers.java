8ebd0182b32bbfb72aec834b46533f23e2f0aefa
신민준 <jxin95@gmail.com>
2021-11-10 16:24:09 +0900

    feat: 수동 구매수량 입력 클래스 추가

---
+++ src/main/java/lotto/view/strategy/InputGameNumbers.java
@@ -0,0 +1,20 @@
+package lotto.view.strategy;
+
+import java.util.regex.Pattern;
+
+public abstract class InputGameNumbers implements Input {
+
+    private static final Pattern REGEX_PATTERN = Pattern.compile("^[0-9]{1,2}+(,\\s?[0-9]{1,2}+)*$");
+    private static final String ERROR_MESSAGE= "숫자와 구분자(,)만 입력할 수 있습니다.";
+
+    @Override
+    public Pattern inputRegexCompiler() {
+        return REGEX_PATTERN;
+    }
+
+    @Override
+    public String getErrorMessage() {
+        return ERROR_MESSAGE;
+    }
+
+}
