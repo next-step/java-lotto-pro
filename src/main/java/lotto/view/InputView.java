6ebcb45abc10efe307b0fd8090ad87326b48e1a2
Jeongkyo Kim <chavitak@gmail.com>
2021-11-11 08:25:01 +0900

    fix: 수동 로또 구입시 출력되는 메시지를 수정한다

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,6 +8,7 @@ public class InputView {
     public static final String INPUT_WINNING_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     public static final String INPUT_BONUS_BALL_MESSAGE = "보너스 볼을 입력해 주세요.";
     public static final String INPUT_COUNTS_OF_MANUAL_TICKETS = "수동으로 구매할 로또 수를 입력해 주세요";
+    public static final String INPUT_LOTTO_NUMBERS_MESSAGE = "수동으로 구매할 번호를 입력해주세요.";
 
     Scanner scanner = new Scanner(System.in);
 
@@ -35,7 +36,7 @@ public class InputView {
     }
 
     public String inputLottoNumbers() {
-        System.out.println(INPUT_WINNING_NUMBER_MESSAGE);
+        System.out.println(INPUT_LOTTO_NUMBERS_MESSAGE);
         return scanner.next();
     }
 }
