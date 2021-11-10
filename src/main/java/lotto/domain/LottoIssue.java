f4824d2e01daf0e921fbd16367789a29ad385e66
donkey <rlaehdzlsla@gmail.com>
2021-11-11 00:03:06 +0900

    fix: Rank를 map 담을떄 전체 랭크를 돌면서 카운트를 구해야해하는데, 전체 랭크를 안돌면서 카운트를 구하는 오류 수정

--- src/main/java/lotto/domain/LottoIssue.java
+++ src/main/java/lotto/domain/LottoIssue.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.IntStream;
 
 import static java.util.stream.Collectors.toList;
@@ -10,11 +12,23 @@ public class LottoIssue {
     private LottoIssue() {
     }
 
-    public static LottoTicket ofAuto(int purchaseQuantity) {
-        List<LottoNumbers> lottoNumbers = IntStream.range(0, purchaseQuantity)
+    public static LottoTicket of(LottoPurchase lottoPurchase, Map<Integer, List<Integer>> inputManualLottoNumbers) {
+        List<LottoNumbers> lottoNumbers = new ArrayList<>();
+        lottoNumbers.addAll(fromManual(inputManualLottoNumbers));
+        lottoNumbers.addAll(fromAuto(lottoPurchase.getAutoPurchaseQuantity()));
+        return new LottoTicket(lottoNumbers);
+    }
+
+    private static List<LottoNumbers> fromManual(Map<Integer, List<Integer>> inputManualLottoNumbers) {
+        return inputManualLottoNumbers.values().stream()
+                .map(LottoNumbers::new)
+                .collect(toList());
+    }
+
+    private static List<LottoNumbers> fromAuto(int purchaseQuantity) {
+        return IntStream.range(0, purchaseQuantity)
                 .mapToObj(i -> LottoGenerator.generate())
                 .collect(toList());
-        return new LottoTicket(lottoNumbers);
     }
 
 }
