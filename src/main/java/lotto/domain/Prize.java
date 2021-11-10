bfeb19e69576cc27d4bfaf9bc3527f59724e5b77
Jeongkyo Kim <chavitak@gmail.com>
2021-11-10 14:13:45 +0900

    feat: Prize Enum 객체에 2등을 추가한다

--- src/main/java/lotto/domain/Prize.java
+++ src/main/java/lotto/domain/Prize.java
@@ -4,6 +4,7 @@ public enum Prize {
     FIFTH(3, 5_000),
     FOURTH(4, 50_000),
     THIRD(5, 1_500_000),
+    SECOND(5, 30_000_000),
     FIRST(6, 2_000_000_000);
 
     private final int countOfMatch;
