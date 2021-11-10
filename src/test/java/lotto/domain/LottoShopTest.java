c839e8046d910ad2647b4ea4eac178eda2fc89a1
codingknowjam <hanwoodww@gmail.com>
2021-11-11 03:42:21 +0900

    refactor: 안쓰는 코드 삭제

--- src/test/java/lotto/domain/LottoShopTest.java
+++ src/test/java/lotto/domain/LottoShopTest.java
@@ -29,22 +29,4 @@ class LottoShopTest {
 		//then
 		assertThat(duplicateCheck).hasSize(6);
 	}
-
-	// @Test
-	// @DisplayName("로또 수동 판매 테스트")
-	// public void LottoShopStringSellTest() {
-	// 	//given
-	// 	//when
-	// 	Lotto lotto = LottoShop.sell("1, 2, 3, 4, 5, 45");
-	// 	//then
-	// 	assertThat(lotto.getLottoNumbers()).hasSize(6);
-	// }
-	//
-	// @Test
-	// @DisplayName("로또 수동 판매시 번호 중복 예외 테스트")
-	// public void LottoShopStringSellExceptionTest() {
-	// 	assertThatThrownBy(() -> LottoShop.sell("1, 2, 32, 32, 5, 45"))
-	// 		.isInstanceOf(IllegalArgumentException.class)
-	// 		.hasMessage("[ERROR] 로또 번호는 중복 될 수 없습니다.");
-	// }
 }
