e146df79ec69b9f9469ae07aa358f155ac48a01b
Jeongkyo Kim <chavitak@gmail.com>
2021-11-10 14:11:20 +0900

    test: view레이어에 의존적인 테스트를 삭제한다

--- src/test/java/lotto/domain/LottoTicketsTest.java
+++ src/test/java/lotto/domain/LottoTicketsTest.java
@@ -39,23 +39,4 @@ class LottoTicketsTest {
                         + "[7, 8, 9, 10, 11, 12]"
         );
     }
-
-    @Test
-    @DisplayName("게임 결과를 출력한다")
-    void getGameResultTest() {
-        // given
-        ArrayList<LottoTicket> lottoTicketsSource = new ArrayList<>();
-        lottoTicketsSource.add(new LottoTicket(Arrays.asList(1, 2, 3, 4, 5, 6)));
-        lottoTicketsSource.add(new LottoTicket(Arrays.asList(7, 8, 9, 10, 11, 12)));
-        LottoTickets lottoTickets = new LottoTickets(lottoTicketsSource);
-
-        // when
-        GameResult gameResult = lottoTickets.getGameResult(new LottoTicket(Arrays.asList(7, 8, 9, 10, 4, 5)));
-
-        // then
-        assertThat(gameResult.toString()).isEqualTo("3개 일치 (5000원)- 0개\n" +
-                "4개 일치 (50000원)- 1개\n" +
-                "5개 일치 (1500000원)- 0개\n" +
-                "6개 일치 (2000000000원)- 0개");
-    }
 }
