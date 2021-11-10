d5778fadceb88263721932f863e67b20c1748d45
박창혁 <changchang73@pionnet.co.kr>
2021-11-11 05:03:44 +0900

    test : 로또기계 테스트

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -2,18 +2,24 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class LottoMachineTest {
-
 	@DisplayName("로또 생성 크기 비교")
 	@ParameterizedTest
-	@CsvSource(value = {"5000:5", "6000:6", "15000:15"}, delimiter = ':')
+	@CsvSource(value = {"5000:5", "10000:10", "1000:1"}, delimiter = ':')
 	public void getPurchaseQuantity(int money, int purchaseQuantity) {
 		LottoMachine lottoMachine = new LottoMachine();
-		Lottos lottos = lottoMachine.generateLottos(new PurchaseAmount(money).getPurchaseQuantity());
+		Lottos manualLottos = new Lottos(new ArrayList<>());
+		ManualNumber manualNumber = new ManualNumber(0);
+		PurchaseAmount purchaseAmount = new PurchaseAmount(money);
+		Buyer buyer = new Buyer(purchaseAmount, manualNumber, manualLottos);
+
+		Lottos lottos = lottoMachine.generateLottos(buyer);
 
 		int lottoNumberSize = lottos.quantity();
 
