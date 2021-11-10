a971bcb1b4b5a45a3971415a3e0cf0772a8809d2
ch3224bin <ahrocksil@gmail.com>
2021-11-10 17:36:49 +0900

    refactor : LottoNumber 캐시 선언과 동시에 초기화 되도록 수정

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,20 +1,19 @@
 package lotto.domain;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LottoNumber {
     protected static final int MIN_NUMBER = 1;
     protected static final int MAX_NUMBER = 45;
-    private static final Map<Integer, LottoNumber> cache = new HashMap<>();
+    private static final Map<Integer, LottoNumber> cache =
+            IntStream.rangeClosed(MIN_NUMBER, MAX_NUMBER)
+                    .mapToObj(LottoNumber::of)
+                    .collect(Collectors.toMap(lottoNumber -> lottoNumber.number, Function.identity()));
     private final int number;
-
-    static {
-        for (int i = MIN_NUMBER; i <= MAX_NUMBER; i++) {
-            cache.put(i, new LottoNumber(i));
-        }
-    }
 
     private LottoNumber(int number) {
         this.number = number;
