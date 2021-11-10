d0bbc1377f1f71b2873b0c0e4eaff71c8d793003
신민준 <jxin95@gmail.com>
2021-11-10 16:27:15 +0900

    feat: 당첨번호 입력 클래스와 수동번호 입력 클래스의 중복되는 부분을 부모클래스로 생성하고, 수동번호 입력 클래스를 추가

---
+++ src/main/java/lotto/view/strategy/InputManualGameNumbers.java
@@ -0,0 +1,12 @@
+package lotto.view.strategy;
+
+public class InputManualGameNumbers extends InputGameNumbers {
+
+    private static final String MESSAGE = "\r\n수동으로 구매할 번호를 입력해 주세요.";
+
+    @Override
+    public String getMessage() {
+        return MESSAGE;
+    }
+
+}
---
+++ src/main/java/lotto/view/strategy/InputPurchaseManualGameCount.java
@@ -0,0 +1,26 @@
+package lotto.view.strategy;
+
+import java.util.regex.Pattern;
+
+public class InputPurchaseManualGameCount implements Input {
+
+    private static final String MESSAGE = "\r\n수동으로 구매할 로또 수를 입력해 주세요.";
+    private static final Pattern REGEX_PATTERN = Pattern.compile("^[0-9]{1,}$");
+    private static final String ERROR_MESSAGE= "숫자만 입력할 수 있습니다.";
+
+    @Override
+    public String getMessage() {
+        return MESSAGE;
+    }
+
+    @Override
+    public Pattern inputRegexCompiler() {
+        return REGEX_PATTERN;
+    }
+
+    @Override
+    public String getErrorMessage() {
+        return ERROR_MESSAGE;
+    }
+
+}
