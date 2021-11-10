195ebec6d82ea7fa7b009889d3b33552ad9069d3
Arasia <rlacndtjs261@gmail.com>
2021-11-10 23:40:14 +0900

    fix: LottoRank의 상수 제거 및 findRank 로직 수정

--- src/main/java/lotto/constant/LottoRank.java
+++ src/main/java/lotto/constant/LottoRank.java
@@ -1,15 +1,16 @@
 package lotto.constant;
 
 import java.util.Arrays;
-import java.util.function.Predicate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public enum LottoRank {
-    NONE(0, Constants.IGNORE_COUNT, 0),
-    FIFTH(3, Constants.IGNORE_COUNT, 5000),
-    FOURTH(4, Constants.IGNORE_COUNT, 5_0000),
+    NONE(0, 0, 0),
+    FIFTH(3, 0, 5000),
+    FOURTH(4, 0, 5_0000),
     THIRD(5, 0, 150_0000),
     SECOND(5, 1, 3000_0000),
-    FIRST(6, Constants.IGNORE_COUNT, 20_0000_0000);
+    FIRST(6, 0, 20_0000_0000);
 
     private final int matchingCount;
     private final int bonusMatchingCount;
@@ -22,21 +23,22 @@ public enum LottoRank {
     }
 
     public static LottoRank findRank(int matchingCount, int bonusMatchingCount) {
-        return Arrays.stream(LottoRank.values())
+        List<LottoRank> filteredLottoRanks = Arrays.stream(LottoRank.values())
                 .filter(lottoRank -> lottoRank.matchingCount == matchingCount)
-                .filter(checkBonusMatchingCount(bonusMatchingCount))
+                .collect(Collectors.toList());
+
+        if (filteredLottoRanks.isEmpty()) {
+            return LottoRank.NONE;
+        }
+
+        if (filteredLottoRanks.size() == 1) {
+            return filteredLottoRanks.get(0);
+        }
+
+        return filteredLottoRanks.stream()
+                .filter(lottoRank -> lottoRank.bonusMatchingCount == bonusMatchingCount)
                 .findFirst()
                 .orElse(LottoRank.NONE);
-    }
-
-    private static Predicate<LottoRank> checkBonusMatchingCount(int bonusMatchingCount) {
-        return lottoRank -> {
-            if (Constants.IGNORE_COUNT == lottoRank.bonusMatchingCount) {
-                return true;
-            }
-
-            return lottoRank.bonusMatchingCount == bonusMatchingCount;
-        };
     }
 
     public int getMatchingCount() {
@@ -45,9 +47,5 @@ public enum LottoRank {
 
     public int getPrice() {
         return price;
-    }
-
-    private static class Constants {
-        public static final int IGNORE_COUNT = -1;
     }
 }
