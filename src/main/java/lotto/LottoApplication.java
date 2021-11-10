1e8fbef10329a315d81349db14bf33b3083dcef5
신민준 <jxin95@gmail.com>
2021-11-10 17:55:39 +0900

    feat: 메인 클래스에서 수동구매수량, 수동번호입력 등의 기능 호출 구문을 추가

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -9,10 +9,12 @@ public class LottoApplication {
     public static void main(String[] args) {
         int purchaseAmount = InputView.inputPurchaseAmount();
         GameCount gameCount = new GameCount(purchaseAmount);
-        ResultView.printPurchasedGameCount(gameCount.getValue());
-        Games games = new Games(gameCount.getValue());
-        WinningNumbers winningNumbers = InputView.inputWinningNumbers();
-        WinningNumber bonusNumber = InputView.inputBonusNumber(winningNumbers);
+        ManualGameCount manualGameCount = InputView.inputPurchaseManualGameCount(gameCount);
+        ResultView.printPurchasedGameCount(gameCount.getValue(), manualGameCount.getValue());
+        ManualGames manualGames = InputView.inputManualGameNumbers(manualGameCount.getValue());
+        Games games = new Games(gameCount.getValue(), manualGames.getList());
+        LottoNumbers winningNumbers = InputView.inputWinningNumbers();
+        LottoNumber bonusNumber = InputView.inputBonusNumber(winningNumbers);
         Checker checker = new Checker(games, winningNumbers, bonusNumber);
         ResultView.printResult(checker.getResults());
         ResultView.printEarningRate(purchaseAmount);
