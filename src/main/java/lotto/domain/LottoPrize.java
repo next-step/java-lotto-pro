8c3478acb359276dd45411dfc01b0f6608c8ee8e
shryu <>
2021-11-11 01:49:27 +0900

    refactor : prize 속성 bonus 추가하여 prize 구하는 로직 수정

--- src/main/java/lotto/domain/LottoPrize.java
+++ src/main/java/lotto/domain/LottoPrize.java
@@ -3,34 +3,33 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum LottoPrize {
-    FIRST (6, 2_000_000_000),
-    SECOND (5, 30_000_000),
-    THIRD (5, 1_500_000),
-    FOURTH (4, 50_000),
-    FIFTH (3, 5000),
-    NONE (0, 0);
+    FIRST (6, 2_000_000_000, false),
+    SECOND (5, 30_000_000, true),
+    THIRD (5, 1_500_000, false),
+    FOURTH (4, 50_000, false),
+    FIFTH (3, 5000, false),
+    NONE (0, 0, false);
 
     private int count;
     private int money;
+    private boolean bonus;
 
-    LottoPrize(int count, int money) {
+    LottoPrize(int count, int money, boolean bonus) {
         this.count = count;
         this.money = money;
+        this.bonus = bonus;
     }
 
     public static LottoPrize findPrize(long matchCount, boolean matchBonus) {
         LottoPrize lottoPrize = Arrays.stream(LottoPrize.values())
-                                    .filter(l -> l.isMatchCount(matchCount))
+                                    .filter(l -> l.matchPrize(matchCount, matchBonus))
                                     .findFirst()
                                     .orElse(NONE);
-        if (!matchBonus && lottoPrize.equals(SECOND)) {
-            return THIRD;
-        }
         return lottoPrize;
     }
 
-    private boolean isMatchCount(long matchCount) {
-        return count == matchCount;
+    private boolean matchPrize(long matchCount, boolean matchBonus) {
+        return count == matchCount && bonus == matchBonus;
     }
 
     public int getCount() {
