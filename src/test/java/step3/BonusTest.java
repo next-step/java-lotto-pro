a352a014f139f3b58b9a14e2eb4bab7de82b577b
vicjed <vicjed@icloud.com>
2021-11-11 19:37:35 +0900

    feat : @Deprecated 대상 삭제 및 수정

--- src/test/java/step3/BonusTest.java
+++ src/test/java/step3/BonusTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import step3.lotto.BonusBall;
 import step3.lotto.LottoNumbers;
+import step3.winner.Winning;
 
 public class BonusTest {
 
@@ -16,15 +17,15 @@ public class BonusTest {
 		"1,42,3,4,5,6:42"}, delimiter = ':')
 	void name(String winningLottoNumbers, int bonusBall) {
 		Assertions.assertThatThrownBy(() -> {
-			BonusBall.of(bonusBall, LottoNumbers.from(winningLottoNumbers));
+			new Winning(LottoNumbers.from(winningLottoNumbers), new BonusBall(bonusBall));
 		}).isInstanceOf(IllegalArgumentException.class);
 	}
 
 	@DisplayName("보너스 번호는 1 ~ 45까지 숫자이어야합니다.")
 	@ParameterizedTest()
 	@CsvSource(value = {"1,2,3,4,5,6:-1", "1,42,3,4,5,6:0"}, delimiter = ':')
-	void lottoNumberIsMinOneAndMaxFiftyFive(String winningLottoNumbers, int number) {
-		Assertions.assertThatThrownBy(() -> BonusBall.of(number, LottoNumbers.from(winningLottoNumbers)))
+	void lottoNumberIsMinOneAndMaxFiftyFive(String winningLottoNumbers, int bonusBall) {
+		Assertions.assertThatThrownBy(() -> new Winning(LottoNumbers.from(winningLottoNumbers), new BonusBall(bonusBall)))
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessage("로또 번호는 1 ~ 45까지 숫자이어야 합니다.");
 	}
