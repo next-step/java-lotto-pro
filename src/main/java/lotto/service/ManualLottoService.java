7eff3af4f4c0d39a4e19f610771b4e7be9a30aff
shryu <>
2021-11-12 01:55:22 +0900

    feat : 입력받은 로또 개수와 입력받은 로또 숫자들 개수 다를 경우 에러처리 구현

--- src/main/java/lotto/service/ManualLottoService.java
+++ src/main/java/lotto/service/ManualLottoService.java
@@ -12,12 +12,19 @@ public class ManualLottoService {
 
     public static List<Lotto> createManualLotties(int manualLottoCount, List<String> inputManualLottoNumbersString) {
         validateManualLottoCount(manualLottoCount);
+        validateManualLottoNumbersStringSize(manualLottoCount, inputManualLottoNumbersString.size());
         List<Lotto> manualLotties = new LinkedList<>();
 
         for (int i = 0; i < manualLottoCount; i++) {
             manualLotties.add(new Lotto(new LottoNumbers(StringUtils.separate(inputManualLottoNumbersString.get(i)))));
         }
         return manualLotties;
+    }
+
+    private static void validateManualLottoNumbersStringSize(int manualLottoCount, int manualLottoNumbersStringSize) {
+        if (manualLottoCount != manualLottoNumbersStringSize) {
+            throw new IllegalArgumentException("수동 로또 개수와 입력받은 수동 로또 번호들의 개수는 같아야 합니다.");
+        }
     }
 
     private static void validateManualLottoCount(int manualLottoCount) {
--- src/test/java/lotto/LottoServiceTest.java
+++ src/test/java/lotto/LottoServiceTest.java
@@ -46,4 +46,12 @@ public class LottoServiceTest {
         Assertions.assertThatThrownBy(() -> ManualLottoService.createManualLotties(manualLottoCount, inputManualLottoNumbersString))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @Test
+    void 수동로또_개수_입력받은로또_개수_오류() {
+        int manualLottoCount = 2;
+        List<String> inputManualLottoNumbersString = Arrays.asList("1,2,3,4,5,6", "7,8,9,10,11,12", "13,14,15,16,17,18");
+        Assertions.assertThatThrownBy(() -> ManualLottoService.createManualLotties(manualLottoCount, inputManualLottoNumbersString))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
