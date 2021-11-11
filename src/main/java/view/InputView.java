1302baa75ea58382b9854d2e02edc76dcb0798bc
daehyun0 <dd70296@gmail.com>
2021-11-12 01:56:42 +0900

    feat: 보너스 번호 입력 요청 입출력 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,9 +6,11 @@ import model.Lottos;
 
 public class InputView {
 	private static final String MESSAGE_REQUEST_INPUT_OF_PURCHASE_PRICE = "구입금액을 입력해 주세요.";
-	private static final String MESSAGE_REQUEST_INPUT_OF_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요.";
 	private static final String MESSAGE_REQUEST_EXCEPTION_INPUT_OF_PURCHASE_PRICE = "잘못된 형식의 구매금액입니다.";
+	private static final String MESSAGE_REQUEST_INPUT_OF_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요.";
 	private static final String MESSAGE_REQUEST_EXCEPTION_INPUT_OF_WINNING_NUMBERS = "잘못된 형식의 당첨 번호입니다.";
+	private static final String MESSAGE_REQUEST_INPUT_OF_BONUS_NUMBER = "보너스 볼을 입력해 주세요.";
+	private static final String MESSAGE_REQUEST_EXCEPTION_INPUT_OF_BONUS_NUMBER = "잘못된 형식의 보너스 볼입니다.";
 
 	public void showRequestInputOfPurchasePrice() {
 		System.out.println(MESSAGE_REQUEST_INPUT_OF_PURCHASE_PRICE);
@@ -24,6 +26,14 @@ public class InputView {
 
 	public void showRequestInputExceptionOfWinningNumbers() {
 		System.out.println(MESSAGE_REQUEST_EXCEPTION_INPUT_OF_WINNING_NUMBERS);
+	}
+
+	public void showRequestInputOfBonusNumber() {
+		System.out.println(MESSAGE_REQUEST_INPUT_OF_BONUS_NUMBER);
+	}
+
+	public void showRequestInputExceptionOfBonusNumber() {
+		System.out.println(MESSAGE_REQUEST_EXCEPTION_INPUT_OF_BONUS_NUMBER);
 	}
 
 	public void showResponseInputOfPurchaseLottos(Lottos lottos) {
