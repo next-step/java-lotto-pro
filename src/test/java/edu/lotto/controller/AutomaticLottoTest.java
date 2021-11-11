5a57fee9776a121f8a63f39c231d31a6b1308516
InmookJeong <jeonginmook@gmail.com>
2021-11-11 17:26:27 +0900

    test: user lotto number duplicate check

--- src/test/java/edu/lotto/controller/AutomaticLottoTest.java
+++ src/test/java/edu/lotto/controller/AutomaticLottoTest.java
@@ -4,7 +4,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import edu.lotto.constants.PatternConstants;
 import edu.lotto.utils.NumberUtil;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * 로또(자동) Test Code
@@ -14,13 +18,14 @@ import org.junit.jupiter.api.Test;
 public class AutomaticLottoTest {
 
 	@Test
-	void test() {
-		String s = "1, 2, 3, 4, 5, 6".replaceAll(" ", "");
-		String[] winningNumberArray = s.split(PatternConstants.DEFAULT_SEPARATOR_PATTERN);
-		assertThat(winningNumberArray.length == 6).isTrue();
-		for(String n : winningNumberArray) {
-			assertThat(NumberUtil.isNumber(n)).isTrue();
-			assertThat(NumberUtil.isNumberBetweenOneAndFortyFive(Integer.parseInt(n)));
-		}
+	@DisplayName("사용자가 입력한 로또 번호에 중복값이 없는지 확인")
+	void hasNotDuplicateNumber() {
+		String[] numbers1 = "1,2,3,4,5,6".split(PatternConstants.DEFAULT_SEPARATOR_PATTERN);
+		String[] numbers2 = "1,1,2,3,4,5".split(PatternConstants.DEFAULT_SEPARATOR_PATTERN);
+
+		List<String> numbersList1 = Arrays.asList(numbers1);
+		List<String> numbersList2 = Arrays.asList(numbers2);
+		assertThat(numbersList1.stream().distinct().count()).isEqualTo(numbersList1.size());
+		assertThat(numbersList2.stream().distinct().count()).isNotEqualTo(numbersList2.size());
 	}
 }
