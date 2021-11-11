e2a287188cf20bb40abc50505eb3c4d71a2a606b
shryu <>
2021-11-12 01:16:01 +0900

    feat : 수동 로또 번호 입력 view 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,8 @@
 package lotto.view;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
@@ -13,6 +16,17 @@ public class InputView {
     public static int inputManualLottoCount() {
         System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
         return Integer.parseInt(scanner.nextLine());
+    }
+
+    public static List<String> inputManualLottoNumbers(int manualLottoCount) {
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+        List<String> manualLottoNumbers = new LinkedList<>();
+
+        for (int i = 0; i < manualLottoCount; i++) {
+            manualLottoNumbers.add(scanner.nextLine());
+        }
+
+        return manualLottoNumbers;
     }
 
     public static String inputWinningNumber() {
