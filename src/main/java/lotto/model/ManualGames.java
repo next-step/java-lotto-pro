c22c503d7895a02e708160c908a3852da588262a
신민준 <jxin95@gmail.com>
2021-11-11 16:07:43 +0900

    refactor: 테스트를 위해 클래스 내에서 값을 입력하지 않고, 생성자에서 값을 전달 받을 수 있도록 리팩토링

--- src/main/java/lotto/model/ManualGames.java
+++ src/main/java/lotto/model/ManualGames.java
@@ -1,46 +1,13 @@
 package lotto.model;
 
-import lotto.exception.InvalidInputException;
-import lotto.view.strategy.Input;
-import lotto.view.strategy.InputManualGameNumbers;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class ManualGames {
 
-    private static final String INPUT_MESSAGE = "\n수동으로 구매할 번호를 입력해 주세요.";
+    private List<LottoNumbers> list;
 
-    List<LottoNumbers> list;
-
-    public ManualGames(final int manualGameCount) {
-        list = new ArrayList<>();
-
-        if (manualGameCount <= 0) {
-            return;
-        }
-
-        System.out.println(INPUT_MESSAGE);
-
-        for (int i = 0; i < manualGameCount; i++) {
-            list.add(addManualGame());
-        }
-    }
-
-    /**
-     * 수동게임번호 추가
-     *
-     * @return
-     */
-    private LottoNumbers addManualGame() {
-        try {
-            Input input = new InputManualGameNumbers();
-            String value = input.getValue();
-            return new LottoNumbers(value);
-        } catch (InvalidInputException e) {
-            System.out.println(e.getMessage());
-            return addManualGame();
-        }
+    public ManualGames(final List<LottoNumbers> list) {
+        this.list = list;
     }
 
     public List<LottoNumbers> getList() {
