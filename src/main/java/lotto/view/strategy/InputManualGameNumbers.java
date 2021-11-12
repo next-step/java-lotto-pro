99baec128a7ca6e4cdde95003654395504d5d3e0
신민준 <jxin95@gmail.com>
2021-11-12 22:10:02 +0900

    feat: 멀티라인을 입력할 수 있는 InputMultiLine 클래스를 추가하고, InputManualGameNumbers 클래스에서 구현체를 생성

--- src/main/java/lotto/view/strategy/InputManualGameNumbers.java
+++ src/main/java/lotto/view/strategy/InputManualGameNumbers.java
@@ -1,12 +1,31 @@
 package lotto.view.strategy;
 
-public class InputManualGameNumbers extends InputGameNumbers {
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class InputManualGameNumbers extends InputLottoNumbers implements InputMultiLine {
 
     private static final String MESSAGE = "\n수동으로 구매할 번호를 입력해 주세요.";
 
     @Override
     public String getMessage() {
         return MESSAGE;
+    }
+
+    /**
+     * 멀티라인 입력값 반환
+     *
+     * @param rowCount
+     * @return
+     */
+    @Override
+    public List<String> getMultiValues(int rowCount) {
+        final int firstIndex = 0;
+        return IntStream.range(firstIndex, rowCount)
+                .boxed()
+                .map(index -> this.getValue(index == firstIndex))
+                .collect(Collectors.toList());
     }
 
 }
---
+++ src/main/java/lotto/view/strategy/InputMultiLine.java
@@ -0,0 +1,9 @@
+package lotto.view.strategy;
+
+import java.util.List;
+
+public interface InputMultiLine {
+
+    List<String> getMultiValues(int rowCount);
+
+}
