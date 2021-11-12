63642f10022926d1b2ea781ded41712e5e7b4cc6
JiHunHong <hongji3354@gmail.com>
2021-11-13 00:39:09 +0900

    refactor : Rank PrizeMoney wrapping

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -4,18 +4,18 @@ import java.util.Arrays;
 
 public enum Rank {
 
-    FIRST_PLACE(6, 2_000_000_000, false),
-    SECOND_PLACE(5, 1_500_000, false),
-    BONUS_SECOND_PLACE(5, 30_000_000, true),
-    THIRD_PLACE(4, 50_000, false),
-    FOURTH_PLACE(3, 5_000, false),
-    LOSER(0 ,0, false);
+    FIRST_PLACE(6, new PrizeMoney(2_000_000_000), false),
+    SECOND_PLACE(5, new PrizeMoney(1_500_000), false),
+    BONUS_SECOND_PLACE(5, new PrizeMoney(30_000_000), true),
+    THIRD_PLACE(4, new PrizeMoney(50_000), false),
+    FOURTH_PLACE(3, new PrizeMoney(5_000), false),
+    LOSER(0 ,new PrizeMoney(0), false);
 
     private int winningNumberCount;
-    private int prizeMoney;
+    private PrizeMoney prizeMoney;
     private boolean isBonus;
 
-    Rank(int winningNumberCount, int prizeMoney, boolean isBonus) {
+    Rank(int winningNumberCount, PrizeMoney prizeMoney, boolean isBonus) {
         this.winningNumberCount = winningNumberCount;
         this.prizeMoney = prizeMoney;
         this.isBonus = isBonus;
@@ -33,10 +33,9 @@ public enum Rank {
         return this != Rank.LOSER;
     }
 
-    public long prizeMoneyCalculation(int winningCount) {
-        return (long) this.prizeMoney * winningCount;
+    public PrizeMoney getPrizeMoney() {
+        return prizeMoney;
     }
-
 }
 
 
