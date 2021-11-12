f4104b35304756ed71a91e762c5309b49d1338d1
changjin.park <pcjin118@gmail.com>
2021-11-13 02:11:27 +0900

    feat: 수동 로또 번호 생성

--- src/main/java/lotto/LotteryNumbers.java
+++ src/main/java/lotto/LotteryNumbers.java
@@ -1,19 +1,46 @@
 package lotto;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class LotteryNumbers {
+    public static final String LOTTO_NUMBER_NEGATIVE_MESSAGE = "로또 번호가 음수일 수 없습니다.";
+    public static final String LOTTO_NUMBER_DUPLICATE_MESSAGE = "로또 번호에 중복된 번호가 있으면 안됩니다.";
+
     private final List<Integer> lottoNumbers;
 
     public LotteryNumbers(List<Integer> lottoNumbers) {
+        validateDuplicate(lottoNumbers);
+        validatePositive(lottoNumbers);
         Collections.sort(lottoNumbers);
         this.lottoNumbers = lottoNumbers;
     }
 
+    private void validatePositive(List<Integer> lottoNumbers) {
+        for (int lottoNo : lottoNumbers) {
+            isNegative(lottoNo);
+        }
+    }
+
+    private void isNegative(int lottoNo) {
+        if (lottoNo < 0) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_NEGATIVE_MESSAGE);
+        }
+    }
+
+    private void validateDuplicate(List<Integer> lottoNumbers) {
+        Set<Integer> set = new HashSet<>(lottoNumbers);
+        if (set.size() != lottoNumbers.size()) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_DUPLICATE_MESSAGE);
+        }
+    }
+
     public static LotteryNumbers createAutoLotteryNumber(List<Integer> autoPickedLottoNumber) {
         return new LotteryNumbers(autoPickedLottoNumber);
+    }
+
+    public static LotteryNumbers createManualLotteryNumber(String[] enterManualLotteryNumbers) {
+        return new LotteryNumbers(Arrays.stream(enterManualLotteryNumbers).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList()));
     }
 
     public int size() {
--- src/test/java/lotto/LotteryNumbersTest.java
+++ src/test/java/lotto/LotteryNumbersTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LotteryNumbersTest {
 
@@ -47,5 +48,24 @@ public class LotteryNumbersTest {
     void contains() {
         assertThat(lotteryNumbers.contains(1)).isTrue();
         assertThat(lotteryNumbers.contains(12)).isFalse();
+    }
+
+    @DisplayName("수동 로또 번호 생성")
+    @Test
+    void createManualLotteryNumber() {
+        LotteryNumbers manualLotteryNumber = LotteryNumbers.createManualLotteryNumber(new String[]{"1", "2", "3", "4", "5", "6"});
+        assertThat(manualLotteryNumber).isEqualTo(LotteryNumbers.createManualLotteryNumber(new String[]{"1", "2", "3", "4", "5", "6"}));
+    }
+
+    @DisplayName("수동 로또 번호에 중복 에러 발생")
+    @Test
+    void validateDuplicate() {
+        assertThatThrownBy(() -> LotteryNumbers.createManualLotteryNumber(new String[]{"1", "2", "3", "4", "5", "1"})).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("수동 로또 번호에 음수 에러 발생")
+    @Test
+    void validatePositive() {
+        assertThatThrownBy(() -> LotteryNumbers.createManualLotteryNumber(new String[]{"1", "2", "3", "4", "5", "-1"})).isInstanceOf(IllegalArgumentException.class);
     }
 }
