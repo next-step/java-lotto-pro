c272b90e63520ac16b64cf40b2d7cdd8ccf98227
lovethefeel <lovethefeel@gmail.com>
2021-11-13 03:48:14 +0900

    refactor: 한글입력 예외처리 추가

--- src/main/java/lotto/domain/Payment.java
+++ src/main/java/lotto/domain/Payment.java
@@ -5,25 +5,41 @@ import lotto.domain.common.Constant;
 public class Payment {
 
     private static final String THE_PRICE_OF_A_LOTTERY_TICKET_IS_1_000_WON = "The price of a lottery ticket is 1,000 won.";
+    private static final String ONLY_NUMBERS_CAN_BE_ENTERED = "Only numbers can be entered.";
+    private static final String THE_PURCHASE_AMOUNT_IS_INSUFFICIENT = "The purchase amount is insufficient.";
     private final int payment;
 
     public Payment(final int payment) {
-        validate(payment);
+        validateAmount(payment);
         this.payment = payment;
     }
 
     public static Payment from(final int payment) {
-        validate(payment);
         return new Payment(payment);
     }
 
-    private static void validate(final int payment) {
+    public static Payment from(final String payment) {
+        validateCharacter(payment);
+        return new Payment(Integer.parseInt(payment));
+    }
+
+    private static void validateCharacter(String payment) {
+        if( !payment.chars().allMatch( Character::isDigit )) {
+            throw new IllegalArgumentException(ONLY_NUMBERS_CAN_BE_ENTERED);
+        }
+    }
+
+    private static void validateAmount(final int payment) {
         if( payment < Constant.LOTTO_PURCHASE_PRICE) {
             throw new IllegalArgumentException(THE_PRICE_OF_A_LOTTERY_TICKET_IS_1_000_WON);
         }
     }
 
     public int getPurchaseCount() {
-        return this.payment / Constant.LOTTO_PURCHASE_PRICE;
+        return getAutoPurchaseCount(0);
+    }
+
+    public int getAutoPurchaseCount(final int manualPurchaseCount) {
+        return this.payment / Constant.LOTTO_PURCHASE_PRICE - manualPurchaseCount;
     }
 }
