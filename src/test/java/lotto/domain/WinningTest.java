63f5c89f8ac91bc75d417dee60007d5e390c1014
JiHunHong <hongji3354@gmail.com>
2021-11-12 23:33:47 +0900

    test : 당첨번호와 로또번호가 3~6개 사이 일치 일 시 당첨 테스트 제
    해당 테스트는 기능 자체가 아닌 기능의 일부이므로 제거

--- src/test/java/lotto/domain/WinningTest.java
+++ src/test/java/lotto/domain/WinningTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Collectors;
@@ -31,15 +30,6 @@ class WinningTest {
         assertThatThrownBy( () -> {
             new WinningLotto(winningNumber, bonusNumber);
         }).isInstanceOf(BonusNumberDuplicateException.class);
-    }
-
-    @DisplayName("당첨번호와 로또번호가 3~6개 사이 일치 일 시 당첨")
-    @ParameterizedTest
-    @CsvSource(value = {"0:false:true", "0:false:false", "1:false:false", "2:false:false", "3:true:false", "4:true:false", "5:true:false", "6:true:false", "7:false:false"}, delimiter = ':')
-    void prizeTest(int winningNumberSameCount, boolean expectedPrize, boolean expectedBonus) {
-        Rank rank = Rank.of(winningNumberSameCount, expectedBonus);
-        boolean prize = rank.isPrize();
-        assertThat(prize).isEqualTo(expectedPrize);
     }
 
     @DisplayName("번호 일치 개수 별 등수")
