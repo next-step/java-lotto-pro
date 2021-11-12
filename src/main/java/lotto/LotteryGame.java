e80b11028dabb766226ecea7e1262aab1ea15498
changjin.park <pcjin118@gmail.com>
2021-11-13 02:41:41 +0900

    feat: 수동 로또 티켓 수와 자동 로또 티켓 수 계산

--- src/main/java/lotto/LotteryGame.java
+++ src/main/java/lotto/LotteryGame.java
@@ -6,9 +6,18 @@ import lotto.view.ResultView;
 public class LotteryGame {
     public static void main(String[] args) {
         int numberOfTicket = InputView.payMoney();
+        int numberOfManualTickets = InputView.enterNumberOfManualLotteryTickets();
+        int numberOfAutoTickets = numberOfTicket - numberOfManualTickets;
+        InputView.enterManualLotteryNumbersMessage();
         PurchaseLotteryTicket purchaseLotteryTicket = new PurchaseLotteryTicket();
-        for (int i = 0; i < numberOfTicket; i++) {
+        for (int i = 0; i < numberOfManualTickets; i++) {
+            purchaseLotteryTicket.add(LotteryNumbers.createManualLotteryNumber(InputView.enterManualLotteryNumbers()));
+        }
+        for (int i = 0; i < numberOfAutoTickets; i++) {
             purchaseLotteryTicket.add(LotteryTicketFactory.createLotteryTicket());
+        }
+        InputView.printLotteryTicketType(numberOfManualTickets, numberOfAutoTickets);
+        for (int i = 0; i < numberOfTicket; i++) {
             InputView.printLottoTicketNumber(purchaseLotteryTicket.get(i).getLotteryNumber());
         }
         WinningNumber winningNumber = WinningNumber.createWinningNumber(InputView.enterWinningNumber(), InputView.enterBonusNumber());
