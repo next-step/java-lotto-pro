338fcd0e3b0c00bbe27bad90ee986f9fd7fbc65b
shryu <>
2021-11-13 13:19:42 +0900

    feat : 로또 객체 당첨통계 기능 구현

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -2,8 +2,10 @@ package lotto.domain;
 
 import lotto.service.CreateLottoStrategy;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class Lottos {
     private List<Lotto> lottos = new LinkedList<>();
@@ -20,5 +22,16 @@ public class Lottos {
 
     public List<Lotto> getLottos() {
         return lottos;
+    }
+
+    public Map<LottoPrize, Integer> createLottosResult(WinningLottoNumbers winningLottoNumbers) {
+        Map<LottoPrize, Integer> lottoResultMap = new HashMap<>();
+        for (Lotto lotto : lottos) {
+            LottoPrize lottoPrize = lotto.compareWinningNumbers(winningLottoNumbers);
+            int lottoWinningCount = lottoResultMap.getOrDefault(lottoPrize, 0);
+            lottoResultMap.put(lottoPrize, ++lottoWinningCount);
+        }
+        return lottoResultMap;
+
     }
 }
--- src/test/java/lotto/LottosTest.java
+++ src/test/java/lotto/LottosTest.java
@@ -1,11 +1,13 @@
 package lotto;
 
 import lotto.domain.*;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottosTest {
     Lottos lottos;
@@ -20,8 +22,16 @@ public class LottosTest {
     @Test
     void 자동로또_생성() {
         lottos.createAutoLottos(() -> Arrays.asList("3", "4", "5", "6", "7", "8"), 1);
-        Assertions.assertThat(lottos.getLottos()).contains(new Lotto(new LottoNumbers(Arrays.asList("1", "2", "3", "4", "5", "6"))),
+        assertThat(lottos.getLottos()).contains(new Lotto(new LottoNumbers(Arrays.asList("1", "2", "3", "4", "5", "6"))),
                                                             new Lotto(new LottoNumbers(Arrays.asList("3", "4", "5", "6", "7", "8"))));
+    }
+
+    @Test
+    void 당첨_통계() {
+        lottos.createAutoLottos(() -> Arrays.asList("3", "4", "5", "6", "7", "8"), 1);
+        Map<LottoPrize, Integer> result =  lottos.createLottosResult(
+                                        new WinningLottoNumbers(Arrays.asList("1", "2", "3", "4", "5", "6"), new BonusNumber("7")));
+        assertThat(result).containsKeys(LottoPrize.FIRST, LottoPrize.FOURTH);
     }
 
 }
