5d75e25d689bbd9b42ddfad7268e050f79a29e7e
shryu <>
2021-11-13 12:41:28 +0900

    feat : ManualLottosCount 객체 생성

---
+++ src/main/java/lotto/domain/ManualLottosCount.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+public class ManualLottosCount {
+    private static final int MANUAL_LOTTO_COUNT_MINIMUM = 0;
+    private final int manualLottoCount;
+
+    public ManualLottosCount(int totalLottoCount, int manualLottoCount) {
+        validateManualLottoCount(totalLottoCount, manualLottoCount);
+        this.manualLottoCount = manualLottoCount;
+    }
+
+    private void validateManualLottoCount(int totalLottoCount, int manualLottoCount) {
+        if (totalLottoCount < manualLottoCount) {
+            throw new IllegalArgumentException("수동 로또 개수는 총 구매한 로또 개수를 넘으면 안됩니다.");
+        }
+        if (manualLottoCount < MANUAL_LOTTO_COUNT_MINIMUM) {
+            throw new IllegalArgumentException("수동 로또 개수는 0 이상이어야 합니다.");
+        }
+    }
+}
---
+++ src/test/java/lotto/ManualLottosTest.java
@@ -0,0 +1,16 @@
+package lotto;
+
+import lotto.domain.ManualLottosCount;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ManualLottosTest {
+    @Test
+    void 수동로또_개수_유효검사() {
+        int totalLottoCount = 3;
+        int manualLottoCount = 5;
+        assertThatThrownBy(() ->
+                new ManualLottosCount(totalLottoCount, manualLottoCount)).isInstanceOf(IllegalArgumentException.class);
+    }
+}
