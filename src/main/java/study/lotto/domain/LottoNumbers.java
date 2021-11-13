765e8b72118aab7f9e49c35d750062f1ac1011ad
sungwoo <ssw890418@gmail.com>
2021-11-13 14:07:53 +0900

    refactor:match 메소드가 보너스 볼까지 고려하여 Rank정보를 업데이트 할수 있도록 기능 수정

--- src/main/java/study/lotto/domain/LottoNumbers.java
+++ src/main/java/study/lotto/domain/LottoNumbers.java
@@ -8,26 +8,32 @@ import java.util.stream.IntStream;
 
 public class LottoNumbers {
 
+    private static final int MISS = 0;
+    private static final int MINIMUM_MATCH_COUNT = 3;
+    private static final int SECOND_RANK_OPPORTUNITY_COUNT = 5;
+
     private static final int MIN_LOTTO_NUMBER = 1;
     private static final int MAX_LOTTO_NUMBER = 45;
     private static final int MIN_LOTTO_COUNT = 0;
     private static final int MAX_LOTTO_COUNT = 6;
+    private static final int INITIAL_MATCH_COUNT = 0;
+    private static final boolean INITIAL_MATCH_BONUS = false;
 
-    private final List<LottoNumber> list;
+    private final List<LottoNumber> lottoNumbers;
 
     public LottoNumbers() {
         List<LottoNumber> totalLottoNumber = generateTotalLottoNumber();
         Collections.shuffle(totalLottoNumber);
         List<LottoNumber> lottoNumbers = totalLottoNumber.subList(MIN_LOTTO_COUNT, MAX_LOTTO_COUNT);
         Collections.sort(lottoNumbers);
-        this.list = lottoNumbers;
+        this.lottoNumbers = lottoNumbers;
     }
 
     public LottoNumbers(String readLastLottoNumbers) {
         String[] split = readLastLottoNumbers.split(",");
-        list = new ArrayList<>();
+        lottoNumbers = new ArrayList<>();
         for(String s : split) {
-            list.add(new LottoNumber(Integer.parseInt(s)));
+            lottoNumbers.add(new LottoNumber(Integer.parseInt(s)));
         }
     }
 
@@ -40,7 +46,7 @@ public class LottoNumbers {
     public String makeLottoNumberPrintFormat() {
         StringBuilder sb = new StringBuilder();
         sb.append('[');
-        for(LottoNumber lottoNumber : list) {
+        for(LottoNumber lottoNumber : lottoNumbers) {
             sb.append(lottoNumber.getNumber() +", ");
         }
         sb.delete(sb.lastIndexOf(","), sb.length());
@@ -48,17 +54,41 @@ public class LottoNumbers {
         return sb.toString();
     }
 
-    public int getMatchCount(LottoNumbers group) {
-        int count = 0;
-        for(LottoNumber lottoNumber : group.getLottoNumbers()){
-            if(list.contains(lottoNumber)) {
-                count++;
-            }
+    public void match(LottoNumbers thisWeekLottoNumbers, LottoNumber bonusBall) {
+
+        Match match = new Match(INITIAL_MATCH_COUNT, INITIAL_MATCH_BONUS);
+
+        for(LottoNumber lottoNumber : thisWeekLottoNumbers.getLottoNumbers()){
+            matchAndIncreaseCount(lottoNumber, match);
         }
-        return count;
+        checkBonusBall(match, bonusBall);
+        validateMatchCount(match);
+        Rank.valueOf(match.getCount(), match.getMatchBonus()).increaseCorrect();
+    }
+
+    private void matchAndIncreaseCount(LottoNumber lottoNumber, Match match) {
+        if(isContain(lottoNumber)) {
+            match.increaseCount();
+        }
+    }
+
+    private boolean isContain(LottoNumber lottoNumber) {
+        return lottoNumbers.contains(lottoNumber);
+    }
+
+    private void checkBonusBall(Match match, LottoNumber bonusBall) {
+        if(match.getCount() == SECOND_RANK_OPPORTUNITY_COUNT) {
+            match.setMatchBonus(isContain(bonusBall));
+        }
+    }
+
+    private void validateMatchCount(Match match) {
+        if(match.getCount() < MINIMUM_MATCH_COUNT) {
+            match.setCount(MISS);
+        }
     }
 
     protected List<LottoNumber> getLottoNumbers() {
-        return this.list;
+        return this.lottoNumbers;
     }
 }
