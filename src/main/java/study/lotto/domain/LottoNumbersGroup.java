3291f2cd4ed8ba3832c7f5382a334f3c86825cae
sungwoo <ssw890418@gmail.com>
2021-11-13 14:14:30 +0900

    refactor:로또번호와 관련된 변수들을 한군데 모아서 사용할 수 있도록 멤버변수 추가

--- src/main/java/study/lotto/domain/LottoNumbersGroup.java
+++ src/main/java/study/lotto/domain/LottoNumbersGroup.java
@@ -4,13 +4,38 @@ import java.util.List;
 
 public class LottoNumbersGroup {
 
-    private final List<LottoNumbers> lottoNumbersList;
+    private final List<LottoNumbers> lottoNumbersGroup;
 
-    public LottoNumbersGroup(List<LottoNumbers> lottoNumbersList) {
-        this.lottoNumbersList = lottoNumbersList;
+    private LottoNumbers lastLottoNumbers;
+    private LottoNumber bonusBall;
+
+    public LottoNumbersGroup(List<LottoNumbers> lottoNumbersGroup) {
+        this.lottoNumbersGroup = lottoNumbersGroup;
+    }
+
+    public LottoNumbersGroup(List<LottoNumbers> lottoNumbersGroup, LottoNumbers lastLottoNumbers, LottoNumber bonusBall) {
+        this.lottoNumbersGroup = lottoNumbersGroup;
+        this.lastLottoNumbers = lastLottoNumbers;
+        this.bonusBall = bonusBall;
     }
 
     public List<LottoNumbers> getLottoNumbersList() {
-        return lottoNumbersList;
+        return lottoNumbersGroup;
+    }
+
+    public void setLastLastLottoNumbers(LottoNumbers lastLottoNumbers) {
+        this.lastLottoNumbers = lastLottoNumbers;
+    }
+
+    public void setBonusBall(LottoNumber bonusBall) {
+        this.bonusBall = bonusBall;
+    }
+
+    public LottoNumbers getLastLottoNumbers() {
+        return this.lastLottoNumbers;
+    }
+
+    public LottoNumber getBonusBall() {
+        return this.bonusBall;
     }
 }
