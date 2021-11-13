a41946e8c9057e3fdda699ad19c845b6d837a179
shryu <>
2021-11-13 12:10:47 +0900

    feat : Lottos 객체 수동 로또 생성 기능 구현

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,10 +1,20 @@
 package lotto.domain;
 
+import lotto.utils.StringUtils;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class Lottos {
     private static final int MANUAL_LOTTO_COUNT_MINIMUM = 0;
+    private final int totalLottoCount;
+    private final int manualLottoCount;
+    private List<Lotto> manualLottos = new LinkedList<>();
 
     public Lottos(int totalLottoCount, int manualLottoCount) {
         validateManualLottoCount(totalLottoCount, manualLottoCount);
+        this.totalLottoCount = totalLottoCount;
+        this.manualLottoCount = manualLottoCount;
     }
 
     private void validateManualLottoCount(int totalLottoCount, int manualLottoCount) {
@@ -14,5 +24,15 @@ public class Lottos {
         if (manualLottoCount < MANUAL_LOTTO_COUNT_MINIMUM) {
             throw new IllegalArgumentException("수동 로또 개수는 0 이상이어야 합니다.");
         }
+    }
+
+    public void createManualLottos(List<String> inputManualLottoNumbersString) {
+        for (int i = 0; i < manualLottoCount; i++) {
+            manualLottos.add(new Lotto(new LottoNumbers(StringUtils.separate(inputManualLottoNumbersString.get(i)))));
+        }
+    }
+
+    public List<Lotto> getManualLotts() {
+        return manualLottos;
     }
 }
--- src/test/java/lotto/LottosTest.java
+++ src/test/java/lotto/LottosTest.java
@@ -1,15 +1,33 @@
 package lotto;
 
+import lotto.domain.Lotto;
+import lotto.domain.LottoNumbers;
 import lotto.domain.Lottos;
+import lotto.service.ManualLottoService;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class LottosTest {
+    Lottos lottos;
+    List<String> inputManualLottoNumbersString;
+
+    @BeforeEach
+    void init() {
+        lottos = new Lottos(2, 1);
+        inputManualLottoNumbersString = Arrays.asList("1,2,3,4,5,6");
+    }
+
     @Test
     void 수동로또_개수_유효검사() {
         int totalLottoCount = 3;
         int manualLottoCount = 5;
-        Assertions.assertThatThrownBy(() ->
+        assertThatThrownBy(() ->
                 new Lottos(totalLottoCount, manualLottoCount)).isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -17,7 +35,13 @@ public class LottosTest {
     void 수동로또_개수_0미만_오류() {
         int totalLottoCount = 3;
         int manualLottoCount = -1;
-        Assertions.assertThatThrownBy(() ->
+        assertThatThrownBy(() ->
                 new Lottos(totalLottoCount, manualLottoCount)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 수동로또_생성() {
+        lottos.createManualLottos(inputManualLottoNumbersString);
+        assertThat(lottos.getManualLotts()).contains(new Lotto(new LottoNumbers(Arrays.asList("1", "2", "3", "4", "5", "6"))));
     }
 }
