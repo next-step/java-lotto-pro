1e9892ff8e10f68227c54fd31ed23991f0793612
wook <mdir2@naver.com>
2021-11-13 12:31:45 +0900

    test(review-step3-lotto): 불필요한 `@ParameterizedTest` 제거

--- src/test/java/step3/domain/LottoTest.java
+++ src/test/java/step3/domain/LottoTest.java
@@ -1,24 +1,20 @@
 package step3.domain;
 
-import static helper.Constants.*;
 import static org.assertj.core.api.Assertions.*;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.Test;
 
 @DisplayName("로또 테스트")
 class LottoTest {
 
-    @ParameterizedTest(name = DISPLAY_NAME)
-    @CsvSource(value = "3,2,42,12,40,11")
-    void _6개의_숫자를_조합해_로또를_만들_수_있다(final int one, final int two, final int three,
-        final int four, final int five, final int six) {
+    @Test
+    void _6개의_숫자를_조합해_로또를_만들_수_있다() {
         //given
         final List<LottoNumber> lottoNumbers = Arrays.asList(
-            new LottoNumber(one), new LottoNumber(two), new LottoNumber(three),
-            new LottoNumber(four), new LottoNumber(five), new LottoNumber(six)
+            new LottoNumber(3), new LottoNumber(2), new LottoNumber(42),
+            new LottoNumber(12), new LottoNumber(40), new LottoNumber(11)
         );
 
         //when
@@ -28,14 +24,12 @@ class LottoTest {
         assertThat(lotto).isNotNull().isEqualTo(new Lotto(lottoNumbers));
     }
 
-    @ParameterizedTest(name = DISPLAY_NAME)
-    @CsvSource(value = "3,2,42,12,40,11,7")
-    void _6개의_숫자이상으로_조합해_로또를_만드려고_하면_예외가발생한다(final int one, final int two, final int three,
-        final int four, final int five, final int six, final int seven) {
+    @Test
+    void _6개의_숫자이상으로_조합해_로또를_만드려고_하면_예외가발생한다() {
         //given
         final List<LottoNumber> lottoNumbers = Arrays.asList(
-            new LottoNumber(one), new LottoNumber(two), new LottoNumber(three),
-            new LottoNumber(four), new LottoNumber(five), new LottoNumber(six), new LottoNumber(seven)
+            new LottoNumber(3), new LottoNumber(2), new LottoNumber(42),
+            new LottoNumber(12), new LottoNumber(40), new LottoNumber(11)
         );
 
         //when
