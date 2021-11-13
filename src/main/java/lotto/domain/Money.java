7ba007211760666996c40e399a48886104e3fb1d
codingknowjam <hanwoodww@gmail.com>
2021-11-13 17:46:08 +0900

    refactor: Money 클래스 원시값 변경 int -> BigDecimal

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,15 +1,16 @@
 package lotto.domain;
 
+import java.math.BigDecimal;
 import java.util.Objects;
 
 public class Money {
 	private static final int MIN_MONEY = 0;
 	private static final String ERROR_MONEY_BOUNDARY = "돈은 0원보다 작을 수 없습니다.";
-	private final int money;
+	private final BigDecimal money;
 
 	public Money(final int money) {
 		validateMoney(money);
-		this.money = money;
+		this.money = new BigDecimal(String.valueOf(money));
 	}
 
 	private void validateMoney(int money) {
@@ -23,31 +24,31 @@ public class Money {
 	}
 
 	private int divide(Money lottoPrice) {
-		return money / lottoPrice.money;
+		return money.divide(lottoPrice.money, 0).intValue();
 	}
 
-	public int multiply(int size) {
-		return money * size;
+	public int multiply(int value) {
+		return money.multiply(new BigDecimal(String.valueOf(value))).intValue();
 	}
 
 	public boolean isLess(int purchaseAmount) {
-		return purchaseAmount < money;
+		return money.compareTo(new BigDecimal(purchaseAmount)) == 1;
 	}
 
 	public int value() {
-		return money;
+		return money.intValue();
 	}
 
 	public Money getPurchaseAmount(int inputQuantity) {
-		return new Money(money * inputQuantity);
+		return new Money(multiply(inputQuantity));
 	}
 
 	public boolean isGreater(Money purchaseAmount) {
-		return money < purchaseAmount.money;
+		return money.compareTo(purchaseAmount.money) == -1;
 	}
 
 	public Money deduct(Money inputPurchaseAmount) {
-		return new Money(money - inputPurchaseAmount.money);
+		return new Money(money.subtract(inputPurchaseAmount.money).intValue());
 	}
 
 	@Override
@@ -57,13 +58,11 @@ public class Money {
 		if (o == null || getClass() != o.getClass())
 			return false;
 		Money money1 = (Money)o;
-		return money == money1.money;
+		return Objects.equals(money, money1.money);
 	}
 
 	@Override
 	public int hashCode() {
 		return Objects.hash(money);
 	}
-
-
 }
