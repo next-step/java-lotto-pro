8ed890b7059fcd28cf612f1b30cadf22df097f3b
wook <mdir2@naver.com>
2021-11-13 12:39:47 +0900

    test(review-step3-lotto): 당첨 로또와 비교해서 맞은 숫자개수를 얻을 수 있다

--- src/test/java/step3/domain/LottoTest.java
+++ src/test/java/step3/domain/LottoTest.java
@@ -35,4 +35,28 @@ class LottoTest {
         //when
         assertThatIllegalArgumentException().isThrownBy(() -> new Lotto(lottoNumbers));
     }
+
+    @Test
+    void 당첨_로또와_비교해서_맞은_숫자개수를_얻을_수_있다() {
+        //given
+        final LottoNumber sameLottoNumberOne = new LottoNumber(3);
+        final LottoNumber sameLottoNumberTwo = new LottoNumber(12);
+        final LottoNumber sameLottoNumberThree = new LottoNumber(11);
+
+        final Lotto purchasedLotto = new Lotto(Arrays.asList(
+            sameLottoNumberOne, new LottoNumber(1), new LottoNumber(40),
+            sameLottoNumberTwo, new LottoNumber(40), sameLottoNumberThree
+        ));
+
+        final Lotto winningLotto = new Lotto(Arrays.asList(
+            sameLottoNumberOne, new LottoNumber(2), new LottoNumber(42),
+            sameLottoNumberTwo, new LottoNumber(43), sameLottoNumberThree
+        ));
+
+        //when
+        final int check = purchasedLotto.check(winningLotto);
+
+        //then
+        assertThat(check).isEqualTo(3);
+    }
 }
