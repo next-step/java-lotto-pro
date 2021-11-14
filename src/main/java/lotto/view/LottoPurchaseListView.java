4367bf245148bf4dafad9113e18fbf5abad653bc
JiHunHong <hongji3354@gmail.com>
2021-11-14 20:42:36 +0900

    feat : 수동/자동으로 몆 장 구매 했는지 출력

--- src/main/java/lotto/view/LottoPurchaseListView.java
+++ src/main/java/lotto/view/LottoPurchaseListView.java
@@ -1,5 +1,6 @@
 package lotto.view;
 
+import lotto.domain.LottoManual;
 import lotto.domain.Lottos;
 
 public class LottoPurchaseListView {
@@ -7,8 +8,8 @@ public class LottoPurchaseListView {
     private LottoPurchaseListView() {
     }
 
-    public static void print(Lottos lottos) {
-        System.out.print(lottos.purchaseNumber() + "개를 구매했습니다.");
+    public static void print(Lottos lottos, LottoManual lottoManual) {
+        System.out.printf("수동으로 %d장, 자동으로 %d개를 구매했습니다.", lottoManual.getPurchaseCount(), lottos.purchaseNumber() - lottoManual.getPurchaseCount());
         System.out.println();
 
         lottos.purchaseList()
