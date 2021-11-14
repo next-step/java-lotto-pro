78bf6fee3df4506a138f94e7125db6b074321271
shryu <>
2021-11-15 03:03:52 +0900

    refactor : service 객체 수정

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -9,20 +9,19 @@ import java.util.Map;
 
 public class LottoService {
     public static void play() {
-        int boughtMoney = new BoughtMoney(InputView.inputBoughtMoney()).getBoughtMoney();
-        int totalCount = LottoServiceCalculator.getLottoCount(boughtMoney);
-        ManualLottos manualLottos = new ManualLottos(new ManualLottosCount(totalCount, InputView.inputManualLottoCount()));
-        int autoLottoCount = totalCount - manualLottos.getManualLottosCount();
-        manualLottos.createManualLottos(InputView.inputManualLottoNumbers(manualLottos.getManualLottosCount()));
+        BoughtMoney boughtMoney = new BoughtMoney(InputView.inputBoughtMoney());
+        LottosCount lottosCount = new LottosCount(boughtMoney.getLottoCount(), InputView.inputManualLottoCount());
+        ManualLottos manualLottos = new ManualLottos();
+        manualLottos.createManualLottos(InputView.inputManualLottoNumbers(lottosCount.getManualLottoCount()));
         Lottos lottos = new Lottos(manualLottos);
-        lottos.createAutoLottos(new ShuffledLottoNumbersStrategy(), totalCount - manualLottos.getManualLottosCount());
+        lottos.createAutoLottos(new ShuffledLottoNumbersStrategy(), lottosCount.getAutoLottoCount());
 
-        ResultView.printLottoCount(manualLottos.getManualLottosCount(), autoLottoCount);
+        ResultView.printLottoCount(lottosCount);
         ResultView.printLottoNumbers(lottos.getLottos());
         WinningLottoNumbers winningLottoNumbers = new WinningLottoNumbers(StringUtils.separate(InputView.inputWinningNumber()),
                                                                                     new BonusNumber(InputView.inputBonusNumber()));
-        Map<LottoPrize, Integer> lottoResultMap = lottos.createLottosResult(winningLottoNumbers);
+        Map<LottoPrize, Integer> lottoResultMap = lottos.getWinningResult(winningLottoNumbers);
         ResultView.printWinningStatistics(lottoResultMap);
-        ResultView.printProfitRate(LottoServiceCalculator.calculateProfitRate(lottoResultMap, boughtMoney));
+        ResultView.printProfitRate(lottos.calculateProfitRate(boughtMoney.getBoughtMoney()));
     }
 }
