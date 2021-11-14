c54ed211a1ab61e3e0ee83d6cdcd4cb412fdd82f
shryu <>
2021-11-15 02:33:51 +0900

    feat : 당첨 결과 map 객체 생성

---
+++ src/main/java/lotto/domain/WinningResult.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WinningResult {
+    private Map<LottoPrize, Integer> winningResultMap;
+
+    public WinningResult(Map<LottoPrize, Integer> winningResultMap) {
+        this.winningResultMap = winningResultMap;
+    }
+
+    public int calculateWinningMoney() {
+        int winningMoney = 0;
+        for (LottoPrize lottoPrize : winningResultMap.keySet()) {
+            winningMoney += lottoPrize.getMoney() * winningResultMap.get(lottoPrize);
+        }
+        return winningMoney;
+    }
+
+    public Map<LottoPrize, Integer> getValue() {
+        return winningResultMap;
+    }
+}
