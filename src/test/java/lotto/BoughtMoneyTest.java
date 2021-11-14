3ce2bc9c15b7ca9e523325f32d8a18dea81396e9
shryu <>
2021-11-15 02:46:48 +0900

    test : 수익률 계산 테스트 추가

--- src/test/java/lotto/BoughtMoneyTest.java
+++ src/test/java/lotto/BoughtMoneyTest.java
@@ -4,10 +4,17 @@ import lotto.domain.BoughtMoney;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class BoughtMoneyTest {
     @Test
     void 구매금액_0원_미만의금액_입력() {
         Assertions.assertThatThrownBy(() ->
                 new BoughtMoney(-1000)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 로또_개수_계산() {
+        assertThat(new BoughtMoney(14000).getLottoCount()).isEqualTo(14);
     }
 }
