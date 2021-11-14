3df0ca18d5e0ca194dfc13205cc6f71b2e28701e
shryu <>
2021-11-15 03:02:27 +0900

    refactor : LottosCount 프로퍼티 삭제

--- src/main/java/lotto/domain/ManualLottos.java
+++ src/main/java/lotto/domain/ManualLottos.java
@@ -6,24 +6,15 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class ManualLottos {
-    private final ManualLottosCount manualLottosCount;
     private List<Lotto> manualLottos = new LinkedList<>();
 
-    public ManualLottos(ManualLottosCount manualLottosCount) {
-        this.manualLottosCount = manualLottosCount;
-    }
-
     public void createManualLottos(List<String> inputManualLottoNumbersString) {
-        for (int i = 0; i < manualLottosCount.getValue(); i++) {
+        for (int i = 0; i < inputManualLottoNumbersString.size(); i++) {
             manualLottos.add(new Lotto(new LottoNumbers(StringUtils.separate(inputManualLottoNumbersString.get(i)))));
         }
     }
 
     public List<Lotto> getManualLottos() {
         return manualLottos;
-    }
-
-    public int getManualLottosCount() {
-        return manualLottosCount.getValue();
     }
 }
