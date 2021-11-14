1e964316306a8baf52a6c64647ea0b8e6a343306
shryu <>
2021-11-15 02:47:11 +0900

    test : 수익률 계산 테스트 추가

--- src/test/java/lotto/LottosTest.java
+++ src/test/java/lotto/LottosTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -29,9 +30,17 @@ public class LottosTest {
     @Test
     void 당첨_통계() {
         lottos.createAutoLottos(() -> Arrays.asList("3", "4", "5", "6", "7", "8"), 1);
-        Map<LottoPrize, Integer> result =  lottos.createLottosResult(
+        Map<LottoPrize, Integer> result =  lottos.getWinningResult(
                                         new WinningLottoNumbers(Arrays.asList("1", "2", "3", "4", "5", "6"), new BonusNumber("7")));
         assertThat(result).containsKeys(LottoPrize.FIRST, LottoPrize.FOURTH);
     }
 
+    @Test
+    void 총_수익률_계산() {
+        Map<LottoPrize, Integer> lottoResultMap = new HashMap<>();
+        lottoResultMap.put(LottoPrize.FIFTH, 1);
+        lottos.getWinningResult(
+                new WinningLottoNumbers(Arrays.asList("4", "5", "6", "7", "8", "9"), new BonusNumber("10")));
+        assertThat(lottos.calculateProfitRate(14000)).isEqualTo(0.35);
+    }
 }
