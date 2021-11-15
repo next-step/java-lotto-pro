29a4cee9fd4ec252f5f7a20c5137a9d503cd3781
sungwoo <ssw890418@gmail.com>
2021-11-15 23:08:39 +0900

    refactor:도메인 영역에서 구현된 출력 로직 구현하도록 수정

--- src/main/java/study/lotto/view/OutputView.java
+++ src/main/java/study/lotto/view/OutputView.java
@@ -1,8 +1,10 @@
 package study.lotto.view;
 
-import study.lotto.domain.LottoNumbers;
-import study.lotto.domain.LottoNumbersGroup;
-import study.lotto.domain.Statics;
+import study.lotto.domain.*;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
 
 import static study.lotto.view.MessageUtil.*;
 
@@ -18,19 +20,47 @@ public class OutputView {
 
     public static void printLottoNumbersGroup(LottoNumbersGroup group) {
         for(LottoNumbers lottoNumbers : group.getLottoNumbersList()){
-            System.out.println(lottoNumbers.makeLottoNumberPrintFormat());
+            System.out.println(makeLottoNumbersPrintFormat(lottoNumbers));
         }
     }
+    private static String makeLottoNumbersPrintFormat(LottoNumbers lottoNumbers) {
+        StringBuilder sb = new StringBuilder();
+        sb.append('[');
+        for(LottoNumber lottoNumber : lottoNumbers.getLottoNumbers()) {
+            sb.append(lottoNumber.getNumber() +", ");
+        }
+        sb.delete(sb.lastIndexOf(","), sb.length());
+        sb.append(']');
+        return sb.toString();
+    }
 
-    public static void requestLastLottoNumberGroup() {
-        System.out.println(REQUEST_LAST_WEEK_LOTTO_NUMBER_INPUT_MSG);
+    public static void requestWinningLottoNumberGroup() {
+        System.out.println(REQUEST_WINNING_LOTTO_NUMBER_INPUT_MSG);
     }
 
     public static void printStatics(Statics statics) {
         System.out.println("당첨통계");
         System.out.println("---------");
-        System.out.println(statics.makeStaticPrintFormat());
-        System.out.println(statics.makeProfitPrintFormat());
+
+        Arrays.asList(Rank.values())
+                .stream()
+                .sorted(Collections.reverseOrder())
+                .forEach(rank -> {
+                    printAboutOneRank(statics, rank);
+                });
+
+        System.out.println(String.format("총 수익률은 %.2f입니다.", statics.getProfitRate()));
+    }
+
+    private static void printAboutOneRank(Statics statics, Rank rank) {
+        Map<Rank, Count> matchCounter = statics.getMatchCounter();
+        int correctCount = matchCounter.get(rank).getCount();
+        int winningMoney = rank.getWinningMoney();
+        int countOfMatch = rank.getCountOfMatch();
+        if(Rank.SECOND == rank)
+            System.out.println(String.format("%d개 일치, 보너스 볼 일치 (%d원)- %d개", countOfMatch, winningMoney, correctCount));
+        if(Rank.MISS != rank && Rank.SECOND != rank)
+            System.out.println(String.format("%d개 일치 (%d원)- %d개", countOfMatch, winningMoney, correctCount));
     }
 
     public static void requestBonusBall() {
