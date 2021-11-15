d673302a2ee0c6669245baf9256314fb180cff91
sungwoo <ssw890418@gmail.com>
2021-11-15 23:03:08 +0900

    refactor:랭킹별로 몇개씩 맞추었는지 카운트하는 Map 추가 및 뷰 로직 분리

--- src/main/java/study/lotto/domain/Statics.java
+++ src/main/java/study/lotto/domain/Statics.java
@@ -1,58 +1,68 @@
 package study.lotto.domain;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Statics {
 
     private final Money money;
     private final LottoNumbersGroup lottoNumbersGroup;
+    private final Map<Rank, Count> matchCounter;
 
     private double profitRate;
-
-    public Statics(Money money) {
-        this.money = money;
-        this.lottoNumbersGroup = new LottoNumbersGroup(null);
-    }
 
     public Statics(Money money, LottoNumbersGroup group) {
         this.money = money;
         this.lottoNumbersGroup = group;
+        this.matchCounter = new HashMap<>();
+        initMatchCounter();
         analyst();
         calculateProfitRate();
+    }
+
+    public Statics(Money money) {
+        this.money = money;
+        this.lottoNumbersGroup = null;
+        this.matchCounter = new HashMap<>();
+        initMatchCounter();
     }
 
     public static Statics valueOf(Money userInputMoney, LottoNumbersGroup lottoNumbersGroup) {
         return new Statics(userInputMoney, lottoNumbersGroup);
     }
 
-    public void analyst() {
-        LottoNumbers lastLottoNumbers = lottoNumbersGroup.getLastLottoNumbers();
-        for(LottoNumbers lottoNumbers : lottoNumbersGroup.getLottoNumbersList()) {
-            lastLottoNumbers.match(lottoNumbers, lottoNumbersGroup.getBonusBall());
+    private void initMatchCounter() {
+        for(Rank rank : Rank.values()) {
+            matchCounter.put(rank, new Count());
         }
+    }
+
+    public void analyst() {
+        LottoNumbers lastLottoNumbers = lottoNumbersGroup.getWinningLottoNumbers();
+        for(LottoNumbers lottoNumbers : lottoNumbersGroup.getLottoNumbersList()) {
+            Rank matchResult = lastLottoNumbers.match(lottoNumbers, lottoNumbersGroup.getBonusBall());
+            renewMatchCounter(matchResult);
+        }
+    }
+
+    private void renewMatchCounter(Rank matchResult) {
+        Count count = matchCounter.get(matchResult);
+        matchCounter.put(matchResult, count.increase());
     }
 
     public void calculateProfitRate() {
         int totalPrize = 0;
         for (Rank rank : Rank.values()) {
-            totalPrize += rank.getCorrect() * rank.getWinningMoney();
+            totalPrize += matchCounter.get(rank).getCount() * rank.getWinningMoney();
         }
         profitRate = Math.floor((double) totalPrize/money.getMoney()*100)/100;
     }
 
-    public String makeStaticPrintFormat() {
-        StringBuilder sb = new StringBuilder();
-
-        for(Rank rank : Rank.values()) {
-            sb.append(rank.makePrintFormat());
-        }
-
-        return sb.toString();
-    }
-
-    public String makeProfitPrintFormat() {
-        return String.format("총 수익률은 %.2f입니다.", profitRate);
-    }
-
     public double getProfitRate() {
         return profitRate;
+    }
+
+    public Map<Rank, Count> getMatchCounter() {
+        return matchCounter;
     }
 }
