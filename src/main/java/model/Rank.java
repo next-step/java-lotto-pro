2050f7aaa785dc513f1a5c6b6a84178c06cea16e
daehyun0 <dd70296@gmail.com>
2021-11-15 22:42:13 +0900

    refactor: 추가 필드를 이용해 하드코딩된 비교 제거

--- src/main/java/model/Rank.java
+++ src/main/java/model/Rank.java
@@ -4,19 +4,21 @@ import java.util.Arrays;
 import java.util.Optional;
 
 public enum Rank {
-	FIRST(2_000_000_000, 6),
-	SECOND(30_000_000, 5),
-	THIRD(1_500_000, 5),
-	FOURTH(50_000, 4),
-	FIFTH(5_000, 3),
-	NONE(0, 0);
+	FIRST(2_000_000_000, 6, false),
+	SECOND(30_000_000, 5, true),
+	THIRD(1_500_000, 5, false),
+	FOURTH(50_000, 4, false),
+	FIFTH(5_000, 3, false),
+	NONE(0, 0, false);
 
 	private final int reward;
 	private final int matchingCount;
+	private final boolean isMatchBonusNumber;
 
-	Rank(int reward, int matchingCount) {
+	Rank(int reward, int matchingCount, boolean isMatchBonusNumber) {
 		this.reward = reward;
 		this.matchingCount = matchingCount;
+		this.isMatchBonusNumber = isMatchBonusNumber;
 	}
 
 	public int getReward() {
@@ -26,13 +28,13 @@ public enum Rank {
 	public static Rank mapByMatchingCountAndBonusFlag(int matchingCount, boolean isMatchBonusNumber) {
 		Optional<Rank> optionalRank = Arrays.stream(values())
 			.filter(rank -> rank.matchingCount == matchingCount)
+			.filter(rank -> mapMatchBonusNumber(rank, isMatchBonusNumber))
 			.findFirst();
 
-		if (matchingCount == SECOND.matchingCount) {
-			return optionalRank.map(rank -> isMatchBonusNumber ? Rank.SECOND : Rank.THIRD)
-				.orElse(Rank.NONE);
-		}
-
 		return optionalRank.orElse(Rank.NONE);
+	}
+
+	private static boolean mapMatchBonusNumber(Rank rank, boolean isMatchBonusNumber) {
+		return !rank.isMatchBonusNumber || isMatchBonusNumber;
 	}
 }
