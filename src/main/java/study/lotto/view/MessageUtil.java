462605d715a09003d90eb45070ff52e1ce0ceab9
sungwoo <ssw890418@gmail.com>
2021-11-15 23:05:54 +0900

    refactor:변수명을 직관적으로 수정

--- src/main/java/study/lotto/view/MessageUtil.java
+++ src/main/java/study/lotto/view/MessageUtil.java
@@ -3,7 +3,7 @@ package study.lotto.view;
 public class MessageUtil {
     public static final String REQUEST_MONEY_INPUT_MSG = "구입금액을 입력해 주세요.";
     public static final String PURCHASE_COUNT_OUPUT_MSG = "%d개를 구매했습니다.";
-    public static final String REQUEST_LAST_WEEK_LOTTO_NUMBER_INPUT_MSG = "지난 주 당첨 번호를 입력해 주세요.";
+    public static final String REQUEST_WINNING_LOTTO_NUMBER_INPUT_MSG = "지난 주 당첨 번호를 입력해 주세요.";
     public static final String REQUEST_BONUS_BALL_INPUT_MSG = "보너스 볼을 입력해 주세요.";
 
     public static final String INVALID_LOTTO_NUMBER_ERR_MSG = "유효하지 않은 로또번호 입니다";
