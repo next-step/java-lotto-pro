0c7b9484c5fd95ce03933ad8e4f211dece1114ba
sungwoo <ssw890418@gmail.com>
2021-11-15 22:44:35 +0900

    refactor:뷰 로직 분리 및 correct 변수 제거

--- src/main/java/study/lotto/domain/Rank.java
+++ src/main/java/study/lotto/domain/Rank.java
@@ -3,19 +3,17 @@ package study.lotto.domain;
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST(0,6, 2_000_000_000),
-    SECOND(0,5, 30_000_000),
-    THIRD(0,5, 1_500_000),
-    FOURTH(0,4, 50_000),
-    FIFTH(0,3, 5_000),
-    MISS(0,0, 0);
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    MISS(0, 0);
 
-    private int correct;
     private int countOfMatch;
     private int winningMoney;
 
-    private Rank(int correct, int countOfMatch, int winningMoney) {
-        this.correct = correct;
+    private Rank( int countOfMatch, int winningMoney) {
         this.countOfMatch = countOfMatch;
         this.winningMoney = winningMoney;
     }
@@ -36,23 +34,6 @@ public enum Rank {
         return this == SECOND;
     }
 
-    private boolean isMiss() {
-        return this == MISS;
-    }
-
-    public void increaseCorrect() {
-        this.correct++;
-    }
-
-    public String makePrintFormat() {
-        String format = "";
-        if(isSecond())
-            format = String.format("%d개 일치, 보너스 볼 일치 (%d원)- %d개\n", countOfMatch, winningMoney, correct);
-        if(!isMiss() && !isSecond())
-            format = String.format("%d개 일치 (%d원)- %d개\n", countOfMatch, winningMoney, correct);
-        return format;
-    }
-
     public int getCountOfMatch() {
         return countOfMatch;
     }
@@ -61,13 +42,4 @@ public enum Rank {
         return winningMoney;
     }
 
-    public int getCorrect() {
-        return correct;
-    }
-
-    public void initCorrect() {
-        for(Rank rank : Rank.values()) {
-            this.correct = 0;
-        }
-    }
 }
--- src/main/java/study/temp/lotto/auto/Main.java
+++
@@ -1,8 +0,0 @@
-package study.temp.lotto.auto;
-
-public class Main {
-    public static void main(String[] args) {
-        LottoController controller = new LottoController();
-        controller.run();
-    }
-}
