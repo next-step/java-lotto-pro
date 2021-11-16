35b10c7afa8d0b61fba6d28ae5352fa8c3c14e1d
daehyun0 <dd70296@gmail.com>
2021-11-17 00:43:13 +0900

    feat: LottoGame 구성

--- src/main/java/controller/LottoGame.java
+++ src/main/java/controller/LottoGame.java
@@ -4,8 +4,11 @@ import java.util.Objects;
 
 import model.Lotto;
 import model.LottoNumber;
+import model.LottoNumberChoiceManual;
 import model.LottoNumberChoiceRandom;
+import model.LottoNumberChoiceStrategy;
 import model.LottoPurchaseCount;
+import model.LottoPurchasePrice;
 import model.Lottos;
 import model.RewardCalculator;
 import view.InputView;
@@ -13,15 +16,34 @@ import view.ResultView;
 
 public class LottoGame {
 	public void start() {
-		LottoPurchaseCount lottoPurchaseCount = requestPurchasingPrice();
-		Lottos purchasedLottos = buyLotto(lottoPurchaseCount);
-		InputView.showResponseInputOfPurchaseLottos(purchasedLottos);
+		LottoPurchasePrice lottoPurchasePrice = requestPurchasingPrice();
+		LottoPurchaseCount totalLottoPurchaseCount = lottoPurchasePrice.toPurchaseCount();
+		LottoPurchaseCount manualLottoPurchaseCount = requestPurchasingManualLottoCount(totalLottoPurchaseCount);
+		LottoPurchaseCount autoLottoPurchaseCount = new LottoPurchaseCount(totalLottoPurchaseCount.get() - manualLottoPurchaseCount.get());
+
+		InputView.showRequestInputOfManualLottos();
+		Lottos purchasedManualLottos = buyLotto(new LottoNumberChoiceManual(), manualLottoPurchaseCount);
+		Lottos purchasedAutoLottos = buyLotto(new LottoNumberChoiceRandom(), autoLottoPurchaseCount);
+		showBuyingLottosResult(manualLottoPurchaseCount, autoLottoPurchaseCount, purchasedManualLottos, purchasedAutoLottos);
 
 		Lotto winningLotto = requestWinningLotto();
 		LottoNumber bonusNumber = requestBonusNumber();
 
-		RewardCalculator rewardCalculator = purchasedLottos.calcReward(winningLotto, bonusNumber);
-		ResultView.showResult(lottoPurchaseCount, rewardCalculator);
+		showRewardResult(lottoPurchasePrice, purchasedManualLottos, purchasedAutoLottos, winningLotto, bonusNumber);
+	}
+
+	private void showRewardResult(LottoPurchasePrice lottoPurchasePrice, Lottos purchasedManualLottos,
+		Lottos purchasedAutoLottos, Lotto winningLotto, LottoNumber bonusNumber) {
+		RewardCalculator rewardCalculator = purchasedManualLottos.calcReward(winningLotto, bonusNumber)
+			.sum(purchasedAutoLottos.calcReward(winningLotto, bonusNumber));
+		ResultView.showResult(lottoPurchasePrice, rewardCalculator);
+	}
+
+	private void showBuyingLottosResult(LottoPurchaseCount manualLottoPurchaseCount, LottoPurchaseCount autoLottoPurchaseCount,
+		Lottos purchasedManualLottos, Lottos purchasedAutoLottos) {
+		InputView.showResponseInputOfPurchaseLottosTitle(manualLottoPurchaseCount, autoLottoPurchaseCount);
+		InputView.showResponseInputOfPurchaseLottos(purchasedManualLottos);
+		InputView.showResponseInputOfPurchaseLottos(purchasedAutoLottos);
 	}
 
 	private LottoNumber requestBonusNumber() {
@@ -51,12 +73,36 @@ public class LottoGame {
 		}
 	}
 
-	private Lottos buyLotto(LottoPurchaseCount lottoPurchaseCount) {
-		return new Lottos(new LottoNumberChoiceRandom(), lottoPurchaseCount);
+	private Lottos buyLotto(LottoNumberChoiceStrategy lottoNumberChoiceStrategy,
+		LottoPurchaseCount lottoPurchaseCount) {
+		return new Lottos(lottoNumberChoiceStrategy, lottoPurchaseCount);
 	}
 
-	private LottoPurchaseCount requestPurchasingPrice() {
+	private LottoPurchaseCount requestPurchasingManualLottoCount(LottoPurchaseCount totalLottoPurchaseCount) {
 		LottoPurchaseCount lottoPurchaseCount = null;
+		while (Objects.isNull(lottoPurchaseCount)) {
+			InputView.showRequestInputOfManualLottoPurchaseCount();
+			lottoPurchaseCount = parseInputOfManualLottoPurchaseCount(totalLottoPurchaseCount);
+		}
+		return lottoPurchaseCount;
+	}
+
+	private LottoPurchaseCount parseInputOfManualLottoPurchaseCount(LottoPurchaseCount totalLottoPurchaseCount) {
+		try {
+			int parsedManualPurchasedCount = Integer.parseInt(InputView.pollInput());
+			if (totalLottoPurchaseCount.isLessThan(parsedManualPurchasedCount)) {
+				InputView.showRequestInputExceptionOfTotalPurchaseCountLessThanManual();
+				return null;
+			}
+			return new LottoPurchaseCount(parsedManualPurchasedCount);
+		} catch (RuntimeException e) {
+			InputView.showRequestInputExceptionOfManualLottoPurchaseCount();
+			return null;
+		}
+	}
+
+	private LottoPurchasePrice requestPurchasingPrice() {
+		LottoPurchasePrice lottoPurchaseCount = null;
 		while (Objects.isNull(lottoPurchaseCount)) {
 			InputView.showRequestInputOfPurchasePrice();
 			lottoPurchaseCount = parseInputOfPurchasePrice();
@@ -64,9 +110,9 @@ public class LottoGame {
 		return lottoPurchaseCount;
 	}
 
-	private LottoPurchaseCount parseInputOfPurchasePrice() {
+	private LottoPurchasePrice parseInputOfPurchasePrice() {
 		try {
-			return new LottoPurchaseCount(Integer.parseInt(InputView.pollInput()));
+			return new LottoPurchasePrice(Integer.parseInt(InputView.pollInput()));
 		} catch (RuntimeException e) {
 			InputView.showRequestInputExceptionOfPurchasedPrice();
 			return null;
