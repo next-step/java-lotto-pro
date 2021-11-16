fb1db37b78edfc21891d410125feab5177bc0555
daehyun0 <dd70296@gmail.com>
2021-11-16 23:20:38 +0900

    refactor(InputView): 상수 변수 이름 변경 및 FormattedString 상수화

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -12,14 +12,15 @@ public class InputView {
 	private static final String MESSAGE_REQUEST_EXCEPTION_INPUT_OF_WINNING_NUMBERS = "잘못된 형식의 당첨 번호입니다.";
 	private static final String MESSAGE_REQUEST_INPUT_OF_BONUS_NUMBER = "보너스 볼을 입력해 주세요.";
 	private static final String MESSAGE_REQUEST_EXCEPTION_INPUT_OF_BONUS_NUMBER = "잘못된 형식의 보너스 볼입니다.";
-	private static final String MESSAGE_REQUEST_INPUT_OF_LOTTO_PURCHASE_COUNT = "수동으로 구매할 로또 수를 입력해 주세요.";
+	private static final String MESSAGE_REQUEST_INPUT_OF_MANUAL_LOTTO_PURCHASE_COUNT = "수동으로 구매할 로또 수를 입력해 주세요.";
+	private static final String MESSAGE_FORMAT_INPUT_OF_PURCHASE_LOTTO_COUNT_TITLE = "수동으로 %d장, 자동으로 %d장 구매했습니다.%n";
 
 	public static void showRequestInputOfPurchasePrice() {
 		System.out.println(MESSAGE_REQUEST_INPUT_OF_PURCHASE_PRICE);
 	}
 
 	public static void showRequestInputOfManualLottoPurchaseCount() {
-		System.out.println(MESSAGE_REQUEST_INPUT_OF_LOTTO_PURCHASE_COUNT);
+		System.out.println(MESSAGE_REQUEST_INPUT_OF_MANUAL_LOTTO_PURCHASE_COUNT);
 	}
 
 	public static void showRequestInputExceptionOfPurchasedPrice() {
@@ -44,7 +45,7 @@ public class InputView {
 
 	public static void showResponseInputOfPurchaseLottosTitle(LottoPurchaseCount manualPurchaseCount,
 		LottoPurchaseCount autoPurchaseCount) {
-		System.out.printf("수동으로 %d장, 자동으로 %d장 구매했습니다.%n", manualPurchaseCount.get(), autoPurchaseCount.get());
+		System.out.printf(MESSAGE_FORMAT_INPUT_OF_PURCHASE_LOTTO_COUNT_TITLE, manualPurchaseCount.get(), autoPurchaseCount.get());
 	}
 
 	public static void showResponseInputOfPurchaseLottos(Lottos lottos) {
