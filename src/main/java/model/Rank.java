a2ba334209581b9ece19ca122f10c5e1df5bfa0e
daehyun0 <dd70296@gmail.com>
2021-11-19 04:04:16 +0900

    refactor(Rank): 불필요한 중복 삭제
    - 생성자 추가

--- src/main/java/model/Rank.java
+++ src/main/java/model/Rank.java
@@ -4,16 +4,20 @@ import java.util.Arrays;
 import java.util.Optional;
 
 public enum Rank {
-	FIRST(2_000_000_000, 6, false),
+	FIRST(2_000_000_000, 6),
 	SECOND(30_000_000, 5, true),
-	THIRD(1_500_000, 5, false),
-	FOURTH(50_000, 4, false),
-	FIFTH(5_000, 3, false),
-	NONE(0, 0, false);
+	THIRD(1_500_000, 5),
+	FOURTH(50_000, 4),
+	FIFTH(5_000, 3),
+	NONE(0, 0);
 
 	private final int reward;
 	private final int matchingCount;
 	private final boolean isMatchBonusNumber;
+
+	Rank(int reward, int matchingCount) {
+		this(reward, matchingCount, false);
+	}
 
 	Rank(int reward, int matchingCount, boolean isMatchBonusNumber) {
 		this.reward = reward;
