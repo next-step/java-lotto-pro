c55db1240901460bb2bf5da8d57feeee255cb910
유한결 <hangyul.yu@42dot.io>
2021-11-19 17:36:21 +0900

    fix - 리뷰 피드백 반영
    - 긴 if 문을 짧게 변형
    - enum에서 if/else 대신 stream을 사용하도록 변경

--- src/main/java/step3/domain/LottoMatchCaseEnum.java
+++ src/main/java/step3/domain/LottoMatchCaseEnum.java
@@ -3,18 +3,20 @@ package step3.domain;
 import java.util.Arrays;
 
 public enum LottoMatchCaseEnum {
-  FIRST(6, 2_000_000_000),
-  SECOND(5, 30_000_000),
-  THIRD(5, 1_500_000),
-  FOURTH(4, 50_000),
-  FIFTH(3, 5_000),
-  MISS(0, 0);
+  FIRST(6, false, 2_000_000_000),
+  SECOND(5, true, 30_000_000),
+  THIRD(5, false, 1_500_000),
+  FOURTH(4, false, 50_000),
+  FIFTH(3, false, 5_000),
+  MISS(0, false, 0);
 
   private final int matchCount;
+  private final boolean matchBonus;
   private final int price;
 
-  LottoMatchCaseEnum(int matchCount, int price) {
+  LottoMatchCaseEnum(int matchCount, boolean matchBonus, int price) {
     this.matchCount = matchCount;
+    this.matchBonus = matchBonus;
     this.price = price;
   }
 
@@ -26,23 +28,28 @@ public enum LottoMatchCaseEnum {
     return price;
   }
 
+  private static boolean isMiss(int matchCount) {
+    return matchCount < LottoMatchCaseEnum.FIFTH.matchCount;
+  }
+
+  private boolean isSameValueOf(int matchCount, boolean matchBonus) {
+    if (matchCount == SECOND.getMatchCount()) {
+      return this.matchCount == matchCount && this.matchBonus == matchBonus;
+    }
+    return this.matchCount == matchCount;
+  }
+
   public static LottoMatchCaseEnum valueOf(int matchCount, boolean matchBonus) {
-    if (matchCount < 3) {
+    if (isMiss(matchCount)) {
       return MISS;
     }
-    if (matchCount == 3) {
-      return FIFTH;
-    }
-    if (matchCount == 4) {
-      return FOURTH;
-    }
-    if (matchCount == 5) {
-      return matchBonus ? SECOND : THIRD;
-    }
-    if (matchCount == 6) {
-      return FIRST;
-    }
-    throw new IllegalArgumentException(String.format(
-            "[ERROR] No MatchCase for matchCount = %d, matchBonus = %b", matchCount, matchBonus));
+
+    return Arrays.stream(LottoMatchCaseEnum.values())
+        .filter(matchCaseEnum -> matchCaseEnum.isSameValueOf(matchCount, matchBonus))
+        .findAny()
+        .orElseThrow(() ->
+            new IllegalArgumentException(String.format(
+                "[ERROR] No MatchCase for matchCount = %d, matchBonus = %b", matchCount,
+                matchBonus)));
   }
 }
--- src/main/java/step3/domain/LottoWinningTicket.java
+++ src/main/java/step3/domain/LottoWinningTicket.java
@@ -5,26 +5,23 @@ import java.util.stream.Collectors;
 
 public class LottoWinningTicket extends LottoTicket {
 
-    private LottoNumber bonusNumber;
+  private LottoNumber bonusNumber;
 
-    // 번호 + 보너스 번호를 입력 받는 티켓
-    public LottoWinningTicket(List<LottoNumber> numbers, LottoNumber bonusNumber) {
-        super(numbers);
-        validate(numbers, bonusNumber);
-        this.bonusNumber = bonusNumber;
-    }
+  // 번호 + 보너스 번호를 입력 받는 티켓
+  public LottoWinningTicket(List<LottoNumber> numbers, LottoNumber bonusNumber) {
+    super(numbers);
+    validate(numbers, bonusNumber);
+    this.bonusNumber = bonusNumber;
+  }
 
-    public LottoNumber getBonusNumber() {
-        return bonusNumber;
-    }
+  public LottoNumber getBonusNumber() {
+    return bonusNumber;
+  }
 
-    private void validate(List<LottoNumber> numbers, LottoNumber bonusNumber) {
-        if (numbers.stream()
-            .map(LottoNumber::getNumber)
-            .collect(Collectors.toList())
-            .contains(bonusNumber.getNumber())) {
-            throw new IllegalArgumentException(
-                "[ERROR] not valid bonus number. bonusNumber = " + bonusNumber);
-        }
+  private void validate(List<LottoNumber> numbers, LottoNumber bonusNumber) {
+    if (this.contains(bonusNumber)) {
+      throw new IllegalArgumentException(
+          "[ERROR] not valid bonus number. bonusNumber = " + bonusNumber);
     }
+  }
 }
