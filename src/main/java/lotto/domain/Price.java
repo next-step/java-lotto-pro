dc3c9c87b5c3572ae89744e7d01d4080c50fcc91
Sung Du Kim <sungdu.alex.kim@gmail.com>
2021-11-19 10:14:17 +0900

    test: 로또 당첨 번호, 보너스 번호 일급 컬렉션 테스트 추가

--- src/main/java/lotto/domain/Price.java
+++ src/main/java/lotto/domain/Price.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import java.util.Objects;
+
 public class Price {
 
     private static final int UNIT_PRICE = 1000;
@@ -61,5 +63,22 @@ public class Price {
 
     public String asString() {
         return Long.valueOf(price).toString();
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Price price1 = (Price) o;
+        return price == price1.price;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(price);
     }
 }
