8e63491ce75a3291569caff53978e8414c659dcb
kms <kmmin78@gmail.com>
2022-05-10 00:08:54 +0900

    feat : String 클래스 요구사항 2 - substring을 활용한 소괄호 제거 기능 구현

---
+++ src/main/java/utils/StringUtil.java
@@ -0,0 +1,7 @@
+package utils;
+
+public class StringUtil {
+    public static String substring(final String target) {
+        return target.substring(target.indexOf("(") + 1, target.indexOf(")"));
+    }
+}
--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -3,6 +3,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import utils.StringUtil;
 
 @DisplayName("String 클래스 학습 테스트")
 public class StringTest {
@@ -42,6 +45,31 @@ public class StringTest {
                 final String[] result = input.split(",");
 
                 assertThat(result).containsExactly("1");
+
+            }
+
+        }
+
+    }
+
+    @DisplayName("StringUtil 클래스의 substring() 메서드는")
+    @Nested
+    class Describe_StringUtil_Substring {
+
+        @DisplayName("\"(1,2)\"을 값을 전달받았을 때")
+        @Nested
+        class Context_One_And_Two_With_Parentheses {
+
+            final String input = "(1,2)";
+
+            @DisplayName("\"1,2\"을 반환한다.")
+            @ParameterizedTest
+            @CsvSource(value = {"(1,2):1,2"}, delimiter = ':')
+            void it_Returns_String_Parentheses_Removed(final String input, final String expected) {
+
+                final String result = StringUtil.substring(input);
+
+                assertThat(result).isEqualTo(expected);
 
             }
 
