0b4d1ea116bb8586a247215d404e43eeb59b4732
mond <revive29@gmail.com>
2022-05-10 03:07:31 +0900

    feat(parsePositiveOrZeroNumber): 숫자 한개 입력시 해당 숫자 반환 메소드 추가

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,6 +1,9 @@
 package calculator;
 
 public class StringAddCalculator {
+    public static final String INVALID_NUMBER_FORMAT_MESSAGE = "올바르지 않는 숫자 형식입니다.";
+    public static final String INVALID_NUMBER_RANGE_MESSAGE = "올바르지 않는 숫자 범위입니다. 가능한 범위는 0부터 2147483647입니다.";
+    private static final String NUMBER_FORMAT_REGEX = "^(0|[1-9]+[0-9]*)$";
 
     private StringAddCalculator() {
     }
@@ -9,8 +12,22 @@ public class StringAddCalculator {
         if (isNotValidNumber(number)) {
             return 0;
         }
+        return parsePositiveOrZeroNumber(number);
+    }
 
-        return 0;
+    private static int parsePositiveOrZeroNumber(String number) {
+        validatePositiveOrZeroNumber(number);
+        try {
+            return Integer.parseInt(number);
+        } catch (NumberFormatException nfe) {
+            throw new RuntimeException(INVALID_NUMBER_RANGE_MESSAGE);
+        }
+    }
+
+    private static void validatePositiveOrZeroNumber(String number) {
+        if (!number.matches(NUMBER_FORMAT_REGEX)) {
+            throw new RuntimeException(INVALID_NUMBER_FORMAT_MESSAGE);
+        }
     }
 
     private static boolean isNotValidNumber(String number) {
--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -1,10 +1,15 @@
 package calculator;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class StringAddCalculatorTest {
 
@@ -20,5 +25,35 @@ class StringAddCalculatorTest {
                 () -> assertThat(blankString2).isZero(),
                 () -> assertThat(nullString).isZero()
         );
+    }
+
+    @Test
+    @DisplayName("입력값이 0 또는 양수인 경우 해당 숫자를 반환")
+    void inputOnlyPositiveOrZeroNumber() {
+        int three = StringAddCalculator.input("3");
+        int zero = StringAddCalculator.input("0");
+
+        assertAll(
+                () -> assertEquals(3, three),
+                () -> assertEquals(0, zero)
+        );
+    }
+
+    @ParameterizedTest(name = "입력값에 음수 및 문자열({0})이 들어오면 RuntimeException을 발생")
+    @ValueSource(strings = {"-5", "abc"})
+    void inputNegativeNumberAndCharacter(String invalidNumber) {
+        assertThatExceptionOfType(RuntimeException.class)
+                .isThrownBy(() -> StringAddCalculator.input(invalidNumber))
+                .withMessage(StringAddCalculator.INVALID_NUMBER_FORMAT_MESSAGE);
+    }
+
+    @Test
+    @DisplayName("입력값에 Integer의 범위를 벗어나는 수가 들어오면 RuntimeException을 발생")
+    void inputLongValue() {
+        String longNumber = String.valueOf(Integer.MAX_VALUE + 1L);
+
+        assertThatThrownBy(() -> StringAddCalculator.input(longNumber))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage(StringAddCalculator.INVALID_NUMBER_RANGE_MESSAGE);
     }
 }
