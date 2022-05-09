b9ef5436dedcc22ac43f1487aa5a099937dc2a02
soob-forest <dltntnqo@naver.com>
2022-05-10 01:13:03 +0900

    test(SetTest): Set의 contains true, false case 테스트
    - CsvSource로 input을 조절 해, true, false의 케이스 모두 테스트 가능하도록 함

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
@@ -33,5 +34,12 @@ public class SetTest {
     @DisplayName("Set의 contains 메서드 테스트")
     void contains(int input) {
         assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false", "6:false"}, delimiter = ':')
+    @DisplayName("Set의 contains true, false 케이스 테스트")
+    void containsCaseTest(int input, boolean result) {
+        assertThat(numbers.contains(input)).isEqualTo(result);
     }
 }
