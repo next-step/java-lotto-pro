f80aec22a053fee8c4d95205d51316aa0fc9d95e
seongyoon <ksy091104@gmail.com>
2022-05-09 23:34:16 +0900

    Set 학습 테스트 추가

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,45 @@
+package study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set 사이즈 확인")
+    void setSize() {
+        assertThat(numbers).hasSize(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("Set 요소 포함 테스트 - @ParameterizedTest - valueSource")
+    void setContains_parameterized_valueSource(int input) {
+        assertThat(numbers).contains(input);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false", "6:false"}, delimiter = ':')
+    @DisplayName("Set 요소 포함 테스트 - @ParameterizedTest - csvSource")
+    void setContains_parameterized_csvSource(int input, boolean result) {
+        assertThat(numbers.contains(input)).isEqualTo(result);
+    }
+}
