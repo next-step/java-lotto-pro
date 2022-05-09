af254aa2ff0f39b291a6c723e27e09925414166a
kwonyongil <ksgkms23@likelion.org>
2022-05-09 23:33:41 +0900

    charAt StringIndexOutOfBoundException study test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,7 +1,9 @@
 package study;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class StringTest {
@@ -15,5 +17,13 @@ public class StringTest {
     void substring() {
         String result = "(1,2)".substring(1, 4);
         assertThat(result).isEqualTo("1,2");
+    }
+
+    @DisplayName("위치 값 벗어난 경우 StringIndexOutOfBoundsException 테스트")
+    @Test
+    void charAt() {
+        assertThatThrownBy(() -> "abc".charAt(6))
+                .isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 6");
     }
 }
