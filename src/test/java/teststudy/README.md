bf264169db13e91795423cca71acf844575a24cf
toughchb <ggang2000@gmail.com>
2022-05-09 22:48:02 +0900

    test: SetTest 요구사항 2 구현

--- src/test/java/teststudy/README.md
+++ src/test/java/teststudy/README.md
@@ -20,8 +20,8 @@
 - [x] Set의 size() 메소드를 활용해 Set의 크기를 확인하는 학습테스트를 구현한다.
 
 ## 요구사항 2
-- [ ] Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습테스트를 구현하려한다.
-- [ ] JUnit의 ParameterizedTest를 활용해 중복 코드를 제거해 본다.
+- [x] Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습테스트를 구현하려한다.
+- [x] JUnit의 ParameterizedTest를 활용해 중복 코드를 제거해 본다.
 
 ## 요구사항 3
 - [ ] 1, 2, 3 값은 contains 메소드 실행결과 true, 4, 5 값을 넣으면 false 가 반환되는 테스트를 하나의 Test Case로 구현한다.
--- src/test/java/teststudy/SetTest.java
+++ src/test/java/teststudy/SetTest.java
@@ -7,6 +7,8 @@ import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
 
@@ -28,4 +30,18 @@ public class SetTest {
         assertThat(numbers.size()).isEqualTo(setSize);
     }
 
+    @Test
+    @DisplayName("Set에 contain을 이용 하여 1, 2, 3의 값이 존재하는지를 확인하는 테스트")
+    void contains() {
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+    }
+
+    @DisplayName("ParameterizedTest를 이용하여 중복 코드 제거")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int number) {
+        assertThat(numbers.contains(number)).isTrue();
+    }
 }
