753d7d381a7c47ccd04b5e288d7fa55d76bb6c41
Donghee Han <handh0413@gmail.com>
2022-05-09 20:52:45 +0900

    test: 요구사항 1 테스트 코드
    - 괄호 없는 부분 문자열 만들기

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -1,4 +1,3 @@
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,14 +7,24 @@ public class StringTest {
     @Test
     @DisplayName("Comma를 이용한 숫자 분리 테스트")
     public void Split_numbers_by_using_comma() {
-        String[] numbers = "1,2".split(",");
+        String input = "1,2";
+        String[] numbers = input.split(",");
         assertThat(numbers).contains("1").contains("2");
     }
 
     @Test
     @DisplayName("Comma를 이용한 단일 숫자 분리 테스트")
     public void Split_single_number_by_using_comma() {
-        String[] numbers = "1,".split(",");
+        String input = "1,";
+        String[] numbers = input.split(",");
         assertThat(numbers).containsExactly("1");
+    }
+
+    @Test
+    @DisplayName("괄호 없는 부분 문자열 만들기")
+    public void Create_substring_without_parentheses() {
+        String input = "(1,2)";
+        String result = input.substring(1, input.length() - 1);
+        assertThat(result).isEqualTo("1,2");
     }
 }
