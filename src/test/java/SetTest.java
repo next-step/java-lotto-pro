42e6f59204f6b26c8db96daa78a0df74ec207864
Ttungga <torb2002@gmail.com>
2022-05-10 01:20:44 +0900

    test: add a test for Set.contains method

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -5,6 +5,8 @@ import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -23,5 +25,13 @@ public class SetTest {
     void when_call_size_should_return_3() {
         // when and then
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("빈 Set 객체에 1, 1, 2, 3을 add 하고 1, 2, 3을 파라미터로 contains 메서드를 호출했을 때 true가 반환되어야 한다")
+    void when_call_contains_for_elements_in_Set_should_return_true(final int element) {
+        // when and then
+        assertThat(numbers.contains(element)).isTrue();
     }
 }
