30463fd0eaca5c9ee5a560bbea4681b52ca878cb
SANGIK KIM <sangik.kim@enel.com>
2022-05-09 23:18:27 +0900

    test(StringTest): add charAt test

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -1,4 +1,5 @@
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -30,8 +31,39 @@ public class StringTest {
         @Test
         @DisplayName("\"(1,2)\" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 \"1,2\"를 반환하도록 구현한다.")
         void substring_테스트1() {
-            String result = "(1,2)".substring(1,4);
+            String result = "(1,2)".substring(1, 4);
             assertThat(result).isEqualTo("1,2");
+        }
+    }
+
+    @Nested
+    @DisplayName("요구사항3")
+    class CharAt_테스트 {
+        private final String source = "abc";
+
+        @Nested
+        @DisplayName("\"abc\" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다.")
+        class CharAt_성공테스트 {
+            @Test
+            void charAt_경계값_성공테스트() {
+                assertThat(source.charAt(0)).isEqualTo('a');
+                assertThat(source.charAt(2)).isEqualTo('c');
+            }
+        }
+
+        @Nested
+        @DisplayName("String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면"
+                + " StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현한다.")
+        class CharAt_실패테스트 {
+            private final String indexOutOfBoundsExceptionMessage = "index out of range";
+
+            @Test
+            void charAt_경계값_실패테스트() {
+                assertThatThrownBy(() -> source.charAt(-1)).isInstanceOf(IndexOutOfBoundsException.class)
+                        .hasMessageContaining(indexOutOfBoundsExceptionMessage);
+                assertThatThrownBy(() -> source.charAt(3)).isInstanceOf(IndexOutOfBoundsException.class)
+                        .hasMessageContaining(indexOutOfBoundsExceptionMessage);
+            }
         }
     }
 }
