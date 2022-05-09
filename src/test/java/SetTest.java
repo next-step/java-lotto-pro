545107d5fb239cc64e165fbc6d6302fe5787e836
sangik-kim <69880781+sangik-kim@users.noreply.github.com>
2022-05-10 00:31:40 +0900

    Step1 - 학습 테스트 실습 (#341)
    * test(StringTest): add String split test
    
    * test(StringTest): add substring test
    
    * test(StringTest): add charAt test
    
    * refactor: remove unnecessary file - empty.txt
    
    * test(SetTest): add Set size test
    
    * refactor(StringTest): modify display name to make it more meaningful
    
    * test(SetTest): add contains test with parameterized test
    
    * test(SetTest): add another contains test with csv source parameterized test
    
    Co-authored-by: SANGIK KIM <sangik.kim@enel.com>

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,57 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@DisplayName("Set Collection에 대한 학습 테스트")
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Nested
+    @DisplayName("요구사항 1")
+    class Size_테스트 {
+        @Test
+        @DisplayName("Set의 size() 메소드를 활용해 Set의 크기를 확인하는 학습테스트를 구현한다.")
+        void Size_테스트() {
+            assertThat(numbers.size()).isEqualTo(3);
+        }
+    }
+
+    @Nested
+    @DisplayName("요구사항 2")
+    class Contains_테스트1 {
+        @ParameterizedTest
+        @ValueSource(ints = {1, 2, 3})
+        @DisplayName("ParameterizedTest를 활용해 Set에 1, 2, 3의 값이 존재하는지 확인한다.")
+        void contains(int number) {
+            assertThat(numbers.contains(number)).isTrue();
+        }
+    }
+
+    @Nested
+    @DisplayName("요구사항 3")
+    class Contains_테스트2 {
+        @ParameterizedTest
+        @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+        @DisplayName("ParameterizedTest를 활용해 입력 값에 따라 결과 값이 다른 경우에 대한 테스트도 가능하도록 구현한다.")
+        void contains(String input, String expected) {
+            assertThat(numbers.contains(Integer.valueOf(input))).isEqualTo(Boolean.valueOf(expected));
+        }
+    }
+}
---
+++ src/test/java/StringTest.java
@@ -0,0 +1,71 @@
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("String 클래스에 대한 학습 테스트")
+public class StringTest {
+    @Nested
+    @DisplayName("요구사항1")
+    class Split_테스트 {
+        @Test
+        @DisplayName("\"1,2\"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다.")
+        void split_테스트1() {
+            String[] result = "1,2".split(",");
+            assertThat(result).containsExactly("1", "2");
+        }
+
+        @Test
+        @DisplayName("\"1\"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.")
+        void split_테스트2() {
+            String[] result = "1".split(",");
+            assertThat(result).containsExactly("1");
+        }
+    }
+
+    @Nested
+    @DisplayName("요구사항2")
+    class Substring_테스트 {
+        @Test
+        @DisplayName("\"(1,2)\" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 \"1,2\"를 반환하도록 구현한다.")
+        void substring_테스트1() {
+            String result = "(1,2)".substring(1, 4);
+            assertThat(result).isEqualTo("1,2");
+        }
+    }
+
+    @Nested
+    @DisplayName("요구사항3")
+    class CharAt_테스트 {
+        private final String source = "abc";
+
+        @Nested
+        @DisplayName("charAt 메소드 성공테스트")
+        class CharAt_성공테스트 {
+            @Test
+            @DisplayName("\"abc\" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다.")
+            void charAt_경계값_성공테스트() {
+                assertThat(source.charAt(0)).isEqualTo('a');
+                assertThat(source.charAt(2)).isEqualTo('c');
+            }
+        }
+
+        @Nested
+        @DisplayName("charAt 메소드 실패테스트")
+        class CharAt_실패테스트 {
+            private final String indexOutOfBoundsExceptionMessage = "index out of range";
+
+            @Test
+            @DisplayName("String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면"
+                    + " StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현한다.")
+            void charAt_경계값_실패테스트() {
+                assertThatThrownBy(() -> source.charAt(-1)).isInstanceOf(IndexOutOfBoundsException.class)
+                        .hasMessageContaining(indexOutOfBoundsExceptionMessage);
+                assertThatThrownBy(() -> source.charAt(3)).isInstanceOf(IndexOutOfBoundsException.class)
+                        .hasMessageContaining(indexOutOfBoundsExceptionMessage);
+            }
+        }
+    }
+}
