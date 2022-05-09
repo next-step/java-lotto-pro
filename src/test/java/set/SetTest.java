3375d2744b68605991faa2f6567ed3eca31d6a4d
gunan <power4889@gmail.com>
2022-05-09 22:28:57 +0900

    test: Set의 contains() 메서드 테스트

--- src/test/java/set/SetTest.java
+++ src/test/java/set/SetTest.java
@@ -3,6 +3,8 @@ package set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -25,5 +27,12 @@ public class SetTest {
 	@Test
 	void size() {
 		assertThat(numbers.size()).isEqualTo(3);
+	}
+
+	@DisplayName("Set에 1, 2, 3의 값이 존재하는 지 확인한다")
+	@ParameterizedTest
+	@ValueSource(ints = {1, 2, 3})
+	void contains(int number) {
+		assertThat(numbers.contains(number)).isTrue();
 	}
 }
