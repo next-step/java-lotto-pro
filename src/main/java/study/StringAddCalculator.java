10d0b40ff2a9cf037b05f7061451af86d99f4cc0
KangSeongHyo <abc9023@naver.com>
2022-05-09 22:05:48 +0900

    feat : splitAndSum 메서드에 null 또는 빈문자인 경우 0을 반환 하도록 추가

---
+++ src/main/java/study/StringAddCalculator.java
@@ -0,0 +1,11 @@
+package study;
+
+public class StringAddCalculator {
+
+    public static int splitAndSum(String numberWords) {
+        if (numberWords == null || numberWords.isEmpty()) {
+            return 0;
+        }
+        return Integer.MAX_VALUE;
+    }
+}
