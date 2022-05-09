b5a790d937698b7fc74322245b248e2ebe7e8207
gunan <power4889@gmail.com>
2022-05-09 22:38:21 +0900

    test: Set에 1, 2, 3의 값이 존재하고 4, 5의 값이 존재하지 않는 지 확인한다

--- src/test/java/set/SetTest.java
+++ src/test/java/set/SetTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -34,5 +35,12 @@ public class SetTest {
 	@ValueSource(ints = {1, 2, 3})
 	void contains(int number) {
 		assertThat(numbers.contains(number)).isTrue();
+	}
+
+	@DisplayName("Set에 1, 2, 3의 값이 존재하고 4, 5의 값이 존재하지 않는 지 확인한다")
+	@ParameterizedTest
+	@CsvSource(value = {"1,true", "2,true", "3,true", "4,false", "5,false"})
+	void contains_not(int number, boolean expected) {
+		assertThat(numbers.contains(number)).isEqualTo(expected);
 	}
 }
