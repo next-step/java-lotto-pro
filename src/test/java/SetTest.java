f945480321298947b77ada712a9fcfde5b1efc8f
kms <kmmin78@gmail.com>
2022-05-10 00:31:17 +0900

    test : Set 클래스 요구사항 2 - contains 학습테스트 with ParameterizedTest

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -6,28 +6,30 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("Set 클래스 학습 테스트")
 public class SetTest {
 
-    @DisplayName("Set 클래스의 size() 메서드는")
+    @DisplayName("1, 2, 3을 값으로 가지고 있을 때")
     @Nested
-    class Describe_size {
+    class Context_One_Two_Three {
 
-        @DisplayName("1, 2, 3을 값으로 가지고 있을 때")
+        private Set<Integer> numbers;
+
+        @BeforeEach
+        void setUp() {
+            numbers = new HashSet<>();
+            numbers.add(1);
+            numbers.add(1);
+            numbers.add(2);
+            numbers.add(3);
+        }
+
+        @DisplayName("Set 클래스의 size() 메서드는")
         @Nested
-        class Context_One_Two_Three {
-
-            private Set<Integer> numbers;
-
-            @BeforeEach
-            void setUp() {
-                numbers = new HashSet<>();
-                numbers.add(1);
-                numbers.add(1);
-                numbers.add(2);
-                numbers.add(3);
-            }
+        class Describe_size {
 
             @DisplayName("3을 반환한다.")
             @Test
@@ -36,6 +38,21 @@ public class SetTest {
                 final int size = numbers.size();
 
                 assertThat(size).isEqualTo(3);
+
+            }
+
+        }
+
+        @DisplayName("Set 클래스의 contains() 메서드는")
+        @Nested
+        class Describe_contains {
+
+            @DisplayName("1,2,3 각 값에 대하여 true를 반환한다.")
+            @ParameterizedTest
+            @ValueSource(ints = {1, 2, 3})
+            void it_Returns_True(final int input) {
+
+                assertThat(numbers.contains(input)).isTrue();
 
             }
 
