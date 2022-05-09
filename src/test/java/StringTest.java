efbc5a85b0786ca7bda5b1511882d9f9ce2ca2a6
SANGIK KIM <sangik.kim@enel.com>
2022-05-09 23:04:21 +0900

    test(StringTest): add substring test

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -23,4 +23,15 @@ public class StringTest {
             assertThat(result).containsExactly("1");
         }
     }
+
+    @Nested
+    @DisplayName("요구사항2")
+    class Substring_테스트 {
+        @Test
+        @DisplayName("\"(1,2)\" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 \"1,2\"를 반환하도록 구현한다.")
+        void substring_테스트1() {
+            String result = "(1,2)".substring(1,4);
+            assertThat(result).isEqualTo("1,2");
+        }
+    }
 }
