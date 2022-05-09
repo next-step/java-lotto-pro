7ddca58d433dd98737ccc2b3b8fbb4a0709391ce
Donghee Han <handh0413@gmail.com>
2022-05-09 21:30:19 +0900

    test: 요구사항 1,2 테스트 코드
    - Set 크기 확인
    - 숫자 포함 여부를 확인

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set 크기 확인")
+    public void Validate_set_size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("숫자 포함 여부를 확인")
+    public void Check_whether_numbers_are_included(int number) {
+        assertThat(numbers.contains(number)).isTrue();
+    }
+
+}
