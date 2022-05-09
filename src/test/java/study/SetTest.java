5e3af2fedcc96b845e06caf8fbd5fd1716a08db1
tasklet1579 <tasklet1579@gmail.com>
2022-05-09 20:46:54 +0900

    test(SetTest) : Set Collection에 대한 학습 테스트 - size - contains

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,54 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("SET 사이즈 측정 테스트")
+    @Test
+    void SET_사이즈_측정_테스트() {
+        // given
+        int expected = 3;
+
+        // when
+        int actual = numbers.size();
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("SET 값 존재 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void SET_값_존재_테스트(int expected) {
+        assertThat(numbers.contains(expected)).isTrue();
+    }
+
+    @DisplayName("SET 값 포함, 미포함 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void SET_값_포함_미포함_테스트(int given, boolean expected) {
+        Assertions.assertThat(numbers.contains(given)).isEqualTo(expected);
+    }
+}
