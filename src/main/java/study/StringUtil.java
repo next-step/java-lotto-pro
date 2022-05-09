65ab6a6c23c3b6889a4a917359790a73d6fbc446
seongyoon <ksy091104@gmail.com>
2022-05-09 23:17:51 +0900

    String 학습 테스트 추가

---
+++ src/main/java/study/StringUtil.java
@@ -0,0 +1,23 @@
+package study;
+
+public class StringUtil {
+    private static final String BRACKET_START_CHAR = "(";
+    private static final String BRACKET_END_CHAR = ")";
+
+    private StringUtil() {
+
+    }
+
+    public static String[] split(String str, String delimiter) {
+        return str.split(delimiter);
+    }
+
+    public static String removeBrackets(String str) {
+        String trimmedStr = str.trim();
+        return trimmedStr.substring(trimmedStr.indexOf(BRACKET_START_CHAR) + 1, trimmedStr.indexOf(BRACKET_END_CHAR));
+    }
+
+    public static char getChar(String str, int index) {
+        return str.charAt(index);
+    }
+}
---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,59 @@
+package study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class StringTest {
+    @Test
+    @DisplayName("문자열 분리 - 콤마 포함 문자열")
+    void splitTest_withDelimiter() {
+        String[] result = StringUtil.split("1,2", ",");
+        SoftAssertions softAssertions = new SoftAssertions();
+        softAssertions.assertThat(result).contains("1");
+        softAssertions.assertThat(result).contains("2");
+        softAssertions.assertThat(result).containsExactly("1", "2");
+        softAssertions.assertAll();
+    }
+
+    @Test
+    @DisplayName("문자열 분리 - 콤마 미포함 문자열")
+    void splitTest_withoutDelimiter() {
+        String[] result = "1".split(",");
+        SoftAssertions softAssertions = new SoftAssertions();
+        softAssertions.assertThat(result).contains("1");
+        softAssertions.assertThat(result).containsExactly("1");
+        softAssertions.assertAll();
+    }
+
+    @Test
+    @DisplayName("괄호 제거 - 괄호가 포함된 문자열")
+    void removeBracket_useSubString() {
+        String result = StringUtil.removeBrackets("(1,2)");
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("괄호 제거 - 괄호와 앞뒤 공백이 포함된 문자열")
+    void removeBracket_useSubString_includeSpace() {
+        String result = StringUtil.removeBrackets("    (1,2)   ");
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("문자열의 특정 index값 가져오기")
+    void getChar() {
+        char result = StringUtil.getChar("abcde", 3);
+        assertThat(result).isEqualTo('d');
+    }
+
+    @Test
+    @DisplayName("문자열의 특정 index값 가져오기 - 범위 밖 index")
+    void getChar_outOfBoundsException() {
+        assertThatThrownBy(() -> StringUtil.getChar("abcde", 99))
+                .isInstanceOf(StringIndexOutOfBoundsException.class);
+    }
+}
