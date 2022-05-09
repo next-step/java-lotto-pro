e536126d89c8dcdb1410a54ead13ba0117515b52
SANGIK KIM <sangik.kim@enel.com>
2022-05-09 23:35:40 +0900

    test(SetTest): add contains test with parameterized test

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("Set Collection에 대한 학습 테스트")
 public class SetTest {
@@ -27,6 +29,17 @@ public class SetTest {
         @DisplayName("Set의 size() 메소드를 활용해 Set의 크기를 확인하는 학습테스트를 구현한다.")
         void Size_테스트() {
             assertThat(numbers.size()).isEqualTo(3);
+        }
+    }
+
+    @Nested
+    @DisplayName("요구사항 2")
+    class Contains_테스트 {
+        @ParameterizedTest
+        @ValueSource(ints = {1, 2, 3})
+        @DisplayName("ParameterizedTest를 활용해 Set에 1, 2, 3의 값이 존재하는지 확인한다.")
+        void contains(int number) {
+            assertThat(numbers.contains(number)).isTrue();
         }
     }
 }
