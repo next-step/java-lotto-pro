b9b0304b796b45bba4bab6501ac797bde7fc4e09
Ttungga <torb2002@gmail.com>
2022-05-10 00:29:37 +0900

    test: add a test for String.substring method

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -26,4 +26,16 @@ public class StringTest {
         // then
         assertThat(result).containsExactly("1");
     }
+    
+    @Test
+    void substring_메서드를_사용했을_문자열_양_끝의_괄호가_제거되어야_한다() {
+        // given
+        final String given = "(1,2)";
+
+        // when
+        final String result = given.substring(1, 4);
+        
+        // then
+        assertThat(result).isEqualTo("1,2");
+    }
 }
