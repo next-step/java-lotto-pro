b28b468bf52a056055ecb2ed1742c6bcc6803537
sangho lee <sangholee.dev@gmail.com>
2022-05-10 00:05:25 +0900

    SetTest 요구사항 2 추가

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -7,6 +7,8 @@ import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class SetTest {
     private Set<Integer> numbers;
@@ -26,4 +28,10 @@ class SetTest {
         assertThat(numbers.size()).isEqualTo(3);
     }
 
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("요구사항 2 Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습테스트를 구현")
+    void contains(int number) {
+        assertThat(numbers.contains(number)).isTrue();
+    }
 }
