940996f03b6e1d3a4323f509384c2087f4514120
ssonsh <chosunci@gmail.com>
2022-05-10 13:29:23 +0900

    test(StringSplitter) : 커스텀 구분자를 이용해 문자열을 분리 테스트 커스텀 구분자가 포함된 문자열을 split 처리하는 테스트 코드 작성

--- src/test/java/calculator/utils/StringSplitterTest.java
+++ src/test/java/calculator/utils/StringSplitterTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class StringSplitterTest {
 
@@ -22,6 +24,39 @@ class StringSplitterTest {
                 () -> assertThat(splitText).contains("1"),
                 () -> assertThat(splitText).contains("2"),
                 () -> assertThat(splitText).contains("3")
+        );
+    }
+
+    @Test
+    @DisplayName("문자열을 커스텀 구분자를 이용해 Split 할 수 있다.")
+    void customSplit01(){
+        // given
+        String text = "//;\n1;2;3";
+
+        // when
+        String[] splitText = StringSplitter.split(text);
+
+        // then
+        assertAll(
+                () -> assertThat(splitText).contains("1"),
+                () -> assertThat(splitText).contains("2"),
+                () -> assertThat(splitText).contains("3")
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"//-\n1-2-3", "///\n1/2/3"})
+    @DisplayName("문자열을 커스텀 구분자를 이용해 Split 할 수 있다. (여러 케이스 테스트)")
+    void customSplit02(String text){
+        // given & when
+        String[] splitText = StringSplitter.split(text);
+
+        // then
+        assertAll(
+                () -> assertThat(splitText).contains("1"),
+                () -> assertThat(splitText).contains("2"),
+                () -> assertThat(splitText).contains("3"),
+                () -> assertThat(splitText.length).isEqualTo(3)
         );
     }
 }
