ff2316ba74818f981f781b1a0d3f24b48728bbd7
breezeunn <je7689@gmail.com>
2022-05-10 22:46:20 +0900

    test: 요구사항 3 test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -3,7 +3,7 @@ package study;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
 
@@ -25,7 +25,27 @@ public class StringTest {
     @DisplayName("문자열 처음과 끝 괄호 제거 테스트")
     void getStringWithoutBrackets() {
         String testData = "(1,2)";
-        String result = testData.substring(1, testData.length()-1);
+        String result = testData.substring(1, testData.length() - 1);
         assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt() 의 위치가 문자열의 길이보다 길 경우 예외 발생 확인")
+    void charAtWithGreaterIndex() {
+        String testData = "abc";
+        assertThatThrownBy(() -> {
+            testData.charAt(4);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+          .hasMessageContaining("index out of range");
+    }
+
+    @Test
+    @DisplayName("charAt() 의 위치가 0 미만인 경우 예외 발생 확인")
+    void charAtWithNegativeIndex() {
+        String testData = "abc";
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+          .isThrownBy(() -> {
+              testData.charAt(-1);
+        }).withMessageMatching("String index out of range: -\\d+");
     }
 }
