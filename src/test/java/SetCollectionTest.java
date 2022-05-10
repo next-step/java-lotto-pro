f76db1ac83373b13c3bd71dbf6cffd3459bd8650
mukeunzi <eunji057@gmail.com>
2022-05-11 02:14:03 +0900

    feat: Set Collection 테스트 요구사항1 테스트코드 작성 완료
    - Set 사이즈 확인을 위한 테스트

---
+++ src/test/java/SetCollectionTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetCollectionTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("중복이 잘 제거되었는지 Set의 사이즈를 확인한다.")
+    void checkSetSize() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+}
