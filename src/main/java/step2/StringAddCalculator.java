2b92a9756f8c5b4f6e7161c31d62457b87aa1740
piopoi <kangsw1988@gmail.com>
2022-05-10 22:48:53 +0900

    feat(StringAddCalculator): 숫자 하나를 입력하면 해당 숫자를 그대로 반환하는 기능 추가

--- src/main/java/step2/StringAddCalculator.java
+++ src/main/java/step2/StringAddCalculator.java
@@ -6,10 +6,22 @@ public class StringAddCalculator {
         if (isEmptyString(inputString)) {
             return 0;
         }
+        if (isSingleNumber(inputString)) {
+            return Integer.parseInt(inputString);
+        }
         return -1;
     }
 
     private static boolean isEmptyString(String source) {
         return source == null || source.isEmpty();
+    }
+
+    private static boolean isSingleNumber(String source) {
+        try {
+            Integer.parseInt(source);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
