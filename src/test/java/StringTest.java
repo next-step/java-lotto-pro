52c649541a996e9b43eef8c54e4326cec3d8b511
최현지 <chj1768@gmail.com>
2022-05-11 00:22:10 +0900

    refactor: indent space, wrap size 설정 반영

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -4,20 +4,20 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
-	@Test
-	@DisplayName("1,2을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인")
-	public void test_split_contains_1_or_2() {
-		String[] underTest = "1,2".split(",");
+    @Test
+    @DisplayName("1,2을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인")
+    public void test_split_contains_1_or_2() {
+        String[] underTest = "1,2".split(",");
 
-		assertThat(underTest).contains("1", "2");
-	}
+        assertThat(underTest).contains("1", "2");
+    }
 
-	@Test
-	@DisplayName("1을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지 확인")
-	public void test_split_contains_exactly_1() {
-		String[] underTest = "1".split(",");
+    @Test
+    @DisplayName("1을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지 확인")
+    public void test_split_contains_exactly_1() {
+        String[] underTest = "1".split(",");
 
-		assertThat(underTest).containsExactly("1");
-	}
+        assertThat(underTest).containsExactly("1");
+    }
 
 }
