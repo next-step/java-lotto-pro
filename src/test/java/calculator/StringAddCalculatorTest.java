26988bf83d3748312e2392f55abf2abd7c75eb2c
ssonsh <chosunci@gmail.com>
2022-05-10 16:24:07 +0900

    test(StringAddCalculatorTest) : 문자열 계산기에 전달되는 인수 정보의 유효성 검증 테스트 코드 작성 숫자 이외의 값 또는 음수는 입력할 경우 RuntimeException이 발생된다.
    - 숫자가 아닌 값이 있는 경우
    
    - 음수가 포함된 경우

--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -31,5 +32,31 @@ class StringAddCalculatorTest {
 
         // then
         assertThat(sum).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("문자열 계산기에 전달된 값이 숫자가 아닌경우 RuntimeException이 발생된다.")
+    void exceptionTest01(){
+        // given
+        String text = "a,f,!";
+
+        // when & then
+        assertThatThrownBy(() -> StringAddCalculator.splitAndSum(text))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("[ERROR]")
+                .hasMessageContaining("a, f, !");
+    }
+
+    @Test
+    @DisplayName("문자열 계산기에 전달된 값이 음수 인 경우 RuntimeException이 발생된다.")
+    void exceptionTest02(){
+        // given
+        String text = "-1,2,3";
+
+        // when & then
+        assertThatThrownBy(() -> StringAddCalculator.splitAndSum(text))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("[ERROR]")
+                .hasMessageContaining("-1, 2, 3");
     }
 }
