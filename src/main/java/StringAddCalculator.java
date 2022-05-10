b46f6ca664b08fe0ed18fc033ac3fea0603e0998
Lisc <qqwer153@gmail.com>
2022-05-11 02:49:23 +0900

    feat: 문자열을 구분자를 기준으로 분리하는 기능 구현

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,9 +1,27 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
+    private static final String DEFAULT_DELIMITER = "[,:]";
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
+
     public static int splitAndSum(String input) {
         if (isNullOrEmpty(input)) {
             return 0;
         }
+        List<String> numbers = splitToNumbers(input);
         return 1;
+    }
+
+    private static List<String> splitToNumbers(String input) {
+        Matcher m = CUSTOM_DELIMITER_PATTERN.matcher(input);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            return Arrays.asList(m.group(2).split(customDelimiter));
+        }
+        return Arrays.asList(input.split(DEFAULT_DELIMITER));
     }
 
     private static boolean isNullOrEmpty(String input) {
