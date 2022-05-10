638073c20d0bb1623b3aa9ff827be5405bc6baee
breezeunn <je7689@gmail.com>
2022-05-10 22:11:35 +0900

    test: 요구사항2 test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -11,4 +11,17 @@ public class StringTest {
         String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1", "2");
     }
+
+    @Test
+    void 구분자가_없는_문자열의_split() {
+        String[] result = "1".split(",");
+        assertThat(result).contains("1");
+    }
+
+    @Test
+    void 문자열_처음과_끝_괄호_제거() {
+        String testData = "(1,2)";
+        String result = testData.substring(1, testData.length()-1);
+        assertThat(result).isEqualTo("1,2");
+    }
 }
