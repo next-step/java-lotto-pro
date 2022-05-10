43e79cc474fa58d14fe428b750107772c564197f
Woody <yuncheol92@naver.com>
2022-05-10 21:03:29 +0900

    feat: 컴마(,) 또는 콜론(:) 으로 구분하여 합한다.

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,20 +1,41 @@
 package calculator;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 public class StringAddCalculator {
 
     private static final int ZERO = 0;
+    private static final String REGEX_DELIMITER = "|";
+    private static final List<String> STANDARD_DELIMISTERS = Arrays.asList(",", ":");
 
     public static int splitAndSum(final String expression) {
         if (isNullOrEmpty(expression)) {
             return ZERO;
         }
 
-        return ZERO;
+        List<String> stringNumbers = split(expression);
+
+        return sum(stringNumbers);
     }
 
     private static boolean isNullOrEmpty(final String expression) {
         return Objects.isNull(expression) || expression.isEmpty();
+    }
+
+    private static List<String> split(final String expression) {
+        String regex = makeStandardDelimiterRegex();
+        return Arrays.asList(expression.split(regex));
+    }
+
+    private static int sum(final List<String> stringNumbers) {
+        return stringNumbers.stream()
+                .mapToInt(Integer::parseInt)
+                .sum();
+    }
+
+    private static String makeStandardDelimiterRegex() {
+        return String.join(REGEX_DELIMITER, STANDARD_DELIMISTERS);
     }
 }
