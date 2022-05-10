7efe78da96155007fcafb2343ec2c8769e8fc35b
ssonsh <chosunci@gmail.com>
2022-05-10 14:10:44 +0900

    feat(StringAddCalculator) : 문자열 계산기 도메인 구성 문자열을 인자로 받고 결과를 Integer 형으로 반환한다.
    - 전달된 인자가 null 혹은 ""인 경우 0이 반환된다.

---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,16 @@
+package calculator;
+
+import calculator.utils.StringUtils;
+
+public class StringAddCalculator {
+
+    private static final Integer DEFAULT_RESULT = 0;
+
+    public static Integer splitAndSum(String text){
+        if(StringUtils.isEmpty(text)){
+            return DEFAULT_RESULT;
+        }
+
+        return DEFAULT_RESULT;
+    }
+}
