cbddb2082b335de3421d8e33a25d3d1dffa3e337
boss.back <boss.back@kakaocorp.com>
2022-05-11 01:11:25 +0900

    feat: Set 요구사항 1 ( 사이즈 확인 ) 구현

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,30 @@
+package study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("Set 의 사이즈를 확인한다.")
+    @Test
+    void findSetSize() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+}
