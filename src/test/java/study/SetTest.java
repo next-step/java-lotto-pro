816933da41bb8892e0a40aeeafd148c0b1be6987
sangho lee <sangholee.dev@gmail.com>
2022-05-10 09:51:07 +0900

    SetTest 리뷰에 작성해주신 DisplayName 에 문서 내용이 아닌 테스트 내용이 나오도록 수정

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -24,21 +24,21 @@ class SetTest {
     }
 
     @Test
-    @DisplayName("요구사항 1 set size() 테스트")
+    @DisplayName("Set의 size() 가 3이 나오는지 확인")
     void set_size() {
         assertThat(numbers.size()).isEqualTo(3);
     }
 
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3})
-    @DisplayName("요구사항 2 Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습테스트를 구현")
+    @DisplayName("Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인")
     void contains(int number) {
         assertThat(numbers.contains(number)).isTrue();
     }
 
     @ParameterizedTest
     @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
-    @DisplayName("요구사항 3 1, 2, 3 값은 contains 메소드 실행결과 true, 4, 5 값을 넣으면 false 가 반환되는 테스트를 하나의 Test Case로 구현")
+    @DisplayName("1, 2, 3 값은 contains 메소드 실행결과 true, 4, 5 값을 넣으면 false 가 반환되는지 확인")
     void contains_3(int number, boolean result) {
         assertThat(numbers.contains(number)).isEqualTo(result);
     }
