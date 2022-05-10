ebeb88cecc6872ba1f68dcf449e367bdddb3415f
MIN SEONG <43296963+MINS99@users.noreply.github.com>
2022-05-11 00:11:24 +0900

    Step1 - 학습 테스트 실습 (#357)
    * docs(README): step1 요구 사항 정리
    
    * test(StringTest): String 클래스의 split에 대한 학습 테스트
    
    * test(StringTest): String 클래스의 substring에 대한 학습 테스트
    
    * test(StringTest): String 클래스의 charAt과 Exception에 대한 학습 테스트
    
    * refactor(StringTest): 메서드와 클래스의 접근 제어자 삭제
    
    * test(SetTest): Set 컬렉션의 size에 대한 학습 테스트
    
    * test(SetTest): Set 컬렉션의 contains와 ParameterizedTest에 대한 학습 테스트
    
    * test(SetTest): ParameterizedTest에 대한 학습 테스트
    
    * docs(README): step1 구현 사항 체크

---
+++ src/test/java/step1/SetTest.java
@@ -0,0 +1,66 @@
+package step1;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("size() 메소드를 활용해 Set의 크기를 확인하는 학습테스트")
+    @Test
+    void setTest_size() {
+        // given & when
+        int size = numbers.size();
+
+        // then
+        assertThat(size).isEqualTo(3);
+    }
+
+    @DisplayName("contains() 메소드를 활용해 1,2,3의 값이 존재하는지를 확인하는 학습테스트")
+    @Test
+    void setTest_contains() {
+        // given & when & then
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+    }
+
+    @DisplayName("ParameterizedTest를 활용해 중복 코드를 제거")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains_ParameterizedTest(int value) {
+        // given & when
+        boolean actual = numbers.contains(value);
+
+        // then
+        assertThat(actual).isTrue();
+    }
+
+    @DisplayName("입력 값에 따라 결과 값이 다른 경우에 대한 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void contains_ParameterizedTest2(int value, boolean expected) {
+        // given & when
+        boolean actual = numbers.contains(value);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+}
---
+++ src/test/java/step1/StringTest.java
@@ -0,0 +1,159 @@
+package step1;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class StringTest {
+
+    private static final String SPLIT_REGEX = ",";
+
+    @DisplayName("\"1,2\"를 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트_contains")
+    @Test
+    void splitTest_contains() {
+        // given
+        String input = "1,2";
+        String[] expected = {"1", "2"};
+
+        // when
+        String[] actual = input.split(SPLIT_REGEX);
+
+        // then
+        assertThat(actual).contains(expected);    // 순서 상관 없음
+    }
+
+    @DisplayName("\"1,2\"를 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트_containsExactly")
+    @Test
+    void splitTest_containsExactly() {
+        // given
+        String input = "1,2";
+        String[] expected = {"1", "2"};
+
+        // when
+        String[] actual = input.split(SPLIT_REGEX);
+
+        // then
+        assertThat(actual).containsExactly(expected);    // containsExactly : 순서 상관 있음
+    }
+
+    @DisplayName("\"1,2\"를 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트_containsExactlyInAnyOrder")
+    @Test
+    void splitTest_containsExactlyInAnyOrder() {
+        // given
+        String input = "1,2";
+        String[] expected = {"2", "1"};
+
+        // when
+        String[] actual = input.split(SPLIT_REGEX);
+
+        // then
+        assertThat(actual).containsExactlyInAnyOrder(expected);    // containsExactlyInAnyOrder : 순서 상관 없음
+    }
+
+    @DisplayName("\"1\"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트_contains")
+    @Test
+    void splitTest2_contains() {
+        // given
+        String input = "1";
+        String[] expected = {"1"};
+
+        // when
+        String[] actual = input.split(SPLIT_REGEX);
+
+        // then
+        assertThat(actual).contains(expected);
+    }
+
+    @DisplayName("\"1\"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트_containsExactly")
+    @Test
+    void splitTest2_containsExactly() {
+        // given
+        String input = "1";
+        String[] expected = {"1"};
+
+        // when
+        String[] actual = input.split(SPLIT_REGEX);
+
+        // then
+        assertThat(actual).containsExactly(expected);
+    }
+
+    @DisplayName("\"(1,2)\" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 \"1,2\"를 반환하도록 구현")
+    @Test
+    void substringTest() {
+        // given
+        String input = "(1,2)";
+        String expected = "1,2";
+
+        // when
+        String actual = input.substring(1, input.length() - 1);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("\"abc\" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트")
+    @Test
+    void charAtTest() {
+        // given
+        String input = "abc";
+        char expected = 'a';
+
+        // when
+        char actual = input.charAt(0);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("\"abc\" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트_ParameterizedTest")
+    @ParameterizedTest
+    @CsvSource(value = {"a:0", "b:1", "c:2"}, delimiter = ':')
+    void charAtTest_ParameterizedTest(char expected, int index) {
+        // given
+        String input = "abc";
+
+        // when
+        char actual = input.charAt(index);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException")
+    @Test
+    void exceptionTest() {
+        // given
+        String input = "abc";
+
+        // when & then
+        assertThatThrownBy(() -> input.charAt(5)).isInstanceOf(StringIndexOutOfBoundsException.class);
+    }
+
+    @DisplayName("StringIndexOutOfBoundsException에 hasMessageContaining 추가")
+    @Test
+    void exceptionTest_hasMessageContaining() {
+        // given
+        String input = "abc";
+
+        // when & then
+        assertThatThrownBy(() -> input.charAt(5)).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 5");
+    }
+
+    @DisplayName("StringIndexOutOfBoundsException에 withMessageMatching 추가")
+    @Test
+    void exceptionTest_withMessageMatching() {
+        // given
+        String input = "abc";
+
+        // when & then
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                .isThrownBy(() -> input.charAt(5)).withMessageMatching("String index out of range: 5");
+    }
+}
