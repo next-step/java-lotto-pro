1359ea2a17e29408cf7cf471f5b091f0b9472935
kbh0581 <kbh0581@gmail.com>
2022-05-11 00:06:31 +0900

    feat: 문자열이 숫자가 아닐경우 RuntimeException 발생

--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -1,13 +1,15 @@
 package calculator;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class StringAddCalculatorTest {
 
     @Test
-    public void splitAndSum_null_또는_빈문자() {
+    void splitAndSum_null_또는_빈문자() {
         int result = StringAddCalculator.splitAndSum(null);
         assertThat(result).isEqualTo(0);
 
@@ -16,21 +18,28 @@ public class StringAddCalculatorTest {
     }
 
     @Test
-    public void splitAndSum_숫자하나() throws Exception {
+    void splitAndSum_숫자하나() throws Exception {
         int result = StringAddCalculator.splitAndSum("1");
         assertThat(result).isEqualTo(1);
     }
 
     @Test
-    public void splitAndSum_쉼표구분자() throws Exception {
+    void splitAndSum_쉼표구분자() throws Exception {
         int result = StringAddCalculator.splitAndSum("1,2");
         assertThat(result).isEqualTo(3);
     }
 
     @Test
-    public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
+    void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
         int result = StringAddCalculator.splitAndSum("1,2:3");
         assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("splitAndSum_문자열이 숫자가 아닐경우 RuntimeException을 발생시킨다.")
+    void splitAndSumIsNotNumberError() {
+        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("1a,2:3"))
+                .isInstanceOf(RuntimeException.class);
     }
 
 
