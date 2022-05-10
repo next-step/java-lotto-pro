7cac90f9907567f40f6c6f42b8f385d55efe622d
KangSeongHyo <abc9023@naver.com>
2022-05-10 18:41:06 +0900

    feat : splitAndSum 메서드에 컴마 구분자로 입력시 더하는 기능 추가
    - 구분자가 없는 경우에도 split을 통해 처리할 수 있도록 수정

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -3,18 +3,25 @@ package study;
 public class StringAddCalculator {
 
     public static int splitAndSum(String numberWords) {
+        int sum = 0;
         if (numberWords == null || numberWords.isEmpty()) {
-            return 0;
+            return sum;
         }
-        if (numberWords.length() == 1) {
-            return wordToNumber(numberWords);
-        }
-        return Integer.MAX_VALUE;
+        String[] splitNumbers = numberWords.split(",");
+        sum = sumNumbers(sum, splitNumbers);
+        return sum;
     }
 
-    private static int wordToNumber(String numberWords) {
-        if (numberWords.matches("\\p{Digit}")) {
-            return Integer.parseInt(numberWords);
+    private static int sumNumbers(int sum, String[] splitNumbers) {
+        for (String number : splitNumbers) {
+            sum += wordToNumber(number);
+        }
+        return sum;
+    }
+
+    private static int wordToNumber(String number) {
+        if (number.matches("\\p{Digit}")) {
+            return Integer.parseInt(number);
         }
         throw new IllegalArgumentException("[ERROR] 숫자가 아닙니다.");
     }
