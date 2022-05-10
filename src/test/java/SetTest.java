64e5b02f817e473758b14f0a6c7f20c2cd978a49
Seung Hun Jee <js223kr92@naver.com>
2022-05-10 11:36:37 +0900

    JUnit 학습 테스트 (#342)
    * test : StringTest 추가
    
    * test : SetTest 추가

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,46 @@
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("size 테스트")
+    void sizeTest() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    @DisplayName("contains 테스트")
+    void containsTest(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true","2:true","3:true","4:false","5:false"}, delimiter = ':')
+    @DisplayName("contains 테스트2")
+    void containsTest2(String input, String expected) {
+        assertThat(String.valueOf(numbers.contains(Integer.parseInt(input)))).isEqualTo(expected);
+    }
+
+}
