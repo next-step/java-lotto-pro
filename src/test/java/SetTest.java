2c046f87979a9588a48a63500af95d3f0823116a
최현지 <chj1768@gmail.com>
2022-05-11 00:53:16 +0900

    test: Set 학습 테스트 작성 - contains 메소드를 활용해 1, 2, 3의 값이 Set에 존재하는지를 확인

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -23,5 +25,12 @@ public class SetTest {
     @DisplayName("Set의 크기를 확인")
     public void test_size() {
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @DisplayName("contains 메소드를 활용해 1, 2, 3의 값이 Set에 존재하는지를 확인")
+    @ValueSource(ints = { 1, 2, 3 })
+    void test_contains(Integer item) {
+        assertThat(numbers.contains(item)).isTrue();
     }
 }
