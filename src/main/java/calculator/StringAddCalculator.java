abe07e74435e0b5d918fc93dbeee9ad30a449c88
kbh0581 <kbh0581@gmail.com>
2022-05-11 00:00:31 +0900

    feat: 구분자(,:)로 분리한 숫자의 합을 구한다.
    - SplitUtils 문자열에 분해하는 유틸 클래스

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,14 +1,23 @@
 package calculator;
 
+import calculator.util.SplitUtils;
 import calculator.util.StringUtils;
+import java.util.Arrays;
+
 
 public class StringAddCalculator {
+
+    private final static String BASIC_SEPERATER = ",|:";
 
     public static int splitAndSum(String text) {
         if (StringUtils.isEmptyString(text)) {
             return 0;
         }
-        return 1;
+        return sum(text);
+    }
+
+    private static int sum(String text) {
+        return Arrays.stream(SplitUtils.splitToInt(text, BASIC_SEPERATER)).sum();
     }
 
 }
--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -15,4 +15,23 @@ public class StringAddCalculatorTest {
         assertThat(result).isEqualTo(0);
     }
 
+    @Test
+    public void splitAndSum_숫자하나() throws Exception {
+        int result = StringAddCalculator.splitAndSum("1");
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    public void splitAndSum_쉼표구분자() throws Exception {
+        int result = StringAddCalculator.splitAndSum("1,2");
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
+        int result = StringAddCalculator.splitAndSum("1,2:3");
+        assertThat(result).isEqualTo(6);
+    }
+
+
 }
