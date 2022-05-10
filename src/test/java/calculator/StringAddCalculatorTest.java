6f6be2b92d73eea692255899285d1bc82032ee44
kbh0581 <kbh0581@gmail.com>
2022-05-11 01:07:04 +0900

    style: 테스트코드의 DisplayName 작성

--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 public class StringAddCalculatorTest {
 
     @Test
+    @DisplayName("Null 또는 빈문자일때 0을 반환한다.")
     void splitAndSum_null_또는_빈문자() {
         int result = StringAddCalculator.splitAndSum(null);
         assertThat(result).isEqualTo(0);
@@ -19,18 +20,21 @@ public class StringAddCalculatorTest {
     }
 
     @Test
+    @DisplayName("숫자가 하나 일 경우 숫자 하나가 반환된다.")
     void splitAndSum_숫자하나() throws Exception {
         int result = StringAddCalculator.splitAndSum("1");
         assertThat(result).isEqualTo(1);
     }
 
     @Test
+    @DisplayName("쉼표 구분인 합")
     void splitAndSum_쉼표구분자() throws Exception {
         int result = StringAddCalculator.splitAndSum("1,2");
         assertThat(result).isEqualTo(3);
     }
 
     @Test
+    @DisplayName("쉼표 또는 콜론 구분인 합")
     void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
         int result = StringAddCalculator.splitAndSum("1,2:3");
         assertThat(result).isEqualTo(6);
@@ -44,6 +48,7 @@ public class StringAddCalculatorTest {
     }
 
     @Test
+    @DisplayName("splitAndSum_문자열이 음수가 아닐경우 RuntimeException을 발생시킨다.")
     public void splitAndSum_negative() throws Exception {
         assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
                 .isInstanceOf(RuntimeException.class);
@@ -51,6 +56,7 @@ public class StringAddCalculatorTest {
 
 
     @Test
+    @DisplayName("커스텀한 구분자의 합")
     void splitAndSum_custom_구분자() throws Exception {
         assertAll(()-> {
             assertThat(StringAddCalculator.splitAndSum("//;\n1;2;3")).isEqualTo(6);
