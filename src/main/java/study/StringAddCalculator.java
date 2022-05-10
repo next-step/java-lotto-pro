802945091596af487e7d6e7c51a4aca06816acd6
KangSeongHyo <abc9023@naver.com>
2022-05-10 18:29:07 +0900

    feat : splitAndSum 메서드에 숫자가 하나일 경우 해당 숫자를 반환 추가
    - 숫자가 아닌 경우 IllegalArgumentException 발생

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -6,6 +6,16 @@ public class StringAddCalculator {
         if (numberWords == null || numberWords.isEmpty()) {
             return 0;
         }
+        if (numberWords.length() == 1) {
+            return wordToNumber(numberWords);
+        }
         return Integer.MAX_VALUE;
+    }
+
+    private static int wordToNumber(String numberWords) {
+        if (numberWords.matches("\\p{Digit}")) {
+            return Integer.parseInt(numberWords);
+        }
+        throw new IllegalArgumentException("[ERROR] 숫자가 아닙니다.");
     }
 }
