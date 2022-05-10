82d8272688d362d8daba5f83b31cd09fdaf5d5d5
limwoobin <drogba02@naver.com>
2022-05-10 19:40:49 +0900

    test: 테스트 코드 수정
    - 상수 위치 내부클래스로 변경, DisplayName 수정

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -6,13 +6,13 @@ import org.junit.jupiter.api.Test;
 
 @DisplayName("String 클래스에 대한 학습 테스트")
 class StringTest {
-    private static final String COMMA = ",";
 
     @Nested
     @DisplayName("문자열 split 테스트")
     class StringSplitTest {
+        private static final String COMMA = ",";
 
-        @DisplayName("문자열 '1,2' 를 ',' 로 split하면 1과 2로 정상적으로 분리되어야 한다")
+        @DisplayName("문자열 \"(1,2)\" 를 \",\" 로 split 하면 1과 2로 정상적으로 분리되어야 한다")
         @Test
         void string_split_comma() {
             String given = "1,2";
@@ -23,7 +23,7 @@ class StringTest {
             assertThat(values).containsExactly("1", "2");
         }
 
-        @DisplayName("문자열 '1'을 ',' 로 split 하면 1만을 포함하는 배열이 반환되어야 한다")
+        @DisplayName("문자열 \"1\"을 \",\" 로 split 하면 1만을 포함하는 배열이 반환되어야 한다")
         @Test
         void string_split_comma_2() {
             String given = "1";
