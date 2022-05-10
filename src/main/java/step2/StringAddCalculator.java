3348f780a6fbf7a4aca20995acfe3a27020ab98e
Donghee Han <handh0413@gmail.com>
2022-05-10 20:21:23 +0900

    refactor: 문자열 계산기 리팩토링
    - 메소드 추출하기

--- src/main/java/step2/StringAddCalculator.java
+++ src/main/java/step2/StringAddCalculator.java
@@ -9,15 +9,19 @@ public class StringAddCalculator {
     public static final String REAR_CUSTOM_DELIMITER = "\n";
 
     public static int splitAndSum(String input) {
-        if (input == null || input.isEmpty()) {
+        if (isNullOrEmpty(input)) {
             return 0;
         }
 
         if (isSingleDigitNumber(input)) {
-            return Integer.valueOf(input);
+            return Integer.parseInt(input);
         }
 
         return getSumWithDelimiter(input);
+    }
+
+    private static boolean isNullOrEmpty(String input) {
+        return input == null || input.isEmpty();
     }
 
     static boolean isSingleDigitNumber(String input) {
@@ -31,12 +35,23 @@ public class StringAddCalculator {
 
     private static int getSumWithDelimiter(String input) {
         String delimiter = findDelimiter(input);
-        input = input.substring(input.indexOf(REAR_CUSTOM_DELIMITER) + 1);
+        input = getSubstringWithoutDelimiter(input);
         String[] numbers = input.split(delimiter);
         return Arrays.stream(numbers)
                 .map(Integer::valueOf)
-                .filter(num -> isPositive(num))
-                .reduce(0, (num1, num2) -> num1 + num2);
+                .filter(StringAddCalculator::isPositive)
+                .reduce(0, Integer::sum);
+    }
+
+    private static String findDelimiter(String input) {
+        if (input.startsWith(FRONT_CUSTOM_DELIMITER)) {
+            return input.substring(FRONT_CUSTOM_DELIMITER.length(), input.indexOf(REAR_CUSTOM_DELIMITER));
+        }
+        return DEFAULT_DELIMITER;
+    }
+
+    private static String getSubstringWithoutDelimiter(String input) {
+        return input.substring(input.indexOf(REAR_CUSTOM_DELIMITER) + REAR_CUSTOM_DELIMITER.length());
     }
 
     private static boolean isPositive(int num) {
@@ -44,13 +59,5 @@ public class StringAddCalculator {
             return true;
         }
         throw new RuntimeException("음수는 포함될 수 없습니다.");
-    }
-
-    private static String findDelimiter(String input) {
-        String delimiter = DEFAULT_DELIMITER;
-        if (input.startsWith(FRONT_CUSTOM_DELIMITER)) {
-            return input.substring(FRONT_CUSTOM_DELIMITER.length(), input.indexOf(REAR_CUSTOM_DELIMITER));
-        }
-        return delimiter;
     }
 }
