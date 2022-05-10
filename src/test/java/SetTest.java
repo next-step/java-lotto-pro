327c0ecade817c81cd824fa82a215b5b492c051f
Ttungga <60680768+Ttungga@users.noreply.github.com>
2022-05-10 10:08:19 +0900

    1단계 - 학습 테스트 실습 (#346)
    * test: add tests for String.split method
    
    * test: add a test for String.substring method
    
    * test: rename methods and add @DisplayName annotations
    
    * test: add tests for String.charAt method
    
    * test: remove empty.txt from test package
    
    * refactor: remove unnecessary method variables and clarify charAt test intent
    
    * test: add a test for Set.size method
    
    * test: add a test for Set.contains method
    
    * test: add a test with @CsvSource annotation for Set.contains method

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,52 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class SetTest {
+    private static final String TRUE_STRING = "true";
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("빈 Set 객체에 1, 1, 2, 3을 add 하고 size 메서드를 호출했을 때 3이 반환되어야 한다")
+    void when_call_size_should_return_3() {
+        // when and then
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("빈 Set 객체에 1, 1, 2, 3을 add 하고 1, 2, 3을 파라미터로 contains 메서드를 호출했을 때 true가 반환되어야 한다")
+    void when_call_contains_for_elements_in_Set_should_return_true(final int element) {
+        // when and then
+        assertThat(numbers.contains(element)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("1, 2, 3이 들어있는 Set 객체에 contains 메서드를 호출하였을 때 파라미터에 따라 올바른 결과가 반환되어야 한다")
+    void when_call_contains_for_elements_in_Set_should_return_correct_result(final String input, final String expected) {
+        // when and then
+        assertThat(numbers.contains(Integer.parseInt(input))).isEqualTo(convertStringToBoolean(expected));
+    }
+
+    private boolean convertStringToBoolean(final String booleanString) {
+        if (booleanString.equals(TRUE_STRING)) return true;
+        return false;
+    }
+}
---
+++ src/test/java/StringTest.java
@@ -0,0 +1,67 @@
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class StringTest {
+    @Test
+    @DisplayName("문자열 '1,2'를 ','로 split 했을 때 '1'과 '2'로 문자열이 분리되어야 한다")
+    void when_split_string_should_return_array_with_two_elements() {
+        // given
+        final String given = "1,2";
+
+        // when and then
+        assertThat(given.split(",")).contains("1", "2");
+    }
+
+    @Test
+    @DisplayName("문자열 '1'을 ','로 split 했을 때 1만을 포함하는 배열이 반환되어야 한다")
+    void when_split_string_should_return_array_with_only_one_element() {
+        // given
+        final String given = "1";
+
+        // when and then
+        assertThat(given.split(",")).containsExactly("1");
+    }
+
+    @Test
+    @DisplayName("문자열 '(1,2)를 substring 했을 떄 문자열 양 끝의 괄호가 제거되어야 한다")
+    void when_substring_string_should_return_string_without_braces() {
+        // given
+        final String given = "(1,2)";
+
+        // when and then
+        assertThat(given.substring(1, 4)).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("문자열 'abc'에 charAt 했을 때 파라미터와 같은 위치의 문자가 반환되어야 한다")
+    void when_call_charAt_should_return_char() {
+        // given
+        final String given = "abc";
+        final Map<Integer, Character> indexValueMap = new HashMap<>();
+        indexValueMap.put(0, 'a');
+        indexValueMap.put(1, 'b');
+        indexValueMap.put(2, 'c');
+
+        // when and then
+        for (final Entry<Integer, Character> entry : indexValueMap.entrySet()) {
+            assertThat(given.charAt(entry.getKey())).isEqualTo(entry.getValue());
+        }
+    }
+
+    @Test
+    @DisplayName("문자열 'abc'에 charAt 했을 때 파라미터가 문자열 길이보다 크면 StringIndexOutOfBoundsException을 발생시킨다")
+    void when_call_charAt_with_parameter_exceeding_length_should_throw_StringIndexOutOfBoundsException() {
+        // given
+        final String given = "abc";
+
+        // when and then
+        assertThatThrownBy(() -> given.charAt(3))
+                .isInstanceOf(StringIndexOutOfBoundsException.class);
+    }
+}
