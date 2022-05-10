ca0af3cc12d496878a1c4c1cca2e41a57105cc0b
ssonsh <chosunci@gmail.com>
2022-05-10 16:23:38 +0900

    feat(StringAddCalculator) : 문자열 계산기에 전달되는 인수 정보의 유효성 검증을 처리한다. 숫자 이외의 값 또는 음수는 입력할 경우 RuntimeException이 발생된다.
    - StringSplitter 를 이용해 split한 결과를 바탕으로 validate 처리를 한다.
    
    - split결과는 양수만으로 이뤄져야 한다.
    
    - 그렇지 않은 경우 RuntimeException이 발생된다.

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,16 +1,29 @@
 package calculator;
 
+import calculator.constants.ErrorMessage;
+import calculator.utils.StringSplitter;
 import calculator.utils.StringUtils;
+import java.util.Arrays;
 
 public class StringAddCalculator {
 
     private static final Integer DEFAULT_RESULT = 0;
+    private static final String POSITIVE_NUMBER_REGEX = "^[0-9]+$";
 
     public static Integer splitAndSum(String text){
         if(StringUtils.isEmpty(text)){
             return DEFAULT_RESULT;
         }
+        String[] splitText = StringSplitter.split(text);
+        validateConsistOfPositiveNumbers(splitText);
 
         return DEFAULT_RESULT;
+    }
+
+    private static void validateConsistOfPositiveNumbers(String[] texts) {
+        boolean isAllPositiveNumbers = Arrays.stream(texts).allMatch(text -> text.matches(POSITIVE_NUMBER_REGEX));
+        if(!isAllPositiveNumbers){
+            throw new RuntimeException(String.format(ErrorMessage.INVALID_INPUT_VALUE, Arrays.toString(texts)));
+        }
     }
 }
---
+++ src/main/java/calculator/constants/ErrorMessage.java
@@ -0,0 +1,5 @@
+package calculator.constants;
+
+public class ErrorMessage {
+    public static final String INVALID_INPUT_VALUE = "[ERROR] 숫자 이외의 값 또는 음수는 입력할 수 없습니다. (input : %s)";
+}
