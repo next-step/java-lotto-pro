99067ee335aff44e0c401fced1574fe2b8b84005
최현지 <chj1768@gmail.com>
2022-05-11 00:21:25 +0900

    test: String 학습 테스트 작성 - 1을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지 확인

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -6,11 +6,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StringTest {
 	@Test
 	@DisplayName("1,2을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인")
-	public void test() {
+	public void test_split_contains_1_or_2() {
 		String[] underTest = "1,2".split(",");
 
 		assertThat(underTest).contains("1", "2");
 	}
 
+	@Test
+	@DisplayName("1을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지 확인")
+	public void test_split_contains_exactly_1() {
+		String[] underTest = "1".split(",");
+
+		assertThat(underTest).containsExactly("1");
+	}
 
 }
