370e16aeb7d2e53ae5727c60341b2ed96978c962
sangho lee <sangholee.dev@gmail.com>
2022-05-10 21:50:29 +0900

    StringAddCalculator splitAndSum 메소드 추가 및 빈 문자열 또는 null 입력시 0 반환 추가

--- src/main/java/stringaddcalculator/StringAddCalculator.java
+++ src/main/java/stringaddcalculator/StringAddCalculator.java
@@ -1,4 +1,19 @@
 package stringaddcalculator;
 
 public class StringAddCalculator {
+    public static int splitAndSum(final String text) {
+        if (validateNullOrEmpty(text)) {
+            return 0;
+        }
+
+        throw new RuntimeException("문자열 계산을 할 수 없습니다.");
+    }
+
+    private static boolean validateNullOrEmpty(final String text) {
+        if (text == null) {
+            return true;
+        }
+        return text.isEmpty();
+    }
+
 }
