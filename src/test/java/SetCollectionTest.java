9fc812b329e06f246f1625a8c112ff7c25201eb1
limwoobin <drogba02@naver.com>
2022-05-10 20:28:38 +0900

    test: Set Collection contains method  테스트 작성
    - Collection 에 존재하면 true, 아니라면 false 를 리턴하는 로직을 검증하라

--- src/test/java/SetCollectionTest.java
+++ src/test/java/SetCollectionTest.java
@@ -1,4 +1,5 @@
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -7,6 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("Set Collection 에 대한 테스트")
@@ -42,6 +44,14 @@ class SetCollectionTest {
         @ValueSource(ints = {1, 2, 3})
         void set_collection_contains_test(int param) {
             assertThat(numbers).contains(param);
+        }
+
+        @DisplayName("해당 요소가 Set Collection 에 존재하면 true, 아니라면 false 를 반환해야 한다")
+        @ParameterizedTest
+        @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+        void set_collection_contains_test_2(int input, boolean expected) {
+            boolean actualValue = numbers.contains(input);
+            assertEquals(expected, actualValue);
         }
     }
 }
