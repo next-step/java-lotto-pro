54048d9abebeb7c0350fb928932cf64f12489c64
Lisc <qqwer153@gmail.com>
2022-05-11 02:46:21 +0900

    feat: 입력받은 문자열 예외처리 기능 구현

---
+++ src/main/java/StringAddCalculator.java
@@ -0,0 +1,12 @@
+public class StringAddCalculator {
+    public static int splitAndSum(String input) {
+        if (isNullOrEmpty(input)) {
+            return 0;
+        }
+        return 1;
+    }
+
+    private static boolean isNullOrEmpty(String input) {
+        return input == null || input.isEmpty();
+    }
+}
