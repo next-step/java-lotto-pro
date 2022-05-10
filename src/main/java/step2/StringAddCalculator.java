e81ec45819a6a98b3e0ccb9835d83de7c2ca1b1f
piopoi <kangsw1988@gmail.com>
2022-05-10 23:20:17 +0900

    feat(StringAddCalculator): 여러 숫자를 컴마를 구분자로 입력할 경우 숫자들의 합을 반환하는 기능 추가
    메서드명 변경
    - isEmptyString -> isEmpty
    - isSingleNumber -> isNumber

--- src/main/java/step2/StringAddCalculator.java
+++ src/main/java/step2/StringAddCalculator.java
@@ -1,27 +1,43 @@
 package step2;
 
+import java.util.Arrays;
+
 public class StringAddCalculator {
 
+    private static final String DELEMETER = ",";
+
     public static int splitAndSum(String inputString) {
-        if (isEmptyString(inputString)) {
+        if (isEmpty(inputString)) {
             return 0;
         }
-        if (isSingleNumber(inputString)) {
+        if (isNumber(inputString)) {
             return Integer.parseInt(inputString);
         }
-        return -1;
+
+        String[] numbers = splitString(inputString);
+        return sum(numbers);
     }
 
-    private static boolean isEmptyString(String source) {
+    private static boolean isEmpty(String source) {
         return source == null || source.isEmpty();
     }
 
-    private static boolean isSingleNumber(String source) {
+    private static boolean isNumber(String source) {
         try {
             Integer.parseInt(source);
             return true;
         } catch (NumberFormatException e) {
             return false;
         }
+    }
+
+    private static String[] splitString(String inputString) {
+        return inputString.split(DELEMETER);
+    }
+
+    private static int sum(String[] numbers) {
+        return Arrays.stream(numbers)
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 }
