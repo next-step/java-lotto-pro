a424281d47262befe262fcbc5415ed62ca68a629
tyakamyz <tyakamyz@naver.com>
2022-05-10 17:25:33 +0900

    feat: 커스텀 구분자일 경우. 문자 계산 처리

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,12 +1,19 @@
 package calculator;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringAddCalculator {
     public static int splitAndSum(String text) {
 
         if (isValidationCheckNullOrEmpty(text)) {
             return 0;
+        }
+
+        Integer result = customDelimiterAddCalculator(text);
+        if (result != null) {
+            return result;
         }
 
         return standardStringAddCalculator(text);
@@ -20,5 +27,17 @@ public class StringAddCalculator {
         String[] tokens = text.split(",|:");
 
         return Arrays.stream(tokens).mapToInt(Integer::parseInt).sum();
+    }
+
+    private static Integer customDelimiterAddCalculator(String text) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            String[] tokens = m.group(2).split(customDelimiter);
+
+            return Arrays.stream(tokens).mapToInt(Integer::parseInt).sum();
+        }
+
+        return null;
     }
 }
