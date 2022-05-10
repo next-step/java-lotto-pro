ba6745bbb855b1976a2f545e08b46770953aa6c2
Celina <luv.mins99@gmail.com>
2022-05-10 19:41:08 +0900

    test(SetTest): Set 컬렉션의 size에 대한 학습 테스트

---
+++ src/test/java/step1/SetTest.java
@@ -0,0 +1,32 @@
+package step1;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("size() 메소드를 활용해 Set의 크기를 확인하는 학습테스트")
+    void setTest_size() {
+        // given & when
+        int size = numbers.size();
+
+        // then
+        assertThat(size).isEqualTo(3);
+    }
+}
