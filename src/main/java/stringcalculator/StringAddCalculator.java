aa064d904fca6cf74a040f71f2687060aaee96f3
toughchb <ggang2000@gmail.com>
2022-05-10 21:26:43 +0900

    feat(stringcalculator): 커스텀 구분자 기능 추가

--- src/main/java/stringcalculator/StringAddCalculator.java
+++ src/main/java/stringcalculator/StringAddCalculator.java
@@ -2,10 +2,15 @@ package stringcalculator;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringAddCalculator {
 
     private static final String DEFAULT_DELIMITERS = "[,:]";
+    public static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
+    public static final int CUSTOM_DELIMITER_GROUP = 1;
+    public static final int NUMBERS_GROUP = 2;
 
     public static int splitAndSum(String input) {
         if (isBlank(input)) {
@@ -20,6 +25,11 @@ public class StringAddCalculator {
     }
 
     private static String[] split(String input) {
+        Matcher m = CUSTOM_DELIMITER_PATTERN.matcher(input);
+        if (m.find()) {
+            String customDelimiter = m.group(CUSTOM_DELIMITER_GROUP);
+            return m.group(NUMBERS_GROUP).split(customDelimiter);
+        }
         return input.split(DEFAULT_DELIMITERS);
     }
 
