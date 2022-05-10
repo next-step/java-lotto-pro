ba44e83080550f97589749d0d1c8426a21207676
WonChul <heowc1992@gmail.com>
2022-05-10 17:04:27 +0900

    feat(StringAddCalculator): StringAddCalculator 클래스에 대한 기능 구현
    - 요구사항에 만족하는 코드 구현

--- src/main/java/step2/StringAddCalculator.java
+++ src/main/java/step2/StringAddCalculator.java
@@ -1,7 +1,35 @@
 package step2;
 
-public class StringAddCalculator {
-    public static int splitAndSum(String value) {
-        return -1;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class StringAddCalculator {
+
+    private static final Pattern DEFAULT_DELIMITER_PATTERN = Pattern.compile("[,:]");
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\\n(.*)");
+
+    static int splitAndSum(String value) {
+        if (value == null || value.isEmpty()) {
+            return 0;
+        }
+
+        final String[] maybeNumbers;
+        final Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(value);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            maybeNumbers = matcher.group(2).split(customDelimiter);
+        } else {
+            maybeNumbers = DEFAULT_DELIMITER_PATTERN.split(value);
+        }
+
+        int total = 0;
+        for (String maybeNumber : maybeNumbers) {
+            int number = Integer.parseInt(maybeNumber);
+            if (number < 0) {
+                throw new RuntimeException("음수는 가질 수 없습니다.");
+            }
+            total += number;
+        }
+        return total;
     }
 }
