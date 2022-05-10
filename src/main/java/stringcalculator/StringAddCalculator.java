9f713a22f6eb0a3c3d5966d4bedca30556160403
toughchb <ggang2000@gmail.com>
2022-05-10 19:50:40 +0900

    feat(stringcalculator): 빈 문자열 또는 null 값일때 0 리턴 구현

---
+++ src/main/java/stringcalculator/StringAddCalculator.java
@@ -0,0 +1,14 @@
+package stringcalculator;
+
+public class StringAddCalculator {
+    public static int splitAndSum(String input) {
+        if (isNull(input) || input.isEmpty()) {
+            return 0;
+        }
+        return 0;
+    }
+
+    private static boolean isNull(String input) {
+        return input == null;
+    }
+}
