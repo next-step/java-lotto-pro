2565e1502e0ff72eaad16b4a54256fcd4c7e0dad
boss.back <boss.back@kakaocorp.com>
2022-05-11 00:54:58 +0900

    feat: 요구사항2 ( substring 테스트 ) 구현

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,6 +1,7 @@
 package study;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.in;
 
 import org.junit.jupiter.api.Test;
 
@@ -16,6 +17,13 @@ public class StringTest {
     void splitOneElement() {
         String[] result = "1" .split(",");
         assertThat(result).containsExactly("1");
+    }
+
+    @Test
+    void substringInput() {
+        String input = "(1,2)";
+        String result = input.substring(1, input.length() - 1);
+        assertThat(result).isEqualTo("1,2");
     }
 
 }
