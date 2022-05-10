a7a1c8b954af084a3ae70723126b162c12b258ad
Woody <yuncheol92@naver.com>
2022-05-10 21:00:40 +0900

    feat: null 또는 빈문자열 일 때 0값 리턴

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,8 +1,20 @@
 package calculator;
 
+import java.util.Objects;
+
 public class StringAddCalculator {
 
+    private static final int ZERO = 0;
+
     public static int splitAndSum(final String expression) {
-        return 0;
+        if (isNullOrEmpty(expression)) {
+            return ZERO;
+        }
+
+        return ZERO;
+    }
+
+    private static boolean isNullOrEmpty(final String expression) {
+        return Objects.isNull(expression) || expression.isEmpty();
     }
 }
