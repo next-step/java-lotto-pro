286480df62470e151d76fd74c73f44aecba038d6
jaesungahn91 <jaesungahn91@gmail.com>
2022-05-11 00:57:02 +0900

    test: 요구사항 3, @DisplayName 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,6 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -12,6 +13,7 @@ public class StringTest {
 
     private static final String SEPARATOR = ",";
 
+    @DisplayName("\"1,2\"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다.")
     @Test
     void split_1() {
         String[] result = "1,2".split(SEPARATOR);
@@ -21,6 +23,7 @@ public class StringTest {
         assertThat(result).containsExactly("1", "2");
     }
 
+    @DisplayName("\"1\"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.")
     @Test
     void split_2() {
         String[] result = "1".split(SEPARATOR);
@@ -29,6 +32,7 @@ public class StringTest {
         assertThat(result).containsExactly("1");
     }
 
+    @DisplayName("\"(1,2)\" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 \"1,2\"를 반환하도록 구현한다.")
     @Test
     void substring() {
         String input = "(1,2)";
@@ -39,6 +43,7 @@ public class StringTest {
         assertThat(result).isEqualTo(expected);
     }
 
+    @DisplayName("\"abc\" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다.")
     @ParameterizedTest
     @CsvSource({"0,a", "1,b", "2,c"})
     void charAt_1(int index, char expected) {
@@ -48,6 +53,7 @@ public class StringTest {
         assertThat(result).isEqualTo(expected);
     }
 
+    @DisplayName("String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현한다.")
     @Test
     void charAt_2_exception() {
         String input = "abc";
