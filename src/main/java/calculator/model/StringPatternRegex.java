4884813c782fe3fdd5a8eaa341b5f6a784cac178
bestsilver <dkvkxm99@gmail.com>
2022-05-11 02:13:39 +0900

    feat: StringPatternRegex (text의 종류를 구분하기 위한 정규표현식들 정의)

---
+++ src/main/java/calculator/model/StringPatternRegex.java
@@ -0,0 +1,6 @@
+package calculator.model;
+
+public class StringPatternRegex {
+    public static final String FIXEDPATTERN = "(\\d+(,|:)?)+";
+    public static final String CUSTOMPATTERN = "//(.)\n(.*)";
+}
