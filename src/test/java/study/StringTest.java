02c3590d0d6105a94faeec6a83ca4f5ea4112763
breezeunn <je7689@gmail.com>
2022-05-10 22:22:27 +0900

    test: displayName 적용 및 메서드명 수정

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,6 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,19 +8,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StringTest {
 
     @Test
-    void 문자열_콤마로_split() {
+    @DisplayName("문자열 콤마 split 테스트")
+    void split() {
         String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1", "2");
     }
 
     @Test
-    void 구분자가_없는_문자열의_split() {
+    @DisplayName("콤마가 없는 문자열의 split 테스트")
+    void splitStringWithNoDelimiter() {
         String[] result = "1".split(",");
         assertThat(result).contains("1");
     }
 
     @Test
-    void 문자열_처음과_끝_괄호_제거() {
+    @DisplayName("문자열 처음과 끝 괄호 제거 테스트")
+    void getStringWithoutBrackets() {
         String testData = "(1,2)";
         String result = testData.substring(1, testData.length()-1);
         assertThat(result).isEqualTo("1,2");
