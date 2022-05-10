a0370662ae795d166597bb1970ac9faa8c5f121e
kbh0581 <kbh0581@gmail.com>
2022-05-10 23:00:57 +0900

    feat: 문자열이 빈문자열 일 경우 0을 반환
    - StringUtils 문자열에 대한 Util 클래스
     - 공백이나 null일시 판별

---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,14 @@
+package calculator;
+
+import calculator.util.StringUtils;
+
+public class StringAddCalculator {
+
+    public static int splitAndSum(String text) {
+        if (StringUtils.isEmptyString(text)) {
+            return 0;
+        }
+        return 1;
+    }
+
+}
