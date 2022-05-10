4fddd2469120ef78c35c99e4637e86c141348afe
soob-forest <dltntnqo@naver.com>
2022-05-11 00:22:00 +0900

    feat(StringAddCalculator): splitAndSum 메서드의 입력값이 숫자하나일 경우 해당 숫자 반환

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -5,11 +5,18 @@ public class StringAddCalculator {
         if (isNullOrEmptyInput(input)) {
             return 0;
         }
-
+        if (isNumeric(input)) {
+            return Integer.parseInt(input);
+        }
         throw new RuntimeException();
     }
 
     private static boolean isNullOrEmptyInput(String input) {
         return input == null || input.isEmpty();
     }
+
+    private static boolean isNumeric(String input) {
+        return input.matches("\\p{Digit}");
+    }
+
 }
