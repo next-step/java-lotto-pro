c70818140934057eb0e23e7fa2d6fb9b9f416e8d
sangho lee <sangholee.dev@gmail.com>
2022-05-10 22:22:46 +0900

    StringAddCalculator 커스텀 구분자를 지정하는 테스트 케이스 추가

--- src/test/java/stringaddcalculator/StringAddCalculatorTest.java
+++ src/test/java/stringaddcalculator/StringAddCalculatorTest.java
@@ -33,4 +33,9 @@ class StringAddCalculatorTest {
         assertThat(result).isEqualTo(6);
     }
 
+    @Test
+    public void splitAndSum_custom_구분자() throws Exception {
+        int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
+        assertThat(result).isEqualTo(6);
+    }
 }
