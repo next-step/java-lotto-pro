66f328e49084ba40f94b6ddb75bc6f2c36d50c7c
limwoobin <drogba02@naver.com>
2022-05-10 20:21:45 +0900

    test: Set Collection contains method  테스트 작성

--- src/test/java/SetCollectionTest.java
+++ src/test/java/SetCollectionTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("Set Collection 에 대한 테스트")
 class SetCollectionTest {
@@ -28,6 +30,18 @@ class SetCollectionTest {
         @DisplayName("Set Collection 의 Size 가 정상적으로 반환되어야 한다")
         void set_collection_size_test() {
             assertThat(numbers).hasSize(3);
+        }
+    }
+
+    @Nested
+    @DisplayName("Set Collection Contains 테스트")
+    class SetContainsTest {
+
+        @DisplayName("해당 요소가 Set Collection 에 존재하면 정상적으로 통과되어야 한다")
+        @ParameterizedTest
+        @ValueSource(ints = {1, 2, 3})
+        void set_collection_contains_test(int param) {
+            assertThat(numbers).contains(param);
         }
     }
 }
