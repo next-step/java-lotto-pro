18889f08b898612eccfa67a6aab96e79b81f3c0e
mukeunzi <eunji057@gmail.com>
2022-05-11 01:59:20 +0900

    feat: String 클래스 요구사항2 테스트코드 작성 완료
    - substring()에 대한 테스트

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -12,4 +12,10 @@ public class StringTest {
         assertThat("1,2".split(separator)).containsExactly("1", "2");
         assertThat("1,".split(separator)).containsExactly("1");
     }
+
+    @Test
+    @DisplayName("String을 substring했을 때 문자열이 잘 제거된다.")
+    void checkSubstring() {
+        assertThat("(1,2)".substring(1, 4)).isEqualTo("1,2");
+    }
 }
