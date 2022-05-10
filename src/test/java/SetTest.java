e15c9b24421b9f531dab54d878af2273d66ea349
Lisc <qqwer153@gmail.com>
2022-05-10 17:40:07 +0900

    Step1 - 학습 테스트 실습 (#345)
    * test: StringTest 추가
    
    * test: SetTest 추가

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("size() 메소드를 활용해 set의 크기 확인")
+    void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @DisplayName("contains() 메소드를 활용해 1,2,3의 값이 존재하는지 확인")
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int number) {
+        assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @DisplayName("1,2,3 값은 true, 4,5 값을 넣으면 false 가 반환되는지 확인")
+    @CsvSource(value = {"1,true", "2,true", "3,true", "4,false", "5,false"})
+    void contains_true_false(int number, boolean expected) {
+        assertThat(numbers.contains(number)).isEqualTo(expected);
+    }
+}
---
+++ src/test/java/StringTest.java
@@ -0,0 +1,49 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringTest {
+    @Test
+    @DisplayName("1,2를 ,로 split 했을 때 1과 2로 잘 분리되는지 확인")
+    void split_1_2() {
+        String[] result = "1,2".split(",");
+        assertThat(result).containsExactly("1","2");
+    }
+
+    @Test
+    @DisplayName("1을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지 확인")
+    void split_1() {
+        String[] result = "1".split(",");
+        assertThat(result).containsExactly("1");
+    }
+
+    @Test
+    @DisplayName("(1,2) 값이 주어졌을 때 substring() 메소드를 활용해 1,2 반환되는지 확인")
+    void substring() {
+        String result = "(1,2)".substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @ParameterizedTest
+    @DisplayName("abc 값이 주어졌을 때 charAt() 메소드를 활용해 특정 위치의 문자 확인")
+    @CsvSource(value = {"0,a", "1,b", "2,c"})
+    void charAt(int index, char expected) {
+        char alphabet = "abc".charAt(index);
+        assertThat(alphabet).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @DisplayName("charAt() 메소드를 활용해 특정 위치 값 벗어났을 때 Exception 발생 확인")
+    @ValueSource(ints = {-1, 4})
+    void charAtException(int index) {
+        assertThatThrownBy(() -> {
+            "abc".charAt(index);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: %d", index);
+    }
+}
