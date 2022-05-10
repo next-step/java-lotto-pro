99437edba0fc9aed463d038f89513d26b16ffd5c
boss.back <boss.back@kakaocorp.com>
2022-05-11 01:18:10 +0900

    feat: set 요구사항2 ( Set 의 원소를 확인한다 ) 구현

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -7,6 +7,8 @@ import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
 
@@ -27,4 +29,10 @@ public class SetTest {
         assertThat(numbers.size()).isEqualTo(3);
     }
 
+    @DisplayName("Set 에 속한 원소를 확인한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void findElement(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
 }
