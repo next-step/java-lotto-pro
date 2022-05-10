46f5a5d5635ac5babf638c518c200ca32edad294
DongGyuJang <56859781+EastStar1129@users.noreply.github.com>
2022-05-10 17:47:01 +0900

    feat: String 클래스에 대한 학습 테스트 구현 (#331)
    * feat: String 클래스에 대한 학습 테스트 구현
    
    * test: Set Collection Test추가 및 리뷰반영

---
+++ src/test/java/util/SetUtilTest.java
@@ -0,0 +1,46 @@
+package util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class SetUtilTest {
+	private Set<Integer> numbers;
+
+	@BeforeEach
+	void setUp() {
+		numbers = new HashSet<>();
+		numbers.add(1);
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+	}
+	
+	@Test
+	@DisplayName("Set Collection의 사이즈를 확인하는 테스트")
+	void setSize_test() {
+		assertThat(numbers.size()).isEqualTo(3);
+	}
+	
+	@ParameterizedTest
+	@ValueSource(ints = {1, 2, 3})
+	@DisplayName("Set Collection의 존재하는 요소를 확인하는 테스트")
+	void contains_test(int number){
+        assertThat(numbers.contains(number)).isTrue();
+	}
+	
+	@ParameterizedTest
+	@CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+	@DisplayName("Set Collection의 요소가 존재하는지 확인하는 테스트")
+	void contains_test(int number, boolean isExists){
+        assertThat(numbers.contains(number)).isEqualTo(isExists);
+	}
+}
