d2195b65975365d198a669559db73217a83c1145
ssonsh <chosunci@gmail.com>
2022-05-11 12:42:05 +0900

    feat(LottoNumberGenerator) : 로또번호를 생성하는 Generator 구현 로또 번호를 구성할 범위의 숫자를 미리 정의해두고 이 숫자 컬렉션을 shuffle 하여 6개의 번호를 생성해낸다.
    - 로또 번호 생성과 관련한 상수 값들은 별도의 Constants 로 묶어낸다.

---
+++ src/main/java/generator/LottoNumberGenerator.java
@@ -0,0 +1,34 @@
+package generator;
+
+import calculator.constants.LottoNumberConstants;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoNumberGenerator implements NumberGenerator{
+
+    private final List<Integer> lottoBaseNumbers;
+
+    public LottoNumberGenerator() {
+        this.lottoBaseNumbers = initializeLottoBaseNumbers();
+    }
+
+    private List<Integer> initializeLottoBaseNumbers() {
+        List<Integer> initializeLottoNumbers = new ArrayList<>();
+        for (int i = LottoNumberConstants.LOTTO_NUMBER_MIN; i <= LottoNumberConstants.LOTTO_NUMBER_MAX; i++) {
+            initializeLottoNumbers.add(i);
+        }
+        return initializeLottoNumbers;
+    }
+
+    @Override
+    public List<Integer> generate() {
+        Collections.shuffle(this.lottoBaseNumbers);
+        List<Integer> generateLottoNumbers = new ArrayList<>();
+        for (int i = 0; i < LottoNumberConstants.LOTTO_NUMBER_SIZE; i++) {
+            generateLottoNumbers.add(this.lottoBaseNumbers.get(i));
+        }
+        Collections.sort(generateLottoNumbers);
+        return generateLottoNumbers;
+    }
+}
