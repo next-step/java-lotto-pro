f85b0ab4d60ea2d5298036cf91686cf3b5d9fa58
leejeongyong <jeongyolee@ebay.com>
2022-05-11 12:28:43 +0900

    feat: Set Collection Test 요구사항 1번(size)

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,25 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void size() {
+        // 요구사항 1번
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+}
