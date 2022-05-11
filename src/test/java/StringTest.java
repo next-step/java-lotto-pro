e112740e0853b050738e48dc62ea0927458837b9
leejeongyong <jeongyolee@ebay.com>
2022-05-11 12:23:27 +0900

    feat: String Class Test 요구사항 3번(charAt_Exception)

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -1,6 +1,8 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -18,12 +20,22 @@ public class StringTest {
         assertThat(arraysForOneAndTwo).containsExactly("1", "2"); // 순서 포함해서 정확하게 일치
     }
 
-
     @Test
     void substring() {
         //요구사항 2번
         String example = "(1,2)";
         example = example.substring(1, 4);
         assertThat(example).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt Throw IndexOutOfBoundsException Test")
+    void charAt_Exception() {
+        //요구사항 3번
+        assertThatThrownBy(() -> {
+            String example = "abc";
+            example.charAt(4); //StringIndexOutOfBoundsException: String index out of range: 4
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+            .hasMessageContaining("index");
     }
 }
