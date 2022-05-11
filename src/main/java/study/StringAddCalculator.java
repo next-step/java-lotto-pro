a563bd0338945e2fc4157da76e343b917ebc851f
wenodev <logicalstark1105@gmail.com>
2022-05-11 23:35:55 +0900

    feat(StringAddCalculator.java): 컴마 구분자로 입력할 경우 두 숫자의 합을 반환한다

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -1,5 +1,7 @@
 package study;
 
+import java.util.Arrays;
+
 public class StringAddCalculator {
     private static final int DEFAULT_NUMBER = 0;
 
@@ -7,6 +9,10 @@ public class StringAddCalculator {
         if (str == null || str.isEmpty()) {
             return DEFAULT_NUMBER;
         }
-        return Integer.parseInt(str);
+
+        return Arrays.stream(str.split(","))
+                .map(s -> Integer.parseInt(s))
+                .mapToInt(i -> i)
+                .sum();
     }
 }
