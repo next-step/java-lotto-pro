ff6e9cb99822c81d6b74e66fd2e9dde844cb0ba5
KangSeongHyo <abc9023@naver.com>
2022-05-11 20:05:24 +0900

    feat : 구매 금액이 1000만원 이상인 경우 검증 메서드 추가
    - validateMoney 메서드로 분리하여 통합처리
    - 1000만원 이상인 경우 IllegalArgumentException

--- src/main/java/lotto/constant/ErrorMessage.java
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -4,6 +4,7 @@ public class ErrorMessage {
     private static final String ERROR_HEAD = "[ERROR] ";
 
     public static final String LACK_MONEY = ERROR_HEAD + "로또 최소 가격은 1000원 입니다.";
+    public static final String OVER_MONEY = ERROR_HEAD + "로또 구매 최대 가격은 10_000_000원 입니다.";
     public static final String CONSTANT_CLASS = "Constant class";
 
     private ErrorMessage() {
--- src/main/java/lotto/model/LottoPlayService.java
+++ src/main/java/lotto/model/LottoPlayService.java
@@ -6,11 +6,19 @@ import lotto.vo.Lottos;
 public class LottoPlayService {
 
     private static final int LOTTO_PRICE = 1000;
+    private static final int LOTTO_MAX_PURCHASE_PRICE = 10_000_000;
+
 
     public Lottos convertMoneyToLottos(int money) {
-        validateLackMoney(money);
+        validateMoney(money);
+
         int playCount = money / LOTTO_PRICE;
         return new Lottos(playCount);
+    }
+
+    private void validateMoney(int money) {
+        validateLackMoney(money);
+        validateOverMoney(money);
     }
 
     private void validateLackMoney(int money) {
@@ -18,4 +26,11 @@ public class LottoPlayService {
             throw new IllegalArgumentException(ErrorMessage.LACK_MONEY);
         }
     }
+
+    private void validateOverMoney(int money) {
+        if(money >= LOTTO_MAX_PURCHASE_PRICE){
+            throw new IllegalArgumentException(ErrorMessage.OVER_MONEY);
+        }
+    }
+
 }
