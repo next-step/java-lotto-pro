bde5e98e08e34e1ea1ecc5950185cc0481798a3f
KangSeongHyo <abc9023@naver.com>
2022-05-11 19:50:59 +0900

    test : 구매 금액이 1000원 미만인 경우 검증 테스트 추가

--- src/test/java/lotto/model/LottoPlayServiceTest.java
+++ src/test/java/lotto/model/LottoPlayServiceTest.java
@@ -3,10 +3,13 @@ package lotto.model;
 import lotto.vo.Lottos;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class LottoPlayServiceTest {
 
@@ -23,6 +26,16 @@ class LottoPlayServiceTest {
     void convertMoneyToLottos(int money, int expectedPlayCount) {
         Lottos lottos = lottoPlayService.convertMoneyToLottos(money);
         assertEquals(expectedPlayCount, lottos.getPlayCount());
+    }
+
+    @DisplayName("구매 금액이 1000원 미만(로또 최소 금액)인 경우")
+    @ParameterizedTest
+    @ValueSource(ints = {100, 0, 900})
+    void convertMoneyToLottos_low_money(int money) {
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> lottoPlayService.convertMoneyToLottos(money))
+                .withMessage("[ERROR] 로또 최소 가격은 1000원 입니다.");
     }
 
 }
