8895148eee88c77acebbd2801ce009dce0d9a0db
ssonsh <chosunci@gmail.com>
2022-05-11 22:35:59 +0900

    test(lotto.domain.*) 추가된 LottoNumbers 의 생성방법에 대해 테스트 코드를 작성한다.
    - LottoNumberStringSplitter 테스트
    
    - LottoNumberStringToIntegerParserTest 테스트
    
    - LottoNumbers 생성 시 유효성 검증 로직 테스트

--- src/test/java/lotto/domain/LottoNumbersTest.java
+++ src/test/java/lotto/domain/LottoNumbersTest.java
@@ -6,6 +6,9 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import generator.LottoNumberGenerator;
 import generator.TestNumberGenerator;
 import java.util.Arrays;
+import java.util.List;
+import lotto.utils.LottoNumberStringSplitter;
+import lotto.utils.LottoNumberStringToIntegerParser;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -29,5 +32,35 @@ class LottoNumbersTest {
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessageContaining("[ERROR]")
             .hasMessageContaining(String.valueOf(Arrays.asList(1, 1, 2, 2, 3, 3)));
+    }
+
+    @Test
+    @DisplayName("지난 주 당첨 번호를 입력받아 LottoNumbers 일급 컬랙션을 생성한다.")
+    void generate02() {
+        // given
+        String text = "1, 2, 3, 4, 5, 6";
+        String[] splitText = LottoNumberStringSplitter.split(text);
+
+        // when
+        List<Integer> lottoNumbers = LottoNumberStringToIntegerParser.parse(splitText);
+        LottoNumbers lotto = LottoNumbers.generateBy(lottoNumbers);
+
+        // then
+        assertThat(lotto.getReadOnlyLottoNumbers()).hasSize(6);
+    }
+
+    @Test
+    @DisplayName("입력한 지난 주 당첨 번호가 로또 번호의 범위가 아닌 경우 Exception이 발생한다.")
+    void exception02() {
+        // given
+        String text = "-1, 2, 3, 4, 5, 66";
+        String[] splitText = LottoNumberStringSplitter.split(text);
+
+        // when & then
+        List<Integer> lottoNumbers = LottoNumberStringToIntegerParser.parse(splitText);
+        assertThatThrownBy(() -> LottoNumbers.generateBy(lottoNumbers))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("[ERROR]")
+            .hasMessageContaining("-1");
     }
 }
