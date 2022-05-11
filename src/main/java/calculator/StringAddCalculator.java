57be32d42f876c3b0aa95333185e8d51f2fac07c
songsimo <chy.song89@gmail.com>
2022-05-11 23:49:02 +0900

    feat: null 또는 빈문자열 합은 0 리턴 구현

---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,15 @@
+package calculator;
+
+public class StringAddCalculator {
+	public static int splitAndSum(String strNumbers) {
+		if(isNullOrEmpty(strNumbers)) {
+			return 0;
+		}
+
+		return 8282;
+	}
+
+	private static boolean isNullOrEmpty(String strNumbers) {
+		return strNumbers == null || strNumbers.isEmpty();
+	}
+}
