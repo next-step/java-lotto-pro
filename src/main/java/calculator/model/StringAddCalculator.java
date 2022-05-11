954392073a7ed255e3e42f8d1450636cc4874730
bestsilver <dkvkxm99@gmail.com>
2022-05-12 01:18:48 +0900

    refactor:
    - text의 매칭값에 따라 StringPattern을 리턴해주는 기능을 StringPattern 클래스 내부로 이동
    - 정수 책임을 가지는 클래스로 분리하기 위해 Numbers 구현 후 분리

--- src/main/java/calculator/model/StringAddCalculator.java
+++ src/main/java/calculator/model/StringAddCalculator.java
@@ -1,9 +1,5 @@
 package calculator.model;
 
-import exception.IllegalArgument;
-
-import java.util.Arrays;
-
 public class StringAddCalculator {
 
     public static int splitAndSum(String text) throws RuntimeException{
@@ -11,35 +7,14 @@ public class StringAddCalculator {
             return 0;
         }
 
-        StringPattern pattern = StringAddCalculator.determineStringPattern(text);
+        StringPattern pattern = StringPattern.determine(text);
         Separator separator = new Separator(pattern, text);
-
-        return StringAddCalculator.sumNumberList(separator.separate());
+        Numbers numbers = new Numbers(separator.separate());
+        return numbers.sumNumbers();
     }
 
     public static boolean isEmpty(String text) {
         return (text == null) || text.isEmpty();
-    }
-
-    public static StringPattern determineStringPattern(String text) {
-        if (text.matches(StringPatternRegex.FIXEDPATTERN)) {
-            return StringPattern.FIXED;
-        }
-
-        if (text.matches(StringPatternRegex.CUSTOMPATTERN)) {
-            return StringPattern.CUSTOM;
-        }
-
-        return StringPattern.NOTHING;
-    }
-
-    public static int sumNumberList(String[] list) throws RuntimeException {
-        try{
-            return Arrays.stream(list).mapToInt(Integer::parseInt).sum();
-        } catch(NumberFormatException e) {
-            throw IllegalArgument.NEGATIVE_OR_NOT_NUMBER;
-        }
-
     }
 
 }
