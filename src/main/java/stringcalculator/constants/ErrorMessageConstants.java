c6e0204fdd6251a8a45a92b83c137ec210d6eaae
toughchb <ggang2000@gmail.com>
2022-05-12 07:50:12 +0900

    refactor: 리뷰반영 - util, constants의 생성자 private화

--- src/main/java/stringcalculator/constants/ErrorMessageConstants.java
+++ src/main/java/stringcalculator/constants/ErrorMessageConstants.java
@@ -2,6 +2,9 @@ package stringcalculator.constants;
 
 public class ErrorMessageConstants {
 
+    private ErrorMessageConstants() {
+    }
+
     public static final String NON_POSITIVE_INTEGER_INPUT_ERROR = "양수의 정수를 입력 하세요.";
 
 }
