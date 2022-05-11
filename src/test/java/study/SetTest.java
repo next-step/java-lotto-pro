c2eecadda7dbf9810bc9df2a2453425b97e9c0a8
jaesungahn91 <jaesungahn91@gmail.com>
2022-05-11 13:40:19 +0900

    test: ParameterizedTest 사용한 true, false 테스트

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -34,6 +35,13 @@ public class SetTest {
     @ValueSource(ints = {1, 2, 3})
     void contains(int number) {
         assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @DisplayName("1, 2, 3 값은 contains 메소드 실행결과 true, 4, 5 값을 넣으면 false 가 반환되는 테스트.")
+    @ParameterizedTest
+    @CsvSource({"1,true", "2,true", "3,true", "4,false", "5,false"})
+    void contains_ParameterizedTest(int number, boolean expected) {
+        assertThat(numbers.contains(number)).isEqualTo(expected);
     }
 
 }
