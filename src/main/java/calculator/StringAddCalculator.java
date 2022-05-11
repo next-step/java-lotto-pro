a7348749cb248a3e1277098f88a766fa12f4006d
Celina <luv.mins99@gmail.com>
2022-05-12 00:54:39 +0900

    feat(StringAddCalculator): 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환하는 기능 구현

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -2,16 +2,29 @@ package calculator;
 
 public class StringAddCalculator {
     private static final int MIN_VALUE = 0;
+    private static final int SINGLE_DIGIT = 1;
 
     public static int splitAndSum(String input) {
         if (isNullOrEmpty(input)) {
             return MIN_VALUE;
         }
 
-        return MIN_VALUE;
+        return splitValue(input);
     }
 
     private static boolean isNullOrEmpty(String input) {
         return input == null || input.isEmpty();
+    }
+
+    private static int splitValue(String input) {
+        if (isSingleDigit(input)) {
+            return Integer.parseInt(input);
+        }
+
+        return Integer.parseInt(input);
+    }
+
+    private static boolean isSingleDigit(String input) {
+        return input.length() == SINGLE_DIGIT;
     }
 }
