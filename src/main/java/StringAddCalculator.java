fe91bcf662266e11256953bc13ecaca3f199734d
최현지 <chj1768@gmail.com>
2022-05-11 22:16:09 +0900

    refactor: rename 파라미터명, rename 메서드 내 지역변수(strs)

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -2,24 +2,24 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
-    public static int splitAndSum(String str) {
-        if (str == null || str.isEmpty())
+    public static int splitAndSum(String text) {
+        if (text == null || text.isEmpty())
             return 0;
 
         int sum = 0;
-        String[] strs;
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(str);
+        String[] tokens;
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
         if (m.find()) {
             String customDelimiter = m.group(1);
-            strs = m.group(2).split(customDelimiter);
-            for (String s : strs)
+            tokens = m.group(2).split(customDelimiter);
+            for (String s : tokens)
                 sum += Integer.parseUnsignedInt(s);
 
             return sum;
         }
 
-        strs = str.split(",|:");
-        for (String s : strs)
+        tokens = text.split(",|:");
+        for (String s : tokens)
             sum += Integer.parseUnsignedInt(s);
 
         return sum;
