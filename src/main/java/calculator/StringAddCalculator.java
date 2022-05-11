57b7d11bf538f8c11fbcea7442e49e973aecfcae
Woody <32324949+Woody-log@users.noreply.github.com>
2022-05-11 18:45:24 +0900

    2단계 - 문자열 덧셈 계산기 (woody-log) (#364)
    * test: TestCase 소스 코드 추가
    
    * feat: null 또는 빈문자열 일 때 0값 리턴
    
    * feat: 컴마(,) 또는 콜론(:) 으로 구분하여 합한다.
    
    * refactor: 양수값만 더하도록 수정
    
    * refactor: 커스텀 구분자로 split 할 수 있도록 수정
    
    * refactor: CUSTOM_DELIMITER_START, CUSTOM_DELIMITER_END 제거, CUSTOM_REGEX 로 변경
    
    * refactor: private 생성자 추가 객체생성 불가처리
    
    * refactor: 음수값 확인 메서드 추가

---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,60 @@
+package calculator;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+
+    private static final int ZERO = 0;
+    private static final String REGEX_DELIMITER = "|";
+    private static final List<String> STANDARD_DELIMISTERS = Arrays.asList(",", ":");
+    private static final String CUSTOM_REGEX = "//(.)\n(.*)";
+
+    private StringAddCalculator() {
+    }
+
+    public static int splitAndSum(final String expression) {
+        if (isNullOrEmpty(expression)) {
+            return ZERO;
+        }
+
+        List<String> stringNumbers = split(expression);
+
+        return sumPositiveNumbers(stringNumbers);
+    }
+
+    private static boolean isNullOrEmpty(final String expression) {
+        return Objects.isNull(expression) || expression.isEmpty();
+    }
+
+    private static List<String> split(final String expression) {
+        Matcher m = Pattern.compile(CUSTOM_REGEX).matcher(expression);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            return Arrays.asList(m.group(2).split(customDelimiter));
+        }
+
+        String regex = makeStandardDelimiterRegex();
+        return Arrays.asList(expression.split(regex));
+    }
+
+    private static int sumPositiveNumbers(final List<String> stringNumbers) {
+        return stringNumbers.stream()
+                .mapToInt(Integer::parseInt)
+                .peek(StringAddCalculator::checkNegativeNumber)
+                .sum();
+    }
+    
+    private static void checkNegativeNumber(final int number) {
+        if (number < 0) {
+            throw new RuntimeException("음수는 허용하지 않습니다. number=" + number);
+        }
+    }
+
+    private static String makeStandardDelimiterRegex() {
+        return String.join(REGEX_DELIMITER, STANDARD_DELIMISTERS);
+    }
+}
