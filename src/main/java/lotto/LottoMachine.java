43c9b131081d40d4389d9f87494be85c24121529
DongGyuJang <cdgu1129@knou.ac.kr>
2022-05-11 22:14:45 +0900

    feat: 로또 당첨리스트 및 수익률 기능 구현

---
+++ src/main/java/lotto/LottoMachine.java
@@ -0,0 +1,52 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import lotto.model.LottoNumbers;
+
+public class LottoMachine {
+	private List<LottoNumbers> lottos;
+	private model.Number money;
+	private int[] winList;
+	
+	public LottoMachine(String money) {
+		this.money = new model.Number(money);
+	}
+	
+	public List<LottoNumbers> buyAutoLottos() {
+		int times = money.getNumber()/LottoUtil.LOTTO_PRICE;
+		
+		lottos = new ArrayList<LottoNumbers>();
+		for (int i=0; i<times; ++i) {
+			lottos.add(new LottoNumbers(LottoUtil.randomLottoNumbers()));
+		}
+		return lottos;
+	}
+	
+	public int[] winList(LottoNumbers winLottoNumbers) {
+		winList = new int[LottoUtil.LOTTO_NUMBERS_COUNT+1];
+		Arrays.fill(winList, 0);
+		
+		for (LottoNumbers lottoNumbers: lottos) {
+			int count = winLottoNumbers.countEqualsLottoNumber(lottoNumbers);
+			++winList[count]; 
+		}
+		
+		return winList;
+	}
+	
+	public int profitRate() {
+		if(winList == null) {
+			return 0;
+		}
+		
+		int sum = 0;
+		for(int i=1; i<LottoUtil.LOTTO_NUMBERS_COUNT+1; ++i) {
+			sum += winList[i]*LottoUtil.WIN_MONEYS[i];
+		}
+		
+		return sum/money.getNumber();
+	}
+}
---
+++ src/test/java/lotto/LottoMachineTest.java
@@ -0,0 +1,47 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import lotto.model.LottoNumber;
+import lotto.model.LottoNumbers;
+
+public class LottoMachineTest {
+	private LottoMachine lottoMachine;
+
+	@BeforeEach
+	void setUp() {
+		lottoMachine = new LottoMachine("1000");
+	}
+
+	@Test
+	@DisplayName("로또 구입 테스트")
+	void buy_lotto(){
+        assertThat(lottoMachine.buyAutoLottos()).hasSize(1);
+	}
+
+	@Test
+	@DisplayName("당첨금액과 수익률을 구하는 기능 테스트")
+	void create_lottoNumber_int(){
+		LottoNumbers lottoNumbers = lottoMachine.buyAutoLottos().get(0);
+		
+		List<LottoNumber> lottoNumbers2 = new ArrayList<>();
+		for (int i=0; i<lottoNumbers.getLottoNumbers().size(); ++i) {
+			lottoNumbers2.add(lottoNumbers.getLottoNumbers().get(i));
+		}
+		int[] winList = lottoMachine.winList(new LottoNumbers(lottoNumbers2));
+
+		assertAll(
+				() -> assertEquals(winList[6], 1),
+				() -> assertEquals(lottoMachine.profitRate(), 2000000.00)
+		);
+	}
+}
