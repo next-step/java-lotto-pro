a873e2379f148fa70b4110767e87e14e76985a3d
songsimo <chy.song89@gmail.com>
2022-05-12 02:14:07 +0900

    refactor: 클래스 책임 분리-StringAddCalculator
    - 구분자를 통한 문자열 분리

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -3,37 +3,24 @@ package calculator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import calculator.domain.Numbers;
+
 public class StringAddCalculator {
 	private static final String DEFAULT_DELIMITER = "[,:]";
 	private static final String CUSTOM_DELIMITER_PATTERN = "//(.)\\n(.*)";
-	private static final String NUMBER_REGEX = "[0-9]+";
-	private static final String INVALID_NUMBER = "0 이상 숫자만 입력 가능합니다.";
 	private static final int DELIMITER_MATCHER_GROUP = 1;
 	private static final int VALUE_MATCHER_GROUP = 2;
 
 	public static int splitAndSum(String strNumbers) {
-		if(isNullOrEmpty(strNumbers)) {
-			return 0;
-		}
-
 		String[] numbers = split(strNumbers);
 
 		return sum(numbers);
 	}
 
-	private static boolean isNullOrEmpty(String strNumbers) {
-		return strNumbers == null || strNumbers.isEmpty();
-	}
+	private static int sum(String[] strNumbers) {
+		Numbers numbers = new Numbers(strNumbers);
 
-	private static int sum(String[] numbers) {
-		int total = 0;
-
-		for(String number: numbers) {
-			valid(number);
-			total += Integer.parseInt(number);
-		}
-
-		return total;
+		return numbers.total();
 	}
 
 	private static String[] split(String strNumbers) {
@@ -45,11 +32,5 @@ public class StringAddCalculator {
 		}
 
 		return strNumbers.split(DEFAULT_DELIMITER);
-	}
-
-	private static void valid(String number) {
-		if(!Pattern.matches(NUMBER_REGEX, number)) {
-			throw new RuntimeException(INVALID_NUMBER);
-		}
 	}
 }
--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -1,54 +1,29 @@
 package calculator;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class StringAddCalculatorTest {
 	@Test
-	@DisplayName("splitAndSum_null_또는_빈문자")
-	void null_or_empty_parameter_return_0() {
-		int result = StringAddCalculator.splitAndSum(null);
-		assertThat(result).isEqualTo(0);
-
-		result = StringAddCalculator.splitAndSum("");
-		assertThat(result).isEqualTo(0);
-	}
-
-	@Test
-	@DisplayName("splitAndSum_숫자하나")
-	void one_value_parameter() throws Exception {
-		int result = StringAddCalculator.splitAndSum("1");
-		assertThat(result).isEqualTo(1);
-	}
-
-	@Test
 	@DisplayName("splitAndSum_쉼표구분자")
-	void comma_delimiter_sum() throws Exception {
+	void comma_delimiter_sum() {
 		int result = StringAddCalculator.splitAndSum("1,2");
 		assertThat(result).isEqualTo(3);
 	}
 
 	@Test
 	@DisplayName("splitAndSum_쉼표_또는_콜론_구분자")
-	void comma_clone_delimiter_sum() throws Exception {
+	void comma_clone_delimiter_sum() {
 		int result = StringAddCalculator.splitAndSum("1,2:3");
 		assertThat(result).isEqualTo(6);
 	}
 
 	@Test
 	@DisplayName("splitAndSum_custom_구분자")
-	void custom_delimiter_sum() throws Exception {
+	void custom_delimiter_sum() {
 		int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
 		assertThat(result).isEqualTo(6);
-	}
-
-	@Test
-	@DisplayName("splitAndSum_negative")
-	void valid_negative() throws Exception {
-		assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
-			.isInstanceOf(RuntimeException.class);
 	}
 }
