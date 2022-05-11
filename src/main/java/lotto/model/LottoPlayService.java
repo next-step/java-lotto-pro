6ab806fd4319626bf713e85e371664ca6f90bcbc
KangSeongHyo <abc9023@naver.com>
2022-05-11 19:39:45 +0900

    feat : 입력받은 구매금액을 로또 개수로 변환 기능 추가
    - 변환한 로또 개수를 Lottos에 담도록 설정

--- src/main/java/lotto/model/LottoPlayService.java
+++ src/main/java/lotto/model/LottoPlayService.java
@@ -3,7 +3,11 @@ package lotto.model;
 import lotto.vo.Lottos;
 
 public class LottoPlayService {
+
+    private static final int LOTTO_PRICE = 1000;
+
     public Lottos convertMoneyToLottos(int money) {
-        return new Lottos();
+        int playCount = money / LOTTO_PRICE;
+        return new Lottos(playCount);
     }
 }
--- src/main/java/lotto/vo/Lottos.java
+++ src/main/java/lotto/vo/Lottos.java
@@ -1,7 +1,14 @@
 package lotto.vo;
 
 public class Lottos {
+
+    private final int playCount;
+
+    public Lottos(int playCount) {
+        this.playCount = playCount;
+    }
+
     public int getPlayCount() {
-        return 0;
+        return playCount;
     }
 }
