18af27eca5a0cf74880a88f2880869734cfaf176
Yul Jeon <notkhaki@gmail.com>
2022-05-11 18:03:45 +0900

    feat(StringAddCalculator): split and sum text that consist of positive number and delimiter comma or colon

--- src/main/java/StringAddCalculator/StringAddCalculator.java
+++ src/main/java/StringAddCalculator/StringAddCalculator.java
@@ -1,7 +1,35 @@
 package StringAddCalculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringAddCalculator {
     public static int splitAndSum(String text) {
-        return -1;
+        if (text == null) {
+            return 0;
+        }
+
+        String[] split = text.split(",|:");
+        List<Integer> numbers = parseInts(split);
+        return numbers.stream()
+                .reduce(0, Integer::sum);
+    }
+
+
+    private static List<Integer> parseInts(String[] split) {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < split.length; i++) {
+            numbers.add(parseInt(split[i]));
+        }
+
+        return numbers;
+    }
+
+    private static int parseInt(String s) {
+        if (s.isEmpty()) {
+            return 0;
+        }
+
+        return Integer.parseInt(s);
     }
 }
