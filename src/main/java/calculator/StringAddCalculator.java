fdec7ae4cca44363d4386067d8c41c03742db8f9
Celina <luv.mins99@gmail.com>
2022-05-12 02:29:56 +0900

    feat(StringAddCalculator): “//”와 “\n” 문자 사이에 커스텀 구분자를 지정하여 숫자의 합을 반환하는 기능 구현

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,10 +1,13 @@
 package calculator;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringAddCalculator {
     private static final int MIN_VALUE = 0;
     private static final String DEFAULT_DELIMITER = ",|:";
+    private static final String CUSTOM_DELIMITER = "//(.)\n(.*)";
 
     public static int splitAndSum(String input) {
         if (isNullOrEmpty(input)) {
@@ -20,6 +23,12 @@ public class StringAddCalculator {
     }
 
     private static String[] splitValue(String input) {
+        Matcher matcher = Pattern.compile(CUSTOM_DELIMITER).matcher(input);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            return matcher.group(2).split(customDelimiter);
+        }
+
         return input.split(DEFAULT_DELIMITER);
     }
 
