c7c1fa80c8e3ab2263d53ae2363504666a8cb9cc
mannue <sppe12@gmail.com>
2022-05-11 23:20:09 +0900

    feat(Set): Set 의 size 메소드를 이용하여 크기 구하기

---
+++ src/test/java/camp/nextstep/edu/SetTest.java
@@ -0,0 +1,30 @@
+package camp.nextstep.edu;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("size 메소드를 이용하여 크기를 구하기")
+    @Test
+    void sizeTest() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+}
