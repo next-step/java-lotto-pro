e7e62c9baeb9cdc9e3a263bf2101563633cceea5
Sung-jin <osj4872@gmail.com>
2022-05-12 00:52:13 +0900

    feat: 로또 당첨 번호 입력 기능 구현

--- src/main/java/camp/nextstep/edu/level1/lotto/lotto/Lotto.java
+++ src/main/java/camp/nextstep/edu/level1/lotto/lotto/Lotto.java
@@ -1,11 +1,17 @@
 package camp.nextstep.edu.level1.lotto.lotto;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Lotto {
-    private final int LOTTO_PRICE = 1000;
+    private final static int LOTTO_PRICE = 1000;
+    private final static int WINNING_NUMBER_COUNT = 6;
+    private final static String WINNING_NUMBER_DELIMITER = ",";
+    private final static Pattern POSITIVE_NUMBER_REGEX = Pattern.compile("^[1-9]\\d*$");
 
     private Money money;
     private List<LottoNumbers> items = new ArrayList<>();
@@ -19,10 +25,29 @@ public class Lotto {
         this.purchaseLotto(availablePurchaseCount);
     }
 
+    public void compareWinningNumber(String winningNumbers) {
+        String[] splitResult = Arrays.stream(winningNumbers.split(WINNING_NUMBER_DELIMITER))
+                .map(String::trim)
+                .toArray(String[]::new);
+        checkValidWinningNumber(splitResult);
+
+        Stream<LottoNumber> convertedWinningNumbers = Arrays.stream(splitResult)
+                .map(number -> new LottoNumber(Integer.parseInt(number)));
+    }
+
     private void checkValidLottoPurchaseMoney(int value) {
         if (value < LOTTO_PRICE) {
             String message = String.format("로또 구입시 최소 %d 원 이상이 있어야 합니다.", LOTTO_PRICE);
             throw new IllegalArgumentException(message);
+        }
+    }
+
+    private void checkValidWinningNumber(String[] winningNumbers) {
+        if (winningNumbers.length != WINNING_NUMBER_COUNT) {
+            throw new IllegalArgumentException("당첨 번호는 6개 이어야 합니다.");
+        }
+        if (Arrays.stream(winningNumbers).anyMatch(value -> !POSITIVE_NUMBER_REGEX.matcher(value).find())) {
+            throw new IllegalArgumentException("당첨 번호는 숫자만 입력이 가능합니다.");
         }
     }
 
--- src/test/java/camp/nextstep/edu/level1/lotto/lotto/LottoTest.java
+++ src/test/java/camp/nextstep/edu/level1/lotto/lotto/LottoTest.java
@@ -22,4 +22,16 @@ class LottoTest {
         assertThatIllegalArgumentException().isThrownBy(() -> new Lotto(LOTTO_PRICE - 1));
         assertThatIllegalArgumentException().isThrownBy(() -> new Lotto(- 1));
     }
+
+    @Test
+    void 로또_당첨_번호_입력_시_숫자와_구분자를_제외한_문자나_음수나_숫자_6개_를_입력하지_않으면_예외가_발생한다() {
+        Lotto lotto = new Lotto(10000);
+        String invalidWinningNumbers = "1, 2, 3, abc4, 5!@# 6";
+        String includeMinusWinningNumbers = "1, -2, 3, 4, -5, 6";
+        String invalidWinningNumberCount = "1, 2, 3, 4, 5";
+
+        assertThatIllegalArgumentException().isThrownBy(() -> lotto.compareWinningNumber(invalidWinningNumbers));
+        assertThatIllegalArgumentException().isThrownBy(() -> lotto.compareWinningNumber(includeMinusWinningNumbers));
+        assertThatIllegalArgumentException().isThrownBy(() -> lotto.compareWinningNumber(invalidWinningNumberCount));
+    }
 }
