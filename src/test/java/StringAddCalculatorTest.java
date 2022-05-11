e61124bd2625ba81daaa8da310370be589d4cb60
Ttungga <torb2002@gmail.com>
2022-05-12 00:27:41 +0900

    test: add @DisplayName annotations

--- src/test/java/StringAddCalculatorTest.java
+++ src/test/java/StringAddCalculatorTest.java
@@ -1,10 +1,12 @@
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class StringAddCalculatorTest {
     @Test
+    @DisplayName("입력값이 null 또는 빈 문자열이면 0이 반환되어야 한다")
     public void splitAndSum_null_또는_빈문자() {
         int result = StringAddCalculator.splitAndSum(null);
         assertThat(result).isEqualTo(0);
@@ -14,30 +16,35 @@ class StringAddCalculatorTest {
     }
 
     @Test
+    @DisplayName("입력값에 숫자가 하나이면 해당 숫자가 반환되어야 한다")
     public void splitAndSum_숫자하나() throws Exception {
         int result = StringAddCalculator.splitAndSum("1");
         assertThat(result).isEqualTo(1);
     }
 
     @Test
+    @DisplayName("쉼표를 구분자로 한 입력값이 들어온 경우 쉼표를 기준으로 구분된 숫자들의 합이 반환되어야 한다")
     public void splitAndSum_쉼표구분자() throws Exception {
         int result = StringAddCalculator.splitAndSum("1,2");
         assertThat(result).isEqualTo(3);
     }
 
     @Test
+    @DisplayName("쉼표와 콜론 둘 다 구분자로 사용될 수 있어야 한다")
     public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
         int result = StringAddCalculator.splitAndSum("1,2:3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
+    @DisplayName("'//'와 '\\n' 문자 사이에 커스텀 구분자를 지정할 수 있어야 한다")
     public void splitAndSum_custom_구분자() throws Exception {
         int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
+    @DisplayName("입력값에 음수가 포함되면 RuntimeException이 발생해야 한다")
     public void splitAndSum_negative() throws Exception {
         assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
                 .isInstanceOf(RuntimeException.class);
