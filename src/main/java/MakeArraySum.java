725d74a61af2d73369eca2f18a42bf7f62727ff9
오진혁 <hyeok7777@gmail.com>
2022-05-12 00:30:47 +0900

    refactor : MakeArraySum.java 포맷팅

--- src/main/java/MakeArraySum.java
+++ src/main/java/MakeArraySum.java
@@ -1,8 +1,8 @@
 public class MakeArraySum {
 
-    public int getSum(String[] numbers) {
+    public static int getSum(String[] numbers) {
         int sum = 0;
-        for(int i = 0 ; i < numbers.length ; i++) {
+        for (int i = 0; i < numbers.length; i++) {
             sum += Integer.parseInt(numbers[i]);
         }
         return sum;
