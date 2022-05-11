524c1cb3b0e568a27779aef43c53dc7cd99fcfce
sy001 <suyounlake@naver.com>
2022-05-11 22:37:56 +0900

    feat: 쉼표를 구분자로 입력 시 각 숫자의 합을 반환

--- src/main/java/stringAddCalculator/StringAddCalculator.java
+++ src/main/java/stringAddCalculator/StringAddCalculator.java
@@ -5,7 +5,20 @@ public class StringAddCalculator {
         if (input == null || input.isEmpty()) {
             return 0;
         }
-        int result = parseStringToInteger(input);
+        String[] splitInput = splitString(input);
+        int result = calculateStringSum(splitInput);
+        return result;
+    }
+
+    private static String[] splitString(String str) {
+        return str.split(",");
+    }
+
+    private static int calculateStringSum(String[] strs) {
+        int result = 0;
+        for (String str : strs) {
+            result += parseStringToInteger(str);
+        }
         return result;
     }
 
