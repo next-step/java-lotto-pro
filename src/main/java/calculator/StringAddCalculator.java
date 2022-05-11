ac0e86e6fd2ec1812b6de1fa9473fabb17926006
songsimo <chy.song89@gmail.com>
2022-05-12 00:26:52 +0900

    feat: 커스텀 구분자로 숫자 분리 기능 구현

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,13 +1,19 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
-	private static final String DELIMITER = "[,:]";
+	private static final String DEFAULT_DELIMITER = "[,:]";
+	private static final String CUSTOM_DELIMITER_PATTERN = "//(.)\\n(.*)";
+	private static final int DELIMITER_MATCHER_GROUP = 1;
+	private static final int VALUE_MATCHER_GROUP = 2;
 	public static int splitAndSum(String strNumbers) {
 		if(isNullOrEmpty(strNumbers)) {
 			return 0;
 		}
 
-		String[] numbers = strNumbers.split(DELIMITER);
+		String[] numbers = split(strNumbers);
 
 		return sum(numbers);
 	}
@@ -24,5 +30,16 @@ public class StringAddCalculator {
 		}
 
 		return total;
+	}
+
+	private static String[] split(String strNumbers) {
+		Matcher matcher = Pattern.compile(CUSTOM_DELIMITER_PATTERN).matcher(strNumbers);
+
+		if (matcher.find()) {
+			String customDelimiter = matcher.group(DELIMITER_MATCHER_GROUP);
+			return matcher.group(VALUE_MATCHER_GROUP).split(customDelimiter);
+		}
+
+		return strNumbers.split(DEFAULT_DELIMITER);
 	}
 }
