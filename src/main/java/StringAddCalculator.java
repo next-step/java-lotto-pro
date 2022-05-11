99c24a489eec93a969495691e94ab0323675ae88
leejeongyong <jeongyolee@ebay.com>
2022-05-11 21:59:37 +0900

    test: splitAndSum_null_또는_빈문자 Test case 추가

---
+++ src/main/java/StringAddCalculator.java
@@ -0,0 +1,9 @@
+public class StringAddCalculator {
+
+    public static int splitAndSum(String actual) {
+        if(actual == null || actual.isEmpty()) {
+            return 0;
+        }
+        return Integer.parseInt(actual);
+    }
+}
