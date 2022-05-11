598d1a009b6387ef04443096d7d98492ca3d4f54
leejeongyong <jeongyolee@ebay.com>
2022-05-11 12:31:52 +0900

    feat: Set Collection Test 요구사항 2번(contains with ParameterizedTest)

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -4,6 +4,8 @@ import java.util.HashSet;
 import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -21,5 +23,15 @@ public class SetTest {
     void size() {
         // 요구사항 1번
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    void contains(int input) {
+        // 요구사항 2번
+        //assertThat(numbers.contains(1)).isTrue();
+        //assertThat(numbers.contains(2)).isTrue();
+        //assertThat(numbers.contains(3)).isTrue();
+        assertThat(numbers.contains(input)).isTrue();
     }
 }
