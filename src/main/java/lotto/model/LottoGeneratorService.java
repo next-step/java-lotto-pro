f842d7a81fe7f3d2d28ba2837f4973cbfefa8f51
KangSeongHyo <abc9023@naver.com>
2022-05-11 21:06:01 +0900

    feat : Lotto 객체 생성 메서드 추가
    - Lotto의 생성자로 RandomNumber를 받고 정렬 후 필드에 등록

--- src/main/java/lotto/model/LottoGeneratorService.java
+++ src/main/java/lotto/model/LottoGeneratorService.java
@@ -1,9 +1,17 @@
 package lotto.model;
 
+import java.util.List;
+import lotto.utils.RandomNumberUtils;
 import lotto.vo.Lotto;
 
 public class LottoGeneratorService {
+    private static final int LOW_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final int NUMBER_COUNT = 6;
+
     public Lotto generateLotto() {
-        return new Lotto();
+        List<Integer> randomNumberToList = RandomNumberUtils
+                .generateRandomNumberToList(LOW_NUMBER, MAX_NUMBER, NUMBER_COUNT);
+        return new Lotto(randomNumberToList);
     }
 }
--- src/main/java/lotto/vo/Lotto.java
+++ src/main/java/lotto/vo/Lotto.java
@@ -4,7 +4,15 @@ import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
+
+    private final List<Integer> numberList;
+
+    public Lotto(List<Integer> numberList) {
+        Collections.sort(numberList);
+        this.numberList = numberList;
+    }
+
     public List<Integer> getNumberList() {
-        return Collections.emptyList();
+        return numberList;
     }
 }
