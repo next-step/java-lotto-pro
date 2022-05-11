02372ce9534bd6720bd43a6e86e8bd3e0ea52510
Ttungga <torb2002@gmail.com>
2022-05-12 00:52:32 +0900

    feat: add StringAddCalculator.splitInput and StringAddCalculator.sumNumbers

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,12 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringAddCalculator {
+    private static final String DEFAULT_SEPARATORS = ",|:";
+
     public static int splitAndSum(final String input) {
         if (!validateInput(input)) {
             return 0;
         }
-        return 1;
+        return sumNumbers(splitInput(input));
     }
 
     private static boolean validateInput(final String input) {
         return input != null && !input.isEmpty();
+    }
+
+    private static List<Integer> splitInput(final String input) {
+        final String[] stringNumbers = input.split(DEFAULT_SEPARATORS);
+        final List<Integer> numbers = new ArrayList<>();
+        for (final String stringNumber : stringNumbers) {
+            numbers.add(Integer.parseInt(stringNumber));
+        }
+        return numbers;
+    }
+
+    private static int sumNumbers(final List<Integer> numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
     }
 }
