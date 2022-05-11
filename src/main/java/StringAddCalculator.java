5e7e4297559d9b3a3d845c9cf1d53076d9c4b470
leejeongyong <jeongyolee@ebay.com>
2022-05-11 22:08:42 +0900

    test: splitAndSum_쉼표구분자 Test case 추가

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,9 +1,16 @@
 public class StringAddCalculator {
 
     public static int splitAndSum(String actual) {
+        int sum = 0;
         if(actual == null || actual.isEmpty()) {
             return 0;
         }
-        return Integer.parseInt(actual);
+
+        String [] numbers = actual.split(",");
+        for(String number : numbers) {
+            sum += Integer.parseInt(number);
+        }
+
+        return sum;
     }
 }
