8285efcfcb091b829eea07731d1499941b55fb2b
Celina <luv.mins99@gmail.com>
2022-05-12 01:00:45 +0900

    feat(StringAddCalculator): 숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환하는 기능 구현
    - 숫자를 분리하는 기능과 합을 계산하는 기능을 분리하여 구현

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,30 +1,45 @@
 package calculator;
 
+import java.util.Arrays;
+
 public class StringAddCalculator {
     private static final int MIN_VALUE = 0;
     private static final int SINGLE_DIGIT = 1;
+    private static final String COMMA_DELIMITER = ",";
 
     public static int splitAndSum(String input) {
         if (isNullOrEmpty(input)) {
             return MIN_VALUE;
         }
 
-        return splitValue(input);
+        String[] inputs = splitValue(input);
+        return calculateSum(inputs);
     }
 
     private static boolean isNullOrEmpty(String input) {
         return input == null || input.isEmpty();
     }
 
-    private static int splitValue(String input) {
+    private static String[] splitValue(String input) {
         if (isSingleDigit(input)) {
-            return Integer.parseInt(input);
+            return new String[]{input};
         }
 
-        return Integer.parseInt(input);
+        if (isCommaInput(input)) {
+            return input.split(COMMA_DELIMITER);
+        }
+        return input.split(COMMA_DELIMITER);
     }
 
     private static boolean isSingleDigit(String input) {
         return input.length() == SINGLE_DIGIT;
+    }
+
+    private static boolean isCommaInput(String input) {
+        return input.matches(COMMA_DELIMITER);
+    }
+
+    private static int calculateSum(String[] inputs) {
+        return Arrays.stream(inputs).mapToInt(Integer::parseInt).sum();
     }
 }
