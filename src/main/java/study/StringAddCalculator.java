fc6752a793eb16fdf8ab7d0c1ca4aa81a955c918
wenodev <logicalstark1105@gmail.com>
2022-05-11 23:44:12 +0900

    feat(StringAddCalculator.java): 구분자를 컴마(,) 이외에 콜론(:)을 사용할 수 있습니다

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -4,13 +4,14 @@ import java.util.Arrays;
 
 public class StringAddCalculator {
     private static final int DEFAULT_NUMBER = 0;
+    private static final String DELIMITER = ",|:";
 
     public static int splitAndSum(String str) {
         if (isNullOrEmpty(str)) {
             return DEFAULT_NUMBER;
         }
 
-        return Arrays.stream(str.split(","))
+        return Arrays.stream(str.split(DELIMITER))
                 .map(Integer::parseInt)
                 .mapToInt(i -> i)
                 .sum();
