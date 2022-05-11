1390804f275a502815eb2f1b98e1ab5c2916521d
최현지 <chj1768@gmail.com>
2022-05-11 22:52:12 +0900

    refactor: 매직넘버, 정규식 패턴 추출

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -2,26 +2,35 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
+
+    private static final Pattern CUSTOM_DELIMITER = Pattern.compile("//(.)\n(.*)");
+    private static final Pattern DELIMITER = Pattern.compile(",|:");
+
+    private static final int CUSTOM_DELIMITER_MATCHER_GROUP = 1;
+    private static final int TEXT_MATCHER_GROUP = 2;
+
+    private static final int DEFAULT_SUM = 0;
+
     public static int splitAndSum(String text) {
         if (isNotNumeric(text))
-            return 0;
+            return DEFAULT_SUM;
 
         String[] tokens = split(text);
         return sum(tokens);
     }
 
     private static String[] split(String text) {
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        Matcher m = CUSTOM_DELIMITER.matcher(text);
         if (m.find()) {
-            String customDelimiter = m.group(1);
-            return m.group(2).split(customDelimiter);
+            String customDelimiter = m.group(CUSTOM_DELIMITER_MATCHER_GROUP);
+            return m.group(TEXT_MATCHER_GROUP).split(customDelimiter);
         }
 
-        return text.split(",|:");
+        return text.split(String.valueOf(DELIMITER));
     }
 
     private static int sum(String[] tokens) {
-        int sum = 0;
+        int sum = DEFAULT_SUM;
         for (String s : tokens)
             sum += getUnsignedInt(s);
 
