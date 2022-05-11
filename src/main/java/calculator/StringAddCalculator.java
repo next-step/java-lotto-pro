523aebded73ff6151318fc6fae49c41c43f13d51
Celina <luv.mins99@gmail.com>
2022-05-12 02:24:40 +0900

    feat(StringAddCalculator): 구분자를 컴마(,) 이외에 콜론(:)을 사용하는 경우에도 숫자의 합을 반환하는 기능 구현

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -6,6 +6,7 @@ public class StringAddCalculator {
     private static final int MIN_VALUE = 0;
     private static final int SINGLE_DIGIT = 1;
     private static final String COMMA_DELIMITER = ",";
+    private static final String COMMA_AND_COLON_DELIMITER = ",|:";
 
     public static int splitAndSum(String input) {
         if (isNullOrEmpty(input)) {
@@ -24,7 +25,7 @@ public class StringAddCalculator {
         if (isSingleDigit(input) || isCommaInput(input)) {
             return input.split(COMMA_DELIMITER);
         }
-        return input.split(COMMA_DELIMITER);
+        return input.split(COMMA_AND_COLON_DELIMITER);
     }
 
     private static boolean isSingleDigit(String input) {
