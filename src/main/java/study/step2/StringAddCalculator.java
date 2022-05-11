795e7cb2540b64ad36f41f35334d569595accaeb
SANGIK KIM <sangik.kim@enel.com>
2022-05-11 20:46:25 +0900

    feature(StringAddCalculator): add a feature to sum the given comma split number string

--- src/main/java/study/step2/StringAddCalculator.java
+++ src/main/java/study/step2/StringAddCalculator.java
@@ -1,15 +1,26 @@
 package study.step2;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 public class StringAddCalculator {
     private static final int ZERO = 0;
 
+    private static final String DELIMITERS = ",";
+
     public static int splitAndSum(String numberString) {
-        return isNullOrEmptyString(numberString) ? ZERO : Integer.parseInt(numberString);
+        if (isNullOrEmptyString(numberString)) {
+            return ZERO;
+        }
+
+        return Arrays.asList(numberString.split(DELIMITERS)).stream()
+                .map(Integer::valueOf)
+                .reduce(ZERO, (num1, num2) -> num1 + num2);
     }
 
     private static boolean isNullOrEmptyString(String numbers) {
         return Objects.isNull(numbers) || numbers.isEmpty();
     }
+
+
 }
--- src/test/java/study/step2/StringAddCalculatorTest.java
+++ src/test/java/study/step2/StringAddCalculatorTest.java
@@ -23,4 +23,11 @@ public class StringAddCalculatorTest {
         int result = StringAddCalculator.splitAndSum("1");
         assertThat(result).isEqualTo(1);
     }
+
+    @Test
+    @DisplayName("숫자 두개를 컴마 구분자로 입력할 경우 두 숫자의 합을 반환한다.")
+    public void splitAndSum_쉼표구분자() throws Exception {
+        int result = StringAddCalculator.splitAndSum("1,2");
+        assertThat(result).isEqualTo(3);
+    }
 }
