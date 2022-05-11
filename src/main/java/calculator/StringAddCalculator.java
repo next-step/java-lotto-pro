e37f07b3e2d2c41354359514e77f5ead470e7195
Celina <luv.mins99@gmail.com>
2022-05-12 02:27:25 +0900

    refactor(StringAddCalculator): 숫자 사이에 구분자가 오는 패턴에 대해 동일 로직으로 구현

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -4,9 +4,7 @@ import java.util.Arrays;
 
 public class StringAddCalculator {
     private static final int MIN_VALUE = 0;
-    private static final int SINGLE_DIGIT = 1;
-    private static final String COMMA_DELIMITER = ",";
-    private static final String COMMA_AND_COLON_DELIMITER = ",|:";
+    private static final String DEFAULT_DELIMITER = ",|:";
 
     public static int splitAndSum(String input) {
         if (isNullOrEmpty(input)) {
@@ -22,18 +20,7 @@ public class StringAddCalculator {
     }
 
     private static String[] splitValue(String input) {
-        if (isSingleDigit(input) || isCommaInput(input)) {
-            return input.split(COMMA_DELIMITER);
-        }
-        return input.split(COMMA_AND_COLON_DELIMITER);
-    }
-
-    private static boolean isSingleDigit(String input) {
-        return input.length() == SINGLE_DIGIT;
-    }
-
-    private static boolean isCommaInput(String input) {
-        return input.matches(COMMA_DELIMITER);
+        return input.split(DEFAULT_DELIMITER);
     }
 
     private static int calculateSum(String[] inputs) {
