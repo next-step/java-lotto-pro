0e4c716d01434c154a56f394089d0f6c490c4fae
piopoi <kangsw1988@gmail.com>
2022-05-11 18:58:47 +0900

    2단계 - 문자열 덧셈 계산기 (#373)
    * test: StringAddCalculator에 대한 TestCase 추가
    
    * docs(README.md): 문자열 덧셈 계산기 기능 요구사항 추가
    
    * feat(StringAddCalculator): 빈 문자열 또는 null을 입력하면 0을 반환
    
    * feat(StringAddCalculator): 숫자 하나를 입력하면 해당 숫자를 그대로 반환하는 기능 추가
    
    * feat(StringAddCalculator): 여러 숫자를 컴마를 구분자로 입력할 경우 숫자들의 합을 반환하는 기능 추가
    
    메서드명 변경
    - isEmptyString -> isEmpty
    - isSingleNumber -> isNumber
    
    * feat(StringAddCalculator): 구분자를 컴마와 콜론 모두 사용할 수 있도록 변경
    
    * refactor(StringAddCalculator): 변수 오타 수정
    
    * feat(StringAddCalculator): 커스텀 구분자 지정 기능 추가
    
    * feat(step2): 양수가 아닌 문자 입력 시 RuntimeException 발생시키는 로직 추가
    
    - 에러메시지는 Enum으로 별도 처리.
    - 에러메시지 관련 테스트코드 수정.
    
    * test(StringAddCalculatorTest): isEqualTo(0) -> isZero() 변경
    
    * refactor(StringAddCalculator): 원시값 포장
    
    * refactor(StringAddCalculator): 인스턴스 생성을 막기 위해 private 생성자 추가

---
+++ src/main/java/step2/StringAddCalculator.java
@@ -0,0 +1,68 @@
+package step2;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+
+    private static final int NUMBER_ZERO = 0;
+    private static final String DELIMITER_DEFAULT = ",|:";
+    private static final String DELIMITER_PIPE = "|";
+    private static final String REGEX_CUSTOM_DELIMITER = "//(.)\n(.*)";
+
+    private StringAddCalculator() {
+    }
+
+    public static int splitAndSum(String inputString) {
+        if (isEmpty(inputString)) {
+            return 0;
+        }
+        if (isPositiveNumber(inputString)) {
+            return Integer.parseInt(inputString);
+        }
+        String[] numbers = split(inputString);
+        validatePositiveNumbers(numbers);
+        return sum(numbers);
+    }
+
+
+    private static boolean isEmpty(String source) {
+        return source == null || source.isEmpty();
+    }
+
+    private static boolean isPositiveNumber(String source) {
+        try {
+            return Integer.parseInt(source) > NUMBER_ZERO;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private static void validatePositiveNumbers(String[] source) {
+        boolean isPositiveNumbers = Arrays.stream(source)
+                                        .allMatch(StringAddCalculator::isPositiveNumber);
+        if (!isPositiveNumbers) {
+            throw new RuntimeException(ErrorMessage.ERROR_NOT_POSITIVE_NUMBER.getMessage());
+        }
+    }
+
+    private static String[] split(String inputString) {
+        Matcher m = Pattern.compile(REGEX_CUSTOM_DELIMITER).matcher(inputString);
+        if (m.find()) {
+            String customDelimiters = makeCustomDelimiters(m.group(1));
+            return m.group(2).split(customDelimiters);
+        }
+        return inputString.split(DELIMITER_DEFAULT);
+    }
+
+    private static int sum(String[] numbers) {
+        return Arrays.stream(numbers)
+                .mapToInt(Integer::parseInt)
+                .sum();
+    }
+
+    private static String makeCustomDelimiters(String customDelimiter) {
+        return String.join(DELIMITER_PIPE, DELIMITER_DEFAULT, customDelimiter);
+    }
+}
