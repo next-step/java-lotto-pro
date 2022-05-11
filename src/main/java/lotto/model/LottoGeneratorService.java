c661a3fcb1662e8abb99a7e7347b0eea1383850f
KangSeongHyo <abc9023@naver.com>
2022-05-11 21:00:38 +0900

    test : Lotto 객체 생성 테스트 추가

---
+++ src/main/java/lotto/model/LottoGeneratorService.java
@@ -0,0 +1,9 @@
+package lotto.model;
+
+import lotto.vo.Lotto;
+
+public class LottoGeneratorService {
+    public Lotto generateLotto() {
+        return new Lotto();
+    }
+}
---
+++ src/main/java/lotto/vo/Lotto.java
@@ -0,0 +1,10 @@
+package lotto.vo;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Lotto {
+    public List<Integer> getNumberList() {
+        return Collections.emptyList();
+    }
+}
