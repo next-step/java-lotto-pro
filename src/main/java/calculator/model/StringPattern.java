1645d1ca29176e16824628ecac7f9ea002177bfa
bestsilver <dkvkxm99@gmail.com>
2022-05-12 01:13:23 +0900

    refactor: StringPatternRegex 삭제, StringPattern으로 합치기

--- src/main/java/calculator/model/StringPattern.java
+++ src/main/java/calculator/model/StringPattern.java
@@ -1,5 +1,25 @@
 package calculator.model;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 public enum StringPattern {
-    FIXED, CUSTOM, NOTHING
+    FIXED("(\\d+(,|:)?)+"),
+    CUSTOM("//(.)\n(.*)"),
+    NOTHING("$^");
+
+    private final String patternValue;
+
+    StringPattern(String patternValue) {
+        this.patternValue = patternValue;
+    }
+
+    public String pattern() {
+        return patternValue;
+    }
+
+    public static StringPattern determine(String text) {
+        Optional<StringPattern> findPattern = Arrays.stream(StringPattern.values()).filter(p -> text.matches(p.pattern())).findAny();
+        return findPattern.orElse(StringPattern.NOTHING);
+    }
 }
--- src/main/java/calculator/model/StringPatternRegex.java
+++
@@ -1,6 +0,0 @@
-package calculator.model;
-
-public class StringPatternRegex {
-    public static final String FIXEDPATTERN = "(\\d+(,|:)?)+";
-    public static final String CUSTOMPATTERN = "//(.)\n(.*)";
-}
