cfc98169b4adacabb3b136c6ee68c452cea9ec3e
최현지 <chj1768@gmail.com>
2022-05-11 21:54:45 +0900

    feat: 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환하는 요구사항 반영

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -2,6 +2,11 @@ public class StringAddCalculator {
     public static int splitAndSum(String str) {
         if (str == null || str.isEmpty())
             return 0;
+
+        String[] strs = str.split(",");
+        if (strs.length == 1)
+            return Integer.parseInt(strs[0]);
+
         return 0;
     }
 }
