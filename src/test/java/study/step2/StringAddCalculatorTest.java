aac3f4193712b855aa446065f0f5352245f495cf
SANGIK KIM <sangik.kim@enel.com>
2022-05-11 20:15:06 +0900

    refactor(StringAddCalculatorTest): add display names

--- src/test/java/study/step2/StringAddCalculatorTest.java
+++ src/test/java/study/step2/StringAddCalculatorTest.java
@@ -2,10 +2,13 @@ package study.step2;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("문자열 덧셈 계산기")
 public class StringAddCalculatorTest {
     @Test
+    @DisplayName("빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.")
     public void splitAndSum_null_또는_빈문자() {
         int result = StringAddCalculator.splitAndSum(null);
         assertThat(result).isEqualTo(0);
