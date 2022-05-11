4bdd459bcf5160e4fedb3d16ca4e4cead9aa0c98
limwoobin <drogba02@naver.com>
2022-05-11 21:00:01 +0900

    test: 숫자 하나를 문자열로 전달하는 case 에 대한 테스트 코드를 작성하라

--- src/test/java/ExpressionUtilsTest.java
+++ src/test/java/ExpressionUtilsTest.java
@@ -3,6 +3,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import domain.ExpressionUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("더하기 Expression 유효성 테스트")
 class ExpressionUtilsTest {
@@ -12,5 +14,12 @@ class ExpressionUtilsTest {
     void empty_expression_test() {
         assertTrue(ExpressionUtils.isEmptyExpression(""));
         assertTrue(ExpressionUtils.isEmptyExpression(null));
+    }
+
+    @DisplayName("숫자 하나인지 체크하는 로직에 숫자 하나를 문자열로 넘기면 정상적으로 통과되어야 한다")
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "2", "3", "4", "5"})
+    void single_number_expression_test(String input) {
+        assertTrue(ExpressionUtils.isSingleNumberExpression(input));
     }
 }
--- src/test/java/PlusStrategyTest.java
+++ src/test/java/PlusStrategyTest.java
@@ -4,9 +4,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import domain.EmptyPlusStrategy;
 import domain.PlusStrategy;
 import domain.PlusStrategyFactory;
+import domain.SingleNumberPlusStrategy;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("더하기 식에 대한 테스트 코드")
 class PlusStrategyTest {
@@ -18,7 +21,7 @@ class PlusStrategyTest {
     }
 
     @Test
-    @DisplayName("팩토리 클래스에 공백을 전달하면 EmptyPlusStrategy 를 반환해야 한다")
+    @DisplayName("팩토리 클래스에 공백을 전달하면 EmptyPlusStrategy 를 반환하고 결과는 0이어야 한다")
     void blank_test() {
         String given = "";
         PlusStrategy plusStrategy = factory.getStrategy(given);
@@ -37,4 +40,21 @@ class PlusStrategyTest {
         assertThat(plusStrategy.result(given)).isEqualTo(0);
     }
 
+    @DisplayName("팩토리 클래스에 숫자 하나를 문자열로 전달할 경우 SingleNumberPlusStrategy 가 반환되어야 한다")
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "2", "3", "4", "5"})
+    void single_number_test(String input) {
+        PlusStrategy plusStrategy = factory.getStrategy(input);
+
+        assertThat(plusStrategy.getClass()).isEqualTo(SingleNumberPlusStrategy.class);
+    }
+
+    @DisplayName("팩토리 클래스에 숫자 하나를 문자열로 전달할 경우 해당 문자가 숫자로 반환되어야 한다")
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "2", "3", "4", "5"})
+    void single_number_test_2(String input) {
+        PlusStrategy plusStrategy = factory.getStrategy(input);
+
+        assertThat(plusStrategy.result(input)).isEqualTo(Integer.parseInt(input));
+    }
 }
