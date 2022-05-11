414975d2eb87e49eb2a590779f10a9d9440e1dce
오진혁 <hyeok7777@gmail.com>
2022-05-12 00:33:20 +0900

    refactor : NumberValidator.java 1) MakeArraySum static 메소드로 호출 2) 매직넘버 치환 3) Matcher 변수선언 제외 후 return 4) Exception 메세지 수정 5) regex 하드코딩 제거

--- src/main/java/NumberValidator.java
+++ src/main/java/NumberValidator.java
@@ -2,8 +2,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class NumberValidator {
+    private static final String BASIC_REGEX = ",|:";
     private static final String CUSTOM_REGEX = "//(.)\n(.*)";
-    private static MakeArraySum makeArraySum = new MakeArraySum();
+    private static final int STATIC_ONE = 1;
+    private static final int STATIC_TWO = 2;
     String text;
 
     public NumberValidator(String text) {
@@ -19,29 +21,28 @@ public class NumberValidator {
         for (int i = 0; i < numbers.length; i++) {
             checkNegative(numbers[i]);
         }
-        sum = makeArraySum.getSum(numbers);
+        sum = MakeArraySum.getSum(numbers);
         return sum;
     }
 
     private void checkNegative(String s) {
         if (s.contains("-")) {
-            throw new RuntimeException();
+            throw new RuntimeException("0이상의 숫자만 입력할 수 있습니다. (음수 입력 불가)");
         }
     }
 
     private String[] checkRegex() {
         Matcher m = getMatcher();
         if (m.find()) {
-            String customDelimiter = m.group(1);
-            String[] numbers = m.group(2).split(customDelimiter);
+            String customDelimiter = m.group(STATIC_ONE);
+            String[] numbers = m.group(STATIC_TWO).split(customDelimiter);
             return numbers;
         }
-        return text.split(",|:");
+        return text.split(BASIC_REGEX);
     }
 
     private Matcher getMatcher() {
-        Matcher m = Pattern.compile(CUSTOM_REGEX).matcher(text);
-        return m;
+        return Pattern.compile(CUSTOM_REGEX).matcher(text);
     }
 
     private boolean checkNull() {
