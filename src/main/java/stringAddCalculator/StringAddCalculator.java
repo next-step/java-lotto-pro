dc146d0ca25bd2fca839859e26ab17066eab6699
sy001 <suyounlake@naver.com>
2022-05-11 22:55:20 +0900

    feat: 숫자 이외의 값 또는 음수를 전달하는 경우 예외처리

--- src/main/java/stringAddCalculator/StringAddCalculator.java
+++ src/main/java/stringAddCalculator/StringAddCalculator.java
@@ -25,12 +25,24 @@ public class StringAddCalculator {
     private static int calculateStringSum(String[] strs) {
         int result = 0;
         for (String str : strs) {
-            result += parseStringToInteger(str);
+            result += parseStringToPositiveInteger(str);
         }
         return result;
     }
 
+    private static int parseStringToPositiveInteger(String str) {
+        int i = parseStringToInteger(str);
+        if (i < 0) {
+            throw new IllegalArgumentException("[Error] 음수는 입력할 수 없습니다.");
+        }
+        return i;
+    }
+
     private static int parseStringToInteger(String str) {
-        return Integer.parseInt(str);
+        try {
+            return Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[Error] 올바른 숫자가 아닙니다.");
+        }
     }
 }
--- src/test/java/stringAddCalculator/StringAddCalculatorTest.java
+++ src/test/java/stringAddCalculator/StringAddCalculatorTest.java
@@ -2,6 +2,7 @@ package stringAddCalculator;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringAddCalculatorTest {
     @Test
@@ -35,5 +36,17 @@ public class StringAddCalculatorTest {
     public void splitAndSum_custom_구분자() throws Exception {
         int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
         assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void splitAndSum_negative() throws Exception {
+        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void splitAndSum_invalid_format() throws Exception {
+        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-+1,2,3"))
+                .isInstanceOf(RuntimeException.class);
     }
 }
