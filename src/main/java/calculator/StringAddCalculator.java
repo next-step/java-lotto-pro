6c27317f13d40645acaa8d63fde5ccca4d35793d
Su-hub9 <su9581@gmail.com>
2022-05-12 10:18:32 +0900

    feat(feature): 커스텀 구분자로 분리하는 기능 추가

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -3,6 +3,9 @@ package calculator;
 import calculator.common.Constants;
 import calculator.common.ErrorMessage;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
     public static int splitAndSum(String text) {
         if (isEmpty(text)) {
@@ -17,7 +20,23 @@ public class StringAddCalculator {
     }
 
     private static String[] splitText(String text) {
-        return text.split(Constants.DEFAULT_DELIMITER);
+        if (useCustomDelimiter(text)) {
+            return splitCustomDelimiter(text);
+        }
+        return text.split(Constants.DEFAULT_DELIMITER_REGEX);
+    }
+
+    private static String[] splitCustomDelimiter(String text) {
+        Matcher matcher = Pattern.compile(Constants.CUSTOM_DELIMITER_REGEX).matcher(text);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            return matcher.group(2).split(customDelimiter);
+        }
+        return new String[0];
+    }
+
+    private static boolean useCustomDelimiter(String text) {
+        return text.matches(Constants.CUSTOM_DELIMITER_REGEX);
     }
 
     private static int sumNumbers(String[] numbers) {
@@ -29,8 +48,8 @@ public class StringAddCalculator {
         return sum;
     }
 
-    private static void matchesPositiveNumber(String text) {
-        if (!text.matches(Constants.POSITIVE_NUMBER_REGEX)) {
+    private static void matchesPositiveNumber(String numberStr) {
+        if (!numberStr.matches(Constants.POSITIVE_NUMBER_REGEX)) {
             throw new IllegalArgumentException(ErrorMessage.NOT_POSITIVE_NUMBER);
         }
     }
--- src/main/java/calculator/common/Constants.java
+++ src/main/java/calculator/common/Constants.java
@@ -2,6 +2,7 @@ package calculator.common;
 
 public class Constants {
     public static final int EMPTY_RETURN = 0;
-    public static final String DEFAULT_DELIMITER = ",|:";
+    public static final String DEFAULT_DELIMITER_REGEX = ",|:";
+    public static final String CUSTOM_DELIMITER_REGEX = "//(.)\\n(.*)";
     public static final String POSITIVE_NUMBER_REGEX = "^\\d+$";
 }
