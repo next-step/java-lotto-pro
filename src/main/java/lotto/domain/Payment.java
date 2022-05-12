ec057032c758341231e6a7f051fe0b14f6316174
Ttungga <torb2002@gmail.com>
2022-05-13 01:09:49 +0900

    feat: add validation logics to Payment
    - add Payment.validateMoney and Payment.checkAmount

--- src/main/java/lotto/domain/Payment.java
+++ src/main/java/lotto/domain/Payment.java
@@ -4,6 +4,18 @@ public class Payment {
     private final int money;
 
     public Payment(final String money) {
-        this.money = Integer.parseInt(money);
+        this.money = validateMoney(money);
+    }
+
+    private static int validateMoney(final String moneyString) throws IllegalArgumentException {
+        final int money = Integer.parseInt(moneyString);
+        checkAmount(money);
+        return money;
+    }
+
+    private static void checkAmount(final int money) {
+        if (money <= 0 || money % 1000 != 0) {
+            throw new IllegalArgumentException();
+        }
     }
 }
--- src/test/java/lotto/domain/PaymentTest.java
+++ src/test/java/lotto/domain/PaymentTest.java
@@ -1,9 +1,12 @@
 package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class PaymentTest {
     @Test
@@ -14,5 +17,25 @@ public class PaymentTest {
 
         // when and then
         assertThat(new Payment(money)).isInstanceOf(Payment.class);
+    }
+
+    @Test
+    @DisplayName("지불 금액이 숫자가 아니면 IllegalArgumentException을 발생시킨다")
+    void when_money_is_not_a_number_should_throw_IllegalArgumentException() {
+        // given
+        final String money = "money";
+
+        // when and then
+        assertThatThrownBy(() -> new Payment(money))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "-1000", "900", "1900"})
+    @DisplayName("지불 금액이 1000원 단위가 아니면 IllegalArgumentException을 발생시킨다")
+    void when_amount_of_money_is_invalid_should_throw_IllegalArgumentException(final String money) {
+        // when and then
+        assertThatThrownBy(() -> new Payment(money))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
