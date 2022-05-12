50f49ef93192f7d4917d6791152526f8fa4a2f95
안재성 <jaesungahn91@gmail.com>
2022-05-13 01:39:06 +0900

    Merge pull request #426
    * docs: 문자열 덧셈 계산기 기능 요구사항 정리
    
    * docs: 기능 요구사항 추가
    
    * feat: 구분자를 기준으로 분리한 각 숫자의 합을 반환하는 기능구현
    
    * feat: 빈문자열 또는 null, 숫자 하나의 문자열 검증
    
    * feat: 커스텀 구분자 사용 기능구현
    
    * feat: 숫자 이외의 값 또는 음수 예외처리

---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,53 @@
+package calculator;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+
+    private static final int ZERO = 0;
+    private static final String DELIMITER = "[,:]";
+    private static final String DELIMITER_REGX = "//(.)\n(.*)";
+
+    public static int splitAndSum(String input) {
+        if (isNullOrEmpty(input)) {
+            return ZERO;
+        }
+        return sum(changeNumbers(split(input)));
+    }
+
+    private static boolean isNullOrEmpty(String input) {
+        return input == null || input.isEmpty();
+    }
+
+    private static String[] split(String input) {
+        Matcher matcher = Pattern.compile(DELIMITER_REGX).matcher(input);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            return matcher.group(2).split(customDelimiter);
+        }
+
+        return input.split(DELIMITER);
+    }
+
+    private static int sum(int[] inputs) {
+        if (isNegativeNumber(inputs)) {
+            throw new RuntimeException();
+        }
+        return Arrays.stream(inputs).sum();
+    }
+
+    private static int[] changeNumbers(String[] inputs) {
+        try {
+            return Arrays.stream(inputs).mapToInt(Integer::parseInt).toArray();
+        } catch (NumberFormatException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    private static boolean isNegativeNumber(int[] inputs) {
+        return Arrays.stream(inputs).anyMatch(input -> input < ZERO);
+    }
+
+}
