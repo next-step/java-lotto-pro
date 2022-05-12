25c073fe550c0393f3d8510bf2f579107462358b
WonChul Heo <heowc1992@gmail.com>
2022-05-12 22:04:42 +0900

    feat(LottoExchanger): LottoExchanger 구현

--- src/main/java/lotto/LottoExchanger.java
+++ src/main/java/lotto/LottoExchanger.java
@@ -4,17 +4,37 @@ import lotto.lotto.Lotto;
 import lotto.lotto.LottoGenerator;
 import lotto.money.Money;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static java.util.Objects.requireNonNull;
 
 public class LottoExchanger {
 
     private final LottoGenerator lottoGenerator;
 
     public LottoExchanger(LottoGenerator lottoGenerator) {
-        throw new RuntimeException("create");
+        this.lottoGenerator = requireNonNull(lottoGenerator, "lottoGenerator");
+    }
+
+    LottoExchanger() {
+        this(LottoGenerator.random());
     }
 
     public List<Lotto> exchange(Money money) {
-        throw new RuntimeException("exchange");
+        final List<Lotto> purchasedLottoes = new ArrayList<>();
+        Money remainMoney = money;
+        while (true) {
+            final Lotto lotto = lottoGenerator.generate();
+            if (!remainMoney.canPurchase(lotto)) {
+                break;
+            }
+            remainMoney = remainMoney.purchase(lotto);
+            purchasedLottoes.add(lotto);
+        }
+        if (purchasedLottoes.isEmpty()) {
+            throw new NothingToPurchasedLottoException();
+        }
+        return purchasedLottoes;
     }
 }
--- src/main/java/lotto/NothingToPurchasedLottoException.java
+++ src/main/java/lotto/NothingToPurchasedLottoException.java
@@ -1,4 +1,8 @@
 package lotto;
 
 public class NothingToPurchasedLottoException extends RuntimeException {
+
+    public NothingToPurchasedLottoException() {
+        super("구매한 Lotto가 없습니다.");
+    }
 }
