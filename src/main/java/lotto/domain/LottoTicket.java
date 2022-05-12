c01d644d86372c7a067fddf84524defb9a47531c
Ttungga <torb2002@gmail.com>
2022-05-13 02:32:35 +0900

    feat: add validation logics to LottoTicket
    - add LottoTicket.validateNumbers, LottoTicket.checkAmountOfNumbers, and LottoTicket.checkRangeOfNumber

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -3,10 +3,33 @@ package lotto.domain;
 import java.util.Arrays;
 
 public class LottoTicket {
+    private static final int AMOUNT_OF_NUMBERS = 6;
+    private static final int MINIMUM_NUMBER = 1;
+    private static final int MAXIMUM_NUMBER = 45;
     private final int[] numbers;
 
     public LottoTicket(final int[] numbers) {
+        validateNumbers(numbers);
         this.numbers = numbers;
+    }
+
+    private static void validateNumbers(final int[] numbers) {
+        checkAmountOfNumbers(numbers);
+        for (final int number : numbers) {
+            checkRangeOfNumber(number);
+        }
+    }
+
+    private static void checkAmountOfNumbers(final int[] numbers) {
+        if (numbers.length != AMOUNT_OF_NUMBERS) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void checkRangeOfNumber(final int number) {
+        if (number < MINIMUM_NUMBER || number > MAXIMUM_NUMBER) {
+            throw new IllegalArgumentException();
+        }
     }
 
     @Override
--- src/test/java/lotto/domain/LottoTicketTest.java
+++ src/test/java/lotto/domain/LottoTicketTest.java
@@ -2,6 +2,9 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -15,5 +18,43 @@ public class LottoTicketTest {
         // when and then
         assertThat(new LottoTicket(numbers)).isInstanceOf(LottoTicket.class);
         assertThat(new LottoTicket(numbers)).isEqualTo(new LottoTicket(numbers));
+    }
+
+    @Test
+    @DisplayName("로또 번호가 6개가 아니면 IllegalArgumentException을 발생시킨다")
+    void when_amount_of_numbers_is_not_six_should_throw_IllegalArgumentException() {
+        // given
+        final int[] empty = {};
+        final int[] lessThanSix = {1};
+        final int[] greaterThanSix = {1, 2, 3, 4, 5, 6, 7};
+        final List<int[]> numbersList = new ArrayList<>();
+        numbersList.add(empty);
+        numbersList.add(lessThanSix);
+        numbersList.add(greaterThanSix);
+
+        // when and then
+        for (int[] numbers : numbersList) {
+            Assertions.assertThatThrownBy(() -> new LottoTicket(numbers))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+    @Test
+    @DisplayName("1보다 작거나 45보다 큰 번호가 하나라도 있으면 IllegalrgumentException을 발생시킨다")
+    void when_number_is_invalid_should_throw_IllegalArgumentException() {
+        // given
+        final int[] zero = {1, 2, 3, 4, 5, 0};
+        final int[] lessThanZero = {-1, 1, 2, 3, 4, 5};
+        final int[] greaterThanFortyFIve = {1, 2, 46, 3, 4, 5};
+        final List<int[]> numbersList = new ArrayList<>();
+        numbersList.add(zero);
+        numbersList.add(lessThanZero);
+        numbersList.add(greaterThanFortyFIve);
+
+        // when and then
+        for (int[] numbers : numbersList) {
+            Assertions.assertThatThrownBy(() -> new LottoTicket(numbers))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
     }
 }
