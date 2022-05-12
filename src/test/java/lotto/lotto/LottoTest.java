b5e7887db20d18993cdf4257a59445118b1f983f
WonChul Heo <heowc1992@gmail.com>
2022-05-12 17:37:15 +0900

    test(Lotto): 오타 수정

--- src/test/java/lotto/lotto/LottoTest.java
+++ src/test/java/lotto/lotto/LottoTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
 import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -20,21 +21,18 @@ class LottoTest {
     @DisplayName("생성된 Lotto 클래스는 LottoNumber가 정렬된 상태로 존재")
     @ParameterizedTest
     @ArgumentsSource(CreateAndSortedArgumentsProvider.class)
-    void createAndSorted(List<Integer> numbers, List<Integer> expected) {
+    void createAndSorted(List<Integer> numbers) {
         Lotto lotto = Lotto.of(numbers);
-        assertThat(lotto.getLottoNumbers()).isEqualTo(expected);
+        assertThat(lotto.getLottoNumbers()).isSorted();
     }
 
     static class CreateAndSortedArgumentsProvider implements ArgumentsProvider {
         @Override
         public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
             return Stream.of(
-                    Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 6),
-                                 Arrays.asList(1, 2, 3, 4, 5, 6)),
-                    Arguments.of(Arrays.asList(2, 1, 4, 3, 6, 5),
-                                 Arrays.asList(1, 2, 3, 4, 5, 6)),
-                    Arguments.of(Arrays.asList(6, 5, 4, 3, 2, 1),
-                                 Arrays.asList(1, 2, 3, 4, 5, 6))
+                    Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                    Arguments.of(Arrays.asList(2, 1, 4, 3, 6, 5)),
+                    Arguments.of(Arrays.asList(6, 5, 4, 3, 2, 1))
             );
         }
     }
@@ -52,12 +50,13 @@ class LottoTest {
 
     @DisplayName("생성시 로또 번호의 갯수가 올바르지 않아 `IncorrectLottoNumberSizeException` 발생")
     @ParameterizedTest
+    @NullAndEmptySource
     @ArgumentsSource(IncorrectLottoNumberSizeArgumentsProvider.class)
     void incorrectLottoNumberSize(List<Integer> numbers) {
         assertThatThrownBy(() -> {
             Lotto.of(numbers);
         })
-        .isInstanceOf(AlreadyExistsLottoNumberException.class)
+        .isInstanceOf(IncorrectLottoNumberSizeException.class)
         .hasMessageContaining("로또 번호의 갯수가 올바르지 않습니다.");
     }
 
@@ -65,7 +64,6 @@ class LottoTest {
         @Override
         public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
             return Stream.of(
-                    Arguments.of(Collections.emptyList()),
                     Arguments.of(Collections.singletonList(1)),
                     Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 6, 7))
             );
