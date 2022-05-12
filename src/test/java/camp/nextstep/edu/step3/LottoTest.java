27fc3e32b104d179eae5a88223b3894ad501c62d
mannue <sppe12@gmail.com>
2022-05-13 02:43:44 +0900

    test(Lotto): 테스트 케이스 추가 로 인하여 리팩토링

--- src/test/java/camp/nextstep/edu/step3/LottoTest.java
+++ src/test/java/camp/nextstep/edu/step3/LottoTest.java
@@ -1,8 +1,11 @@
 package camp.nextstep.edu.step3;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
@@ -10,14 +13,22 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOf
 public class LottoTest {
 
     @DisplayName("로또는 숫자 6개를 입력이 안될 경우 RuntimeException 을 발생한다")
-    @Test
-    void invalidInputTest() {
+    @ParameterizedTest
+    @MethodSource("provideIntegerArrays")
+    void invalidInputTest(final int[] numbers) {
         assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> new Lotto(createLottoNumberArray(1)))
+                .isThrownBy(() -> new Lotto(createLottoNumberArray(numbers)))
                 .withMessageContaining("LottoNumberArray invalid size : ");
     }
 
-    private LottoNumber[] createLottoNumberArray(final Integer ... numbers) {
-        return  Stream.of(numbers).map(LottoNumber::new).toArray(LottoNumber[]::new);
+    private static Stream<Arguments> provideIntegerArrays() {
+        return Stream.of(
+                Arguments.of(new int[]{1}),
+                Arguments.of(new int[]{1, 2, 3, 4, 5})
+        );
+    }
+
+    private LottoNumber[] createLottoNumberArray(final int[] numbers) {
+        return Arrays.stream(numbers).mapToObj(LottoNumber::new).toArray(LottoNumber[]::new);
     }
 }
