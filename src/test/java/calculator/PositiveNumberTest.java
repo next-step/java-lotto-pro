595fc1ac83215aacf68a047b4a1eadc35347c746
kwonyongil <ksgkms23@likelion.org>
2022-05-12 10:15:00 +0900

    refactor: 테스트메서드 재명명

--- src/test/java/calculator/PositiveNumberTest.java
+++ src/test/java/calculator/PositiveNumberTest.java
@@ -1,7 +1,7 @@
 package calculator;
 
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -21,17 +21,17 @@ class PositiveNumberTest {
     @ParameterizedTest(name = "음수 {0}를 문자열로 초기화하면 RuntimeException 예외가 발생한다.")
     @ValueSource(strings = {"-1", "-3", "-5"})
     void parseNotNegativeInt_negative(String input) {
-        assertThatThrownBy(() -> PositiveNumber.parseNotNegativeNumber(input))
-                .isInstanceOf(RuntimeException.class)
-                .hasMessageContaining("음수가 입력되었습니다. 0 이상 값을 입력해주세요.");
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> PositiveNumber.parseNotNegativeNumber(input))
+                .withMessage("음수가 입력되어 유효하지 않습니다.");
     }
 
     @DisplayName("숫자가 아닌 문자열로 초기화하면 RuntimeException 예외가 발생한다.")
     @ParameterizedTest(name = "숫자가 아닌 문자열 {0}로 초기화하면 RuntimeException 예외가 발생한다.")
     @ValueSource(strings = {"!", ":", "e", "rr"})
     void parseNotNegativeInt_number(String input) {
-        assertThatThrownBy(() -> PositiveNumber.parseNotNegativeNumber(input))
-                .isInstanceOf(RuntimeException.class)
-                .hasMessageContaining("숫자 이외의 값이 입력되었습니다. 숫자를 입력해주세요.");
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> PositiveNumber.parseNotNegativeNumber(input))
+                .withMessage("유효하지 않은 입력값입니다.");
     }
 }
--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -1,7 +1,7 @@
 package calculator;
 
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.junit.jupiter.api.DisplayName;
@@ -13,7 +13,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 class StringAddCalculatorTest {
     @DisplayName("null 또는 빈문자는 0을 반환한다.")
     @Test
-    void splitAndSum_null_또는_빈문자() {
+    void splitAndSum_null_or_empty() {
         assertAll(
                 () -> assertThat(StringAddCalculator.splitAndSum("")).isZero(),
                 () -> assertThat(StringAddCalculator.splitAndSum(null)).isZero()
@@ -23,7 +23,7 @@ class StringAddCalculatorTest {
     @DisplayName("숫자 하나를 문자열로 입력받은 경우 해당 숫자를 반환한다.")
     @ParameterizedTest(name = "숫자 {0}를 문자열로 입력받은 경우 {1} 숫자 반환한다.")
     @CsvSource(value = {"1:1", "2:2", "3:3", "15:15"}, delimiter = ':')
-    void splitAndSum_숫자하나(String input, int expect) {
+    void splitAndSum_one_number(String input, int expect) {
         int result = StringAddCalculator.splitAndSum(input);
         assertThat(result).isEqualTo(expect);
     }
@@ -31,7 +31,7 @@ class StringAddCalculatorTest {
     @DisplayName("숫자 두개를 컴마(,) 구분자로 입력할 경우 숫자의 합을 반환한다.")
     @ParameterizedTest(name = "숫자 {0}를 컴마(,) 구분자로 입력할 경우 합 {1}을 반환한다.")
     @CsvSource(value = {"1,2:3", "2,2:4", "3,0:3", "15,15:30", "1,2,3:6"}, delimiter = ':')
-    void splitAndSum_쉼표구분자(String input, int expect) {
+    void splitAndSum_comma_delimiter(String input, int expect) {
         int result = StringAddCalculator.splitAndSum(input);
         assertThat(result).isEqualTo(expect);
     }
@@ -39,14 +39,14 @@ class StringAddCalculatorTest {
     @DisplayName("컴마(,), 콜론(:) 구분자로 숫자의 합을 반환한다.")
     @ParameterizedTest(name = "숫자 {0}를 컴마(,), 콜론(:) 구분자로 입력할 경우 합 {1}을 반환한다.")
     @CsvSource(value = {"1:2/3", "2:2/4", "3:0,3/6", "15,15:70/100", "1,2:2:3/8"}, delimiter = '/')
-    void splitAndSum_쉼표_또는_콜론_구분자(String input, int expect) {
+    void splitAndSum_comma_or_colon_delimiter(String input, int expect) {
         int result = StringAddCalculator.splitAndSum(input);
         assertThat(result).isEqualTo(expect);
     }
 
     @DisplayName("“//”와 “\\n” 문자 사이에 커스텀 구분자로 숫자합을 반환한다")
     @Test
-    void splitAndSum_custom_구분자() {
+    void splitAndSum_custom_delimiter() {
         assertAll(
                 () -> assertThat(StringAddCalculator.splitAndSum("//;\n1;2;3")).isEqualTo(6),
                 () -> assertThat(StringAddCalculator.splitAndSum("//q\n1q3q3")).isEqualTo(7)
@@ -57,17 +57,17 @@ class StringAddCalculatorTest {
     @ParameterizedTest(name = "음수를 포함한 {0}를 전달하면 RuntimeException 예외가 발생한다.")
     @ValueSource(strings = {"3,-1", "-1:-1", "1:2:-3"})
     void splitAndSum_negative(String input) {
-        assertThatThrownBy(() -> StringAddCalculator.splitAndSum(input))
-                .isInstanceOf(RuntimeException.class)
-                .hasMessageContaining("음수가 입력되었습니다. 0 이상 값을 입력해주세요.");
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> StringAddCalculator.splitAndSum(input))
+                .withMessage("음수가 입력되어 유효하지 않습니다.");
     }
 
     @DisplayName("숫자가 아닌 값이 포함된 문자열을 전달하면 RuntimeException 예외가 발생한다.")
     @ParameterizedTest(name = "숫자가 아닌 값이 포함된 문자열 {0}를 전달하면 RuntimeException 예외가 발생한다.")
     @ValueSource(strings = {"3,p", ":3", "1::3", "e:rr:or"})
     void splitAndSum_not_number(String input) {
-        assertThatThrownBy(() -> StringAddCalculator.splitAndSum(input))
-                .isInstanceOf(RuntimeException.class)
-                .hasMessageContaining("숫자 이외의 값이 입력되었습니다. 숫자를 입력해주세요.");
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> StringAddCalculator.splitAndSum(input))
+                .withMessage("유효하지 않은 입력값입니다.");
     }
 }
