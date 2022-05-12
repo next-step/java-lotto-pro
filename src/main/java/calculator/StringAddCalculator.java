c0716363eacc3391718616cff15e323df05c43b3
breezeunn <je7689@gmail.com>
2022-05-13 08:56:38 +0900

    feat(StringAddCalculator): null 또는 빈문자 입력값 처리

---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class StringAddCalculator {
+    public static int splitAndSum(String input) {
+        if (input == null || input.trim().isEmpty()) {
+            return 0;
+        }
+
+        return -1;
+    }
+}
