3eca98f1fd5a8d11a86bdf0f8d165bd6c5a7f3d8
soob-forest <dltntnqo@naver.com>
2022-05-12 20:15:11 +0900

    refactor: 적절한 패키지로 변경
    - study -> calculator

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,4 +1,4 @@
-package study;
+package calculator;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -1,4 +1,4 @@
-package study;
+package calculator;
 
 import org.junit.jupiter.api.Test;
 
