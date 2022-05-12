319e2ee7d1d3bcc2c583093aee585b7898e5ea60
jiyeonghwang <gyp0288@naver.com>
2022-05-12 15:20:16 +0900

    feat: Numbers, Parse 적용

--- src/main/java/stringAddCalculator/StringAddCalculator.java
+++ src/main/java/stringAddCalculator/StringAddCalculator.java
@@ -1,54 +1,33 @@
 package stringAddCalculator;
 
+import stringAddCalculator.domain.Numbers;
+import stringAddCalculator.utils.Parse;
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import static stringAddCalculator.utils.Validation.*;
 
 public class StringAddCalculator {
-    public static int splitAndSum(String input) {
+    public static final int ZERO = 0;
+    public static int splitAndSum(String input) throws IllegalAccessException {
+        Numbers numbers;
         if (isNotNullAndEmpty(input)){
-            return 0;
+            return ZERO;
         }
         if (isNumber(input)) {
             return Integer.parseInt(input);
         }
         if (!input.startsWith("//")) {
-            return splitSumNormal(input);
+            numbers = new Numbers(Parse.splitNormal(input));
+            return numbers.sumNumbers();
         }
-
-        return splicSumCustom(input);
-    }
-    
-    public static int splitSumNormal(String input) {
-        String[] numbers = input.split(",|:");
-        return addArr(numbers);
+        numbers = new Numbers(Parse.splitCustom(input));
+        return numbers.sumNumbers();
     }
 
-    public static int splicSumCustom(String input) {
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
-        if (m.find()) {
-            String customDelimiter = m.group(1);
-            String[] tokens= m.group(2).split(customDelimiter);
-            return addArr(tokens);
-        }
-        throw new RuntimeException();
-    }
-    
-    public static int addArr(String[] tokens) {
-        int result = 0;
-        for (String s : tokens) {
-            isNumberAndSizeOne(s);
-            result += Integer.parseInt(s);
-        }
-        return result;
-    }
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        splitAndSum(br.readLine());
+        System.out.println(splitAndSum(br.readLine()));
     }
 }
