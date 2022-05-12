3ddf76f578ba4661a449700a752c5004a1d03cd0
Seung Hun Jee <js223kr92@naver.com>
2022-05-13 02:51:51 +0900

    Step2 : 문자열 계산기 (#429)
    * test : StringAddCalculatorTest 추가
    
    * docs : 초기 설계 및 기능목록 추가
    
    * feat : 빈문자열, null을 입력받은 경우 처리 기능
    
    * docs : 설계 내용 보강
    
    * feat : 표현식 해석기 추가 및 숫자 계산 기능 추가
    
    * format : 포맷 정리

---
+++ docs/StringAddCalculator.md
@@ -0,0 +1,17 @@
+# 설계
+## 입력값 = expression
+## expression = custom delimiter part + numbers part
+- //로 시작하면 custom delimiter part가 있는 걸로 판단할 수 있다.
+
+## Delimiter를 생성하는 역할
+- 디폴트 Delimiter는 ",|:" 로 한다.
+
+## 숫자를 계산하는 역할 
+- 양수만 계산 가능(음수를 받은 경우 RuntimeException을 던진다.)
+
+---
+
+# 기능목록
+- [X] 빈 문자열 또는 null 값을 입력할 경우 0을 반환
+- [X] 표현식해석기 추가
+- [X] 숫자 계산 기능 추가
---
+++ src/main/java/ExpressionInterpretor.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExpressionInterpretor {
+    private static final String DEFAULT_DELIMITER = ",|:";
+
+    private String expression;
+
+    private String[] numbers;
+
+    public ExpressionInterpretor(String expression) {
+        this.expression = expression;
+        interpret();
+    }
+
+    private void interpret() {
+        if (hasCustomDelimitPart()) {
+            Matcher m = Pattern.compile("//(.)\n(.*)").matcher(expression);
+            m.find();
+            String customDelimiter = m.group(1);
+            numbers = m.group(2).split(customDelimiter);
+            return;
+        }
+        numbers = expression.split(DEFAULT_DELIMITER);
+        return;
+    }
+
+    private boolean hasCustomDelimitPart() {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(expression);
+        return m.find();
+    }
+
+    public List<Integer> getNumbers() {
+        List<Integer> numberList = new ArrayList<>();
+        for (String numberString : numbers) {
+            int number = Integer.parseInt(numberString);
+            checkNagative(number);
+            numberList.add(number);
+        }
+        return numberList;
+    }
+
+    private void checkNagative(int number) {
+        if (number < 0) {
+            throw new RuntimeException();
+        }
+    }
+}
