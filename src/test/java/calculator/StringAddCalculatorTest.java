34d7f3b5f534aaf17c522beb3466a70bb499a2e4
Min Je Jang <minjjang1117@gmail.com>
2022-05-12 11:07:27 +0900

    2단계 - 문자열 덧셈 계산기 (#405)
    * test: string 학습 테스트 작성
    
    * test: set 학습 테스트 작성
    
    * refactor(SetTest): 학습 테스트 피드백 반영
    
    - AssertJ 학습 테스트 생성
    
    * docs: 문자열 덧셈 계산기 구현 기능 정리
    
    * feat: 빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다
    
    * feat: 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다
    
    * feat: 숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다
    
    * feat: 구분자를 컴마(,) 이외에 콜론(:)을 사용할 수 있다
    
    * feat: '//'와 '\n' 문자 사이에 커스텀 구분자를 지정할 수 있다
    
    * feat: 문자열 계산기에 음수를 전달하는 경우 RuntimeException 예외가 발생한다

---
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -0,0 +1,83 @@
+package calculator;
+
+import exception.ExceptionMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class StringAddCalculatorTest {
+
+    @DisplayName("빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다")
+    @ParameterizedTest
+    @MethodSource
+    void nullOrEmptyInputReturnZero(String input, int expected) {
+        // when
+        int result = StringAddCalculator.add(input);
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> nullOrEmptyInputReturnZero() {
+        return Stream.of(
+                Arguments.of(
+                        null, 0
+                ),
+                Arguments.of(
+                        "", 0
+                )
+        );
+    }
+
+    @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다")
+    @Test
+    void numberInputReturnCorrespondingNumber() {
+        // when
+        int result = StringAddCalculator.add("10");
+        // then
+        assertThat(result).isEqualTo(10);
+    }
+
+    @DisplayName("숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다")
+    @Test
+    void twoNumbersInputReturnSumOfNumbers() {
+        // when
+        int result = StringAddCalculator.add("1,2");
+        // then
+        assertThat(result).isEqualTo(3);
+    }
+
+    @DisplayName("구분자를 컴마(,) 이외에 콜론(:)을 사용할 수 있다")
+    @Test
+    void colonsUsedAsDelimiters() {
+        // when
+        int result = StringAddCalculator.add("1:2,3");
+        // then
+        assertThat(result).isEqualTo(6);
+    }
+
+    @DisplayName("“//”와 “\n” 문자 사이에 커스텀 구분자를 지정할 수 있다")
+    @Test
+    void customDelimiters() {
+        // when
+        int result = StringAddCalculator.add("//;\n1;2;7");
+        // then
+        assertThat(result).isEqualTo(10);
+    }
+
+    @DisplayName("문자열 계산기에 음수를 전달하는 경우 RuntimeException 예외가 발생한다")
+    @Test
+    void negative() {
+        // when and then
+        assertThatThrownBy(() -> StringAddCalculator.add("-1,2"))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage(ExceptionMessage.NOT_POSITIVE_NUMBER.message());
+    }
+
+}
