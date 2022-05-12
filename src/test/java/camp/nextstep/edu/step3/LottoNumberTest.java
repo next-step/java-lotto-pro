f2e4cbe48c1fb9022e369ab739b808bd34181c72
mannue <sppe12@gmail.com>
2022-05-13 01:17:42 +0900

    refactor(LottoNumber): 테스트 케이스 추가 로 인하여 리팩토링

--- src/test/java/camp/nextstep/edu/step3/LottoNumberTest.java
+++ src/test/java/camp/nextstep/edu/step3/LottoNumberTest.java
@@ -1,16 +1,17 @@
 package camp.nextstep.edu.step3;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoNumberTest {
 
     @DisplayName("1부터 45까지 정수 값만 가진다")
-    @Test
-    void createTest() {
-        LottoNumber one = new LottoNumber(1);
-        assertThat(one).isEqualTo(new LottoNumber(1));
+    @ParameterizedTest
+    @ValueSource(ints = {1, 45})
+    void createTest(final int number) {
+        assertThat(new LottoNumber(number)).isEqualTo(new LottoNumber(number));
     }
 }
