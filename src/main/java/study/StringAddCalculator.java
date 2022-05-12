911197078504c0d1724a829e523bc06e0aeaf0fa
boss.back <boss.back@kakaocorp.com>
2022-05-13 03:14:26 +0900

    feat: “//”와 “\n” 문자 사이에 커스텀 구분자를 지정 및 리팩토링

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -1,18 +1,55 @@
 package study;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
 
-    private static final String SEPARATOR = ",|:";
+    private static final int DELIMITER_INDEX = 1;
+    private static final int TEXT_INDEX = 2;
+
+    private static final String DELIMITER = ",|:";
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
 
     public static int splitAndSum(String text) {
         if (isNullOrEmpty(text)) {
             return 0;
         }
-        return sumNumbers(convertStringArrayToIntArray(splitText(text)));
+        if (hasCustomDelimiter(text)) {
+            return sumNumbers(convertStringArrayToIntArray(splitTextWithCustomDelimiter(text)));
+        }
+        return sumNumbers(convertStringArrayToIntArray(splitText(text, DELIMITER)));
     }
 
-    private static String[] splitText(String text) {
-        return text.split(SEPARATOR);
+    private static boolean isNullOrEmpty(String text) {
+        if (text == null || text.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean hasCustomDelimiter(String text) {
+        return CUSTOM_DELIMITER_PATTERN.matcher(text).find();
+    }
+
+    private static String[] splitTextWithCustomDelimiter(String text) {
+        Matcher matcher = makePatterFindMatcher(text);
+        String customDelimiter = findCustomDelimiter(matcher);
+        return splitText(findInputText(matcher), customDelimiter);
+    }
+
+    private static Matcher makePatterFindMatcher(String text){
+        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(text);
+        matcher.find();
+        return matcher;
+    }
+
+    private static String findCustomDelimiter(Matcher matcher) {
+        return matcher.group(DELIMITER_INDEX);
+    }
+
+    private static String findInputText(Matcher matcher) {
+        return matcher.group(TEXT_INDEX);
     }
 
     private static int[] convertStringArrayToIntArray(String[] stringNumbers) {
@@ -23,6 +60,10 @@ public class StringAddCalculator {
         return numbers;
     }
 
+    private static String[] splitText(String text, String delimiter) {
+        return text.split(delimiter);
+    }
+
     private static int sumNumbers(int[] numbers) {
         int sum = 0;
         for (int number : numbers) {
@@ -31,10 +72,4 @@ public class StringAddCalculator {
         return sum;
     }
 
-    private static boolean isNullOrEmpty(String text) {
-        if (text == null || text.isEmpty()) {
-            return true;
-        }
-        return false;
-    }
 }
