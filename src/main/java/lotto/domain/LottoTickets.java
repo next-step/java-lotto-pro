df5d67ae397907dc7fbd58769655b302aa3c9a7a
Ttungga <torb2002@gmail.com>
2022-05-13 03:32:12 +0900

    feat: add LottoTickets

---
+++ src/main/java/lotto/domain/LottoTickets.java
@@ -0,0 +1,29 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class LottoTickets {
+    private static final List<Integer> LOTTO_NUMBER_CANDIDATES = IntStream.range(LottoTicket.MINIMUM_NUMBER, LottoTicket.MAXIMUM_NUMBER + 1)
+            .boxed()
+            .collect(Collectors.toList());
+
+    private final List<LottoTicket> lottoTickets;
+
+    public LottoTickets(final int amountOfTickets) {
+        lottoTickets = new ArrayList<>();
+        for (int i = 0; i < amountOfTickets; i++){
+            lottoTickets.add(createTicketAutomatically());
+        }
+    }
+
+    private static LottoTicket createTicketAutomatically() {
+        Collections.shuffle(LOTTO_NUMBER_CANDIDATES);
+        List<Integer> numbers = LOTTO_NUMBER_CANDIDATES.subList(0, 7);
+        Collections.sort(numbers);
+        return new LottoTicket(numbers);
+    }
+}
