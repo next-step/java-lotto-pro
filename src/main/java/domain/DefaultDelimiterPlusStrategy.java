726623782cf4d82133720f911c9e738f61baf4fb
Woobeen <drogba02@naver.com>
2022-05-12 14:43:14 +0900

    2단계 - 문자열 덧셈 계산기 (#396)
    * docs: 문자열 덧셈 계산기에 대한 기능명세를 작성하라
    
    * test: 문자열 덧셈 계산기에 대한 테스트 case 를 추가하라
    
    * feature: 식에 대해 어떤 case 인지 판단하는 util 클래스를 추가하라
    
    - 공백/null 값에 대해 판단하는 테스트 코드를 작성하라
    
    * feature: 예외처리를 위한 상수 클래스를 추가하라
    
    * feature: 해당 식의 계산을 위한 Strategy 클래스, 해당 클래스를 반환하기 위한 Factory 클래스를 작성하라
    
    - 각 식에 대한 case는 각자의 클래스를 갖는다
    
    * test: 공백 혹은 null 값이 전달되는 case 에 대해 테스트 코드를 작성하라
    
    * test: 숫자 하나를 문자열로 전달하는 case 에 대한 테스트 코드를 작성하라
    
    * feature: 숫자 하나를 문자열로 전달하는 경우 해당 숫자를 반환하는 기능을 구현하라
    
    * test: 컴마로 구분된 숫자 두개를 문자열로 전달하는 case 에 대한 테스트 코드를 작성하라
    
    * feature:  컴마로 구분된 숫자 두개를 문자열로 전달하는 경우 해당 숫자의 합을 반환하는 기능을 구현하라
    
    * test: DefaultDelimiterPlusStrategy 에 대해 추가 테스트 case 를 작성하라
    
    - 세 수 이상, 컴마/콜론 혼용시에도 정상적으로 통과되어야 한다
    
    * test: CustomDelimiterPlusStrategy 에 대해 추가 테스트 case 를 작성하라
    
    * feature: 커스텀 구분자가 포함된 식에 대해 숫자의 합을 반환하는 기능을 구현하라
    
    * feature: StringAddCalculator 를 구현하라
    
    * refactor: 상황에 맞는 예외를 반환하라
    
    * test: 음수, 알 수 없는 형식에 대한 테스트 case 를 추가하라

---
+++ src/main/java/domain/DefaultDelimiterPlusStrategy.java
@@ -0,0 +1,14 @@
+package domain;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DefaultDelimiterPlusStrategy implements PlusStrategy {
+    private static final String DEFAULT_DELIMITER = ",|:";
+
+    @Override
+    public int result(String expressionStr) {
+        List<String> stringList = Arrays.asList(expressionStr.split(DEFAULT_DELIMITER));
+        return stringList.stream().mapToInt(Integer::parseInt).sum();
+    }
+}
---
+++ src/main/java/domain/PlusStrategyFactory.java
@@ -0,0 +1,25 @@
+package domain;
+
+import exception.ExceptionType;
+
+public class PlusStrategyFactory {
+    public PlusStrategy getStrategy(String expressionStr) {
+        if (ExpressionUtils.isEmptyExpression(expressionStr)) {
+            return new EmptyPlusStrategy();
+        }
+
+        if (ExpressionUtils.isSingleNumberExpression(expressionStr)) {
+            return new SingleNumberPlusStrategy();
+        }
+
+        if (ExpressionUtils.isDefaultExpression(expressionStr)) {
+            return new DefaultDelimiterPlusStrategy();
+        }
+
+        if (ExpressionUtils.isCustomExpression(expressionStr)) {
+            return new CustomDelimiterPlusStrategy();
+        }
+
+        throw new IllegalArgumentException(ExceptionType.INVALID_EXPRESSION.getMessage());
+    }
+}
