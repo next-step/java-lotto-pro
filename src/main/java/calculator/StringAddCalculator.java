b03e3109355627c493a7d9ba433a3bf8395c4273
soob-forest <dltntnqo@naver.com>
2022-05-13 01:38:34 +0900

    refactor(StringAddCalculator): 문자열 자르기를 StringSplitter class를 사용하도록 변경

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,8 +1,8 @@
 package calculator;
 
+import calculator.domain.StringSplitter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
@@ -11,6 +11,8 @@ public class StringAddCalculator {
 
     private static final String MSG_NOT_NUMBER = "숫자 이외의 값을 입력으로 받을 수 없습니다.";
     private static final String MSG_NEGATIVE = "음수를 입력으로 받을 수 없습니다";
+
+    private static final StringSplitter splitter = new StringSplitter(DELIMITER, CUSTOM_DELIMITER_PATTERN);
 
     static int splitAndSum(String input) {
 
@@ -27,20 +29,11 @@ public class StringAddCalculator {
     private static List<Integer> splitToNumberArray(String input) {
 
         List<Integer> numbers = new ArrayList<>();
-        for (String splitInput : split(input)) {
+        for (String splitInput : splitter.split(input)) {
             validateSplitInput(splitInput);
             numbers.add(Integer.parseInt(splitInput));
         }
         return numbers;
-    }
-
-    private static String[] split(String input) {
-        Matcher m = CUSTOM_DELIMITER_PATTERN.matcher(input);
-        if (m.find()) {
-            String customDelimiter = m.group(1);
-            return m.group(2).split(customDelimiter);
-        }
-        return input.split(DELIMITER);
     }
 
     private static int sumNumbers(List<Integer> numbers) {
