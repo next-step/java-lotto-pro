237a28c4f847d64074497908110479ef49f797f6
Ttungga <60680768+Ttungga@users.noreply.github.com>
2022-05-12 11:17:19 +0900

    2단계 - 문자열 덧셈 계산기 (#416)
    * refactor: remove Integer.parseInt method call
    
    * feat: create StringAddCalculator, add empty splitAndSum method, and remove empty.txt
    
    * test: create StringAddCalculatorTest and add tests of requirements
    
    * test: add @DisplayName annotations
    
    * feat: add StringAddCalculator.validateInput
    
    * fix: modify StringAddCalculator.validateInput condition
    
    * feat: add StringAddCalculator.splitInput and StringAddCalculator.sumNumbers
    
    * feat: add a function for the custom separator
    
    - separate StringAddCalculator.splitInput into StringAddCalculator.findNumbers and StringAddCalculator.split
    
    * feat: add StringAddCalculator.validateNumber for RuntimeException

---
+++ src/main/java/StringAddCalculator.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+    private static final String DEFAULT_SEPARATORS = ",|:";
+    private static final Pattern CUSTOM_SEPARATOR_PATTERN = Pattern.compile("//(.)\n(.*)");
+
+    public static int splitAndSum(final String input) {
+        if (!validateInput(input)) {
+            return 0;
+        }
+        return sumNumbers(findNumbers(input));
+    }
+
+    private static boolean validateInput(final String input) {
+        return input != null && !input.isEmpty();
+    }
+
+    private static List<Integer> findNumbers(final String input) {
+        final String[] stringNumbers = split(input);
+        final List<Integer> numbers = new ArrayList<>();
+        for (final String stringNumber : stringNumbers) {
+            System.out.println(stringNumber);
+            numbers.add(Integer.parseInt(stringNumber));
+        }
+        return numbers;
+    }
+
+    private static int sumNumbers(final List<Integer> numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            validateNumber(number);
+            sum += number;
+        }
+        return sum;
+    }
+
+    private static String[] split(final String input) {
+        Matcher matcher = CUSTOM_SEPARATOR_PATTERN.matcher(input);
+        if (matcher.find()) {
+            return matcher.group(2).split(matcher.group(1));
+        }
+        return input.split(DEFAULT_SEPARATORS);
+    }
+
+    private static void validateNumber(final int number) {
+        if (number < 0) {
+            throw new RuntimeException();
+        }
+    }
+}
