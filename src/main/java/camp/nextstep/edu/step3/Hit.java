be251058271831dcbc3b0997e64ae6024efdc8c4
mannue <sppe12@gmail.com>
2022-05-13 02:00:07 +0900

    feat(Hit): Hit 는 일치 갯수 와 당첨금을 가진 enum 이다.

--- src/main/java/camp/nextstep/edu/step3/Hit.java
+++ src/main/java/camp/nextstep/edu/step3/Hit.java
@@ -1,7 +1,12 @@
 package camp.nextstep.edu.step3;
 
 public enum Hit {
-    ONE(1, 0);
+    ONE(1, 0),
+    TWO(2, 0),
+    THREE(3, 5000),
+    FOUR(4, 50000),
+    FIVE(5, 1500000),
+    ALL(6, 2000000000);
 
     private final int count;
     private final int prizeMoney;
--- src/test/java/camp/nextstep/edu/step3/HitTest.java
+++ src/test/java/camp/nextstep/edu/step3/HitTest.java
@@ -1,16 +1,36 @@
 package camp.nextstep.edu.step3;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static camp.nextstep.edu.step3.Hit.ONE;
+import java.util.stream.Stream;
+
+import static camp.nextstep.edu.step3.Hit.*;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 public class HitTest {
 
     @DisplayName("Hit 출력시 일치 갯수와 당청금이 출력된다")
-    @Test
-    void printTest() {
-        assertThat(ONE.toString()).isEqualTo("1개 일치 (0원)");
+    @ParameterizedTest
+    @MethodSource("provideHitAndCountAndPrizeMoney")
+    void printTest(final Hit hit, final int count, final int prizeMoney) {
+        assertThat(hit.toString()).isEqualTo(expectedMessage(count, prizeMoney));
+    }
+
+    private String expectedMessage(final int count, final int prizeMoney) {
+        return String.format("%d개 일치 (%d원)", count, prizeMoney);
+    }
+
+    private static Stream<Arguments> provideHitAndCountAndPrizeMoney() {
+        return Stream.of(
+                Arguments.of(ONE, 1, 0),
+                Arguments.of(TWO, 2, 0),
+                Arguments.of(THREE, 3, 5000),
+                Arguments.of(FOUR, 4, 50000),
+                Arguments.of(FIVE, 5, 1500000),
+                Arguments.of(ALL, 6, 2000000000)
+        );
     }
 }
