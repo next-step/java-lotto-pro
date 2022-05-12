753d9ad5d6f28a43dcac0d24663ee70d322c3b0d
Celina <luv.mins99@gmail.com>
2022-05-12 11:08:57 +0900

    refactor(StringAddCalculatorTest): 테스트 케이스 리팩토링
    - given/when/then 패턴 체크
    - 일부 테스트 로직 분리
    - 테스트 클래스와 메서드의 접근 제어자 삭제

--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -3,45 +3,70 @@ package calculator;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class StringAddCalculatorTest {
 
     @Test
-    void splitAndSum_null_또는_빈문자() {
+    @DisplayName("입력값이 null이면 결과는 0이다")
+    void splitAndSum_null() {
+        // given & when
         int result = StringAddCalculator.splitAndSum(null);
-        assertThat(result).isEqualTo(0);
 
-        result = StringAddCalculator.splitAndSum("");
-        assertThat(result).isEqualTo(0);
+        // then
+        assertThat(result).isZero();
+    }
+
+    @Test
+    @DisplayName("입력값이 빈문자이면 결과는 0이다")
+    void splitAndSum_빈문자() {
+        // given & when
+        int result = StringAddCalculator.splitAndSum("");
+
+        // then
+        assertThat(result).isZero();
     }
 
     @Test
     void splitAndSum_숫자하나() {
+        // given & when
         int result = StringAddCalculator.splitAndSum("1");
+
+        // then
         assertThat(result).isEqualTo(1);
     }
 
     @Test
     void splitAndSum_쉼표구분자() {
+        // given & when
         int result = StringAddCalculator.splitAndSum("1,2");
+
+        // then
         assertThat(result).isEqualTo(3);
     }
 
     @Test
     void splitAndSum_쉼표_또는_콜론_구분자() {
+        // given & when
         int result = StringAddCalculator.splitAndSum("1,2:3");
+
+        // then
         assertThat(result).isEqualTo(6);
     }
 
     @Test
-    public void splitAndSum_custom_구분자() {
+    void splitAndSum_custom_구분자() {
+        // given & when
         int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
+
+        // then
         assertThat(result).isEqualTo(6);
     }
 
     @Test
-    public void splitAndSum_negative() {
+    void splitAndSum_negative() {
+        // given & when & then
         assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
                 .isInstanceOf(RuntimeException.class);
     }
