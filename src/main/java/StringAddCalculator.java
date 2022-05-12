524a7dc2ced3a9910e41dd0c117853079c89079b
chj1768 <chj1768@gmail.com>
2022-05-12 10:55:57 +0900

    2단계 - 문자열 덧셈 계산기 (#404)
    * docs: 초기 계획 문서 작성
    
    * feat: test 적용. TDD 를 위한 구현 클래스(StringAddCalculator) 프로토타입 생성
    
    * feat: 빈 문자열 또는 null 값을 입력할 경우 0을 반환하는 요구사항 반영
    
    * feat: 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환하는 요구사항 반영
    
    * feat: 숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환하는 요구사항 구현
    
    * feat: 숫자 여러개가 구분자로 입력할 경우 모든 숫자의 합을 반환하도록 구현 확장
    
    * feat: 구분자로 컴마 이외에 콜론을 사용할 수 있도록 요구사항 구현
    
    * feat: 커스텀 구분자 지정 가능하도록 하는 요구사항 구현
    
    * feat: 음수를 전달할 경우 RuntimeException 예외가 발생하도록 요구사항 구현
    
    * refactor: rename 파라미터명, rename 메서드 내 지역변수(strs)
    
    * refactor: null, 공백 문자열에 대한 판별 로직 메서드 추출
    
    * refactor: 문자열 배열을 sum 하는 중복 코드 메서드로 추출
    
    * refactor: 음수를 다루지 않는다는 특징을 강조하기 위한 메서드 추출
    
    * refactor: split 메서드 추출
    
    * refactor: rename isNotNumericString -> isNotNumeric
    
    * refactor: 매직넘버, 정규식 패턴 추출
    
    * refactor: rename DELIMITER
    
    * refactor: 테스트 코드에 DisplayName, ParameterizedTest 방식 적용하여 테스트케이스 보완
    
    * refactor: repo 내 불필요 파일 제거
    
    * fix: 깨지는 테스트 수정
    
    * fix: 중괄호 반영
    
    * fix: stream sum 방식으로 변경
    
    * fix: 프라이빗 생성자 추가
    
    * fix: docs 수정. 작업 step 별 진행사항 체크박스 목록 추가

---
+++ src/main/java/StringAddCalculator.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+
+    private static final Pattern CUSTOM_DELIMITER = Pattern.compile("//(.)\n(.*)");
+    private static final Pattern DEFAULT_DELIMITER = Pattern.compile(",|:");
+
+    private static final int CUSTOM_DELIMITER_MATCHER_GROUP = 1;
+    private static final int TEXT_MATCHER_GROUP = 2;
+
+    private static final int DEFAULT_SUM = 0;
+
+    private StringAddCalculator() {
+    }
+
+    public static int splitAndSum(String text) {
+        if (isNotNumeric(text)) {
+            return DEFAULT_SUM;
+        }
+
+        String[] tokens = split(text);
+        return sum(tokens);
+    }
+
+    private static String[] split(String text) {
+        Matcher m = CUSTOM_DELIMITER.matcher(text);
+        if (m.find()) {
+            String customDelimiter = m.group(CUSTOM_DELIMITER_MATCHER_GROUP);
+            return m.group(TEXT_MATCHER_GROUP).split(customDelimiter);
+        }
+
+        return text.split(String.valueOf(DEFAULT_DELIMITER));
+    }
+
+    private static int sum(String[] tokens) {
+        return Arrays.stream(tokens).map(StringAddCalculator::getUnsignedInt).reduce(DEFAULT_SUM, Integer::sum);
+    }
+
+    private static int getUnsignedInt(String token) {
+        return Integer.parseUnsignedInt(token);
+    }
+
+    private static boolean isNotNumeric(String text) {
+        return text == null || text.isEmpty();
+    }
+}
