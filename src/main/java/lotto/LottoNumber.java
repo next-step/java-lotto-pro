d40fad90f77763ee06f31784321c254ff8c1b4b7
sangho lee <sangholee.dev@gmail.com>
2022-05-12 23:36:50 +0900

    feat: 로또 번호 추가

---
+++ src/main/java/lotto/LottoNumber.java
@@ -0,0 +1,53 @@
+package lotto;
+
+import java.util.Objects;
+
+public class LottoNumber {
+
+    private static final int START_NUM = 1;
+    private static final int LAST_NUM = 45;
+    private final int number;
+
+    private LottoNumber(final int number) {
+        validateNumber(number);
+        this.number = number;
+    }
+
+    public static LottoNumber valueOf(final int number) {
+        return new LottoNumber(number);
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    private void validateNumber(final int number) {
+        if (number > LAST_NUM || number < START_NUM) {
+            throw new IllegalArgumentException("잘못된 로또 번호 입니다.");
+        }
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof LottoNumber)) {
+            return false;
+        }
+        final LottoNumber that = (LottoNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+
+    @Override
+    public String toString() {
+        return "LottoNumber{" +
+                "number=" + number +
+                '}';
+    }
+}
