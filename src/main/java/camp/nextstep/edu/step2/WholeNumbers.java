e3e9d6d300a46e585274a19c38d908a9153d3c77
mannue <sppe12@gmail.com>
2022-05-12 19:04:16 +0900

    feat(WholeNumbers): String[] 를 위한 생성자 추가

--- src/main/java/camp/nextstep/edu/step2/WholeNumbers.java
+++ src/main/java/camp/nextstep/edu/step2/WholeNumbers.java
@@ -1,14 +1,17 @@
 package camp.nextstep.edu.step2;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class WholeNumbers {
-    private final List<WholeNumber> numbers = new ArrayList<>();
+    private final List<WholeNumber> numbers;
+
+    public WholeNumbers(final String ... numbers) {
+       this(Arrays.stream(numbers).map(WholeNumber::new).toArray(WholeNumber[]::new));
+    }
 
     public WholeNumbers(final WholeNumber... wholeNumbers) {
-        numbers.addAll(Arrays.asList(wholeNumbers));
+        numbers = Arrays.asList(wholeNumbers);
     }
 
     public int sum() {
