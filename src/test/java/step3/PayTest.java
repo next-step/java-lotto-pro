34675867644431d93780a0d0e644ba8743ab34ca
piopoi <kangsw1988@gmail.com>
2022-05-12 18:35:54 +0900

    test(PayTest): 구매가능개수 확인 테스트 수정

--- src/test/java/step3/PayTest.java
+++ src/test/java/step3/PayTest.java
@@ -2,13 +2,13 @@ package step3;
 
 import static org.assertj.core.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class PayTest {
-    @Test
-    void 구매할_수_있는_로또_개수_확인() {
-        assertThat(Pay.ableToBuyLottoCount(1000)).isEqualTo(1);
-        assertThat(Pay.ableToBuyLottoCount(5000)).isEqualTo(5);
-        assertThat(Pay.ableToBuyLottoCount(100)).isEqualTo(0);
+    @ParameterizedTest(name = "구입가능개수 반환 ({0} : {1})")
+    @CsvSource(value = {"1000:1", "5000:5", "100:0"}, delimiter = ':')
+    void ableToBuyLottoCount(int money, int expected) {
+        assertThat(Pay.ableToBuyLottoCount(money)).isEqualTo(expected);
     }
 }
