1f26e847670f06be3c2fadfdbde68ee7040808a2
jhsong2580 <thdwmdgns@naver.com>
2022-05-12 23:45:49 +0900

    feat: ,와 :로 split하는 DefaultResolver 생성

---
+++ src/main/java/step2/ArgumentResolver/DefaultResolver.java
@@ -0,0 +1,22 @@
+package step2.ArgumentResolver;
+
+public class DefaultResolver implements Resolver {
+
+    private final String DELIMITER_COMMA = ",";
+    private final String DELIMITER_COLON = ":";
+    private final int NO_DELIMITER = -1;
+
+    @Override
+    public boolean canResolve(String source) {
+        return haveDelimiter(source, DELIMITER_COLON) || haveDelimiter(source, DELIMITER_COMMA);
+    }
+
+    @Override
+    public String[] resolve(String source) {
+        return source.split(DELIMITER_COLON+"|"+DELIMITER_COMMA);
+    }
+
+    private boolean haveDelimiter(String source, String delimiter) {
+        return source.indexOf(delimiter) != NO_DELIMITER;
+    }
+}
--- src/test/java/step2/ResolverTest.java
+++ src/test/java/step2/ResolverTest.java
@@ -5,8 +5,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import step2.ArgumentResolver.DefaultResolver;
+import step2.ArgumentResolver.Resolver;
 
 public class ResolverTest {
+
+    Resolver defaultResolver = new DefaultResolver();
 
     @ParameterizedTest
     @CsvSource(value = {"1,2,3&,&3", "1,2,3&:&1", "1:2:3&:&3", "1:2:3&,&1"}, delimiter = '&')
@@ -21,5 +25,12 @@ public class ResolverTest {
     @DisplayName("delimiter가 정상적으로 확인되는가")
     public void checkDelimiter(String source, String delimiter, boolean expected) {
         assertThat(source.indexOf(delimiter) != -1).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,2,3&3", "1,2,3&3", "1:2:3&3", "1:2,3&3"}, delimiter = '&')
+    public void defaultResolverTest(String source, String expectedSize) {
+        assertThat(defaultResolver.canResolve(source)).isTrue();
+        assertThat(defaultResolver.resolve(source)).hasSize(3).containsExactly("1", "2", "3");
     }
 }
