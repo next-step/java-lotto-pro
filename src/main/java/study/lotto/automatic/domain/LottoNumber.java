493ad7d4ef6b862ea4ee977b880893e2396db695
SANGIK KIM <sangik.kim@enel.com>
2022-05-13 01:28:41 +0900

    feat(LottoNumber): add a lotto number class

---
+++ src/main/java/study/lotto/automatic/domain/LottoNumber.java
@@ -0,0 +1,19 @@
+package study.lotto.automatic.domain;
+
+public class LottoNumber {
+    private static final int MINIMUM_NUMBER = 1;
+    private static final int MAXIMUM_NUMBER = 45;
+
+    private int lottoNumber;
+
+    public LottoNumber(int lottoNumber) {
+        if (validate(lottoNumber)) {
+            throw new IllegalArgumentException("로또 번호는 1부터 45사이의 숫자만 허용합니다.");
+        }
+        this.lottoNumber = lottoNumber;
+    }
+
+    private boolean validate(int lottoNumber) {
+        return lottoNumber < MINIMUM_NUMBER || lottoNumber > MAXIMUM_NUMBER;
+    }
+}
