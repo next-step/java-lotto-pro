738cfea7c980cc4e2800a22346824fedd88666ea
boss.back <boss.back@kakaocorp.com>
2022-05-13 02:36:53 +0900

    feat: 숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환, 메소드에서 역할 수행 분리

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -6,7 +6,27 @@ public class StringAddCalculator {
         if (isNullOrEmpty(text)) {
             return 0;
         }
-       return Integer.parseInt(text);
+        return sumNumbers(convertStringArrayToIntArray(splitText(text)));
+    }
+
+    private static String[] splitText(String text) {
+        return text.split(",");
+    }
+
+    private static int[] convertStringArrayToIntArray(String[] stringNumbers) {
+        int[] numbers = new int[stringNumbers.length];
+        for (int i = 0; i < stringNumbers.length; i++) {
+            numbers[i] = Integer.parseInt(stringNumbers[i]);
+        }
+        return numbers;
+    }
+
+    private static int sumNumbers(int[] numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
     }
 
     private static boolean isNullOrEmpty(String text) {
