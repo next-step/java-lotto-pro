442bc3d298bc8908cd28a83dc31ef75b379a87ca
WonChul Heo <heowc1992@gmail.com>
2022-05-13 11:58:04 +0900

    test(LottoAnalyzer): LottoAnalyzer 테스트

---
+++ src/main/java/lotto/LottoAnalyzer.java
@@ -0,0 +1,17 @@
+package lotto;
+
+import lotto.lotto.Lotto;
+import java.util.List;
+
+public class LottoAnalyzer {
+
+    private final Lotto previousWinningLotto;
+
+    public LottoAnalyzer(Lotto previousWinningLotto) {
+        this.previousWinningLotto = previousWinningLotto;
+    }
+
+    public WinningResult analyze(List<Lotto> lottoes) {
+        return null;
+    }
+}
