34e878d68a96e3eeba58ad82b64233e1c83de2c1
wenodev <logicalstark1105@gmail.com>
2022-05-13 17:48:33 +0900

    feat(LottoNumber.java): toString 추가한다

--- src/main/java/lotto/LottoNumber.java
+++ src/main/java/lotto/LottoNumber.java
@@ -35,8 +35,6 @@ public class LottoNumber {
 
     @Override
     public String toString() {
-        return "LottoNumber{" +
-                "number=" + number +
-                '}';
+        return String.valueOf(number);
     }
 }
