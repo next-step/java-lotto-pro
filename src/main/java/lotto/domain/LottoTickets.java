5e4790efd40acf9088b1a0a6d94795085d6054f8
Ttungga <torb2002@gmail.com>
2022-05-14 03:05:42 +0900

    feat: add a function for prizes of lotto tickets

--- src/main/java/lotto/domain/LottoTickets.java
+++ src/main/java/lotto/domain/LottoTickets.java
@@ -1,10 +1,14 @@
 package lotto.domain;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import lotto.service.AutoLottoNumbersIssuer;
 
 public class LottoTickets {
+    public static final int[] MATCH_COUNTS = {3, 4, 5, 6};
+
     private final List<LottoTicket> lottoTickets;
 
     public LottoTickets(final int amountOfTickets) {
@@ -12,6 +16,24 @@ public class LottoTickets {
         for (int i = 0; i < amountOfTickets; i++) {
             lottoTickets.add(new LottoTicket(AutoLottoNumbersIssuer.issueLottoNumbers()));
         }
+    }
+
+    public Map<Prize, Integer> prizeMap(final LottoNumbers winningNumbers) {
+        final Map<Prize, Integer> prizeMap = emptyPrizeMap();
+        for (final LottoTicket lottoTicket : lottoTickets) {
+            final Prize prize = lottoTicket.prize(winningNumbers);
+            prizeMap.put(prize, prizeMap.get(prize) + 1);
+        }
+        prizeMap.remove(Prize.NO_MATCHES);
+        return prizeMap;
+    }
+
+    private Map<Prize, Integer> emptyPrizeMap() {
+        final Map<Prize, Integer> emptyPrizeMap = new HashMap<>();
+        for (Prize prize : Prize.values()) {
+            emptyPrizeMap.put(prize, 0);
+        }
+        return emptyPrizeMap;
     }
 
     @Override
--- src/test/java/lotto/domain/LottoTicketsTest.java
+++ src/test/java/lotto/domain/LottoTicketsTest.java
@@ -2,6 +2,8 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -17,5 +19,25 @@ class LottoTicketsTest {
 
         // when and then
         assertThat(lottoTickets).isInstanceOf(LottoTickets.class);
+    }
+
+    @Test
+    @DisplayName("prizeMap 호출 시 올바른 구조의 당첨 결과 Map 객체가 반환되어야 한다")
+    void prizeMap() {
+        // given
+        final int amountOfTickets = 5;
+        final LottoTickets lottoTickets = new LottoTickets(amountOfTickets);
+        final LottoNumbers winningNumbers = new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6));
+
+        // when
+        final Map<Prize, Integer> prizeMap = lottoTickets.prizeMap(winningNumbers);
+
+        // then
+        assertThat(prizeMap.containsKey(Prize.NO_MATCHES)).isFalse();
+        assertThat(prizeMap.containsKey(Prize.THREE_MATCHES)).isTrue();
+        assertThat(prizeMap.containsKey(Prize.FOUR_MATCHES)).isTrue();
+        assertThat(prizeMap.containsKey(Prize.FIVE_MATCHES)).isTrue();
+        assertThat(prizeMap.containsKey(Prize.SIX_MATCHES)).isTrue();
+        assertThat(prizeMap.keySet().size()).isEqualTo(4);
     }
 }
