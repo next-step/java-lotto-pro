b9240cbf57975d0ffd6c93962e423b704d751852
mukeunzi <eunji057@gmail.com>
2022-05-14 00:34:01 +0900

    feat(utils): 커스텀 구분자로 문자열 쪼개는 기능 구현
    - 기능구현 및 테스트코드 작성

--- src/main/java/utils/Splitter.java
+++ src/main/java/utils/Splitter.java
@@ -1,9 +1,24 @@
 package utils;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Splitter {
     private final static String DEFAULT_DELIMITERS = ",|:";
+    private final static String CUSTOM_DELIMITER_REGEX = "^//(.)\n(.*)";
+    private final static Pattern pattern = Pattern.compile(CUSTOM_DELIMITER_REGEX);
+    private static Matcher matcher;
 
     public static String[] splitString(String input) {
-        return input.split(DEFAULT_DELIMITERS);
+        boolean isExist = hasCustomDelimiter(input);
+        String delimiter = isExist ? matcher.group(1) : DEFAULT_DELIMITERS;
+        String targetString = isExist ? matcher.group(2) : input;
+
+        return targetString.split(delimiter);
+    }
+
+    private static boolean hasCustomDelimiter(String input) {
+        matcher = pattern.matcher(input);
+        return matcher.find();
     }
 }
--- src/test/java/SplitterTest.java
+++ src/test/java/SplitterTest.java
@@ -12,4 +12,11 @@ public class SplitterTest {
         assertThat(Splitter.splitString("1:2")).containsExactly("1", "2");
         assertThat(Splitter.splitString("1:2,3:4")).containsExactly("1", "2", "3", "4");
     }
+
+    @Test
+    @DisplayName("커스텀 구분자(//커스텀구분자\\n)로 문자열이 쪼개진다.")
+    void checkStringSplitterWithCustomDelimiters() {
+        assertThat(Splitter.splitString("//-\n1-2-3")).containsExactly("1", "2", "3");
+        assertThat(Splitter.splitString("//#\n1#2#3")).containsExactly("1", "2", "3");
+    }
 }
