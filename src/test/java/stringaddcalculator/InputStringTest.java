f1c88e36d3c76978cf4b7ab6f6cff7ed1b5eabc7
msm <msmasd@naver.com>
2022-05-13 22:07:17 +0900

    test: assertAll 적용 및 @ParameterizedTest 적용

--- src/test/java/stringaddcalculator/InputStringTest.java
+++ src/test/java/stringaddcalculator/InputStringTest.java
@@ -1,22 +1,29 @@
 package stringaddcalculator;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class InputStringTest {
 
     @Test
     void 기본_구분자로_숫자를_구분한다() {
-        assertThat(new InputString("1,2").getNumbers()).containsExactly(1, 2);
-        assertThat(new InputString("1:2").getNumbers()).containsExactly(1, 2);
+        assertAll(
+                () -> assertThat(new InputString("1,2").getNumbers()).containsExactly(1, 2),
+                () -> assertThat(new InputString("1:2").getNumbers()).containsExactly(1, 2)
+        );
+
     }
 
-    @Test
-    void 빈문자열_또는_null인_경우에는_0을_반환() {
-        assertThat(new InputString("").getNumbers()).containsExactly(0);
-        assertThat(new InputString(null).getNumbers()).containsExactly(0);
+    @ParameterizedTest
+    @CsvSource(value = {"null", "''"}, nullValues = {"null"})
+    void 빈문자열_또는_null인_경우에는_0을_반환(String input) {
+        assertThat(new InputString(input).getNumbers()).containsExactly(0);
     }
 
     @Test
@@ -29,11 +36,10 @@ class InputStringTest {
         assertThat(new InputString("//;\n1;2;3").getNumbers()).containsExactly(1, 2, 3);
     }
 
-    @Test
-    void 숫자이외의_값이_입력될때_runtimeException_발생() {
+    @ParameterizedTest
+    @ValueSource(strings = {"가", "1,가"})
+    void 숫자이외의_값이_입력될때_runtimeException_발생(String value) {
         assertThatExceptionOfType(RuntimeException.class)
-                .isThrownBy(() -> new InputString("가"));
-        assertThatExceptionOfType(RuntimeException.class)
-                .isThrownBy(() -> new InputString("1,가"));
+                .isThrownBy(() -> new InputString(value));
     }
 }
