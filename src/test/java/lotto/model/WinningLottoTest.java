e28140d709a829922ef48ee20d244dee47ea70bb
tyakamyz <tyakamyz@naver.com>
2022-05-13 17:28:31 +0900

    test: 테스트명 변경

--- src/test/java/lotto/model/WinningLottoTest.java
+++ src/test/java/lotto/model/WinningLottoTest.java
@@ -15,7 +15,7 @@ public class WinningLottoTest {
     }
 
     @Test
-    void compareWinningLottoNumber() {
+    void compareWinningLottos() {
         WinningLotto winningLotto = new WinningLotto(new LottoNumbers(Arrays.asList(1,2,3,4,5,6)));
         winningLotto.compareWinningLotto(new Lotto(new LottoNumbers(Arrays.asList(1,2,3,9,10,11))));
 
@@ -25,10 +25,5 @@ public class WinningLottoTest {
                 () -> assertThat(winningLotto.getWinningCount(MatchPoint.FIVE)).isEqualTo(0),
                 () -> assertThat(winningLotto.getWinningCount(MatchPoint.SIX)).isEqualTo(0)
         );
-    }
-
-    @Test
-    void compareWinningLottos() {
-
     }
 }
