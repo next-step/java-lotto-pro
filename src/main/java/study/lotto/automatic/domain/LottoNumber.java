4c18d9ab19a3522f77b0d65bc4cd0c4e7fbd832e
SANGIK KIM <sangik.kim@enel.com>
2022-05-13 20:04:28 +0900

    refactor(LottoNumber): change property name

--- src/main/java/study/lotto/automatic/domain/LottoNumber.java
+++ src/main/java/study/lotto/automatic/domain/LottoNumber.java
@@ -6,13 +6,13 @@ public class LottoNumber {
     static final int MINIMUM_NUMBER = 1;
     static final int MAXIMUM_NUMBER = 45;
 
-    private int lottoNumber;
+    private int number;
 
-    public LottoNumber(int lottoNumber) {
-        if (validate(lottoNumber)) {
+    public LottoNumber(int number) {
+        if (validate(number)) {
             throw new IllegalArgumentException("로또 번호는 1부터 45사이의 숫자만 허용합니다.");
         }
-        this.lottoNumber = lottoNumber;
+        this.number = number;
     }
 
     private boolean validate(int lottoNumber) {
@@ -21,7 +21,7 @@ public class LottoNumber {
 
     @Override
     public String toString() {
-        return String.valueOf(lottoNumber);
+        return String.valueOf(number);
     }
 
     @Override
@@ -33,11 +33,11 @@ public class LottoNumber {
             return false;
         }
         LottoNumber that = (LottoNumber) o;
-        return lottoNumber == that.lottoNumber;
+        return number == that.number;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(lottoNumber);
+        return Objects.hash(number);
     }
 }
