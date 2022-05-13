2f18e76f9953dde11cf6c997013db3658161fe07
jhsong2580 <thdwmdgns@naver.com>
2022-05-13 22:20:15 +0900

    refactor: group을 이용한 delimiter/splitSource구분, group 숫자 상수화, Pattern 객체 재사용

--- src/main/java/step2/argumentresolver/CustomDelimiterStringArrayResolver.java
+++ src/main/java/step2/argumentresolver/CustomDelimiterStringArrayResolver.java
@@ -5,26 +5,23 @@ import java.util.regex.Pattern;
 
 public class CustomDelimiterStringArrayResolver implements StringArrayResolver {
 
-    private final String REGEX_DELIMITER = "//(.)\n(.*)";
-    private final String REGEX_SPLIT_SOURCE = "//(.)\n";
+    private final String REGEX = "//(.)\n(.*)";
+    private final Pattern pattern = Pattern.compile(REGEX);
+    private final int DELIMITER_GROUP = 1;
+    private final int SPLIT_SOURCE_GROUP = 2;
 
     @Override
     public boolean canResolve(String source) {
-        source = "//;\n1;2;3";
-        return extractMatcher(source, REGEX_DELIMITER).find();
-    }
-
-    private Matcher extractMatcher(String source, String regex) {
-        return Pattern.compile(regex).matcher(source);
+        return pattern.matcher(source).find();
     }
 
     @Override
     public String[] resolve(String source) {
-        Matcher delimiterMatcher = extractMatcher(source, REGEX_DELIMITER);
+        Matcher delimiterMatcher = pattern.matcher(source);
 
         delimiterMatcher.find();
-        String delimiter = delimiterMatcher.toMatchResult().group(1);
-        String splitSource = extractMatcher(source, REGEX_SPLIT_SOURCE).replaceAll("");
+        String delimiter = delimiterMatcher.group(DELIMITER_GROUP);
+        String splitSource = delimiterMatcher.group(SPLIT_SOURCE_GROUP);
 
         return splitSource.split(delimiter);
     }
