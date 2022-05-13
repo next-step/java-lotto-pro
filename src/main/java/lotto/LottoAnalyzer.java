e44828fef66021f9cdb63ab470e339a2ba66b271
WonChul Heo <heowc1992@gmail.com>
2022-05-13 12:00:47 +0900

    feat(LottoAnalyzer): LottoAnalyzer 구현

--- src/main/java/lotto/LottoAnalyzer.java
+++ src/main/java/lotto/LottoAnalyzer.java
@@ -1,17 +1,27 @@
 package lotto;
 
 import lotto.lotto.Lotto;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
+import static java.util.Objects.requireNonNull;
 
 public class LottoAnalyzer {
 
+    private static final int INIT_VALUE = 1;
     private final Lotto previousWinningLotto;
 
     public LottoAnalyzer(Lotto previousWinningLotto) {
-        this.previousWinningLotto = previousWinningLotto;
+        this.previousWinningLotto = requireNonNull(previousWinningLotto, "previousWinningLotto");
     }
 
     public WinningResult analyze(List<Lotto> lottoes) {
-        return null;
+        final Map<LottoPrize, Integer> results = new EnumMap<>(LottoPrize.class);
+        for (Lotto lotto : lottoes) {
+            final int matchCount = previousWinningLotto.countMatches(lotto);
+            final LottoPrize lottoPrize = LottoPrize.valueOf(matchCount);
+            results.merge(lottoPrize, INIT_VALUE, Integer::sum);
+        }
+        return new WinningResult(results);
     }
 }
