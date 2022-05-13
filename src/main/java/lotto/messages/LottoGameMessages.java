1ca5ea836a7c78fabd5b0412fc2e3346b4e86119
toughchb <ggang2000@gmail.com>
2022-05-14 04:10:06 +0900

    feat(LottoGameMessages): 로또 게임 메세지 정의

---
+++ src/main/java/lotto/messages/LottoGameMessages.java
@@ -0,0 +1,20 @@
+package lotto.messages;
+
+public class LottoGameMessages {
+    private LottoGameMessages() {
+    }
+
+    public static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
+    public static final String PURCHASED_TICKETS_NUMBER = "%s개를 구매했습니다.";
+    public static final String INPUT_WINNING_LOTTO = "지난 주 당첨 번호를 입력해 주세요.";
+    public static final String RESULT_PRIZED_RATE = "당첨 통계";
+    public static final String LINE_STRING = "---------";
+    public static final String TOTAL_WINNING_COUNT = "%s개 일치 (%s원)- %s개";
+    public static final String TOTAL_PROFIT_RATE = "총 수익률은 %.2f입니다.(기준이 1이기 때문에 결과적으로 %s라는 의미임)";
+
+    public static final String BENEFIT = "이익";
+    public static final String LOSS = 의손해";
+
+
+
+}
