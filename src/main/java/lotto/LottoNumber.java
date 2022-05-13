9210f4669bb3cb585f95a27500d98540ff11576a
Yul Jeon <notkhaki@gmail.com>
2022-05-13 10:58:45 +0900

    feat: add the class wrapping an integer that belongs to lotto

---
+++ src/main/java/lotto/LottoNumber.java
@@ -0,0 +1,24 @@
+package lotto;
+
+public class LottoNumber {
+    public static final String LOTTO_NUMBER_OUT_OF_BOUNDS_EXCEPTION_MESSAGE = "숫자가 정상적인 범위를 벗어납니다.";
+    private final int value;
+
+    public LottoNumber(int lottoNumber) {
+        validateBounds(lottoNumber);
+        this.value = lottoNumber;
+    }
+
+    private void validateBounds(int lottoNumber) {
+        if (lottoNumber < 1 || lottoNumber > 45) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_OUT_OF_BOUNDS_EXCEPTION_MESSAGE);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "LottoNumber{" +
+                "value=" + value +
+                '}';
+    }
+}
