1ff49d4f4347f6bd209fcd5543e328c9a96b4d54
jhsong2580 <thdwmdgns@naver.com>
2022-05-13 23:52:07 +0900

    refactor: resolver 병합 및 Input에 따른 선 분기 기능 추가

--- src/main/java/step2/StringAdderCalculator.java
+++ src/main/java/step2/StringAdderCalculator.java
@@ -1,41 +1,22 @@
 package step2;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import step2.argumentresolver.CustomDelimiterStringArrayResolver;
-import step2.argumentresolver.DefaultDelimiterStringArrayResolver;
-import step2.argumentresolver.EmptyInputStringArrayResolver;
-import step2.argumentresolver.SingleNumberStringArrayResolver;
-import step2.argumentresolver.StringArrayResolver;
 import step2.utils.StringArrayElementValidator;
+import step2.utils.StringToStringArrayResolver;
 
 public class StringAdderCalculator {
 
-    private static final ArrayList<StringArrayResolver> STRING_ARRAY_RESOLVERS = new ArrayList<StringArrayResolver>();
     private static String[] splitArray = null;
 
     public static int splitAndSum(String source) {
         init();
-        getSplitArrayByInput(source);
+        splitArray = StringToStringArrayResolver.resolve(source);
         try {
             StringArrayElementValidator.validateSplitResult(splitArray);
         } catch (RuntimeException e) {
             throw new RuntimeException(e.getMessage());
         }
         return sumNumbers();
-    }
-
-
-    private static void getSplitArrayByInput(String source) {
-        for (int i = 0; i < STRING_ARRAY_RESOLVERS.size() && splitArray == null; i++) {
-            executeResolver(source, STRING_ARRAY_RESOLVERS.get(i));
-        }
-    }
-
-    private static void executeResolver(String source, StringArrayResolver stringArrayResolver) {
-        if (stringArrayResolver.canResolve(source)) {
-            splitArray = stringArrayResolver.resolve(source);
-        }
     }
 
     private static int sumNumbers() {
@@ -46,11 +27,6 @@ public class StringAdderCalculator {
 
     private static void init() {
         splitArray = null;
-
-        STRING_ARRAY_RESOLVERS.add(new EmptyInputStringArrayResolver());
-        STRING_ARRAY_RESOLVERS.add(new SingleNumberStringArrayResolver());
-        STRING_ARRAY_RESOLVERS.add(new DefaultDelimiterStringArrayResolver());
-        STRING_ARRAY_RESOLVERS.add(new CustomDelimiterStringArrayResolver());
     }
 
 
--- src/main/java/step2/argumentresolver/StringArrayResolver.java
+++
@@ -1,9 +0,0 @@
-package step2.argumentresolver;
-
-public interface StringArrayResolver {
-
-    boolean canResolve(String source);
-
-    String[] resolve(String source);
-
-}
