440aff0c6f8e734847622647d64ffbbe4073b920
mannue <sppe12@gmail.com>
2022-05-13 21:37:27 +0900

    feat(Presenter): 출력 메소드 수정

--- src/main/java/camp/nextstep/edu/step3/Presenter.java
+++ src/main/java/camp/nextstep/edu/step3/Presenter.java
@@ -4,35 +4,30 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Presenter {
-    private final Scanner scanner = new Scanner(System.in);
 
     public int askPurchaseAmount() {
-        System.out.println("구입금액을 입력해 주세요\n.");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("구입금액을 입력해 주세요.");
         return scanner.nextInt();
     }
 
-    public void printPurchaseComplete(final int count) {
-        System.out.printf("%d개를 구매했습니다.%n", count);
-    }
-
     public void printLottoList(final LottoPaper paper) {
+        System.out.printf("%d개를 구매했습니다.%n", paper.numberOfPurchases());
         System.out.println(paper);
-        System.out.println();
     }
 
     public int[] askLastWeekWinningNumber() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-        int[] ints = Arrays.stream(scanner.next().split(",")).mapToInt(Integer::parseInt).toArray();
+        Scanner scanner = new Scanner(System.in);
+        int[] ints = Arrays.stream(scanner.nextLine().replace(" ", "").split(",")).mapToInt(Integer::parseInt).toArray();
         System.out.println();
         return ints;
     }
 
-    public String printResult(final Total total, final EarningsRate earningsRate) {
-        return new StringBuilder()
-                .append(String.format("%s\n", "당첨 통계"))
-                .append("---------\n")
-                .append(total)
-                .append(earningsRate)
-                .toString();
+    public void printResult(final Total total, final EarningsRate earningsRate) {
+        System.out.println("당첨 통계");
+        System.out.println("---------");
+        System.out.print(total);
+        System.out.println(earningsRate);
     }
 }
