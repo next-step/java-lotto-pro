6f0a922acb5b1b49e65122f551974b8e1c25c83a
msm <msmasd@naver.com>
2022-05-13 22:20:56 +0900

    refac: 2depth인 함수 리팩토링

--- src/main/java/stringaddcalculator/ValidateStringUtils.java
+++ src/main/java/stringaddcalculator/ValidateStringUtils.java
@@ -10,10 +10,14 @@ public class ValidateStringUtils {
 
     public static void validatePositiveNumber(List<Integer> values) {
         for (Integer value : values) {
-            if (value < 0) {
-                throw new RuntimeException("음수가 될 수 없습니다.");
-            }
+            raiseIfNegativeNumber(value);
         }
 
+    }
+
+    private static void raiseIfNegativeNumber(Integer value) {
+        if (value < 0) {
+            throw new RuntimeException("음수가 될 수 없습니다.");
+        }
     }
 }
