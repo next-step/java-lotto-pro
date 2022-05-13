1fe3df3fea49505bae6236f3677d7c6d0a76ac98
mannue <sppe12@gmail.com>
2022-05-13 19:57:07 +0900

    refactor(LottoVendingMachine): Lotto 생성하는 부분을 LottoGenerator 로 역할 이전

--- src/main/java/camp/nextstep/edu/step3/LottoVendingMachine.java
+++ src/main/java/camp/nextstep/edu/step3/LottoVendingMachine.java
@@ -1,33 +1,15 @@
 package camp.nextstep.edu.step3;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class LottoVendingMachine {
-    private final List<LottoNumber> lottoNumbers = new ArrayList<>();
     private final int PRICE_PER = 1000;
+    private final LottoGenerator generator;
 
-    public LottoVendingMachine() {
-        initLottoNumbers();
-    }
-
-    private void initLottoNumbers() {
-        final int START_NUMBER = 1;
-        final int END_NUMBER = 45;
-        for (int i = START_NUMBER; i <= END_NUMBER; i++) {
-            lottoNumbers.add(new LottoNumber(i));
-        }
-
+    public LottoVendingMachine(final LottoGenerator generator) {
+        this.generator = generator;
     }
 
     public LottoPaper issued(final int amount) {
-        Lotto[] lottoArray = new Lotto[numberOfPurchases(amount)];
-        for (int i = 0; i < lottoArray.length; i++) {
-            Collections.shuffle(lottoNumbers);
-            lottoArray[i] = (new Lotto(lottoNumbers.subList(0, 6).toArray(new LottoNumber[0])));
-        }
-        return new LottoPaper(lottoArray);
+        return new LottoPaper(generator.auto(numberOfPurchases(amount)));
     }
 
     private int numberOfPurchases(final int amount) {
--- src/test/java/camp/nextstep/edu/step3/LottoVendingMachineTest.java
+++ src/test/java/camp/nextstep/edu/step3/LottoVendingMachineTest.java
@@ -3,14 +3,16 @@ package camp.nextstep.edu.step3;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
 
 public class LottoVendingMachineTest {
+    private LottoGenerator generator = spy(new LottoGenerator());
 
     @DisplayName("1000원 단위로 Lotto 를 발급하고 LottoPaper 를 반환한다.")
     @Test
     void issuedTest() {
-        LottoPaper lottoPaper = new LottoVendingMachine().issued(14000);
-        assertThat(lottoPaper.isIssuedLottoCount(14)).isTrue();
+        new LottoVendingMachine(generator).issued(14000);
+       verify(generator).auto(14);
     }
 }
