1cffa2b5ef108ddd88215ec7bd699602376e2d19
14km <k.connor614@gmail.com>
2022-05-14 00:06:10 +0900

    feat: LottoNumber 추가

--- src/main/java/lotto/common/Messages.java
+++ src/main/java/lotto/common/Messages.java
@@ -5,5 +5,8 @@ public class Messages {
 
 
     public static final String POSITIVE_MONEY = "[ERROR] 금액은 0원 이상을 입력해주시기 바랍니다.";
-    public static final String NOT_NUMBER = "[ERROR] 금액은 숫자를 입력해주시기 바랍니다.";
+    public static final String MONEY_NOT_NUMBER = "[ERROR] 금액은 숫자를 입력해주시기 바랍니다.";
+
+    public static final String LOTTO_NUMBER_OUT_OF_RANGE = "[ERROR] 숫자는 1부터 45까지 입력이 가능합니다.";
 }
+
