3c2128305802e5329263a7f03ce471d3c7fe2e6e
mond <revive29@gmail.com>
2022-05-13 18:33:28 +0900

    refactor(LottoRank): 상금 표기법에 맞게 Underline 추가

--- src/main/java/lotto/model/LottoRank.java
+++ src/main/java/lotto/model/LottoRank.java
@@ -3,10 +3,10 @@ package lotto.model;
 import java.util.Arrays;
 
 public enum LottoRank {
-    FIRST(6, 2000000000),
-    SECOND(5, 1500000),
-    THIRD(4, 50000),
-    FOURTH(3, 5000),
+    FIRST(6, 200_0000_000),
+    SECOND(5, 1_500_000),
+    THIRD(4, 50_000),
+    FOURTH(3, 5_000),
     FIFTH(2, 0),
     SIXTH(1, 0),
     SEVENTH(0, 0);
