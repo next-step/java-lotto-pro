b2f7ffb24557f1f079ff797adac490efe4516ce9
boss.back <boss.back@kakaocorp.com>
2022-05-14 03:22:48 +0900

    fix: 클래스 분리 ( 공용역할을 하는 StirngUtil, 인풋처리하는 CalculatorInputUtil )

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -2,6 +2,7 @@ package study;
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import static study.StringUtil.convertStringArrayToIntArray;
 
 
@@ -40,68 +41,28 @@ public class StringAddCalculator {
 
     private static final int DELIMITER_INDEX = 1;
     private static final int TEXT_INDEX = 2;
+=======
+import static study.StringUtil.convertStringArrayToIntArray;
 
-    private static final String DELIMITER = ",|:";
-    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
+>>>>>>> 4b62bd1 (fix: 클래스 분리 ( 공용역할을 하는 StirngUtil, 인풋처리하는 CalculatorInputUtil ))
+
+public class StringAddCalculator {
 
     public static int splitAndSum(String text) {
-        if (isNullOrEmpty(text)) {
+        if (StringUtil.isNullOrEmpty(text)) {
             return 0;
         }
-        if (hasCustomDelimiter(text)) {
-            return sumNumbers(convertStringArrayToIntArray(splitTextWithCustomDelimiter(text)));
+        if (CalculatorInputUtil.hasCustomDelimiter(text)) {
+            return sumNumbers(convertStringArrayToIntArray(
+                CalculatorInputUtil.splitTextWithCustomDelimiter(text)));
         }
-        return sumNumbers(convertStringArrayToIntArray(splitText(text, DELIMITER)));
+        return sumNumbers(convertStringArrayToIntArray(StringUtil.splitText(text, CalculatorInputUtil.DELIMITER)));
     }
 
-    private static boolean isNullOrEmpty(String text) {
-        if (text == null || text.isEmpty()) {
-            return true;
-        }
-        return false;
-    }
-
-    private static boolean hasCustomDelimiter(String text) {
-        return CUSTOM_DELIMITER_PATTERN.matcher(text).find();
-    }
-
-    private static String[] splitTextWithCustomDelimiter(String text) {
-        Matcher matcher = makePatterFindMatcher(text);
-        String customDelimiter = findCustomDelimiter(matcher);
-        return splitText(findInputText(matcher), customDelimiter);
-    }
-
-    private static Matcher makePatterFindMatcher(String text){
-        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(text);
-        matcher.find();
-        return matcher;
-    }
-
-    private static String findCustomDelimiter(Matcher matcher) {
-        return matcher.group(DELIMITER_INDEX);
-    }
-
-    private static String findInputText(Matcher matcher) {
-        return matcher.group(TEXT_INDEX);
-    }
-
-    private static int[] convertStringArrayToIntArray(String[] stringNumbers) {
-        int[] numbers = new int[stringNumbers.length];
-        for (int i = 0; i < stringNumbers.length; i++) {
-            numbers[i] = Integer.parseInt(stringNumbers[i]);
-            checkNegativeNumber(numbers[i]);
-        }
-        return numbers;
-    }
-
-    private static void checkNegativeNumber(int number) {
-        if(number < 0) {
+    public static void checkNegativeNumber(int number) {
+        if (number < 0) {
             throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
         }
-    }
-
-    private static String[] splitText(String text, String delimiter) {
-        return text.split(delimiter);
     }
 
     private static int sumNumbers(int[] numbers) {
@@ -111,6 +72,7 @@ public class StringAddCalculator {
         }
         return sum;
     }
+<<<<<<< HEAD
 
 <<<<<<< HEAD
     private static boolean isNullOrEmpty(String text) {
@@ -122,4 +84,6 @@ public class StringAddCalculator {
     }
 =======
 >>>>>>> 9111970 (feat: “//”와 “\n” 문자 사이에 커스텀 구분자를 지정 및 리팩토링)
+=======
+>>>>>>> 4b62bd1 (fix: 클래스 분리 ( 공용역할을 하는 StirngUtil, 인풋처리하는 CalculatorInputUtil ))
 }
