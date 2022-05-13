629ef916f8851c178eadf6d505e6f9d38198af5f
breezeunn <je7689@gmail.com>
2022-05-13 23:30:18 +0900

    feat(StringAddCalculator): 구분자 포함 여부 판별 기능 추가

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,11 +1,24 @@
 package calculator;
 
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
+
+    private static final Pattern CUSTOM_DELIMITER = Pattern.compile("//(.)\n(.*)");
+
     public static int splitAndSum(String input) {
         if (input == null || input.trim().isEmpty()) {
             return 0;
         }
 
         return -1;
+    }
+
+    public static boolean hasDelimiter(String input) {
+        if (input == null || input.isEmpty()) {
+            return false;
+        }
+
+        return input.contains(",") || input.contains(":") || CUSTOM_DELIMITER.matcher(input).matches();
     }
 }
--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -1,8 +1,11 @@
 package calculator;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class StringAddCalculatorTest {
 
@@ -14,5 +17,21 @@ public class StringAddCalculatorTest {
         result = StringAddCalculator.splitAndSum("");
         assertThat(result).isEqualTo(0);
     }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,2|true", "1:2|true", "1;2|false"}, delimiter = '|')
+    public void 기본_구분자를_가진_문자(String input, boolean expected) {
+        boolean actualResult = StringAddCalculator.hasDelimiter(input);
+        assertEquals(expected, actualResult);
+    }
+
+    @Test
+    public void 커스텀_구분자를_가진_문자() {
+        String hasCorrectDelimiter = "//;\n1;2;3|true";
+        String hasIncorrectDelimiter = "/\n1;2;3|false";
+        assertTrue(StringAddCalculator.hasDelimiter(hasCorrectDelimiter));
+        assertFalse(StringAddCalculator.hasDelimiter(hasIncorrectDelimiter));
+    }
+
 
 }
