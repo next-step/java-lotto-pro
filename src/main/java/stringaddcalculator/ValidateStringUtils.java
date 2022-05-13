4fd0c5a4720bb12b4dfab8eac4444efb9757cb35
msm <msmasd@naver.com>
2022-05-13 22:19:16 +0900

    refac: Util 클래스의 기본 생성자를 private로 변경

--- src/main/java/stringaddcalculator/ValidateStringUtils.java
+++ src/main/java/stringaddcalculator/ValidateStringUtils.java
@@ -3,6 +3,11 @@ package stringaddcalculator;
 import java.util.List;
 
 public class ValidateStringUtils {
+
+    private ValidateStringUtils() {
+        throw new UnsupportedOperationException();
+    }
+
     public static void validatePositiveNumber(List<Integer> values) {
         for (Integer value : values) {
             if (value < 0) {
