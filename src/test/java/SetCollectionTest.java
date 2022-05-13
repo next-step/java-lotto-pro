8a764aa151d928f8fcc70321f3fd6a01658a01a3
mukeunzi <eunji057@gmail.com>
2022-05-14 01:37:05 +0900

    refactor: 불필요한 구문 삭제(이번 미션과 무관)
    - 이번 미션과는 무관하지만 불필요한 구문이 있어 삭제

--- src/test/java/SetCollectionTest.java
+++ src/test/java/SetCollectionTest.java
@@ -39,7 +39,6 @@ public class SetCollectionTest {
     @ParameterizedTest
     @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
     void checkHasValue(String input, String expected) {
-        System.out.println(input);
         assertThat(numbers.contains(Integer.parseInt(input))).isEqualTo(Boolean.parseBoolean(expected));
     }
 }
