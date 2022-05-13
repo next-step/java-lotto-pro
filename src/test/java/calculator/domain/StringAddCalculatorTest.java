8cb10e0d216db09899cc409c9b4a888cc2d72692
soob-forest <dltntnqo@naver.com>
2022-05-13 23:22:24 +0900

    refactor(StringAddCalculatorTest): 테스트 함수명 모두 한글로 변경

--- src/test/java/calculator/domain/StringAddCalculatorTest.java
+++ src/test/java/calculator/domain/StringAddCalculatorTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringAddCalculatorTest {
     @Test
-    void splitAndSum_null_또는_빈문자() {
+    void 널_또는_빈문자() {
 
         int result = StringAddCalculator.splitAndSum(null);
         assertThat(result).isEqualTo(0);
@@ -17,37 +17,37 @@ public class StringAddCalculatorTest {
     }
 
     @Test
-    void splitAndSum_숫자하나() throws Exception {
+    void 숫자하나() throws Exception {
         int result = StringAddCalculator.splitAndSum("1");
         assertThat(result).isEqualTo(1);
     }
 
     @Test
-    void splitAndSum_쉼표구분자() throws Exception {
+    void 쉼표구분자() throws Exception {
         int result = StringAddCalculator.splitAndSum("1,2");
         assertThat(result).isEqualTo(3);
     }
 
     @Test
-    void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
+    void 쉼표_또는_콜론_구분자() throws Exception {
         int result = StringAddCalculator.splitAndSum("1,2:3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
-    void splitAndSum_custom_구분자() throws Exception {
+    void 커스텀_구분자() throws Exception {
         int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
-    void splitAndSum_negative() throws Exception {
+    void 음수_예외() throws Exception {
         assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
                 .isInstanceOf(RuntimeException.class);
     }
 
     @Test
-    void splitAndSum_not_number() throws Exception {
+    void 숫자_아닌_값_예외() throws Exception {
         assertThatThrownBy(() -> StringAddCalculator.splitAndSum("a,2,3"))
                 .isInstanceOf(RuntimeException.class);
     }
