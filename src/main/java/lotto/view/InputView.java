888b78a09fc5e8fd5883dfc5c0abe57aace38351
cyr9210 <jipsyer@naver.com>
2022-05-13 18:44:12 +0900

    feat: 보너스를 입력받는다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -12,14 +12,13 @@ public class InputView {
     private static final String INPUT_WINNER_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String DELIMITER = ",";
     private static final String BLANK = " ";
+    public static final String INPUT_BONUS_MESSAGE = "보너스 볼을 입력해 주세요.";
 
     private static Scanner scanner = new Scanner(System.in);
 
     public static int inputMoney() {
         System.out.println(INPUT_MONEY_MESSAGE);
-        int quantity = scanner.nextInt();
-        scanner.nextLine();
-        return quantity;
+        return Integer.parseInt(scanner.nextLine());
     }
 
     public static Lotto inputWinnerNumber() {
@@ -37,4 +36,9 @@ public class InputView {
         return winnerLotto;
     }
 
+    public static LottoNumber inputBonusNumber() {
+        System.out.println(INPUT_BONUS_MESSAGE);
+        int number = Integer.parseInt(scanner.nextLine());
+        return new LottoNumber(number);
+    }
 }
