4f91374d12c63c081e0bf705ac633f0953cf478c
Celina <luv.mins99@gmail.com>
2022-05-13 11:28:54 +0900

    fix(StringAddCalculator): 숫자 객체를 Numbers로 변경
    - 숫자 유효성 검증 로직과 합계 로직을 Numbers가 처리하도록 함

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,6 +1,5 @@
 package calculator;
 
-import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -8,16 +7,14 @@ public class StringAddCalculator {
     private static final int MIN_VALUE = 0;
     private static final String DEFAULT_DELIMITER = ",|:";
     private static final String CUSTOM_DELIMITER = "//(.)\n(.*)";
-    private static final String NEGATIVE_NUMBER_ERROR_MESSAGE = "음수는 입력할 수 없습니다.";
-    private static final String INVALID_NUMBER_ERROR_MESSAGE = "유효한 숫자를 입력하세요.";
 
     public static int splitAndSum(String input) {
         if (isNullOrEmpty(input)) {
             return MIN_VALUE;
         }
 
-        String[] inputs = splitValue(input);
-        return calculateSum(inputs);
+        Numbers numbers = new Numbers(splitValue(input));
+        return calculateSum(numbers);
     }
 
     private static boolean isNullOrEmpty(String input) {
@@ -34,24 +31,7 @@ public class StringAddCalculator {
         return input.split(DEFAULT_DELIMITER);
     }
 
-    private static int calculateSum(String[] inputs) {
-        int[] numbers = getNumbers(inputs);
-        negativeNumberCheck(numbers);
-        return Arrays.stream(numbers).sum();
-    }
-
-    private static int[] getNumbers(String[] inputs) {
-        try {
-            return Arrays.stream(inputs).mapToInt(Integer::parseInt).toArray();
-        } catch (NumberFormatException e) {
-            throw new RuntimeException(INVALID_NUMBER_ERROR_MESSAGE);
-        }
-    }
-
-    private static void negativeNumberCheck(int[] numbers) {
-        boolean isNegative = Arrays.stream(numbers).anyMatch(number -> number < MIN_VALUE);
-        if(isNegative) {
-            throw new RuntimeException(NEGATIVE_NUMBER_ERROR_MESSAGE);
-        }
+    private static int calculateSum(Numbers numbers) {
+        return numbers.sumNumbers();
     }
 }
