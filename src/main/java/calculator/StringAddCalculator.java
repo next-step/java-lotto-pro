8d8ce1fc8189aed65e7918590e29ad8fdd64f55a
soob-forest <dltntnqo@naver.com>
2022-05-13 22:34:21 +0900

    refactor(StringAddCalculator): StringUtils 클래스를 이용하도록 변경

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,16 +1,13 @@
 package calculator;
 
 import calculator.domain.StringSplitter;
-import java.util.ArrayList;
-import java.util.List;
+import calculator.domain.StringUtils;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
     private static final String DELIMITER = ",|:";
     private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
 
-    private static final String MSG_NOT_NUMBER = "숫자 이외의 값을 입력으로 받을 수 없습니다.";
-    private static final String MSG_NEGATIVE = "음수를 입력으로 받을 수 없습니다";
 
     private static final StringSplitter splitter = new StringSplitter(DELIMITER, CUSTOM_DELIMITER_PATTERN);
 
@@ -19,24 +16,14 @@ public class StringAddCalculator {
         if (isNullOrEmptyInput(input)) {
             return 0;
         }
-        return sumNumbers(splitToNumberArray(input));
+        return sumNumbers(StringUtils.toPositiveNumbers(splitter.split(input)));
     }
 
     private static boolean isNullOrEmptyInput(String input) {
         return input == null || input.isEmpty();
     }
 
-    private static List<Integer> splitToNumberArray(String input) {
-
-        List<Integer> numbers = new ArrayList<>();
-        for (String splitInput : splitter.split(input)) {
-            validateSplitInput(splitInput);
-            numbers.add(Integer.parseInt(splitInput));
-        }
-        return numbers;
-    }
-
-    private static int sumNumbers(List<Integer> numbers) {
+    private static int sumNumbers(int[] numbers) {
         int sum = 0;
         for (int number : numbers) {
             sum = sum + number;
@@ -44,14 +31,5 @@ public class StringAddCalculator {
         return sum;
     }
 
-
-    private static void validateSplitInput(String splitInput) {
-        if (!splitInput.matches("\\p{Digit}")) {
-            throw new RuntimeException(MSG_NOT_NUMBER);
-        }
-        if (Integer.parseInt(splitInput) < 0) {
-            throw new RuntimeException(MSG_NEGATIVE);
-        }
-    }
 
 }
