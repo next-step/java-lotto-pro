00cfcee26d43c90ac799f529dacfd143d8e238b2
Yul Jeon <notkhaki@gmail.com>
2022-05-14 01:33:04 +0900

    feat: implement first class collection containing lotto numbers

---
+++ src/main/java/lotto/LottoNumberSet.java
@@ -0,0 +1,41 @@
+package lotto;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LottoNumberSet {
+
+    public static final int SIZE = 6;
+    public static final String ILLEGAL_SIZE_EXCEPTION_MESSAGE = "로또 번호는 " + SIZE + "개여야 합니다.";
+    public static final String NUMBER_DUPLICATE_EXCEPTION_MESSAGE = "번호는 중복되면 안 됩니다.";
+    private final List<LottoNumber> numbers;
+
+    public LottoNumberSet(List<Integer> numbers) {
+        validateSize(numbers);
+        validateDuplicate(numbers);
+        this.numbers = numbers.stream()
+                .map(n -> new LottoNumber(n))
+                .collect(Collectors.toList());
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != SIZE) {
+            throw new IllegalArgumentException(ILLEGAL_SIZE_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        HashSet<Integer> distinctNumbers = new HashSet<>(numbers);
+        if (distinctNumbers.size() != SIZE) {
+            throw new IllegalArgumentException(NUMBER_DUPLICATE_EXCEPTION_MESSAGE);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "LottoNumberSet{" +
+                "numbers=" + numbers +
+                '}';
+    }
+}
