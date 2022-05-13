4fd520d80e9d4f28df56f197af3e96c075daa408
lcjltj <lcjltj@gmail.com>
2022-05-13 20:39:40 +0900

    feat(StringCalculator): 문자열 sum 기능 추가
    - isEmpty() 공백, null 확인 메소드 추가
    - isValid() 음수 입력시 Exception 메소드 추가
    - lessThenZero() 음수 확인 메소드 추가
    - sum() 합계 메소드 추가

--- src/main/java/study/StringCalculator.java
+++ src/main/java/study/StringCalculator.java
@@ -1,5 +1,7 @@
 package study;
 
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,13 +11,39 @@ public class StringCalculator {
     private static final Pattern CUSTOM_PATTERN = Pattern.compile(CUSTOM_REGEX);
     private static final int CUSTOM_DELIMITER = 1;
     private static final int SPLIT_WORD = 2;
+    private static final int ZERO = 0;
+    private static final String EMPTY = "";
+    private static final String NEGATIVE_NUMBER_ERROR = "음수는 사용 불가능 합니다.";
 
-    public static String[] split(final String word) {
+    public static int splitAndSum(final String word) {
+        if (isEmpty(word)) {
+            return ZERO;
+        }
+
+        return sum(toNumbers(split(word)));
+    }
+
+    private static boolean isEmpty(final String word) {
+        return word == null || EMPTY.equals(word);
+    }
+
+    private static String[] split(final String word) {
         Matcher matcher = CUSTOM_PATTERN.matcher(word);
         if (matcher.find()) {
             return splitCustom(matcher);
         }
         return splitDefault(word);
+    }
+
+    private static int sum(final int[] numbers) {
+        isValid(numbers);
+        return Arrays.stream(numbers).sum();
+    }
+
+    private static int[] toNumbers(final String[] stringNumbers) {
+        return Arrays.stream(stringNumbers)
+                .mapToInt(Integer::parseInt)
+                .toArray();
     }
 
     private static String[] splitCustom(final Matcher matcher) {
@@ -26,6 +54,16 @@ public class StringCalculator {
         return word.split(DEFAULT_DELIMITER);
     }
 
+    private static void isValid(final int[] numbers) {
+        boolean negativeNumber = Arrays.stream(numbers)
+                .anyMatch(StringCalculator::lessThenZero);
 
+        if (negativeNumber) {
+            throw new IllegalArgumentException(NEGATIVE_NUMBER_ERROR);
+        }
+    }
 
+    private static <T extends Number> boolean lessThenZero(final T number) {
+        return number.intValue() < ZERO;
+    }
 }
