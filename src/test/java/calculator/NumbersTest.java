449f3e962f042a35acfde8d5da6c10930d723bab
Celina <luv.mins99@gmail.com>
2022-05-13 15:30:17 +0900

    test(NumbersTest): 클래스 접근 제어자 삭제
    - 메인 클래스에서 테스트 클래스를 생성하면 접근 제어자가 없는 클래스로 생성된다

--- src/test/java/calculator/NumbersTest.java
+++ src/test/java/calculator/NumbersTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class NumbersTest {
+class NumbersTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"-1", "-2", "-3"})
