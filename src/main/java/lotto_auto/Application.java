468d94f5bc22bc207e81f3cfccac62168a0818cd
bestsilver <dkvkxm99@gmail.com>
2022-05-14 13:34:30 +0900

    feat: Application 구현
    - 게임 실행

---
+++ src/main/java/lotto_auto/Application.java
@@ -0,0 +1,9 @@
+package lotto_auto;
+
+import lotto_auto.controller.GameController;
+
+public class Application {
+    public static void main(String[] args) {
+        new GameController().run();
+    }
+}
