e1cb9df901906e5bc81cc9dc96b5de1ffe40c9bb
wenodev <logicalstark1105@gmail.com>
2022-05-14 23:18:00 +0900

    fix(LottoPrizeRank.java): 숫자 표기법을 변경한다

--- src/main/java/lotto/model/LottoPrizeRank.java
+++ src/main/java/lotto/model/LottoPrizeRank.java
@@ -6,9 +6,9 @@ import java.util.stream.Stream;
 public enum LottoPrizeRank {
     NONE(0, 0),
     THREE(3, 5000),
-    FOUR(4, 50000),
-    FIVE(5, 1500000),
-    SIX(6, 2000000000);
+    FOUR(4, 50_000),
+    FIVE(5, 1_500_000),
+    SIX(6, 2_000_000_000);
 
     private final int numberOfMatch;
     private final int prize;
