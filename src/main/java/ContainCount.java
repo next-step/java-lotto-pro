84d183e2e7b91ecde8fd128ef60b15a6687b5508
chj1768 <chj1768@gmail.com>
2022-05-15 03:02:30 +0900

    feat: 로또 번호가 포함하고 있는 당첨번호 총 개수를 표현하는 객체 생성

---
+++ src/main/java/ContainCount.java
@@ -0,0 +1,31 @@
+import java.util.Objects;
+
+public class ContainCount {
+    private final int containCount;
+
+    public ContainCount(int containCount) {
+        if (containCount < 0)
+            throw new RuntimeException();
+
+        if (containCount > LottoNumbers.SIZE)
+            throw new RuntimeException();
+
+        this.containCount = containCount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof ContainCount))
+            return false;
+        ContainCount that = (ContainCount) o;
+        return containCount == that.containCount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(containCount);
+    }
+
+}
