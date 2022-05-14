770f2b04b10b646cb6becdd691ae2483963759e2
sy001 <suyounlake@naver.com>
2022-05-14 23:22:11 +0900

    test: DisplayName 설명 추가

--- src/test/java/stringAddCalculator/StringAddCalculatorTest.java
+++ src/test/java/stringAddCalculator/StringAddCalculatorTest.java
@@ -1,4 +1,5 @@
 package stringAddCalculator;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -6,6 +7,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringAddCalculatorTest {
     @Test
+    @DisplayName("문자열계산기는 null 또는 빈문자 입력시 0을 반환한다.")
     public void splitAndSum_null_또는_빈문자() {
         int result = StringAddCalculator.splitAndSum(null);
         assertThat(result).isEqualTo(0);
@@ -15,36 +17,42 @@ public class StringAddCalculatorTest {
     }
 
     @Test
+    @DisplayName("문자열계산기는 숫자 하나 입력시 해당숫자를 반환한다.")
     public void splitAndSum_숫자하나() throws Exception {
         int result = StringAddCalculator.splitAndSum("1");
         assertThat(result).isEqualTo(1);
     }
 
     @Test
+    @DisplayName("문자열계산기는 쉼표를 구분자로 사용한다.")
     public void splitAndSum_쉼표구분자() throws Exception {
         int result = StringAddCalculator.splitAndSum("1,2");
         assertThat(result).isEqualTo(3);
     }
 
     @Test
+    @DisplayName("문자열계산기는 쉼표 또는 콜론을 구분자로 사용한다.")
     public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
         int result = StringAddCalculator.splitAndSum("1,2:3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
+    @DisplayName("문자열계산기는 Custom 구분자를 사용한다.")
     public void splitAndSum_custom_구분자() throws Exception {
         int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
+    @DisplayName("문자열계산기에 음수 입력시 Exception 발생한다.")
     public void splitAndSum_negative() throws Exception {
         assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
                 .isInstanceOf(RuntimeException.class);
     }
 
     @Test
+    @DisplayName("문자열계산기에 숫자 외의 값 입력시 Exception 발생한다.")
     public void splitAndSum_invalid_format() throws Exception {
         assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-+1,2,3"))
                 .isInstanceOf(RuntimeException.class);
