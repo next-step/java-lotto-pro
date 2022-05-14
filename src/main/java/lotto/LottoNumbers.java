cc3b9cfaa5cba886ddad9b097eb091266da5bacd
sangho lee <sangholee.dev@gmail.com>
2022-05-14 21:35:50 +0900

    feat: LottoNumbers 추가

---
+++ src/main/java/lotto/LottoNumbers.java
@@ -0,0 +1,82 @@
+package lotto;
+
+import static java.util.Comparator.comparing;
+import static util.ListUtils.randomPickCount;
+import static util.ListUtils.sort;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class LottoNumbers {
+
+    public static final LottoNumbers ALL_NUMBERS = new LottoNumbers(totalLottoNumberList());
+    private static final int DEFAULT_PICK_COUNT = 6;
+
+    private final List<LottoNumber> lottoNumbers;
+
+    protected LottoNumbers(final List<LottoNumber> lottoNumbers) {
+        this.lottoNumbers = sort(lottoNumbers, comparing(LottoNumber::getNumber));
+    }
+
+    public LottoNumbers pickNumbers() {
+        return new LottoNumbers(randomPickCount(this.lottoNumbers, DEFAULT_PICK_COUNT));
+    }
+
+    private static List<LottoNumber> totalLottoNumberList() {
+        List<LottoNumber> allNumbers = new ArrayList<>();
+        for (int i = LottoNumber.START_NUM; i <= LottoNumber.LAST_NUM; i++) {
+            allNumbers.add(LottoNumber.valueOf(i));
+        }
+        return allNumbers;
+    }
+
+    public boolean isPicked() {
+        return this.lottoNumbers.size() == DEFAULT_PICK_COUNT;
+    }
+
+    public int compareCount(final LottoNumbers lottoNumbers) {
+        int count = 0;
+        for (LottoNumber lottoNumber : this.lottoNumbers) {
+            count += lottoNumbers.containsToOne(lottoNumber);
+        }
+
+        return count;
+    }
+
+    public int containsToOne(final LottoNumber lottoNumber) {
+        if (contains(lottoNumber)) {
+            return 1;
+        }
+
+        return 0;
+    }
+
+    public boolean contains(final LottoNumber lottoNumber) {
+        return this.lottoNumbers.contains(lottoNumber);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof LottoNumbers)) {
+            return false;
+        }
+        final LottoNumbers that = (LottoNumbers) o;
+        return Objects.equals(lottoNumbers, that.lottoNumbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoNumbers);
+    }
+
+    @Override
+    public String toString() {
+        return "LottoNumbers{" +
+                "lottoNumbers=" + lottoNumbers +
+                '}';
+    }
+}
---
+++ src/test/java/lotto/LottoNumbersTest.java
@@ -0,0 +1,107 @@
+package lotto;
+
+import static java.util.stream.IntStream.range;
+import static org.assertj.core.api.Assertions.assertThat;
+import static util.ListUtils.sort;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class LottoNumbersTest {
+
+    @Test
+    void 전체_기본_로또번호_1_45_확인() {
+        // given
+        List<LottoNumber> lottoNumberList = range(1, 46)
+                .mapToObj(LottoNumber::valueOf)
+                .collect(Collectors.toList());
+
+        // when & then
+        assertThat(LottoNumbers.ALL_NUMBERS).isEqualTo(new LottoNumbers(lottoNumberList));
+    }
+
+    @Test
+    void 로또_번호_6개를_선택() {
+        // when
+        LottoNumbers lottoNumbers = LottoNumbers.ALL_NUMBERS.pickNumbers();
+
+        // then
+        assertThat(lottoNumbers.isPicked()).isTrue();
+    }
+
+    @Test
+    @DisplayName("로또 번호가 순서에 상관없이 번호끼리 같은지 비교")
+    void 로또_번호_비교() {
+        // given
+        List<LottoNumber> lottoNumberList = range(1, 7)
+                .mapToObj(LottoNumber::valueOf)
+                .collect(Collectors.toList());
+
+        final LottoNumbers lottoNumbers = new LottoNumbers(lottoNumberList);
+        final LottoNumbers expectedLottoNumbers = new LottoNumbers(
+                sort(lottoNumberList, (o1, o2) -> o2.getNumber() - o1.getNumber()));
+
+        // when & then
+        assertThat(lottoNumbers).isEqualTo(expectedLottoNumbers);
+    }
+
+    @Test
+    @DisplayName("로또 번호가 1 ~ 6 과 2 ~ 7 비교")
+    void 로또_다른_번호_비교() {
+        // given
+        final LottoNumbers lottoNumbers = new LottoNumbers(range(1, 7)
+                .mapToObj(LottoNumber::valueOf)
+                .collect(Collectors.toList()));
+        final LottoNumbers expectedLottoNumbers = new LottoNumbers(range(2, 8)
+                .mapToObj(LottoNumber::valueOf)
+                .collect(Collectors.toList()));
+
+        // when & then
+        assertThat(lottoNumbers).isNotEqualTo(expectedLottoNumbers);
+    }
+
+    @ParameterizedTest
+    @MethodSource("compareLottoNumberListSource")
+    void 로또_번호를_비교하여_같은_갯수_찾기(List<LottoNumber> lottoNumberList, List<LottoNumber> expectedLottoNumberList,
+                              int result) {
+        // given
+        final LottoNumbers lottoNumbers = new LottoNumbers(lottoNumberList);
+        final LottoNumbers expectedLottoNumbers = new LottoNumbers(expectedLottoNumberList);
+
+        // when & then
+        assertThat(lottoNumbers.compareCount(expectedLottoNumbers)).isEqualTo(result);
+
+    }
+
+    static Stream<Arguments> compareLottoNumberListSource() {
+        return Stream.of(
+                Arguments.of(range(1, 7)
+                                .mapToObj(LottoNumber::valueOf)
+                                .collect(Collectors.toList()),
+                        range(2, 8)
+                                .mapToObj(LottoNumber::valueOf)
+                                .collect(Collectors.toList()), 5
+                ),
+                Arguments.of(range(1, 7)
+                                .mapToObj(LottoNumber::valueOf)
+                                .collect(Collectors.toList()),
+                        range(7, 13)
+                                .mapToObj(LottoNumber::valueOf)
+                                .collect(Collectors.toList()), 0
+                ),
+                Arguments.of(range(1, 7)
+                                .mapToObj(LottoNumber::valueOf)
+                                .collect(Collectors.toList()),
+                        range(4, 10)
+                                .mapToObj(LottoNumber::valueOf)
+                                .collect(Collectors.toList()), 3
+                )
+        );
+    }
+}
