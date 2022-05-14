6fb502f39cde9a88fc1997ed16d1c53d612ba8b2
sy001 <suyounlake@naver.com>
2022-05-15 00:45:30 +0900

    feat: 입력받은 금액만큼 자동 로또 구매

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,10 +1,20 @@
 package lotto.controller;
 
+import lotto.domain.Lottos;
 import lotto.domain.Money;
 import lotto.view.InputView;
 
 public class LottoController {
     public static void simulateLotto(){
         Money money = new Money(InputView.readMoney());
+        Lottos lottos = purchaseLottos(money.lottoCountToBuy());
+    }
+
+    private static Lottos purchaseLottos(int count) {
+        Lottos lottos = new Lottos();
+        for (int i = 0; i < count; i++) {
+            lottos.addRandomLotto();
+        }
+        return lottos;
     }
 }
