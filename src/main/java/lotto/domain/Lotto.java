04d7e6ea6953670d37d5e65b3527e1b78b52d5a5
sy001 <suyounlake@naver.com>
2022-05-14 23:31:38 +0900

    feat: Lotto 도메인 클래스 구현(Lotto는 6개의 숫자를 가짐)
    - LottoTest 구현 후 Test기반 구현
    - Lotto는 6개의 숫자를 가짐

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,36 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Lotto {
+    private static final int LOTTO_NUMBERS_SIZE = 6;
+    List<LottoNumber> lottoNumbers = new ArrayList<>();
+
+    public Lotto(List<Integer> numbers) {
+        validateNumberSize(numbers);
+        for (int number : numbers) {
+            LottoNumber lottoNumber = new LottoNumber(number);
+            lottoNumbers.add(lottoNumber);
+        }
+    }
+
+    private void validateNumberSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_NUMBERS_SIZE) {
+            throw new IllegalArgumentException("[ERROR] Lotto의 숫자는 6개여야 합니다.");
+        }
+    }
+
+    public List<Integer> lottoNumbers(){
+        return lottoNumbersToIntegerList(lottoNumbers);
+    }
+
+    private List<Integer> lottoNumbersToIntegerList(List<LottoNumber> lottoNumbers) {
+        List<Integer> result = new ArrayList<>();
+        for (LottoNumber lottoNumber : lottoNumbers) {
+            result.add(lottoNumber.getNumber());
+        }
+        return result;
+    }
+}
---
+++ src/test/java/lotto/domain/LottoTest.java
@@ -0,0 +1,40 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class LottoTest {
+    @Test
+    @DisplayName("Lotto는 6개의 LottoNumber로 이루어져있다.")
+    void Lotto_정상_테스트(){
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        Lotto lotto = new Lotto(numbers);
+        assertThat(lotto.lottoNumbers()).containsExactly(1, 2, 3, 4, 5, 6);
+    }
+
+    @Test
+    @DisplayName("Lotto는 6개의 LottoNumber로 이루어져있다 : 6개 초과시 오류")
+    void Lotto_6개초과_테스트(){
+        assertThatThrownBy(() -> {
+            List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7));
+            Lotto lotto = new Lotto(numbers);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("Lotto는 6개의 LottoNumber로 이루어져있다 : 6개 미달시 오류")
+    void Lotto_6개미달_테스트(){
+        assertThatThrownBy(() -> {
+            List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+            Lotto lotto = new Lotto(numbers);
+        }).isInstanceOf(IllegalArgumentException.class);
+
+    }
+}
