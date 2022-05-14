efe157a63fc6019c6744a6de570bb16d7ec32ace
soob-forest <dltntnqo@naver.com>
2022-05-14 14:36:07 +0900

    refactor(StringSplitter): DELIMITER , CUSTOM_DELIMITER_PATTERN에 대한 책임을 StringSplitter로 변경

--- src/main/java/calculator/domain/StringAddCalculator.java
+++ src/main/java/calculator/domain/StringAddCalculator.java
@@ -3,11 +3,9 @@ package calculator.domain;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
-    private static final String DELIMITER = ",|:";
-    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
+
     private static final int EMPTY_OUTPUT = 0;
 
-    private static final StringSplitter splitter = new StringSplitter(DELIMITER, CUSTOM_DELIMITER_PATTERN);
 
     private StringAddCalculator() {
     }
@@ -17,7 +15,7 @@ public class StringAddCalculator {
         if (isNullOrEmptyInput(input)) {
             return EMPTY_OUTPUT;
         }
-        return ArrayCalculator.sum(StringUtils.toPositiveNumbers(splitter.split(input)));
+        return ArrayCalculator.sum(StringUtils.toPositiveNumbers(StringSplitter.split(input)));
     }
 
     private static boolean isNullOrEmptyInput(String input) {
--- src/main/java/calculator/domain/StringSplitter.java
+++ src/main/java/calculator/domain/StringSplitter.java
@@ -5,39 +5,37 @@ import java.util.regex.Pattern;
 
 public class StringSplitter {
 
+    private static final String DELIMITER = ",|:";
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
     private static final int CUSTOM_DELIMITER_INDEX = 1;
     private static final int CUSTOM_INPUT_INDEX = 2;
 
-    private String delimiter;
-    private Pattern customDelimiterPattern;
 
-    public StringSplitter(String delimiter, Pattern pattern) {
-        this.delimiter = delimiter;
-        this.customDelimiterPattern = pattern;
+    private StringSplitter() {
     }
 
-    public String[] split(String input) {
-        Matcher matcher = this.matcher(input);
+    public static String[] split(String input) {
+        Matcher matcher = matcher(input);
         if (isCustomSplitPattern(matcher)) {
             return splitByPattern(matcher);
         }
         return splitByDelimiter(input);
     }
 
-    private String[] splitByDelimiter(String input) {
-        return input.split(delimiter);
+    private static String[] splitByDelimiter(String input) {
+        return input.split(DELIMITER);
     }
 
-    private String[] splitByPattern(Matcher matcher) {
+    private static String[] splitByPattern(Matcher matcher) {
         String customDelimiter = matcher.group(CUSTOM_DELIMITER_INDEX);
         return matcher.group(CUSTOM_INPUT_INDEX).split(customDelimiter);
     }
 
-    private Matcher matcher(String input) {
-        return customDelimiterPattern.matcher(input);
+    private static Matcher matcher(String input) {
+        return CUSTOM_DELIMITER_PATTERN.matcher(input);
     }
 
-    private boolean isCustomSplitPattern(Matcher matcher) {
+    private static boolean isCustomSplitPattern(Matcher matcher) {
         return matcher.find();
     }
 }
