6c2033ea8552b6bd779bb8f8b2729247f309c57d
chj1768 <chj1768@gmail.com>
2022-05-15 03:04:34 +0900

    refactor: Prize 내장에 final 키워드 추가. find 메서드 구현 stream 사용하여 변경

--- src/main/java/Prize.java
+++ src/main/java/Prize.java
@@ -1,11 +1,13 @@
+import java.util.Arrays;
+
 public enum Prize {
     FIRST(6, 2000000000), SECOND(5, 1500000), THIRD(4, 50000),
     FOURTH(3, 5000), NONE(0, 0);
 
     private final static int PRIZE_LOWER_BOUND_COUNT = FOURTH.count;
 
-    private int count;
-    private long money;
+    private final int count;
+    private final long money;
 
     Prize(int count, long money) {
         this.count = count;
@@ -16,12 +18,10 @@ public enum Prize {
         if (PRIZE_LOWER_BOUND_COUNT > count)
             return NONE;
 
-        for (Prize prize : values()) {
-            if (prize.count == count)
-                return prize;
-        }
-
-        throw new RuntimeException();
+        return Arrays.stream(values())
+                .filter(it -> it.count == count)
+                .findAny()
+                .orElseThrow(RuntimeException::new);
     }
 
     @Override
