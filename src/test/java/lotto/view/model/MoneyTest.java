cc2b40e54974315fbdfde063f531c99befd9bd32
Philip <pch4623@gmail.com>
2022-05-14 21:58:26 +0900

    test(MoneyTest): 구매가격 단위 예외발생 테스트

--- src/test/java/lotto/view/model/MoneyTest.java
+++ src/test/java/lotto/view/model/MoneyTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static lotto.view.constants.LottoConstant.LOTTO_PRICE;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MoneyTest {
@@ -16,4 +17,14 @@ class MoneyTest {
                 .isExactlyInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 구매금액은 음수일 수 없습니다! : [%s]", input);
     }
+
+    @ParameterizedTest
+    @DisplayName("생성자 파라미터(구매금액)의 단위가 올바르지 않을 경우 IllegalArgumentException 예외를 발생시킨다.")
+    @ValueSource(ints = {1500, 300, 12700})
+    void Money_구매금액_단위(int input) {
+        assertThatThrownBy(() -> Money.of(input))
+                .isExactlyInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 구매금액은 %s원 단위만 가능합니다! : 입력금액 [%d]", LOTTO_PRICE, input);
+    }
+
 }
