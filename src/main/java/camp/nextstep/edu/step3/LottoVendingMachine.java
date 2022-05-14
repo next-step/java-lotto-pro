d3f277143bbb2700a205fcf214e72c72c0f7c0a3
mannue <sppe12@gmail.com>
2022-05-14 21:00:18 +0900

    refactor(LottoVendingMachine): LottoMoney를 이용하여 금액만큼 자동으로 뽑도록 수정

--- src/main/java/camp/nextstep/edu/step3/LottoVendingMachine.java
+++ src/main/java/camp/nextstep/edu/step3/LottoVendingMachine.java
@@ -1,18 +1,29 @@
 package camp.nextstep.edu.step3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoVendingMachine {
-    private final int PRICE_PER = 1000;
     private final LottoGenerator generator;
 
     public LottoVendingMachine(final LottoGenerator generator) {
         this.generator = generator;
     }
 
-    public LottoPaper issued(final int amount) {
-        return new LottoPaper(generator.auto(numberOfPurchases(amount)));
+    public LottoPaper issued(final LottoMoney money) {
+        return new LottoPaper(buyLotto(money));
     }
 
-    private int numberOfPurchases(final int amount) {
-        return amount / PRICE_PER;
+    private List<Lotto> buyLotto(LottoMoney money) {
+        return extractLottoByAuto(money);
+    }
+
+    private List<Lotto> extractLottoByAuto(LottoMoney money) {
+        List<Lotto> lottoList = new ArrayList<>();
+        while (money.isBuyable()) {
+            money = money.buy();
+            lottoList.add(generator.auto());
+        }
+        return lottoList;
     }
 }
--- src/test/java/camp/nextstep/edu/step3/LottoVendingMachineTest.java
+++ src/test/java/camp/nextstep/edu/step3/LottoVendingMachineTest.java
@@ -3,34 +3,22 @@ package camp.nextstep.edu.step3;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.verify;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class LottoVendingMachineTest {
-    private final LottoGenerator generator = spy(new LottoGenerator());
+
     private LottoVendingMachine vendingMachine;
 
     @BeforeEach
     void setUp() {
-        vendingMachine = new LottoVendingMachine(generator);
+        vendingMachine = new LottoVendingMachine(new LottoGenerator());
     }
 
     @DisplayName("1000원 단위로 Lotto 를 발급하고 LottoPaper 를 반환한다.")
     @Test
     void issuedTest() {
-        vendingMachine.issued(14000);
-        verify(generator).auto(14);
-    }
-
-    @DisplayName("지불금액이 1000원 보다 낮으면 에러를 발생한다.")
-    @ParameterizedTest
-    @ValueSource(ints = {900, 0, -100})
-    void invalidInputTest(final int amount) {
-        assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> vendingMachine.issued(amount));
+        LottoPaper paper = vendingMachine.issued(new LottoMoney(14000));
+        assertThat(paper.numberOfPurchases()).isEqualTo(14);
     }
 }
