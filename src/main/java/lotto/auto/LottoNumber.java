9aa5844836a17e4890a1566095f6e177cad98b38
vvsungho <vvsungho@gmail.com>
2022-05-14 23:46:42 +0900

    test: 로또 번호 발급 테스트 케이스 작성

---
+++ src/main/java/lotto/auto/LottoNumber.java
@@ -0,0 +1,11 @@
+package lotto.auto;
+
+import java.util.List;
+
+public class LottoNumber {
+    private List<Integer> numbers;
+
+    public LottoNumber() {
+    }
+}
+
---
+++ src/test/java/lotto/auto/LottoNumberTest.java
@@ -0,0 +1,13 @@
+package lotto.auto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoNumberTest {
+
+    @Test
+    @DisplayName("6개의 번호로 발급되는지 확인한다")
+    void 테스트_로또번호6개_발급() {
+        LottoNumber lottoNumber = new LottoNumber();
+    }
+}
