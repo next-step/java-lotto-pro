692ffce3477b1fb0dc06b571c74b333bd2abca35
tasklet1579 <tasklet1579@gmail.com>
2022-05-14 17:09:56 +0900

    test(NumberTest) : 테스틐 ㅔ이스 추가 - 양의 정수, 0 입력 - 파싱 불가능한 문자열 입력

--- src/test/java/calculator/NumberTest.java
+++ src/test/java/calculator/NumberTest.java
@@ -4,14 +4,30 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class NumberTest {
+    @DisplayName("양의 정수, 0 입력")
+    @ParameterizedTest
+    @ValueSource(ints = {112, 119, 0, 1234567890})
+    void 양의_정수_0_입력(final int number) {
+        assertThatCode(() -> new Number(number)).doesNotThrowAnyException();
+    }
+
     @DisplayName("음의 정수 입력")
     @ParameterizedTest
-    @ValueSource(ints = {-1, -2, -3})
+    @ValueSource(ints = {-1, -2, -3, -1})
     void 음의_정수_입력(final int number) {
         assertThatThrownBy(() -> new Number(number))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("파싱 에러")
+    @ParameterizedTest
+    @ValueSource(strings = {"abc1", "가나다라"})
+    void 파싱_에러(final String number) {
+        assertThatThrownBy(() -> new Number(number))
+                .isInstanceOf(NumberFormatException.class);
     }
 }
