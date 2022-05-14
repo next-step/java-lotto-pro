85044bd05227a9fe25d3b3bfe89306a24a15559c
wenodev <logicalstark1105@gmail.com>
2022-05-15 00:56:01 +0900

    test(LottoStaticsTest.java): 메소드 이름 변경한다

--- src/test/java/lotto/LottoStaticsTest.java
+++ src/test/java/lotto/LottoStaticsTest.java
@@ -15,7 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("LottoStatics는 ")
 public class LottoStaticsTest {
-    @DisplayName("5000원 1개를 가진다")
+    @DisplayName("당첨 순위 별 갯수를 수집한다")
     @Test
     void collect_1() {
         List<Integer> winNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
@@ -27,19 +27,5 @@ public class LottoStaticsTest {
 
         LottoPrizeRanks lottoPrizeRanks = lottoStatics.collect();
         assertThat(lottoPrizeRanks).isEqualTo(new LottoPrizeRanks(Arrays.asList(THREE, NONE)));
-    }
-
-    @DisplayName("5000원 2개를 가진다")
-    @Test
-    void collect_2() {
-        List<Integer> winNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
-        List<LottoNumbers> lottoNumbers = Arrays.asList(
-                new LottoNumbers(Arrays.asList(1, 2, 3, 7, 8, 9)),
-                new LottoNumbers(Arrays.asList(1, 2, 3, 10, 11, 12)));
-
-        LottoStatics lottoStatics = new LottoStatics(lottoNumbers, winNumbers);
-
-        LottoPrizeRanks lottoPrizeRanks = lottoStatics.collect();
-        assertThat(lottoPrizeRanks).isEqualTo(new LottoPrizeRanks(Arrays.asList(THREE, THREE)));
     }
 }
