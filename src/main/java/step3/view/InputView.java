b92abc24a12c5d300799154c2628d68fb3476c05
jhsong2580 <thdwmdgns@naver.com>
2022-05-14 16:01:52 +0900

    feat: InputView기능 구현(구입금액 받기, 당첨번호 받기)

---
+++ src/main/java/step3/view/InputView.java
@@ -0,0 +1,33 @@
+package step3.view;
+
+import java.util.Scanner;
+import step3.InputStatus;
+
+public class InputView {
+
+    private final Scanner scanIn;
+
+    public InputView(Scanner scan) {
+        this.scanIn = scan;
+    }
+
+    public String getInput(InputStatus inputStatus) {
+        if (inputStatus == InputStatus.MONEY) {
+            return getMoney();
+        }
+        if (inputStatus == InputStatus.LOTTO) {
+            return getWinnerLotto();
+        }
+        return null;
+    }
+
+    private String getMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return scanIn.next();
+    }
+
+    private String getWinnerLotto() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        return scanIn.next();
+    }
+}
