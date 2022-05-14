50a0ed5cdfe07ecd8aaffe5d2283f1a825a40642
Philip <pch4623@gmail.com>
2022-05-14 21:57:35 +0900

    feat(Money): 구매금액의 단위가 로또 가격과 맞지 않는 경우 예외 발생

--- src/main/java/lotto/view/model/Money.java
+++ src/main/java/lotto/view/model/Money.java
@@ -1,5 +1,7 @@
 package lotto.view.model;
 
+import static lotto.view.constants.LottoConstant.LOTTO_PRICE;
+
 public class Money {
     private final int amount;
 
@@ -9,12 +11,20 @@ public class Money {
 
     public static Money of(int amount) {
         checkAmountRange(amount);
+        checkAmountUnit(amount);
         return new Money(amount);
     }
 
     private static void checkAmountRange(int amount) {
         if (amount < 0) {
-            String message = String.format("[ERROR] 구매금액은 음수일 수 없습니다! : [%d]", amount);
+            String message = String.format("[ERROR] 구매금액은 음수일 수 없습니다! : 입력금액 [%d]", amount);
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    private static void checkAmountUnit(int amount) {
+        if (amount % LOTTO_PRICE != 0) {
+            String message = String.format("[ERROR] 구매금액은 %s원 단위만 가능합니다! : 입력금액 [%d]", LOTTO_PRICE, amount);
             throw new IllegalArgumentException(message);
         }
     }
