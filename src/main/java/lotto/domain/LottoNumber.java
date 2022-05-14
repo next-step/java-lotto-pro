6ea63c1ccd95c6011246807bef1fdbad22f001a9
lcjltj <lcjltj@gmail.com>
2022-05-14 20:38:30 +0900

    feat(LottoNumber): 로또 번호 포장 클래스 생성
    - validate() 1 ~ 45 값이 아닐 경우 Exception 발생

---
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -0,0 +1,41 @@
+package lotto.domain;
+
+import java.util.Objects;
+
+public class LottoNumber {
+    private static final int MIN = 1;
+    private static final int MAX = 45;
+
+    private final int lottoNumber;
+
+    public LottoNumber(final int inputNumber) {
+        validate(inputNumber);
+        this.lottoNumber = inputNumber;
+    }
+
+    private void validate(int inputNumber) {
+        if (inputNumber < MIN || inputNumber > MAX) {
+            throw new IllegalArgumentException("범위 값오류");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return lottoNumber == that.lottoNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "LottoNumber{" +
+                "lottoNumber=" + lottoNumber +
+                '}';
+    }
+}
