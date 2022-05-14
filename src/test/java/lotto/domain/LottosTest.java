2b1f059f6165ccfbd2e39b1bf6c8a355772e1f90
sy001 <suyounlake@naver.com>
2022-05-15 00:43:36 +0900

    test: LottosTest 테스트케이스 추가

--- src/test/java/lotto/domain/LottosTest.java
+++ src/test/java/lotto/domain/LottosTest.java
@@ -1,2 +1,16 @@
-package lotto.domain;public class LottosTest {
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class LottosTest {
+    @Test
+    @DisplayName("")
+    void Lottos_초기화_테스트(){
+        Lottos lottos = new Lottos();
+        lottos.addRandomLotto();
+        assertThat(lottos.size()).isEqualTo(1);
+    }
 }
