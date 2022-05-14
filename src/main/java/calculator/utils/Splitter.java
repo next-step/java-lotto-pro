6ac5bb9d47fce11a794b073391fb688b6ceefacc
mukeunzi <eunji057@gmail.com>
2022-05-14 15:23:38 +0900

    refactor(utils): 매직넘버 상수화
    - 커스텀 구분자를 포함한 문자열이 입력될 때 사용되는 매직넘버 상수로 변경

--- src/main/java/calculator/utils/Splitter.java
+++ src/main/java/calculator/utils/Splitter.java
@@ -7,12 +7,14 @@ public class Splitter {
     private static final String DEFAULT_DELIMITERS = ",|:";
     private static final String CUSTOM_DELIMITER_REGEX = "^//(.)\n(.*)";
     private static final Pattern pattern = Pattern.compile(CUSTOM_DELIMITER_REGEX);
+    private static final int CUSTOM_DELIMITER_INDEX = 1;
+    private static final int CUSTOM_TARGET_INDEX = 2;
     private static Matcher matcher;
 private Splitter() {}
     public static String[] splitString(String input) {
         boolean isExist = hasCustomDelimiter(input);
-        String delimiter = isExist ? matcher.group(1) : DEFAULT_DELIMITERS;
-        String targetString = isExist ? matcher.group(2) : input;
+        String delimiter = isExist ? matcher.group(CUSTOM_DELIMITER_INDEX) : DEFAULT_DELIMITERS;
+        String targetString = isExist ? matcher.group(CUSTOM_TARGET_INDEX) : input;
 
         return targetString.split(delimiter);
     }
