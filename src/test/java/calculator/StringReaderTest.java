03301410c01bc5d3f4c7c53fa5ec4e6141a031cf
tasklet1579 <tasklet1579@gmail.com>
2022-05-15 07:36:35 +0900

    refactor : 테스트 케이스 중복 표현 삭제 - @DisplayName 어노테이션 삭제

--- src/test/java/calculator/StringReaderTest.java
+++ src/test/java/calculator/StringReaderTest.java
@@ -1,6 +1,5 @@
 package calculator;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -9,17 +8,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class StringReaderTest {
-    @DisplayName("숫자 이외의 값 입력")
     @ParameterizedTest
     @ValueSource(strings = {"1,2:3값", "1,two,3", "//구분자\n1;2;3"})
-    void NON_NUMERIC_VALUE(String input) {
+    void 숫자_이외의_값_입력(String input) {
         assertThatThrownBy(() -> StringReader.readString(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("빈 문자열 또는 NULL은 \"0\"으로 바꿈")
     @Test
-    void EMPTY_STRING_OR_NULL_TO_ZERO_STRING() {
+    void 빈_문자열_또는_NULL_0으로_바꿈() {
         assertThat(StringReader.readString(null)).isEqualTo("0");
         assertThat(StringReader.readString("")).isEqualTo("0");
     }
--- src/test/java/calculator/StringSeparatorTest.java
+++ src/test/java/calculator/StringSeparatorTest.java
@@ -1,6 +1,5 @@
 package calculator;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -11,14 +10,13 @@ import java.util.stream.Stream;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class StringSeparatorTest {
-    @DisplayName("숫자_하나만_분리")
     @ParameterizedTest
-    @MethodSource("ONE_NUMBER")
-    void ONE_NUMBER_TO_ONE_NUMBER(String input, String[] output) throws Exception {
+    @MethodSource("숫자_하나")
+    void 숫자_하나만_분리(String input, String[] output) throws Exception {
         assertThat(output).isEqualTo(StringSeparator.split(input));
     }
 
-    static Stream<Arguments> ONE_NUMBER() {
+    static Stream<Arguments> 숫자_하나() {
         return Stream.of(
                 Arguments.of("0", new String[]{"0"}),
                 Arguments.of("10", new String[]{"10"}),
@@ -26,21 +24,18 @@ class StringSeparatorTest {
         );
     }
 
-    @DisplayName("쉼표_구분자로_숫자_분리")
     @Test
-    void COMMA_SPLIT() throws Exception {
+    void 쉼표_구분자로_숫자_분리() throws Exception {
         assertThat(new String[]{"1", "2"}).isEqualTo(StringSeparator.split("1,2"));
     }
 
-    @DisplayName("쉼표_또는_콜론_구분자로_숫자_분리")
     @Test
-    void COMMA_OR_COLON_SPLIT() throws Exception {
+    void 쉼표_또는_콜론_구분자로_숫자_분리() throws Exception {
         assertThat(new String[]{"1", "2", "3"}).isEqualTo(StringSeparator.split("1,2:3"));
     }
 
-    @DisplayName("커스텀_구분자로_숫자_분리")
     @Test
-    void CUSTOM_SPLIT() throws Exception {
+    void 커스텀_구분자로_숫자_분리() throws Exception {
         assertThat(new String[]{"1", "2", "3"}).isEqualTo(StringSeparator.split("//;\n1;2;3"));
     }
 }
