f115a3873929955e36794ae1c0eed19ea7c36dfc
mond <revive29@gmail.com>
2022-05-14 23:30:38 +0900

    feat(LottoRank): 로또 등수(LottoRank) 상수값 및 등수 산정 방식 변경

--- src/main/java/lotto/model/LottoRank.java
+++ src/main/java/lotto/model/LottoRank.java
@@ -1,16 +1,22 @@
 package lotto.model;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public enum LottoRank {
-    FIRST(6, 200_0000_000),
-    SECOND(5, 1_500_000),
-    THIRD(4, 50_000),
-    FOURTH(3, 5_000),
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
     MISS(0, 0);
 
     private final int hits;
     private final int cashPrize;
+//    Predicate<LottoRank> isPositive
 
     LottoRank(int hits, int cashPrize) {
         this.hits = hits;
@@ -25,10 +31,15 @@ public enum LottoRank {
         return cashPrize;
     }
 
-    public static LottoRank findByHits(int hits) {
-        return Arrays.stream(LottoRank.values())
+    public static LottoRank findByHits(int hits, boolean hasBonus) {
+        return filterBonusLottoRank(hasBonus)
                 .filter(lottoRank -> lottoRank.hits == hits)
                 .findAny()
                 .orElse(MISS);
+    }
+
+    private static Stream<LottoRank> filterBonusLottoRank(boolean hasBonus) {
+        return Arrays.stream(LottoRank.values())
+                .filter(lottoRank -> (hasBonus && lottoRank != THIRD) || (!hasBonus && lottoRank != SECOND));
     }
 }
--- src/test/java/lotto/model/LottoRankTest.java
+++ src/test/java/lotto/model/LottoRankTest.java
@@ -9,21 +9,22 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 class LottoRankTest {
 
-    @ParameterizedTest(name = "맞춘 개수({0})에 따른 로또 등수 확인")
+    @ParameterizedTest(name = "맞춘 개수({0}) 보너스 여부({1})에 따른 로또 등수({2}) 확인")
     @MethodSource("parameterVerifyLottoRank")
-    void verifyLottoRank(int hits, LottoRank rank) {
-        assertEquals(LottoRank.findByHits(hits), rank);
+    void verifyLottoRank(int hits, boolean hasBonus, LottoRank rank) {
+        assertEquals(LottoRank.findByHits(hits, hasBonus), rank);
     }
 
     public static Stream<Arguments> parameterVerifyLottoRank() {
         return Stream.of(
-                Arguments.of(6, LottoRank.FIRST),
-                Arguments.of(5, LottoRank.SECOND),
-                Arguments.of(4, LottoRank.THIRD),
-                Arguments.of(3, LottoRank.FOURTH),
-                Arguments.of(2, LottoRank.MISS),
-                Arguments.of(1, LottoRank.MISS),
-                Arguments.of(0, LottoRank.MISS)
+                Arguments.of(6, false, LottoRank.FIRST),
+                Arguments.of(5, true, LottoRank.SECOND),
+                Arguments.of(5, false, LottoRank.THIRD),
+                Arguments.of(4, false, LottoRank.FOURTH),
+                Arguments.of(3, false, LottoRank.FIFTH),
+                Arguments.of(2, false, LottoRank.MISS),
+                Arguments.of(1, false, LottoRank.MISS),
+                Arguments.of(0, false, LottoRank.MISS)
         );
     }
 }
