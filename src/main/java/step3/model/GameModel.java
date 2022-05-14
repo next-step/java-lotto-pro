f9189a19b2b0feddea84727536017eac9dceb412
jhsong2580 <thdwmdgns@naver.com>
2022-05-14 19:39:10 +0900

    feat: 금액/정답로또 매칭 갯수 추출 기능 구현

--- src/main/java/step3/model/GameModel.java
+++ src/main/java/step3/model/GameModel.java
@@ -6,6 +6,8 @@ import static step3.LottoConstant.LOTTO_PRICE;
 import static step3.LottoConstant.LOTTO_VALID_ELEMENTS;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 import step3.InputStatus;
@@ -13,8 +15,16 @@ import step3.domain.LottoTicket;
 
 public class GameModel {
 
-    private final List<LottoTicket> tickets = new ArrayList<>();
+    private List<LottoTicket> tickets = new ArrayList<>();
     private final String LOTTO_DELIMITER = ",";
+    private HashMap<Integer, Integer> statistics;
+
+    public GameModel(List<LottoTicket> tickets) {
+        this.tickets = tickets;
+    }
+
+    public GameModel() {
+    }
 
     private LottoTicket makeLottoTicket() {
         List<Integer> lottoElements = new ArrayList<>();
@@ -22,9 +32,7 @@ public class GameModel {
         for (int i = 0; i < LOTTO_ELEMENTS_SIZE; i++) {
             lottoElements.add(LOTTO_VALID_ELEMENTS.get(i));
         }
-        return LottoTicket.create(
-            lottoElements.stream().map(String::valueOf).collect(Collectors.toList())
-        );
+        return LottoTicket.create(lottoElements.stream().map(String::valueOf).collect(Collectors.toList()));
     }
 
     public int buyTicket(int money) {
@@ -60,6 +68,23 @@ public class GameModel {
             validResult = false;
         } finally {
             return validResult;
+        }
+    }
+
+    public HashMap<Integer, Integer> checkWin(String winLottoSource) {
+        initStatistics();
+        LottoTicket winLotto = LottoTicket.create(Arrays.asList(winLottoSource.split(LOTTO_DELIMITER)));
+        for (int i = 0; i < tickets.size(); i++) {
+            int matchCount = winLotto.getMatchCountWith(tickets.get(i));
+            statistics.replace(matchCount, statistics.get(matchCount) + 1);
+        }
+        return statistics;
+    }
+
+    private void initStatistics() {
+        statistics = new HashMap<>();
+        for (int i = 1; i <= LOTTO_ELEMENTS_SIZE; i++) {
+            statistics.put(i, 0);
         }
     }
 }
--- src/test/java/step3/GameModelTest.java
+++ src/test/java/step3/GameModelTest.java
@@ -2,14 +2,22 @@ package step3;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static step3.LottoConstant.FOUR_NUMBER_MATCH;
 import static step3.LottoConstant.LOTTO_PRICE;
+import static step3.LottoConstant.SIX_NUMBER_MATCH;
+import static step3.LottoConstant.THREE_NUMBER_MATCH;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
+import step3.domain.LottoTicket;
 import step3.model.GameModel;
 
 public class GameModelTest {
@@ -50,5 +58,22 @@ public class GameModelTest {
         if (!expected) {
             assertThatThrownBy(() -> gameModel.validInput(money, InputStatus.MONEY)).isInstanceOf(IllegalArgumentException.class);
         }
+    }
+
+    @Test
+    @DisplayName("각 로또별 매칭 갯수를 HashMap으로 반환한다")
+    public void testCheckWin() {
+        List<LottoTicket> tickets = new ArrayList<>();
+        tickets.add(LottoTicket.create(Arrays.asList("1", "2", "3", "4", "5", "6")));
+        tickets.add(LottoTicket.create(Arrays.asList("1", "2", "3", "4", "8", "7")));
+        tickets.add(LottoTicket.create(Arrays.asList("1", "2", "3", "9", "8", "7")));
+        tickets.add(LottoTicket.create(Arrays.asList("1", "2", "3", "9", "8", "7")));
+        tickets.add(LottoTicket.create(Arrays.asList("1", "2", "10", "9", "8", "7")));
+        GameModel testModel = new GameModel(tickets);
+
+        assertThat(testModel.checkWin("1,2,3,4,5,6"))
+            .containsEntry(THREE_NUMBER_MATCH, 2)
+            .containsEntry(FOUR_NUMBER_MATCH, 1)
+            .containsEntry(SIX_NUMBER_MATCH, 1);
     }
 }
