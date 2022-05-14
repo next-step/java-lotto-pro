070e1cbe58a74e57e250910064418bb838bb686a
Philip <pch4623@gmail.com>
2022-05-14 14:08:33 +0900

    feat(StringAddCalculator): 문자열 입력값 덧셈 기능 호출

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,12 +1,16 @@
 package calculator;
 
+import static calculator.utils.CalculatorUtils.split;
+
 public class StringAddCalculator {
     private static final int DEFAULT_VALUE = 0;
 
     public static int splitAndSum(String input) {
         if (isNullOrEmpty(input))
             return DEFAULT_VALUE;
-        return 0;
+
+        Numbers numbers = new Numbers(split(input));
+        return numbers.sum();
     }
 
     private static boolean isNullOrEmpty(String input) {
