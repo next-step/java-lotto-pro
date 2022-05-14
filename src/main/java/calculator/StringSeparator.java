8e075c33619be0ddcedec76fac3d0b1b3724b1bf
tasklet1579 <tasklet1579@gmail.com>
2022-05-15 07:52:12 +0900

    refactor(StringSeparator) : split 파라미터 상수화 처리

--- src/main/java/calculator/StringSeparator.java
+++ src/main/java/calculator/StringSeparator.java
@@ -7,13 +7,16 @@ public class StringSeparator {
     private static final String BASIC_SPLIT = ",|:";
     private static final Pattern CUSTOM_SPLIT = Pattern.compile("//(.)\n(.*)");
 
+    private static final int ELEMENTS = 2;
+    private static final int CUSTOM_DELIMITER = 1;
+
     private StringSeparator() {
     }
 
     public static String[] split(String input) {
         Matcher custom = CUSTOM_SPLIT.matcher(input);
         if (custom.find()) {
-            return custom.group(2).split(custom.group(1));
+            return custom.group(ELEMENTS).split(custom.group(CUSTOM_DELIMITER));
         }
         return input.split(BASIC_SPLIT);
     }
