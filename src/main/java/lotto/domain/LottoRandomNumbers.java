d85d9a56efa9fdb88ffd8b7160640352b720643e
14km <k.connor614@gmail.com>
2022-05-14 12:06:14 +0900

    feat: LottoRandomNumbers 추가

---
+++ src/main/java/lotto/domain/LottoRandomNumbers.java
@@ -0,0 +1,31 @@
+package lotto.domain;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class LottoRandomNumbers {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+
+    private static final List<LottoNumber> DEFAULT_LOTTO_NUMBERS = IntStream
+            .rangeClosed(MIN_NUMBER, MAX_NUMBER)
+            .mapToObj(LottoNumber::new)
+            .collect(Collectors.toList());
+
+    private final List<LottoNumber> lottoNumbers;
+
+    public LottoRandomNumbers() {
+        this(DEFAULT_LOTTO_NUMBERS);
+    }
+
+    public LottoRandomNumbers(List<LottoNumber> numList) {
+        this.lottoNumbers = numList;
+    }
+
+    public List<LottoNumber> generate() {
+        Collections.shuffle(lottoNumbers);
+        return lottoNumbers.subList(0, 6);
+    }
+}
