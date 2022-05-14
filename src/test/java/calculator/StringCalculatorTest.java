349195e9bd4ae6a5f9027d9b111614725cb45032
mukeunzi <eunji057@gmail.com>
2022-05-14 15:40:43 +0900

    refactor(test): assertThat() -> assertAll()로 리팩토링

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,19 +1,22 @@
 package calculator;
 
-import calculator.StringCalculator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class StringCalculatorTest {
     @Test
     @DisplayName("빈 문자열 혹은 null일 경우 0이 응답된다.")
     void checkEmptyOrNull() {
         StringCalculator stringCalculator = new StringCalculator();
-        assertThat(stringCalculator.sum("")).isEqualTo(0);
-        assertThat(stringCalculator.sum(null)).isEqualTo(0);
+
+        assertAll(
+                () -> assertThat(stringCalculator.sum("")).isEqualTo(0),
+                () -> assertThat(stringCalculator.sum(null)).isEqualTo(0)
+        );
     }
 
     @Test
