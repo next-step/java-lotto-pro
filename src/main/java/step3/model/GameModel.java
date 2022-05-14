1c55f602a62956c7c516a1e4864770ad7b61971c
jhsong2580 <thdwmdgns@naver.com>
2022-05-14 17:40:02 +0900

    feat: 금액/정답로또 입력 검증 기능 구현

--- src/main/java/step3/model/GameModel.java
+++ src/main/java/step3/model/GameModel.java
@@ -8,11 +8,13 @@ import static step3.LottoConstant.LOTTO_VALID_ELEMENTS;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import step3.InputStatus;
 import step3.domain.LottoTicket;
 
 public class GameModel {
 
     private final List<LottoTicket> tickets = new ArrayList<>();
+    private final String LOTTO_DELIMITER = ",";
 
     private LottoTicket makeLottoTicket() {
         List<Integer> lottoElements = new ArrayList<>();
@@ -30,5 +32,34 @@ public class GameModel {
             tickets.add(makeLottoTicket());
         }
         return tickets.size();
+    }
+
+    public boolean validInput(String input, InputStatus inputStatus) {
+        boolean validResult = true;
+        if (inputStatus == InputStatus.MONEY) {
+            validResult = validMoney(input);
+        }
+        if (inputStatus == InputStatus.LOTTO) {
+            validResult = validLottoSource(input);
+        }
+        if (!validResult) {
+            throw new IllegalArgumentException();
+        }
+        return validResult;
+    }
+
+    private boolean validLottoSource(String input) {
+        return input.split(LOTTO_DELIMITER).length == LOTTO_ELEMENTS_SIZE;
+    }
+
+    private boolean validMoney(String input) {
+        boolean validResult = true;
+        try {
+            validResult = Integer.parseInt(input) >= LOTTO_PRICE;
+        } catch (NumberFormatException e) {
+            validResult = false;
+        } finally {
+            return validResult;
+        }
     }
 }
--- src/test/java/step3/GameModelTest.java
+++ src/test/java/step3/GameModelTest.java
@@ -1,11 +1,14 @@
 package step3;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static step3.LottoConstant.LOTTO_PRICE;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import step3.model.GameModel;
 
@@ -22,8 +25,30 @@ public class GameModelTest {
     @ValueSource(ints = {1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 12000})
     @DisplayName("금액에 맞춰 로또를 생성한다")
     public void createTicketByMoney(int money) {
-        Assertions.assertThat(gameModel.buyTicket(money)).isEqualTo(money / LOTTO_PRICE);
-
+        assertThat(gameModel.buyTicket(money)).isEqualTo(money / LOTTO_PRICE);
     }
 
+    @ParameterizedTest
+    @CsvSource(value = {"1,2,3,4,5,6:true", "1,2,3,4,5,-1:true", "1,2,3,4,a,-1:true", "1,2,3,4,4,-1:true", "1,1,3,4,5,-1:true",
+        "1,1,3,4,-1:false",}, delimiter = ':')
+    @DisplayName("로또 정보 입력시 검증 결과 확인. 갯수가 6개인지만 확인한다")
+    public void validLottoInputTest(String lottoInfo, boolean expected) {
+        if (expected) {
+            Assertions.assertDoesNotThrow(() -> gameModel.validInput(lottoInfo, InputStatus.LOTTO));
+        }
+        if (!expected) {
+            assertThatThrownBy(() -> gameModel.validInput(lottoInfo, InputStatus.LOTTO)).isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1000:true", "10001:true", "2050:true", "1050:true", "900:false", "-1:false",}, delimiter = ':')
+    public void validMoneyInputTest(String money, boolean expected) {
+        if (expected) {
+            Assertions.assertDoesNotThrow(() -> gameModel.validInput(money, InputStatus.MONEY));
+        }
+        if (!expected) {
+            assertThatThrownBy(() -> gameModel.validInput(money, InputStatus.MONEY)).isInstanceOf(IllegalArgumentException.class);
+        }
+    }
 }
