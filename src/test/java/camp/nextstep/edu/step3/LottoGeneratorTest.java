b29f73a9f8969fbbf48e4ff395b154332cf43403
mannue <sppe12@gmail.com>
2022-05-14 20:58:01 +0900

    refactor(LottoGenerator): LottoNumbers 를통해서 로또번호를 추출하도록 수정

--- src/test/java/camp/nextstep/edu/step3/LottoGeneratorTest.java
+++ src/test/java/camp/nextstep/edu/step3/LottoGeneratorTest.java
@@ -3,6 +3,9 @@ package camp.nextstep.edu.step3;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
@@ -11,8 +14,9 @@ public class LottoGeneratorTest {
     @DisplayName("6개 숫자를 가진 Lotto 를 요청한 만큼 자동으로 생성한다")
     @Test
     void autoTest() {
-        Lotto[] lottoArray = new LottoGenerator().auto(5);
-        assertThat(lottoArray.length).isEqualTo(5);
+
+        assertThat(new LottoGenerator(testLottoNumbers()).auto())
+                .isEqualTo(new Lotto(LottoTest.createLottoNumberArray(new int[] {1,2,3,4,5,6})));
     }
 
     @DisplayName("Lotto 를 수동으로 생성한다.")
@@ -27,5 +31,9 @@ public class LottoGeneratorTest {
     void distinctTest() {
         assertThatThrownBy(() -> new LottoGenerator().manual(new int[] {1,2,3,4,5,5}))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private LottoNumbers testLottoNumbers() {
+        return new LottoNumbers(Arrays.stream(new int[]{1, 2, 3, 4, 5, 6}).mapToObj(LottoNumber::new).collect(Collectors.toList()));
     }
 }
