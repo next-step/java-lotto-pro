a52c75205f613030d7bea4828f991765fbd6e6ce
Philip <pch4623@gmail.com>
2022-05-15 01:16:37 +0900

    feat(LottoNumber): 하나의 로또 번호를 나타낼 클래스 추가

---
+++ src/main/java/lotto/model/LottoNumber.java
@@ -0,0 +1,24 @@
+package lotto.model;
+
+import static lotto.constants.LottoConstant.MAX_LOTTO_NUMBER;
+import static lotto.constants.LottoConstant.MIN_LOTTO_NUMBER;
+
+public class LottoNumber {
+    private final int number;
+
+    private LottoNumber(int number) {
+        this.number = number;
+    }
+
+    public static LottoNumber of(int number) {
+        checkNumberRange(number);
+        return new LottoNumber(number);
+    }
+
+    private static void checkNumberRange(int number) {
+        if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
+            String message = String.format("[ERROR] 로또 번호는 %d와 %d 사이 입니다!", MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER);
+            throw new IllegalArgumentException(message);
+        }
+    }
+}
