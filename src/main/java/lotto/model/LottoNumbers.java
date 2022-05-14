48568d7d3576a7ac75761a54ddf44e668b831f88
wenodev <logicalstark1105@gmail.com>
2022-05-15 00:08:36 +0900

    fix(LottoNumbers.java): LottoNumber가 6개가 아닌경우 예외를 던진다

--- src/main/java/lotto/model/LottoNumbers.java
+++ src/main/java/lotto/model/LottoNumbers.java
@@ -1,16 +1,24 @@
 package lotto.model;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class LottoNumbers {
+    private static final int LOTTO_STANDARD_SIZE = 6;
+    private static final String INVALID_SIZE = "6개의 숫자가 입력되야합니다";
     private final List<LottoNumber> numbers;
 
     public LottoNumbers(List<Integer> numbers) {
+        validateSize(numbers);
         this.numbers = mapLottoNumber(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_STANDARD_SIZE) {
+            throw new IllegalArgumentException(INVALID_SIZE);
+        }
     }
 
     public List<LottoNumber> getNumbers() {
--- src/test/java/lotto/LottoNumbersTest.java
+++ src/test/java/lotto/LottoNumbersTest.java
@@ -2,22 +2,29 @@ package lotto;
 
 import lotto.model.LottoNumbers;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 @DisplayName("LottoNumbers는 ")
 public class LottoNumbersTest {
-    @DisplayName("6개의 LottoNumber로 구성된다.")
-    @Test
-    void validLottoNumber() {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+    private static Stream<List> provideInvalidSizeList() {
+        return Stream.of(
+                Arrays.asList(1, 2, 3, 4, 5, 6, 7),
+                Arrays.asList(1, 2, 3, 4, 5)
+        );
+    }
 
-        LottoNumbers lottoNumbers = new LottoNumbers(numbers);
-
-        assertThat(lottoNumbers).isEqualTo(new LottoNumbers(numbers));
+    @DisplayName("6개의 LottoNumber가 아니라면 예외를 던진다.")
+    @ParameterizedTest
+    @MethodSource("provideInvalidSizeList")
+    void invalidLottoNumberSize(List<Integer> numbers) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new LottoNumbers(numbers));
     }
 }
