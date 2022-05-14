58ff3cef864614ea093ff04fa32c03f56e72fecb
seungin yang <rhfpdk92@naver.com>
2022-05-14 09:07:09 +0900

    refactor: money validate 추가

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -4,7 +4,14 @@ public class Money {
     private final int value;
 
     private Money(int value) {
+        validate(value);
         this.value = value;
+    }
+
+    private void validate(int value) {
+        if (value < 0) {
+            throw new IllegalArgumentException("돈은 0보다 작을 수 없습니다.");
+        }
     }
 
     public static Money from(int money) {
