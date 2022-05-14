471ccc8f3aa5d713e9e2e05749467480b62f151d
Philip <pch4623@gmail.com>
2022-05-15 01:13:38 +0900

    feat(LottoConstant): 로또 숫자 최소/최대값 전역 상수 설정

--- src/main/java/lotto/constants/LottoConstant.java
+++ src/main/java/lotto/constants/LottoConstant.java
@@ -2,4 +2,6 @@ package lotto.constants;
 
 public class LottoConstant {
     public static final int LOTTO_PRICE = 1000;
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
 }
