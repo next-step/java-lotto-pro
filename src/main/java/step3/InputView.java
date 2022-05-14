a797c79916d790a8e8a2d5ea6e09c7db78534e62
piopoi <kangsw1988@gmail.com>
2022-05-14 21:53:47 +0900

    feat(InputView): 금액, 당첨번호 입력 기능 추가

---
+++ src/main/java/step3/InputView.java
@@ -0,0 +1,40 @@
+package step3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class InputView {
+
+    private static final String LOTTO_NUMBER_DELIMITER = ",";
+
+    private InputView() {
+    }
+
+    public static Integer inputMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return inputNumber();
+    }
+
+    public static List<Integer> inputWinnerNumbers() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        return Arrays.stream(inputText().split(LOTTO_NUMBER_DELIMITER))
+                .map(text -> Integer.parseInt(text.trim()))
+                .collect(Collectors.toList());
+    }
+
+    private static int inputNumber() {
+        try {
+            return Integer.parseInt(inputText());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자만 입력해주세요.");
+        }
+    }
+
+    private static String inputText() {
+        Scanner scan = new Scanner(System.in);
+        return scan.next();
+    }
+}
