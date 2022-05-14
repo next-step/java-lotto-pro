ac4f732f4d09b78fd2014c87d9a2ccbd56a8ce84
Philip <pch4623@gmail.com>
2022-05-15 01:23:24 +0900

    test(LottoNumberTest): 로또 번호 범위 오류 테스트

--- src/test/java/lotto/model/LottoNumberTest.java
+++ src/test/java/lotto/model/LottoNumberTest.java
@@ -4,16 +4,28 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static lotto.constants.LottoConstant.MAX_LOTTO_NUMBER;
+import static lotto.constants.LottoConstant.MIN_LOTTO_NUMBER;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoNumberTest {
 
     @ParameterizedTest
     @DisplayName("로또 숫자를 필드에 셋팅하고 LottoNumber 객체를 생성한다.")
     @ValueSource(ints = {1, 10, 25, 45})
-    void LottoNumber_(int input) {
+    void LottoNumber_생성(int input) {
         assertThat(LottoNumber.of(input))
                 .isExactlyInstanceOf(LottoNumber.class)
                 .hasFieldOrPropertyWithValue("number", input);
+    }
+
+    @ParameterizedTest
+    @DisplayName("로또 숫자 생성 시 최소 혹은 최대값을 벗어나면 에러를 발생시킨다.")
+    @ValueSource(ints = {-1, -10, 46, 50})
+    void LottoNumber_범위오류(int input) {
+        assertThatThrownBy(() -> LottoNumber.of(input))
+                .isExactlyInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 로또 번호는 %d와 %d 사이 입니다!", MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER);
     }
 }
