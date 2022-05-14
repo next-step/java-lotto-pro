0e14a1283c535a57ef8189f179fb82f434353b82
kms <kmmin78@gmail.com>
2022-05-14 14:31:12 +0900

    fix : step3 로또 객체 구현

---
+++ src/main/java/step3/Lotto.java
@@ -0,0 +1,42 @@
+package step3;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Lotto {
+    private final static int LOTTO_MIN_NUMBER = 1;
+    private final static int LOTTO_MAX_NUMBER = 45;
+    public final static int LOTTO_FIRST_INDEX = 0;
+    public final static int LOTTO_SIZE = 6;
+
+    private final List<Integer> numbers;
+
+    public Lotto() {
+        this.numbers = createSixRandomNumbers();
+    }
+
+    private List<Integer> createSixRandomNumbers() {
+        final List<Integer> candidates = createLottoNumbers();
+        Collections.shuffle(candidates);
+        final List<Integer> sixNumbers = new ArrayList<>(candidates.subList(LOTTO_FIRST_INDEX, LOTTO_SIZE));
+        Collections.sort(sixNumbers);
+        return sixNumbers;
+    }
+
+    private List<Integer> createLottoNumbers() {
+        final List<Integer> candidates = new ArrayList<>();
+        for (int i = LOTTO_MIN_NUMBER; i <= LOTTO_MAX_NUMBER; i++) {
+            candidates.add(i);
+        }
+        return candidates;
+    }
+
+    public int size() {
+        return numbers.size();
+    }
+
+    public Integer get(int index) {
+        return numbers.get(index);
+    }
+}
---
+++ src/test/java/step3/LottoTest.java
@@ -0,0 +1,55 @@
+package step3;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Lotto 클래스")
+public class LottoTest {
+    @DisplayName("로또 객체는 생성될 경우")
+    @Nested
+    class Lotto_Is_Created_Then {
+        private Lotto lotto;
+
+        @BeforeEach
+        void setUp() {
+            lotto = new Lotto();
+        }
+
+        @DisplayName("총 6개의 숫자를 갖는다.")
+        @Test
+        void has_Six_Numbers() {
+            assertThat(lotto.size()).isEqualTo(6);
+        }
+
+        @DisplayName("각 숫자는 1부터 45까지 숫자여야 한다.")
+        @Test
+        void has_One_to_FourtyFive_Range() {
+            for (int i = Lotto.LOTTO_FIRST_INDEX; i < Lotto.LOTTO_SIZE; i++) {
+                final Integer number = lotto.get(i);
+                assertThat(number).isGreaterThanOrEqualTo(1);
+                assertThat(number).isLessThanOrEqualTo(45);
+            }
+        }
+
+        @DisplayName("각 숫자가 중복이 될 수 없다.")
+        @Test
+        void cannot_Be_Duplicated() {
+            final Set<Integer> lottoSet = createNewSetUsingLotto(lotto);
+            assertThat(lottoSet.size()).isEqualTo(lotto.size());
+        }
+
+        private Set<Integer> createNewSetUsingLotto(final Lotto lotto) {
+            final Set<Integer> lottoSet = new HashSet<>();
+            for(int i= 0; i < lotto.size(); i++){
+                lottoSet.add(lotto.get(i));
+            }
+            return lottoSet;
+        }
+    }
+}
