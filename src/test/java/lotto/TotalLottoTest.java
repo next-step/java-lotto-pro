cba19d294702abbebf7395bae5fb2247a283c4a1
jiyeonghwang <gyp0288@naver.com>
2022-05-15 02:32:55 +0900

    test: 로또 구매 개수 test

---
+++ src/test/java/lotto/TotalLottoTest.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import lotto.domain.TotalLotto;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TotalLottoTest {
+    @Test
+    public void 구매한_로또_개수_count() throws IllegalArgumentException {
+        TotalLotto totalLotto = new TotalLotto();
+        totalLotto.count("14000");
+        TotalLotto expectedTrue = new TotalLotto(14);
+        assertThat(totalLotto.equals(expectedTrue)).isEqualTo(true);
+
+        TotalLotto expectedFalse = new TotalLotto(20);
+        assertThat(totalLotto.equals(expectedFalse)).isEqualTo(false);
+    }
+}
