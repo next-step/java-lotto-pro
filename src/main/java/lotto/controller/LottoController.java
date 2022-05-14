0badc4bff1446479f9df59d8ba054e2510be4021
sy001 <suyounlake@naver.com>
2022-05-14 23:54:55 +0900

    feat: 사용자로부터 구입금액을 입력받기
    - main 메소드생성
    - LottoController 생성
    - InputView 생성

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,10 @@
+package lotto.controller;
+
+import lotto.domain.Lotto;
+import lotto.view.InputView;
+
+public class LottoController {
+    public static void simulateLotto(){
+        InputView.readMoney();
+    }
+}
---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,12 @@
+package lotto.view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static String readMoney(){
+        System.out.println("구입금액을 입력해 주세요.");
+        return scan.nextLine();
+    }
+}
