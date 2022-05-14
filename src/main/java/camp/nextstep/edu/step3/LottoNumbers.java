59e7172053ecb2d566031d3744408c6c024b39bc
mannue <sppe12@gmail.com>
2022-05-14 20:55:31 +0900

    refactor(LottoNumbers): 생성자 추가

--- src/main/java/camp/nextstep/edu/step3/LottoNumbers.java
+++ src/main/java/camp/nextstep/edu/step3/LottoNumbers.java
@@ -5,22 +5,26 @@ import java.util.List;
 import java.util.function.Consumer;
 
 public class LottoNumbers {
-    private final List<LottoNumber> lottoNumbers = new ArrayList<>();
+    private final List<LottoNumber> numbers = new ArrayList<>();
 
     public LottoNumbers() {
         final int START_NUMBER = 1;
         final int END_NUMBER = 45;
         for (int i = START_NUMBER; i <= END_NUMBER; i++) {
-            lottoNumbers.add(new LottoNumber(i));
+            numbers.add(new LottoNumber(i));
         }
     }
 
+    public LottoNumbers(List<LottoNumber> numbers) {
+        this.numbers.addAll(numbers);
+    }
+
     public boolean isContains(LottoNumber lottoNumber) {
-        return this.lottoNumbers.contains(lottoNumber);
+        return this.numbers.contains(lottoNumber);
     }
 
     public Lotto extract(Consumer<List<LottoNumber>> consumer)  {
-        consumer.accept(lottoNumbers);
-        return new Lotto(lottoNumbers.subList(0, 6).toArray(new LottoNumber[0]));
+        consumer.accept(numbers);
+        return new Lotto(numbers.subList(0, 6).toArray(new LottoNumber[0]));
     }
 }
