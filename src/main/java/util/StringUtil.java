9d552733b62f6e48842afbf4d9b9ecd549e3979c
millennion-jin <Jadestern2@>
2022-05-14 16:31:28 +0900

    feat(StringUtil) : String이 비었는지 확인하는 기능, String을 split하여 숫자열로 리턴하는 기능 구현

--- src/main/java/util/StringUtil.java
+++ src/main/java/util/StringUtil.java
@@ -4,15 +4,54 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StringUtil {
-    public static boolean isEmpty(String text) {
-        return false;
+
+    private static final String DEFAULT_DELIMITER = ",|:";
+    private static final String ERROR_MESSAGE_NOT_NUMBER_FORMAT = "[ERROR] %s is not a number";
+
+    private StringUtil() {
     }
 
-    public static List<Integer> splitNumbersString(String numbers) {
-        return new ArrayList<>();
+    public static boolean isEmpty(final String text) {
+        return text == null || text.isEmpty();
     }
 
-    public static List<Integer> splitNumbersString(String numbers, String delimiter) {
-        return new ArrayList<>();
+    public static List<Integer> splitNumbersString(final String numbers) {
+        return splitNumbersString(numbers, DEFAULT_DELIMITER);
+    }
+
+    public static List<Integer> splitNumbersString(final String numbers, final String delimiter) {
+        if (StringUtil.isEmpty(numbers)) {
+            return new ArrayList<>();
+        }
+
+        if (StringUtil.isEmpty(delimiter)) {
+            return extractNumberListFromString(numbers, DEFAULT_DELIMITER);
+        }
+
+        return extractNumberListFromString(numbers, delimiter);
+    }
+
+    private static List<Integer> extractNumberListFromString(final String numbers, final String delimiter) {
+        List<Integer> numberList = new ArrayList<>();
+
+        String[] numberStrings = numbers.split(delimiter);
+        for (String numberString : numberStrings) {
+            addNumberToList(numberList, numberString);
+        }
+
+        return numberList;
+    }
+
+    private static void addNumberToList(final List<Integer> numberList, final String numberString) {
+        if (StringUtil.isEmpty(numberString)) {
+            return;
+        }
+
+        try {
+            numberList.add(Integer.parseInt(numberString));
+        } catch (NumberFormatException ex) {
+            System.out.println(String.format(ERROR_MESSAGE_NOT_NUMBER_FORMAT, numberString));
+            throw ex;
+        }
     }
 }
