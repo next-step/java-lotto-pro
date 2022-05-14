77edd682105f1e56ccb7161b1bcb12d12443cdbe
Philip <pch4623@gmail.com>
2022-05-14 13:59:51 +0900

    test(StringAddCalculatorTest): null 또는 빈 문자 입력 시 0 리턴 테스트

---
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -0,0 +1,20 @@
+package calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StringAddCalculatorTest {
+    
+    @Test
+    @DisplayName("null 또는 빈문자가 입력되는 경우 0을 반환하는지 확인한다.")
+    public void splitAndSum_null_또는_빈문자() {
+        int result = StringAddCalculator.splitAndSum(null);
+        assertThat(result).isEqualTo(0);
+
+        result = StringAddCalculator.splitAndSum("");
+        assertThat(result).isEqualTo(0);
+    }
+
+}
