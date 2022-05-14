630cdb48b2750034bf02450ad64850b6318c2046
Yul Jeon <notkhaki@gmail.com>
2022-05-14 22:56:30 +0900

    feat(LottoTicket): check lotto ticket to get result

--- src/main/java/lotto/LottoTicket.java
+++ src/main/java/lotto/LottoTicket.java
@@ -1,7 +1,10 @@
 package lotto;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class LottoTicket {
 
@@ -17,6 +20,12 @@ public class LottoTicket {
         this.lottoGames = lottoGames;
     }
 
+    public LottoTicket(List<List<Integer>> lottoGamesNumbers) {
+        this.lottoGames = lottoGamesNumbers.stream()
+                .map(gameNumbers -> new LottoGame(gameNumbers))
+                .collect(Collectors.toList());
+    }
+
     public int size() {
         return lottoGames.size();
     }
@@ -25,5 +34,15 @@ public class LottoTicket {
         if (money / PRICE <= 0) {
             throw new IllegalArgumentException("잘못된 금액");
         }
+    }
+
+    public Map<Long, Integer> check(final List<LottoNumber> winningNumbers) {
+        Map<Long, Integer> result = new HashMap<>();
+        lottoGames.forEach(lottoGame -> {
+            Long match = lottoGame.check(winningNumbers);
+            result.put(match, result.getOrDefault(match, 0) + 1);
+        });
+
+        return result;
     }
 }
--- src/test/java/LottoTicketTest.java
+++ src/test/java/LottoTicketTest.java
@@ -1,5 +1,11 @@
+import lotto.LottoNumber;
 import lotto.LottoTicket;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Arrays;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -17,5 +23,29 @@ public class LottoTicketTest {
         assertThatThrownBy(() -> {
             LottoTicket lottoTicket = new LottoTicket(900);
         }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"0:1", "1:3", "2:1", "3:2", "6:1"}, delimiter = ':')
+    void 당첨_확인(long input, int expected) {
+        LottoTicket lottoTicket = new LottoTicket(Arrays.asList(
+                Arrays.asList(10, 11, 12, 13, 14, 15),
+                Arrays.asList(6, 10, 11, 12, 13, 14),
+                Arrays.asList(5, 10, 11, 12, 13, 14),
+                Arrays.asList(4, 10, 11, 12, 13, 14),
+                Arrays.asList(1, 2, 10, 11, 12, 13),
+                Arrays.asList(1, 2, 3, 10, 11, 12),
+                Arrays.asList(1, 2, 4, 10, 11, 12),
+                Arrays.asList(1, 2, 3, 4, 5, 6)));
+
+        Map<Long, Integer> result = lottoTicket.check(Arrays.asList(
+                new LottoNumber(1),
+                new LottoNumber(2),
+                new LottoNumber(3),
+                new LottoNumber(4),
+                new LottoNumber(5),
+                new LottoNumber(6)));
+
+        assertThat(result).containsEntry(input, expected);
     }
 }
