a293933491cfec0abedca7223b392f233e7f3528
jiyeonghwang <gyp0288@naver.com>
2022-05-15 02:32:02 +0900

    feat: TotalLotto domain create

---
+++ src/main/java/lotto/domain/TotalLotto.java
@@ -0,0 +1,50 @@
+package lotto.domain;
+
+import java.util.Objects;
+
+public class TotalLotto {
+    private static final int LOTTO_PRICE = 1000;
+    private static final String INPUT_ERROR = "잘못된 금액을 입력하셨습니다.";
+
+    private int count;
+
+    public TotalLotto() {
+    }
+
+    public TotalLotto(int count) {
+        this.count = count;
+    }
+
+    public void count(String input) throws IllegalArgumentException {
+        int amount = amountStringToInt(input);
+        this.count = calculatorCount(amount);
+    }
+
+    private int amountStringToInt(String input) throws IllegalArgumentException {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INPUT_ERROR);
+        }
+    }
+
+    private int calculatorCount(int amount) throws IllegalArgumentException {
+        if (amount % LOTTO_PRICE > 0) {
+            throw new IllegalArgumentException(INPUT_ERROR);
+        }
+        return amount/LOTTO_PRICE;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TotalLotto totalLotto = (TotalLotto) o;
+        return count == totalLotto.count;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(count);
+    }
+}
