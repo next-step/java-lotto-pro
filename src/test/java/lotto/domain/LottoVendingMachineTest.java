07acecb1a7e53876eed8efa8b06303ae66371e3b
toughchb <ggang2000@gmail.com>
2022-05-14 11:15:51 +0900

    test(LottoVendingMachineTest): 테스트 변경

--- src/test/java/lotto/domain/LottoVendingMachineTest.java
+++ src/test/java/lotto/domain/LottoVendingMachineTest.java
@@ -1,6 +1,9 @@
 package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +12,11 @@ class LottoVendingMachineTest {
     @Test
     void 자판기에서_금액을_입력하면_로또를_살_수_있다() {
         LottoVendingMachine vendingMachine = new LottoVendingMachine();
-        LottoTickets tickets = vendingMachine.purchase(Money.from(10_000));
-        assertThat(tickets.purchasedTicketsCount()).isEqualTo(10);
+        assertAll(
+                ()-> assertEquals(vendingMachine.purchase(Money.from(10_000)).purchasedTicketsCount(), 10),
+                ()-> assertEquals(vendingMachine.purchase(Money.from(15_000)).purchasedTicketsCount(), 15),
+                ()-> assertEquals(vendingMachine.purchase(Money.from(10_500)).purchasedTicketsCount(), 10),
+                ()-> assertEquals(vendingMachine.purchase(Money.from(17_700)).purchasedTicketsCount(), 17)
+        );
     }
 }
