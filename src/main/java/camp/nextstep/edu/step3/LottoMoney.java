36dc429a71a8e674b9e37023fa5da70e9d1b19c2
mannue <sppe12@gmail.com>
2022-05-14 20:21:04 +0900

    feat(LottoMoney): 로또 구입 금액 정보를 가진 클래스 생성

---
+++ src/main/java/camp/nextstep/edu/step3/LottoMoney.java
@@ -0,0 +1,48 @@
+package camp.nextstep.edu.step3;
+
+import java.util.Objects;
+
+public class LottoMoney {
+    private final static int LOTTO_PRICE = 1000;
+    private final int money;
+
+    public LottoMoney(final int money) {
+        if (0 > money) {
+            throw new IllegalArgumentException("input is not negative");
+        }
+        this.money = money;
+    }
+
+    public boolean isBuyable() {
+        return money >= LOTTO_PRICE;
+    }
+
+    public LottoMoney buy() {
+        return new LottoMoney(this.money - LOTTO_PRICE);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoMoney lottoMoney = (LottoMoney) o;
+        return money == lottoMoney.money;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(money);
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public EarningsRate calculate(final long totalMoney) {
+        if (0 >= this.money) {
+            throw new IllegalStateException("invalid money");
+        }
+        double rate = Double.longBitsToDouble(totalMoney) / Double.longBitsToDouble(this.money);
+        return  new EarningsRate(Math.floor(rate * 100) / 100.0);
+    }
+}
