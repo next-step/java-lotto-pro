09cee5ca6f599702cfa5b5b5dff956a03d29b369
Philip <pch4623@gmail.com>
2022-05-14 18:17:48 +0900

    refactor(InputView): 변수 및 메서드 이름 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
-    private static final String MONEY_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String PURCHASE_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
 
-    public static String readInput() {
-        System.out.println(MONEY_INPUT_MESSAGE);
+    public static String readPurchaseAmount() {
+        System.out.println(PURCHASE_AMOUNT_INPUT_MESSAGE);
         return scanner.nextLine();
     }
 
