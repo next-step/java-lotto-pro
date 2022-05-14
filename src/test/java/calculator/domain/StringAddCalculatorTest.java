15c7d6682ab57a9cba917b0996e77eab444d1979
soob-forest <dltntnqo@naver.com>
2022-05-14 14:48:17 +0900

    test(StringUtils): assertAll 적용

--- src/test/java/calculator/domain/StringAddCalculatorTest.java
+++ src/test/java/calculator/domain/StringAddCalculatorTest.java
@@ -1,19 +1,19 @@
 package calculator.domain;
 
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import org.junit.jupiter.api.Test;
+
 
 public class StringAddCalculatorTest {
     @Test
     void 널_또는_빈문자() {
-
-        int result = StringAddCalculator.splitAndSum(null);
-        assertThat(result).isEqualTo(0);
-
-        result = StringAddCalculator.splitAndSum("");
-        assertThat(result).isEqualTo(0);
+        assertAll(
+                () -> assertThat(StringAddCalculator.splitAndSum(null)).isEqualTo(0),
+                () -> assertThat(StringAddCalculator.splitAndSum("")).isEqualTo(0)
+        );
     }
 
     @Test
