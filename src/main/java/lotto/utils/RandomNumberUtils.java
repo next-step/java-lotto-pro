772f8d38ab5e73d26d65ddf3ac74a02b82758ba6
KangSeongHyo <abc9023@naver.com>
2022-05-14 14:21:20 +0900

    refactor : shuffle 대상인 NumberList cacheMap 추가

--- src/main/java/lotto/utils/RandomNumberUtils.java
+++ src/main/java/lotto/utils/RandomNumberUtils.java
@@ -2,19 +2,39 @@ package lotto.utils;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import lotto.constant.ErrorMessage;
 
 public class RandomNumberUtils {
+
+    private static final String UNDER_SCORE = "_";
+    private static final Map<String, List<Integer>> numberListMap = new HashMap<>();
 
     private RandomNumberUtils() {
         throw new IllegalStateException(ErrorMessage.UTILITY_CLASS);
     }
 
     public static List<Integer> generateRandomNumberToList(int low, int max, int count) {
-        List<Integer> numberList = createNumberList(low, max);
+        List<Integer> numberList;
+        numberList = getNumberList(low, max);
         Collections.shuffle(numberList);
         return createRandomNumberList(count, numberList);
+    }
+
+    private static List<Integer> getNumberList(int low, int max) {
+        String cacheKey = generateKey(low, max);
+        if (numberListMap.containsKey(cacheKey)) {
+            return numberListMap.get(cacheKey);
+        }
+        List<Integer>  numberList= createNumberList(low, max);
+        numberListMap.put(cacheKey, numberList);
+        return numberList;
+    }
+
+    private static String generateKey(int low, int max) {
+        return low + UNDER_SCORE + max;
     }
 
     private static List<Integer> createNumberList(int low, int max) {
