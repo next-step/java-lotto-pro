0914b174cc667872b97201eac4a981e2f054a529
minzzang <minjjang1117@gmail.com>
2022-05-14 20:42:40 +0900

    feat: 로또는 6개의 숫자로 이루어져 있다.

---
+++ src/main/java/lotto/Lotto.java
@@ -0,0 +1,22 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lotto {
+
+    public static final int LOTTO_NUMBER_SIZE = 6;
+
+    private List<LottoNumber> lotto = new ArrayList<>();
+
+    public Lotto(List<Integer> lottoNumbers) {
+        if (lottoNumbers.size() != LOTTO_NUMBER_SIZE) {
+            throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
+        }
+        for (Integer number : lottoNumbers) {
+            lotto.add(new LottoNumber(number));
+        }
+    }
+
+
+}
---
+++ src/test/java/lotto/LottoTest.java
@@ -0,0 +1,18 @@
+package lotto;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LottoTest {
+
+    @Test
+    void Integer_list_size가_6이_아닐경우_에러가_발생한다() {
+        // when and then
+        assertThatThrownBy(() -> {
+            Lotto lotto = new Lotto(Arrays.asList(1,2,3,4,5));
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+}
