d9a4871cb9149597ab0f40ead8c135279b1519b6
breezeunn <je7689@gmail.com>
2022-05-15 00:04:52 +0900

    test(ZeroOrPositiveNumber): 음수 입력값에 대한 test 추가

--- src/test/java/calculator/ZeroOrPositiveNumberTest.java
+++ src/test/java/calculator/ZeroOrPositiveNumberTest.java
@@ -7,9 +7,14 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class ZeroOrPositiveNumberTest {
 
     @Test
-    public void 유효하지_않은_숫자하나() {
+    public void 유효하지_않은_입력값_문자() {
         assertThatThrownBy(() -> new ZeroOrPositiveNumber("d"))
                 .isInstanceOf(RuntimeException.class);
     }
 
+    @Test
+    public void 유효하지_않은_입력값_음수() {
+        assertThatThrownBy(() -> new ZeroOrPositiveNumber(-1))
+                .isInstanceOf(RuntimeException.class);
+    }
 }
