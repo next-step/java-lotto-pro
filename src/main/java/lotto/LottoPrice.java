a6c123481a5880b23fd908cd1008ffd7a608d91a
minzzang <minjjang1117@gmail.com>
2022-05-14 22:46:14 +0900

    feat: 구입 가능한 로또 개수를 반환한다

--- src/main/java/lotto/LottoPrice.java
+++ src/main/java/lotto/LottoPrice.java
@@ -4,4 +4,10 @@ public class LottoPrice {
 
     private static final int PRICE = 1_000;
 
+    public static int purchase(int money) {
+        if (money < PRICE) {
+            throw new IllegalArgumentException("로또 1개 가격은 1000원입니다.");
+        }
+        return money / PRICE;
+    }
 }
---
+++ src/test/java/lotto/LottoPriceTest.java
@@ -0,0 +1,29 @@
+package lotto;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoPriceTest {
+
+    @Test
+    void 로또금액을_입력받으면_구매_가능한_로또_개수를_반환한다() {
+        // when
+        int count = LottoPrice.purchase(5000);
+        // then
+        assertThat(count).isEqualTo(5);
+    }
+
+    @ValueSource(ints = {-1000, 0, 999})
+    @ParameterizedTest
+    void 입력받은_금액이_1000원_미만이면_예외가_발생한다(int money) {
+        // when and then
+        assertThatThrownBy(() ->
+            LottoPrice.purchase(money)
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+}
