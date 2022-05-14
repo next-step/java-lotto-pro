48963b4a6e5d74bf10088844c7ecd9529a806897
jhsong2580 <thdwmdgns@naver.com>
2022-05-14 10:43:48 +0900

    refactor: step2코드리뷰건 적용(불필요한 멤버변수 삭제후 함수 인자로 사용)

--- src/main/java/step2/StringAdderCalculator.java
+++ src/main/java/step2/StringAdderCalculator.java
@@ -6,28 +6,20 @@ import step2.utils.StringToStringArrayResolver;
 
 public class StringAdderCalculator {
 
-    private static String[] splitArray = null;
-
     public static int splitAndSum(String source) {
-        init();
-        splitArray = StringToStringArrayResolver.resolve(source);
+        String[] splitArray = StringToStringArrayResolver.resolve(source);
         try {
             StringArrayElementValidator.validateSplitResult(splitArray);
         } catch (RuntimeException e) {
             throw new RuntimeException(e.getMessage());
         }
-        return sumNumbers();
+        return sumNumbers(splitArray);
     }
 
-    private static int sumNumbers() {
+    private static int sumNumbers(String[] splitArray) {
         return Arrays.stream(splitArray)
             .mapToInt(value -> Integer.parseInt(value))
             .sum();
     }
-
-    private static void init() {
-        splitArray = null;
-    }
-
 
 }
