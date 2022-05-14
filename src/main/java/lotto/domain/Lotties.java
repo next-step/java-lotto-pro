5b80c91d4d2c4a5408186e23151f9241d229c17e
seungin yang <rhfpdk92@naver.com>
2022-05-14 09:39:20 +0900

    feat(Lotties): Lotto 일급컬렉션 추가

---
+++ src/main/java/lotto/domain/Lotties.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class Lotties {
+
+    private List<Lotto> lotties;
+
+    public Lotties(List<Lotto> lotties) {
+        validate(lotties);
+        this.lotties = lotties;
+    }
+
+    private void validate(List<Lotto> lotties) {
+        if (lotties.isEmpty()) {
+            throw new IllegalArgumentException("로또는 1개 이상 구매해야 합니다.");
+        }
+    }
+}
---
+++ src/test/java/lotto/domain/LottiesTest.java
@@ -0,0 +1,31 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+class LottiesTest {
+
+    @Test
+    void Lotties_생성() {
+        LottoNumberStrategy strategy = () -> IntStream.rangeClosed(1, 6)
+                .mapToObj(LottoNumber::new)
+                .collect(Collectors.toList());
+
+        Lotties lotties = new Lotties(Arrays.asList(Lotto.create(strategy)));
+
+        assertThat(lotties).isNotNull();
+    }
+
+    @Test
+    void 비어있는_Lotties는_생성불가() {
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> new Lotties(Arrays.asList())
+        ).withMessageContaining("로또는 1개 이상 구매해야 합니다.");
+    }
+}
