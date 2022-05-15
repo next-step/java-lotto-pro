520571c95cd551857a71286a039e05c08fe96bc1
bestsilver <dkvkxm99@gmail.com>
2022-05-15 22:05:35 +0900

    refactor: 설계 변경에 따른 기능 호출 변경

--- src/main/java/lotto_auto/controller/GameController.java
+++ src/main/java/lotto_auto/controller/GameController.java
@@ -1,46 +1,38 @@
 package lotto_auto.controller;
 
-import lotto_auto.exception.IllegalLottoException;
-import lotto_auto.exception.IllegalMoneyException;
+import lotto_auto.dto.Profit;
 import lotto_auto.model.*;
 import lotto_auto.view.Output;
 import lotto_auto.view.UserInputView;
 
-import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
 public class GameController {
-    private final Money money;
+
     private final Output output = new Output();
 
-    public GameController() {
-        this.money = getMoney();
-    }
-
     public void run() {
-        PurchasedLottoCount purchasedLottoCount = new PurchasedLottoCount(money);
-        output.showNotice(Output.purchaseCountGuideFormat, purchasedLottoCount.getCount());
 
-        Lottos lottos = new Lottos();
-        for (int i=0; i < purchasedLottoCount.getCount(); i++) {
-            lottos.appendLotto(purchaseLotto());
-        }
+        Money money = getMoney();
+        Lottos purchasedLottos = new Lottos(money.canBuyLottoCount());
 
-        output.showNotice(lottos.toString());
+        output.showPurchasedLottos(purchasedLottos);
         Lotto winingLotto = getWinningLotto();
 
-        noticeLottoFigures(lottos, winingLotto);
+        Figures figures = new Figures(purchasedLottos, winingLotto);
+        output.showFigures(figures);
+
+        ProfitRate rate = new ProfitRate(money, new Profit(figures.getTotalWinning()));
+        output.showProfitRate(rate);
+
         UserInputView.closeUserInput();
     }
+
 
     private Money getMoney() {
         boolean isValid;
         String userInput;
 
         do {
-            output.showNotice(Output.purchaseStartGuide);
+            output.showMoneyInputNotice();
             userInput = UserInputView.getUserInput();
             isValid = isValidMoney(userInput);
         } while (!isValid);
@@ -52,40 +44,7 @@ public class GameController {
         try {
             new Money(money);
             return true;
-        } catch (IllegalMoneyException e) {
-            output.showError(e);
-            return false;
-        }
-    }
-
-    private Lotto purchaseLotto() {
-        boolean isValid;
-        List<Integer> numberList;
-
-        do {
-            Random generatorNumber = new Random();
-            IntStream numberStream =  generatorNumber.ints(Lotto.validSize, Lotto.minNumber, Lotto.maxNumber);
-            numberList = numberStream.boxed().collect(Collectors.toList());
-            isValid = isValidLotto(numberList);
-        } while (!isValid);
-
-        return new Lotto(numberList);
-    }
-
-    private boolean isValidLotto(List<Integer> list) {
-        try {
-            new Lotto(list);
-            return true;
-        } catch (IllegalLottoException e) {
-            return false;
-        }
-    }
-
-    private boolean isValidLotto(String str) {
-        try {
-            new Lotto(str);
-            return true;
-        } catch (IllegalLottoException e) {
+        } catch (IllegalArgumentException e) {
             output.showError(e);
             return false;
         }
@@ -95,22 +54,22 @@ public class GameController {
         boolean isValid;
         String userinput;
         do {
-            output.showNotice(Output.winningLottoInputGuide);
+            output.showWinningLottoNotice();
             userinput = UserInputView.getUserInput();
-            isValid = isValidLotto(userinput);
+            isValid = isValidLottoNumber(userinput);
         } while (!isValid);
-        return new Lotto(userinput);
+        return new Lotto(new LottoNumbers(userinput));
     }
 
-    private void noticeLottoFigures(Lottos lottos, Lotto winning) {
-        output.showNotice(Output.lottoFiguresTitle);
-        output.showNotice(Output.lottoFiguresDelimiter);
-
-        Figures figures = new Figures(lottos, winning);
-        output.showNotice(figures.toString());
-
-        ProfitRate rate = new ProfitRate(money, figures.getProfit());
-        output.showNotice(Output.profitRateGuideFormat, rate.printRate());
+    private boolean isValidLottoNumber(String lottoString) {
+        try {
+            LottoNumbers numbers = new LottoNumbers(lottoString);
+            new Lotto(numbers);
+            return true;
+        } catch (IllegalArgumentException e) {
+            output.showError(e);
+            return false;
+        }
     }
 
 
