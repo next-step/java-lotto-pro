22116401486973e522fd32187eb739a050d8aa49
tasklet1579 <tasklet1579@gmail.com>
2022-05-15 12:42:34 +0900

    feat(Number) : 하나의 정수만을 포함하는 클래스 - 1 보다 작은 정수가 입력되는 경우 예외 처리한다

---
+++ src/test/java/lotto/vo/NumberTest.java
@@ -0,0 +1,22 @@
+package lotto.vo;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class NumberTest {
+    @ParameterizedTest
+    @ValueSource(ints = {112, 119, 1234567890})
+    void 양의_정수입력(final int number) {
+        assertThatCode(() -> new Number(number)).doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1, -2, -3, -1})
+    void 음의_정수_0_입력(final int number) {
+        assertThatThrownBy(() -> new Number(number))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
