bca8643aacfc81c62b9034864b156f38486ce8e3
limwoobin <drogba02@naver.com>
2022-05-15 15:37:09 +0900

    test: 테스트 코드의 displayname 을 명시하라

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -13,10 +13,10 @@ import org.junit.jupiter.params.provider.CsvSource;
 @DisplayName("로또에 대한 테스트")
 class LottoTest {
 
-    @DisplayName("z")
+    @DisplayName("로또에 당첨번호를 넘기면 당첨 개수를 정상적으로 반환해야 한다")
     @ParameterizedTest
     @CsvSource(value = {"1,2,3,4,5,6:3", "10,2,43,6,19,22:1", "45,4,5,19,23,30:2", "12,10,7,5,4,44:3"}, delimiter = ':')
-    void asd(String 당첨_번호, int 예상_당첨_개수) {
+    void lotto_winning_number_test(String 당첨_번호, int 예상_당첨_개수) {
         List<Integer> 로또_번호 = Arrays.asList(4,5,6,7,8,9);
 
         Lotto lotto = new Lotto(로또_번호);
@@ -26,7 +26,7 @@ class LottoTest {
     }
 
     private List<Integer> convert(String input) {
-        return Stream.of(input.split(",")).mapToInt(Integer::parseInt).boxed()
+        return Stream.of(input.split(WinningNumbers.SPLIT_SYMBOL)).mapToInt(Integer::parseInt).boxed()
             .collect(Collectors.toList());
     }
 }
