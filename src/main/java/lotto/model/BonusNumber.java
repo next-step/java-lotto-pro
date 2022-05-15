964601e288082c0ace6ac788daa683bfab337661
KangSeongHyo <abc9023@naver.com>
2022-05-15 13:15:14 +0900

    feat : 보너스 번호가 1~45 숫자 사이가 아닌 경우 검증 메서드 추가

--- src/main/java/lotto/model/BonusNumber.java
+++ src/main/java/lotto/model/BonusNumber.java
@@ -1,7 +1,21 @@
 package lotto.model;
 
-public class BonusNumber {
-    public BonusNumber(int bonusNumber) {
+import lotto.constant.ErrorMessage;
+import lotto.constant.LottoRoleConst;
 
+public class BonusNumber {
+
+    private final int bounsNumber;
+
+    public BonusNumber(int bonusNumber) {
+        validateLottoNumber(bonusNumber);
+        this.bounsNumber = bonusNumber;
     }
+
+    private static void validateLottoNumber(int bonusNumber) {
+        if (bonusNumber < LottoRoleConst.LOW_NUMBER || bonusNumber > LottoRoleConst.MAX_NUMBER) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_LOTTO_NUMBER);
+        }
+    }
+
 }
