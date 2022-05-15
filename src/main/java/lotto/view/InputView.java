1a28a4288e59a4150dff470844538877175f2036
minzzang <minjjang1117@gmail.com>
2022-05-15 14:54:50 +0900

    feat: 구매 금액을 입력 받는다

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,16 @@
+package lotto.view;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InputView {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static String inputPurchaseAmount() throws IOException {
+        System.out.println("구매금액을 입력해 주세요.");
+        return br.readLine();
+    }
+
+}
