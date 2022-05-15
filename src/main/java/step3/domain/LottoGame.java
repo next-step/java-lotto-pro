b6fb3a450487f678f3988e881946014e4ae8eadc
piopoi <kangsw1988@gmail.com>
2022-05-15 14:11:30 +0900

    feat(LottoGame): 로또 게임 실행 기능/테스트 추가.

--- src/main/java/step3/domain/LottoGame.java
+++ src/main/java/step3/domain/LottoGame.java
@@ -2,27 +2,39 @@ package step3.domain;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import step3.view.InputView;
+import step3.view.OutputView;
 
 public class LottoGame {
     private static final int PRICE_LOTTO = 1000;
 
-    public static int ableToBuyLottoCount(int money) {
+    public void play() {
+        int inputMoney = InputView.inputMoney();
+        int buyCount = buyCount(inputMoney);
+
+        Lottos lottos = new Lottos(buy(buyCount));
+        OutputView.printLottos(lottos);
+
+        List<Integer> winnerNumbers = InputView.inputWinnerNumbers();
+        LottoResult lottoResult = lottos.allMatch(winnerNumbers);
+        lottoResult.calculateYield(investmentAmount(buyCount));
+        OutputView.printResult(lottoResult);
+    }
+
+    public static int buyCount(int money) {
         return money / PRICE_LOTTO;
     }
 
-    public List<Lotto> buy(int ableToBuyLottoCount) {
+    private List<Lotto> buy(int buyCount) {
+        System.out.printf("%d개를 구매했습니다.%n", buyCount);
         List<Lotto> lottos = new ArrayList<>();
-        for (int i = 0; i < ableToBuyLottoCount; i++) {
+        for (int i = 0; i < buyCount; i++) {
             lottos.add(LottoFactory.createAutoLotto());
         }
         return lottos;
     }
 
-    public Lotto createWinnerLotto(List<Integer> winnerNumbers) {
-        return new Lotto(winnerNumbers.stream()
-                .map(LottoNumber::of)
-                .collect(Collectors.toList())
-        );
+    int investmentAmount(int buyCount) {
+        return buyCount * PRICE_LOTTO;
     }
 }
--- src/test/java/step3/domain/LottoGameTest.java
+++ src/test/java/step3/domain/LottoGameTest.java
@@ -2,8 +2,6 @@ package step3.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.Arrays;
-import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -20,18 +18,11 @@ class LottoGameTest {
     @ParameterizedTest(name = "구입가능개수 반환 ({0} : {1})")
     @CsvSource(value = {"1000:1", "5000:5", "100:0"}, delimiter = ':')
     void 구입가능개수_반환(int money, int expected) {
-        assertThat(LottoGame.ableToBuyLottoCount(money)).isEqualTo(expected);
+        assertThat(LottoGame.buyCount(money)).isEqualTo(expected);
     }
 
     @Test
-    void 구입가능개수만큼_로또_생성() {
-        List<Lotto> lottos = lottoGame.buy(5);
-        assertThat(lottos.size()).isEqualTo(5);
-    }
-
-    @Test
-    void 지난주_당첨번호로_로또_생성() {
-        Lotto winnerLotto = lottoGame.createWinnerLotto(Arrays.asList(1, 2, 3, 4, 5, 6));
-        assertThat(winnerLotto).isInstanceOf(Lotto.class);
+    void 투자금액_계산() {
+        assertThat(lottoGame.investmentAmount(5)).isEqualTo(5000);
     }
 }
