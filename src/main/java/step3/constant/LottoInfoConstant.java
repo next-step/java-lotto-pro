d51a185a78b5398d881b00e3486e6d19763137da
jhsong2580 <thdwmdgns@naver.com>
2022-05-15 16:05:20 +0900

    refactor: 숫자 가독성 향샹을 위한 자릿수 표시

--- src/main/java/step3/constant/LottoInfoConstant.java
+++ src/main/java/step3/constant/LottoInfoConstant.java
@@ -8,7 +8,7 @@ public class LottoInfoConstant {
 
     public static String LOTTO_DELIMITER = ",";
     public static int LOTTO_ELEMENTS_SIZE = 6;
-    public static int LOTTO_PRICE = 1000;
+    public static int LOTTO_PRICE = 1_000;
     public static int LOTTO_MIN = 1;
     public static int LOTTO_MAX = 46;
     public static List<Integer> LOTTO_VALID_ELEMENTS = IntStream.rangeClosed(LOTTO_MIN, LOTTO_MAX).boxed().collect(Collectors.toList());
