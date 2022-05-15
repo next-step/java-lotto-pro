575f536588b057e0a8a09a8772201d49749b1743
breezeunn <je7689@gmail.com>
2022-05-15 21:59:30 +0900

    test(LOTTO): 지난주 당첨 번호 입력 기능

--- src/test/java/lotto/LottoGameTest.java
+++ src/test/java/lotto/LottoGameTest.java
@@ -19,4 +19,25 @@ public class LottoGameTest {
         assertEquals(game.getTicketCount(), 9);
     }
 
+    @Test
+    void 지난주_당첨_번호_5개만_입력() {
+        LottoGame game = new LottoGame(14000);
+        assertThatThrownBy(() -> game.setWinnerNumbers("1,2,3,4,5"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 지난주_당첨_번호_공백_입력() {
+        LottoGame game = new LottoGame(14000);
+        assertThatThrownBy(() -> game.setWinnerNumbers("1,2,3,,5,6"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 지난주_당첨_번호_중복_입력() {
+        LottoGame game = new LottoGame(14000);
+        assertThatThrownBy(() -> game.setWinnerNumbers("1,2,3,4,6,6"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 }
