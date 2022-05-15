56cc25816f4ec2c16b5b9036843f0fecd1e044ac
Philip <pch4623@gmail.com>
2022-05-15 20:40:51 +0900

    feat(LottoNumbers): 자동 랜덤 로또 리스트 생성

---
+++ src/main/java/lotto/model/LottoNumbers.java
@@ -0,0 +1,23 @@
+package lotto.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static lotto.constants.LottoConstant.*;
+import static lotto.utils.RandomUtils.createRandomNumbers;
+
+public class LottoNumbers {
+    private final List<LottoNumber> lottoNumbers;
+
+    private LottoNumbers(List<Integer> numbers) {
+        lottoNumbers = new ArrayList<>();
+        for (int number : numbers) {
+            lottoNumbers.add(LottoNumber.of(number));
+        }
+    }
+
+    public static LottoNumbers createLottoNumbers() {
+        List<Integer> randomNumbers = createRandomNumbers(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_PICK_COUNT);
+        return new LottoNumbers(randomNumbers);
+    }
+}
