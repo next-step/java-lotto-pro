1bba446413e19158b9d858b9781df7805aaddea7
KangSeongHyo <abc9023@naver.com>
2022-05-15 11:26:05 +0900

    refactor(test) : convertNumbers 테스트 추가(List를 Array 타입으로 변환)

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
@@ -19,6 +20,13 @@ class LottoTest {
                 () -> assertEquals(LottoRank.SECOND, lotto.matchRank(Arrays.asList(1, 10, 15, 25, 36, 45))),
                 () -> assertEquals(LottoRank.FIRST, lotto.matchRank(Arrays.asList(1, 3, 15, 25, 36, 45)))
         );
+    }
+
+    @DisplayName("로또번호 List를 Array 타입으로 변환")
+    @Test
+    void convertNumbers() {
+        Lotto lotto = new Lotto(Arrays.asList(1, 3, 15, 25, 36, 45));
+        assertThat(lotto.convertNumbers()).isEqualTo(new int[]{1,3,15,25,36,45});
     }
 
 }
