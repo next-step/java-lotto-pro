de49b3968d71843fe0ccdf3d4526af8b9f17fd30
tasklet1579 <tasklet1579@gmail.com>
2022-05-15 15:09:14 +0900

    refactor(Money) : 오버라이드 메서드 추가 - equals - hashCode - toString

--- src/main/java/lotto/vo/Money.java
+++ src/main/java/lotto/vo/Money.java
@@ -1,5 +1,7 @@
 package lotto.vo;
 
+import java.util.Objects;
+
 public class Money {
     private final static String NEGATIVE_AMOUNTS_OR_ZERO_CANNOT_BE_ENTERED = "1원 보다 작은 금액은 입력할 수 없습니다.";
 
@@ -14,5 +16,25 @@ public class Money {
 
     public int value() {
         return money;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money1 = (Money) o;
+        return money == money1.money;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(money);
+    }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+                "money=" + money +
+                '}';
     }
 }
