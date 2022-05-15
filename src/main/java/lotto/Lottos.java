1a384a9a5570763115c8bd45d3cda523df15f70a
rockpro87 <rockpro87@naver.com>
2022-05-16 00:19:13 +0900

    feat: 로또 개수 만큼 저장하는 콜렉션 추가

---
+++ src/main/java/lotto/Lottos.java
@@ -0,0 +1,24 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lottos {
+    private final List<Lotto> lottos;
+
+    public Lottos(int count) {
+        lottos = createLotto(count);
+    }
+
+    public List<Lotto> allGames () {
+        return lottos;
+    }
+
+    private List<Lotto> createLotto(int count) {
+        List<Lotto> results = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            results.add(new Lotto());
+        }
+        return results;
+    }
+}
