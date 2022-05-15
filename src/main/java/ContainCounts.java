e581b251cfd9b07127b8019e0b9569270d92e120
chj1768 <chj1768@gmail.com>
2022-05-15 19:53:41 +0900

    refactor: 불필요 equals, hashcode 메서드 오버라이딩 제거

--- src/main/java/ContainCounts.java
+++ src/main/java/ContainCounts.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Objects;
 
 public class ContainCounts implements Iterable<ContainCount> {
     private final List<ContainCount> containCounts;
@@ -16,21 +15,6 @@ public class ContainCounts implements Iterable<ContainCount> {
 
     public int size() {
         return this.containCounts.size();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (!(o instanceof ContainCounts))
-            return false;
-        ContainCounts that = (ContainCounts) o;
-        return containCounts.containsAll(that.containCounts);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(containCounts);
     }
 
     @Override
