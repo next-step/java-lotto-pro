28293bde9854e19a1d59583bb3839315838f9f94
limwoobin <drogba02@naver.com>
2022-05-15 20:09:55 +0900

    feat: 로또게임을 진행하는 비지니스 로직을 작성하라

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,27 @@
+package lotto.controller;
+
+import lotto.domain.LottoScore;
+import lotto.domain.LottoShop;
+import lotto.domain.Lottos;
+import lotto.domain.ProfitRate;
+import lotto.domain.WinningNumbers;
+import lotto.domain.Winnings;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoController {
+    public static void play() {
+        int purchaseAmount = InputView.inputPurchaseAmount();
+        Lottos lottos = LottoShop.sale(InputView.inputPurchaseAmount());
+        OutputView.printPurchaseCount(lottos.getLottos().size());
+        OutputView.printLottoNumbers(lottos.getLottos());
+
+        WinningNumbers winningNumbers = new WinningNumbers(InputView.inputWinningNumbers());
+        LottoScore lottoScore = lottos.calculateLottoScore(winningNumbers);
+        OutputView.printLottoStats(lottoScore.getLottoScoreMap());
+
+        Winnings winnings = lottoScore.getWinnings();
+        ProfitRate profitRate = new ProfitRate(winnings, purchaseAmount);
+        OutputView.printProfitRate(profitRate.getRate());
+    }
+}
