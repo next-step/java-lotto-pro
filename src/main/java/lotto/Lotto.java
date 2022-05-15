bb0346ac9f1bb38b1465b9306275bb962c60a7c6
rockpro87 <rockpro87@naver.com>
2022-05-16 00:09:54 +0900

    feat: 로또 게임 도에인 개발

---
+++ src/main/java/lotto/Lotto.java
@@ -0,0 +1,43 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Lotto {
+    private static final int MAX_NUMBER = 45;
+    private static final int START_INDEX = 0;
+    private static final int END_INDEX = 6;
+    private static final String PRINT_FORM = "%s";
+    private static final String PRINT_DELIMITER = ", ";
+
+    private final List<Integer> numbers;
+
+    public Lotto() {
+        numbers = prepareNumbers();
+    }
+
+    public Lotto(Integer... customNumbers) {
+        numbers = Arrays.asList(customNumbers);
+    }
+
+    public List<Integer> seeNumbers() {
+        return this.numbers;
+    }
+
+    public String printText() {
+        return String.format(PRINT_FORM, String.join(PRINT_DELIMITER, this.numbers.toString()));
+    }
+
+    private List<Integer> prepareNumbers() {
+        List<Integer> results = new ArrayList<>();
+        for (int i = 1; i <= MAX_NUMBER; i++) {
+            results.add(i);
+        }
+        Collections.shuffle(results);
+        results = results.subList(START_INDEX, END_INDEX);
+        Collections.sort(results);
+        return results;
+    }
+}
