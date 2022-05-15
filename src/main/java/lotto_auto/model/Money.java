095388b8752c46e68a55d545911c5c9e2436e335
bestsilver <dkvkxm99@gmail.com>
2022-05-15 22:04:42 +0900

    refactor: 머니 최소 사이즈 예외 추가, lotto price 해당 위치로 옮기기, 살수있는 로또 개수 구하기 기능 추가

--- src/main/java/lotto_auto/model/Money.java
+++ src/main/java/lotto_auto/model/Money.java
@@ -1,20 +1,36 @@
 package lotto_auto.model;
 
-import lotto_auto.exception.IllegalMoneyException;
-
 public class Money {
     int money;
-    public static final String NOT_NUMBER_MSG = "[ERROR] 구입금액은 숫자 이외의 문자가 올 수 없습니다.";
+    public static final int LOTTO_PRICE = 1_000;
+    public static final int MIN_SIZE = 0;
+    public static final String NOT_NUMBER = "[ERROR] 구입 금액은 숫자 이외의 문자가 올 수 없습니다.";
+    public static final String LESS_THAN_MIN_SIZE = "[ERROR] 구입금액은 0보다 큰수이여야 합니다.";
 
-    public Money(String value) throws IllegalMoneyException {
+    public Money(String value) {
         try {
             this.money = Integer.parseInt(value);
+            checkMoneyMinSize();
         } catch (NumberFormatException e) {
-            throw IllegalMoneyException.NOT_NUMBER_ERROR;
+            throw new IllegalArgumentException(NOT_NUMBER);
+        }
+    }
+
+    private void checkMoneyMinSize() {
+        if (this.money < MIN_SIZE) {
+            throw new IllegalArgumentException(LESS_THAN_MIN_SIZE);
         }
     }
 
     public int getMoney() {
         return money;
     }
+
+    public int canBuyLottoCount() {
+        if (money < LOTTO_PRICE) {
+            return 0;
+        }
+        return money/LOTTO_PRICE;
+    }
+
 }
--- src/test/java/lotto_auto/model/MoneyTest.java
+++ src/test/java/lotto_auto/model/MoneyTest.java
@@ -1,6 +1,5 @@
 package lotto_auto.model;
 
-import lotto_auto.exception.IllegalMoneyException;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
@@ -8,9 +7,16 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 public class MoneyTest {
     @Test
     public void 머니_숫자_이외의_값_테스트() {
-        assertThatExceptionOfType(IllegalMoneyException.class)
+        assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> new Money("abc"))
-                .withMessage(Money.NOT_NUMBER_MSG);
+                .withMessage(Money.NOT_NUMBER);
+    }
+
+    @Test
+    public void 머니_음수_테스트() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> new Money("-1"))
+                .withMessage(Money.LESS_THAN_MIN_SIZE);
     }
 
 
