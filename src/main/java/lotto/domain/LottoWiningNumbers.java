654f9df290c6972ab7db404f1a1d66ff25fc9a97
14km <k.connor614@gmail.com>
2022-05-15 14:40:42 +0900

    feat: LottoWiningNumbers 구현

---
+++ src/main/java/lotto/domain/LottoWiningNumbers.java
@@ -0,0 +1,45 @@
+package lotto.domain;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static lotto.common.Messages.LOTTO_NUMBERS_SIZE;
+import static lotto.common.Messages.LOTTO_WINING_SEPARATOR;
+
+public class LottoWiningNumbers {
+    private static final int SIZE = 6;
+    private static final String SEPARATOR = ",";
+
+    private final String[] winingNumbers;
+
+    public LottoWiningNumbers(String winningNumberString) {
+        validateSeparator(winningNumberString);
+        validateWiningNumbers(winningNumberString);
+
+        this.winingNumbers = splitWiningNumber(winningNumberString);
+    }
+
+    public LottoNumbers generate() {
+        return new LottoNumbers(Arrays.stream(winingNumbers)
+                .mapToInt(Integer::parseInt)
+                .mapToObj(LottoNumber::new)
+                .collect(Collectors.toList())
+        );
+    }
+
+    private void validateWiningNumbers(String text) {
+        if (splitWiningNumber(text).length < SIZE) {
+            throw new IllegalArgumentException(LOTTO_NUMBERS_SIZE);
+        }
+    }
+
+    private void validateSeparator(String text) {
+        if (!text.contains(SEPARATOR)) {
+            throw new IllegalArgumentException(LOTTO_WINING_SEPARATOR);
+        }
+    }
+
+    private String[] splitWiningNumber(String winningNumberString) {
+        return winningNumberString.split(SEPARATOR);
+    }
+}
