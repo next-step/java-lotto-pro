3c4759849b96585cbd3b9ea66204356d00ecf90a
Donghee Han <handh0413@gmail.com>
2022-05-15 19:01:03 +0900

    test: 테스트 수정
    - CsvSource를 이용한 테스트 재작성
    - 하드 코딩된 결과로 테스트 검증하는 것이 아닌 프로그램 처리 결과로 테스트 검증

--- src/test/java/lotto/domain/LottoResultTest.java
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -1,31 +1,40 @@
 package lotto.domain;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static lotto.domain.LottoConstant.LOTTO_MINIMUM_MATCHING_COUNT;
+import static lotto.domain.LottoConstant.LOTTO_SIZE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoResultTest {
 
-    @Test
-    public void 로또_결과_조회_테스트() {
+    @ParameterizedTest
+    @CsvSource(value = {"6|1", "5|0", "4|0", "3|2"}, delimiter = '|')
+    public void 로또_결과_조회_테스트(int matchingCount, int listSize) {
         PurchasedLotto purchasedLotto = createSamplePurchasedLotto();
         LottoResult result = new LottoResult(purchasedLotto.compareLottos(new Lotto(1, 2, 3, 4, 5, 6)));
-        List<Ranking> firstRankings = result.findRankings(6);
-        assertThat(firstRankings).hasSize(1);
-
-        List<Ranking> fourthRankings = result.findRankings(3);
-        assertThat(fourthRankings).hasSize(2);
+        List<Ranking> firstRankings = result.findRankings(matchingCount);
+        assertThat(firstRankings).hasSize(listSize);
     }
 
     @Test
     public void 총_당첨_금액_계산() {
         PurchasedLotto purchasedLotto = createSamplePurchasedLotto();
         LottoResult result = new LottoResult(purchasedLotto.compareLottos(new Lotto(1, 2, 3, 4, 5, 6)));
-        int winningMoney = result.calculateWinningMoney();
-        assertThat(winningMoney).isEqualTo(2000010000);
+        int actualWinningMoney = result.calculateWinningMoney();
+
+        int expectedWinningMoney = 0;
+        for (int matchingCount = LOTTO_MINIMUM_MATCHING_COUNT; matchingCount <= LOTTO_SIZE; matchingCount++) {
+            List<Ranking> list = result.findRankings(matchingCount);
+            Ranking ranking = Ranking.findRank(matchingCount);
+            expectedWinningMoney += ranking.getReward() * list.size();
+        }
+        assertThat(actualWinningMoney).isEqualTo(expectedWinningMoney);
     }
 
     private PurchasedLotto createSamplePurchasedLotto() {
