149e23948ec3a43cf38c8b2c880cb9db483ffa52
Philip <pch4623@gmail.com>
2022-05-15 23:13:22 +0900

    refactor(ResultViewTest): 메서드명 수정

--- src/test/java/lotto/view/ResultViewTest.java
+++ src/test/java/lotto/view/ResultViewTest.java
@@ -4,16 +4,16 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static lotto.view.ResultView.printPurchaseCountOutput;
+import static lotto.view.ResultView.printPurchaseCountView;
 import static org.assertj.core.api.Assertions.assertThat;
 
-class ResultViewTest extends ViewTest{
+class ResultViewTest extends ViewTest {
 
     @ParameterizedTest
     @DisplayName("입력값을 활용해 구매개수 확인 메시지를 출력한다.")
     @ValueSource(ints = {10, 20, 30, 0})
-    void printPurchaseCountOutput_구매개수(int input) {
-        printPurchaseCountOutput(input);
+    void printPurchaseCountView_구매개수(int input) {
+        printPurchaseCountView(input);
         assertThat(output())
                 .isEqualTo(String.format("%d개를 구매했습니다.", input));
     }
