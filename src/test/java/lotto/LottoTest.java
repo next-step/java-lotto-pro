eb9cbeaa27f929b560bec15ae04651a05b63c5ad
jiyeonghwang <gyp0288@naver.com>
2022-05-15 14:12:42 +0900

    test: Lotto 당첨 번호 입력 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,19 +1,57 @@
 package lotto;
 
 import lotto.domain.Lotto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class LottoTest {
+    private Lotto stringToLotto;
+
+    @BeforeEach
+    void setUp() {
+        String input = "1, 2, 3, 4, 5, 6";
+        Lotto lotto = new Lotto(input);
+        stringToLotto = lotto;
+    }
     @Test
-    public void 로또_생성() {
+    public void 로또_생성_자동() {
         List<Integer> list = new ArrayList<>();
         for (int i=1; i<=45; i++) {
             list.add(i);
         }
         Lotto lotto = new Lotto(list);
         lotto.printLottoNumber();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6})
+    public void 로또_생성_입력(int input) {
+        assertTrue(stringToLotto.getLottoNumber().contains(input));
+    }
+
+    @Test
+    public void 로또_생성_0보다_작은_수() {
+        assertThatThrownBy(() -> new Lotto("-1, 2, 3, 4, 5, 6"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 로또_생성_45보다_큰_수() {
+        assertThatThrownBy(() -> new Lotto("46, 2, 3, 4, 5, 6"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 로또_생성_중복_수() {
+        assertThatThrownBy(() -> new Lotto("1, 1, 3, 4, 5, 6"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
