dca595da7032582cb9c7e7aacc72c3d9fef92be4
bestsilver <dkvkxm99@gmail.com>
2022-05-15 22:04:55 +0900

    refactor: notice 기능 분리

--- src/main/java/lotto_auto/view/Output.java
+++ src/main/java/lotto_auto/view/Output.java
@@ -1,23 +1,45 @@
 package lotto_auto.view;
 
+import lotto_auto.model.*;
+
+import java.util.stream.Collectors;
+
 public class Output {
-    public static final String purchaseStartGuide = "구입금액을 입력해 주세요.";
-    public static final String purchaseCountGuideFormat = "%d개를 구매했습니다.\n";
-    public static final String winningLottoInputGuide = "지난 주 당첨 번호를 입력해 주세요.";
-    public static final String lottoFiguresTitle = "\n당첨 총계";
-    public static final String lottoFiguresDelimiter = "----------";
-    public static final String profitRateGuideFormat = "총 수익률은 %s입니다.\n";
+    public static final String MONEY_INPUT_NOTICE = "구입금액을 입력해 주세요.";
+    public static final String PURCHASE_LOTTO_COUNT_FORMAT = "%d개를 구매했습니다.\n";
+    public static final String WINNING_LOTTO_INPUT_NOTICE = "지난 주 당첨 번호를 입력해 주세요.";
+    public static final String PROFIT_RATE_NOTICE_FORMAT = "총 수익률은 %s입니다.";
 
-    public void showNotice(String str) {
-        System.out.println(str);
+    public void showMoneyInputNotice() {
+        System.out.println(MONEY_INPUT_NOTICE);
     }
 
-    public void showNotice(String strFormat, int value) {
-        System.out.format(strFormat, value);
+    public void showProfitRate(ProfitRate profitRate) {
+        System.out.format(PROFIT_RATE_NOTICE_FORMAT, profitRate.printRate());
     }
 
-    public void showNotice(String strFormat, String value) {
-        System.out.format(strFormat, value);
+    public void showPurchaseLottoCountNotice(int count) {
+        System.out.format(PURCHASE_LOTTO_COUNT_FORMAT, count);
+    }
+
+    public void showPurchasedLottos(Lottos purchasedLottos) {
+        String result = purchasedLottos.getLottoList()
+                .stream()
+                .map(Lotto::printLotto)
+                .collect(Collectors.joining("\n"));
+
+        System.out.println(result);
+    }
+
+    public void showWinningLottoNotice() {
+        System.out.println(WINNING_LOTTO_INPUT_NOTICE);
+    }
+
+    public void showFigures(Figures figures) {
+        String figuresTitle = "당첨 통계\n---------";
+        System.out.println(figuresTitle);
+
+        System.out.println(figures.printFigures());
     }
 
     public void showError(Throwable e) {
