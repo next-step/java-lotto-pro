65a71582bb8c4fd210430e206893ddf50bcfe9a7
jhsong2580 <thdwmdgns@naver.com>
2022-05-15 14:59:00 +0900

    test: 공백처리를 view단에서 진행하므로 element test에서 제거

--- src/test/java/step3/domain/LottoElementTest.java
+++ src/test/java/step3/domain/LottoElementTest.java
@@ -16,13 +16,4 @@ public class LottoElementTest {
             () -> assertThatThrownBy(() -> new LottoElement("0")).isInstanceOf(IllegalArgumentException.class)
         );
     }
-
-    @Test
-    public void createLottoElementWithSpace() {
-        LottoElement expected = new LottoElement("2");
-        assertThat(new LottoElement(" 2")).isEqualTo(expected);
-        assertThat(new LottoElement(" 2 ")).isEqualTo(expected);
-        assertThat(new LottoElement(" 2       ")).isEqualTo(expected);
-        assertThat(new LottoElement("         2       ")).isEqualTo(expected);
-    }
 }
