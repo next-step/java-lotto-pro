78667de9e002ee13144d7515d7c0bd4848c8ef93
bestsilver <dkvkxm99@gmail.com>
2022-05-15 21:38:14 +0900

    refactor: 필요없는 에러 삭제

--- src/main/java/lotto_auto/exception/IllegalMoneyException.java
+++
@@ -1,8 +0,0 @@
-package lotto_auto.exception;
-
-import lotto_auto.model.Money;
-
-public class IllegalMoneyException extends IllegalArgumentException{
-    public static final IllegalMoneyException NOT_NUMBER_ERROR = new IllegalMoneyException(Money.NOT_NUMBER_MSG);
-    public IllegalMoneyException(final String errorMessage) { super(errorMessage);}
-}
