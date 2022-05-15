c3bfdc1b3b435d1c4eebd89334a59c704bdc24a9
tasklet1579 <tasklet1579@gmail.com>
2022-05-15 15:09:28 +0900

    refactor(Coupon) : 오버라이드 메서드 추가 - equals - hashCode - toString

--- src/main/java/lotto/vo/Coupon.java
+++ src/main/java/lotto/vo/Coupon.java
@@ -1,5 +1,7 @@
 package lotto.vo;
 
+import java.util.Objects;
+
 public class Coupon {
     private static final int LOTTERY_PURCHASE_UNIT = 1000;
     private static final String LOTTERY_MINIMUM_PURCHASE_AMOUNT_IS_1000_WON = "로또 구입 금액은 최소 1,000원 입니다.";
@@ -15,5 +17,25 @@ public class Coupon {
             throw new IllegalArgumentException(LOTTERY_PURCHASE_AMOUNT_IS_IN_UNITS_OF_1000_WON);
         }
         this.coupons = money.value() / LOTTERY_PURCHASE_UNIT;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coupon coupon = (Coupon) o;
+        return coupons == coupon.coupons;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(coupons);
+    }
+
+    @Override
+    public String toString() {
+        return "Coupon{" +
+                "coupons=" + coupons +
+                '}';
     }
 }
