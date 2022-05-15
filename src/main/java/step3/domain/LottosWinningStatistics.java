1e64df41e89cb2faf3e6561b109e9f91b970c283
kms <kmmin78@gmail.com>
2022-05-15 20:28:17 +0900

    refactor : step3 LottosWinningStatistics의 상수를 enum으로 정의

--- src/main/java/step3/domain/LottosWinningStatistics.java
+++ src/main/java/step3/domain/LottosWinningStatistics.java
@@ -2,18 +2,9 @@ package step3.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import step3.constants.Matched;
 
 public class LottosWinningStatistics {
-    private static final int THREE_MATCHED_COUNT = 3;
-    private static final int FOUR_MATCHED_COUNT = 4;
-    private static final int FIVE_MATCHED_COUNT = 5;
-    private static final int SIX_MATCHED_COUNT = 6;
-    private static final int NOT_MATCHED_REWARD = 0;
-    private static final int THREE_MATCHED_REWARD = 5_000;
-    private static final int FOUR_MATCHED_REWARD = 50_000;
-    private static final int FIVE_MATCHED_REWARD = 1_500_000;
-    private static final int SIX_MATCHED_REWARD = 2_000_000_000;
-
     private final List<Integer> matches;
     private final int price;
     private final int totalReward;
@@ -35,33 +26,22 @@ public class LottosWinningStatistics {
     }
 
     private int calculateReward(final Integer match) {
-        if(match == THREE_MATCHED_COUNT){
-            return THREE_MATCHED_REWARD;
-        }
-        if(match == FOUR_MATCHED_COUNT){
-            return FOUR_MATCHED_REWARD;
-        }
-        if(match == FIVE_MATCHED_COUNT){
-            return FIVE_MATCHED_REWARD;
-        }
-        if(match == SIX_MATCHED_COUNT){
-            return SIX_MATCHED_REWARD;
-        }
-        return NOT_MATCHED_REWARD;
+        return Matched.getByCount(match)
+                .getReward();
     }
 
     public String[] toStringArray() {
         final List<String> stringList = new ArrayList<>();
-        final int threeMatchedCount = getMatchedCount(THREE_MATCHED_COUNT);
-        final int fourMatchedCount = getMatchedCount(FOUR_MATCHED_COUNT);
-        final int fiveMatchedCount = getMatchedCount(FIVE_MATCHED_COUNT);
-        final int sixMatchedCount = getMatchedCount(SIX_MATCHED_COUNT);
+        final int threeMatchedCount = getMatchedCount(Matched.THREE_MATCHED.getCount());
+        final int fourMatchedCount = getMatchedCount(Matched.FOUR_MATCHED.getCount());
+        final int fiveMatchedCount = getMatchedCount(Matched.FIVE_MATCHED.getCount());
+        final int sixMatchedCount = getMatchedCount(Matched.SIX_MATCHED.getCount());
 
         stringList.add("3개 일치 (5000원)- " + threeMatchedCount + "개");
         stringList.add("4개 일치 (50000원)- " + fourMatchedCount + "개");
         stringList.add("5개 일치 (1500000원)- " + fiveMatchedCount + "개");
         stringList.add("6개 일치 (2000000000원)- " + sixMatchedCount + "개");
-        stringList.add("총 수익률은 " +  String.format("%.2f", yield) + "입니다.");
+        stringList.add("총 수익률은 " + String.format("%.2f", yield) + "입니다.");
 
         return stringList.toArray(new String[0]);
     }
