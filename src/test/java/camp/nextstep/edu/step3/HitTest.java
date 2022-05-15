83ccbc270a9b3aa37d9c22cc55aecaf0c7608b55
mannue <sppe12@gmail.com>
2022-05-15 16:32:26 +0900

    test(Hit Test): 5개 맞고 보너스 맞는 케이스 추가

--- src/test/java/camp/nextstep/edu/step3/HitTest.java
+++ src/test/java/camp/nextstep/edu/step3/HitTest.java
@@ -23,15 +23,17 @@ public class HitTest {
 
     @DisplayName("일치한 갯수 값은 0 과 6 사이 값을 가진다.")
     @ParameterizedTest
-    @MethodSource("provideHitCountAndExpectedHit")
-    void valueOfTest(final int hitCount, Hit expectedHit) {
-        assertThat(Hit.valueOf(hitCount, false)).isEqualTo(expectedHit);
+    @MethodSource("provideHitCountAndHitBonusAndExpectedHit")
+    void valueOfTest(final int hitCount, final boolean isHitBonus, Hit expectedHit) {
+        assertThat(Hit.valueOf(hitCount, isHitBonus)).isEqualTo(expectedHit);
     }
 
-    private static Stream<Arguments> provideHitCountAndExpectedHit() {
+    private static Stream<Arguments> provideHitCountAndHitBonusAndExpectedHit() {
         return Stream.of(
-                Arguments.of(0, ZERO),
-                Arguments.of(6, ALL)
+                Arguments.of(0,true,ZERO),
+                Arguments.of(5,false,FIVE),
+                Arguments.of(5,true,FIVE_BONUS),
+                Arguments.of(6,true,ALL)
         );
     }
 
