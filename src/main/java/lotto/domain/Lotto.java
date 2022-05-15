2d8e7269cd26bc7164d5d6a59bb696918b8d2c04
jiyeonghwang <gyp0288@naver.com>
2022-05-15 14:12:20 +0900

    feat: 당첨 번호 입력 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,6 +2,8 @@ package lotto.domain;
 
 import java.util.*;
 
+import static stringAddCalculator.utils.Parse.INPUT_ERROR;
+
 public class Lotto {
     private final List<Integer> lottoNumber;
 
@@ -15,7 +17,52 @@ public class Lotto {
         this.lottoNumber = list;
     }
 
+    public Lotto(String input) {
+        String[] inputArr = input.replace(" ", "").split(",");
+        vaildCount(inputArr);
+        List<Integer> list = new ArrayList<>();
+        for (String s : inputArr) {
+            list.add(validNumber(s));
+        }
+        Collections.sort(list);
+        this.lottoNumber = list;
+    }
+
+    public List<Integer> getLottoNumber() {
+        return this.lottoNumber;
+    }
+
     public void printLottoNumber() {
         System.out.println(lottoNumber.toString());
+    }
+
+    private void vaildCount(String[] input) {
+        Set<String> set = new HashSet<>();
+        for (String s : input) {
+            set.add(s);
+        }
+        if (set.size() != 6) {
+            throw new IllegalArgumentException(INPUT_ERROR);
+        }
+    }
+
+    private int validNumber(String input) {
+        try {
+            int num = Integer.parseInt(input);
+            validRangeLottoNumber(num);
+            return num;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INPUT_ERROR);
+        }
+    }
+
+    private void validRangeLottoNumber(int num) {
+        if (num < 1) {
+            throw new IllegalArgumentException(INPUT_ERROR);
+        }
+
+        if (num > 45) {
+            throw new IllegalArgumentException(INPUT_ERROR);
+        }
     }
 }
