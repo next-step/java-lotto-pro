a945340553dd80f5633e40118a9c590b742a73ee
Donghee Han <handh0413@gmail.com>
2022-05-15 18:33:45 +0900

    refactor: 테스트 데이터를 생성하는 메서드를 이용한 테스트 코드 안정화

--- src/test/java/lotto/domain/LottoResultTest.java
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -1,6 +1,5 @@
 package lotto.domain;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -9,19 +8,10 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoResultTest {
-    private PurchasedLottos purchasedLottos;
-
-    @BeforeEach
-    public void beforeEach() {
-        List<Lotto> lottoList = Arrays.asList(
-                new Lotto(1, 2, 3, 4, 5, 6),
-                new Lotto(1, 3, 5, 7, 9, 11),
-                new Lotto(2, 4, 6, 8, 10, 12));
-        purchasedLottos = new PurchasedLottos(lottoList);
-    }
 
     @Test
     public void 로또_결과_조회_테스트() {
+        PurchasedLottos purchasedLottos = createSamplePurchasedLotto();
         LottoResult result = new LottoResult(purchasedLottos.compareLottos(new Lotto(1, 2, 3, 4, 5, 6)));
         List<Ranking> firstRankings = result.findRankings(6);
         assertThat(firstRankings).hasSize(1);
@@ -32,8 +22,17 @@ class LottoResultTest {
 
     @Test
     public void 총_당첨_금액_계산() {
+        PurchasedLottos purchasedLottos = createSamplePurchasedLotto();
         LottoResult result = new LottoResult(purchasedLottos.compareLottos(new Lotto(1, 2, 3, 4, 5, 6)));
         int winningMoney = result.calculateWinningMoney();
         assertThat(winningMoney).isEqualTo(2000010000);
+    }
+
+    private PurchasedLottos createSamplePurchasedLotto() {
+        List<Lotto> lottoList = Arrays.asList(
+                new Lotto(1, 2, 3, 4, 5, 6),
+                new Lotto(1, 3, 5, 7, 9, 11),
+                new Lotto(2, 4, 6, 8, 10, 12));
+        return new PurchasedLottos(lottoList);
     }
 }
