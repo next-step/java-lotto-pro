e23f66b6294e3da175c1c6b7899f5ba172b26ead
chj1768 <chj1768@gmail.com>
2022-05-15 18:45:37 +0900

    refactor: Lottery 에게 집계 역할 제외

--- src/main/java/Lottery.java
+++ src/main/java/Lottery.java
@@ -1,6 +1,3 @@
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 public class Lottery {
@@ -10,24 +7,19 @@ public class Lottery {
 
     private final LottoNumbers winner;
 
-    public Map<Prize, Long> get(Lottos lottos) {
+    public ContainCounts get(Lottos lottos) {
         ContainCounts containCounts = new ContainCounts();
-        StreamSupport.stream(lottos.spliterator(), false)
-                .forEach(lottoNumbers -> containCounts.add(get(lottoNumbers)));
-        return aggregateByPrize(containCounts);
+        for (LottoNumbers lottoNumbers : lottos) {
+            containCounts.add(get(lottoNumbers));
+        }
+        return containCounts;
     }
 
     private ContainCount get(LottoNumbers lottoNumbers) {
         return new ContainCount(Math.toIntExact(
                 StreamSupport.stream(lottoNumbers.spliterator(), false)
                         .filter(winner::contains)
-                        .count()));
-    }
-
-    private Map<Prize, Long> aggregateByPrize(ContainCounts containCounts) {
-        return StreamSupport.stream(containCounts.spliterator(), false).collect(
-                Collectors.groupingBy(ContainCount::find,
-                        TreeMap::new,
-                        Collectors.counting()));
+                        .count()
+        ));
     }
 }
