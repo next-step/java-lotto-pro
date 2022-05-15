32f344b3136668851ae7e59334be2bffe628a207
vvsungho <vvsungho@gmail.com>
2022-05-15 12:24:59 +0900

    test: 로또수익률 확인 테스트 케이스 작성

---
+++ src/main/java/lotto/auto/LottoWinning.java
@@ -0,0 +1,27 @@
+package lotto.auto;
+
+import java.util.Objects;
+
+public class LottoWinning {
+    private double profit;
+
+    public LottoWinning() {
+    }
+
+    public LottoWinning(double profit) {
+        this.profit = profit;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoWinning that = (LottoWinning) o;
+        return Double.compare(that.profit, profit) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(profit);
+    }
+}
---
+++ src/test/java/lotto/auto/LottoWinningTest.java
@@ -0,0 +1,15 @@
+package lotto.auto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoWinningTest {
+    @Test
+    @DisplayName("로또 수익률을 확인한다.")
+    void 테스트_로또_수익률_확인() {
+        double result = 0.5;
+        assertThat(new LottoWinning(result)).isEqualTo(new LottoWinning(0.5));
+    }
+}
