442fa2729750a7917994c8acea6f3609531835da
choi-ys <project.log.062@gmail.com>
2022-05-15 22:02:00 +0900

    refactor: 명확하지 않은 TC 명 수정
    로또 번호 객체 생성 검증 TC명 수정
    - AS-IS : lottoNumberTest
    - TO-BE : createLottoNumberTest
    
    로또 번호 객체 생성 시 예외 검증 TC명 수정
    - AS-IS : lottoNumberFailTest
    - TO-BE : throwException_WhenGivenNumberIsInvalidRange

--- src/test/java/step3/lotto/domain/LottoNumberTest.java
+++ src/test/java/step3/lotto/domain/LottoNumberTest.java
@@ -18,7 +18,7 @@ class LottoNumberTest {
     @ParameterizedTest
     @ValueSource(ints = {1, 45})
     @DisplayName("1~45 범위를 보장하는 로또 번호 객체 생성")
-    public void lottoNumberTest(final int given) {
+    public void createLottoNumberTest(final int given) {
         // When & Then
         assertThat(LottoNumber.of(given).getLottoNumber()).isEqualTo(given);
     }
@@ -26,7 +26,7 @@ class LottoNumberTest {
     @ParameterizedTest
     @ValueSource(ints = {0, 46})
     @DisplayName("1~45 범위를 벗어나는 로또 번호 객체 생성 시 예외")
-    public void lottoNumberFailTest(final int given) {
+    public void throwException_WhenGivenNumberIsInvalidRange(final int given) {
         // When & Then
         assertThatExceptionOfType(IllegalArgumentException.class)
             .isThrownBy(() -> LottoNumber.of(given))
