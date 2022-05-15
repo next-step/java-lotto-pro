db99e064465f24628f563eb3a7a34f545ed36400
Celina <luv.mins99@gmail.com>
2022-05-16 03:30:46 +0900

    feat(InputView): 사용자 입력 기능 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,20 @@
+package lotto.view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private final Scanner scanner = new Scanner(System.in);
+    private static final String INSERT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String WINNER_NUMBER_INPUT_MESSAGE = "\n지난 주 당첨 번호를 입력해 주세요.";
+
+    public String insertMoney() {
+        System.out.println(INSERT_MONEY_MESSAGE);
+        return scanner.nextLine();
+    }
+
+    public String inputWinnerNumbers() {
+        System.out.println(WINNER_NUMBER_INPUT_MESSAGE);
+        return scanner.nextLine();
+    }
+}
