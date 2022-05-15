d7f08ff321bd895adeed298c280450daaa56dab8
Yul Jeon <notkhaki@gmail.com>
2022-05-15 19:32:53 +0900

    feat: add views

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,28 @@
+package lotto.view;
+
+import lotto.LottoNumber;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class InputView {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static int takeMoney() {
+        System.out.println("구입 금액을 입력해 주세요.");
+
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    public static List<LottoNumber> readWinningNumbers() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+
+        String input = scanner.nextLine();
+        return Arrays.stream(input.split(","))
+                .map(s -> new LottoNumber(Integer.parseInt(s.trim())))
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/lotto/view/ResultView.java
@@ -0,0 +1,23 @@
+package lotto.view;
+
+import lotto.LottoTicket;
+import lotto.dto.LottoResultItem;
+import lotto.dto.LottoResult;
+
+public class ResultView {
+
+    public static void printTicket(LottoTicket lottoTicket) {
+        System.out.println(lottoTicket.size() + "개를 구매했습니다.");
+        System.out.println(lottoTicket);
+    }
+
+    public static void printStats(LottoResult result) {
+        System.out.println();
+        System.out.println("당첨 통계");
+        System.out.println("---------");
+        for (LottoResultItem item : result.getItems()) {
+            System.out.println(item.getMatch() + "개 일치 (" + item.getPrizeMoney() + "원)- " + item.getCount() + "개");
+        }
+        System.out.println("총 수익률은 " + result.getEarningRatio() + "입니다.");
+    }
+}
