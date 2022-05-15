699ff5f331fa7d45925807387cc011c21bec188f
bestsilver <dkvkxm99@gmail.com>
2022-05-15 21:40:43 +0900

    refactor: 매직넘버 삭제

--- src/main/java/lotto_auto/model/ProfitRate.java
+++ src/main/java/lotto_auto/model/ProfitRate.java
@@ -1,18 +1,21 @@
 package lotto_auto.model;
 
+import lotto_auto.dto.Profit;
+
 public class ProfitRate {
     private final double rate;
+    private static final int ZERO_VALUE = 0;
 
     public ProfitRate(Money money, Profit profit) {
         if (isZero(money, profit)) {
-            this.rate = 0;
+            this.rate = ZERO_VALUE;
             return;
         }
         this.rate = (float) profit.getProfit() / money.getMoney();
     }
 
     private boolean isZero(Money money, Profit profit) {
-        return money.getMoney() == 0 || profit.getProfit() == 0;
+        return money.getMoney() == ZERO_VALUE || profit.getProfit() == ZERO_VALUE;
     }
     public String printRate() {
         return String.format("%.2f",rateFloor());
--- src/test/java/lotto_auto/model/ProfitRateTest.java
+++ src/test/java/lotto_auto/model/ProfitRateTest.java
@@ -1,5 +1,6 @@
 package lotto_auto.model;
 
+import lotto_auto.dto.Profit;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,37 +9,31 @@ public class ProfitRateTest {
     @Test
     void 머니가_0인_경우_수익률() {
         Money moneyIsZero = new Money("0");
-        Profit profit= new Profit();
 
-        profit.addProfit(5000);
-        ProfitRate rate = new ProfitRate(moneyIsZero, profit);
+        ProfitRate rate = new ProfitRate(moneyIsZero, new Profit(5000));
         assertThat(rate.printRate()).isEqualTo("0.00");
     }
 
     @Test
     void 이익이_0인_경우_수익률() {
         Money money = new Money("10000");
-        Profit profit= new Profit();
 
-        ProfitRate rate = new ProfitRate(money, profit);
+        ProfitRate rate = new ProfitRate(money, new Profit(0));
         assertThat(rate.printRate()).isEqualTo("0.00");
     }
 
     @Test
     void 머니와_이익이_0인_경우_수익률() {
         Money moneyIsZero = new Money("0");
-        Profit profit= new Profit();
 
-        ProfitRate rate = new ProfitRate(moneyIsZero, profit);
+        ProfitRate rate = new ProfitRate(moneyIsZero, new Profit(0));
         assertThat(rate.printRate()).isEqualTo("0.00");
     }
 
     @Test
     void 수익률_프린트_테스트() {
         Money money = new Money("14000");
-        Profit prifit = new Profit();
-        prifit.addProfit(5000);
 
-        assertThat(new ProfitRate(money, prifit).printRate()).isEqualTo("0.35");
+        assertThat(new ProfitRate(money, new Profit(5000)).printRate()).isEqualTo("0.35");
     }
 }
