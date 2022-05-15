791d63343b39890bc55d74a282c1196bf3c0a457
14km <k.connor614@gmail.com>
2022-05-15 16:10:56 +0900

    feat: 추가 메세지 구현

--- src/main/java/lotto/common/Messages.java
+++ src/main/java/lotto/common/Messages.java
@@ -7,9 +7,12 @@ public class Messages {
     public static final String WINNING_STATISTICS = "당첨 통계";
     public static final String SEPARATOR = "---------";
     public static final String GAME_RESULT_MESSAGE = "%d개 일치 (%d원)- %d개";
+    public static final String GAME_RESULT_EARNINGS_RATE = "총 수익률은 %s 입니다.";
+    public static final String GAME_RESULT_CRITERION_MESSAGE = "(기준이 %s이기 때문에 결과적으로 %s라는 의미임)";
     // [ERROR] --
     public static final String POSITIVE_MONEY = "[ERROR] 금액은 0원 이상을 입력해주시기 바랍니다.";
     public static final String MONEY_NOT_NUMBER = "[ERROR] 금액은 숫자를 입력해주시기 바랍니다.";
+    public static final String LOTTO_MINIMUM_PRICE = "[ERROR] 로또의 최소 구매금액은 1000원 입니다.";
     public static final String LOTTO_NUMBER_OUT_OF_RANGE = "[ERROR] 숫자는 1부터 45까지 입력이 가능합니다.";
     public static final String LOTTO_NUMBERS_SIZE = "[ERROR] 로또 번호는 최대 6개까지 입력이 가능합니다.";
     public static final String LOTTO_NUMBERS_DUPLICATE = "[ERROR] 로또 번호는 중복될 수 없습니다.";
