d833594ed43d91be33b633480488b5f99e79c314
songsimo <chy.song89@gmail.com>
2022-05-15 20:25:38 +0900

    feat: 금액의 null 혹은 빈값 검증

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -2,12 +2,24 @@ package lotto.domain;
 
 public class Money {
 	private final String INVALID_NUMBER = "0 이상의 수를 입력해주세요.";
+	private final String EMPTY_VALUE = "빈값을 입력할 수 없습니다.";
 	private final int ZERO = 0;
 
 	private int money;
 
+	public Money(String money) {
+		validEmpty(money);
+	}
+
 	public Money(int money) {
 		validPositiveNumber(money);
+		this.money = money;
+	}
+
+	private void validEmpty(String money) {
+		if(money == null || money.isEmpty()) {
+			throw new IllegalArgumentException(EMPTY_VALUE);
+		}
 	}
 
 	private void validPositiveNumber(int money) {
--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class MoneyTest {
@@ -11,6 +12,14 @@ public class MoneyTest {
 	@ValueSource(ints = {-2, -10, -30000})
 	@DisplayName("0 이상의 숫자 입력 검증")
 	void valid_number(int number) {
+		assertThatThrownBy(() -> new Money(number))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@ParameterizedTest
+	@NullAndEmptySource
+	@DisplayName("공백 혹은 빈값 검증")
+	void valid_empty_or_null(String number) {
 		assertThatThrownBy(() -> new Money(number))
 			.isInstanceOf(IllegalArgumentException.class);
 	}
