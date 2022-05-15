c1bc63973815679d786bc57ddee177b9dfa7e0ba
millennion-jin <Jadestern2@>
2022-05-16 01:33:56 +0900

    feat(LottoNumberPool) : 랜덤 로또번호 추출 기능 추가

--- src/main/java/lotto/domain/LottoNumberPool.java
+++ src/main/java/lotto/domain/LottoNumberPool.java
@@ -1,7 +1,32 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import lotto.constants.LottoConstants;
+
 public class LottoNumberPool {
+
+    private final List<LottoNumber> numberList;
+
+    public LottoNumberPool() {
+        numberList = new ArrayList<>();
+
+        for (int number = LottoConstants.MIN_LOTTO_NUMBER; number <= LottoConstants.MAX_LOTTO_NUMBER; number++) {
+            numberList.add(new LottoNumber(number));
+        }
+    }
+
     public Lotto generateLotto() {
-        return null;
+        shuffleLottoNumberList();
+        return new Lotto(drawLottoNumbers());
+    }
+
+    private void shuffleLottoNumberList() {
+        Collections.shuffle(numberList);
+    }
+
+    private List<LottoNumber> drawLottoNumbers() {
+        return numberList.subList(0, LottoConstants.LOTTO_SIZE);
     }
 }
