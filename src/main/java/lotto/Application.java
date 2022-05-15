6009d9aa7d7432afd2980058ed58f11bc0ebc168
kbh0581 <kbh0581@gmail.com>
2022-05-15 10:47:24 +0900

    feat: 당첨 번호를 입력하는 기능

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import lotto.domain.Lotto;
 import lotto.domain.Player;
 import lotto.view.InputView;
 import lotto.view.ResultView;
@@ -8,8 +9,7 @@ public class Application {
 
     public static void main(String[] args) {
         Player player = createPlayer();
-
-
+        Lotto lotto = lastWeekWinnerLotto();
     }
 
     private static Player createPlayer() {
@@ -17,6 +17,11 @@ public class Application {
         ResultView.createLotto(player);
         return player;
     }
+
+    private static Lotto lastWeekWinnerLotto() {
+        return new Lotto(InputView.winnerNumberInput());
+    }
+
 
 
 }
