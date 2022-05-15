c0c2b1d68237ea24cdab8a0e236c5f958769b222
jhsong2580 <thdwmdgns@naver.com>
2022-05-15 14:42:15 +0900

    refactor: 불필요한 static변수 제거

--- src/main/java/step3/LottoConstant.java
+++ src/main/java/step3/LottoConstant.java
@@ -12,18 +12,7 @@ public class LottoConstant {
     public static int LOTTO_MIN = 1;
     public static int LOTTO_MAX = 46;
     public static List<Integer> LOTTO_VALID_ELEMENTS = IntStream.rangeClosed(LOTTO_MIN, LOTTO_MAX).boxed().collect(Collectors.toList());
-    public static int LOTTO_SIX_NUMBER_REWARD = 2000000000;
-    public static int SIX_NUMBER_MATCH = 6;
-    public static int LOTTO_FIVE_NUMBER_REWARD = 1500000;
-    public static int FIVE_NUMBER_MATCH = 5;
-    public static int LOTTO_FOUR_NUMBER_REWARD = 50000;
-    public static int FOUR_NUMBER_MATCH = 4;
-    public static int LOTTO_THREE_NUMBER_REWARD = 5000;
-    public static int THREE_NUMBER_MATCH = 3;
-    public static String OVERVIEW_FORMAT = "%s개 일치 (%s)- %s개";
+    public static String OVERVIEW_FORMAT = "%s개 일치 (%s원)- %s개";
     public static String REWARDRATE_FORMAT = "총 수익률은 %.2f입니다.(기준이 1이기 때문에 결과적으로 %s라는 의미임)";
     public static String LOTTOS_INFO_FORMAT = "%s개를 구매했습니다.";
-    public static boolean INPUT_IS_NOT_VALID = false;
-    public static boolean INPUT_IS_VALID = true;
-
 }
--- src/main/java/step3/view/OutputView.java
+++ src/main/java/step3/view/OutputView.java
@@ -1,15 +1,7 @@
 package step3.view;
 
-import static step3.LottoConstant.FIVE_NUMBER_MATCH;
-import static step3.LottoConstant.FOUR_NUMBER_MATCH;
 import static step3.LottoConstant.LOTTOS_INFO_FORMAT;
-import static step3.LottoConstant.LOTTO_FIVE_NUMBER_REWARD;
-import static step3.LottoConstant.LOTTO_FOUR_NUMBER_REWARD;
-import static step3.LottoConstant.LOTTO_SIX_NUMBER_REWARD;
-import static step3.LottoConstant.LOTTO_THREE_NUMBER_REWARD;
 import static step3.LottoConstant.OVERVIEW_FORMAT;
-import static step3.LottoConstant.SIX_NUMBER_MATCH;
-import static step3.LottoConstant.THREE_NUMBER_MATCH;
 
 import java.util.HashMap;
 import java.util.List;
@@ -19,12 +11,10 @@ import step3.constant.LottoReward;
 
 public class OutputView {
 
-    private final HashMap<Integer, Integer> rewardPerMatch = new HashMap<>();
     private final String isLoss = "손해";
     private final String isBenefit = "이득";
 
     public OutputView() {
-        init();
     }
 
     public void printOutput(HashMap<String, Integer> statistics, int money) {
@@ -37,12 +27,7 @@ public class OutputView {
     }
 
     private void printRewardRate(long reward, int money) {
-        System.out.println(
-            String.format(
-                LottoConstant.REWARDRATE_FORMAT,
-                reward * 1.0 / money,
-                checkBenefit(reward, money)
-            ));
+        System.out.println(String.format(LottoConstant.REWARDRATE_FORMAT, reward * 1.0 / money, checkBenefit(reward, money)));
     }
 
     private String checkBenefit(long reward, int money) {
@@ -73,12 +58,5 @@ public class OutputView {
         for (int i = 0; i < lottoNumbers.size(); i++) {
             System.out.println(lottoNumbers.get(i).toString());
         }
-    }
-
-    private void init() {
-        rewardPerMatch.put(THREE_NUMBER_MATCH, LOTTO_THREE_NUMBER_REWARD);
-        rewardPerMatch.put(FOUR_NUMBER_MATCH, LOTTO_FOUR_NUMBER_REWARD);
-        rewardPerMatch.put(FIVE_NUMBER_MATCH, LOTTO_FIVE_NUMBER_REWARD);
-        rewardPerMatch.put(SIX_NUMBER_MATCH, LOTTO_SIX_NUMBER_REWARD);
     }
 }
