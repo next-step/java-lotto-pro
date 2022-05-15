f3373b75f600d347335ed392b4cc8cd21d131ed3
bestsilver <dkvkxm99@gmail.com>
2022-05-15 21:39:12 +0900

    refactor: Profit dto로 이동

---
+++ src/main/java/lotto_auto/dto/Profit.java
@@ -0,0 +1,13 @@
+package lotto_auto.dto;
+
+public class Profit {
+    int profit;
+
+    public Profit(int profit) {
+        this.profit = profit;
+    }
+
+    public int getProfit() {
+        return profit;
+    }
+}
