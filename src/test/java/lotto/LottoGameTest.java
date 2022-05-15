7fbc6dbbd87f5a26b32c225414f19ffe1f9e178e
breezeunn <je7689@gmail.com>
2022-05-16 01:02:26 +0900

    test(LOTTO): 6개 번호가 일치하는 로또 개수 계산

--- src/test/java/lotto/LottoGameTest.java
+++ src/test/java/lotto/LottoGameTest.java
@@ -105,4 +105,21 @@ public class LottoGameTest {
         assertThat(game.getScore().get(5)).isEqualTo(2);
     }
 
+    @Test
+    void 번호_6개_일치() {
+        List<LottoNumbers> tickets = new ArrayList<>();
+        tickets.add(new LottoNumbers("1,2,3,4,5,6"));
+        LottoGame game = new LottoGame(tickets, "1,2,3,4,5,6");
+        assertThat(game.getScore().get(6)).isEqualTo(1);
+    }
+
+    @Test
+    void 번호_6개_일치_2건() {
+        List<LottoNumbers> tickets = new ArrayList<>();
+        tickets.add(new LottoNumbers("1,2,3,4,5,6"));
+        tickets.add(new LottoNumbers("1,2,3,4,5,6"));
+        tickets.add(new LottoNumbers("21,32,3,4,5,6"));
+        LottoGame game = new LottoGame(tickets, "1,2,3,4,5,6");
+        assertThat(game.getScore().get(6)).isEqualTo(2);
+    }
 }
