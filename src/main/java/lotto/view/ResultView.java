8467f2851fcdf9d5082ddff74016ca26ea73764b
Philip <pch4623@gmail.com>
2022-05-15 22:59:57 +0900

    feat(ResultView): 로또 번호 출력

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -1,9 +1,15 @@
 package lotto.view;
 
+import lotto.model.LottoNumbers;
+
 public class ResultView {
     private static final String PURCHASE_COUNT_OUTPUT_MESSAGE = "%d개를 구매했습니다.";
 
     public static void printPurchaseCountOutput(int count) {
         System.out.printf(PURCHASE_COUNT_OUTPUT_MESSAGE + "\n", count);
+    }
+
+    public static void printLottoNumbers(LottoNumbers lottoNumbers) {
+        System.out.println(lottoNumbers);
     }
 }
