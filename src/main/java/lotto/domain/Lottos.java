56c27311c4de040e632d3b27caa69de3ad864c09
limwoobin <drogba02@naver.com>
2022-05-15 20:08:10 +0900

    feat: 로또목록과 정답번호를 비교해 스코어를 계산하는 기능을 작성하라

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -13,4 +13,22 @@ public class Lottos {
     public List<Lotto> getLottos() {
         return lottos;
     }
+
+    public LottoScore calculateLottoScore(WinningNumbers winningNumbers) {
+        LottoScore lottoScore = new LottoScore();
+
+        lottos.forEach(x -> {
+            int winningNumberCount = x.getWinningOfNumbersCount(winningNumbers.getWinningNumbers());
+            addLottoScore(lottoScore, winningNumberCount);
+        });
+
+        return lottoScore;
+    }
+
+    private void addLottoScore(LottoScore lottoScore, int winningNumberCount) {
+        LottoWinnings lottoWinnings = LottoWinnings.getWinningsByCount(winningNumberCount);
+        if (lottoWinnings != null) {
+            lottoScore.addScore(lottoWinnings);
+        }
+    }
 }
