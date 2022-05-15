76438a741bac1b84af8c308dfc278bafeff75536
bestsilver <dkvkxm99@gmail.com>
2022-05-15 21:56:28 +0900

    refactor: - 상수 대문자 수정 - LottoNumbers 적용 - default rank count 상수화

--- src/main/java/lotto_auto/model/Figures.java
+++ src/main/java/lotto_auto/model/Figures.java
@@ -4,16 +4,15 @@ import java.util.*;
 
 public class Figures {
     private Map<LottoRank, Integer> figures;
-    private static final String printResultFormat = "%d개 일치 (%d원)- %d개\n";
-    private Profit profit;
+    private static final int DEFAULT_RANK_COUNT = 0;
+    public static final String FIGURES_FORMAT = "%d개 일치 (%d원)- %d개\n";
 
     public Figures(Lottos lottos, Lotto winningsLotto) {
         init();
-        for (Lotto lotto : lottos) {
-            LottoRank rank = LottoRank.matches(lotto, winningsLotto);
-            int count = figures.getOrDefault(rank, 0);
+        for (Lotto lotto : lottos.getLottoList()) {
+            LottoRank rank = lotto.matches(winningsLotto);
+            int count = figures.getOrDefault(rank, DEFAULT_RANK_COUNT);
             figures.put(rank, count + 1);
-            profit.addProfit(rank.winnings());
         }
 
         figures.remove(LottoRank.NOTHING);
@@ -21,22 +20,24 @@ public class Figures {
 
     private void init() {
         figures = new EnumMap<>(LottoRank.class);
-        profit = new Profit();
+
         for (LottoRank rank : LottoRank.values()) {
-            figures.put(rank,0);
+            figures.put(rank, DEFAULT_RANK_COUNT);
         }
     }
 
-    public Profit getProfit(){
-        return this.profit;
+    public int getTotalWinning() {
+        int winningMoney = 0;
+        for (LottoRank rank : figures.keySet()) {
+            winningMoney += rank.winnings() * figures.get(rank);
+        }
+        return winningMoney;
     }
 
-    @Override
-    public String toString() {
+    public String printFigures() {
         StringBuilder result = new StringBuilder();
-
         for (LottoRank rank : LottoRank.valuesTheLowestOrder()) {
-            result.append(String.format(printResultFormat, rank.matchedCount(), rank.winnings(), figures.get(rank)));
+            result.append(String.format(FIGURES_FORMAT, rank.matchedCount(), rank.winnings(), figures.get(rank)));
         }
 
         return result.toString();
--- src/test/java/lotto_auto/model/FiguresTest.java
+++ src/test/java/lotto_auto/model/FiguresTest.java
@@ -10,12 +10,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class FiguresTest {
     @Test
     void 당첨_통계_결과_테스트() {
-        Lottos lottos = new Lottos(Collections.singletonList(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6))));
-        Lotto winning = new Lotto(Arrays.asList(1,2,3,4,5,6));
+        Lottos lottos = new Lottos(Arrays.asList(new Lotto(new LottoNumbers("1, 2, 3, 4, 5, 6"))));
+        Lotto winning = new Lotto(new LottoNumbers("1, 2, 3, 4, 5, 6"));
         Figures figures = new Figures(lottos, winning);
 
-        assertThat(figures.toString())
-                .isEqualTo("3개 일치 (5000원)- 0개\n" +
+        assertThat(figures.printFigures())
+                .isEqualTo(
+                "3개 일치 (5000원)- 0개\n" +
                 "4개 일치 (50000원)- 0개\n" +
                 "5개 일치 (1500000원)- 0개\n" +
                 "6개 일치 (2000000000원)- 1개\n");
