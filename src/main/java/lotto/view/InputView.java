25e95e382e8efced56d052c39c00f53cc0f3aa06
toughchb <ggang2000@gmail.com>
2022-05-15 14:03:43 +0900

    refactor: public Messages private로 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -10,9 +10,8 @@ import lotto.domain.Money;
 
 public class InputView {
 
-    public static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
-    public static final String INPUT_WINNING_LOTTO = "지난 주 당첨 번호를 입력해 주세요.";
-
+    private static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_LOTTO = "지난 주 당첨 번호를 입력해 주세요.";
 
     private static final Scanner scanner = new Scanner(System.in);
 
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,14 +2,13 @@ package lotto.view;
 
 public class OutputView {
 
-    public static final String PURCHASED_TICKETS_NUMBER = "%s개를 구매했습니다.";
-    public static final String RESULT_PRIZED_RATE = "당첨 통계";
-    public static final String LINE_STRING = "---------";
-    public static final String TOTAL_WINNING_COUNT = "%s개 일치 (%s원)- %s개";
-    public static final String TOTAL_PROFIT_RATE = "총 수익률은 %.2f입니다.(기준이 1이기 때문에 결과적으로 %s라는 의미임)";
-
-    public static final String BENEFIT = "이익이";
-    public static final String LOSS = "손해";
+    private static final String PURCHASED_TICKETS_NUMBER = "%s개를 구매했습니다.";
+    private static final String RESULT_PRIZED_RATE = "당첨 통계";
+    private static final String LINE_STRING = "---------";
+    private static final String TOTAL_WINNING_COUNT = "%s개 일치 (%s원)- %s개";
+    private static final String TOTAL_PROFIT_RATE = "총 수익률은 %.2f입니다.(기준이 1이기 때문에 결과적으로 %s라는 의미임)";
+    private static final String BENEFIT = "이익이";
+    private static final String LOSS = "손해";
 
 
     private static final int BENEFIT_STANDARD = 1;
