e70eee9493cfe3e0f058ec0e4bd201380171e6b8
Celina <luv.mins99@gmail.com>
2022-05-16 02:49:42 +0900

    feat(Price): 로또 한 장의 금액을 가지는 객체 추가
    - 사용자의 구입금액이 로또 한 장 보다 적은 금액인지 체크

---
+++ src/main/java/lotto/model/Price.java
@@ -0,0 +1,21 @@
+package lotto.model;
+
+public class Price {
+    private static final int LOTTO_PRICE = 1000;
+    private static final String MINIMUM_PRICE_ERROR_MESSAGE = "최소 구입금액은 " + LOTTO_PRICE + "원 입니다.";
+
+    public Price() {
+
+    }
+
+    public static void minimumPriceCheck(String money) {
+        int price = Integer.parseInt(money);
+        if (price < LOTTO_PRICE) {
+            throw new IllegalArgumentException(MINIMUM_PRICE_ERROR_MESSAGE);
+        }
+    }
+
+    public static int lottoPrice() {
+        return LOTTO_PRICE;
+    }
+}
