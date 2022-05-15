3df930c55126053512fa7cdc93b7fe53014d3d5e
chj1768 <chj1768@gmail.com>
2022-05-15 18:37:24 +0900

    refactor: vendor - 1개당 로또 가격 상수 rename, 집계 역할 추가

--- src/main/java/Vendor.java
+++ src/main/java/Vendor.java
@@ -4,20 +4,23 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Vendor {
-    public static final int LOTTO_BILL = 1000;
-    private static final List<LottoNumber> ALL_AVAILABLE_LOTTO_NUMBER_LIST =
-            IntStream.range(LottoNumber.LOWER_BOUND, LottoNumber.UPPER_BOUND)
-                    .boxed()
-                    .map(LottoNumber::new)
-                    .collect(Collectors.toList());
+    public static final int LOTTO_PRICE = 1000;
+    private final List<LottoNumber> ALL_AVAILABLE_LOTTO_NUMBER_LIST;
+
+    public Vendor() {
+        this.ALL_AVAILABLE_LOTTO_NUMBER_LIST = IntStream.range(LottoNumber.LOWER_BOUND, LottoNumber.UPPER_BOUND)
+                .boxed()
+                .map(LottoNumber::new)
+                .collect(Collectors.toList());
+    }
 
     public Lottos buy(long money) {
-        final long maxCount = money / LOTTO_BILL;
+        final long maxCount = money / LOTTO_PRICE;
         if (maxCount < 1)
             throw new RuntimeException();
 
         Lottos lottos = new Lottos();
-        for (int count = 1; count < Math.toIntExact(maxCount); count++) {
+        for (int count = 1; count <= maxCount; count++) {
             lottos.add(get());
         }
         return lottos;
@@ -27,5 +30,9 @@ public class Vendor {
         Collections.shuffle(ALL_AVAILABLE_LOTTO_NUMBER_LIST);
         return new LottoNumbers(
                 ALL_AVAILABLE_LOTTO_NUMBER_LIST.stream().limit(LottoNumbers.SIZE).collect(Collectors.toList()));
+    }
+
+    public Aggregator aggregate(ContainCounts containCounts) {
+        return new Aggregator(containCounts);
     }
 }
--- src/test/java/VendorTest.java
+++ src/test/java/VendorTest.java
@@ -6,11 +6,16 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class VendorTest {
     @Test
     void 로또를_구매할_수_있다() {
-        assertThat(new Vendor().buy(Vendor.LOTTO_BILL)).isInstanceOf(Lottos.class);
+        assertThat(new Vendor().buy(Vendor.LOTTO_PRICE)).isInstanceOf(Lottos.class);
     }
 
     @Test
     void 가격_미만이면_로또를_구매할_수_없다() {
-        assertThatThrownBy(() -> new Vendor().buy(Vendor.LOTTO_BILL - 1)).isInstanceOf(RuntimeException.class);
+        assertThatThrownBy(() -> new Vendor().buy(Vendor.LOTTO_PRICE - 1)).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    void 집계할_수_있다() {
+        assertThat(new Vendor().aggregate(new ContainCounts())).isInstanceOf(Aggregator.class);
     }
 }
