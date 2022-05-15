a6fc2534439036ed73cded1886f0d789a8fe93fc
bestsilver <dkvkxm99@gmail.com>
2022-05-15 22:01:58 +0900

    refactor: 로또끼리 비교기능 해당 위치로 이동

--- src/main/java/lotto_auto/model/Lotto.java
+++ src/main/java/lotto_auto/model/Lotto.java
@@ -1,86 +1,25 @@
 package lotto_auto.model;
 
-import lotto_auto.exception.IllegalLottoException;
-
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Lotto {
-    private final List<Integer> lottoNumbers;
-
-    public static final int validSize = 6;
-    public static final int minNumber = 1;
-    public static final int maxNumber = 45;
-    private static final String lottoDelimiter = ", ";
-    public static final int price = 1000;
-
-    public static final String NOT_NUMBER = "[ERROR] 로또의 번호에 숫자 이외의 값이 존재합니다.";
-    public static final String NOT_MATCHED_NUMBER_SIZE = "[ERROR] 로또 번호는 6개의 숫자가 필요합니다.";
-    public static final String EXIST_DUPLICATE_VALUE = "[ERROR] 로또 번호는 중복 숫자가 존재할 수 없습니다.";
-    public static final String NOT_RANGE_NUMBER = "[ERROR] 로또 번호는 1~45의 숫자입니다.";
-
-    public Lotto(List<Integer> numbers) throws IllegalLottoException {
-        checkValidLottoNumber(numbers);
-
+    private final LottoNumbers lottoNumbers;
+    public Lotto(LottoNumbers numbers) {
         this.lottoNumbers = numbers;
     }
 
-     public Lotto(String stringNumber) throws IllegalLottoException {
-        try {
-            List<Integer> tmpLotto = Arrays.stream(stringNumber.split(lottoDelimiter))
-                    .mapToInt(Integer::parseInt)
-                    .boxed()
-                    .collect(Collectors.toList());
-
-            checkValidLottoNumber(tmpLotto);
-
-            this.lottoNumbers = tmpLotto;
-        } catch (NumberFormatException err) {
-            throw IllegalLottoException.NOT_NUMBER_ERROR;
-        }
+    public LottoRank matches(Lotto from) {
+        int count = this.lottoNumbers.countSameLottoNumber(from.lottoNumbers);
+        return LottoRank.getLottoRuleFromMatchedCount(count);
     }
 
-    private void checkValidLottoNumber(List<Integer> numbers) throws IllegalLottoException {
-        checkNumberSize(numbers);
-        checkNumberRange(numbers);
-        checkDuplicateNumber(numbers);
-    }
+    public String printLotto() {
+        char fisrtCharactor = '[';
+        char lastCharactor = ']';
 
-    private void checkNumberSize(List<Integer> numbers) throws IllegalLottoException {
-        if (!(numbers.size() == validSize)) {
-            throw IllegalLottoException.NOT_MATCHED_NUMBER_SIZE_ERROR;
-        }
-    }
-
-    private void checkNumberRange(List<Integer> numbers) throws IllegalLottoException {
-        if (!numbers.stream().allMatch(n ->  n >= minNumber && n <= maxNumber)) {
-            throw IllegalLottoException.NOT_RANGE_NUMBER_ERROR;
-        }
-    }
-
-    private void checkDuplicateNumber(List<Integer> numbers) throws IllegalLottoException {
-        Set<Integer> set = new HashSet<>();
-        boolean isUnique = numbers.stream().allMatch(set::add);
-
-        if (!isUnique) {
-            throw IllegalLottoException.EXIST_DUPLICATE_VALUE_ERROR;
-        }
-    }
-
-    public List<Integer> getLottoNumbers() {
-        return this.lottoNumbers;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder result = new StringBuilder();
-        char printFirstChar = '[';
-        char printLastChar = ']';
-
-        for (int number : lottoNumbers) {
-            result.append(number).append(lottoDelimiter);
-        }
-
-        return printFirstChar + result.substring(0, result.length() -2) + printLastChar;
+        return fisrtCharactor + this.lottoNumbers.getLottoNumberList().stream()
+                .map(lottoNumber -> Integer.toString(lottoNumber.getNumber()))
+                .collect(Collectors.joining(", ")) +lastCharactor;
     }
 }
--- src/test/java/lotto_auto/model/LottoTest.java
+++ src/test/java/lotto_auto/model/LottoTest.java
@@ -1,56 +1,22 @@
 package lotto_auto.model;
-
-import lotto_auto.exception.IllegalLottoException;
 import org.junit.jupiter.api.Test;
-import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class LottoTest {
 
     @Test
-    public void 로또번호_범위_체크() {
-        assertThatExceptionOfType(IllegalLottoException.class)
-                .isThrownBy(() -> new Lotto(Arrays.asList(1,2,3,4,5,100)))
-                .withMessage(Lotto.NOT_RANGE_NUMBER);
+    void 로또_비교후_결과_랭크() {
+        Lotto lotto = new Lotto(new LottoNumbers("1, 2, 3, 4, 5, 6"));
+        Lotto winningLotto = new Lotto(new LottoNumbers("1, 2, 3, 4, 5, 6"));
 
-        assertThatExceptionOfType(IllegalLottoException.class)
-                .isThrownBy(() -> new Lotto("1, 2, 3, 4, 5, 100"))
-                .withMessage(Lotto.NOT_RANGE_NUMBER);
+        assertThat(lotto.matches(winningLotto)).isEqualTo(LottoRank.FIRST);
     }
 
     @Test
-    public void 로또번호_개수_체크() {
-        assertThatExceptionOfType(IllegalLottoException.class)
-                .isThrownBy(() -> new Lotto(Arrays.asList(1,2,3,4,5,6,7)))
-                .withMessage(Lotto.NOT_MATCHED_NUMBER_SIZE);
+    void 로또_출력_테스트() {
+        Lotto lotto = new Lotto(new LottoNumbers("1, 2, 3, 4, 5, 6"));
 
-        assertThatExceptionOfType(IllegalLottoException.class)
-                .isThrownBy(() -> new Lotto("1, 2, 3, 4, 5, 6, 7"))
-                .withMessage(Lotto.NOT_MATCHED_NUMBER_SIZE);
-    }
-
-    @Test
-    public void 로또번호_중복_체크() {
-        assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> new Lotto(Arrays.asList(1,1,2,3,4,5)))
-                .withMessage(Lotto.EXIST_DUPLICATE_VALUE);
-
-        assertThatExceptionOfType(IllegalLottoException.class)
-                .isThrownBy(() -> new Lotto("1, 1, 2, 3, 4, 5"))
-                .withMessage(Lotto.EXIST_DUPLICATE_VALUE);
-    }
-
-    @Test
-    public void 로또번호_숫자_이외의_값_체크() {
-        assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() ->  new Lotto("a, b, c, 1, 2, 3"))
-                .withMessage(Lotto.NOT_NUMBER);
-    }
-
-    @Test
-    public void 로또_프린트_테스트() {
-        Lotto lotto = new Lotto(Arrays.asList(1,6,2,3,4,5));
-        assertThat(lotto.toString()).isEqualTo("[1, 6, 2, 3, 4, 5]");
+        assertThat(lotto.printLotto()).isEqualTo("[1, 2, 3, 4, 5, 6]");
     }
 }
