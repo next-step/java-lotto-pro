a596404e68ca822aed16408aef31fe214a77e01f
vvsungho <vvsungho@gmail.com>
2022-05-15 10:33:50 +0900

    test: 로또번호 오름차순 정렬 테스트 케이스 작성

--- src/main/java/lotto/auto/LottoNumber.java
+++ src/main/java/lotto/auto/LottoNumber.java
@@ -1,11 +1,48 @@
 package lotto.auto;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class LottoNumber {
+    private final static int LOTTO_NUMBER_SIZE_VALUE = 6;
+
     private List<Integer> numbers;
 
     public LottoNumber() {
+        for (int i = 0; i < LOTTO_NUMBER_SIZE_VALUE; i++) {
+            this.numbers = new ArrayList<>();
+            this.numbers.add(i);
+        }
+    }
+
+    public int lottoNumberCount() {
+        return LOTTO_NUMBER_SIZE_VALUE;
+    }
+
+    public void sortLottoNumber() {
+        this.numbers= this.numbers.stream()
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return Objects.equals(numbers, that.numbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numbers);
+    }
+
+    @Override
+    public String toString() {
+        return this.numbers.toString();
     }
 }
 
--- src/test/java/lotto/auto/LottoNumberTest.java
+++ src/test/java/lotto/auto/LottoNumberTest.java
@@ -3,11 +3,24 @@ package lotto.auto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class LottoNumberTest {
 
     @Test
-    @DisplayName("6개의 번호로 발급되는지 확인한다")
+    @DisplayName("6개의 번호로 발급되는지 확인한다.")
     void 테스트_로또번호6개_발급() {
         LottoNumber lottoNumber = new LottoNumber();
+        assertThat(lottoNumber.lottoNumberCount()).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("로또 번호를 오름차순 정렬한다.")
+    void 테스트_로또번호_오름차순() {
+        LottoNumber lottoNumber = new LottoNumber();
+        lottoNumber.sortLottoNumber();
+        assertThat(lottoNumber.equals(Arrays.asList(1, 2, 3, 4, 5, 6))).isEqualTo(true);
     }
 }
