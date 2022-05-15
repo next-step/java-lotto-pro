8911c92586e19f22e0e40571ced32a56c76bf6bb
otakjong <tj3828@naver.com>
2022-05-15 11:57:08 +0900

    feat(Money): Money 클래스 및 테스트 작성

---
+++ src/test/java/lotto/auto/model/MoneyTest.java
@@ -0,0 +1,17 @@
+package lotto.auto.model;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MoneyTest {
+
+    @Test
+    @DisplayName("입력된 금액이 0보다 작을 경우 예외처리")
+    void 입력된_금액이_0원보다_작을_경우() {
+        assertThatIllegalArgumentException().isThrownBy(() -> new Money(-1));
+        assertThatIllegalArgumentException().isThrownBy(() -> new Money("s"));
+    }
+
+}
