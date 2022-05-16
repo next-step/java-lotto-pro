d81f1176dbd9cf35e6155a22e83b4395b90414a1
bestsilver <dkvkxm99@gmail.com>
2022-05-16 21:18:14 +0900

    refactor: string argument 생성자 삭제

--- src/main/java/lotto_auto/model/LottoNumbers.java
+++ src/main/java/lotto_auto/model/LottoNumbers.java
@@ -2,35 +2,18 @@ package lotto_auto.model;
 
 import java.util.*;
 
-import java.util.stream.Collectors;
-
 public class LottoNumbers {
     private final Set<LottoNumber> lottoNumberSet;
 
     public static final int VALID_SIZE = 6;
-    public static final String LOTTO_DELIMITER = ", ";
 
     public static final String NOT_MATCHED_NUMBER_SIZE = "[ERROR] 로또 번호는 6개의 숫자가 필요합니다.";
     public static final String EXIST_DUPLICATE_VALUE = "[ERROR] 로또 번호는 중복 숫자가 존재할 수 없습니다.";
-    public static final String NOT_NUMBER = "[ERROR] 로또의 번호에 숫자 이외의 값이 존재합니다.";
 
     public LottoNumbers(List<LottoNumber> numberList) {
         this.lottoNumberSet = Collections.unmodifiableSet(new HashSet<>(numberList));
+        checkDuplicateNumber(numberList);
         checkNumbersSize();
-    }
-
-    public LottoNumbers(String stringNumbers) {
-        try {
-            this.lottoNumberSet =
-                    Collections.unmodifiableSet(Arrays.stream(stringNumbers.split(LOTTO_DELIMITER))
-                            .map(Integer::parseInt)
-                            .map(LottoNumber::new)
-                            .collect(Collectors.toSet()));
-
-            checkNumbersSize();
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(NOT_NUMBER);
-        }
     }
 
     private void checkNumbersSize() {
@@ -43,9 +26,16 @@ public class LottoNumbers {
         return lottoNumberSet;
     }
 
+    private void checkDuplicateNumber(List<LottoNumber> numberList) {
+        if (numberList.size() > this.lottoNumberSet.size()) {
+            throw new IllegalArgumentException(EXIST_DUPLICATE_VALUE);
+        }
+    }
+
     public int countSameLottoNumber(LottoNumbers numbers) {
         return (int) this.lottoNumberSet
                 .stream()
                 .filter(lottoNumber -> numbers.getLottoNumberSet().contains(lottoNumber)).count();
     }
+
 }
