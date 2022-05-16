0693358b9e22979e1eca45f1d5f503de77b500f8
Ttungga <torb2002@gmail.com>
2022-05-17 02:06:09 +0900

    fix: change AutoNumbersIssuer.issueNumbers to return List<Integer>
    - rename AutoLottoNumbersIssuer to AutoNumbersIssuer

--- src/main/java/lotto/domain/LottoTicketsFactory.java
+++ src/main/java/lotto/domain/LottoTicketsFactory.java
@@ -2,13 +2,13 @@ package lotto.domain;
 
 import java.util.ArrayList;
 import java.util.List;
-import lotto.service.AutoLottoNumbersIssuer;
+import lotto.service.AutoNumbersIssuer;
 
 public class LottoTicketsFactory {
     public LottoTickets createAutomatically(final int count) {
         final List<LottoTicket> lottoTickets = new ArrayList<>();
         for (int i = 0; i < count; i++) {
-            lottoTickets.add(new LottoTicket(AutoLottoNumbersIssuer.issueLottoNumbers()));
+            lottoTickets.add(new LottoTicket(new LottoNumbers(AutoNumbersIssuer.issueNumbers())));
         }
         return new LottoTickets(lottoTickets);
     }
--- src/main/java/lotto/service/AutoNumbersIssuer.java
+++ src/main/java/lotto/service/AutoNumbersIssuer.java
@@ -9,18 +9,17 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import lotto.domain.LottoNumbers;
 
-public class AutoLottoNumbersIssuer {
+public class AutoNumbersIssuer {
     private static final List<Integer> NUMBER_CANDIDATES = IntStream.range(MINIMUM_NUMBER.getCondition(),
                     MAXIMUM_NUMBER.getCondition() + 1)
             .boxed()
             .collect(Collectors.toList());
 
-    public static LottoNumbers issueLottoNumbers() {
+    public static List<Integer> issueNumbers() {
         Collections.shuffle(NUMBER_CANDIDATES);
         final List<Integer> numbers = new ArrayList<>(NUMBER_CANDIDATES.subList(0, AMOUNT_OF_NUMBERS.getCondition()));
         Collections.sort(numbers);
-        return new LottoNumbers(numbers);
+        return numbers;
     }
 }
