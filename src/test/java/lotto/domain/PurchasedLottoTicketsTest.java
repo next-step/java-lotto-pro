f235595ce7a19717131f4f6049cf176586d632e8
toughchb <ggang2000@gmail.com>
2022-05-16 11:04:55 +0900

    test(PurchasedLottoTicketsTest): checkWinningLotto 테스트 추가

--- src/test/java/lotto/domain/PurchasedLottoTicketsTest.java
+++ src/test/java/lotto/domain/PurchasedLottoTicketsTest.java
@@ -27,4 +27,11 @@ class PurchasedLottoTicketsTest {
                 contains("[1, 2, 3, 4, 5, 6]").
                 contains("[11, 22, 33, 44, 5, 6]");
     }
+
+    @Test
+    void 구입한_티켓들을_이용하여_우승로또와_비교() {
+        WinningLotto winningLotto = WinningLotto.of(LottoNumbers.from(Arrays.asList(1, 2, 3, 4, 5, 7)), LottoNumber.from(6));
+        LottoWinningResults results = tickets.checkWinningLotto(winningLotto);
+        assertThat(results.prizedMoney()).isEqualTo(30_000_000);
+    }
 }
