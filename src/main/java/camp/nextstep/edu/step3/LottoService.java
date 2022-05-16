38f200e0762762388cbcbc6cfa83f41c5044c4f3
mannue <sppe12@gmail.com>
2022-05-16 16:00:12 +0900

    refactor(LottoService): LottoAnswer 를 생성하도록 수정

--- src/main/java/camp/nextstep/edu/step3/LottoService.java
+++ src/main/java/camp/nextstep/edu/step3/LottoService.java
@@ -1,32 +1,19 @@
 package camp.nextstep.edu.step3;
 
-import java.util.List;
-
 public class LottoService {
     private final Presenter presenter;
     private final LottoVendingMachine machine;
-    private final LottoGenerator generator;
 
     public LottoService(Presenter presenter) {
         this.presenter = presenter;
-        this.generator = new LottoGenerator();
-        this.machine = new LottoVendingMachine(this.generator);
+        this.machine = new LottoVendingMachine(new LottoGenerator());
     }
 
     public void task() {
         final LottoMoney purchaseAmount = presenter.askPurchaseAmount();
         final LottoPaper lottoPaper = machine.issued(purchaseAmount);
         presenter.printLottoList(lottoPaper);
-        List<LottoNumber> lastWeekWinningNumber = presenter.askLastWeekWinningNumber();
-        final LottoResult winningResult = lottoPaper.checkAll(generator.manual(lastWeekWinningNumber), validBonusNumber(lastWeekWinningNumber));
+        final LottoResult winningResult = lottoPaper.checkAll(new LottoAnswer(presenter.askLastWeekWinningNumber(),  presenter.askLottoBonusNumber()));
         presenter.printResult(winningResult, winningResult.earningRate(purchaseAmount));
-    }
-
-    private LottoNumber validBonusNumber(List<LottoNumber> lastWeekWinningNumber) {
-        LottoNumber bonusNumber = presenter.askLottoBonusNumber();
-        if (lastWeekWinningNumber.contains(bonusNumber)) {
-            throw new IllegalArgumentException("보너스 숫자는 기존 번호와 중복이 될수 없습니다.");
-        }
-        return bonusNumber;
     }
 }
