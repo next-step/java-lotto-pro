a94f530b4232d4e56c74f60b3d8d08b89e44050e
breezeunn <je7689@gmail.com>
2022-05-16 21:31:36 +0900

    fix(PRINT): 오타 수정

--- src/main/java/lotto/ui/ResultView.java
+++ src/main/java/lotto/ui/ResultView.java
@@ -17,7 +17,7 @@ public class ResultView {
         System.out.println(String.format("3개 일치 (5000원)- %d개", score.get(3)));
         System.out.println(String.format("4개 일치 (50000원)- %d개", score.get(4)));
         System.out.println(String.format("5개 일치 (1500000원)- %d개", score.get(5)));
-        System.out.println(String.format("3개 일치 (2000000000원)- %d개", score.get(6)));
+        System.out.println(String.format("6개 일치 (2000000000원)- %d개", score.get(6)));
         StringBuffer earningRateMessage = new StringBuffer(String.format("총 수익률은 %.2f입니다.", earningRate));
         if (earningRate < 1) {
             earningRateMessage.append("(기준이 1이기 때문에 결과적으로 손해라는 의미임)");
