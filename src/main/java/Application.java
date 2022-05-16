20ca9b472731091046b5074cd155a6cff1909139
SSG\221287 <hyeok7777@gmail.com>
2022-05-16 16:36:41 +0900

    merge를 위한 파일 삭제

--- src/main/java/Application.java
+++
@@ -1,8 +0,0 @@
-import lottoauto.LottoStarter;
-
-public class Application {
-    public static void main(String[] args) {
-        LottoStarter lottoStarter = new LottoStarter();
-        lottoStarter.startLotto();
-    }
-}
