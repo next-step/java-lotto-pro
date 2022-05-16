31b4657166dba4510951802a6decde34ea569b67
mannue <sppe12@gmail.com>
2022-05-16 21:09:09 +0900

    feat(LottoVendingMachine): History 정보 추가

--- src/main/java/camp/nextstep/edu/step3/LottoVendingMachine.java
+++ src/main/java/camp/nextstep/edu/step3/LottoVendingMachine.java
@@ -1,33 +1,66 @@
 package camp.nextstep.edu.step3;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 public class LottoVendingMachine {
     private final LottoGenerator generator;
+    private IssuedHistory latestIssuedHistory;
 
     public LottoVendingMachine(final LottoGenerator generator) {
         this.generator = generator;
     }
 
     public LottoPaper issued(final LottoMoney money) {
+        List<Lotto> autoLotto = buyLotto(money);
+        this.latestIssuedHistory = createIssuedHistory(
+                Collections.singletonList(new IssuedInformation(IssuedMode.Auto, autoLotto))
+        );
         return new LottoPaper(buyLotto(money));
     }
 
     public LottoPaper issued(final LottoMoney money, List<Lotto> manualLotto) {
-        if (Objects.isNull(manualLotto) || manualLotto.isEmpty()) {
+        if (isAllAuto(manualLotto)) {
             return this.issued(money);
         }
-        return issuedManualWithAuto(money, manualLotto);
+        if (isAllManual(money.excludingAmount(manualLotto))) {
+            this.latestIssuedHistory = createIssuedHistory(
+                    Collections.singletonList(new IssuedInformation(IssuedMode.Manual, manualLotto))
+            );
+            return new LottoPaper(manualLotto);
+        }
+
+        return issuedManualWithAuto(money.excludingAmount(manualLotto), manualLotto);
     }
 
-    private LottoPaper issuedManualWithAuto(LottoMoney money, List<Lotto> manualLotto) {
-        List<Lotto> automaticallyIssuedLotto = buyLotto(money.excludingAmount(manualLotto));
+    public LottoPaper issuedManualWithAuto(LottoMoney money, List<Lotto> manualLotto) {
+        List<Lotto> automaticallyIssuedLotto = buyLotto(money);
         List<Lotto> totalLotto = new ArrayList<>();
         totalLotto.addAll(manualLotto);
         totalLotto.addAll(automaticallyIssuedLotto);
+        this.latestIssuedHistory = createIssuedHistory(Arrays.asList(
+                new IssuedInformation(IssuedMode.Auto, automaticallyIssuedLotto),
+                new IssuedInformation(IssuedMode.Manual, manualLotto)
+        ));
         return new LottoPaper(totalLotto);
+    }
+
+    private boolean isAllAuto(List<Lotto> manualLotto) {
+        return Objects.isNull(manualLotto) || manualLotto.isEmpty();
+    }
+
+    private boolean isAllManual(final LottoMoney lottoMoney) {
+        return Objects.equals(lottoMoney, new LottoMoney(0));
+    }
+
+    private IssuedHistory createIssuedHistory(List<IssuedInformation> issuedInformationList) {
+        return new IssuedHistory(issuedInformationList);
+    }
+
+    public IssuedHistory printIssuedLotto() {
+        if (Objects.isNull(latestIssuedHistory)) {
+            return new IssuedHistory(Collections.emptyList());
+        }
+        return latestIssuedHistory;
     }
 
     private List<Lotto> buyLotto(LottoMoney money) {
--- src/test/java/camp/nextstep/edu/step3/LottoVendingMachineTest.java
+++ src/test/java/camp/nextstep/edu/step3/LottoVendingMachineTest.java
@@ -3,12 +3,18 @@ package camp.nextstep.edu.step3;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static camp.nextstep.edu.step3.LottoTest.createLottoNumberList;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
 public class LottoVendingMachineTest {
 
@@ -40,5 +46,66 @@ public class LottoVendingMachineTest {
     void 발급시수동로또가비어있거나없을경우() {
         assertThat(vendingMachine.issued(new LottoMoney(14000), null).numberOfPurchases()).isEqualTo(14);
         assertThat(vendingMachine.issued(new LottoMoney(14000), Collections.emptyList()).numberOfPurchases()).isEqualTo(14);
+    }
+
+    @DisplayName("로또 머신은 발급된 로또 정보를 발급할수 있어야 한다.")
+    @ParameterizedTest
+    @MethodSource("provideIssuedHistory")
+    void printIssuedLottoInformation(List<Lotto> manualLotto,IssuedHistory issuedHistory ) {
+        List<Lotto> autoLotto = Collections.singletonList(new Lotto(createLottoNumberList(new int[]{1, 2, 3, 4, 5, 6})));
+        LottoGenerator spyLottoGenerator = createMockLottoGenerator(autoLotto.get(0));
+        LottoVendingMachine lottoVendingMachine = new LottoVendingMachine(spyLottoGenerator);
+        lottoVendingMachine.issued(new LottoMoney(3000), manualLotto);
+        assertThat(lottoVendingMachine.printIssuedLotto()).isEqualTo(issuedHistory);
+    }
+
+    @DisplayName("로또머신은 발급된 기록이 없을때 에러를 반환한다.")
+    @Test
+    void invalidPrintIssuedLottoInformation() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(vendingMachine::printIssuedLotto);
+    }
+
+    private static Stream<Arguments> provideIssuedHistory() {
+        return Stream.of(
+                Arguments.of(
+                        createManualLotto(Arrays.asList(new int[]{7, 8, 9, 10, 11, 12}, new int[]{1, 2, 3, 4, 5, 7})),
+                        createIssuedHistory(
+                                Collections.singletonList(new Lotto(createLottoNumberList(new int[]{1, 2, 3, 4, 5, 6}))),
+                                createManualLotto(Arrays.asList(new int[]{7, 8, 9, 10, 11, 12}, new int[]{1, 2, 3, 4, 5, 7}))
+                        )
+
+                ),
+
+                Arguments.of(
+                        createManualLotto(Arrays.asList(new int[]{7, 8, 9, 10, 11, 12}, new int[]{1, 2, 3, 4, 5, 7}, new int[] {1,2,3,4,5,6})),
+                        createIssuedHistory(
+                                 Collections.emptyList(),
+                                createManualLotto(Arrays.asList(new int[]{7, 8, 9, 10, 11, 12}, new int[]{1, 2, 3, 4, 5, 7}, new int[] {1,2,3,4,5,6}))
+                        )
+
+                )
+        );
+    }
+
+    private static IssuedHistory createIssuedHistory(List<Lotto> autoLotto, List<Lotto> manualLotto) {
+        List<IssuedInformation> expectedIssuedInformation = new ArrayList<>();
+        if (Objects.nonNull(autoLotto) && !autoLotto.isEmpty()) {
+            expectedIssuedInformation.add(new IssuedInformation(IssuedMode.Auto, autoLotto));
+        }
+        if (Objects.nonNull(manualLotto) && !manualLotto.isEmpty()) {
+            expectedIssuedInformation.add(new IssuedInformation(IssuedMode.Manual, manualLotto));
+        }
+        return new IssuedHistory(expectedIssuedInformation);
+    }
+
+    private LottoGenerator createMockLottoGenerator(Lotto lotto) {
+        LottoGenerator spyLottoGenerator = spy(new LottoGenerator());
+        when(spyLottoGenerator.auto()).thenReturn(lotto);
+        return spyLottoGenerator;
+    }
+
+    private static List<Lotto> createManualLotto(List<int[]> dataList) {
+        return dataList.stream().map(LottoTest::createLottoNumberList).map(Lotto::new).collect(Collectors.toList());
     }
 }
