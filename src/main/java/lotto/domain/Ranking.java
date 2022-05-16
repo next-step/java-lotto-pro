94988e7b68cb75e68124736b9b45be50574f2bf0
Donghee Han <handh0413@gmail.com>
2022-05-17 00:45:58 +0900

    feat: 당첨 통계 출력 기능 수정

--- src/main/java/lotto/domain/Ranking.java
+++ src/main/java/lotto/domain/Ranking.java
@@ -3,13 +3,13 @@ package lotto.domain;
 import java.util.Arrays;
 import java.util.function.Predicate;
 
-public enum Ranking {
-    FIRST(6, false, 2_000_000_000),
-    SECOND(5, true, 30_000_000),
-    THIRD(5, false, 15_000_000),
-    FOURTH(4, false, 50_000),
+public enum Ranking implements Comparable<Ranking> {
+    NONE(0, false, 0),
     FIFTH(3, false, 5_000),
-    NONE(0, false, 0);
+    FOURTH(4, false, 50_000),
+    THIRD(5, false, 15_000_000),
+    SECOND(5, true, 30_000_000),
+    FIRST(6, false, 2_000_000_000);
 
     private int matchingCount;
     private boolean matchBonus;
@@ -53,4 +53,6 @@ public enum Ranking {
     public boolean isMatchBonus() {
         return matchBonus;
     }
+
+
 }
