f5ea20961b81960f3b3373ef7d66685d2d99c36b
toughchb <ggang2000@gmail.com>
2022-05-16 11:54:56 +0900

    test(LottoWinningResultsTest): winningRankCount 메소드 테스트 추가

--- src/test/java/lotto/domain/LottoWinningResultsTest.java
+++ src/test/java/lotto/domain/LottoWinningResultsTest.java
@@ -30,4 +30,17 @@ class LottoWinningResultsTest {
         double prizedMoney = lottoWinningResults.prizedMoney();
         assertThat(prizedMoney).isEqualTo(2_030_000_000);
     }
+
+    @Test
+    void 등수의_개수를_구할수_있다() {
+        List<LottoRank> ranks = new ArrayList<>();
+        ranks.add(LottoRank.FIRST);
+        ranks.add(LottoRank.SECOND);
+        ranks.add(LottoRank.SECOND);
+
+        LottoWinningResults lottoWinningResults = LottoWinningResults.from(ranks);
+
+        int rankCount = lottoWinningResults.winningRankCount(LottoRank.SECOND);
+        assertThat(rankCount).isEqualTo(2);
+    }
 }
