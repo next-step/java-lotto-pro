ee9df9c9aeb13f705308beebed54d79bb4c7b4b2
SSG\221287 <hyeok7777@gmail.com>
2022-05-16 16:34:50 +0900

    test(LottoTicketTest.java) : Lotto의 리스트인 LottoTicket 추가 및 정상생성 테스트 feat(LottoTicket.java) : Lotto 리스트 생성

---
+++ src/main/java/lottoauto/service/LottoTicket.java
@@ -0,0 +1,28 @@
+package lottoauto.service;
+
+import lottoauto.wrapper.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoTicket {
+    List<Lotto> listLotto;
+
+    public LottoTicket() {
+        this.listLotto = new ArrayList<>();
+    }
+
+    public void add(Lotto lotto) {
+        listLotto.add(lotto);
+    }
+
+    public void printAll() {
+        for (Lotto lotto : listLotto) {
+            System.out.println(lotto.toString());
+        }
+    }
+
+    public int size() {
+        return listLotto.size();
+    }
+}
