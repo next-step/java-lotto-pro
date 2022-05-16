68fff8038ae7849318f92809aa8c286aaee1db3c
boss.back <boss.back@kakaocorp.com>
2022-05-17 06:05:44 +0900

    feat: 자동으로 로또 번호 6개를 뽑는 기능 구현 및 테스트

--- src/test/java/lotto/LottoNumbersTest.java
+++ src/test/java/lotto/LottoNumbersTest.java
@@ -5,25 +5,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import lotto.domain.LottoNumber;
-import lotto.domain.LottoNumbers;
-import lotto.strategy.AutoPickNumberStrategy;
 import org.junit.jupiter.api.Test;
 
 public class LottoNumbersTest {
 
     @Test
     public void differentSixNumbers() {
-        //given
-        List<LottoNumber> numberList = new AutoPickNumberStrategy().pickLottoNumbers();
-        LottoNumbers lottoNumbers = new LottoNumbers(numberList);
-        Set<LottoNumber> lottoNumberSet = new HashSet<>(lottoNumbers.getValues());
-        int expectedSize = 6;
-
-        //when
-        int actualSize = lottoNumberSet.size();
-
-        //then
-        assertThat(actualSize).isEqualTo(expectedSize);
+        LottoNumbers lottoNumbers = new LottoNumbers();
+        List<LottoNumber> lottoNumberList = lottoNumbers.makeLottoNumbers(new AutoPickNumberStrategy());
+        Set<LottoNumber> lottoNumberSet = new HashSet<>(lottoNumberList);
+        assertThat(lottoNumberSet).hasSize(6);
     }
+
 }
