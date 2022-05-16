b60b45500570ae1a03adf30d5f1a76850e241152
iamjunsulee <mellow0924@gmail.com>
2022-05-16 13:04:31 +0900

    refactor(LottoNumber): 정적팩토리 메소드 작성

--- src/main/java/lotto/domain/LottoNumbers.java
+++ src/main/java/lotto/domain/LottoNumbers.java
@@ -15,7 +15,7 @@ public class LottoNumbers {
     private static final String SPLIT_DELIMITER = ",";
 
     private static final List<LottoNumber> LOTTO_NUMBERS = IntStream.rangeClosed(MIN_NUMBER, MAX_NUMBER)
-            .mapToObj(LottoNumber::new)
+            .mapToObj(LottoNumber::from)
             .collect(Collectors.toList());
 
     public static List<LottoNumber> generateLottoNumbers() {
@@ -29,7 +29,7 @@ public class LottoNumbers {
         return Arrays
                 .stream(lottoNumbers.replace(BLANK_FORMAT, REPLACE_FORMAT).split(SPLIT_DELIMITER))
                 .mapToInt(Integer::parseInt)
-                .mapToObj(LottoNumber::new)
+                .mapToObj(LottoNumber::from)
                 .collect(Collectors.toList());
     }
 }
