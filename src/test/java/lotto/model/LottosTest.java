db069a8e74b0d2c6611688bbe7d1d008a8c037bc
Philip <pch4623@gmail.com>
2022-05-17 01:55:54 +0900

    test(LottosTest): 등수별 카운트 테스트

--- src/test/java/lotto/model/LottosTest.java
+++ src/test/java/lotto/model/LottosTest.java
@@ -1,10 +1,10 @@
 package lotto.model;
 
+import lotto.enums.Rank;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,5 +23,29 @@ class LottosTest {
                 .hasFieldOrProperty("lottos")
                 .extracting("lottos")
                 .isInstanceOf(List.class);
+    }
+
+    @Test
+    @DisplayName("당첨번호와 비교하여 등수별로 카운트 한다.")
+    void rankCount_등수별_카운트() {
+        List<LottoNumbers> lottoNumbersList = Arrays.asList(
+                new LottoNumbers(Arrays.asList(2, 4, 20, 27, 43, 45)),  // LOSE
+                new LottoNumbers(Arrays.asList(1, 3, 5, 27, 40, 45)),   // LOSE
+                new LottoNumbers(Arrays.asList(1, 5, 25, 30, 40, 41)),  // FOURTH
+                new LottoNumbers(Arrays.asList(3, 5, 18, 37, 42, 45)),  // THIRD
+                new LottoNumbers(Arrays.asList(1, 5, 18, 25, 37, 43)),  // SECOND
+                new LottoNumbers(Arrays.asList(1, 5, 18, 25, 37, 44)),  // SECOND
+                new LottoNumbers(Arrays.asList(1, 5, 18, 25, 37, 42))   // FIRST
+        );
+        Lottos lottos = Lottos.from(lottoNumbersList);
+
+        LottoNumbers winningNumbers = new LottoNumbers(Arrays.asList(1, 5, 18, 25, 37, 42));
+
+        assertThat(lottos.rankCount(winningNumbers))
+                .isInstanceOf(Map.class)
+                .isExactlyInstanceOf(LinkedHashMap.class)
+                .containsKeys(Rank.values())
+                .extractingByKeys(Rank.values())
+                .containsExactly(1, 2, 1, 1, 2);
     }
 }
