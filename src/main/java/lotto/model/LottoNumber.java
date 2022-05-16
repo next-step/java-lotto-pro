570ee7c2a4f2eca73c0dbd54500799043f9abc23
KangSeongHyo <abc9023@naver.com>
2022-05-16 18:07:23 +0900

    refactor : Money, LottoNumber 클래스를 추가하여 책임 이관
    -  LottoStore에 있던 책임을 Money로 이관 하기 위한 테스트 이동

---
+++ src/main/java/lotto/model/LottoNumber.java
@@ -0,0 +1,28 @@
+package lotto.model;
+
+import lotto.constant.ErrorMessage;
+import lotto.constant.LottoRoleConst;
+
+public class LottoNumber {
+
+    private final int number;
+
+    public LottoNumber(String numberWord) {
+        this.number = parseLottoNumber(numberWord);
+    }
+    private int parseLottoNumber(String numberWord){
+        return validateLottoNumber(parseNumber(numberWord));
+    }
+    private int parseNumber(String numberWord) {
+        if (!numberWord.matches("\\p{Digit}+")) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_NUMBER);
+        }
+        return Integer.parseInt(numberWord);
+    }
+    private int validateLottoNumber(int number) {
+        if (number < LottoRoleConst.LOW_NUMBER || number > LottoRoleConst.MAX_NUMBER) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_LOTTO_NUMBER);
+        }
+        return number;
+    }
+}
--- src/test/java/lotto/model/LottoStoreTest.java
+++ src/test/java/lotto/model/LottoStoreTest.java
@@ -1,12 +1,10 @@
 package lotto.model;
 
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 class LottoStoreTest {
 
@@ -17,47 +15,6 @@ class LottoStoreTest {
     void issueLottoPaper(String moneyWord, int expectedPlayCount) {
         LottoStore lottoStore = new LottoStore(moneyWord);
         assertEquals(new LottoPaper(expectedPlayCount),lottoStore.issueLottoPaper());
-    }
-
-
-    @DisplayName("구매 금액이 1000원 미만(로또 최소 금액)인 경우")
-    @ParameterizedTest
-    @ValueSource(strings = {"100", "0", "900"})
-    void buyLottoCount_low_money(String moneyWord) {
-
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> new LottoStore(moneyWord))
-                .withMessage("[ERROR] 로또 최소 가격은 1000원 입니다.");
-    }
-
-    @DisplayName("구매 금액이 10_000_000원 이상인 경우")
-    @ParameterizedTest
-    @ValueSource(strings = {"10001000","100000000"})
-    void buyLottoCount_over_money(String moneyWord) {
-
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> new LottoStore(moneyWord))
-                .withMessage("[ERROR] 로또 구매 최대 가격은 10_000_000원 입니다.");
-    }
-
-    @DisplayName("구매 금액이 1000원 단위가 아닌 경우")
-    @ParameterizedTest
-    @ValueSource(strings = {"1500", "1200", "59900"})
-    void buyLottoCount_incorrect_unit(String moneyWord) {
-
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> new LottoStore(moneyWord))
-                .withMessage("[ERROR] 구매 금액은 1000원 단위로 입력해주세요.");
-    }
-
-    @DisplayName("잘못된 구매금액을 입력하는 경우")
-    @ParameterizedTest
-    @ValueSource(strings = {"천원", "2dollars", "$2"})
-    void buyLottoCount_incorrect_input_money(String moneyWord) {
-
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> new LottoStore(moneyWord))
-                .withMessage("[ERROR] 구매금액 변환에 오류가 발생했습니다.");
     }
 
 }
