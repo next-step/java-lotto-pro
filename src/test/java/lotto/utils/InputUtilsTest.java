4395e2fae53dd7b9a965e7b324138ebb28acc51e
Philip <pch4623@gmail.com>
2022-05-16 19:00:41 +0900

    test(InputUtilsTest): 리스트 타입 변환 테스트

--- src/test/java/lotto/utils/InputUtilsTest.java
+++ src/test/java/lotto/utils/InputUtilsTest.java
@@ -5,10 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static lotto.utils.InputUtils.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class InputUtilsTest {
 
@@ -30,5 +32,28 @@ class InputUtilsTest {
                 .hasSize(6)
                 .containsExactly("1", "2", "3", "4", "5", "6");
     }
+
+    @Test
+    @DisplayName("입력받은 String 리스트를 Integer 리스트로 변환한다.")
+    void convertToIntegerList_리스트_변환() {
+        List<String> inputList = Arrays.asList("1", "2", "3", "4", "5", "6");
+
+        assertThat(convertToIntegerList(inputList))
+                .isInstanceOf(List.class)
+                .hasOnlyElementsOfType(Integer.class)
+                .hasSize(6)
+                .containsExactly(1, 2, 3, 4, 5, 6);
+    }
+
+    @Test
+    @DisplayName("타입 변환 시 입력값에 숫자가 아닌 값이 존재하면 예외를 발생시킨다.")
+    void convertToIntegerList_예외발생() {
+        List<String> input = Arrays.asList("1", "2", "A", "4", "5", "6");
+
+        assertThatThrownBy(() -> convertToIntegerList(input))
+                .isExactlyInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 숫자만 입력이 가능합니다!");
+    }
+
 
 }
