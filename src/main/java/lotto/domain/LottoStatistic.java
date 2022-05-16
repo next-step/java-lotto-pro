b8ca5ad294778eee9973969cbc67dc7c8899c01f
jiyeonghwang <gyp0288@naver.com>
2022-05-17 04:07:48 +0900

    refactor: LottoStatistic enum type으로 변경

--- src/main/java/lotto/domain/LottoStatistic.java
+++ src/main/java/lotto/domain/LottoStatistic.java
@@ -1,81 +1,34 @@
 package lotto.domain;
 
-import java.util.Objects;
+import java.util.Arrays;
 
-public class LottoStatistic {
-    private static final int THREE_WINNING_AMOUNT = 5000;
-    private static final int FOUR_WINNING_AMOUNT = 50000;
-    private static final int FIVE_WINNING_AMOUNT = 1500000;
-    private static final int SIX_WINNING_AMOUNT = 2000000000;
+public enum LottoStatistic {
+    SIX_WINNING_AMOUNT(6, 2_000_000_000),
+    FIVE_WINNING_AMOUNT(5, 1_500_000),
+    FOUR_WINNING_AMOUNT(4, 50_000),
+    THREE_WINNING_AMOUNT(3, 5_000),
+    NOTHING(0, 0);
 
-    private int three;
-    private int four;
-    private int five;
-    private int six;
-    private double profitLate;
+    private int matchCount;
+    private int winningAmount;
 
-    public LottoStatistic() {
+    LottoStatistic(int matchCount, int winningAmount) {
+        this.matchCount = matchCount;
+        this.winningAmount = winningAmount;
     }
 
-    public LottoStatistic(int three, int four, int five, int six) {
-        this.three = three;
-        this.four = four;
-        this.five = five;
-        this.six = six;
+    public int getMatchCount() {
+        return this.matchCount;
     }
 
-    public double getProfitLate() {
-        return this.profitLate;
+    public int getWinningAmount() {
+        return this.winningAmount;
     }
 
-    public void calculatorCount(int count) {
-        if (count == 3) this.three++;
-        if (count == 4) this.four++;
-        if (count == 5) this.five++;
-        if (count == 6) this.six++;
-    }
-
-    public void calculatorProfitLate(int amount) {
-        double profit = 0;
-        profit += this.three * THREE_WINNING_AMOUNT;
-        profit += this.four * FOUR_WINNING_AMOUNT;
-        profit += this.five * FIVE_WINNING_AMOUNT;
-        profit += this.six * SIX_WINNING_AMOUNT;
-
-        double profitLate = Math.floor((profit / (double)amount) * 100) / 100.0;
-        this.profitLate = profitLate;
-    }
-
-    public String toResultString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("3개 일치 (" + THREE_WINNING_AMOUNT + "원)- " + this.three + "개");
-        sb.append("\n4개 일치 (" + FOUR_WINNING_AMOUNT +"원)- " + this.four + "개");
-        sb.append("\n5개 일치 (" + FIVE_WINNING_AMOUNT + "원)- " + this.five + "개");
-        sb.append("\n6개 일치 (" + SIX_WINNING_AMOUNT + "원)- " + this.six + "개");
-        sb.append("\n총 수익률은 " + this.profitLate + "입니다.");
-
-        if (this.profitLate < 1) {
-            sb.append("(기준이 1이기 때문에 결과적으로 손해라는 의미임)");
-        }
-
-        return sb.toString();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        LottoStatistic lottoStatistic = (LottoStatistic) o;
-
-        if (three != lottoStatistic.three) return false;
-        if (four != lottoStatistic.four) return false;
-        if (five != lottoStatistic.five) return false;
-        if (six != lottoStatistic.six) return false;
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(three, four, five, six);
+    public static LottoStatistic valueOf(int matchCount) {
+        return Arrays.stream(values())
+                .filter(lottoStatistic -> lottoStatistic.matchCount == matchCount)
+                .findFirst()
+                .orElse(LottoStatistic.NOTHING);
     }
 }
--- src/test/java/lotto/LottosTest.java
+++ src/test/java/lotto/LottosTest.java
@@ -1,12 +1,32 @@
 package lotto;
 
+import lotto.domain.Lotto;
+import lotto.domain.LottoStatistic;
 import lotto.domain.Lottos;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class LottosTest {
     @Test
     public void 로또_그룹_생성_후_출력() {
-        Lottos lottos = new Lottos(5);
-        lottos.printLottoList();
+        Lottos lottos = new Lottos();
+        lottos.autoGenerator(5);
+        assertThat(lottos.getLottoList().size()).isEqualTo(5);
+    }
+
+    @Test
+    public void 로또_매치() {
+        Lottos lottos = new Lottos();
+        String[] input = new String[2];
+        input[0] = "1,2,3,4,5,6";
+        input[1] = "1,2,3,7,8,9";
+        lottos.manualGenerator(input);
+        Lotto winningLotto = new Lotto("1,2,3,4,5,6");
+        List<LottoStatistic> list = lottos.matchLottoStatic(winningLotto);
+        assertThat(list.get(0)).isEqualTo(LottoStatistic.valueOf(6));
+        assertThat(list.get(1)).isEqualTo(LottoStatistic.valueOf(3));
     }
 }
