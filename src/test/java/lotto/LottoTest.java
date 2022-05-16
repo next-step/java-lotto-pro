aac1930373ad45ef743afee06a0d79323cdf9ae3
boss.back <boss.back@kakaocorp.com>
2022-05-17 07:19:36 +0900

    feat: 로또 넘버를 가지는 로또 클래스 생성 및 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -4,24 +4,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.HashSet;
 import java.util.Set;
-import lotto.domain.Lotto;
 import lotto.domain.LottoNumber;
 import lotto.strategy.AutoPickNumberStrategy;
 import org.junit.jupiter.api.Test;
 
 public class LottoTest {
-    
+
+
     @Test
     public void makeDifferentNumberLotto() {
-        //given
         Lotto lotto = new Lotto(new AutoPickNumberStrategy());
+        lotto.getNumbers().getValues();
         Set<LottoNumber> lottoNumberSet = new HashSet<>(lotto.getNumbers().getValues());
-        int expectedSize = 6;
-
-        //when
-        int actualSize = lottoNumberSet.size();
-
-        //then
-        assertThat(actualSize).isEqualTo(expectedSize);
+        assertThat(lottoNumberSet).hasSize(6);
     }
 }
