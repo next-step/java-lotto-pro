1c4729b09872e19f29ac69c659ea13b38448e8cb
gunan <power4889@gmail.com>
2022-05-17 00:53:14 +0900

    refactor: 클래스명을 명확한 의미로 변경

--- src/test/java/lotto/domain/RandomNumbersGeneratorTest.java
+++ src/test/java/lotto/domain/RandomNumbersGeneratorTest.java
@@ -7,24 +7,24 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class ShuffledNumbersTest {
-    ShuffledNumbers numbers = new ShuffledNumbers();
+class RandomNumbersGeneratorTest {
+    RandomNumbersGenerator numbers = new RandomNumbersGenerator();
 
     @ParameterizedTest
     @ValueSource(ints = {0, 45})
     void 숫자사이즈_0_45_뽑기(int size) {
-        assertThat(numbers.get(size)).hasSize(size);
+        assertThat(numbers.generate(size)).hasSize(size);
     }
 
     @Test
     void 숫자사이즈_마이너스() {
-        assertThatThrownBy(() -> numbers.get(-1))
+        assertThatThrownBy(() -> numbers.generate(-1))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 숫자사이즈_45초과() {
-        assertThatThrownBy(() -> numbers.get(46))
+        assertThatThrownBy(() -> numbers.generate(46))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
