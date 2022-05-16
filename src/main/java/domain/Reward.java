73981f5214524b59cc70a292e122e91f79188070
seok2 <seogi89@gmail.com>
2022-05-16 20:51:19 +0900

    <feat> 로또 당첨금을 Money 객체로 관리한다.

--- src/main/java/domain/Reward.java
+++ src/main/java/domain/Reward.java
@@ -4,16 +4,16 @@ import java.util.Arrays;
 
 public enum Reward {
 
-    FIRST(6, 200_000_000),
-    SECOND(5, 15_000_00),
-    THIRD(4, 50_000),
-    FOURTH(3, 5_000),
-    MISS(0, 0);
+    FIRST(6, new Money(200_000_000L)),
+    SECOND(5, new Money(15_000_00)),
+    THIRD(4, new Money(50_000)),
+    FOURTH(3, new Money(5_000)),
+    MISS(0, new Money(0));
 
     private final int numberOfMatches;
-    private final int prize;
+    private final Money prize;
 
-    Reward(int numberOfMatches, int prize) {
+    Reward(int numberOfMatches, Money prize) {
         this.numberOfMatches = numberOfMatches;
         this.prize = prize;
     }
