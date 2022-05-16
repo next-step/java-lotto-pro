cd67f43e553d70f6eef099d21b38c046ab83b14c
bestsilver <dkvkxm99@gmail.com>
2022-05-16 20:25:29 +0900

    refactor: List를 Set으로 변경하여 Duplicate 로직 삭제

--- src/main/java/lotto_auto/model/LottoNumbers.java
+++ src/main/java/lotto_auto/model/LottoNumbers.java
@@ -3,68 +3,49 @@ package lotto_auto.model;
 import java.util.*;
 
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class LottoNumbers {
-    List<LottoNumber> lottoNumberList;
+    private final Set<LottoNumber> lottoNumberSet;
 
     public static final int VALID_SIZE = 6;
     public static final String LOTTO_DELIMITER = ", ";
 
     public static final String NOT_MATCHED_NUMBER_SIZE = "[ERROR] 로또 번호는 6개의 숫자가 필요합니다.";
     public static final String EXIST_DUPLICATE_VALUE = "[ERROR] 로또 번호는 중복 숫자가 존재할 수 없습니다.";
+    public static final String NOT_NUMBER = "[ERROR] 로또의 번호에 숫자 이외의 값이 존재합니다.";
 
-    public LottoNumbers() {
-        this.lottoNumberList = generateRandomLottoNumbers();
+    public LottoNumbers(List<LottoNumber> numberList) {
+        this.lottoNumberSet = Collections.unmodifiableSet(new HashSet<>(numberList));
+        checkNumbersSize();
     }
 
     public LottoNumbers(String stringNumbers) {
-        this.lottoNumberList = Arrays.stream(stringNumbers.split(LOTTO_DELIMITER))
-                .map(LottoNumber::new)
-                .collect(Collectors.toList());
-        checkNumbersSize();
-        checkDuplicateNumber();
+        try {
+            this.lottoNumberSet =
+                    Collections.unmodifiableSet(Arrays.stream(stringNumbers.split(LOTTO_DELIMITER))
+                            .map(Integer::parseInt)
+                            .map(LottoNumber::new)
+                            .collect(Collectors.toSet()));
+
+            checkNumbersSize();
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NOT_NUMBER);
+        }
     }
 
     private void checkNumbersSize() {
-        if (!(lottoNumberList.size() == VALID_SIZE)) {
+        if (!(lottoNumberSet.size() == VALID_SIZE)) {
             throw new IllegalArgumentException(NOT_MATCHED_NUMBER_SIZE);
         }
     }
 
-    private void checkDuplicateNumber() {
-        Set<LottoNumber> lottoNumbersSet = new HashSet<>();
-        boolean isUnique = this.lottoNumberList.stream().allMatch(lottoNumbersSet::add);
-
-        if (!isUnique) {
-            throw new IllegalArgumentException(EXIST_DUPLICATE_VALUE);
-        }
-    }
-
-    public List<LottoNumber> getLottoNumberList() {
-        return lottoNumberList;
+    public Set<LottoNumber> getLottoNumberSet() {
+        return lottoNumberSet;
     }
 
     public int countSameLottoNumber(LottoNumbers numbers) {
-        return (int) this.lottoNumberList
+        return (int) this.lottoNumberSet
                 .stream()
-                .filter(lottoNumber -> numbers.getLottoNumberList().contains(lottoNumber)).count();
+                .filter(lottoNumber -> numbers.getLottoNumberSet().contains(lottoNumber)).count();
     }
-
-    private List<Integer> getRandomNumberList() {
-        List<Integer> numbers = IntStream
-                .range(LottoNumber.MIN_NUMBER, LottoNumber.MAX_NUMBER + 1)
-                .boxed()
-                .collect(Collectors.toList());
-        Collections.shuffle(numbers);
-        return numbers.subList(0, LottoNumbers.VALID_SIZE);
-    }
-
-    private List<LottoNumber> generateRandomLottoNumbers() {
-        return getRandomNumberList()
-                .stream()
-                .map(LottoNumber::new)
-                .collect(Collectors.toList());
-    }
-
 }
