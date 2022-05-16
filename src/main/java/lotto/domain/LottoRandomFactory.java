241d5c7ecea8e02c69a87438760dbaf499da7588
soob-forest <dltntnqo@naver.com>
2022-05-16 23:52:42 +0900

    feat(LottoRandomFactory): 랜덤 로또 1개를 생성하기 위한 클래스

---
+++ src/main/java/lotto/domain/LottoRandomFactory.java
@@ -0,0 +1,23 @@
+package lotto.domain;
+
+public class LottoRandomFactory {
+    private final RandomNumberMachine randomNumberMachine;
+    private final Money price;
+    private final int numberSize;
+
+    public LottoRandomFactory(RandomNumberMachine randomNumberMachine, Money price, int numberSize) {
+        this.randomNumberMachine = randomNumberMachine;
+        this.price = price;
+        this.numberSize = numberSize;
+    }
+
+    public Lotto create() {
+        LottoNumber[] lottoNumberList = new LottoNumber[numberSize];
+        for (int index = 0; index < numberSize; index++) {
+            lottoNumberList[index] = randomNumberMachine.popRandomNumber();
+        }
+        randomNumberMachine.refill();
+
+        return new Lotto(price, lottoNumberList);
+    }
+}
