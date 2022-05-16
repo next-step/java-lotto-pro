e2d72f44212c4078d2252f734483569a246e7e69
Ttungga <torb2002@gmail.com>
2022-05-16 23:21:14 +0900

    refactor: change @Test to @ParameterizedTest

--- src/test/java/lotto/domain/LottoNumbersTest.java
+++ src/test/java/lotto/domain/LottoNumbersTest.java
@@ -6,11 +6,17 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class LottoNumbersTest {
+    private static final LottoNumbers winningNumbers = new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6));
+
     @Test
     @DisplayName("6개의 숫자 배열을 파라미터로 로또 번호가 생성되어야 한다")
     void create_by_number_list() {
@@ -54,64 +60,28 @@ public class LottoNumbersTest {
         }
     }
 
-    @Test
-    @DisplayName("로또 번호가 6개가 아니면 IllegalArgumentException을 발생시킨다")
-    void when_amount_of_numbers_is_not_six_should_throw_IllegalArgumentException() {
-        // given
-        final List<Integer> empty = new ArrayList<>();
-        final List<Integer> lessThanSix = Arrays.asList(1);
-        final List<Integer> greaterThanSix = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
-        final List<List<Integer>> numbersList = new ArrayList<>();
-        numbersList.add(empty);
-        numbersList.add(lessThanSix);
-        numbersList.add(greaterThanSix);
-
-        // when and then
-        for (List<Integer> numbers : numbersList) {
-            Assertions.assertThatThrownBy(() -> new LottoNumbers(numbers))
-                    .isInstanceOf(IllegalArgumentException.class);
-        }
+    private static Stream<Arguments> numbersNotHavingSixNumbers() {
+        return Stream.of(Arguments.of(new ArrayList<>()),
+                Arguments.of(Arrays.asList(1)),
+                Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 6, 7)));
     }
 
-    @Test
-    @DisplayName("1보다 작거나 45보다 큰 번호가 하나라도 있으면 IllegalrgumentException을 발생시킨다")
-    void when_number_is_invalid_should_throw_IllegalArgumentException() {
-        // given
-        final List<Integer> zero = Arrays.asList(1, 2, 3, 4, 5, 0);
-        final List<Integer> lessThanZero = Arrays.asList(-1, 1, 2, 3, 4, 5);
-        final List<Integer> greaterThanFortyFIve = Arrays.asList(1, 2, 46, 3, 4, 5);
-        final List<List<Integer>> numbersList = new ArrayList<>();
-        numbersList.add(zero);
-        numbersList.add(lessThanZero);
-        numbersList.add(greaterThanFortyFIve);
-
-        // when and then
-        for (List<Integer> numbers : numbersList) {
-            Assertions.assertThatThrownBy(() -> new LottoNumbers(numbers))
-                    .isInstanceOf(IllegalArgumentException.class);
-        }
+    private static Stream<Arguments> numbersHavingLessThanOneOrGreaterThanFortyFive() {
+        return Stream.of(Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 0)),
+                Arguments.of(Arrays.asList(-1, 1, 2, 3, 4, 5)),
+                Arguments.of(Arrays.asList(1, 2, 46, 3, 4, 5)));
     }
 
-    @Test
-    @DisplayName("주어진 번호가 3개 미만으로 일치하면 0이 반환되어야 한다")
-    void when_no_matches_should_return_0() {
-        // given
-        final LottoNumbers winningNumbers = new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6));
-        final LottoNumbers noMatchesNumbers = new LottoNumbers(Arrays.asList(13, 21, 34, 37, 41, 44));
-        final LottoNumbers oneMatchesNumbers = new LottoNumbers(Arrays.asList(3, 21, 34, 37, 41, 44));
-        final LottoNumbers twoMatchesNumbers = new LottoNumbers(Arrays.asList(2, 5, 34, 37, 41, 44));
-
-        // when and then
-        assertThat(noMatchesNumbers.matches(winningNumbers)).isEqualTo(0);
-        assertThat(oneMatchesNumbers.matches(winningNumbers)).isEqualTo(0);
-        assertThat(twoMatchesNumbers.matches(winningNumbers)).isEqualTo(0);
+    private static Stream<Arguments> lottoNumbersMatchingLessThanThreeNumbers() {
+        return Stream.of(Arguments.of(new LottoNumbers(Arrays.asList(13, 21, 34, 37, 41, 44))),
+                Arguments.of(new LottoNumbers(Arrays.asList(3, 21, 34, 37, 41, 44))),
+                Arguments.of(new LottoNumbers(Arrays.asList(2, 5, 34, 37, 41, 44))));
     }
 
     @Test
     @DisplayName("주어진 번호가 3개 일치하면 3이 반환되어야 한다")
     void when_three_matches_should_return_3() {
         // given
-        final LottoNumbers winningNumbers = new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6));
         final LottoNumbers numbers = new LottoNumbers(Arrays.asList(2, 3, 6, 37, 41, 44));
 
         // when and then
@@ -122,7 +92,6 @@ public class LottoNumbersTest {
     @DisplayName("주어진 번호가 4개 일치하면 4가 반환되어야 한다")
     void when_four_matches_should_return_4() {
         // given
-        final LottoNumbers winningNumbers = new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6));
         final LottoNumbers numbers = new LottoNumbers(Arrays.asList(1, 3, 5, 6, 41, 44));
 
         // when and then
@@ -133,7 +102,6 @@ public class LottoNumbersTest {
     @DisplayName("주어진 번호가 5개 일치하면 5가 반환되어야 한다")
     void when_five_matches_should_return_5() {
         // given
-        final LottoNumbers winningNumbers = new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6));
         final LottoNumbers numbers = new LottoNumbers(Arrays.asList(1, 3, 4, 5, 6, 44));
 
         // when and then
@@ -144,10 +112,35 @@ public class LottoNumbersTest {
     @DisplayName("주어진 번호가 6개 일치하면 6이 반환되어야 한다")
     void when_six_matches_should_return_6() {
         // given
-        final LottoNumbers winningNumbers = new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6));
         final LottoNumbers numbers = new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6));
 
         // when and then
         assertThat(numbers.matches(winningNumbers)).isEqualTo(6);
+    }
+
+    @ParameterizedTest
+    @MethodSource("numbersNotHavingSixNumbers")
+    @DisplayName("로또 번호가 6개가 아니면 IllegalArgumentException을 발생시킨다")
+    void when_amount_of_numbers_is_not_six_should_throw_IllegalArgumentException(final List<Integer> numbers) {
+        // when and then
+        Assertions.assertThatThrownBy(() -> new LottoNumbers(numbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @MethodSource("numbersHavingLessThanOneOrGreaterThanFortyFive")
+    @DisplayName("1보다 작거나 45보다 큰 번호가 하나라도 있으면 IllegalrgumentException을 발생시킨다")
+    void when_number_is_invalid_should_throw_IllegalArgumentException(final List<Integer> numbers) {
+        // when and then
+        Assertions.assertThatThrownBy(() -> new LottoNumbers(numbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @MethodSource("lottoNumbersMatchingLessThanThreeNumbers")
+    @DisplayName("주어진 번호가 3개 미만으로 일치하면 0이 반환되어야 한다")
+    void when_no_matches_should_return_0(final LottoNumbers lottoNumbers) {
+        // when and then
+        assertThat(lottoNumbers.matches(winningNumbers)).isEqualTo(0);
     }
 }
--- src/test/java/lotto/domain/LottoTicketTest.java
+++ src/test/java/lotto/domain/LottoTicketTest.java
@@ -3,10 +3,16 @@ package lotto.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class LottoTicketTest {
+    private static final LottoNumbers winningNumbers = new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6));
+
     @Test
     @DisplayName("LottoNumbers 객체를 파라미터로 LottoTicket 객체가 생성되어야 한다")
     void create() {
@@ -21,26 +27,16 @@ public class LottoTicketTest {
         assertThat(lottoTicket).isEqualTo(new LottoTicket(lottoNumbers));
     }
 
-    @Test
-    @DisplayName("번호가 3개 미만으로 일치하면 Prize.NO_MATCHES가 반환되어야 한다")
-    void when_no_matches() {
-        // given
-        final LottoNumbers winningNumbers = new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6));
-        final LottoTicket noMatchesTicket = new LottoTicket(new LottoNumbers(Arrays.asList(13, 21, 34, 37, 41, 44)));
-        final LottoTicket oneMatchesTicket = new LottoTicket(new LottoNumbers(Arrays.asList(3, 21, 34, 37, 41, 44)));
-        final LottoTicket twoMatchesTicket = new LottoTicket(new LottoNumbers(Arrays.asList(2, 5, 34, 37, 41, 44)));
-
-        // when and then
-        assertThat(noMatchesTicket.prize(winningNumbers)).isEqualTo(Prize.NO_MATCHES);
-        assertThat(oneMatchesTicket.prize(winningNumbers)).isEqualTo(Prize.NO_MATCHES);
-        assertThat(twoMatchesTicket.prize(winningNumbers)).isEqualTo(Prize.NO_MATCHES);
+    private static Stream<Arguments> lottoTicketMatchingLessThanThreeNumbers() {
+        return Stream.of(Arguments.of(new LottoTicket(new LottoNumbers(Arrays.asList(13, 21, 34, 37, 41, 44)))),
+                Arguments.of(new LottoTicket(new LottoNumbers(Arrays.asList(3, 21, 34, 37, 41, 44)))),
+                Arguments.of(new LottoTicket(new LottoNumbers(Arrays.asList(2, 5, 34, 37, 41, 44)))));
     }
 
     @Test
     @DisplayName("번호가 3개 일치하면 Prize.THREE_MATCHES이 반환되어야 한다")
     void when_three_matches() {
         // given
-        final LottoNumbers winningNumbers = new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6));
         final LottoTicket ticket = new LottoTicket(new LottoNumbers(Arrays.asList(2, 3, 6, 37, 41, 44)));
 
         // when and then
@@ -51,7 +47,6 @@ public class LottoTicketTest {
     @DisplayName("번호가 4개 일치하면 Prize.FOUR_MATCHES가 반환되어야 한다")
     void when_four_matches() {
         // given
-        final LottoNumbers winningNumbers = new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6));
         final LottoTicket ticket = new LottoTicket(new LottoNumbers(Arrays.asList(1, 3, 5, 6, 41, 44)));
 
         // when and then
@@ -62,7 +57,6 @@ public class LottoTicketTest {
     @DisplayName("번호가 5개 일치하면 Prize.FIVE_MATCHES가 반환되어야 한다")
     void when_five_matches() {
         // given
-        final LottoNumbers winningNumbers = new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6));
         final LottoTicket ticket = new LottoTicket(new LottoNumbers(Arrays.asList(1, 3, 4, 5, 6, 44)));
 
         // when and then
@@ -73,10 +67,17 @@ public class LottoTicketTest {
     @DisplayName("번호가 6개 일치하면 Prize.SIX_MATCHES이 반환되어야 한다")
     void when_six_matches() {
         // given
-        final LottoNumbers winningNumbers = new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6));
         final LottoTicket ticket = new LottoTicket(new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6)));
 
         // when and then
         assertThat(ticket.prize(winningNumbers)).isEqualTo(Prize.SIX_MATCHES);
+    }
+
+    @ParameterizedTest
+    @MethodSource("lottoTicketMatchingLessThanThreeNumbers")
+    @DisplayName("번호가 3개 미만으로 일치하면 Prize.NO_MATCHES가 반환되어야 한다")
+    void when_no_matches(final LottoTicket lottoTicket) {
+        // when and then
+        assertThat(lottoTicket.prize(winningNumbers)).isEqualTo(Prize.NO_MATCHES);
     }
 }
