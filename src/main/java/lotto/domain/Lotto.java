4254a6d53a69bb0f20c3c125d95910b1471ac982
soob-forest <dltntnqo@naver.com>
2022-05-16 23:44:36 +0900

    feat(Lotto): 로또 클래스 생성
    - 로또 숫자 개수, 중복 숫자 validate 추가
    - 매치 결과 반환 멤버함수 추가

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,50 @@
+package lotto.domain;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Lotto {
+    private static final int DEFAULT_NUMBER_SIZE = 6;
+    private final Money price;
+    private final List<LottoNumber> lottoNumbers;
+    private final int lottoNumberSize;
+
+    public Lotto(Money price, LottoNumber[] lottoNumbers) {
+        this.price = price;
+        this.lottoNumberSize = DEFAULT_NUMBER_SIZE;
+        this.lottoNumbers = Arrays.asList(lottoNumbers);
+        validateNumbersCount();
+        validateDuplicated();
+    }
+
+
+    public MatchResult match(List<LottoNumber> prizeNumbers) {
+        int matchCount = 0;
+        for (LottoNumber prizeNumber : prizeNumbers) {
+            if (lottoNumbers.contains(prizeNumber)) {
+                matchCount++;
+            }
+        }
+        return MatchResult.from(matchCount);
+    }
+
+    private void validateDuplicated() {
+        if (hasDuplicatedLottoNumber()) {
+            throw new IllegalArgumentException("로또는 중복된 숫자를 가질 수 없습니다.");
+        }
+    }
+
+    private void validateNumbersCount() {
+        if (lottoNumberSize != this.lottoNumbers.size()) {
+            throw new IllegalArgumentException("로또는 6자리 숫자이어야 합니다.");
+        }
+    }
+
+    private boolean hasDuplicatedLottoNumber() {
+        Set<LottoNumber> lottoNumberSet = new HashSet<>(this.lottoNumbers);
+        return this.lottoNumbers.size() != lottoNumberSet.size();
+    }
+
+}
