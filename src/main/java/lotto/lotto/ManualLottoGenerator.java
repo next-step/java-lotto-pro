3b66771688dea5d85324202e055dc7a6c34926a1
WonChul Heo <heowc1992@gmail.com>
2022-05-16 12:55:41 +0900

    feat(ManualLottoGenerator) : ManualLottoGenerator 구현

--- src/main/java/lotto/lotto/ManualLottoGenerator.java
+++ src/main/java/lotto/lotto/ManualLottoGenerator.java
@@ -1,22 +1,32 @@
 package lotto.lotto;
 
 import java.util.List;
+import static java.util.Objects.requireNonNull;
 
 public class ManualLottoGenerator implements BatchLottoGenerator {
 
     private final List<String> rawLottoes;
-
+    private int tryCount;
     public ManualLottoGenerator(ManualLottoes manualLottoes) {
-        this.rawLottoes = manualLottoes.lottoes();
+        requireNonNull(manualLottoes, "manualLottoes");
+        this.rawLottoes = requireNonNull(manualLottoes.lottoes(), "rawLottoes");
     }
 
     @Override
     public Lotto generate() {
-        return null;
+        if (!hasMore()) {
+            throw new CanNotGenerateLottoException();
+        }
+        try {
+            final LottoGenerator lottoGenerator = LottoGenerator.commaSplitting(rawLottoes.get(tryCount));
+            return lottoGenerator.generate();
+        } finally {
+            tryCount++;
+        }
     }
 
     @Override
     public boolean hasMore() {
-        return false;
+        return rawLottoes.size() > tryCount;
     }
 }
