a49be5bbfb41638bf09674a87ad9a6c621995f68
tasklet1579 <tasklet1579@gmail.com>
2022-05-16 11:02:12 +0900

    feat(Lotteries) : 1개 이상의 로또를 포함하는 클래스

---
+++ src/main/java/lotto/vo/Lotteries.java
@@ -0,0 +1,49 @@
+package lotto.vo;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Lotteries {
+    private final static String LOTTERIES_WERE_NOT_GENERATED = "로또가 생성되지 않았습니다.";
+
+    private List<Lottery> lotteries;
+
+    public Lotteries(List<Lottery> lotteries) {
+        if (isNull(lotteries)) {
+            throw new NullPointerException(LOTTERIES_WERE_NOT_GENERATED);
+        }
+        this.lotteries = lotteries;
+    }
+
+    private boolean isNull(List<Lottery> lotteries) {
+        return lotteries == null;
+    }
+
+    public List<Lottery> list() {
+        return lotteries;
+    }
+
+    public int size() {
+        return lotteries.size();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Lotteries lotteries1 = (Lotteries) o;
+        return Objects.equals(lotteries, lotteries1.lotteries);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lotteries);
+    }
+
+    @Override
+    public String toString() {
+        return "Lotteries{" +
+                "lotteries=" + lotteries +
+                '}';
+    }
+}
---
+++ src/test/java/lotto/vo/LotteriesTest.java
@@ -0,0 +1,12 @@
+package lotto.vo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LotteriesTest {
+    @Test
+    void 로또_생성_예외() {
+        assertThatThrownBy(() -> new Lotteries(null)).isInstanceOf(NullPointerException.class);
+    }
+}
