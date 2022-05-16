818aa591be8e6eaa5bf0155ae04e7438ea3770f7
KangSeongHyo <abc9023@naver.com>
2022-05-16 21:26:45 +0900

    refactor : LottoNumbersTest 테스트 누락 메서드 추가

--- src/test/java/lotto/model/LottoNumbersTest.java
+++ src/test/java/lotto/model/LottoNumbersTest.java
@@ -1,5 +1,6 @@
 package lotto.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,4 +26,37 @@ class LottoNumbersTest {
                 .withMessage("[ERROR] 6개의 로또 번호에 중복이 있습니다.");
     }
 
+    @DisplayName("LottoNumbers에 특정 LottoNumber가 있는지 확인")
+    @Test
+    void contains() {
+        LottoNumbers lottoNumbers = new LottoNumbers((Arrays.asList("3", "7", "10", "13", "25", "35")));
+        assertAll(
+                ()-> assertThat(lottoNumbers.contains(new LottoNumber("7"))).isTrue(),
+                ()-> assertThat(lottoNumbers.contains(new LottoNumber("25"))).isTrue(),
+                ()-> assertThat(lottoNumbers.contains(new LottoNumber("35"))).isTrue()
+        );
+    }
+
+    @DisplayName("LottoNumbers에 로또 번호들이 int 배열로 변환")
+    @Test
+    void numberToIntArray() {
+        LottoNumbers lottoNumbers = new LottoNumbers((Arrays.asList("3", "7", "10", "13", "25", "35")));
+        assertThat(lottoNumbers.numberToIntArray()).isEqualTo(new int[]{3,7,10,13,25,35});
+    }
+
+    @DisplayName("당첨로또번호와 구매 로또 번호 일치 개수 확인")
+    @Test
+    void matchCount() {
+        LottoNumbers lottoNumbers = new LottoNumbers((Arrays.asList("3", "7", "10", "13", "25", "35")));
+        LottoNumbers winningNumbers3 = new LottoNumbers((Arrays.asList("3", "7", "10", "15", "20", "37")));
+        LottoNumbers winningNumbers4 = new LottoNumbers((Arrays.asList("3", "7", "10", "13", "20", "37")));
+        LottoNumbers winningNumbers5 = new LottoNumbers((Arrays.asList("3", "7", "10", "13", "25", "37")));
+        LottoNumbers winningNumbers6 = new LottoNumbers((Arrays.asList("3", "7", "10", "13", "25", "35")));
+        assertAll(
+                ()->assertThat(lottoNumbers.matchCount(winningNumbers3)).isEqualTo(3),
+                ()->assertThat(lottoNumbers.matchCount(winningNumbers4)).isEqualTo(4),
+                ()->assertThat(lottoNumbers.matchCount(winningNumbers5)).isEqualTo(5),
+                ()->assertThat(lottoNumbers.matchCount(winningNumbers6)).isEqualTo(6)
+        );
+    }
 }
