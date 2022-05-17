da1e08c0c77483e412cd0de87f751bb883c509d5
ssamzag <ssamzag@gmail.com>
2022-05-18 01:56:58 +0900

    feat(InputView, ResultView): 입력 및 화면출력을 담당하는 클래스

--- src/main/java/lotto/ui/InputView.java
+++ src/main/java/lotto/ui/InputView.java
@@ -1,2 +1,19 @@
-package lotto.ui;public class InputView {
+package lotto.ui;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final String INPUT_BUY_PRICE_MESSAEG = "구매금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
+
+    public static String getBuyPrice() {
+        System.out.println(INPUT_BUY_PRICE_MESSAEG);
+        return scanner.nextLine();
+    }
+
+    public static String getWinningNumbers() {
+        System.out.println(INPUT_WINNING_MESSAGE);
+        return scanner.nextLine();
+    }
 }
