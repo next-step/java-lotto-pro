f50840eeb9c7f0bf0ea367dfc12b40069e0bd8ef
millennion-jin <Jadestern2@>
2022-05-18 03:13:17 +0900

    feat(LottoPlay) : 게임 실행 클래스 구현

---
+++ src/main/java/lotto/LottoMain.java
@@ -0,0 +1,10 @@
+package lotto;
+
+import lotto.domain.LottoPlay;
+
+public class LottoMain {
+    public static void main(String[] args) {
+        LottoPlay lottoPlay = new LottoPlay();
+        lottoPlay.play();
+    }
+}
