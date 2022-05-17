1abbdbe3f51a84664563751eb0e51d465283fde2
breezeunn <je7689@gmail.com>
2022-05-17 23:23:42 +0900

    refactor(LOTTO): InputView 내 Scanner 재활용 가능한 형태로 변경

--- src/main/java/lotto/ui/InputView.java
+++ src/main/java/lotto/ui/InputView.java
@@ -4,13 +4,18 @@ import java.util.Scanner;
 
 public class InputView {
 
+    private static Scanner scanner;
+
+    public InputView() {
+        this.scanner = new Scanner(System.in);
+    }
+
     public static int getPurchasePrice() {
         System.out.println("구입금액을 입력해 주세요.");
 
-        Scanner sc = new Scanner(System.in);
         int purchasePrice;
         try {
-            purchasePrice = sc.nextInt();
+            purchasePrice = scanner.nextInt();
         } catch (Exception e) {
             throw new IllegalArgumentException("The purchase price should contain only numbers.");
         }
@@ -20,7 +25,6 @@ public class InputView {
 
     public static String getWinnerNumbers() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-        Scanner sc = new Scanner(System.in);
-        return sc.nextLine();
+        return scanner.nextLine();
     }
 }
