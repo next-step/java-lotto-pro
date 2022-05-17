a8194157dda496c95c03d8a9f416607b362e99e2
ssamzag <ssamzag@gmail.com>
2022-05-18 01:53:55 +0900

    feat(Lottos): 구매한 로또번호 상태를 가지는 클래스

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,2 +1,51 @@
-package lotto.domain;public class Lottos {
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+public class Lottos {
+    private final List<Lotto> lottos;
+
+    public Lottos() {
+        this(new ArrayList<>());
+    }
+
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public void add(Lotto lotto) {
+        this.lottos.add(lotto);
+    }
+
+    public List<Lotto> getLottos() {
+        return Collections.unmodifiableList(lottos);
+    }
+
+    public LottoResult getLottoResult(Lotto winningLotto) {
+        return new LottoResult(lottos, winningLotto);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Lottos lottos1 = (Lottos) o;
+        return Objects.equals(lottos, lottos1.lottos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottos);
+    }
+
+    @Override
+    public String
+    toString() {
+        return "Lottos{" +
+                "lottos=" + lottos +
+                '}';
+    }
 }
--- src/test/java/lotto/domain/LottosTest.java
+++ src/test/java/lotto/domain/LottosTest.java
@@ -1,4 +1,24 @@
+package lotto.domain;
+
+import lotto.domain.factory.LottoFactory;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
 class LottosTest {
-  
+
+    @Test
+    @DisplayName("로또를 2번 추가하면 Lottos 사이즈는 2이다")
+    void add() {
+        Lottos lottos = new Lottos();
+        lottos.add(LottoFactory.createAuto());
+        lottos.add(LottoFactory.createAuto());
+        assertThat(lottos.getLottos()).hasSize(2);
+
+    }
+
 }
