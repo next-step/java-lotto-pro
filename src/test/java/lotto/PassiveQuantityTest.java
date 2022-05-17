cfe4cfcfc0d0cceb2de93acb2361ee6d5bce8f6e
wenodev <logicalstark1105@gmail.com>
2022-05-17 11:30:31 +0900

    refactor: package 변경한다

--- src/test/java/lotto/PassiveQuantityTest.java
+++ src/test/java/lotto/PassiveQuantityTest.java
@@ -1,5 +1,6 @@
-package lotto.model;
+package lotto;
 
+import lotto.model.PassiveQuantity;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
