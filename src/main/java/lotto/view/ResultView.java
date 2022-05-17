92ba610099cffa96d2613fa679d57dfe5872c9db
Celina <luv.mins99@gmail.com>
2022-05-17 14:24:41 +0900

    refactor(ResultView): 입력 파라미터를 불변으로 리팩토링

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -13,21 +13,21 @@ public class ResultView {
     private static final String BENEFIT_MESSAGE = "총 수익률은 %.2f입니다. (기준이 1이기 때문에 결과적으로 이익이라는 의미임)";
     private static final String DIS_BENEFIT_MESSAGE = "총 수익률은 %.2f입니다. (기준이 1이기 때문에 결과적으로 손해라는 의미임)";
 
-    public void printGameStart(int amount, List<Lotto> userLotto) {
+    public void printGameStart(final int amount, final List<Lotto> userLotto) {
         System.out.println(amount + LOTTO_AMOUNT_MESSAGE);
         for (Lotto lotto : userLotto) {
             System.out.println(lotto.toString());
         }
     }
 
-    public void printGameResult(Map<Rank, Integer> gameResult) {
+    public void printGameResult(final Map<Rank, Integer> gameResult) {
         System.out.println(GAME_RESULT_MESSAGE);
         for (Rank result : gameResult.keySet()) {
             printStatisticResult(result, gameResult.get(result));
         }
     }
 
-    public void printBenefitResult(double benefit, double referenceValue) {
+    public void printBenefitResult(final double benefit, final double referenceValue) {
         if (benefit >= referenceValue) {
             System.out.printf(BENEFIT_MESSAGE, benefit);
             return;
@@ -35,7 +35,7 @@ public class ResultView {
         System.out.printf(DIS_BENEFIT_MESSAGE, benefit);
     }
 
-    private void printStatisticResult(Rank result, int matchResult) {
+    private void printStatisticResult(final Rank result, final int matchResult) {
         if (result == Rank.SECOND) {
             System.out.printf(GAME_BONUS_STATISTICS_MESSAGE,
                     result.getCountOfMatch(), result.getWinningMoney(), matchResult);
