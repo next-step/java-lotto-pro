315583237d721e170acf477754b0400e03864e92
chj1768 <chj1768@gmail.com>
2022-05-18 02:40:39 +0900

    수익률 확인 테스트 추가

--- src/test/java/VendorTest.java
+++ src/test/java/VendorTest.java
@@ -1,5 +1,8 @@
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -12,5 +15,27 @@ public class VendorTest {
     @Test
     void 가격_미만이면_로또를_구매할_수_없다() {
         assertThatThrownBy(() -> new Vendor().buy(Vendor.LOTTO_PRICE - 1)).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    void 수익률을_계산할_수_있다() {
+        Ranks ranks = new Ranks();
+        ranks.add(Rank.NONE);
+        ranks.add(Rank.NONE);
+        ranks.add(Rank.NONE);
+        ranks.add(Rank.NONE);
+        ranks.add(Rank.NONE);
+        ranks.add(Rank.NONE);
+        ranks.add(Rank.NONE);
+        ranks.add(Rank.NONE);
+        ranks.add(Rank.NONE);
+        ranks.add(Rank.NONE);
+        ranks.add(Rank.NONE);
+        ranks.add(Rank.NONE);
+        ranks.add(Rank.NONE);
+        ranks.add(Rank.FIFTH);
+
+        BigDecimal yield = new Vendor().yield(ranks);
+        assertThat(yield.setScale(2, RoundingMode.DOWN).doubleValue()).isEqualTo(0.35);
     }
 }
