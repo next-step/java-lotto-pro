7f364f0e2dfddba08bc87d3bd925e77bb033cb9f
ssamzag <ssamzag@gmail.com>
2022-05-18 01:45:54 +0900

    feat(LottoNumber): 로또번호객체 캐싱 및 반환기능 추가

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,2 +1,62 @@
-package lotto.domain;public class LottoNumber {
+package lotto.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.IntStream;
+
+public class LottoNumber {
+    public static final Map<Integer, LottoNumber> lottoNumbersCache = new HashMap();
+    private static final int FIRST_LOTTO_NUMBER = 1;
+    private static final int LAST_LOTTO_NUMBER = 45;
+    private static final String WRONG_LOTTO_NUMBER_ERROR_MESSAGE = "잘못된 로또 번호입니다";
+
+    private final int lottoNumber;
+
+    static {
+        IntStream.rangeClosed(FIRST_LOTTO_NUMBER, LAST_LOTTO_NUMBER)
+                .forEach(lottoNumber -> lottoNumbersCache.put(lottoNumber, new LottoNumber(lottoNumber)));
+    }
+
+    private LottoNumber(int lottoNumber) {
+        this.lottoNumber = lottoNumber;
+    }
+
+    public static LottoNumber of(String lottoNumber) {
+        return of(Integer.parseInt(lottoNumber));
+    }
+
+    public static LottoNumber of(int lottoNumber) {
+        LottoNumber cachedLottoNumber = lottoNumbersCache.get(lottoNumber);
+        validateLottoNumber(cachedLottoNumber);
+        return cachedLottoNumber;
+    }
+
+    private static void validateLottoNumber(LottoNumber cachedLottoNumber) {
+        if (cachedLottoNumber == null) {
+            throw new IllegalArgumentException(WRONG_LOTTO_NUMBER_ERROR_MESSAGE);
+        }
+    }
+
+    public int getLottoNumber() {
+        return lottoNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return lottoNumber == that.lottoNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoNumber);
+    }
+
+    @Override
+    public String toString() {
+        return Integer.toString(lottoNumber);
+    }
 }
--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -1,2 +1,26 @@
-package lotto.domain;public class LottoNumberTest {
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class LottoNumberTest {
+    @Test
+    @DisplayName("1~45의 캐싱된 로또 번호를 가져온다")
+    void caching() {
+        for (int i = 1; i <= 45; i++) {
+            assertThat(LottoNumber.of(i).getLottoNumber()).isEqualTo(i);
+        }
+    }
+
+    @ParameterizedTest
+    @DisplayName("로또번호 정상범위(1~45) 이외의 값을 입력 받으면 예외가 발생한다")
+    @ValueSource(ints = {0, -1, 46, 47, 100})
+    void outOfLottoNumberRangeException(int wrongLottoNumber) {
+        assertThatIllegalArgumentException().isThrownBy(() -> LottoNumber.of(wrongLottoNumber))
+                .withMessage("잘못된 로또 번호입니다");
+    }
 }
