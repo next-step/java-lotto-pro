de9005072074951d823e775f19dd10f4484f6b7f
ssamzag <ssamzag@gmail.com>
2022-05-18 01:57:34 +0900

    feat(LottoGame): 로또 게임 상태를 관리하는 클래스

--- src/main/java/lotto/domain/LottoGame.java
+++ src/main/java/lotto/domain/LottoGame.java
@@ -1,2 +1,41 @@
-package lotto.domain;public class LottoGame {
+package lotto.domain;
+
+import lotto.domain.factory.LottoFactory;
+
+import java.util.List;
+
+public class LottoGame {
+    private final Lottos lottos;
+    private final Money money;
+
+    private Quantity quantity;
+
+    public LottoGame(String money) {
+        this.lottos = new Lottos();
+        this.money = new Money(money);
+        this.quantity = new Quantity(this.money.getQuantity());
+    }
+
+    public void buyLottos() {
+        while (isPurchasable()) {
+            this.lottos.add(LottoFactory.createAuto());
+            this.quantity = this.quantity.increase();
+        }
+    }
+
+    private boolean isPurchasable() {
+        return this.quantity.isPurchasable();
+    }
+
+    public int getMaxQuantity() {
+        return this.quantity.getMaxQuantity();
+    }
+
+    public List<Lotto> getLottos() {
+        return this.lottos.getLottos();
+    }
+
+    public LottoResult getLottoResult(String winningNumber) {
+        return lottos.getLottoResult(LottoFactory.create(winningNumber));
+    }
 }
