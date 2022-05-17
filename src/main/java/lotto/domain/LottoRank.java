6c1d575ecefa3735f8d9980286d1e197d685baf3
ssamzag <ssamzag@gmail.com>
2022-05-18 01:55:29 +0900

    feat(LottoRank): 우승상금 등수별 매칭카운터를 관리하는 enum 클래스

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -1,2 +1,34 @@
-package lotto.domain;public class LottoRank {
+package lotto.domain;
+
+import java.util.Arrays;
+
+public enum LottoRank {
+    MISS(0, 0),
+    FIFTH(3, 5_000),
+    FOURTH(4,50_000),
+    THIRD(5, 1_500_000),
+    FIRST(6, 2_000_000_000);
+
+    private final int matchCount;
+    private final double winningMoney;
+
+    LottoRank(int matchCount, double money) {
+        this.matchCount = matchCount;
+        this.winningMoney = money;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public double getWinningMoney() {
+        return winningMoney;
+    }
+
+    public static LottoRank of(int matchCount) {
+        return Arrays.stream(LottoRank.values())
+                .filter(r -> r.matchCount == matchCount)
+                .findFirst()
+                .orElse(MISS);
+    }
 }
