eb71954ab7f0599d9aa5494b9a78d512a3ed1921
Celina <luv.mins99@gmail.com>
2022-05-17 11:22:16 +0900

    refactor(RandomNumberGenerator): step3 피드백 반영(stream)
    - stream의 메서드 체이닝시 개행 적용
    - 최종연산 전까지는 개행을 통해 단계별로 실행되는 부분임을 보여줄 수 있다

--- src/main/java/lotto/model/RandomNumberGenerator.java
+++ src/main/java/lotto/model/RandomNumberGenerator.java
@@ -10,11 +10,14 @@ public class RandomNumberGenerator implements NumberGenerator {
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 45;
     private static final int SIZE = 6;
-    private static final List<Number> numberList
-            = IntStream.rangeClosed(MIN_NUMBER, MAX_NUMBER).mapToObj(Number::new).collect(Collectors.toList());
+    private static final List<Number> numberList = IntStream.rangeClosed(MIN_NUMBER, MAX_NUMBER)
+            .mapToObj(Number::new)
+            .collect(Collectors.toList());
 
     public List<Number> pickNumbers() {
         Collections.shuffle(numberList);
-        return Collections.unmodifiableList(numberList.stream().limit(SIZE).collect(Collectors.toList()));
+        return Collections.unmodifiableList(numberList.stream()
+                .limit(SIZE)
+                .collect(Collectors.toList()));
     }
 }
