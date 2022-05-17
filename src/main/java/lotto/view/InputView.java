9ca45604e7252cf93b4fe27bab897a9c27b06152
Celina <luv.mins99@gmail.com>
2022-05-17 11:19:21 +0900

    refactor(InputView): step3 scanner 피드백 반영
    - 한번만 생성되고 재사용 가능하므로 클래스 변수로 선언

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private final Scanner scanner = new Scanner(System.in);
     private static final String INSERT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String WINNER_NUMBER_INPUT_MESSAGE = "\n지난 주 당첨 번호를 입력해 주세요.";
+    private static final Scanner scanner = new Scanner(System.in);
 
     public String insertMoney() {
         System.out.println(INSERT_MONEY_MESSAGE);
