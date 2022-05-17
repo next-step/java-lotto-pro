8f7e12ef98f70994936c46b3eb755879ac11d108
sangho lee <sangholee.dev@gmail.com>
2022-05-18 01:05:23 +0900

    refactor: BiPredicate 를 당첨 판별 수정

--- src/main/java/lotto/domain/LottoWinResult.java
+++ src/main/java/lotto/domain/LottoWinResult.java
@@ -3,41 +3,32 @@ package lotto.domain;
 import generic.Money;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BiPredicate;
 
 public enum LottoWinResult {
 
-    FIRST(6, Money.valueOf(2_000_000_000)),
-    SECOND(5, Money.valueOf(30_000_000)),
-    THIRD(5, Money.valueOf(1_500_000)),
-    FOURTH(4, Money.valueOf(50_000)),
-    FIFTH(3, Money.valueOf(5_000)),
-    NO_WIN(-1, Money.valueOf(0)),
+    FIRST(6, Money.valueOf(2_000_000_000), ((count, isCorrectBonus) -> count == 6)),
+    SECOND(5, Money.valueOf(30_000_000), ((count, isCorrectBonus) -> count == 5 && isCorrectBonus)),
+    THIRD(5, Money.valueOf(1_500_000), ((count, isCorrectBonus) -> count == 5 && !isCorrectBonus)),
+    FOURTH(4, Money.valueOf(50_000), ((count, isCorrectBonus) -> count == 4)),
+    FIFTH(3, Money.valueOf(5_000), ((count, isCorrectBonus) -> count == 3)),
+    NO_WIN(0, Money.valueOf(0), ((count, isCorrectBonus) -> count < 3)),
     ;
 
     public static final List<LottoWinResult> WIN_RESULTS = Arrays.asList(FIFTH, FOURTH, THIRD, SECOND, FIRST);
     private final int winningCount;
     private final Money winningMoney;
+    private final BiPredicate<Integer, Boolean> winPredicate;
 
-    LottoWinResult(final int winningCount, final Money winningMoney) {
+    LottoWinResult(final int winningCount, final Money winningMoney, final BiPredicate<Integer, Boolean> winPredicate) {
         this.winningCount = winningCount;
         this.winningMoney = winningMoney;
+        this.winPredicate = winPredicate;
     }
 
     public static LottoWinResult confirm(final int count, final boolean bonusCorrect) {
-        if (isSecond(count, bonusCorrect)) {
-            return SECOND;
-        }
-
-        return findWinResult(count);
-    }
-
-    private static boolean isSecond(final int count, final boolean bonusCorrect) {
-        return count == SECOND.winningCount && bonusCorrect;
-    }
-
-    private static LottoWinResult findWinResult(final int count) {
         return WIN_RESULTS.stream()
-                .filter(winResult -> winResult.winningCount == count)
+                .filter(winResult -> winResult.winPredicate.test(count, bonusCorrect))
                 .findFirst()
                 .orElse(NO_WIN);
     }
