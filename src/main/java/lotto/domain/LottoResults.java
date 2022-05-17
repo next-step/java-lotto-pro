c0387fef7bc8ca9200da078ecd9d60a45d6809ef
millennion-jin <Jadestern2@>
2022-05-18 00:06:08 +0900

    feat(LottoResults) : 로또 결과 객체 구현

--- src/main/java/lotto/domain/LottoResults.java
+++ src/main/java/lotto/domain/LottoResults.java
@@ -1,17 +1,25 @@
 package lotto.domain;
 
+import java.util.HashMap;
+import java.util.Map;
 import lotto.enums.LottoRank;
 
 public class LottoResults {
-    public LottoResults() {
+    private final Map<LottoRank, Integer> resultList;
 
+    public LottoResults() {
+        resultList = new HashMap<>();
+
+        for (LottoRank rank : LottoRank.values()) {
+            resultList.put(rank, 0);
+        }
     }
 
     public void increaseRankCount(LottoRank rank) {
-
+        resultList.put(rank, resultList.get(rank) + 1);
     }
 
     public Integer getRankCount(LottoRank rank) {
-        return 0;
+        return resultList.get(rank);
     }
 }
