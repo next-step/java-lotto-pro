fe0b65b641eda7f5b0f9a76cd27ed36f012aa3fc
bestsilver <dkvkxm99@gmail.com>
2022-05-17 20:17:01 +0900

    refactor: 추가 사항 - 보너스볼 입력 문구 - rank에 따른 통계 문구 출력 - profitrate가 손해이면 알림글 추가

--- src/main/java/lotto_auto/view/Output.java
+++ src/main/java/lotto_auto/view/Output.java
@@ -9,14 +9,20 @@ public class Output {
     public static final String PURCHASE_LOTTO_COUNT_FORMAT = "%d개를 구매했습니다.\n";
     public static final String WINNING_LOTTO_INPUT_NOTICE = "지난 주 당첨 번호를 입력해 주세요.";
     public static final String PROFIT_RATE_NOTICE_FORMAT = "총 수익률은 %s입니다.";
+    public static final String NEGATIVE_PROFIT_RATE_NOTICE = "(기준이 1이기 때문에 결과적으로 손해라는 의미임)";
+    public static final String BONUS_INPUT_NOTICE = "보너스 볼을 입력해 주세요.";
+
     public static final String FIGURES_FORMAT = "%d개 일치 (%d원)- %d개\n";
+    public static final String SECOND_FIGURES_FORMAT = "%d개 일치, 보너스 볼 일치(%d) - %d개\n";
 
     public void showMoneyInputNotice() {
         System.out.println(MONEY_INPUT_NOTICE);
     }
 
     public void showProfitRate(ProfitRate profitRate) {
-        System.out.format(PROFIT_RATE_NOTICE_FORMAT, profitRate.printRate());
+        String format = (profitRate.isNegativeProfitRate()) ?
+                PROFIT_RATE_NOTICE_FORMAT + " " + NEGATIVE_PROFIT_RATE_NOTICE : PROFIT_RATE_NOTICE_FORMAT;
+        System.out.format(format, profitRate.printRate());
     }
 
     public void showPurchaseLottoCountNotice(int count) {
@@ -42,17 +48,21 @@ public class Output {
         result.append(figuresTitle);
 
         for (LottoRank rank : LottoRank.valuesTheLowestOrder()) {
-            result.append(String.format(FIGURES_FORMAT, rank.matchedCount(), rank.winnings(), figures.getCountBy(rank)));
+            result.append(String.format(getFiguresFormat(rank), rank.matchedCount(), rank.winnings(), figures.getCountBy(rank)));
         }
 
         System.out.println(result);
+    }
+
+    public void showBonusBallInputNotice() {
+        System.out.println(BONUS_INPUT_NOTICE);
     }
 
     public void showError(Throwable e) {
         System.out.println(e.getMessage());
     }
 
-    public String printLotto(Lotto lotto) {
+    private String printLotto(Lotto lotto) {
         char fisrtCharactor = '[';
         char lastCharactor = ']';
         String printDelimiter = ", ";
@@ -60,6 +70,13 @@ public class Output {
         return fisrtCharactor + lotto.getLottoNumbers().getLottoNumberSet().stream()
                 .map(lottoNumber -> Integer.toString(lottoNumber.getNumber()))
                 .collect(Collectors.joining(printDelimiter)) +lastCharactor;
+    }
+
+    private String getFiguresFormat(LottoRank rank) {
+        if (rank == LottoRank.SECOND) {
+            return SECOND_FIGURES_FORMAT;
+        }
+        return FIGURES_FORMAT;
     }
 
 }
