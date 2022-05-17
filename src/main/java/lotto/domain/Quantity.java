86467e68c977bfac95251e64500b7cb22c652a5a
ssamzag <ssamzag@gmail.com>
2022-05-18 01:52:53 +0900

    feat(Quantity): 로또 수량을 관리하는 클래스 추가

--- src/main/java/lotto/domain/Quantity.java
+++ src/main/java/lotto/domain/Quantity.java
@@ -1,2 +1,36 @@
-package lotto.domain;public class Quantity {
+package lotto.domain;
+
+public class Quantity {
+    private static final String EXCEED_QUANTITY_ERROR_MESSAGE = "구매할 수 있는 수량을 초과하였습니다. 구매가능수량: %d, 구매수량:%d";
+
+    private final int quantity;
+    private final int maxQuantity;
+
+    public Quantity(int maxQuantity) {
+        this(0, maxQuantity);
+    }
+
+    public Quantity(int quantity, int maxQuantity) {
+        validate(quantity, maxQuantity);
+        this.quantity = quantity;
+        this.maxQuantity = maxQuantity;
+    }
+
+    private void validate(int quantity, int maxQuantity) {
+        if (quantity > maxQuantity) {
+            throw new IllegalArgumentException(String.format(EXCEED_QUANTITY_ERROR_MESSAGE, maxQuantity, quantity));
+        }
+    }
+
+    public Quantity increase() {
+         return new Quantity(this.quantity + 1, maxQuantity);
+    }
+
+    public boolean isPurchasable() {
+        return maxQuantity > quantity;
+    }
+
+    public int getMaxQuantity() {
+        return maxQuantity;
+    }
 }
--- src/test/java/lotto/domain/QuantityTest.java
+++ src/test/java/lotto/domain/QuantityTest.java
@@ -1,4 +1,30 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.junit.jupiter.api.Assertions.*;
+
 class QuantityTest {
-  
+    @Test
+    @DisplayName("구매가능3, 구매수량2 이라면 True 를 반환한다")
+    void isPurchasableTrue() {
+        assertThat(new Quantity(2, 3).isPurchasable()).isTrue();
+    }
+
+    @Test
+    @DisplayName("구매가능3, 구매수량3 이라면 False 를 반환한다")
+    void isPurchasableFalse() {
+        assertThat(new Quantity(3, 3).isPurchasable()).isFalse();
+    }
+
+    @Test
+    @DisplayName("구매가능 수량 3을 초과하면 예외가 발생한다")
+    void increaseException() {
+        Quantity quantity = new Quantity(3, 3);
+        assertThatIllegalArgumentException().isThrownBy(quantity::increase)
+                .withMessage("구매할 수 있는 수량을 초과하였습니다. 구매가능수량: 3, 구매수량:4");
+    }
 }
