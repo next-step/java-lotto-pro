5bf2c81416998bf8e4da455c9233a0517c425448
bestsilver <dkvkxm99@gmail.com>
2022-05-17 20:24:20 +0900

    test: 매치 카운트에 따른 보너스볼 매치 테스트 추가

--- src/test/java/lotto_auto/model/LottoRankTest.java
+++ src/test/java/lotto_auto/model/LottoRankTest.java
@@ -3,11 +3,19 @@ package lotto_auto.model;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class LottoRankTest {
     @Test
-    void 매치_카운트에_따른_랭크_얻기() {
-        LottoRank rank = LottoRank.getLottoRuleFromMatchedCount(3);
-        assertThat(rank).isEqualTo(LottoRank.FOURTH);
+    void 매치_카운트와_보너스볼_매치에_따른_랭크_얻기() {
+        LottoRank rank = LottoRank.getLottoRuleFromMatchedCount(3, false);
+        assertAll(
+            () -> assertThat(LottoRank.getLottoRuleFromMatchedCount(6, false)).isEqualTo(LottoRank.FIRST),
+            ()-> assertThat(LottoRank.getLottoRuleFromMatchedCount(5, true)).isEqualTo(LottoRank.SECOND),
+            () -> assertThat(LottoRank.getLottoRuleFromMatchedCount(5, false)).isEqualTo(LottoRank.THIRD),
+            () -> assertThat(LottoRank.getLottoRuleFromMatchedCount(3, false)).isEqualTo(LottoRank.FIFTH),
+            () -> assertThat(LottoRank.getLottoRuleFromMatchedCount(0, false)).isEqualTo(LottoRank.NOTHING)
+        );
+
     }
 }
