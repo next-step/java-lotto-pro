8c844dea20a9ac505eb757155d15f77fe319694c
Celina <luv.mins99@gmail.com>
2022-05-17 14:02:25 +0900

    feat(Lotto): Number 비교를 위한 contains 로직 추가
    - 이전에 구현하면서 생겨난 new ArrayList<>() 삭제
    - Number 비교를 Lotto 가 하도록 위임

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,12 +1,11 @@
 package lotto.model;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
 
-    private List<Number> numbers = new ArrayList<>();
+    private List<Number> numbers;
 
     public Lotto(NumberGenerator numberGenerator) {
         createNumbers(numberGenerator);
@@ -14,6 +13,10 @@ public class Lotto {
 
     public List<Number> getNumbers() {
         return Collections.unmodifiableList(numbers);
+    }
+
+    public boolean contains(Number number) {
+        return this.numbers.contains(number);
     }
 
     @Override
