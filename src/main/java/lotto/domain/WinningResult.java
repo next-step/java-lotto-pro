8866242b7ef7a87b5baf6071a5e47bb6520f42dc
Lisc <qqwer153@gmail.com>
2022-05-18 01:30:42 +0900

    feat: 당첨 결과에 따른 총 수익률을 구하는 기능 구현

--- src/main/java/lotto/domain/WinningResult.java
+++ src/main/java/lotto/domain/WinningResult.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class WinningResult {
+    private final double MATH_ROUND_DIGIT = 100d;
     private static final String MESSAGE_MATCHED_COUNT = "%d개 일치 (%d원)- %d개\n";
+    private static final String TEXT_PROFIT = "이득이";
+    private static final String TEXT_PRINCIPAL = "본전이";
+    private static final String TEXT_LOSS = "손해";
 
     private List<LottoRank> winningRanks = new ArrayList<>();
 
@@ -13,14 +17,40 @@ public class WinningResult {
     }
 
     public void addWinningRank(LottoRank lottoRank) {
-        if(LottoRank.isWinning(lottoRank))
-        this.winningRanks.add(lottoRank);
+        if (LottoRank.isWinning(lottoRank)) {
+            this.winningRanks.add(lottoRank);
+        }
     }
 
     public int countRank(LottoRank findRank) {
         return (int) this.winningRanks.stream()
                 .filter(rank -> rank.name().equals(findRank.name()))
                 .count();
+    }
+
+    public double profitRate(Money purchaseMoney) {
+        double totalPrizeMoneyDouble = totalPrizeMoney();
+        double purchaseMoneyDouble = purchaseMoney.getMoney();
+        double profitRate = totalPrizeMoneyDouble / purchaseMoneyDouble;
+        return Math.round(profitRate * MATH_ROUND_DIGIT) / MATH_ROUND_DIGIT;
+    }
+
+    public int totalPrizeMoney() {
+        int totalPrizeMoney = this.winningRanks.stream()
+                .map(LottoRank::getPrizeMoney)
+                .reduce(0, Integer::sum);
+        return totalPrizeMoney;
+    }
+
+    public String profitResultDescription(Money purchaseMoney) {
+        int totalPrizeMoney = totalPrizeMoney();
+        if (purchaseMoney.getMoney() < totalPrizeMoney) {
+            return TEXT_PROFIT;
+        }
+        if (purchaseMoney.getMoney() > totalPrizeMoney) {
+            return TEXT_LOSS;
+        }
+        return TEXT_PRINCIPAL;
     }
 
     @Override
--- src/test/java/lotto/domain/WinningResultTest.java
+++ src/test/java/lotto/domain/WinningResultTest.java
@@ -17,38 +17,74 @@ public class WinningResultTest {
         winningResult = new WinningResult();
         winningNumbers = new LottoTicket(Arrays.asList(1, 2, 3, 4, 5, 6)).getLottoNumbers();
 
+        LottoNumbers firstLottoNumbers = new LottoTicket(Arrays.asList(1, 2, 3, 4, 5, 6)).getLottoNumbers();
+        winningResult.addWinningRank(firstLottoNumbers.rank(winningNumbers));
+
+        LottoNumbers secondLottoNumbers = new LottoTicket(Arrays.asList(1, 2, 3, 4, 5, 9)).getLottoNumbers();
+        winningResult.addWinningRank(secondLottoNumbers.rank(winningNumbers));
+
+        LottoNumbers thirdLottoNumbers = new LottoTicket(Arrays.asList(1, 2, 3, 4, 8, 9)).getLottoNumbers();
+        winningResult.addWinningRank(thirdLottoNumbers.rank(winningNumbers));
+
+        LottoNumbers fourthLottoNumbers = new LottoTicket(Arrays.asList(1, 2, 3, 7, 8, 9)).getLottoNumbers();
+        winningResult.addWinningRank(fourthLottoNumbers.rank(winningNumbers));
     }
 
     @Test
     @DisplayName("1등 당첨 로또 순위 개수 확인")
     void countRankFirst() {
-        LottoNumbers firstLottoNumbers = new LottoTicket(Arrays.asList(1, 2, 3, 4, 5, 6)).getLottoNumbers();
-        winningResult.addWinningRank(firstLottoNumbers.rank(winningNumbers));
         assertThat(winningResult.countRank(LottoRank.FIRST)).isEqualTo(1);
     }
 
     @Test
     @DisplayName("2등 당첨 로또 순위 개수 확인")
     void countRankSecond() {
-        LottoNumbers secondLottoNumbers = new LottoTicket(Arrays.asList(1, 2, 3, 4, 5, 9)).getLottoNumbers();
-        winningResult.addWinningRank(secondLottoNumbers.rank(winningNumbers));
         assertThat(winningResult.countRank(LottoRank.SECOND)).isEqualTo(1);
     }
 
     @Test
     @DisplayName("3등 당첨 로또 순위 개수 확인")
     void countRankThird() {
-        LottoNumbers thirdLottoNumbers = new LottoTicket(Arrays.asList(1, 2, 3, 4, 8, 9)).getLottoNumbers();
-        winningResult.addWinningRank(thirdLottoNumbers.rank(winningNumbers));
         assertThat(winningResult.countRank(LottoRank.THIRD)).isEqualTo(1);
     }
 
     @Test
     @DisplayName("4등 당첨 로또 순위 개수 확인")
     void countRankFourth() {
-        LottoNumbers fourthLottoNumbers = new LottoTicket(Arrays.asList(1, 2, 3, 7, 8, 9)).getLottoNumbers();
-        winningResult.addWinningRank(fourthLottoNumbers.rank(winningNumbers));
         assertThat(winningResult.countRank(LottoRank.FOURTH)).isEqualTo(1);
     }
 
+    @Test
+    @DisplayName("로또 수익률 확인")
+    void profitRate() {
+        Money money = new Money(10000);
+        assertThat(winningResult.profitRate(money)).isEqualTo(200155.5d);
+    }
+
+    @Test
+    @DisplayName("로또 총 당첨 금액 확인")
+    void totalPrizeMoney() {
+        assertThat(winningResult.totalPrizeMoney()).isEqualTo(2001555000);
+    }
+
+    @DisplayName("수익률 손해")
+    @Test
+    void loss() {
+        Money money = new Money("2001556000");
+        assertThat(winningResult.profitResultDescription(money)).contains("손해");
+    }
+
+    @DisplayName("수익률 본전")
+    @Test
+    void principal() {
+        Money money = new Money("2001555000");
+        assertThat(winningResult.profitResultDescription(money)).contains("본전이");
+    }
+
+    @DisplayName("수익률 이득")
+    @Test
+    void profit() {
+        Money money = new Money("10000");
+        assertThat(winningResult.profitResultDescription(money)).contains("이득이");
+    }
 }
