806235cb5b972b6bbf72461fee2b027aee188ce6
Lisc <qqwer153@gmail.com>
2022-05-18 02:39:25 +0900

    refactor: LottoTicket 클래스 equals, hashCode 메서드 제거

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -2,7 +2,6 @@ package lotto.domain;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class LottoTicket {
     private LottoNumbers lottoNumbers;
@@ -21,23 +20,6 @@ public class LottoTicket {
 
     public LottoNumbers getLottoNumbers() {
         return this.lottoNumbers;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        LottoTicket that = (LottoTicket) o;
-        return Objects.equals(lottoNumbers, that.lottoNumbers);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(lottoNumbers);
     }
 
     @Override
