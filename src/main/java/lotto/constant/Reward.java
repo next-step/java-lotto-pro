81027d2e77df8e6b5551faf5f3e489ae35a02adf
tasklet1579 <tasklet1579@gmail.com>
2022-05-18 00:00:18 +0900

    feat(Reward) : 로또 상금 관리 클래스 선언

---
+++ src/main/java/lotto/constant/Reward.java
@@ -0,0 +1,28 @@
+package lotto.constant;
+
+import java.util.stream.Stream;
+
+public enum Reward {
+    THREE(3, 5000),
+    FOUR(4, 50000),
+    FIVE(5, 1500000),
+    SIX(6, 2000000000);
+
+    private final int match;
+    private final int reward;
+
+    Reward(int match, int reward) {
+        this.match = match;
+        this.reward = reward;
+    }
+
+    public static int find(int match) {
+        return stream().filter(a -> a.match == match)
+                       .findFirst()
+                       .get().reward;
+    }
+
+    private static Stream<Reward> stream() {
+        return Stream.of(Reward.values());
+    }
+}
