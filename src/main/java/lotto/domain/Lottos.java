e873edc66c993d28e79c58acbe90a653b56cd402
boss.back <boss.back@kakaocorp.com>
2022-05-17 18:17:39 +0900

    feat: Lottos를 통해서 당첨통계 및 수익률 계산 및 테스트

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -6,12 +6,29 @@ import java.util.Objects;
 public class Lottos {
 
     private List<Lotto> elements;
+    private WinningStatistics winningStatistics;
 
     public Lottos(List<Lotto> elements) {
         this.elements = elements;
+        this.winningStatistics = new WinningStatistics();
+    }
+
+    public void makeWinningResult(Lotto winningLotto) {
+        for (Lotto lotto : elements) {
+            winningStatistics.addLottoRanking(LottoRanking.findLottoRaking(lotto, winningLotto));
+        }
+    }
+
+    public double calculateRateOfReturn(int purchaseAmount) {
+        return winningStatistics.calculateRateOfReturn(purchaseAmount);
+    }
+
+    public WinningStatistics getWinningStatistics() {
+        return winningStatistics;
     }
 
     @Override
+
     public boolean equals(Object o) {
         if (this == o) {
             return true;
--- src/test/java/lotto/LottosTest.java
+++ src/test/java/lotto/LottosTest.java
@@ -16,4 +16,19 @@ public class LottosTest {
         assertThat(lottos).isEqualTo(
             new Lottos(List.of(new Lotto(() -> List.of(new LottoNumber(1))))));
     }
+
+    @Test
+    public void calculateReturnRate() {
+        Lottos lottos = new Lottos(List.of(new Lotto(
+            () -> List.of(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3),
+                new LottoNumber(4), new LottoNumber(5), new LottoNumber(6)))));
+        lottos.makeWinningResult(
+            new Lotto(() -> List.of(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3),
+                new LottoNumber(4), new LottoNumber(5), new LottoNumber(6))));
+        lottos.calculateRateOfReturn(1000);
+
+        assertThat(lottos.calculateRateOfReturn(20000)).isEqualTo(100000);
+
+    }
+
 }
