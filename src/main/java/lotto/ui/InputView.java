a37c86901d837cc1b018cc963271b04897d978f6
millennion-jin <Jadestern2@>
2022-05-18 02:25:24 +0900

    feat(InputView) : 지난주 당첨번호 입력기능 추가

--- src/main/java/lotto/ui/InputView.java
+++ src/main/java/lotto/ui/InputView.java
@@ -8,15 +8,32 @@ public class InputView {
     private static final String MSG_PLEASE_INPUT_LOTTO_NUMBER = "지난 주 당첨 번호를 입력해 주세요.";
 
     public Integer inputMoneyForPurchase() {
-        System.out.println(MSG_PLEASE_INPUT_MONEY);
-
-        Integer result = null;
+        printPleaseInputMoneyForPurchase();
         return scanMoneyForPurchase();
+    }
+
+    private void printPleaseInputMoneyForPurchase() {
+        System.out.println(MSG_PLEASE_INPUT_MONEY);
     }
 
     private Integer scanMoneyForPurchase() {
         Scanner scanner = new Scanner(System.in);
         return scanner.nextInt();
+    }
+
+    public String inputReferenceLottoNumbers() {
+        printPleaseInputLottoNumbers();
+        return scanLottoNumbers();
+    }
+
+    private void printPleaseInputLottoNumbers() {
+        System.out.println();
+        System.out.println(MSG_PLEASE_INPUT_LOTTO_NUMBER);
+    }
+
+    private String scanLottoNumbers() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
     }
 
 
