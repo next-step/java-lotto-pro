59b133c4845417ff79a220c2919354f93cae53a3
june2 <>
2022-05-17 12:30:10 +0900

    fix: 수동 로또구매 0개 예외처리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -32,9 +32,12 @@ public class InputView {
         System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
         int count = validatetManualLottoCount(scanner.nextLine());
 
-        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
         List<List<Integer>> lottoNumbers = new ArrayList<>();
+        if (count == 0) {
+            return lottoNumbers;
+        }
 
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
         for (int i = 0; i < count; i++) {
             lottoNumbers.add(generateLottoNumber(scanner.nextLine()));
         }
