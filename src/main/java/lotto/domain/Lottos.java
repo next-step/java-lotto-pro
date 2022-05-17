d183c1a9ffea8f05fd6d69fd0ac1964a871dbadf
millennion-jin <Jadestern2@>
2022-05-18 00:43:08 +0900

    test(LottosTest) : 로또 목록 객체 테스트 추가 및 로또 결과 도출 테스트 추가

---
+++ src/main/java/lotto/domain/Lottos.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class Lottos {
+    private final List<Lotto> lottoList;
+
+    public Lottos(List<Lotto> lottoList) {
+        this.lottoList = lottoList;
+    }
+
+    public Lotto get(int index) {
+        return null;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+
+    public LottosResults matchWithReference(Lotto referenceLotto) {
+        return null;
+    }
+}
