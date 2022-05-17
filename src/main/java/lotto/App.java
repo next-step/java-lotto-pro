0b93f045852b7df4ce8473ea3ad846c1f15abd4d
ssamzag <ssamzag@gmail.com>
2022-05-18 01:58:02 +0900

    feat(App): 입력 및 출력을 제어하는 Controller 클래스

--- src/main/java/lotto/App.java
+++ src/main/java/lotto/App.java
@@ -1,2 +1,19 @@
-package lotto;public class App {
+package lotto;
+
+import lotto.domain.LottoGame;
+import lotto.domain.LottoResult;
+import lotto.domain.factory.LottoFactory;
+import lotto.ui.InputView;
+import lotto.ui.ResultView;
+
+public class App {
+    public static void main(String[] args) {
+        LottoGame lottoGame = new LottoGame(InputView.getBuyPrice());
+        lottoGame.buyLottos();
+        ResultView.printBuyLottos(lottoGame);
+
+        LottoResult lottoResult = lottoGame.getLottoResult(InputView.getWinningNumbers());
+        ResultView.printWinningResult(lottoResult);
+
+    }
 }
