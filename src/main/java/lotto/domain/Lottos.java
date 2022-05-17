2a90c339cad4d7c31d06b742ea89ecf50a8063bd
millennion-jin <Jadestern2@>
2022-05-18 00:59:31 +0900

    feat(Lottos) : 로또 목록 객체 구현 및 로또 결과 도출 기능 구현

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,24 +1,44 @@
 package lotto.domain;
 
 import java.util.List;
+import lotto.enums.LottoRank;
 
 public class Lottos {
+
+    private static final String ERROR_MESSAGE_LOTTO_LIST_NULL_OR_EMPTY = "[ERROR] lottoList is null or empty.";
     private final List<Lotto> lottoList;
 
     public Lottos(List<Lotto> lottoList) {
+        validate(lottoList);
         this.lottoList = lottoList;
     }
 
+    private void validate(List<Lotto> lottoList) {
+        if(lottoList == null || lottoList.isEmpty()) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_LOTTO_LIST_NULL_OR_EMPTY);
+        }
+    }
+
     public Lotto get(int index) {
-        return null;
+        return lottoList.get(index);
     }
 
     public int size() {
-        return 0;
+        return lottoList.size();
     }
 
-
     public LottosResults matchWithReference(Lotto referenceLotto) {
-        return null;
+        LottosResults results = new LottosResults();
+
+        for(Lotto lotto : lottoList) {
+            matchLottoWithReference(results, referenceLotto, lotto);
+        }
+
+        return results;
+    }
+
+    private void matchLottoWithReference(LottosResults results, Lotto referenceLotto, Lotto targetLotto) {
+        LottoRank matchedRank = referenceLotto.match(targetLotto).convertToLottoRank();
+        results.increaseRankCount(matchedRank);
     }
 }
