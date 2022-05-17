c81fcd72296444a06694ad1a961403574083e573
bestsilver <dkvkxm99@gmail.com>
2022-05-17 20:18:45 +0900

    feat: match count에 따라 rank 변경, count와 보너스볼 매치에 따른 rank 리턴 함수 추가

--- src/main/java/lotto_auto/model/LottoRank.java
+++ src/main/java/lotto_auto/model/LottoRank.java
@@ -3,12 +3,15 @@ package lotto_auto.model;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public enum LottoRank {
     FIRST(6, 2_000_000_000),
-    SECOND(5, 1_500_000),
-    THIRD(4, 50_000),
-    FOURTH(3, 5000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3,5_000),
     NOTHING(0,0);
 
     private final int matchedCount;
@@ -27,15 +30,27 @@ public enum LottoRank {
         return winnings;
     }
 
-    public static LottoRank getLottoRuleFromMatchedCount(int count) {
+    public static LottoRank getLottoRuleFromMatchedCount(int count, boolean matchedBonusBall) {
         Optional<LottoRank> rank = Arrays.stream(LottoRank.values())
                 .filter(r -> r.matchedCount == count)
                 .findAny();
-        return rank.orElse(NOTHING);
+
+        LottoRank resultRank = rank.orElse(NOTHING);
+
+        if (resultRank.matchedCount != 5) {
+            return resultRank;
+        }
+
+        return resultRank.isSecondRank(matchedBonusBall) ? SECOND : THIRD;
     }
 
+    private boolean isSecondRank(boolean matchedBonusBall) {
+        return this.matchedCount == 5 && matchedBonusBall;
+    }
+
+
     public static List<LottoRank> valuesTheLowestOrder() {
-        return Arrays.asList(FOURTH, THIRD, SECOND, FIRST);
+        return Arrays.asList(FIFTH, FOURTH, THIRD, SECOND, FIRST);
     }
 
 }
