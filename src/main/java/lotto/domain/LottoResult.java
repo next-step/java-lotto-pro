52e3ace882f37c14f195ab60ed2e86d8257eb486
ssamzag <ssamzag@gmail.com>
2022-05-18 01:54:20 +0900

    feat(LottoResult): 로또 당첨 결과를 관리하는 클래스

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,12 +1,19 @@
-package lotto;
+package lotto.domain;
 
-import lotto.domain.Lotto;
-
-import java.util.List;
+import java.util.*;
 
 public class LottoResult {
+    private final Map<LottoRank, Integer> lottoResult = new EnumMap<>(LottoRank.class);
 
-    public LottoResult(String winningNumbers, List<Lotto> lottos) {
+    public LottoResult(List<Lotto> lottos, Lotto winningLotto) {
+        for (Lotto lotto : lottos) {
+            LottoRank lottoRank = LottoRank.of(lotto.matchCount(winningLotto));
+            lottoResult.put(lottoRank, getWinningCount(lottoRank) + 1);
+        }
+    }
 
+    public int getWinningCount(LottoRank lottoRank) {
+        return Optional.ofNullable(lottoResult.get(lottoRank))
+                .orElse(0);
     }
 }
