c6e71ac9980241a9c6e8b4c10aac5ff48022d3d8
bestsilver <dkvkxm99@gmail.com>
2022-05-17 20:23:56 +0900

    test: 스트림 방식으로 초기화

--- src/test/java/lotto_auto/model/FiguresTest.java
+++ src/test/java/lotto_auto/model/FiguresTest.java
@@ -4,9 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -15,10 +14,14 @@ public class FiguresTest {
     private Figures figures;
     @BeforeEach
     void setUp() {
-        List<LottoNumber> lottoNumberList = Arrays.asList(new LottoNumber(1),
-                new LottoNumber(2),new LottoNumber(3),new LottoNumber(4),new LottoNumber(5),new LottoNumber(6));
-        Lottos lottos = new Lottos(Arrays.asList(new Lotto(new LottoNumbers(lottoNumberList))));
-        Lotto winning = new Lotto(new LottoNumbers(lottoNumberList));
+        LottoNumbers lottoNumbers = new LottoNumbers(IntStream
+                .rangeClosed(1, 6)
+                .boxed()
+                .map(LottoNumber::new)
+                .collect(Collectors.toList()));
+
+        Lottos lottos = new Lottos(Arrays.asList(new Lotto(lottoNumbers)));
+        WinningLotto winning = new WinningLotto(lottoNumbers, new LottoNumber(7));
         figures = new Figures(lottos, winning);
     }
     @Test
