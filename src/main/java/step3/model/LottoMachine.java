747aca87b6a63f569d8ea2e5763415a94ed48ea7
jhsong2580 <thdwmdgns@naver.com>
2022-05-18 16:16:48 +0900

    refactor: manager 기능 machine및 user로 분리

--- src/main/java/step3/model/LottoMachine.java
+++ src/main/java/step3/model/LottoMachine.java
@@ -1,72 +1,104 @@
 package step3.model;
 
+import static java.util.Collections.shuffle;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import step3.domain.LottoElement;
-import step3.domain.LottoManager;
 import step3.domain.LottoTicket;
-import step3.domain.Money;
 import step3.domain.Ticket;
 import step3.enums.LottoReward;
 
 public class LottoMachine {
 
-    private final LottoManager lottoManager;
-    private final int LOTTO_PRICE = 1_000;
-    private final String CANT_BUY_LOTTO_EXCEPTION = "돈은 최소 " + LOTTO_PRICE + "이상 입력해야합니다";
+    private final int LOTTO_MIN = 1;
+    private final int LOTTO_MAX = 46;
+    private final int LOTTO_ELEMENTS_SIZE = 6;
+    private List<Integer> LOTTO_VALID_ELEMENTS = IntStream.rangeClosed(LOTTO_MIN, LOTTO_MAX).boxed().collect(Collectors.toList());
+    private final int MATCH_COUNT_BASE = 0;
+    private final String SET_BONUS_NUMBER_EXCEPTION_MSG = "정답티켓과 다른 번호를 설정해야합니다";
 
+    private LottoElement bonusNumber;
+    private LottoTicket winnerLotto;
 
-    public LottoMachine(LottoManager lottoManager) {
-        this.lottoManager = lottoManager;
+    public LottoMachine() {
+
     }
 
-
-    public HashMap<LottoReward, Integer> checkWin(LottoTicket winnerLotto, LottoElement bonusNumber) {
-        return lottoManager.checkWin(winnerLotto, bonusNumber);
-    }
 
     public LottoTicket makeManualLottoTicket(String manualLottoSource) {
         try {
-            return lottoManager.makeManualLottoTicket(manualLottoSource);
+            return new LottoTicket(manualLottoSource);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             return null;
         }
     }
 
-    public int buyLotto(Ticket ticket) {
-        return lottoManager.buyRandomTicket(ticket.getTicket());
-    }
-
-    public Ticket buyTicket(Money money) {
-        try {
-            return new Ticket(money);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return null;
+    public List<LottoTicket> makeRandomLottoTickets(Ticket ticket) {
+        List<LottoTicket> lottoTickets = new ArrayList<>();
+        for (int i = 0; i < ticket.getTicket(); i++) {
+            lottoTickets.add(makeRandomLottoTicket());
         }
-
+        return lottoTickets;
     }
 
-    public Money insertMoney(String money) {
+    private LottoTicket makeRandomLottoTicket() {
+        List<Integer> lottoElements = new ArrayList<>();
+        shuffle(LOTTO_VALID_ELEMENTS);
+        for (int i = 0; i < LOTTO_ELEMENTS_SIZE; i++) {
+            lottoElements.add(LOTTO_VALID_ELEMENTS.get(i));
+        }
+        return new LottoTicket(lottoElements.stream().map(String::valueOf).collect(Collectors.toList()));
+    }
+
+    public boolean setBonusNumber(String lottoElementSource) {
+        if (!validBonusNumber(lottoElementSource)) {
+            System.out.println(SET_BONUS_NUMBER_EXCEPTION_MSG);
+            return false;
+        }
+        bonusNumber = new LottoElement(Integer.parseInt(lottoElementSource));
+        return true;
+    }
+
+    private boolean validBonusNumber(String lottoElementSource) {
+        int isExist = 1;
         try {
-            return new Money(money);
+            int bonusExistInWinnerTicket = winnerLotto.getMatchCountWith(Arrays.asList(new LottoElement(Integer.parseInt(lottoElementSource))));
+            return bonusExistInWinnerTicket != isExist;
+        } catch (NumberFormatException e) {
+            return false;
         } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return null;
+            return false;
         }
     }
 
-    public List<List<LottoElement>> getLottoNumbers() {
-        return lottoManager.getLottoNumbers();
+    public boolean setWinnerLotto(String winnerSource) {
+        winnerLotto = makeManualLottoTicket(winnerSource);
+        return winnerLotto != null;
     }
 
-    public LottoElement setBonusNumber(String lottoElementSource, LottoTicket winnerTicket) {
-        try {
-            return lottoManager.setBonusNumber(lottoElementSource, winnerTicket);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return null;
+    public HashMap<LottoReward, Integer> checkWin(List<LottoTicket> lottoTickets) {
+        //Lottouser로
+        LinkedHashMap<LottoReward, Integer> statistics = new LinkedHashMap<>();
+        initStatistics(statistics);
+        for (LottoTicket lottoTicket : lottoTickets) {
+            int matchNumber = lottoTicket.getMatchCountWith(winnerLotto.getLottoNumbers());
+            int matchBonus = lottoTicket.getMatchCountWith(Arrays.asList(bonusNumber));
+            LottoReward lottoReward = LottoReward.valueOf(matchNumber + matchBonus, matchBonus == 1);
+            statistics.replace(lottoReward, statistics.get(lottoReward) + 1);
+        }
+        return statistics;
+    }
+
+    private void initStatistics(LinkedHashMap<LottoReward, Integer> statistics) {
+        for (LottoReward lottoReward : LottoReward.values()) {
+            statistics.put(lottoReward, MATCH_COUNT_BASE);
         }
     }
 }
--- src/test/java/step3/LottoMachineTest.java
+++ src/test/java/step3/LottoMachineTest.java
@@ -2,12 +2,17 @@ package step3;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import step3.domain.LottoManager;
+import step3.domain.LottoTicket;
 import step3.domain.Money;
 import step3.domain.Ticket;
+import step3.enums.LottoReward;
 import step3.model.LottoMachine;
 
 public class LottoMachineTest {
@@ -16,21 +21,49 @@ public class LottoMachineTest {
 
     @BeforeEach
     public void init() {
-        lottoMachine = new LottoMachine(new LottoManager());
+        lottoMachine = new LottoMachine();
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"1000:1", "1500:1", "3000:3", "10001:10"}, delimiter = ':')
-    public void buyTicketTest(String money, int expected) {
-        Money moneyEntity = new Money(money);
-        assertThat(lottoMachine.buyTicket(moneyEntity)).isEqualTo(new Ticket(moneyEntity));
+    @CsvSource(value = {"1,2,3,4,5,6:true", "1,2,3,4,5:false", "a,b,c,d,e,f:false"}, delimiter = ':')
+    public void makeManualLottoTicketTest(String manualLottoSource, boolean expected) {
+        assertThat(lottoMachine.makeManualLottoTicket(manualLottoSource) != null).isEqualTo(expected);
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"1000:1", "1500:1", "3000:3", "10001:10"}, delimiter = ':')
-    public void buyLottoTest(String money, int expected) {
+    @CsvSource(value = {"1000:1", "2000:2", "10000:10"}, delimiter = ':')
+    public void makeRandomTicketsTest(String money, int expected) {
         Ticket ticket = new Ticket(new Money(money));
-        assertThat(lottoMachine.buyLotto(ticket)).isEqualTo(expected);
+        assertThat(lottoMachine.makeRandomLottoTickets(ticket)).hasSize(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "40:true", "3:false", "-a:false", "-1:false"}, delimiter = ':')
+    public void setBonusTest(String bonusElement, boolean expected) {
+        lottoMachine.setWinnerLotto("3,4,5,6,7,8");
+        assertThat(lottoMachine.setBonusNumber(bonusElement)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,2,3,4,5,6:true", "-a:false", "1,2,3,4,5:false", "-1,2,3,4,5,6:false"}, delimiter = ':')
+    public void setWinnerLottoTest(String winnerSource, boolean expected) {
+        assertThat(lottoMachine.setWinnerLotto(winnerSource)).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("각 로또별 매칭 갯수를 HashMap으로 반환한다")
+    public void checkWinTest() {
+        lottoMachine.setWinnerLotto("1,2,3,4,5,6");
+        lottoMachine.setBonusNumber("7");
+        List<LottoTicket> lottoTickets = new ArrayList<>();
+
+        lottoTickets.add(new LottoTicket("1,2,3,4,5,6")); //6
+        lottoTickets.add(new LottoTicket("1,2,3,4,5,8")); //5
+        lottoTickets.add(new LottoTicket("1,2,11,12,13,14")); //MISS
+        lottoTickets.add(new LottoTicket("1,2,3,4,7,8")); //5_bonus
+        lottoTickets.add(new LottoTicket("11,12,13,14,15,16")); // MISS
+        assertThat(lottoMachine.checkWin(lottoTickets)).containsEntry(LottoReward.MISS, 2).containsEntry(LottoReward.FIVE_BONUS, 1)
+            .containsEntry(LottoReward.FIVE, 1).containsEntry(LottoReward.SIX, 1);
     }
 }
 
