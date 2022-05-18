a4ba103853a89566d9b85587ff028c6ed5bf005d
Celina <luv.mins99@gmail.com>
2022-05-18 23:27:15 +0900

    test(LottoGameTest): 로또 게임 메인 로직 테스트 개선 및 수동 로또 관련 테스트 추가

--- src/test/java/lotto/model/LottoGameTest.java
+++ src/test/java/lotto/model/LottoGameTest.java
@@ -2,7 +2,6 @@ package lotto.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
@@ -38,12 +37,13 @@ class LottoGameTest {
         assertThat(actual).isFalse();
     }
 
-    @ParameterizedTest(name = "로또 티켓을 {1}장 구매한다")
+    @ParameterizedTest(name = "로또 티켓을 자동 {1}장 구매한다")
     @CsvSource(value = {"1000:1", "1100:1", "10000:10"}, delimiter = ':')
     void buyLottoTicketTest(String amount, int expected) {
         // given & when
         lottoGame.insertMoney(amount);
-        int actual = lottoGame.buyLottoTicket(new RandomNumberGenerator());
+        lottoGame.purchaseManualLotto("0");
+        int actual = lottoGame.purchaseAutoLotto(new RandomNumberGenerator());
 
         // then
         assertThat(actual).isEqualTo(expected);
@@ -112,12 +112,13 @@ class LottoGameTest {
         assertThat(lottoGame.referenceValue()).isEqualTo(1);
     }
 
-    @ParameterizedTest(name = "게임의 결과 미당첨을 제외한 각 등수가 몇개인지를 반환한다")
+    @ParameterizedTest(name = "게임의 결과 미당첨을 제외한 5개의 등수 정보를 반환한다")
     @MethodSource("gameResultProvider")
-    void gameResultTest(String userInputs, String winnerInputs, String bonusNumber, HashMap<Rank, Integer> expected) {
+    void gameResultTest(String userInputs, String winnerInputs, String bonusNumber) {
         // given
         lottoGame.insertMoney("1000");
-        lottoGame.buyLottoTicket(new InputNumberGenerator(userInputs));
+        lottoGame.purchaseManualLotto("1");
+        lottoGame.inputManualLottoNumber(userInputs);
         lottoGame.winnersNumber(winnerInputs);
         lottoGame.bonusNumber(bonusNumber);
 
@@ -125,21 +126,19 @@ class LottoGameTest {
         Map<Rank, Integer> actual = lottoGame.gameResult();
 
         // then
-        assertThat(actual).isEqualTo(expected);
+        assertThat(actual).hasSize(5);
     }
 
-    // benefitResult
     @ParameterizedTest(name = "게임의 결과 수익률을 반환한다")
     @MethodSource("gameResultProvider")
-    void benefitResultTest(String userInputs, String winnerInputs, String bonusNumber,
-                           HashMap<Rank, Integer> expectedGameResult,
-                           double expected) {
+    void benefitResultTest(String userInputs, String winnerInputs, String bonusNumber, double expected) {
         // given
         lottoGame.insertMoney("1000");
-        lottoGame.buyLottoTicket(new InputNumberGenerator(userInputs));
+        lottoGame.purchaseManualLotto("1");
+        lottoGame.inputManualLottoNumber(userInputs);
         lottoGame.winnersNumber(winnerInputs);
         lottoGame.bonusNumber(bonusNumber);
-        Map<Rank, Integer> gameResult = lottoGame.gameResult();
+        lottoGame.gameResult();
 
         // when
         double actual = lottoGame.benefitResult();
@@ -149,11 +148,12 @@ class LottoGameTest {
     }
 
     @ParameterizedTest(name = "사용자가 {0}원을 입력하면 {1}개의 로또가 생성된다")
-    @MethodSource("userLotteryProvider")
+    @CsvSource(value = {"50000:50", "10000:10", "1500:1", "1000:1"}, delimiter = ':')
     void getUserLotteryTest(String money, int expected) {
         // given
         lottoGame.insertMoney(money);
-        lottoGame.buyLottoTicket(new RandomNumberGenerator());
+        lottoGame.purchaseManualLotto("0");
+        lottoGame.purchaseAutoLotto(new RandomNumberGenerator());
 
         // when
         List<Lotto> actual = lottoGame.getUserLotto();
@@ -162,87 +162,61 @@ class LottoGameTest {
         assertThat(actual).hasSize(expected);
     }
 
-    private static Stream<Arguments> gameResultProvider() {
-        return Stream.of(
-                Arguments.of("1,2,3,4,5,6", "1,2,3,4,5,6", "7", new HashMap<Rank, Integer>() {
-                    {
-                        put(Rank.FIFTH, 0);
-                        put(Rank.FOURTH, 0);
-                        put(Rank.THIRD, 0);
-                        put(Rank.SECOND, 0);
-                        put(Rank.FIRST, 1);
-                    }
-                }, 2000000.00),
-                Arguments.of("1,2,3,4,5,7", "1,2,3,4,5,6", "7", new HashMap<Rank, Integer>() {
-                    {
-                        put(Rank.FIFTH, 0);
-                        put(Rank.FOURTH, 0);
-                        put(Rank.THIRD, 0);
-                        put(Rank.SECOND, 1);
-                        put(Rank.FIRST, 0);
-                    }
-                }, 30000.00),
-                Arguments.of("1,2,3,4,5,8", "1,2,3,4,5,6", "7", new HashMap<Rank, Integer>() {
-                    {
-                        put(Rank.FIFTH, 0);
-                        put(Rank.FOURTH, 0);
-                        put(Rank.THIRD, 1);
-                        put(Rank.SECOND, 0);
-                        put(Rank.FIRST, 0);
-                    }
-                }, 1500.00),
-                Arguments.of("1,2,3,4,8,9", "1,2,3,4,5,6", "7", new HashMap<Rank, Integer>() {
-                    {
-                        put(Rank.FIFTH, 0);
-                        put(Rank.FOURTH, 1);
-                        put(Rank.THIRD, 0);
-                        put(Rank.SECOND, 0);
-                        put(Rank.FIRST, 0);
-                    }
-                }, 50.00),
-                Arguments.of("1,2,3,8,9,10", "1,2,3,4,5,6", "7", new HashMap<Rank, Integer>() {
-                    {
-                        put(Rank.FIFTH, 1);
-                        put(Rank.FOURTH, 0);
-                        put(Rank.THIRD, 0);
-                        put(Rank.SECOND, 0);
-                        put(Rank.FIRST, 0);
-                    }
-                }, 5.00),
-                Arguments.of("1,2,8,9,10,11", "1,2,3,4,5,6", "7", new HashMap<Rank, Integer>() {
-                    {
-                        put(Rank.FIFTH, 0);
-                        put(Rank.FOURTH, 0);
-                        put(Rank.THIRD, 0);
-                        put(Rank.SECOND, 0);
-                        put(Rank.FIRST, 0);
-                    }
-                }, 0.00),
-                Arguments.of("1,10,11,12,13,14", "1,2,3,4,5,6", "7", new HashMap<Rank, Integer>() {
-                    {
-                        put(Rank.FIFTH, 0);
-                        put(Rank.FOURTH, 0);
-                        put(Rank.THIRD, 0);
-                        put(Rank.SECOND, 0);
-                        put(Rank.FIRST, 0);
-                    }
-                }, 0.00),
-                Arguments.of("8,9,10,11,12,13", "1,2,3,4,5,6", "7", new HashMap<Rank, Integer>() {
-                    {
-                        put(Rank.FIFTH, 0);
-                        put(Rank.FOURTH, 0);
-                        put(Rank.THIRD, 0);
-                        put(Rank.SECOND, 0);
-                        put(Rank.FIRST, 0);
-                    }
-                }, 0.00)
-        );
+    @ParameterizedTest(name = "{0}원으로 수동 로또 {1}장을 구매가능한지 여부를 반환한다")
+    @CsvSource(value = {"10000:0:true", "10000:10:true", "1000:10:false", "1000:2:false"}, delimiter = ':')
+    void purchaseManualLottoTest(String money, String countOfManualLotto, boolean expected) {
+        // given
+        lottoGame.insertMoney(money);
+
+        // when
+        boolean actual = lottoGame.purchaseManualLotto(countOfManualLotto);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
     }
 
-    private static Stream<Arguments> userLotteryProvider() {
-        return Stream.of(Arguments.of("10000", 10),
-                Arguments.of("50000", 50),
-                Arguments.of("3000", 3),
-                Arguments.of("1010", 1));
+    @ParameterizedTest(name = "유효한 번호를 입력받으면 유저의 로또 객체가 생성된다")
+    @ValueSource(strings = {"1,2,3,4,5,6", "10, 15, 20, 25, 30, 35"})
+    void inputManualLottoTest(String inputs) {
+        // given
+        String countOfManualLotto = "1";
+        lottoGame.insertMoney("1000");
+
+        // when
+        lottoGame.purchaseManualLotto(countOfManualLotto);
+        lottoGame.inputManualLottoNumber(inputs);
+        List<Lotto> actual = lottoGame.getUserLotto();
+
+        // then
+        assertThat(actual).isNotEmpty();
+    }
+
+    @ParameterizedTest(name = "유효하지 않은 로또 번호를 입력하면 로또 객체가 생성되지 않는다")
+    @ValueSource(strings = {"1,2,3,4,5", "1,2,3,4,5,6,7", "1,2,3,4,5,5", "일,이,삼,사,오,륙"})
+    void invalidInputManualLottoTest(String inputs) {
+        // given
+        String countOfManualLotto = "1";
+        lottoGame.insertMoney("1000");
+
+        // when
+        lottoGame.purchaseManualLotto(countOfManualLotto);
+        lottoGame.inputManualLottoNumber(inputs);
+        List<Lotto> actual = lottoGame.getUserLotto();
+
+        // then
+        assertThat(actual).isEmpty();
+    }
+
+    private static Stream<Arguments> gameResultProvider() {
+        return Stream.of(
+                Arguments.of("1,2,3,4,5,6", "1,2,3,4,5,6", "7", 2000000.00),
+                Arguments.of("1,2,3,4,5,7", "1,2,3,4,5,6", "7", 30000.00),
+                Arguments.of("1,2,3,4,5,8", "1,2,3,4,5,6", "7", 1500.00),
+                Arguments.of("1,2,3,4,8,9", "1,2,3,4,5,6", "7", 50.00),
+                Arguments.of("1,2,3,8,9,10", "1,2,3,4,5,6", "7", 5.00),
+                Arguments.of("1,2,8,9,10,11", "1,2,3,4,5,6", "7", 0.00),
+                Arguments.of("1,10,11,12,13,14", "1,2,3,4,5,6", "7", 0.00),
+                Arguments.of("8,9,10,11,12,13", "1,2,3,4,5,6", "7", 0.00)
+        );
     }
 }
