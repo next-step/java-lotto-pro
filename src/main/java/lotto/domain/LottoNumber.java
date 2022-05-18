dbf604535dbf5006f484e1553d414f276dc89e9d
millennion-jin <Jadestern2@>
2022-05-19 03:15:46 +0900

    refactor(LottoNumber) : LottoNumber 의 변수인 number의 자료형을 Integer에서 int로 변경

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -5,30 +5,21 @@ import lotto.constants.LottoConstants;
 
 public class LottoNumber {
 
-    private static final String ERROR_MESSAGE_NUMBER_IS_NULL = "[ERROR] This number is null.";
     private static final String ERROR_MESSAGE_NUMBER_OUT_OF_RANGE = "[ERROR] This number is out of range.";
 
-    private final Integer number;
+    private final int number;
 
-    public LottoNumber(final Integer number) {
-        validate(number);
+    public LottoNumber(final int number) {
+        validateNumberRange(number);
 
         this.number = number;
     }
 
-    public Integer getNumber() {
+    public int getNumber() {
         return number;
     }
 
-    private void validate(final Integer number) {
-        if(number == null) {
-            throw new IllegalArgumentException(ERROR_MESSAGE_NUMBER_IS_NULL);
-        }
-
-        validateNumberRange(number);
-    }
-
-    private void validateNumberRange(final Integer number) {
+    private void validateNumberRange(final int number) {
         if (number < LottoConstants.MIN_LOTTO_NUMBER || number > LottoConstants.MAX_LOTTO_NUMBER) {
             throw new IllegalArgumentException(ERROR_MESSAGE_NUMBER_OUT_OF_RANGE);
         }
@@ -39,7 +30,7 @@ public class LottoNumber {
         if (!(obj instanceof LottoNumber)) {
             return false;
         }
-        return this.number.equals(((LottoNumber) obj).getNumber());
+        return this.number == ((LottoNumber) obj).getNumber();
     }
 
     @Override
