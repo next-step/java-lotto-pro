970a7861777862ad93572a1894b400d412cd76dc
Woody <yuncheol92@naver.com>
2022-05-18 23:19:18 +0900

    test : convertIntegers 메소드 테스트 코드 추가

--- src/test/java/lotto/infrastructure/common/util/StringUtilsTest.java
+++ src/test/java/lotto/infrastructure/common/util/StringUtilsTest.java
@@ -1,11 +1,15 @@
 package lotto.infrastructure.common.util;
 
 import lotto.infrastructure.util.StringUtils;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class StringUtilsTest {
 
@@ -44,5 +48,17 @@ class StringUtilsTest {
     @ValueSource(strings = {"1, 2, 3", "1\t,\t2,\t3", "1\n,2\n,3\n"})
     public void splitAndTrim(String text) {
         assertThat(StringUtils.splitAndTrim(text, ",")).containsExactly("1", "2", "3");
+    }
+
+    @Test
+    public void convertInters_성공() {
+        assertThat(StringUtils.convertIntegers(Arrays.asList("1", "2", "3"))).containsExactly(1, 2, 3);
+    }
+
+    @Test
+    public void convertInters_실패() {
+        assertThatThrownBy(() -> {
+            StringUtils.convertIntegers(Arrays.asList("a", "b", "c"));
+        }).isInstanceOf(NumberFormatException.class);
     }
 }
