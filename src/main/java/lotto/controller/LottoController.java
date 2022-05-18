0317c4442a973b300a2c68d02db5fd3a2b86dee1
lcjltj <lcjltj@gmail.com>
2022-05-19 00:06:43 +0900

    feat(LottoController): LottoController 클래스 추가 외 1
    - LottoController 클래스 추가
        - buyLotto(money) 입력 금액 만큼 로또 구입
        - exchangePrize(Lottoes, answer) 구입한 로또들과 당첨번호로 총 지급액 계산
        - printYield(totalPrize, money) 구입한 금액 당첨금액 수익율 계산
    - ResultView 로또 번호 출력 메소드 추가

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,40 @@
+package lotto.controller;
+
+import lotto.domain.Lotto;
+import lotto.domain.LottoMachine;
+import lotto.domain.Prize;
+import lotto.domain.PrizeExchanger;
+import lotto.view.ResultView;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.List;
+
+public class LottoController {
+    private static final LottoController INSTANCE = new LottoController();
+    private static final int YIELD_SCALE = 2;
+
+    private LottoController() {
+    }
+
+    public static LottoController getInstance() {
+        return INSTANCE;
+    }
+
+    public List<Lotto> buyLotto(final long money) {
+        return LottoMachine.purchase(money);
+    }
+
+    public long exchangePrize(final List<Lotto> lottoes, final Lotto answer) {
+        PrizeExchanger prizeExchanger = PrizeExchanger.getInstance();
+        ResultView.statisticMessage();
+        return prizeExchanger.exchange(Prize.matchLotto(lottoes, answer));
+    }
+
+    public void printYield(final long totalPrize, final long money) {
+        float yield = new BigDecimal(totalPrize)
+                .divide(new BigDecimal(money), YIELD_SCALE, RoundingMode.CEILING)
+                .floatValue();
+        ResultView.resultYield(yield);
+    }
+}
