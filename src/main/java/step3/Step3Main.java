27c1db72b5a75728da259c31ea4bd6a90040662f
jhsong2580 <thdwmdgns@naver.com>
2022-05-18 16:19:28 +0900

    refactor: LottoMachine 생성자 변경에 따른 호출문 재정의

--- src/main/java/step3/Step3Main.java
+++ src/main/java/step3/Step3Main.java
@@ -2,7 +2,6 @@ package step3;
 
 import java.util.Scanner;
 import step3.controller.LottoController;
-import step3.domain.LottoManager;
 import step3.model.LottoMachine;
 import step3.view.InputView;
 import step3.view.OutputView;
@@ -11,7 +10,7 @@ public class Step3Main {
 
     public static void main(String[] args) {
         final Scanner s = new Scanner(System.in);
-        LottoController lottoController = new LottoController(new LottoMachine(new LottoManager()), new InputView(s), new OutputView());
+        LottoController lottoController = new LottoController(new LottoMachine(), new InputView(s), new OutputView());
         lottoController.startLotto();
 
     }
