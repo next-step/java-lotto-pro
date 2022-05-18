bffdf8dc222e15affc3189667b420e8220b3d46f
kms <kmmin78@gmail.com>
2022-05-18 21:59:19 +0900

    refactor : step4 코드 리뷰 반영

--- src/main/java/lotto/constants/Matched.java
+++ src/main/java/lotto/constants/Matched.java
@@ -1,21 +1,18 @@
 package lotto.constants;
 
 public enum Matched {
-    NOT_MATCHED(0),
-    THREE_MATCHED(5_000),
-    FOUR_MATCHED(50_000),
-    FIVE_MATCHED(1_500_000),
-    FIVE_AND_BONUS_MATCHED(30_000_000),
-    SIX_MATCHED(2_000_000_000);
+    NOT_MATCHED(0, 0),
+    THREE_MATCHED(3, 5_000),
+    FOUR_MATCHED(4, 50_000),
+    FIVE_MATCHED(5, 1_500_000),
+    FIVE_AND_BONUS_MATCHED(5, 30_000_000),
+    SIX_MATCHED(6, 2_000_000_000);
 
-    private static int THREE_MATCHED_COUNT = 3;
-    private static int FOUR_MATCHED_COUNT = 4;
-    private static int FIVE_MATCHED_COUNT = 5;
-    private static int SIX_MATCHED_COUNT = 6;
-
+    private final int count;
     private final int reward;
 
-    Matched(final int reward) {
+    Matched(final int count, final int reward) {
+        this.count = count;
         this.reward = reward;
     }
 
@@ -24,19 +21,19 @@ public enum Matched {
     }
 
     public static Matched getByCountAndBonusMatched(final int count, final boolean bonusMatched) {
-        if(THREE_MATCHED_COUNT == count) {
+        if(THREE_MATCHED.count == count) {
             return THREE_MATCHED;
         }
-        if(FOUR_MATCHED_COUNT == count) {
+        if(FOUR_MATCHED.count == count) {
             return FOUR_MATCHED;
         }
-        if(FIVE_MATCHED_COUNT == count && !bonusMatched) {
+        if(FIVE_MATCHED.count == count && !bonusMatched) {
             return FIVE_MATCHED;
         }
-        if(FIVE_MATCHED_COUNT == count && bonusMatched) {
+        if(FIVE_MATCHED.count == count && bonusMatched) {
             return FIVE_AND_BONUS_MATCHED;
         }
-        if(SIX_MATCHED_COUNT == count) {
+        if(SIX_MATCHED.count == count) {
             return SIX_MATCHED;
         }
         return NOT_MATCHED;
