2edf5a1d1e2f6c104102fb6f700f25d8bf400cf8
Philip <pch4623@gmail.com>
2022-05-19 00:04:37 +0900

    refactor(Rank): 당첨금액 관련 데이터 타입을 Prize로 변경

--- src/main/java/lotto/enums/Rank.java
+++ src/main/java/lotto/enums/Rank.java
@@ -1,18 +1,20 @@
 package lotto.enums;
 
+import lotto.model.Prize;
+
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
-    THIRD(4, 1_500_000),
-    FOURTH(3, 50_000),
-    LOSE(0, 0);
+    FIRST(6, Prize.of(2_000_000_000)),
+    SECOND(5, Prize.of(30_000_000)),
+    THIRD(4, Prize.of(1_500_000)),
+    FOURTH(3, Prize.of(50_000)),
+    LOSE(0, Prize.of(0));
 
     private final int matchingCount;
-    private final int prize;
+    private final Prize prize;
 
-    Rank(int matchingCount, int prize) {
+    Rank(int matchingCount, Prize prize) {
         this.matchingCount = matchingCount;
         this.prize = prize;
     }
@@ -22,5 +24,9 @@ public enum Rank {
                 .filter(rank -> rank.matchingCount == numberOfMatch)
                 .findAny()
                 .orElse(LOSE);
+    }
+
+    public Prize getPrize() {
+        return prize;
     }
 }
