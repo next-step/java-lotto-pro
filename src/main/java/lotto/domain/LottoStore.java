936de3ce05ef02f4a1f74d996fb5038655561231
mukeunzi <eunji057@gmail.com>
2022-05-18 22:29:43 +0900

    refactor(domain): 의미없는 코드 삭제

--- src/main/java/lotto/domain/LottoStore.java
+++ src/main/java/lotto/domain/LottoStore.java
@@ -6,9 +6,6 @@ import java.util.List;
 public class LottoStore {
     public static final int LOTTO_PRICE = 1000;
 
-    public LottoStore() {
-    }
-
     public List<LottoTicket> buy(Money money) {
         List<LottoTicket> lottoAutoNumbers = new ArrayList<>();
         for (int i = 0; i < money.getMoney() / LOTTO_PRICE; i++) {
