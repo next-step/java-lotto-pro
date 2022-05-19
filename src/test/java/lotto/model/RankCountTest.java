cd0bd6070cefeefeb520866988b37db0ebcf1689
Philip <pch4623@gmail.com>
2022-05-20 01:06:29 +0900

    test(RankCountTest): toString 테스트

--- src/test/java/lotto/model/RankCountTest.java
+++ src/test/java/lotto/model/RankCountTest.java
@@ -27,4 +27,21 @@ class RankCountTest {
         assertThat(earningRate.getEarningRate())
                 .isCloseTo(BigDecimal.valueOf(3150), Percentage.withPercentage(100));
     }
+
+    @Test
+    @DisplayName("toString 메서드가 정상적으로 문자열을 리턴하는지 확인한다.")
+    void earningRate_toString() {
+        RankCount rankCount = RankCount.from(new LinkedHashMap<Rank, Integer>() {{
+            put(SECOND, 3);
+            put(FOURTH, 1);
+            put(THIRD, 2);
+            put(FIRST, 1);
+        }});
+
+        assertThat(rankCount.toString())
+                .contains("3개 일치 (5000원)- 1개")
+                .contains("4개 일치 (50000원)- 2개")
+                .contains("5개 일치 (1500000원)- 3개")
+                .contains("6개 일치 (2000000000원)- 1개");
+    }
 }
