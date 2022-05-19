647be1f60247cea4e9ff9a028ea2a46b08d11408
Philip <pch4623@gmail.com>
2022-05-20 01:19:16 +0900

    feat(ResultView): 수익률 정보 출력

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -1,10 +1,12 @@
 package lotto.view;
 
+import lotto.model.Earning;
 import lotto.model.LottoNumbers;
 import lotto.model.RankCount;
 
 public class ResultView {
     private static final String PURCHASE_COUNT_OUTPUT_MESSAGE = "%d개를 구매했습니다.";
+    private static final String EARNINGS_RATE_OUTPUT_MESSAGE = "총 수익률은 %s입니다.";
 
     public static void printPurchaseCountView(int count) {
         System.out.printf(PURCHASE_COUNT_OUTPUT_MESSAGE + "\n", count);
@@ -21,5 +23,10 @@ public class ResultView {
 
     public static void printRankCount(RankCount rankCount) {
         System.out.println(rankCount);
+    }
+
+    public static void printEarning(Earning earning) {
+        System.out.printf(EARNINGS_RATE_OUTPUT_MESSAGE, earning);
+        System.out.println();
     }
 }
