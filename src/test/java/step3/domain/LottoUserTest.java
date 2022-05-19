7be9fc6597845cdac7c50622152386e0f250c36f
jhsong2580 <thdwmdgns@naver.com>
2022-05-19 17:51:34 +0900

    test: user.getnumbers 테스트 추가

--- src/test/java/step3/domain/LottoUserTest.java
+++ src/test/java/step3/domain/LottoUserTest.java
@@ -1,9 +1,12 @@
 package step3.domain;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import step3.model.LottoUser;
 
 public class LottoUserTest {
@@ -15,10 +18,23 @@ public class LottoUserTest {
         user = new LottoUser();
     }
 
-    @ParameterizedTest
-    @CsvSource(value = {"1000:true", "a:false", "999:true", "-1:false"}, delimiter = ':')
-    public void setMoneyTest(String money, boolean expected) {
-        Assertions.assertThat(user.setMoney(money)).isEqualTo(expected);
+    @Test
+    @DisplayName("user의 getLottoNumbers는 LottoTicket의 getLottoNumbers의 값을 받아온다")
+    public void getLottoNumbersTest() {
+        List<LottoTicket> lottoTickets = new ArrayList<>();
+        lottoTickets.add(new LottoTicket("1,2,3,4,5,6"));
+        lottoTickets.add(new LottoTicket("7,2,3,4,5,6"));
+        lottoTickets.add(new LottoTicket("8,2,3,4,5,6"));
+        lottoTickets.add(new LottoTicket("9,2,3,4,5,6"));
+
+        user.setUserLotto(lottoTickets);
+
+        assertThat(user.getLottoNumbers())
+            .hasSize(4)
+            .contains(new LottoTicket("1,2,3,4,5,6").getLottoNumbers())
+            .contains(new LottoTicket("7,2,3,4,5,6").getLottoNumbers())
+            .contains(new LottoTicket("8,2,3,4,5,6").getLottoNumbers())
+            .contains(new LottoTicket("9,2,3,4,5,6").getLottoNumbers());
     }
 
 }
