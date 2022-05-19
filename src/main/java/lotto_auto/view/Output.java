810e6b7671c1598feb91b040547f5fe44ff6033a
bestsilver <dkvkxm99@gmail.com>
2022-05-19 21:33:42 +0900

    refactor: 수동 구매 안내글 추가

--- src/main/java/lotto_auto/view/Output.java
+++ src/main/java/lotto_auto/view/Output.java
@@ -6,7 +6,6 @@ import java.util.stream.Collectors;
 
 public class Output {
     public static final String MONEY_INPUT_NOTICE = "구입금액을 입력해 주세요.";
-    public static final String PURCHASE_LOTTO_COUNT_FORMAT = "%d개를 구매했습니다.\n";
     public static final String WINNING_LOTTO_INPUT_NOTICE = "지난 주 당첨 번호를 입력해 주세요.";
     public static final String PROFIT_RATE_NOTICE_FORMAT = "총 수익률은 %s입니다.";
     public static final String NEGATIVE_PROFIT_RATE_NOTICE = "(기준이 1이기 때문에 결과적으로 손해라는 의미임)";
@@ -14,6 +13,10 @@ public class Output {
 
     public static final String FIGURES_FORMAT = "%d개 일치 (%d원)- %d개\n";
     public static final String SECOND_FIGURES_FORMAT = "%d개 일치, 보너스 볼 일치(%d) - %d개\n";
+
+    public static final String MANUAL_LOTTO_COUNT_INPUT_NOTICE = "\n수동으로 구매할 로또 수를 입력해 주세요.";
+    public static final String MANUAL_LOTTO_INPUT_NOTICE = "\n수동으로 구매할 번호를 입력해 주세요.";
+    public static final String PURCHASED_LOTTO_COUNT_FORMAT = "수동으로 %d장, 자동으로 %d개를 구매했습니다.\n";
 
     public void showMoneyInputNotice() {
         System.out.println(MONEY_INPUT_NOTICE);
@@ -25,8 +28,12 @@ public class Output {
         System.out.format(format, profitRate.printRate());
     }
 
-    public void showPurchaseLottoCountNotice(int count) {
-        System.out.format(PURCHASE_LOTTO_COUNT_FORMAT, count);
+    public void showPurchaseManualLottoNotice() {
+        System.out.println(MANUAL_LOTTO_INPUT_NOTICE);
+    }
+
+    public void showPurchasedLottoCountNotice(PurchaseInfo info) {
+        System.out.format(PURCHASED_LOTTO_COUNT_FORMAT, info.getManualLottoCount(), info.getAutoLottoCount());
     }
 
     public void showPurchasedLottos(Lottos purchasedLottos) {
@@ -56,6 +63,10 @@ public class Output {
 
     public void showBonusBallInputNotice() {
         System.out.println(BONUS_INPUT_NOTICE);
+    }
+
+    public void showManualLottoCountNotice() {
+        System.out.println(MANUAL_LOTTO_COUNT_INPUT_NOTICE);
     }
 
     public void showError(Throwable e) {
