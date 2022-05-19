f700d5fa925387b6d6728649c289a79e7e0af9b9
ssamzag <ssamzag@gmail.com>
2022-05-19 17:36:15 +0900

    feat&refactor(Money.java): sum 함수 및 이익률 계산, krw 반환 메서드 추가

--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -19,4 +19,33 @@ class MoneyTest {
         assertThatIllegalArgumentException().isThrownBy(() -> new Money(999)).withMessage("금액이 부족합니다.(최소필요금액: 1000)");
     }
 
+    @Test
+    @DisplayName("구입금액이 로또 최소금액 1000원 미만이면 true 를 반환한다")
+    void lessThenLottoPriceTrue() {
+        assertThat(new Money(999).lessThenLottoPrice()).isTrue();
+    }
+
+    @Test
+    @DisplayName("구입금액이 로또 최소금액 1000원 이상이면 false 를 반환한다")
+    void lessThenLottoPriceFalse() {
+        assertThat(new Money(1000).lessThenLottoPrice()).isFalse();
+    }
+
+    @Test
+    @DisplayName("5000원에 5000원을 더하면 10000원을 가진 Money 객체를 반환한다")
+    void sumMoney() {
+        assertThat(new Money(5000).sumMoney(new Money(5000))).isEqualTo(new Money(10000));
+    }
+
+    @Test
+    @DisplayName("구입금액5000, 당첨금액 5000을 입력하면 수익률은 1이다")
+    void calculateRateOfReturn() {
+        assertThat(new Money(5000).calculateRateOfReturn(new Money(5000))).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("멤버변수 값이 5000이면 문자열 ₩ 5000을 반환한다")
+    void krw() {
+        assertThat(new Money(5000).krw()).isEqualTo("₩ 5,000");
+    }
 }
