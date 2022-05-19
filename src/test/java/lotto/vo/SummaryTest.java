506d341810e135c78febf337d1a1e2efd4566d59
tasklet1579 <tasklet1579@gmail.com>
2022-05-19 10:48:05 +0900

    refactor(SummaryTest) : 테스트 케이스 분리

--- src/test/java/lotto/vo/SummaryTest.java
+++ src/test/java/lotto/vo/SummaryTest.java
@@ -2,6 +2,7 @@ package lotto.vo;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -9,18 +10,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class SummaryTest {
     @Test
-    void 로또_수익_금액() {
+    void 로또_수익_금액_합계() {
         Result result1 = new Result(3, 1, 5000);
         Result result2 = new Result(4, 2, 50000);
         Result result3 = new Result(5, 3, 1500000);
         Result result4 = new Result(6, 1, 2000000000);
-        List<Result> results = new LinkedList<>();
-        results.add(result1);
-        results.add(result2);
-        results.add(result3);
-        results.add(result4);
+        List<Result> results = Arrays.asList(result1, result2, result3, result4);
+
         Summary summary = new Summary(results);
+
         assertThat(summary.sum()).isEqualTo(2004605000);
+    }
+
+    @Test
+    void 로또_수익_금액_없음() {
         assertThat(new Summary(new LinkedList<>()).sum()).isEqualTo(0);
     }
 }
