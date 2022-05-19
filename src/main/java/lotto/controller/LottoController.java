d209cf37c827d0495da026a92810c4a6520bd222
boss.back <boss.back@kakaocorp.com>
2022-05-20 01:11:39 +0900

    fix: 핵심 로직에서 UI 로직을 제거하고 주입을 받도록 변경

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -3,7 +3,9 @@ package lotto.controller;
 import java.util.List;
 import lotto.domain.Lotto;
 import lotto.domain.LottoManager;
+import lotto.domain.LottoNumber;
 import lotto.domain.LottoPrice;
+import lotto.domain.LottoUtil;
 import lotto.strategy.ManualPickNumberStrategy;
 import lotto.view.InputView;
 import lotto.view.ResultView;
@@ -12,7 +14,6 @@ public class LottoController {
 
     public static void startLotto() {
         int purchaseAmount = InputView.enterNumber();
-
         int autoLottoCount = LottoPrice.numberOfLottoCanBuy(purchaseAmount);
         LottoManager lottoManager = new LottoManager(autoLottoCount);
 
@@ -20,11 +21,13 @@ public class LottoController {
 
         ResultView.purchaseLottoResult(autoLottoCount, lottoElements);
 
-        Lotto winningLotto = new Lotto(new ManualPickNumberStrategy());
+        int[] numberArray = InputView.enterManualLotto();
+        List<LottoNumber> lottoNumbers = LottoUtil.convertToLottoNumber(numberArray);
+        Lotto winningLotto = new Lotto(new ManualPickNumberStrategy(lottoNumbers));
+
         lottoManager.makeWinningLotto(winningLotto);
 
         ResultView.winningResult(lottoManager.getWinningStatistics());
-
         ResultView.yieldResult(lottoManager.calculateRateOfReturn(purchaseAmount));
     }
 }
--- src/main/java/lotto/domain/LottoUtil.java
+++ src/main/java/lotto/domain/LottoUtil.java
@@ -5,35 +5,15 @@ import java.util.List;
 
 public class LottoUtil {
 
-    private static final int LOTTO_SIZE = 6;
-
     private LottoUtil() {
         throw new UnsupportedOperationException();
     }
 
     public static List<LottoNumber> convertToLottoNumber(int[] intArray) {
-        validateLottoSize(intArray.length);
-
         List<LottoNumber> lottoNumbers = new ArrayList<>();
         for (int number : intArray) {
             lottoNumbers.add(new LottoNumber(number));
         }
         return lottoNumbers;
-    }
-
-    public static int[] convertLottoStringToIntArray(String[] lottoString) {
-        validateLottoSize(lottoString.length);
-
-        int[] lottoNumbers = new int[lottoString.length];
-        for (int i = 0; i < lottoString.length; i++) {
-            lottoNumbers[i] = Integer.parseInt(lottoString[i]);
-        }
-        return lottoNumbers;
-    }
-
-    private static void validateLottoSize(int lottoLength) {
-        if (lottoLength != LOTTO_SIZE) {
-            throw new IllegalArgumentException("6자리 숫자를 , 로 구분하여 입력하세요");
-        }
     }
 }
