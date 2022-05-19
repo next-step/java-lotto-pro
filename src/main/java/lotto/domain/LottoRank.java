c1f1d619b3f39a97f3af12629c81751f95853c70
ssamzag <ssamzag@gmail.com>
2022-05-19 14:43:53 +0900

    chore(LottoRank.java): of 정적 팩토리 메서드를 생성자 아래로 이동

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public enum LottoRank {
     MISS(0, 0),
     FIFTH(3, 5_000),
-    FOURTH(4,50_000),
+    FOURTH(4, 50_000),
     THIRD(5, 1_500_000),
     FIRST(6, 2_000_000_000);
 
@@ -17,6 +17,13 @@ public enum LottoRank {
         this.winningMoney = money;
     }
 
+    public static LottoRank of(int matchCount) {
+        return Arrays.stream(LottoRank.values())
+                .filter(r -> r.matchCount == matchCount)
+                .findFirst()
+                .orElse(MISS);
+    }
+
     public int getMatchCount() {
         return matchCount;
     }
@@ -25,10 +32,4 @@ public enum LottoRank {
         return winningMoney;
     }
 
-    public static LottoRank of(int matchCount) {
-        return Arrays.stream(LottoRank.values())
-                .filter(r -> r.matchCount == matchCount)
-                .findFirst()
-                .orElse(MISS);
-    }
 }
