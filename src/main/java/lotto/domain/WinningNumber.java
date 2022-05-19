4f0c8bf2280f6df4bf16f751e95a3f29470a930f
ssamzag <ssamzag@gmail.com>
2022-05-19 17:31:46 +0900

    feat(WinningNumber.java): 당첨 로또번호와 보너스를 가지는 클래스 생성

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -1,2 +1,25 @@
-package lotto.domain;public class WinningNumber {
+package lotto.domain;
+
+import lotto.domain.factory.LottoFactory;
+
+public class WinningNumber {
+    private final Lotto winningLotto;
+    private final LottoNumber bonusNumber;
+
+    public WinningNumber(String winningLotto, String bonusNumber) {
+        this(LottoFactory.create(winningLotto), LottoNumber.of(bonusNumber));
+    }
+
+    public WinningNumber(Lotto winningLotto, LottoNumber bonusNumber) {
+        this.winningLotto = winningLotto;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public int matchCount(Lotto lotto) {
+        return lotto.matchCount(winningLotto);
+    }
+
+    public boolean bonus(Lotto lotto) {
+        return lotto.contains(bonusNumber);
+    }
 }
