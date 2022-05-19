8dd3cdc3a30a85e727b8190be09c6c278ce4c525
limwoobin <drogba02@naver.com>
2022-05-19 22:08:30 +0900

    feat: 요구사항에 맞는 비지니스 플로우를 작성하라

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,23 +1,33 @@
 package lotto.controller;
 
-import lotto.domain.BonusNumber;
-import lotto.domain.LottoScore;
 import lotto.domain.LottoShop;
+import lotto.domain.LottoNo;
+import lotto.domain.LottoScore;
 import lotto.domain.Lottos;
+import lotto.domain.ManualLottoCount;
+import lotto.domain.Price;
 import lotto.domain.WinningNumbers;
 import lotto.domain.Winnings;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoController {
+
     public static void play() {
         int purchaseAmount = InputView.inputPurchaseAmount();
-        Lottos lottos = LottoShop.sale(purchaseAmount);
-        OutputView.printPurchaseCount(lottos.getLottos().size());
-        OutputView.printLottoNumbers(lottos.getLottos());
+        Price price = new Price(purchaseAmount);
+
+        ManualLottoCount manualLottoCount = new ManualLottoCount(InputView.inputManualLottoCount());
+        Lottos manualLottos = LottoShop.buyManualLottos(InputView.inputManualLottoNumber(
+            manualLottoCount.getManualLottoCount()), price);
+        Lottos autoLottos = LottoShop.buyAutoLottos(price);
+
+        OutputView.printPurchaseCountByLottoType(manualLottos.size(), autoLottos.size());
+        OutputView.printLottoNumbers(autoLottos.getLottos());
+        Lottos lottos = Lottos.merge(manualLottos, autoLottos);
 
         WinningNumbers winningNumbers = new WinningNumbers(InputView.inputWinningNumbers());
-        BonusNumber bonusNumber = new BonusNumber(InputView.inputBonusNumber());
+        LottoNo bonusNumber = new LottoNo(InputView.inputBonusNumber());
         winningNumbers.addBonusNumber(bonusNumber);
 
         LottoScore lottoScore = lottos.calculateLottoScore(winningNumbers);
