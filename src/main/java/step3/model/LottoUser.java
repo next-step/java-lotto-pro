046820211711c63c97303bd2a53cf39d08f88442
jhsong2580 <thdwmdgns@naver.com>
2022-05-19 16:54:13 +0900

    refactor: 일급컬렉션으로 만들기 위해 money,ticket 관련 로직 삭제

--- src/main/java/step3/model/LottoUser.java
+++ src/main/java/step3/model/LottoUser.java
@@ -5,30 +5,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 import step3.domain.LottoElement;
 import step3.domain.LottoTicket;
-import step3.domain.Money;
 
 public class LottoUser {
-
-    private Money money = null;
-    private int ticket = 0;
     private List<LottoTicket> lottoTickets = new ArrayList<>();
-
-    public boolean setMoney(String money) {
-        try {
-            this.money = new Money(money);
-            return true;
-        } catch (IllegalArgumentException e) {
-            this.money = new Money(0);
-            System.out.println(e.getMessage());
-            return false;
-        }
-    }
-
-    public boolean setTicket(int ticket) {
-        int noTicket = 0;
-        this.ticket = ticket;
-        return ticket != noTicket;
-    }
 
 
     public void setUserLotto(List<LottoTicket> lottoTickets) {
@@ -38,15 +17,6 @@ public class LottoUser {
     public List<List<LottoElement>> getLottoNumbers() {
         return lottoTickets.stream().map(LottoTicket::getLottoNumbers).collect(Collectors.toList());
     }
-
-    public Money getMoney() {
-        return money;
-    }
-
-    public int getTicket() {
-        return ticket;
-    }
-
     public List<LottoTicket> getLottoTickets() {
         return lottoTickets;
     }
