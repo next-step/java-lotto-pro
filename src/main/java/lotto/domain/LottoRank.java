6e728a62e6383c4ba4179b1b21fb5287739d00f9
ssamzag <ssamzag@gmail.com>
2022-05-19 16:42:25 +0900

    refactor(LottoRank.java): winningMoney 를 money 형으로 변경 및 2등 추가

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -7,29 +7,40 @@ public enum LottoRank {
     FIFTH(3, 5_000),
     FOURTH(4, 50_000),
     THIRD(5, 1_500_000),
+    SECOND(5, 30_000_000),
     FIRST(6, 2_000_000_000);
 
     private final int matchCount;
-    private final double winningMoney;
+    private final Money winningMoney;
 
-    LottoRank(int matchCount, double money) {
+    LottoRank(int matchCount, double winningMoney) {
         this.matchCount = matchCount;
-        this.winningMoney = money;
+        this.winningMoney = new Money(winningMoney);
     }
 
-    public static LottoRank of(int matchCount) {
+    public static LottoRank of(int matchCount, boolean hasBonus) {
+        if (isSecond(matchCount, hasBonus)) {
+            return SECOND;
+        }
         return Arrays.stream(LottoRank.values())
                 .filter(r -> r.matchCount == matchCount)
                 .findFirst()
                 .orElse(MISS);
     }
 
+    private static boolean isSecond(int matchCount, boolean hasBonus) {
+        return matchCount == SECOND.matchCount && hasBonus;
+    }
+
     public int getMatchCount() {
         return matchCount;
     }
 
-    public double getWinningMoney() {
+    public Money getWinningMoney() {
         return winningMoney;
     }
 
+    public Money sumWinningMoney(Money moneySum) {
+        return winningMoney.sumMoney(moneySum);
+    }
 }
