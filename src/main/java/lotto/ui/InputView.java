fb6cdbe82252a0a5f5b6947de0f59754cfa5aa6a
ssamzag <ssamzag@gmail.com>
2022-05-19 17:38:50 +0900

    feat(InputView.java): 보너스볼 입력 추가

--- src/main/java/lotto/ui/InputView.java
+++ src/main/java/lotto/ui/InputView.java
@@ -4,11 +4,16 @@ import java.util.Scanner;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
-    private static final String INPUT_BUY_PRICE_MESSAEG = "구매금액을 입력해 주세요.";
+    private static final String INPUT_BUY_PRICE_MESSAGE = "구매금액을 입력해 주세요.";
     private static final String INPUT_WINNING_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
 
     public static String getBuyPrice() {
-        System.out.println(INPUT_BUY_PRICE_MESSAEG);
+        System.out.println(INPUT_BUY_PRICE_MESSAGE);
+        return scanner.nextLine();
+    }
+
+    public static String getBonusNumber() {
+        System.out.println("보너스 볼을 입력해 주세요.");
         return scanner.nextLine();
     }
 
