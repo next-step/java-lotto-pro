d8470e7c3fe20991fa50eb2874fd1d1e8bc7ad40
boss.back <boss.back@kakaocorp.com>
2022-05-20 01:53:15 +0900

    feat: 인풋 에러 처리, 소숫점 2자리 출력 처리

--- src/main/java/lotto/domain/LottoUtil.java
+++ src/main/java/lotto/domain/LottoUtil.java
@@ -5,11 +5,15 @@ import java.util.List;
 
 public class LottoUtil {
 
+    private static final int LOTTO_SIZE = 6;
+
     private LottoUtil() {
         throw new UnsupportedOperationException();
     }
 
     public static List<LottoNumber> convertToLottoNumber(int[] intArray) {
+        validateLottoSize(intArray.length);
+
         List<LottoNumber> lottoNumbers = new ArrayList<>();
         for (int number : intArray) {
             lottoNumbers.add(new LottoNumber(number));
@@ -17,11 +21,19 @@ public class LottoUtil {
         return lottoNumbers;
     }
 
-    public static int[] convertToIntArray(String[] lottoString) {
+    public static int[] convertLottoStringToIntArray(String[] lottoString) {
+        validateLottoSize(lottoString.length);
+
         int[] lottoNumbers = new int[lottoString.length];
         for (int i = 0; i < lottoString.length; i++) {
             lottoNumbers[i] = Integer.parseInt(lottoString[i]);
         }
         return lottoNumbers;
+    }
+
+    private static void validateLottoSize(int lottoLength) {
+        if (lottoLength != LOTTO_SIZE) {
+            throw new IllegalArgumentException("6자리 숫자를 , 로 구분하여 입력하세요");
+        }
     }
 }
