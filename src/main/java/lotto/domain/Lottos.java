0d6a696c011fc08d3e8275f534fc24cc63551798
boss.back <boss.back@kakaocorp.com>
2022-05-20 01:00:18 +0900

    fix: List.of -> Arrays.asList 로 수정

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -6,33 +6,24 @@ import java.util.Objects;
 public class Lottos {
 
     private List<Lotto> elements;
-    private WinningStatistics winningStatistics;
 
     public Lottos(List<Lotto> elements) {
         this.elements = elements;
-        this.winningStatistics = new WinningStatistics();
     }
 
-    public void makeWinningResult(Lotto winningLotto) {
+    public void makeWinningResult(Lotto winningLotto, WinningStatistics winningStatistics) {
         for (Lotto lotto : elements) {
             winningStatistics.addLottoRanking(LottoRanking.findLottoRaking(lotto, winningLotto));
         }
     }
 
-    public double calculateRateOfReturn(int purchaseAmount) {
-        return winningStatistics.calculateRateOfReturn(purchaseAmount);
-    }
 
     public List<Lotto> getElements() {
         return elements;
     }
 
-    public WinningStatistics getWinningStatistics() {
-        return winningStatistics;
-    }
 
     @Override
-
     public boolean equals(Object o) {
         if (this == o) {
             return true;
