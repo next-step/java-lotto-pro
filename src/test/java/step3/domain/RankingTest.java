fb6fb38a612a3d8c1b5b0d39599e5bc82f00c195
piopoi <kangsw1988@gmail.com>
2022-05-20 19:27:26 +0900

    test(RankingTest): 불필요한 import 제거

--- src/test/java/step3/domain/RankingTest.java
+++ src/test/java/step3/domain/RankingTest.java
@@ -3,12 +3,9 @@ package step3.domain;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
-import java.util.Arrays;
 import java.util.stream.Stream;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class RankingTest {
