2b716de83f5f70e724a195ca4783e8cd372f7b69
millennion-jin <Jadestern2@>
2022-05-21 02:29:26 +0900

    feat(InputView) : 보너스 로또 번호 입력기능 추가

--- src/main/java/lotto/ui/InputView.java
+++ src/main/java/lotto/ui/InputView.java
@@ -4,37 +4,45 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private static final String MSG_PLEASE_INPUT_MONEY = "구입금액을 입력해 주세요.";
-    private static final String MSG_PLEASE_INPUT_LOTTO_NUMBER = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String MESSAGE_PLEASE_INPUT_MONEY = "구입금액을 입력해 주세요.";
+    private static final String MESSAGE_PLEASE_INPUT_LOTTO_NUMBER = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String MESSAGE_PLEASE_INPUT_BONUS_NUMBER = "보너스 볼을 입력해 주세요.";
 
-    public Integer inputMoneyForPurchase() {
+    public int inputMoneyForPurchase() {
         printPleaseInputMoneyForPurchase();
-        return scanMoneyForPurchase();
+        return scanNumber();
     }
 
     private void printPleaseInputMoneyForPurchase() {
-        System.out.println(MSG_PLEASE_INPUT_MONEY);
+        System.out.println(MESSAGE_PLEASE_INPUT_MONEY);
     }
 
-    private Integer scanMoneyForPurchase() {
+    private int scanNumber() {
         Scanner scanner = new Scanner(System.in);
         return scanner.nextInt();
     }
 
     public String inputReferenceLottoNumbers() {
         printPleaseInputLottoNumbers();
-        return scanLottoNumbers();
+        return scanNumbersString();
     }
 
     private void printPleaseInputLottoNumbers() {
         System.out.println();
-        System.out.println(MSG_PLEASE_INPUT_LOTTO_NUMBER);
+        System.out.println(MESSAGE_PLEASE_INPUT_LOTTO_NUMBER);
     }
 
-    private String scanLottoNumbers() {
+    private String scanNumbersString() {
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
 
+    public int inputBonusLottoNumber() {
+        printPleaseInputBonusLottoNumber();
+        return scanNumber();
+    }
 
+    private void printPleaseInputBonusLottoNumber() {
+        System.out.println(MESSAGE_PLEASE_INPUT_BONUS_NUMBER);
+    }
 }
