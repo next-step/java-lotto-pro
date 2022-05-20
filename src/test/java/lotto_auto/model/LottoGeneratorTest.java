46ae77dc12059db0906495726312544c9b6e8439
bestsilver <dkvkxm99@gmail.com>
2022-05-20 21:18:25 +0900

    test: PurchaseInfo에 따른 로또 생성 테스트 추가

--- src/test/java/lotto_auto/model/LottoGeneratorTest.java
+++ src/test/java/lotto_auto/model/LottoGeneratorTest.java
@@ -2,14 +2,25 @@ package lotto_auto.model;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoGeneratorTest {
     @Test
-    void 입력된_수에_따라_로또들_생성() {
-        Lottos lottos = LottoGenerator.createLottos(6);
+    void 로또들_생성() {
+        PurchaseInfo info = new PurchaseInfo(new Money("4000"), new LottoCount("1"));
 
-        assertThat(lottos.getLottoList().size()).isEqualTo(6);
+        Set<LottoNumber> lottoNumberList = new HashSet<>(Arrays.asList(new LottoNumber(1),
+                new LottoNumber(2),new LottoNumber(3),new LottoNumber(4),new LottoNumber(5),new LottoNumber(6)));
+
+        Lotto lotto = new Lotto(new LottoNumbers(lottoNumberList));
+        Lottos lottos = LottoGenerator.buyLottos(info, new Lottos(Arrays.asList(lotto)));
+
+        assertThat(lottos.getLottoList().size()).isEqualTo(4);
     }
 
 }
