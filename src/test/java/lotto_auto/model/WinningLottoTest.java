bdeea1e094882114a369185f6f69e0c75496c49f
bestsilver <dkvkxm99@gmail.com>
2022-05-20 21:20:15 +0900

    test: 로또 비교 후 LottoRank 결과 체크 테스트 추가

--- src/test/java/lotto_auto/model/WinningLottoTest.java
+++ src/test/java/lotto_auto/model/WinningLottoTest.java
@@ -3,9 +3,14 @@ package lotto_auto.model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class WinningLottoTest {
@@ -17,7 +22,7 @@ public class WinningLottoTest {
                 .rangeClosed(1, 6)
                 .boxed()
                 .map(LottoNumber::new)
-                .collect(Collectors.toList()));
+                .collect(Collectors.toSet()));
     }
 
     @Test
@@ -25,5 +30,16 @@ public class WinningLottoTest {
         assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> new WinningLotto(new Lotto(lottoNumbers), new LottoNumber(1)))
                 .withMessage(LottoNumbers.EXIST_DUPLICATE_VALUE);
+    }
+
+    @Test
+    void 로또_비교후_결과_랭크() {
+        Set<LottoNumber> lottoNumberSet = new HashSet<>(Arrays.asList(new LottoNumber(1),
+                new LottoNumber(2),new LottoNumber(3),new LottoNumber(4),new LottoNumber(5),new LottoNumber(6)));
+
+        Lotto lotto = new Lotto(new LottoNumbers(lottoNumberSet));
+        WinningLotto winningLotto = new WinningLotto(lotto, new LottoNumber(7));
+
+        assertThat(winningLotto.matches(lotto)).isEqualTo(LottoRank.FIRST);
     }
 }
