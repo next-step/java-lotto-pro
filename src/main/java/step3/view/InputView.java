b2132a101bc420d36207483d7cd1818099e504f9
jhsong2580 <thdwmdgns@naver.com>
2022-05-20 23:29:16 +0900

    fix: 수동 구매 번호 입력 전 티켓 검증을 위한 순서 변경

--- src/main/java/step3/view/InputView.java
+++ src/main/java/step3/view/InputView.java
@@ -12,6 +12,7 @@ public class InputView {
     private final String GET_BONUS_LOTTO = "보너스 볼을 입력해 주세요.";
     private final String GET_MANUAL_LOTTO_COUNT = "수동으로 구매할 로또 수를 입력해 주세요.";
     private final String GET_MANUAL_LOTTO_MESSAGE = "수동으로 구매할 번호를 입력해 주세요.";
+    private final int TICKET_EXIST_BOUNDARY = 1;
 
     public String getMoney() {
         System.out.println(GET_MONEY_MESSAGE);
@@ -30,8 +31,10 @@ public class InputView {
 
     public List<String> getManualLotto(int manualTicket) {
         List<String> manualLottoTicketsSource = new ArrayList<>();
-        for (int i = 0; i < manualTicket; i++) {
+        if (manualTicket >= TICKET_EXIST_BOUNDARY) {
             System.out.println(GET_MANUAL_LOTTO_MESSAGE);
+        }
+        for (int i = 0; i < manualTicket; i++) {
             manualLottoTicketsSource.add(customTrim(scanIn.nextLine()));
         }
         return manualLottoTicketsSource;
