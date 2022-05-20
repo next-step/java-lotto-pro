8458022157b3325d865631ffb844e7d76a9fdc4c
DESKTOP-IMQ723M\SKMG <lcjltj@gmail.com>
2022-05-20 10:03:59 +0900

    refactor(Lotto): UI(print)로직 제외 외 1
    - Lotto class
        - printLotto() UI 관련 메소드 삭제(OutView 클래스에서 구현)
        - suffle() , generateAuto() , auto() 자동 로또 메소드 삭제(LottoMachine 클래서에서 구현)
    - LottoNumber
        - 필드 명 변경 lottoNumber -> value
        - static 필드명 변경 -> LOTTO_NUMBER -> COLLECTION

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -4,73 +4,29 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-import static lotto.constants.ExceptionConstants.*;
+import static lotto.constants.ExceptionConstants.LOTTO_LENGTH_EXCEPTION;
+import static lotto.constants.ExceptionConstants.LOTTO_OVERLAP_EXCEPTION;
 
 public class Lotto {
-    private static final int BEGIN_NUMBER = 1;
-    private static final int END_NUMBER = 46;
     private static final int LIMIT_LOTTO = 6;
-    private static final Lotto INSTANCE = new Lotto();
-    private static final String PRINT_DELIMITER = ",";
-    private static final String PRINT_PREFIX = "[";
-    private static final String PRINT_SUFFIX = "]";
-
+    private static final String DELIMITER = ",";
     private final List<LottoNumber> lotto;
 
-    public static Lotto ofAnswer(final String lottoNumbers) {
-        return new Lotto(
-                Stream.of(lottoNumbers.split(PRINT_DELIMITER))
-                .map(String::trim)
-                .map(Integer::parseInt)
-                .map(LottoNumber::of)
-                .collect(Collectors.toList())
+    public Lotto(final String lottoNumbers) {
+        this(
+                Stream.of(lottoNumbers.split(DELIMITER))
+                        .map(String::trim)
+                        .map(Integer::parseInt)
+                        .map(LottoNumber::of)
+                        .collect(Collectors.toList())
         );
     }
 
     public Lotto(final List<LottoNumber> lottoNumbers) {
         validate(lottoNumbers);
         this.lotto = lottoNumbers;
-    }
-
-    private Lotto() {
-        lotto = IntStream.range(BEGIN_NUMBER, END_NUMBER)
-                .mapToObj(LottoNumber::of)
-                .collect(Collectors.toList());
-    }
-
-    public static Lotto auto() {
-        shuffle();
-        return new Lotto(generateAuto());
-    }
-
-    private static List<LottoNumber> generateAuto() {
-        return INSTANCE.lotto.stream()
-                .limit(LIMIT_LOTTO)
-                .collect(Collectors.toList());
-    }
-
-    private static void shuffle() {
-        Collections.shuffle(INSTANCE.lotto);
-    }
-
-    public String printLotto() {
-        return lotto.stream()
-                .map(LottoNumber::getLottoNumber)
-                .map(Objects::toString)
-                .collect(Collectors.joining(PRINT_DELIMITER, PRINT_PREFIX, PRINT_SUFFIX));
-    }
-
-    public long matchCount(final Lotto answer) {
-        return answer.lotto.stream()
-                .filter(this::contains)
-                .count();
-    }
-
-    private boolean contains(final LottoNumber lottoNumber) {
-        return lotto.contains(lottoNumber);
     }
 
     private void validate(final List<LottoNumber> lottoNumbers) {
@@ -90,6 +46,20 @@ public class Lotto {
         if (count != LIMIT_LOTTO) {
             throw new IllegalArgumentException(LOTTO_OVERLAP_EXCEPTION);
         }
+    }
+
+    public List<LottoNumber> getLottoNumber() {
+        return Collections.unmodifiableList(lotto);
+    }
+
+    public long matchCount(final Lotto answer) {
+        return answer.lotto.stream()
+                .filter(this::contains)
+                .count();
+    }
+
+    private boolean contains(final LottoNumber lottoNumber) {
+        return lotto.contains(lottoNumber);
     }
 
     @Override
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -13,41 +13,21 @@ public class LottoTest {
     @Test
     @DisplayName("6자리가 아닌 로또 번호 생성 시 오류 테스트")
     void create_lotto_range_exception_test() {
-        List<LottoNumber> lotto = Arrays.asList(LottoNumber.of(1), LottoNumber.of(2), LottoNumber.of(3)
-                , LottoNumber.of(4), LottoNumber.of(5));
-
-        assertThatThrownBy(() -> new Lotto(lotto))
+        assertThatThrownBy(() -> new Lotto("1, 2, 3, 4, 5"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("중복 로또 번호 생성 시 오류 테스트")
     void create_lotto_overlap_test() {
-        List<LottoNumber> lotto = Arrays.asList(LottoNumber.of(1), LottoNumber.of(2), LottoNumber.of(3)
-                , LottoNumber.of(4), LottoNumber.of(5), LottoNumber.of(5));
-
-        assertThatThrownBy(() -> new Lotto(lotto))
+        assertThatThrownBy(() -> new Lotto("1, 2, 3, 4, 5, 5"))
                 .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    @DisplayName("로또 자동 생성 테스트")
-    void create_auto_lotto_test() {
-        Lotto lotto = Lotto.auto();
-        assertThat(lotto.getClass()).isEqualTo(Lotto.class);
     }
 
     @Test
     @DisplayName("로또 번호 맞은 횟수 테스트")
     void lotto_matches_test() {
-        List<LottoNumber> lotto = Arrays.asList(LottoNumber.of(1), LottoNumber.of(2), LottoNumber.of(3)
-                , LottoNumber.of(4), LottoNumber.of(5), LottoNumber.of(6));
-
-        List<LottoNumber> lottoTwo = Arrays.asList(LottoNumber.of(1), LottoNumber.of(2), LottoNumber.of(3)
-                , LottoNumber.of(4), LottoNumber.of(5), LottoNumber.of(7));
-
-        Lotto lottoTicket = new Lotto(lotto);
-        assertThat(lottoTicket.matchCount(new Lotto(lotto))).isEqualTo(6);
-        assertThat(lottoTicket.matchCount(new Lotto(lottoTwo))).isEqualTo(5);
+        Lotto lottoTicket = new Lotto("1, 2, 3, 4, 5, 7");
+        assertThat(lottoTicket.matchCount(new Lotto("1, 2, 3, 4, 5, 6"))).isEqualTo(5);
     }
 }
