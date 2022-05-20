9837f5169fb6f70dc3e0aea222acb4fe291d3f2f
SANGIK KIM <sangik.kim@enel.com>
2022-05-20 16:42:44 +0900

    refactor(Division): move the division rule class into the division enum to increase encapsulation

--- src/main/java/study/lotto/domain/draw/Division.java
+++ src/main/java/study/lotto/domain/draw/Division.java
@@ -1,6 +1,10 @@
 package study.lotto.domain.draw;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public enum Division {
     DIVISION_NONE(0, BigDecimal.ZERO, false),
@@ -50,5 +54,32 @@ public enum Division {
 
     public boolean getBonusMandatory() {
         return bonusMandatory;
+    }
+
+    private static class DivisionRule {
+        private final Map<Boolean, List<Division>> values;
+
+        DivisionRule() {
+            this.values = new HashMap<>();
+        }
+
+        void add(Division division, Boolean matchBonus) {
+            if (!values.containsKey(matchBonus)) {
+                values.put(matchBonus, new ArrayList<>());
+            }
+            addElement(division, matchBonus);
+        }
+
+        Division check(int matchCount, boolean matchBonus) {
+            return values.get(matchBonus).stream()
+                    .filter(division -> division.hasSameMatchCount(matchCount))
+                    .findFirst()
+                    .orElse(DIVISION_NONE);
+        }
+
+        void addElement(Division division, Boolean matchBonus) {
+            List<Division> divisions = values.get(matchBonus);
+            divisions.add(division);
+        }
     }
 }
