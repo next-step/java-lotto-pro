348f0fc03559338c64cb942c5840cb51c660848a
DESKTOP-IMQ723M\SKMG <lcjltj@gmail.com>
2022-05-20 10:04:57 +0900

    refactor(LottoNumber): LottoNumber 필드명 변경
    - LottoNumber
        - 필드 명 변경 lottoNumber -> value
        - static 필드명 변경 -> LOTTO_NUMBER -> COLLECTION

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -6,26 +6,26 @@ import java.util.stream.IntStream;
 import static lotto.constants.ExceptionConstants.*;
 
 public class LottoNumber {
-    private static final LottoNumber[] LOTTO_NUMBER;
+    private static final LottoNumber[] COLLECTION;
     private static final int BEGIN_NUMBER = 1;
     private static final int END_NUMBER = 46;
     private static final int ARRAY_MINUS = 1;
 
     static {
-        LOTTO_NUMBER = IntStream.range(BEGIN_NUMBER, END_NUMBER)
+        COLLECTION = IntStream.range(BEGIN_NUMBER, END_NUMBER)
                 .mapToObj(LottoNumber::new)
                 .toArray(LottoNumber[]::new);
     }
 
-    private final int lottoNumber;
+    private final int value;
 
     public static LottoNumber of(final int inputNumber) {
         validate(inputNumber);
-        return LOTTO_NUMBER[inputNumber - ARRAY_MINUS];
+        return COLLECTION[inputNumber - ARRAY_MINUS];
     }
 
     private LottoNumber(final int inputNumber) {
-        this.lottoNumber = inputNumber;
+        this.value = inputNumber;
     }
 
     private static void validate(int inputNumber) {
@@ -34,8 +34,8 @@ public class LottoNumber {
         }
     }
 
-    public int getLottoNumber() {
-        return lottoNumber;
+    public int getValue() {
+        return value;
     }
 
     @Override
@@ -43,18 +43,18 @@ public class LottoNumber {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         LottoNumber that = (LottoNumber) o;
-        return lottoNumber == that.lottoNumber;
+        return value == that.value;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(lottoNumber);
+        return Objects.hash(value);
     }
 
     @Override
     public String toString() {
         return "LottoNumber{" +
-                "lottoNumber=" + lottoNumber +
+                "lottoNumber=" + value +
                 '}';
     }
 }
