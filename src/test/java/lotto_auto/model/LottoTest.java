7db4dc61a62ec8b1cec5b9d4459f0fd5ce85847f
bestsilver <dkvkxm99@gmail.com>
2022-05-20 21:19:11 +0900

    test: 로또 inContain 테스트 추가

--- src/test/java/lotto_auto/model/LottoTest.java
+++ src/test/java/lotto_auto/model/LottoTest.java
@@ -1,23 +1,38 @@
 package lotto_auto.model;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class LottoTest {
-    @Test
-    void 로또_비교후_결과_랭크() {
-        List<LottoNumber> lottoNumberList = Arrays.asList(new LottoNumber(1),
-                new LottoNumber(2),new LottoNumber(3),new LottoNumber(4),new LottoNumber(5),new LottoNumber(6));
+    private static Lotto lotto;
+    @BeforeAll
+    static void setup() {
+        Set<LottoNumber> lottoNumberList = new HashSet<>(Arrays.asList(new LottoNumber(1),
+                new LottoNumber(2),new LottoNumber(3),new LottoNumber(4),new LottoNumber(5),new LottoNumber(6)));
 
-        Lotto lotto = new Lotto(new LottoNumbers(lottoNumberList));
-        WinningLotto winningLotto = new WinningLotto(lotto, new LottoNumber(7));
-
-        assertThat(lotto.matches(winningLotto)).isEqualTo(LottoRank.FIRST);
+        lotto = new Lotto(new LottoNumbers(lottoNumberList));
     }
 
+    @Test
+    void isContains_테스트() {
+        assertThat(lotto.isContain(new LottoNumber(1))).isTrue();
+    }
+    @Test
+    void 같은_로또번호_갯수_체크() {
+        Set<LottoNumber> lottoNumberList = new HashSet<>(Arrays.asList(new LottoNumber(1),
+                new LottoNumber(2),new LottoNumber(3),new LottoNumber(4),new LottoNumber(5),new LottoNumber(6)));
+
+        Lotto other = new Lotto(new LottoNumbers(lottoNumberList));
+
+        assertThat(lotto.countSameLottoNumber(other)).isEqualTo(6);
+    }
 }
