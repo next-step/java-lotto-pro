d5bc81944238dd70a62b6589ea6f954b20dbcb44
piopoi <kangsw1988@gmail.com>
2022-05-21 01:36:45 +0900

    feat(InputView): 수동로또 관련 입력기능 추가

--- src/main/java/step3/view/InputView.java
+++ src/main/java/step3/view/InputView.java
@@ -1,5 +1,6 @@
 package step3.view;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -7,7 +8,7 @@ import java.util.stream.Collectors;
 
 public class InputView {
 
-    private static final String LOTTO_NUMBER_DELIMITER = ",";
+    private static final String NUMBER_DELIMITER = ",";
 
     private InputView() {
     }
@@ -17,15 +18,33 @@ public class InputView {
         return inputNumber();
     }
 
+    public static int inputManualLottoCount() {
+        System.out.println("\n수동으로 구매할 로또 수를 입력해 주세요.");
+        return inputNumber();
+    }
+
+    public static List<List<Integer>> inputManualLottoNumbers(int manualLottoCount) {
+        System.out.println("\n수동으로 구매할 번호를 입력해 주세요.");
+        List<List<Integer>> manualLottoNumbers = new ArrayList<>();
+        for(int i = 0; i < manualLottoCount; i++) {
+            manualLottoNumbers.add(inputNumbers());
+        }
+        return manualLottoNumbers;
+    }
+
     public static int inputBonusNumber() {
         System.out.println("보너스 볼을 입력해 주세요.");
         return inputNumber();
     }
 
     public static List<Integer> inputWinnerNumbers() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        return inputNumbers();
+    }
+
+    private static List<Integer> inputNumbers() {
         try {
-            System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-            return Arrays.stream(inputText().split(LOTTO_NUMBER_DELIMITER))
+            return Arrays.stream(inputText().split(NUMBER_DELIMITER))
                     .map(text -> Integer.parseInt(text.trim()))
                     .collect(Collectors.toList());
         } catch (NumberFormatException e) {
