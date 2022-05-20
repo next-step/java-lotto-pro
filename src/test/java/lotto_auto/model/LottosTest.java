ee1e63a463883a1377db730a818220a3536eabde
bestsilver <dkvkxm99@gmail.com>
2022-05-20 21:18:52 +0900

    test: 당첨된 로또들 테스트 추가

--- src/test/java/lotto_auto/model/LottosTest.java
+++ src/test/java/lotto_auto/model/LottosTest.java
@@ -21,7 +21,7 @@ public class LottosTest {
                 .rangeClosed(1, 6)
                 .boxed()
                 .map(LottoNumber::new)
-                .collect(Collectors.toList()));
+                .collect(Collectors.toSet()));
 
         Lottos lottos = new Lottos(Arrays.asList(new Lotto(numbers),
                 new Lotto(numbers)));
@@ -36,13 +36,13 @@ public class LottosTest {
                 .rangeClosed(1, 6)
                 .boxed()
                 .map(LottoNumber::new)
-                .collect(Collectors.toList()));
+                .collect(Collectors.toSet()));
 
         LottoNumbers otherNumbers = new LottoNumbers(IntStream
                 .rangeClosed(7, 12)
                 .boxed()
                 .map(LottoNumber::new)
-                .collect(Collectors.toList()));
+                .collect(Collectors.toSet()));
 
         Lottos lottos = new Lottos(Arrays.asList(new Lotto(lottoNumbers)));
         Lottos otherLottos = new Lottos(Arrays.asList(new Lotto(otherNumbers)));
@@ -52,6 +52,21 @@ public class LottosTest {
         assertThat(getLottoToString(mergedLottos.getLottoList().get(0))).isEqualTo("1, 2, 3, 4, 5, 6");
         assertThat(getLottoToString(mergedLottos.getLottoList().get(1))).isEqualTo("7, 8, 9, 10, 11, 12");
         assertThat(mergedLottos.getLottoList().size()).isEqualTo(2);
+    }
+
+    @Test
+    void 당첨된_로또들_결과_체크() {
+        LottoNumbers otherNumbers = new LottoNumbers(IntStream
+                .rangeClosed(7, 12)
+                .boxed()
+                .map(LottoNumber::new)
+                .collect(Collectors.toSet()));
+        WinningLotto winningLotto = new WinningLotto(new Lotto(otherNumbers), new LottoNumber(1));
+        Lottos lottos = new Lottos(Arrays.asList(new Lotto(otherNumbers)));
+
+        Figures result = lottos.matchedLottos(winningLotto);
+        assertThat(result.getCountBy(LottoRank.FIRST)).isEqualTo(1);
+
     }
 
     private String getLottoToString(Lotto lotto) {
