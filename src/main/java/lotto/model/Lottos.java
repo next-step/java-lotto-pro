bf5b50c11b85bb2372bc20f23a3344931571dbfa
kwonyongil <ksgkms23@likelion.org>
2022-05-21 19:19:25 +0900

    feat(lotto) Lottos 수동+자동 구매 메서드 추가

--- src/main/java/lotto/model/Lottos.java
+++ src/main/java/lotto/model/Lottos.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import lotto.generator.RandomLottoNumberGenerator;
 
 public class Lottos {
@@ -20,19 +21,48 @@ public class Lottos {
         return new Lottos(lottos);
     }
 
-    public static Lottos buy(Money money) {
-        List<Lotto> lottos = new ArrayList<>();
-        int count = money.maxLottoCount();
-        for (int i = ZERO_NUM; i < count; i++) {
-            lottos.add(Lotto.draw(new RandomLottoNumberGenerator()));
-        }
+    public static Lottos purchase(Money money) {
+        List<Lotto> lottos = randomLottos(money);
         return new Lottos(lottos);
     }
 
-    public LottoStatistics lottoStatistics(Lotto winningLotto, LottoNumber bonusLottoNumber) {
+    public static Lottos purchase(Money totalMoney, List<Lotto> manualLottos) {
+        validateManualLottosNotNUll(manualLottos);
+        Money leftMoney = leftMoney(totalMoney, manualLottos);
+        List<Lotto> lottos = randomLottos(leftMoney);
+        lottos.addAll(manualLottos);
+        return new Lottos(lottos);
+    }
+
+    private static List<Lotto> randomLottos(Money leftMoney) {
+        List<Lotto> lottos = new ArrayList<>();
+        int count = leftMoney.maxLottoCount();
+        for (int i = ZERO_NUM; i < count; i++) {
+            lottos.add(Lotto.draw(new RandomLottoNumberGenerator()));
+        }
+        return lottos;
+    }
+
+    private static Money leftMoney(Money totalMoney, List<Lotto> manualLottos) {
+        Money leftMoney;
+        try {
+            leftMoney = totalMoney.subtract(Money.valueOf(Lotto.PRICE).multiply(manualLottos.size()));
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("수동 로또 구매에 필요한 돈이 부족합니다.");
+        }
+        return leftMoney;
+    }
+
+    private static void validateManualLottosNotNUll(List<Lotto> manualLottos) {
+        if (Objects.isNull(manualLottos)) {
+            throw new IllegalArgumentException("로또 번호 갯수가 올바르지 않습니다.");
+        }
+    }
+
+    public LottoStatistics lottoStatistics(WinningLotto winningLotto) {
         Map<LottoRanking, Integer> lottoStatistics = new EnumMap<>(LottoRanking.class);
         for (Lotto lotto : this.lottos) {
-            LottoRanking lottoRanking = lotto.lottoRanking(winningLotto, bonusLottoNumber);
+            LottoRanking lottoRanking = lotto.lottoRanking(winningLotto);
             lottoStatistics.put(lottoRanking, lottoStatistics.getOrDefault(lottoRanking, ZERO_NUM) + EXTRA_NUM);
         }
         return new LottoStatistics(lottoStatistics);
