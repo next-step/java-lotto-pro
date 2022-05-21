67b660b92363345c463f75544feccbb54ad8e792
hyun <jhyun_yi@naver.com>
2022-05-21 20:09:11 +0900

    refactor : Rank depth 2 해소

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,7 +1,10 @@
 package lotto;
 
 public enum Rank {
-    FIRST(6, 2_000_000_000), THIRD(5, 1_500_000), FOURTH(4, 50_000), FIFTH(3, 5_000);
+    FIRST(6, 2_000_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000);
 
     private final int countOfMatch;
     private final int winningMoney;
@@ -20,15 +23,17 @@ public enum Rank {
     }
 
     public static Rank valueOf(int countOfMatch) {
-        Rank[] values = Rank.values();
         Rank rank = null;
-        for (Rank value : values) {
-            if (value.getCountOfMatch() == countOfMatch) {
-                rank = value;
-            }
+        for (Rank value : Rank.values()) {
+            rank = getRankCountOfMatch(countOfMatch, value);
         }
-
         return rank;
     }
 
+    private static Rank getRankCountOfMatch(int countOfMatch, Rank value) {
+        if (value.getCountOfMatch() == countOfMatch) {
+            return value;
+        }
+        return null;
+    }
 }
