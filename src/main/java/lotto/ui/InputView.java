382cb2e08cc0737fe177b5aaf4f146388ba6c86f
ssamzag <ssamzag@gmail.com>
2022-05-21 17:44:03 +0900

    feat: 수동 구매 관련 입/출력 (InputView.java): 수동 수량 및 로또번호 입력 (ResultView.java): 수동/자동 구입 수량 출력

--- src/main/java/lotto/ui/InputView.java
+++ src/main/java/lotto/ui/InputView.java
@@ -1,5 +1,12 @@
 package lotto.ui;
 
+import lotto.domain.Lotto;
+import lotto.domain.LottoNumber;
+import lotto.domain.Lottos;
+import lotto.domain.factory.LottoFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
@@ -20,5 +27,21 @@ public class InputView {
     public static String getWinningNumbers() {
         System.out.println(INPUT_WINNING_MESSAGE);
         return scanner.nextLine();
+    }
+
+    public static Lottos getManualLottoNumbers() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        String lottoCount = scanner.nextLine();
+        return getLottos(lottoCount);
+    }
+
+    private static Lottos getLottos(String lottoCount) {
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+
+        List<Lotto> manualLottos = new ArrayList<>();
+        for (int i = 0, count = Integer.parseInt(lottoCount); i < count; i++) {
+            manualLottos.add(LottoFactory.create(scanner.nextLine()));
+        }
+        return new Lottos(manualLottos);
     }
 }
