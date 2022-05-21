19da18204b03f012ea9c002ee166ca4f2f531506
Philip <pch4623@gmail.com>
2022-05-21 10:02:05 +0900

    feat(Prize): equals, hashCode 오버라이딩

--- src/main/java/lotto/model/Prize.java
+++ src/main/java/lotto/model/Prize.java
@@ -1,5 +1,7 @@
 package lotto.model;
 
+import java.util.Objects;
+
 public class Prize {
     final long amount;
 
@@ -33,5 +35,18 @@ public class Prize {
     @Override
     public String toString() {
         return String.valueOf(amount);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Prize prize = (Prize) o;
+        return amount == prize.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
     }
 }
