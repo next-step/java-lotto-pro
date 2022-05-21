d4ba81ed718e111d29ead7039b74eedab29ce7bf
Woody <yuncheol92@naver.com>
2022-05-21 22:12:30 +0900

    test : LottoNumber 테스트 코드 추가

---
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -0,0 +1,50 @@
+package lotto.domain;
+
+import lotto.domain.error.LottoNumberErrorCode;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LottoNumberTest {
+
+    @ParameterizedTest(name = "String lottoNumber가 null 또는 empty일 때")
+    @NullAndEmptySource
+    public void String_생성자_테스트_null_or_empty(String lottoNumber) throws Exception {
+        assertThatThrownBy(() -> new LottoNumber(lottoNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(LottoNumberErrorCode.NOT_ALLOW_NULL.getMessage());
+    }
+
+    @ParameterizedTest(name = "String lottoNumber가 로또 번호가 아닐 때")
+    @ValueSource(strings = {"0", "46"})
+    public void String_생성자_테스트(String lottoNumber) throws Exception {
+        assertThatThrownBy(() -> new LottoNumber(lottoNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(
+                        String.format(LottoNumberErrorCode.INVALID_LOTTO_NUMBER.getMessage(),
+                                LottoNumber.LOTTO_MIN_NUMBER, LottoNumber.LOTTO_MAX_NUMBER));
+    }
+
+    @ParameterizedTest(name = "int lottoNumber가 로또 번호가 아닐 때")
+    @ValueSource(ints = {0, 46})
+    public void int_생성자_테스트(int lottoNumber) {
+        assertThatThrownBy(() -> new LottoNumber(lottoNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(
+                        String.format(LottoNumberErrorCode.INVALID_LOTTO_NUMBER.getMessage(),
+                                LottoNumber.LOTTO_MIN_NUMBER, LottoNumber.LOTTO_MAX_NUMBER));
+    }
+
+    @Test
+    @DisplayName("LottoNumber 동등성")
+    public void equals_identity_test() {
+        LottoNumber lottoNumber = new LottoNumber(1);
+
+        assertThat(lottoNumber).isEqualTo(new LottoNumber(1));
+    }
+}
