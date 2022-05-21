f72feb8a4a35f16b63dcac3bc155767568b5e402
mukeunzi <eunji057@gmail.com>
2022-05-22 00:28:44 +0900

    refactor(test): 실행 시 중복사용되는 코드 @BeforeAll 적용

--- src/test/java/lotto/domain/LottoWinningTicketTest.java
+++ src/test/java/lotto/domain/LottoWinningTicketTest.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,15 +11,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoWinningTicketTest {
-    @Test
-    @DisplayName("로또 당첨번호가 정상적으로 초기화된다.")
-    void checkLottoWinningNumbers() {
-        List<LottoNumber> lottoNumbers = new ArrayList<>();
+    private static List<LottoNumber> lottoNumbers;
+
+    @BeforeAll
+    static void initialize() {
+        lottoNumbers = new ArrayList<>();
         for (int i = 0; i < 6; i++) {
             lottoNumbers.add(new LottoNumber(i + 1));
         }
-        LottoNumber bonusNumber = new LottoNumber(45);
+    }
 
+    @Test
+    @DisplayName("로또 당첨번호가 정상적으로 초기화된다.")
+    void checkLottoWinningNumbers() {
+        LottoNumber bonusNumber = new LottoNumber(45);
         assertThat(new LottoWinningTicket(new LottoTicket(lottoNumbers), bonusNumber)).isNotNull();
     }
 
@@ -31,11 +37,6 @@ public class LottoWinningTicketTest {
     @Test
     @DisplayName("보너스볼을 입력하지 않을 경우 오류가 발생한다.")
     void checkInvalidNullBonusBall() {
-        List<LottoNumber> lottoNumbers = new ArrayList<>();
-        for (int i = 0; i < 6; i++) {
-            lottoNumbers.add(new LottoNumber(i + 1));
-        }
-
         assertThatThrownBy(() -> new LottoWinningTicket(new LottoTicket(lottoNumbers), null)).isInstanceOf(IllegalArgumentException.class);
     }
 }
