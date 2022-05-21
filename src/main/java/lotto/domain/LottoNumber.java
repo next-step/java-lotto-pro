89676cf218f7554e7afa34f98b37d3bb8c6eda4c
taeyong-sim <sim2ty@naver.com>
2022-05-21 19:12:09 +0900

    feat(LottoNumber) : LottoNumber DTO 메서드 추가

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,14 +1,13 @@
 package lotto.domain;
 
-import lotto.constant.Message;
-import lotto.exception.LottoNumberRangeException;
+import static lotto.constant.LottoConstant.LOTTO_NUMBER_LOWER_BOUND;
+import static lotto.constant.LottoConstant.LOTTO_NUMBER_UPPER_BOUND;
 
 import java.util.Objects;
+import lotto.dto.LottoNumberDTO;
+import lotto.exception.LottoNumberRangeException;
 
 public class LottoNumber {
-
-    public static final int MIN_LOTTO_NUMBER = 1;
-    public static final int MAX_LOTTO_NUMBER_FORTY_FIVE = 45;
 
     private final int lottoNumber;
 
@@ -17,25 +16,28 @@ public class LottoNumber {
         this.lottoNumber = lottoNumber;
     }
 
-    private static void validateLottoNumber(int lottoNumber) {
-        if (lottoNumber < MIN_LOTTO_NUMBER || lottoNumber > MAX_LOTTO_NUMBER_FORTY_FIVE) {
+    public static LottoNumber from(Integer lottoNumber) {
+        return new LottoNumber(lottoNumber);
+    }
+
+    public LottoNumberDTO toLottoNumberDTO() {
+        return new LottoNumberDTO(lottoNumber);
+    }
+
+    private void validateLottoNumber(int lottoNumber) {
+        if (lottoNumber < LOTTO_NUMBER_LOWER_BOUND || lottoNumber > LOTTO_NUMBER_UPPER_BOUND) {
             throw new LottoNumberRangeException();
         }
     }
 
-    public static LottoNumber from(final int lottoNumber) {
-        validateLottoNumber(lottoNumber);
-        return new LottoNumber(lottoNumber);
-    }
-
-    public int extractLottoNumber(){
-        return lottoNumber;
-    }
-
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         LottoNumber that = (LottoNumber) o;
         return lottoNumber == that.lottoNumber;
     }
