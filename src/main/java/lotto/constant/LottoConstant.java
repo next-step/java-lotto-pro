d1145e95572cf8340e620e3152360989efc4b44f
taeyong-sim <sim2ty@naver.com>
2022-05-21 19:15:50 +0900

    feat(LottoConstant) : LottoConstant 추가

---
+++ src/main/java/lotto/constant/LottoConstant.java
@@ -0,0 +1,14 @@
+package lotto.constant;
+
+public class LottoConstant {
+    public static final int LOTTO_NUMBER_LOWER_BOUND = 1;
+    public static final int LOTTO_NUMBER_UPPER_BOUND = 45;
+    public static final int LOTTO_LINE_LENGTH = 6;
+    public static final int LOTTO_PRICE_EACH = 1000;
+    public static final int LOTTO_PAYMENT_MIN = 0;
+    public static final int DEFAULT_MATCH_COUNT = 0;
+    public static final int EARNING_RATE_STANDARD = 1;
+    public static final String SEPARATOR = ", ";
+    public static final String WINNING_NUMBER_REGEX = "\\s*,\\s*";
+    public static final String LINE_BREAK = "\n";
+}
