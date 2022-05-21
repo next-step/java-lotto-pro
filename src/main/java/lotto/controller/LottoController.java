f77b74b86a3a87b68d73cd4b04985b788a16a250
taeyong-sim <sim2ty@naver.com>
2022-05-21 19:20:28 +0900

    feat(LottoController) : LottoController 추가

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,51 @@
+package lotto.controller;
+
+import lotto.domain.LottoGame;
+import lotto.domain.LottoLine;
+import lotto.domain.LottoPayment;
+import lotto.domain.LottoResult;
+import lotto.service.LottoService;
+import lotto.util.LottoStringGenerator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final LottoService lottoService;
+
+    public LottoController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.lottoService = new LottoService();
+    }
+
+    public void run(){
+        LottoPayment lottoPayment = inputLottoPayment();
+        LottoGame lottoGame = buyLotto(lottoPayment);
+        LottoLine winLottoLine = inputWinInformation();
+        printResult(lottoGame, winLottoLine, lottoPayment);
+    }
+
+    private LottoPayment inputLottoPayment(){
+        return new LottoPayment(inputView.inputTotalPayment());
+    }
+
+    private LottoGame buyLotto(LottoPayment lottoPayment){
+        LottoGame lottoGame = lottoService.buyLotto(lottoPayment);
+        outputView.printPayment(lottoGame.toLottoGameDTO());
+        return lottoGame;
+    }
+
+    private LottoLine inputWinInformation(){
+        return LottoStringGenerator.toWinLottoLine(inputView.inputLastWeekWinningLottoLine());
+    }
+
+    private void printResult(LottoGame lottoGame, LottoLine winLottoLine, LottoPayment lottoPayment){
+        LottoResult lottoResult = lottoService.getLottoResult(lottoGame, winLottoLine);
+        LottoPayment prize = new LottoPayment(lottoResult.getLottoPrize());
+        outputView.printLottoResult(outputView.getLottoResultString(lottoResult));
+        outputView.printEarningRate(outputView.getEarningRateString(lottoPayment.toLottoPaymentDTO(), prize.toLottoPaymentDTO()));
+    }
+}
