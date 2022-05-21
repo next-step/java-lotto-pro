d22fc0535562383f409eadf13ad27e8d848292e6
Philip <pch4623@gmail.com>
2022-05-21 23:33:33 +0900

    test(RankTest): 등수 정보 변경에 따른 테스트 코드 수정

--- src/test/java/lotto/enums/RankTest.java
+++ src/test/java/lotto/enums/RankTest.java
@@ -14,7 +14,7 @@ class RankTest {
 
     @ParameterizedTest
     @DisplayName("일치하는 개수에 따라 적절한 랭킹을 리턴하는지 확인한다.")
-    @CsvSource({"6, FIRST", "5, SECOND", "4, THIRD", "3, FOURTH", "2, FIFTH", "1, SIXTH", "0, LOSE"})
+    @CsvSource({"6, FIRST", "4, FOURTH", "3, FIFTH", "2, SIXTH", "1, SEVENTH", "0, LOSE"})
     void getRank_일치하는_개수(int numberOfMatch, Rank expected) {
         assertThat(getRank(numberOfMatch))
                 .isEqualTo(expected);
@@ -24,12 +24,12 @@ class RankTest {
     @DisplayName("해당 등수의 총 당첨금액을 계산하여 리턴한다.")
     void getPrizeWithCount_총_당첨금액() {
         assertThat(THIRD.getPrizeWithCount(3))
-                .isEqualTo(Prize.of(150_000));
+                .isEqualTo(Prize.of(4_500_000));
     }
 
     @ParameterizedTest
     @DisplayName("당첨 및 낙첨 여부를 판단한다(당첨).")
-    @EnumSource(value = Rank.class, names = {"FIRST", "SECOND", "THIRD", "FOURTH"})
+    @EnumSource(value = Rank.class, names = {"FIRST", "SECOND", "THIRD", "FOURTH", "FIFTH"})
     void isOverMinWinningRank_당첨(Rank rank) {
         assertThat(rank.isOverMinWinningRank())
                 .isTrue();
@@ -38,7 +38,7 @@ class RankTest {
 
     @ParameterizedTest
     @DisplayName("당첨 및 낙첨 여부를 판단한다(낙첨).")
-    @EnumSource(value = Rank.class, names = {"FIFTH", "SIXTH", "LOSE"})
+    @EnumSource(value = Rank.class, names = {"SIXTH", "SEVENTH", "LOSE"})
     void isOverMinWinningRank_낙첨(Rank rank) {
         assertThat(rank.isOverMinWinningRank())
                 .isFalse();
