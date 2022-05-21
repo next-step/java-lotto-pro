0e52210355347890fc79dfc1f16b8a5b32d47eaf
taeyong-sim <sim2ty@naver.com>
2022-05-21 19:19:17 +0900

    feat(LottoService) : LottoService 추가

---
+++ src/main/java/lotto/service/LottoService.java
@@ -0,0 +1,21 @@
+package lotto.service;
+
+import lotto.domain.LottoGame;
+import lotto.domain.LottoLine;
+import lotto.domain.LottoPayment;
+import lotto.domain.LottoResult;
+
+public class LottoService {
+
+    public LottoGame buyLotto(LottoPayment lottoPayment){
+        return LottoGame.issueLotto(getLottoLineCount(lottoPayment));
+    }
+
+    private int getLottoLineCount(LottoPayment lottoPayment){
+        return lottoPayment.getLottoLineCount();
+    }
+
+    public LottoResult getLottoResult(LottoGame lottoGame, LottoLine winLottoLine){
+        return lottoGame.getLottoResult(winLottoLine);
+    }
+}
