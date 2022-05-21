80dc5ffd884a80ce508913e39123e1a23a7b9580
boss.back <boss.back@kakaocorp.com>
2022-05-21 20:08:25 +0900

    feat: 당첨 로또 입력받는 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,8 +1,13 @@
 package lotto.view;
 
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
+import lotto.WinningLotto;
+import lotto.domain.Lotto;
+import lotto.domain.LottoNumber;
 import lotto.domain.LottoUtil;
+import lotto.strategy.ManualPickNumberStrategy;
 
 public class InputView {
 
@@ -20,17 +25,40 @@ public class InputView {
         }
     }
 
+    public static WinningLotto enterWinningLotto() {
+        while (true) {
+            try {
+                int[] lottoNumber = enterManualLotto();
+                List<LottoNumber> lottoNumbers = LottoUtil.convertToLottoNumber(lottoNumber);
+                Lotto lotto = new Lotto(new ManualPickNumberStrategy(lottoNumbers));
+                LottoNumber bonusLottoNumber = new LottoNumber(enterBonusNumber());
+                WinningLotto winningLotto = new WinningLotto(lotto, bonusLottoNumber);
+                return winningLotto;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
     public static String enterString() {
         return SCANNER.next();
     }
 
+    public static int enterBonusNumber() {
+        System.out.println("보너스 볼을 입력해주세요");
+        return enterNumber();
+    }
 
-    public static int enterNumber() throws InputMismatchException {
+
+    public static int enterPurchaseAmount() throws InputMismatchException {
         System.out.println("구입금액을 입력해주세요");
-        int number;
+        return enterNumber();
+    }
+
+    private static int enterNumber() throws InputMismatchException {
         while (true) {
             try {
-                number = SCANNER.nextInt();
+                int number = SCANNER.nextInt();
                 return number;
             } catch (InputMismatchException e) {
                 System.out.println("숫자가 아닌 값을 입력하였습니다");
