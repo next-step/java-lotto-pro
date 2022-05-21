0dde93f11c95f4a9ecfa1d762fc82212a0d9f677
mukeunzi <eunji057@gmail.com>
2022-05-22 00:11:33 +0900

    feat(domain): LottoRank에 2등 출력을 위한 보너스볼 값 추가

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -1,17 +1,25 @@
 package lotto.domain;
 
 public enum LottoRank {
-    FIRST(6, 2000000000),
-    SECOND(5, 1500000),
-    THIRD(4, 50000),
-    FOURTH(3, 5000);
+    FIRST(6, 2000000000, false),
+    SECOND(5, 30000000, true),
+    THIRD(5, 1500000, false),
+    FOURTH(4, 50000, false),
+    FIFTH(3, 5000, false),
+    NONE(0, 0, false);
 
     private final int match;
     private final long prize;
+    private final boolean hasBonus;
 
-    LottoRank(int match, long prize) {
+    LottoRank(int match, long prize, boolean hasBonus) {
         this.match = match;
         this.prize = prize;
+        this.hasBonus = hasBonus;
+    }
+
+    private boolean matchBonus(boolean hasBonus) {
+        return this.hasBonus == hasBonus;
     }
 
     public int getMatch() {
