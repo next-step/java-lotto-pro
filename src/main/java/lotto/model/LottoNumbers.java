b839dba66bf5205d92c4d6be757aa24b0a12f65f
Philip <pch4623@gmail.com>
2022-05-22 21:13:03 +0900

    feedback(LottoNumbers): 정합성 체크 로직 Generator로 이동

--- src/main/java/lotto/model/LottoNumbers.java
+++ src/main/java/lotto/model/LottoNumbers.java
@@ -3,11 +3,7 @@ package lotto.model;
 import lotto.generator.LottoNumbersGenerator;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-
-import static lotto.constants.LottoConstant.*;
 
 public class LottoNumbers {
     private final List<LottoNumber> lottoNumbers;
@@ -18,31 +14,6 @@ public class LottoNumbers {
 
     public static LottoNumbers createLottoNumbers(LottoNumbersGenerator lottoNumbersGenerator) {
         return new LottoNumbers(lottoNumbersGenerator.drawNumbers());
-    }
-
-    public static LottoNumbers createLottoNumbers(List<Integer> numbers) {
-        checkNumberDuplicate(numbers);
-        checkNumberSize(numbers);
-
-        List<LottoNumber> lottoNumbers = new ArrayList<>();
-        numbers.forEach(number -> lottoNumbers.add(LottoNumber.of(number)));
-        return new LottoNumbers(lottoNumbers);
-    }
-
-    private static void checkNumberDuplicate(List<Integer> numbers) {
-        Set<Integer> numberSet = new HashSet<>(numbers);
-        boolean hasDuplicate = numberSet.size() <  LOTTO_PICK_COUNT;
-        if (hasDuplicate) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복 값이 존재합니다!");
-        }
-    }
-
-    private static void checkNumberSize(List<Integer> numbers) {
-        boolean isOverSize = numbers.size() != LOTTO_PICK_COUNT;
-        if (isOverSize) {
-            String message = String.format("[ERROR] 로또 번호는 %d개 입력해 주세요!", LOTTO_PICK_COUNT);
-            throw new IllegalArgumentException(message);
-        }
     }
 
     public int matchCount(LottoNumbers winningNumbers) {
