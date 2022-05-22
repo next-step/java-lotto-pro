7f2580ea0bd757fbf4f6f83b0efe9a2089ee5e1d
jiyeonghwang <gyp0288@naver.com>
2022-05-23 01:48:20 +0900

    test: 보너스 볼 계산 테스트 추가

--- src/test/java/lotto/LottosTest.java
+++ src/test/java/lotto/LottosTest.java
@@ -17,16 +17,22 @@ public class LottosTest {
 
     @Test
     public void 로또_매치() {
-        String[] input = new String[2];
+        String[] input = new String[3];
         input[0] = "1,2,3,4,5,6";
         input[1] = "1,2,3,7,8,9";
+        input[2] = "1,2,3,4,5,7";
         List<Lotto> lottoList = new ArrayList<>();
         lottoList.add(LottoFactory.manualGenerator(input[0]));
         lottoList.add(LottoFactory.manualGenerator(input[1]));
+        lottoList.add(LottoFactory.manualGenerator(input[2]));
         Lottos lottos = new Lottos(lottoList);
-        Lotto winningLotto = LottoFactory.manualGenerator(input[0]);
+
+        Lotto winningLottoNumbers = LottoFactory.manualGenerator(input[0]);
+        LottoNumber bonusNumber = new LottoNumber(7);
+        WinningLotto winningLotto = new WinningLotto(winningLottoNumbers, bonusNumber);
         List<Rank> list = lottos.matchLottoStatic(winningLotto);
-        assertThat(list.get(0)).isEqualTo(Rank.valueOf(6));
-        assertThat(list.get(1)).isEqualTo(Rank.valueOf(3));
+        assertThat(list.get(0)).isEqualTo(Rank.FIRST);
+        assertThat(list.get(1)).isEqualTo(Rank.FIFTH);
+        assertThat(list.get(2)).isEqualTo(Rank.SECOND);
     }
 }
