d3b7a700ccf8f905131ff54b6f618c88448bb106
Philip <pch4623@gmail.com>
2022-05-22 21:10:05 +0900

    feedback(LottoNumbersTest,LottosTest): 테스트용 구현 클래스 사용

--- src/test/java/lotto/model/LottoNumbersTest.java
+++ src/test/java/lotto/model/LottoNumbersTest.java
@@ -1,58 +1,32 @@
 package lotto.model;
 
+import lotto.generator.TestLottoNumbersGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.Arrays;
-import java.util.List;
 
 import static lotto.constants.LottoConstant.LOTTO_PICK_COUNT;
 import static lotto.model.LottoNumbers.createLottoNumbers;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoNumbersTest {
-
     @Test
     @DisplayName("당첨 번호와 일치하는 개수를 카운트 한다.")
     void matchCount_일치_카운트() {
-        LottoNumbers lottoNumbers =  createLottoNumbers(Arrays.asList(10, 4, 1, 33, 23, 45));
-        LottoNumbers winningNumbers = createLottoNumbers(Arrays.asList(1, 3, 19, 25, 33, 45));
+        LottoNumbers lottoNumbers = createLottoNumbers(new TestLottoNumbersGenerator("10,4,1,33,23,45"));
+        LottoNumbers winningNumbers = createLottoNumbers(new TestLottoNumbersGenerator("1,3,19,25,33,45"));
 
         assertThat(lottoNumbers.matchCount(winningNumbers))
                 .isEqualTo(3);
     }
 
     @Test
-    @DisplayName("입력 값에 중복 번호가 존재하면 예외를 발생시킨다.")
-    void createWinningNumbers_중복예외() {
-        List<Integer> numbers = Arrays.asList(10, 4, 1, 23, 23, 45);
-
-        assertThatThrownBy(() -> createLottoNumbers(numbers))
-                .isExactlyInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 로또 번호에 중복 값이 존재합니다!");
-    }
-
-    @Test
-    @DisplayName("입력 값이 사이즈를 초과하면 예외를 발생시킨다.")
-    void createWinningNumbers_사이즈예외() {
-        List<Integer> numbers = Arrays.asList(10, 4, 1, 23, 23, 45, 2);
-
-        assertThatThrownBy(() -> createLottoNumbers(numbers))
-                .isExactlyInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 로또 번호는 %d개 입력해 주세요!", LOTTO_PICK_COUNT);
-    }
-
-    @Test
     @DisplayName("로또 번호 생성을 확인한다.")
     void createWinningNumbers_생성() {
-        List<Integer> numbers = Arrays.asList(10, 4, 1, 23, 25, 45);
-        LottoNumbers winningNumbers = createLottoNumbers(numbers);
+        LottoNumbers winningNumbers = createLottoNumbers(new TestLottoNumbersGenerator("10, 4, 1, 23, 25, 45"));
 
         assertThat(winningNumbers.size())
                 .isEqualTo(LOTTO_PICK_COUNT);
         assertThat(winningNumbers.toString())
                 .isEqualTo("[10, 4, 1, 23, 25, 45]");
     }
-
 }
--- src/test/java/lotto/model/LottosTest.java
+++ src/test/java/lotto/model/LottosTest.java
@@ -1,6 +1,7 @@
 package lotto.model;
 
 import lotto.enums.Rank;
+import lotto.generator.TestLottoNumbersGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -16,9 +17,9 @@ class LottosTest {
     @Test
     @DisplayName("모든 로또 번호 리스트를 담고 있는 객체를 생성한다.")
     void Lottos_생성() {
-        LottoNumbers lottoNumbers1 = createLottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6));
-        LottoNumbers lottoNumbers2 = createLottoNumbers(Arrays.asList(7, 8, 9, 10, 11, 12));
-        LottoNumbers lottoNumbers3 = createLottoNumbers(Arrays.asList(13, 14, 15, 16, 17, 18));
+        LottoNumbers lottoNumbers1 = createLottoNumbers(new TestLottoNumbersGenerator("1,2,3,4,5,6"));
+        LottoNumbers lottoNumbers2 = createLottoNumbers(new TestLottoNumbersGenerator("7,8,9,10,11,12"));
+        LottoNumbers lottoNumbers3 = createLottoNumbers(new TestLottoNumbersGenerator("13,14,15,16,17,18"));
         List<LottoNumbers> lottoNumbers = Arrays.asList(lottoNumbers1, lottoNumbers2, lottoNumbers3);
 
         assertThat(Lottos.from(lottoNumbers))
@@ -33,18 +34,18 @@ class LottosTest {
     @DisplayName("당첨번호와 비교하여 등수별로 카운트 한다.")
     void rankCount_등수별_카운트(Rank rank, int expected) {
         List<LottoNumbers> lottoNumbersList = Arrays.asList(
-                createLottoNumbers(Arrays.asList(2, 4, 20, 27, 43, 45)),  // LOSE
-                createLottoNumbers(Arrays.asList(1, 3, 6, 27, 40, 45)),   // SIXTH
-                createLottoNumbers(Arrays.asList(1, 2, 5, 27, 40, 45)),   // FIFTH
-                createLottoNumbers(Arrays.asList(1, 5, 25, 30, 40, 41)),  // FOURTH
-                createLottoNumbers(Arrays.asList(3, 5, 18, 37, 42, 45)),  // THIRD
-                createLottoNumbers(Arrays.asList(1, 5, 18, 25, 37, 43)),  // SECOND
-                createLottoNumbers(Arrays.asList(1, 5, 18, 25, 37, 44)),  // SECOND
-                createLottoNumbers(Arrays.asList(1, 5, 18, 25, 37, 42))   // FIRST
+                createLottoNumbers(new TestLottoNumbersGenerator("2, 4, 20, 27, 43, 45")),  // LOSE
+                createLottoNumbers(new TestLottoNumbersGenerator("1, 3, 6, 27, 40, 45")),   // SIXTH
+                createLottoNumbers(new TestLottoNumbersGenerator("1, 2, 5, 27, 40, 45")),   // FIFTH
+                createLottoNumbers(new TestLottoNumbersGenerator("1, 5, 25, 30, 40, 41")),  // FOURTH
+                createLottoNumbers(new TestLottoNumbersGenerator("3, 5, 18, 37, 42, 45")),  // THIRD
+                createLottoNumbers(new TestLottoNumbersGenerator("1, 5, 18, 25, 37, 43")),  // SECOND
+                createLottoNumbers(new TestLottoNumbersGenerator("1, 5, 18, 25, 37, 44")),  // SECOND
+                createLottoNumbers(new TestLottoNumbersGenerator("1, 5, 18, 25, 37, 42"))   // FIRST
         );
         Lottos lottos = Lottos.from(lottoNumbersList);
 
-        LottoNumbers winningNumbers = createLottoNumbers(Arrays.asList(1, 5, 18, 25, 37, 42));
+        LottoNumbers winningNumbers = createLottoNumbers(new TestLottoNumbersGenerator("1, 5, 18, 25, 37, 42"));
 
         RankCount rankCount = lottos.rankCount(winningNumbers);
         assertThat(rankCount)
