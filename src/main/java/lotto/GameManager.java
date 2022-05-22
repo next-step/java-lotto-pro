ce6986d4d113b56e0b64030959c789d6c246e6de
Yul Jeon <notkhaki@gmail.com>
2022-05-22 23:29:17 +0900

    refactor(GameManager): don't return null

--- src/main/java/lotto/GameManager.java
+++ src/main/java/lotto/GameManager.java
@@ -20,115 +20,79 @@ public class GameManager {
     public void run() {
         LottoVendingMachine machine = new LottoVendingMachine(new LottoNumbersGeneratorKr());
 
-        Money money = money();
+        Money money = takeMoney();
 
         int numberOfTotalGames = money.numberOfGames(LottoVendingMachine.PRICE_PER_GAME);
-        int numberOfManualGames = numberOfManualLottoGames(numberOfTotalGames).getValue();
+        int numberOfManualGames = getNumberOfManualLottoGames(numberOfTotalGames).getValue();
         int numberOfAutoGames = numberOfTotalGames - numberOfManualGames;
 
-        LottoTicket lottoTicket = machine.sellTicket(money, manualLottoGames(numberOfManualGames));
+        LottoTicket lottoTicket = machine.sellTicket(money, getManualLottoGames(numberOfManualGames));
         ResultView.printTicket(numberOfAutoGames, numberOfManualGames, lottoTicket);
 
-        WinningNumbers winningNumbers = winningNumbers();
-        LottoNumber bonusLottoNumber = bonusLottoNumber(winningNumbers);
+        WinningNumbers winningNumbers = getWinningNumbers();
+        LottoNumber bonusLottoNumber = getBonusLottoNumber(winningNumbers);
         LottoResult result = machine.check(
                 lottoTicket,
                 new LottoWin(winningNumbers, bonusLottoNumber));
         ResultView.printStats(result);
     }
 
-    private Money money() {
-        Money money = takeMoney();
-        while (money == null) {
-            money = takeMoney();
-        }
-        return money;
-    }
-
     private Money takeMoney() {
-        try {
-            Money money = new Money(InputView.readMoney(), LottoVendingMachine.PRICE_PER_GAME);
-            return money;
-        } catch (IllegalArgumentException e) {
-            ResultView.printExceptionMessage(e.getMessage());
-            return null;
+        while (true) {
+            try {
+                Money money = new Money(InputView.readMoney(), LottoVendingMachine.PRICE_PER_GAME);
+                return money;
+            } catch (IllegalArgumentException e) {
+                ResultView.printExceptionMessage(e.getMessage());
+            }
         }
-    }
-
-    private NumberOfGames numberOfManualLottoGames(int limit) {
-        NumberOfGames number = getNumberOfManualLottoGames(limit);
-        while (number == null) {
-            number = getNumberOfManualLottoGames(limit);
-        }
-        return number;
     }
 
     private NumberOfGames getNumberOfManualLottoGames(int limit) {
-        try {
-            return new NumberOfGames(InputView.readNumberOfManualLottoGames(), limit);
-        } catch (IllegalArgumentException e) {
-            ResultView.printExceptionMessage(e.getMessage());
-            return null;
+        while (true) {
+            try {
+                return new NumberOfGames(InputView.readNumberOfManualLottoGames(), limit);
+            } catch (IllegalArgumentException e) {
+                ResultView.printExceptionMessage(e.getMessage());
+            }
         }
     }
 
-    private List<LottoGame> manualLottoGames(int numberOfGames) {
+    private List<LottoGame> getManualLottoGames(int numberOfGames) {
         if (numberOfGames == 0) {
             return new ArrayList<>();
         }
 
-        List<LottoGame> manualLottoGames = getManualLottoGames(numberOfGames);
-        while (manualLottoGames == null) {
-            manualLottoGames = getManualLottoGames(numberOfGames);
+        while (true) {
+            try {
+                List<String> games = InputView.readManualLottoGames(numberOfGames);
+                return games.stream()
+                        .map(numbers -> new LottoGame(numbers))
+                        .collect(Collectors.toList());
+            } catch (IllegalArgumentException e) {
+                ResultView.printExceptionMessage(e.getMessage());
+            }
         }
-        return manualLottoGames;
-    }
-
-    private List<LottoGame> getManualLottoGames(int numberOfGames) {
-        try {
-            List<String> games = InputView.readManualLottoGames(numberOfGames);
-            return games.stream()
-                    .map(numbers -> new LottoGame(numbers))
-                    .collect(Collectors.toList());
-        } catch (IllegalArgumentException e) {
-            ResultView.printExceptionMessage(e.getMessage());
-            return null;
-        }
-    }
-
-    private WinningNumbers winningNumbers() {
-        WinningNumbers winningNumbers = getWinningNumbers();
-        while (winningNumbers == null) {
-            winningNumbers = getWinningNumbers();
-        }
-
-        return winningNumbers;
     }
 
     private WinningNumbers getWinningNumbers() {
-        try {
-            return new WinningNumbers(InputView.readWinningNumbers());
-        } catch (IllegalArgumentException e) {
-            ResultView.printExceptionMessage(e.getMessage());
-            return null;
+        while (true) {
+            try {
+                return new WinningNumbers(InputView.readWinningNumbers());
+            } catch (IllegalArgumentException e) {
+                ResultView.printExceptionMessage(e.getMessage());
+                return null;
+            }
         }
-    }
-
-    private LottoNumber bonusLottoNumber(WinningNumbers winningNumbers) {
-        LottoNumber bonusLottoNumber = getBonusLottoNumber(winningNumbers);
-        while (bonusLottoNumber == null) {
-            bonusLottoNumber = getBonusLottoNumber(winningNumbers);
-        }
-
-        return bonusLottoNumber;
     }
 
     private LottoNumber getBonusLottoNumber(WinningNumbers winningNumbers) {
-        try {
-            return new LottoNumber(InputView.readBonusNumber(), winningNumbers);
-        } catch (IllegalArgumentException e) {
-            ResultView.printExceptionMessage(e.getMessage());
-            return null;
+        while (true) {
+            try {
+                return new LottoNumber(InputView.readBonusNumber(), winningNumbers);
+            } catch (IllegalArgumentException e) {
+                ResultView.printExceptionMessage(e.getMessage());
+            }
         }
     }
 }
