35f1d6531c872a3bcbe163e62459d47e6904de05
Philip <pch4623@gmail.com>
2022-05-22 18:00:17 +0900

    feedback(LottoNumbers): 팩토리 매서드에서 인터페이스 받도록 변경

--- src/main/java/lotto/model/LottoNumbers.java
+++ src/main/java/lotto/model/LottoNumbers.java
@@ -1,32 +1,32 @@
 package lotto.model;
 
+import lotto.generator.LottoNumbersGenerator;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import static lotto.constants.LottoConstant.*;
-import static lotto.utils.RandomUtils.createRandomNumbers;
 
 public class LottoNumbers {
     private final List<LottoNumber> lottoNumbers;
 
-    public LottoNumbers(List<Integer> numbers) {
-        lottoNumbers = new ArrayList<>();
-        for (int number : numbers) {
-            lottoNumbers.add(LottoNumber.of(number));
-        }
+    private LottoNumbers(List<LottoNumber> lottoNumbers) {
+        this.lottoNumbers = lottoNumbers;
     }
 
-    public static LottoNumbers createLottoNumbers() {
-        List<Integer> randomNumbers = createRandomNumbers(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_PICK_COUNT);
-        return new LottoNumbers(randomNumbers);
+    public static LottoNumbers createLottoNumbers(LottoNumbersGenerator lottoNumbersGenerator) {
+        return new LottoNumbers(lottoNumbersGenerator.drawNumbers());
     }
 
-    public static LottoNumbers createWinningNumbers(List<Integer> numbers) {
+    public static LottoNumbers createLottoNumbers(List<Integer> numbers) {
         checkNumberDuplicate(numbers);
         checkNumberSize(numbers);
-        return new LottoNumbers(numbers);
+
+        List<LottoNumber> lottoNumbers = new ArrayList<>();
+        numbers.forEach(number -> lottoNumbers.add(LottoNumber.of(number)));
+        return new LottoNumbers(lottoNumbers);
     }
 
     private static void checkNumberDuplicate(List<Integer> numbers) {
