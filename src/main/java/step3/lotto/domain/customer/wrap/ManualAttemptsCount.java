3d76fdd6de7b35e5e276f194233d1c3890690c08
choi-ys <project.log.062@gmail.com>
2022-05-22 16:56:57 +0900

    feat: 수동으로 구매할 로또 수 도메인 설계 및 TC 작성
    - 입력 금액 내의 구매 횟수를 보장 할 수 있도록, 원시 타입 포장 객체로 설계 및 구현
    - 입력 금액 범위 내에 유효한 구매 횟수인지에 대한 검증 처리
    
    docs 갱신
    - 기능 구현에 따른 TODO List 완료 여부 갱신

---
+++ src/main/java/step3/lotto/domain/customer/wrap/ManualAttemptsCount.java
@@ -0,0 +1,46 @@
+package step3.lotto.domain.customer.wrap;
+
+import java.util.Objects;
+
+/**
+ * @author : choi-ys
+ * @date : 2022/05/22 4:34 오후
+ */
+public class ManualAttemptsCount {
+
+    public static final String INVALID_MANUAL_ATTEMPTS_COUNT_RANGE_ERROR = "수동으로 진행할 로또 횟수는 입력한 금액 범위내에서 0보다 큰 수를 입력하세요.";
+
+    private int manualAttemptsCount;
+
+    private ManualAttemptsCount(int manualAttemptsCount) {
+        this.manualAttemptsCount = manualAttemptsCount;
+    }
+
+    public static ManualAttemptsCount of(int manualAttemptsCount, Price price) {
+        validateManualAttemptsCount(manualAttemptsCount, price);
+        return new ManualAttemptsCount(manualAttemptsCount);
+    }
+
+    private static void validateManualAttemptsCount(int manualAttemptsCount, Price price) {
+        if (manualAttemptsCount < 0 || manualAttemptsCount > price.calculateTryCount()) {
+            throw new IllegalArgumentException(INVALID_MANUAL_ATTEMPTS_COUNT_RANGE_ERROR);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ManualAttemptsCount that = (ManualAttemptsCount) o;
+        return manualAttemptsCount == that.manualAttemptsCount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(manualAttemptsCount);
+    }
+}
---
+++ src/test/java/step3/lotto/domain/customer/wrap/ManualAttemptsCountTest.java
@@ -0,0 +1,61 @@
+package step3.lotto.domain.customer.wrap;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static step3.lotto.domain.customer.wrap.ManualAttemptsCount.INVALID_MANUAL_ATTEMPTS_COUNT_RANGE_ERROR;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/**
+ * @author : choi-ys
+ * @date : 2022/05/22 4:31 오후
+ */
+@DisplayName("Domain:ManualAttemptsCount")
+public class ManualAttemptsCountTest {
+
+    @Test
+    @DisplayName("수동 로또 횟수 객체 생성 검증")
+    public void createManualAttemptsCount() {
+        // Given
+        final Price price = Price.of(15000);
+        final int manualAttemptsCount = 5;
+
+        // When
+        ManualAttemptsCount actual = ManualAttemptsCount.of(manualAttemptsCount, price);
+
+        // Then
+        assertAll(
+            () -> assertThat(actual).isNotNull(),
+            () -> assertThat(actual).isEqualTo(ManualAttemptsCount.of(manualAttemptsCount, price))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    @DisplayName("유효하지 못한 수동 로또 구매 횟수 입력 시 예외 검증")
+    public void throwException_WhenInvalidManualAttemptsCount(
+        final int manualAttemptsCount,
+        final Price price,
+        final String givenDescription
+    ) {
+        // When & Then
+        assertThatExceptionOfType(IllegalArgumentException.class)
+            .isThrownBy(() -> ManualAttemptsCount.of(manualAttemptsCount, price))
+            .as(givenDescription)
+            .withMessageMatching(INVALID_MANUAL_ATTEMPTS_COUNT_RANGE_ERROR);
+    }
+
+    private static Stream throwException_WhenInvalidManualAttemptsCount() {
+        final Price price = Price.of(15000);
+        return Stream.of(
+            Arguments.of(-1, price, "수동 로또 구매 횟수가 음수인 경우"),
+            Arguments.of(price.calculateTryCount() + 1, price, "수동 로또 구매 횟수가 구매 금액을 벗어나는 경우")
+        );
+    }
+}
