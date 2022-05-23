f8cbff1a985e83107de9b8b29c8801b696324cbd
jhsong2580 <thdwmdgns@naver.com>
2022-05-23 21:12:38 +0900

    test: LottoWinInfoChecker에 대한 테스트

---
+++ src/test/java/step3/LottoWinInfoCheckerTest.java
@@ -0,0 +1,69 @@
+package step3;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import step3.domain.LottoTicket;
+import step3.enums.LottoReward;
+import step3.model.LottoGenerator;
+import step3.model.LottoMachine;
+import step3.model.LottoTickets;
+import step3.model.LottoWinChecker;
+import step3.model.LottoWinInfoChecker;
+
+
+public class LottoWinInfoCheckerTest {
+
+    private LottoWinInfoChecker lottoWinInfoChecker;
+    private LottoMachine lottoMachine;
+
+
+    @BeforeEach
+    public void init(){
+        lottoWinInfoChecker = new LottoWinInfoChecker();
+        lottoMachine = new LottoMachine(
+            new LottoGenerator(),
+            new LottoWinChecker(),
+            new LottoWinInfoChecker());
+    }
+    @Test
+    public void checkMatchLottoResultTest() {
+        initMachine();
+        LottoTickets userLottoTickets = createUserLottoTickets();
+        Map<LottoReward, Integer> matchCountPerLottoReward = lottoMachine.checkWin(
+            userLottoTickets.getLottoTickets());
+        Map<String, String> lottoResult = lottoWinInfoChecker.checkMatchLottoResult(
+            matchCountPerLottoReward, 4);
+
+
+        long reward = 30_000_000 + 50_000 + 5_000 + 2_000_000_000;
+
+        assertThat(lottoResult)
+            .containsEntry("isBenefit", "이득")
+            .containsEntry("profitRate", String.valueOf(reward * 1.0 / (1000 * 4)))
+            .containsEntry("reward", String.valueOf(reward));
+    }
+
+    private void initMachine() {
+        lottoMachine.setWinnerLottoTicket("1,2,3,4,5,6");
+        lottoMachine.setBonusNumber("7");
+
+    }
+
+    private LottoTickets createUserLottoTickets() {
+        ArrayList<LottoTicket> lottoTicketsArray = new ArrayList<>();
+        lottoTicketsArray.add(new LottoTicket("1,2,3,4,5,7")); //5_b
+        lottoTicketsArray.add(new LottoTicket("1,2,3,4,10,8"));//4
+        lottoTicketsArray.add(new LottoTicket("1,2,3,11,12,13"));//3
+        lottoTicketsArray.add(new LottoTicket("1,2,3,4,5,6"));//6
+        LottoTickets lottoTickets = new LottoTickets();
+        lottoTickets.addLottoTickets(lottoTicketsArray);
+        return lottoTickets;
+    }
+
+
+
+}
