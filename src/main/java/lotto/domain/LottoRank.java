9bdc0b6488942b3c5e88381feb14c47e78b51059
ssamzag <ssamzag@gmail.com>
2022-05-24 00:29:37 +0900

    refactor(LottoRank.java): 등수 매칭 로직 변경(BiPredicate 적용)

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -1,39 +1,31 @@
 package lotto.domain;
 
 import java.util.Arrays;
+import java.util.function.BiPredicate;
 
 public enum LottoRank {
-    MISS(0, 0),
-    FIFTH(3, 5_000),
-    FOURTH(4, 50_000),
-    THIRD(5, 1_500_000),
-    SECOND(5, true, 30_000_000),
-    FIRST(6, 2_000_000_000);
+    MISS(0, 0, (matchCount, bonus) -> matchCount < 3),
+    FIFTH(3, 5_000, (matchCount, bonus) -> matchCount == 3),
+    FOURTH(4, 50_000, (matchCount, bonus) -> matchCount == 4),
+    THIRD(5, 1_500_000, (matchCount, bonus) -> matchCount == 5 && !bonus),
+    SECOND(5, 30_000_000,(matchCount, bonus) -> matchCount == 5 && bonus),
+    FIRST(6, 2_000_000_000, (matchCount, bonus) -> matchCount == 6);
 
     private final int matchCount;
-    private final boolean bonus;
     private final Money winningMoney;
+    private final BiPredicate<Integer, Boolean> matchingExp;
 
-    LottoRank(int matchCount, double winningMoney) {
-        this(matchCount, false, winningMoney);
-    }
-
-    LottoRank(int matchCount, boolean bonus, double winningMoney) {
+    LottoRank(int matchCount, double winningMoney, BiPredicate<Integer, Boolean> exp) {
         this.matchCount = matchCount;
-        this.bonus = bonus;
         this.winningMoney = new Money(winningMoney);
+        this.matchingExp = exp;
     }
 
     public static LottoRank of(int matchCount, boolean hasBonus) {
         return Arrays.stream(LottoRank.values())
-                .filter(r -> r.matchCount == matchCount)
-                .map(r -> SECOND.isMatchCountAndBonus(matchCount, hasBonus) ? SECOND : r)
+                .filter(r -> r.matchingExp.test(matchCount, hasBonus))
                 .findFirst()
                 .orElse(MISS);
-    }
-
-    private boolean isMatchCountAndBonus(int matchCount, boolean bonus) {
-        return this.matchCount == matchCount && this.bonus == bonus;
     }
 
     public int getMatchCount() {
