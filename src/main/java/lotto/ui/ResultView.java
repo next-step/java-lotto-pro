0508ffd4cdeeaa217141c5b9a54000f978c20739
breezeunn <je7689@gmail.com>
2022-05-24 00:57:35 +0900

    refactor(PRINT): 당첨 통계 출력 로직 리팩토링

--- src/main/java/lotto/ui/ResultView.java
+++ src/main/java/lotto/ui/ResultView.java
@@ -2,7 +2,11 @@ package lotto.ui;
 
 import lotto.domain.Rank;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.EnumMap;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ResultView {
 
@@ -19,15 +23,35 @@ public class ResultView {
     public static void printGameResult(EnumMap<Rank, Integer> score, double earningRate) {
         System.out.println("당첨 통계");
         System.out.println("---------");
-        System.out.println(String.format("3개 일치 (%d원)- %d개", Rank.FIFTH.getWinningMoney(), score.get(Rank.FIFTH) == null ? 0 : score.get(Rank.FIFTH)));
-        System.out.println(String.format("4개 일치 (%d원)- %d개", Rank.FOURTH.getWinningMoney(), score.get(Rank.FOURTH) == null ? 0 : score.get(Rank.FOURTH)));
-        System.out.println(String.format("5개 일치 (%d원)- %d개", Rank.THIRD.getWinningMoney(), score.get(Rank.THIRD) == null ? 0 : score.get(Rank.THIRD)));
-        System.out.println(String.format("5개 일치, 보너스 볼 일치 (%d원)- %d개", Rank.SECOND.getWinningMoney(), score.get(Rank.SECOND) == null ? 0 : score.get(Rank.SECOND)));
-        System.out.println(String.format("6개 일치 (%d원)- %d개", Rank.FIRST.getWinningMoney(), score.get(Rank.FIRST) == null ? 0 : score.get(Rank.FIRST)));
+
+        List<Rank> printRankList = Arrays.stream(Rank.values())
+                .filter(rank -> rank != Rank.MISS)
+                .sorted(Comparator.comparing(Rank::getWinningMoney))
+                .collect(Collectors.toList());
+
+        for (Rank rank : printRankList) {
+            printGameResultByRank(score, rank);
+        }
+
+        printEarningRate(earningRate);
+    }
+
+    private static void printEarningRate(double earningRate) {
         StringBuilder earningRateMessage = new StringBuilder(String.format("총 수익률은 %.2f입니다.", earningRate));
         if (earningRate < 1) {
             earningRateMessage.append("(기준이 1이기 때문에 결과적으로 손해라는 의미임)");
         }
         System.out.println(earningRateMessage);
+    }
+
+    private static void printGameResultByRank(EnumMap<Rank, Integer> score, Rank rank) {
+        if (rank == Rank.SECOND) {
+            System.out.println(String.format("%d개 일치, 보너스 볼 일치 (%d원)- %d개"
+                    , rank.getCountOfMatch(), rank.getWinningMoney(), score.getOrDefault(rank, 0)));
+            return;
+        }
+
+        System.out.println(String.format("%d개 일치 (%d원)- %d개"
+                , rank.getCountOfMatch(), rank.getWinningMoney(), score.getOrDefault(rank, 0)));
     }
 }
