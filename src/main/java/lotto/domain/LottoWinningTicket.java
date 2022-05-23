bc464d2dcd40a0cd2a61b1c330fc03030e9a19ae
mukeunzi <eunji057@gmail.com>
2022-05-23 20:22:20 +0900

    feat(domain): 로또6자리 번호와 보너스볼 중복체크 로직 추가

--- src/main/java/lotto/domain/LottoWinningTicket.java
+++ src/main/java/lotto/domain/LottoWinningTicket.java
@@ -3,17 +3,27 @@ package lotto.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-import static lotto.message.InputMessage.INVALID_BONUS_NUMBER;
-import static lotto.message.InputMessage.INVALID_LOTTO_TICKET;
+import static lotto.message.InputMessage.*;
 
 public class LottoWinningTicket {
     private final LottoTicket lottoWinningTicket;
     private final LottoNumber bonusNumber;
 
     public LottoWinningTicket(LottoTicket lottoWinningTicket, LottoNumber bonusNumber) {
-        validateNotNull(lottoWinningTicket, bonusNumber);
+        validate(lottoWinningTicket, bonusNumber);
         this.lottoWinningTicket = lottoWinningTicket;
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validate(LottoTicket lottoWinningTicket, LottoNumber bonusNumber) {
+        validateNotNull(lottoWinningTicket, bonusNumber);
+        validateUnique(lottoWinningTicket, bonusNumber);
+    }
+
+    private void validateUnique(LottoTicket lottoWinningTicket, LottoNumber bonusNumber) {
+        if (lottoWinningTicket.contains(bonusNumber)) {
+            throw new IllegalArgumentException(INVALID_LOTTO_UNIQUE);
+        }
     }
 
     private void validateNotNull(LottoTicket lottoTicket, LottoNumber bonusNumber) {
