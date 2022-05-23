13b8d4104ce42f0f00758c16038b094f6264e251
songsimo <chy.song89@gmail.com>
2022-05-23 21:48:58 +0900

    feat: 로또를 수동으로 살 수 있는지 검증 기능 추가

--- src/main/java/lotto/domain/LottoPrice.java
+++ src/main/java/lotto/domain/LottoPrice.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 public class LottoPrice {
 	private final String INVALID_NUMBER = "0 이상의 수를 입력해주세요.";
+	private final String INVALID_ORDER = "로또를 살 수 있는 개수를 초과했습니다.";
 	private final int LOTTO_PRICE = 1000;
 	private final int ZERO = 0;
 
@@ -20,9 +21,20 @@ public class LottoPrice {
 		return availableQuantity() * LOTTO_PRICE;
 	}
 
-	private void validatePositiveNumber(int money) {
-		if(money < ZERO) {
+	public void validateOrder(int count) {
+		validatePositiveNumber(count);
+		validateQuantity(count);
+	}
+
+	private void validatePositiveNumber(int number) {
+		if(number < ZERO) {
 			throw new IllegalArgumentException(INVALID_NUMBER);
+		}
+	}
+
+	private void validateQuantity(int count) {
+		if(count > availableQuantity()) {
+			throw new IllegalArgumentException(INVALID_ORDER);
 		}
 	}
 }
--- src/test/java/lotto/domain/LottoPriceTest.java
+++ src/test/java/lotto/domain/LottoPriceTest.java
@@ -4,11 +4,12 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class MoneyTest {
+public class LottoPriceTest {
 	@ParameterizedTest
 	@ValueSource(ints = {-2, -10, -30000})
 	@DisplayName("0 이상의 숫자 입력 검증")
@@ -31,5 +32,14 @@ public class MoneyTest {
 	void expenses(int money, int cost) {
 		LottoPrice lottoPrice = new LottoPrice(money);
 		assertThat(lottoPrice.expenses()).isEqualTo(cost);
+	}
+
+	@Test
+	@DisplayName("살 수 없는 개수를 입력하면 오류 발생")
+	void validateOrder() {
+		LottoPrice lottoPrice = new LottoPrice(3000);
+
+		assertThatThrownBy(() -> lottoPrice.validateOrder(5))
+			.isInstanceOf(IllegalArgumentException.class);
 	}
 }
