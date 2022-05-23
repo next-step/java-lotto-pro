9748c3368368a464ed741a643cba54440fa8f4d7
jhsong2580 <thdwmdgns@naver.com>
2022-05-23 21:19:12 +0900

    fix: lottoMachine 생성자 변경에 의해 LottoWInInfoChecker 추가

--- src/main/java/step3/Step3Main.java
+++ src/main/java/step3/Step3Main.java
@@ -4,6 +4,7 @@ import step3.controller.LottoController;
 import step3.model.LottoGenerator;
 import step3.model.LottoMachine;
 import step3.model.LottoWinChecker;
+import step3.model.LottoWinInfoChecker;
 import step3.view.InputView;
 import step3.view.OutputView;
 
@@ -11,8 +12,11 @@ public class Step3Main {
 
     public static void main(String[] args) {
         LottoController lottoController = new LottoController(
-            new LottoMachine(new LottoGenerator(), new LottoWinChecker()),
-            new InputView(), new OutputView());
+            new LottoMachine(
+                new LottoGenerator(),
+                new LottoWinChecker(),
+                new LottoWinInfoChecker()
+            ), new InputView(), new OutputView());
         lottoController.startLotto();
     }
 
