025a972c998b5ab6dd94627474f5a5b2395c3f21
taeyong-sim <sim2ty@naver.com>
2022-05-24 00:04:58 +0900

    fix : 변수 정리(특정 클래스에서만 사용하는 변수는 해당 클래스로 이동)

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,7 +3,6 @@ package lotto.view;
 import java.util.List;
 import java.util.stream.Collectors;
 import lotto.constant.LottoConstant;
-import lotto.constant.Message;
 import lotto.domain.LottoRank;
 import lotto.domain.LottoResult;
 import lotto.dto.LottoGameDTO;
@@ -19,14 +18,16 @@ public class OutputView {
     private static final String RESULT_MESSAGE_PREFIX_NO_BONUS = "%d개 일치 (";
     private static final String RESULT_MESSAGE_PREFIX_BONUS = "%d개 일치, 보너스 볼 일치(";
     private static final String RESULT_MESSAGE_POSTFIX = "%s%d원)- %d개";
+    private static final String LOTTO_EARNING_RATE_STRING_START = "총 수익률은 ";
+    private static final String LOTTO_EARNING_RATE_STRING_END = "입니다.";
+    private static final String LOTTO_EARNING_RATE_STRING_REFERENCE = "(기준이 1이기 때문에 결과적으로 손해라는 의미임)";
 
-
-    public void printPayment(LottoGameDTO lottoGameDTO){
+    public void printPayment(LottoGameDTO lottoGameDTO) {
         System.out.println(lottoGameDTO.size() + LOTTO_PAYMENT_COUNT_MESSAGE);
         System.out.println(getLottoGameString(lottoGameDTO));
     }
 
-    public String getLottoGameString(LottoGameDTO lottoGameDTO){
+    public String getLottoGameString(LottoGameDTO lottoGameDTO) {
         List<String> lottoGameString = lottoGameDTO.getLottoGameDTO()
             .stream()
             .map(LottoLineDTO::toString)
@@ -34,21 +35,22 @@ public class OutputView {
         return String.join(LINE_BREAK, lottoGameString);
     }
 
-    public void printLottoResult(String result){
+    public void printLottoResult(String result) {
         System.out.println(LOTTO_RESULT_STRING_PREFIX);
         System.out.println(result);
     }
 
-    public String getLottoResultString(LottoResult lottoResult){
+    public String getLottoResultString(LottoResult lottoResult) {
         StringBuilder sb = new StringBuilder();
-        for(LottoRank lottoRank : LottoRank.values()){
+        for (LottoRank lottoRank : LottoRank.values()) {
             sb = appendWithoutZeroMatch(lottoRank, lottoResult, sb);
         }
         return sb.toString();
     }
 
-    private StringBuilder appendWithoutZeroMatch(LottoRank lottoRank, LottoResult lottoResult, StringBuilder sb){
-        if(lottoRank.getMatchCount() > DEFAULT_MATCH_COUNT){
+    private StringBuilder appendWithoutZeroMatch(LottoRank lottoRank, LottoResult lottoResult,
+        StringBuilder sb) {
+        if (lottoRank.getMatchCount() > DEFAULT_MATCH_COUNT) {
             sb.append(chooseMessage(lottoRank, lottoResult))
                 .append(LINE_BREAK);
         }
@@ -64,23 +66,24 @@ public class OutputView {
             lottoRank.getMatchCount());
     }
 
-    private String chooseMessage(LottoRank lottoRank, LottoResult lottoResult){
+    private String chooseMessage(LottoRank lottoRank, LottoResult lottoResult) {
         return String.format(RESULT_MESSAGE_POSTFIX,
-            choosePrefixMessage(lottoRank), lottoRank.getPrize(), lottoResult.getMatchCount(lottoRank));
+            choosePrefixMessage(lottoRank), lottoRank.getPrize(),
+            lottoResult.getMatchCount(lottoRank));
     }
 
-    public void printEarningRate(String earningRate){
+    public void printEarningRate(String earningRate) {
         System.out.println(earningRate);
     }
 
-    public String getEarningRateString(LottoPaymentDTO payment, LottoPaymentDTO prize){
+    public String getEarningRateString(LottoPaymentDTO payment, LottoPaymentDTO prize) {
         float earningRate = (float) prize.getTotalPayment() / payment.getTotalPayment();
         StringBuilder sb = new StringBuilder();
-        sb.append(Message.LOTTO_EARNING_RATE_STRING_START)
+        sb.append(LOTTO_EARNING_RATE_STRING_START)
             .append(earningRate)
-            .append(Message.LOTTO_EARNING_RATE_STRING_END);
-        if(earningRate < LottoConstant.EARNING_RATE_STANDARD){
-            sb.append(Message.LOTTO_EARNING_RATE_STRING_REFERENCE);
+            .append(LOTTO_EARNING_RATE_STRING_END);
+        if (earningRate < LottoConstant.EARNING_RATE_STANDARD) {
+            sb.append(LOTTO_EARNING_RATE_STRING_REFERENCE);
         }
         return sb.toString();
     }
