48cdce7e7dcc49e371f974fa44e0ce21d922dbb1
soob-forest <dltntnqo@naver.com>
2022-05-23 23:22:05 +0900

    refactor(MatchResult): 요구사항에 맞도록, enum 값 변경

--- src/main/java/lotto/domain/MatchResult.java
+++ src/main/java/lotto/domain/MatchResult.java
@@ -4,33 +4,21 @@ import java.util.Arrays;
 import java.util.Optional;
 
 public enum MatchResult {
-    FIRST(6, Money.from(2000000000)),
+    FIRST(6, Money.from(2000000000), false),
     SECOND(5, Money.from(30000000), true),
-    THIRD(5, Money.from(1500000)),
-    FOURTH(4, Money.from(50000)),
-    FIFTH(3, Money.from(5000)),
-    SIXTH(2, Money.from(0)),
-    SEVENTH(1, Money.from(0)),
-    EIGHTH(0, Money.from(0)),
-
-    FIRST_WITH_BONUS(6, Money.from(2000000000), true),
-    FOURTH_WITH_BONUS(4, Money.from(50000), true),
-    FIFTH_WITH_BONUS(3, Money.from(5000), true),
-    SIXTH_WITH_BONUS(2, Money.from(0), true),
-    SEVENTH_WITH_BONUS(1, Money.from(0), true),
-    EIGHTH_WITH_BONUS(0, Money.from(0), true);
+    THIRD(5, Money.from(1500000), false),
+    FOURTH(4, Money.from(50000), false),
+    FIFTH(3, Money.from(5000), false),
+    SIXTH(2, Money.from(0), false),
+    SEVENTH(1, Money.from(0), false),
+    EIGHTH(0, Money.from(0), false);
 
     private final int matchCount;
     private final Money cashPrize;
     private final boolean isBonus;
 
-    private MatchResult(int matchCount, Money cashPrize) {
-        this.matchCount = matchCount;
-        this.cashPrize = cashPrize;
-        this.isBonus = false;
-    }
 
-    private MatchResult(int matchCount, Money cashPrize, boolean isBonus) {
+    MatchResult(int matchCount, Money cashPrize, boolean isBonus) {
         this.matchCount = matchCount;
         this.cashPrize = cashPrize;
         this.isBonus = isBonus;
@@ -49,13 +37,13 @@ public enum MatchResult {
         if (result.isPresent()) {
             return result.get();
         }
-        throw new IllegalArgumentException("당첨 번호와 일치하는 로또 숫자의 개수는 0과 6 범위이어야 합니다");
+        throw new IllegalArgumentException("당첨번호 매칭 결과 케이스에 존재하지 않는 케이스입니다.");
     }
 
     private static boolean isSameMatchResult(MatchResult matchResult, int matchCount, boolean isBonus) {
         return matchResult.matchCount == matchCount && matchResult.isBonus == isBonus;
     }
-    
+
     public static MatchResult[] winningMatchResults() {
         return Arrays.stream(MatchResult.values())
                 .filter(MatchResult::isWinningMatchResult)
