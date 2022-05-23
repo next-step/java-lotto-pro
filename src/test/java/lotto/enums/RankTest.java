bf52a0dc67c26487190b95ee2261c645d968b9b9
Philip <pch4623@gmail.com>
2022-05-23 22:04:28 +0900

    test(RankTest): 등수 조회 테스트

--- src/test/java/lotto/enums/RankTest.java
+++ src/test/java/lotto/enums/RankTest.java
@@ -12,21 +12,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 class RankTest {
 
     @ParameterizedTest
-    @DisplayName("일치하는 개수에 따라 적절한 랭킹을 리턴하는지 확인한다.")
-    @CsvSource(value = {"6:FIRST", "5:SECOND", "4:THIRD", "3:FOURTH", "2:FIFTH", "1:SIXTH", "0:LOSE"}, delimiter = ':')
-    void getRank_일치하는_개수(int numberOfMatch, Rank expected) {
-        System.out.println(numberOfMatch);
-//        assertThat(getRank(numberOfMatch))
-//                .isEqualTo(expected);
-    }
-
-    @Test
-    @DisplayName("보너스 번호 당첨 여부에 따라 해당 등수를 리턴한다.")
-    void getRank_보너스() {
-        assertThat(getRank(5, false))
-                .isEqualTo(THIRD);
-        assertThat(getRank(5, true))
-                .isEqualTo(SECOND);
+    @DisplayName("일치하는 개수와 보너스 번호 여부에 따라 적절한 랭킹을 리턴하는지 확인한다.")
+    @CsvSource(value = {
+            "6:false:FIRST",
+            "5:true:SECOND",
+            "5:false:THIRD",
+            "4:false:FOURTH",
+            "3:false:FIFTH",
+            "2:false:SIXTH",
+            "1:false:SEVENTH",
+            "0:false:LOSE"
+    }, delimiter = ':')
+    void getRank_등수_조회(int numberOfMatch, boolean hasBonusNumber, Rank expected) {
+        assertThat(getRank(numberOfMatch, hasBonusNumber))
+                .isEqualTo(expected);
     }
 
     @ParameterizedTest
