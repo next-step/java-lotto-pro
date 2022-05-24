aaecfaec46a5f0d0e15c473bf11674bd9b5aabeb
tasklet1579 <tasklet1579@gmail.com>
2022-05-24 10:08:43 +0900

    refactor : 수익률 계산 로직 수정

--- src/main/java/lotto/service/YieldCalculator.java
+++ src/main/java/lotto/service/YieldCalculator.java
@@ -3,11 +3,16 @@ package lotto.service;
 import lotto.model.Money;
 import lotto.model.Summary;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class YieldCalculator {
     private YieldCalculator() {
     }
 
     public static double earningsRate(Summary summary, Money money) {
-        return 1.0 * summary.totalPrizeMoney() / money.value();
+        BigDecimal value1 = BigDecimal.valueOf(summary.totalPrizeMoney());
+        BigDecimal value2 = BigDecimal.valueOf(money.value());
+        return value1.divide(value2, 2, RoundingMode.FLOOR).doubleValue();
     }
 }
