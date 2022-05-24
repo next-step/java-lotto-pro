20562ec05bc377ef1c71bbbd96018d82636e9c39
millennion-jin <Jadestern2@>
2022-05-25 01:50:34 +0900

    feat(InputView) : 수동 로또번호 입력 기능 추가

--- src/main/java/lotto/ui/InputView.java
+++ src/main/java/lotto/ui/InputView.java
@@ -1,6 +1,9 @@
 package lotto.ui;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import lotto.domain.PurchaseQuantity;
 
 public class InputView {
 
@@ -8,6 +11,7 @@ public class InputView {
     private static final String MESSAGE_PLEASE_INPUT_LOTTO_NUMBER = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String MESSAGE_PLEASE_INPUT_BONUS_NUMBER = "보너스 볼을 입력해 주세요.";
     private static final String MESSAGE_PLEASE_INPUT_MANUAL_QUANTITY = "수동으로 구매할 로또 수를 입력해 주세요.";
+    private static final String MESSAGE_PLEASE_INPUT_MANUAL_LOTTO_NUMBERS = "수동으로 구매할 번호를 입력해 주세요.";
 
     public int inputMoneyForPurchase() {
         printMessage(MESSAGE_PLEASE_INPUT_MONEY);
@@ -24,6 +28,11 @@ public class InputView {
         return scanNumbersString();
     }
 
+    public List<String> inputManualLottoNumbers(PurchaseQuantity purchaseQuantity) {
+        printMessage(MESSAGE_PLEASE_INPUT_MANUAL_LOTTO_NUMBERS);
+        return scanNumbersStringList(purchaseQuantity.getManualQuantity());
+    }
+
     public int inputManualQuantity() {
         printMessage(MESSAGE_PLEASE_INPUT_MANUAL_QUANTITY);
         return scanNumber();
@@ -38,6 +47,14 @@ public class InputView {
     private String scanNumbersString() {
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
+    }
+
+    private List<String> scanNumbersStringList(int size) {
+        List<String> list = new ArrayList<>();
+        for(int i = 0; i < size; i++) {
+            list.add(scanNumbersString());
+        }
+        return list;
     }
 
     private void printMessage(String message) {
