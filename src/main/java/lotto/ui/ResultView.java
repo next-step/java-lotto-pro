6c00da7ada66bf6c52bfded1a987d4ff70c49941
breezeunn <je7689@gmail.com>
2022-05-24 13:20:00 +0900

    fix(PRINT): 수동과 자동 개수가 바뀌어 출력되는 bugfix

--- src/main/java/lotto/ui/ResultView.java
+++ src/main/java/lotto/ui/ResultView.java
@@ -10,7 +10,7 @@ public class ResultView {
     private ResultView() { }
 
     public static void printTicketCount(int autoTicketCount, int selfTicketCount) {
-        System.out.printf("수동으로 %d장, 자동으로 %d개를 구매했습니다.%n", autoTicketCount, selfTicketCount);
+        System.out.printf("수동으로 %d장, 자동으로 %d개를 구매했습니다.%n", selfTicketCount, autoTicketCount);
     }
 
     public static void printTicket(String ticketLottoNumbers) {
