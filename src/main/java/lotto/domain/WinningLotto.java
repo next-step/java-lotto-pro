f38e134e3362d09f06033118cbbfa8e43ae9ea1e
millennion-jin <Jadestern2@>
2022-05-26 00:33:07 +0900

    feat(WinningLotto) : 에러 메시지 한글화 및 보너스볼이 당첨로또 번호에 포함되어 있는 경우의 validation 추가

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -3,9 +3,10 @@ package lotto.domain;
 import lotto.enums.LottoRank;
 
 public class WinningLotto {
-    private static final String ERROR_MESSAGE_WINNING_LOTTO_NULL = "[ERROR] referenceLotto is null.";
-    private static final String ERROR_MESSAGE_BONUS_NUMBER_NULL = "[ERROR] bonusNumber is null.";
-    private static final String ERROR_MESSAGE_MATCHING_LOTTO_NULL = "[ERROR] lotto for matching is null.";
+    private static final String ERROR_MESSAGE_WINNING_LOTTO_NULL = "[ERROR] 당첨로또가 null 입니다.";
+    private static final String ERROR_MESSAGE_BONUS_NUMBER_NULL = "[ERROR] 보너스 로또 번호가 null 입니다.";
+    private static final String ERROR_MESSAGE_BONUS_ALREADY_EXIST = "[ERROR] 보너스 로또 번호가 당첨번호에 포함되어 있습니다.";
+    private static final String ERROR_MESSAGE_MATCHING_LOTTO_NULL = "[ERROR] 매칭 시킬 로또 객체가 null 입니다.";
 
     private final Lotto referenceLotto;
     private final LottoNumber bonusNumber;
@@ -40,6 +41,7 @@ public class WinningLotto {
     private void validate(Lotto winningLotto, LottoNumber bonusNumber) {
         validateWinningLotto(winningLotto);
         validateBonusNumber(bonusNumber);
+        validateBonusNumber(winningLotto, bonusNumber);
     }
 
     private void validateWinningLotto(Lotto winningLotto) {
@@ -51,6 +53,12 @@ public class WinningLotto {
     private void validateBonusNumber(LottoNumber bonusNumber) {
         if (bonusNumber == null) {
             throw new IllegalArgumentException(ERROR_MESSAGE_BONUS_NUMBER_NULL);
+        }
+    }
+
+    private void validateBonusNumber(Lotto winningLotto, LottoNumber bonusNumber) {
+        if (winningLotto.hasNumber(bonusNumber)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_BONUS_ALREADY_EXIST);
         }
     }
 
