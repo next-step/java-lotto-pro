e9507b257e8d20c291cec58f6e08a5a5725c583f
jiyeonghwang <gyp0288@naver.com>
2022-05-26 03:44:21 +0900

    feat: Lottos 생성 시 수동 개수, 자동 개수만큼 생성하여 반환

--- src/main/java/lotto/domain/LottoShop.java
+++ src/main/java/lotto/domain/LottoShop.java
@@ -7,9 +7,12 @@ public class LottoShop {
     private LottoShop() {
     }
 
-    public static Lottos generateLottos(int count) {
+    public static Lottos generateLottos(Money money, List<String> inputManualLottoNumbers) {
         List<Lotto> lottos = new ArrayList<>();
-        for (int i=0; i<count; i++) {
+        for (String s : inputManualLottoNumbers) {
+            lottos.add(LottoFactory.manualGenerator(s));
+        }
+        for (int i=0; i<money.getAutoCount(); i++) {
             lottos.add(LottoFactory.autoGenerator());
         }
         return new Lottos(lottos);
