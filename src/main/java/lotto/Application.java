5887e9f8e282602f55d15e1ea0e7ee250ca2a3c6
DESKTOP-IMQ723M\SKMG <lcjltj@gmail.com>
2022-05-25 13:20:08 +0900

    refactor: Application 로직 controller로 이동
    - 기존 Application 클래스를 사용자 관점으로 생각 하여 로직을 작성 했지만 application은 하나의 프로그램을
      실행시키는 클래스로 생각하여 로직 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,26 +1,11 @@
 package lotto;
 
 import lotto.controller.LottoController;
-import lotto.domain.Lotto;
-import lotto.domain.LottoNumber;
-import lotto.domain.WinningNumbers;
-import lotto.view.InputView;
-import lotto.view.ResultView;
-
-import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         LottoController lottoController = LottoController.getInstance();
-        long money = InputView.inputMoney();
-
-        List<Lotto> lottoes = lottoController.buyLotto(money);
-        ResultView.printLottoNumber(lottoes);
-
-        Lotto winner = new Lotto(InputView.inputAnswer());
-        LottoNumber bonusNumber = LottoNumber.ofString(InputView.inputBonusAnswer());
-        long totalPrize = lottoController.exchangePrize(lottoes, new WinningNumbers(winner, bonusNumber));
-        lottoController.printYield(totalPrize, money);
+        lottoController.start();
     }
 }
 
