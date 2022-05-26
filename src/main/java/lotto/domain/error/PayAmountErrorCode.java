9661986513a27e792d029f049aef38799f5b0ddf
Woody <yuncheol92@naver.com>
2022-05-27 00:32:37 +0900

    refactor : PayAmount 생성자 매개변수 타입 변경 및 자동 로또 티켓 카운트 계산

--- src/main/java/lotto/domain/error/PayAmountErrorCode.java
+++ src/main/java/lotto/domain/error/PayAmountErrorCode.java
@@ -2,7 +2,6 @@ package lotto.domain.error;
 
 public enum PayAmountErrorCode {
 
-    NOT_ALLOW_NULL_OR_EMPTY("payAmount가 null 또는 empty입니다."),
     ONLY_ALLOW_POSITIVE_NUMBER("payAmount는 양수만 허용합니다. payAmount = %s"),
     ALLOW_MIN_PAY_AMOUNT("허용하는 최소금액은 %d입니다.");
 
