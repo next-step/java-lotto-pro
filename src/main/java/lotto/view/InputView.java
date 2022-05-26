283128b49dbb3b6aa47851ad6219346213c2b5a7
boss.back <boss.back@kakaocorp.com>
2022-05-27 01:26:19 +0900

    feat: 피드백 반영

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -11,39 +11,20 @@ import lotto.strategy.ManualPickNumberStrategy;
 public class InputView {
 
     private static final String SPLIT_DELIMITER = "[\\s,]+";
-    private static final Object ERROR_OBJECT = null;
-    private static final int ERROR_NUMBER = -1;
     private static final Scanner SCANNER = new Scanner(System.in);
 
     public static int[] enterManualLotto() {
-        int[] winningNumber;
-        do {
-            winningNumber = enterManualWinningNumberOrThrowNull();
-        } while (winningNumber.equals(ERROR_OBJECT));
-
-        return winningNumber;
-    }
-
-    private static int[] enterManualWinningNumberOrThrowNull() {
         try {
             System.out.println("지난 주 당첨 번호를 입력해 주세요.");
             return LottoUtil.convertLottoStringToIntArray(enterString().split(SPLIT_DELIMITER));
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
-            return null;
+            return enterManualLotto();
         }
     }
 
+
     public static WinningLotto enterWinningLotto() {
-        WinningLotto winningLotto;
-        do {
-            winningLotto = makeWinningLottoOrThrowNull();
-        } while (winningLotto.equals(ERROR_OBJECT));
-
-        return winningLotto;
-    }
-
-    private static WinningLotto makeWinningLottoOrThrowNull() {
         try {
             int[] lottoNumber = enterManualLotto();
             List<LottoNumber> lottoNumbers = LottoUtil.convertToLottoNumber(lottoNumber);
@@ -52,9 +33,10 @@ public class InputView {
             return new WinningLotto(lotto, bonusLottoNumber);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
-            return null;
+            return enterWinningLotto();
         }
     }
+
 
     public static String enterString() {
         return SCANNER.nextLine();
@@ -73,38 +55,23 @@ public class InputView {
 
     public static int enterManualLottoCount(int numberOfLottoCanBuy) {
         System.out.println("수동으로 구매할 로또 수를 입력해주세요.");
-        int manualLottoCount;
-        do {
-            manualLottoCount = makeManualLottoCountOrThrowErrorNumber(numberOfLottoCanBuy);
-        } while (manualLottoCount == ERROR_NUMBER);
-        return manualLottoCount;
-    }
-
-    private static int makeManualLottoCountOrThrowErrorNumber(int numberOfLottoCanBuy) {
         try {
             int purchaseManualLottoCount = enterNumber();
             LottoUtil.checkManualLottoBuyCount(numberOfLottoCanBuy, purchaseManualLottoCount);
             return purchaseManualLottoCount;
         } catch (IllegalArgumentException e) {
-            return ERROR_NUMBER;
+            return enterManualLottoCount(numberOfLottoCanBuy);
         }
     }
 
-    private static int enterNumber() {
-        int number;
-        do {
-            number = makeNumber();
-        } while (number == ERROR_NUMBER);
-        return number;
-    }
 
-    private static int makeNumber() {
+    private static int enterNumber() {
         try {
             return Integer.parseInt(SCANNER.nextLine());
         } catch (NumberFormatException e) {
             System.out.println("숫자가 아닌 값을 입력하였습니다");
             SCANNER.reset();
-            return ERROR_NUMBER;
+            return enterNumber();
         }
     }
 
@@ -118,14 +85,6 @@ public class InputView {
     }
 
     public static int[] enterManualLotto(int[][] manualLottos, int index) {
-        int[] manualLotto;
-        do {
-            manualLotto = makeManualLotto(manualLottos, index);
-        } while (manualLotto == null);
-        return manualLotto;
-    }
-
-    private static int[] makeManualLotto(int[][] manualLottos, int index) {
         try {
             manualLottos[index] = LottoUtil.convertLottoStringToIntArray(
                 enterString().split(SPLIT_DELIMITER));
@@ -133,7 +92,7 @@ public class InputView {
             return manualLottos[index];
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
-            return null;
+            return enterManualLotto(manualLottos, index);
         }
     }
 }
