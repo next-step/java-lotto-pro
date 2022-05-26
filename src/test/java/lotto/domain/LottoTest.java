76b795b49e483870bdaddc95f3f7f51c2fa6b0c4
millennion-jin <Jadestern2@>
2022-05-27 03:39:55 +0900

    test(LottoTest) : 테스트 내용이 변경되어 테스트 메서드명이 테스트의 내용을 설명하지 못하는 건들을 수정

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -12,9 +12,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoTest {
-    @DisplayName("로또 생성시 입력된 숫자열의 사이즈와 동일한 사이즈를 가진 로또 객체 생성 확인")
+    @DisplayName("로또 생성시 입력된 숫자열과 동일한 값을 가지고 있는지 확인")
     @Test
-    void lottoSize() {
+    void lottoHasCorrectNumbers() {
         List<Integer> inputList = Arrays.asList(5, 41, 23, 7, 8, 1);
         Lotto lotto = new Lotto(inputList.stream().map(LottoNumber::new).collect(Collectors.toList()));
 
