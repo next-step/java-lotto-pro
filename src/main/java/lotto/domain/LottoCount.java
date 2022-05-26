985b73e3d5fc77422c05fb644716752424ecd4f5
Woody <yuncheol92@naver.com>
2022-05-26 23:27:19 +0900

    refactor : LottoCount 최소값 0으로 수정

--- src/main/java/lotto/domain/LottoCount.java
+++ src/main/java/lotto/domain/LottoCount.java
@@ -2,13 +2,15 @@ package lotto.domain;
 
 import lotto.domain.error.LottoCountErrorCode;
 
+import java.util.Objects;
+
 public class LottoCount {
 
-    private static final int MIN_LOTTO_COUNT = 1;
+    private static final int MIN = 0;
     private final int lottoCount;
 
     public LottoCount(int lottoCount) {
-        if (lottoCount < MIN_LOTTO_COUNT) {
+        if (lottoCount < MIN) {
             throw new IllegalArgumentException(LottoCountErrorCode.NOT_ALLOW_SMALLER_THAN_ONE.getMessage());
         }
         this.lottoCount = lottoCount;
@@ -16,6 +18,10 @@ public class LottoCount {
 
     public int getLottoCount() {
         return lottoCount;
+    }
+
+    public boolean isZero() {
+        return Objects.equals(lottoCount, MIN);
     }
 
     @Override
