5a4f99e48ee3642f35d3bebda35bcb84ec202e93
Philip <pch4623@gmail.com>
2022-05-28 14:53:53 +0900

    test(PurchaseTest): 수동 구매 개수 입력 로직 추가에 따른 테스트코드 수정

--- src/test/java/lotto/model/PurchaseTest.java
+++ src/test/java/lotto/model/PurchaseTest.java
@@ -10,28 +10,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 class PurchaseTest {
 
     @Test
-    @DisplayName("구매금액을 받아 로또 구매개수를 셋팅한다.")
+    @DisplayName("구매금액과 수동 구매개수를 입력받아 로또 구매내역을 셋팅한다.")
     void createPurchase_구매내역() {
-        Purchase purchase = Purchase.createPurchase(3000);
+        Purchase purchase = Purchase.createPurchase(5000, 2);
 
         assertThat(purchase.getPurchaseAmount())
                 .isExactlyInstanceOf(Money.class)
-                .isEqualTo(Money.of(3000));
-        assertThat(purchase.getCount())
+                .isEqualTo(Money.of(5000));
+        assertThat(purchase.getManualCount())
+                .isEqualTo(2);
+        assertThat(purchase.getAutoCount())
                 .isEqualTo(3);
     }
 
     @Test
-    @DisplayName("구매금액 만큼 로또 리스트를 생성한다.")
+    @DisplayName("구매개수 만큼 로또 리스트를 생성한다.")
     void createLottos_로또_리스트() {
-        Purchase purchase = Purchase.createPurchase(3000);
+        Purchase purchase = Purchase.createPurchase(5000, 2);
+        int autoCount = purchase.getAutoCount();
         LottoNumbersGenerator lottoNumbersGenerator = new AutoLottoNumbersGenerator();
 
-        Lottos lottos = purchase.createLottos(lottoNumbersGenerator);
+        Lottos lottos = purchase.createLottos(lottoNumbersGenerator, autoCount);
         assertThat(lottos)
                 .isNotNull();
         assertThat(lottos.lottosCount())
-                .isEqualTo(3);
+                .isEqualTo(autoCount);
     }
 
 }
