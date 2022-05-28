fc31591da25e559f08d4a1fd5f6b4c9a12baabe0
Philip <pch4623@gmail.com>
2022-05-28 14:34:00 +0900

    refactor(InputView): 당첨번호 입력 메시지에 개행 제거

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -31,7 +31,7 @@ public class InputView {
     }
 
     public static String readWinningNumbers() {
-        System.out.println("\n" + WINNING_NUMBERS_INPUT_MESSAGE);
+        System.out.println(WINNING_NUMBERS_INPUT_MESSAGE);
         return scanner.nextLine();
     }
 
