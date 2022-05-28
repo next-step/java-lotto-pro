b759f5bb3b1406111bd043ec71078130456a5e56
Philip <pch4623@gmail.com>
2022-05-28 10:58:02 +0900

    feat(InputView): 수동 구매할 로또 수 입력 메시지

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,12 +7,17 @@ public class InputView {
     private static final String PURCHASE_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String WINNING_NUMBERS_INPUT_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 볼을 입력해 주세요.";
-
+    private static final String MANUAL_PURCHASE_COUNT_INPUT_MESSAGE = "수동으로 구매할 로또 수를 입력해 주세요.";
 
     private InputView() {}
     
     public static String readPurchaseAmount() {
         System.out.println(PURCHASE_AMOUNT_INPUT_MESSAGE);
+        return scanner.nextLine();
+    }
+
+    public static String readManualPurchaseCount() {
+        System.out.println(MANUAL_PURCHASE_COUNT_INPUT_MESSAGE);
         return scanner.nextLine();
     }
 
