3fc6b9b1a930772fc1086d499556133034d48658
hyun <jhyun_yi@naver.com>
2022-05-29 17:36:01 +0900

    refactor : LottoConst 삭제

--- src/main/java/lotto/domain/LottoConst.java
+++
@@ -1,8 +0,0 @@
-package lotto.domain;
-
-public class LottoConst {
-    public static final int LOTTO_PRICE = 1000;
-    public static final int LOTTO_NO_START_NUMBER = 1;
-    public static final int LOTTO_NO_END_NUMBER = 45;
-    public static final int LOTTO_NO_SIZE = 6;
-}
