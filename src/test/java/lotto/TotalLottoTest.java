f86573c6c0f0656fc389cf705cadada6e6e5f8a8
jiyeonghwang <gyp0288@naver.com>
2022-06-01 19:23:36 +0900

    refactor: TotalLottoTest 개선 사항 반영

--- src/test/java/lotto/TotalLottoTest.java
+++ src/test/java/lotto/TotalLottoTest.java
@@ -11,10 +11,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class TotalLottoTest {
     private TotalLotto generatorTotalLotto() {
         Money money = Money.of(2000, 0);
+        List<String> stringList = new ArrayList<>();
+        ManualLotto manualLotto = ManualLotto.of(money, stringList);
+
         List<Lotto> lottoList = new ArrayList<>();
         lottoList.add(LottoFactory.manualGenerator("1,2,3,4,5,6"));
         lottoList.add(LottoFactory.manualGenerator("2,3,4,5,6,45"));
-        TotalLotto totalLotto = TotalLotto.of(money, new Lottos(lottoList));
+        TotalLotto totalLotto = TotalLotto.of(manualLotto, new Lottos(lottoList));
         return totalLotto;
     }
 
@@ -23,26 +26,5 @@ public class TotalLottoTest {
     void getTotalLotto() {
         TotalLotto totalLotto = generatorTotalLotto();
         assertThat(totalLotto.getCount()).isEqualTo(2);
-    }
-
-    @Test
-    @Description(value = "로또 최종 스코어 계산")
-    public void getLottoScore() {
-        Lotto win = LottoFactory.manualGenerator("1,2,3,4,5,6");
-        LottoNumber bonusNumber = LottoNumber.from(45);
-        WinningLotto winningLotto = WinningLotto.of(win, bonusNumber);
-
-        TotalLotto totalLotto = generatorTotalLotto();
-        LottoScore lottoScore = totalLotto.getLottoScore(winningLotto);
-
-        Map<Rank, Integer> map = new HashMap<>();
-        map.put(Rank.FIRST, 1);
-        map.put(Rank.SECOND, 1);
-
-        int result = (int) lottoScore.getLottoScore().entrySet().stream()
-                        .filter(rankIntegerEntry -> map.get(rankIntegerEntry.getKey()).equals(rankIntegerEntry.getValue()))
-                        .count();
-
-        assertThat(result).isEqualTo(2);
     }
 }
