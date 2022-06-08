229a519d0ecad57c2b353282a345650edeac7cc1
tasklet1579 <tasklet1579@gmail.com>
2022-06-08 22:50:36 +0900

    refactor : 금액만큼 티켓으로 교환한다

--- src/main/java/lotto/model/Ticket.java
+++ src/main/java/lotto/model/Ticket.java
@@ -1,41 +1,19 @@
 package lotto.model;
 
-import java.util.Objects;
-
 public class Ticket {
-    private static final int LOTTERY_PURCHASE_UNIT = 1000;
-    private static final String LOTTERY_MINIMUM_PURCHASE_AMOUNT_IS_1000_WON = "로또 구입 금액은 최소 1,000원 입니다.";
+    private static final int UNIT = 1000;
 
-    private int coupons;
+    private int ticket;
 
     public Ticket(Money money) {
-        if (money.value() < LOTTERY_PURCHASE_UNIT) {
-            throw new IllegalArgumentException(LOTTERY_MINIMUM_PURCHASE_AMOUNT_IS_1000_WON);
-        }
-        this.coupons = money.value() / LOTTERY_PURCHASE_UNIT;
+        ticket = money.value() / UNIT;
+    }
+
+    public void use(int minus) {
+        ticket -= minus;
     }
 
     public int size() {
-        return coupons;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Ticket ticket = (Ticket) o;
-        return coupons == ticket.coupons;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(coupons);
-    }
-
-    @Override
-    public String toString() {
-        return "Ticket{" +
-                "coupons=" + coupons +
-                '}';
+        return ticket;
     }
 }
