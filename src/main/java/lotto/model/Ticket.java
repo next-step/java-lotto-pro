ea434e00cd24ef8009aba5a93ddfa9bd5ef305e0
tasklet1579 <tasklet1579@gmail.com>
2022-06-08 23:52:51 +0900

    refactor : 오버라이드 메서드를 추가한다

--- src/main/java/lotto/model/Ticket.java
+++ src/main/java/lotto/model/Ticket.java
@@ -1,5 +1,7 @@
 package lotto.model;
 
+import java.util.Objects;
+
 public class Ticket {
     private static final int UNIT = 1000;
 
@@ -15,5 +17,25 @@ public class Ticket {
 
     public int size() {
         return ticket;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ticket ticket1 = (Ticket) o;
+        return ticket == ticket1.ticket;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ticket);
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{" +
+                "ticket=" + ticket +
+                '}';
     }
 }
