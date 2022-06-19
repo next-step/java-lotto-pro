f42dd51c33df90d5e72465851d2f559fe6610346
tasklet1579 <tasklet1579@gmail.com>
2022-06-19 22:12:41 +0900

    test : 오류 수정

--- src/test/java/lotto/service/LotterySummaryTest.java
+++ src/test/java/lotto/service/LotterySummaryTest.java
@@ -11,14 +11,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class LotterySummaryTest {
     @Test
-    void 당첨_내역() {
+    void createSummary() {
+        // given
         Map<Rank, Integer> ranks = new LinkedHashMap<>();
-        for (Rank rank : Rank.reverseValues()) {
-            ranks.put(rank, 0);
-        }
-        ranks.put(Rank.FIRST, 1);
+        Arrays.stream(Rank.reverseValues())
+              .forEach(rank -> {
+                  ranks.put(rank, 0);
+                  ranks.put(Rank.FIRST, 1);
+                  ranks.remove(Rank.MISS);
+              });
+
         Summary expected = new Summary(ranks);
 
+        // when
         List<Number> numbers = new LinkedList<>();
         IntStream.rangeClosed(1, 6)
                  .forEach(num -> numbers.add(new Number(num)));
@@ -26,6 +31,7 @@ class LotterySummaryTest {
         Lotteries purchase = new Lotteries(Collections.singletonList(new Lottery(numbers)));
         Summary actual = LotterySummary.createDetails(details, purchase);
 
+        // then
         assertThat(actual).isEqualTo(expected);
     }
 }
