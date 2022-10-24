2cde6984d846c76b4f545338d74347d579239fc4
choiyongseok90 <choi.yongseok@yanolja.com>
2022-10-24 22:46:27 +0900

    test: 요구사항2. 셋이 특정 숫자를 포함하는지 검증한다. ParameterizedTest를 활용한다.

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -4,6 +4,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
 
@@ -24,6 +26,13 @@ public class SetTest {
         Assertions.assertThat(numbers).hasSize(3);
         numbers.add(1);
         Assertions.assertThat(numbers).hasSize(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    @DisplayName("세트는 주어진 1,2,3의 값을 모두 포함하고 있다. 중복 검증 하지 않는다.")
+    void checkSetContainsNumbers(int number) {
+        Assertions.assertThat(numbers).contains(number);
     }
 
 }
