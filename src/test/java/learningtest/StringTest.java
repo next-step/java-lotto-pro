d7550c4451296273ccedaf72071e1864f288a294
cwJohnPark <pierrotcw@gmail.com>
2022-10-24 22:43:59 +0900

    test: String 클래스에 대한 학습 테스트 - 요구사항 2
    - "(1,2)" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 "1,2"를 반환하도록 구현한다.

--- src/test/java/learningtest/StringTest.java
+++ src/test/java/learningtest/StringTest.java
@@ -22,4 +22,11 @@ class StringTest {
         assertThat(숫자1만_포함된_배열).contains("1");
     }
 
+    @Test
+    void 괄호로_둘러싸인_1과_2값이_주어졌을_때_String의_substring_메소드를_활용해_괄호를_제거하고_1과2가_반환된다() {
+        String 숫자1과_2 = "(1,2)";
+        String 괄호_없는_1과_2 = 숫자1과_2.substring(숫자1과_2.indexOf("(")+1, 숫자1과_2.indexOf(")"));
+
+        assertThat(괄호_없는_1과_2).isEqualTo("1,2");
+    }
 }
