50e9ebe4cfb5a86e5cb6bfa1cb424f8179096bb0
sonyoon7 <sonyoon7@gmail.com>
2022-10-25 00:01:14 +0900

    refact: 스텝별 패키지 생성

--- src/test/java/step1/SetTest.java
+++ src/test/java/step1/SetTest.java
@@ -1,7 +1,6 @@
-package study;
+package step1;
 
 import org.assertj.core.api.Assertions;
-import org.assertj.core.internal.Strings;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
