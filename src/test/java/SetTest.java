969771598e8e499ca7e7e341215bd06448869a5f
jimbaemon <jimbae@kakao.com>
2022-10-24 21:30:09 +0900

    test: 요구사항2 @ParameterizedTest 를 활용한 중복 제거 테스트 코드 작성.

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -1,6 +1,8 @@
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -20,6 +22,12 @@ public class SetTest {
     @Test
     void 사이즈_확인(){
         Assertions.assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void 포함_여부_확인(final int number){
+        Assertions.assertThat(numbers.contains(number)).isTrue();
     }
 
 
