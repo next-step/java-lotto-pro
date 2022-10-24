05f4e635b15614331c5c175979dd34c825229a1b
velcronicity <velcronicity@gmail.com>
2022-10-24 16:54:46 +0900

    test: Set contains
    contains method test using @ParameterizedTest

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -8,6 +8,9 @@ import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -28,5 +31,22 @@ public class SetTest {
         int size = numbers.size();
         //then
         assertThat(size).isEqualTo(3);
+    }
+
+    @ParameterizedTest(name = "contains() 은 Collection 에 저장된 element 여부를 확인할 수 있다")
+    @ValueSource(ints = {1, 2, 3})
+    void containsTest(int input) throws Exception {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest(name = "contains() 은 Collection 에 저장된 element 여부를 확인할 수 있다")
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void containsTrueOrFalseTest(int element, boolean hasElement) throws Exception {
+        if (hasElement) {
+            assertThat(numbers.contains(element)).isTrue();
+        }
+        if (!hasElement) {
+            assertThat(numbers.contains(element)).isFalse();
+        }
     }
 }
