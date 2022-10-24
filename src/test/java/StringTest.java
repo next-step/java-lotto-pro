45ca8c5eaebdd9065e34bc2ce866a7773f1a9d4f
jimbaemon <jimbae@kakao.com>
2022-10-24 21:19:10 +0900

    test: 요구사항 1 테스트.

---
+++ src/test/java/StringTest.java
@@ -0,0 +1,19 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StringTest {
+
+    @Test
+    void split_one_and_two_test(){
+        Assertions.assertThat("1,2".split(","))
+            .hasSize(2)
+            .containsExactly("1", "2");
+    }
+
+    @Test
+    void split_one_only_test(){
+        Assertions.assertThat("1".split(","))
+            .hasSize(1)
+            .containsExactly("1");
+    }
+}
