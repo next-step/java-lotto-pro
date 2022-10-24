1e0b0058966c87485f2296d598f6242cbe739608
deokmoon <deok2moon@gmail.com>
2022-10-24 23:59:57 +0900

    Test: SetCollection Size 확인 테스트코드 작성

--- src/test/java/study/SetCollectionTest.java
+++ src/test/java/study/SetCollectionTest.java
@@ -20,4 +20,11 @@ public class SetCollectionTest {
         numbers.add(2);
         numbers.add(3);
     }
+
+    @DisplayName("set의 크기(size) 확인")
+    @Test
+    public void size_test() {
+        // then
+        assertThat(numbers.size()).isEqualTo(3);
+    }
 }
