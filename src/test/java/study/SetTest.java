660b1fb1dcf9a24755047a3dbf096ebcba0c4eea
sangjaeoh <45838341+sangjaeoh@users.noreply.github.com>
2022-10-25 08:39:46 +0900

    test: String 클래스, Set Collection 학습 테스트 (#654)
    StringTest
    - split, substring, chatAt 테스트
    
    SetTest
    - size, contains 테스트
    contains 테스트는 @ParameterizedTest 를 이용

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,47 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("set contains is true")
+    void contains_true(int number) {
+        assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "4:false"}, delimiter = ':')
+    @DisplayName("set contains is true or false")
+    void contains_true_or_false(int number, boolean is) {
+        assertEquals(numbers.contains(number), is);
+    }
+
+}
