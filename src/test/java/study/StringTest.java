15b48c65c4a71c284ecee3908b5c418351f20c76
chunhodong <yhms4432@naver.com>
2022-10-24 23:44:23 +0900

    test: split메소드 테스트

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,36 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringTest {
+
+    @Test
+    @DisplayName("split메소드는 1,2를 입력하면 1과2가 포함된 배열을 리턴한다")
+    void returns_array_containing_1_and_2(){
+        //given
+        String input = "1,2";
+
+        //when
+        String[] result = input.split(",");
+
+        //then
+        assertThat(result).containsExactly("1","2");
+    }
+
+    @Test
+    @DisplayName("split메소드는 1,을 입력하면 1만 리턴한다")
+    void returns_only_1(){
+        //given
+        String input = "1,";
+
+        //when
+        String[] result = input.split(",");
+
+        //then
+        assertThat(result).contains("1");
+    }
+
+}
