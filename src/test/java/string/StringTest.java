112f034769bf481aea3c2ea77e7f65c75c58bd89
daegyeom.kim <daegyeom.kim@qcells.com>
2022-10-24 14:57:35 +0900

    test(string): charAt 테스트

--- src/test/java/string/StringTest.java
+++ src/test/java/string/StringTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 @DisplayName("String 클래스에 대한 학습 테스트")
 public class StringTest {
@@ -36,5 +37,24 @@ public class StringTest {
 
         // then
         assertThat(results).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("abc 값이 주어졌을 때 charAt 메소드를 호출하여 특정 위치의 문자값이 반환되는지 확인")
+    void charAt() {
+        // when
+        char results = "abc".charAt(0);
+
+        // then
+        assertThat(results).isEqualTo('a');
+    }
+
+    @Test
+    @DisplayName("charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생")
+    void charAtException() {
+        // when & then
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                .isThrownBy(() -> "abc".charAt(-1))
+                .withMessageMatching("String index out of range: (-)?\\d+");
     }
 }
