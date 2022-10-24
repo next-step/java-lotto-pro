2adca01561d8d37079108f396d036c536a7e239a
sjk5766 <wjdrnr6662@gmail.com>
2022-10-24 15:43:44 +0900

    test(Set): Set 학습 테스트 요구사항 1
    size 테스트 코드 작성

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("size 메소드로 Set의 크기를 구할 수 있다")
+    @Test
+    void size_테스트() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+}
