f1d8e533e7b133242cb72b859a31a730436ab17a
이성혁 <46282970+seonghyeoklee@users.noreply.github.com>
2022-10-25 00:54:37 +0900

    [로또 - TDD] 1단계 - 학습 테스트 실습 PR 요청 (#658)
    * docs : 기능 요구사항 정리
    
    * test : String 클래스에 대한 학습 테스트
    
    * test : Set Collection 에 대한 학습 테스트
    
    Co-authored-by: seonghyeoklee <woody@seonghyeokleeui-MacBookPro.local>

---
+++ src/test/java/SetCollectionTest.java
@@ -0,0 +1,47 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SetCollectionTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set의 size() 메소드를 활용해 Set의 크기를 확인한다.")
+    void setCollectionTest1() {
+        assertThat(numbers).hasSize(3);
+    }
+
+    @DisplayName("Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void setCollectionTest2(int number) {
+        assertTrue(numbers.contains(number));
+    }
+
+    @DisplayName("1, 2, 3 값은 contains 메소드 실행결과 true, 4, 5 값을 넣으면 false 가 반환된다.")
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void setCollectionTest3(int number, boolean isExact) {
+        assertEquals(numbers.contains(number), isExact);
+    }
+}
