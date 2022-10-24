f62b703bca0f1ddfd197cd20f6937611b92a18c0
jimbaemon <jimbae@kakao.com>
2022-10-24 21:26:37 +0900

    test: 요구사항 3 charAt 예외 테스트.

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -1,4 +1,5 @@
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class StringTest {
@@ -20,5 +21,22 @@ public class StringTest {
     @Test
     void remove_parentheses_test(){
         Assertions.assertThat("(1,2)".substring(1,4)).isEqualTo("1,2");
+    }
+
+    @Test
+    void char_at_test(){
+        Assertions.assertThat(charAt("abc", 1)).isEqualTo('b');
+    }
+
+    @Test
+    @DisplayName("charAt 위치가 벗어나면 Exception 발생하는 부분 테스트")
+    void char_at_index_out_of_bound_exception_test(){
+        Assertions.assertThatThrownBy(() -> charAt("abc", 5))
+            .isInstanceOf(IndexOutOfBoundsException.class)
+            .hasMessage("String index out of range: 5");
+    }
+
+    private char charAt(final String string, final int index){
+        return string.charAt(index);
     }
 }
