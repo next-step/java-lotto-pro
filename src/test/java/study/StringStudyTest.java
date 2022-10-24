e9b72e1e2565443c729277272eab900f7bbf6d40
sawook <lna1003@naver.com>
2022-10-24 21:17:32 +0900

    feat: 코드 리팩토링

--- src/test/java/study/StringStudyTest.java
+++ src/test/java/study/StringStudyTest.java
@@ -3,6 +3,7 @@ package study;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -16,7 +17,9 @@ class StringStudyTest {
     @DisplayName("[요구사항 1] `1,2`를 `,` 로 split 하였을 경우 1, 2를 포함한 배열을 라턴한다 ")
     void splitTwoString() {
         String target = "1,2";
+
         String[] result = target.split(",");
+
         assertThat(result).containsExactly("1", "2");
     }
 
@@ -24,7 +27,9 @@ class StringStudyTest {
     @DisplayName("[요구사항 1] `1`을 `,` 로 split 하였을 경우 1만을 포함한 배열을 라턴한다 ")
     void splitOneString() {
         String target = "1";
+
         String[] result = target.split(",");
+
         assertThat(result).containsExactly("1");
     }
 
@@ -32,23 +37,25 @@ class StringStudyTest {
     @DisplayName("[요구사항 2] `(1,2)`를 `(,)`를 제거하고 `1,2`를 반환한다. ")
     void splitSubString() {
         String target = "(1,2)";
+
         String result = target.substring(1, target.length() - 1);
+
         assertEquals(result, "1,2");
     }
 
-    @Test
+    @ParameterizedTest
+    @CsvSource(value = {"0:a", "1:b", "2:c"}, delimiter = ':')
     @DisplayName("[요구사항 3] charAt을 이용하여 `abc`의 특정위치의 값을 가져온다 - 성공")
-    void charAtSuccessTest() {
+    void charAtSuccess(int index, char value) {
         String target = "abc";
-        assertEquals(target.charAt(0), 'a');
-        assertEquals(target.charAt(1), 'b');
-        assertEquals(target.charAt(2), 'c');
+
+        assertEquals(target.charAt(index), value);
     }
 
     @ParameterizedTest
     @ValueSource(ints = {-1, 4})
     @DisplayName("[요구사항 3] charAt이`abc`의 범위를 벗어 날 경우 예외가 발생한다. - 실패")
-    void charAtFailTest(int value) {
+    void charAtException(int value) {
         String target = "abc";
 
         assertThatThrownBy(() -> {
