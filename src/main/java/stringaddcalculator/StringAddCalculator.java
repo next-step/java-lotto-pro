339d31afa7e2343dddfdc1b84b3ebc4f18bf7d4a
hahoho87 <bbaing4564@gmail.com>
2022-10-25 00:34:03 +0900

    refactor: 리팩토링
    - 메서드 추출
    - 상수 추출
    - private 생성자 추가
    - 메서드 분리

--- src/main/java/stringaddcalculator/StringAddCalculator.java
+++ src/main/java/stringaddcalculator/StringAddCalculator.java
@@ -4,18 +4,30 @@ import java.util.Arrays;
 
 public class StringAddCalculator {
 
+    private static final String DEFAULT_DELIMITER_REGEX = "[,:]";
+
+    private StringAddCalculator() {
+    }
+
     public static int splitAndSum(String input) {
-        if (input == null || input.isEmpty()) {
+        if (isBlank(input)) {
             return 0;
         }
+        String[] strings = split(input);
+        return sum(strings);
+    }
 
-        String[] strings = input.split("[,:]");
-        if (strings.length == 1) {
-            return Integer.parseInt(input);
-        }
-
+    private static int sum(String[] strings) {
         return Arrays.stream(strings)
                 .mapToInt(Integer::parseInt)
                 .sum();
+    }
+
+    private static String[] split(String input) {
+        return input.split(DEFAULT_DELIMITER_REGEX);
+    }
+
+    private static boolean isBlank(String input) {
+        return input == null || input.isEmpty();
     }
 }
