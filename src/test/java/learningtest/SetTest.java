8851084e2018cde6d60b6f08398351ef16e50a9e
cwJohnPark <pierrotcw@gmail.com>
2022-10-24 22:56:26 +0900

    test: Set Collection에 대한 학습 테스트 - 요구사항 2
    - Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습테스트를 구현하려한다.

--- src/test/java/learningtest/SetTest.java
+++ src/test/java/learningtest/SetTest.java
@@ -3,6 +3,8 @@ package learningtest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -25,5 +27,12 @@ class SetTest {
     @DisplayName("Set의 size() 메소드를 활용해 Set의 크기를 확인할 수 있다")
     void testSetSize() {
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @DisplayName("Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지 확인할 수 잇다")
+    @ValueSource(strings = {"1", "2", "3"})
+    void testContains(int element) {
+        assertThat(numbers).contains(element);
     }
 }
