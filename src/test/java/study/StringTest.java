e03bde6731443f44f5c14e6c8ae139a55afeaefc
deokmoon <deok2moon@gmail.com>
2022-10-24 23:53:12 +0900

    Test: chartAt() 메서드 관련 테스트코드 작성  - 특정 문자 제대로 가져오는지  - Out of bound의 index 값으로 문자 가져올 떄 예외 발생하는지

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class StringTest {
     @Test
@@ -35,5 +36,22 @@ public class StringTest {
         str = str.substring(1, str.length() - 1);
         // then
         assertThat(str).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("문자열에서 특정 위치 문자 가져오기")
+    public void 문자열_특정_위치_문자_GET_테스트() {
+        // given // when
+        char chr = "1,2".charAt(1);
+        // then
+        assertThat(chr).isEqualTo(',');
+    }
+
+    @Test
+    @DisplayName("문자열 OutOfBound의 index로 문자 가져올 때 에러 발생 테스트")
+    public void CharAt메서드_OutOfBound_테스트() {
+        // given // when // then
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> "1,2".charAt(4));
     }
 }
