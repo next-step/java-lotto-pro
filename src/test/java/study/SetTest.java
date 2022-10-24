7bd33384332efa5e4d8a14fe240acef844cee331
sonyoon7 <sonyoon7@gmail.com>
2022-10-24 21:20:01 +0900

    test: ParameterizedTest with expect value

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -5,6 +5,7 @@ import org.assertj.core.internal.Strings;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -38,5 +39,12 @@ public class SetTest {
     @ValueSource(ints = {1, 2, 3})
     void refactoring_contains(int input) {
         org.junit.jupiter.api.Assertions.assertTrue(numbers.contains(input));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"} , delimiter = ':')
+    void refactoring_contains_except(int input, boolean expected) {
+
+        org.junit.jupiter.api.Assertions.assertEquals(expected, numbers.contains(input));
     }
 }
