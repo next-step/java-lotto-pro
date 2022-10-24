fb98a28aeb3c3deeb6220bb391e550e053e2fb2b
deokmoon <deok2moon@gmail.com>
2022-10-25 00:11:03 +0900

    Test: ParameterizedTest 활용하여 Contains 중복코드 제거한 테스트코드 작성

--- src/test/java/study/SetCollectionTest.java
+++ src/test/java/study/SetCollectionTest.java
@@ -3,11 +3,14 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SetCollectionTest {
     private Set<Integer> numbers;
@@ -26,5 +29,12 @@ public class SetCollectionTest {
     public void size_test() {
         // then
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @DisplayName("ParameterizedTest 활용하여 Contains 중복코드 제거")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void isContains_ShouldReturnTrueForNumbersHad(int input) {
+        assertTrue(numbers.contains(input));
     }
 }
