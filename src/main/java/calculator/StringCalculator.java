63493b0e906c8730d5bc50833fd10c5b3efbf31d
dongha-byun <byunsw4@naver.com>
2022-10-25 21:00:01 +0900

    Feat: 문자열 계산기 기능 구현

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,23 +2,46 @@ package calculator;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
-    public static int isEmpty(String input) {
-        return 0;
+
+    public static final String DEFAULT_DELIMITER = ",|:";
+
+    public static boolean isEmpty(String input) {
+        return input.isEmpty();
     }
 
-    public static int isNull(String input) {
-        return 0;
+    public static boolean isNull(String input) {
+        return input == null;
     }
 
-    public static int plusOperationByComma(String input) {
-        List<String> strings = Arrays.asList(input.split(","));
+    public static int plusOperation(String input) {
+        if(isNull(input) || isEmpty(input)){
+            return 0;
+        }
+
+        String delimiter = DEFAULT_DELIMITER;
+        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(input);
+        if(matcher.find()){
+            delimiter += "|" + matcher.group(1);
+            input = matcher.group(2);
+        }
+
         int result = 0;
+        List<String> strings = Arrays.asList(input.split(delimiter));
 
         for(String data : strings){
-            result += Integer.valueOf(data);
+            validateNumber(data);
+            result += Integer.parseInt(data);
         }
         return result;
+    }
+
+    public static void validateNumber(String input) {
+        if(!Pattern.matches("^\\d*$", input)){
+            throw new RuntimeException("숫자 이외의 문자가 포함되어 있습니다.");
+        }
     }
 }
