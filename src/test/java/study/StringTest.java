3a39b54458fe478438f1eac9d30837bdd4683125
noelleseo <noelleseo@gmail.com>
2022-10-25 21:49:09 +0900

    feat: substring 테스트 구현

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -11,11 +11,18 @@ public class StringTest {
         String[] arr = "1,2".split(",");
         assertThat(arr).containsExactly("1", "2");
     }
-    
+
     @Test
     @DisplayName("split_test_only1")
     void splitOnlyOne() {
         String[] arr = "1,".split(",");
         assertThat(arr).containsExactly("1");
+    }
+
+    @Test
+    @DisplayName("substring_test")
+    void substring() {
+        String str = "(1,2)";
+        assertThat(str.substring(1, str.length()-1)).isEqualTo("1,2");
     }
 }
