280e7ecfc481646d0e641736c74a3bf96b3a6b02
yeoojin <only1year100@gmail.com>
2022-10-25 20:17:56 +0900

    rename(StringTest.java): 알맞은 메소드명으로 수정

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -9,21 +9,21 @@ public class StringTest {
 
     @Test
     @DisplayName("String contains 테스트")
-    void splitContains() {
+    void stringSplitContains() {
         String[] result = "1,2".split(",");
         assertThat(result).contains("1");
     }
 
     @Test
     @DisplayName("String containsExactly 테스트")
-    void splitContainExactly() {
+    void stringContainExactly() {
         String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1", "2");
     }
 
     @Test
     @DisplayName("String substring 테스트")
-    void splitSubstring() {
+    void stringSubstring() {
         String str = "(1,2)";
         String subStr = str.substring(1, str.length() - 1);
 
