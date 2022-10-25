b92ee8bf2d303e33dae8243f93811fde12498c5f
taewonh <40269387+taewonh@users.noreply.github.com>
2022-10-25 22:52:18 +0900

    feat(StringAddCalculator):  기능 구현

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,6 +1,62 @@
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
 
-    public static int splitAndSum(Object o) {
-        return 0;
+    private static final Pattern delimiterPattern = Pattern.compile("//(.)\n(.*)");
+
+    public static int splitAndSum(String text) {
+        if (isEmpty(text)) {
+            return 0;
+        }
+        Matcher delimiterMatcher = delimiterPattern.matcher(text);
+        text = removeDelimiter(text, delimiterMatcher);
+        String delimiter = generateDelimiter(delimiterMatcher);
+        String[] split = text.split(delimiter);
+        return sum(split);
+    }
+
+    private static boolean isEmpty(String text) {
+        return text == null || text.isEmpty();
+    }
+
+    private static String generateDelimiter(Matcher delimiterMatcher) {
+        delimiterMatcher.reset();
+        StringBuilder delimiterBuilder = new StringBuilder();
+        delimiterBuilder.append("[,:");
+        if (delimiterMatcher.find()) {
+            delimiterBuilder.append(delimiterMatcher.group(1));
+        }
+        return delimiterBuilder.append("]").toString();
+    }
+
+    private static String removeDelimiter(String text, Matcher delimiterMatcher) {
+        delimiterMatcher.reset();
+        if (delimiterMatcher.find()) {
+            return delimiterMatcher.group(2);
+        }
+        return text;
+    }
+
+    private static int sum(String[] split) {
+        return Arrays.stream(split)
+                .mapToInt(StringAddCalculator::parseInt)
+                .peek(StringAddCalculator::checkMinus)
+                .sum();
+    }
+
+    private static int parseInt(String text) {
+        try {
+            return Integer.parseInt(text);
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+    }
+
+    private static void checkMinus(int number) {
+        if (number < 0) {
+            throw new RuntimeException();
+        }
     }
 }
