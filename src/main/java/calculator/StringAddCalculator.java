ad565c264229facb93173c608aa6b9e7815f2e39
msares <msares123@gamil.com>
2022-10-26 03:07:53 +0900

    feat(StringAddCalculator): 문자열 덧셈 계산기 구현
    - 빈 문자열 또는 null 값을 입력할 경우 0을 반환기능 구현
    - 숫자 하나를 입력하면 입력한 숫자를 반환기능 구현
    - 쉼표(,) 또는 콜론(:) 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환기능 구현
    - 커스텀 구분자는 문자열 앞부분의 “//”와 “\n” 사이에 위치하는 문자를 커스텀 구분자로 사용기능 구현
    - 숫자 이외의 값 또는 음수를 전달할 경우 RuntimeException 예외 발생 기능 구현

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,7 +1,49 @@
 package calculator;
 
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
+    private static final int DEFAULT_VALUE = 0;
+    private static final String DEFAULT_DELIMITER = "[,:]";
+    private static final String CUSTOM_DELIMITER_REGEX = "//(.)\n(.*)";
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile(CUSTOM_DELIMITER_REGEX);
+    private static final int MATCHER_GROUP_INDEX_FOR_DELIMITER = 1;
+    private static final int MATCHER_GROUP_INDEX_FOR_TARGET_STRING = 2;
+    private static final String POSITIVE_NUMBER_REGEX = "^[0-9]+$";
+    public static final String INVALID_NUMBER_MESSAGE = "유효하지 않은 값이 입력되었습니다.";
+
     public static int splitAndSum(String text) {
-        return 0;
+        if (isEmpty(text)) {
+            return DEFAULT_VALUE;
+        }
+        String[] numbers = splitText(text);
+        return sumNumbers(numbers);
+    }
+
+    private static boolean isEmpty(String text) {
+        return text == null || text.isEmpty();
+    }
+
+    private static String[] splitText(String text) {
+        Matcher m = CUSTOM_DELIMITER_PATTERN.matcher(text);
+        if (m.find()) {
+            String customDelimiter = m.group(MATCHER_GROUP_INDEX_FOR_DELIMITER);
+            return m.group(MATCHER_GROUP_INDEX_FOR_TARGET_STRING).split(customDelimiter);
+        }
+        return text.split(DEFAULT_DELIMITER);
+    }
+
+    private static int sumNumbers(String[] numbers) {
+        validateNumbers(numbers);
+        return Arrays.stream(numbers).mapToInt(Integer::parseInt).sum();
+    }
+
+    private static void validateNumbers(String[] numbers) {
+        boolean isPositiveNumber = Arrays.stream(numbers).allMatch(number -> number.matches(POSITIVE_NUMBER_REGEX));
+        if (!isPositiveNumber) {
+            throw new IllegalArgumentException(INVALID_NUMBER_MESSAGE);
+        }
     }
 }
