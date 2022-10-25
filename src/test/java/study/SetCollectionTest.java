e334929d2e260d8b4906e49f0d921eb6b4434388
yeoojin <only1year100@gmail.com>
2022-10-25 20:55:54 +0900

    test(SetCollectionTest.java): Set size() 테스트

---
+++ src/test/java/study/SetCollectionTest.java
@@ -0,0 +1,33 @@
+package study;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetCollectionTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set collection 사이즈 테스트")
+    void numbersSizeCheck() {
+        Assertions.assertThat(numbers.size()).isEqualTo(3);
+
+    }
+
+
+}
