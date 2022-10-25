144b135ac4841428f0c5d0f9adcc030cc39a6ec0
jimbaemon <jimbae@kakao.com>
2022-10-26 01:58:26 +0900

    feat: 단일형 문자 입력시 해당 문자에 해당하는 숫자 반환 테스트.

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,7 +1,11 @@
 package calculator;
 
+
 public class StringAddCalculator {
     public static int calculate(String input) {
-        return 0;
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+        return Integer.parseInt(input);
     }
 }
