cf203532cf2e38cc24477ea65a929d2000a93c20
jisu1211 <114671383+jisu1211@users.noreply.github.com>
2022-10-25 13:14:56 +0900

    test: String 클래스에 대한 학습 테스트/ Set Collection에 대한 학습 테스트 (#664)

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,48 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set의 size를 확인하는 테스트")
+    public void checkSize() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("주어진 각 원소가 Set에 포함되어 있는지 확인하는 테스트")
+    public void containsElement(int num) {
+        assertTrue(numbers.contains(num));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("주어진 각 원소가 Set에 포함되어 있는지 확인하는 테스트(확장)")
+    public void containsElement(int num, boolean expected) {
+        assertEquals(expected, numbers.contains(num));
+    }
+}
