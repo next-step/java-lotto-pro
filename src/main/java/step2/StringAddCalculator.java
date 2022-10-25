311de78121b04ba5bfca716916c48fd21b14f115
CHEESE <81912261+ychxexn@users.noreply.github.com>
2022-10-25 22:23:46 +0900

    feat: 쉼표 또는 콜론을 구분자로 가지는 문자열의 경우 구분자를 기준으로 분리한 숫자의 합을 반환

--- src/main/java/step2/StringAddCalculator.java
+++ src/main/java/step2/StringAddCalculator.java
@@ -1,7 +1,22 @@
 package step2;
 
+import java.util.Arrays;
+
 public class StringAddCalculator {
+    private static final String DEFAULT_SEPARATOR = "[,:]";
+
     public static int splitAndSum(String text) {
-        return 0;
+        String[] tokens = splitWithDefaultSeparator(text);
+        return getTokensSum(tokens);
+    }
+
+    private static String[] splitWithDefaultSeparator(String text) {
+        return text.split(DEFAULT_SEPARATOR);
+    }
+
+    private static int getTokensSum(String[] tokens) {
+        return Arrays.stream(tokens)
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 }
