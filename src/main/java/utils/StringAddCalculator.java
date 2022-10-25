33cfd7d6573b5d87af48d6c40e9af6272f68ea97
hyeongju <hybrid.simpler@gmail.com>
2022-10-25 22:46:43 +0900

    refactor: 기능요구사항2 - 커스텀 구분자를 사용하도록 코드 변경

--- src/main/java/utils/StringAddCalculator.java
+++ src/main/java/utils/StringAddCalculator.java
@@ -18,13 +18,20 @@ public class StringAddCalculator {
     }
 
     private static String[] split(String text){
+
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            return m.group(2).split(customDelimiter);
+        }
+
         return text.split(DELIMITERS);
     }
 
     private static int sum(String[] texts){
         int sum = 0;
         for (String text: texts){
-            sum += Integer.parseInt(text);
+            sum += parseInt(text);
         }
 
         return sum;
--- src/test/java/utils/StringAddCalculatorTest.java
+++ src/test/java/utils/StringAddCalculatorTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 public class StringAddCalculatorTest {
 
+    // 기능 요구사항1 - 쉼표, 콜른의 기본 구분자로 split
     @Test
     void splitAndSum_null_또는_빈문자(){
         int result = StringAddCalculator.splitAndSum(null);
@@ -31,5 +32,14 @@ public class StringAddCalculatorTest {
         int result = StringAddCalculator.splitAndSum("1,2:3");
         assertThat(result).isEqualTo(6);
     }
+
+
+    // 기능 요구사항2 - 커스텀 구분자를 지정하여 split
+    @Test
+    public void splitAndSum_custom_구분자() throws Exception {
+        int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
+        assertThat(result).isEqualTo(6);
+    }
+
 
 }
