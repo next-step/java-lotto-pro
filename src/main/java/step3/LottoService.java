7e4240b53a34257553f78b21866ebafa7598cf1b
sonyoon7 <sonyoon7@gmail.com>
2022-10-26 00:38:07 +0900

    feat (view) 로또UI 객체 구현

---
+++ src/main/java/step3/LottoService.java
@@ -0,0 +1,60 @@
+package step3;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LottoService {
+
+    private final static int LOTTO_PRICE = 1000;
+    private final List<Lotto> lottos;
+    private final Map<Integer, Integer> statistics;
+
+    public LottoService() {
+        lottos = new ArrayList<>();
+        statistics = new HashMap<>();
+        initStatistics();
+    }
+
+    public int calculateLottoCount(int money) {
+        return money / LOTTO_PRICE;
+    }
+
+    public void generateLottos(int count) {
+        System.out.println(count + Message.COUNT.getMessage());
+        for (int i = 0; i < count; i++) {
+            lottos.add(new Lotto());
+        }
+    }
+
+    public void matchWinningNumbers(List<Integer> winningNumbers) {
+        lottos.forEach(lotto -> {
+            lotto.match(winningNumbers);
+        });
+    }
+
+    public double statisticLottos(int money) {
+        lottos.forEach(lotto -> {
+            statistics.computeIfPresent(lotto.getMatchCount(), (k, v) -> v + 1);
+        });
+        int sum = statistics.get(Award.THREE.getCount()) * Award.THREE.getAmount();
+        sum += statistics.get(Award.FOUR.getCount()) * Award.FOUR.getAmount();
+        sum += statistics.get(Award.FIVE.getCount()) * Award.FIVE.getAmount();
+        sum += statistics.get(Award.SIX.getCount()) * Award.SIX.getAmount();
+        return sum / money * 100;
+    }
+
+    public Map<Integer, Integer> getStatistics() {
+        return statistics;
+    }
+
+    private void initStatistics() {
+        statistics.put(Award.THREE.getCount(), 0);
+        statistics.put(Award.FOUR.getCount(), 0);
+        statistics.put(Award.FIVE.getCount(), 0);
+        statistics.put(Award.SIX.getCount(), 0);
+    }
+
+
+}
