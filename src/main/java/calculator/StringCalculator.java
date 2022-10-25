6e540f96b670d69da06aa6ac326b317852ddbb5f
dongha-byun <byunsw4@naver.com>
2022-10-25 21:25:07 +0900

    Refactor: 문자열 계산기 구현코드 리펙터링 - 구현코드 리펙터링에 따라 테스트코드도 같이 변경

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,12 +2,19 @@ package calculator;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
 
     public static final String DEFAULT_DELIMITER = ",|:";
+
+    public static final int CUSTOM_DELIMITER_INDEX = 1;
+    public static final int CUSTOM_CALCULATE_INDEX = 2;
+    public static final String CUSTOM_CALCULATOR_REGEX = "//(.)\n(.*)";
+    public static final String ONLY_NUMBER_REGEX = "^\\d*$";
+
 
     public static boolean isEmpty(String input) {
         return input.isEmpty();
@@ -17,30 +24,36 @@ public class StringCalculator {
         return input == null;
     }
 
-    public static int plusOperation(String input) {
-        if(isNull(input) || isEmpty(input)){
+    private static boolean validateNullOrEmpty(String input) {
+        return isNull(input) || isEmpty(input);
+    }
+
+    public static int addOperation(String input) {
+        if(validateNullOrEmpty(input)){
             return 0;
         }
 
-        String delimiter = DEFAULT_DELIMITER;
-        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(input);
+        Matcher matcher = Pattern.compile(CUSTOM_CALCULATOR_REGEX).matcher(input);
         if(matcher.find()){
-            delimiter += "|" + matcher.group(1);
-            input = matcher.group(2);
+           return addByDelimiter(matcher.group(CUSTOM_CALCULATE_INDEX), matcher.group(CUSTOM_DELIMITER_INDEX));
         }
 
-        int result = 0;
-        List<String> strings = Arrays.asList(input.split(delimiter));
+        return addByDelimiter(input, DEFAULT_DELIMITER);
+    }
 
-        for(String data : strings){
-            validateNumber(data);
-            result += Integer.parseInt(data);
+    private static int addByDelimiter(String input, String delimiter) {
+        AtomicInteger result = new AtomicInteger();
+        List<String> numbers = Arrays.asList(input.split(delimiter));
+        for (String number : numbers) {
+            validateNumber(number);
+            result.addAndGet(Integer.parseInt(number));
         }
-        return result;
+
+        return result.get();
     }
 
     public static void validateNumber(String input) {
-        if(!Pattern.matches("^\\d*$", input)){
+        if(!Pattern.matches(ONLY_NUMBER_REGEX, input)){
             throw new RuntimeException("숫자 이외의 문자가 포함되어 있습니다.");
         }
     }
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -14,35 +14,35 @@ public class StringCalculatorTest {
     @DisplayName("빈 문자열은 0으로 반환한다.")
     void emptyStringIsZeroTest() {
         assertThat(StringCalculator.isEmpty("")).isTrue();
-        assertThat(StringCalculator.plusOperation("")).isEqualTo(0);
+        assertThat(StringCalculator.addOperation("")).isEqualTo(0);
     }
 
     @Test
     @DisplayName("Null 은 0으로 반환한다.")
     void nullStringIsZeroTest(){
         assertThat(StringCalculator.isNull(null)).isTrue();
-        assertThat(StringCalculator.plusOperation(null)).isEqualTo(0);
+        assertThat(StringCalculator.addOperation(null)).isEqualTo(0);
     }
 
     @ParameterizedTest
     @CsvSource(value={"1,2:3", "1,2,3:6", "2,4,:6", "1:1"}, delimiterString = ":")
     @DisplayName("쉼표(,)를 구분자로 취급하여 덧셈한다.")
     void plusOperationByComma(String input, int result){
-        assertThat(StringCalculator.plusOperation(input)).isEqualTo(result);
+        assertThat(StringCalculator.addOperation(input)).isEqualTo(result);
     }
 
     @ParameterizedTest
     @CsvSource(value = {"1:2=3", "2:3:4=9", "5:2:=7"}, delimiterString = "=")
     @DisplayName("콜론(:)을 구분자로 취급하여 덧셈한다.")
     void plusOperationByColon(String input, int result){
-        assertThat(StringCalculator.plusOperation(input)).isEqualTo(result);
+        assertThat(StringCalculator.addOperation(input)).isEqualTo(result);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"a:1,2", "3:b", "우,", "P"})
     @DisplayName("숫자 이외의 값이 나오는 경우, 예외를 발생시킨다.")
     void exceptionByIllegalData(String input){
-        assertThatThrownBy(() -> StringCalculator.plusOperation(input))
+        assertThatThrownBy(() -> StringCalculator.addOperation(input))
                 .isInstanceOf(RuntimeException.class);
     }
 
@@ -50,14 +50,14 @@ public class StringCalculatorTest {
     @ValueSource(strings = {"-1:2:3", "5,10:-4", "10000,-10", "-9"})
     @DisplayName("음수가 나오는 경우, 예외를 발생시킨다.")
     void exceptionByMinusNumber(String input){
-        assertThatThrownBy(() -> StringCalculator.plusOperation(input))
+        assertThatThrownBy(() -> StringCalculator.addOperation(input))
                 .isInstanceOf(RuntimeException.class);
     }
 
     @Test
     @DisplayName("임의의 문자를 구분자로 지정하여 덧셈합니다.")
     void customDelimiterOperation(){
-        assertThat(StringCalculator.plusOperation("//;\n1;2;3")).isEqualTo(6);
+        assertThat(StringCalculator.addOperation("//;\n1;2;3")).isEqualTo(6);
     }
 }
 
