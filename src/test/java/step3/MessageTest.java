3f9c9d3b401fe769acf0c4e51d4d12cc5a98c527
sonyoon7 <sonyoon7@gmail.com>
2022-10-25 23:59:52 +0900

    refactor (message) enum 이름 변경

--- src/test/java/step3/MessageTest.java
+++ src/test/java/step3/MessageTest.java
@@ -5,26 +5,26 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-public class LottoMessageTest {
+public class MessageTest {
 
     @Test
     void 시작_문구_테스트() {
-        Assertions.assertEquals(LottoMessage.START.getMessage(), "구입금액을 입력해 주세요.");
+        Assertions.assertEquals(Message.START.getMessage(), "구입금액을 입력해 주세요.");
     }
 
     @Test
     void 당첨_번호_입력_문구_테스트() {
-        Assertions.assertEquals(LottoMessage.WIN_NUMBERS.getMessage(), "지난 주 당첨 번호를 입력해 주세요.");
+        Assertions.assertEquals(Message.WIN_NUMBERS.getMessage(), "지난 주 당첨 번호를 입력해 주세요.");
     }
 
     @ParameterizedTest
     @CsvSource(value = {"1:3개 일치 (5000원)- 1개", "2:3개 일치 (5000원)- 2개", "3:3개 일치 (5000원)- 3개"}, delimiter = ':')
     void 당첨_통계_문구_테스트(int input, String expected) {
-        Assertions.assertEquals(expected, LottoMessage.MATCH_THREE.resultMatchNumber(input));
+        Assertions.assertEquals(expected, Message.MATCH_THREE.resultMatchNumber(input));
     }
 
     @Test
     void 총수익률_문구_테스트() {
-        Assertions.assertEquals(LottoMessage.STATISTICS_RESULT.resultStatistic(0.35), "총 수익률은 0.35입니다.");
+        Assertions.assertEquals(Message.STATISTICS_RESULT.resultStatistic(0.35), "총 수익률은 0.35입니다.");
     }
 }
