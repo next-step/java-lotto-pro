2e041d14e05563880c3f8b3613a0476a382ab939
noelleseo <noelleseo@gmail.com>
2022-10-25 22:13:38 +0900

    feat: contains 테스트 구현

--- src/test/java/study/CollectionTest.java
+++ src/test/java/study/CollectionTest.java
@@ -6,6 +6,9 @@ import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CollectionTest {
     private Set<Integer> numbers;
@@ -18,10 +21,24 @@ public class CollectionTest {
         numbers.add(2);
         numbers.add(3);
     }
-    
+
     @Test
     @DisplayName("size_test")
     void size() {
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("contains_test")
+    void contains(int param) {
+        assertThat(numbers.contains(param)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("contains_false_test")
+    void containsFalse(int param, boolean expected) {
+        assertThat(numbers.contains(param)).isEqualTo(expected);
     }
 }
