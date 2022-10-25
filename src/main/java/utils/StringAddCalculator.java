8738d395ed91611c44e7e6d609060f4e7cd04145
HJ Kim <hjkim880527@gmail.com>
2022-10-26 00:03:55 +0900

    Step2 - 문자열 덧셈 계산기 실습 PR 요청합니다 (#687)
    * docs: 2단계 - 문자열 덧셈 계산기 요구사항 추
    
    * test: 빈 문자열 또는 null 값을 입력할 경우 0을 반환한다
    
    * refactor: Step1 피드백 반영
    
    * feat: 기능요구사항1 - 쉼표 또는 콜론을 구분자로 문자열을 전달하는 함수 생성
    
    테스트코드도 함께 추가했습니다.
    
    * refactor: 기능요구사항2 - 커스텀 구분자를 사용하도록 코드 변경
    
    * refactor: 기능요구사항3 - 유효하지 않은 값에 대한 Exception 발생 처리
    
    문자열 계산기에 숫자 이외의 값 또는 음수를 전달하는 경우 RuntimeException 예외를 throw한다.
    
    * refactor: code 줄바꿈 정리

---
+++ src/main/java/utils/StringAddCalculator.java
@@ -0,0 +1,46 @@
+package utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+
+    private static String DELIMITERS = ",|:";
+
+    public static int splitAndSum(String text) {
+        if(isTextNullOrEmpty(text)){
+            return 0;
+        }
+        String[] texts = split(text);
+        return sum(texts);
+    }
+
+    private static boolean isTextNullOrEmpty(String text) {
+        return text == null || text.isEmpty();
+    }
+
+    private static String[] split(String text){
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            return m.group(2).split(customDelimiter);
+        }
+        return text.split(DELIMITERS);
+    }
+
+    private static int sum(String[] texts){
+        int sum = 0;
+        for (String text: texts){
+            sum += parseInt(text);
+        }
+        return sum;
+    }
+
+    private static int parseInt(String text){
+        if(Integer.parseInt(text) < 0){
+            throw new RuntimeException();
+        }
+        return Integer.parseInt(text);
+    }
+
+}
---
+++ src/test/java/utils/StringAddCalculatorTest.java
@@ -0,0 +1,57 @@
+package utils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class StringAddCalculatorTest {
+
+    // 기능 요구사항1 - 쉼표, 콜른의 기본 구분자로 split
+    @Test
+    void splitAndSum_null_또는_빈문자(){
+        int result = StringAddCalculator.splitAndSum(null);
+        assertThat(result).isEqualTo(0);
+
+        result = StringAddCalculator.splitAndSum("");
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    void splitAndSum_숫자하나(){
+        int result = StringAddCalculator.splitAndSum("1");
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    public void splitAndSum_쉼표구분자() throws Exception {
+        int result = StringAddCalculator.splitAndSum("1,2");
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
+        int result = StringAddCalculator.splitAndSum("1,2:3");
+        assertThat(result).isEqualTo(6);
+    }
+
+
+    // 기능 요구사항2 - 커스텀 구분자를 지정하여 split
+    @Test
+    public void splitAndSum_custom_구분자() throws Exception {
+        int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
+        assertThat(result).isEqualTo(6);
+    }
+
+
+    // 기능 요구사항3 - 숫자 이외의 값 또는 음수를 전달하는 경우 RuntimeException 발생 테스트
+    @ParameterizedTest
+    @ValueSource(strings = {"-1,2,3", "a,2,3"})
+    public void splitAndSum_negative(String strings) throws Exception {
+        assertThatThrownBy(() -> StringAddCalculator.splitAndSum(strings))
+                .isInstanceOf(RuntimeException.class);
+    }
+
+}
