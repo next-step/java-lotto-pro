cba0ca5897079c733d812d35827d4e520f5e138a
velcronicity <velcronicity@gmail.com>
2022-10-25 14:50:14 +0900

    refactor: 리뷰 반영
    테스트코드 리팩토링(리뷰 반영)

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -27,25 +26,18 @@ public class SetTest {
     @DisplayName("Set 자료구조는 중복된 데이터를 제거하고, size()는 Collection 의 크기를 반환한다")
     @Test
     void sizeTest() throws Exception {
-        int size = numbers.size();
-
-        assertThat(size).isEqualTo(3);
+        assertThat(numbers).hasSize(3);
     }
 
-    @ParameterizedTest(name = "contains() 은 Collection 에 저장된 element 여부를 확인할 수 있다")
-    @ValueSource(ints = {1, 2, 3})
-    void containsTest(int input) throws Exception {
-        assertThat(numbers.contains(input)).isTrue();
+    @DisplayName("contains() 은 Collection 에 저장된 element 여부를 확인할 수 있다")
+    @Test
+    void containsTest() throws Exception {
+        assertThat(numbers).contains(1, 2, 3);
     }
 
     @ParameterizedTest(name = "contains() 은 Collection 에 저장된 element 여부를 확인할 수 있다")
     @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
     void containsTrueOrFalseTest(int element, boolean hasElement) throws Exception {
-        if (hasElement) {
-            assertThat(numbers.contains(element)).isTrue();
-        }
-        if (!hasElement) {
-            assertThat(numbers.contains(element)).isFalse();
-        }
+        assertThat(numbers.contains(element)).isEqualTo(hasElement);
     }
 }
