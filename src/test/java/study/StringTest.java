95eafcc73c6225fc7acffdcbf493c5d90440761d
지영진 <youngjin.jee@nhn.com>
2022-10-25 20:08:40 +0900

    feat: String 클래스 학습 테스트 요구사항 3 구현

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @DisplayName("1,2 분리")
@@ -26,5 +27,25 @@ public class StringTest {
         String input = "(1,2)";
         String result = input.substring(1, input.length() - 1);
         assertThat(result).isEqualTo("1,2");
+    }
+
+    @DisplayName("특정 위치의 문자 가져오기")
+    @Test
+    void charAt() {
+        char result1 = "abc".charAt(0);
+        char result2 = "abc".charAt(1);
+        char result3 = "abc".charAt(2);
+
+        assertThat(result1).isEqualTo('a');
+        assertThat(result2).isEqualTo('b');
+        assertThat(result3).isEqualTo('c');
+    }
+
+    @DisplayName("위치 값 벗어났을 때 exception")
+    @Test
+    void charAt_exception() {
+        String input = "abc";
+        assertThatThrownBy(() -> input.charAt(3))
+                .isInstanceOf(StringIndexOutOfBoundsException.class);
     }
 }
