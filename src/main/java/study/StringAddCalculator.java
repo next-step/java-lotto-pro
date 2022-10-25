c96678923c74759273b62d238e5464c0c368e7e0
지영진 <youngjin.jee@nhn.com>
2022-10-26 01:29:44 +0900

    feat: 쉼표 구분자 외에 쌍따옴표 구분자 사용하여 계산하기
    * 메서드 분리 시행

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -3,18 +3,33 @@ package study;
 public class StringAddCalculator {
 
     public static int splitAndSum(String input) {
-        if (input == null || input.isEmpty()) {
-            return 0;
-        }
-        String[] numbers = input.split(",");
-        int result = 0;
-        if (numbers.length == 1) {
-            return 1;
-        }
-        for (int i = 0; i < numbers.length; i++) {
-            result += Integer.parseInt(numbers[i]);
-        }
+        if (isNullOrEmptyString(input)) return 0;
+        String[] numbers = input.split(",|:");
+        if (isOnlyNumber(numbers)) return 1;
+        int result = caculate(numbers);
 
         return result;
+    }
+
+    private static boolean isOnlyNumber(String[] numbers) {
+        if (numbers.length == 1) {
+            return true;
+        }
+        return false;
+    }
+
+    private static int caculate(String[] numbers) {
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += Integer.parseInt(numbers[i]);
+        }
+        return sum;
+    }
+
+    private static boolean isNullOrEmptyString(String input) {
+        if (input == null || input.isEmpty()) {
+            return true;
+        }
+        return false;
     }
 }
