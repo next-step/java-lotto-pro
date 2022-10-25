5aa53c730edb9c6a04f96fec7fdc853fe4fa2e86
bronze <yhms4432@naver.com>
2022-10-26 07:10:10 +0900

    Step1 학습 테스트 PR 요청드립니다. (#679)
    * test: split메소드 테스트
    
    * docs: empty.txt 파일삭제
    
    * test: substring메소드 테스트
    
    * test: charAt메소드 테스트
    
    * test: Set컬렉션 테스트
    
    - size,contains메소드를 테스트
    
    * refactor: 테스트코드 계층구조로 변형
    
    -같은 메소드를 테스트하는경우 클래스로 구조화

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,85 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class StringTest {
+
+    @Nested
+    @DisplayName("split메소드는")
+    class Split {
+        @Test
+        @DisplayName("1,2를 입력하면 1과2가 포함된 배열을 리턴한다")
+        void returns_array_containing_1_and_2() {
+            //given
+            String input = "1,2";
+
+            //when
+            String[] result = input.split(",");
+
+            //then
+            assertThat(result).containsExactly("1", "2");
+        }
+
+        @Test
+        @DisplayName("1,을 입력하면 1만 리턴한다")
+        void returns_only_1() {
+            //given
+            String input = "1,";
+
+            //when
+            String[] result = input.split(",");
+
+            //then
+            assertThat(result).contains("1");
+        }
+
+    }
+
+
+    @Test
+    @DisplayName("substring메소드는 (1,2)를 입력하면 1,2를 리턴한다")
+    void returns_1_comma_2() {
+        //given
+        String input = "(1,2)";
+
+        //when
+        String result = input.substring(1, input.length() - 1);
+
+        //then
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Nested
+    @DisplayName("charAt메소드는")
+    class ChatAt {
+
+        @Test
+        @DisplayName("위치값의 범위를 벗어나면 StringIndexOutOfBoundsException이 발생")
+        void throws_StringIndexOutOfBoundsException() {
+            //given
+            String input = "abc";
+
+            //when,then
+            assertThatThrownBy(() -> input.charAt(5))
+                    .isInstanceOf(StringIndexOutOfBoundsException.class);
+        }
+
+        @Test
+        @DisplayName("위치값에 해당하는 문자를 리턴")
+        void returns_character_corresponding_position_value() {
+            //given
+            String input = "abc";
+
+            //when
+            char result = input.charAt(1);
+
+            assertThat(result).isEqualTo('b');
+        }
+
+    }
+
+}
