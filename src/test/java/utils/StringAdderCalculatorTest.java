6aa795165e6026f28a5639c5606a34ae630dcb4f
cwJohnPark <pierrotcw@gmail.com>
2022-10-25 17:33:38 +0900

    test: 숫자하나를 전달할 경우 숫자를 그대로 반환한다

--- src/test/java/utils/StringAdderCalculatorTest.java
+++ src/test/java/utils/StringAdderCalculatorTest.java
@@ -2,6 +2,7 @@ package utils;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,5 +21,17 @@ class StringAdderCalculatorTest {
         int 덧셈_결과 = StringAdderCalculator.splitAndSum(null);
         assertThat(덧셈_결과).isEqualTo(0);
     }
+
+    @ParameterizedTest
+    @CsvSource({"0,1,100"})
+    void 숫자하나를_전달할_경우_숫자를_그대로_반환한다(String 숫자하나) {
+        int 덧셈_결과 = StringAdderCalculator.splitAndSum(숫자하나);
+        assertThat(정수_변환(숫자하나)).isEqualTo(덧셈_결과);
+    }
+
+    private int 정수_변환(String literalInteger) {
+        return Integer.parseInt(literalInteger);
+    }
+
 
 }
