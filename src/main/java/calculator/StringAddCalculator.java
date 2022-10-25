86b00aebb491bb6f5f1f16d7446eb94c41dddc9b
crongcm <ucm5459@kakao.com>
2022-10-25 23:49:40 +0900

    feat(StringAddCalculator): 문자열 계산기 구현

---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,63 @@
+package calculator;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+    public static final int ZERO = 0;
+    public static final String REGEX_COMMA_OR_COLON_SEPARATOR = ",|:";
+    public static final String REGEX_CUSTOM_SEPARATOR = "//(.)\n(.*)";
+    public static final String ERROR_MESSAGE_NEGATIVE = "음수는 입력 불가능 합니다.";
+    public static final String ERROR_MESSAGE_ONLY_NUMBER = "숫자 이외의 값은 입력 불가능합니다.";
+
+    private StringAddCalculator() {
+    }
+
+    public static int splitAndSum(String input) {
+        if (isNull(input) || input.isEmpty()) {
+            return ZERO;
+        }
+
+        String[] split = splitText(input);
+        return sum(split);
+    }
+
+    private static boolean isNull(String input) {
+        return input == null;
+    }
+
+    private static String[] splitText(String input) {
+        Matcher matcher = Pattern.compile(REGEX_CUSTOM_SEPARATOR).matcher(input);
+        if (matcher.find()) {
+            String customSeparator = matcher.group(1);
+            return matcher.group(2).split(customSeparator);
+        }
+        return input.split(REGEX_COMMA_OR_COLON_SEPARATOR);
+    }
+
+    private static int parseInt(String text) {
+        int number;
+
+        try {
+            number = Integer.parseInt(text);
+        } catch (Exception e) {
+            throw new RuntimeException(ERROR_MESSAGE_ONLY_NUMBER);
+        }
+
+        validNegative(number);
+        return number;
+    }
+
+    private static void validNegative(int number) {
+        if (number < ZERO) {
+            throw new RuntimeException(ERROR_MESSAGE_NEGATIVE);
+        }
+    }
+
+    private static int sum(String[] split) {
+        return Arrays.stream(split)
+                .mapToInt(StringAddCalculator::parseInt)
+                .sum();
+    }
+}
