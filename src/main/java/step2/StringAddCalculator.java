5e5e06942f875e728cd42b1226a980e5ba4ad0d8
choiyongseok90 <badaelephant>
2022-10-26 00:32:21 +0900

    feat: splitAndSum의 기본 함수 골격 설정 1. 값이 비면 0을 리턴한다. 2. 값이 하나의 숫자면 그 숫자를 리턴한다. 3. 문자열을 스플릿하여 배열을 리턴한다. 4. 문자열 배열을 더한 값을 리턴한다.

--- src/main/java/step2/StringAddCalculator.java
+++ src/main/java/step2/StringAddCalculator.java
@@ -1,7 +1,32 @@
 package step2;
 
+
 public class StringAddCalculator {
 
-    public static int splitAndSum(Object o) {
+    public static int splitAndSum(String text) {
+        if (isStringNullOrEmpty(text)) {
+            return 0;
+        }
+        if (isNumber(text)) {
+            return Integer.parseInt(text);
+        }
+        String[] splitNumbers = splitNumber(text);
+        return addAllNumbers(splitNumbers);
+    }
+
+    private static int addAllNumbers(String[] splitNumbers) {
+        return 0;
+    }
+
+    private static boolean isNumber(String text) {
+        return false;
+    }
+
+    private static boolean isStringNullOrEmpty(String text) {
+        return true;
+    }
+
+    private static String[] splitNumber(String text) {
+        return new String[0];
     }
 }
