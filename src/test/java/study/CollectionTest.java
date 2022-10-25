dcb75690c456429f87868fd756c5cdc6edfca137
noelleseo <noelleseo@gmail.com>
2022-10-25 22:05:14 +0900

    feat: size 테스트 구현

---
+++ src/test/java/study/CollectionTest.java
@@ -0,0 +1,27 @@
+package study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CollectionTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+    
+    @Test
+    @DisplayName("size_test")
+    void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+}
