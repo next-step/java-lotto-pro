bf0b832095d66b1f3cc665e77e82a221dc4bc063
soojungkk <sj921101@naver.com>
2022-10-26 20:57:02 +0900

    test : ()괄호 제거 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -11,4 +11,11 @@ public class StringTest {
         String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1","2");
     }
+
+    @Test
+    void 괄호제거(){
+        String result = "(1,2)".replaceAll("\\(","");
+        result=result.replaceAll("\\)","");
+        assertThat(result).isEqualTo("1,2");
+    }
 }
