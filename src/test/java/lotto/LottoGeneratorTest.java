a5e8c5c604951f21f7fa26fda56230458bef228b
ilmare-cbk <cbk104325@gmail.com>
2022-10-27 00:39:45 +0900

    test(LottoGeneratorTest.java): 로또 한 장당 가격이 다를 경우 구입할 수 있는 로또 갯수 확인 테스트 추가

--- src/test/java/lotto/LottoGeneratorTest.java
+++ src/test/java/lotto/LottoGeneratorTest.java
@@ -8,16 +8,24 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 class LottoGeneratorTest {
 
+    private final LottoNumberGenerator lottoNumberGenerator =
+            LottoNumberGenerator.from(new DefaultNumberGeneratorStrategy());
+
     @ParameterizedTest
     @CsvSource(value = {"14000:14", "17500:17", "0:0"}, delimiter = ':')
-    @DisplayName("로또 구입 금액이 입력되면 구입할 수 있는 로또 갯수만큼 로또를 생성한다.")
-    void generateLotto(int input, int expected) {
-        LottoGenerator lottoGenerator =
-                LottoGenerator.from(
-                        LottoNumberGenerator.from(new DefaultNumberGeneratorStrategy()),
-                        Money.from(1000)
-                );
+    @DisplayName("로또 구입 금액이 입력되면 구입할 수 있는 로또 갯수만큼 로또를 생성한다.(로또 한 장당 가격은 1000원)")
+    void generateLotto1(int input, int expected) {
+        LottoGenerator lottoGenerator = LottoGenerator.from(lottoNumberGenerator, Money.from(1000));
         Lottos lottos = lottoGenerator.generate(Money.from(input));
+        Assertions.assertThat(lottos.size()).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1000:14", "2000:7", "7000:2"}, delimiter = ':')
+    @DisplayName("로또 구입 금액이 고정(14000)되어 있을 때 로또 가격에 따른 구입할 수 있는 로또 갯수 확인")
+    void generateLotto2(int input, int expected) {
+        LottoGenerator lottoGenerator = LottoGenerator.from(lottoNumberGenerator, Money.from(input));
+        Lottos lottos = lottoGenerator.generate(Money.from(14000));
         Assertions.assertThat(lottos.size()).isEqualTo(expected);
     }
 }
