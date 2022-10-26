029b0c590071a77dd69ccbb87bcc745e42ceb8ce
jimbaemon <jimbae@kakao.com>
2022-10-26 16:41:56 +0900

    refactor: Pattern 명명, MagicString 제거.

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -6,7 +6,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
-    private final static Pattern SPLITER = Pattern.compile("//(.)\n(.*)");
+    private final static Pattern SPLIT_REGEX_PATTERN = Pattern.compile("//(.)\n(.*)");
+    private final static String DEFAULT_SPLIT_REGEX = "[,:]";
 
     public static int calculate(String input) {
         if (input == null || input.isEmpty()) {
@@ -16,13 +17,13 @@ public class StringAddCalculator {
     }
 
     private static String[] split(final String input){
-        Matcher matcher = SPLITER.matcher(input);
+        Matcher matcher = SPLIT_REGEX_PATTERN.matcher(input);
         if(matcher.find()){
             String customDelimiter = matcher.group(1);
             String string = matcher.group(2);
             return string.split(customDelimiter);
         }
-        return input.split("[,:]");
+        return input.split(DEFAULT_SPLIT_REGEX);
     }
 
     private static int sum(final String[] input) {
