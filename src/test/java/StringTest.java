087b06278109ac6fbe7fabeb2691aa75341cb87e
victor-jo <joasjn45@gmail.com>
2022-10-26 09:10:30 +0900

    test(stringTest): 스트링 콤마 나누기 테스트 적용
    콤마로 끝나는 문자열에 대한 예외 테스트

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -12,4 +12,12 @@ public class StringTest {
         assertThat(token).contains("2");
         assertThat(token).containsExactly("1", "2");
     }
+
+    @Test
+    void 스트링_콤마_나누기_하나만_적용_예외() {
+        String str = "1,";
+        String[] token = str.split(",");
+        assertThat(token).contains("1");
+        assertThat(token).containsExactly("1");
+    }
 }
