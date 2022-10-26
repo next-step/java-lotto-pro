ba3df4525445d5d3ddf8ccef3c62d9b3793160c3
chunhodong <yhms4432@naver.com>
2022-10-26 13:59:25 +0900

    test: splitAndSum메소드에서 구분자로 구분된 숫자합 테스트 작성

--- src/main/java/step2/StringAddCalculator.java
+++ src/main/java/step2/StringAddCalculator.java
@@ -1,17 +1,26 @@
 package step2;
 
+import java.util.Arrays;
+
 public class StringAddCalculator {
 
     private static final int DEFAULT_VALUE = 0;
+    private static final String DEFAULT_DELIMITER = ",|:";
+
     public static int splitAndSum(String text) {
-
         if(isEmptyOrNull(text))return DEFAULT_VALUE;
-
-        return DEFAULT_VALUE;
+        String[] tokens = text.split(DEFAULT_DELIMITER);
+        return sumNumbers(tokens);
     }
 
     private static boolean isEmptyOrNull(String text){
         return text == null || text.isEmpty();
+    }
+
+    private static int sumNumbers(String[] numbers){
+        return Arrays.stream(numbers)
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 
 
--- src/test/java/step2/StringAddCalculatorTest.java
+++ src/test/java/step2/StringAddCalculatorTest.java
@@ -9,16 +9,31 @@ public class StringAddCalculatorTest {
 
 
     @Test
-    @DisplayName("splitAndSum메소드는 null값을 입력하면 0을 리턴")
+    @DisplayName("null값을 입력하면 0을 반환")
     public void returns_0_if_null_value_input() {
         int result = StringAddCalculator.splitAndSum(null);
         assertThat(result).isEqualTo(0);
     }
 
     @Test
-    @DisplayName("splitAndSum메소드는 빈값을 입력하면 0을 리턴")
+    @DisplayName("빈값을 입력하면 0을 반환")
     public void returns_0_if_empty_value_input() {
         int result = StringAddCalculator.splitAndSum("");
         assertThat(result).isEqualTo(0);
     }
+
+    @Test
+    @DisplayName("숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환")
+    public void returns_sum_if_two_number_entered_with_comma_delimiter() {
+        int result = StringAddCalculator.splitAndSum("1,2");
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("숫자 두개를 컴마(,)또는콜론(:) 구분자로 입력할 경우 두 숫자의 합을 반환")
+    public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
+        int result = StringAddCalculator.splitAndSum("1,2:3");
+        assertThat(result).isEqualTo(6);
+    }
+
 }
