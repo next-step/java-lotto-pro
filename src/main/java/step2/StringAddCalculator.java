7a88d29495ce55a772bca9fb65d83919e9c804d5
CHEESE <81912261+ychxexn@users.noreply.github.com>
2022-10-26 22:34:22 +0900

    feat: 커스텀 구분자는 문자열 앞부분의 “//”와 “\n” 사이에 위치하는 문자를 커스텀 구분자로 사용

--- src/main/java/step2/StringAddCalculator.java
+++ src/main/java/step2/StringAddCalculator.java
@@ -1,17 +1,36 @@
 package step2;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringAddCalculator {
     private static final String DEFAULT_SEPARATOR = "[,:]";
 
     public static int splitAndSum(String text) {
-        String[] tokens = splitWithDefaultSeparator(text);
+        String[] tokens = split(text);
         return getTokensSum(tokens);
+    }
+
+    private static String[] split(String text) {
+        String[] tokens = null;
+        if(text.contains(",") || text.contains(":")) {
+            tokens = splitWithDefaultSeparator(text);
+        }
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (m.find()) {
+            String customSeparator = m.group(1);
+            tokens = splitWithCustomSeparator(m.group(2), customSeparator);
+        }
+        return tokens;
     }
 
     private static String[] splitWithDefaultSeparator(String text) {
         return text.split(DEFAULT_SEPARATOR);
+    }
+
+    private static String[] splitWithCustomSeparator(String text, String separator) {
+        return text.split(separator);
     }
 
     private static int getTokensSum(String[] tokens) {
