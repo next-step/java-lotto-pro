a0022dc9c2cf4713817fdab2bbb60342086bec46
jimbaemon <jimbae@kakao.com>
2022-10-26 16:32:27 +0900

    feat : 구분자 지정 사용 합산 구현.

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -2,11 +2,22 @@ package calculator;
 
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringAddCalculator {
+    private final static Pattern SPLITER = Pattern.compile("//(.)\n(.*)");
+
     public static int calculate(String input) {
         if (input == null || input.isEmpty()) {
             return 0;
+        }
+        Matcher matcher = SPLITER.matcher(input);
+        if(matcher.find()){
+            String customDelimiter = matcher.group(1);
+            return Arrays.stream(matcher.group(2).split(customDelimiter))
+                .mapToInt(Integer::parseInt)
+                .sum();
         }
         return Arrays.stream(input.split("[,:]"))
             .mapToInt(Integer::parseInt)
