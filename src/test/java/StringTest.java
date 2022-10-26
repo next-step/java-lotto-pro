9e54af6daecbbcf7e0d1d78d451367a8a2450cb4
victor-jo <joasjn45@gmail.com>
2022-10-26 09:55:34 +0900

    style(string): 테스트 케이스별 테스트 의도 추가
    테스트 의도를 @DisplayName 으로 표현

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -5,6 +6,7 @@ import static org.assertj.core.api.Assertions.*;
 public class StringTest {
 
     @Test
+    @DisplayName("문자열을 ','로 분리한 배열로 가져온다.")
     void 스트링_콤마_나누기_적용() {
         String str = "1,2";
         String[] token = str.split(",");
@@ -14,6 +16,7 @@ public class StringTest {
     }
 
     @Test
+    @DisplayName("문자열을 ','로 분리한 배열로 가져올 때 ','뒤에 값이 없으면 배열에서 제외한다.")
     void 스트링_콤마_나누기_하나만_적용_예외() {
         String str = "1,";
         String[] token = str.split(",");
@@ -22,6 +25,7 @@ public class StringTest {
     }
 
     @Test
+    @DisplayName("문자열 범위 내에서 특정위치의 값 하나를 가져온다.")
     void 스트링_특정위치_값_가져오기() {
         String str = "abc";
         assertThat(str.charAt(0)).isSameAs('a');
@@ -30,6 +34,7 @@ public class StringTest {
     }
 
     @Test
+    @DisplayName("문자열 범위를 벗어나 특정위치의 값을 가져올 때 예외가 발생된다.")
     void 스트링_특정위치_값_가져오기_예외() {
         String str = "abc";
         assertThatThrownBy(() -> str.charAt(-1))
