bf645bce10711c1e73b6b519e77b57bc750e8652
anwjrrp33 <38122225+anwjrrp33@users.noreply.github.com>
2022-10-27 01:56:58 +0900

    Step2 - 학습 테스트 실습 PR 요청드립니다. (#672)
    * docs: 요구사항 정리
    
    * refactor: 1단계 피드백 반영
    
    * test: TestCase 소스 코드 반영
    
    * feat: 문자열 덧셈 계산기 기능 구현
    
    * docs: 요구사항 점검
    
    * refactor: 1단계 피드백 반영
    
    * refactor: String 클래스 1단계 피드백 반영
    
    * docs: 피드백 정리
    
    * refactor: 문자열 덧셈 계산기 테스트 피드백 반영
    
    * refactor: 계산에만 집중할 수 있도록 역할 분리
    
    * docs: 피드백 점검
    
    * refactor: 런타임 클래스 변경
    
    * refactor: 문자열 덧셈 계산기 분리
    
    * style: 코드 정리
    
    * test: 문자열 덧셈 계산기 테스트 코드

---
+++ src/main/java/step2/Calculation.java
@@ -0,0 +1,34 @@
+package step2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class Calculation {
+
+    private static final String SEPARATOR = ",|:";
+
+    private static final Pattern CUSTOM_SEPARATOR_PATTERN = Pattern.compile("\\/\\/(.)\n(.*)");
+
+    private static final int CUSTOM_SEPARATOR_GROUP = 1;
+
+    private static final int NUMBER_SEPARATOR_GROUP = 2;
+
+    public static SplitNumbers split(final String input) {
+        Matcher matcher = CUSTOM_SEPARATOR_PATTERN.matcher(input);
+
+        if(matcher.find()) {
+            String group = matcher.group(CUSTOM_SEPARATOR_GROUP);
+            return new SplitNumbers(splitNumberList(matcher.group(NUMBER_SEPARATOR_GROUP).split(group)));
+        }
+        return new SplitNumbers(splitNumberList(input.split(SEPARATOR)));
+    }
+
+    private static List<SplitNumber> splitNumberList(final String[] inputs) {
+        return Arrays.stream(inputs)
+            .map(splitNumber -> new SplitNumber(splitNumber))
+            .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/step2/StringAddCalculator.java
@@ -0,0 +1,26 @@
+package step2;
+
+import java.util.List;
+import java.util.Objects;
+
+public class StringAddCalculator {
+
+    private static final int ZERO = 0;
+
+    public static int splitAndSum(final String input) {
+        if(isNullOrEmpty(input)) {
+            return ZERO;
+        }
+        return sum(Calculation.split(input).getSplitNumbers());
+    }
+
+    private static int sum(final List<Integer> splitNumbers) {
+        return splitNumbers.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+
+    private static boolean isNullOrEmpty(final String input) {
+        return Objects.isNull(input) || input.isEmpty();
+    }
+}
