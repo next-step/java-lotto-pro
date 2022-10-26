063930a5138238920e287eb191904ee4e84250be
mansik <90mansin@gmail.com>
2022-10-27 01:19:06 +0900

    test: 구분자를 이용한 split 후 sum 검증 구현
    - 기본 구분자[쉼표(,), 콜론(:)]으로 문자열 split 후 sum 검증 구현
    - 커스텀 구분자[//와 \n 사이 위치 문자]으로 문자열 split 후 sum 검증 구현

--- src/test/java/study/step2/StringAddCalculatorTest.java
+++ src/test/java/study/step2/StringAddCalculatorTest.java
@@ -2,6 +2,7 @@ package study.step2;
 
 
 import domain.StringAddCalculator;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,8 +10,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringAddCalculatorTest {
 
-
-
+    private StringAddCalculator stringAddCalculator;
+    @BeforeEach
+    void setup(){
+        stringAddCalculator = new StringAddCalculator();
+    }
 
     @Test
     @DisplayName("쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 split")
@@ -18,7 +22,7 @@ public class StringAddCalculatorTest {
         String given = "1,2:3";
         String[] expectValue = {"1", "2", "3"};
 
-        String[] result = StringAddCalculator.split(given);
+        String[] result = stringAddCalculator.split(given);
 
         assertThat(result).isEqualTo(expectValue);
     }
@@ -29,7 +33,29 @@ public class StringAddCalculatorTest {
         String given = "//;\n1;2;3";
         String[] expectValue = {"1", "2", "3"};
 
-        String[] result = StringAddCalculator.split(given);
+        String[] result = stringAddCalculator.split(given);
+
+        assertThat(result).isEqualTo(expectValue);
+    }
+
+    @Test
+    @DisplayName("기본 구분자의 split과 sum을 검증")
+    public void 기본구분자_sum(){
+        String given = "1,2:3";
+        int expectValue = 6;
+
+        int result = stringAddCalculator.splitAndSum(given);
+
+        assertThat(result).isEqualTo(expectValue);
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자의 split과 sum을 검증")
+    public void 커스텀구분자_sum(){
+        String given = "//;\n1;2;3";
+        int expectValue = 6;
+
+        int result = stringAddCalculator.splitAndSum(given);
 
         assertThat(result).isEqualTo(expectValue);
     }
