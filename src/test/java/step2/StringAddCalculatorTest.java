f9638e6efb3b7696d25c85b50371fb3ef279e3bf
CHEESE <81912261+ychxexn@users.noreply.github.com>
2022-10-26 23:03:00 +0900

    test: 숫자 이외의 값 입력 시 예외 발생 테스트 케이스 추가 및 DisplayName 작성

--- src/test/java/step2/StringAddCalculatorTest.java
+++ src/test/java/step2/StringAddCalculatorTest.java
@@ -1,5 +1,6 @@
 package step2;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,6 +8,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringAddCalculatorTest {
     @Test
+    @DisplayName("null 또는 빈 문자열을 입력하면 0을 반환")
     public void splitAndSum_null_또는_빈문자() {
         int result = StringAddCalculator.splitAndSum(null);
         assertThat(result).isEqualTo(0);
@@ -16,32 +18,44 @@ public class StringAddCalculatorTest {
     }
 
     @Test
+    @DisplayName("숫자 하나를 문자열로 입력하면 해당 숫자를 반환")
     public void splitAndSum_숫자하나() throws Exception {
         int result = StringAddCalculator.splitAndSum("1");
         assertThat(result).isEqualTo(1);
     }
 
     @Test
+    @DisplayName("쉼표 구분자로 문자열을 입력하면 해당 숫자들의 합을 반환")
     public void splitAndSum_쉼표구분자() throws Exception {
         int result = StringAddCalculator.splitAndSum("1,2");
         assertThat(result).isEqualTo(3);
     }
 
     @Test
+    @DisplayName("쉼표 또는 콜론 구분자로 문자열을 입력하면 해당 숫자들의 합을 반환")
     public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
         int result = StringAddCalculator.splitAndSum("1,2:3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
+    @DisplayName("커스텀 구분자는 문자열 앞부분의 “//”와 “\n” 사이에 위치하는 문자를 커스텀 구분자로 사용")
     public void splitAndSum_custom_구분자() throws Exception {
         int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
+    @DisplayName("음수를 입력하면 RuntimeException 예외 발생")
     public void splitAndSum_negative() throws Exception {
         assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    @DisplayName("숫자 이외의 값을 입력하면 RuntimeException 예외 발생")
+    public void splitAndSum_containsChar() throws Exception {
+        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("a,b,1,2,3"))
                 .isInstanceOf(RuntimeException.class);
     }
 }
