996be939e45838ff0f911a8319aac3ceeea9cc8b
heonseopShin <shinsub6120@gmail.com>
2022-10-27 03:55:55 +0900

    feat : 문자열 비었을 때, 숫자만 들어올 때 계산 기능 구현

--- src/main/java/step2/StringAddCalculator.java
+++ src/main/java/step2/StringAddCalculator.java
@@ -3,6 +3,22 @@ package step2;
 public class StringAddCalculator {
 
     public static int splitAndSum(String text) {
+        if (text == null) return 0;
+        if (text.isEmpty()) return 0;
+        if (isNumber(text)) return convertNumber(text);
         return 0;
+    }
+
+    private static boolean isNumber(String text) {
+        try {
+            Integer.parseInt(text);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private static int convertNumber(String text) {
+        return Integer.parseInt(text);
     }
 }
