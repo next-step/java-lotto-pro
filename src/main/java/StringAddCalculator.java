1764f919907aacb06cf846f03e5e605221c6d66a
yomni <yearsheep@gmail.com>
2022-10-26 20:52:11 +0900

    feat: splitAndSum 숫자하나, 쉼표 또는 콜론 구분자로 합계 부분 구현 완료

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,14 +1,23 @@
 public class StringAddCalculator {
 
+    public static final String DELIMITER_REGEX = ",|:";
+
     public static int splitAndSum(String text) {
         if (isEmpty(text)) {
             return 0;
         }
-
-        return 1;
+        return sum(text.split(DELIMITER_REGEX));
     }
 
     private static boolean isEmpty(String text) {
         return text == null || text.isEmpty();
+    }
+
+    private static int sum(String[] splitText) {
+        int sum = 0;
+        for (String number : splitText) {
+            sum += Integer.parseInt(number);
+        }
+        return sum;
     }
 }
