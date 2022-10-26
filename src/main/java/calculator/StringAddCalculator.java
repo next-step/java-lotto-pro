073503961dd1c08b9ddbdf6868c09059b981e6ce
jimbaemon <jimbae@kakao.com>
2022-10-26 16:20:43 +0900

    feat : 쉼표로 구분된 값 합산 구현.

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,11 +1,15 @@
 package calculator;
 
 
+import java.util.Arrays;
+
 public class StringAddCalculator {
     public static int calculate(String input) {
         if (input == null || input.isEmpty()) {
             return 0;
         }
-        return Integer.parseInt(input);
+        return Arrays.stream(input.split(","))
+            .mapToInt(Integer::parseInt)
+            .sum();
     }
 }
