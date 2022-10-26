db51e9228be1d80d1aa3993ddb71ecabf750a75c
victor-jo <joasjn45@gmail.com>
2022-10-26 13:06:09 +0900

    test(set): 주어진 데이터에 대한 값을 검사하는 테스트케이스 추가
    Set 데이터셋에 포함되어야 하는 필수값 검사

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -1,5 +1,8 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -24,5 +27,11 @@ public class SetTest {
     void SET의_크기는_3이어야_한다() {
         assertThat(numbers).hasSize(3);
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest(name = "SET에 포함되는 값들이 정확한지 검사 - {0}")
+    @ValueSource(ints = {1, 2, 3})
+    void SET에_포함되는_값들을_검사(int value) {
+        assertThat(numbers).contains(value);
     }
 }
