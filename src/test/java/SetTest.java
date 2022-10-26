363d9a9a93e5b3e97e56de9fc77453724d7bf500
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-10-27 01:25:34 +0900

    1단계 - 학습 테스트 실습 리뷰 요청합니다 (#730)
    * docs(README.md) : String 클래스에 대한 학습 테스트 요구사항 정리
    
    * test(StringUtilsTest.java) : 1,2를 ,로 split 한다.
    
    * test(StringUtilsTest.java) : 1을 ,로 split 한다.
    
    * test(StringUtilsTest.java) : (1,2)의 ()를 제거한다.
    
    * test(StringTest.java) : 특정 위치의 문자를 가져온다.
    
    * test(StringTest.java) : 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생한다.
    
    * docs(README.md) : Set Collection에 대한 학습 테스트 요구사항 정리
    
    * test(SetTest.java) : Set의 크기를 확인한다.
    
    * test(SetTest.java) : Set의 값을 확인한다. - @ParameterizedTets를 통해 중복을 제거한다.
    
    * test(SetTest.java) : Set의 값을 확인한다. - 성공과 실패를 모두 검증하는 테스트를 구현한다.

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Set Collection에 대한 학습 테스트")
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("Set의 크기를 확인한다.")
+    @Test
+    void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @DisplayName("Set의 값을 확인한다. - ParameterizedTest를 중복 코드를 제거한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @DisplayName("Set의 값을 확인한다. - 성공과 실패를 모두 검증하는 테스트를 구현한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void contains(int input, boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
+    }
+}
---
+++ src/test/java/StringTest.java
@@ -0,0 +1,71 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@DisplayName("String 클래스에 대한 학습 테스트")
+class StringTest {
+
+    public static final String DELIMITER = ",";
+
+    @DisplayName("1,2를 ,로 split 하여 1,2로 분리한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2"})
+    void splitTwoWords(String text) {
+
+        String[] actual = text.split(DELIMITER);
+
+        assertAll(
+                () -> assertThat(actual).contains("1"),
+                () -> assertThat(actual).contains("2"),
+                () -> assertThat(actual).containsExactly("1", "2")
+        );
+    }
+
+    @DisplayName("1을 ,로 split 한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1"})
+    void splitOneWord(String text) {
+
+        String[] actual = text.split(DELIMITER);
+
+        assertAll(
+                () -> assertThat(actual).contains("1"),
+                () -> assertThat(actual).containsExactly("1")
+        );
+    }
+
+    @DisplayName("(1,2)의 ()를 제거하여  1,2를 반환한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"(1,2)"})
+    void substring(String text) {
+
+        String actual = text.substring(1, 5);
+
+        assertThat(actual).contains("1,2");
+    }
+
+    @DisplayName("특정 위치의 문자를 가져온다.")
+    @ParameterizedTest
+    @CsvSource({"abcdef", "1bevcs", "ubsdfeve"})
+    void charAt(String text) {
+
+        char actual = text.charAt(1);
+
+        assertThat(actual).isEqualTo('b');
+    }
+
+    @DisplayName("특정 위치의 문자를 가져올때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생한다.")
+    @ParameterizedTest
+    @CsvSource({"abcdef, 7", "1bevcs, 8", "ubsdfeve, 10"})
+    void StringIndexOutOfBoundsException(String text, int outOfIndex) {
+
+        assertThatThrownBy(() -> text.charAt(outOfIndex))
+                .isInstanceOf(IndexOutOfBoundsException.class);
+    }
+
+}
