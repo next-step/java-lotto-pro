cafe275bd39b8a0675ff7371a23d9482365696f5
namhyun.sung <namhyun.sung@yanolja.com>
2022-10-26 19:26:46 +0900

    test : Set의 contains() 메소드 테스트 코드 작성

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -1,11 +1,14 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -30,5 +33,13 @@ public class SetTest {
 
         //then
         assertThat(size).isEqualTo(expect);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("Set의 contains() 메소드를 활용해 값이 존재하는지 확인한다")
+    void set_contains_test(int input) {
+        //then
+        assertTrue(numbers.contains(input));
     }
 }
