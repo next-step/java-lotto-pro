22621d2a1d01271b78f4739174cb0aef6b079b53
namhyun.sung <namhyun.sung@yanolja.com>
2022-10-26 21:47:29 +0900

    test : 숫자 하나 입력 시 그대로 반환

--- src/test/java/step2/StringAddCalculatorTest.java
+++ src/test/java/step2/StringAddCalculatorTest.java
@@ -2,6 +2,7 @@ package step2;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
@@ -22,5 +23,12 @@ class StringAddCalculatorTest {
     void input_test(String inputText) {
         int result = stringAddCalculator.sum(inputText);
         assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("숫자 하나 입력 테스트를 해본다.")
+    void one_number_test() {
+        int result = stringAddCalculator.sum("2");
+        assertThat(result).isEqualTo(1);
     }
 }
