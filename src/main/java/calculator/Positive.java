adf5d2fe38da6231175a2078020c391a152274c7
jimbaemon <jimbae@kakao.com>
2022-10-26 17:03:30 +0900

    feat: 합산 구현.

---
+++ src/main/java/calculator/Positive.java
@@ -0,0 +1,32 @@
+package calculator;
+
+import java.util.Objects;
+
+public class Positive {
+    private final int positive;
+
+    public Positive(int positive) {
+        if(positive < 0){
+            throw new RuntimeException("음수는 사용이 불가능");
+        }
+        this.positive = positive;
+    }
+
+
+    public Positive sum(Positive otherPositive) {
+        return new Positive(this.positive + otherPositive.positive);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Positive positive1 = (Positive) o;
+        return positive == positive1.positive;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(positive);
+    }
+}
