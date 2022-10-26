811ce9388cb7954d5d10e98c78405b7f29e6241d
sawook <lna1003@naver.com>
2022-10-26 23:39:54 +0900

    feat: 정규식을 이용하여 구분자를 찾아낸다.
    - 정규식을 통한 커스텀 구분자를 찾아내는 기능
    - 구분자를 통해 숫자를 자르는 기능

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -3,15 +3,36 @@ import java.util.regex.Pattern;
 
 public class StringAddCalculator {
 
+    private static final String DIVIDER = ",|:";
+    private static final String AND_DIVIDER = "|";
     private static final String REGEX = "//(.)\n(.*)";
+    private static final Pattern PATTERN_COMPILE = Pattern.compile(REGEX);
+    private static final String MINUS_EXCEPTION_MESSAGE = "음수는 입력이 불가능해요";
     private static final int EMPTY_RESULT = 0;
+    private static final int MATCH_FIRST_GROUP = 1;
+    private static final int MATCH_SECOND_GROUP = 2;
+    private static final int VALID_CHECK_STANDARD_NUMBER = 0;
 
     public static int splitAndSum(String input) {
         if (isNullOrEmpty(input)) {
             return EMPTY_RESULT;
         }
+
+        calculateResult(input);
     }
 
+    private static int calculateResult(String input) {
+        Matcher matchResult = PATTERN_COMPILE.matcher(input);
+        String delimiter = DIVIDER;
+        String value = input;
+
+        if (matchResult.find()) {
+            delimiter = delimiter + AND_DIVIDER + matchResult.group(MATCH_FIRST_GROUP);
+            value = matchResult.group(MATCH_SECOND_GROUP);
+        }
+
+        String[] splitNumber = value.split(delimiter);
+    }
 
     private static boolean isNullOrEmpty(String value) {
         return value == null || value.isEmpty();
