763357da92874482ce4e7bf1118b11de3589d07b
victor-jo <joasjn45@gmail.com>
2022-10-26 09:06:52 +0900

    test(stringTest): 스트링 콤마 나누기 테스트 적용

---
+++ src/test/java/StringTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class StringTest {
+
+    @Test
+    void 스트링_콤마_나누기_적용() {
+        String str = "1,2";
+        String[] token = str.split(",");
+        assertThat(token).contains("1");
+        assertThat(token).contains("2");
+        assertThat(token).containsExactly("1", "2");
+    }
+}
