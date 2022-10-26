0172f2340a0e27f50075c5b3e999c91b68a917f4
sonyoon7 <46583453+sonyoon7@users.noreply.github.com>
2022-10-26 09:16:23 +0900

    2단계 - 문자열 덧셈 계산기 PR 리뷰 부탁드립니다.  (#659)
    * test: string split test
    
    * test: string substring test
    
    * test: string charAt() test
    
    * test: Collection Set size() test
    
    * test: ParameterizedTest
    
    * test: ParameterizedTest with expect value
    
    * refact: 스텝별 패키지 생성
    
    * test: 기능 요구사항별 테스트 코드 작성
    
    * feat: 문자열 덧셈 계산기 기능 구현
    
    * style: code style 정렬
    
    * style: 컨벤션 재적용
    
    * refactor: StringAddCalculator.splitAndSum 메소드 테스트
    
    * refactor: split 기능 통합, 지역변수 위치 변경

---
+++ src/test/java/step1/SetTest.java
@@ -0,0 +1,49 @@
+package step1;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void checkSetSize() {
+        Assertions.assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @Test
+    void contains() {
+        Assertions.assertThat(numbers.contains(1)).isTrue();
+        Assertions.assertThat(numbers.contains(2)).isTrue();
+        Assertions.assertThat(numbers.contains(3)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void refactoring_contains(int input) {
+        org.junit.jupiter.api.Assertions.assertTrue(numbers.contains(input));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"} , delimiter = ':')
+    void refactoring_contains_except(int input, boolean expected) {
+
+        org.junit.jupiter.api.Assertions.assertEquals(expected, numbers.contains(input));
+    }
+}
