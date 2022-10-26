89d6edb4c5f6276a4b2ae1c63f28e9f1ef4e966a
deveo-pyo <jungpyo9@gmail.com>
2022-10-27 03:00:35 +0900

    refactor: input값에 대한 유효성검사 로직 분리

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -3,34 +3,16 @@ package calculator;
 public class StringAddCalculator {
 
     private static int DEFAULT_VALUE = 0;
-    private static String ERROR_MESSAGE_INVALID_NUMBER = "[ERROR] 양수가 아닌 문자 입력 값";
-    private static String REGEX_DIGIT = "\\d+";
 
     public static int splitAndSum(String input) {
-        if(isEmpty(input)){
+        if(NumberValidator.isEmpty(input)){
             return DEFAULT_VALUE;
         }
 
         String[] stringFormatNumbers = StringSplitter.split(input);
-        validateNumbers(stringFormatNumbers);
+        NumberValidator.validateNumbers(stringFormatNumbers);
 
         return sumStringFormatNumbers(stringFormatNumbers);
-    }
-
-    private static boolean isEmpty(String input){
-        return input == null || input.isEmpty();
-    }
-
-    private static void validateNumbers(String[] stringFormatNumbers){
-        for(String str : stringFormatNumbers){
-            validateNumber(str);
-        }
-    }
-
-    private static void validateNumber(String stringFormatNumber){
-        if(!stringFormatNumber.matches(REGEX_DIGIT)){
-            throw new IllegalArgumentException(ERROR_MESSAGE_INVALID_NUMBER);
-        }
     }
 
     private static int sumStringFormatNumbers(String[] stringFormatNumbers){
