edbeeac63a931dc09ec2572009b4fa6b6f2bcb26
Oh Joonoh <jack.ohjoonoh@gmail.com>
2022-10-26 13:29:35 +0900

    feat(test): string과 set 테스트 구현 (#680)

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,45 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void size() {
+        int result = numbers.size();
+
+        assertThat(result).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "2", "3"})
+    void contains(int number) {
+        boolean result = numbers.contains(number);
+
+        assertThat(result).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void contains(int number, boolean expected) {
+        boolean result = numbers.contains(number);
+
+        assertThat(result).isEqualTo(expected);
+    }
+}
