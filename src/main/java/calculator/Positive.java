8750400fb40ea0d84166abd91d6dad506565be5d
jimbaemon <jimbae@kakao.com>
2022-10-27 00:56:33 +0900

    refactor: if 에 항상 괄호 붙이는 컨벤션.

--- src/main/java/calculator/Positive.java
+++ src/main/java/calculator/Positive.java
@@ -26,10 +26,12 @@ public class Positive {
 
 	@Override
 	public boolean equals(Object o) {
-		if (this == o)
+		if (this == o) {
 			return true;
-		if (o == null || getClass() != o.getClass())
+		}
+		if (o == null || getClass() != o.getClass()) {
 			return false;
+		}
 		Positive positive1 = (Positive)o;
 		return positive == positive1.positive;
 	}
