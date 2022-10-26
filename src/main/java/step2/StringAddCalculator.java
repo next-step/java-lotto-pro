7afa759ad7c42dc24767203ba40df31e3295dfa4
namhyun.sung <namhyun.sung@yanolja.com>
2022-10-26 22:02:24 +0900

    feat : 커스텀 구분자 사용 구현

--- src/main/java/step2/StringAddCalculator.java
+++ src/main/java/step2/StringAddCalculator.java
@@ -1,7 +1,12 @@
 package step2;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
     private static final String DEFAULT_DELIMITER = ",|:";
+    private static final String CUSTOM_DELIMITER = "//(.)\n(.*)";
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile(CUSTOM_DELIMITER);
 
     public int sum(String inputText) {
         if (validateInputText(inputText)) {
@@ -15,11 +20,20 @@ public class StringAddCalculator {
     }
 
     private int splitAndSum(String inputText) {
-        String[] stringNumbers = inputText.split(DEFAULT_DELIMITER);
+        String[] stringNumbers = splitText(inputText);
         int result = 0;
         for (String stringNumber : stringNumbers) {
             result += Integer.parseInt(stringNumber);
         }
         return result;
+    }
+
+    private String[] splitText(String inputText) {
+        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(inputText);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            return matcher.group(2).split(customDelimiter);
+        }
+        return inputText.split(DEFAULT_DELIMITER);
     }
 }
