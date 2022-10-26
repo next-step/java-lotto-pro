e6cb1aacd866c7a74117884f63ac2406a5195249
sixthou <justyuk@naver.com>
2022-10-26 20:11:44 +0900

    test : Number 테스트 작성

---
+++ src/test/java/lotto/domain/NumberTest.java
@@ -0,0 +1,31 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class NumberTest {
+
+    @DisplayName("1에서 45 사이의 숫자만 가능하다.")
+    @Test
+    void possible_number() {
+
+    }
+
+    @DisplayName("1에서 45 사이의 숫자의 숫자가 아니면 IllegalArgumentException 이 발생한다.")
+    @Test
+    void impossible_number() {
+
+    }
+
+    @DisplayName("같은 숫자이면 동일하다.")
+    @Test
+    void equal_number() {
+
+    }
+
+    @DisplayName("다른 숫자이면 동일하지 않다.")
+    @Test
+    void name() {
+
+    }
+}
