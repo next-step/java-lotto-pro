b03415239ea3672c49f87c8dd4677889b4945483
namhyun.sung <namhyun.sung@yanolja.com>
2022-10-26 22:13:08 +0900

    feat : 음수인 경우 예외 발생 구현

--- src/main/java/step2/StringAddCalculator.java
+++ src/main/java/step2/StringAddCalculator.java
@@ -7,6 +7,7 @@ public class StringAddCalculator {
     private static final String DEFAULT_DELIMITER = ",|:";
     private static final String CUSTOM_DELIMITER = "//(.)\n(.*)";
     private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile(CUSTOM_DELIMITER);
+    private static final String MINUS_NUMBER_EXCEPTION_MESSAGE = "음수는 입력할 수 없습니다.";
 
     public int sum(String inputText) {
         if (validateInputText(inputText)) {
@@ -23,9 +24,15 @@ public class StringAddCalculator {
         String[] stringNumbers = splitText(inputText);
         int result = 0;
         for (String stringNumber : stringNumbers) {
-            result += Integer.parseInt(stringNumber);
+            result += parseInt(stringNumber);
         }
         return result;
+    }
+
+    private int parseInt(String stringNumber) {
+        int inputNumber = Integer.parseInt(stringNumber);
+        validationMinusNumber(inputNumber);
+        return inputNumber;
     }
 
     private String[] splitText(String inputText) {
@@ -35,5 +42,11 @@ public class StringAddCalculator {
             return matcher.group(2).split(customDelimiter);
         }
         return inputText.split(DEFAULT_DELIMITER);
+    }
+
+    private void validationMinusNumber(int number) {
+        if (number < 0) {
+            throw new RuntimeException(MINUS_NUMBER_EXCEPTION_MESSAGE);
+        }
     }
 }
