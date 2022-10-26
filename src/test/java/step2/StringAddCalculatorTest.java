de4c7b07f98e043fee8ecc7351f414651e64e536
chunhodong <yhms4432@naver.com>
2022-10-26 14:58:18 +0900

    refactor: 테스트메소드앞에 test_that추가
    - 테스트 메소드가 하는 일을 명확하게 하기위해 문구추가

--- src/test/java/step2/StringAddCalculatorTest.java
+++ src/test/java/step2/StringAddCalculatorTest.java
@@ -13,14 +13,14 @@ public class StringAddCalculatorTest {
 
     @Test
     @DisplayName("null값을 입력하면 0을 반환")
-    public void returns_0_if_null_value_input() {
+    public void test_that_returns_0_if_null_value_input() {
         int result = StringAddCalculator.splitAndSum(null);
         assertThat(result).isEqualTo(0);
     }
 
     @Test
     @DisplayName("빈값을 입력하면 0을 반환")
-    public void returns_0_if_empty_value_input() {
+    public void test_that_returns_0_if_empty_value_input() {
         int result = StringAddCalculator.splitAndSum("");
         assertThat(result).isEqualTo(0);
     }
@@ -28,7 +28,7 @@ public class StringAddCalculatorTest {
     @ParameterizedTest
     @CsvSource(value = {"12:12", "13:13", "3:3"}, delimiter = ':')
     @DisplayName("숫자만 입력할 경우 입력한 숫자 반환")
-    public void returns_number_if_only_number_entered(String text,int value){
+    public void test_that_returns_number_if_only_number_entered(String text,int value){
         int result = StringAddCalculator.splitAndSum(text);
         assertThat(result).isEqualTo(value);
     }
@@ -36,7 +36,7 @@ public class StringAddCalculatorTest {
     @ParameterizedTest
     @CsvSource(value = {"1,2:3", "6,4,3:13", "7,2,9:18", "0,5,9:14"}, delimiter = ':')
     @DisplayName("숫자를 컴마(,) 구분자로 입력할 경우 숫자의 합을 반환")
-    public void returns_sum_if_number_entered_with_comma(String text,int sum) {
+    public void test_that_returns_sum_if_number_entered_with_comma(String text,int sum) {
         int result = StringAddCalculator.splitAndSum(text);
         assertThat(result).isEqualTo(sum);
     }
@@ -44,28 +44,28 @@ public class StringAddCalculatorTest {
     @ParameterizedTest
     @CsvSource(value = {"1:2=3", "6:4,3=13", "7:2:9=18", "0,5:9=14"}, delimiter = '=')
     @DisplayName("숫자를 컴마(,)또는콜론(:) 구분자로 입력할 경우 숫자의 합을 반환")
-    public void returns_sum_if_number_entered_with_comma_or_delimiter(String text, int sum) {
+    public void test_that_returns_sum_if_number_entered_with_comma_or_delimiter(String text, int sum) {
         int result = StringAddCalculator.splitAndSum(text);
         assertThat(result).isEqualTo(sum);
     }
 
     @Test
     @DisplayName("커스텀 구분자를 지정할 경우 구분된 숫자의 합을 반환")
-    public void returns_sum_if_number_entered_with_custom_delimite1r()  {
+    public void test_that_returns_sum_if_number_entered_with_custom_delimite1r()  {
         int result = StringAddCalculator.splitAndSum("//@\n1@2@3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
     @DisplayName("구분된 숫자중 음수가 포함된 경우 RuntimeException 예외를 발생")
-    public void throw_exception_if_netative_number_included() {
+    public void test_that_throw_exception_if_netative_number_included() {
         assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
                 .isInstanceOf(RuntimeException.class);
     }
 
     @Test
     @DisplayName("구분된 문자열에 숫자 이외에 값이 포함된 경우 RuntimeException 예외를 발생")
-    public void throw_exception_if_text_contains_value_other_than_number() {
+    public void test_that_throw_exception_if_text_contains_value_other_than_number() {
         assertThatThrownBy(() -> StringAddCalculator.splitAndSum("31,a,3"))
                 .isInstanceOf(RuntimeException.class);
     }
