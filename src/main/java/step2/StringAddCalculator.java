dba7a716a63ce1bf325e74dd4e972625f9f1b761
choiyongseok90 <badaelephant>
2022-10-26 12:22:05 +0900

    refactor: 중복되는 양수 검증이슈 수정, 패턴 스태틱 정의 수정 수정내용 1. 상수의 경우 빠진 final을 추가하여 변경 불가능 하도록, 최대한 상수표현 2. Pattern을 미리 static으로 생성하고 호출될때 사용하도록 수정 3. 기존에는 문자열 체크와 음수 체크를 나눠서 했는데, 요구조건인 양수만 받도록 수정

--- src/main/java/step2/StringAddCalculator.java
+++ src/main/java/step2/StringAddCalculator.java
@@ -7,33 +7,34 @@ import java.util.regex.Pattern;
 
 public class StringAddCalculator {
 
-    private static String DELIMITER = ",|:";
-    private static String NOT_NUMBER = "숫자가 아닙니다.";
-    private static String MINUS_NUMBER = "음수입니다.";
+    private static final int DEFAULT_EMPTY_RETURN = 0;
+    private static final String DEFAULT_DELIMITER_REGEX = ",|:";
+    private static final String CUSTOM_DELIMITER_REGEX  = "//(.)\n(.*)";
+    private static final String POSITIVE_NUMBER_REGEX = "^[0-9]+$";
+    private static final String NOT_POSITIVE_NUMBER = "0~9 사이의 숫자가 아닙니다.";
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile(CUSTOM_DELIMITER_REGEX);
+
+
 
     public static int splitAndSum(String text) {
         if (isStringNullOrEmpty(text)) {
-            return 0;
-        }
-        if (isNumber(text)) {
-            return Integer.parseInt(text);
+            return DEFAULT_EMPTY_RETURN;
         }
         String[] splitNumbers = splitNumber(text);
+        validateNumbersPositive(splitNumbers);
         return addAllNumbers(splitNumbers);
+    }
+
+    private static void validateNumbersPositive(String[] splitNumbers) {
+        boolean isValidate = Arrays.stream(splitNumbers).allMatch(number->number.matches(POSITIVE_NUMBER_REGEX));
+        if (!isValidate){
+            throw new RuntimeException(NOT_POSITIVE_NUMBER);
+        }
     }
 
     private static int addAllNumbers(String[] splitNumbers) {
         return Arrays.stream(splitNumbers)
-                .mapToInt(s -> changeToInt(s)).sum();
-    }
-
-    private static boolean isNumber(String text) {
-        try {
-            Integer.parseInt(text);
-        } catch (NumberFormatException e) {
-            return false;
-        }
-        return true;
+                .mapToInt(Integer::parseInt).sum();
     }
 
     private static boolean isStringNullOrEmpty(String text) {
@@ -41,7 +42,7 @@ public class StringAddCalculator {
     }
 
     private static String[] splitNumber(String text) {
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        Matcher m = CUSTOM_DELIMITER_PATTERN.matcher(text);
         if (m.find()) {
             String customDelimiter = m.group(1);
             return m.group(2).split(customDelimiter);
@@ -50,17 +51,7 @@ public class StringAddCalculator {
     }
 
     private static String[] splitByCommaAndColon(String text) {
-        return text.split(DELIMITER);
+        return text.split(DEFAULT_DELIMITER_REGEX);
     }
 
-    private static int changeToInt(String text) {
-        if (!isNumber(text)) {
-            throw new RuntimeException(NOT_NUMBER);
-        }
-        int number = Integer.parseInt(text);
-        if (number < 0) {
-            throw new RuntimeException(MINUS_NUMBER);
-        }
-        return number;
-    }
 }
