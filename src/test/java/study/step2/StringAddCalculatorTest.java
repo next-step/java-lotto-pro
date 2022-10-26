b2946e338dfa42854a47aa2d4c2b1d9b67868aea
mansik <90mansin@gmail.com>
2022-10-27 02:21:39 +0900

    test: 입력받은 받은 값에 대한 숫자 유효성 검증 구현
    - 음수 or 숫자 이외의 경우 RuntimeException 발생 검증 구현

--- src/test/java/study/step2/StringAddCalculatorTest.java
+++ src/test/java/study/step2/StringAddCalculatorTest.java
@@ -2,14 +2,15 @@ package study.step2;
 
 
 import domain.StringAddCalculator;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringAddCalculatorTest {
-
     private StringAddCalculator stringAddCalculator;
     @BeforeEach
     void setup(){
@@ -58,6 +59,44 @@ public class StringAddCalculatorTest {
         int result = stringAddCalculator.splitAndSum(given);
 
         assertThat(result).isEqualTo(expectValue);
+    }
+
+    @Test
+    @DisplayName("입력받은 문자열이 null 또는 공백이면 0을 리턴 검증")
+    public void 입력문자열_null_또는_빈문자(){
+        String nullCheckParameter = null;
+        String emptyCheckParameter = "";
+        int expectValue = 0;
+
+        Assertions.assertAll(
+                () -> assertThat(stringAddCalculator.splitAndSum(nullCheckParameter)).isEqualTo(expectValue)
+                ,() -> assertThat(stringAddCalculator.splitAndSum(emptyCheckParameter)).isEqualTo(expectValue)
+        );
+    }
+
+    @Test
+    @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자 반환")
+    public void 입력문자열_숫자하나(){
+        String given = "3";
+        int expectValue = 3;
+
+        int result = stringAddCalculator.splitAndSum(given);
+
+        assertThat(result).isEqualTo(expectValue);
+    }
+
+    @Test
+    public void 입력문자열_음수(){
+        assertThatThrownBy(() -> stringAddCalculator.splitAndSum("-1,2,3"))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("입력받은 숫자 값이 음수 입니다");
+    }
+
+    @Test
+    public void 입력문자열_숫자_이외의값(){
+        assertThatThrownBy(() -> stringAddCalculator.splitAndSum("a,2,3"))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("숫자만 입력 가능 합니다.");
     }
 
 }
