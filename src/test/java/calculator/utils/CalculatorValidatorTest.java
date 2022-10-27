1b7dd0b58ea3aa4b7e4e7346f95c4e6020451057
dongha-byun <byunsw4@naver.com>
2022-10-27 10:55:02 +0900

    Test: 테스트 코드 리펙터링

--- src/test/java/calculator/utils/CalculatorValidatorTest.java
+++ src/test/java/calculator/utils/CalculatorValidatorTest.java
@@ -6,22 +6,26 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorValidatorTest {
 
     @Test
     @DisplayName("빈 문자열인지 확인한다.")
     void checkInputIsEmpty(){
-        assertThat(CalculatorValidator.isEmpty("")).isTrue();
-        assertThat(CalculatorValidator.isEmpty("1")).isFalse();
-        assertThat(CalculatorValidator.isEmpty("1;2;3")).isFalse();
+        assertAll(
+                () -> assertTrue(CalculatorValidator.isEmpty(""), "빈 문자열 검증 실패"),
+                () -> assertFalse(CalculatorValidator.isEmpty("1"), "빈 문자열 아닌데 검증 실패")
+        );
     }
 
     @Test
     @DisplayName("null 문자열인지 확인한다.")
     void checkInputIsNull(){
-        assertThat(CalculatorValidator.isNull(null)).isTrue();
-        assertThat(CalculatorValidator.isNull("")).isFalse();
+        assertAll(
+                () -> assertTrue(CalculatorValidator.isNull(null), "null 문자열 검증 실패"),
+                () -> assertFalse(CalculatorValidator.isNull(""), "빈 문자열은 null 이 아닌데 null 로 검증함")
+        );
     }
 
     @ParameterizedTest
--- src/test/java/calculator/utils/StringCalculateParserTest.java
+++ src/test/java/calculator/utils/StringCalculateParserTest.java
@@ -1,26 +1,32 @@
 package calculator.utils;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class StringCalculateParserTest {
 
     @Test
     @DisplayName("쉼표(,) 로 계산식을 파싱한다.")
     void commaParsingTest(){
-        assertThat(StringCalculateParser.parseStringToArray("1,2")).containsExactly("1","2");
-        assertThat(StringCalculateParser.parseStringToArray("1,")).containsExactly("1");
-        assertThat(StringCalculateParser.parseStringToArray("1")).containsExactly("1");
+        assertAll(
+                () -> assertThat(StringCalculateParser.parseStringToArray("1,2")).containsExactly("1","2"),
+                () -> assertThat(StringCalculateParser.parseStringToArray("1,")).containsExactly("1"),
+                () -> assertThat(StringCalculateParser.parseStringToArray("1")).containsExactly("1")
+        );
     }
 
     @Test
     @DisplayName("콜론(:) 으로 계산식을 파싱한다.")
     void colonParsingTest(){
-        assertThat(StringCalculateParser.parseStringToArray("1:2")).containsExactly("1","2");
-        assertThat(StringCalculateParser.parseStringToArray("1:")).containsExactly("1");
-        assertThat(StringCalculateParser.parseStringToArray("1")).containsExactly("1");
+        assertAll(
+                () -> assertThat(StringCalculateParser.parseStringToArray("1:2")).containsExactly("1","2"),
+                () -> assertThat(StringCalculateParser.parseStringToArray("1:")).containsExactly("1"),
+                () -> assertThat(StringCalculateParser.parseStringToArray("1")).containsExactly("1")
+        );
     }
 
     @Test
