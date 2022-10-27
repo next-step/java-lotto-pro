1f38f6036280da598a0f0a134c18dd6f92376a20
noelleseo <noelleseo@gmail.com>
2022-10-28 01:06:30 +0900

    feat: result 클래스 구현

--- src/main/java/calculator/domain/Result.java
+++ src/main/java/calculator/domain/Result.java
@@ -12,6 +12,26 @@ public class Result {
     public int sum() {
         int result = Constants.NULL_VALUE_INT;
         
+        if(numberStrArr == null) throw new RuntimeException(Constants.ERR_VALUE_NOT_VALID);
+        
+        for(String e : this.numberStrArr) {
+            result += validateNumber(e);
+        }
+        
         return result;
+    }
+    
+    private int validateNumber(String numstr){
+        int num = Constants.NULL_VALUE_INT;
+        
+        try {
+            num = Integer.parseInt(numstr);
+        }catch(NumberFormatException e) {
+            throw new RuntimeException(Constants.ERR_VALUE_NOT_VALID);
+        }
+        
+        if(num < 0) throw new RuntimeException(Constants.ERR_NEGATIVE_VALUE);
+        
+        return num;
     }
 }
