e09e9b0e7303f0d8ede3539802784d3277680f61
Dongmin Nam <dmut7691@gmail.com>
2022-10-27 12:03:24 +0900

    🚀 1단계 - 학습 테스트 실습 (#747)
    * feat(study): String 클래스 학습을 위한 테스트 구현
    
    * feat(study): Set collection 학습을 위한 테스트 구현

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,44 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    // Test Case
+
+    @DisplayName("Set의 크기는 추가한 요소에서 중복을 제거한 개수를 반환한다.")
+    @Test
+    void size() {
+        final int actual = numbers.size();
+
+        assertThat(actual).isEqualTo(3);
+    }
+
+    @ParameterizedTest(name = "주어진 Set에 [{0}]을 요소로 포함하는가 = {1}.")
+    @CsvSource({
+            "1, true",
+            "2, true",
+            "3, true",
+            "4, false",
+            "5, false"
+    })
+    void contains(int element, boolean expectedContains) {
+        assertThat(numbers.contains(element)).isEqualTo(expectedContains);
+    }
+}
