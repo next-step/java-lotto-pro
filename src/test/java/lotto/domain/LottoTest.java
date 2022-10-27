686a380e5b08f295bc18a7b84b8fd4e693c4c776
sixthou <justyuk@naver.com>
2022-10-27 15:12:24 +0900

    test : Lotto 기본 테스트 케이스 작성

---
+++ src/test/java/lotto/domain/LottoTest.java
@@ -0,0 +1,13 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoTest {
+
+    @DisplayName("서로 다른 로또의 일치 갯수를 구할 수 있다.")
+    @Test
+    void match_count() {
+
+    }
+}
