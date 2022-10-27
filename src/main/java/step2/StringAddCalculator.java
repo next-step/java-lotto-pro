9250923813867d5c7c55ff0b0416aa74203b1bf9
CHEESE <81912261+ychxexn@users.noreply.github.com>
2022-10-27 20:25:23 +0900

    refactor: TokenValidator 클래스 분리

--- src/main/java/step2/StringAddCalculator.java
+++ src/main/java/step2/StringAddCalculator.java
@@ -7,34 +7,12 @@ public class StringAddCalculator {
         if(text == null || text.isEmpty()) {
             return 0;
         }
-        if(isNumberOnly(text)) {
+        if(TokenValidator.isNumberOnly(text)) {
             return Integer.parseInt(text);
         }
         String[] tokens = TokenParser.split(text);
-        validateTokens(tokens);
+        TokenValidator.validateTokens(tokens);
         return getTokensSum(tokens);
-    }
-
-    private static boolean isNumberOnly(String text) {
-        try {
-            Integer.parseInt(text);
-        } catch (NumberFormatException e) {
-            return false;
-        }
-        return true;
-    }
-
-    private static void validateTokens(String[] tokens) throws RuntimeException {
-        long charCount = Arrays.stream(tokens)
-                .filter(token -> !isNumberOnly(token))
-                .count();
-        long negativeCount = Arrays.stream(tokens)
-                .mapToInt(Integer::parseInt)
-                .filter(token -> token < 0)
-                .count();
-        if(charCount > 0 || negativeCount > 0) {
-            throw new RuntimeException("숫자 이외의 값 또는 음수는 지원하지 않습니다.");
-        }
     }
 
     private static int getTokensSum(String[] tokens) {
