428e5ab0ea821f40f5715a5438dd2b3933a8427e
OptimistLabyrinth <keesung0705@gmail.com>
2022-10-28 00:10:46 +0900

    feat: GREEN - customDelimiterExistsInBetween 메서드 테스트 통과는 했지만 너무 복잡해서 분리 필요
    이제는 GREEN 상태
    그러나 customDelimiterExistsInBetween 메서드 내부 로직이 너무 복잡해서
    오류 발생할 여지가 너무 많다
    메서드를 분리해야 한다

--- src/main/java/string/calculator/DelimiterFinder.java
+++ src/main/java/string/calculator/DelimiterFinder.java
@@ -10,4 +10,21 @@ public class DelimiterFinder {
     public boolean ifIncludeBothSlashAndNewline() {
         return input.contains("//") && input.contains("\\n");
     }
+
+    public boolean customDelimiterExistsInBetween() {
+        final int indexOfDoubleSlash = input.indexOf("//");
+        final int indexOfNewline = input.indexOf("\\n");
+        if (indexOfDoubleSlash + 2 == indexOfNewline) {
+            return false;
+        }
+        final String customDelimiter = input.substring(indexOfDoubleSlash + 2, indexOfNewline);
+        if (1 < customDelimiter.length()) {
+            return false;
+        }
+        final char charCustomDelimiter = customDelimiter.charAt(0);
+        if (Character.isDigit(charCustomDelimiter)) {
+            return false;
+        }
+        return true;
+    }
 }
