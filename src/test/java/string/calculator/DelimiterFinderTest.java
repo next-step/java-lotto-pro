8de013005680accf0628fd3e4f3cd57b718e8fd9
OptimistLabyrinth <keesung0705@gmail.com>
2022-10-27 23:47:55 +0900

    chore: 오타 수정

--- src/test/java/string/calculator/DelimiterFinderTest.java
+++ src/test/java/string/calculator/DelimiterFinderTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class DelimiterFinderTest {
     @ParameterizedTest
     @ValueSource(strings = {"//\\n", "//~\\n", "//.,\\n"})
-    @DisplayName("입력 문자열에 // 와 \\n 둘 다 포함하는 경우 ifIncludeBothSlashAndNewline 메서드의 결과값은 '참' 이 된다")
+    @DisplayName("입력 문자열이 // 와 \\n 둘 다 포함하는 경우 ifIncludeBothSlashAndNewline 메서드의 결과값은 '참' 이 된다")
     void trueIfIncludeBothDoubleSlashAndNewline(String input) {
         final DelimiterFinder delimiterFinder = new DelimiterFinder(input);
         final boolean checkResult = delimiterFinder.ifIncludeBothSlashAndNewline();
@@ -18,7 +18,7 @@ public class DelimiterFinderTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"", "//", "\\n"})
-    @DisplayName("입력 문자열에 // 와 \\n 둘 중에 하나라도 경우 ifIncludeBothSlashAndNewline 메서드의 결과값은 '거짓' 이 된다")
+    @DisplayName("입력 문자열에 // 와 \\n 둘 중에 하나라도 없는 경우 ifIncludeBothSlashAndNewline 메서드의 결과값은 '거짓' 이 된다")
     void falseIfNeitherIncludeDoubleSlashNorNewline(String input) {
         final DelimiterFinder delimiterFinder = new DelimiterFinder(input);
         final boolean checkResult = delimiterFinder.ifIncludeBothSlashAndNewline();
