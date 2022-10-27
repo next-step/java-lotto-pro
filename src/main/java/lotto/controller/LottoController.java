fc9f9c4c133bea98c30f089f3dbf18214a6e5edf
sjk5766 <wjdrnr6662@gmail.com>
2022-10-27 16:25:54 +0900

    feat: 구입 금액을 입력받는다.
    프로그램 구조 및 금액을 입력받는 기능 개발
    - 메인 파일(Application) 추가
    - Controller 추가
    - UI를 담당하는 InputView, ResultView 추가

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,9 @@
+package lotto.controller;
+
+import lotto.view.InputView;
+
+public class LottoController {
+    public void run() {
+        int price = InputView.getMoney();
+    }
+}
---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,22 @@
+package lotto.view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static final String INVALID_NUMBER_MESSAGE = "입력값이 숫자가 아닙니다.";
+
+    public static int getMoney() {
+        ResultView.printGetMoney();
+
+        try {
+            return Integer.parseInt(read());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INVALID_NUMBER_MESSAGE, e);
+        }
+    }
+
+    private static String read() {
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
+    }
+}
