505735dc0b97d2d89cda6ff29f368e03811f6a30
gunkim <gunkim.dev@gmail.com>
2022-10-28 01:58:11 +0900

    feat: Calculator를 구현했다.

---
+++ src/main/java/step2/Calculator.java
@@ -0,0 +1,47 @@
+package step2;
+
+import static java.util.stream.Collectors.toList;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class Calculator {
+
+    private static final Pattern STRING_REGEX = Pattern.compile("((//)\\D(\\\\n))|[:,]");
+    private static final Pattern NUMBER_REGEX = Pattern.compile("[1-9]");
+
+    public Number sum(String input) {
+        return sum(parseNumber(split(input)));
+    }
+
+    private Number sum(List<Number> numbers) {
+        return numbers.stream()
+                .reduce(new Number(0), Number::sum);
+    }
+
+    private List<String> split(String input) {
+        return Arrays.stream(STRING_REGEX.split(input))
+                .map(String::trim)
+                .collect(toList());
+    }
+
+    private List<Number> parseNumber(List<String> numbers) {
+        validation(numbers);
+
+        return numbers.stream()
+                .map(Integer::new)
+                .map(Number::new)
+                .collect(toList());
+    }
+
+    private void validation(List<String> nums) {
+        long count = nums.stream()
+                .filter(num -> !NUMBER_REGEX.matcher(num).matches())
+                .count();
+
+        if (count > 0) {
+            throw new RuntimeException("숫자가 아닌 값이 포함되어 있습니다.");
+        }
+    }
+}
