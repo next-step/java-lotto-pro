8e123f1e92e62217ef882cd3db7c830375598100
OptimistLabyrinth <keesung0705@gmail.com>
2022-10-27 23:45:58 +0900

    feat: GREEN-REFACTOR - 메서드 ifIncludeBothSlashAndNewline 구현
    이제는 GREEN 상태
    이중 슬래시(`//`)와 뉴라인(`\n`) 을 모두 포함하는 경우 true 를 반환한다

--- src/main/java/string/calculator/DelimiterFinder.java
+++ src/main/java/string/calculator/DelimiterFinder.java
@@ -1,4 +1,13 @@
 package string.calculator;
 
 public class DelimiterFinder {
+    private final String input;
+
+    public DelimiterFinder(String input) {
+        this.input = input;
+    }
+
+    public boolean ifIncludeBothSlashAndNewline() {
+        return input.contains("//") && input.contains("\\n");
+    }
 }
