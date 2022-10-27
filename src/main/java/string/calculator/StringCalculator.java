6f49497bcc9ac5a34ed3ad52ec36bcedf965fea6
OptimistLabyrinth <keesung0705@gmail.com>
2022-10-27 22:56:55 +0900

    feat: GREEN - zeroIfNullOrEmpty 테스트 통과하게 만들기
    StringCalculator 클래스에 생성자, 메서드를 적절하게 추가해서
    테스트코드가 통과하도록 만든다

--- src/main/java/string/calculator/StringCalculator.java
+++ src/main/java/string/calculator/StringCalculator.java
@@ -1,4 +1,13 @@
 package string.calculator;
 
 public class StringCalculator {
+    private final String input;
+
+    public StringCalculator(String input) {
+        this.input = input;
+    }
+
+    public int calculate() {
+        return 0;
+    }
 }
