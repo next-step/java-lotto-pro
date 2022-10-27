52cb20fa6f8e39e8a4f9f67b6ca7a2962965ae38
sixthou <justyuk@naver.com>
2022-10-27 20:54:25 +0900

    test : LottoResultStat 기본 테스트 작성

---
+++ src/test/java/lotto/domain/LottoResultStatTest.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoResultStatTest {
+
+    @DisplayName("순위별로 당첨 수를 확인 할 수 있다.")
+    @Test
+    void result_by_rank() {
+
+    }
+
+    @DisplayName("전체 수익률을 알 수 있다.")
+    @Test
+    void name() {
+
+    }
+
+}
