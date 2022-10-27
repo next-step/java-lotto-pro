7e5bcd825a46a6648048421c43f9b97276509912
지영진 <youngjin.jee@nhn.com>
2022-10-27 09:17:57 +0900

    refactor: 상수 추가
    * custom 인지 아닌지를 판단하는 구분자 추가

--- src/main/java/study/StringInputParser.java
+++ src/main/java/study/StringInputParser.java
@@ -6,9 +6,10 @@ import java.util.regex.Pattern;
 public class StringInputParser {
     public static final String DEFAULT_SPLIT_DELIMITER_REGEX = ",|:";
     public static final String CUSTOM_SPLIT_DELIMITER_REGEX = "//(.)\n(.*)";
+    public static final String CUSTOM_DELIMITER = "//";
 
     public static String[] toStringArray(String input) {
-        if (input.contains("//")) {
+        if (input.contains(CUSTOM_DELIMITER)) {
             Matcher m = Pattern.compile(CUSTOM_SPLIT_DELIMITER_REGEX).matcher(input);
             if (m.find()) {
                 String customDelimiter = m.group(1);
