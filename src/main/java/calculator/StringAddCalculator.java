21f5740dbd7cd2fda790c303fd9df21218897665
ggugi <wjdrnr6662@gmail.com>
2022-10-27 11:37:26 +0900

    Step2: 문자열 덧셈 계산기 (#727)
    * refactor: 1 단계 피드백 반영
    
    * docs(README.md): 요구사항 및 기능 구현 목록 작성
    
    * feat: 요구사항 1. 빈 문자열 또는 null이 입력될 경우 0을 반환한다
    
    * feat: 요구사항 2. 숫자 하나를 입력하면 해당 숫자를 반환한다
    
    * feat: 요구사항 3. 숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.
    
    * feat: 요구사항 4. 구분자를 컴마(,) 이외에 콜론(:)을 사용할 수 있다
    
    * feat: 요구사항 5. "//"와 "\n" 문자 사이에 커스텀 구분자를 지정할 수 있다
    
    * feat: 요구사항 6. 음수를 전달할 경우 RuntimeException 예외가 발생해야 한다
    
    * refactor: 예외 메시지와 패턴을 상수로 선언
    
    * test: 테스트 코드 추가 및 수정
    
    주어진 TestCase 추가 및 수정
    - DisplayName 어노테이션 추가
    - 두 개 이상을 검증하는 테스트 분리
    - ParameterizedTest 추가

---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,66 @@
+package calculator;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class StringAddCalculator {
+    private static final int DEFAULT_VALUE = 0;
+    private static final int MINIMUM = 0;
+    private static final String DELIMITER = ",|:";
+    private static final String SHOULD_POSITIVE_MESSAGE = "입력값은 양수여야 합니다";
+    private static final Pattern CUSTOM_PATTERN = Pattern.compile("//(.)\n(.*)");
+
+    public static int splitAndSum(String input) {
+        if (isEmpty(input)) {
+            return DEFAULT_VALUE;
+        }
+
+        return sum(parse(split(input)));
+    }
+
+    private static boolean isEmpty(String input) {
+        if (input == null || input.isEmpty()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private static String[] split(String input) {
+        Matcher m = CUSTOM_PATTERN.matcher(input);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            return m.group(2).split(customDelimiter);
+        }
+
+        return input.split(DELIMITER);
+    }
+
+    private static List<Integer> parse(String[] inputs) {
+        try {
+            return Arrays.stream(inputs)
+                    .map(StringAddCalculator::toPositive)
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    private static int toPositive(String input) {
+        int value = Integer.parseInt(input);
+
+        if (value >= MINIMUM) {
+            return value;
+        }
+
+        throw new IllegalArgumentException(SHOULD_POSITIVE_MESSAGE);
+    }
+
+    private static int sum(List<Integer> numbers) {
+        return numbers.stream()
+                .reduce(DEFAULT_VALUE, (a, b) -> a + b);
+    }
+}
