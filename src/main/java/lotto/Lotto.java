f157f5074e82d03611b604271707633123ed1a5e
jimbaemon <jimbae@kakao.com>
2022-10-28 01:50:06 +0900

    feat: 로또의 번호는 중복될 수 없다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,18 +1,20 @@
 package lotto;
 
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Lotto {
 	private final static int SIZE_OF_LOTTO = 6;
 
-	private final List<Integer> numbers;
+	private final Set<Integer> numbers;
 
 	public Lotto(final List<Integer> numbers) {
-		validate(numbers);
-		this.numbers = numbers;
+		this.numbers = new TreeSet<>(numbers);
+		validate();
 	}
 
-	private void validate(List<Integer> numbers) {
+	private void validate() {
 		if (numbers.size() != SIZE_OF_LOTTO) {
 			throw new IllegalArgumentException("로또의 번호는 6개 이다.");
 		}
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -16,4 +16,11 @@ class LottoTest {
 			.withMessage("로또의 번호는 6개 이다.");
 	}
 
+	@Test
+	void 로또의_번호는_중복될수_없다(){
+		assertThatIllegalArgumentException()
+			.isThrownBy(() -> new Lotto(Arrays.asList(1, 2, 3, 4, 6, 6)))
+			.withMessage("로또의 번호는 6개 이다.");
+	}
+
 }
