655cd80c68e3e9cfd9544b9696cb1358042a7c13
gyeom <koreatech93@naver.com>
2022-10-27 09:18:07 +0900

    refactor(LottoGenerator): Stream 활용하여 숫자 리스트 생성되도록 수정

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -2,15 +2,17 @@ package lotto.domain;
 
 import lotto.ui.ResultView;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class LottoGenerator {
 
     private static final List<Integer> availableNumbers =
-            Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
-                    27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45);
+            Stream.iterate(1, number -> number + 1)
+                    .limit(45)
+                    .collect(Collectors.toList());
 
     public static final int LOTTO_NUMBER_COUNT = 6;
 
