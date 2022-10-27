6c8f434affae0eedb713fe672061bbb19a1bcdce
dominiqn <dmut7691@gmail.com>
2022-10-28 08:53:11 +0900

    feat(stringadder): 숫자 사이에 콤마로 구분하면 합을 반환

--- src/test/java/stringadder/StringAdder.java
+++ src/test/java/stringadder/StringAdder.java
@@ -1,13 +1,21 @@
 package stringadder;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 public class StringAdder {
+    private static final String DELIMITER = ",";
+    private static final int INIT_VALUE = 0;
+
     public int calculate(String value) {
         if (Objects.isNull(value) || value.isEmpty()) {
-            return 0;
+            return INIT_VALUE;
         }
 
-        return Integer.parseInt(value);
+        final String[] tokens = value.split(DELIMITER);
+
+        return Arrays.stream(tokens)
+                .map(Integer::parseInt)
+                .reduce(INIT_VALUE, Integer::sum);
     }
 }
