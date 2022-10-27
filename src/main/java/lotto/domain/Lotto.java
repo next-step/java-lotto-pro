22398051e7fea0c70843729d6c2567fc25566f9b
gyeom <koreatech93@naver.com>
2022-10-27 09:09:40 +0900

    refactor(Lotto): 리스코프 치환원칙을 위한 Set 사용

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,7 +3,6 @@ package lotto.domain;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 public class Lotto {
@@ -11,14 +10,14 @@ public class Lotto {
     private final Set<LottoNumber> lottoNumbers;
 
     public Lotto(final List<Integer> numbers) {
-        HashSet<Integer> uniqueNumbers = new HashSet<>(numbers);
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
         validate(uniqueNumbers);
         this.lottoNumbers = uniqueNumbers.stream()
                 .map(LottoNumber::new)
-                .collect(Collectors.toCollection(TreeSet::new));
+                .collect(Collectors.toSet());
     }
 
-    private void validate(final HashSet<Integer> numbers) {
+    private void validate(final Set<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("로또 숫자는 6개가 필요 합니다.");
         }
