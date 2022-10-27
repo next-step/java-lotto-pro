ee252c59764a91a0abfd46c01b961fe2031cd22b
sixthou <justyuk@naver.com>
2022-10-27 16:48:40 +0900

    test : Lottos 기본 테스트 작성

---
+++ src/test/java/lotto/domain/LottosTest.java
@@ -0,0 +1,13 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottosTest {
+
+    @DisplayName("입력한 수 만큼 로또를 구매할 수 있다.")
+    @Test
+    void lottos_size() {
+
+    }
+}
