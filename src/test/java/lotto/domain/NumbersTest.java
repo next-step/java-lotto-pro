3f31dc09c1bc3a51b85030150b0dcce4a74831c8
sixthou <justyuk@naver.com>
2022-10-27 09:46:33 +0900

    test : Numbers 기본 테스트 작성 및 요구사항 추가

---
+++ src/test/java/lotto/domain/NumbersTest.java
@@ -0,0 +1,31 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class NumbersTest {
+
+    @DisplayName("Numbers 의 size 는 6이다.")
+    @Test
+    void numbers_size() {
+
+    }
+
+    @DisplayName("Numbers 의 size 가 6이 아니면 에러가 발생한다..")
+    @Test
+    void numbers_size_valid() {
+
+    }
+
+    @DisplayName("Numbers 에 Number가 속해 있는지 확인 할 수 있다.")
+    @Test
+    void contain_numbers() {
+
+    }
+
+    @DisplayName("Numbers 는 숫자 크기 오른차순으로 정렬되어 출력된다.")
+    @Test
+    void sort_numbers() {
+
+    }
+}
