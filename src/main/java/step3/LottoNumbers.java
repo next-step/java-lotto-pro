e72eff06c29cbb24de07742b57e207298ca33bc9
taewonh <40269387+taewonh@users.noreply.github.com>
2022-10-27 23:04:27 +0900

    feat: 당첨 번호 비교 기능 구현
    - LottoNumbers 간 match 메소드를 이용하여 일치하는 번호 개수를 추출하는 기능 추가
    - 번호 비교시 각 비교대상의 번호가 다른 경우 예외 처리

--- src/main/java/step3/LottoNumbers.java
+++ src/main/java/step3/LottoNumbers.java
@@ -24,6 +24,15 @@ public class LottoNumbers {
         return numbers.size();
     }
 
+    public int match(LottoNumbers selectNumbers) {
+        if (numbers.size() != selectNumbers.getSize()) {
+            throw new IndexOutOfBoundsException("Incomparable subject. please check lottoNumbers size.");
+        }
+        return (int) numbers.stream()
+                .filter(selectNumbers.numbers::contains)
+                .count();
+    }
+
     private void validate(List<Integer> numbers) {
         int size = numbers.size();
         long count = numbers.stream().distinct().count();
--- src/test/java/step3/LottoNumbersTest.java
+++ src/test/java/step3/LottoNumbersTest.java
@@ -2,20 +2,61 @@ package step3;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@TestInstance(Lifecycle.PER_CLASS)
 public class LottoNumbersTest {
+
+    private final LottoNumbers winning = LottoNumbers.generate(Arrays.asList(1,2,3,4,5,6));
+
+    @ParameterizedTest(name = "{displayName} - select: {0}, matchCount: [{1}]")
+    @MethodSource("numberProvider")
+    @DisplayName("일치하는 번호 개수 확인")
+    public void testMatch(List<Integer> selectNumbers, int expected) {
+        LottoNumbers select = LottoNumbers.generate(selectNumbers);
+        assertThat(winning.match(select)).isEqualTo(expected);
+    }
+
+    private Stream<Arguments> numberProvider() {
+        return Stream.of(
+                Arguments.of(Arrays.asList(7,8,9,10,11,12), 0),
+                Arguments.of(Arrays.asList(6,7,8,9,10,11), 1),
+                Arguments.of(Arrays.asList(5,6,7,8,9,10), 2),
+                Arguments.of(Arrays.asList(4,5,6,7,8,9), 3),
+                Arguments.of(Arrays.asList(3,4,5,6,7,8), 4),
+                Arguments.of(Arrays.asList(2,3,4,5,6,7), 5),
+                Arguments.of(Arrays.asList(1,2,3,4,5,6), 6)
+        );
+    }
 
     @Test
     @DisplayName("중복 번호 선택시 Exception 발생")
     public void testValidateError() {
         assertThatThrownBy(() -> {
-            List<Integer> numbers = Arrays.asList(1,1,1,1,1,1);
-            LottoNumbers.generate(numbers);
+            LottoNumbers.generate(Arrays.asList(1,1,1,1,1,1));
         })
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("Duplicate numbers cannot input.");
+    }
+
+    @Test
+    @DisplayName("번호 비교시 대상 개수가 다른 경우 Exception 발생")
+    public void testMatchError() {
+        assertThatThrownBy(() -> {
+            LottoNumbers select = LottoNumbers.generate(Arrays.asList(1,2,3,4,5,6,7));
+            winning.match(select);
+        })
+                .isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("Incomparable subject. please check lottoNumbers size.");
     }
 }
