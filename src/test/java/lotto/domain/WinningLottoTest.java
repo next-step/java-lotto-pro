a838f51745e0370bfc65c413c60a3a130e984915
sixthou <justyuk@naver.com>
2022-10-27 20:13:25 +0900

    test : WinningLotto 테스트 작성

---
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WinningLottoTest {
+
+    @DisplayName("WinningLotto 는 당첨번호를 가지고 있다.")
+    @Test
+    void winning_lotto() {
+
+    }
+
+    @DisplayName("Lotto 와 비교하여 결과를 알 수 있다.")
+    @Test
+    void winning_rank() {
+
+    }
+}
