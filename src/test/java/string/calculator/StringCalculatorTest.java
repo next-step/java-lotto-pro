cebf6c3195077ae95df4f888516bbbc428ef1e4a
OptimistLabyrinth <keesung0705@gmail.com>
2022-10-27 23:10:00 +0900

    test: 테스트 케이스에 대해 DisplayName 한글 설명 추가

--- src/test/java/string/calculator/StringCalculatorTest.java
+++ src/test/java/string/calculator/StringCalculatorTest.java
@@ -1,5 +1,6 @@
 package string.calculator;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
@@ -8,6 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class StringCalculatorTest {
     @ParameterizedTest
     @NullAndEmptySource
+    @DisplayName("입력 문자열이 null 또는 \"\"(empty string) 이면 결과값은 0 이다.")
     void zeroIfNullOrEmpty(String input) {
         final StringCalculator stringCalculator = new StringCalculator(input);
         final int result = stringCalculator.calculate();
