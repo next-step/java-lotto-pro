2284589297efda62fd5f4a636b125dfcd35cfeb5
victor-jo <joasjn45@gmail.com>
2022-10-27 23:47:36 +0900

    feat(number): 숫자를 담는 객체 추가

---
+++ src/main/java/calculator/Number.java
@@ -0,0 +1,40 @@
+package calculator;
+
+import java.util.Objects;
+
+public class Number {
+
+    private final int number;
+
+    private Number(int number) {
+        this.number = number;
+    }
+
+    public static Number valueOf(String str) {
+        if (str == null || "".equals(str)) {
+            return new Number(0);
+        }
+        return new Number(parseNumber(str));
+    }
+
+    public static int parseNumber(String str) {
+        int number = Integer.parseInt(str);
+        if (number < 0) {
+            throw new RuntimeException("음수는 입력할 수 없습니다.");
+        }
+        return number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Number target = (Number) o;
+        return number == target.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+}
