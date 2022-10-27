f34633fd8c956d5098b95e855d9fae78a8c33be1
cylee9409 <67910616+cylee9409@users.noreply.github.com>
2022-10-27 22:55:06 +0900

    [feat] 음수 값 포함 시 예외 처리 기능 구현
    * 음수 값 포함 시 RuntimeException 테스트 코드 추가
    * 음수 값 포함 시 예외 처리 기능 구현

--- src/main/java/calculator/domain/Num.java
+++ src/main/java/calculator/domain/Num.java
@@ -2,10 +2,34 @@ package calculator.domain;
 
 public class Num {
 
+    private static final String ILLEGAL_ELEMENT_MESSAGE = "계산기의 연산 대상은 양의 정수여야 합니다. 입력값을 확인하세요";
     int element;
 
     public Num(String str) {
-        this.element = Integer.parseInt(str);
+        int num = isNumberType(str);
+        isNegative(num);
+        this.element = num;
+    }
+
+    private int isNumberType(String str) {
+
+        int num = 0;
+
+        try {
+            num =  Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            System.out.println(ILLEGAL_ELEMENT_MESSAGE);
+            throw new RuntimeException();
+        }
+
+        return num;
+    }
+
+    private void isNegative(int num) {
+        if (num < 0) {
+            System.out.println(ILLEGAL_ELEMENT_MESSAGE);
+            throw new RuntimeException();
+        }
     }
 
     public int getElement() {
