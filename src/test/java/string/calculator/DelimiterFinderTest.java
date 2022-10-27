c9b7a4a1d82a51bf099b01ed078239c12a811c25
OptimistLabyrinth <keesung0705@gmail.com>
2022-10-27 23:39:32 +0900

    test: RED - 입력에 커스텀 구분자를 표시하는 문자가 있는지 확인하는 테스트 코드
    여기까지는 RED 상태
    커스텀 구분자를 감싸는 이중 슬래시, 뉴라인 캐릭터가 모두 들어있는지 확인하는
    메서드에 대한 테스트 코드 작성

--- src/test/java/string/calculator/DelimiterFinderTest.java
+++ src/test/java/string/calculator/DelimiterFinderTest.java
@@ -1,4 +1,27 @@
 package string.calculator;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class DelimiterFinderTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"//\\n", "//~\\n", "//.,\\n"})
+    @DisplayName("입력 문자열에 // 와 \\n 둘 다 포함하는 경우 ifIncludeBothSlashAndNewline 메서드의 결과값은 '참' 이 된다")
+    void trueIfIncludeBothDoubleSlashAndNewline(String input) {
+        final DelimiterFinder delimiterFinder = new DelimiterFinder(input);
+        final boolean checkResult = delimiterFinder.ifIncludeBothSlashAndNewline();
+        assertThat(checkResult).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "//", "\\n"})
+    @DisplayName("입력 문자열에 // 와 \\n 둘 중에 하나라도 경우 ifIncludeBothSlashAndNewline 메서드의 결과값은 '거짓' 이 된다")
+    void falseIfNeitherIncludeDoubleSlashNorNewline(String input) {
+        final DelimiterFinder delimiterFinder = new DelimiterFinder(input);
+        final boolean checkResult = delimiterFinder.ifIncludeBothSlashAndNewline();
+        assertThat(checkResult).isFalse();
+    }
 }
