e12660ebfd19efef8c0cc596b16e86ce8ae928c2
sonyoon7 <sonyoon7@gmail.com>
2022-10-27 23:05:22 +0900

    refactor (lotto) Lotto 테스트 코드 구현

--- src/test/java/step3/LottoTest.java
+++ src/test/java/step3/LottoTest.java
@@ -18,10 +18,16 @@ public class LottoTest {
     @Test
     @DisplayName("자동 로또 번호와 당첨 번호 비교 로직이지만, 자동 로또번호 자신과 비교하면 무조건 6이 나옴")
     void 당첨번호_비교() {
-        System.out.println(lotto.gainAutoNumbers());
         lotto.match(lotto.gainAutoNumbers());
         Assertions.assertEquals(6, lotto.getMatchCount());
+    }
 
+    @Test
+    @DisplayName("자동 로또 번호와 당첨 번호 비교 로직이지만, 자동 로또번호 자신과 비교하면 무조건 6이 나옴")
+    void 로또_숫자_범위() {
+        lotto.gainAutoNumbers().forEach(ball -> {
+            Assertions.assertTrue(0 < ball && ball <= 45);
+        });
     }
 
 }
