ff2f207f52a3a2bb3ca0ee2720c758c171c4893b
jimbae <58099885+jimbaemon@users.noreply.github.com>
2022-10-27 09:16:07 +0900

    2단계 - 문자열 덧셈 계산기 (#713)
    * test: null 이나 "" 입력시 0 반환 테스트.
    
    * feat: null 이나 "" 입력시 0 반환 테스트.
    
    * test: 단일형 문자 입력시 해당 문자에 해당하는 숫자 반환 테스트.
    
    * feat: 단일형 문자 입력시 해당 문자에 해당하는 숫자 반환 테스트.
    
    * test : 쉼표로 구분된값 합산.
    
    * feat : 쉼표로 구분된 값 합산 구현.
    
    * test : 쉼표와 콜론으로 구분된 값 합산 테스트.
    
    * feat : 쉼표와 콜론으로 구분된 값 합산 구현.
    
    * test : 구분자 지정 사용 합산 테스트.
    
    * feat : 구분자 지정 사용 합산 구현.
    
    * refactor: 행위를 2개로 분리.
    
    * refactor: Pattern 명명, MagicString 제거.
    
    * test: 음수 입력시 에러 발생 테스트 작성.
    
    * feat: 음수 입력시 에러 발생 구현.
    
    * test: 객체 분리를 위해 테스트 분리.
    
    * test: 합산 테스트 생성.
    
    * feat: 합산 구현.
    
    * test: 문자열 생성자 테스트.
    
    * feat: 문자열 생성자 구현.
    
    * refactor: Positive 클래스를 이용하도록 변경.
    
    * refactor: 네이버 핵테이 컨벤션 적용.
    
    * refactor: if 에 항상 괄호 붙이는 컨벤션.
    
    * refactor: String#split -> Pattern#split 으로 변경.
    
    * fix: EOL 유지 :)

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -4,39 +4,39 @@ import org.junit.jupiter.api.Test;
 
 public class StringTest {
 
-    @Test
-    void split_one_and_two_test(){
-        Assertions.assertThat("1,2".split(","))
-            .hasSize(2)
-            .containsExactly("1", "2");
-    }
+	@Test
+	void split_one_and_two_test() {
+		Assertions.assertThat("1,2".split(","))
+			.hasSize(2)
+			.containsExactly("1", "2");
+	}
 
-    @Test
-    void split_one_only_test(){
-        Assertions.assertThat("1".split(","))
-            .hasSize(1)
-            .containsExactly("1");
-    }
+	@Test
+	void split_one_only_test() {
+		Assertions.assertThat("1".split(","))
+			.hasSize(1)
+			.containsExactly("1");
+	}
 
-    @Test
-    void remove_parentheses_test(){
-        Assertions.assertThat("(1,2)".substring(1,4)).isEqualTo("1,2");
-    }
+	@Test
+	void remove_parentheses_test() {
+		Assertions.assertThat("(1,2)".substring(1, 4)).isEqualTo("1,2");
+	}
 
-    @Test
-    void char_at_test(){
-        Assertions.assertThat(charAt("abc", 1)).isEqualTo('b');
-    }
+	@Test
+	void char_at_test() {
+		Assertions.assertThat(charAt("abc", 1)).isEqualTo('b');
+	}
 
-    @Test
-    @DisplayName("charAt 위치가 벗어나면 Exception 발생하는 부분 테스트")
-    void char_at_index_out_of_bound_exception_test(){
-        Assertions.assertThatThrownBy(() -> charAt("abc", 5))
-            .isInstanceOf(IndexOutOfBoundsException.class)
-            .hasMessage("String index out of range: 5");
-    }
+	@Test
+	@DisplayName("charAt 위치가 벗어나면 Exception 발생하는 부분 테스트")
+	void char_at_index_out_of_bound_exception_test() {
+		Assertions.assertThatThrownBy(() -> charAt("abc", 5))
+			.isInstanceOf(IndexOutOfBoundsException.class)
+			.hasMessage("String index out of range: 5");
+	}
 
-    private char charAt(final String string, final int index){
-        return string.charAt(index);
-    }
+	private char charAt(final String string, final int index) {
+		return string.charAt(index);
+	}
 }
