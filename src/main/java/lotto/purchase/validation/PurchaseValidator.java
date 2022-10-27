558eb8a840b3b0508a16c9ea54321afa344765e4
yoonmin-kim <rladbsals23@gmail.com>
2022-10-27 23:30:58 +0900

    feat(purchase): 구입 금액이 1000원 이상 인지 확인
    검증 기능:
    - 1000원 이상이라면 통과
    - 1000원 미만이면 IllegalArgumentException 발생

--- src/main/java/lotto/purchase/validation/PurchaseValidator.java
+++ src/main/java/lotto/purchase/validation/PurchaseValidator.java
@@ -3,6 +3,7 @@ package lotto.purchase.validation;
 public interface PurchaseValidator {
 
     String ERROR_NUMBER_MESSAGE = "[ERROR] 구입 금액은 숫자로만 입력해주세요.";
+    String ERROR_MIN_COST_MESSAGE = "[ERROR] 구입 가능 금액은 최소 1000원 입니다";
 
     void validate(String purchase);
 }
