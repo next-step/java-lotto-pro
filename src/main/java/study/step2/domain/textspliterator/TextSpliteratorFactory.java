e5647073f8bdec924a12701cd08436559c9cb8df
SanghoonSon <mue0329@naver.com>
2022-10-27 23:23:31 +0900

    refactor: step2 피드백 반영
    하나의 테스트 메소드에서 하나의 유형만 테스트되도록 수정
    추상 클래스에서 일반 클래스로 변경

--- src/main/java/study/step2/domain/textspliterator/TextSpliteratorFactory.java
+++ src/main/java/study/step2/domain/textspliterator/TextSpliteratorFactory.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public abstract class TextSpliteratorFactory {
+public class TextSpliteratorFactory {
 
     private static final List<TextSpliterator> SPLITERATORS;
 
