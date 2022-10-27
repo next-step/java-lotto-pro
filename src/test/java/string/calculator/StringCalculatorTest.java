f78e43953476e606996f59ebcd4fb3fa5adc3d57
OptimistLabyrinth <keesung0705@gmail.com>
2022-10-27 22:54:56 +0900

    test: RED - 입력이 null 또는 ""(empty string) 인 경우
    여기서는 RED 상태
    입력 문자열이 null 또는 empty string 인 경우에 대한 유닛 테스트 코드 작성

--- src/test/java/string/calculator/StringCalculatorTest.java
+++ src/test/java/string/calculator/StringCalculatorTest.java
@@ -1,4 +1,16 @@
 package string.calculator;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class StringCalculatorTest {
+    @ParameterizedTest
+    @NullAndEmptySource
+    void zeroIfNullOrEmpty(String input) {
+        final StringCalculator stringCalculator = new StringCalculator(input);
+        final int result = stringCalculator.calculate();
+        assertThat(result).isEqualTo(0);
+    }
 }
