72f149b4eb30f7b9d0d1f609a65986051062dc02
sixthou <justyuk@naver.com>
2022-10-27 14:33:34 +0900

    test : LottoRank 기본 테스트 작성

---
+++ src/test/java/lotto/domain/LottoRankTest.java
@@ -0,0 +1,44 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoRankTest {
+
+    @DisplayName("일치하는 갯수에 따라서 순위를 알 수 있다.")
+    @Test
+    void rank() {
+
+    }
+
+    @DisplayName("6개가 일치하면 2_000_000_000이 상금이다.")
+    @Test
+    void first() {
+
+    }
+
+    @DisplayName("5개가 일치하면 1_500_000이 상금이다.")
+    @Test
+    void second() {
+
+    }
+
+    @DisplayName("4개가 일치하면 50_000이 상금이다.")
+    @Test
+    void third() {
+
+    }
+
+    @DisplayName("3개가 일치하면 5_000이 상금이다.")
+    @Test
+    void fourth() {
+
+    }
+
+    @DisplayName("2개, 1개, 0개 일치하면 0원이 상금이다.")
+    @Test
+    void fail() {
+
+    }
+
+}
