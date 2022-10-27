e1d0ed399113e54637ee1b0f4a62b042e2eec595
sixthou <justyuk@naver.com>
2022-10-27 14:43:26 +0900

    feat : LottoRank 구현 및 테스트 작성

--- src/test/java/lotto/domain/LottoRankTest.java
+++ src/test/java/lotto/domain/LottoRankTest.java
@@ -1,44 +1,30 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class LottoRankTest {
 
     @DisplayName("일치하는 갯수에 따라서 순위를 알 수 있다.")
-    @Test
-    void rank() {
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6})
+    void rank(int matchCount) {
 
+        assertThat(LottoRank.getRank(matchCount)).isInstanceOf(LottoRank.class);
     }
 
-    @DisplayName("6개가 일치하면 2_000_000_000이 상금이다.")
-    @Test
-    void first() {
+    @DisplayName("순위에 따라서 상금을 구할 수 있다.")
+    @ParameterizedTest
+    @CsvSource(value = {"6:2000000000", "5:1500000", "4:50000", "3:5000", "2:0", "1:0", "0:0"}, delimiter = ':')
+    void winning_price(int matchCount, int winningPrice) {
 
-    }
+        LottoRank rank = LottoRank.getRank(matchCount);
 
-    @DisplayName("5개가 일치하면 1_500_000이 상금이다.")
-    @Test
-    void second() {
-
-    }
-
-    @DisplayName("4개가 일치하면 50_000이 상금이다.")
-    @Test
-    void third() {
-
-    }
-
-    @DisplayName("3개가 일치하면 5_000이 상금이다.")
-    @Test
-    void fourth() {
-
-    }
-
-    @DisplayName("2개, 1개, 0개 일치하면 0원이 상금이다.")
-    @Test
-    void fail() {
-
+        assertThat(rank.getWinningPrice()).isEqualTo(winningPrice);
     }
 
 }
