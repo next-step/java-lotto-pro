59572b5e705449ba9db7e88ab54d97bb18a08ef9
jimbaemon <jimbae@kakao.com>
2022-10-28 01:14:00 +0900

    refactor: Pattern#split 사용하도록 변경.

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,11 +1,13 @@
 package calculator;
 
+import static java.util.regex.Pattern.*;
+
 import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
-	private final static Pattern SPLIT_REGEX_PATTERN = Pattern.compile("//(.)\n(.*)");
+	private final static Pattern SPLIT_REGEX_PATTERN = compile("//(.)\n(.*)");
 	private final static String DEFAULT_SPLIT_REGEX = "[,:]";
 
 	public static int calculate(String input) {
@@ -18,10 +20,10 @@ public class StringAddCalculator {
 	private static String[] split(final String input) {
 		Matcher matcher = SPLIT_REGEX_PATTERN.matcher(input);
 		if (matcher.find()) {
-			return Pattern.compile(matcher.group(1))
+			return compile(matcher.group(1))
 				.split(matcher.group(2));
 		}
-		return input.split(DEFAULT_SPLIT_REGEX);
+		return compile(DEFAULT_SPLIT_REGEX).split(input);
 	}
 
 	private static int sum(final String[] input) {
