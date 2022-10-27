366e3559226d788a33a3e2fef4c23af233f08d17
yeoojin <only1year100@gmail.com>
2022-10-28 01:26:48 +0900

    feat(StringAddCalculator.java): 널, 빈값 체크 단순 구현

---
+++ src/main/java/step2/StringAddCalculator.java
@@ -0,0 +1,20 @@
+package step2;
+
+public class StringAddCalculator {
+
+
+    public static int splitAndSum(String reqStr) {
+        int resultSum = 0;
+
+        if(reqStr == null) {
+            return resultSum;
+        }
+
+        if(reqStr.isEmpty()) {
+            return resultSum;
+        }
+
+
+        return resultSum;
+    }
+}
