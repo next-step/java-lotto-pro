368158d51af517938e17404f0323052096ad5d98
chunhodong <yhms4432@naver.com>
2022-10-27 17:09:54 +0900

    feat: 로또진행을 위한 Controller,화면출력을 위한 View클래스 생성
    - 로또 진행순서에 맞게 Controller로직추가
    - 입력받기위한 View클래스 메소드 추가

---
+++ src/main/java/step3/controller/LottoController.java
@@ -0,0 +1,33 @@
+package step3.controller;
+
+import step2.StringParser;
+import step3.io.Console;
+import step3.model.LottoService;
+import step3.model.Lottos;
+import step3.model.dto.LottosNumberDto;
+import step3.view.InputView;
+import step3.view.ResultView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoController {
+
+
+    public void start(){
+        InputView.printPurchasingAmount();
+        int amount = 14000;
+        LottoService lottoService = new LottoService(amount,new Lottos(new ArrayList<>()));
+        lottoService.purchaseByAuto();
+        LottosNumberDto lottosNumberDto = lottoService.getLottoTicketState();
+        ResultView.printPurchasingLottos(lottosNumberDto);
+        InputView.printWinningNumber();
+        List<Integer> winningNumbers = new ArrayList<>();
+        ResultView.printWinStats(lottoService.getRankStatus(winningNumbers));
+    }
+
+
+    public static void main(String[]args){
+        new LottoController().start();
+    }
+}
