985c19bfe70dadea690a362ae0510020a963174b
anwjrrp33 <anwjrrp33@gmail.com>
2022-10-28 23:08:06 +0900

    test: 금액 클래스 테스트 코드 작성

---
+++ src/main/java/lotto/domain/Amount.java
@@ -0,0 +1,60 @@
+package lotto.domain;
+
+import java.util.Objects;
+import java.util.regex.Pattern;
+
+public class Amount {
+
+    private final static int MIN_AMOUNT = 1_000;
+
+    private static final int MIN_BUY_LOTTO_COUNT = 1;
+
+    private final static String NOT_ALLOW_NULL_OR_EMPTY_MESSAGE = "금액은 Null 또는 Empty 값이 불가능합니다.";
+
+    private final static String ONLY_NUMBER_MESSAGE = "금액은 숫자만 입력 가능합니다.";
+
+    private final static String ONLY_POSITIVE_MESSAGE = "금액은 양수만 입력 가능합니다.";
+
+    private final static String MIN_BUY_LOTTO_MESSAGE = "로또는 1개 이상 구매해야 합니다.";
+
+    private final int amount;
+
+    public Amount(final String amount) {
+        validate(amount);
+        this.amount = Integer.parseInt(amount);
+    }
+
+    private void validate(final String amount) {
+        validateNullOrEmpty(amount);
+        validateNumber(amount);
+        validatePositive(amount);
+    }
+
+    private void validateNullOrEmpty(final String amount) {
+        if (Objects.isNull(amount) || amount.isEmpty()) {
+            throw new IllegalArgumentException(NOT_ALLOW_NULL_OR_EMPTY_MESSAGE);
+        }
+    }
+
+    private void validateNumber(final String amount) {
+        if (amount.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException(ONLY_NUMBER_MESSAGE);
+        }
+    }
+
+    private void validatePositive(final String amount) {
+        if (Pattern.matches("^[0-9]*$", amount)) {
+            throw new IllegalArgumentException(ONLY_POSITIVE_MESSAGE);
+        }
+    }
+
+    public int buyLottoCount() {
+        int buyLottoCount = this.amount / MIN_AMOUNT;
+
+        if (buyLottoCount < MIN_BUY_LOTTO_COUNT) {
+            throw new IllegalArgumentException(MIN_BUY_LOTTO_MESSAGE);
+        }
+
+        return buyLottoCount;
+    }
+}
---
+++ src/test/java/lotto/domain/AmountTest.java
@@ -0,0 +1,40 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+public class AmountTest {
+
+    @DisplayName("금액이 Null 또는 빈값인 경우 에러가 발생되는지 확인")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void amountNullOrEmpty(String amount) {
+        assertThatThrownBy(() -> new Amount(amount))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("금액이 숫자가 아닌 경우 에러가 발생되는지 확인")
+    @Test
+    void amountOnlyNumber() {
+        assertThatThrownBy(() -> new Amount("ABCDEF"))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("금액이 양수가 아닌 경우 에러가 발생되는지 확인")
+    @Test
+    void amountOnlyPositive() {
+        assertThatThrownBy(() -> new Amount("-1000"))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("입력된 금액으로 구매한 로또 개수가 1개 이하인 경우 에러가 발생되는지 확인")
+    @Test
+    void buyLottoCount() {
+        assertThatThrownBy(() -> new Amount("900"))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+}
