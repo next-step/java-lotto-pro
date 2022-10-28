3c84ce69bf433eaacfafd1611ed22fde5a477554
velcronicity <velcronicity@gmail.com>
2022-10-28 10:29:36 +0900

    feat: 구매갯수 구하기

--- src/main/java/lotto/Game.java
+++ src/main/java/lotto/Game.java
@@ -1,4 +1,22 @@
 package lotto;
 
 public class Game {
+    public static final int PRICE = 1000;
+    private final UserLotto userLotto;
+
+    public Game(int purchaseMoney) {
+        validate(purchaseMoney);
+        int count = purchaseMoney / PRICE;
+        this.userLotto = new UserLotto(count);
+    }
+
+    private void validate(int money) {
+        if (money < 1000) {
+            throw new IllegalArgumentException("로또 1장 가격은 1000원입니다. 1000원 이상의 금액을 입력해주세요.");
+        }
+    }
+
+    public UserLotto getUserLotto() {
+        return userLotto;
+    }
 }
