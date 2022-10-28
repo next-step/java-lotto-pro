372a8a159b04c0612dd18d4c0ba87c2eca54de6d
jimbaemon <jimbae@kakao.com>
2022-10-28 23:00:43 +0900

    feat: 로또들의 순위 확인.

---
+++ src/main/java/lotto/Lottos.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Lottos {
+	private final List<Lotto> lottos;
+
+	public Lottos(final List<Lotto> lottos) {
+		this.lottos = lottos;
+	}
+
+	public Ranks match(final Lotto winnerLotto){
+		return lottos.stream()
+			.mapToInt(lotto -> lotto.match(winnerLotto))
+			.mapToObj(Rank::from)
+			.collect(Collectors.collectingAndThen(Collectors.toList(), Ranks::new));
+	}
+}
