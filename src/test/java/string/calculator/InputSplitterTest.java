7d1e1cd9379eca58187515bd18dcf52ddc6fa135
OptimistLabyrinth <keesung0705@gmail.com>
2022-10-28 22:08:41 +0900

    test: InputSplitter 단위 테스트에 입력이 숫자 하나인 경우도 추가

--- src/test/java/string/calculator/InputSplitterTest.java
+++ src/test/java/string/calculator/InputSplitterTest.java
@@ -8,6 +8,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class InputSplitterTest {
     @ParameterizedTest
+    @ValueSource(strings = {"0", "1", "9"})
+    @DisplayName("입력 문자열이 숫자 하나인 경우")
+    void singleDigitInput(String input) {
+        final InputSplitter inputSplitter = new InputSplitter(input);
+        final String[] tokens = inputSplitter.split();
+        assertThat(tokens).contains(input);
+    }
+
+    @ParameterizedTest
     @ValueSource(strings = {"1:2:3", "1,2,3"})
     @DisplayName("입력 문자열이 기본 구분자를 사용하는 경우")
     void splitInputWithDefaultDelimiter(String input) {
