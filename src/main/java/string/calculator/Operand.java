e718a633d0d0552f5ff405ee66802bcfc0bba8e9
OptimistLabyrinth <keesung0705@gmail.com>
2022-10-28 21:39:22 +0900

    feat: (REFACTOR-GREEN) Operand 클래스에 add 메서드 추가, equals, hashCode 메서드 오버라이드
    add 메서드에 대한 테스트 코드도 추가

--- src/main/java/string/calculator/Operand.java
+++ src/main/java/string/calculator/Operand.java
@@ -1,5 +1,7 @@
 package string.calculator;
 
+import java.util.Objects;
+
 public class Operand {
     private final int value;
 
@@ -15,5 +17,23 @@ public class Operand {
 
     private boolean isNegative(int value) {
         return value < 0;
+    }
+
+    public Operand add(Operand other) {
+        int addResult = value + other.value;
+        return new Operand(Integer.toString(addResult));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Operand operand = (Operand) o;
+        return value == operand.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
--- src/test/java/string/calculator/OperandTest.java
+++ src/test/java/string/calculator/OperandTest.java
@@ -3,8 +3,10 @@ package string.calculator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -31,6 +33,20 @@ class OperandTest {
         @DisplayName("숫자가 아닌 값으로 Operand 객체를 생성하려고 하면 예외가 발생한다")
         void cannotCreateOperandIfNonDigit(String token) {
             assertThrows(RuntimeException.class, () -> new Operand(token));
+        }
+    }
+
+    @Nested
+    @DisplayName("add 메서드 테스트")
+    class Add {
+        @ParameterizedTest
+        @CsvSource(value = {"1,2,3", "1,9,10", "0,0,0", "444,555,999"}, delimiter = ',')
+        @DisplayName("두 개의 Operand 를 더한 결과로 새로운 Operand 객체를 얻을 수 있다")
+        void addTest(String left, String right, String result) {
+            Operand opLeft = new Operand(left);
+            Operand opRight = new Operand(right);
+            Operand opResult = opLeft.add(opRight);
+            assertThat(opResult).isEqualTo(new Operand(result));
         }
     }
 }
