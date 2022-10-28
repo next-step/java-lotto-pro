1bd84f11a9efb32179c9340f94e75605cd4f45cd
CHEESE <81912261+ychxexn@users.noreply.github.com>
2022-10-28 17:28:40 +0900

    feat: 번호 일치 개수로 당첨금 확인 추가

---
+++ src/test/java/lotto/LottoTest.java
@@ -0,0 +1,17 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LottoTest {
+    @ParameterizedTest
+    @CsvSource(value = {"6:2000000000", "5:1500000", "4:50000", "3:5000"}, delimiter = ':')
+    @DisplayName("번호 일치 개수로 당첨금 구하기")
+    void 일치_개수로_당첨금_구하기(int matchCount, int expected) {
+        Lotto lotto = new Lotto(matchCount);
+        assertThat(lotto.getPrize()).isEqualTo(expected);
+    }
+}
