fe3cdbaba91d8786c0d9f189297b731e629459b3
chunhodong <yhms4432@naver.com>
2022-10-29 01:49:54 +0900

    feat: ConsoleView클래스 수동입력 메소드추가

--- src/main/java/step3/view/LottoConsoleView.java
+++ src/main/java/step3/view/LottoConsoleView.java
@@ -3,8 +3,11 @@ package step3.view;
 import step3.model.dto.LottoResultDto;
 import step3.model.dto.LottosNumberDto;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LottoConsoleView {
 
@@ -56,6 +59,14 @@ public class LottoConsoleView {
         if (priceRatio < 1.0) {
             System.out.println("(기준이 1이기 때문에 결과적으로 손해라는 의미임)");
         }
+    }
+
+    public static List<String> printPurchasingManual() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        int count = SCANNER.nextInt();
+        return count <= 0 ? Collections.EMPTY_LIST : IntStream.rangeClosed(1, count)
+                .mapToObj(operand -> SCANNER.next())
+                .collect(Collectors.toList());
     }
 
 }
