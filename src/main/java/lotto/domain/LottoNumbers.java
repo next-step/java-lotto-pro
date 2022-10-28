66c2afa950e133ff4270abffbf6c76056c9eefc2
Haseop Lee <haservi@naver.com>
2022-10-28 21:31:21 +0900

    test(Lotto): 랜덤 숫자 테스트 코드 작성

--- src/main/java/lotto/domain/LottoNumbers.java
+++ src/main/java/lotto/domain/LottoNumbers.java
@@ -11,6 +11,10 @@ public class LottoNumbers {
         this.lottoNumbers = null;
     }
 
+    public int size() {
+        return this.lottoNumbers.size();
+    }
+
     private void validLottoNumber(List<Integer> lottoNumbers) {
         // TODO: 정상적인 로또 숫자 인지 확인 필요
         // TODO: 로또 번호 유효성 체크
--- src/main/java/lotto/utils/LottoUtils.java
+++ src/main/java/lotto/utils/LottoUtils.java
@@ -1,7 +1,19 @@
 package lotto.utils;
 
+import lotto.domain.LottoNumbers;
+
 public class LottoUtils {
 
+    private LottoUtils() {
+
+    }
     // 랜덤한 숫자를 생성하도록 기능 추가
+    private static final int MAX_SIZE = 6;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+
+    public static LottoNumbers generateLottoNumber() {
+        return null;
+    }
 
 }
