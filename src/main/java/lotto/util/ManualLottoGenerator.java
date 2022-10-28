bbc2a26234419e5964afc8b97fb1a1753509eb81
sjk5766 <wjdrnr6662@gmail.com>
2022-10-29 02:11:40 +0900

    refactor: 사용하지 않는 import 문 제거

--- src/main/java/lotto/util/ManualLottoGenerator.java
+++ src/main/java/lotto/util/ManualLottoGenerator.java
@@ -4,10 +4,7 @@ import lotto.domain.LottoNumber;
 import lotto.domain.LottoTicket;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class ManualLottoGenerator implements LottoGenerator {
     private final List<Integer> numbers;
--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -1,7 +1,5 @@
 package lotto.domain;
 
-import lotto.domain.dto.StatisticDto;
-import lotto.domain.enums.Rank;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
