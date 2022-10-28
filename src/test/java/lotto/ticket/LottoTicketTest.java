95d6ac8592d96c2d021689f053fad4ab85027821
hyeongju <hybrid.simpler@gmail.com>
2022-10-28 15:00:32 +0900

    test: 로또 갯수 일치 테스트 코드 추가

--- src/test/java/lotto/ticket/LottoTicketTest.java
+++ src/test/java/lotto/ticket/LottoTicketTest.java
@@ -3,6 +3,7 @@ package lotto.ticket;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Random;
+import lotto.match.Rank;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -35,6 +36,46 @@ public class LottoTicketTest {
         LottoTicket winnerLottoTicket = new LottoTicket("1,2,3,4,5,6");
         int matchCount = lottoTicket.match(winnerLottoTicket);
         assertThat(matchCount).isEqualTo(6);
+    }
+
+    @Test
+    void 로또_6개_일치(){
+        LottoTicket userLotto = new LottoTicket("1,2,3,4,5,6");
+        WinnerLottoTicket winnerLotto = new WinnerLottoTicket(
+                new LottoTicket("1,2,3,4,5,6"),
+                7);
+        int match = userLotto.match(winnerLotto.getLotto());
+        assertThat(match).isEqualTo(Rank.FIRST.getCountOfMatch());
+    }
+
+    @Test
+    void 로또_5개_일치(){
+        LottoTicket userLotto = new LottoTicket("1,2,3,4,5,45");
+        WinnerLottoTicket winnerLotto = new WinnerLottoTicket(
+                new LottoTicket("1,2,3,4,5,7"),
+                45);
+        int match = userLotto.match(winnerLotto.getLotto());
+        assertThat(match).isEqualTo(Rank.SECOND.getCountOfMatch());
+    }
+
+    @Test
+    void 로또_4개_일치(){
+        LottoTicket userLotto = new LottoTicket("1,2,3,4,5,6");
+        WinnerLottoTicket winnerLotto = new WinnerLottoTicket(
+                new LottoTicket("1,2,3,4,10,11"),
+                45);
+        int match = userLotto.match(winnerLotto.getLotto());
+        assertThat(match).isEqualTo(Rank.FOURTH.getCountOfMatch());
+    }
+
+    @Test
+    void 로또_3개_일치(){
+        LottoTicket userLotto = new LottoTicket("1,2,3,4,5,6");
+        WinnerLottoTicket winnerLotto = new WinnerLottoTicket(
+                new LottoTicket("1,2,3,10,11,12"),
+                45);
+        int match = userLotto.match(winnerLotto.getLotto());
+        assertThat(match).isEqualTo(Rank.FIFTH.getCountOfMatch());
     }
 
 }
