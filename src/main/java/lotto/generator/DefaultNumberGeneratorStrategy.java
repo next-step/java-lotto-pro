19a27bea0ce4ca4556bada065cb46531efcda3ff
ilmare-cbk <cbk104325@gmail.com>
2022-10-29 05:53:57 +0900

    refactor(DefaultNumberGeneratorStrategy.java): 한 번 만든 1 ~ 45 숫자 목록을 static하게 설정하여 캐싱되도록 수정

--- src/main/java/lotto/generator/DefaultNumberGeneratorStrategy.java
+++ src/main/java/lotto/generator/DefaultNumberGeneratorStrategy.java
@@ -3,35 +3,25 @@ package lotto.generator;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import lotto.constant.LottoConstant;
 import lotto.domain.lotto.LottoNumber;
 
 public class DefaultNumberGeneratorStrategy implements LottoNumberGeneratorStrategy {
 
-    private final List<Integer> seedNumbers;
-
-    public DefaultNumberGeneratorStrategy() {
-        this.seedNumbers = createSeedNumbers();
-    }
-
-    private List<Integer> createSeedNumbers() {
-        List<Integer> numbers = new ArrayList<>();
-
-        for (int i = LottoConstant.MIN_LOTTO_NUMBER; i <= LottoConstant.MAX_LOTTO_NUMBER; i++) {
-            numbers.add(i);
-        }
-
-        return numbers;
-    }
+    private static final List<Integer> SEED_NUMBERS =
+            IntStream.rangeClosed(LottoConstant.MIN_LOTTO_NUMBER, LottoConstant.MAX_LOTTO_NUMBER)
+                    .boxed().collect(Collectors.toList());
 
     @Override
     public List<LottoNumber> generate() {
-        Collections.shuffle(seedNumbers);
+        Collections.shuffle(SEED_NUMBERS);
 
         List<LottoNumber> lottoNumbers = new ArrayList<>();
 
         for (int i = 0; i < LottoConstant.LOTTO_NUMBER_COUNT; i++) {
-            lottoNumbers.add(LottoNumber.from(seedNumbers.get(i)));
+            lottoNumbers.add(LottoNumber.from(SEED_NUMBERS.get(i)));
         }
 
         return lottoNumbers;
