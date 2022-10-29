25bce261ed67c1d31cd78a9ff924e6cfa228c153
giraffelim <taeyang7704@naver.com>
2022-10-29 17:15:38 +0900

    feat(Lotto): 동등성 비교를 위한 equals, hashCode 오버라이딩

--- src/main/java/step3/domain/Lotto.java
+++ src/main/java/step3/domain/Lotto.java
@@ -1,5 +1,7 @@
 package step3.domain;
 
+import java.util.Objects;
+
 public class Lotto {
     private final LottoNumbers lottoNumbers;
 
@@ -9,6 +11,23 @@ public class Lotto {
 
     public int compareNumbers(final LottoNumbers winningNumbers) {
         return this.lottoNumbers.compareTo(winningNumbers);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Lotto lotto = (Lotto) o;
+        return Objects.equals(lottoNumbers, lotto.lottoNumbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoNumbers);
     }
 
     @Override
--- src/test/java/step3/domain/LottoTest.java
+++ src/test/java/step3/domain/LottoTest.java
@@ -13,6 +13,12 @@ class LottoTest {
     }
 
     @Test
+    void 로또_번호가_같을경우_동등() {
+        LottoNumbers lottoNumbers = new LottoNumbers("1, 2, 3, 4, 5, 6");
+        assertThat(new Lotto(lottoNumbers)).isEqualTo(new Lotto(lottoNumbers));
+    }
+
+    @Test
     void 보유한_LottoNumbers_와_다른_LottoNumbers_를_비교해_숫자가_일치하는_개수를_찾을_수_있다() {
         Lotto lotto = new Lotto(new LottoNumbers("1, 2, 3, 4, 5, 6"));
         LottoNumbers winningNumbers = new LottoNumbers("1, 2, 3, 4, 5, 7");
