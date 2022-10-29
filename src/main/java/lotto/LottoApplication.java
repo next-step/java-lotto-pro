33e2f83787fdcf60758c7a9e312f481154bb85a1
Minjae Kim <minjae.kim@mytrum.com>
2022-10-30 00:38:28 +0900

    feat: application 구현
    LottoApplication 구현
    README 기능 업데이트

---
+++ src/main/java/lotto/LottoApplication.java
@@ -0,0 +1,9 @@
+package lotto;
+
+public class LottoApplication {
+
+    public static void main(String[] args) {
+        LottoStore lottoStore = new LottoStore();
+        lottoStore.entrance();
+    }
+}
