3ad9b9db5b6027d921257d307593bbf456c590d3
OptimistLabyrinth <keesung0705@gmail.com>
2022-10-30 00:46:04 +0900

    feat: Controller 에서의 사용 방식을 고려해서  LottoTicketsBucket 생성자 수정
    외부에서 LottoTickets 객체를 만들어서 사용한 후에
    나중에 저장 및 관리를 위해서 LottoTicketsBucket 객체에 넣어주게 된다
    
    실제 사용 방식을 LottoTicketsBucket 클래스에 대한 단위 테스트에서 확인 가능

--- src/main/java/lotto/model/lotto/ticket/LottoTicketsBucket.java
+++ src/main/java/lotto/model/lotto/ticket/LottoTicketsBucket.java
@@ -7,23 +7,14 @@ import java.util.List;
  * 로또 티켓(LottoTicket) 여러 개를 저장하고 관리하는 객체이다.
  */
 public class LottoTicketsBucket {
-    private static final int SIZE_OF_FULL_CANDIDATE_LIST = 45;
     protected final List<LottoTicket> lottoTickets;
 
     public LottoTicketsBucket(int howManyTickets) {
-        lottoTickets = new ArrayList<>(howManyTickets);
-        for (int i = 0; i < howManyTickets; ++i) {
-            final List<Integer> candidates = intsFromOneToFortyFive();
-            final LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator(candidates);
-            lottoTickets.add(new LottoTicket(lottoNumberGenerator));
-        }
+        this.lottoTickets = new ArrayList<>(howManyTickets);
     }
 
-    private List<Integer> intsFromOneToFortyFive() {
-        final List<Integer> fullCandidateList = new ArrayList<>(SIZE_OF_FULL_CANDIDATE_LIST);
-        for (int i = 1; i <= SIZE_OF_FULL_CANDIDATE_LIST; ++i) {
-            fullCandidateList.add(i);
-        }
-        return fullCandidateList;
+    public int addLottoTicket(LottoTicket lottoTicket) {
+        lottoTickets.add(lottoTicket);
+        return lottoTickets.size();
     }
 }
--- src/test/java/lotto/model/lotto/ticket/LottoTicketsBucketTest.java
+++ src/test/java/lotto/model/lotto/ticket/LottoTicketsBucketTest.java
@@ -4,9 +4,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoTicketsBucketTest {
+    private static final int SIZE_OF_FULL_CANDIDATE_LIST = 45;
+
     static class LottoTicketsBucketForTest extends LottoTicketsBucket {
         LottoTicketsBucketForTest(int howManyTickets) {
             super(howManyTickets);
@@ -22,6 +27,19 @@ class LottoTicketsBucketTest {
     @DisplayName("LottoTicketsBucket 클래스 생성자 실행 결과, 생성자에 야규먼트로 전달한 개수 만큼 로또 티켓을 만든다")
     void constructor(int numberOfTickets) {
         final LottoTicketsBucketForTest lottoTicketsBucketForTest = new LottoTicketsBucketForTest(numberOfTickets);
+        for (int i = 0; i < numberOfTickets; ++i) {
+            final List<Integer> candidates = intsFromOneToFortyFive();
+            final LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator(candidates);
+            lottoTicketsBucketForTest.addLottoTicket(new LottoTicket(lottoNumberGenerator));
+        }
         assertThat(lottoTicketsBucketForTest.bucketSize()).isEqualTo(numberOfTickets);
+    }
+
+    private List<Integer> intsFromOneToFortyFive() {
+        final List<Integer> fullCandidateList = new ArrayList<>(SIZE_OF_FULL_CANDIDATE_LIST);
+        for (int i = 1; i <= SIZE_OF_FULL_CANDIDATE_LIST; ++i) {
+            fullCandidateList.add(i);
+        }
+        return fullCandidateList;
     }
 }
