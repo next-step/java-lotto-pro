d6c818dacaa37de9d934098bf4b085651b64cf4b
yomni <yearsheep@gmail.com>
2022-10-29 11:21:42 +0900

    feat: 로또번호 엔티티에 대한 기능 구현 완료
    1. 로또번호는 1~45 사이의 숫자로 생성
    2. 1~45 이외 숫자면 예외처리

---
+++ src/main/java/step3/domain/LottoNumber.java
@@ -0,0 +1,29 @@
+package step3.domain;
+
+public class LottoNumber {
+
+
+    public static final int MINIMUM_LOTTO_NUMBER = 1;
+    public static final int MAXIMUM_LOTTO_NUMBER = 45;
+    public static final String EXCEPTION_MESSAGE_FOR_OUT_OF_LOTTO_NUMBER_BOUNDARY = "로또번호는 " + MINIMUM_LOTTO_NUMBER + "과 " + MAXIMUM_LOTTO_NUMBER + "사이의 숫자이어야 합니다.";
+    private final int lottoNumber;
+
+    public LottoNumber(int lottoNumber) {
+        validateLottoNumber(lottoNumber);
+        this.lottoNumber = lottoNumber;
+    }
+
+    private void validateLottoNumber(int lottoNumber) {
+        if (lottoNumber < MINIMUM_LOTTO_NUMBER || lottoNumber > MAXIMUM_LOTTO_NUMBER) {
+            throw new IllegalArgumentException(EXCEPTION_MESSAGE_FOR_OUT_OF_LOTTO_NUMBER_BOUNDARY);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof LottoNumber)) return false;
+        LottoNumber that = (LottoNumber) o;
+        return lottoNumber == that.lottoNumber;
+    }
+}
