a15c5c4f49afc6fd9d55d1b4a33382a31ac9d2ef
OptimistLabyrinth <keesung0705@gmail.com>
2022-10-30 02:41:01 +0900

    feat: 당첨 통계 제목을 출력한다

--- src/main/java/lotto/view/LottoScoreTitlePrinter.java
+++ src/main/java/lotto/view/LottoScoreTitlePrinter.java
@@ -4,4 +4,9 @@ package lotto.view;
  * 당첨 통계 제목을 출력한다.
  */
 public class LottoScoreTitlePrinter {
+    private static final String LOTTO_SCORE_TITLE = "당첨 통계\n---------";
+
+    public static void print() {
+        System.out.println(LOTTO_SCORE_TITLE);
+    }
 }
