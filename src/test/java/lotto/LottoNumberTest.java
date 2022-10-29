31a056a7c1e94cfae8a2bf8724d050e12fc0f771
seogineer <dgseo8981@gmail.com>
2022-10-29 19:37:53 +0900

    feat: Add test code

--- src/test/java/lotto/LottoNumberTest.java
+++ src/test/java/lotto/LottoNumberTest.java
@@ -12,4 +12,10 @@ public class LottoNumberTest {
         assertThat(new LottoNumber(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6)))
                 .countHit(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6)))).isEqualTo(6);
     }
+
+    @Test
+    void 로또_번호_확인_2() {
+        assertThat(new LottoNumber(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6)))
+                .countHit(new ArrayList<>(Arrays.asList(1, 2, 3, 10, 20, 40)))).isEqualTo(3);
+    }
 }
