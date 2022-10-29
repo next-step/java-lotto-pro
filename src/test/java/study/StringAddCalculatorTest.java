df83ab10e97446da4c9491251b15111bd65d3ed7
iamsojung <sojungkid@gmail.com>
2022-10-29 14:56:25 +0900

    refactor: DisplayName, 테스트 코드 수정

--- src/test/java/study/StringAddCalculatorTest.java
+++ src/test/java/study/StringAddCalculatorTest.java
@@ -2,19 +2,20 @@ package study;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@DisplayName("문자열 덧셈 계산기 테스트")
 public class StringAddCalculatorTest {
 
-    @Test
+    @ParameterizedTest
+    @NullAndEmptySource
     @DisplayName("빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.")
-    public void splitAndSum_null_or_blank() {
-        int result = StringAddCalculator.splitAndSum(null);
-        assertThat(result).isEqualTo(0);
-
-        result = StringAddCalculator.splitAndSum("");
+    public void splitAndSum_null_or_blank(String text) {
+        int result = StringAddCalculator.splitAndSum(text);
         assertThat(result).isEqualTo(0);
     }
 
@@ -40,7 +41,7 @@ public class StringAddCalculatorTest {
     }
 
     @Test
-    @DisplayName("숫자 구분자를 컴마(,)이외에 콜론(:)으로도 구분하여 숫자의 합을 계산할 수 있다.")
+    @DisplayName("//와 \n 문자 사이에 커스텀 구분자를 지정할 수 있다.")
     public void splitAndSum_custom_seperator() throws Exception {
         int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
         assertThat(result).isEqualTo(6);
@@ -52,5 +53,4 @@ public class StringAddCalculatorTest {
         assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
             .isInstanceOf(RuntimeException.class);
     }
-
 }
