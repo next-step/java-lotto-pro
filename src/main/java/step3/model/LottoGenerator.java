522f739a4db9dd57729438a1ef8982e9298910a9
yeoojin <only1year100@gmail.com>
2022-10-29 12:21:41 +0900

    feat(LottoGenerator): 로또 금액 빈값, 숫자만 입력 체크 추가

--- src/main/java/step3/model/LottoGenerator.java
+++ src/main/java/step3/model/LottoGenerator.java
@@ -1,17 +1,39 @@
 package step3.model;
 
+import static step3.constant.Message.ERROR_IS_NUMBERIC;
+import static step3.constant.Message.NOT_VALID_NULL;
+
 public class LottoGenerator {
     private int purchasePrice;
     private Lottos lottos = new Lottos();
 
     public void setPurchasePrice(String price) {
-        //todo price 1000원 단위인지 validate
-        this.purchasePrice = Integer.parseInt(price);
+        checkEmpty(price);
+
+        try {
+            this.purchasePrice = Integer.parseInt(price);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ERROR_IS_NUMBERIC);
+        }
     }
 
+
     public Lottos generateLottos() {
-        lottos.setPurchasedCount(purchasePrice);
+        if(validatePrice()) {
+            lottos.setPurchasedCount(purchasePrice);
+        }
+
         lottos.setLottos();
         return lottos;
     }
+    private boolean validatePrice() {
+        boolean valid = false;
+        return valid;
+    }
+    private void checkEmpty(String price) {
+        if(price == null || price.isEmpty()) {
+            throw new IllegalArgumentException(NOT_VALID_NULL);
+        }
+    }
+
 }
