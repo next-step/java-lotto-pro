df08a828fc047fa9d21a1c00ffb88e3c20825fa6
sueH237 <suyh237cod@naver.com>
2022-10-29 19:14:22 +0900

    refactor: CalculatorUtil 생성

--- src/main/java/StringAddCalculator/StringAddCalculator.java
+++ src/main/java/StringAddCalculator/StringAddCalculator.java
@@ -1,49 +1,25 @@
 package StringAddCalculator;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import utils.CalculatorUtil;
 
 public class StringAddCalculator {
 
-	public static int splitAndSum(String input) {
-		if (input == null || input.isEmpty()) {
-			return 0;
-		}
-		String[] splitInput = splitInput(input);
-		return calculatorInput(splitInput);
-	}
+    private static final int DEFAULT_INT = 0;
 
-	private static String[] splitInput(String input) {
-		Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
-		if (m.find()) {
-			String customDelimiter = m.group(1);
-			return m.group(2).split(customDelimiter);
-		}
-		return input.split(",|:");
-	}
+    public static int splitAndSum(String input) {
+        if (input == null || input.isEmpty()) {
+            return DEFAULT_INT;
+        }
+        String[] splitInput = CalculatorUtil.splitInput(input);
+        return calculatorInput(splitInput);
+    }
 
-	private static int calculatorInput(String[] splitInput) {
-		int result = 0;
-		for (String str : splitInput) {
-			result += checkPositiveNum(str);
-		}
-		return result;
-	}
-
-	private static int checkPositiveNum(String str) {
-		int num = checkNum(str);
-		if(num < 0) {
-			throw new IllegalArgumentException("[Error] 입력 값이 양수가 아닙니다.");
-		}
-		return num;
-	}
-
-	private static int checkNum(String str) {
-		try {
-			return Integer.parseInt(str);
-		} catch (NumberFormatException e) {
-			throw new IllegalArgumentException("[Error] 입력 값이 숫자가 아닙니다.");
-		}
-	}
+    private static int calculatorInput(String[] splitInput) {
+        int result = DEFAULT_INT;
+        for (String str : splitInput) {
+            result += CalculatorUtil.checkPositiveNum(str);
+        }
+        return result;
+    }
 
 }
