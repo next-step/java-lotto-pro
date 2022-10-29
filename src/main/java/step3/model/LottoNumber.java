0b9d6bc46badb7ac30d443bbd07213e2834bf65a
yeoojin <only1year100@gmail.com>
2022-10-29 12:01:17 +0900

    refactor(LottoNumber): 로또 번호 equals, toString 오버라이드 추가

--- src/main/java/step3/model/LottoNumber.java
+++ src/main/java/step3/model/LottoNumber.java
@@ -1,10 +1,28 @@
 package step3.model;
 
 import java.util.List;
+import java.util.Objects;
 
 public class LottoNumber {
     private int number;
 
     public LottoNumber(int number) { this.number = number; }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+
+    @Override
+    public String toString() {
+        return "" + number;
+    }
 }
