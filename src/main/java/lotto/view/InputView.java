4820f6896f4ce9c5d47d30358ac7a187f75f520a
deveo-pyo <jungpyo9@gmail.com>
2022-10-29 22:53:44 +0900

    refactor: 상수값 분리
    상수값 분리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,18 +7,24 @@ import java.util.stream.Collectors;
 
 public class InputView {
 
+    private static final String ASK_MESSAGE_PAY_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String ASK_MESSAGE_LAST_WINNER_NUMBERS = "\n지난 주 당첨 번호를 입력해 주세요.";
+    private static final String WINNER_NUMBERS_DELIMITER = ",";
+    private static final String WHITE_SPACE = " ";
+    private static final String EMPTY_STRING = "";
+
     public static int inputPayAmount(){
         Scanner sc = new Scanner(System.in);
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(ASK_MESSAGE_PAY_AMOUNT);
         return sc.nextInt();
     }
 
     public static List<Integer> inputLottoWinnerNumbers(){
         Scanner sc = new Scanner(System.in);
-        System.out.println("\n지난 주 당첨 번호를 입력해 주세요.");
+        System.out.println(ASK_MESSAGE_LAST_WINNER_NUMBERS);
         String input = sc.nextLine();
-        input = input.replaceAll(" ", "");
-        String[] winnerNumbers = input.split(",");
+        input = input.replaceAll(WHITE_SPACE, EMPTY_STRING);
+        String[] winnerNumbers = input.split(WINNER_NUMBERS_DELIMITER);
         return Arrays.stream(winnerNumbers).map(Integer::parseInt).collect(Collectors.toList());
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,27 +1,32 @@
 package lotto.view;
 
-import lotto.Lotto;
-import lotto.LottoResult;
+import lotto.domain.Lotto;
+import lotto.domain.LottoResult;
 
-import static lotto.LottoResult.PRIZE_MONEY;
+import static lotto.domain.LottoResult.PRIZE_MONEY;
 
 public class OutputView {
+
+    private static final String OUT_MESSAGE_LOTTO_AMOUNT = "개를 구매했습니다.";
+    private static final String OUT_MESSAGE_RESULT_STATISTICS = "당첨 통계";
+    private static final String OUT_MESSAGE_DIVIDER_LINE = "----------";
+    private static final String OUT_MESSAGE_LOTTO_RESULT = "%d개 일치(%d)- %d개\n";
+    private static final String OUT_MESSAGE_PROFIT_RATIO = "총 수익률은 %.2f 입니다.";
 
     public static void printLottos(Lotto lotto){
         System.out.println(lotto);
     }
 
     public static void printLottoAmount(int lottoAmount){
-        System.out.println(lottoAmount+"개를 구매했습니다.");
+        System.out.println(lottoAmount+OUT_MESSAGE_LOTTO_AMOUNT);
     }
 
     public static void printLottoResult(LottoResult lottoResult){
-        System.out.println("당첨 통계");
-        System.out.println("------------");
+        System.out.println(OUT_MESSAGE_RESULT_STATISTICS);
+        System.out.println(OUT_MESSAGE_DIVIDER_LINE);
         for(int i=3; i< PRIZE_MONEY.length; i++){
-            System.out.println(i + "개 일치(" + PRIZE_MONEY[i] + ")- "
-                    + lottoResult.getLottoResult(i) + "개");
+            System.out.printf(OUT_MESSAGE_LOTTO_RESULT, i, PRIZE_MONEY[i], lottoResult.getLottoResult(i));
         }
-        System.out.println("총 수익률은 " + lottoResult.calculateProfitRatio() + "입니다.");
+        System.out.printf(OUT_MESSAGE_PROFIT_RATIO, lottoResult.calculateProfitRatio());
     }
 }
