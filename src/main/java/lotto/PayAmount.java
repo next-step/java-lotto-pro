a392e5398668bf46da9aff7729a969f6ae9d3c0a
victor-jo <joasjn45@gmail.com>
2022-10-29 22:28:04 +0900

    feat(lottoStore): 로또 상점 추가
    - 지불금액이 오면 지불금액 만큼 로또를 구입하는 기능 추가

---
+++ src/main/java/lotto/PayAmount.java
@@ -0,0 +1,17 @@
+package lotto;
+
+public class PayAmount {
+
+    private long payAmount;
+    public PayAmount(long payAmount) {
+        this.payAmount = payAmount;
+    }
+
+    public boolean payable(int lottoAmount) {
+        return this.payAmount >= lottoAmount;
+    }
+
+    public void pay(int lottoAmount) {
+        this.payAmount -= lottoAmount;
+    }
+}
