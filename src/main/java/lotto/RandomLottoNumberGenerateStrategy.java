6fc937d5dd98c1cda73ee4e3aa403ddc174d4c18
victor-jo <joasjn45@gmail.com>
2022-10-29 22:27:16 +0900

    refactor(randomLottoNumberStrategy): 자동 로또번호 지정 전략 리팩터링
    - 생성시마다 새로운 번호 배열을 활용할 수 있도록 수정
    
    before:
        private static 으로 선언된 후보 숫자군
    after:
        static 제거하여 private 으로 생성시에 항상 새로운 후보군으로 로또번호 생성

--- src/main/java/lotto/RandomLottoNumberGenerateStrategy.java
+++ src/main/java/lotto/RandomLottoNumberGenerateStrategy.java
@@ -9,11 +9,12 @@ public class RandomLottoNumberGenerateStrategy implements LottoNumberGenerateStr
 
     private static final int START_INCLUSIVE = 1;
     private static final int END_EXCLUSIVE = 46;
-    private static final LinkedList<Integer> candidateLottoNumbers =
+    private static final String NO_LOTTO_NUMBER = "생성에 사용할 수 있는 로또 번호가 없습니다.";
+
+    private final LinkedList<Integer> candidateLottoNumbers =
         IntStream.range(START_INCLUSIVE, END_EXCLUSIVE)
             .boxed()
             .collect(Collectors.toCollection(LinkedList::new));
-    private static final String NO_LOTTO_NUMBER = "생성에 사용할 수 있는 로또 번호가 없습니다.";
 
     @Override
     public LottoNumber generate() {
