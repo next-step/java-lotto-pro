74afa3a895becf6bdc8387b511fb712620b9505b
Hyeongwon-up <leehwdatabase@gmail.com>
2022-10-30 04:04:58 +0900

    refactor(Lotto.java):
    - 같은 로또 도메인끼리 numbers 의 엘리먼트 를 비교하여 일치 개수 확인하는 countMatchingNumber 메서드 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -11,8 +11,26 @@ public class Lotto {
     numbers = LottoGenerator.generateLottoNumbers();
   }
 
+  public Lotto(List<Integer> numbers) {
+    this.numbers = numbers;
+  }
+
   public List<Integer> getNumbers() {
     return this.numbers;
+  }
+
+  public int countMatchingNumber(Lotto targetLotto) {
+    int count = 0;
+
+    List<Integer> targetLottoNumber = targetLotto.getNumbers();
+
+    for (Integer i : targetLottoNumber) {
+      if (this.numbers.contains(i)) {
+        count++;
+      }
+    }
+
+    return count;
   }
 
 }
--- src/test/java/step3/LottoTest.java
+++ src/test/java/step3/LottoTest.java
@@ -4,6 +4,7 @@ package step3;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import java.util.List;
 import lotto.model.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,6 +24,18 @@ public class LottoTest {
             number -> assertThat(number).isBetween(1, 45))
     );
 
+  }
+
+  @Test
+  @DisplayName("로또끼리 비교를 하여 동일한 값의 개수를 주는지 확인")
+  void 로또_비교() {
+    //given
+    Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+    Lotto targetLotto = new Lotto(List.of(4, 5, 6, 7, 8, 9));
+    //when
+    int count = lotto.countMatchingNumber(targetLotto);
+    //then
+    assertThat(count).isEqualTo(3);
   }
 
 }
