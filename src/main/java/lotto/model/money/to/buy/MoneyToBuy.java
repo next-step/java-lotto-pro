a9d865d5c3d18792289ef491da8a8a9e782742c9
OptimistLabyrinth <keesung0705@gmail.com>
2022-10-29 21:41:32 +0900

    feat: 구입 금액 입력값에서 로또 구입 금액으로 나눈 나머지는 제거한다
    테스트 코드도 함께 작성

--- src/main/java/lotto/model/money/to/buy/MoneyToBuy.java
+++ src/main/java/lotto/model/money/to/buy/MoneyToBuy.java
@@ -4,18 +4,19 @@ package lotto.model.money.to.buy;
  * 로또 구매를 위해 입력한 금액을 저장하는 객체
  */
 public class MoneyToBuy {
-    private int money;
+    protected int money;
 
     public MoneyToBuy(String input) {
+        int inputMoney;
         try {
-            money = Integer.parseInt(input);
+            inputMoney = Integer.parseInt(input);
         } catch (Exception e) {
             throw new NumberFormatException("구입 금액은 반드시 숫자여야 합니다.");
         }
-
-        if (isNegative(money)) {
+        if (isNegative(inputMoney)) {
             throw new NumberFormatException("구입 금액은 음수가 될 수 없습니다.");
         }
+        money = inputMoney / 1000 * 1000;
     }
 
     private boolean isNegative(int input) {
--- src/test/java/lotto/model/money/to/buy/MoneyToBuyTest.java
+++ src/test/java/lotto/model/money/to/buy/MoneyToBuyTest.java
@@ -3,13 +3,26 @@ package lotto.model.money.to.buy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class MoneyToBuyTest {
+
+    class MoneyToBuyForTest extends MoneyToBuy {
+        public MoneyToBuyForTest(String input) {
+            super(input);
+        }
+
+        public boolean isMoneySameWith(int money) {
+            return this.money == money;
+        }
+    }
+
     @Nested
     @DisplayName("MoneyToBuy 생성자 테스트")
     class Constructor {
@@ -17,7 +30,7 @@ class MoneyToBuyTest {
         @ValueSource(ints = {0, 1, 2, 1000, 14000, Integer.MAX_VALUE})
         @DisplayName("구입 금액이 0 이상의 양수이면 MoneyToBuy 객체 생성 성공")
         void success(int value) {
-            String input = String.valueOf(value);
+            final String input = String.valueOf(value);
             assertDoesNotThrow(() -> new MoneyToBuy(input));
         }
 
@@ -25,7 +38,7 @@ class MoneyToBuyTest {
         @ValueSource(ints = {Integer.MIN_VALUE, -96000, -1000, -2, -1})
         @DisplayName("구입 금액이 음수이면 MoneyToBuy 객체 생성 실패")
         void errorInputNegative(int value) {
-            String input = String.valueOf(value);
+            final String input = String.valueOf(value);
             assertThrows(NumberFormatException.class, () -> new MoneyToBuy(input));
         }
 
@@ -41,6 +54,27 @@ class MoneyToBuyTest {
         @DisplayName("구입 금액 입력값이 숫자가 아니면 MoneyToBuy 객체 생성 실패")
         void errorInputNotDigit(String input) {
             assertThrows(NumberFormatException.class, () -> new MoneyToBuy(input));
+        }
+    }
+
+    @Nested
+    @DisplayName("MoneyToBuy 클래스 money 값 확인")
+    class MoneyAfterDiscardingRemainder {
+        @ParameterizedTest
+        @ValueSource(ints = {0, 1, 2, 999})
+        @DisplayName("사용자가 입력한 구입 금액이 1000 보다 작으면 money 값은 0 이 된다")
+        void moneyZero(int value) {
+            final String input = String.valueOf(value);
+            final MoneyToBuyForTest moneyToBuyForTest = new MoneyToBuyForTest(input);
+            assertThat(moneyToBuyForTest.isMoneySameWith(0)).isTrue();
+        }
+
+        @ParameterizedTest
+        @CsvSource(value = {"1000,1000", "2500,2000", "987654321,987654000"}, delimiter = ',')
+        @DisplayName("사용자 입력한 구입 금액에서 나머지를 제거한 값이 money 가 된다")
+        void moneySame(String input, int money) {
+            final MoneyToBuyForTest moneyToBuyForTest = new MoneyToBuyForTest(input);
+            assertThat(moneyToBuyForTest.isMoneySameWith(money)).isTrue();
         }
     }
 }
