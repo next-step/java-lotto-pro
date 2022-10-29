3847c99e62fff4ba32fd820a67b3f66137e65bbd
deveo-pyo <jungpyo9@gmail.com>
2022-10-29 19:21:06 +0900

    feat: 로또 도메인 구현
    중복되지않는 1~45 숫자 중 6개를 갖는 로또 도메인
    테스트 코드 구현
    1) 중복되지 않은 로또 번호 6개 생성 테스트
    2) 적중한 로또번호 갯수 리턴 테스트

---
+++ src/main/java/lotto/Lotto.java
@@ -0,0 +1,42 @@
+package lotto;
+
+import java.util.*;
+
+public class Lotto {
+
+    private final List<Integer> lottoNumbers;
+
+    public Lotto(){
+        lottoNumbers = generateUniqueLottoNumbers();
+    }
+
+    public Lotto(List<Integer> lottoNumbers){
+        this.lottoNumbers = lottoNumbers;
+    }
+
+    public int countCollectNumber(List<Integer> winnerNumbers) {
+        int result = 0;
+        for(int winnerNumber : winnerNumbers){
+            if(lottoNumbers.contains(winnerNumber)){
+                result++;
+            }
+        }
+        return result;
+    }
+
+    public List<Integer> generateUniqueLottoNumbers(){
+        List<Integer> lottoRange = new ArrayList<>();
+        for(int i=1; i<=45; i++){
+            lottoRange.add(i);
+        }
+        Collections.shuffle(lottoRange);
+        List<Integer> lottoNumbers = lottoRange.subList(0, 6);
+        lottoRange.subList(0, 6).sort(Comparator.comparingInt(i -> i));
+        return lottoNumbers;
+    }
+
+    @Override
+    public String toString() {
+        return lottoNumbers.toString();
+    }
+}
