31cc08666991e225640b2260bcd50742fd76e191
yeoojin <only1year100@gmail.com>
2022-10-29 10:52:25 +0900

    refactor(Lottos): 로또번호 List<Integer>에서 LottoNumber 객체로 변경

--- src/main/java/step3/model/Lotto.java
+++ src/main/java/step3/model/Lotto.java
@@ -1,15 +1,31 @@
 package step3.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import static step3.constant.Constant.MIN_LOTTO_NUMBER;
+import static step3.constant.Constant.MAX_LOTTO_NUMBER;
 public class Lotto {
-    private List<Integer> lottoNumbers;
-    
+    private List<LottoNumber> lottoNumbers;
+    private List<LottoNumber> rangeNumbers;
+
     public void setLottoNumbers() {
         lottoNumbers = generateRandomNumbers();
     }
 
-    private List<Integer> generateRandomNumbers() {
+    private List<LottoNumber> generateRandomNumbers() {
+        rangeNumbers = setRangeNumbers();
 
+
+
+        return lottoNumbers;
+    }
+
+    private List<LottoNumber> setRangeNumbers() {
+        List<LottoNumber> numbers = new ArrayList<>();
+        for(int i = MIN_LOTTO_NUMBER; i <= MAX_LOTTO_NUMBER; i++) {
+            numbers.add(new LottoNumber(i));
+        }
+        return numbers;
     }
 }
---
+++ src/main/java/step3/model/LottoNumber.java
@@ -0,0 +1,10 @@
+package step3.model;
+
+import java.util.List;
+
+public class LottoNumber {
+    private int number;
+
+    public LottoNumber(int number) { this.number = number; }
+
+}
