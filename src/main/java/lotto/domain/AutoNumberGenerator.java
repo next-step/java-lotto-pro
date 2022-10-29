c8a71e5dda60fb23ccd38803a1ff0b25906208a6
crongcm <ucm5459@kakao.com>
2022-10-30 01:10:55 +0900

    refactor: 매직넘버 수정

--- src/main/java/lotto/domain/AutoNumberGenerator.java
+++ src/main/java/lotto/domain/AutoNumberGenerator.java
@@ -7,13 +7,16 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class AutoNumberGenerator implements LottoNumberGenerator {
+    private static final int START_INDEX = 0;
+    private static final int LOTTO_LOTTERY_NUMBER_SIZE = 6;
+
     @Override
     public Set<LottoNumber> generate() {
         List<Integer> lottoNumbers = IntStream.rangeClosed(LottoNumber.MINIMUM_LOTTO_NUMBER, LottoNumber.MAXIMUM_LOTTO_NUMBER)
                 .boxed()
                 .collect(Collectors.toList());
         Collections.shuffle(lottoNumbers);
-        return lottoNumbers.subList(0, 6).stream()
+        return lottoNumbers.subList(START_INDEX, LOTTO_LOTTERY_NUMBER_SIZE).stream()
                 .map(LottoNumber::new)
                 .collect(Collectors.toSet());
     }
