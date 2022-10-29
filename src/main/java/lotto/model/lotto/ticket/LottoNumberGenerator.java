4db2d3d2b281d9fe9c57452d0b475c69b70d07a1
OptimistLabyrinth <keesung0705@gmail.com>
2022-10-29 23:29:42 +0900

    refactor: 불필요하게 리스트 정렬하는 부분 개선

--- src/main/java/lotto/model/lotto/ticket/LottoNumberGenerator.java
+++ src/main/java/lotto/model/lotto/ticket/LottoNumberGenerator.java
@@ -2,7 +2,6 @@ package lotto.model.lotto.ticket;
 
 import java.util.List;
 import java.util.Random;
-import java.util.stream.Collectors;
 
 /**
  * 1 이상 45 이하의 번호 중에서 무작위 번호를 하나 선택하는 객체이다.
@@ -11,7 +10,7 @@ public class LottoNumberGenerator {
     private final List<Integer> lottoNumberCandidates;
 
     public LottoNumberGenerator(List<Integer> lottoNumberCandidates) {
-        this.lottoNumberCandidates = lottoNumberCandidates.stream().sorted().collect(Collectors.toList());
+        this.lottoNumberCandidates = lottoNumberCandidates;
     }
 
     public int generate() {
