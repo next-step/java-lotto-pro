95b75bc8733668136d68216be4f915c207d7562e
namhyun.sung <namhyun.sung@yanolja.com>
2022-10-29 20:45:07 +0900

    fix(Lotto): 로또 객체 생성 시 number 객체 적용

--- src/test/java/step3/LottoTest.java
+++ src/test/java/step3/LottoTest.java
@@ -6,17 +6,19 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class LottoTest {
     @Test
     @DisplayName("로또 6자리를 저장한다.")
     void save_lotto_numbers_test() {
-        List<Integer> numbers = new ArrayList<>();
-        numbers.add(1);
-        numbers.add(12);
-        numbers.add(23);
-        numbers.add(34);
-        numbers.add(43);
-        numbers.add(44);
+        List<Number> numbers = new ArrayList<>();
+        numbers.add(new Number(1));
+        numbers.add(new Number(12));
+        numbers.add(new Number(23));
+        numbers.add(new Number(34));
+        numbers.add(new Number(43));
+        numbers.add(new Number(44));
 
         Lotto lotto = new Lotto(numbers);
 
