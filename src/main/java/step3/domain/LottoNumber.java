6be800421aed119b4cee23f614d5588602ff2edb
gunkim <gunkim.dev@gmail.com>
2022-10-29 19:16:41 +0900

    feat: LottoNumber 테스트 케이스 추가 구현

--- src/main/java/step3/domain/LottoNumber.java
+++ src/main/java/step3/domain/LottoNumber.java
@@ -4,7 +4,8 @@ import java.util.Objects;
 
 public class LottoNumber {
 
-    private static final int MINIMUM_NUMBER = 0;
+    private static final int MINIMUM_NUMBER = 1;
+    private static final int MAXIMUM_NUMBER = 45;
 
     private final int value;
 
@@ -30,9 +31,14 @@ public class LottoNumber {
         return Objects.hash(value);
     }
 
+    @Override
+    public String toString() {
+        return String.valueOf(value);
+    }
+
     private void validation(int value) {
-        if (value < MINIMUM_NUMBER) {
-            throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
+        if (value < MINIMUM_NUMBER || value > MAXIMUM_NUMBER) {
+            throw new IllegalArgumentException(String.format("로또 번호는 1 ~ 45 사이 숫자여야 합니다. (lottoNumber: %s)", value));
         }
     }
 }
