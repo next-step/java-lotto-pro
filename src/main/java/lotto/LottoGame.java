e5e4179a8c9e9f6d5932bb8e8dbbd6f4a872a7d2
deveo-pyo <jungpyo9@gmail.com>
2022-10-29 20:19:01 +0900

    feat: 로또게임 도메인 구현
    1. 구매한 금액으로 구매가능한 로또 갯수만큼 로또 생성하는 로또게임 도메인 구현
    2. 로또 당첨번호를 입력받아 로또 N개의 결과 생성 메소드 구현
    3. 테스트 구현
      1) 구매 가능 로또 갯수 테스트

---
+++ src/main/java/lotto/LottoGame.java
@@ -0,0 +1,41 @@
+package lotto;
+
+import lotto.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoGame {
+
+    private final int payAmount;
+    List<Lotto> lottos = new ArrayList<>();
+    private LottoResult lottoResult;
+
+    public LottoGame(int payAmount){
+        this.payAmount = payAmount;
+        int lottoAmount = calculateLottoAmount();
+        OutputView.printLottoAmount(lottoAmount);
+
+        for(int i=0; i<lottoAmount; i++) {
+            Lotto lotto = new Lotto();
+            lottos.add(lotto);
+            OutputView.printLottos(lotto);
+        }
+    }
+
+    public int calculateLottoAmount(){
+        return payAmount/1000;
+    }
+
+    public void findWinner(List<Integer> winnerNumbers){
+        lottoResult = new LottoResult(lottos.size());
+        for(Lotto lotto : lottos){
+            int collectNumber = lotto.countCollectNumber(winnerNumbers);
+            lottoResult.putLottoResult(collectNumber);
+        }
+    }
+
+    public void printResult(){
+        OutputView.printLottoResult(lottoResult);
+    }
+}
---
+++ src/test/java/lotto/LottoGameTest.java
@@ -0,0 +1,18 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LottoGameTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"1000,1", "5000,5"}, delimiter = ',')
+    @DisplayName("로또 구매 가능 갯수 테스트")
+    public void calculateLottoAmountTest(int payAmount, int lottoAmount){
+        LottoGame lottoGame = new LottoGame(payAmount);
+        assertThat(lottoGame.calculateLottoAmount()).isEqualTo(lottoAmount);
+    }
+}
