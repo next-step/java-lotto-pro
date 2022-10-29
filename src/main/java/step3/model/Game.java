df5de54ba452199bad76ad40f1f404e875e1c2b0
heonseopShin <shinsub6120@gmail.com>
2022-10-29 15:26:38 +0900

    feat : 로또 추첨 기능 구현

--- src/main/java/step3/model/Game.java
+++ src/main/java/step3/model/Game.java
@@ -2,9 +2,21 @@ package step3.model;
 
 import step3.constant.ErrorMessageConstant;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 class Game {
     private static final int LOTTO_ONE_GAME_MONEY = 1000;
     private final int lottoBuyCount;
+    private final List<Integer> lottoCandidateNumbers = new ArrayList<>();
+    private final List<List<Integer>> lottoResult;
+
+    public Game(int count) {
+        this.lottoBuyCount = count;
+        initLottoCandidateNumbers();
+        this.lottoResult = new ArrayList<>();
+    }
 
     public Game(String money) {
         if (money == null || money.isEmpty()) {
@@ -12,6 +24,14 @@ class Game {
         }
 
         this.lottoBuyCount = getLottoBuyCount(convertNumber(money));
+        initLottoCandidateNumbers();
+        this.lottoResult = new ArrayList<>();
+    }
+
+    private void initLottoCandidateNumbers() {
+        for (int i = 1; i < 46; i++) {
+            this.lottoCandidateNumbers.add(i);
+        }
     }
 
     private int getLottoBuyCount(int money) {
@@ -33,5 +53,14 @@ class Game {
 
     public int getLottoBuyCount() {
         return lottoBuyCount;
+    }
+
+    public List<List<Integer>> getLottoResult() {
+        for (int i = 0; i < lottoBuyCount; i++) {
+            Collections.shuffle(this.lottoCandidateNumbers);
+            this.lottoResult.add(this.lottoCandidateNumbers.subList(0, 6));
+            Collections.sort(this.lottoResult.get(i));
+        }
+        return this.lottoResult;
     }
 }
--- src/test/java/step3/model/GameTest.java
+++ src/test/java/step3/model/GameTest.java
@@ -1,14 +1,29 @@
 package step3.model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class GameTest {
+
+    List<Integer> candidateLottoNumbers = new ArrayList<>();
+
+    @BeforeEach
+    void before() {
+        for (int i = 1; i < 46; i++) {
+            candidateLottoNumbers.add(i);
+        }
+    }
+
     @DisplayName("입력된_금액에_맞추어_로또_구매_개수_조회")
     @ParameterizedTest
     @CsvSource(value = {"1000:1", "10000:10", "0:0"}, delimiter = ':')
@@ -33,4 +48,54 @@ public class GameTest {
         assertThatThrownBy(() -> new Game(money))
                 .isInstanceOf(NumberFormatException.class);
     }
+
+
+    @DisplayName("로또_게임_결과는_게임_수와_동일")
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "10"})
+    public void getLottoResult_pass_01(int count) {
+        Game game = new Game(count);
+        List<List<Integer>> result = game.getLottoResult();
+
+        assertThat(result.size()).isEqualTo(count);
+    }
+
+    @DisplayName("로또_게임_결과값은_1과_45_사이의_값")
+    @ParameterizedTest
+    @ValueSource(strings = {"1000", "2000"})
+    public void getLottoResult_pass_02(String money) {
+        Game game = new Game(money);
+        List<List<Integer>> result = game.getLottoResult();
+
+        for (List<Integer> lotto : result) {
+            assertThat(candidateLottoNumbers).containsAll(lotto);
+        }
+    }
+
+    @DisplayName("로또_게임_결과값은_숫자_기준으로_정렬되어야_한다")
+    @ParameterizedTest
+    @ValueSource(strings = {"1000", "2000"})
+    public void getLottoResult_pass_03(String money) {
+        Game game = new Game(money);
+        List<List<Integer>> result = game.getLottoResult();
+
+        for (List<Integer> lotto : result) {
+            List<Integer> beforeData = new ArrayList<>(lotto);
+            Collections.sort(lotto);
+            assertThat(lotto).isEqualTo(beforeData);
+        }
+    }
+
+    @DisplayName("로또_게임_결과값은_0미만_46이상은_없어야_한다")
+    @ParameterizedTest
+    @ValueSource(strings = {"1000", "2000"})
+    public void getLottoResult_fail_01(String money) {
+        Game game = new Game(money);
+        List<List<Integer>> result = game.getLottoResult();
+
+        for (List<Integer> lotto : result) {
+            assertThat(lotto).doesNotContain(0, 46);
+        }
+    }
+
 }
