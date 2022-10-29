32bea43e811671ab57bd43488ded96fd4f227545
지영진 <youngjin.jee@nhn.com>
2022-10-29 14:39:59 +0900

    feat: 로또 몇 개 샀는지 출력

--- src/main/java/study/step3/ResultView.java
+++ src/main/java/study/step3/ResultView.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class ResultView {
     public static void printLottos(List<Lotto> lottoList) {
+        System.out.printf("%d개를 구매했습니다.\n", lottoList.size());
         for (Lotto lotto : lottoList) {
             System.out.println(lotto);
         }
