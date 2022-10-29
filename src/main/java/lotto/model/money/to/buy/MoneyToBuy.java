7ebcac1b3649fc248c67f48f328d195d2aeadb11
OptimistLabyrinth <keesung0705@gmail.com>
2022-10-30 04:52:23 +0900

    feat: 수익률 계산하는 메서드 구현
    MoneyToBuy 클래스 profitRatio 메서드 내부 로직 구현
    
    단위 테스트 작성

--- src/main/java/lotto/model/money/to/buy/MoneyToBuy.java
+++ src/main/java/lotto/model/money/to/buy/MoneyToBuy.java
@@ -1,11 +1,15 @@
 package lotto.model.money.to.buy;
 
 import lotto.constant.numbers.LottoConstant;
+import lotto.model.lotto.ticket.LottoTicketsBucket;
+
+import java.text.DecimalFormat;
 
 /**
  * 로또 구매를 위해 입력한 금액을 저장하는 객체
  */
 public class MoneyToBuy {
+    private static final DecimalFormat decimalFormat = new DecimalFormat("0.00");
     protected int money;
     private double profitRatio;
 
@@ -22,11 +26,20 @@ public class MoneyToBuy {
         money = inputMoney / LottoConstant.PRICE_OF_SINGLE_LOTTO_TICKET * LottoConstant.PRICE_OF_SINGLE_LOTTO_TICKET;
     }
 
+    public MoneyToBuy(int money) {
+        this.money = money;
+    }
+
     private boolean isNegative(int input) {
         return input < 0;
     }
 
     public int numberOfAffordableLottoTickets() {
         return money / LottoConstant.PRICE_OF_SINGLE_LOTTO_TICKET;
+    }
+
+    public String profitRatio(LottoTicketsBucket lottoTicketsBucket) {
+        profitRatio = (double) lottoTicketsBucket.sumProfit() / money;
+        return decimalFormat.format(profitRatio);
     }
 }
