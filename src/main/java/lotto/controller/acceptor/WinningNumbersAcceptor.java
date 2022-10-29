be6526918afe2eb7dfdb0c4e0408093a7f5f7aba
OptimistLabyrinth <keesung0705@gmail.com>
2022-10-30 01:28:35 +0900

    feat: UI 에서 사용자에게 당첨 번호를 입력 받는다
    입력 받은 결과를 WinningNumbers 타입 객체로 래핑해서 반환한다

--- src/main/java/lotto/controller/acceptor/WinningNumbersAcceptor.java
+++ src/main/java/lotto/controller/acceptor/WinningNumbersAcceptor.java
@@ -1,7 +1,16 @@
 package lotto.controller.acceptor;
 
+import lotto.model.winning.numbers.WinningNumbers;
+
+import java.util.Scanner;
+
 /**
  * UI 에서 당첨 번호 6 개를 입력 받는 객체이다.
  */
 public class WinningNumbersAcceptor {
+    public WinningNumbers accept() {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        return new WinningNumbers(input);
+    }
 }
--- src/main/java/lotto/model/winning/numbers/WinningNumbers.java
+++ src/main/java/lotto/model/winning/numbers/WinningNumbers.java
@@ -4,4 +4,6 @@ package lotto.model.winning.numbers;
  * 당첨 번호 6 개를 저장하는 객체이다.
  */
 public class WinningNumbers {
+    public WinningNumbers(String input) {
+    }
 }
