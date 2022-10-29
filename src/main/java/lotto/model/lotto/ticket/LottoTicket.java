9203ef95a6dfac2009643ab05dea9201d1ca7c72
OptimistLabyrinth <keesung0705@gmail.com>
2022-10-29 22:18:57 +0900

    feat: 로또 한 장을 만들어내는 로직 구현
    LottoTicket 클래스 구현

--- src/main/java/lotto/model/lotto/ticket/LottoTicket.java
+++ src/main/java/lotto/model/lotto/ticket/LottoTicket.java
@@ -1,7 +1,20 @@
 package lotto.model.lotto.ticket;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 자동 생성한 6 개의 로또 번호 한 줄을 저장하고 관리하는 객체이다.
  */
 public class LottoTicket {
+    private static final int COUNT_OF_NUMBERS_IN_SINGLE_LOTTO_TICKET = 6;
+    private final List<Integer> numbers;
+
+    public LottoTicket() {
+        this.numbers = new ArrayList<>(COUNT_OF_NUMBERS_IN_SINGLE_LOTTO_TICKET);
+        final LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
+        for (int i = 0; i < COUNT_OF_NUMBERS_IN_SINGLE_LOTTO_TICKET; ++i) {
+            numbers.add(lottoNumberGenerator.generate());
+        }
+    }
 }
