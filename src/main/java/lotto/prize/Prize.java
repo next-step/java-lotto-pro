c13cf242279b78078ceff0636eb97dab7ecece21
lunatic <lsklsk45@naver.com>
2022-10-29 23:56:29 +0900

    refactor(Prize) : 일치하는 로또 개수와 보너스 볼 일치 여부를 토대로 몇 등인지 반환하는 기능(Prize)

--- src/main/java/lotto/prize/Prize.java
+++ src/main/java/lotto/prize/Prize.java
@@ -1,27 +1,42 @@
 package lotto.prize;
 
 import java.math.BigDecimal;
-import java.util.Collections;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Arrays;
 
 public enum Prize {
     FIRST(6, BigDecimal.valueOf(2000000000)),
-    SECOND(5, BigDecimal.valueOf(1500000)),
-    THIRD(4, BigDecimal.valueOf(50000)),
-    FOURTH(3, BigDecimal.valueOf(5000));
+    SECOND(5, BigDecimal.valueOf(30000000)),
+    THIRD(5, BigDecimal.valueOf(1500000)),
+    FOURTH(4, BigDecimal.valueOf(50000)),
+    FIFTH(3, BigDecimal.valueOf(5000)),
+    MISS(0, BigDecimal.ZERO);
 
     private final int matchCount;
     private final BigDecimal prizeMoney;
-    public static final Map<Integer, Prize> matchCounts =
-            Collections.unmodifiableMap(Stream.of(values())
-                    .collect(Collectors.toMap(Prize::getMatchCount, Function.identity())));
 
     Prize(int matchCount, BigDecimal prizeMoney) {
         this.matchCount = matchCount;
         this.prizeMoney = prizeMoney;
+    }
+
+    public static Prize prizeOf(int matchCount, boolean isBonusMatch) {
+        return Arrays.stream(Prize.values())
+                .filter(v -> v.matchCount == matchCount)
+                .map(v -> {
+                    if (Prize.THIRD.matchCount == matchCount) {
+                        return Prize.checkThirdOrSecond(matchCount, isBonusMatch);
+                    }
+                    return v;
+                })
+                .findFirst()
+                .orElse(Prize.MISS);
+    }
+
+    private static Prize checkThirdOrSecond(int matchCount, boolean isBonusMatch) {
+        if (isBonusMatch) {
+            return Prize.SECOND;
+        }
+        return Prize.THIRD;
     }
 
     public int getMatchCount() {
