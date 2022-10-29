8e44f4f772548ade439aecf6df1117abbc9a7022
Joonseo Lee <43516757+joonseolee@users.noreply.github.com>
2022-10-29 18:08:14 +0900

    feat: 문자열 덧셈 계산기 추가 (#749)

---
+++ src/main/java/step2/StringAddCalculator.java
@@ -0,0 +1,64 @@
+package step2;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public final class StringAddCalculator {
+
+    private static final String DEFAULT_PATTERN = ",|:";
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
+    private static final int DEFAULT_SUM = 0;
+    private static final int NUMBERS_INDEX = 2;
+    private static final int DELIMITER_INDEX = 1;
+    private static final int ZERO = 0;
+
+    private StringAddCalculator() {}
+
+    public static int splitAndSum(String text) {
+        if (isNullOrEmpty(text)) {
+            return DEFAULT_SUM;
+        }
+
+        String[] numbers = split(text);
+
+        return sum(numbers);
+    }
+
+    private static boolean isNullOrEmpty(String text) {
+        return text == null || text.length() == ZERO;
+    }
+
+    private static String[] split(String text) {
+        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(text);
+        if (matcher.find()) {
+            return matcher.group(NUMBERS_INDEX).split(matcher.group(DELIMITER_INDEX));
+        }
+
+        return text.split(DEFAULT_PATTERN);
+    }
+
+    private static int sum(String[] numbers) {
+        return Arrays.stream(numbers)
+                .mapToInt(StringAddCalculator::toInt)
+                .sum();
+    }
+
+    private static int toInt(String value) {
+        int number = fromString(value);
+
+        if (number < ZERO) {
+            throw new RuntimeException("음수가 될순없습니다.");
+        }
+
+        return number;
+    }
+
+    private static int fromString(String value) {
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            throw new RuntimeException("숫자가 아닌 값이 존재합니다.");
+        }
+    }
+}
