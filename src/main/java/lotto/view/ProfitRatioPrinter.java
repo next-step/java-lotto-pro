58eafacf10536065e41b04e17cdcd869f95c1f4d
OptimistLabyrinth <keesung0705@gmail.com>
2022-10-30 05:03:12 +0900

    feat: 로또 구매를 통해서 얻은 총 수익률을 출력한다

--- src/main/java/lotto/view/ProfitRatioPrinter.java
+++ src/main/java/lotto/view/ProfitRatioPrinter.java
@@ -4,4 +4,10 @@ package lotto.view;
  * 총 수익률을 계산해서 출력한다.
  */
 public class ProfitRatioPrinter {
+    private static final String PROFILE_RATIO_MESSAGE = "총 수익률은 ";
+    private static final String FORMAL_SENTENCE_POSTFIX = "입니다.";
+
+    public static void print(String profitRatioString) {
+        System.out.println(PROFILE_RATIO_MESSAGE + profitRatioString + FORMAL_SENTENCE_POSTFIX);
+    }
 }
