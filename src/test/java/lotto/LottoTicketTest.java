383ca2dba9723d504d15b6fdbf5b1e12880d4198
cwJohnPark <pierrotcw@gmail.com>
2022-10-29 15:03:43 +0900

    refactor: 사용하지 않는 메서드 제거

--- src/test/java/lotto/LottoTicketTest.java
+++ src/test/java/lotto/LottoTicketTest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import lotto.domain.LottoTicket;
-import utils.Randoms;
 
 class LottoTicketTest {
 
@@ -63,9 +62,5 @@ class LottoTicketTest {
 			Arguments.of(Lists.newArrayList(1, 2, 3, 4, 5, 6, 7)),
 			Arguments.of(Lists.newArrayList(45, 44, 43, 42, 41, 40, 39))
 		);
-	}
-
-	private static List<Integer> generateUniqueNumbers() {
-		return Randoms.generateUniqueNumbers(1, 45, 6);
 	}
 }
