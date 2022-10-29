eee53c296193a181cc847030389e866b65d7c78d
victor-jo <joasjn45@gmail.com>
2022-10-29 22:04:17 +0900

    refactor(manualLottoNumberStrategy): 수동 로또번호 지정 전략 리팩터링
    - 여러 자리의 숫자를 받아서 지정하고 발생시키는 수만큼의 로또 번호가 없을 경우 에러 처리
    
    before:
        하나의 숫자만 받아서 그대로 전달하는 기능
    after:
        여러개의 숫자를 넣어서 하나씩 전달하는 기능

--- src/main/java/lotto/ManualLottoNumberGenerateStrategy.java
+++ src/main/java/lotto/ManualLottoNumberGenerateStrategy.java
@@ -1,15 +1,22 @@
 package lotto;
 
+import java.util.Iterator;
+import java.util.List;
+
 public class ManualLottoNumberGenerateStrategy implements LottoNumberGenerateStrategy {
 
-    private final int number;
+    private final Iterator<Integer> numbersIterator;
+    private static final String NO_LOTTO_NUMBER = "생성에 사용할 수 있는 로또 번호가 없습니다.";
 
-    public ManualLottoNumberGenerateStrategy(int number) {
-        this.number = number;
+    public ManualLottoNumberGenerateStrategy(List<Integer> numbers) {
+        this.numbersIterator = numbers.iterator();
     }
 
     @Override
     public LottoNumber generate() {
-        return LottoNumber.valueOf(this.number);
+        if (!this.numbersIterator.hasNext()) {
+            throw new RuntimeException(NO_LOTTO_NUMBER);
+        }
+        return LottoNumber.valueOf(this.numbersIterator.next());
     }
 }
