81005d5af811d0d09864b6347b117ba8f7009766
ilmare-cbk <cbk104325@gmail.com>
2022-10-29 14:30:31 +0900

    refactor(Money.java): 불필요한 메소드 및 import 제거

--- src/main/java/lotto/domain/money/Money.java
+++ src/main/java/lotto/domain/money/Money.java
@@ -1,6 +1,5 @@
 package lotto.domain.money;
 
-import java.util.Objects;
 import lotto.constant.LottoConstant;
 import lotto.message.ErrorMessages;
 
@@ -24,27 +23,5 @@ public class Money {
 
     public int purchasableQuantity() {
         return (int) (this.value / LottoConstant.PRICE_OF_ONE_LOTTO);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Money money = (Money) o;
-        return Double.compare(money.value, value) == 0;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(value);
-    }
-
-    @Override
-    public String toString() {
-        return String.valueOf((int) value);
     }
 }
