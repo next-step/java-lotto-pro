99219029d81f206311b0c6f606b592f1a1021208
namhyun.sung <namhyun.sung@yanolja.com>
2022-10-29 20:43:34 +0900

    feat(Lotto, Number): 로또 객체, 숫자 객체 구현

---
+++ src/main/java/step3/Lotto.java
@@ -0,0 +1,47 @@
+package step3;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+
+public class Lotto {
+    private static final int LOTTO_NUMBER_SIZE = 6;
+
+    private List<Number> numbers;
+
+    public Lotto(List<Number> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Number> numbers) {
+        if (new HashSet<>(numbers).size() < LOTTO_NUMBER_SIZE) {
+            throw new IllegalArgumentException("중복된 숫자가 존재합니다.");
+        }
+
+        if (numbers.size() != LOTTO_NUMBER_SIZE) {
+            throw new IllegalArgumentException("로또 입력 숫자는 6개여야합니다.");
+        }
+    }
+
+    public List<Number> getNumbers() {
+        return numbers;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Lotto lotto = (Lotto) o;
+        return Objects.equals(numbers, lotto.numbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numbers);
+    }
+}
