9ee13aea3fdb3c26570c412684465c78b4fd6e90
namhyun.sung <namhyun.sung@yanolja.com>
2022-10-29 17:01:14 +0900

    test: 로또 6자리 저장 테스트 코드 작성

---
+++ src/test/java/step3/model/LottoTest.java
@@ -0,0 +1,28 @@
+package step3.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoTest {
+    @Test
+    @DisplayName("로또 6자리를 저장한다.")
+    void save_lotto_numbers_test() {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(12);
+        numbers.add(23);
+        numbers.add(34);
+        numbers.add(43);
+        numbers.add(44);
+
+        Lotto lotto = new Lotto(numbers);
+
+        assertThat(lotto).isEqualTo(new Lotto(numbers));
+    }
+}
