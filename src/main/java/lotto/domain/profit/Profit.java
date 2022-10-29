29266d3ec9bd7e80b955682b3699a489cbf00578
ilmare-cbk <cbk104325@gmail.com>
2022-10-30 05:02:08 +0900

    feat(Profit.java): 보너스 번호를 고려하여 수익률 계산 구현
    - Profit에 보너스 번호를 인스턴스 변수로 추가
    - profit의 수익률 계산 로직을 수정 (보너스 번호 고려)
    - profit의 수익률 결과 값 확인 테스트 및 손실여부 판단 테스트 추가

--- src/main/java/lotto/domain/profit/Profit.java
+++ src/main/java/lotto/domain/profit/Profit.java
@@ -2,6 +2,7 @@ package lotto.domain.profit;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import lotto.constant.LottoConstant;
 import lotto.domain.lotto.LottoNumber;
 import lotto.domain.lotto.Lottos;
@@ -10,26 +11,42 @@ import lotto.domain.win.WinRanking;
 public class Profit {
     private final Lottos lottos;
     private final List<LottoNumber> winningNumbers;
+    private final LottoNumber bonusNumber;
 
-    private Profit(Lottos lottos, List<LottoNumber> winningNumbers) {
+    private Profit(Lottos lottos, List<LottoNumber> winningNumbers, LottoNumber bonusNumber) {
         this.lottos = lottos;
         this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
     }
 
     public static Profit of(Lottos lottos, List<LottoNumber> winningNumbers) {
-        return new Profit(lottos, winningNumbers);
+        return new Profit(lottos, winningNumbers, null);
+    }
+
+    public static Profit of(Lottos lottos, List<LottoNumber> winningNumbers, LottoNumber bonusNumber) {
+        return new Profit(lottos, winningNumbers, bonusNumber);
     }
 
     public double profit() {
-        double purchasePrice = (double) lottos.size() * LottoConstant.PRICE_OF_ONE_LOTTO;
-        double winningAmount = Arrays.stream(WinRanking.values())
-                .map(this::winningAmountOf)
-                .reduce(0.0, Double::sum);
+        double purchasePrice = calculatePurchasePrice();
+        double winningAmount = calculateWinningAmount();
         return Math.floor((winningAmount / purchasePrice) * 100) / 100;
     }
 
-    private double winningAmountOf(WinRanking ranking) {
-        return (double) lottos.winningCount(winningNumbers, ranking.getMatchCount()) * ranking.getWinningMoney();
+    private double calculatePurchasePrice() {
+        return (double) lottos.size() * LottoConstant.PRICE_OF_ONE_LOTTO;
+    }
+
+    private double calculateWinningAmount() {
+        Map<WinRanking, Integer> winningCountByWinRanking = lottos.winResults(winningNumbers, bonusNumber);
+        return Arrays.stream(WinRanking.values())
+                .map(ranking -> winningAmount(
+                        winningCountByWinRanking.getOrDefault(ranking, LottoConstant.EMPTY_WINNING_COUNT), ranking))
+                .reduce(0.0, Double::sum);
+    }
+
+    private double winningAmount(int winningCount, WinRanking ranking) {
+        return (double) winningCount * ranking.getWinningMoney();
     }
 
 
