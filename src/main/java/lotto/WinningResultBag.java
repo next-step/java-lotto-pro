2cce4dc02a3fc8de174370a77865aca3dbfb8edc
Minjae Kim <minjae.kim@mytrum.com>
2022-10-30 00:34:25 +0900

    feat: 당첨 결과 목록 구현
    * 속성
      * 당첨 결과 목록
    * 역할
      * 당첨 결과 목록 일치 카운트 제공
      * 수익률 제공

---
+++ src/main/java/lotto/WinningResultBag.java
@@ -0,0 +1,30 @@
+package lotto;
+
+import java.util.List;
+
+import static lotto.LottoIssuer.LOTTO_PRICE;
+
+public class WinningResultBag {
+
+    public static final int MATCH_THREE_REWARD = 5000;
+    public static final int MATCH_FOUR_REWARD = 50000;
+    public static final int MATCH_FIVE_REWARD = 1500000;
+    public static final int MATCH_SIX_REWARD = 2000000000;
+    List<WinningResult> results;
+
+    public WinningResultBag(List<WinningResult> results) {
+        this.results = results;
+    }
+
+    public long countByWinningResult(WinningResult winningResult) {
+        return results.stream().filter(result -> result.equals(winningResult)).count();
+    }
+
+    public double calculateProfitRate() {
+        long reward = MATCH_THREE_REWARD * countByWinningResult(WinningResult.MATCH_THREE)
+                + MATCH_FOUR_REWARD * countByWinningResult(WinningResult.MATCH_FOUR)
+                + MATCH_FIVE_REWARD * countByWinningResult(WinningResult.MATCH_FIVE)
+                + MATCH_SIX_REWARD * countByWinningResult(WinningResult.MATCH_SIX);
+        return reward / ((double) LOTTO_PRICE * results.size());
+    }
+}
---
+++ src/test/java/lotto/WinningResultBagTest.java
@@ -0,0 +1,45 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static lotto.WinningResult.MATCH_FIVE;
+import static lotto.WinningResult.MATCH_FOUR;
+import static lotto.WinningResult.MATCH_THREE;
+import static lotto.WinningResultBag.MATCH_FIVE_REWARD;
+import static lotto.WinningResultBag.MATCH_FOUR_REWARD;
+import static lotto.WinningResultBag.MATCH_THREE_REWARD;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.ParameterizedTest.DEFAULT_DISPLAY_NAME;
+
+class WinningResultBagTest {
+
+    private static final int LOTTO_PRICE = 1000;
+
+    @ParameterizedTest(name = "당첨 결과 목록 제공 " + DEFAULT_DISPLAY_NAME)
+    @CsvSource(value = { "MATCH_THREE:1", "MATCH_FOUR:1", "MATCH_FIVE:1" }, delimiter = ':')
+    void countByWinningResult_results_success(WinningResult winningResult, int matchCount) {
+        //given:
+        WinningResultBag resultBag = new WinningResultBag(Arrays.asList(MATCH_THREE, MATCH_FOUR, MATCH_FIVE));
+        //when then:
+        assertThat(resultBag.countByWinningResult(winningResult)).isEqualTo(matchCount);
+    }
+
+    @DisplayName("수익률 제공")
+    @Test
+    void calculateProfitRate_results_success() {
+        //given:
+        List<WinningResult> winningResultList = Arrays.asList(MATCH_THREE, MATCH_FOUR, MATCH_FIVE);
+        double result = (double) (MATCH_THREE_REWARD + MATCH_FOUR_REWARD + MATCH_FIVE_REWARD) /
+                (LOTTO_PRICE * winningResultList.size());
+        //when:
+        WinningResultBag resultBag = new WinningResultBag(winningResultList);
+        //then:
+        assertThat(resultBag.calculateProfitRate()).isEqualTo(result);
+    }
+}
