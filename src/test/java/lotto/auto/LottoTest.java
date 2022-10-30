298ad52e5c54a018fc1bd25e43a9a9b65d1d4a6c
deokmoon <deok2moon@gmail.com>
2022-10-31 00:30:48 +0900

    Test: 로또 객체의 테스트코드 수정  - 가격을 int가 아닌 원시값 포장한 Money 사용

--- src/test/java/lotto/auto/LottoTest.java
+++ src/test/java/lotto/auto/LottoTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoTest {
-    private final int DEFAULT_PRICE = 1000;
+    private final String DEFAULT_PRICE = "1000";
 
     @Test
     public void 로또_가격_저장_확인(){
