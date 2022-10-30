56ed3f5655a657abb34fe3949de9c97172919770
jimbaemon <jimbae@kakao.com>
2022-10-31 00:13:46 +0900

    feat: 로또 보너스 번호 포함 여부 확인.

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -3,6 +3,8 @@ package lotto.model;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class LottoTest {
 
@@ -29,6 +31,13 @@ class LottoTest {
 	void 로또의_번호와_승리번호의_일치_갯수_확인() {
 		Lotto lotto = new Lotto(LottoNumber.of(6, 5, 4, 3, 2, 1));
 		assertThat(lotto.match(new Lotto(LottoNumber.of(1, 2, 3, 4, 5, 7)))).isEqualTo(5);
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"1,true", "2,false"})
+	void 로또_보너스_번호_포함_여부_확인(int number, boolean expected) {
+		Lotto lotto = new Lotto(LottoNumber.of(1, 3, 4, 5, 6, 7));
+		assertThat(lotto.matchBonus(new LottoNumber(number))).isEqualTo(expected);
 	}
 
 }
