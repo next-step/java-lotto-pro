2aee2e0cbeb3efb177202bb39331b81efacde344
taewonh <40269387+taewonh@users.noreply.github.com>
2022-10-30 20:44:01 +0900

    feat(Rank): 보너스 번호 적용
    - matchBonus 필드 추가 및 bonusMatch를 파라미터로 받는 valueOf 및 내부 검증 메소드 추가
    - 하위호환을 지원하기 위해 기존 방식의 valueOf 역시 지원하며 이때 bonusMatch의 기본값은 false로 지정

--- src/main/java/step3_4/domain/Rank.java
+++ src/main/java/step3_4/domain/Rank.java
@@ -4,18 +4,26 @@ import java.util.Arrays;
 
 public enum Rank {
     FIRST(6, 2_000_000_000),
-//    SECOND(5, 30_000_000),
-    THIRD(5, 1_500_000),
+    SECOND(5, 30_000_000, true),
+    THIRD(5, 1_500_000, false),
     FOURTH(4, 50_000),
     FIFTH(3, 5_000),
     MISS(0, 0);
 
     private int countOfMatch;
     private int winningMoney;
+    private boolean matchBonus;
 
     private Rank(int countOfMatch, int winningMoney) {
         this.countOfMatch = countOfMatch;
         this.winningMoney = winningMoney;
+        this.matchBonus = false;
+    }
+
+    private Rank(int countOfMatch, int winningMoney, boolean matchBonus) {
+        this.countOfMatch = countOfMatch;
+        this.winningMoney = winningMoney;
+        this.matchBonus = matchBonus;
     }
 
     public int getCountOfMatch() {
@@ -27,9 +35,18 @@ public enum Rank {
     }
 
     public static Rank valueOf(int countOfMatch) {
+        return valueOf(countOfMatch, false);
+    }
+
+    public static Rank valueOf(int countOfMatch, boolean bonusMatch) {
         return Arrays.stream(Rank.values())
-                .filter(rank -> rank.countOfMatch == countOfMatch)
+                .filter(rank -> rank.isMatch(countOfMatch, bonusMatch))
                 .findFirst()
                 .orElseGet(() -> Rank.MISS);
+    }
+
+    private boolean isMatch(int countOfMatch, boolean matchBonus) {
+        return this.countOfMatch == countOfMatch
+                && this.matchBonus == matchBonus;
     }
 }
---
+++ src/test/java/step3_4/domain/RankTest.java
@@ -0,0 +1,20 @@
+package step3_4.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RankTest {
+
+    @Test
+    @DisplayName("Rank 값 생성 테스트")
+    public void test() {
+        assertThat(Rank.valueOf(0)).isEqualTo(Rank.MISS);
+        assertThat(Rank.valueOf(3)).isEqualTo(Rank.FIFTH);
+        assertThat(Rank.valueOf(4)).isEqualTo(Rank.FOURTH);
+        assertThat(Rank.valueOf(5, false)).isEqualTo(Rank.THIRD);
+        assertThat(Rank.valueOf(5, true)).isEqualTo(Rank.SECOND);
+        assertThat(Rank.valueOf(6)).isEqualTo(Rank.FIRST);
+    }
+}
