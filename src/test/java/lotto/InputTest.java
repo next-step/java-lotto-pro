419f5843ef3cb70db521f4fc74d0580937718ffe
noelleseo <noelleseo@gmail.com>
2022-10-30 12:03:34 +0900

    test: 구입금액 테스트 코드 작성

---
+++ src/test/java/lotto/InputTest.java
@@ -0,0 +1,33 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import calculator.domain.Input;
+import lotto.util.Constants;
+
+public class InputTest {
+    @Test
+    @DisplayName("구입금액_검증_숫자")
+    public void input_validate_number() {
+        assertThatThrownBy(() -> new Input("abcd")).hasMessage(Constants.ERR_VALUE_NOT_VALID);
+    }
+    
+    @Test
+    @DisplayName("구입금액_검증_음수")
+    public void input_validate_negative() {
+        assertThatThrownBy(() -> new Input("-14000")).hasMessage(Constants.ERR_NEGATIVE_VALUE);
+    }
+    
+    @Test
+    @DisplayName("구입금액_검증_null")
+    public void input_validate_null() {
+        assertThatThrownBy(() -> new Input(null)).hasMessage(Constants.ERR_VALUE_NOT_VALID);
+    }
+    
+    @Test
+    @DisplayName("구입금액_검증_빈값")
+    public void input_validate_emtpy() {
+        assertThatThrownBy(() -> new Input("")).hasMessage(Constants.ERR_VALUE_NOT_VALID);
+    }
+}
