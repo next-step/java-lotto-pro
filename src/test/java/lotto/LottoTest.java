5d984e87eb13be3f3a92503cbc639d3647851cd3
noelleseo <noelleseo@gmail.com>
2022-10-30 18:07:48 +0900

    test: Lotto 클래스 테스트 코드 구조 변경

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,26 +1,24 @@
 package lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import java.util.ArrayList;
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 import lotto.domain.Lotto;
 import lotto.domain.Ticket;
+import lotto.domain.TicketList;
 
 public class LottoTest {    
     Lotto lotto;
-    List<Ticket> myTickets;
+    TicketList myTickets = new TicketList();
     Ticket winningTicket = new Ticket("1,2,3,4,5,6");
     
     @Test
     @DisplayName("구입금액에_따른_발급로또_개수_확인")
     public void lotto_issue_count() {
         lotto = new Lotto("14000");
-        assertThat(lotto.ticketList).hasSize(14);
+        assertThat(lotto.ticketList.size()).isEqualTo(14);
     }
 
     @ParameterizedTest
@@ -31,19 +29,19 @@ public class LottoTest {
                         }, delimiter = ':')
     @DisplayName("사용자_입력값과_로또_비교하여_결과_출력")
     public void lotto_get_result(String param, String expected) {
-        myTickets.add(new Ticket(param));
+        myTickets.addTicket(new Ticket(param));
         
-        lotto = new Lotto(myTickets, winningTicket);
-        assertThat(lotto.result).contains(expected);
+        lotto = new Lotto(myTickets);
+        assertThat(lotto.getResultStr(winningTicket)).contains(expected);
     }  
     
     @Test
     @DisplayName("사용자_입력값과_로또_비교하여_수익률_출력")
     public void lotto_get_return_rate() {
-        myTickets.add(new Ticket("1,2,3,7,8,9"));
-        myTickets.add(new Ticket("7,8,9,10,11,12"));
+        myTickets.addTicket(new Ticket("1,2,3,7,8,9"));
+        myTickets.addTicket(new Ticket("7,8,9,10,11,12"));
         
-        lotto = new Lotto(myTickets, winningTicket);
-        assertThat(lotto.result).contains("총 수익률은 2.5입니다.");
+        lotto = new Lotto(myTickets);
+        assertThat(lotto.getResultStr(winningTicket)).contains("총 수익률은 2.5입니다.");
     } 
 }
