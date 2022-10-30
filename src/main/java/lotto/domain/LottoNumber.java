806e64a6ecb088d224d105be2fb98bc5c4094553
crongcm <ucm5459@kakao.com>
2022-10-30 22:42:52 +0900

    feat: 로또 번호 캐싱

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,17 +1,33 @@
 package lotto.domain;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import lotto.view.OutputView;
 
 public class LottoNumber {
     public static final int MINIMUM_LOTTO_NUMBER = 1;
     public static final int MAXIMUM_LOTTO_NUMBER = 45;
+    private static final Map<Integer, LottoNumber> numberMap = new HashMap<>();
+
+    static {
+        for (int i = MINIMUM_LOTTO_NUMBER; i <= MAXIMUM_LOTTO_NUMBER; i++) {
+            numberMap.put(i, new LottoNumber(i));
+        }
+    }
 
     private final int number;
 
     public LottoNumber(int number) {
         validLottoNumber(number);
         this.number = number;
+    }
+
+    public static LottoNumber of(int number) {
+        if (numberMap.containsKey(number)) {
+            return numberMap.get(number);
+        }
+        return new LottoNumber(number);
     }
 
     private static void validLottoNumber(int number) {
