933b7d3450d3d4c9e8429945ea7ec0c70ba698c0
iamsojung <sojungkid@gmail.com>
2022-10-30 17:36:06 +0900

    feat: 사용자로부터 입력을 받는 기능

---
+++ src/main/java/step3/controller/LottoController.java
@@ -0,0 +1,11 @@
+package step3.controller;
+
+import step3.view.InputView;
+
+public class LottoController {
+
+    public void run() {
+        int money = InputView.inputPurchaseAmount();
+    }
+
+}
