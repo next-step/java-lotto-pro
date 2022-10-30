cc9eba4442b160560cb8e18462d50654f8135d81
taewonh <40269387+taewonh@users.noreply.github.com>
2022-10-30 13:44:00 +0900

    refactor(Rank): 불필요 주석 제거

--- src/main/java/step3/domain/Rank.java
+++ src/main/java/step3/domain/Rank.java
@@ -31,6 +31,5 @@ public enum Rank {
                 .filter(rank -> rank.countOfMatch == countOfMatch)
                 .findFirst()
                 .orElseGet(() -> Rank.MISS);
-        // TODO 일치하는 수를 로또 등수로 변경한다. enum 값 목록은 "Rank[] ranks = values();"와 같이 가져올 수 있다.
     }
 }
