8d90aa161fad8b35e8143b80f234c6e80890aa25
deveo-pyo <jungpyo9@gmail.com>
2022-10-31 01:17:38 +0900

    feat: 중복값 검사 메소드 구현
    1.중복값 검사 메소드 구현
    2.중복값 검사 메소드 테스트 구현

--- src/main/java/lotto/util/InputValidator.java
+++ src/main/java/lotto/util/InputValidator.java
@@ -1,28 +1,42 @@
 package lotto.util;
 
-import lotto.domain.Lotto;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static lotto.util.LottoNumberGenerator.MAX_LOTTO_NUMBER;
+import static lotto.util.LottoNumberGenerator.MIN_LOTTO_NUMBER;
 
 public class InputValidator {
     private static final String ERROR_MESSAGE_INVALID_NUMBER = "[ERROR] 유효하지 않은 구매금액입니다.";
-    private static final String ERROR_MESSAGE_INVALID_LOTTO_NUMBER = "[ERROR] 유효하지 않은 당첨 번호";
+    private static final String ERROR_MESSAGE_INVALID_RANGE_NUMBER = "[ERROR] 유효하지 않은 당첨 번호(입력범위:1~45)";
+    private static final String ERROR_MESSAGE_DUPLICATED_NUMBER = "[ERROR] 유효하지 않은 당첨 번호(중복)";
+    private static final String ERROR_MESSAGE_INVALID_SIZE = "[ERROR] 유효하지 않은 당첨 번호(6개 숫자 필요)";
     private static final String REGEX_DIGIT = "\\d+";
-    private static final int VALID_LOTTO_NUMBER_COUNT = 6;
+    private static final int VALID_LOTTO_NUMBER_SIZE = 6;
 
-    public static void validateNumberFormat(String input){
-        if(!input.matches(REGEX_DIGIT)){
+    public static void validateNumberFormat(String input) {
+        if (!input.matches(REGEX_DIGIT)) {
             throw new IllegalArgumentException(ERROR_MESSAGE_INVALID_NUMBER);
         }
     }
 
-    public static void validateLottoNumber(int number){
-        if(number < Lotto.MIN_LOTTO_NUMBER || number > Lotto.MAX_LOTTO_NUMBER){
-            throw new IllegalArgumentException(ERROR_MESSAGE_INVALID_LOTTO_NUMBER);
+    public static void validateDuplicateLottoNumber(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (numbers.size() != uniqueNumbers.size()) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_DUPLICATED_NUMBER);
         }
     }
 
-    public static void validateLottoNumberCount(int lottoNumberCount){
-        if(lottoNumberCount != VALID_LOTTO_NUMBER_COUNT){
-            throw new IllegalArgumentException(ERROR_MESSAGE_INVALID_LOTTO_NUMBER);
+    public static void validateLottoNumberRange(int number) {
+        if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_INVALID_RANGE_NUMBER);
+        }
+    }
+
+    public static void validateLottoNumberCount(int lottoNumberCount) {
+        if (lottoNumberCount != VALID_LOTTO_NUMBER_SIZE) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_INVALID_SIZE);
         }
     }
 }
--- src/test/java/lotto/util/InputValidatorTest.java
+++ src/test/java/lotto/util/InputValidatorTest.java
@@ -3,33 +3,45 @@ package lotto.util;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import static lotto.util.InputValidator.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class InputValidatorTest {
     @Test
     @DisplayName("당첨번호 입력시 숫자포맷 오류 검사")
-    public void validateNumberFormatTest(){
+    public void validateNumberFormatTest() {
         assertThatThrownBy(
-            () -> validateNumberFormat("1d")
+                () -> validateNumberFormat("1d")
         ).isInstanceOf(IllegalArgumentException.class)
          .hasMessageContaining("[ERROR]");
     }
 
     @Test
     @DisplayName("1~45 사이의 숫자가 아닌 경우 테스트")
-    public void validateLottoNumberTest(){
+    public void validateLottoNumberRangeTest() {
         assertThatThrownBy(
-            () -> validateLottoNumber(46)
+                () -> validateLottoNumberRange(46)
         ).isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR]");
+         .hasMessageContaining("[ERROR]");
     }
 
     @Test
-    @DisplayName("6개의 로또 숫자가 아닌 경우 검사")
-    public void validateLottoNumberCountTest(){
+    @DisplayName("6개의 로또 숫자가 아닌 경우 테스트")
+    public void validateLottoNumberCountTest() {
         assertThatThrownBy(
-            () -> validateLottoNumberCount(7)
+                () -> validateLottoNumberCount(7)
+        ).isInstanceOf(IllegalArgumentException.class)
+         .hasMessageContaining("[ERROR]");
+    }
+
+    @Test
+    @DisplayName("당첨번호 중복 테스트")
+    public void validateDuplicateLottoNumberTest(){
+        assertThatThrownBy(
+                () -> validateDuplicateLottoNumber(Arrays.stream(new int[]{1,1,2,3,4,5}).boxed().collect(Collectors.toList()))
         ).isInstanceOf(IllegalArgumentException.class)
          .hasMessageContaining("[ERROR]");
     }
