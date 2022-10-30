a9e1fa8dacbf848ffb3205f73f6a55a7130aec59
deokmoon <deok2moon@gmail.com>
2022-10-30 23:33:05 +0900

    Test: Money(돈) 원시값 포장 클래스 테스트코드 작성  - 입력받은 금액 String to Int 변환 저장 테스트  - 금액 문자열 존재 시 예외 테스트

---
+++ src/test/java/lotto/auto/MoneyTest.java
@@ -0,0 +1,26 @@
+package lotto.auto;
+
+import jdk.internal.joptsimple.internal.Strings;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class MoneyTest {
+    @ParameterizedTest
+    @CsvSource(value = {"1000:1000", "2000:2000", "3000:3000"}, delimiter = ':')
+    public void 금액_Int_저장_확인(String input, int Expected) {
+        Money money = new Money(input);
+        assertThat(money.getMoney()).isEqualTo(Expected);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"100a", "10a0", "0dsaqwe"})
+    public void 금액_문자열_예외_확인(String input) {
+        assertThatThrownBy(() -> new Money().changeStringToInt(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
