7f638f23ca704796ae28e19b103eabcb1b0ee59c
지영진 <youngjin.jee@nhn.com>
2022-10-30 18:52:10 +0900

    refactor: 애플리케이션 로직 문단 나누기

--- src/main/java/study/step3/LottoApplication.java
+++ src/main/java/study/step3/LottoApplication.java
@@ -5,10 +5,13 @@ import java.util.List;
 public class LottoApplication {
     public static void main(String[] args) {
         int money = InputView.inputMoney();
+
         Lottos lottos = new Lottos();
         List<Lotto> lottoList = lottos.makeLottos(money);
         ResultView.printLottos(lottoList);
+
         String winLottoNumbers = InputView.inputWinningNumbers();
+
         Winners winners = lottos.findWinners(winLottoNumbers);
         ResultView.printLottoWinners(winners);
         ResultView.printEarningRate(winners, money);
