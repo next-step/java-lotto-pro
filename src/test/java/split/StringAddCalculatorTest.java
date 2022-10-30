fd803fb387f33bcba1adebc11a7291521eb5fb72
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-10-30 11:51:23 +0900

    test(StringAddCalculatorTest.java) : test 코드 접근제어자 제거

--- src/test/java/split/StringAddCalculatorTest.java
+++ src/test/java/split/StringAddCalculatorTest.java
@@ -4,18 +4,17 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
-import split.StringAddCalculator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("문자열 덧셈 계산기")
-public class StringAddCalculatorTest {
+class StringAddCalculatorTest {
 
     @DisplayName("공백 또는 null 을 입력할 경우 0을 반환한다.")
     @ParameterizedTest
     @NullAndEmptySource
-    public void splitAndSum_null_또는_빈문자(String text) {
+    void splitAndSum_null_또는_빈문자(String text) {
         int result = StringAddCalculator.splitAndSum(text);
         assertThat(result).isEqualTo(0);
     }
@@ -23,7 +22,7 @@ public class StringAddCalculatorTest {
     @DisplayName("숫자하나 입력하면 해당 숫자를 반환한다.")
     @ParameterizedTest
     @ValueSource(strings = {"1"})
-    public void splitAndSum_숫자하나(String text) {
+    void splitAndSum_숫자하나(String text) {
         int result = StringAddCalculator.splitAndSum(text);
         assertThat(result).isEqualTo(1);
     }
@@ -31,7 +30,7 @@ public class StringAddCalculatorTest {
     @DisplayName("숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.")
     @ParameterizedTest
     @ValueSource(strings = {"1,2"})
-    public void splitAndSum_쉼표구분자(String text) {
+    void splitAndSum_쉼표구분자(String text) {
         int result = StringAddCalculator.splitAndSum(text);
         assertThat(result).isEqualTo(3);
     }
@@ -39,7 +38,7 @@ public class StringAddCalculatorTest {
     @DisplayName("구분자를 컴마(,) 이외에 콜론(:)을 사용할 수 있다.")
     @ParameterizedTest
     @ValueSource(strings = {"1,2:3", "1,2,3", "1:2:3"})
-    public void splitAndSum_쉼표_또는_콜론_구분자(String text) {
+    void splitAndSum_쉼표_또는_콜론_구분자(String text) {
         int result = StringAddCalculator.splitAndSum(text);
         assertThat(result).isEqualTo(6);
     }
@@ -47,7 +46,7 @@ public class StringAddCalculatorTest {
     @DisplayName("//와 \\n 문자 사이에 커스텀 구분자를 지정할 수 있다.")
     @ParameterizedTest
     @ValueSource(strings = {"//;\n1;2;3"})
-    public void splitAndSum_custom_구분자(String text) {
+    void splitAndSum_custom_구분자(String text) {
         int result = StringAddCalculator.splitAndSum(text);
         assertThat(result).isEqualTo(6);
     }
@@ -55,7 +54,7 @@ public class StringAddCalculatorTest {
     @DisplayName("음수를 전달할 경우 RuntimeException 예외가 발생해야 한다.")
     @ParameterizedTest
     @ValueSource(strings = {"-1,2,3"})
-    public void splitAndSum_negative(String text) {
+    void splitAndSum_negative(String text) {
         assertThatThrownBy(() -> StringAddCalculator.splitAndSum(text))
                 .isInstanceOf(IllegalArgumentException.class);
     }
@@ -63,7 +62,7 @@ public class StringAddCalculatorTest {
     @DisplayName("문자열 계산기에 숫자 이외의 값을 전달하는 경우 RuntimeException 예외를 throw 한다.")
     @ParameterizedTest
     @ValueSource(strings = {"1,2:a", "b", "3:c"})
-    public void splitAndSum_not_number(String text) {
+    void splitAndSum_not_number(String text) {
         assertThatThrownBy(() -> StringAddCalculator.splitAndSum(text))
                 .isInstanceOf(IllegalArgumentException.class);
     }
