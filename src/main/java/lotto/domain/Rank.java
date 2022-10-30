747dc4577d005247c32b042722cd1896324669eb
CHEESE <81912261+ychxexn@users.noreply.github.com>
2022-10-30 23:06:14 +0900

    refactor: 기존 Rank enum 상수 값 제거

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -8,11 +8,6 @@ public enum Rank {
     THIRD(5, 1_500_000),
     SECOND(5, 30_000_000),
     FIRST(6, 2_000_000_000),
-
-    THREE(3, 5_000),
-    FOUR(4, 50_000),
-    FIVE(5, 1_500_000),
-    SIX(6, 2_000_000_000),
     NONE(0, 0);
 
     private final int matchCount;
