1c962c35675ac674b7b418d7a2165a9f7e34e61f
noelleseo <noelleseo@gmail.com>
2022-10-30 15:20:50 +0900

    refactor: winningTicket과 Ticket을 분리하지 않도록 수정

--- src/main/java/lotto/domain/Ticket.java
+++ src/main/java/lotto/domain/Ticket.java
@@ -2,18 +2,28 @@ package lotto.domain;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import lotto.util.Constants;
+import lotto.util.IntUtil;
+import lotto.util.RegexUtil;
+import lotto.util.StringUtil;
 
 public class Ticket {
     public List<Integer> lottoNumbers = new ArrayList<>();
 
+    private static final int SIX = 6;
     private static final int ZERO = 0;
 
     public Ticket() {
         generateTicket();
+    }
+    
+    public Ticket(String winningTicketStr) {
+        validateWinningTicketStr(winningTicketStr);
     }
 
     public String toString() {
@@ -46,5 +56,35 @@ public class Ticket {
     private String removeLastComma(String str) {
         int lastCommaIndex = str.lastIndexOf(Constants.COMMA);
         return str.substring(ZERO, lastCommaIndex);
+    }
+
+    private void validateWinningTicketStr(String winningTicketStr) {
+        if(StringUtil.isNullOrEmpty(winningTicketStr)) {
+            throw new IllegalArgumentException(Constants.ERR_NULL_VALUE);
+        }
+        
+        String[] winningTicketStrArray = winningTicketStr.split(Constants.COMMA);
+        
+        if(winningTicketStrArray.length != SIX) {
+            throw new IllegalArgumentException(Constants.ERR_SIX_NUMBERS);
+        }
+        
+        IntStream.rangeClosed(Constants.TICKET_MIN_IDX, Constants.TICKET_MAX_IDX).forEach(i -> {
+            validateWinningTicketNum(winningTicketStrArray[i].trim());
+        });
+        
+        Set<Integer> tmpLottoSet = new HashSet<>(this.lottoNumbers);
+        
+        if(tmpLottoSet.size() != this.lottoNumbers.size()) {
+            throw new IllegalArgumentException(Constants.ERR_DUP_NUMBERS);
+        }
+    }
+    
+    private void validateWinningTicketNum(String numStr) {
+        if(!RegexUtil.match(Constants.REGEX_1_TO_45_CHAR, numStr)) {
+            throw new IllegalArgumentException(Constants.ERR_VALUE_NOT_VALID);
+        }
+        
+        this.lottoNumbers.add(IntUtil.parseInt(numStr));
     }
 }
--- src/test/java/lotto/TicketTest.java
+++ src/test/java/lotto/TicketTest.java
@@ -1,36 +1,77 @@
 package lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.IntStream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import lotto.domain.Ticket;
 import lotto.util.Constants;
 
 public class TicketTest {
     Ticket ticket;
+    
+    @Nested
+    @DisplayName("발급로또_테스트")
+    class IssuedTicket {
+        @BeforeEach
+        public void generateTicket() {
+            ticket = new Ticket();
+        }
 
-    @BeforeEach
-    public void generateTicket() {
-        ticket = new Ticket();
+        @Test
+        @DisplayName("발급로또_번호_검증_범위")
+        public void ticket_num_validate_one_to_fortyfive() {
+            IntStream.rangeClosed(Constants.TICKET_MIN_IDX, Constants.TICKET_MAX_IDX).forEach(i -> {
+                assertThat(ticket.lottoNumbers.get(i)).isBetween(Constants.TICKET_MIN_LOTTO_NUM, Constants.TICKET_MAX_LOTTO_NUM);
+            });
+        }
+
+        @Test
+        @DisplayName("발급로또_번호_검증_중복")
+        public void ticket_num_validate_duplicate() {
+            Set<Integer> lottoSet = new HashSet<>(ticket.lottoNumbers);
+            
+            assertThat(lottoSet).hasSameSizeAs(ticket.lottoNumbers);
+        }
     }
 
-    @Test
-    @DisplayName("발급로또_번호_검증_범위")
-    public void ticket_num_validate_one_to_fortyfive() {
-        IntStream.rangeClosed(Constants.TICKET_MIN_IDX, Constants.TICKET_MAX_IDX).forEach(i -> {
-            assertThat(ticket.lottoNumbers.get(i)).isBetween(Constants.TICKET_MIN_LOTTO_NUM, Constants.TICKET_MAX_LOTTO_NUM);
-        });
-    }
-
-    @Test
-    @DisplayName("발급로또_번호_검증_중복")
-    public void ticket_num_validate_duplicate() {
-        Set<Integer> lottoSet = new HashSet<>(ticket.lottoNumbers);
+    @Nested
+    @DisplayName("당첨로또_테스트")
+    class WinningTicket {
+        @Test
+        @DisplayName("당첨번호_검증_개수")
+        public void winningTicket_validate_count() {
+            assertThatThrownBy(() -> new Ticket("1,2,3,4,5")).hasMessage(Constants.ERR_SIX_NUMBERS);
+        }
         
-        assertThat(lottoSet).hasSameSizeAs(ticket.lottoNumbers);
+        @Test
+        @DisplayName("당첨번호_검증_숫자")
+        public void winningTicket_validate_number() {
+            assertThatThrownBy(() -> new Ticket("a,b,c,d,e,f")).hasMessage(Constants.ERR_VALUE_NOT_VALID);
+        }
+        
+        @Test
+        @DisplayName("당첨번호_검증_null")
+        public void winningTicket_validate_null() {
+            assertThatThrownBy(() -> new Ticket(null)).hasMessage(Constants.ERR_NULL_VALUE);
+            
+        }
+        
+        @Test
+        @DisplayName("당첨번호_검증_빈값")
+        public void winningTicket_validate_emtpy() {
+            assertThatThrownBy(() -> new Ticket("")).hasMessage(Constants.ERR_NULL_VALUE);
+        } 
+        
+        @Test
+        @DisplayName("당첨번호_검증_중복")
+        public void winningTicket_validate_duplicate() {
+            assertThatThrownBy(() -> new Ticket("1,2,3,4,5,5")).hasMessage(Constants.ERR_DUP_NUMBERS);
+        }  
     }
 }
