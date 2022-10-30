f5aa843a17b1eefa7f1d525e9f8ea812375f03c4
seogineer <dgseo8981@gmail.com>
2022-10-30 15:22:37 +0900

    refactor: 로또 프로젝트에서 사용할 클래스가 아니라서 접근자 변경

--- src/main/java/calculator/Spliter.java
+++ src/main/java/calculator/Spliter.java
@@ -1,17 +1,17 @@
 package calculator;
 
 import calculator.constant.Delimiter;
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Spliter {
+class Spliter {
     private static final Pattern CUSTOM_PATTERN = Pattern.compile(Delimiter.CUSTOM_DELIMITER.getDelimiter());
 
-    Numbers split(String text){
+    Numbers split(String text) {
         Matcher m = CUSTOM_PATTERN.matcher(text);
-        if (m.find())
+        if (m.find()) {
             return new Numbers(m.group(2).split(m.group(1)));
+        }
         return new Numbers(text.split(Delimiter.BASIC_DELIMITER.getDelimiter()));
     }
 }
