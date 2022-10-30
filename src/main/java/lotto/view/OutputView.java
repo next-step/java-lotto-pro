f8921c86361bc1ac21a5f43fd8527b97868216e5
deveo-pyo <jungpyo9@gmail.com>
2022-10-31 01:35:08 +0900

    refactor: 수익률 계산 메소드 별도 클래스로 분리
    1. 수익률 계산 메소드 별도 클래스로 분리
    2. 수익률 계산 메소드 테스트 분리

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,7 +4,7 @@ import lotto.domain.Lotto;
 import lotto.domain.LottoResult;
 
 import static lotto.domain.LottoResult.MIN_WINNING_NUM;
-import static lotto.domain.LottoResult.PRIZE_MONEY;
+import static lotto.util.ProfitCalculator.PRIZE_MONEY;
 
 public class OutputView {
 
@@ -13,21 +13,34 @@ public class OutputView {
     private static final String OUT_MESSAGE_DIVIDER_LINE = "----------";
     private static final String OUT_MESSAGE_LOTTO_RESULT = "%d개 일치(%d)- %d개\n";
     private static final String OUT_MESSAGE_PROFIT_RATIO = "총 수익률은 %.2f 입니다.";
+    private static final String OUT_MESSAGE_PROFIT_LESS_THAN_EVEN_POINT = "(기준이 1이기 때문에 결과적으로 손해라는 의미임)";
+    private static final int EVEN_POINT_PROFIT_RATIO = 1;
 
-    public static void printLottos(Lotto lotto){
+    public static void printLottos(Lotto lotto) {
         System.out.println(lotto);
     }
 
-    public static void printLottoAmount(int lottoAmount){
-        System.out.println(lottoAmount+OUT_MESSAGE_LOTTO_AMOUNT);
+    public static void printLottoAmount(int lottoAmount) {
+        System.out.println(lottoAmount + OUT_MESSAGE_LOTTO_AMOUNT);
     }
 
-    public static void printLottoResult(LottoResult lottoResult){
+    public static void printLottoResult(LottoResult lottoResult, double profitRatio) {
         System.out.println(OUT_MESSAGE_RESULT_STATISTICS);
         System.out.println(OUT_MESSAGE_DIVIDER_LINE);
-        for(int i=MIN_WINNING_NUM; i< PRIZE_MONEY.length; i++){
+        printLottoStatistics(lottoResult);
+        printLottoProfitRatio(profitRatio);
+    }
+
+    private static void printLottoStatistics(LottoResult lottoResult) {
+        for (int i = MIN_WINNING_NUM; i < PRIZE_MONEY.length; i++) {
             System.out.printf(OUT_MESSAGE_LOTTO_RESULT, i, PRIZE_MONEY[i], lottoResult.getLottoResult(i));
         }
-        System.out.printf(OUT_MESSAGE_PROFIT_RATIO, lottoResult.calculateProfitRatio());
+    }
+
+    private static void printLottoProfitRatio(double profitRatio) {
+        System.out.printf(OUT_MESSAGE_PROFIT_RATIO, profitRatio);
+        if (profitRatio < EVEN_POINT_PROFIT_RATIO) {
+            System.out.println(OUT_MESSAGE_PROFIT_LESS_THAN_EVEN_POINT);
+        }
     }
 }
