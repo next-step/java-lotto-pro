b88902475be93ce77a1b9874bf2a3c9d4fb4b8b6
Minjae Kim <minjae.kim@mytrum.com>
2022-10-30 23:21:32 +0900

    refactor: 로또 번호 목록 생성자 유효성 검증 생성

--- src/main/java/lotto/LottoNumberBag.java
+++ src/main/java/lotto/LottoNumberBag.java
@@ -6,6 +6,10 @@ import java.util.List;
 
 public class LottoNumberBag {
 
+    private static final int LOTTO_NUMBER_SIZE = 6;
+    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final int LOTTO_MAX_NUMBER = 45;
+
     private List<Integer> lottoNumbers;
 
     public LottoNumberBag() {
@@ -14,8 +18,7 @@ public class LottoNumberBag {
     }
 
     public LottoNumberBag(List<Integer> numbers) {
-        // Test를 위해 만든 생성자
-        // validation 로직이 필요할까..?
+        validNumbers(numbers);
         lottoNumbers = numbers;
     }
 
@@ -23,5 +26,35 @@ public class LottoNumberBag {
         return (int) lottoNumbers.stream()
                 .filter(winningNumbers::contains)
                 .count();
+    }
+
+    private void validNumbers(List<Integer> numbers) {
+        validNumberSize(numbers);
+        validUnique(numbers);
+        validRange(numbers);
+    }
+
+    private void validNumberSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_NUMBER_SIZE) {
+            throw new IllegalArgumentException("로또 숫자는 6개여야 합니다");
+        }
+    }
+
+    private void validUnique(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != numbers.size()) {
+            throw new IllegalArgumentException("로또 숫자는 중복되지 않은 값이어야 합니다");
+        }
+    }
+
+    private void validRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            checkRange(number);
+        }
+    }
+
+    private void checkRange(int number) {
+        if (number < LOTTO_MIN_NUMBER || LOTTO_MAX_NUMBER < number) {
+            throw new IllegalArgumentException("로또 숫자는 1 ~ 45 사이의 값이어야 합니다");
+        }
     }
 }
--- src/test/java/lotto/LottoNumberBagTest.java
+++ src/test/java/lotto/LottoNumberBagTest.java
@@ -2,12 +2,18 @@ package lotto;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.junit.jupiter.params.ParameterizedTest.DEFAULT_DISPLAY_NAME;
 
 @DisplayName("로또 번호 목록 테스트")
 class LottoNumberBagTest {
@@ -25,5 +31,23 @@ class LottoNumberBagTest {
         List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
         int correctCount = lottoNumberBag.correctCount(winningNumbers);
         assertThat(correctCount).isEqualTo(3);
+    }
+
+    @ParameterizedTest(name = "생성 예외 테스트 - " + DEFAULT_DISPLAY_NAME)
+    @MethodSource("provideExceptionValues")
+    void validRange_bag_IllegalArgumentException(List<Integer> lottoNumbers) {
+        assertThatIllegalArgumentException().isThrownBy(() ->
+                new LottoNumberBag(lottoNumbers));
+    }
+
+    static Stream<Arguments> provideExceptionValues() {
+        return Stream.of(
+                /* 로또 숫자 갯수 불일치 */
+                Arguments.of(Arrays.asList(1, 2, 3, 10, 20, 30, 7)),
+                /* 중복 된 로또 숫자 */
+                Arguments.of(Arrays.asList(1, 1, 3, 10, 20, 30)),
+                /* 로또 숫자 범위를 벗어난 숫자 */
+                Arguments.of(Arrays.asList(1, 1, 3, 10, 20, 99))
+        );
     }
 }
