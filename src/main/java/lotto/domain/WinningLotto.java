e3e4e1678df811a1851ec4408577e06cddd0c5c2
CHEESE <81912261+ychxexn@users.noreply.github.com>
2022-10-30 23:12:04 +0900

    feat: WinningLotto 보너스 볼 포함 생성자 추가

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -3,8 +3,15 @@ package lotto.domain;
 import java.util.List;
 
 public class WinningLotto extends Lotto {
+    private LottoNumber bonusBall;
+
     public WinningLotto(List<LottoNumber> insertWinningLotto) {
         super(insertWinningLotto);
+    }
+
+    public WinningLotto(List<LottoNumber> insertWinningLotto, LottoNumber bonusBall) {
+        this(insertWinningLotto);
+        this.bonusBall = bonusBall;
     }
 
     public int getCorrectCount(Lotto lotto) {
