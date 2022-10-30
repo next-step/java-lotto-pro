627f36d53d4fd2ec460c948631ea73bd284f142b
mansik <90mansin@gmail.com>
2022-10-31 01:05:03 +0900

    test: Lotto 도메인 테스트 구현
    - 로또번호 6개 저장 테스트
    - 로또번호 중복 입력시 Exception 발생 검증
    - 로또번호 6자리와 다를 경우 Exception 발생 검증

---
+++ src/test/java/study/step3/LottoTest.java
@@ -0,0 +1,65 @@
+package study.step3;
+
+import domain.Lotto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class LottoTest {
+
+    @Test
+    @DisplayName("로또번호 6개를 저장한다.")
+    public void 로또번호_저장(){
+        List<Integer> lottoNumbers = new ArrayList<>();
+        lottoNumbers.add(1);
+        lottoNumbers.add(45);
+        lottoNumbers.add(3);
+        lottoNumbers.add(4);
+        lottoNumbers.add(18);
+        lottoNumbers.add(20);
+
+        Lotto lotto = new Lotto(lottoNumbers);
+
+        assertThat(lotto).isEqualTo(new Lotto(lottoNumbers));
+
+    }
+
+    @Test
+    @DisplayName("로또번호 중복 입력 시 Exception발생")
+    public void 로또번호_중복번호_체크(){
+        List<Integer> lottoNumbers = new ArrayList<>();
+        lottoNumbers.add(1);
+        lottoNumbers.add(45);
+        lottoNumbers.add(4);
+        lottoNumbers.add(4);
+        lottoNumbers.add(18);
+        lottoNumbers.add(20);
+
+        assertThatThrownBy(() ->
+                new Lotto(lottoNumbers)
+        ).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또번호는 중복 발행 불가능 합니다");
+    }
+
+    @Test
+    @DisplayName("로또번호가 6자리가 입력 되지 않으면 Exception")
+    public void 로또번호_6자리_체크(){
+        List<Integer> lottoNumbers = new ArrayList<>();
+        lottoNumbers.add(1);
+        lottoNumbers.add(45);
+        lottoNumbers.add(4);
+        lottoNumbers.add(5);
+        lottoNumbers.add(18);
+
+        assertThatThrownBy(() ->
+                new Lotto(lottoNumbers)
+        ).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또번호는 6개 입력 되어야 합니다.");
+    }
+
+}
