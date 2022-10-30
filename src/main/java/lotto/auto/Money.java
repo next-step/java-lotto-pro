bc0dc0893ad18a692b2801f2fb92418bd071f791
deokmoon <deok2moon@gmail.com>
2022-10-30 23:35:54 +0900

    Feat: Money(돈) 원시값 포장 클래스 프로덕트코드 작성  - 금액 문자열 검증  - 입력받은 금액 String to Int 변환 저장

---
+++ src/main/java/lotto/auto/Money.java
@@ -0,0 +1,30 @@
+package lotto.auto;
+
+import java.math.BigInteger;
+
+public class Money {
+    private final int DEFAULT_MONEY = 0;
+    private int money;
+
+    public Money() {
+        this.money = DEFAULT_MONEY;
+    }
+
+    public Money(String money) {
+        this.money = changeStringToInt(money);
+    }
+
+    public int changeStringToInt(String money) {
+        if (money == null) {
+            throw new IllegalArgumentException();
+        }
+        if (money.chars().allMatch(Character::isDigit)) {
+            return Integer.parseInt(money);
+        }
+        throw new IllegalArgumentException();
+    }
+
+    public int getMoney() {
+        return money;
+    }
+}
