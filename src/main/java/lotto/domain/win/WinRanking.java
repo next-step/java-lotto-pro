cafbd95fdee0e8bfde9295e4f3f20707a2843042
ilmare-cbk <cbk104325@gmail.com>
2022-10-30 15:15:12 +0900

    refactor(WinRanking.java): matchCount와 보너스 볼 여부를 가지고 당첨등수 판단하는 로직 수정
    - matchCount와 보너스볼 여부 값이 주어졌을 때 당첨등수 확인하는 테스트 추가
    - matchCount가 0에서 3미만일때 MISS 반환하도록 수정
    - bonusNumber를 hasBonusNumber로 변수명 변경

--- src/main/java/lotto/domain/win/WinRanking.java
+++ src/main/java/lotto/domain/win/WinRanking.java
@@ -4,14 +4,15 @@ package lotto.domain.win;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import lotto.message.ErrorMessages;
 
 public enum WinRanking {
     MISS(0, 0),
-    FIFTH(3, 5000),
-    FOURTH(4, 50000),
-    THIRD(5, 1500000),
-    SECOND(5, 30000000),
-    FIRST(6, 2000000000),
+    FIFTH(3, 5_000),
+    FOURTH(4, 50_000),
+    THIRD(5, 1_500_000),
+    SECOND(5, 30_000_000),
+    FIRST(6, 2_000_000_000),
     ;
 
     private final int matchCount;
@@ -22,15 +23,30 @@ public enum WinRanking {
         this.winningMoney = winningMoney;
     }
 
-    public static WinRanking of(int matchCount, boolean bonusNumber) {
-        if (matchCount == SECOND.getMatchCount() && bonusNumber) {
+    public static WinRanking of(int matchCount, boolean hasBonusNumber) {
+        if (isMissRanking(matchCount)) {
+            return MISS;
+        }
+
+        if (isSecondRanking(matchCount, hasBonusNumber)) {
             return SECOND;
         }
+
         return Arrays.stream(WinRanking.values())
                 .filter(winRanking -> winRanking != SECOND)
                 .filter(winRanking -> winRanking.getMatchCount() == matchCount)
                 .findAny()
-                .orElse(MISS);
+                .orElseThrow(() -> new IllegalArgumentException(
+                        String.format(ErrorMessages.INVALID_MATCH_COUNT, matchCount))
+                );
+    }
+
+    private static boolean isMissRanking(int matchCount) {
+        return MISS.getMatchCount() <= matchCount && matchCount < FIFTH.getMatchCount();
+    }
+
+    private static boolean isSecondRanking(int matchCount, boolean bonusNumber) {
+        return matchCount == SECOND.getMatchCount() && bonusNumber;
     }
 
     public static List<WinRanking> winnableRankings() {
--- src/main/java/lotto/message/ErrorMessages.java
+++ src/main/java/lotto/message/ErrorMessages.java
@@ -5,6 +5,7 @@ public class ErrorMessages {
     public static final String INVALID_PURCHASE_PRICE = "%s원은 로또 1장을 구입할 수 없는 금액입니다.";
     public static final String DUPLICATED_LOTTO_NUMBERS = "%s는 중복된 로또 번호가 존재합니다.";
     public static final String INVALID_LOTTO_NUMBERS_COUNT = "%s는 로또 번호가 6개가 아닙니다.";
+    public static final String INVALID_MATCH_COUNT = "%d(은)는 유효하지 않은 일치 숫자입니다.";
 
     private ErrorMessages() {
     }
