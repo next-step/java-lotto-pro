e34ee2a4a33ea2a23aa2ee321b154dfbdcd433e8
namhyun.sung <namhyun.sung@yanolja.com>
2022-10-31 00:31:54 +0900

    feat(UI): 수동 입력 UI 수정

--- src/main/java/step3/LottoApplication.java
+++ src/main/java/step3/LottoApplication.java
@@ -1,5 +1,7 @@
 package step3;
 
+import step3.model.CustomLottoGenerator;
+import step3.model.LottoGenerator;
 import step3.model.LottoPaper;
 import step3.model.LottoShop;
 import step3.model.Money;
@@ -7,17 +9,28 @@ import step3.model.WinningResult;
 import step3.view.InputView;
 import step3.view.ResultView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoApplication {
     public static void main(String[] args) {
 
         // 금액 입력
         Money money = new Money(InputView.inputPurchasePrice());
 
+        int countOfCustomLotto = InputView.inputCountOfCustomLotto(money);
+
+        InputView.inputCustomLottoNumbers();
+        List<LottoGenerator> customLottoGenerators = new ArrayList<>();
+        for (int i = 0; i < countOfCustomLotto; i++) {
+            customLottoGenerators.add(new CustomLottoGenerator(InputView.inputCustomLotto()));
+        }
+
         // 총 구매 티켓 수 알림
         ResultView.announceTotalLottoTicketCount(money.countOfLottoPurchases());
 
         // 구매 티켓 번호 출력
-        LottoPaper lottoPaper = new LottoShop().buyLotto(money);
+        LottoPaper lottoPaper = new LottoShop().buyLotto(money, customLottoGenerators);
         ResultView.announceTotalLottoNumbers(lottoPaper);
 
         // 지난주 당첨 번호 입력
