1d6e7ae30ac14d4bdab19a62fc33d7d32ee5310b
sawook <lna1003@naver.com>
2022-10-30 16:24:12 +0900

    fix: 객체지향 규칙에 따른 코드 수정
    - depth가 2를 넘지 않도록 기능 수정

--- src/main/java/model/Revenue.java
+++ src/main/java/model/Revenue.java
@@ -8,14 +8,16 @@ public class Revenue {
 
     public Revenue(Map<LottoRankType, Integer> stats) {
         for (Map.Entry<LottoRankType, Integer> entry : stats.entrySet()) {
-            addWinMoneyIfValuePercent(entry);
+            winMoney += addWinMoneyIfValuePercent(entry);
         }
     }
 
     public int addWinMoneyIfValuePercent(Map.Entry<LottoRankType, Integer> entry) {
         if (entry.getValue() > 0) {
-            winMoney += entry.getKey().getWinMoney();
+            return entry.getKey().getWinMoney();
         }
+
+        return 0;
     }
 
     public double getPercent(int inputMoney) {
