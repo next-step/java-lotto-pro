ca462d1091507b23d281c20555f95a41c21f4d43
iamsojung <sojungkid@gmail.com>
2022-10-30 19:47:49 +0900

    feat: 로또를 생성해주는 LottoGenerator 클래스 추가

---
+++ src/main/java/step3/domain/Lottos.java
@@ -0,0 +1,17 @@
+package step3.domain;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Lottos {
+    private final List<Lotto> lottoList;
+
+    public Lottos(List<Lotto> list) {
+        this.lottoList = Collections.unmodifiableList(list);
+    }
+
+    public List<Lotto> getLottoList() {
+        return lottoList;
+    }
+
+}
