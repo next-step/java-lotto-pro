b6f389d572e5ef1440f0da45c9228565da2f7c0d
yeojiin <47417580+yeojiin@users.noreply.github.com>
2022-10-30 13:51:48 +0900

    [yeojiin(임정연)]2단계 - 문자열 덧셈 계산기 PR 요청 (#759)
    * feat(StringAddCalculaotr.java) step2 테스트 클래스 생성
    
    * feat(StringAddCalculator.java): 널, 빈값 체크 단순 구현
    
    * feat(StringAddCalculator.java): 기능목록 tdd 분리 없이 구현
    
    * refactor(StringAddCalculator.java): 특수문자 구분자 경우 추가
    
    * refactor(StringAddCalculator.java): 1. 빈문자열, null 체크 분리
    2. 한개의 숫자 체크
    
    * refactor(StringAddCalculator.java): boolean return을 메서드로 분리
    
    * refactor(StringAddCalculator.java): 메서드 분리
    
    * refactor(StringAddCalculator.java): isSingleNum 불필요 메서드 삭제
    
    * fix(StringAddCalculator.java): 특수문자 split 시 변경된 delimiter 사용 버그 수정
    
    * Test(StringAddCalculatorTest.java): 전체 테스트 성공
    
    * fix(StringAddCalculator): 상수 정의 시 final 추가, 에러 메세지 상수 처리
    
    * refactor(StringAddCalculatorTest): @BeforeEach 추가 후 생성자로 해결x -> 같은 메소드 내에서 여전히 누적된 수
    
    * fix(StringAddCalculator): splitAndSum() 호출 시 초기하
    
    Co-authored-by: jylim <jylim@12cm.co.kr>

---
+++ src/main/java/step2/StringAddCalculator.java
@@ -0,0 +1,82 @@
+package step2;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+    private static final String patternRegex = "//(.)\n(.*)";
+    private static final String delimiterExceptionRegex = "[!@#$%^&*(),.?\":{}|<>]";
+    private static final String basicSplitRegex = ",|:";
+    private static final String negativeExceptionMessage = "negative number included Exception";
+    private static int sum;
+    private static String delimiter;
+    private static String exceptDelimiterString;
+
+    public static int splitAndSum(String target) {
+        sum = 0;
+        delimiter = basicSplitRegex;
+
+        if (isNullOrEmpty(target)) {
+            return sum;
+        }
+        String[] numbers = doSplitAndSum(target);
+        sumSplitedNumbers(numbers);
+        return sum;
+    }
+
+    private static void sumSplitedNumbers(String[] numbers) {
+        for (String i : numbers) {
+            isUnderZeroNumber(i);
+            sum += Integer.parseInt(i);
+        }
+    }
+
+    private static String[] doSplitAndSum(String target) {
+        if (!isFind(patternRegex, target)) {
+            return target.split(basicSplitRegex);
+        }
+        return getCustomDelimiterNumbers(patternRegex, target);
+    }
+
+    private static String[] getCustomDelimiterNumbers(String regex, String target) {
+        Matcher m = getMatcher(regex, target);
+        if (m.find()) {
+            delimiter = m.group(1);
+            exceptDelimiterString = m.group(2);
+        }
+
+        if (isFind(delimiterExceptionRegex, target)) {
+            setCustomDelimiter();
+        }
+
+        return exceptDelimiterString.split(delimiter);
+    }
+
+    private static void isUnderZeroNumber(String number) {
+        if (Integer.parseInt(number) < 0) {
+            throw new RuntimeException(negativeExceptionMessage);
+        }
+    }
+
+    private static boolean isFind(String regex, String target) {
+        return getMatcher(regex, target).find();
+    }
+
+    private static Matcher getMatcher(String regex, String target) {
+        return patternCompile(regex).matcher(target);
+    }
+
+    private static Pattern patternCompile(String regex) {
+        return Pattern.compile(regex);
+    }
+
+    private static boolean isNullOrEmpty(String target) {
+        return target == null || target.isEmpty();
+    }
+
+    private static void setCustomDelimiter() {
+        delimiter = "[" + delimiter + "]";
+    }
+
+
+}
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
 
