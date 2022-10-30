0bb4ad0e0f0b001991a15c94d73a84b5c560ad78
crongcm <ucm5459@kakao.com>
2022-10-30 20:42:25 +0900

    test: 로또 수익률 계산 테스트 비즈니스 로직 제거

--- src/test/java/lotto/domain/WinningRanksTest.java
+++ src/test/java/lotto/domain/WinningRanksTest.java
@@ -2,27 +2,16 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.Test;
 
 class WinningRanksTest {
-    @ParameterizedTest
-    @CsvSource(value = {
-            "1, 2, 3, 4, 5, 6/1, 2, 3, 4, 5, 6/2000000000",
-            "1, 2, 3, 4, 5, 6/1, 2, 3, 4, 5, 7/1500000",
-            "1, 2, 3, 4, 5, 6/1, 2, 3, 4, 7, 8/50000",
-            "1, 2, 3, 4, 5, 6/1, 2, 3, 7, 8, 9/5000",
-            "1, 2, 3, 4, 5, 6/1, 2, 7, 8, 9, 10/0",
-            "1, 2, 3, 4, 5, 6/1, 7, 8, 9, 10, 11/0",
-            "1, 2, 3, 4, 5, 6/7, 8, 9, 10, 11, 12/0",
-    }, delimiter = '/')
+    @Test
     @DisplayName("로또 수익률을 구한다")
-    void lotto_calculate_earning_rate(String originNumbers, String winningNumbers, int expect) {
-        LottoPurchaseAmount lottoPurchaseAmount = new LottoPurchaseAmount("1000");
-        LottoLottery lottoLottery = lottoPurchaseAmount.toLottoLottery(new ManualNumberGenerator(originNumbers));
-        WinningRanks winningRanks = lottoLottery.checkWinningRank(WinningNumbers.of(winningNumbers));
-        assertThat(winningRanks.calculateEarningRatio(lottoPurchaseAmount))
-                .isEqualTo(Math.floor((double) expect / 1000 * 100) / 100.0);
+    void lotto_calculate_earning_rate() {
+        WinningRanks winningRanks = WinningRanks.of(Arrays.asList(WinningRank.MATCH_THREE, WinningRank.MATCH_FOUR));
+        assertThat(winningRanks.calculateEarningRatio(new LottoPurchaseAmount("100000")))
+                .isEqualTo(0.55);
     }
 }
