c5281c6a65b921ad613a1e2497285a267d28ce88
jimbaemon <jimbae@kakao.com>
2022-10-31 00:22:03 +0900

    feat: 승리 로또에 보너스 번호 포함시 예외 발생.

---
+++ src/main/java/lotto/model/Winner.java
@@ -0,0 +1,18 @@
+package lotto.model;
+
+public class Winner {
+	private final Lotto lotto;
+	private final LottoNumber bonusNumber;
+
+	public Winner(final Lotto lotto, final LottoNumber bonusNumber) {
+		validate(lotto, bonusNumber);
+		this.lotto = lotto;
+		this.bonusNumber = bonusNumber;
+	}
+
+	private void validate(final Lotto lotto, final LottoNumber bonusNumber) {
+		if (lotto.matchBonus(bonusNumber)) {
+			throw new IllegalArgumentException("보너스 번호는 승리 번호에 포함될 수 없다.");
+		}
+	}
+}
