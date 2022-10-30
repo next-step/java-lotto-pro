aa6201797143025d0e6a16270ed500b3be4ab21b
gunkim <gunkim.dev@gmail.com>
2022-10-30 16:53:06 +0900

    test: 테스트 케이스 추가

---
+++ src/test/java/step3/domain/StatisticsTests.java
@@ -0,0 +1,23 @@
+package step3.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class StatisticsTests {
+
+    @Test
+    @DisplayName("수익률을 계산한다")
+    void totalReward() {
+        List<Rank> ranks = new ArrayList<>();
+        ranks.add(Rank.MISS);
+        ranks.add(Rank.FOUR);
+
+        Statistics statistics = new Statistics(ranks);
+        assertThat(statistics.totalReward()).isEqualTo(new BigDecimal("2.50"));
+    }
+}
