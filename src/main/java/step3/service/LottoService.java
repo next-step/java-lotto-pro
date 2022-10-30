f76901a5a637adffcec786ba0e3fc7e5dd7e98a9
giraffelim <taeyang7704@naver.com>
2022-10-30 19:11:34 +0900

    feat(LottoService): 보너스 번호를 입력받고, 당첨 결과를 확인할 수 있다.

--- src/main/java/step3/service/LottoService.java
+++ src/main/java/step3/service/LottoService.java
@@ -1,10 +1,13 @@
 package step3.service;
 
-import step3.domain.LottoNumbers;
+import static step3.view.ConsoleInputView.receiveBonusNumber;
+import static step3.view.ConsoleInputView.receiveWinningNumber;
+
 import step3.domain.LottoVendingMachine;
 import step3.domain.Lottos;
 import step3.domain.Money;
 import step3.domain.Rewards;
+import step3.domain.WinningLottoNumber;
 import step3.view.ConsoleInputView;
 import step3.view.ConsoleOutputView;
 
@@ -19,8 +22,8 @@ public class LottoService {
         Money paidByUser = new Money(ConsoleInputView.receivePurchaseAmount());
         Lottos lottos = lottoVendingMachine.buy(paidByUser);
         ConsoleOutputView.printLottos(lottos);
-        LottoNumbers winningNumbers = new LottoNumbers(ConsoleInputView.receiveWinningNumber());
-        Rewards rewards = lottos.check(winningNumbers);
+        WinningLottoNumber winningLottoNumber = new WinningLottoNumber(receiveWinningNumber(), receiveBonusNumber());
+        Rewards rewards = winningLottoNumber.check(lottos);
         ConsoleOutputView.printRewards(rewards);
     }
 }
