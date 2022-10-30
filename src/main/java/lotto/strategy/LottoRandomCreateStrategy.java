468820d2252fdf1d1acb03ddce4e8815bdd1e8ac
jimbaemon <jimbae@kakao.com>
2022-10-31 00:51:03 +0900

    refactor: Integer 캐싱처리 참조...

--- src/main/java/lotto/strategy/LottoRandomCreateStrategy.java
+++ src/main/java/lotto/strategy/LottoRandomCreateStrategy.java
@@ -1,8 +1,6 @@
 package lotto.strategy;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Random;
 import java.util.Set;
 
@@ -14,23 +12,23 @@ public class LottoRandomCreateStrategy implements LottoCreateStrategy {
 	private final static int MAX_NUMBER = 45;
 	private final static int LOTTO_SIZE = 6;
 
-	private final List<LottoNumber> numberPool = createPool();
-
-	private List<LottoNumber> createPool() {
-		List<LottoNumber> result = new ArrayList<>();
-		for (int number = MIN_NUMBER; number <= MAX_NUMBER; number++) {
-			result.add(new LottoNumber(number));
-		}
-		return result;
-	}
-
 	@Override
 	public Lotto create() {
 		Set<LottoNumber> result = new HashSet<>();
 		Random random = new Random();
 		while (result.size() < LOTTO_SIZE) {
-			result.add(numberPool.get(random.nextInt(MAX_NUMBER)));
+			result.add(LottoNumberCache.cache[random.nextInt(MAX_NUMBER)]);
 		}
 		return new Lotto(result);
+	}
+
+	private static class LottoNumberCache{
+		static final LottoNumber[] cache = new LottoNumber[MAX_NUMBER];
+
+		static {
+			for (int number = MIN_NUMBER; number <= MAX_NUMBER; number++) {
+				cache[number-1] = new LottoNumber(number);
+			}
+		}
 	}
 }
