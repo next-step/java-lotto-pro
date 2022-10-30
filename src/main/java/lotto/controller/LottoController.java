e336644de094448d82224dbcd2ffe52504880ee5
OptimistLabyrinth <keesung0705@gmail.com>
2022-10-30 19:29:45 +0900

    refactor: 불필요한 주석 제거, 메서드 길이 10줄 이내로 만들기
    관련 링크
    [https://github.com/next-step/java-lotto-pro/pull/802#discussion_r1008780406](https://github.com/next-step/java-lotto-pro/pull/802#discussion_r1008780406)
    [https://github.com/next-step/java-lotto-pro/pull/802#discussion_r1008780509](https://github.com/next-step/java-lotto-pro/pull/802#discussion_r1008780509)

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -15,43 +15,29 @@ import java.util.List;
 
 public class LottoController {
     public void run() {
-        // * 사용자가 구입 금액을 입력한다.
-        // * 입력받은 구입 금액을 저장한다.
-        // * 거스름돈은 제거한다.
-        DemandMoneyToBuyInputPrinter.print();
-        final MoneyToBuy moneyToBuy = new MoneyToBuyAcceptor().accept();
-
-        // * 로또 몇 개를 구매했는지 출력한다.
-        NumberOfLottoTicketsPrinters.print(moneyToBuy);
-
-        // * 로또를 구매할 수 있는 만큼 구매한다.
-        // * 로또 한 장에는 6 개의 무작위 숫자가 있다.
-        // * 로또 한 장을 생성할 때마다 그 로또 용지를 출력해서 사용자에게 보여준다.
+        final MoneyToBuy moneyToBuy = userInputMoneyToBuy();
         int numberOfAffordableLotto = moneyToBuy.numberOfAffordableLottoTickets();
         final LottoTicketsBucket lottoTicketsBucket = new LottoTicketsBucket(numberOfAffordableLotto);
         while (0 < numberOfAffordableLotto--) {
-            final List<Integer> candidates = intsFromOneToFortyFive();
-            final LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator(candidates);
-            final LottoTicket lottoTicket = new LottoTicket(lottoNumberGenerator);
-            LottoTicketPrinter.print(lottoTicket);
-            lottoTicketsBucket.addLottoTicket(lottoTicket);
+            numberOfAffordableLottoLeft(lottoTicketsBucket);
         }
-        BlankLinePrinter.print();
+        final WinningNumbers winningNumbers = userInputWinningNumbers();
+        displayResultOfBoughtLotto(winningNumbers, lottoTicketsBucket, moneyToBuy);
+    }
 
-        // * 사용자가 당첨 번호 6 개를 입력한다.
-        DemandWinningNumbersInputPrinter.print();
-        final WinningNumbers winningNumbers = new WinningNumbersAcceptor().accept();
-        BlankLinePrinter.print();
+    private MoneyToBuy userInputMoneyToBuy() {
+        DemandMoneyToBuyInputPrinter.print();
+        final MoneyToBuy moneyToBuy = new MoneyToBuyAcceptor().accept();
+        NumberOfLottoTicketsPrinters.print(moneyToBuy);
+        return moneyToBuy;
+    }
 
-        // * 로또 각 장과 당첨 번호를 비교해서 3 개 일치, 4 개 일치, 5 개 일치, 6 개 일치하는 개수를 저장한다.
-        // * 번호 일치 개수를 확인하면서 최종 당첨금 총 합을 구한다.
-        LottoScoreTitlePrinter.print();
-        lottoTicketsBucket.sameNumberCountOfAllLottoTickets(winningNumbers);
-        LottoWinningStatisticsPrinter.print(lottoTicketsBucket);
-
-        // * [사용자의 초기 구입 금액에서 거스름돈을 뺀 금액] 을 [최종 당첨금 총 합] 으로 나누어서 총 수익률을 구한다.
-        final String profitRatioString = moneyToBuy.profitRatio(lottoTicketsBucket);
-        ProfitRatioPrinter.print(profitRatioString);
+    private void numberOfAffordableLottoLeft(LottoTicketsBucket lottoTicketsBucket) {
+        final List<Integer> candidates = intsFromOneToFortyFive();
+        final LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator(candidates);
+        final LottoTicket lottoTicket = new LottoTicket(lottoNumberGenerator);
+        LottoTicketPrinter.print(lottoTicket);
+        lottoTicketsBucket.addLottoTicket(lottoTicket);
     }
 
     private List<Integer> intsFromOneToFortyFive() {
@@ -60,5 +46,21 @@ public class LottoController {
             fullCandidateList.add(i);
         }
         return fullCandidateList;
+    }
+
+    private WinningNumbers userInputWinningNumbers() {
+        BlankLinePrinter.print();
+        DemandWinningNumbersInputPrinter.print();
+        return new WinningNumbersAcceptor().accept();
+    }
+
+    private void displayResultOfBoughtLotto(WinningNumbers winningNumbers, LottoTicketsBucket lottoTicketsBucket,
+                                            MoneyToBuy moneyToBuy) {
+        BlankLinePrinter.print();
+        LottoScoreTitlePrinter.print();
+        lottoTicketsBucket.sameNumberCountOfAllLottoTickets(winningNumbers);
+        LottoWinningStatisticsPrinter.print(lottoTicketsBucket);
+        final String profitRatioString = moneyToBuy.profitRatio(lottoTicketsBucket);
+        ProfitRatioPrinter.print(profitRatioString);
     }
 }
