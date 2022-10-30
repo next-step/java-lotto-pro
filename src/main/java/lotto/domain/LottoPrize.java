43bb01689bbc401d7a7da6e3c9dabb0bb38880e2
earth-h <earth.h.183@gmail.com>
2022-10-30 13:34:57 +0900

    refactor(domain): LottoPrize 불필요한 메소드 삭제
    LottoPrize
    - 불필요한 메소드 삭제
    - enum값 int 보기 편하도록 _ 추가

--- src/main/java/lotto/domain/LottoPrize.java
+++ src/main/java/lotto/domain/LottoPrize.java
@@ -5,11 +5,11 @@ import java.util.Arrays;
 public enum LottoPrize {
 
     NO_PRIZE(0, 0),
-    FIFTH(3, 5000),
-    FOURTH(4, 50000),
-    THIRD(5, 1500000),
-    SECOND(5, 30000000),
-    FIRST(6, 2000000000)
+    FIFTH(3, 5_000),
+    FOURTH(4, 50_000),
+    THIRD(5, 1_500_000),
+    SECOND(5, 30_000_000),
+    FIRST(6, 2_000_000_000)
     ;
 
     private final int matchCount;
@@ -18,12 +18,6 @@ public enum LottoPrize {
     LottoPrize(int matchCount, int lottoPrizeMoney) {
         this.matchCount = matchCount;
         this.lottoPrizeMoney = lottoPrizeMoney;
-    }
-
-    public static LottoPrize findLottoPrize(int matchCount) {
-        return Arrays.stream(LottoPrize.values()).filter(prize -> prize.getMatchCount() == matchCount)
-                .findFirst()
-                .orElse(NO_PRIZE);
     }
 
     public static LottoPrize findLottoPrize(int matchCount, boolean isMatchBonusLottoNumber) {
