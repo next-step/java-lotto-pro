9811707d172448f7117e4b28101b1a407797f8bf
seogineer <dgseo8981@gmail.com>
2022-10-30 15:10:43 +0900

    style: 상수 변수 형식으로 변경

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -9,16 +9,16 @@ import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class InputView {
-    private final Scanner sc = new Scanner(System.in);
+    private final Scanner SCANNER = new Scanner(System.in);
 
     int inputPay() {
         System.out.println(INPUT_PAY_MONEY);
-        return Validate.validatePay(sc.nextLine());
+        return Validate.validatePay(SCANNER.nextLine());
     }
 
     List<Integer> inputWinningNumberLastWeek() {
         System.out.println("\n" + INPUT_WINNING_NUMBER_LAST_WEEK);
-        StringTokenizer input = new StringTokenizer(sc.nextLine(), ", ");
+        StringTokenizer input = new StringTokenizer(SCANNER.nextLine(), ", ");
         List<Integer> winningNumbers = new ArrayList<>();
         for (int i = 0; i < 6; i++) {
             winningNumbers.add(Integer.parseInt(input.nextToken()));
