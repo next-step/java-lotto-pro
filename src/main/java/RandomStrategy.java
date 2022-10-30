5e32dbb0eb912730cbaa0381340f75c1f336b65b
sawook <lna1003@naver.com>
2022-10-30 13:16:52 +0900

    refactor: 외부에서 범위 숫자를 입력받도록 기능 변경

--- src/main/java/RandomStrategy.java
+++ src/main/java/RandomStrategy.java
@@ -1,16 +1,18 @@
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class RandomStrategy implements NumberStrategy {
 
-    // TODO 다른방향으로 수정
-    private static final List<Integer> lottoNumberArrange
-            = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45);
+    List<Integer> arrangeLottoNumber;
+
+
+    public RandomStrategy(List<Integer> arrangeNumber) {
+        this.arrangeLottoNumber = arrangeNumber;
+    }
 
     @Override
     public List<Integer> shuffle() {
-        Collections.shuffle(lottoNumberArrange);
-        return lottoNumberArrange.subList(0, 6);
+        Collections.shuffle(arrangeLottoNumber);
+        return arrangeLottoNumber.subList(0, 6);
     }
 }
