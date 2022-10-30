02c6655d177059eda2ec9930d90a8317f4e3aab0
taewonh <40269387+taewonh@users.noreply.github.com>
2022-10-30 13:38:28 +0900

    refactor(NumbersTest): 불필요 어노테이션 제거
    - 테스트 실행 단위를 클래스 단위로 묶어주는 어노테이션 제거
    - Stream 형태로 제공하는 MethodSource를 사용하기 위해 작성했던 어노테이션으로 해당 어노테이션을 사용하지 않고 provider를 stream 방식으로 변경하여 해결

--- src/test/java/step3/domain/NumbersTest.java
+++ src/test/java/step3/domain/NumbersTest.java
@@ -4,15 +4,12 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@TestInstance(Lifecycle.PER_CLASS)
 public class NumbersTest {
 
     private final Numbers winning = Numbers.generate(Arrays.asList(1,2,3,4,5,6));
@@ -25,7 +22,7 @@ public class NumbersTest {
         assertThat(winning.match(select)).isEqualTo(expected);
     }
 
-    private Stream<Arguments> numberProvider() {
+    private static Stream<Arguments> numberProvider() {
         return Stream.of(
                 Arguments.of(Arrays.asList(7,8,9,10,11,12), 0),
                 Arguments.of(Arrays.asList(6,7,8,9,10,11), 1),
