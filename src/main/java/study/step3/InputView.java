28b5275fd4147623212ce2037f0463dde2412081
지영진 <youngjin.jee@nhn.com>
2022-10-30 16:58:18 +0900

    refactor: Scanner 초기화 후 재사용하도록 수정

--- src/main/java/study/step3/InputView.java
+++ src/main/java/study/step3/InputView.java
@@ -3,17 +3,15 @@ package study.step3;
 import java.util.Scanner;
 
 public class InputView {
-    private static Scanner scanner;
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static int inputMoney() {
-        scanner = new Scanner(System.in);
         System.out.println("구입금액을 입력해 주세요.");
-        return scanner.nextInt();
+        return Integer.parseInt(scanner.nextLine());
     }
 
     public static String inputWinningNumbers() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-        scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
 }
