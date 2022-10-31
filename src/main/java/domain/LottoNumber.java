87179ceb8675e8a071f928a1d689ab52c6496bd3
mansik <90mansin@gmail.com>
2022-10-31 18:16:45 +0900

    feat: LottoNumber equlas,hasCode,toString 메서드 추가

--- src/main/java/domain/LottoNumber.java
+++ src/main/java/domain/LottoNumber.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.Objects;
+
 public class LottoNumber {
 
     private static final int LOTTO_MIN_NUMBER = 1;
@@ -20,5 +22,24 @@ public class LottoNumber {
         if( number < LOTTO_MIN_NUMBER || number > LOTTO_MAX_NUMBER ){
             throw new IllegalArgumentException("로또번호는 1~45까지 가능합니다.");
         }
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(number);
     }
 }
