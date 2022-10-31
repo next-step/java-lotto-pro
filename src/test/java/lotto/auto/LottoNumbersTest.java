2370436a104fb90b94d1d006dba3b0c49afa15b6
deokmoon <deok2moon@gmail.com>
2022-11-01 01:04:11 +0900

    Test: 로또번호 수동입력 시 예외처리 테스트코드 추가 작성

--- src/test/java/lotto/auto/LottoNumbersTest.java
+++ src/test/java/lotto/auto/LottoNumbersTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoNumbersTest {
     @Test
@@ -14,10 +15,16 @@ public class LottoNumbersTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {1, 2, 3})
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6})
     public void 당첨로또번호_저장_확인(int Expected) {
         String inputStr = "1, 2, 3, 4, 5, 6";
         LottoNumbers lottoNumbers = new LottoNumbers(inputStr);
         assertThat(lottoNumbers.getLottoNumbers()).contains(Expected);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "1, 2, 4, 5, 6"})
+    public void 당첨로또번호_예외_확인(String inputStr) {
+        assertThatThrownBy(() -> new LottoNumbers(inputStr)).isInstanceOf(IllegalArgumentException.class);
     }
 }
