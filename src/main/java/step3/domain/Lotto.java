0e0b5a261e605941b6ef46c3301019a234feef08
yomni <yearsheep@gmail.com>
2022-11-01 01:18:57 +0900

    feat: 당첨랭킹에 대한 구현 완료

--- src/main/java/step3/domain/Lotto.java
+++ src/main/java/step3/domain/Lotto.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Objects;
 
 public class Lotto {
+    private static final int MATCH_POINT = 1;
+    private static final int MISMATCH_POINT = 0;
     public static final int LOTTO_NUMBERS_SIZE = 6;
     public static final String EXCEPTION_MESSAGE_INCONSISTENCY_LOTTO_NUMBERS_SIZE = "로또번호 갯수는 " + LOTTO_NUMBERS_SIZE + "이어야 합니다.";
-    private final List<LottoNumber> lottoNumbers;
+    protected final List<LottoNumber> lottoNumbers;
 
     public Lotto(List<LottoNumber> lottoNumbers) {
         validateLottoNumbers(lottoNumbers);
@@ -17,6 +19,10 @@ public class Lotto {
         if (lottoNumbers.size() != LOTTO_NUMBERS_SIZE) {
             throw new IllegalArgumentException(EXCEPTION_MESSAGE_INCONSISTENCY_LOTTO_NUMBERS_SIZE);
         }
+    }
+
+    public int getMatchPoint(LottoNumber lottoNumber) {
+        return this.lottoNumbers.contains(lottoNumber) ? MATCH_POINT : MISMATCH_POINT;
     }
 
     @Override
---
+++ src/main/java/step3/domain/WinningLottoRank.java
@@ -0,0 +1,26 @@
+package step3.domain;
+
+import java.util.Arrays;
+
+public enum WinningLottoRank {
+    FIRST(6, 2_000_000_000)
+    , SECOND(5, 1_500_000)
+    , THIRD(4, 50_000)
+    , FOURTH(3, 5_000)
+    , FIFTH(0, 0);
+
+    private final int matchCount;
+    private final int reward;
+
+    WinningLottoRank(int matchCount, int reward) {
+        this.matchCount = matchCount;
+        this.reward = reward;
+    }
+
+    public static WinningLottoRank getRank(int matchCount) {
+        return Arrays.stream(WinningLottoRank.values())
+                .filter(v -> v.matchCount == matchCount)
+                .findFirst()
+                .orElse(FIFTH);
+    }
+}
