efb07f7844932e73bc254f1bc10ca7aab9886b3d
earth-h <earth.h.183@gmail.com>
2022-10-31 18:47:49 +0900

    feat(controller,view): Controller, View 로직 추가
    LottoController
    - 자동/수동 로또 나뉨에 따라 로직 추가
    
    ResultView
    - 자동/수동 로또 나뉨에 따라 출력 메시지 변경
    
    InputView
    - 수동 로또 생성에 따라 관련 출력 메시지 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,7 +1,9 @@
 package lotto.controller;
 
 import static lotto.view.InputView.printBonusLottoDirection;
+import static lotto.view.InputView.printManualLottoDirection;
 import static lotto.view.InputView.printPurchasingLottoDirection;
+import static lotto.view.InputView.printPurchasingManualLottoCountDirection;
 import static lotto.view.InputView.printWinningLottoDirection;
 import static lotto.view.InputView.readLine;
 import static lotto.view.ResultView.printExceptionErrorMessage;
@@ -11,6 +13,8 @@ import static lotto.view.ResultView.printPurchasingLottos;
 
 import common.utils.IntegerUtils;
 import common.utils.LongUtils;
+import java.util.ArrayList;
+import java.util.List;
 import lotto.domain.Lotto;
 import lotto.domain.LottoNumber;
 import lotto.domain.LottoResults;
@@ -21,13 +25,17 @@ import lotto.domain.ReadLineLottoNumberGenerator;
 
 public class LottoController {
 
+    private static final int ZERO = 0;
+
     public LottoController() {
     }
 
     public void process() {
         Lottos lottos = createLottos(getMoney());
+
         Lotto winningLotto = getWinningLotto();
         LottoNumber bonusLottoNumber = getBonusLottoNumber(winningLotto);
+
         getLottoResults(lottos.createLottoResults(winningLotto, bonusLottoNumber), lottos.findTotalPrice());
     }
 
@@ -43,9 +51,50 @@ public class LottoController {
     }
 
     private Lottos createLottos(Money money) {
-        printPurchasingLottoCount(money.maxLottoCount());
-        Lottos lottos = new Lottos(money, new RandomLottoNumberGenerator());
+        int manualLottoCount = getManualLottoCount(money);
+        List<Lotto> manualLottos = createManualLottos(manualLottoCount);
+
+        int autoLottoCount = money.maxLottoCountExclude(manualLottoCount);
+        printPurchasingLottoCount(autoLottoCount, manualLottoCount);
+
+        Lottos lottos = new Lottos(autoLottoCount, new RandomLottoNumberGenerator(), manualLottos);
         printPurchasingLottos(lottos.unmodifiedLottos());
+
+        return lottos;
+    }
+
+    private int getManualLottoCount(Money money) {
+        printPurchasingManualLottoCountDirection();
+        try {
+            String readCount = readLine();
+            int manualLottoCount = IntegerUtils.parseInt(readCount);
+            money.isBuyableLottoCount(manualLottoCount);
+            return manualLottoCount;
+        } catch (IllegalArgumentException e) {
+            printExceptionErrorMessage(e);
+            return getManualLottoCount(money);
+        }
+    }
+
+    private List<Lotto> createManualLottos(int manualLottoCount) {
+        if(manualLottoCount == ZERO) {
+            return new ArrayList<>();
+        }
+
+        printManualLottoDirection();
+        try {
+            return generateManualLottos(manualLottoCount);
+        } catch (IllegalArgumentException e) {
+            printExceptionErrorMessage(e);
+            return createManualLottos(manualLottoCount);
+        }
+    }
+
+    private List<Lotto> generateManualLottos(int manualLottoCount) {
+        List<Lotto> lottos = new ArrayList<>();
+        for(int count = 0; count < manualLottoCount; count++) {
+            lottos.add(Lotto.generateLotto(new ReadLineLottoNumberGenerator(readLine())));
+        }
         return lottos;
     }
 
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,13 +5,26 @@ import java.util.Scanner;
 public class InputView {
 
     private static final String PURCHASING_LOTTO_DIRECTION = "구입금액을 입력해 주세요.";
+    private static final String PURCHASING_MANUAL_LOTTO_DIRECTION = "수동으로 구매할 로또 수를 입력해 주세요.";
+    private static final String MANUAL_LOTTO_DIRECTION = "수동으로 구매할 번호를 입력해 주세요.";
     private static final String WINNING_LOTTO_DIRECTION = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String BONUS_LOTTO_DIRECTION = "보너스 볼을 입력해 주세요.";
+    private static final String NEW_LINE = "";
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void printPurchasingLottoDirection() {
         System.out.println(PURCHASING_LOTTO_DIRECTION);
+    }
+
+    public static void printPurchasingManualLottoCountDirection() {
+        System.out.println(NEW_LINE);
+        System.out.println(PURCHASING_MANUAL_LOTTO_DIRECTION);
+    }
+
+    public static void printManualLottoDirection() {
+        System.out.println(NEW_LINE);
+        System.out.println(MANUAL_LOTTO_DIRECTION);
     }
 
     public static String readLine() {
