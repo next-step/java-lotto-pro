2f26f5a2f5b6621b4c7dcce9173bd1535a72a7ac
강민준 <diqksrk123@naver.com>
2022-10-31 22:41:49 +0900

    feat : 당첨 통계에 2등 출력 추가

--- src/main/java/view/OutputWriter.java
+++ src/main/java/view/OutputWriter.java
@@ -4,11 +4,15 @@ import enums.Match;
 import model.LottoPurchaseAmount;
 import model.Result;
 
+import static enums.Match.isSecond;
+import static enums.Match.isZero;
+
 public class OutputWriter {
     private static final String RESULT_START_MESSAGE = System.lineSeparator() + "당첨 통계";
     private static final String RESULT_START_DELIMETER = "---------";
     private static final String PRINT_QUANTITY_FORMAT = "%d개를 구매했습니다.";
     private static final String LOTTO_RESULT = "%d개 일치 (%d원)- %d개";
+    private static final String LOTTO_RESULT_CONTAIN_BONUS = "%d개 일치, 보너스 볼 일치(%d원) - %d개";
     private static final String LOTTO_RESULT_PROFITS = "총 수익률은 %.2f입니다. (기준이 1이기 때문에 결과적으로 손해라는 의미임)";
 
     public static void printLottoQuantity(int quantity) {
@@ -28,8 +32,13 @@ public class OutputWriter {
     }
 
     private static void printResult(Match match, int lottoResultCount) {
-        if (!Match.ZERO.equals(match)) {
-            System.out.println(String.format(LOTTO_RESULT, match.getCount(), match.getAmount(), lottoResultCount));
+        String printLottoMatchFomat = LOTTO_RESULT;
+        if (isSecond(match)) {
+            printLottoMatchFomat = LOTTO_RESULT_CONTAIN_BONUS;
+        }
+
+        if (!isZero(match)) {
+            System.out.println(String.format(printLottoMatchFomat, match.getCount(), match.getAmount(), lottoResultCount));
         }
     }
 
