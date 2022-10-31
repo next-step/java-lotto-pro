73689b8b9e524f7e79dffdfb2f45565deeeea0c2
deokmoon <deok2moon@gmail.com>
2022-10-31 20:11:54 +0900

    Refactor: 잘못된 Import 제거

--- src/test/java/lotto/auto/MoneyTest.java
+++ src/test/java/lotto/auto/MoneyTest.java
@@ -1,7 +1,5 @@
 package lotto.auto;
 
-import jdk.internal.joptsimple.internal.Strings;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
