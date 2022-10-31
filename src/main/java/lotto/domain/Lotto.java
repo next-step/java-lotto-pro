38d492f0b638fbbf277e5d34c750042e0c3c0816
sueH237 <suyh237cod@naver.com>
2022-10-31 22:46:58 +0900

    feat: lotto 도메인 추가
    - lotto 6개의 중복되지 않은 숫자

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,43 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Lotto {
+    private final List<LottoNumber> lottoNumbers = new ArrayList<>();
+
+    public Lotto(List<Integer> numbers) {
+        validateLottoSize(numbers);
+        for (int number : numbers) {
+            LottoNumber lottoNumber = new LottoNumber(number);
+            duplicateLottoNumbers(lottoNumber);
+            lottoNumbers.add(lottoNumber);
+        }
+        Collections.sort(lottoNumbers);
+    }
+
+    private void validateLottoSize(List<Integer> lottoNumbers) {
+        if (lottoNumbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] Lotto는 6개의 숫자입니다.");
+        }
+    }
+
+    private void duplicateLottoNumbers(LottoNumber lottoNumber) {
+        if (lottoNumbers.contains(lottoNumber)) {
+            throw new IllegalArgumentException("[ERROR] Lotto 내 숫자는 중복될 수 없습니다.");
+        }
+    }
+
+    public List<Integer> lottoNumbers() {
+        return lottoNumberIntList(lottoNumbers);
+    }
+
+    private List<Integer> lottoNumberIntList(List<LottoNumber> lottoNumbers) {
+        List<Integer> list = new ArrayList<>();
+        for(LottoNumber lottoNumber : lottoNumbers) {
+            list.add(lottoNumber.getLottoNumber());
+        }
+        return list;
+    }
+}
---
+++ src/test/java/lotto/domain/LottoTest.java
@@ -0,0 +1,49 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class LottoTest {
+
+    @Test
+    @DisplayName("Lotto 정상 생성")
+    public void Lotto_정상생성() {
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 20, 10, 6, 34, 45));
+        Lotto lotto = new Lotto(numbers);
+        assertThat(lotto.lottoNumbers()).containsExactly(1, 6, 10, 20, 34, 45);
+    }
+
+    @Test
+    @DisplayName("Lotto 6개의 숫자로 초과 시, 예외가 발생해야 한다.")
+    public void Lotto_6개_초과() {
+        assertThatThrownBy(() -> {
+            List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 20, 10, 6, 34, 45, 32));
+            new Lotto(numbers);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("Lotto 6개의 숫자로 미만 시, 예외가 발생해야 한다.")
+    public void Lotto_6개_미만() {
+        assertThatThrownBy(() -> {
+            List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 20, 10, 6, 34));
+            new Lotto(numbers);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("Lotto 6개의 숫자 중 중복된 숫자가 있으면 예외가 발생해야 한다.")
+    public void Lotto_숫자_중복() {
+        assertThatThrownBy(() -> {
+            List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 20, 10, 6, 34, 6));
+            new Lotto(numbers);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+}
