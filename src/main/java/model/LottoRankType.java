574b4bac322b3ae2a471b7cf86a2144c9573e6e0
sawook <lna1003@naver.com>
2022-10-31 21:01:18 +0900

    feat: 보너스 넘버로 2등과 3등을 판별하도록 함
    - 5개이상의 숫자가 일치하고, 보너스 번호가 일치할경우 2등으로 판별한다.

--- src/main/java/model/LottoRankType.java
+++ src/main/java/model/LottoRankType.java
@@ -1,45 +1,50 @@
 package model;
 
 import java.util.Arrays;
-import java.util.Optional;
 
 public enum LottoRankType {
     RANK_ONE(6, 2_000_000_000),
-    RANK_TWO(5, 30_000_000, true),
-    RANK_THREE(5, 1_500_000, false),
+    RANK_TWO(5, 30_000_000),
+    RANK_THREE(5, 1_500_000),
     RANK_FOUR(4, 50_000),
     RANK_FIVE(3, 5_000),
     RANK_FAIL(0, 0);
 
     private final int count;
     private final int winMoney;
-    private boolean matchBonus;
 
     LottoRankType(int sameCount, int winMoney) {
         this.count = sameCount;
         this.winMoney = winMoney;
     }
 
-    LottoRankType(int sameCount, int winMoney, boolean matchBonus) {
-        this.count = sameCount;
-        this.winMoney = winMoney;
-        this.matchBonus = matchBonus;
-    }
-
-    public static LottoRankType convertRank(int winNumberCount) {
-        return convertRank(winNumberCount, false);
-    }
-
     public static LottoRankType convertRank(int countOfContain, boolean isContainBonusNumber) {
-        Optional<LottoRankType> result = Arrays.stream(LottoRankType.values())
-                .filter(rankType -> rankType.isMatch(countOfContain, isContainBonusNumber))
-                .findFirst();
+        LottoRankType lottoRankType = Arrays.stream(LottoRankType.values())
+                .filter(rankType -> rankType.isSame(countOfContain))
+                .findFirst()
+                .orElse(RANK_FAIL);
 
-        return result.orElse(RANK_FAIL);
+        return convertPossibleRankTwo(lottoRankType, isContainBonusNumber);
     }
 
-    private boolean isMatch(int countOfContain, boolean isContainBonusNumber) {
-        return this.count == countOfContain && this.matchBonus == isContainBonusNumber;
+    private static LottoRankType convertPossibleRankTwo(LottoRankType lottoRankType, boolean isContainBonusNumber) {
+        if (!lottoRankType.isCountFive()) {
+            return lottoRankType;
+        }
+
+        if (isContainBonusNumber) {
+            return LottoRankType.RANK_TWO;
+        }
+
+        return LottoRankType.RANK_THREE;
+    }
+
+    private boolean isCountFive() {
+        return this.count == 5;
+    }
+
+    private boolean isSame(int countOfContain) {
+        return this.count == countOfContain;
     }
 
     public int getWinMoney() {
