8edbee750c58ac236c1623e880ee113a8796d392
earth-h <earth.h.183@gmail.com>
2022-10-31 19:57:32 +0900

    test(domain): LottosTest 리팩토링
    LottosTest
    - createLottos() 메소드 리팩토링

--- src/test/java/lotto/domain/LottosTest.java
+++ src/test/java/lotto/domain/LottosTest.java
@@ -46,10 +46,9 @@ public class LottosTest {
     }
 
     private Lottos createLottos() {
-        Lottos lottos = new Lottos(Arrays.asList(
+        return new Lottos(Arrays.asList(
                 Lotto.generateLotto(new ReadLineLottoNumberGenerator("1,2,3,4,5,6")),
                 Lotto.generateLotto(new ReadLineLottoNumberGenerator("1,3,4,5,6,7")),
                 Lotto.generateLotto(new ReadLineLottoNumberGenerator("4,8,10,23,32,45"))));
-        return lottos;
     }
 }
