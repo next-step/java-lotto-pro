5acf46f4b218f9f758ce2cc9b0f71a88edccf0a8
deokmoon <deok2moon@gmail.com>
2022-11-01 01:04:53 +0900

    Feat: 로또번호 수동입력 시 참조 로또 디폴트 사이즈 정의

--- src/main/java/lotto/auto/common/Constants.java
+++ src/main/java/lotto/auto/common/Constants.java
@@ -3,4 +3,5 @@ package lotto.auto.common;
 public class Constants {
     public static final String DEFAULT_PRICE = "1000";
     public static final String DELIMITER = ",";
+    public static final int LOTTO_LENGTH = 6;
 }
