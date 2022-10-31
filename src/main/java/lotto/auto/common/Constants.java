89b0dc00ec625fdbfd89212ecdf99d61c758ce95
deokmoon <deok2moon@gmail.com>
2022-11-01 02:57:22 +0900

    Feat: 최소 당첨 숫자개수 상수 추가

--- src/main/java/lotto/auto/common/Constants.java
+++ src/main/java/lotto/auto/common/Constants.java
@@ -4,4 +4,6 @@ public class Constants {
     public static final String DEFAULT_PRICE = "1000";
     public static final String DELIMITER = ",";
     public static final int LOTTO_LENGTH = 6;
+    public static final int NUMBER_OF_MINIMUM_WINNING = 3;
+
 }
