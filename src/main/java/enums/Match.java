7583406e5d7a41e667cb8f33e3c7ae6d12fe7c17
강민준 <diqksrk123@naver.com>
2022-10-31 20:06:10 +0900

    refactor : 숫자값 잘 보일수 있도록 단위당 _찍기

--- src/main/java/enums/Match.java
+++ src/main/java/enums/Match.java
@@ -4,10 +4,11 @@ import java.util.Arrays;
 
 public enum Match {
     ZERO(0, 0),
-    THREE(3, 5000),
-    FOUR(4, 50000),
-    FIVE(5, 1500000),
-    SIX(6, 2000000000);
+    FIFTH(3, 5_000),
+    FOURTH(4, 50_000),
+    THIRD(5, 1_500_000),
+    SECOND(5, 30_000_000),
+    FIRST(6, 2_000_000_000);
 
     private final int matchCount;
     private final int amount;
