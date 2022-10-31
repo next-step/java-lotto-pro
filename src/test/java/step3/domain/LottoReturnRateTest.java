4bb3d9cc6da1a12eaab75c7dbafdde2f3f8a7fd8
dongha-byun <byunsw4@naver.com>
2022-10-31 14:32:05 +0900

    Refactor: 테스트코드 리펙터링 - LottoWinningMoney class 의존관계 변경으로 인한 테스트코드 수정 - LottoReturnRate class 생성자 수정으로 인한 테스트코드 수정

--- src/test/java/step3/domain/LottoReturnRateTest.java
+++ src/test/java/step3/domain/LottoReturnRateTest.java
@@ -10,8 +10,7 @@ public class LottoReturnRateTest {
     @Test
     @DisplayName("수익률을 계산한다.")
     void 수익률_계산(){
-        LottoReturnRate returnRate = new LottoReturnRate();
-        returnRate.calculateReturnRate(15000, 20000);
+        LottoReturnRate returnRate = new LottoReturnRate(15000, 20000);
         assertThat(returnRate).isEqualTo(new LottoReturnRate(0.75));
     }
 
@@ -19,7 +18,7 @@ public class LottoReturnRateTest {
     @DisplayName("구매금액 0원에 대한 예외 처리")
     void 구매금액_0원_예외_테스트(){
         assertThatIllegalArgumentException().isThrownBy(
-                () -> new LottoReturnRate().calculateReturnRate(15000, 0)
+                () -> new LottoReturnRate(15000, 0)
         );
     }
 }
--- src/test/java/step3/domain/LottoWinningMoneyTest.java
+++ src/test/java/step3/domain/LottoWinningMoneyTest.java
@@ -5,30 +5,31 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class LottoWinningMoneyTest {
-    LottoResult resultAll;
-    LottoResult resultOnlyThree;
+    Map<Integer, List<Lotto>> resultAll;
+    Map<Integer, List<Lotto>> resultOnlyThree;
 
     @BeforeEach
     void beforeEach(){
-        resultAll = new LottoResult(new HashMap<Integer, List<Lotto>>(){{
+        resultAll = new HashMap<Integer, List<Lotto>>(){{
             put(3, Arrays.asList(Lotto.create(Arrays.asList(1,2,3,4,5,6))));
             put(4, Arrays.asList(Lotto.create(Arrays.asList(1,2,3,4,5,6))));
             put(5, Arrays.asList(Lotto.create(Arrays.asList(1,2,3,4,5,6))));
             put(6, Arrays.asList(Lotto.create(Arrays.asList(1,2,3,4,5,6))));
-        }});
+        }};
 
-        resultOnlyThree =  new LottoResult(new HashMap<Integer, List<Lotto>>(){{
+        resultOnlyThree =  new HashMap<Integer, List<Lotto>>(){{
             put(3, Arrays.asList(Lotto.create(Arrays.asList(1,2,3,4,5,6))
                                 ,Lotto.create(Arrays.asList(1,2,3,4,5,6))
                                 ,Lotto.create(Arrays.asList(1,2,3,4,5,6))));
-        }});
+            put(4, new ArrayList<>());
+            put(5, new ArrayList<>());
+            put(6, new ArrayList<>());
+        }};
     }
 
     @Test
