3670b9800afeaf0c2c23de19243aaabf3b8e150b
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-10-31 22:33:55 +0900

    test(DelimitersTest.java) : 구분자 생성 시 기본 구분자를 포함한다.

--- src/main/java/calculator/Delimiters.java
+++ src/main/java/calculator/Delimiters.java
@@ -2,31 +2,19 @@ package calculator;
 
 import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
 import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Delimiters {
 
-    private static final Pattern regex = Pattern.compile("//(.)\n(.*)");
-    private static final List<String> defaultDelimiters = List.of(",", ":");
-    public static final int CUSTOM_DELIMITER = 1;
+    private static final List<String> DEFAULT_DELIMITERS = List.of(",", ":");
     private Set<String> delimiters = new HashSet<>();
 
-    public Delimiters(String text) {
-        delimiters.addAll(defaultDelimiters);
-        addCustomDelimiter(text);
+    public Delimiters() {
+        delimiters.addAll(DEFAULT_DELIMITERS);
     }
 
-    public void addCustomDelimiter(String text) {
-        if (Objects.isNull(text) || text.isEmpty()) {
-            return;
-        }
-        Matcher m = regex.matcher(text);
-        if (m.find()) {
-            this.delimiters.add(m.group(CUSTOM_DELIMITER));
-        }
+    public Set<String> getDelimiters() {
+        return delimiters;
     }
 
     public String delimiter() {
---
+++ src/test/java/calculator/DelimitersTest.java
@@ -0,0 +1,17 @@
+package calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("구분자")
+class DelimitersTest {
+
+    @DisplayName("구분자 생성 시 기본 구분자를 포함한다.")
+    @Test
+    void constructor() {
+        Delimiters delimiters = new Delimiters();
+        assertThat(delimiters.getDelimiters()).containsExactly(":", ",");
+    }
+}
