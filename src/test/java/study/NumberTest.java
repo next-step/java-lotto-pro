c7b7cd9fb5ca514e1af12ae9a791ecd97401cbfe
soojungkk <sj921101@naver.com>
2022-10-31 23:49:05 +0900

    test : NumberTest,NumbersTest 테스트 클래스 추가

--- src/test/java/study/NumberTest.java
+++ src/test/java/study/NumberTest.java
@@ -1,2 +1,15 @@
-package study;public class NumberTest {
+package study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class NumberTest {
+    @Test
+    void numberAddTest() {
+        Number a = Number.validator("1");
+        Number b = Number.validator("2");
+        Number result = a.add(b);
+        assertThat(result).isEqualTo(Number.validator("3"));
+    }
 }
