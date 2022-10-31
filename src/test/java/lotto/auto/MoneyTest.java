a2b65a9ea78931a7256eb0f9c2ae6963fa44c609
deokmoon <deok2moon@gmail.com>
2022-10-31 23:50:02 +0900

    Test: 문자열 숫자 변환 공통메서드화로 인한 테스트코드 재작성

--- src/test/java/lotto/auto/MoneyTest.java
+++ src/test/java/lotto/auto/MoneyTest.java
@@ -1,12 +1,9 @@
 package lotto.auto;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class MoneyTest {
     @ParameterizedTest
@@ -14,13 +11,6 @@ public class MoneyTest {
     public void 금액_Int_저장_확인(String input, int Expected) {
         Money money = new Money(input);
         assertThat(money.getMoney()).isEqualTo(Expected);
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"", "10a0", "0dsaqwe"})
-    public void 금액_문자열_예외_확인(String input) {
-        assertThatThrownBy(() -> new Money().changeStringToInt(input))
-                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @ParameterizedTest
