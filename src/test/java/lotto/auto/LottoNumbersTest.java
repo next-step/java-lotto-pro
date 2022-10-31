965122006333c541c8ac71612e4cfe76794adc60
deokmoon <deok2moon@gmail.com>
2022-10-31 23:51:44 +0900

    Test: 입력받은 문자열로 당첨로또 저장 테스트코드 작성

--- src/test/java/lotto/auto/LottoNumbersTest.java
+++ src/test/java/lotto/auto/LottoNumbersTest.java
@@ -1,6 +1,8 @@
 package lotto.auto;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -9,5 +11,13 @@ public class LottoNumbersTest {
     public void 숫자_6개_생성() {
         LottoNumbers lottoNumbers = new LottoNumbers();
         assertThat(lottoNumbers.getLottoNumbers()).hasSize(6);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    public void 당첨로또번호_저장_확인(int Expected) {
+        String inputStr = "1, 2, 3, 4, 5, 6";
+        LottoNumbers lottoNumbers = new LottoNumbers(inputStr);
+        assertThat(lottoNumbers.getLottoNumbers()).contains(Expected);
     }
 }
