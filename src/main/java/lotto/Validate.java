5ebb6b62e22a521c39ab911aa5906462be00b05d
seogineer <dgseo8981@gmail.com>
2022-10-31 16:50:09 +0900

    refactor: 숫자 범위 상수값으로 변경

--- src/main/java/lotto/Validate.java
+++ src/main/java/lotto/Validate.java
@@ -11,8 +11,10 @@ import static lotto.Constant.ERROR_INPUT_SIX_NUMBER;
 import static lotto.Constant.ERROR_LOTTO_COST;
 import static lotto.Constant.ERROR_NUMBER_RANGE;
 import static lotto.Constant.ERROR_ONLY_NUMBER;
+import static lotto.Constant.LOTTO_END_NUMBER;
 import static lotto.Constant.LOTTO_NUMBER_SIZE;
 import static lotto.Constant.LOTTO_PRICE;
+import static lotto.Constant.LOTTO_START_NUMBER;
 import static lotto.Constant.NULL;
 import static lotto.Constant.REGEX_ONLY_NUMBER;
 
@@ -49,7 +51,7 @@ public class Validate {
     }
 
     static void validateNumberRange(int number) {
-        if (number <= 0 || number >= 46) {
+        if (number < LOTTO_START_NUMBER || number > LOTTO_END_NUMBER) {
             throw new IllegalArgumentException(ERROR_NUMBER_RANGE);
         }
     }
