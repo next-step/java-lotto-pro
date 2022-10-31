b41ef638b937de13b10082c6a58bffbde94b65eb
yomni <yearsheep@gmail.com>
2022-10-31 21:15:07 +0900

    faet: 로또들 구현
    - List<Lotto>로 구성되어 있는 로또들 구현

---
+++ src/test/java/step3/domain/LottosTest.java
@@ -0,0 +1,27 @@
+package step3.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottosTest {
+
+    @DisplayName("로또리스트로 구성되어있는 로또들 생성 테스트")
+    @Test
+    void 로또들_생성() {
+        List<Lotto> lottoList = new ArrayList<>();
+        lottoList.add(LottoMachine.getLotto());
+        lottoList.add(LottoMachine.getLotto());
+        lottoList.add(LottoMachine.getLotto());
+        lottoList.add(LottoMachine.getLotto());
+        lottoList.add(LottoMachine.getLotto());
+
+        Lottos lottos = new Lottos(lottoList);
+
+        assertThat(lottos).isInstanceOf(Lottos.class);
+    }
+}
