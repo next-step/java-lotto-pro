21690cd8a5c1eedfb55b0ca064a419d8423cee7d
noelleseo <noelleseo@gmail.com>
2022-10-31 20:55:42 +0900

    refactor: 모호한 DisplayName 수정

--- src/test/java/lotto/MoneyTest.java
+++ src/test/java/lotto/MoneyTest.java
@@ -8,25 +8,25 @@ import lotto.util.Constants;
 
 public class MoneyTest {
     @Test
-    @DisplayName("구입금액_검증_숫자")
+    @DisplayName("구입금액이_숫자가_아닌_경우_오류_발생")
     public void money_validate_number() {
         assertThatThrownBy(() -> new Money("abcd")).hasMessage(Constants.ERR_VALUE_NOT_VALID);
     }
     
     @Test
-    @DisplayName("구입금액_검증_null")
+    @DisplayName("구입금액이_null인_경우_오류_발생")
     public void money_validate_null() {
         assertThatThrownBy(() -> new Money(null)).hasMessage(Constants.ERR_NULL_VALUE);
     }
     
     @Test
-    @DisplayName("구입금액_검증_빈값")
+    @DisplayName("구입금액이_빈값인_경우_오류_발생")
     public void money_validate_emtpy() {
         assertThatThrownBy(() -> new Money("")).hasMessage(Constants.ERR_NULL_VALUE);
     }
     
     @Test
-    @DisplayName("구입금액_검증_1000미만")
+    @DisplayName("구입금액이_1000미만인_경우_오류_발생")
     public void money_validate_under_thousand() {
         assertThatThrownBy(() -> new Money("999")).hasMessage("1000 이상의 숫자를 입력해 주세요.");
     }    
--- src/test/java/lotto/TicketTest.java
+++ src/test/java/lotto/TicketTest.java
@@ -24,7 +24,7 @@ public class TicketTest {
         }
 
         @Test
-        @DisplayName("발급로또_번호_검증_범위")
+        @DisplayName("발급로또_번호_1에서_45_사이인지_확인")
         public void ticket_num_validate_one_to_fortyfive() {
             IntStream.rangeClosed(0, 5).forEach(i -> {
                 assertThat(ticket.lottoNumbers.get(i)).isBetween(1, 45);
@@ -32,7 +32,7 @@ public class TicketTest {
         }
 
         @Test
-        @DisplayName("발급로또_번호_검증_중복")
+        @DisplayName("발급로또_번호가_중복을_허용하지_않는지_확인")
         public void ticket_num_validate_duplicate() {
             Set<Integer> lottoSet = new HashSet<>(ticket.lottoNumbers);
             
@@ -44,32 +44,32 @@ public class TicketTest {
     @DisplayName("당첨로또_테스트")
     class WinningTicket {
         @Test
-        @DisplayName("당첨번호_검증_개수")
+        @DisplayName("당첨번호가_6개가_아닌_경우_오류_발생")
         public void winningTicket_validate_count() {
             assertThatThrownBy(() -> new Ticket("1,2,3,4,5")).hasMessage("여섯 개의 숫자를 입력해 주세요.");
         }
         
         @Test
-        @DisplayName("당첨번호_검증_숫자")
+        @DisplayName("당첨번호가_숫자가_아닌_경우_오류_발생")
         public void winningTicket_validate_number() {
             assertThatThrownBy(() -> new Ticket("a,b,c,d,e,f")).hasMessage(Constants.ERR_VALUE_NOT_VALID);
         }
         
         @Test
-        @DisplayName("당첨번호_검증_null")
+        @DisplayName("당첨번호가_null인_경우_오류_발생")
         public void winningTicket_validate_null() {
             assertThatThrownBy(() -> new Ticket(null)).hasMessage(Constants.ERR_NULL_VALUE);
             
         }
         
         @Test
-        @DisplayName("당첨번호_검증_빈값")
+        @DisplayName("당첨번호가_빈값인_경우_오류_발생")
         public void winningTicket_validate_emtpy() {
             assertThatThrownBy(() -> new Ticket("")).hasMessage(Constants.ERR_NULL_VALUE);
         } 
         
         @Test
-        @DisplayName("당첨번호_검증_중복")
+        @DisplayName("당첨번호에_중복숫자가_있는_경우_오류_발생")
         public void winningTicket_validate_duplicate() {
             assertThatThrownBy(() -> new Ticket("1,2,3,4,5,5")).hasMessage("중복된 숫자는 허용되지 않습니다.");
         }  
