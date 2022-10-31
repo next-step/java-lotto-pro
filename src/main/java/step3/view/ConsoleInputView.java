feebac039633ad69c73d2974b3a2f6b6188b15c5
giraffelim <taeyang7704@naver.com>
2022-10-31 21:26:08 +0900

    refactor(ConsoleInputView): ENTER_THE_BONUS_NUMBER 상수 메시지 변경

--- src/main/java/step3/view/ConsoleInputView.java
+++ src/main/java/step3/view/ConsoleInputView.java
@@ -6,7 +6,7 @@ public class ConsoleInputView {
     private final static Scanner SCANNER = new Scanner(System.in);
     private static final String ENTER_THE_PURCHASE_AMOUNT = "구입 금액을 입력해주세요.";
     private static final String ENTER_THE_LAST_WEEK_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해주세요.";
-    private static final String ENTER_THE_BONUS_NUMBER = "지난 주 당첨 번호를 입력해주세요.";
+    private static final String ENTER_THE_BONUS_NUMBER = "보너스 볼을 입력해 주세요.";
 
     private ConsoleInputView() {
 
