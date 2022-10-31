3956432c2e92787aad18381c0899393de591c54d
deokmoon <deok2moon@gmail.com>
2022-11-01 01:09:18 +0900

    Refactor: 불필요한 Import 삭제

--- src/test/java/lotto/auto/BuyerTest.java
+++ src/test/java/lotto/auto/BuyerTest.java
@@ -4,9 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BuyerTest {
