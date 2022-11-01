811c67d16b9034f795d5ba62a689b5a340246236
cylee9409 <67910616+cylee9409@users.noreply.github.com>
2022-11-01 18:02:01 +0900

    [feat] 일치하는 숫자와 그에 따른 당첨금 정보를 가진 Winning 생성
    * Winning Enum 생성
    * 테스트 코드 추가

---
+++ src/test/java/lotto/domain/WinningTest.java
@@ -0,0 +1,18 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class WinningTest {
+
+    @ParameterizedTest
+    @DisplayName("로또 숫자 일치 개수에 따른 당첨금 확인 테스트")
+    @CsvSource(value = {"3:5000", "4:50000", "5:1500000", "6:2000000000"}, delimiter = ':')
+    void get_reward_by_matches_test(int matches, int reward) {
+        assertThat(Winning.getRewardsByMatch(matches)).isEqualTo(reward);
+    }
+}
