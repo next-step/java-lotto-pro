85ef69f97dc4aa386f0219501235b498a53db7b6
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-11-01 13:36:04 +0900

    test(SetTest.java) : 패키지 이동

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -1,5 +1,3 @@
-package calculator;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -1,5 +1,3 @@
-package calculator;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
