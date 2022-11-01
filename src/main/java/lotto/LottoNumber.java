75f40bbc4e259692e67909f65dd37acbf0b9ded5
seogineer <dgseo8981@gmail.com>
2022-11-01 18:29:54 +0900

    feat: 보너스볼 번호 변수, 보너스볼 번호 포함 여부 메소드 추가

--- src/main/java/lotto/LottoNumber.java
+++ src/main/java/lotto/LottoNumber.java
@@ -9,13 +9,24 @@ import java.util.List;
 
 public class LottoNumber {
     private final List<Integer> sixNumbers;
+    private final int bonusNumber;
 
     public LottoNumber(List<Integer> sixNumbers) {
         this.sixNumbers = sixNumbers;
+        this.bonusNumber = 0;
+    }
+
+    public LottoNumber(List<Integer> sixNumbers, int bonusNumber) {
+        this.sixNumbers = sixNumbers;
+        this.bonusNumber = bonusNumber;
     }
 
     public boolean contains(int number) {
         return sixNumbers.contains(number);
+    }
+
+    public boolean containsBonus(LottoNumber lottoNumber) {
+        return lottoNumber.contains(bonusNumber);
     }
 
     public int countHit(LottoNumber lottoNumber) {
--- src/test/java/lotto/LottoNumberTest.java
+++ src/test/java/lotto/LottoNumberTest.java
@@ -14,6 +14,27 @@ public class LottoNumberTest {
     }
 
     @Test
+    void 보너스_번호_포함_여부() {
+        LottoNumber lottoNumber = new LottoNumber(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6)));
+        LottoNumber winningNumber = new LottoNumber(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6)), 7);
+        assertThat(winningNumber.containsBonus(lottoNumber)).isFalse();
+    }
+
+    @Test
+    void 당첨_번호에게_로또_번호_6개가_동일한지_물어보기() {
+        LottoNumber lottoNumber = new LottoNumber(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6)));
+        LottoNumber winningNumber = new LottoNumber(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6)));
+        assertThat(winningNumber.countHit(lottoNumber)).isEqualTo(6);
+    }
+
+    @Test
+    void 당첨_번호에게_로또_번호_3개가_동일한지_물어보기() {
+        LottoNumber lottoNumber = new LottoNumber(new ArrayList<>(Arrays.asList(1, 2, 3, 10, 20, 30)));
+        LottoNumber winningNumber = new LottoNumber(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6)));
+        assertThat(winningNumber.countHit(lottoNumber)).isEqualTo(3);
+    }
+
+    @Test
     void 로또_번호_출력() {
         assertThat(new LottoNumber(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6))).toString()).isEqualTo(
                 "[1, 2, 3, 4, 5, 6]");
