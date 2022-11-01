37c99e56f5cf31a46d4699aee513da226f73a440
dongha-byun <byunsw4@naver.com>
2022-11-02 04:33:41 +0900

    Refactor: 로또번호 캐싱 처리 - LottoNumber class 에서 1~45 List 캐시생성 추가 - AutoLottoNumberStrategy 에서 LottoNumber의 캐시를 통해 로또번호 생성하도록 수정

--- src/main/java/lottoauto/domain/LottoNumber.java
+++ src/main/java/lottoauto/domain/LottoNumber.java
@@ -1,11 +1,21 @@
 package lottoauto.domain;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class LottoNumber {
     public static final int MIN_NUMBER = 1;
     public static final int MAX_NUMBER = 45;
     private int number;
+    public static final List<Integer> CACHE = new ArrayList<>();
+
+    static {
+        for(int i=MIN_NUMBER; i<=MAX_NUMBER; i++){
+            CACHE.add(i);
+        }
+    }
+
     private LottoNumber(){}
 
     private void validateRange(int number) {
@@ -13,6 +23,7 @@ public class LottoNumber {
             throw new IllegalArgumentException("로또번호의 범위(1~45)에 맞지 않습니다.");
         }
     }
+
     public static LottoNumber create(int number){
         LottoNumber lottoNumber = new LottoNumber();
         lottoNumber.validateRange(number);
--- src/main/java/lottoauto/strategy/AutoLottoNumberStrategy.java
+++ src/main/java/lottoauto/strategy/AutoLottoNumberStrategy.java
@@ -1,21 +1,16 @@
 package lottoauto.strategy;
 
-import java.util.ArrayList;
 import java.util.List;
+import lottoauto.domain.LottoNumber;
 
 import static java.util.Collections.*;
 import static lottoauto.domain.Lotto.*;
-import static lottoauto.domain.LottoNumber.MAX_NUMBER;
-import static lottoauto.domain.LottoNumber.MIN_NUMBER;
 
 public class AutoLottoNumberStrategy implements LottoNumberStrategy{
     @Override
     public List<Integer> generateNumbers() {
 
-        List<Integer> numbers = new ArrayList<>();
-        for(int i = MIN_NUMBER; i<=MAX_NUMBER; i++){
-            numbers.add(i);
-        }
+        List<Integer> numbers = LottoNumber.CACHE;
         shuffle(numbers);
         return numbers.subList(0, NUMBER_COUNT);
     }
