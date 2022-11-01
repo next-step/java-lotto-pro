37f5138f43925998b86351adaf2283a6908d07ef
cylee9409 <67910616+cylee9409@users.noreply.github.com>
2022-11-01 14:09:35 +0900

    [feat] 구입금액 유효성 검사 기능 구현
    * 구입 금액에 문자가 입력된 경우
    * 구입 금액이 양의 정수가 아닌 경우

--- src/main/java/lotto/domain/Payment.java
+++ src/main/java/lotto/domain/Payment.java
@@ -2,14 +2,37 @@ package lotto.domain;
 
 public class Payment {
 
+    private static final String NUMBER_TYPE_REQUIRED_MESSAGE = "구입금액은 숫자여야 합니다.";
+    private static final String POSITIVE_NUMBER_REQUIRED_MESSAGE = "구입금액은 양수여야 합니다.";
     private int payment;
 
     public Payment(String input) {
-        this.payment = Integer.parseInt(input);
+        this.payment = validateInputValue(input);
+    }
+
+    private int validateInputValue(String input) {
+        int num = getNumberType(input);
+        validateNegativeNumber(num);
+        return num;
+    }
+
+    private int getNumberType(String input) {
+        int num = 0;
+        try {
+            num = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NUMBER_TYPE_REQUIRED_MESSAGE);
+        }
+        return num;
+    }
+
+    private void validateNegativeNumber(int num) {
+        if (num < 0) {
+            throw new IllegalArgumentException(POSITIVE_NUMBER_REQUIRED_MESSAGE);
+        }
     }
 
     public int getPayment() {
         return this.payment;
     }
-
 }
