0d4904f336d25709e2cec21306c1331def4caadc
cylee9409 <67910616+cylee9409@users.noreply.github.com>
2022-11-01 16:21:32 +0900

    [feat] Messages에 항목 추가

--- src/main/java/lotto/view/Messages.java
+++ src/main/java/lotto/view/Messages.java
@@ -3,7 +3,13 @@ package lotto.view;
 public enum Messages {
 
     ASK_PAYMENT("구입 금액을 입력해주세요."),
-    NOTIFY_PURCHASES("개를 구매했습니다.");
+    NOTIFY_PURCHASES("개를 구매했습니다."),
+    ASK_LAST_WIN_LOTTO_NUMBERS("지난 주 당첨 번호를 입력해주세요."),
+    WIN_STATISTICS("당첨 통계"),
+    MESSAGE_DELIMITER("--------------"),
+    SHOW_MATCHES_INFO("%d개 일치 (%d원) - %d개"),
+    SHOW_TOTAL_YIELD("총 수익률은 %.2f입니다. (기준이 1이기 때문에 결과적으로 손해라는 의미임)");
+
     private String msg;
 
     Messages(String msg) {
