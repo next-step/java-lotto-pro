e0a0368d9afc0cfe6227e0b663e3f789ef792836
iamsojung <sojungkid@gmail.com>
2022-11-01 22:51:15 +0900

    feat: Rank에 2등 추가

--- src/main/java/step3/domain/Rank.java
+++ src/main/java/step3/domain/Rank.java
@@ -6,9 +6,10 @@ import java.util.Optional;
 
 public enum Rank {
     FIRST(6, 2_000_000_000),
-    SECOND(5, 1_500_000),
-    THIRD(4, 50_000),
-    FOURTH(3, 5000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5000),
     NOTHING(0,0);
 
     private final int count;
@@ -35,7 +36,7 @@ public enum Rank {
     }
 
     public static List<Rank> valuesTheLowestOrder() {
-        return Arrays.asList(FOURTH, THIRD, SECOND, FIRST);
+        return Arrays.asList(FIFTH, FOURTH, THIRD, SECOND, FIRST);
     }
 
 }
