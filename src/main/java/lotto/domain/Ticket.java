4fb015ee3582716a0e82984d742ce3ecab98f914
noelleseo <noelleseo@gmail.com>
2022-11-01 21:14:04 +0900

    style: 자바 컨벤션에 따른 들여쓰기 수정

--- src/main/java/lotto/domain/Ticket.java
+++ src/main/java/lotto/domain/Ticket.java
@@ -4,14 +4,14 @@ import lotto.util.Constants;
 
 public class Ticket {
     private static final String STR_LOTTO_NUM_LIST = "[%d, %d, %d, %d, %d, %d]\n";
-    
+
     public LottoNumbers lottoNumbers;
     public BonusNum bonusNum;
 
     public Ticket() {
         this.lottoNumbers = new LottoNumbers();
     }
-    
+
     public Ticket(String ticketStr) {
         this.lottoNumbers = new LottoNumbers(ticketStr);
     }
@@ -19,24 +19,24 @@ public class Ticket {
     public Ticket(String ticketStr, String bonusNumStr) {
         this.lottoNumbers = new LottoNumbers(ticketStr);
         this.bonusNum = new BonusNum(bonusNumStr);
-        
+
         isBonusballDuplicate();
     }
 
     public String toString() {
         return String.format(STR_LOTTO_NUM_LIST, lottoNumbers.numbers);
     }
-    
+
     public boolean isBonusballMatch(BonusNum bonusNum) {
         return this.lottoNumbers.isNumberContains(bonusNum.num);
     }
-    
+
     public int getCountOfMatch(LottoNumbers winningNumbers) {
         return this.lottoNumbers.getCountOfMatch(winningNumbers);
     }
-    
+
     private void isBonusballDuplicate() {
-        if(this.lottoNumbers.isNumberContains(this.bonusNum.num)) {
+        if (this.lottoNumbers.isNumberContains(this.bonusNum.num)) {
             throw new IllegalArgumentException(Constants.ERR_DUP_NUMBERS);
         }
     }
