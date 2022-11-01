97b361598b11b51f8a18525bbe2b70b4eef4d000
choiyongseok90 <badaelephant>
2022-11-01 19:31:12 +0900

    refactor: 일급 컬렉션을 만드는 인자는, 단위 원소 객체의 컬렉션 객체가 되도록 수정 List<Integer>를 생성자에 넣는 대신 List<LottoNumber>를 넣도록 수정

--- src/main/java/step3/model/lotto/Lotto.java
+++ src/main/java/step3/model/lotto/Lotto.java
@@ -6,27 +6,22 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
+import step3.model.machine.Match;
 import step3.model.value.ErrMsg;
 import step3.model.value.Rule;
 
 public class Lotto{
-    private List<LottoNumber> lotto;
-    public Lotto(List<Integer> lotto) {
+    private final List<LottoNumber> lotto;
+    public Lotto(List<LottoNumber> lotto) {
         verifyLotto(lotto);
-        this.lotto =createLotto(lotto);
+        this.lotto = lotto;
     }
 
-    private List<LottoNumber> createLotto(List<Integer> lotto) {
-        return Collections.unmodifiableList(lotto.stream().map(LottoNumber::new).collect(Collectors.toList()));
-    }
 
     public boolean isMatched(LottoNumber lottoNumber) {
         return lotto.contains(lottoNumber);
     }
-    public int getMatchCount(Lotto winningLotto){
-        return (int) lotto.stream().filter(winningLotto::isMatched).count();
-    }
-    private void verifyLotto(List<Integer> lottoNumbers) {
+    private void verifyLotto(List<LottoNumber> lottoNumbers) {
         if(lottoNumbers.size() != Rule.LOTTO_NUMBER_LENGTH){
             throw new IllegalArgumentException(ErrMsg.WRONG_LENGTH);
         }
--- src/test/java/step3/model/lotto/LottoTest.java
+++ src/test/java/step3/model/lotto/LottoTest.java
@@ -4,24 +4,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 class LottoTest {
-
-    @Test
-    void 정상_생성_프린트_검증() {
-        Lotto lotto = new Lotto(Arrays.asList(1,2,3,4,5,6));
-        assertThat(lotto.toString()).isEqualTo("[1, 2, 3, 4, 5, 6]");
+    private List<LottoNumber> changeToLottoNumbers(List<Integer> numbers){
+        return numbers.stream().map(LottoNumber::new).collect(Collectors.toList());
     }
     @Test
-    void 로또_같음_테스트() {
-        assertThat(new Lotto(Arrays.asList(1,2,3,4,5,6))).isEqualTo(new Lotto(Arrays.asList(6,5,4,3,2,1)));
+    void 정상_생성_프린트_검증() {
+        Lotto lotto = new Lotto(changeToLottoNumbers(Arrays.asList(1,2,3,4,5,6)));
+        assertThat(lotto.toString()).isEqualTo("[1, 2, 3, 4, 5, 6]");
     }
     @ParameterizedTest
     @CsvSource(value = {"1:true", "2:true", "3:true", "7:false", "8:false"}, delimiter = ':')
     void 로또_번호_포함_테스트(int number, boolean result) {
-        Lotto lotto = new Lotto(Arrays.asList(1,2,3,4,5,6));
+        Lotto lotto = new Lotto(changeToLottoNumbers(Arrays.asList(1,2,3,4,5,6)));
         assertThat(lotto.isMatched(new LottoNumber(number))).isEqualTo(result);
     }
 
