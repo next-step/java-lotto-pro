60253ba83657d507c78bdc5e2852df16f767e9bf
cylee9409 <67910616+cylee9409@users.noreply.github.com>
2022-11-01 14:46:48 +0900

    [feat] 로또 최소 구입 금액 유효성 검증 기능 구현

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -4,12 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Lottos {
-
+    private static final String MIN_PAYMENT_REQUIRED_MESSAGE = "로또 구입을 위해서는 최소 1000원 이상의 구입금액이 필요합니다.";
     private static final int UNIT_PRICE_PER_LOTTO = 1000;
     private List<Lotto> lottos;
 
     public Lottos(Payment payment) {
+        validateMinPayment(payment);
         this.lottos = generateLottos(payment);
+    }
+
+    private void validateMinPayment(Payment payment) {
+        if (payment.getPayment() < UNIT_PRICE_PER_LOTTO) {
+            throw new IllegalArgumentException(MIN_PAYMENT_REQUIRED_MESSAGE);
+        }
     }
 
     private List<Lotto> generateLottos(Payment payment) {
--- src/test/java/lotto/domain/LottosTest.java
+++ src/test/java/lotto/domain/LottosTest.java
@@ -4,13 +4,20 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class LottosTest {
 
     @Test
     @DisplayName("로또 구매 개수")
-    void purchaseLottoCnt() {
+    void purchase_lotto_cnt_test() {
         assertThat(new Lottos(new Payment("14000")).getLottosSize()).isEqualTo(14);
     }
 
+    @Test
+    @DisplayName("구입금액에 1000원 미만인 경우 예외처리")
+    void payment_less_than_1000_test() {
+        assertThatThrownBy(() -> new Lottos(new Payment("400")))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
