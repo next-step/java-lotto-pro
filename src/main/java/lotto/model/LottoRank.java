01012e2e2aaa98a84046fbdbc3be419cba93f9b8
Hyeongwon-up <leehwdatabase@gmail.com>
2022-11-02 00:45:59 +0900

    feat(LottoRank.java): - 로또의 당첨 조건 및 금액을 관리할 LottoRank 추가. - 위 enum 클래스 추가로 인하여 사용하지 않는 RankCode.java 제거

---
+++ src/main/java/lotto/model/LottoRank.java
@@ -0,0 +1,54 @@
+package lotto.model;
+
+import java.util.Arrays;
+
+public enum LottoRank {
+  FIRST(6, false, 2_000_000_000),
+  SECOND(5, true, 30_000_000),
+  THIRD(5, false, 1_500_000),
+  FORTH(4, false, 50_000),
+  FIFTH(3, false, 5_000),
+  NOTHING(0, false, 0);
+
+  private final int containsCount;
+  private final boolean isContainBonusNumber;
+  private final int money;
+
+  LottoRank(int containsCount, boolean isContainBonusNumber, int money) {
+    this.containsCount = containsCount;
+    this.isContainBonusNumber = isContainBonusNumber;
+    this.money = money;
+  }
+
+  public static LottoRank getLottoRank(int containsCount, boolean isContainBonusNumber) {
+    if (isSecondRank(containsCount)) {
+      return getSecondLottoRank(containsCount, isContainBonusNumber);
+    }
+
+    return Arrays.stream(LottoRank.values())
+        .filter(lottoRank -> lottoRank.containsCount == containsCount)
+        .findAny()
+        .orElse(NOTHING);
+  }
+
+  public static LottoRank getSecondLottoRank(int containsCount, boolean isContainBonusNumber) {
+    return Arrays.stream(LottoRank.values())
+        .filter(
+            lottoRank -> lottoRank.containsCount == containsCount
+                && isContainBonusNumber == SECOND.isContainBonusNumber)
+        .findAny()
+        .orElse(THIRD);
+  }
+
+  public static boolean isSecondRank(int containsCount) {
+    return containsCount == LottoRank.SECOND.containsCount;
+  }
+
+  public int containsCount() {
+    return this.containsCount;
+  }
+
+  public int getMoney() {
+    return this.money;
+  }
+}
