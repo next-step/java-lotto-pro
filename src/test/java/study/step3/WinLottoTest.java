d067e12dba5d20f3dffe02171ebc23e2d59b1a30
mansik <90mansin@gmail.com>
2022-11-02 00:25:13 +0900

    refactor: 테스트 중복 코드 제거
    - ParameterizedTest를 통한 중복 코드 제거

--- src/test/java/study/step3/WinLottoTest.java
+++ src/test/java/study/step3/WinLottoTest.java
@@ -4,6 +4,9 @@ import domain.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +16,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 class WinLottoTest {
     private List<LottoNumber> lottoNumbers;
     private Lotto lotto;
+
+    private Lottos lottos;
 
     @BeforeEach
     public void inputLottoSetup() {
@@ -25,65 +30,20 @@ class WinLottoTest {
         lottoNumbers.add(new LottoNumber(20));
 
         lotto = new Lotto(lottoNumbers);
-    }
-
-    @Test
-    @DisplayName("로또번호 3개 맞춘 카운트 검증")
-    public void 맞춘개수_3개_검증() {
-        String given = "1,10,3,4,7,19";
-
-        Lotto winLottoList = new Lotto(lottoNumbers);
-        WinLotto winLotto = new WinLotto(given, new WinReport());
-
-        List<Lotto> lottoList = new ArrayList<>();
-        lottoList.add(lotto);
-
-        WinReport winReport = winLotto.findWinner(lottoList);
-        assertThat(winReport.getLottoResult(3)).isEqualTo(1);
+        lottos = new Lottos(new ArrayList<>());
 
     }
 
-    @Test
-    @DisplayName("로또번호 4개 맞춘 카운트 검증")
-    public void 맞춘개수_4개_검증() {
-        String given = "1,45,3,4,17,19";
-        Lotto winLottoList = new Lotto(lottoNumbers);
-        WinLotto winLotto = new WinLotto(given, new WinReport());
+    @ParameterizedTest
+    @CsvSource(value = {"1,10,3,4,7,19:3", "1,45,3,4,7,19:4", "1,45,3,4,18,19:5", "1,45,3,4,18,20:6"}, delimiter = ':')
+    @DisplayName("로또번호 맞춘 갯수 검증")
+    public void 로또번호_맞춘개수_검증(String given, int collectNumber) {
+        WinLotto winLotto = new WinLotto(given);
 
-        List<Lotto> lottoList = new ArrayList<>();
-        lottoList.add(lotto);
+        lottos.add(lotto);
 
-        WinReport winReport = winLotto.findWinner(lottoList);
-        assertThat(winReport.getLottoResult(4)).isEqualTo(1);
+        WinReport winReport = winLotto.findWinner(lottos);
+        assertThat(winReport.getLottoResult(collectNumber)).isEqualTo(1);
     }
 
-    @Test
-    @DisplayName("로또번호 5개 맞춘 카운트 검증")
-    public void 맞춘개수_5개_검증() {
-        String given = "1,45,3,4,18,19";
-
-        Lotto winLottoList = new Lotto(lottoNumbers);
-        WinLotto winLotto = new WinLotto(given, new WinReport());
-
-        List<Lotto> lottoList = new ArrayList<>();
-        lottoList.add(lotto);
-
-        WinReport winReport = winLotto.findWinner(lottoList);
-        assertThat(winReport.getLottoResult(5)).isEqualTo(1);
-    }
-
-    @Test
-    @DisplayName("로또번호 6개 맞춘 카운트 검증")
-    public void 맞춘개수_6개_검증() {
-        String given = "1,45,3,4,18,20";
-
-        Lotto winLottoList = new Lotto(lottoNumbers);
-        WinLotto winLotto = new WinLotto(given, new WinReport());
-
-        List<Lotto> lottoList = new ArrayList<>();
-        lottoList.add(lotto);
-
-        WinReport winReport = winLotto.findWinner(lottoList);
-        assertThat(winReport.getLottoResult(6)).isEqualTo(1);
-    }
 }
