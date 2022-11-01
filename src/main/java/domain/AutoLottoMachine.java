b99e722d7253aa8dffa5ddd8dfdaa04376529069
mansik <90mansin@gmail.com>
2022-11-01 22:30:31 +0900

    fix: 불필요한 public static 사용 제거
    AS IS:
     public static void setupLotto()
     : 외부에서 호출하면 중복으로 로또 넘버가 추가될 수 있음.
    TO BE:
     private void setUpLotto()

--- src/main/java/domain/AutoLottoMachine.java
+++ src/main/java/domain/AutoLottoMachine.java
@@ -4,32 +4,33 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class AutoLottoMachine implements LottoMachine{
+public class AutoLottoMachine implements LottoMachine {
     private static final List<LottoNumber> LOTTO_NUMBERS = new ArrayList<>();
     private static final int LOTTE_MIN_NUMBER = 1;
-    private static final int LOTTE_MAX_NUMBER = 45 ;
+    private static final int LOTTE_MAX_NUMBER = 45;
 
-    public AutoLottoMachine(){
+    public AutoLottoMachine() {
         setupLotto();
     }
 
-    public static void setupLotto(){
-        for (int i=LOTTE_MIN_NUMBER; i<=LOTTE_MAX_NUMBER; i++ ){
+    private void setupLotto() {
+        for (int i = LOTTE_MIN_NUMBER; i <= LOTTE_MAX_NUMBER; i++) {
             LOTTO_NUMBERS.add(new LottoNumber(i));
         }
 
     }
+
     @Override
     public Lotto createLottoNumber() {
         Collections.shuffle(LOTTO_NUMBERS);
-        return new Lotto(new ArrayList<>(LOTTO_NUMBERS.subList(0,6)));
+        return new Lotto(new ArrayList<>(LOTTO_NUMBERS.subList(0, 6)));
     }
 
 
     @Override
     public List<Lotto> purchaseLotte(int lottoTicketCount) {
         List<Lotto> lottoList = new ArrayList<>();
-        for(int i=0; i<lottoTicketCount; i++){
+        for (int i = 0; i < lottoTicketCount; i++) {
             lottoList.add(createLottoNumber());
         }
         return lottoList;
