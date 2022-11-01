2b6dc2fb66ad8bec96303f6587350dbd7369b143
dongha-byun <byunsw4@naver.com>
2022-11-02 01:42:34 +0900

    Refactor: AutoLottoNumberStrategy class 코드 리펙터링 - validate 로직 불필요로 인해 삭제처리

--- src/main/java/lottoauto/strategy/AutoLottoNumberStrategy.java
+++ src/main/java/lottoauto/strategy/AutoLottoNumberStrategy.java
@@ -11,8 +11,6 @@ import static lottoauto.domain.LottoNumber.MIN_NUMBER;
 public class AutoLottoNumberStrategy implements LottoNumberStrategy{
     @Override
     public List<Integer> generateNumbers() {
-        validateRange(MIN_NUMBER, MAX_NUMBER);
-        validateCount(MIN_NUMBER, MAX_NUMBER, NUMBER_COUNT);
 
         List<Integer> numbers = new ArrayList<>();
         for(int i = MIN_NUMBER; i<=MAX_NUMBER; i++){
@@ -20,17 +18,5 @@ public class AutoLottoNumberStrategy implements LottoNumberStrategy{
         }
         shuffle(numbers);
         return numbers.subList(0, NUMBER_COUNT);
-    }
-
-    public void validateRange(int min, int max) {
-        if(min > max){
-            throw new IllegalArgumentException("최소 최대 범위가 옳바르지 않습니다.");
-        }
-    }
-
-    public void validateCount(int min, int max, int count) {
-        if(max - min + 1 < count){
-            throw new IllegalArgumentException("생성하고자 하는 원소의 갯수가 범위 내 원소의 갯수보다 많습니다.");
-        }
     }
 }
