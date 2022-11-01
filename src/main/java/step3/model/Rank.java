1529dbfd515b6e7fbee0e0dad39e5a747eb41843
chunhodong <yhms4432@naver.com>
2022-11-02 00:01:36 +0900

    refactor: Rank클래스에서 RankFunction제거
    - 필요한 BiFuction함수와,matchCount를 Rank에서 초기화

--- src/main/java/step3/model/Rank.java
+++ src/main/java/step3/model/Rank.java
@@ -1,27 +1,30 @@
 package step3.model;
 
 import java.util.Arrays;
+import java.util.function.BiFunction;
 
 public enum Rank {
 
-    FIRST(new RankFunction((countOfMatch, isMatchBonus) -> countOfMatch == 6, 6), 2_000_000_000),
-    SECOND(new RankFunction((countOfMatch, isMatchBonus) -> countOfMatch == 5 && isMatchBonus, 5), 30_000_000),
-    THIRD(new RankFunction((countOfMatch, isMatchBonus) -> countOfMatch == 5 && !isMatchBonus, 5), 1_500_000),
-    FOURTH(new RankFunction((countOfMatch, isMatchBonus) -> countOfMatch == 4, 4), 50_000),
-    FIFTH(new RankFunction((countOfMatch, isMatchBonus) -> countOfMatch == 3, 3), 5_000),
-    MISS(new RankFunction((countOfMatch, isMatchBonus) -> countOfMatch < 3, 0), 0);
+    FIRST((matchCount, isBonus) -> matchCount == 6, 6, 2_000_000_000),
+    SECOND((matchCount, isBonus) -> matchCount == 5 && isBonus, 5, 30_000_000),
+    THIRD((matchCount, isBonus) -> matchCount == 5 && !isBonus, 5, 1_500_000),
+    FOURTH((matchCount, isBonus) -> matchCount == 4, 4, 50_000),
+    FIFTH((matchCount, isBonus) -> matchCount == 3, 3, 5_000),
+    MISS((matchCount, isBonus) -> matchCount < 3, 0, 0);
 
     private final int winningPrice;
-    private final RankFunction rankFunction;
+    private final BiFunction<Integer, Boolean, Boolean> matchFunction;
+    private final int matchCount;
 
-    Rank(RankFunction rankFunction, int winningPrice) {
-        this.rankFunction = rankFunction;
+    Rank(BiFunction<Integer, Boolean, Boolean> matchFunction, int matchCount, int winningPrice) {
+        this.matchCount = matchCount;
+        this.matchFunction = matchFunction;
         this.winningPrice = winningPrice;
     }
 
     public static Rank valueOf(int matchCount, boolean hasBonus) {
         return Arrays.stream(Rank.values())
-                .filter(rank -> rank.rankFunction.isMatch(matchCount, hasBonus))
+                .filter(rank -> rank.matchFunction.apply(matchCount, hasBonus))
                 .findFirst()
                 .orElse(MISS);
     }
@@ -31,7 +34,7 @@ public enum Rank {
     }
 
     public int getMatchCount() {
-        return rankFunction.getMatchCount();
+        return matchCount;
     }
 
 }
