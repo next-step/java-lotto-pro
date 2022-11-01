950c80be5e95061be14579ec198879e7d3cf0b36
deveo-pyo <jungpyo9@gmail.com>
2022-11-02 04:39:04 +0900

    feat: 보너스 번호 입력 메소드 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -10,12 +10,22 @@ public class InputView {
 
     private static final String ASK_MESSAGE_PAY_AMOUNT = "구입금액을 입력해 주세요.";
     private static final String ASK_MESSAGE_LAST_WINNING_NUMBERS = "\n지난 주 당첨 번호를 입력해 주세요.";
+    private static final String ASK_MESSAGE_BONUS_BALL = "보너스 볼을 입력해 주세요.";
     private static final String WINNING_NUMBERS_DELIMITER = ",";
     private static final String WHITE_SPACE = " ";
     private static final String EMPTY_STRING = "";
 
     public static int inputPayAmount() {
         System.out.println(ASK_MESSAGE_PAY_AMOUNT);
+        return nextInt();
+    }
+
+    public static int inputBonusBall() {
+        System.out.println(ASK_MESSAGE_BONUS_BALL);
+        return nextInt();
+    }
+
+    private static int nextInt(){
         String input = new Scanner(System.in).next();
         InputValidator.validateNumberFormat(input);
         return Integer.parseInt(input);
@@ -30,7 +40,6 @@ public class InputView {
             InputValidator.validateNumberFormat(stringFormatNumber);
             winningNumbers.add(Integer.parseInt(stringFormatNumber));
         }
-
         return winningNumbers;
     }
 }
