a84923eb931b1124e12175c0cf0ac1ac7729e70a
paki1019 <paki1019@gmail.com>
2022-11-01 21:38:10 +0900

    refactor: 코드 리뷰 반영 - 정적 팩토리 메서드 캐싱 반영

--- src/main/java/lotto/domain/lotto/LottoNumber.java
+++ src/main/java/lotto/domain/lotto/LottoNumber.java
@@ -1,22 +1,33 @@
 package lotto.domain.lotto;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
+import java.util.stream.IntStream;
 
 public class LottoNumber implements Comparable<LottoNumber> {
 	public static final int MIN_RANGE = 1;
 	public static final int MAX_RANGE = 45;
+
+	private static final Map<Integer, LottoNumber> lottoNumberCache = new HashMap<>();
+
+	static {
+		IntStream.rangeClosed(MIN_RANGE, MAX_RANGE)
+			.forEach(i -> lottoNumberCache.put(i, new LottoNumber(i)));
+	}
+
 	private final int lottoNumber;
 
 	private LottoNumber(final int lottoNumber) {
-		validateLottoNumberRange(lottoNumber);
 		this.lottoNumber = lottoNumber;
 	}
 
 	public static LottoNumber from(int lottoNumber) {
-		return new LottoNumber(lottoNumber);
+		validateLottoNumberRange(lottoNumber);
+		return lottoNumberCache.get(lottoNumber);
 	}
 
-	private void validateLottoNumberRange(final int lottoNumber) {
+	private static void validateLottoNumberRange(final int lottoNumber) {
 		if (lottoNumber < MIN_RANGE) {
 			throw new IllegalArgumentException("로또 번호는 1 ~ 45 범위여야 합니다.");
 		}
