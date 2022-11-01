52a4771b5441bc4d8fc2a2c2afb6a97d1758e2f8
giraffelim <taeyang7704@naver.com>
2022-11-01 23:01:54 +0900

    feat(LottoService): 수동 로또를 구매하고 병합해 당첨 통계를 도출하는 기능 추가

--- src/main/java/step3/service/LottoService.java
+++ src/main/java/step3/service/LottoService.java
@@ -1,5 +1,6 @@
 package step3.service;
 
+import java.util.List;
 import step3.domain.LottoVendingMachine;
 import step3.domain.Lottos;
 import step3.domain.Money;
@@ -10,6 +11,7 @@ import step3.view.ConsoleOutputView;
 
 public class LottoService {
     private final LottoVendingMachine lottoVendingMachine;
+    private final Money LOTTO_PRICE = new Money(1000L);
 
     public LottoService(final LottoVendingMachine lottoVendingMachine) {
         this.lottoVendingMachine = lottoVendingMachine;
@@ -17,14 +19,28 @@ public class LottoService {
 
     public void execute() {
         Money paidByUser = new Money(ConsoleInputView.receivePurchaseAmount());
-        Lottos lottos = lottoVendingMachine.buy(paidByUser);
-        ConsoleOutputView.printLottos(lottos);
+        Lottos lottos = buyLottos(paidByUser);
         WinningLottoNumber winningLottoNumber = receiveWiningLottoNumber();
         Rewards rewards = lottos.check(winningLottoNumber);
         ConsoleOutputView.printRewards(rewards);
     }
 
+    private Lottos buyLottos(Money paidByUser) {
+        Lottos manualLottos = lottoVendingMachine.buy(paidByUser, receiveManualLottoNumbers());
+        Money change = paidByUser.pay(LOTTO_PRICE.multiply(manualLottos.getHasLottoSize()));
+        Lottos autoLottos = lottoVendingMachine.buy(change);
+        ConsoleOutputView.printLottosCount(manualLottos.getHasLottoSize(), autoLottos.getHasLottoSize());
+        Lottos lottos = manualLottos.merge(autoLottos);
+        ConsoleOutputView.printLottos(lottos);
+        return lottos;
+    }
+
     private WinningLottoNumber receiveWiningLottoNumber() {
         return new WinningLottoNumber(ConsoleInputView.receiveWinningNumber(), ConsoleInputView.receiveBonusNumber());
+    }
+
+    private List<String> receiveManualLottoNumbers() {
+        int purchaseCount = ConsoleInputView.receiveManualLottoCount();
+        return ConsoleInputView.receiveManualLottoNumbers(purchaseCount);
     }
 }
