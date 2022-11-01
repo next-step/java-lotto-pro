de78fd04dd5de1a3ba158b8cd10996180ff76990
chunhodong <yhms4432@naver.com>
2022-11-02 00:34:13 +0900

    refactor: LottoTest에서 자동여부확인 테스트삭제

--- src/test/java/step3/LottoTest.java
+++ src/test/java/step3/LottoTest.java
@@ -161,36 +161,4 @@ public class LottoTest {
         assertThat(rank).isEqualTo(Rank.MISS);
     }
 
-    @Test
-    @DisplayName("기본생성자인경우 로또조회시 자동로또")
-    void test_that_returns_auto_if_base_constructor() {
-        //given
-        List<LottoNumber> numbers = IntStream.rangeClosed(1, 6).boxed()
-                .map(LottoNumber::valueOf)
-                .collect(Collectors.toList());
-
-        //when
-        Lotto lotto = new Lotto(numbers);
-
-        //then
-        assertThat(lotto.isAuto()).isTrue();
-    }
-
-    @ParameterizedTest
-    @ValueSource(booleans = {true, false})
-    @DisplayName("생성자에서 로또 자동여부 지정시 메소드에서 조회")
-    void test_that_returns_auto_if_selected_at_constructor(boolean isAuto) {
-        //given
-        List<LottoNumber> numbers = IntStream.rangeClosed(1, 6).boxed()
-                .map(LottoNumber::valueOf)
-                .collect(Collectors.toList());
-
-        //when
-        Lotto lotto = new Lotto(numbers, isAuto);
-
-        //then
-        assertThat(lotto.isAuto()).isEqualTo(isAuto);
-    }
-
-
 }
