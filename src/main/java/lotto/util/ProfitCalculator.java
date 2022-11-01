32bfbe23ff5ea119640c4958d0872edd32109a3d
deveo-pyo <jungpyo9@gmail.com>
2022-11-02 04:53:16 +0900

    feat: 로또 수익률 계산시 enum 사용하도록 로직 수정
    1.로또 수익률 계산시 enum 사용하도록 로직 수정
    2.테스트 수정

--- src/main/java/lotto/util/ProfitCalculator.java
+++ src/main/java/lotto/util/ProfitCalculator.java
@@ -1,18 +1,16 @@
 package lotto.util;
 
+import lotto.constants.Rank;
 import lotto.domain.LottoResult;
 
 import static lotto.domain.PayAmount.LOTTO_PRICE;
 
 public class ProfitCalculator {
 
-    public static final int[] PRIZE_MONEY = {0, 0, 0, 5000, 50000, 1500000, 2000000000};
-    public static final int MIN_WINNING_NUM = 3;
-
     public static double calculateProfit(LottoResult lottoResult, int lottoAmount) {
         double profit = 0;
-        for (int i = MIN_WINNING_NUM; i < PRIZE_MONEY.length; i++) {
-            profit += lottoResult.getLottoResult(i) * PRIZE_MONEY[i];
+        for (Rank rank : lottoResult.getLottoResult().keySet()) {
+            profit += rank.getWinningMoney() * lottoResult.getLottoResult().getOrDefault(rank, 0);
         }
 
         return Math.floor(profit / (lottoAmount * LOTTO_PRICE) * 100) / 100;
