c38e4ee07c9e4fcf29fb86aeedf4ed242268d16c
cylee9409 <67910616+cylee9409@users.noreply.github.com>
2022-11-01 13:13:41 +0900

    [fix] 2단계 문자열 덧셈 계산기 리뷰 반영

--- src/main/java/calculator/domain/Numbers.java
+++ src/main/java/calculator/domain/Numbers.java
@@ -5,22 +5,23 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Nums {
+public class Numbers {
 
     private static final String ILLEGAL_DELIMITER_MESSAGE = "커스텀 구분자는 1자리의 단일 값이어야 합니다.";
     private static String DELIMITER_PATTERN = ",|:";
     private static String SEPARATOR = "|";
     private static final String CUSTOM_DELIMITER_PATTERN = "//(.)\\n(.*)";
-    List<Num> nums;
+    private final Pattern pattern = Pattern.compile(CUSTOM_DELIMITER_PATTERN);
+    private List<Number> numbers;
 
-    public Nums(String input) {
-        this.nums = new ArrayList<>();
+    public Numbers(String input) {
+        this.numbers = new ArrayList<>();
         setElements(input);
     }
 
     private void setElements(String input) {
 
-        Matcher m = Pattern.compile(CUSTOM_DELIMITER_PATTERN).matcher(input);
+        Matcher m = pattern.matcher(input);
 
         if (m.find()) {
             validateDelimiterPattern(m);
@@ -31,22 +32,21 @@ public class Nums {
         String[] nums = input.split(DELIMITER_PATTERN);
 
         for (int i = 0; i < nums.length; i++) {
-            this.nums.add(new Num(nums[i]));
+            this.numbers.add(new Number(nums[i]));
         }
 
     }
 
     private void validateDelimiterPattern(Matcher m) {
         if (m.group(1).length() != 1) {
-            System.out.println(ILLEGAL_DELIMITER_MESSAGE);
-            throw new RuntimeException();
+            throw new RuntimeException(ILLEGAL_DELIMITER_MESSAGE);
         }
     }
 
     public int addElements() {
         int sum = 0;
 
-        for (Num e : nums) {
+        for (Number e : numbers) {
             sum += e.getElement();
         }
 
