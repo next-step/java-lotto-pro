0571031dc19f26aa062042cbd99c3c8b4968cfb3
CHEESE <81912261+ychxexn@users.noreply.github.com>
2022-11-01 22:49:30 +0900

    refactor: LottoController 메소드 분리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,26 +1,48 @@
 package lotto.controller;
 
-import lotto.domain.LottoResult;
-import lotto.domain.Lottos;
-import lotto.domain.Money;
-import lotto.domain.WinningLotto;
+import lotto.domain.*;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
+import java.util.List;
+
 public class LottoController {
 
+    private Money money;
+    private Lottos lottos;
+    private WinningLotto winningLotto;
+
     public void run() {
-        Money money = new Money(InputView.insertMoney());
+        insertMoney();
+        buyLottos();
+        insertWinningLotto();
+        printResult();
+    }
+
+    private void insertMoney() throws IllegalArgumentException {
+        money = new Money(InputView.insertMoney());
+    }
+
+    private void buyLottos() throws IllegalArgumentException {
         int manualLottoCount = InputView.insertManualLottoCount();
         if(manualLottoCount > money.getBuyableLottoCount()) {
             throw new IllegalArgumentException("수동으로 구매할 로또 수가 구매할 수 있는 로또 수보다 많습니다.");
         }
-        Lottos lottos = new Lottos(InputView.insertManualLotto(manualLottoCount));
+
+        lottos = new Lottos(InputView.insertManualLotto(manualLottoCount));
         lottos.buyLottos(money.getBuyableLottoCount() - manualLottoCount);
+
         OutputView.printLottoCount(manualLottoCount, money.getBuyableLottoCount());
         OutputView.print(lottos.toString());
-        WinningLotto winningLotto = new WinningLotto(InputView.insertWinningLotto(), InputView.insertBonusBall());
+    }
+
+    private void insertWinningLotto() throws IllegalArgumentException {
+        winningLotto = new WinningLotto(InputView.insertWinningLotto(), InputView.insertBonusBall());
+    }
+
+    private void printResult() {
         LottoResult result = lottos.getResult(winningLotto);
+
         OutputView.printResult(result);
     }
 }
