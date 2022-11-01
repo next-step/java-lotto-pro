041e73c26a4d0cf10c67e20b9f6661613c136e04
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-11-01 11:09:46 +0900

    refactor(Numbers.java) : 숫자 관련 로직 분리

--- src/main/java/split/Numbers.java
+++ src/main/java/split/Numbers.java
@@ -1,2 +1,31 @@
-package split;public class Numbers {
+package split;
+
+public class Numbers {
+
+    private final String[] numbers;
+
+    public Numbers(String[] numbers) {
+        validateNegativeNumbers(numbers);
+        this.numbers = numbers;
+    }
+
+    private static void validateNegativeNumbers(String[] numbers) {
+        for (String number : numbers) {
+            validateNegativeNumber(number);
+        }
+    }
+
+    private static void validateNegativeNumber(String number) {
+        if (Integer.parseInt(number) < 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public int sum() {
+        int sum = 0;
+        for (String number : this.numbers) {
+            sum = sum + Integer.parseInt(number);
+        }
+        return sum;
+    }
 }
