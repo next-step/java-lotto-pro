b214803849d9256d3c7ddfe403eddcbdec94ab90
Bryan | JJ <with.beth@gmail.com>
2022-11-01 19:16:36 +0900

    refactor : rename Lotto to LottoTicket

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,9 +1,9 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -27,59 +27,49 @@ class LottoTest {
             .withMessageContaining(EXCEPTION_MESSAGE_PREFIX);
     }
 
-    @Test
-    void containsInvalidLottoNumbers() {
-        final List<Integer> numbers = Arrays.asList(1, 2, 46, 0, -1, 6);
-
-        for (int i = 0; i < numbers.size(); i++) {
-
-            Collections.shuffle(numbers);
-
-            assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> new Lotto(mapToLottoNumberList(numbers)))
-                .withMessageContaining(EXCEPTION_MESSAGE_PREFIX);
-        }
-    }
-
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3, 4, 5, 7, 8, 9, 10})
     void invalidLottoNumberSize(final int size) {
-        final List<Integer> numbers = RandomUtils.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER,
-            MAX_LOTTO_NUMBER, size);
+        final List<LottoNumber> numbers = pickUniqueLottoNumbers(size);
 
         assertThatExceptionOfType(IllegalArgumentException.class)
-            .isThrownBy(() -> new Lotto(mapToLottoNumberList(numbers)))
+            .isThrownBy(() -> new Lotto(numbers))
             .withMessageContaining(EXCEPTION_MESSAGE_PREFIX);
     }
 
     @Test
     void duplicatedLottoNumbers() {
-        final List<Integer> numbers = RandomUtils.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER,
-            MAX_LOTTO_NUMBER, LOTTO_SIZE);
+        final List<LottoNumber> numbers = pickUniqueLottoNumbers(LOTTO_SIZE);
+
         // make it duplicated
         numbers.set(0, numbers.get(1));
+
+        assertThat(numbers.get(0)).isEqualTo(numbers.get(1));
+
         // shuffle and test
         for (int i = 0; i < numbers.size(); i++) {
             Collections.shuffle(numbers);
 
             assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> new Lotto(mapToLottoNumberList(numbers)))
+                .isThrownBy(() -> new Lotto(numbers))
                 .withMessageContaining(EXCEPTION_MESSAGE_PREFIX);
         }
     }
 
     @Test
     void validLottoNumbers() {
-        final List<Integer> numbers = RandomUtils.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER,
-            MAX_LOTTO_NUMBER, LOTTO_SIZE);
+        final List<LottoNumber> numbers = pickUniqueLottoNumbers(LOTTO_SIZE);
 
         for (int i = 0; i < numbers.size(); i++) {
             Collections.shuffle(numbers);
 
-            assertThatCode(
-                () -> new Lotto(mapToLottoNumberList(numbers))).doesNotThrowAnyException();
+            assertThatCode(() -> new Lotto(numbers)).doesNotThrowAnyException();
         }
-
+    }
+   
+    private static List<LottoNumber> pickUniqueLottoNumbers(final int count) {
+        return mapToLottoNumberList(
+            RandomUtils.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, count));
     }
 
     private static List<LottoNumber> mapToLottoNumberList(final List<Integer> numbers) {
