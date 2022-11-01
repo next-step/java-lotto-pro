fbbeff8201102a9e9fad4e8402fe3c69de82c0e7
choiyongseok90 <badaelephant>
2022-11-01 19:59:02 +0900

    fix: Result의 경우 직접 비교대신 값으로 비교

--- src/main/java/step3/model/machine/Results.java
+++ src/main/java/step3/model/machine/Results.java
@@ -2,6 +2,7 @@ package step3.model.machine;
 
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Results {
@@ -9,7 +10,6 @@ public class Results {
     private final LinkedHashMap<Result, Integer> lottoResultMap;
 
     private static final int DEFAULT_RESULT_COUNT = 0;
-
     private static final int RESULT_INCREASE_STEP = 1;
 
     public Results() {
@@ -23,6 +23,10 @@ public class Results {
         lottoResultMap.put(result, lottoResultMap.get(result)+RESULT_INCREASE_STEP);
 
     }
+    public int getResultCount(Result result){
+        return lottoResultMap.get(result);
+    }
+
     private long getTotalPrize(Result result) {
         int winningCount = lottoResultMap.get(result);
         return result.getTotalPrize(winningCount);
