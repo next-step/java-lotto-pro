2db16c7f5e1ded2845767c2a96e717365ac698e6
dongha-byun <byunsw4@naver.com>
2022-11-03 03:03:54 +0900

    Feat: 당첨로또 구현 class 추가  - 기본 6개 번호와 보너스 로또 번호 구분을 위한 enum 포함

---
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -0,0 +1,40 @@
+package lotto.domain;
+
+import static lotto.domain.LottoWinningMoneyEnum.*;
+import static lotto.domain.WinningLottoType.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WinningLotto {
+
+    private Map<WinningLottoType, Object> numbers = new HashMap<>();
+    public static final int FIVE = 5;
+    private WinningLotto() {}
+
+    public static WinningLotto create(Lotto basicLotto, LottoNumber bonusNumber){
+        WinningLotto winningLotto = new WinningLotto();
+        winningLotto.numbers.put(BASIC, basicLotto);
+        winningLotto.numbers.put(BONUS, bonusNumber);
+
+        return winningLotto;
+    }
+
+    public int getMatchCountByBasic(Lotto lotto) {
+        return lotto.getContainNumberCount((Lotto) numbers.get(BASIC));
+    }
+
+    public boolean isContainBonusNumber(Lotto lotto) {
+        return lotto.isContain((LottoNumber) numbers.get(BONUS));
+    }
+
+    public LottoWinningMoneyEnum getLottoRank(Lotto lotto) {
+        int matchCountByBasic = getMatchCountByBasic(lotto);
+        boolean isBonusContain = false;
+        if(matchCountByBasic == FIVE){
+            isBonusContain = isContainBonusNumber(lotto);
+        }
+
+        return findEnumByContainCountAndBonusContain(matchCountByBasic, isBonusContain);
+    }
+}
---
+++ src/main/java/lotto/domain/WinningLottoType.java
@@ -0,0 +1,5 @@
+package lotto.domain;
+
+public enum WinningLottoType {
+    BASIC, BONUS
+}
