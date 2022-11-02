ba03bc31dd3fe7153e73c0a732e976ca12c0db04
deokmoon <deok2moon@gmail.com>
2022-11-03 01:49:25 +0900

    Feat: 매칭 결과 저장하는 일급콜렉션 클래스 구현

---
+++ src/main/java/lotto/auto/MatchNumberMap.java
@@ -0,0 +1,33 @@
+package lotto.auto;
+
+import java.util.Map;
+
+import static lotto.auto.common.Constants.INIT_NUM;
+
+// 일급콜렉션
+public class MatchNumberMap {
+    Map<Rank, Integer> matchNumberMap;
+
+    public MatchNumberMap(Map<Rank, Integer> matchNumberMap) {
+        this.matchNumberMap = matchNumberMap;
+    }
+
+    public Map<Rank, Integer> getMatchNumberMap() {
+        return matchNumberMap;
+    }
+
+    public int value(Rank rank) {
+        if (!matchNumberMap.containsKey(rank)) {
+            return INIT_NUM;
+        }
+        return matchNumberMap.get(rank);
+    }
+
+    public int profit() {
+        int profit = INIT_NUM;
+        for (Rank keyRank : matchNumberMap.keySet()) {
+            profit += keyRank.getWinningMoney() * matchNumberMap.get(keyRank);
+        }
+        return profit;
+    }
+}
