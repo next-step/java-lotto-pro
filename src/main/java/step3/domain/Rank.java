115b144bcdc40930b4728dbf95a3114e1287e925
iamsojung <sojungkid@gmail.com>
2022-11-02 20:59:55 +0900

    refactor: BiPredicate 사용

--- src/main/java/step3/domain/Rank.java
+++ src/main/java/step3/domain/Rank.java
@@ -3,29 +3,30 @@ package step3.domain;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.BiPredicate;
 
 public enum Rank {
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
-    THIRD(5, 1_500_000),
-    FOURTH(4, 50_000),
-    FIFTH(3, 5000),
-    NOTHING(0,0);
+    FIRST(6, 2_000_000_000, (count, bonus) -> count == 6),
+    SECOND(5, 30_000_000, (count, bonus) -> count == 5 && bonus),
+    THIRD(5, 1_500_000, (count, bonus) -> count == 5 && !bonus),
+    FOURTH(4, 50_000, (count, bonus) -> count == 4),
+    FIFTH(3, 5000, (count, bonus) -> count == 3),
+    NOTHING(0, 0, (count, bonus) -> count < 3);
 
     private final int count;
     private final int winnings;
+    private final BiPredicate<Integer, Boolean> isMatch;
 
-    Rank(int count, int winnings) {
+    Rank(int count, int winnings,
+        BiPredicate<Integer, Boolean> isMatch) {
         this.count = count;
         this.winnings = winnings;
+        this.isMatch = isMatch;
     }
 
-    public static Rank from(int count, boolean bonus) {
-        if (count == SECOND.count && bonus) {
-            return SECOND;
-        }
+    public static Rank valueOf(int count, boolean bonus) {
         Optional<Rank> rank = Arrays.stream(Rank.values())
-            .filter(r -> r.count == count)
+            .filter(r -> r.isMatch.test(count,bonus))
             .findAny();
         return rank.orElse(NOTHING);
     }
--- src/test/java/step3/RankTest.java
+++ src/test/java/step3/RankTest.java
@@ -1,18 +1,29 @@
 package step3;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import step3.domain.Rank;
 
 public class RankTest {
 
     @Test
-    @DisplayName("매치 카운트에 따른 랭크 값을 출력하는 테스트")
+    @DisplayName("매치 카운트에 따른 랭크 값을 출력하는 테스트1")
     void rankTest() {
-        assertThat(Rank.from(5, true)).isEqualTo(Rank.SECOND);
-        assertThat(Rank.from(3, false)).isEqualTo(Rank.FIFTH);
+        assertThat(Rank.valueOf(5, true)).isEqualTo(Rank.SECOND);
+        assertThat(Rank.valueOf(3, false)).isEqualTo(Rank.FIFTH);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"6:false:FIRST", "5:true:SECOND", "5:false:THIRD","4:false:FOURTH"
+        ,"3:false:FIFTH","3:true:FIFTH","2:false:NOTHING", "2:true:NOTHING"}, delimiter = ':')
+    @DisplayName("매치 카운트에 따른 랭크 값을 출력하는 테스트2")
+    void setTest3(int count, boolean bonus, Rank rank) {
+        assertThat(Rank.valueOf(count, bonus)).isEqualTo(rank);
     }
 
 }
