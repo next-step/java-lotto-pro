425fab851c2afbdc2cfadfc199b939cb9b5106e0
victor-jo <joasjn45@gmail.com>
2022-11-02 23:25:36 +0900

    feat(lottoBuyer): 로또 구매자 객체 추가
    - 로또 구매자는 수동구매, 자동구매를 선택하여 산다
    - 내가 산 모든 로또를 모아놓았다가 제출한다
    - 구매한 로또 중 수동이 몇 개인지 알려준다
    - 구매한 로또 중 자동이 몇 개인지 알려준다

---
+++ src/main/java/lotto/LottoBuyer.java
@@ -0,0 +1,51 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoBuyer {
+    private final PayAmount payAmount;
+    private final List<Lotto> lottoList = new ArrayList<>();
+    private int manualLottoCount = 0;
+    private int randomLottoCount = 0;
+
+    public LottoBuyer(PayAmount payAmount) {
+        this.payAmount = payAmount;
+    }
+
+    public void buyWithManual(LottoStore lottoStore, List<Integer> numbers) {
+        lottoStore.buyWith(() -> new ManualLottoNumberGenerateStrategy(numbers));
+        List<Lotto> newLottoOne = lottoStore.pay(this.payAmount, new PayCount(1));
+        this.manualLottoCount += newLottoOne.size();
+        lottoList.addAll(newLottoOne);
+    }
+
+    public void buyWithRandom(LottoStore lottoStore) {
+        lottoStore.buyWith(RandomLottoNumberGenerateStrategy::new);
+        List<Lotto> newLottoMany = lottoStore.payAll(this.payAmount);
+        this.randomLottoCount += newLottoMany.size();
+        lottoList.addAll(newLottoMany);
+    }
+
+    public int getManualLottoCount() {
+        return this.manualLottoCount;
+    }
+
+    public int getRandomLottoCount() {
+        return this.randomLottoCount;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        for (Lotto lotto : lottoList) {
+            builder.append(lotto.toString())
+                .append("\n");
+        }
+        return builder.toString();
+    }
+
+    public List<Lotto> reportLottoList() {
+        return this.lottoList;
+    }
+}
---
+++ src/test/java/lotto/LottoBuyerTest.java
@@ -0,0 +1,46 @@
+package lotto;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoBuyerTest {
+
+    @Test
+    void 자신의_돈보다_수동구매_개수가_더_많으면_최대로_산다() {
+        LottoBuyer lottoBuyer = new LottoBuyer(new PayAmount(3000));
+        LottoStore lottoStore = new LottoStore();
+
+        lottoBuyer.buyWithManual(lottoStore, Arrays.asList(8, 21, 23, 41, 42, 43));
+        lottoBuyer.buyWithManual(lottoStore, Arrays.asList(3, 5, 11, 16, 32, 38));
+        lottoBuyer.buyWithManual(lottoStore, Arrays.asList(7, 11, 16, 35, 36, 44));
+        lottoBuyer.buyWithManual(lottoStore, Arrays.asList(1, 2, 3, 4, 5, 6));
+        lottoBuyer.buyWithManual(lottoStore, Arrays.asList(1, 2, 3, 4, 5, 6));
+        lottoBuyer.buyWithManual(lottoStore, Arrays.asList(1, 2, 3, 4, 5, 6));
+        lottoBuyer.buyWithManual(lottoStore, Arrays.asList(1, 2, 3, 4, 5, 6));
+
+        assertThat(lottoBuyer.reportLottoList()).isEqualTo(Arrays.asList(
+            Lotto.valueOf(new ManualLottoNumberGenerateStrategy(Arrays.asList(8, 21, 23, 41, 42, 43))),
+            Lotto.valueOf(new ManualLottoNumberGenerateStrategy(Arrays.asList(3, 5, 11, 16, 32, 38))),
+            Lotto.valueOf(new ManualLottoNumberGenerateStrategy(Arrays.asList(7, 11, 16, 35, 36, 44)))
+        ));
+        assertThat(lottoBuyer.getManualLottoCount()).isEqualTo(3);
+        assertThat(lottoBuyer.getRandomLottoCount()).isEqualTo(0);
+    }
+
+    @Test
+    void 수동구매_이후에_나머지_돈으로_자동구매를_한다() {
+        LottoBuyer lottoBuyer = new LottoBuyer(new PayAmount(14000));
+        LottoStore lottoStore = new LottoStore();
+
+        lottoBuyer.buyWithManual(lottoStore, Arrays.asList(8, 21, 23, 41, 42, 43));
+        lottoBuyer.buyWithManual(lottoStore, Arrays.asList(3, 5, 11, 16, 32, 38));
+        lottoBuyer.buyWithManual(lottoStore, Arrays.asList(7, 11, 16, 35, 36, 44));
+        lottoBuyer.buyWithRandom(lottoStore);
+
+        assertThat(lottoBuyer.getManualLottoCount()).isEqualTo(3);
+        assertThat(lottoBuyer.getRandomLottoCount()).isEqualTo(11);
+    }
+}
