d1b762bf65185c96af60585c03518aa1168b4d1f
deokmoon <deok2moon@gmail.com>
2022-11-03 01:51:36 +0900

    Refactor: 책임 분리 및 Refactoring

--- src/main/java/lotto/auto/ui/ResultView.java
+++ src/main/java/lotto/auto/ui/ResultView.java
@@ -1,40 +1,16 @@
 package lotto.auto.ui;
 
+import lotto.auto.Lotto;
 import lotto.auto.Rank;
-import lotto.auto.common.LottoAutoUtils;
 
 import java.text.NumberFormat;
-import java.util.Map;
-
-import static lotto.auto.common.Constants.LOTTO_LENGTH;
-import static lotto.auto.common.Constants.NUMBER_OF_MINIMUM_WINNING;
+import java.util.List;
 
 public class ResultView {
+    private StringBuilder sb;
 
     public ResultView() {
-    }
-
-    public void printResult(String inputMoney, Map<Integer, Integer> matchWinningNumbers) {
-        float profit = 0;
-        StringBuilder sb = new StringBuilder();
-        sb.append("당첨 통계\n" + "---------\n");
-
-        for (int i = NUMBER_OF_MINIMUM_WINNING; i <= LOTTO_LENGTH; i++) {
-            int cnt = getMatchCount(matchWinningNumbers, i);
-            sb.append(i + "개 일치(" + Rank.getRank(i) + ")-" + cnt + "개\n");
-            profit = profit + Rank.getMoney(i, cnt);
-        }
-        if (profit > 0) {
-            profit = profit / new LottoAutoUtils().stringToNumber(inputMoney);
-        }
-        System.out.println(sb + "총 수익률은 " + cutDecimal(2, profit) + "입니다.");
-    }
-
-    private int getMatchCount(Map<Integer, Integer> matchWinningNumbers, int numberOfMatched) {
-        if (matchWinningNumbers.containsKey(numberOfMatched)) {
-            return matchWinningNumbers.get(numberOfMatched);
-        }
-        return 0;
+        sb = new StringBuilder();
     }
 
     public String cutDecimal(int cutSize, double value) {
@@ -43,5 +19,25 @@ public class ResultView {
         nf.setGroupingUsed(false);
 
         return nf.format(value);
+    }
+
+    public void printBuyLottoCountMessage(int buyLotto) {
+        System.out.println(buyLotto + "개를 구매하였습니다.");
+    }
+
+    public void printBuyLotto(List<Lotto> lottoList) {
+        for (Lotto eachLotto : lottoList) {
+            System.out.println(eachLotto.toString());
+        }
+    }
+
+    public void printWinningStatisticsMessage(Rank rank, int value) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("당첨 통계\n" + "---------\n");
+        sb.append(rank.getCountOfMatch() + "개 일치(" + rank.getWinningMoney() + ")-" + value + "개\n");
+    }
+
+    public void printProfitMessage(int stringToNumber, int profit) {
+        System.out.println(sb + "총 수익률은 " + cutDecimal(2, profit) + "입니다.");
     }
 }
