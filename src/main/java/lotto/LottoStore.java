8b4c485df9490f6c87a8599df35cdf466318858d
victor-jo <joasjn45@gmail.com>
2022-11-02 22:32:36 +0900

    refactor(lottoStore): 로또 상점에 구매 전략을 말하는 메시지 추가
    - 로또 상점에 자동으로 산다고 말하면 자동으로 삼
    - 로또 상점에 수동으로 산다고 말하면 수동으로 삼

--- src/main/java/lotto/LottoStore.java
+++ src/main/java/lotto/LottoStore.java
@@ -2,21 +2,27 @@ package lotto;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Supplier;
 
 public class LottoStore {
 
     private static final int LOTTO_AMOUNT = 1000;
+    private Supplier<LottoNumberGenerateStrategy> strategySupplier = RandomLottoNumberGenerateStrategy::new;
 
     public List<Lotto> pay(PayAmount payAmount) {
         List<Lotto> sellLottoList = new ArrayList<>();
         while (payAmount.payable(LOTTO_AMOUNT)) {
             payAmount.pay(LOTTO_AMOUNT);
-            sellLottoList.add(makeLotto(new RandomLottoNumberGenerateStrategy()));
+            sellLottoList.add(makeLotto(strategySupplier.get()));
         }
         return sellLottoList;
     }
 
     private Lotto makeLotto(LottoNumberGenerateStrategy lottoNumberGenerateStrategy) {
         return Lotto.valueOf(lottoNumberGenerateStrategy);
+    }
+
+    public void buyWith(Supplier<LottoNumberGenerateStrategy> strategySupplier) {
+        this.strategySupplier = strategySupplier;
     }
 }
--- src/test/java/lotto/LottoStoreTest.java
+++ src/test/java/lotto/LottoStoreTest.java
@@ -1,8 +1,10 @@
 package lotto;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,5 +19,31 @@ public class LottoStoreTest {
         List<Lotto> lottoList = lottoStore.pay(new PayAmount(payAmount));
 
         assertThat(lottoList).hasSize(expectedLottoCount);
+    }
+
+    @Test
+    void 수동으로_사겠다고_말하고_돈을_지불하면_수동으로_로또를_산다() {
+        LottoStore lottoStore = new LottoStore();
+
+        lottoStore.buyWith(() -> new ManualLottoNumberGenerateStrategy(
+            Arrays.asList(1, 2, 3, 4, 5, 6)
+        ));
+
+        List<Lotto> lottoList = lottoStore.pay(new PayAmount(1000));
+
+        assertThat(lottoList).hasSize(1);
+        assertThat(lottoList.get(0))
+            .isEqualTo(Lotto.valueOf(new ManualLottoNumberGenerateStrategy(Arrays.asList(1, 2, 3, 4, 5, 6))));
+    }
+
+    @Test
+    void 자동으로_사겠다고_말하고_돈을_지불하면_자동으로_로또를_산다() {
+        LottoStore lottoStore = new LottoStore();
+
+        lottoStore.buyWith(RandomLottoNumberGenerateStrategy::new);
+
+        List<Lotto> lottoList = lottoStore.pay(new PayAmount(1000));
+
+        assertThat(lottoList).hasSize(1);
     }
 }
