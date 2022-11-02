427a5717c79047fab0360b69b582a52abe6a91b6
lunatic <lsklsk45@naver.com>
2022-11-02 17:31:15 +0900

    refactor (LottoNumberGenerator) : 수동 로또 번호를 LottoNumber로 변환하여 반환하는 기능 제공

--- src/test/java/lotto/generate/LottoNumberGeneratorTest.java
+++ src/test/java/lotto/generate/LottoNumberGeneratorTest.java
@@ -17,7 +17,15 @@ public class LottoNumberGeneratorTest {
     @Test
     @DisplayName("로또 번호 6개를 자동으로 생성하며 반환 값은 List<LottoNumber> 형태이다.")
     void lotto_number_generator_return_Lotto_number_list() {
-        assertThat(LottoNumberGenerator.generate().size()).isEqualTo(6);
+        assertThat(LottoNumberGenerator.generate()).hasSize(6);
         assertThat(LottoNumberGenerator.generate()).first().isInstanceOf(LottoNumber.class);
+    }
+
+    @Test
+    @DisplayName("로또 번호 6개를 받은 문자열로 생성 한다")
+    void lotto_number_manual_generate() {
+        String manualTestCaseOne = "1,2,3,4,5,6";
+        assertThat(LottoNumberGenerator.generateManulLotto(manualTestCaseOne)).hasSize(6);
+        assertThat(LottoNumberGenerator.generateManulLotto(manualTestCaseOne)).first().isInstanceOf(LottoNumber.class);
     }
 }
