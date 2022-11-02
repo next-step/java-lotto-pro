a7d5e7c9ce0f330489b924a743087cd7430f4a69
deokmoon <deok2moon@gmail.com>
2022-11-03 02:52:48 +0900

    Refactor: 소수점 고통 상수화 적용

--- src/main/java/lotto/auto/common/Constants.java
+++ src/main/java/lotto/auto/common/Constants.java
@@ -4,10 +4,9 @@ public class Constants {
     public static final String DEFAULT_PRICE = "1000";
     public static final String DELIMITER = ",";
     public static final int LOTTO_LENGTH = 6;
-    public static final int NUMBER_OF_MINIMUM_WINNING = 3;
-    public static final int SECOND_COUNT_OF_MATCH = 5;
     public static final int BEGIN_INDEX = 0;
     public static final int LOTTO_MIN_NUM = 1;
     public static final int LOTTO_MAX_NUM = 45;
     public static final int INIT_NUM = 0;
+    public static final int DECIMAL_POINT = 2;
 }
