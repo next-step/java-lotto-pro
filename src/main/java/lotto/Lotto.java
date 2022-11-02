bd6d54768a407a87f5bb2ed146ae465117f93ebc
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-11-03 07:28:07 +0900

    test(LottoTest.java) : 45 초과의 수를 가질 수 없다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,6 +3,7 @@ package lotto;
 public class Lotto {
 
     public static final int MINIMUM_NUMBER = 1;
+    public static final int MAXIMUM_NUMBER = 45;
 
     private Lotto(){}
 
@@ -12,11 +13,18 @@ public class Lotto {
 
     private static void validate(int number) {
         validateMinimumNumber(number);
+        validateMaximumNumber(number);
     }
 
     private static void validateMinimumNumber(int number) {
         if (number < MINIMUM_NUMBER) {
             throw new IllegalArgumentException(MINIMUM_NUMBER + "보다 작을 수 없습니다.");
+        }
+    }
+
+    private static void validateMaximumNumber(int number) {
+        if (number > MAXIMUM_NUMBER) {
+            throw new IllegalArgumentException(MAXIMUM_NUMBER + "보다 클 수 없습니다.");
         }
     }
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static lotto.Lotto.MAXIMUM_NUMBER;
 import static lotto.Lotto.MINIMUM_NUMBER;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -17,5 +18,14 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(number))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(MINIMUM_NUMBER + "보다 작을 수 없습니다.");
+    }
+
+    @DisplayName("45 초과의 수를 가질 수 없다.")
+    @ParameterizedTest
+    @ValueSource(ints = {46})
+    void maximumNumber(int number) {
+        assertThatThrownBy(() -> new Lotto(number))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(MAXIMUM_NUMBER + "보다 클 수 없습니다.");
     }
 }
