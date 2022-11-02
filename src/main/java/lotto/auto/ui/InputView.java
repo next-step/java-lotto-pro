a61ac2e3508af61607caa795209a08843cc5b2b0
deokmoon <deok2moon@gmail.com>
2022-11-03 01:51:36 +0900

    Refactor: 책임 분리 및 Refactoring

--- src/main/java/lotto/auto/ui/InputView.java
+++ src/main/java/lotto/auto/ui/InputView.java
@@ -1,8 +1,5 @@
 package lotto.auto.ui;
 
-import lotto.auto.Lotto;
-
-import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
@@ -14,19 +11,15 @@ public class InputView {
         return consoleInput();
     }
 
-    public String getInputWinningNubers() {
+    public String getInputWinningNumbers() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         return consoleInput();
     }
+
 
     public String consoleInput() {
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
 
-    public void printLotto(List<Lotto> lottoList) {
-        for (Lotto eachLotto : lottoList) {
-            System.out.println(eachLotto.toString());
-        }
-    }
 }
