4e9a2fc77e546472ba1912faaaa85e4bd0335c5e
deokmoon <deok2moon@gmail.com>
2022-11-03 02:55:09 +0900

    Refactor: 이넘명 수정

--- src/main/java/lotto/auto/Rank.java
+++ src/main/java/lotto/auto/Rank.java
@@ -1,8 +1,8 @@
 package lotto.auto;
 
 public enum Rank {
-    FOUR(3, 5_000),
-    THREE(4, 50_000),
+    FOURTH(3, 5_000),
+    THIRD(4, 50_000),
     SECOND(5, 1_500_000),
     FIRST(6, 2_000_000_000)
     ;
