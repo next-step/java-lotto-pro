0ca264e7fb47ee1be180f0b8584da2be7f3ca9cd
msares <msares123@gamil.com>
2022-11-02 22:25:48 +0900

    feat(LottoTicket,LottoTicketTest): 로또 티켓 String으로 생성 추가

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -2,30 +2,42 @@ package lotto.domain;
 
 import lotto.common.ErrorMessage;
 
-import java.math.BigDecimal;
-import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.Set;
-import java.util.function.DoublePredicate;
-import java.util.function.IntPredicate;
-import java.util.function.LongPredicate;
-import java.util.stream.DoubleStream;
-import java.util.stream.IntStream;
-import java.util.stream.LongStream;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class LottoTicket {
     private static final int LOTTO_NUMBER_SIZE = 6;
+    private static final Pattern DEFAULT_DELIMITER_PATTERN = Pattern.compile(",");
+    private static final String EMPTY_STRING = "";
+    private static final Pattern SPACE_DELIMITER_PATTERN = Pattern.compile("\\s");
     private final Set<LottoNumber> ticket;
 
     public LottoTicket(Set<LottoNumber> lottoTicket) {
-        this.validateDuplication(lottoTicket);
         this.ticket = lottoTicket;
+        this.validateDuplication(ticket);
+    }
+
+    public LottoTicket(String strNumber) {
+        String trimStrNumber = SPACE_DELIMITER_PATTERN.matcher(strNumber).replaceAll(EMPTY_STRING);
+        this.ticket = Arrays.stream(DEFAULT_DELIMITER_PATTERN.split(trimStrNumber))
+                .map((number) -> new LottoNumber(Integer.parseInt(number)))
+                .collect(Collectors.toSet());
+        this.validateDuplication(ticket);
     }
 
     private void validateDuplication(Set<LottoNumber> lottoTicket) {
-        if (lottoTicket.size() == LOTTO_NUMBER_SIZE) {
+        if (lottoTicket.size() != LOTTO_NUMBER_SIZE) {
             throw new IllegalArgumentException(ErrorMessage.ERROR_LOTTO_NUMBER_DUPLICATION);
         }
+    }
+
+    public int compareTicket(LottoTicket winningTicket) {
+        return (int) this.ticket.stream()
+                .filter(winningTicket.ticket::contains)
+                .count();
     }
 
     @Override
--- src/test/java/lotto/domain/LottoTicketTest.java
+++ src/test/java/lotto/domain/LottoTicketTest.java
@@ -31,4 +31,32 @@ public class LottoTicketTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.ERROR_LOTTO_NUMBER_DUPLICATION);
     }
+
+    @Test
+    @DisplayName("문자열_로또_티켓_생성")
+    void 문자열_로또_티켓_생성() {
+        String lottoNumbers = "1,2, 3,4,5,  6";
+        assertThat(new LottoTicket(lottoNumbers)).isEqualTo(new LottoTicket(lottoNumbers));
+    }
+
+    @Test
+    @DisplayName("문자열_로또_티켓_중복_채크")
+    void 문자열_로또_티켓_중복_채크() {
+        String lottoNumbers = "1,2,3,4,5,5";
+        assertThatThrownBy(() -> new LottoTicket(lottoNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.ERROR_LOTTO_NUMBER_DUPLICATION);
+    }
+
+    @Test
+    @DisplayName("로또_티켓_비교")
+    void 로또_티켓_비교() {
+        String lottoNumbers = "1,2,3,4,6,7";
+        Set<LottoNumber> lottoTicketSet = Stream.of(1, 2, 3, 4, 5, 6)
+                .map(LottoNumber::new)
+                .collect(Collectors.toSet());
+        LottoTicket winningTicket = new LottoTicket(lottoNumbers);
+        LottoTicket purchaseLottoTicket = new LottoTicket(lottoTicketSet);
+        assertThat(purchaseLottoTicket.compareTicket(winningTicket)).isEqualTo(5);
+    }
 }
