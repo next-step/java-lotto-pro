4852e27673cdc601749c97245edd610afbbcbee3
msares <msares123@gamil.com>
2022-11-03 00:59:32 +0900

    refactor(LottoStore,LottoStoreTest): 디자인 패턴 GenerateStrategy으로 수정

--- src/main/java/lotto/domain/LottoStore.java
+++ src/main/java/lotto/domain/LottoStore.java
@@ -1,11 +1,27 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class LottoStore {
-    public static LottoTicket buy(GenerateStrategy GenerateStrategy) {
-        Set<Integer> generatedNumber = GenerateStrategy.generate();
+    private final GenerateStrategy generateStrategy;
+
+    public LottoStore(final GenerateStrategy generateStrategy) {
+        this.generateStrategy = generateStrategy;
+    }
+
+    public LottoTickets buy(int purchaseAmount) {
+        List<LottoTicket> lottoList = new ArrayList<>();
+        for (int i = 0; i < purchaseAmount; i++) {
+            lottoList.add(ticketPrinting(generateStrategy));
+        }
+        return new LottoTickets(lottoList);
+    }
+
+    public static LottoTicket ticketPrinting(GenerateStrategy generateStrategy) {
+        Set<Integer> generatedNumber = generateStrategy.generate();
         return new LottoTicket(generatedNumber.stream()
                 .map(LottoNumber::new)
                 .collect(Collectors.toSet()));
--- src/test/java/lotto/domain/LottoStoreTest.java
+++ src/test/java/lotto/domain/LottoStoreTest.java
@@ -15,7 +15,7 @@ public class LottoStoreTest {
     @Test
     @DisplayName("로또_티켓_구매")
     void 로또_티켓_구매() {
-        LottoTicket buyTicket = LottoStore.buy(() -> new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)));
+        LottoTicket buyTicket = LottoStore.ticketPrinting(() -> new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)));
         Set<LottoNumber> lottoTicket = Stream.of(1, 2, 3, 4, 5, 6)
                 .map(LottoNumber::new)
                 .collect(Collectors.toSet());
