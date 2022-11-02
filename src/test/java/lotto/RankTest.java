e7279ae0b9814ece1fd88797350bdd6aaaaba1dd
seogineer <dgseo8981@gmail.com>
2022-11-02 11:18:12 +0900

    test: 테스트 코드 추가

--- src/test/java/lotto/RankTest.java
+++ src/test/java/lotto/RankTest.java
@@ -11,6 +11,11 @@ public class RankTest {
     }
 
     @Test
+    void Rank에게_6개_맞추고_보너스볼도_맞춘_경우_등수_물어보기() {
+        assertThat(Rank.valueOf(6, true)).isEqualTo(Rank.FIRST);
+    }
+
+    @Test
     void Rank에게_5개_맞추고_보너스볼을_못_맞춘_경우_등수_물어보기() {
         assertThat(Rank.valueOf(5, false)).isEqualTo(Rank.THIRD);
     }
@@ -26,7 +31,27 @@ public class RankTest {
     }
 
     @Test
+    void Rank에게_4개_맞추고_보너스볼은_맞은_경우_등수_물어보기() {
+        assertThat(Rank.valueOf(4, true)).isEqualTo(Rank.FOURTH);
+    }
+
+    @Test
     void Rank에게_3개_맞춘_경우_등수_물어보기() {
         assertThat(Rank.valueOf(3, false)).isEqualTo(Rank.FIFTH);
+    }
+
+    @Test
+    void Rank에게_3개_맞추고_보너스볼은_맞은_경우_등수_물어보기() {
+        assertThat(Rank.valueOf(3, true)).isEqualTo(Rank.FIFTH);
+    }
+
+    @Test
+    void Rank에게_0개_맞춘_경우_등수_물어보기() {
+        assertThat(Rank.valueOf(0, false)).isEqualTo(Rank.MISS);
+    }
+
+    @Test
+    void Rank에게_0개_맞추고_보너스볼은_맞은_경우_등수_물어보기() {
+        assertThat(Rank.valueOf(0, true)).isEqualTo(Rank.MISS);
     }
 }
