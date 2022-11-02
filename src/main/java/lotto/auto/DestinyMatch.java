cbfc013804d64c074b75526b3d03de6b373e22ae
deokmoon <deok2moon@gmail.com>
2022-11-03 02:31:25 +0900

    Fix: 출력 메세지 조정을 위한 버그 수정  - 상수 순서 수정  - 메세지 버그 수정

--- src/main/java/lotto/auto/DestinyMatch.java
+++ src/main/java/lotto/auto/DestinyMatch.java
@@ -1,13 +1,19 @@
 package lotto.auto;
 
   import lotto.auto.ui.InputView;
+  import lotto.auto.ui.ResultView;
 
 public class DestinyMatch {
     public void start() {
         InputView inputView = new InputView();
+        ResultView resultView = new ResultView();
         String inputMoney = inputView.getInputString();
+        Buyer buyer = new Buyer(inputMoney);
 
-        WinningMatcher winningMatcher = new WinningMatcher(new Buyer(inputMoney), new LottoNumbers(inputView.getInputWinningNumbers()));
+        resultView.printBuyLottoCountMessage(buyer.buyLotto());
+        resultView.printBuyLotto(buyer.getLottos());
+
+        WinningMatcher winningMatcher = new WinningMatcher(buyer, new LottoNumbers(inputView.getInputWinningNumbers()));
         winningMatcher.printWinningStatistics();
         winningMatcher.printProfit(inputMoney);
     }
--- src/main/java/lotto/auto/Rank.java
+++ src/main/java/lotto/auto/Rank.java
@@ -1,14 +1,12 @@
 package lotto.auto;
 
-import java.util.Arrays;
-
 public enum Rank {
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
-    THIRD(5, 1_500_000),
-    FOURTH(4, 50_000),
-    FIFTH(3, 5_000),
-    MISS(0, 0);
+    FOUR(3, 5_000),
+    THREE(4, 50_000),
+    SECOND(5, 1_500_000),
+    FIRST(6, 2_000_000_000)
+    ;
+
 
     private static final int SECOND_COUNT_OF_MATCH = 5;
     private final int countOfMatch;
@@ -26,37 +24,21 @@ public enum Rank {
     public int getWinningMoney() {
         return winningMoney;
     }
-    public static int getRank(int numberOfMatch) {
-        return Arrays.stream(values())
-                .filter(Rank -> Rank.countOfMatch == numberOfMatch)
-                .findFirst()
-                .get()
-                .winningMoney;
-    }
 
-    public static int getMoney(int countOfMatch, int cnt) {
-        if (cnt > 0) {
-            return (Arrays.stream(values())
-                    .filter(Rank -> Rank.countOfMatch == countOfMatch)
-                    .findFirst()
-                    .get()
-                    .winningMoney) * cnt;
-        }
-        return 0;
-    }
     public static Rank valueOf(int countOfMatch) {
         Rank[] ranks = values();
-        Rank rank = MISS;
+        Rank rank = null;
         for (Rank tempRank : ranks) {
-            rank = findCountOfMatch(tempRank, countOfMatch);
+            rank = findCountOfMatch(tempRank, countOfMatch) == true ? tempRank : rank;
         }
+
         return rank;
     }
 
-    private static Rank findCountOfMatch(Rank rank, int countOfMatch) {
+    private static boolean findCountOfMatch(Rank rank, int countOfMatch) {
         if (rank.countOfMatch == countOfMatch) {
-            return rank;
+            return true;
         }
-        return MISS;
+        return false;
     }
 }
