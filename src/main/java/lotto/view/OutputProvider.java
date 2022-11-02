cadc0a236a40c78227a7718aea83801f0401d490
msares <msares123@gamil.com>
2022-11-02 22:48:37 +0900

    feat(InputProvider,OutputProvider): input, output view 구현

---
+++ src/main/java/lotto/view/OutputProvider.java
@@ -0,0 +1,32 @@
+package lotto.view;
+
+import lotto.domain.LottoTicket;
+import lotto.domain.LottoTickets;
+import lotto.domain.Rank;
+import lotto.domain.Rewards;
+
+public class OutputProvider {
+    private static final String PURCHASE_LOTTO_COUNT = "%d개를 구매하셨습니다.%n";
+    private static final String WINNING_STATS = "당첨 통계";
+    private static final String BOUNDARY = "---------";
+    private static final String FIRST_MATCH_COUNT = "6개 일치 (2000000000원)- %d개";
+    private static final String THIRD_MATCH_COUNT = "5개 일치 (1500000원)- %d개";
+    private static final String FOURTH_MATCH_COUNT = "4개 일치 (50000원)- %d개";
+    private static final String FIFTH_MATCH_COUNT = "3개 일치 (5000원)- %d개";
+    private static final String TOTAL_RATE_OF_REWARD = "총 수익률은 %.2f입니다.";
+
+    public static void printLottoTicket(LottoTickets lottoTickets) {
+        System.out.printf(PURCHASE_LOTTO_COUNT, lottoTickets.getTicketCount());
+        System.out.println(lottoTickets);
+    }
+
+    public static void printRewards(Rewards rewards) {
+        System.out.println(WINNING_STATS);
+        System.out.println(BOUNDARY);
+        System.out.println(FIFTH_MATCH_COUNT + rewards.count(Rank.FIFTH));
+        System.out.println(FOURTH_MATCH_COUNT + rewards.count(Rank.FOURTH));
+        System.out.println(THIRD_MATCH_COUNT + rewards.count(Rank.THIRD));
+        System.out.println(FIRST_MATCH_COUNT + rewards.count(Rank.FIRST));
+        System.out.println(TOTAL_RATE_OF_REWARD + rewards.calculateRateReward());
+    }
+}
