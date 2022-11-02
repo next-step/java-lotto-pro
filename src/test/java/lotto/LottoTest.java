b9bda60998dc99d0c6527db7ccb79ecdfd627f36
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-11-03 07:33:06 +0900

    test(LottoTest.java) : 로또 번호를 생성한다.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import static lotto.Lotto.MAXIMUM_NUMBER;
 import static lotto.Lotto.MINIMUM_NUMBER;
+import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("로또")
@@ -27,5 +28,12 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(number))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(MAXIMUM_NUMBER + "보다 클 수 없습니다.");
+    }
+
+    @DisplayName("로또 번호를 생성한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 45})
+    void constructor(int number) {
+        assertThatNoException().isThrownBy(() -> new Lotto(number));
     }
 }
