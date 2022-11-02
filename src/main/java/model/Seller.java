1c04021a8ea3937e1c68c8e819741bef9252dbb3
sawook <lna1003@naver.com>
2022-11-02 21:52:25 +0900

    feat: 수동으로 로또를 구매가능하도록 기능 추가
    - seller 클래스에서 수동으로 로또를 판매하도록 기능추가
    - 테스트코드 구현

--- src/main/java/model/Seller.java
+++ src/main/java/model/Seller.java
@@ -13,11 +13,19 @@ public class Seller {
         this.lotto = new ArrayList<>();
     }
 
-    public List<LottoNumber> buy(int count, NumberStrategy strategy) {
+    public List<LottoNumber> buyAuto(int count, NumberStrategy strategy) {
         for (int i = 0; i < count; i++) {
             lotto.add(new LottoNumber(strategy.shuffle()));
         }
 
+        return lotto;
+    }
+
+    public void buyManual(List<LottoNumber> manualList) {
+        lotto.addAll(manualList);
+    }
+
+    public List<LottoNumber> getLotto() {
         return lotto;
     }
 }
--- src/test/java/model/SellerTest.java
+++ src/test/java/model/SellerTest.java
@@ -3,7 +3,9 @@ package model;
 import model.strategy.MockStrategy;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,11 +13,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 class SellerTest {
 
     @Test
-    void 입력한_숫자만큼_로또를_구매한다() {
+    void 입력한_숫자만큼_자동으로_로또를_구매한다() {
         int count = 10;
+        Seller seller = new Seller();
+        List<LottoNumber> lottoNumbers = seller.buyAuto(count, new MockStrategy(Arrays.asList(1, 2, 3, 4, 5, 6)));
+        assertThat(lottoNumbers).hasSize(count);
+    }
 
-        List<LottoNumber> result = new Seller().buy(count, new MockStrategy(Arrays.asList(1, 2, 3, 4, 5, 6)));
+    @Test
+    void 수동으로_로또를_구매한다() {
+        Seller seller = new Seller();
+        List<LottoNumber> number = new ArrayList<>();
+        number.add(new LottoNumber(Arrays.asList(1, 2, 3, 4, 5)));
 
-        assertThat(result).hasSize(count);
+        seller.buyManual(number);
+
+        assertThat(number).hasSize(1);
     }
 }
