b5a7e87c9250fde42cb46391b521a255f710f392
sixthou <justyuk@naver.com>
2022-11-02 20:24:14 +0900

    refactor : LottoGame 메서드 분리 및 테스트 수정

--- src/main/java/lotto/domain/LottoGame.java
+++ src/main/java/lotto/domain/LottoGame.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import java.util.List;
+import java.util.stream.IntStream;
 import lotto.ui.Input;
 import lotto.ui.Output;
 import lotto.ui.View;
@@ -7,19 +9,46 @@ import lotto.ui.View;
 public class LottoGame {
 
     private final View view;
-    private final Lottos lottos;
 
     public LottoGame(Input input, Output output) {
         this.view = new View(input, output);
-        lottos = new Lottos(new RandomNumberPickStrategy());
+
     }
 
     public void run() {
-        Money money = Money.from(view.insertMoney());
-        view.printLottoSize(money.availableLottoSize());
-        lottos.buyRandomNumberLottos(money.availableLottoSize());
+        Lottos lottos = lottoBuyingProcess();
+        lottoResultProcess(lottos);
+    }
+
+    private Lottos lottoBuyingProcess() {
+        Lottos lottos = new Lottos(new RandomNumberPickStrategy());
+
+        Money totalMoney = Money.from(view.insertMoney());
+        int manualLottoCount = view.insertManualLottoCount();
+        Money manualMoney = Money.fromCount(totalMoney.checkPossibleLottoCount(manualLottoCount));
+        Money autoMoney = totalMoney.minus(manualMoney);
+
+        buyManualLottos(lottos, manualLottoCount);
+        lottos.buyRandomNumberLottos(autoMoney.availableLottoSize());
+
+        view.printLottoSize(manualMoney.availableLottoSize(), autoMoney.availableLottoSize());
+
+        return lottos;
+    }
+
+    private void lottoResultProcess(Lottos lottos) {
         view.printText(lottos.toString());
-        view.printResultStat(lottos.checkResultStat(
-                new WinningLotto(view.insertWinningNumber(), view.insertBonusNumber())));
+
+        List<Integer> winningNumbers = view.insertWinningNumber();
+        int bonusNumber = view.insertBonusNumber();
+
+        view.printResultStat(lottos.checkResultStat(new WinningLotto(winningNumbers, bonusNumber)));
+    }
+
+    private void buyManualLottos(Lottos lottos, int manualLottoCount) {
+        view.printInsertManualLotto();
+        IntStream.iterate(0, i -> i + 1)
+                .limit(manualLottoCount)
+                .forEach(i -> lottos.buyManualLotto(view::insertManualLotto));
     }
 }
