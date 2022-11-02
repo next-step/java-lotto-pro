fd3958b00daf1a0f27a9385b7cbd00aea67e8461
victor-jo <joasjn45@gmail.com>
2022-11-02 23:26:24 +0900

    refactor(lottoApp): 로또 구매자 객체 적용
    - 로또 수동구매 기능 추가

--- src/main/java/lotto/LottoApp.java
+++ src/main/java/lotto/LottoApp.java
@@ -3,7 +3,6 @@ package lotto;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.List;
 
 import static lotto.LottoPrinter.newLine;
 import static lotto.LottoPrinter.print;
@@ -12,9 +11,11 @@ import static lotto.LottoScanner.*;
 public class LottoApp implements App {
 
     private static final String WELCOME = "구입금액을 입력해 주세요.";
-    private static final String BUY_LOTTO = "%d개를 구매했습니다.";
+    private static final String BUY_LOTTO = "수동으로 %d장, 자동으로 %d개를 구매했습니다.";
     private static final String WINNING_NUMBER = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER = "보너스 볼을 입력해 주세요.";
+    private static final String BUY_MANUAL_COUNT = "수동으로 구매할 로또 수를 입력해 주세요.";
+    private static final String BUY_MANUAL_NUMBER = "수동으로 구매할 번호를 입력해 주세요.";
 
     public void run() throws IOException {
         LottoStore lottoStore = new LottoStore();
@@ -22,15 +23,27 @@ public class LottoApp implements App {
         print(WELCOME);
         int payAmount = scanOneNumber();
 
-        List<Lotto> lottoList = lottoStore.payAll(new PayAmount(payAmount));
-        print(String.format(BUY_LOTTO, lottoList.size()));
+        newLine();
 
-        for (Lotto lotto : lottoList) {
-            print(lotto.toString());
-        }
+        LottoBuyer lottoBuyer = new LottoBuyer(new PayAmount(payAmount));
+
+        print(BUY_MANUAL_COUNT);
+        int buyManualCount = scanOneNumber();
 
         newLine();
-        
+
+        print(BUY_MANUAL_NUMBER);
+        for (int i = 0; i < buyManualCount; i++) {
+            lottoBuyer.buyWithManual(lottoStore, Arrays.asList(scanManyNumbers()));
+        }
+
+        lottoBuyer.buyWithRandom(lottoStore);
+
+        newLine();
+
+        print(String.format(BUY_LOTTO, lottoBuyer.getManualLottoCount(), lottoBuyer.getRandomLottoCount()));
+        print(lottoBuyer.toString());
+
         print(WINNING_NUMBER);
         Lotto winLotto = Lotto.valueOf(new ManualLottoNumberGenerateStrategy(Arrays.asList(scanManyNumbers())));
 
@@ -40,7 +53,7 @@ public class LottoApp implements App {
         newLine();
 
         App lottoStaticApp = LottoStaticApp.builder()
-            .lottoList(lottoList)
+            .lottoList(lottoBuyer.reportLottoList())
             .winLotto(winLotto)
             .bonusLottoNumber(bonusLottoNumber)
             .payAmount(new PayAmount(payAmount))
