db30ba238822152df207ef6cb92aa7652f73645c
deokmoon <deok2moon@gmail.com>
2022-11-02 23:37:33 +0900

    Test: 로또, 일급콜렉션 LotttoNumbers 테스트 코드 수정  - 리뷰 피드백 반영으로, 클랙스 역할 책임에 따른 테스트 코드 수정  - LotttoNumbers 일급콜렉션으로 Validation 책임 추가

--- src/test/java/lotto/auto/LottoTest.java
+++ src/test/java/lotto/auto/LottoTest.java
@@ -2,13 +2,12 @@ package lotto.auto;
 
 import org.junit.jupiter.api.Test;
 
-import static lotto.auto.common.Constants.DEFAULT_PRICE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoTest {
     @Test
-    public void 로또_가격_저장_확인(){
-        Lotto lotto = new Lotto(DEFAULT_PRICE);
-        assertThat(lotto.getPrice()).isEqualTo(1000);
+    public void 숫자_6개_생성() {
+        Lotto lotto = new Lotto();
+        assertThat(lotto.getLottoNumbers()).hasSize(6);
     }
 }
