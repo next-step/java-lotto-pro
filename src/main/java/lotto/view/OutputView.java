72ffe7b18ee8f8bd683e08eaec963a069362e56c
CHEESE <81912261+ychxexn@users.noreply.github.com>
2022-11-02 20:43:55 +0900

    fix: 수익률 1 미만인 경우에만 손해 메시지 출력되도록 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -10,7 +10,9 @@ public class OutputView {
     private static final String MSG_CORRECT_COUNT = "%d개 일치";
     private static final String MSG_BONUS = ", 보너스 볼 일치";
     private static final String MSG_PRIZE = " (%d원)- %d개";
-    private static final String MSG_RETURN_RATE = "총 수익률은 %.2f입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)";
+    private static final String MSG_RETURN_RATE = "총 수익률은 %.2f입니다.";
+
+    private static final String MSG_LOSS = "(기준이 1이기 때문에 결과적으로 손해라는 의미임)";
 
     private OutputView() {
 
@@ -27,10 +29,16 @@ public class OutputView {
     public static void printResult(LottoResult result) {
         print("당첨 통계");
         print("---------");
+
         Arrays.stream(Rank.values())
                 .filter(rank -> rank != Rank.NONE)
                 .forEach(rank -> print(generateResultMessage(rank, result.getRankCount(rank))));
-        print(String.format(MSG_RETURN_RATE, result.getReturnRate()));
+
+        String msg = MSG_RETURN_RATE;
+        if (result.getReturnRate() < 1) {
+            msg += MSG_LOSS;
+        }
+        print(String.format(msg, result.getReturnRate()));
     }
 
     private static String generateResultMessage(Rank rank, int count) {
