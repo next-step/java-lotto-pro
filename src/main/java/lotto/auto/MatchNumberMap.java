b45eafa555f6f106a1724e2e13dfdb4c35cc9f43
deokmoon <deok2moon@gmail.com>
2022-11-03 03:03:57 +0900

    Refactor: 로또 꽝인 경우 예외처리 로직 적용

--- src/main/java/lotto/auto/MatchNumberMap.java
+++ src/main/java/lotto/auto/MatchNumberMap.java
@@ -3,6 +3,7 @@ package lotto.auto;
 import java.util.Map;
 
 import static lotto.auto.common.Constants.INIT_NUM;
+import static lotto.auto.common.Constants.ZERO;
 
 // 일급콜렉션
 public class MatchNumberMap {
@@ -10,10 +11,6 @@ public class MatchNumberMap {
 
     public MatchNumberMap(Map<Rank, Integer> matchNumberMap) {
         this.matchNumberMap = matchNumberMap;
-    }
-
-    public Map<Rank, Integer> getMatchNumberMap() {
-        return matchNumberMap;
     }
 
     public int value(Rank rank) {
@@ -24,6 +21,9 @@ public class MatchNumberMap {
     }
 
     public float profit() {
+        if (matchNumberMap.containsKey(null)) {
+            return ZERO;
+        }
         float profit = INIT_NUM;
         for (Rank keyRank : matchNumberMap.keySet()) {
             profit += keyRank.getWinningMoney() * matchNumberMap.get(keyRank);
