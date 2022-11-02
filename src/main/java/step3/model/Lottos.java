88eb596f5498a30218a69edf3fb3de0fd26bd64a
jylim <jylim@12cm.co.kr>
2022-11-02 13:27:51 +0900

    fix(Lotts): 코드 버그 수정

--- src/main/java/step3/model/Lottos.java
+++ src/main/java/step3/model/Lottos.java
@@ -1,27 +1,16 @@
-package step3.view;
+package step3.model;
 
-import step3.model.Lotto;
-import step3.model.LottoCalculator;
-import step3.model.LottoGenerator;
+import java.util.List;
 
-import java.util.Scanner;
+public class Lottos {
+    // 일급 컬렉션은 다른 맴버 변수가 없음
+    public List<Lotto> lottos;
 
-import static step3.constant.Message.System.*;
-public class InputView {
-    private final static Scanner scanner = new Scanner(System.in);
-    public static void inputPurchasePrice(LottoGenerator lottoGenerator) {
-        System.out.println(TOTAL_LOTTO_PRICE_INPUT_MESSAGE);
-        lottoGenerator.setPurchasePrice(scanner.nextLine());
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
     }
 
-    public static void inputLastWeekLottoNumbers(LottoCalculator calculator) {
-        System.out.println(LAST_LOTTO_NUMBERS_INPUT_MESSAGE);
-        String[] lastWeekNumbers = new Lotto().validateLastWeekWinner(scanner.nextLine());
-        calculator.setLastWeekWinner(inputLastWeekBonusNumber(lastWeekNumbers));
-    }
-
-    public static Lotto inputLastWeekBonusNumber(String[] lastWeekLottos) {
-        System.out.println(LAST_BONUS_NUMBER_INPUT_MESSAGE);
-        return new Lotto(lastWeekLottos, scanner.nextLine());
+    public List<Lotto> getLottoList() {
+        return lottos;
     }
 }
