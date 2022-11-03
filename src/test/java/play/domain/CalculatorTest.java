57ba2296c1705c5e7555f8ca8347e798113a72a0
soojungkk <sj921101@naver.com>
2022-11-04 00:47:21 +0900

    test : 로또 생성 검증 테스트 코드

--- src/test/java/play/domain/CalculatorTest.java
+++ src/test/java/play/domain/CalculatorTest.java
@@ -1,2 +1,17 @@
-package play.domain;public class CalculatorTest {
+package play.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    @Test
+    @DisplayName("금액을 입력받았을 떄 맞는 개수의 로또리스트를 반환하는지 테스트")
+    void calculate() {
+        String input = "14000";
+        Calculator counter = new Calculator(input);
+        assertThat(counter.getLottos().getLottoList()).hasSize(14);
+    }
 }
