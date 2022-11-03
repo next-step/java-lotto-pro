adf02f4c02d87ca389ff4ede830140bb9d4aa6b9
mansik <90mansin@gmail.com>
2022-11-04 00:34:08 +0900

    test: 수익률 검증을 위한 테스트 구현

---
+++ src/test/java/study/step3/WinReportTest.java
@@ -0,0 +1,25 @@
+package study.step3;
+
+import domain.WinReport;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WinReportTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"14:3:false:0.35", "1500:5:false:1", "30000:5:true:1"}, delimiter = ':')
+    @DisplayName("수익률 게산")
+    public void 총_수익률_계산(int lottoTicketCount, int collectNumber, boolean matchBonus, double expectValue){
+
+        WinReport winReport = new WinReport();
+        winReport.putLottoResult(collectNumber);
+
+        double result =  winReport.calculateProfit(lottoTicketCount,matchBonus);
+
+        assertThat(result).isEqualTo(expectValue);
+    }
+}
