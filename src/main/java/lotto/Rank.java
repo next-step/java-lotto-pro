26b42ee240e37689791c483f93adfb63502ab33f
seogineer <dgseo8981@gmail.com>
2022-11-03 18:42:58 +0900

    refactor: merge comment에 있던 내용 반영

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -22,8 +22,7 @@ public enum Rank {
 
     public static Rank valueOf(int countOfMatch, boolean matchBonus) {
         return Arrays.stream(values())
-                .filter(rank -> rank.countOfMatch == countOfMatch)
-                .filter(rank -> !rank.equals(SECOND) || rank.matchBonus == matchBonus)
+                .filter(rank -> rank.countOfMatch == countOfMatch && rank.matchBonus == matchBonus)
                 .findFirst()
                 .orElse(MISS);
     }
