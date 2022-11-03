b41c1db4447f0218ee2dac8a90ef2fb947d4e88b
Minjae Kim <minjae.kim@mytrum.com>
2022-11-03 17:48:32 +0900

    refactor: 정적 클래스 생성자 생성

--- src/main/java/lotto/ScoreResultConverter.java
+++ src/main/java/lotto/ScoreResultConverter.java
@@ -2,6 +2,10 @@ package lotto;
 
 public class ScoreResultConverter {
 
+    private ScoreResultConverter() {
+        throw new IllegalStateException("유틸 클래스 입니다");
+    }
+
     public static Score toResult(Score sourceScore) {
         if (sourceScore.equals(WinningResult.WIN_SECOND_BONUS.getMatchScore())) {
             return sourceScore;
