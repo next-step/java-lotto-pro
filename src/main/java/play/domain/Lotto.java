e5b240e97526b5d94d4cbb7c1810dd9591f4d376
soojungkk <sj921101@naver.com>
2022-11-04 00:46:07 +0900

    feat : Lotto 클래스 구현

--- src/main/java/play/domain/Lotto.java
+++ src/main/java/play/domain/Lotto.java
@@ -1,2 +1,40 @@
-package play.domain;public class Lotto {
+package play.domain;
+
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    public static Lotto createAutoLotto() {
+        return new Lotto(LottoGenerator.generateLottoNumbers());
+    }
+
+    public static Lotto MakeLotto(List<Integer> numbers) {
+        return new Lotto(numbers);
+    }
+
+    public List<Integer> getNumbers() {
+        return this.numbers;
+    }
+
+    public int getMatchingCount(Lotto targetLotto) {
+        int count = 0;
+
+        List<Integer> targetLottoNumber = targetLotto.getNumbers();
+
+        for (int number : targetLottoNumber) {
+            count = this.countContainNumber(number, count);
+        }
+
+        return count;
+    }
+
+    private int countContainNumber(int number, int count) {
+        return this.numbers.contains(number) ? count + 1 : count;
+    }
+
 }
