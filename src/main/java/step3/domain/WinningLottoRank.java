d8b4bfb188f81767c86269d5ce6dd2f78a46951a
yomni <yearsheep@gmail.com>
2022-11-04 16:15:09 +0900

    style: 코드 포맷팅 변경

--- src/main/java/step3/domain/WinningLottoRank.java
+++ src/main/java/step3/domain/WinningLottoRank.java
@@ -3,11 +3,11 @@ package step3.domain;
 import java.util.Arrays;
 
 public enum WinningLottoRank {
-    FIRST(6, 2_000_000_000)
-    , SECOND(5, 1_500_000)
-    , THIRD(4, 50_000)
-    , FOURTH(3, 5_000)
-    , MISS(0, 0);
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 1_500_000),
+    THIRD(4, 50_000),
+    FOURTH(3, 5_000),
+    MISS(0, 0);
 
     private final int matchCount;
     private final int reward;
