b4caa4daed007326950dc4d8037f5652330ab12f
jisu1211 <dbdnzl1211@gmail.com>
2022-11-04 22:57:15 +0900

    refactor: view와 controller, domain 분리

--- src/main/java/study/lotto/view/LottoInput.java
+++ src/main/java/study/lotto/view/LottoInput.java
@@ -1,9 +1,9 @@
 package study.lotto.view;
 
-import study.lotto.domain.WinningLotto;
-import study.lotto.domain.order.UserOrder;
 import study.util.NumberUtil;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
@@ -11,25 +11,23 @@ public class LottoInput {
 
     private static final Scanner scanner = new Scanner(System.in);
 
-    public UserOrder inputUserPurchase() {
-        return new UserOrder(scanner.nextLine());
+    public String inputAmount() {
+        return scanner.nextLine();
     }
 
-    public int inputManualQuantity(UserOrder userOrder) {
-        return userOrder.checkManualQuantity(NumberUtil.convertToPositiveInt(scanner.nextLine()));
+    public int inputManualQuantity() {
+        return NumberUtil.convertToPositiveInt(scanner.nextLine());
     }
 
-    public void inputManualLottos(int quantity, UserOrder userOrder) {
-        IntStream.range(NumberUtil.INIT_ZERO, quantity).forEach((i) -> {
-            userOrder.createManualOrder(scanner.nextLine());
-        });
+    public String inputManualLotto() {
+        return scanner.nextLine();
     }
 
-    public WinningLotto inputWinningNumbers() {
-        return new WinningLotto(scanner.nextLine());
+    public String inputWinningNumbers() {
+        return scanner.nextLine();
     }
 
-    public void inputBonusBall(WinningLotto winningLotto) {
-        winningLotto.addBonusBall(scanner.nextInt());
+    public int inputBonusBall() {
+        return NumberUtil.convertToPositiveInt(scanner.nextLine());
     }
 }
