d009447f0a8645ddfe0d671afb7f3be69d96948b
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-11-04 19:43:58 +0900

    test(TotalWinningMoneyTest.java) : 주석 제거

--- src/test/java/lotto/winning/domain/TotalWinningMoneyTest.java
+++ src/test/java/lotto/winning/domain/TotalWinningMoneyTest.java
@@ -8,15 +8,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("당첨 금액 총합")
 class TotalWinningMoneyTest {
-    /*
-        0개 일치 1개
-        2개 일치 1개
-        3개 일치 1개         5_000
-        4개 일치 1개        50_000
-        5개 일치 1개     1_500_000
-        6개 일치 1개 2_000_000_000
-        총합        2_001_555_000
-        */
+
     @DisplayName("당첨 금액 총합 구하기")
     @Test
     void sum() {
