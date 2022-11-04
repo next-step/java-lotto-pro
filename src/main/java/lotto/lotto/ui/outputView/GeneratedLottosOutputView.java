08fba6cb29a8f08f27bb1482136a12c672dbd1b9
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-11-04 18:24:16 +0900

    refactor(README.md) : 인덴트

--- src/main/java/lotto/lotto/ui/outputView/GeneratedLottosOutputView.java
+++ src/main/java/lotto/lotto/ui/outputView/GeneratedLottosOutputView.java
@@ -11,11 +11,15 @@ public class GeneratedLottosOutputView {
         System.out.println("당첨 통계");
         System.out.println("-----------");
         for (Lotto lotto : lottos) {
-            for (Number number : lotto.getLottos()) {
-                System.out.print(number.getNumber() + ",");
-            }
+            printLotto(lotto);
             System.out.println();
         }
 
+    }
+
+    private static void printLotto(Lotto lotto) {
+        for (Number number : lotto.getLottos()) {
+            System.out.print(number.getNumber() + ",");
+        }
     }
 }
