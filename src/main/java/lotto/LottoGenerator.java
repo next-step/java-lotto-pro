b72c367066c7d43f3fc3a11e1f19cad07e125383
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-11-04 15:04:29 +0900

    test(LottoGeneratorTest.java) : 로또 번호들 리스트를 생성한다.

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -9,19 +9,29 @@ public class LottoGenerator {
     public static final int MINIMUM_LOTTO_NUMBER = 1;
     public static final int MAX_LOTTO_NUMBER = 45;
     private final List<Integer> range = new ArrayList<>();
+    private int purchaseCount;
 
-    public LottoGenerator() {
-        init();
+    public LottoGenerator(int purchaseCount) {
+        init(purchaseCount);
     }
 
-    private void init() {
+    private void init(int purchaseCount) {
         for (int i = MINIMUM_LOTTO_NUMBER; i <= MAX_LOTTO_NUMBER; i++) {
             this.range.add(i);
         }
-        Collections.shuffle(this.range);
+        this.purchaseCount = purchaseCount;
     }
 
-    public List<Integer> geneate() {
-        return this.range.subList(0, 6);
+    public List<Lottos> generateLottoses() {
+        List<Lottos> lottosList = new ArrayList<>();
+        for (int i = 0; i < purchaseCount; i++) {
+            lottosList.add(generateLottos());
+        }
+        return lottosList;
+    }
+
+    private Lottos generateLottos() {
+        Collections.shuffle(this.range);
+        return new Lottos(this.range.subList(0, 6));
     }
 }
--- src/test/java/lotto/LottoGeneratorTest.java
+++ src/test/java/lotto/LottoGeneratorTest.java
@@ -1,20 +1,19 @@
 package lotto;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("로또 번호 생성기")
 class LottoGeneratorTest {
 
-    @DisplayName("로또 번호 생성")
-    @Test
-    void generate() {
-        LottoGenerator lottoGenerator = new LottoGenerator();
-        List<Integer> lottoNumbers = lottoGenerator.geneate();
-        assertThat(lottoNumbers).hasSize(6);
+    @DisplayName("구매횟수만큼 로또 번호들 리스트 생성")
+    @ParameterizedTest
+    @ValueSource(ints = {3, 4, 5})
+    void generate(int purchaseCount) {
+        LottoGenerator lottoGenerator = new LottoGenerator(purchaseCount);
+        assertThat(lottoGenerator.generateLottoses()).hasSize(purchaseCount);
     }
 }
