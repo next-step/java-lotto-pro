701c22acbd289608124ab7e8f9ba01c09c8f6479
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-11-04 22:26:44 +0900

    test(ReturnRateTest.java) : 수익률 계산기 생성.

--- src/test/java/lotto/winning/domain/ReturnRateTest.java
+++ src/test/java/lotto/winning/domain/ReturnRateTest.java
@@ -1,12 +1,14 @@
 package lotto.winning.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static lotto.lotto.domain.fixture.LottoMoneyFixture.로또구매금액_10_000;
 import static lotto.winning.fixture.TotalWinningMoneyFixture.당첨금액_5000;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
 
 @DisplayName("수익률")
 class ReturnRateTest {
@@ -16,5 +18,11 @@ class ReturnRateTest {
     @ValueSource(doubles = {0.5})
     void name(double expected) {
         assertThat(new ReturnRate(로또구매금액_10_000(), 당첨금액_5000()).calculate()).isEqualTo(expected);
+    }
+
+    @DisplayName("수익률 생성")
+    @Test
+    void constructor() {
+        assertThatNoException().isThrownBy(() -> new ReturnRate(로또구매금액_10_000(), 당첨금액_5000()));
     }
 }
