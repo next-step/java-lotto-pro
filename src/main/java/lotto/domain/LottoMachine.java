462eee7fae8f56910186f11e75cfcf3adeba2432
gunkim <gunkim.dev@gmail.com>
2022-11-05 21:14:25 +0900

    feat: 수동 로또 입력 구현 완료

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -2,11 +2,12 @@ package lotto.domain;
 
 import static java.util.stream.Collectors.toList;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 
 public class LottoMachine {
+
+    private static final int MINIMUM_MONEY = 0;
 
     private final int price;
     private final CreateLottoNumberPolicy createLottoNumberPolicy;
@@ -16,19 +17,20 @@ public class LottoMachine {
         this.createLottoNumberPolicy = createLottoNumberPolicy;
     }
 
-    public Lottos issue(int money) {
-        if (money < price) {
-            return new Lottos(new ArrayList<>());
+    public Lottos issue(int money, Lottos manualLottos) {
+        int leftMoney = money - (price * manualLottos.size());
+        if (leftMoney < MINIMUM_MONEY) {
+            throw new IllegalArgumentException("금액이 부족합니다.");
         }
 
-        int count = money / price;
-        return new Lottos(createLottos(count));
+        int count = leftMoney / price;
+        return new Lottos(createLottos(count)).combine(manualLottos);
     }
 
     private List<Lotto> createLottos(int count) {
         return IntStream.range(0, count)
             .mapToObj(__ -> createLottoNumberPolicy.create())
-            .map(Lotto::new)
+            .map(Lotto::auto)
             .collect(toList());
     }
 }
