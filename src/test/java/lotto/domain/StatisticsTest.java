1249fdc1c3ef918f1d970f2ed6a7702467ef6b1d
cylee9409 <67910616+cylee9409@users.noreply.github.com>
2022-11-05 18:27:14 +0900

    [fix] 4단계 - 로또(2등) 리뷰 반영 - 테스트 코드 given/when/then 패턴 적용
    * assertAll() 로 테스트 항목 묶기
    * given/when/then 패턴 적용

--- src/test/java/lotto/domain/StatisticsTest.java
+++ src/test/java/lotto/domain/StatisticsTest.java
@@ -7,13 +7,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class StatisticsTest {
 
     @Test
     @DisplayName("Winning에 따른 구매 로또 개수 테스트")
     void valid_lotto_count_by_winning_test() {
-
+        //given
         List<Lotto> lottoList = new ArrayList<>();
         lottoList.add(new Lotto("1,2,3,4,5,6"));
         lottoList.add(new Lotto("1,2,3,4,5,7"));
@@ -21,43 +22,65 @@ public class StatisticsTest {
         lottoList.add(new Lotto("1,2,3,4,8,9"));
         lottoList.add(new Lotto("1,2,3,8,9,10"));
         lottoList.add(new Lotto("1,2,8,9,10,11"));
-
         Lottos lottos = new Lottos(lottoList);
 
-        WinLotto winLotto = new WinLotto("1,2,3,4,5,6", "7");
-        Statistics statistics = new Statistics(lottos, winLotto);
+        Lotto winLotto = new Lotto("1,2,3,4,5,6");
+        LottoNumber bonusBall = new LottoNumber(winLotto, "7");
 
-        assertThat(statistics.getLottoCntByWinning(Winning.FIRST)).isEqualTo(1);
-        assertThat(statistics.getLottoCntByWinning(Winning.SECOND)).isEqualTo(1);
-        assertThat(statistics.getLottoCntByWinning(Winning.THIRD)).isEqualTo(1);
-        assertThat(statistics.getLottoCntByWinning(Winning.FOURTH)).isEqualTo(1);
-        assertThat(statistics.getLottoCntByWinning(Winning.FIFTH)).isEqualTo(1);
-        assertThat(statistics.getLottoCntByWinning(Winning.MISS)).isEqualTo(1);
+        //when
+        Statistics statistics = new Statistics(lottos, winLotto, bonusBall);
+
+        //then
+        assertAll(
+
+                () -> assertThat(statistics.getLottoCntByWinning(Winning.FIRST)).isEqualTo(1),
+                () -> assertThat(statistics.getLottoCntByWinning(Winning.SECOND)).isEqualTo(1),
+                () -> assertThat(statistics.getLottoCntByWinning(Winning.THIRD)).isEqualTo(1),
+                () -> assertThat(statistics.getLottoCntByWinning(Winning.FOURTH)).isEqualTo(1),
+                () -> assertThat(statistics.getLottoCntByWinning(Winning.FIFTH)).isEqualTo(1),
+                () -> assertThat(statistics.getLottoCntByWinning(Winning.MISS)).isEqualTo(1)
+
+        );
 
     }
 
     @Test
     @DisplayName("보너스 볼 매치 여부에 따른 등수 테스트")
     void winning_info_by_matchBonus_test() {
+        //given
+        Lotto winLotto = new Lotto("1,2,3,4,5,6");
+        LottoNumber bonusBall = new LottoNumber(winLotto, "7");
+
         List<Lotto> lottoList = new ArrayList<>();
         lottoList.add(new Lotto("1,2,3,4,5,7"));
         Lottos lottos = new Lottos(lottoList);
-        WinLotto winLotto = new WinLotto("1,2,3,4,5,6", "7");
-        assertThat(new Statistics(lottos, winLotto).getRewardsByWinning()).isEqualTo(Winning.SECOND.getReward());
+
+        //when
+        Statistics statistics = new Statistics(lottos, winLotto, bonusBall);
+        double rewards = statistics.getRewardsByWinning();
+
+        //then
+        assertThat(rewards).isEqualTo(Winning.SECOND.getReward());
     }
 
     @Test
     @DisplayName("수익률 정상 산출 테스트")
     void valid_yield_test() {
+        //given
+        Lotto winLotto = new Lotto("1,2,3,4,5,6");
+        LottoNumber bonusBall = new LottoNumber(winLotto, "7");
 
         List<Lotto> lottoList = new ArrayList<>();
         lottoList.add(new Lotto("1,2,3,11,12,13"));
         lottoList.add(new Lotto("11,12,13,14,15,16"));
         lottoList.add(new Lotto("12,13,14,15,16,17"));
         Lottos lottos = new Lottos(lottoList);
-        WinLotto winLotto = new WinLotto("1,2,3,4,5,6", "7");
 
-        Statistics statistics = new Statistics(lottos, winLotto);
-        assertThat(statistics.getYield(new Payment("14000"))).isEqualTo(0.35);
+        //when
+        Statistics statistics = new Statistics(lottos, winLotto, bonusBall);
+        double yield = statistics.getYield(new Payment("14000"));
+
+        //then
+        assertThat(yield).isEqualTo(0.35);
     }
 }
