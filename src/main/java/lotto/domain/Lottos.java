94c8d7b6798404fdc8ee478a59aec5bc613447b7
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-11-05 18:25:36 +0900

    refactor(Lottos.java) : 메서드 분리

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -22,33 +22,40 @@ public class Lottos {
 
     public Map<Integer, Lottos> matchLottos(WinningNumber winningNumber) {
         Map<Integer, Lottos> map = new HashMap<>();
-        Lottos three = new Lottos();
-        Lottos four = new Lottos();
-        Lottos five = new Lottos();
-        Lottos six = new Lottos();
-        for (Lotto lotto : this.lottos) {
-            if (matchCount(winningNumber, lotto) == 3) {
-                three.add(lotto);
-            } else if (matchCount(winningNumber, lotto) == 4) {
-                four.add(lotto);
-            } else if (matchCount(winningNumber, lotto) == 5) {
-                five.add(lotto);
-            } else if (matchCount(winningNumber, lotto) == 6) {
-                six.add(lotto);
-            }
-        }
-        map.put(3, three);
-        map.put(4, four);
-        map.put(5, five);
-        map.put(6, six);
+        map.put(3, findLottosByMatchCount(3, winningNumber));
+        map.put(4, findLottosByMatchCount(4, winningNumber));
+        map.put(5, findLottosByMatchCount(5, winningNumber));
+        map.put(6, findLottosByMatchCount(6, winningNumber));
         return map;
     }
 
-    private int matchCount(WinningNumber winningNumber, Lotto lotto) {
-        return (int) lotto.getNumbers()
-                .stream()
-                .filter(winningNumber::contains)
-                .count();
+    private Lottos findLottosByMatchCount(int matchCount, WinningNumber winningNumber) {
+        Lottos lottos = new Lottos();
+        for (Lotto lotto : this.lottos) {
+            addLotto(matchCount, winningNumber, lottos, lotto);
+        }
+        return lottos;
+    }
+
+    private void addLotto(int matchCount, WinningNumber winningNumber, Lottos lottos, Lotto lotto) {
+        if (isMatch(matchCount, winningNumber, lotto)) {
+            lottos.add(lotto);
+        }
+    }
+
+    private boolean isMatch(int matchCount, WinningNumber winningNumber, Lotto lotto) {
+        int count = 0;
+        for (Number number : lotto.getNumbers()) {
+            count = countUp(winningNumber, count, number);
+        }
+        return count == matchCount;
+    }
+
+    private static int countUp(WinningNumber winningNumber, int count, Number number) {
+        if (winningNumber.contains(number)) {
+            count++;
+        }
+        return count;
     }
 
     public int sum() {
