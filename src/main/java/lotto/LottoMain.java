57119b79d7ef41b4562e1adb2f04241ce7fe8356
dongha-byun <byunsw4@naver.com>
2022-11-06 19:50:04 +0900

    Refactor: 미사용 import 제거

--- src/main/java/lotto/LottoMain.java
+++ src/main/java/lotto/LottoMain.java
@@ -4,16 +4,11 @@ import static lotto.domain.LottoPayment.*;
 import static lotto.view.InputView.*;
 import static lotto.view.OutputView.*;
 
-import java.util.ArrayList;
-import java.util.List;
 import lotto.domain.*;
-import lotto.io.InputUtils;
 import lotto.service.LottoService;
 import lotto.strategy.AutoLottoNumberStrategy;
 import lotto.strategy.ConsoleLottoNumberStrategy;
 import lotto.strategy.LottoNumberStrategy;
-import lotto.view.InputView;
-import lotto.view.OutputView;
 
 public class LottoMain {
     public static void main(String[] args) {
--- src/main/java/lotto/domain/BuyingLottoGroup.java
+++ src/main/java/lotto/domain/BuyingLottoGroup.java
@@ -2,7 +2,6 @@ package lotto.domain;
 
 import java.util.ArrayList;
 import java.util.List;
-import javax.lang.model.type.ArrayType;
 import lotto.strategy.LottoNumberStrategy;
 
 public class BuyingLottoGroup {
