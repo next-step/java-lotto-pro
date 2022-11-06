55cc57c4792d085b5d35ccf19217e20a7d37fc2e
Joonseo Lee <43516757+joonseolee@users.noreply.github.com>
2022-11-06 12:52:55 +0900

    Revert "Feature/step3"

--- src/main/java/step3/Step3Application.java
+++
@@ -1,8 +0,0 @@
-package step3;
-
-public class Step3Application {
-    public static void main(String[] args) {
-        LottoController lottoController = new LottoController();
-        lottoController.gameStart();
-    }
-}
--- src/main/java/step3/constant/StringConstant.java
+++
@@ -1,8 +0,0 @@
-package step3.constant;
-
-public final class StringConstant {
-
-    public static final String COMMA = ",";
-
-    private StringConstant() {}
-}
