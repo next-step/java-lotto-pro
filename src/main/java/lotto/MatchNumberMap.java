179755a5b3e3d84d6461a1ac09fd3a43bfbcadbe
deokmoon <deok2moon@gmail.com>
2022-11-07 00:35:19 +0900

    Refactor: MatchNumberMap 메서드 추가 적용  - WinningMatcher 클래스와 책임분리

--- src/main/java/lotto/MatchNumberMap.java
+++ src/main/java/lotto/MatchNumberMap.java
@@ -2,14 +2,17 @@ package lotto;
 
 import lotto.common.Constants;
 
+import java.util.HashMap;
 import java.util.Map;
+
+import static lotto.common.Constants.ZERO;
 
 // 일급콜렉션
 public class MatchNumberMap {
     Map<Rank, Integer> matchNumberMap;
 
-    public MatchNumberMap(Map<Rank, Integer> matchNumberMap) {
-        this.matchNumberMap = matchNumberMap;
+    public MatchNumberMap() {
+        this.matchNumberMap = new HashMap<>();
     }
 
     public int value(Rank rank) {
@@ -21,12 +24,23 @@ public class MatchNumberMap {
 
     public float profit() {
         if (matchNumberMap.containsKey(null)) {
-            return Constants.ZERO;
+            return ZERO;
         }
         float profit = Constants.INIT_NUM;
         for (Rank keyRank : matchNumberMap.keySet()) {
             profit += keyRank.getWinningMoney() * matchNumberMap.get(keyRank);
         }
         return profit;
+    }
+
+    public void put(Rank rank, int cnt) {
+        this.matchNumberMap.put(rank, cnt);
+    }
+
+    public int get(Rank rank) {
+        if (this.matchNumberMap.containsKey(rank)) {
+            return this.matchNumberMap.get(rank);
+        }
+        return ZERO;
     }
 }
