572d6852c3447633c6b93c710832c6e7ebb07ff4
msares <msares123@gamil.com>
2022-11-06 23:14:00 +0900

    feat(Rank,RankTest): Rank 2등 추가

--- src/test/java/lotto/domain/RankTest.java
+++ src/test/java/lotto/domain/RankTest.java
@@ -9,9 +9,13 @@ public class RankTest {
     @Test
     @DisplayName("매칭숫자_등수_확인")
     void 매칭숫자_등수_확인() {
-        assertThat(Rank.valueOf(6)).isEqualTo(Rank.FIRST);
-        assertThat(Rank.valueOf(5)).isEqualTo(Rank.THIRD);
-        assertThat(Rank.valueOf(4)).isEqualTo(Rank.FOURTH);
-        assertThat(Rank.valueOf(3)).isEqualTo(Rank.FIFTH);
+        assertThat(Rank.valueOf(6, true)).isEqualTo(Rank.FIRST);
+        assertThat(Rank.valueOf(6, false)).isEqualTo(Rank.FIRST);
+        assertThat(Rank.valueOf(5, true)).isEqualTo(Rank.SECOND);
+        assertThat(Rank.valueOf(5, false)).isEqualTo(Rank.THIRD);
+        assertThat(Rank.valueOf(4, true)).isEqualTo(Rank.FOURTH);
+        assertThat(Rank.valueOf(4, false)).isEqualTo(Rank.FOURTH);
+        assertThat(Rank.valueOf(3, true)).isEqualTo(Rank.FIFTH);
+        assertThat(Rank.valueOf(3, false)).isEqualTo(Rank.FIFTH);
     }
 }
