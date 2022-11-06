af6c999bc0c6dbfd421fc1a7abf027802a8a4f38
deokmoon <deok2moon@gmail.com>
2022-11-07 00:32:17 +0900

    Feat: LottoNumber 정적메서드로 적용

--- src/main/java/lotto/LottoNumber.java
+++ src/main/java/lotto/LottoNumber.java
@@ -1,16 +1,31 @@
 package lotto;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static lotto.common.Constants.LOTTO_MAX_NUM;
 import static lotto.common.Constants.LOTTO_MIN_NUM;
 
 public class LottoNumber {
-    int lottoNum;
+    private static Map<Integer, LottoNumber> lottoNumbers = new HashMap<>();
+    private final int lottoNum;
 
-    public LottoNumber(int num) {
-        if (num < LOTTO_MIN_NUM || num > LOTTO_MAX_NUM) {
+    static {
+        for (int i = LOTTO_MIN_NUM; i <= LOTTO_MAX_NUM; i++) {
+            lottoNumbers.put(i, new LottoNumber(i));
+        }
+    }
+
+    private LottoNumber(int num) {
+        this.lottoNum = num;
+    }
+
+    public static LottoNumber of(int num) {
+        LottoNumber lottoNumber = lottoNumbers.get(num);
+        if (lottoNumber == null) {
             throw new IllegalArgumentException("로또의 숫자 범위는 1~45입니다.");
         }
-        this.lottoNum = num;
+        return lottoNumber;
     }
 
     public int getLottoNum() {
