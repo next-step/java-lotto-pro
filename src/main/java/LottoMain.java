a22ffcc52d275b8660d9e729232905f0274721d9
mansik <90mansin@gmail.com>
2022-11-08 02:43:09 +0900

    fix: 수동 로또가 0건 입력 됬을 경우 문구 노출 버그 수정

--- src/main/java/LottoMain.java
+++ src/main/java/LottoMain.java
@@ -13,17 +13,13 @@ public class LottoMain {
         int lottoTicketCount = lottoMain.getLottoTicketCount();
         //수동 구매 횟수 입력
         int manualLottoTicketCount = InputView.inputManualLottoCount();
-
         // 자동으로 발급된 로또 번호 저장
         Lottos lottos = lottoMain.purchaseLotto(lottoTicketCount, manualLottoTicketCount);
-
-
-
         // 당첨 비교
         WinLotto winLotto = new WinLotto(InputView.inputWinLottoNumbers(), new LottoNumber(InputView.inputBonusNumber()));
         WinReport winReport = new WinReport();
 
-        winReport.findWinner(winLotto,lottos);
+        winReport.findWinner(winLotto, lottos);
         OutputView.outputReportStart();
         // 결과 출력
         lottoMain.getResultPrintMessage(winReport, lottoTicketCount);
@@ -34,21 +30,23 @@ public class LottoMain {
         OutputView.outputProfit(winReport, lottoTicketCount);
     }
 
-    public int getLottoTicketCount(){
+    public int getLottoTicketCount() {
         Money money = new Money(InputView.inputReadPurchaseMoney());
         int lottoTicketCount = money.countOfLottoTicket();
 
 
         return lottoTicketCount;
     }
-    public Lottos purchaseLotto(int lottoTicketCount, int manualTicketCount){
+
+    public Lottos purchaseLotto(int lottoTicketCount, int manualTicketCount) {
         Lottos lottos = new Lottos(new ArrayList<>());
-        LottoMachine lottoMachine = new ManualLottoMachine();
 
-        lottos = InputView.inputManualLottos(manualTicketCount,lottos);
+        if (manualTicketCount > 0) {
+            lottos = InputView.inputManualLottos(manualTicketCount, lottos);
+        }
 
-        lottoMachine = new AutoLottoMachine();
-        lottoMachine.purchaseLotto(lottoTicketCount-manualTicketCount, lottos );
+        LottoMachine lottoMachine = new AutoLottoMachine();
+        lottoMachine.purchaseLotto(lottoTicketCount - manualTicketCount, lottos);
 
         OutputView.outputCountLottoTicket(lottoTicketCount, manualTicketCount);
         // 지난주 당첨번호 입력
