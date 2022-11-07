028dbcb2aa906c2281d804a8cd818382de7c908d
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-11-07 17:30:22 +0900

    refactor(LottoPurchaseAmountTest.java) : 로또 구매 금액 클래스 숫자 타입 변경

--- src/main/java/lotto/LottoPurchaseAmount.java
+++ src/main/java/lotto/LottoPurchaseAmount.java
@@ -4,8 +4,7 @@ public class LottoPurchaseAmount {
 
     private final int number;
 
-    public LottoPurchaseAmount(String stringNumber) {
-        int number = convertNumber(stringNumber);
+    public LottoPurchaseAmount(int number) {
         this.number = number;
         validateMultiple(number);
     }
@@ -14,15 +13,5 @@ public class LottoPurchaseAmount {
         if (number % 1000 != 0) {
             throw new IllegalArgumentException("1000의 배수만 입력가능합니다.");
         }
-    }
-
-    private int convertNumber(String stringNumber) {
-        int number = 0;
-        try {
-            number = Integer.parseInt(stringNumber);
-        } catch (NumberFormatException e) {
-            throw new NumberFormatException("숫자만 입력 가능합니다.");
-        }
-        return number;
     }
 }
--- src/test/java/lotto/LottoPurchaseAmountTest.java
+++ src/test/java/lotto/LottoPurchaseAmountTest.java
@@ -10,19 +10,10 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 @DisplayName("로또 구매 금액")
 class LottoPurchaseAmountTest {
 
-    @DisplayName("숫자만 입력 가능하다.")
-    @ParameterizedTest
-    @ValueSource(strings = {"s"})
-    public void number(String number) {
-        assertThatThrownBy(() -> new LottoPurchaseAmount(number))
-                .isInstanceOf(NumberFormatException.class)
-                .hasMessageContaining("숫자만 입력 가능합니다.");
-    }
-
     @DisplayName("1000의 배수만 입력 가능하다")
     @ParameterizedTest
-    @ValueSource(strings = {"1001", "10"})
-    public void multiple(String number) {
+    @ValueSource(ints = {1001, 10})
+    public void multiple(int number) {
         assertThatThrownBy(() -> new LottoPurchaseAmount(number))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("1000의 배수만 입력가능합니다.");
@@ -30,8 +21,8 @@ class LottoPurchaseAmountTest {
 
     @DisplayName("로또 구매 금액을 생성한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"1000"})
-    public void constructor(String number) {
+    @ValueSource(ints = {1000})
+    public void constructor(int number) {
         assertThatNoException().isThrownBy(() -> new LottoPurchaseAmount(number));
     }
 }
