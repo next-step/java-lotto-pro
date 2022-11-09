52811fa74d02502a06bd50cb05a008a38ac2fb1c
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-11-10 08:36:18 +0900

    refactor(WinningMoney.java) : WinningMoney 노출여부 메서드로 추출

--- src/main/java/lotto/domain/WinningMoney.java
+++ src/main/java/lotto/domain/WinningMoney.java
@@ -8,25 +8,23 @@ import static java.util.Collections.singletonList;
 
 public enum WinningMoney {
 
-    NONE("0개 일치", 0, 0, asList(true, false), false),
-    THREE_MATCH("3개 일치", 5_000L, 3, asList(true, false), true),
-    FOUR_MATCH("4개 일치", 50_000L, 4, asList(true, false), true),
-    FIVE_MATCH("5개 일치", 1_500_000L, 5, singletonList(false), true),
-    FIVE_MATCH_AND_BONUS_BALL_MATCH("5개 일치, 보너스 볼 일치", 30_000_000, 5, singletonList(true), true),
-    SIX_MATCH("6개 일치", 2_000_000_000L, 6, asList(true, false), true);
+    NONE("0개 일치", 0, 0, asList(true, false)),
+    THREE_MATCH("3개 일치", 5_000L, 3, asList(true, false)),
+    FOUR_MATCH("4개 일치", 50_000L, 4, asList(true, false)),
+    FIVE_MATCH("5개 일치", 1_500_000L, 5, singletonList(false)),
+    FIVE_MATCH_AND_BONUS_BALL_MATCH("5개 일치, 보너스 볼 일치", 30_000_000, 5, singletonList(true)),
+    SIX_MATCH("6개 일치", 2_000_000_000L, 6, asList(true, false));
 
     private final int count;
     private final long money;
     private final String message;
     private final List<Boolean> isMatchBonusBalls;
-    private final boolean isShow;
 
-    WinningMoney(String message, long money, int count, List<Boolean> isMatchBonusBalls, boolean isShow) {
+    WinningMoney(String message, long money, int count, List<Boolean> isMatchBonusBalls) {
         this.message = message;
         this.money = money;
         this.count = count;
         this.isMatchBonusBalls = isMatchBonusBalls;
-        this.isShow = isShow;
     }
 
     public long getMoney() {
@@ -54,6 +52,6 @@ public enum WinningMoney {
     }
 
     public boolean isShow() {
-        return this.isShow;
+        return this.count >= 3;
     }
 }
