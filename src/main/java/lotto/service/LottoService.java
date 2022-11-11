de7bfee627a0f4d730d9e8ecdb7c454991f7a43f
msares <msares123@gamil.com>
2022-11-11 21:37:13 +0900

    feat(LottoService): 서비스 수동 구매 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -8,11 +8,17 @@ public class LottoService {
     private final LottoStore lottoStore = new LottoStore(new RandomLottoNumberGenerateStrategy());
 
     public void execute() {
-        LottoPurchaseAmount lottoPurchaseAmount = new LottoPurchaseAmount(InputProvider.purchaseAmount());
-        LottoTickets lottoTickets = lottoStore.buy((lottoPurchaseAmount.getQuantity()));
+        LottoTickets lottoTickets = buyTickets(new LottoPurchaseAmount(InputProvider.purchaseAmount()));
         OutputProvider.printLottoTicket(lottoTickets);
         WinningLottoNumber winningLottoNumber = new WinningLottoNumber(InputProvider.lottoNumbers(), InputProvider.bonusNumber());
         Rewards rewards = lottoTickets.check(winningLottoNumber);
         OutputProvider.printRewards(rewards);
+    }
+
+    private LottoTickets buyTickets(LottoPurchaseAmount lottoPurchaseAmount) {
+        LottoTickets manualLottoTickets = lottoStore.buy(InputProvider.manualLottoNumbers(InputProvider.manualLottoCount()));
+        LottoPurchaseAmount change = lottoPurchaseAmount.pay(manualLottoTickets.getTicketCount());
+        LottoTickets autoLottoTickets = lottoStore.buy((change.getQuantity()));
+        return manualLottoTickets.merge(autoLottoTickets);
     }
 }
