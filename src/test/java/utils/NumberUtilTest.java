57e7bd01ae7afd60918e4139b9c49aa981541dde
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-11-11 19:22:08 +0900

    test(NumberUtilTest.java) : 문자 배열 숫자 리스트 변환

--- src/test/java/utils/NumberUtilTest.java
+++ src/test/java/utils/NumberUtilTest.java
@@ -1,4 +1,28 @@
-import static org.junit.jupiter.api.Assertions.*;
+package utils;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("숫자유틸")
 class NumberUtilTest {
-  
+
+    @DisplayName("문자 숫자 리스트 변환")
+    @ParameterizedTest
+    @MethodSource("convert")
+    void convert(String[] text) {
+        assertThat(NumberUtil.numbers(text)).containsExactly(1, 2, 3, 4, 5, 6);
+    }
+
+    static Stream<Arguments> convert() {
+        return Stream.of(
+                Arguments.of((Object) new String[]{"1", "2", "3", "4", "5", "6"}, Arrays.asList(1, 2, 3, 4, 5, 6))
+        );
+    }
 }
