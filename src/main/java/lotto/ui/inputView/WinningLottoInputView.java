011668ea0863cb148d635f352b845b8d21674b79
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-11-11 17:54:40 +0900

    feat(WinningLottoInputView.java) : 수동으로 구매할 번호를 입력해 주세요.

--- src/main/java/lotto/ui/inputView/WinningLottoInputView.java
+++ src/main/java/lotto/ui/inputView/WinningLottoInputView.java
@@ -8,4 +8,9 @@ public class WinningLottoInputView {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         return new Scanner(System.in).nextLine();
     }
+
+    public static String readWinningLotto1() {
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+        return new Scanner(System.in).nextLine();
+    }
 }
