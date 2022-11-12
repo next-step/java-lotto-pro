6cb0137c0b7dcc1ac2abff25bbe6a886679baeae
지영진 <youngjin.jee@nhn.com>
2022-11-12 21:38:56 +0900

    refactor: Rank가 도메인 패키지로 이동하면서 출력 관련 로직 제거
    * 출력 문장 상수화

--- src/main/java/study/step4/models/Rank.java
+++ src/main/java/study/step4/models/Rank.java
@@ -3,25 +3,21 @@ package study.step4.models;
 import java.util.Arrays;
 
 public enum Rank {
-    NONE(0, 0, false, false, ""),
-    FIFTH(3, 5_000, false, true, "3개 일치 (5000원)"),
-    FOURTH(4, 50_000, false, true, "4개 일치 (50000원)"),
-    THIRD(5, 1_500_000, false, true, "5개 일치 (1500000원)"),
-    SECOND(5, 30_000_000, true, true, "5개 일치, 보너스 볼 일치(30000000원)"),
-    FIRST(6, 2_000_000_000, false, true, "6개 일치 (2000000000원)");
+    NONE(0, 0, false),
+    FIFTH(3, 5_000, false),
+    FOURTH(4, 50_000, false),
+    THIRD(5, 1_500_000, false),
+    SECOND(5, 30_000_000, true),
+    FIRST(6, 2_000_000_000, false);
 
     private final int numberOfMatching;
     private final int reward;
     private final boolean hasBonusBallNumber;
-    private final boolean show;
-    private final String message;
 
-    Rank(int numberOfMatching, int reward, boolean hasBonusBallNumber, boolean show, String message) {
+    Rank(int numberOfMatching, int reward, boolean hasBonusBallNumber) {
         this.numberOfMatching = numberOfMatching;
         this.reward = reward;
         this.hasBonusBallNumber = hasBonusBallNumber;
-        this.show = show;
-        this.message = message;
     }
 
     public int getNumberOfMatching() {
@@ -30,14 +26,6 @@ public enum Rank {
 
     public int getReward() {
         return reward;
-    }
-
-    public boolean isShow() {
-        return show;
-    }
-
-    public String getMessage() {
-        return message;
     }
 
     public boolean hasBonusBallNumber() {
@@ -60,5 +48,9 @@ public enum Rank {
 
     private boolean hasNumberOfMatching(int numberOfMatching) {
         return this.numberOfMatching == numberOfMatching;
+    }
+
+    public boolean isWinningRank() {
+        return NONE != this;
     }
 }
--- src/main/java/study/step4/views/ResultView.java
+++ src/main/java/study/step4/views/ResultView.java
@@ -6,30 +6,41 @@ import study.step4.models.Money;
 import study.step4.models.Winners;
 
 public class ResultView {
+    private static final String PURCHASE_COUNT_DESCRIPTION = "%d개를 구매했습니다.\n";
+    private static final String STATISTICS_DESCRIPTION = "당첨 통계\n---------";
+    private static final String BONUS_BALL_MATCHING_DESCRIPTION = "보너스 볼 일치";
+    private static final String MATCHING_RESULT_DESCRIPTION = "%d개 일치 %s(%d원)- %d개\n";
+    private static final int PROFIT_STANDARD = 1;
+    private static final String TOTAL_EARNING_RATE_DESCRIPTION = "총 수익률은 %.2f입니다.";
+    private static final String UNDER_ONE_DESCRIPTION = "(기준이 1이기 때문에 결과적으로 손해라는 의미임)";
+
     public static void printLottos(Lottos lottos) {
-        System.out.printf("%d개를 구매했습니다.\n", lottos.size());
+        System.out.printf(PURCHASE_COUNT_DESCRIPTION, lottos.size());
         lottos.printAll();
     }
 
     public static void printLottoWinners(Winners winners) {
-        System.out.println("당첨 통계");
-        System.out.println("---------");
+        System.out.println(STATISTICS_DESCRIPTION);
         for (Rank rank : Rank.values()) {
             printMatchingResult(winners, rank);
         }
     }
 
     private static void printMatchingResult(Winners winners, Rank rank) {
-        if (rank.isShow()) {
-            System.out.printf("%s- %d개\n"
-                    , rank.getMessage()
+        if (rank.isWinningRank())
+            System.out.printf(MATCHING_RESULT_DESCRIPTION
+                    , rank.getNumberOfMatching()
+                    , rank.hasBonusBallNumber() ? BONUS_BALL_MATCHING_DESCRIPTION : ""
+                    , rank.getReward()
                     , winners.numberOfRankers(rank));
-        }
     }
 
     public static void printEarningRate(Winners winners, Money money) {
-        System.out.println(
-                String.format("총 수익률은 %.2f입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)"
-                        , winners.earningRate(money)));
+        String description = TOTAL_EARNING_RATE_DESCRIPTION;
+        double earningRate = winners.earningRate(money);
+        if (earningRate < PROFIT_STANDARD) {
+            description += UNDER_ONE_DESCRIPTION;
+        }
+        System.out.printf(description + "\n", earningRate);
     }
 }
