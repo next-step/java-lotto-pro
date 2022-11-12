5427d24ffbf241233757636e2ea7c8db09645501
지영진 <youngjin.jee@nhn.com>
2022-11-12 23:39:57 +0900

    refactor: Rank 클래스 당첨 규칙 관리 수정
    * BiFunction으로 매칭 정보와 보너스 볼 정보를 이용해서 당첨 규칙을 관리하도록 수정

--- src/main/java/study/step4/models/Rank.java
+++ src/main/java/study/step4/models/Rank.java
@@ -1,23 +1,26 @@
 package study.step4.models;
 
 import java.util.Arrays;
+import java.util.function.BiFunction;
 
 public enum Rank {
-    NONE(0, 0, false),
-    FIFTH(3, 5_000, false),
-    FOURTH(4, 50_000, false),
-    THIRD(5, 1_500_000, false),
-    SECOND(5, 30_000_000, true),
-    FIRST(6, 2_000_000_000, false);
+    NONE(0, 0, false, (numberOfMatching, hasBonusBallNumber) -> numberOfMatching == 0),
+    FIFTH(3, 5_000, false, (numberOfMatching, hasBonusBallNumber) -> numberOfMatching == 3),
+    FOURTH(4, 50_000, false, (numberOfMatching, hasBonusBallNumber) -> numberOfMatching == 4),
+    THIRD(5, 1_500_000, false, (numberOfMatching, hasBonusBallNumber) -> numberOfMatching == 5 && !hasBonusBallNumber),
+    SECOND(5, 30_000_000, true, (numberOfMatching, hasBonusBallNumber) -> numberOfMatching == 5 && hasBonusBallNumber),
+    FIRST(6, 2_000_000_000, false, (numberOfMatching, hasBonusBallNumber) -> numberOfMatching == 6);
 
     private final int numberOfMatching;
     private final int reward;
     private final boolean hasBonusBallNumber;
+    private final BiFunction<Integer, Boolean, Boolean> match;
 
-    Rank(int numberOfMatching, int reward, boolean hasBonusBallNumber) {
+    Rank(int numberOfMatching, int reward, boolean hasBonusBallNumber, BiFunction<Integer, Boolean, Boolean> match) {
         this.numberOfMatching = numberOfMatching;
         this.reward = reward;
         this.hasBonusBallNumber = hasBonusBallNumber;
+        this.match = match;
     }
 
     public int getNumberOfMatching() {
@@ -33,21 +36,10 @@ public enum Rank {
     }
 
     public static Rank valueOf(int numberOfMatching, boolean hasBonusBallNumber) {
-        if (numberOfMatching == SECOND.getNumberOfMatching()) {
-            return Arrays.stream(Rank.values())
-                    .filter(rank -> rank.hasNumberOfMatching(numberOfMatching))
-                    .filter(rank -> rank.hasBonusBallNumber() == hasBonusBallNumber)
-                    .findFirst()
-                    .orElse(NONE);
-        }
         return Arrays.stream(Rank.values())
-                .filter(rank -> rank.hasNumberOfMatching(numberOfMatching))
+                .filter(rank -> rank.match.apply(numberOfMatching, hasBonusBallNumber))
                 .findFirst()
                 .orElse(Rank.NONE);
-    }
-
-    private boolean hasNumberOfMatching(int numberOfMatching) {
-        return this.numberOfMatching == numberOfMatching;
     }
 
     public boolean isWinningRank() {
