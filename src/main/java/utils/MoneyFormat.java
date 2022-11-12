c539dcf4d8cb26eb568a88b7d73db06bae6b3cd1
tlaqk229 <tlaqk229@gmail.com>
2022-11-12 20:09:11 +0900

    fix(MoneyFromat): Formatter final선언 및 인스턴스 생성 불가처리

--- src/main/java/utils/MoneyFormat.java
+++ src/main/java/utils/MoneyFormat.java
@@ -2,9 +2,11 @@ package utils;
 
 import java.text.DecimalFormat;
 
-public class MoneyFormat {
+public final class MoneyFormat {
 
     private static final DecimalFormat DF = new DecimalFormat("###,###");
+
+    private MoneyFormat() {}
 
     public static String getMoneyExpression(long money) {
         return DF.format(money);
