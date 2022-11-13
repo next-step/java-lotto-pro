df91371519f7f98c4308d80c21d4bb49e36af760
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-11-13 21:56:08 +0900

    test(LottosMapTest.java) : 로또 집합을 저장한다.

--- src/main/java/lotto/domain/LottosMap.java
+++ src/main/java/lotto/domain/LottosMap.java
@@ -5,21 +5,13 @@ import java.util.Map;
 
 public class LottosMap {
 
-    private final Map<String, Lottos> lottos = new HashMap<>();
+    private final Map<LottoType, Lottos> lottos = new HashMap<>();
 
-    public void putManualLottos(Lottos lottos) {
-        this.lottos.put("manual", lottos);
+    public void put(LottoType type, Lottos lottos) {
+        this.lottos.put(type, lottos);
     }
 
-    public Lottos getManualLottos() {
-        return this.lottos.get("manual");
-    }
-
-    public void putAutoLottos(Lottos lottos) {
-        this.lottos.put("auto", lottos);
-    }
-
-    public Lottos getAutoLottos() {
-        return this.lottos.get("auto");
+    public Lottos getLottos(LottoType type) {
+        return this.lottos.get(type);
     }
 }
--- src/test/java/lotto/domain/LottosMapTest.java
+++ src/test/java/lotto/domain/LottosMapTest.java
@@ -2,26 +2,22 @@ package lotto.domain;
 
 import lotto.fixture.LottosFixture;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("로또맵")
 class LottosMapTest {
 
-    @DisplayName("수동 로또 집합을 저장한다.")
-    @Test
-    void putManualLottos() {
-        LottosMap lottosMap = new LottosMap();
-        lottosMap.putManualLottos(LottosFixture.lottos());
-        assertThat(lottosMap.getManualLottos().getLottos()).hasSize(1);
-    }
+    @DisplayName("로또 집합을 저장한다.")
+    @ParameterizedTest
+    @CsvSource({"AUTO", "MANUAL"})
+    void putManualLottos(LottoType type) {
 
-    @DisplayName("자동 로또 집합을 저장한다.")
-    @Test
-    void putAutoLottos() {
         LottosMap lottosMap = new LottosMap();
-        lottosMap.putAutoLottos(LottosFixture.lottos());
-        assertThat(lottosMap.getAutoLottos().getLottos()).hasSize(1);
+        lottosMap.put(type, LottosFixture.lottos());
+
+        assertThat(lottosMap.getLottos(type).getLottos()).hasSize(1);
     }
 }
