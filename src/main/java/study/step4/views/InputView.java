fcdd5a407bc1f8d452b4082248d9bc7800092a6c
지영진 <youngjin.jee@nhn.com>
2022-11-17 01:29:54 +0900

    feat: 수동 구매 관련 입력 부 추가

--- src/main/java/study/step4/views/InputView.java
+++ src/main/java/study/step4/views/InputView.java
@@ -1,5 +1,7 @@
 package study.step4.views;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
@@ -18,5 +20,19 @@ public class InputView {
     public static String inputBonusBall() {
         System.out.println("보너스 볼을 입력해 주세요.");
         return scanner.nextLine();
+    }
+
+    public static int inputNumberOfManualLotto() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    public static List<String> inputManualLottoNumber(int numberOfManualLotto) {
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+        List<String> manualLottos = new ArrayList<>();
+        while (numberOfManualLotto-- > 0) {
+            manualLottos.add(scanner.nextLine());
+        }
+        return manualLottos;
     }
 }
