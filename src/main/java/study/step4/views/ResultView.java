841d00c79e2847a2dd135e5f55203e99bec8e8ac
지영진 <youngjin.jee@nhn.com>
2022-11-17 01:30:08 +0900

    feat: 수동 구매 관련 출력 부 추가

--- src/main/java/study/step4/views/ResultView.java
+++ src/main/java/study/step4/views/ResultView.java
@@ -5,7 +5,7 @@ import study.step4.models.*;
 import java.util.Collections;
 
 public class ResultView {
-    private static final String PURCHASE_COUNT_DESCRIPTION = "%d개를 구매했습니다.\n";
+    private static final String PURCHASE_COUNT_DESCRIPTION = "수동으로 %d장, 자동으로 %d개를 구매했습니다.\n";
     private static final String STATISTICS_DESCRIPTION = "당첨 통계\n---------";
     private static final String BONUS_BALL_MATCHING_DESCRIPTION = "보너스 볼 일치";
     private static final String MATCHING_RESULT_DESCRIPTION = "%d개 일치 %s(%d원)- %d개\n";
@@ -13,8 +13,13 @@ public class ResultView {
     private static final String TOTAL_EARNING_RATE_DESCRIPTION = "총 수익률은 %.2f입니다.";
     private static final String UNDER_ONE_DESCRIPTION = "(기준이 1이기 때문에 결과적으로 손해라는 의미임)";
 
-    public static void printLottos(Lottos lottos) {
-        System.out.printf(PURCHASE_COUNT_DESCRIPTION, lottos.size());
+    public static void printIntegratedLottos(IntegratedLottos integratedLottos) {
+        System.out.printf(PURCHASE_COUNT_DESCRIPTION, integratedLottos.manualSize(), integratedLottos.autoSize());
+        printLottos(integratedLottos.getManualLottos());
+        printLottos(integratedLottos.getAutoLottos());
+    }
+
+    private static void printLottos(Lottos lottos) {
         for (Lotto lotto : lottos.getLottos()) {
             Collections.sort(lotto.getLottoNumbers(), LottoNumber::compare);
             System.out.println(lotto);
