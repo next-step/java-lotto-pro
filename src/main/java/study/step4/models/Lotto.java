4853cd37e8e6552597fc2678d25bb270336fff2c
지영진 <youngjin.jee@nhn.com>
2022-11-17 01:20:03 +0900

    refactor: Lottos에서 인자로 넘어온 Lotto를 포함하는지 확인하기 위한 equals 메서드 추가

--- src/main/java/study/step4/models/Lotto.java
+++ src/main/java/study/step4/models/Lotto.java
@@ -4,6 +4,7 @@ import study.step4.exception.LottoInvalidSizeException;
 import study.step4.helper.LottoStringParser;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Lotto {
     public static final int LOTTO_SIZE = 6;
@@ -42,5 +43,18 @@ public class Lotto {
 
     public boolean contains(LottoNumber bonusBall) {
         return lottoNumbers.contains(bonusBall);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Lotto lotto = (Lotto) o;
+        return Objects.equals(lottoNumbers, lotto.lottoNumbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoNumbers);
     }
 }
