c2400ca8cc626b7c9904fc340637e38ba89513cf
지영진 <youngjin.jee@nhn.com>
2022-11-17 01:25:40 +0900

    feat: 통합 로또(수동, 자동) 클래스 생성 및 테스트 추가, 수정

--- src/test/java/study/step4/models/LottosTest.java
+++ src/test/java/study/step4/models/LottosTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import study.step4.exception.LottoNumberDuplicateException;
 
 import java.util.Arrays;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -12,25 +13,23 @@ public class LottosTest {
     @Test
     void 당첨_확인하기() {
         WinningLotto winLottoNumbers = new WinningLotto(new Lotto("4, 5, 6, 7, 8, 9"), new LottoNumber("3"));
-        Lottos autoLottos = new Lottos(Arrays.asList(new Lotto("1, 2, 3, 4, 5, 6")
+        Lottos lottos = new Lottos(Arrays.asList(
+                new Lotto("1, 2, 3, 4, 5, 6")
                 , new Lotto("2, 3, 4, 5, 6, 7")
                 , new Lotto("2, 4, 5, 6, 7, 8")
                 , new Lotto("4, 5, 6, 7, 10, 11")
                 , new Lotto("3, 4, 5, 6, 7, 8")));
-        Lottos manualLottos = new Lottos(Arrays.asList(new Lotto("11, 12, 13, 14, 15, 16")
-                , new Lotto("12, 13, 14, 15, 16, 17")));
-        IntegratedLottos integratedLottos = new IntegratedLottos(manualLottos, autoLottos);
 
-        Winners winners = new Winners(integratedLottos.findWinningLottos(winLottoNumbers));
+        Map<Rank, Integer> winners = lottos.findWinningLottos(winLottoNumbers);
 
         // 4, 5, 6
-        assertThat(winners.numberOfRankers(Rank.FIFTH)).isEqualTo(1);
+        assertThat(winners.get(Rank.FIFTH)).isEqualTo(1);
         // 4, 5, 6, 7
-        assertThat(winners.numberOfRankers(Rank.FOURTH)).isEqualTo(2);
+        assertThat(winners.get(Rank.FOURTH)).isEqualTo(2);
         // 4, 5, 6, 7, 8
-        assertThat(winners.numberOfRankers(Rank.THIRD)).isEqualTo(1);
+        assertThat(winners.get(Rank.THIRD)).isEqualTo(1);
         // 4, 5, 6, 7, 8 bonus 3
-        assertThat(winners.numberOfRankers(Rank.SECOND)).isEqualTo(1);
+        assertThat(winners.get(Rank.SECOND)).isEqualTo(1);
     }
 
     @Test
