6ecc833a8d866fc881683092e56a1b7b19c2280c
지영진 <youngjin.jee@nhn.com>
2022-11-17 01:30:23 +0900

    feat: 애플리케이션 수동 관련 부분 추가

--- src/main/java/study/step4/LottoApplication.java
+++ src/main/java/study/step4/LottoApplication.java
@@ -5,20 +5,24 @@ import study.step4.models.*;
 import study.step4.views.InputView;
 import study.step4.views.ResultView;
 
+import java.util.List;
+
 public class LottoApplication {
     public static void main(String[] args) {
         Money money = new Money(InputView.inputMoney());
 
         int numberOfManualLotto = InputView.inputNumberOfManualLotto();
         money.validateEnoughToBuyLotto(numberOfManualLotto);
-        Lottos lottos = LottoMaker.makeLottos(money.numberAvailable());
-        ResultView.printLottos(lottos);
+
+        List<String> manualLottosString = InputView.inputManualLottoNumber(numberOfManualLotto);
+        IntegratedLottos integratedLottos = LottoMaker.makeLottos(money.numberAvailable(), manualLottosString);
+        ResultView.printIntegratedLottos(integratedLottos);
 
         Lotto winningLottoNumbers = new Lotto(InputView.inputWinningNumbers());
         LottoNumber bonusBall = new LottoNumber(InputView.inputBonusBall());
         WinningLotto winningLotto = new WinningLotto(winningLottoNumbers, bonusBall);
 
-        Winners winners = new Winners(lottos.findWinningLottos(winningLotto));
+        Winners winners = new Winners(integratedLottos.findWinningLottos(winningLotto));
         ResultView.printLottoWinners(winners);
         ResultView.printEarningRate(winners, money);
     }
