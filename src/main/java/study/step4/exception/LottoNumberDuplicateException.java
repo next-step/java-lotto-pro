e4f0da4e0f90ea126a00855906c3b97d419757f1
지영진 <youngjin.jee@nhn.com>
2022-11-17 01:18:58 +0900

    refactor: Lottos 내부에서 중복된 로또가 있는지 유효성 검사 추가(빠진 exception class) 추가

---
+++ src/main/java/study/step4/exception/LottoNumberDuplicateException.java
@@ -0,0 +1,7 @@
+package study.step4.exception;
+
+public class LottoNumberDuplicateException extends RuntimeException {
+    public LottoNumberDuplicateException(String message) {
+        super(message);
+    }
+}
