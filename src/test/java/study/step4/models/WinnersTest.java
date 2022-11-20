9717686fe6a581fc0d4bba05fba00e17af04b9e6
지영진 <youngjin.jee@nhn.com>
2022-11-20 21:13:33 +0900

    refactor: IntegratedLotto 삭제하면서 영향 있는 곳 수정

--- src/test/java/study/step4/models/WinnersTest.java
+++ src/test/java/study/step4/models/WinnersTest.java
@@ -11,16 +11,12 @@ public class WinnersTest {
     @Test
     void 수익률_계산하기() {
         WinningLotto winningLotto = new WinningLotto(new Lotto("4, 5, 6, 7, 8, 9"), new LottoNumber("10"));
-        Lottos autoLottos = new Lottos(Arrays.asList(new Lotto("1, 2, 3, 4, 5, 6")
+        Lottos lottos = new Lottos(Arrays.asList(new Lotto("1, 2, 3, 4, 5, 6")
                 , new Lotto("2, 3, 4, 5, 6, 7")
                 , new Lotto("3, 4, 5, 6, 7, 8")));
-        Lottos manualLottos = new Lottos(Arrays.asList(new Lotto("11, 12, 13, 14, 15, 16")
-                , new Lotto("12, 13, 14, 15, 16, 17")));
 
-        IntegratedLottos integratedLottos = new IntegratedLottos(manualLottos, autoLottos);
-        Winners winners = new Winners(integratedLottos.findWinningLottos(winningLotto));
-
-        Money inputMoney = new Money(String.valueOf(integratedLottos.autoSize() * PRICE_PER_LOTTO));
+        Winners winners = new Winners(winningLotto.findWinningLottos(lottos));
+        Money inputMoney = new Money(String.valueOf(lottos.size() * PRICE_PER_LOTTO));
         double earningRate = winners.earningRate(inputMoney);
 
         // 총 상금 예상 금액
